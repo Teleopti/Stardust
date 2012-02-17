e7511668bd49beb7c881051193d2a33404ff0d93
tamasb <none@none>
2012-02-17 17:22:16 +0100

    add them too

---
+++ Domain/Optimization/SchedulingOptionsSyncronizer.cs
@@ -0,0 +1,22 @@
+﻿using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Optimization
+{
+    public interface ISchedulingOptionsSyncronizer
+    {
+        void SyncronizeSchedulingOption(
+            IOptimizationPreferences optimizationPreferences,
+            ISchedulingOptions schedulingOptions);
+    }
+
+    public class SchedulingOptionsSyncronizer : ISchedulingOptionsSyncronizer
+    {
+        public void SyncronizeSchedulingOption(
+            IOptimizationPreferences optimizationPreferences, 
+            ISchedulingOptions schedulingOptions)
+        {
+            
+        }
+    }
+}
---
+++ DomainTest/Optimization/SchedulingOptionsCreatorTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization
+{
+    [TestFixture]
+    public class SchedulingOptionsCreatorTest
+    {
+        private SchedulingOptionsSyncronizer _target;
+        private IOptimizationPreferences _optimizationPreferences;
+
+        [SetUp]
+        public void Setup()
+        {
+            _optimizationPreferences = new OptimizationPreferences();
+            _target = new SchedulingOptionsSyncronizer();
+        }
+
+        [Test]
+        public void ShouldCreateNewSchedulingOption()
+        {
+            ISchedulingOptions result = _target.CreateSchedulingOption(_optimizationPreferences);
+            Assert.IsNotNull(result);
+        }
+    }
+}
