5393254c20db5ac210c04d5f8fa747961c0a991e
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-02-20 14:57:18 +0100

    Minor refact Func<T> to IResolve<T> in mapping profiles

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Preference/Mapping/PreferenceDomainDataMappingProfile.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Preference/Mapping/PreferenceDomainDataMappingProfile.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Linq;
 using AutoMapper;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Common.DataProvider;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Preference.DataProvider;
+using Teleopti.Ccc.Web.Core.IoC;
 using Teleopti.Ccc.Web.Core.RequestContext;
 using Teleopti.Interfaces.Domain;
 
@@ -10,15 +10,17 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Preference.Mapping
 {
 	public class PreferenceDomainDataMappingProfile : Profile
 	{
-		private readonly Func<IVirtualSchedulePeriodProvider> _virtualSchedulePeriodProvider;
-		private readonly Func<IPreferenceProvider> _preferenceProvider;
-		private readonly Func<ILoggedOnUser> _loggedOnUser;
-		private readonly Func<IWorkTimeMinMaxCalculator> _workTimeMinMaxCalculator;
+		private readonly IResolve<IVirtualSchedulePeriodProvider> _virtualSchedulePeriodProvider;
+		private readonly IResolve<IPreferenceProvider> _preferenceProvider;
+		private readonly IResolve<ILoggedOnUser> _loggedOnUser;
+		private readonly IResolve<IWorkTimeMinMaxCalculator> _workTimeMinMaxCalculator;
 
-		public PreferenceDomainDataMappingProfile(Func<IVirtualSchedulePeriodProvider> virtualSchedulePeriodProvider,
-																Func<IPreferenceProvider> preferenceProvider,
-																Func<ILoggedOnUser> loggedOnUser,
-																Func<IWorkTimeMinMaxCalculator> workTimeMinMaxCalculator)
+		public PreferenceDomainDataMappingProfile(
+			IResolve<IVirtualSchedulePeriodProvider> virtualSchedulePeriodProvider,
+			IResolve<IPreferenceProvider> preferenceProvider,
+			IResolve<ILoggedOnUser> loggedOnUser,
+			IResolve<IWorkTimeMinMaxCalculator> workTimeMinMaxCalculator
+			)
 		{
 			_virtualSchedulePeriodProvider = virtualSchedulePeriodProvider;
 			_preferenceProvider = preferenceProvider;
@@ -32,25 +34,25 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Preference.Mapping
 
 			CreateMap<DateOnly, PreferenceDomainData>()
 				.ConvertUsing(s =>
-								{
-									var domainData = new PreferenceDomainData
-														{
-															SelectedDate = s,
-															Period = _virtualSchedulePeriodProvider().GetCurrentOrNextVirtualPeriodForDate(s),
-															WorkflowControlSet = _loggedOnUser().CurrentUser().WorkflowControlSet
-														};
-									domainData.PreferenceDays = _preferenceProvider().GetPreferencesForPeriod(domainData.Period);
-									domainData.WorkTimeMinMax = from d in domainData.Period.DayCollection()
-									                            let personPeriod = _loggedOnUser().CurrentUser().PersonPeriods(new DateOnlyPeriod(d, d)).
-									                            	FirstOrDefault()
-									                            where personPeriod != null
-									                            let ruleSetBag =
-																	personPeriod.RuleSetBag
-																where ruleSetBag != null
-																select
-																	new WorkTimeMinMaxDomainData(_workTimeMinMaxCalculator().WorkTimeMinMax(ruleSetBag, d), d);
-									return domainData;
-								});
+				              	{
+				              		var domainData = new PreferenceDomainData
+				              		                 	{
+				              		                 		SelectedDate = s,
+				              		                 		Period = _virtualSchedulePeriodProvider.Invoke().GetCurrentOrNextVirtualPeriodForDate(s),
+				              		                 		WorkflowControlSet = _loggedOnUser.Invoke().CurrentUser().WorkflowControlSet
+				              		                 	};
+				              		domainData.PreferenceDays = _preferenceProvider.Invoke().GetPreferencesForPeriod(domainData.Period);
+				              		domainData.WorkTimeMinMax = from d in domainData.Period.DayCollection()
+				              		                            let personPeriod = _loggedOnUser.Invoke().CurrentUser().PersonPeriods(new DateOnlyPeriod(d, d)).
+				              		                            	FirstOrDefault()
+				              		                            where personPeriod != null
+				              		                            let ruleSetBag =
+				              		                            	personPeriod.RuleSetBag
+				              		                            where ruleSetBag != null
+				              		                            select
+				              		                            	new WorkTimeMinMaxDomainData(_workTimeMinMaxCalculator.Invoke().WorkTimeMinMax(ruleSetBag, d), d);
+				              		return domainData;
+				              	});
 		}
 	}
 }
