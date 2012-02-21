2bc331cc11ef05d5cddf264c84033bd225b790b0
Mattias Engblom <mattias.engblom@teleopti.com>
2012-02-21 09:07:29 +0100

    Fix display of full day absence on day off

---
+++ Domain/Scheduling/Assignment/HasDayOffUnderFullDayAbsence.cs
@@ -0,0 +1,29 @@
+using System.Collections.ObjectModel;
+using System.Linq;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.Assignment
+{
+	public class HasDayOffUnderFullDayAbsence
+	{
+		private readonly IScheduleDay _scheduleDay;
+		private SchedulePartView _significantPart;
+		private ReadOnlyCollection<IPersonDayOff> _dayOffCollection;
+		private HasDayOffDefinition _hasDayOff;
+
+
+		public HasDayOffUnderFullDayAbsence(IScheduleDay scheduleDay)
+		{
+			_scheduleDay = scheduleDay;
+			_significantPart = _scheduleDay.SignificantPartForDisplay();
+			_dayOffCollection = _scheduleDay.PersonDayOffCollection();
+			_hasDayOff = new HasDayOffDefinition(_scheduleDay);
+		}
+
+		public bool HasDayOff()
+		{
+			return (((_dayOffCollection != null && _dayOffCollection.ToList().Count > 0) || _hasDayOff.IsDayOff()) &&
+			        _significantPart == SchedulePartView.FullDayAbsence);
+		}
+	}
+}
