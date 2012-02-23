5c03cd157c8eb74cea61f482527588fefd0c68c0
David Jonsson <david.jonsson@teleopti.com>
2012-02-23 10:50:24 +0100

    Remove MSbuild imports from NightlyBuild

--- ccnet/BuildOnLocalPC.bat
+++ ccnet/BuildOnLocalPC.bat
@@ -2,8 +2,8 @@
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-7%
 SET Raptor2=ccnet\Raptor2.proj
+SET CCNetProject=
 SET NightlyBuild=ccnet\NightlyBuild.proj
-SET CCNetProject=MyLocalProject
 SET CCNetWorkingDirectory=%ROOTDIR%
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 
@@ -18,13 +18,18 @@ ECHO %CCNetWorkingDirectory%
 ::Select build type
 CHOICE /C rn /M "Do you want to run (r)aptor2 or (n)ightlyBuild"
 IF ERRORLEVEL 1 SET MsbuildProj=%Raptor2%
-IF ERRORLEVEL 2 SET MsbuildProj=%NightlyBuild%
+IF ERRORLEVEL 2 (
+SET MsbuildProj=%NightlyBuild%
+SET CCNetProject=NightlyBuild
+)
 ECHO.
 
 ::Apply special project name?
-ECHO Some special CCNET projects names (NightlyBuild,RaptorMain) will provide
-ECHO extended test scenarios (To run standard tests leave blank)
+IF "CCNetProject"=="" (
+ECHO Some special CCNET projects names ^(NightlyBuild,RaptorMain^) will provide
+ECHO extended test scenarios ^(To run standard tests leave blank^)
 SET /P CCNetProject=CCNetProject: 
+)
 
 ::Run Build
 ECHO "%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProj%"
