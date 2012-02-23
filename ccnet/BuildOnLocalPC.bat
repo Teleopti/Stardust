1724959791edee82311ef8757e283467f8f295a7
David Jonsson <david.jonsson@teleopti.com>
2012-02-23 23:12:53 +0100

    Adding HG revert in BuildOnLocalPC.bat

--- ccnet/BuildOnLocalPC.bat
+++ ccnet/BuildOnLocalPC.bat
@@ -2,12 +2,13 @@
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-7%
 SET Raptor2=ccnet\Raptor2.proj
-SET CCNetProject=none
+SET DefaultCCNetProject=none
+SET CCNetProject=%DefaultCCNetProject%
 SET NightlyBuild=ccnet\NightlyBuild.proj
 SET CCNetWorkingDirectory=%ROOTDIR%
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 
-::register ncover licens locally ^(we only have two ...^)
+::register ncover licens locally ^(we only have two lics on Teleopti...^)
 CD "%CCNetWorkingDirectory%\ccnet\NCover64"
 NCover.Registration.exe //License NC3CMPLIC.lic
 
@@ -23,18 +24,27 @@ SET MsbuildProj=%NightlyBuild%
 SET CCNetProject=NightlyBuild
 )
 ECHO.
-ECHO 
+
 ::Apply special project name?
-IF "%CCNetProject%"=="" (
+IF "%CCNetProject%"=="%DefaultCCNetProject%" (
 ECHO Some special CCNET projects names will provide extended test scenarios.
-ECHO PBI15494, NightlyBuild and BuildMSIxxxxxx
+ECHO PBI15494, NightlyBuild
 ECHO To run standard tests leave blank
 SET /P CCNetProject=CCNetProject:
 )
-
+ECHO %CCNetProject%
+PAUSE
 ::Run Build
 ECHO "%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProj%"
 "%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProj%"
 ECHO.
+ECHO reverting updated checked in config files ...
+hg revert -C "%ROOTDIR%\ccnet\Infratest.ini"
+hg revert -C "%ROOTDIR%\ccnet\CruiseControl.config"
+hg revert -C "%ROOTDIR%\ccnet\SetupTestWeb\Teleopti.Ccc.WebBehaviorTest.dll.config"
+hg revert -C "%ROOTDIR%\InfrastructureTest\Teleopti.Ccc.InfrastructureTest.dll.config"
+hg revert -C "%ROOTDIR%\Teleopti.Ccc.Web\Teleopti.Ccc.WebBehaviorTest\App.config"
+hg revert -C "%ROOTDIR%\Teleopti.Ccc.ApplicationConfigTest\Teleopti.Ccc.ApplicationConfigTest.dll.Config"
+
 ECHO Done!
 PAUSE
