8273e6efa88ce6090c1ad841e1ad26b8fe5ae9a5
Jonas Nordh <jonas.nordh@teleopti.com>
2012-02-27 15:59:26 +0100

    Fxcop issues

--- Teleopti.Analytics/WebPortal/Teleopti.Analytics.Portal/Teleopti.Analytics.Portal/PmContainer.aspx.designer.cs
+++ Teleopti.Analytics/WebPortal/Teleopti.Analytics.Portal/Teleopti.Analytics.Portal/PmContainer.aspx.designer.cs
@@ -13,12 +13,12 @@ namespace Teleopti.Analytics.Portal {
     public partial class PmContainer {
         
         /// <summary>
-        /// iframePmContainer control.
+        /// ContainerControl control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl iframePmContainer;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl ContainerControl;
     }
 }
