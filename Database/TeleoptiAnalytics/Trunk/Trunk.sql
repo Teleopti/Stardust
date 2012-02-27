42285cff6e8988a109a4b03de4851e0feb71dd2f
David Jonsson <david.jonsson@teleopti.com>
2012-02-27 11:08:29 +0100

    Adding Azure-test to RaptorMain, this checkin should fail!

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -32,16 +32,194 @@ IF NOT EXISTS (SELECT 1 FROM [mart].[etl_maintenance_configuration] WHERE config
 IF NOT EXISTS (SELECT 1 FROM [mart].[etl_maintenance_configuration] WHERE configuration_id = 13 AND [configuration_name] = 'YearsToKeepFactSchedulePreferences')
 	INSERT INTO [mart].[etl_maintenance_configuration] VALUES(13,'YearsToKeepFactSchedulePreferences',50)
 
+--rename existing tables and PKs
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF_report_visible]') AND type = 'D')
+BEGIN
+	ALTER TABLE [mart].[report] DROP CONSTRAINT [DF_report_visible]
+END
+EXEC dbo.sp_rename @objname = N'[mart].[report]', @newname = N'report_old', @objtype = N'OBJECT'
+EXEC sp_rename N'[mart].[report_old].[PK_report]', N'PK_report_old', N'INDEX'
+GO
+
+EXEC dbo.sp_rename @objname = N'[mart].[report_control]', @newname = N'report_control_old', @objtype = N'OBJECT'
+EXEC sp_rename N'[mart].[report_control_old].[PK_report_control]', N'PK_report_control_old', N'INDEX'
+GO
+EXEC dbo.sp_rename @objname = N'[mart].[report_control_collection]', @newname = N'report_control_collection_old', @objtype = N'OBJECT'
+EXEC sp_rename N'[mart].[report_control_collection_old].[PK_report_control_collection]', N'PK_report_control_collection_old', N'INDEX'
+GO
+EXEC dbo.sp_rename @objname = N'[mart].[report_user_setting]', @newname = N'report_user_setting_old', @objtype = N'OBJECT'
+EXEC sp_rename N'[mart].[report_user_setting_old].[PK_report_user_setting]', N'PK_report_user_setting_old', N'INDEX'
+GO
+
+
+CREATE TABLE [mart].[report_user_setting](
+	[ReportId] [uniqueidentifier] NOT NULL,
+	[person_code] [uniqueidentifier] NOT NULL,
+	[report_id] [int] NOT NULL,
+	[param_name] [varchar](50) NOT NULL,
+	[saved_name_id] [int] NOT NULL,
+	[control_setting] [varchar](max) NULL
+ CONSTRAINT [PK_report_user_setting] PRIMARY KEY CLUSTERED 
+(
+	person_code,
+	param_name,
+	saved_name_id,
+	ReportId
+)
+) 
+
+CREATE TABLE [mart].[report](
+	[Id] [uniqueidentifier] NOT NULL,
+	[report_id] [int] NOT NULL,
+	[control_collection_id] [int] NOT NULL,
+	[url] [nvarchar](500) NULL,
+	[target] [nvarchar](50) NULL,
+	[report_name] [nvarchar](500) NULL,
+	[report_name_resource_key] [nvarchar](50) NOT NULL,
+	[visible] [bit] NOT NULL,
+	[rpt_file_name] [varchar](100) NOT NULL,
+	[proc_name] [varchar](100) NOT NULL,
+	[help_key] [nvarchar](500) NULL,
+	[sub1_name] [varchar](50) NOT NULL,
+	[sub1_proc_name] [varchar](50) NOT NULL,
+	[sub2_name] [varchar](50) NOT NULL,
+	[sub2_proc_name] [varchar](50) NOT NULL,
+	[ControlCollectionId] [uniqueidentifier] NOT NULL
+ CONSTRAINT [PK_report] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+)
+
+CREATE TABLE [mart].[report_control](
+	[Id] [uniqueidentifier] NOT NULL,
+	[control_id] [int] NOT NULL,
+	[control_name] [varchar](50) NULL,
+	[fill_proc_name] [varchar](200) NOT NULL	
+ CONSTRAINT [PK_report_control] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+) 
 
 GO
-ALTER TABLE mart.report_control
-ADD Id uniqueidentifier null
+
+CREATE TABLE [mart].[report_control_collection](
+	[Id] [uniqueidentifier] NOT NULL,
+	[ControlId] [uniqueidentifier] NOT NULL,
+	[CollectionId] [uniqueidentifier] NOT NULL,
+	[control_collection_id] [int] NOT NULL,
+	[collection_id] [int] NOT NULL,
+	[print_order] [int] NOT NULL,
+	[control_id] [int] NOT NULL,
+	[default_value] [nvarchar](4000) NOT NULL,
+	[control_name_resource_key] [nvarchar](50) NOT NULL,
+	[fill_proc_param] [varchar](100) NULL,
+	[param_name] [varchar](50) NULL,
+	[depend_of1] [int] NULL,
+	[depend_of2] [int] NULL,
+	[depend_of3] [int] NULL,
+	[depend_of4] [int] NULL,
+	[DependOf1] [uniqueidentifier] NULL,
+	[DependOf2] [uniqueidentifier] NULL,
+	[DependOf3] [uniqueidentifier] NULL,
+	[DependOf4] [uniqueidentifier] NULL
+ CONSTRAINT [PK_report_control_collection] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+)
+
+
+-----------
+--mart.report
+-----------
+INSERT INTO [mart].[report]
+SELECT
+[Id] = NEWID(),
+[report_id] =[report_id],
+[control_collection_id]=[control_collection_id],
+[url]=[url],
+[target]=[target],
+[report_name]=[report_name],
+[report_name_resource_key]=[report_name_resource_key],
+[visible]=[visible],
+[rpt_file_name]=[rpt_file_name],
+[proc_name]=[proc_name],
+[help_key]=[help_key],
+[sub1_name]=[sub1_name],
+[sub1_proc_name]=[sub1_proc_name],
+[sub2_name]=[sub2_name],
+[sub2_proc_name]=[sub2_proc_name],
+[ControlCollectionId]=newid()
+FROM [mart].[report_old]
+
 GO
-UPDATE mart.report_control SET Id = NEWID()
+-- Set fixed Guids for each report
+UPDATE mart.report SET Id = '0E3F340F-C05D-4A98-AD23-A019607745C9'
+WHERE report_id = 1
+UPDATE mart.report SET Id = '5C133E8F-DF3E-48FC-BDEF-C6586B009481'
+WHERE report_id = 2
+UPDATE mart.report SET Id = '7F918C26-4044-4F6B-B0AE-7D27625D052E'
+WHERE report_id = 3
+UPDATE mart.report SET Id = 'C5B88862-F7BE-431B-A63F-3DD5FF8ACE54'
+WHERE report_id = 4
+UPDATE mart.report SET Id = '61548D4F-7D2C-4865-AB76-8A4D01800F1C'
+WHERE report_id = 6
+UPDATE mart.report SET Id = '720A5D88-D2B5-49E1-83EE-8D05239094BF'
+WHERE report_id = 7
+UPDATE mart.report SET Id = 'C232D751-AEC5-4FD7-A274-7C56B99E8DEC'
+WHERE report_id = 8
+UPDATE mart.report SET Id = 'AE758403-C16B-40B0-B6B2-E8F6043B6E04'
+WHERE report_id = 9
+UPDATE mart.report SET Id = '8D8544E4-6B24-4C1C-8083-CBE7522DD0E0'
+WHERE report_id = 10
+UPDATE mart.report SET Id = '71BDB56D-C12F-489B-8275-04873A668D90'
+WHERE report_id = 11
+UPDATE mart.report SET Id = '0065AA84-FD47-4022-ABE3-DD1B54FD096C'
+WHERE report_id = 12
+UPDATE mart.report SET Id = 'D1ADE4AC-284C-4925-AEDD-A193676DBD2F'
+WHERE report_id = 13
+UPDATE mart.report SET Id = 'F7937D02-FA54-4679-AF70-D9798E1690D5'
+WHERE report_id = 14
+UPDATE mart.report SET Id = '4F5DDE81-C264-4756-B1F1-F65BFE54B16B'
+WHERE report_id = 15
+UPDATE mart.report SET Id = '80D31D84-68DB-45A7-977F-75C3250BB37C'
+WHERE report_id = 16
+UPDATE mart.report SET Id = '132E3AF2-3557-4EA7-813E-05CD4869D5DB'
+WHERE report_id = 17
+UPDATE mart.report SET Id = '63243F7F-016E-41D1-9432-0787D26F9ED5'
+WHERE report_id = 18
+UPDATE mart.report SET Id = '009BCDD2-3561-4B59-A719-142CD9216727'
+WHERE report_id = 19
+UPDATE mart.report SET Id = '35649814-4DE8-4CB3-A51C-DDBA2A073E09'
+WHERE report_id = 20
+UPDATE mart.report SET Id = 'BAA446C2-C060-4F39-83EA-B836B1669331'
+WHERE report_id = 21
+UPDATE mart.report SET Id = 'D45A8874-57E1-4EB9-826D-E216A4CBC45B'
+WHERE report_id = 22
+UPDATE mart.report SET Id = 'EB977F5B-86C6-4D98-BEDF-B79DC562987B'
+WHERE report_id = 23
+UPDATE mart.report SET Id = '479809D8-4DAE-4852-BF67-C98C3744918D'
+WHERE report_id = 24
+UPDATE mart.report SET Id = 'E15400E7-892A-4EDE-9377-AE693AA56829'
+WHERE report_id = 25
+UPDATE mart.report SET Id = '2F222F0A-4571-4462-8FBE-0C747035994A'
+WHERE report_id = 26
+UPDATE mart.report SET Id = '8DE1AB0F-32C2-4619-A2B2-97385BE4C49C'
+WHERE report_id = 27
+
 GO
-ALTER TABLE mart.report_control
-ALTER COLUMN Id uniqueidentifier not null
-GO
+---------
+--report_control
+---------
+INSERT INTO [mart].[report_control]
+SELECT
+id = NEWID(),
+control_id=[control_id],
+control_name=[control_name],
+fill_proc_name=[fill_proc_name]
+  FROM [mart].[report_control_old]
 
 -- we need to know these
 UPDATE mart.report_control
@@ -100,34 +278,17 @@ UPDATE mart.report_control
 SET id = 'EFE140D0-904A-4326-BEC2-D45945F7EC6E'
 WHERE control_id = 38
 
-CREATE TABLE [mart].[report_control_collection_new](
-	[control_collection_id] [int] NOT NULL,
-	[collection_id] [int] NOT NULL,
-	[print_order] [int] NOT NULL,
-	[control_id] [int] NOT NULL,
-	[default_value] [nvarchar](4000) NOT NULL,
-	[control_name_resource_key] [nvarchar](50) NOT NULL,
-	[fill_proc_param] [varchar](100) NULL,
-	[param_name] [varchar](50) NULL,
-	[depend_of1] [int] NULL,
-	[depend_of2] [int] NULL,
-	[depend_of3] [int] NULL,
-	[depend_of4] [int] NULL,
-	[Id] [uniqueidentifier] NOT NULL,
-	[DependOf1] [uniqueidentifier] NULL,
-	[DependOf2] [uniqueidentifier] NULL,
-	[DependOf3] [uniqueidentifier] NULL,
-	[DependOf4] [uniqueidentifier] NULL,
-	[CollectionId] [uniqueidentifier] NULL,
-	[ControlId] [uniqueidentifier] NULL,
- CONSTRAINT [PK_report_control_collection_new] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)
-)
 
-INSERT INTO [mart].[report_control_collection_new]
+GO
+
+-----------
+--report_control_collection
+-----------
+INSERT INTO [mart].[report_control_collection]
 SELECT
+[Id] = NewId(),
+[ControlId] = newid(),
+[CollectionId] = newid(),
 [control_collection_id]= [control_collection_id],
 [collection_id]= [collection_id],
 [print_order]=[print_order],
@@ -140,148 +301,88 @@ SELECT
 [depend_of2]=[depend_of2],
 [depend_of3]=[depend_of3],
 [depend_of4]=[depend_of4],
-[Id] = NEWID(),
 [DependOf1] = NULL,
 [DependOf2] = NULL,
 [DependOf3] = NULL,
-[DependOf4] = NULL,
-[CollectionId] = NULL,
-[ControlId] = NULL
-FROM [mart].[report_control_collection]
+[DependOf4] = NULL
+FROM [mart].[report_control_collection_old]
 
 CREATE TABLE #collections(id int not null, collId uniqueidentifier not null)
 INSERT INTO #collections SELECT DISTINCT collection_id, NEWID() FROM mart.report_control_collection
 
-UPDATE mart.report_control_collection_new SET CollectionId = collID
-FROM mart.report_control_collection_new c INNER JOIN #collections
+GO
+UPDATE mart.report_control_collection SET CollectionId = collID
+FROM mart.report_control_collection c INNER JOIN #collections
 ON c.collection_id = #collections.id
 GO 
-ALTER TABLE mart.report_control_collection_new
-ALTER COLUMN CollectionId uniqueidentifier not null
 
-UPDATE mart.report_control_collection_new
-SET ControlId = c.Id
-FROM mart.report_control_collection_new cc INNER JOIN mart.report_control c
+UPDATE mart.report_control_collection SET ControlId = c.Id
+FROM mart.report_control_collection cc INNER JOIN mart.report_control c
 ON cc.control_id = c.control_id
 GO 
-ALTER TABLE mart.report_control_collection_new
-ALTER COLUMN ControlId uniqueidentifier not null
-GO
 
 CREATE TABLE #ids (control int ,Id uniqueidentifier)
 INSERT INTO  #ids 
 SELECT control_collection_id as control, Id
-FROM mart.report_control_collection_new
+FROM mart.report_control_collection
  
-UPDATE mart.report_control_collection_new SET DependOf1 = #ids.Id
-FROM mart.report_control_collection_new  
+UPDATE mart.report_control_collection SET DependOf1 = #ids.Id
+FROM mart.report_control_collection  
 INNER JOIN #ids
 ON #ids.control = depend_of1
 	
-UPDATE mart.report_control_collection_new SET DependOf2 = #ids.Id
-FROM mart.report_control_collection_new  
+UPDATE mart.report_control_collection SET DependOf2 = #ids.Id
+FROM mart.report_control_collection  
 INNER JOIN #ids
 ON #ids.control = depend_of2
 	
-UPDATE mart.report_control_collection_new SET DependOf3 = #ids.Id
-FROM mart.report_control_collection_new  
+UPDATE mart.report_control_collection SET DependOf3 = #ids.Id
+FROM mart.report_control_collection  
 INNER JOIN #ids
 ON #ids.control = depend_of3
 	
-UPDATE mart.report_control_collection_new SET DependOf4 = #ids.Id
-FROM mart.report_control_collection_new
+UPDATE mart.report_control_collection SET DependOf4 = #ids.Id
+FROM mart.report_control_collection  
 INNER JOIN #ids
 ON #ids.control = depend_of4
 GO
 
-ALTER TABLE mart.report
-ADD Id uniqueidentifier null
+ALTER TABLE [mart].[report_control_collection]  WITH CHECK ADD  CONSTRAINT [FK_report_control_collection_report_control] FOREIGN KEY([ControlId])
+REFERENCES [mart].[report_control] ([Id])
 GO
-UPDATE mart.report SET Id = NEWID()
-GO
-ALTER TABLE mart.report
-ALTER COLUMN Id uniqueidentifier not null
-GO
--- we need to know this
-UPDATE mart.report SET Id = '0E3F340F-C05D-4A98-AD23-A019607745C9'
-WHERE report_id = 1
-UPDATE mart.report SET Id = '5C133E8F-DF3E-48FC-BDEF-C6586B009481'
-WHERE report_id = 2
-UPDATE mart.report SET Id = '7F918C26-4044-4F6B-B0AE-7D27625D052E'
-WHERE report_id = 3
-UPDATE mart.report SET Id = 'C5B88862-F7BE-431B-A63F-3DD5FF8ACE54'
-WHERE report_id = 4
-UPDATE mart.report SET Id = '61548D4F-7D2C-4865-AB76-8A4D01800F1C'
-WHERE report_id = 6
-UPDATE mart.report SET Id = '720A5D88-D2B5-49E1-83EE-8D05239094BF'
-WHERE report_id = 7
-UPDATE mart.report SET Id = 'C232D751-AEC5-4FD7-A274-7C56B99E8DEC'
-WHERE report_id = 8
-UPDATE mart.report SET Id = 'AE758403-C16B-40B0-B6B2-E8F6043B6E04'
-WHERE report_id = 9
-UPDATE mart.report SET Id = '8D8544E4-6B24-4C1C-8083-CBE7522DD0E0'
-WHERE report_id = 10
-UPDATE mart.report SET Id = '71BDB56D-C12F-489B-8275-04873A668D90'
-WHERE report_id = 11
-UPDATE mart.report SET Id = '0065AA84-FD47-4022-ABE3-DD1B54FD096C'
-WHERE report_id = 12
-UPDATE mart.report SET Id = 'D1ADE4AC-284C-4925-AEDD-A193676DBD2F'
-WHERE report_id = 13
-UPDATE mart.report SET Id = 'F7937D02-FA54-4679-AF70-D9798E1690D5'
-WHERE report_id = 14
-UPDATE mart.report SET Id = '4F5DDE81-C264-4756-B1F1-F65BFE54B16B'
-WHERE report_id = 15
-UPDATE mart.report SET Id = '80D31D84-68DB-45A7-977F-75C3250BB37C'
-WHERE report_id = 16
-UPDATE mart.report SET Id = '132E3AF2-3557-4EA7-813E-05CD4869D5DB'
-WHERE report_id = 17
-UPDATE mart.report SET Id = '63243F7F-016E-41D1-9432-0787D26F9ED5'
-WHERE report_id = 18
-UPDATE mart.report SET Id = '009BCDD2-3561-4B59-A719-142CD9216727'
-WHERE report_id = 19
-UPDATE mart.report SET Id = '35649814-4DE8-4CB3-A51C-DDBA2A073E09'
-WHERE report_id = 20
-UPDATE mart.report SET Id = 'BAA446C2-C060-4F39-83EA-B836B1669331'
-WHERE report_id = 21
-UPDATE mart.report SET Id = 'D45A8874-57E1-4EB9-826D-E216A4CBC45B'
-WHERE report_id = 22
-UPDATE mart.report SET Id = 'EB977F5B-86C6-4D98-BEDF-B79DC562987B'
-WHERE report_id = 23
-UPDATE mart.report SET Id = '479809D8-4DAE-4852-BF67-C98C3744918D'
-WHERE report_id = 24
-UPDATE mart.report SET Id = 'E15400E7-892A-4EDE-9377-AE693AA56829'
-WHERE report_id = 25
-UPDATE mart.report SET Id = '2F222F0A-4571-4462-8FBE-0C747035994A'
-WHERE report_id = 26
-UPDATE mart.report SET Id = '8DE1AB0F-32C2-4619-A2B2-97385BE4C49C'
-WHERE report_id = 27
 
+ALTER TABLE [mart].[report_control_collection] CHECK CONSTRAINT [FK_report_control_collection_report_control]
+GO
+
+GO
 
 UPDATE mart.report
 SET url = '~/Selection.aspx?ReportId=' + CAST(Id As nvarchar(100))
 WHERE SUBSTRING(url,0, 26) = '~/Selection.aspx?ReportID'
 
-ALTER TABLE mart.report
-ADD ControlCollectionId uniqueidentifier null
-GO
 UPDATE mart.report SET ControlCollectionId = CollectionId
-FROM mart.report INNER JOIN mart.report_control_collection_new c
+FROM mart.report INNER JOIN mart.report_control_collection c
 ON mart.report.control_collection_id = c.collection_id
 GO
-ALTER TABLE mart.report
-ALTER COLUMN ControlCollectionId uniqueidentifier not null
-GO
 
-ALTER TABLE mart.report_user_setting
-ADD ReportId uniqueidentifier null
-GO
+
+INSERT INTO mart.report_user_setting
+SELECT
+[ReportId] = newid(),
+[person_code],
+[report_id],
+[param_name],
+[saved_name_id],
+[control_setting]
+FROM mart.report_user_setting_old
+
 UPDATE mart.report_user_setting SET ReportId = Id
 FROM mart.report r INNER JOIN mart.report_user_setting s
 ON r.report_id = s.report_id
 GO
+
 ALTER TABLE mart.report_user_setting
-ALTER COLUMN ReportId uniqueidentifier not null
-GO
+DROP COLUMN report_id
 
 ALTER TABLE mart.permission_report
 ADD ReportId uniqueidentifier null
@@ -310,17 +411,8 @@ GO
 ALTER TABLE mart.permission_report
 	DROP CONSTRAINT FK_permission_report_data_report
 GO
-ALTER TABLE mart.report
-	DROP CONSTRAINT PK_report
-GO
-ALTER TABLE mart.report ADD CONSTRAINT
-	PK_report PRIMARY KEY CLUSTERED 
-	(
-	Id
-	)
 
-GO
-
+--safe below
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF_stg_permission_datasource_id]') AND type = 'D')
 BEGIN
 ALTER TABLE [stage].[stg_permission_report] DROP CONSTRAINT [DF_stg_permission_datasource_id]
@@ -392,57 +484,26 @@ GO
 -- remove the old and unused stuff
 ALTER TABLE mart.permission_report DROP COLUMN report_id
 GO
+
 IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[mart].[FK_report_report_group]') AND parent_object_id = OBJECT_ID(N'[mart].[report]'))
 ALTER TABLE [mart].[report] DROP CONSTRAINT [FK_report_report_group]
 GO
 
-ALTER TABLE mart.report DROP COLUMN report_group_id
+--drop temp tables
+DROP TABLE mart.report_old
+GO
+DROP TABLE mart.report_control_collection_old
+GO
+DROP TABLE mart.report_control_old
+GO
+DROP TABLE mart.report_user_setting_old
+GO
+
+
+
+--ALTER TABLE mart.report DROP COLUMN report_group_id
 
 DROP TABLE mart.report_group
-GO
-
-ALTER TABLE mart.report DROP COLUMN text_id
-GO
-ALTER TABLE mart.report_control DROP COLUMN attribute_id
-GO
-
-ALTER TABLE mart.report_user_setting
-	DROP CONSTRAINT PK_report_user_setting
-GO
-ALTER TABLE mart.report_user_setting ADD CONSTRAINT
-	PK_report_user_setting PRIMARY KEY CLUSTERED 
-	(
-	person_code,
-	param_name,
-	saved_name_id,
-	ReportId
-	)
-
-GO
-ALTER TABLE mart.report_user_setting
-	DROP COLUMN report_id
-GO
-	ALTER TABLE mart.report_control_collection
-	DROP CONSTRAINT FK_report_control_collection_control
-GO
-
-ALTER TABLE mart.report_control
-	DROP CONSTRAINT PK_report_control
-GO
-ALTER TABLE mart.report_control ADD CONSTRAINT
-	PK_report_control PRIMARY KEY CLUSTERED 
-	(
-	Id
-	)
-GO
-ALTER TABLE mart.report_control_collection ADD CONSTRAINT
-	FK_report_control_collection_report_control FOREIGN KEY
-	(
-	ControlId
-	) REFERENCES mart.report_control
-	(
-	Id
-	)
 GO
 
 CREATE TABLE [mart].[custom_report_control](
@@ -510,3 +571,11 @@ GO
 
 ALTER TABLE [mart].[custom_report] ADD  CONSTRAINT [DF_custom_report_visible]  DEFAULT ((1)) FOR [visible]
 GO
+
+--Test, should fail on build server
+PRINT 'next statement should fail in Azure:'
+ALTER TABLE [mart].[report] DROP CONSTRAINT [PK_report]
+PRINT 'Failde?!'
+GO
+
+
