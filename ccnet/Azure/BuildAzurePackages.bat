7f33d418a8faa02e201e70d844684a90a1ff66f4
TFSIntegration@Teleopti625.toptinet.teleopti.com <TFSIntegration@Teleopti625.toptinet.teleopti.com>
2012-02-29 00:11:40 +0100

    Adding new build stuff for SSL in Azure

--- ccnet/Azure/BuildAzurePackages.bat
+++ ccnet/Azure/BuildAzurePackages.bat
@@ -1,10 +1,12 @@
 @echo off
-::Example: 7.1.334.48819 0 \\hebe\Installation\msi \\hebe\Installation\PreviousBuilds
+::Example: 7.1.334.48819 0 C:\Installation
 ::Get CCNet input
 SET version=%1
 SET ProductId=%2
-SET OUTDIR=%3
-SET DEPLOYSHARE=%4
+SET buildServerRoot=%3
+SET OUTDIR=%buildServerRoot%\AzurePackage
+SET MSIDIR=%buildServerRoot%\msi
+SET DEPLOYSHARE=%buildServerRoot%\PreviousBuilds
 
 IF "%version%"=="" GOTO :noinput
 
@@ -14,8 +16,8 @@ SET AzureWork=%ROOTDIR%\Temp
 SET ConfigPath=%AzureWork%
 SET ContentDest=%AzureWork%\AzureContent
 SET ContentSource=%DEPLOYSHARE%\%version%
-SET msi=%OUTDIR%\%version%\Teleopti CCC AzureConfig %version%.msi
-SET output=%OUTDIR%\%version%\Azure
+SET msi=%MSIDIR%\%version%\Teleopti CCC AzureConfig %version%.msi
+SET output=%OUTDIR%\%version%
 SET DriveLetter=%ROOTDIR:~0,2%
 
 ::Get us to correct reletive location
@@ -29,20 +31,16 @@ IF NOT EXIST "%output%" MKDIR "%output%"
 ::Copy StartupTask and root dir
 XCOPY /e /d /y "%ROOTDIR%\TeleoptiCCC" "%ContentDest%\TeleoptiCCC\"
 
-::Copy *.sccfg-files
-ECHO XCOPY /e /d /y "Customer\*.cscfg" "%output%\"
-XCOPY /e /d /y "Customer\*.cscfg" "%output%\"
-
 ::Get content from Previous build (1) to Content foler (2)
 Echo Getting Previous build ...
 for /f "tokens=1,2 delims=," %%g in (contentMapping.txt) do ROBOCOPY "%ContentSource%\%%g" "%ContentDest%\%%h" /mir /XF *.pdb*
 Echo Getting Previous build. Done
 
 ::Get ReportViewer
-XCOPY /d /y "%ContentSource%\Wise\ccc7_server\ReportViewer2010.exe" "%ContentDest%\TeleoptiCCC\bin"
+XCOPY /d /y "%buildServerRoot%\Dependencies\ccc7_server\ReportViewer2010.exe" "%ContentDest%\TeleoptiCCC\bin"
 
 ::update config and run scpack
-FOR /F %%G IN ('DIR /B Customer\*.txt') DO CALL DeployConfig.bat %%G %version%
+FOR /F %%G IN ('DIR /B Customer\*.txt') DO CALL DeployConfig.bat %%G
 
 GOTO :eof
 
--- ccnet/Azure/DeployConfig.bat
+++ ccnet/Azure/DeployConfig.bat
@@ -10,17 +10,31 @@
 ::init
 SET customerFile=%1
 SET customer=%customerFile:~0,-4%
+SET customerSSLCertConfig=%customer%.SSLcert
+SET customerRDPCertConfig=%customer%.RDPcert
+
+
+::Get base url into variable
+SET /P baseurl=< Customer\%customer%.BaseUrl
+
+::Replace tempoprary txt-file for Msi configuration
+COPY Customer\%customerFile% MSIInput.txt /Y
+
+::Replace Baseurl accoring to settings
+cscript replace.vbs $(BASEURL) %baseurl% MSIInput.txt
 
 ::Uninstall customer config
 MSIExec /uninstall "%msi%" /passive
 
-::Install customer config files
-for /f "tokens=* delims= " %%a in (Customer\%customerFile%) do (
+::Get all input on one string
+for /f "tokens=* delims= " %%a in (MSIInput.txt) do (
 set S=!S!%%a 
 )
 ::set S=!S:~0,-1!
 set S=MSIExec /i "%msi%" INSTALLDIR="%ConfigPath%" !S! /passive
 > installConfig.bat echo.!S!
+
+::Install customer config files
 CALL installConfig.bat
 
 ::AzureConfig is added by WISE
@@ -35,12 +49,24 @@ SET ClickOnceSignPath=%ContentDest%\TeleoptiCCC\Tools\ClickOnceSign
 SET ClientPath=%ContentDest%\Client
 SET MyTimePath=%ContentDest%\MyTime
 CD "%ClickOnceSignPath%"
-"%ClickOnceSignPath%\ClickOnceSign.exe" -s -a Teleopti.Ccc.SmartClientPortal.Shell.application -m "Teleopti.Ccc.SmartClientPortal.Shell.exe.manifest" -u "http://teleopticcc-%customer%.cloudapp.net/Client/" -c "%ClickOnceSignPath%\TemporaryKey.pfx" -p "" -dir "%ClientPath%"
-"%ClickOnceSignPath%\ClickOnceSign.exe" -s -a Teleopti.Ccc.AgentPortal.application -m "Teleopti.Ccc.AgentPortal.exe.manifest" -u "http://teleopticcc-%customer%.cloudapp.net/MyTime/" -c "%ClickOnceSignPath%\TemporaryKey.pfx" -p "" -dir "%MyTimePath%"
+"%ClickOnceSignPath%\ClickOnceSign.exe" -s -a Teleopti.Ccc.SmartClientPortal.Shell.application -m "Teleopti.Ccc.SmartClientPortal.Shell.exe.manifest" -u "https://%baseurl%/Client/" -c "%ClickOnceSignPath%\TemporaryKey.pfx" -p "" -dir "%ClientPath%"
+"%ClickOnceSignPath%\ClickOnceSign.exe" -s -a Teleopti.Ccc.AgentPortal.application -m "Teleopti.Ccc.AgentPortal.exe.manifest" -u "https://%baseurl%/MyTime/" -c "%ClickOnceSignPath%\TemporaryKey.pfx" -p "" -dir "%MyTimePath%"
 CD "%ROOTDIR%"
+
+::copy and update ServiceDefinition.csdef.BuildTemplate
+COPY ServiceDefinition.csdef.BuildTemplate ServiceDefinition.csdef /Y
+COPY ServiceConfig.cscfg.BuildTemplate ServiceConfig.cscfg /Y
+
+::Replace SSL config in Azure config files
+for /f "tokens=1,2 delims=," %%g in (Customer\%customerSSLCertConfig%) do cscript replace.vbs %%g %%h ServiceDefinition.csdef
+for /f "tokens=1,2 delims=," %%g in (Customer\%customerSSLCertConfig%) do cscript replace.vbs %%g %%h ServiceConfig.cscfg
+
+::Deploy the customer .cscfg-file
+ECHO COPY "ServiceConfig.cscfg" "%output%\%customer%.cscfg" /Y
+COPY "ServiceConfig.cscfg" "%output%\%customer%.cscfg" /Y
 
 ::cxpack
 echo building cspack ...
 if not exist "%output%" MKDIR "%output%"
-"c:\Program Files\Windows Azure SDK\v1.4\bin\cspack.exe" "ServiceDefinition.csdef" /role:TeleoptiCCC;Temp\AzureContent\TeleoptiCCC /out:"%output%\%customer%.cspkg"
+"c:\Program Files\Windows Azure SDK\v1.6\bin\cspack.exe" "ServiceDefinition.csdef" /role:TeleoptiCCC;Temp\AzureContent\TeleoptiCCC /out:"%output%\%customer%.cspkg"
 echo building cspack. done!
