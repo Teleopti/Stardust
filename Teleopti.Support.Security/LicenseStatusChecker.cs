fb6198115c3474cc67afbbb6af49a678621eb15a
Ola Håkansson <none@none>
2012-02-28 11:27:45 +0100

    add an initial value in LicenseStatus in the Security Tool

--- Teleopti.Support.Security/LicenseStatusChecker.cs
+++ Teleopti.Support.Security/LicenseStatusChecker.cs
@@ -1,13 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Globalization;
 using System.Threading;
-using Teleopti.Ccc.Domain.Security;
 using Teleopti.Ccc.Infrastructure.Licensing;
-using Teleopti.Ccc.Infrastructure.Repositories;
-using Teleopti.Ccc.Infrastructure.UnitOfWork;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Support.Security
 {
@@ -38,7 +33,7 @@ namespace Teleopti.Support.Security
                 SqlCommand command;
                 using (command = connection.CreateCommand())
                 {
-                    command.CommandText = "SELECT COUNT(*) FROM dbo.DatabaseVersion WHERE BuildNumber=('-360')";
+                    command.CommandText = "SELECT COUNT(*) FROM dbo.LicenseStatus";
                     var versionCount = (int)command.ExecuteScalar();
                     if (versionCount > 0)
                     {
@@ -48,43 +43,32 @@ namespace Teleopti.Support.Security
                     }
                 }
 
-                using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+                var status = new LicenseStatusXml
+                                 {
+                                     StatusOk = true,
+                                     NumberOfActiveAgents = 1,
+                                     CheckDate = DateTime.Today.Date,
+                                     LastValidDate = DateTime.Today.Date.AddDays(1),
+                                     DaysLeft = 30
+                                 };
+
+                var value = status.XmlDocument.OuterXml;
+                try
                 {
-                    ILicenseService service = null;
-                    
-                    var numberOfActiveAgents = new PersonRepository(uow).NumberOfActiveAgents();
-                    var statusXml = new LicenseStatusXml
-                                        {
-                                            StatusOk = true,
-                                            NumberOfActiveAgents = numberOfActiveAgents,
-                                            CheckDate = DateTime.Today.Date,
-                                            LastValidDate = DateTime.Today.Date.AddDays(1),
-                                            DaysLeft = 30
-                                        };
-                    try
-                    {
-                        service = new XmlLicenseService(new LicenseRepository(uow), numberOfActiveAgents);
-                    }
-                    catch (TooManyActiveAgentsException)
-                    {
-                        statusXml.StatusOk = false;
-                        statusXml.LastValidDate = DateTime.Today.Date.AddDays(30);
-                    }
-                    if (service != null)
-                    {
-                        statusXml.AlmostTooMany = service.IsThisAlmostTooManyActiveAgents(numberOfActiveAgents);
-                    }
-                    
-                    var status = new LicenseStatus { XmlString = statusXml.XmlDocument.OuterXml };
-                    new LicenseStatusRepository(uow).Add(status);
-                    uow.PersistAll();
+                    command = connection.CreateCommand();
+                    command.CommandText = string.Format(CultureInfo.InvariantCulture,
+                                                        "INSERT INTO dbo.LicenseStatus (Id, XmlString) VALUES (NewID(),'{0}')",
+                                                        value);
+
+                    command.ExecuteNonQuery();
                 }
-                command = connection.CreateCommand();
-                command.CommandText = string.Format(CultureInfo.InvariantCulture,
-                                                    "INSERT INTO dbo.DatabaseVersion (BuildNumber,SystemVersion,AddedDate,AddedBy) VALUES ('-360','7.1.360.1',GetDate(),'{0}')",
-                                                    Environment.UserName);
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Could not save the status: {0}", ex.Message);
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                    return;
+                }
                 
-                command.ExecuteNonQuery();
                 Thread.Sleep(TimeSpan.FromSeconds(2));
             }
         }
