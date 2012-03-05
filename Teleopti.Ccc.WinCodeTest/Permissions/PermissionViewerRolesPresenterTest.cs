bf33986f82f43865b1dc53926fa92a2a1e70c3a0
Ola Håkansson <none@none>
2012-03-05 10:29:59 +0100

    fixing a test

--- Teleopti.Ccc.WinCodeTest/Permissions/PermissionViewerRolesPresenterTest.cs
+++ Teleopti.Ccc.WinCodeTest/Permissions/PermissionViewerRolesPresenterTest.cs
@@ -69,6 +69,7 @@ namespace Teleopti.Ccc.WinCodeTest.Permissions
             Expect.Call(_loadPersonsCommand.Execute);
             Expect.Call(_loadFunctionsLightCommand.Execute);
             Expect.Call(_view.Show);
+            Expect.Call(_view.BringToFront);
             _mocks.ReplayAll();
             _target.ShowViewer();
             _mocks.VerifyAll();
