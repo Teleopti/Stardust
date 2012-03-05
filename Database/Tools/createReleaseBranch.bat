36395cebd6900aaf99d25d34f6273bc4562dcd07
Roger Kratz <roger.kratz@teleopti.com>
2012-03-05 14:09:54 +0100

    full path to versionbuild.bat

--- Database/Tools/createReleaseBranch.bat
+++ Database/Tools/createReleaseBranch.bat
@@ -1,8 +1,10 @@
 :: Creates a new release branch in the source repository
 @ECHO off
 
+SET THISDIR=%~dp0
+
 :: Call VersionBuild.bat
-call versionbuild.bat
+call "%THISDIR\versionbuild.bat"
 
 if %myError% EQU 0 (
 :: create new release branch and push it to server
