86ceeaaf6ad25a333b8b77f196c82826822ed33c
Roger Kratz <roger.kratz@teleopti.com>
2012-03-08 15:47:14 +0100

    setting a tag + removing auto push (for safety reasons)

--- Database/Tools/CreateReleaseBranch.bat
+++ Database/Tools/CreateReleaseBranch.bat
@@ -6,7 +6,8 @@ SET THISDIR=%~dp0
 ::Confirm
 ECHO This will create release db scripts and
 ECHO create a new named branch on server repository.
-ECHO This operation cant be rolled back.
+ECHO For safety reasons - you need to push manually. Named branches cannot easily be deleted.
+ECHO When you push - you need the --new-branch flag to be set.
 CHOICE /M "Are you sure you want to continue"
 IF ERRORLEVEL 2 exit
 ECHO.
@@ -24,10 +25,10 @@ echo Creating branch %releaseBranchName%
 hg pull
 hg update default
 hg branch %releaseBranchName%
-hg tip "Starting %SYSTEMVERSION%"
+hg tag "Starting %SYSTEMVERSION%" -r %releaseBranchName%
 hg ci -m "Creating release %SYSTEMVERSION%"
-hg push --new-branch
 hg update default
+::hg push
 
 echo Finished!
 )
