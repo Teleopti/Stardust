726ee630ec01ab33fa0816506e70ac6b3ace2e41
ola@teleopti.com <ola@teleopti.com>
2012-03-20 14:43:16 +0100

    Added two new commands to sort by contract time in scheduler - dayview

---
+++ Teleopti.Ccc.WinCode/Scheduling/ScheduleSortingCommands/SortByContractTimeAscendingCommand.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.WinCode.Common;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCode.Scheduling.ScheduleSortingCommands
+{
+    public class SortByContractTimeAscendingCommand : ScheduleSortCommandBase, IScheduleSortCommand
+    {
+        public SortByContractTimeAscendingCommand(ISchedulerStateHolder schedulerState)
+            : base(schedulerState)
+        {}
+
+        public void Execute(DateOnly dateToExecuteOn)
+        {
+            CreateLists(dateToExecuteOn);
+            Projections = sort(Projections);
+            Absence = sort(Absence);
+            MergeLists();
+        }
+
+        private static List<IVisualLayerCollection> sort(IEnumerable<IVisualLayerCollection> projections)
+        {
+            var sorted = from p in projections
+                            orderby p.ContractTime() 
+                            select p;
+            return sorted.ToList();
+        }
+    }
+}
