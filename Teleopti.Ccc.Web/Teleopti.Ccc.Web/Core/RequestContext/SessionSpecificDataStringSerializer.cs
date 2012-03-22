844b40c14acc663e9ff611e567ca5c559e036985
Roger Kratz <roger.kratz@teleopti.com>
2012-03-22 11:02:23 +0100

    Changed serialization of user data to use binaryformatter

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/RequestContext/SessionSpecificDataStringSerializer.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/RequestContext/SessionSpecificDataStringSerializer.cs
@@ -1,5 +1,6 @@
 using System;
-using Teleopti.Interfaces.Domain;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Teleopti.Ccc.Web.Core.RequestContext
 {
@@ -7,19 +8,25 @@ namespace Teleopti.Ccc.Web.Core.RequestContext
 	{
 		public string Serialize(SessionSpecificData data)
 		{
-			return string.Format("{0}{1:N}{2:N}{3}", (int)data.AuthenticationType, data.BusinessUnitId, data.PersonId, data.DataSourceName);
+			var formatter = new BinaryFormatter();
+			using (var stream = new MemoryStream())
+			{
+				formatter.Serialize(stream, data);
+				var bytes = stream.ToArray();
+				return Convert.ToBase64String(bytes);	
+			}
 		}
 
 		public SessionSpecificData Deserialize(string stringData)
 		{
-			if (string.IsNullOrEmpty(stringData) || stringData.Length <= 64)
+			if (string.IsNullOrEmpty(stringData))
 				return null;
-
-			var authType = (AuthenticationTypeOption)Enum.Parse(typeof(AuthenticationTypeOption), stringData.Substring(0, 1));
-			return new SessionSpecificData(Guid.ParseExact(stringData.Substring(1, 32), "N"),
-			                               stringData.Substring(65),
-			                               Guid.ParseExact(stringData.Substring(33, 32), "N"),
-			                               authType);
+			var formatter = new BinaryFormatter();
+			var bytes = Convert.FromBase64String(stringData);
+			using (var stream = new MemoryStream(bytes))
+			{
+				return (SessionSpecificData)formatter.Deserialize(stream);				
+			}
 		}
 	}
 }
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/RequestContext/SessionSpecificDataStringSerializerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Core.RequestContext;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WebTest.Core.RequestContext
+{
+	[TestFixture]
+	public class SessionSpecificDataStringSerializerTest
+	{
+		private ISessionSpecificDataStringSerializer target;
+
+		[SetUp]
+		public void Setup()
+		{
+			target = new SessionSpecificDataStringSerializer();
+		}
+
+		[Test]
+		public void ShouldSerialize()
+		{
+			var original = new SessionSpecificData(Guid.NewGuid(), "data source", Guid.NewGuid(), AuthenticationTypeOption.Application);
+			var serialized = target.Serialize(original);
+			var deserialized = target.Deserialize(serialized);
+			deserialized.PersonId.Should().Be.EqualTo(original.PersonId);
+			deserialized.DataSourceName.Should().Be.EqualTo(original.DataSourceName);
+			deserialized.BusinessUnitId.Should().Be.EqualTo(original.BusinessUnitId);
+			deserialized.AuthenticationType.Should().Be.EqualTo(original.AuthenticationType);
+		}
+
+		[Test]
+		public void FailingDeserializationShouldThrow()
+		{
+			Assert.Throws<SerializationException>(() => target.Deserialize("totally wrong"));
+		}
+
+		[Test]
+		public void DeserializeShouldReturnNullIfUserdataIsNull()
+		{
+			target.Deserialize(null).Should().Be.Null();
+		}
+
+		[Test]
+		public void DeserializeShouldReturnNullIfUserdataIsEmpty()
+		{
+			target.Deserialize(string.Empty).Should().Be.Null();
+		}
+	}
+}
