cc63d095fb4570662169405eb042bc85f32f2635
David Jonsson <david.jonsson@teleopti.com>
2012-03-23 11:46:04 +0100

    #18738 - very slow to fetch request from MyTimeWeb
    --HG--
    branch : Release 7.1.356

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -12,3 +12,14 @@ We have changed the lowest possible resolution to one hour to avoid issues with 
 
 UPDATE dbo.Skill SET DefaultResolution = 60 WHERE DefaultResolution > 60
 GO
+
+----------------  
+--Name: David Jonsson
+--Date: 2012-03-23
+--Desc: #18738 - very slow to fetch request from MyTimeWeb
+----------------  
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ShiftTradeSwapDetail]') AND name = N'IX_ShiftTradeSwapDetail')
+CREATE NONCLUSTERED INDEX IX_ShiftTradeSwapDetail_Parent
+ON [dbo].[ShiftTradeSwapDetail] ([Parent])
+INCLUDE ([PersonFrom],[PersonTo])
+GO
