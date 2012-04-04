fa3144d34ede4886452a8d4e95ecdbba54e654f3
Robin Karlsson <robin.karlsson@teleopti.com>
2012-04-04 15:29:20 +0200

    Updating SignalR and added dummy character to avoid implicit date time conversion in JQuery.

--- Teleopti.Ccc.Web.Broker/MessageBrokerHub.cs
+++ Teleopti.Ccc.Web.Broker/MessageBrokerHub.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using log4net;
 using SignalR.Hubs;
 using Teleopti.Interfaces.MessageBroker;
@@ -16,16 +14,16 @@ namespace Teleopti.Ccc.Web.Broker
 		{
 			if (Logger.IsDebugEnabled)
 			{
-				Logger.DebugFormat("New subscription from client {0} with route {1}.",Context.ConnectionId,subscription.Route());
+				Logger.DebugFormat("New subscription from client {0} with route {1} (Id: {2}).",Context.ConnectionId,subscription.Route(),subscription.Route().GetHashCode());
 			}
-			AddToGroup(subscription.Route().GetHashCode().ToString());
+			AddToGroup(subscription.Route().GetHashCode().ToString()).ContinueWith(t => Logger.InfoFormat("Added subscription {0}.",subscription.Route()));
 		}
 
 		public void RemoveSubscription(string route)
 		{
 			if (Logger.IsDebugEnabled)
 			{
-				Logger.DebugFormat("Remove subscription from client {0} with route {1}.", Context.ConnectionId, route);
+				Logger.DebugFormat("Remove subscription from client {0} with route {1} (Id: {2}).", Context.ConnectionId, route,route.GetHashCode());
 			}
 			RemoveFromGroup(route);
 		}
@@ -36,7 +34,7 @@ namespace Teleopti.Ccc.Web.Broker
 
 			if (Logger.IsDebugEnabled)
 			{
-				Logger.DebugFormat("New notification from client {0} with routes {1}.", Context.ConnectionId, string.Join(", ",notification.Routes()));
+				Logger.DebugFormat("New notification from client {0} with routes {1} (Id's: {2}).", Context.ConnectionId, string.Join(", ", notification.Routes()), string.Join(", ", notification.Routes().Select(r => r.GetHashCode())));
 			}
 
 			foreach (var route in routes)
--- Teleopti.Messaging/Teleopti.Interfaces/MessageBroker/Subscription.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/MessageBroker/Subscription.cs
@@ -100,7 +100,7 @@ namespace Teleopti.Interfaces.MessageBroker
 		/// </summary>
 		public DateTime LowerBoundaryAsDateTime()
 		{
-			return XmlConvert.ToDateTime(LowerBoundary, XmlDateTimeSerializationMode.Unspecified);
+			return AsDateTime(LowerBoundary);
 		}
 
 		/// <summary>
@@ -108,7 +108,17 @@ namespace Teleopti.Interfaces.MessageBroker
 		/// </summary>
 		public DateTime UpperBoundaryAsDateTime()
 		{
-			return XmlConvert.ToDateTime(UpperBoundary, XmlDateTimeSerializationMode.Unspecified);
+			return AsDateTime(UpperBoundary);
+		}
+
+		///<summary>
+		/// Convert the string to date time.
+		///</summary>
+		///<param name="dateTime"></param>
+		///<returns></returns>
+		public static DateTime AsDateTime(string dateTime)
+		{
+			return XmlConvert.ToDateTime(dateTime.Substring(1), XmlDateTimeSerializationMode.Unspecified);
 		}
 
 		/// <summary>
@@ -124,7 +134,7 @@ namespace Teleopti.Interfaces.MessageBroker
 		/// </summary>
 		public static string DateToString(DateTime date)
 		{
-			return XmlConvert.ToString(date, XmlDateTimeSerializationMode.Unspecified);
+			return "D" + XmlConvert.ToString(date, XmlDateTimeSerializationMode.Unspecified);
 		}
 
 		private string excludeDatasourceForCertainTypes()
