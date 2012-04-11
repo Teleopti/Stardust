35f5dd5c0bb58bcefb3a46e3ac22604baf906cc8
Claes Stenman <claes.stenman@teleopti.com>
2012-04-11 10:14:53 +0200

    Bugfix: 18694 [Arabic names mirrored in pdf exports when user has western culture]
    --HG--
    branch : Release 7.1.356

---
+++ Teleopti.Ccc.WinCode/Scheduling/ScheduleReporting/CccPdfStringFormat.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Pdf.Graphics;
+
+namespace Teleopti.Ccc.WinCode.Scheduling.ScheduleReporting
+{
+    public class CccPdfStringFormat
+    {
+        public PdfWordWrapType WordWrapType { get; set; }
+        public PdfTextAlignment Alignment { get; set; }
+        public PdfVerticalAlignment LineAlignment { get; set; }
+
+        public CccPdfStringFormat()
+        {
+            WordWrapType = PdfWordWrapType.Word;
+            Alignment = PdfTextAlignment.Left;
+            LineAlignment = PdfVerticalAlignment.Top;
+        }
+
+        //Always set RTL to true, bug in syncfusion pdfgrapthics.drawstring. Text is mirrored when rtl = false and text is arabic
+        public PdfStringFormat PdfStringFormat
+        {
+            get { return new PdfStringFormat {RightToLeft = true, WordWrap = WordWrapType, Alignment = Alignment, LineAlignment = LineAlignment}; }   
+        }
+    }
+}
