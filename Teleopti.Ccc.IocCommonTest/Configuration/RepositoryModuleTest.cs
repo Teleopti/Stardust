64a480a1655364c46ea59048c7d5dd62c6d6fcb7
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-04-16 14:05:17 +0200

    "Fixed" a test =)

--- Teleopti.Ccc.IocCommonTest/Configuration/RepositoryModuleTest.cs
+++ Teleopti.Ccc.IocCommonTest/Configuration/RepositoryModuleTest.cs
@@ -35,17 +35,17 @@ namespace Teleopti.Ccc.IocCommonTest.Configuration
 		[Test]
 		public void RepositoriesWithIncorrectCtorAreNotWired()
 		{
-            /*
-             * RK
-             * If this test fails because IWorkShiftRuleSetRepository accepts an iunitofworkfactory,
-             * please change to another rep.
-             * There is no reason why I test IWorkShiftRuleSetRepository below, just want a rep
-             * where "new uow handling" not yet supported
-             */
+			/*
+			 * RK
+			 * If this test fails because IValidatedVolumeDayRepository accepts an iunitofworkfactory,
+			 * please change to another rep.
+			 * There is no reason why I test IValidatedVolumeDayRepository below, just want a rep
+			 * where "new uow handling" not yet supported
+			 */
 
-            using (var container = containerBuilder.Build())
+			using (var container = containerBuilder.Build())
 			{
-				Assert.IsFalse(container.IsRegistered(typeof (IWorkShiftRuleSetRepository)));
+				Assert.IsFalse(container.IsRegistered(typeof(IValidatedVolumeDayRepository)));
 				Assert.IsTrue(container.IsRegistered(typeof(IPersonRepository)));
 			}
 		}
