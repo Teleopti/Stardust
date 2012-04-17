7f308667a0762427dfb51a9fa3b2be8545d76012
Roger Kratz <roger.kratz@teleopti.com>
2012-04-17 16:01:11 +0200

    perf: faster projections (~25%)

---
+++ Domain/Common/EntityExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Common
+{
+	public static class EntityExtensions
+	{
+		 public static bool OptimizedEquals(this IEntity entity, IEntity entityToCompare)
+		 {
+			 return entity == entityToCompare || (entity.Id.HasValue && entity.Id.Equals(entityToCompare.Id));
+		 }
+	}
+}
