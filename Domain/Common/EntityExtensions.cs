6ff060b2c5d496da361bd7bff46d836491c59051
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-04-18 10:16:57 +0200

    fxCop

--- Domain/Common/EntityExtensions.cs
+++ Domain/Common/EntityExtensions.cs
@@ -4,6 +4,8 @@ namespace Teleopti.Ccc.Domain.Common
 {
 	public static class EntityExtensions
 	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "1"), 
+		System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
 		public static bool OptimizedEquals(this IEntity entity, IEntity entityToCompare)
 		{
 			if (entity == entityToCompare)
