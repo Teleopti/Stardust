d657afef2feb364cc454be243fd76c65a80b27b8
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-04-20 09:04:37 +0200

    Fixed another one of those caching lazy loading bugs

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Preference/RuleSetProjectionServiceForMultiSessionCaching.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Preference/RuleSetProjectionServiceForMultiSessionCaching.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Infrastructure.Foundation;
 using Teleopti.Interfaces.Domain;
 
@@ -26,6 +28,8 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Preference
 								_lazyLoadingManager.Initialize(r.WorkShift);
 								_lazyLoadingManager.Initialize(r.WorkShift.ShiftCategory);
 								_lazyLoadingManager.Initialize(r.VisualLayerCollection);
+			               		var visualLayers = r.VisualLayerCollection.OfType<VisualLayer>();
+			               		visualLayers.ForEach(l => _lazyLoadingManager.Initialize(l.HighestPriorityActivity));
 							});
 			return result;
 		}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/Preference/RuleSetProjectionServiceForCachingTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/Preference/RuleSetProjectionServiceForCachingTest.cs
@@ -7,6 +7,7 @@ using Rhino.Mocks;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
 using Teleopti.Ccc.Infrastructure.Foundation;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Preference;
@@ -34,7 +35,9 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.Preference
 			var service = MockRepository.GenerateMock<IRuleSetProjectionService>();
 			var lazyLoadingManager = MockRepository.GenerateMock<ILazyLoadingManager>();
 			var target = new RuleSetProjectionServiceForMultiSessionCaching(service, lazyLoadingManager);
-			var data = new WorkShiftVisualLayerInfo(new WorkShift(new ShiftCategory(" ")), new VisualLayerCollection(null, new List<IVisualLayer>(), null));
+			var visualLayer = new VisualLayer(new Activity(" "), new DateTimePeriod(), new Activity(" "));
+			var layers = new List<IVisualLayer>(new[] {visualLayer});
+			var data = new WorkShiftVisualLayerInfo(new WorkShift(new ShiftCategory(" ")), new VisualLayerCollection(null, layers, new ProjectionPayloadMerger()));
 			service.Stub(x => x.ProjectionCollection(null)).Return(new[] { data });
 
 			target.ProjectionCollection(null);
@@ -42,6 +45,7 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.Preference
 			lazyLoadingManager.AssertWasCalled(x => x.Initialize(data.WorkShift));
 			lazyLoadingManager.AssertWasCalled(x => x.Initialize(data.VisualLayerCollection));
 			lazyLoadingManager.AssertWasCalled(x => x.Initialize(data.WorkShift.ShiftCategory));
+			lazyLoadingManager.AssertWasCalled(x => x.Initialize(visualLayer.HighestPriorityActivity));
 		}
 	}
 
