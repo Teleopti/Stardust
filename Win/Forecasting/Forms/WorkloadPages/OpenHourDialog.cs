d40daf49490bec746c8ca31638fe1768420f6ff2
Xianwei Shen <xianwei.shen@teleopti.com>
2012-04-27 11:43:09 +0200

    Bug 19170.
    --HG--
    branch : Release 7.1.356

--- Win/Forecasting/Forms/WorkloadPages/OpenHourDialog.cs
+++ Win/Forecasting/Forms/WorkloadPages/OpenHourDialog.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Globalization;
 using System.Windows.Forms;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Domain.Time;
 using Teleopti.Ccc.Win.Common;
 using Teleopti.Ccc.WinCode.Common.GuiHelpers;
+using Teleopti.Ccc.WinCode.Forecasting;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Win.Forecasting.Forms.WorkloadPages
@@ -32,14 +34,13 @@ namespace Teleopti.Ccc.Win.Forecasting.Forms.WorkloadPages
                 SetTexts();
             SetColor();
 
-            //Whole day should not be visible anymore, shoud this be taken away?
-            timePicker.WholeDay.Visible = false;
-
             _workload = workload;
 
             TimeSpan start = workload.Skill.MidnightBreakOffset;
             TimeSpan end = start.Add(TimeSpan.FromHours(24d));
 
+            timePicker.MinMaxStartTime = new MinMax<TimeSpan>(start, end);
+            timePicker.MinMaxEndTime = new MinMax<TimeSpan>(start, end);
             timePicker.StartTime.CreateAndBindList(start, end);
             timePicker.EndTime.CreateAndBindList(start, end);
 
@@ -59,10 +60,20 @@ namespace Teleopti.Ccc.Win.Forecasting.Forms.WorkloadPages
                     TimeHelper.FitToDefaultResolution(TimeSpan.FromHours(17), workload.Skill.DefaultResolution)); //Must to this to enable users with higher resolution than one hour
             }
 
-            if (_displayPeriod.StartTime == _openHourPeriod.EndTime)
-                timePicker.WholeDay.Checked = true;
-
             timePicker.StartTime.Select();
+
+            timePicker.StartTime.TextChanged += startTimeTextChanged;
+            timePicker.EndTime.TextChanged += endTimeTextChanged;
+        }
+
+        private void endTimeTextChanged(object sender, EventArgs e)
+        {
+            checkIfInputTimePeriodIsValid();
+        }
+
+        private void startTimeTextChanged(object sender, EventArgs e)
+        {
+            checkIfInputTimePeriodIsValid();
         }
 
         private void SetColor()
@@ -88,54 +99,40 @@ namespace Teleopti.Ccc.Win.Forecasting.Forms.WorkloadPages
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            if (_isClosed)
+            {
+                _openHourPeriod = new TimePeriod(_workload.Skill.MidnightBreakOffset,
+                                                 _workload.Skill.MidnightBreakOffset);
+                DialogResult = DialogResult.OK;
+                return;
+            }
+            if (checkIfInputTimePeriodIsValid())
+                DialogResult = DialogResult.OK;
+        }
+
+        private bool checkIfInputTimePeriodIsValid()
+        {
             try
             {
-                if (_isClosed)
+                var startTime = timePicker.StartTime.TimeValue();
+                var endTime = timePicker.EndTime.TimeValue();
+                if(startTime > endTime)
                 {
-                    _openHourPeriod = new TimePeriod(_workload.Skill.MidnightBreakOffset, _workload.Skill.MidnightBreakOffset);
+                    errorProvider1.SetError(chbClose,
+                                            string.Format(TeleoptiPrincipal.Current.Regional.UICulture, UserTexts.Resources.StartTimeShouldBeEarlierThanEndTimeDot,
+                                                          DateTime.MinValue.Add(startTime).ToShortTimeStringWithDays(),
+                                                          DateTime.MinValue.Add(endTime).ToShortTimeStringWithDays()));
+                    return false;
                 }
-                else
-                {
-                    var startTime = timePicker.StartTime.TimeValue();
-                    var endTime = timePicker.EndTime.TimeValue();
-                    if (startTime < _workload.Skill.MidnightBreakOffset)
-                    {
-                        startTime = startTime.Add(TimeSpan.FromDays(1));
-                    }
-                    if (timePicker.WholeDay.Checked || startTime >= endTime)
-                    {
-                        _openHourPeriod = new TimePeriod(startTime, endTime.Add(TimeSpan.FromDays(1)));
-                    }
-                    else
-                    {
-                        _openHourPeriod = new TimePeriod(startTime, endTime);
-                    }
-                }
-
-                if (!WinCode.Forecasting.OpenHourHelper.IsValidOpenHour(_openHourPeriod, _workload.Skill.MidnightBreakOffset))
-                {
-                    MessageBoxOptions option = new MessageBoxOptions();
-                    if (RightToLeft == RightToLeft.Yes)
-                    {
-                        option = MessageBoxOptions.RtlReading;
-                    }
-                    string message = string.Format(CultureInfo.CurrentCulture, "{0} {1}: {2}", UserTexts.Resources.NotValidOpenHoursForMidnightBreak,
-                                                   _workload.Skill.Name, _workload.Skill.MidnightBreakOffset);
-                    Syncfusion.Windows.Forms.MessageBoxAdv.Show(message, UserTexts.Resources.OpenHours, MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1, option, false);
-                    DialogResult = DialogResult.None;
-                }
+                _openHourPeriod = new TimePeriod(startTime, endTime);
+                errorProvider1.Clear();
+                return true;
             }
             catch (ArgumentException ex)
             {
-                MessageBoxOptions option = new MessageBoxOptions();
-                if (RightToLeft == RightToLeft.Yes)
-                {
-                    option = MessageBoxOptions.RtlReading;
-                }
-                Syncfusion.Windows.Forms.MessageBoxAdv.Show(string.Concat(ex.Message, "  "), UserTexts.Resources.UnknownTimeFormat, MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1, option, false);
-                DialogResult = DialogResult.Abort;
+                errorProvider1.SetError(chbClose, ex.Message);
+                return false;
             }
-
         }
 
         private void OpenHourDialog_Load(object sender, EventArgs e)
@@ -150,6 +147,8 @@ namespace Teleopti.Ccc.Win.Forecasting.Forms.WorkloadPages
         {
             _isClosed = ((CheckBox)sender).Checked;
             timePicker.Enabled = !_isClosed;
+            if (_isClosed) errorProvider1.Clear();
+            else checkIfInputTimePeriodIsValid();
         }
     }
 }
