bfd1cd6df6fcc54ba8fe3967acee30f01c81e413
Robin Karlsson <robin.karlsson@teleopti.com>
2012-04-27 16:09:15 +0200

    ...

---
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/QueryHandler/GetCurrentBusinessUnitQueryHandlerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos;
+using Teleopti.Ccc.Sdk.Logic.QueryHandler;
+
+namespace Teleopti.Ccc.Sdk.LogicTest.QueryHandler
+{
+	[TestFixture]
+	public class GetCurrentBusinessUnitQueryHandlerTest
+	{
+		private GetCurrentBusinessUnitQueryHandler target;
+
+		[SetUp]
+		public void Setup()
+		{
+			target = new GetCurrentBusinessUnitQueryHandler();
+		}
+
+		[Test]
+		public void ShouldGetTheAgentPortalSettings()
+		{
+			var result = target.Handle(new GetCurrentBusinessUnitQueryDto());
+			result.First().Name.Should().Be.EqualTo(((TeleoptiIdentity)TeleoptiPrincipal.Current.Identity).BusinessUnit.Name);
+		}
+	}
+}
