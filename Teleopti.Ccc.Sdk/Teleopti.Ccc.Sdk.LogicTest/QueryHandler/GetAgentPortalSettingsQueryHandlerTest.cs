c66123e0f49e5702effb430423ae397e7dc6276c
Robin Karlsson <robin.karlsson@teleopti.com>
2012-04-27 14:49:08 +0200

    First test for query

---
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/QueryHandler/GetAgentPortalSettingsQueryHandlerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos;
+using Teleopti.Ccc.Sdk.Logic;
+using Teleopti.Ccc.Sdk.Logic.QueryHandler;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.Sdk.LogicTest.QueryHandler
+{
+	[TestFixture]
+	public class GetAgentPortalSettingsQueryHandlerTest
+	{
+		private MockRepository mocks;
+		private IPersonalSettingDataRepository personalSettingDataRepository;
+		private IUnitOfWorkFactory unitOfWorkFactory;
+		private GetAgentPortalSettingsQueryHandler target;
+
+		[SetUp]
+		public void Setup()
+		{
+			mocks = new MockRepository();
+			personalSettingDataRepository = mocks.DynamicMock<IPersonalSettingDataRepository>();
+			unitOfWorkFactory = mocks.DynamicMock<IUnitOfWorkFactory>();
+			target = new GetAgentPortalSettingsQueryHandler(personalSettingDataRepository, unitOfWorkFactory);
+		}
+
+		[Test]
+		public void ShouldGetTheAgentPortalSettings()
+		{
+			var settings = new AgentPortalSettings {Resolution = 15};
+			var unitOfWork = mocks.DynamicMock<IUnitOfWork>();
+			using (mocks.Record())
+			{
+				Expect.Call(personalSettingDataRepository.FindValueByKey("AgentPortalSettings", new AgentPortalSettings())).
+					IgnoreArguments().Return(settings);
+				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			}
+			using (mocks.Playback())
+			{
+				var result = target.Handle(new GetAgentPortalSettingsQueryDto());
+				result.First().Resolution.Should().Be.EqualTo(15);
+			}
+		}
+	}
+}
