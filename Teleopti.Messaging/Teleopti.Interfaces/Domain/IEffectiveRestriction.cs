b8c330212722be006d314350fc6a832a8ce98729
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-04-30 11:19:13 +0200

    FxCop cant spell

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IEffectiveRestriction.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IEffectiveRestriction.cs
@@ -133,6 +133,7 @@ namespace Teleopti.Interfaces.Domain
 	/// <summary>
 	/// Layer that can be used by IEffectiveRestriction.VisualLayerCollectionSatisfiesActivityRestriction
 	/// </summary>
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Restrictable")]
 	public interface IActivityRestrictableVisualLayer : IPeriodized
 	{
 		/// <summary>
