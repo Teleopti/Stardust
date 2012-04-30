9f135110518d6707cce508e72e06f7dbc592cabe
Robin Karlsson <robin.karlsson@teleopti.com>
2012-04-30 08:42:01 +0200

    A few more tests...

---
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/QueryHandler/GetGroupsForGroupPageAtDateQueryHandlerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos;
+using Teleopti.Ccc.Sdk.Logic.QueryHandler;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Sdk.LogicTest.QueryHandler
+{
+	[TestFixture]
+	public class GetGroupsForGroupPageAtDateQueryHandlerTest
+	{
+		private MockRepository mocks;
+		private IGroupingReadOnlyRepository groupingReadOnlyRepository;
+		private GetGroupsForGroupPageAtDateQueryHandler target;
+		
+		[SetUp]
+		public void Setup()
+		{
+			mocks = new MockRepository();
+			groupingReadOnlyRepository = mocks.DynamicMock<IGroupingReadOnlyRepository>();
+			target = new GetGroupsForGroupPageAtDateQueryHandler(groupingReadOnlyRepository);
+		}
+
+		[Test]
+		public void ShouldGetCustomGroups()
+		{
+			var readOnlyGroupPage = new ReadOnlyGroupPage{PageId = Guid.NewGuid(),PageName = "Test"};
+			var groupDetail = new ReadOnlyGroupDetail{GroupId = Guid.NewGuid(),GroupName = "My Group"};
+			var groupDetailList = new List<ReadOnlyGroupDetail> { groupDetail };
+			var dateOnly = new DateOnly(2012, 4, 30);
+			using (mocks.Record())
+			{
+				Expect.Call(groupingReadOnlyRepository.AvailableGroups(readOnlyGroupPage, dateOnly)).Constraints(
+					new Rhino.Mocks.Constraints.PredicateConstraint<ReadOnlyGroupPage>(p => p.PageId == readOnlyGroupPage.PageId),
+					Rhino.Mocks.Constraints.Is.Equal(dateOnly)).Return(groupDetailList);
+			}
+			using (mocks.Playback())
+			{
+				var result = target.Handle(new GetGroupsForGroupPageAtDateQueryDto{PageId = readOnlyGroupPage.PageId,QueryDate = new DateOnlyDto{DateTime = dateOnly}});
+				result.Count.Should().Be.EqualTo(1);
+			}
+		}
+	}
+}
---
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/QueryHandler/GetPayrollResultStatusByExportIdQueryHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Payroll;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos;
+using Teleopti.Ccc.Sdk.Logic.Assemblers;
+using Teleopti.Ccc.Sdk.Logic.QueryHandler;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.Sdk.LogicTest.QueryHandler
+{
+	[TestFixture]
+	public class GetPayrollResultStatusByExportIdQueryHandlerTest
+	{
+		private MockRepository mocks;
+		private IPayrollExportRepository payrollExportRepository;
+		private IUnitOfWorkFactory unitOfWorkFactory;
+		private GetPayrollResultStatusByExportIdQueryHandler target;
+		private IAssembler<IPayrollResult, PayrollResultDto> assembler;
+		private IPayrollResultRepository payrollResultRepository;
+
+		[SetUp]
+		public void Setup()
+		{
+			mocks = new MockRepository();
+			payrollExportRepository = mocks.DynamicMock<IPayrollExportRepository>();
+			payrollResultRepository = mocks.DynamicMock<IPayrollResultRepository>();
+			unitOfWorkFactory = mocks.DynamicMock<IUnitOfWorkFactory>();
+			assembler = mocks.DynamicMock<IAssembler<IPayrollResult, PayrollResultDto>>();
+			target = new GetPayrollResultStatusByExportIdQueryHandler(assembler, payrollResultRepository, payrollExportRepository, unitOfWorkFactory);
+		}
+
+		[Test]
+		public void ShouldGetAllPayrollExports()
+		{
+			var payrollExport = new PayrollExport();
+			payrollExport.SetId(Guid.NewGuid());
+			var payrollResult = new PayrollResult(payrollExport, null, DateTime.UtcNow);
+			var payrollExportList = new List<IPayrollResult> {payrollResult};
+			var dto = new PayrollResultDto();
+			var unitOfWork = mocks.DynamicMock<IUnitOfWork>();
+			using (mocks.Record())
+			{
+				Expect.Call(payrollExportRepository.Get(payrollExport.Id.GetValueOrDefault())).Return(payrollExport);
+				Expect.Call(assembler.DomainEntitiesToDtos(payrollExportList)).Return(new[] {dto});
+				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			}
+			using (mocks.Playback())
+			{
+				var result = target.Handle(new GetPayrollResultStatusByExportIdQueryDto{PayrollExportId = payrollExport.Id.GetValueOrDefault()});
+				result.First().Should().Be.EqualTo(dto);
+			}
+		}
+	}
+}
