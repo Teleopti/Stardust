3e2852c2547962d538e6eb913c3617128a0f1392
tamasb <none@none>
2012-05-04 13:39:19 +0200

    add general preferences personal settings file

---
+++ Domain/Optimization/GeneralPreferencesPersonalSettings.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Teleopti.Ccc.Domain.SystemSetting;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
+
+namespace Teleopti.Ccc.Domain.Optimization
+{
+	public class GeneralPreferencesPersonalSettings : SettingValue, IPersonalSettings<IGeneralPreferences>
+	{
+		private IList<IScheduleTag> ScheduleTags { get; set; }
+
+		private Guid? ScheduleTagId { get; set; }
+
+		private bool OptimizationStepDaysOff { get; set; }
+		private bool OptimizationStepTimeBetweenDays { get; set; }
+		private bool OptimizationStepShiftsWithinDay { get; set; }
+		private bool OptimizationStepShiftsForFlexibleWorkTime { get; set; }
+		private bool OptimizationStepDaysOffForFlexibleWorkTime { get; set; }
+		
+		private bool UsePreferences { get; set; }
+		private bool UseMustHaves { get; set; }
+		private bool UseRotations { get; set; }
+		private bool UseAvailabilities { get; set; }
+		private bool UseStudentAvailabilities { get; set; }
+		private bool UseShiftCategoryLimitations { get; set; }
+
+		private double PreferencesValue { get; set; }
+		private double MustHavesValue { get; set; }
+		private double RotationsValue { get; set; }
+		private double AvailabilitiesValue { get; set; }
+		private double StudentAvailabilitiesValue { get; set; }
+
+		public GeneralPreferencesPersonalSettings(IList<IScheduleTag> scheduleTags)
+		{
+			// add initial values
+			ScheduleTags = scheduleTags;
+		}
+
+		public void MapTo(IGeneralPreferences target)
+		{
+			foreach (var scheduleTag in ScheduleTags)
+			{
+				if (ScheduleTagId == scheduleTag.Id)
+					target.ScheduleTag = scheduleTag;
+			}
+
+			target.OptimizationStepDaysOff = OptimizationStepDaysOff;
+			target.OptimizationStepTimeBetweenDays = OptimizationStepTimeBetweenDays;
+			target.OptimizationStepShiftsWithinDay = OptimizationStepShiftsWithinDay;
+			target.OptimizationStepShiftsForFlexibleWorkTime = OptimizationStepShiftsForFlexibleWorkTime;
+			target.OptimizationStepDaysOffForFlexibleWorkTime = OptimizationStepDaysOffForFlexibleWorkTime;
+
+			target.UsePreferences = UsePreferences;
+			target.UseMustHaves = UseMustHaves;
+			target.UseRotations = UseRotations;
+			target.UseAvailabilities = UseAvailabilities;
+			target.UseStudentAvailabilities = UseStudentAvailabilities;
+			target.UseShiftCategoryLimitations = UseShiftCategoryLimitations;
+
+			target.PreferencesValue = PreferencesValue;
+			target.MustHavesValue = MustHavesValue;
+			target.RotationsValue = RotationsValue;
+			target.AvailabilitiesValue = AvailabilitiesValue;
+			target.StudentAvailabilitiesValue = StudentAvailabilitiesValue;
+
+		}
+
+		public void MapFrom(IGeneralPreferences source)
+		{
+			ScheduleTagId = source.ScheduleTag.Id;
+
+			OptimizationStepDaysOff = source.OptimizationStepDaysOff;
+			OptimizationStepTimeBetweenDays = source.OptimizationStepTimeBetweenDays;
+			OptimizationStepShiftsWithinDay = source.OptimizationStepShiftsWithinDay;
+			OptimizationStepShiftsForFlexibleWorkTime = source.OptimizationStepShiftsForFlexibleWorkTime;
+			OptimizationStepDaysOffForFlexibleWorkTime = source.OptimizationStepDaysOffForFlexibleWorkTime;
+
+			UsePreferences = source.UsePreferences;
+			UseMustHaves = source.UseMustHaves;
+			UseRotations = source.UseRotations;
+			UseAvailabilities = source.UseAvailabilities;
+			UseStudentAvailabilities = source.UseStudentAvailabilities;
+			UseShiftCategoryLimitations = source.UseShiftCategoryLimitations;
+
+			PreferencesValue = source.PreferencesValue;
+			MustHavesValue = source.MustHavesValue;
+			RotationsValue = source.RotationsValue;
+			AvailabilitiesValue = source.AvailabilitiesValue;
+			StudentAvailabilitiesValue = source.StudentAvailabilitiesValue;
+		}
+
+		/// <summary>
+		/// Sets the schedule tag id.
+		/// </summary>
+		/// <param name="guid">The GUID.</param>
+		/// <remarks>Used in tests only</remarks>
+		public void SetScheduleTagId(Guid guid)
+		{
+			ScheduleTagId = guid;
+		}
+
+	}
+}
---
+++ DomainTest/Optimization/GeneralPreferencesPersonalSettingsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization
+{
+	[TestFixture]
+	public class GeneralPreferencesPersonalSettingsTest
+	{
+		private GeneralPreferencesPersonalSettings _target;
+		private IList<IScheduleTag> _scheduleTags;
+		private IScheduleTag _scheduleTag1;
+		private Guid _scheduleTag1Id;
+		private IScheduleTag _scheduleTag2;
+		private Guid _scheduleTag2Id;
+		private IGeneralPreferences _generalPreferences;
+
+			[SetUp]
+		public void Setup()
+		{
+			_scheduleTag1Id = Guid.NewGuid();
+			_scheduleTag2Id = Guid.NewGuid();
+			_scheduleTag1 = new ScheduleTag();
+			_scheduleTag2 = new ScheduleTag();
+			_scheduleTag1.SetId(_scheduleTag1Id);
+			_scheduleTag2.SetId(_scheduleTag2Id);
+			_scheduleTags = new List<IScheduleTag> { _scheduleTag1 };
+			_target = new GeneralPreferencesPersonalSettings(_scheduleTags);
+			_generalPreferences = new GeneralPreferences();
+		}
+
+		[Test]
+		public void ShouldFindScheduleTagByIdIfExistInScheduleTagList()
+		{
+			_target.SetScheduleTagId(_scheduleTag1Id);	
+			_target.MapTo(_generalPreferences);
+			Assert.AreEqual(_generalPreferences.ScheduleTag.Id.Value, _scheduleTag1Id);
+		}
+
+		[Test]
+		public void ShouldUseDefaultScheduleTagIfIdNotExistInScheduleTagList()
+		{
+			_target.SetScheduleTagId(_scheduleTag2Id);
+			_target.MapTo(_generalPreferences);
+			Assert.AreEqual(_generalPreferences.ScheduleTag, NullScheduleTag.Instance);
+		}
+	}
+}
