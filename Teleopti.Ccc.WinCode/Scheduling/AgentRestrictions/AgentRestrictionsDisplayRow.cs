e9a40862c48a913b1724cd28d24c1a0743e0b82e
Claes Stenman <claes.stenman@teleopti.com>
2012-05-04 09:52:08 +0200

    Added presenter & model classes for AgentRestrictionsView

---
+++ Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsDisplayRow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions
+{
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces")]
+	public interface  IAgentRestrictionsDisplayRow
+	{
+		
+	}
+
+	public class AgentRestrictionsDisplayRow : IAgentRestrictionsDisplayRow
+	{
+
+	}
+}
