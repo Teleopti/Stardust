2edcc6e6d05100aa514921dc6f1cc048fc7dc9bd
Robin Karlsson <robin.karlsson@teleopti.com>
2012-05-04 16:36:02 +0200

    Added new query to get schedule for one person.

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Common/DataTransferObject/QueryDtos/GetAllScenariosQueryDto.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Common/DataTransferObject/QueryDtos/GetAllScenariosQueryDto.cs
@@ -1,5 +1,11 @@
-﻿namespace Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos
+﻿using System.Runtime.Serialization;
+
+namespace Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos
 {
+	/// <summary>
+	/// Query for all the defined scenarios.
+	/// </summary>
+	[DataContract(Namespace = "http://schemas.ccc.teleopti.com/sdk/2012/04/")]
 	public class GetAllScenariosQueryDto : QueryDto
 	{
 	}
---
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/QueryHandler/GetSchedulesByPersonQueryHandlerTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ServiceModel;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject.QueryDtos;
+using Teleopti.Ccc.Sdk.Logic.Assemblers;
+using Teleopti.Ccc.Sdk.Logic.QueryHandler;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.Sdk.LogicTest.QueryHandler
+{
+	[TestFixture]
+	public class GetSchedulesByPersonQueryHandlerTest
+	{
+		private MockRepository mocks;
+		private IScheduleRepository scheduleRepository;
+		private IUnitOfWorkFactory unitOfWorkFactory;
+		private GetSchedulesByPersonQueryHandler target;
+		private IDateTimePeriodAssembler dateTimePeriodAssembler;
+		private ISchedulePartAssembler scheduleDayAssembler;
+		private IPersonRepository personRepository;
+		private IScenarioRepository scenarioRepository;
+		private Guid scenarioId;
+		private Guid person1Id;
+		private IUnitOfWork unitOfWork;
+
+		[SetUp]
+		public void Setup()
+		{
+			mocks = new MockRepository();
+			scheduleRepository = mocks.DynamicMock<IScheduleRepository>();
+			personRepository = mocks.DynamicMock<IPersonRepository>();
+			dateTimePeriodAssembler = mocks.DynamicMock<IDateTimePeriodAssembler>();
+			scheduleDayAssembler = mocks.DynamicMock<ISchedulePartAssembler>();
+			scenarioRepository = mocks.DynamicMock<IScenarioRepository>();
+			unitOfWorkFactory = mocks.DynamicMock<IUnitOfWorkFactory>();
+			
+			scenarioId = Guid.NewGuid();
+			person1Id = Guid.NewGuid();
+			unitOfWork = mocks.DynamicMock<IUnitOfWork>();
+			
+			target = new GetSchedulesByPersonQueryHandler(unitOfWorkFactory,scheduleRepository,personRepository,scenarioRepository,dateTimePeriodAssembler,scheduleDayAssembler);
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling"), Test]
+		public void ShouldGetScheduleForTeamInGivenScenario()
+		{
+			var scenario = ScenarioFactory.CreateScenarioAggregate();
+			var dictionary = mocks.DynamicMock<IScheduleDictionary>();
+			var person1 = PersonFactory.CreatePerson();
+			var scheduleRange = mocks.DynamicMock<IScheduleRange>();
+			using (mocks.Record())
+			{
+				Expect.Call(scenarioRepository.Get(scenarioId)).Return(scenario);
+				Expect.Call(personRepository.Get(person1Id)).IgnoreArguments().Return(person1);
+				Expect.Call(scheduleRepository.FindSchedulesOnlyInGivenPeriod(null,null,new DateTimePeriod(), scenario)).IgnoreArguments().Return(dictionary);
+				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+				Expect.Call(dictionary[person1]).Return(scheduleRange);
+			}
+			using (mocks.Playback())
+			{
+				var result =
+					target.Handle(new GetSchedulesByPersonQueryHandlerDto
+					              	{
+					              		StartDate = new DateOnlyDto {DateTime = new DateTime(2012, 5, 2)},
+					              		EndDate = new DateOnlyDto {DateTime = new DateTime(2012, 5, 2)},
+					              		ScenarioId = scenarioId,
+					              		PersonId = person1Id,
+					              		TimeZoneId = "W. Europe Standard Time",
+					              		SpecialProjection = "special"
+					              	});
+				result.Count.Should().Be.EqualTo(1);
+			}
+		}
+
+		[Test]
+		public void ShouldNotGetScheduleForTeamUsingInvalidScenarioId()
+		{
+			using (mocks.Record())
+			{
+				Expect.Call(scenarioRepository.Get(scenarioId)).Return(null);
+				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			}
+			using (mocks.Playback())
+			{
+				Assert.Throws<FaultException>(()=>target.Handle(new GetSchedulesByPersonQueryHandlerDto
+					{
+						StartDate = new DateOnlyDto { DateTime = new DateTime(2012, 5, 2) },
+						EndDate = new DateOnlyDto { DateTime = new DateTime(2012, 5, 2) },
+						ScenarioId = scenarioId,
+						PersonId = person1Id,
+						TimeZoneId = "W. Europe Standard Time",
+						SpecialProjection = "special"
+					}));
+			}
+		}
+
+		[Test]
+		public void ShouldGetScheduleForTeamInDefaultScenario()
+		{
+			var scenario = ScenarioFactory.CreateScenarioAggregate();
+			var dictionary = mocks.DynamicMock<IScheduleDictionary>();
+			var person1 = PersonFactory.CreatePerson();
+			var scheduleRange = mocks.DynamicMock<IScheduleRange>();
+			using (mocks.Record())
+			{
+				Expect.Call(scenarioRepository.LoadDefaultScenario()).Return(scenario);
+				Expect.Call(personRepository.Get(person1Id)).IgnoreArguments().Return(person1);
+				Expect.Call(scheduleRepository.FindSchedulesOnlyInGivenPeriod(null, null, new DateTimePeriod(), scenario)).IgnoreArguments().Return(dictionary);
+				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+				Expect.Call(dictionary[person1]).Return(scheduleRange);
+			}
+			using (mocks.Playback())
+			{
+				var result =
+					target.Handle(new GetSchedulesByPersonQueryHandlerDto
+					{
+						StartDate = new DateOnlyDto { DateTime = new DateTime(2012, 5, 2) },
+						EndDate = new DateOnlyDto { DateTime = new DateTime(2012, 5, 2) },
+						ScenarioId = null,
+						PersonId = person1Id,
+						TimeZoneId = "W. Europe Standard Time",
+						SpecialProjection = "special"
+					});
+				result.Count.Should().Be.EqualTo(1);
+			}
+		}
+	}
+}
