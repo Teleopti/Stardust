7063e15fdb8d5b4fa6815af7bdf4930c16de4f91
micke@teleopti.com <micke@teleopti.com>
2012-05-08 18:17:40 +0200

    fixing ccnet build

--- Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsDisplayDataExtractor.cs
+++ Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsDisplayDataExtractor.cs
@@ -1,15 +1,30 @@
-﻿namespace Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions
+﻿using System;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions
 {
 	public interface IAgentRestrictionsDisplayDataExtractor
 	{
-		void ExtractTo(IAgentDisplayData agentDisplayData);
+		void ExtractTo(IAgentDisplayData agentDisplayData, ISchedulingOptions schedulingOptions);
 	}
 
 	public class AgentRestrictionsDisplayDataExtractor : IAgentRestrictionsDisplayDataExtractor
 	{
-		public void ExtractTo(IAgentDisplayData agentDisplayData)
+		private readonly IWorkShiftMinMaxCalculator _workShiftMinMaxCalculator;
+
+		public AgentRestrictionsDisplayDataExtractor(IWorkShiftMinMaxCalculator workShiftMinMaxCalculator)
 		{
-			throw new System.NotImplementedException();
+			_workShiftMinMaxCalculator = workShiftMinMaxCalculator;
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
+		public void ExtractTo(IAgentDisplayData agentDisplayData, ISchedulingOptions schedulingOptions)
+		{
+			_workShiftMinMaxCalculator.ResetCache();
+			MinMax<TimeSpan> minMaxTime = _workShiftMinMaxCalculator.PossibleMinMaxTimeForPeriod(agentDisplayData.Matrix,
+			                                                                                     schedulingOptions);
+			agentDisplayData.MinimumPossibleTime = minMaxTime.Minimum;
+			agentDisplayData.MaximumPossibleTime = minMaxTime.Maximum;
 		}
 	}
 }
