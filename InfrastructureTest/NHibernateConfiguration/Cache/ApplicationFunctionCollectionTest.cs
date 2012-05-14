a9b48f19909e678016fa6279ffcb980b2e118339
Roger Kratz <roger.kratz@teleopti.com>
2012-05-14 13:01:44 +0200

    caching applicationRole.ApplicationFunctionCollection

---
+++ InfrastructureTest/NHibernateConfiguration/Cache/ApplicationFunctionCollectionTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Security.AuthorizationEntities;
+using Teleopti.Ccc.Domain.Time;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.Infrastructure.NHibernateConfiguration;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.NHibernateConfiguration.Cache
+{
+	[TestFixture]
+	[Category("LongRunning")]
+	public class ApplicationFunctionCollectionTest
+	{
+		private IDataSource dataSource;
+		private IApplicationRole applicationRole;
+		private IApplicationFunction applicationFunction;
+
+		[Test]
+		public void ShouldCacheApplicationRoleFunctionCollection()
+		{
+			var sessionFactory = ((NHibernateUnitOfWorkFactory)dataSource.Application).SessionFactory;
+			using (var uow = dataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				var p = new ApplicationRoleRepository(uow).Get(applicationRole.Id.Value);
+				sessionFactory.Statistics.Clear();
+				LazyLoadingManager.Initialize(p.ApplicationFunctionCollection);
+			}
+			sessionFactory.Statistics.CollectionLoadCount.Should().Be.EqualTo(0);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			var dsFactory = new DataSourcesFactory(new EnversConfiguration(), new List<IDenormalizer>(), new DataSourceConfigurationSetter(true, false, "call"));
+			dataSource = dsFactory.Create(SetupFixtureForAssembly.Sql2005conf(ConnectionStringHelper.ConnectionStringUsedInTests, null), null);
+			applicationFunction = new ApplicationFunction();
+			applicationRole = new ApplicationRole { Name = "hejhej" };
+			applicationRole.AddApplicationFunction(applicationFunction);
+
+			using (var uow = dataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				new ApplicationFunctionRepository(uow).Add(applicationFunction);
+				new ApplicationRoleRepository(uow).Add(applicationRole);
+				uow.PersistAll();
+			}
+
+			//fill cache
+			using (var uow = dataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				var role = new ApplicationRoleRepository(uow).Get(applicationRole.Id.Value);
+				LazyLoadingManager.Initialize(role.ApplicationFunctionCollection);
+			}
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			using (var uow = dataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				new PersonRepository(uow).Remove(applicationFunction);
+				new ApplicationRoleRepository(uow).Remove(applicationRole);
+				uow.PersistAll();
+			}
+		}
+	}
+}
