703c4d2358186a80e1bddbd5319ac6742a1023d7
Mattias Engblom <mattias.engblom@teleopti.com>
2012-05-15 14:32:02 +0200

    License options tests

--- DomainTest/Security/LicenseOptions/TeleoptiCccAgentSelfServiceLicenseOptionTest.cs
+++ DomainTest/Security/LicenseOptions/TeleoptiCccAgentSelfServiceLicenseOptionTest.cs
@@ -23,7 +23,7 @@ namespace Teleopti.Ccc.DomainTest.Security.LicenseOptions
 
             _target.EnableApplicationFunctions(inputList);
             IList<IApplicationFunction> resultList = _target.EnabledApplicationFunctions;
-            Assert.AreEqual(9, resultList.Count);
+            Assert.AreEqual(10, resultList.Count);
         }
     }
 }
