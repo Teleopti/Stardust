db0ffd0ef1e97e9c0726aeaa714293c3f7d54e41
ola@teleopti.com <ola@teleopti.com>
2012-05-15 14:41:50 +0200

    changes in the denormalizer

--- Infrastructure/UnitOfWork/PersonFinderDenormalizer.cs
+++ Infrastructure/UnitOfWork/PersonFinderDenormalizer.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NHibernate;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.Infrastructure;
 
@@ -29,8 +31,14 @@ namespace Teleopti.Ccc.Infrastructure.UnitOfWork
 
             if (affectedInterfaces.Any(t => _triggerInterfaces.Contains(t)))
             {
-                runSql.Create("exec [ReadModel].[UpdateFindPerson]")
-                    .Execute();
+				var persons = (from p in modifiedRoots where p.Root is Person select p.Root).ToList();
+				foreach (var personList in persons.Batch(400))
+				{
+					var idsAsString = (from p in personList select ((Person) p).Id.ToString()).ToArray();
+					var ids = string.Join(",", idsAsString);
+					runSql.Create(string.Format("exec [ReadModel].[UpdateFindPerson] '{0}'",ids))
+						.Execute();
+				}
             }
         }
     }
