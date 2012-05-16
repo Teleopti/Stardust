133367769f388f9b8a2373c17d608f2ab144a5ce
Kunning Mao <kunning.mao@teleopti.com>
2012-05-16 11:21:06 +0200

    improve test coverage

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Models/LayoutBase/SplitButtonSplitterTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.Portal;
+using SharpTestsEx;
+
+namespace Teleopti.Ccc.WebTest.Areas.MyTime.Models.LayoutBase
+{
+	[TestFixture]
+	public class SplitButtonSplitterTest
+	{
+		[Test]
+		public void ShoudReturnCorrectProperty()
+		{
+			var splitButtonSplitter=new SplitButtonSplitter();
+			splitButtonSplitter.Value.Should().Be.EqualTo("-");
+			splitButtonSplitter.Text.Should().Be.EqualTo("-");
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/ServiceBus/ServiceBusSenderTest.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Core.ServiceBus;
+
+namespace Teleopti.Ccc.WebTest.Core.ServiceBus
+{
+	[TestFixture]
+	public class ServiceBusSenderTest
+	{
+
+		[Test]
+		public void ShouldEnsureBus()
+		{
+			using (var sender = new ServiceBusSender())
+			{
+				sender.EnsureBus();
+				sender.FieldValue<IContainer>("_customHost").Should().Not.Be.Null();
+				sender.FieldValue<bool>("_isRunning").Should().Be.False(); //due to miss config file
+			}
+		}
+	}
+}
