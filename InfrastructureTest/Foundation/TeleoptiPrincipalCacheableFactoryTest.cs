f8c22f7a59b77b99cf5ea7b77f8ee9eb5a00c19d
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-05-16 11:11:39 +0200

    Added those missing tests...

---
+++ InfrastructureTest/Foundation/TeleoptiPrincipalCacheableFactoryTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.Foundation
+{
+	[TestFixture]
+	public class TeleoptiPrincipalCacheableFactoryTest
+	{
+		[Test]
+		public void ShouldMakeCachablePrincipal()
+		{
+			var person = PersonFactory.CreatePerson();
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit();
+			var internalsFactory = new TeleoptiPrincipalInternalsFactory();
+			var target = new TeleoptiPrincipalCacheableFactory(internalsFactory, internalsFactory, internalsFactory);
+
+			var result = target.MakePrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Application);
+
+			result.Should().Be.OfType<TeleoptiPrincipalCacheable>();
+		}
+
+		[Test]
+		public void ShouldMakeUsingCachableDependecies()
+		{
+			var person = PersonFactory.CreatePerson();
+			var makeRegionalFromPerson = MockRepository.GenerateMock<IMakeRegionalFromPerson>();
+			var makeOrganizationFromPerson = MockRepository.GenerateMock<IMakeOrganisationMembershipFromPerson>();
+			var retrievePersonNameForPerson = MockRepository.GenerateMock<IRetrievePersonNameForPerson>();
+			var target = new TeleoptiPrincipalCacheableFactory(makeRegionalFromPerson, makeOrganizationFromPerson, retrievePersonNameForPerson);
+			retrievePersonNameForPerson.Stub(x => x.NameForPerson(person)).Return(person.Name.ToString());
+
+			target.MakePrincipal(person, null, null, AuthenticationTypeOption.Windows);
+
+			makeRegionalFromPerson.AssertWasCalled(x => x.MakeRegionalFromPerson(person));
+			makeOrganizationFromPerson.AssertWasCalled(x => x.MakeOrganisationMembership(person));
+			retrievePersonNameForPerson.AssertWasCalled(x => x.NameForPerson(person));
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/RequestContext/WebRequestPrincipalContextTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using MvcContrib.TestHelper.Fakes;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.Web.Core.RequestContext;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WebTest.Core.RequestContext
+{
+	[TestFixture]
+	public class WebRequestPrincipalContextTest
+	{
+		[Test]
+		public void ShouldMakePrincipal()
+		{
+			var principalFactory = MockRepository.GenerateMock<IPrincipalFactory>();
+			var person = PersonFactory.CreatePerson();
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit();
+			var target = new WebRequestPrincipalContext(new FakeCurrentHttpContext(new FakeHttpContext("")), principalFactory);
+
+			target.SetCurrentPrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Windows);
+
+			principalFactory.AssertWasCalled(x => x.MakePrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Windows));
+		}
+
+		[Test]
+		public void ShouldSetMadePrincipalToCurrentThread()
+		{
+			var principalFactory = MockRepository.GenerateMock<IPrincipalFactory>();
+			var person = PersonFactory.CreatePerson();
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit();
+			var principal = TeleoptiPrincipalCacheable.Make(new TeleoptiIdentity("", dataSource, businessUnit, WindowsIdentity.GetCurrent(), AuthenticationTypeOption.Windows), person);
+			principalFactory.Stub(x => x.MakePrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Windows)).Return(principal);
+			var target = new WebRequestPrincipalContext(new FakeCurrentHttpContext(new FakeHttpContext("")), principalFactory);
+
+			target.SetCurrentPrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Windows);
+
+			Thread.CurrentPrincipal.Should().Be(principal);
+		}
+
+		[Test]
+		public void ShouldSetMadePrincipalToCurrentHttpContext()
+		{
+			var principalFactory = MockRepository.GenerateMock<IPrincipalFactory>();
+			var person = PersonFactory.CreatePerson();
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit();
+			var principal = TeleoptiPrincipalCacheable.Make(new TeleoptiIdentity("", dataSource, businessUnit, WindowsIdentity.GetCurrent(), AuthenticationTypeOption.Windows), person);
+			var httpContext = new FakeHttpContext("");
+			principalFactory.Stub(x => x.MakePrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Windows)).Return(principal);
+			var target = new WebRequestPrincipalContext(new FakeCurrentHttpContext(httpContext), principalFactory);
+
+			target.SetCurrentPrincipal(person, dataSource, businessUnit, AuthenticationTypeOption.Windows);
+
+			httpContext.User.Should().Be(principal);
+		}
+
+		[Test]
+		public void ShouldSetPrincipalToCurrentThread()
+		{
+			var person = PersonFactory.CreatePerson();
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit();
+			var principal = TeleoptiPrincipalCacheable.Make(new TeleoptiIdentity("", dataSource, businessUnit, WindowsIdentity.GetCurrent(), AuthenticationTypeOption.Windows), person);
+			var target = new WebRequestPrincipalContext(new FakeCurrentHttpContext(new FakeHttpContext("")), null);
+
+			target.SetCurrentPrincipal(principal);
+
+			Thread.CurrentPrincipal.Should().Be(principal);
+		}
+
+		[Test]
+		public void ShouldSetPrincipalToCurrentHttpContext()
+		{
+			var person = PersonFactory.CreatePerson();
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit();
+			var principal = TeleoptiPrincipalCacheable.Make(new TeleoptiIdentity("", dataSource, businessUnit, WindowsIdentity.GetCurrent(), AuthenticationTypeOption.Windows), person);
+			var httpContext = new FakeHttpContext("");
+			var target = new WebRequestPrincipalContext(new FakeCurrentHttpContext(httpContext), null);
+
+			target.SetCurrentPrincipal(principal);
+
+			httpContext.User.Should().Be(principal);
+		}
+	}
+}
