3953187c32b95c27251276c82995e5958fd30075
Claes Stenman <claes.stenman@teleopti.com>
2012-05-21 09:46:16 +0200

    Added AgentRestrictionTask for threading...

---
+++ Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions
+{
+	public class AgentRestrictionsTask
+	{
+		public IAgentDisplayData AgentDisplayData { get; private set; }
+		public BackgroundWorker Worker { get; private set; }
+		public int Priority { get; set; }
+		
+
+		public AgentRestrictionsTask(IAgentDisplayData agentDisplayData, BackgroundWorker worker)
+		{
+			if(worker == null) throw new ArgumentNullException("worker");
+
+			AgentDisplayData = agentDisplayData;
+			Worker = worker;
+			worker.WorkerReportsProgress = true;
+			worker.WorkerSupportsCancellation = true;
+			Priority = 1;
+		}
+	}
+}
