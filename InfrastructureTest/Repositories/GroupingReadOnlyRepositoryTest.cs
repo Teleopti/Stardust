53fc1378af199bcdc53358b8690ebe0f09d4f1ee
ola@teleopti.com <ola@teleopti.com>
2012-05-25 08:16:51 +0200

    added some test

--- InfrastructureTest/Repositories/GroupingReadOnlyRepositoryTest.cs
+++ InfrastructureTest/Repositories/GroupingReadOnlyRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using NUnit.Framework;
+using Rhino.Mocks;
 using SharpTestsEx;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
@@ -12,32 +13,50 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories
 	[TestFixture]
 	public class GroupingReadOnlyRepositoryTest : DatabaseTest
 	{
-		private IGroupingReadOnlyRepository target;
+		private IGroupingReadOnlyRepository _target;
 
 		protected override void SetupForRepositoryTest()
 		{
-			target = new GroupingReadOnlyRepository(UnitOfWorkFactory.Current);
+			_target = new GroupingReadOnlyRepository(UnitOfWorkFactory.Current);
 		}
 
 		[Test]
 		public void ShouldGroupPagesFromReadModel()
 		{
-			var items = target.AvailableGroupPages();
+			var items = _target.AvailableGroupPages();
 			items.Count().Should().Be.EqualTo(0);
 		}
 
 		[Test]
 		public void ShouldLoadAvailableGroupsFromReadModel()
 		{
-			var items = target.AvailableGroups(new ReadOnlyGroupPage{PageId = new Guid("6CE00B41-0722-4B36-91DD-0A3B63C545CF"),PageName = "xxMain"},DateOnly.Today);
+			var items = _target.AvailableGroups(new ReadOnlyGroupPage{PageId = new Guid("6CE00B41-0722-4B36-91DD-0A3B63C545CF"),PageName = "xxMain"},DateOnly.Today);
 			items.Count().Should().Be.EqualTo(0);
 		}
 
 		[Test]
 		public void ShouldLoadDetailsForGroupFromReadModel()
 		{
-			var items = target.DetailsForGroup(Guid.Empty, DateOnly.Today);
+			var items = _target.DetailsForGroup(Guid.Empty, DateOnly.Today);
 			items.Count().Should().Be.EqualTo(0);
+		}
+
+		[Test]
+		public void ShouldCallUpdateReadModelWithoutCrash()
+		{
+			_target.UpdateGroupingReadModel(Guid.NewGuid().ToString());
+		}
+
+		[Test]
+		public void ShouldCallUpdateGroupingReadModelGroupPageWithoutCrash()
+		{
+			_target.UpdateGroupingReadModelGroupPage(Guid.NewGuid().ToString());
+		}
+
+		[Test]
+		public void ShouldCallUpdateGroupingReadModelDataWithoutCrash()
+		{
+			_target.UpdateGroupingReadModelData(Guid.NewGuid().ToString());
 		}
 	}
 }
