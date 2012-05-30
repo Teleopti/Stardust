411fb199a43785896157ef93811714b6d89f47aa
ola@teleopti.com <ola@teleopti.com>
2012-05-30 11:54:04 +0200

    fx

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/ISchedulingResultStateHolder.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/ISchedulingResultStateHolder.cs
@@ -163,6 +163,7 @@ namespace Teleopti.Interfaces.Domain
 		/// <summary>
 		/// 
 		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
 		IList<IOptionalColumn> OptionalColumns { get; set; } 
     }
 }
