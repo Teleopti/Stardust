8e74f21bf387ca81e04d8fd3bea4caed74357608
ola@teleopti.com <ola@teleopti.com>
2012-05-31 08:15:02 +0200

    the ones added

---
+++ Teleopti.Ccc.WinCode/Grouping/SchedulerGroupPagesProvider.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.UserTexts;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.WinCode.Grouping
+{
+	public interface ISchedulerGroupPagesProvider
+	{
+		IList<IGroupPageLight> GetGroups(bool includeSkills);
+	}
+	public class SchedulerGroupPagesProvider : ISchedulerGroupPagesProvider
+	{
+		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+		private readonly IRepositoryFactory _repositoryFactory;
+
+		public SchedulerGroupPagesProvider(IUnitOfWorkFactory unitOfWorkFactory, IRepositoryFactory repositoryFactory)
+		{
+			_unitOfWorkFactory = unitOfWorkFactory;
+			_repositoryFactory = repositoryFactory;
+		}
+
+		public IList<IGroupPageLight> GetGroups(bool includeSkills)
+		{
+			var lst = new List<IGroupPageLight>{new GroupPageLight{Key = "Main",Name = Resources.Main},
+					new GroupPageLight{Key = "Contracts",Name = Resources.Contract},
+					new GroupPageLight{Key = "ContractSchedule",Name = Resources.ContractSchedule},
+					new GroupPageLight{Key = "PartTimepercentages",Name = Resources.PartTimePercentage},
+					new GroupPageLight{Key = "Note",Name = Resources.Note},
+					new GroupPageLight{Key = "RuleSetBag",Name = Resources.RuleSetBag}};
+			if (includeSkills)
+				lst.Add(new GroupPageLight { Key = "Skill", Name = Resources.Skill });
+
+			using (var uow = _unitOfWorkFactory.CreateAndOpenStatelessUnitOfWork())
+			{
+				var rep = _repositoryFactory.CreatePersonSelectorReadOnlyRepository(uow);
+				var userTabs = rep.GetUserDefinedTabs();
+				foreach (var userDefinedTabLight in userTabs)
+				{
+					lst.Add(new GroupPageLight { Key = userDefinedTabLight.Id.ToString(), Name = userDefinedTabLight.Name });
+				}
+			}
+			return lst;
+		}
+	}
+
+	public class GroupPageLight : IGroupPageLight
+	{
+		public string Name { get; set; }
+		public string Key { get; set; }
+	}
+}
