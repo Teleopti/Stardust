ba0d15977a2c946207e76f9c39e651b04be0c7d4
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-05-31 11:01:08 +0200

    oops, I know I forgot something...

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/PreferenceFeedbackControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/PreferenceFeedbackControllerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using Rhino.Mocks;
 using SharpTestsEx;
@@ -11,37 +12,43 @@ using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.WebTest.Areas.MyTime.Controllers
 {
-	//[TestFixture]
-	//public class PreferenceFeedbackControllerTest
-	//{
-	//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope"), Test]
-	//    public void ShouldReturnFeedback()
-	//    {
-	//        var viewModelFactory = MockRepository.GenerateMock<IPreferenceViewModelFactory>();
-	//        var model = new PreferenceDayFeedbackViewModel();
-	//        var target = new PreferenceFeedbackController(viewModelFactory, null);
-	//        viewModelFactory.Stub(x => x.CreateDayFeedbackViewModel(DateOnly.Today)).Return(model);
+	[TestFixture]
+	public class PreferenceFeedbackControllerTest
+	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope"), Test]
+		public void ShouldReturnFeedback()
+		{
+			var viewModelFactory = MockRepository.GenerateMock<IPreferenceViewModelFactory>();
+			var model = new PreferenceDayFeedbackViewModel();
+			var target = new PreferenceFeedbackController(viewModelFactory, null);
+			viewModelFactory.Stub(x => x.CreateDayFeedbackViewModel(DateOnly.Today)).Return(model);
 
-	//        target.FeedbackTask(DateOnly.Today);
-	//        //var result = target.FeedbackCompleted(target.AsyncManager.Parameters["model"] as PreferenceDayFeedbackViewModel, null);
+			target.FeedbackTask(DateOnly.Today);
+			var result = target.FeedbackCompleted(
+				target.AsyncManager.Parameters["model"] as PreferenceDayFeedbackViewModel,
+				Task.Factory.StartNew(() => { })
+				);
 
-	//        //result.Data.Should().Be(model);
-	//    }
+			result.Data.Should().Be(model);
+		}
 
-	//    [Test]
-	//    public void ShouldReturnDaysOff()
-	//    {
-	//        var preferencePeriodFeedbackProvider = MockRepository.GenerateMock<IPreferencePeriodFeedbackProvider>();
-	//        var target = new PreferenceFeedbackController(null, preferencePeriodFeedbackProvider);
-	//        var daysOff = new Random().Next(0, 10000000);
-	//        var date = DateOnly.Today.AddDays(new Random().Next(0, 1000));
+		[Test]
+		public void ShouldReturnDaysOff()
+		{
+			var preferencePeriodFeedbackProvider = MockRepository.GenerateMock<IPreferencePeriodFeedbackProvider>();
+			var target = new PreferenceFeedbackController(null, preferencePeriodFeedbackProvider);
+			var daysOff = new Random().Next(0, 10000000);
+			var date = DateOnly.Today.AddDays(new Random().Next(0, 1000));
 
-	//        preferencePeriodFeedbackProvider.Stub(x => x.ShouldHaveDaysOff(date)).Return(daysOff);
+			preferencePeriodFeedbackProvider.Stub(x => x.ShouldHaveDaysOff(date)).Return(daysOff);
 
-	//        target.ShouldHaveDaysOffTask(date);
-	//        //var result = target.ShouldHaveDaysOffCompleted((int) target.AsyncManager.Parameters["daysOff"], null);
+			target.ShouldHaveDaysOffTask(date);
+			var result = target.ShouldHaveDaysOffCompleted(
+				(int) target.AsyncManager.Parameters["daysOff"],
+				Task.Factory.StartNew(() => { })
+				);
 
-	//        //result.Data.Should().Be(daysOff);
-	//    }
-	//}
+			result.Data.Should().Be(daysOff);
+		}
+	}
 }
