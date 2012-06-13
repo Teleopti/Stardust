7df03890affa0972fcec191eeb650bd622359a05
Roger Kratz <roger.kratz@teleopti.com>
2012-06-13 14:36:49 +0200

    xml comment

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IWorkShiftWorkTime.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IWorkShiftWorkTime.cs
@@ -5,6 +5,12 @@
 	/// </summary>
 	public interface IWorkShiftWorkTime
 	{
+		/// <summary>
+		/// Returns a <see cref="IWorkTimeMinMax"/> based on ruleset and restriction.
+		/// </summary>
+		/// <param name="workShiftRuleSet"></param>
+		/// <param name="effectiveRestriction"></param>
+		/// <returns></returns>
 		IWorkTimeMinMax CalculateMinMax(IWorkShiftRuleSet workShiftRuleSet,
 		                                                IEffectiveRestriction effectiveRestriction);
 	}
