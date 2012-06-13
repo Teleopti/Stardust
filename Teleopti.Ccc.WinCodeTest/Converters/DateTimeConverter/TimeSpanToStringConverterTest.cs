32022e59e135fc450a8955153b34d11e7fa2dfa0
Erik Sundberg <erik.sundberg@teleopti.com>
2012-06-13 15:32:11 +0200

    #17673 Usability: Add activity length to tooltip in shift editor (Schedules and Intraday)

---
+++ Teleopti.Ccc.WinCodeTest/Converters/DateTimeConverter/TimeSpanToStringConverterTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using NUnit.Framework;
+using Teleopti.Ccc.WinCode.Converters.DateTimeConverter;
+using Teleopti.Ccc.Domain.Security.Principal;
+
+namespace Teleopti.Ccc.WinCodeTest.Converters.DateTimeConverter
+{
+    [TestFixture]
+    public class TimeSpanToStringConverterTest
+    {
+        private TimeSpanToStringConverter _target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _target = new TimeSpanToStringConverter();
+        }
+
+        [Test]
+        public void VerifyConvert()
+        {
+            TimeSpan timeSpan = new TimeSpan(0, 5, 30, 25);
+            object value = _target.Convert(timeSpan, null, null, TeleoptiPrincipal.Current.Regional.Culture);
+
+            Assert.AreEqual("5:30", value);
+        }
+
+        [Test]
+        public void VerifyConvertBack()
+        {
+            object value = _target.ConvertBack(null, null, null, null);
+            Assert.AreEqual(DependencyProperty.UnsetValue, value);
+        }
+    }
+}
