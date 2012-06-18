3971969028d0621f3d8a9664d462d6a940d9985d
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-06-18 10:09:34 +0200

    javascript tests in the build (the easy way)

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/QUnitTestsStepDefinition.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using Teleopti.Ccc.WebBehaviorTest.Core;
+using Find = WatiN.Core.Find;
+
+namespace Teleopti.Ccc.WebBehaviorTest
+{
+	[Binding]
+	public class QUnitTestsStepDefinition
+	{
+
+		[When(@"I navigate to (.*)")]
+		public void WhenINavigateTo(string url)
+		{
+			Navigation.GoTo(url);
+		}
+
+		[Then(@"I should see the tests run")]
+		public void ThenIShouldSeeTheTestsRun()
+		{
+			EventualAssert.That(() => int.Parse(Browser.Current.Para("qunit-testresult").Span(Find.ByClass("total", false)).InnerHtml), Is.GreaterThan(0));
+		}
+
+		[Then(@"I should see all tests pass")]
+		public void ThenIShouldSeeAllTestsPass()
+		{
+			EventualAssert.That(() => int.Parse(Browser.Current.Para("qunit-testresult").Span(Find.ByClass("failed", false)).InnerHtml), Is.EqualTo(0));
+		}
+
+	}
+
+}
