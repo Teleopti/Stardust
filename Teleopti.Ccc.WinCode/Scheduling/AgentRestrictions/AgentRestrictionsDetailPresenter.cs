50a812260d303219150b4d68767137c7685a7c90
Claes Stenman <claes.stenman@teleopti.com>
2012-06-18 11:52:28 +0200

    Added some formatting to AgentRestrictionsDetailView...

--- Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsDetailPresenter.cs
+++ Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsDetailPresenter.cs
@@ -1,11 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Teleopti.Ccc.Domain.Scheduling.Rules;
+using Teleopti.Ccc.WinCode.Common;
+using Teleopti.Ccc.WinCode.Common.Clipboard;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions
 {
-	public class AgentRestrictionsDetailPresenter
+	public class AgentRestrictionsDetailPresenter : SchedulePresenterBase
 	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+		private IAgentRestrictionsDetailView _view;
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+		private IAgentRestrictionsDetailModel _model;
+
+		public AgentRestrictionsDetailPresenter(IAgentRestrictionsDetailView view, IAgentRestrictionsDetailModel model, ISchedulerStateHolder schedulerState, IGridlockManager lockManager,
+			ClipHandler<IScheduleDay> clipHandler, SchedulePartFilter schedulePartFilter,IOverriddenBusinessRulesHolder overriddenBusinessRulesHolder, 
+			IScheduleDayChangeCallback scheduleDayChangeCallback, IScheduleTag defaultScheduleTag)
+			: base(view, schedulerState, lockManager, clipHandler, schedulePartFilter, overriddenBusinessRulesHolder, scheduleDayChangeCallback, defaultScheduleTag)
+		{
+			_view = view;
+			_model = model;
+		}
+
+		public override int RowCount
+		{
+			get
+			{
+				return 7;
+			}
+		}
+
+		public override int ColCount
+		{
+			get
+			{
+				return 7;
+			}
+		}
+
+		public override void QueryCellInfo(object sender, Syncfusion.Windows.Forms.Grid.GridQueryCellInfoEventArgs e)
+		{
+			if (e == null) return;
+			if (e.RowIndex < 0 || e.ColIndex < 0) return;
+			if (e.RowIndex == 0 && e.ColIndex == 0) return;
+
+			if (e.RowIndex == 0 && e.ColIndex > 0)
+			{
+				e.Style.CellValue = "--Veckodag--";
+				return;
+			}
+
+			if (e.RowIndex > 0 && e.ColIndex == 0)
+			{
+				e.Style.CellValue = "--VeckoNum--";
+				return;
+			}
+
+			e.Style.CellValue = "--ScheduleDay--";		
+		}
 	}
 }
--- Teleopti.Ccc.WinCodeTest/Scheduler/AgentRestrictions/AgentRestrictionsDetailPresenterTest.cs
+++ Teleopti.Ccc.WinCodeTest/Scheduler/AgentRestrictions/AgentRestrictionsDetailPresenterTest.cs
@@ -1,11 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using NUnit.Framework;
+using Syncfusion.Windows.Forms.Grid;
+using Teleopti.Ccc.Domain.Scheduling.Rules;
+using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
+using Teleopti.Ccc.WinCode.Common;
+using Teleopti.Ccc.WinCode.Common.Clipboard;
+using Teleopti.Ccc.WinCode.Scheduling;
+using Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions;
+using Rhino.Mocks;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCodeTest.Scheduler.AgentRestrictions
 {
-	public class AgentRestrictionsDetailPresenterTest
+	[TestFixture]
+	public class AgentRestrictionsDetailPresenterTest : IDisposable
 	{
+		private AgentRestrictionsDetailPresenter _presenter;
+		private IAgentRestrictionsDetailView _view;
+		private IAgentRestrictionsDetailModel _model;
+		private MockRepository _mocks;
+		private ISchedulerStateHolder _schedulerStateHolder;
+		private IGridlockManager _gridlockManager;
+		private ClipHandler<IScheduleDay> _clipHandler;
+		private IOverriddenBusinessRulesHolder _overriddenBusinessRulesHolder;
+		private IScheduleDayChangeCallback _scheduleDayChangeCallback;
+		private GridStyleInfo _info;// = new GridStyleInfo { CellValue = string.Empty };
+
+		[SetUp]
+		public void Setup()
+		{
+			_mocks = new MockRepository();
+			_view = _mocks.StrictMock<IAgentRestrictionsDetailView>();
+			_model = _mocks.StrictMock<IAgentRestrictionsDetailModel>();
+			_schedulerStateHolder = _mocks.StrictMock<ISchedulerStateHolder>();
+			_gridlockManager = _mocks.StrictMock<IGridlockManager>();
+			_clipHandler = new ClipHandler<IScheduleDay>();
+			_overriddenBusinessRulesHolder = _mocks.StrictMock<IOverriddenBusinessRulesHolder>();
+			_scheduleDayChangeCallback = _mocks.DynamicMock<IScheduleDayChangeCallback>();
+			_presenter = new AgentRestrictionsDetailPresenter(_view, _model, _schedulerStateHolder, _gridlockManager, _clipHandler, SchedulePartFilter.None, _overriddenBusinessRulesHolder, _scheduleDayChangeCallback, NullScheduleTag.Instance);
+			_info = new GridStyleInfo();//{ CellValue = string.Empty };
+		}
+
+		[Test]
+		public void ShouldHaveSevenCols()
+		{
+			Assert.AreEqual(7, _presenter.ColCount);	
+		}
+
+		[Test]
+		public void ShouldReturnRowCount()
+		{
+			//just testing testdata at the moment
+			Assert.AreEqual(7, _presenter.RowCount);
+		}
+
+		[Test]
+		public void ShouldQueryCellInfo()
+		{
+			//just testing testdata at the moment
+			
+
+			var e = new GridQueryCellInfoEventArgs(-1, -1, _info);
+			_presenter.QueryCellInfo(null, e);
+			Assert.AreEqual(string.Empty, e.Style.CellValue.ToString());
+
+			e = new GridQueryCellInfoEventArgs(0, 0, _info);
+			_presenter.QueryCellInfo(null, e);
+			Assert.AreEqual(string.Empty, e.Style.CellValue.ToString());
+
+			e = new GridQueryCellInfoEventArgs(0, 1, _info);
+			_presenter.QueryCellInfo(null, e);
+			Assert.AreEqual("--Veckodag--", e.Style.CellValue.ToString());
+
+			e = new GridQueryCellInfoEventArgs(1, 0, _info);
+			_presenter.QueryCellInfo(null, e);
+			Assert.AreEqual("--VeckoNum--", e.Style.CellValue.ToString());
+
+			e = new GridQueryCellInfoEventArgs(1, 1, _info);
+			_presenter.QueryCellInfo(null, e);
+			Assert.AreEqual("--ScheduleDay--", e.Style.CellValue.ToString());
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_info.Dispose();
+			}
+		}
 	}
 }
