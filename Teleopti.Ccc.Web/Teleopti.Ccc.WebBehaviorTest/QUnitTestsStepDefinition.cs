63d5d8924aa7d8edf006680646e5d38a86d47572
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-06-18 15:45:57 +0200

    maybe?

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/QUnitTestsStepDefinition.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/QUnitTestsStepDefinition.cs
@@ -18,13 +18,17 @@ namespace Teleopti.Ccc.WebBehaviorTest
 		[Then(@"I should see the tests run")]
 		public void ThenIShouldSeeTheTestsRun()
 		{
-			EventualAssert.That(() => int.Parse(Browser.Current.Para("qunit-testresult").Span(Find.ByClass("total", false)).InnerHtml), Is.GreaterThan(0));
+			var testResults = Browser.Current.Para("qunit-testresult");
+			EventualAssert.That(() => testResults.Exists, Is.True);
+			EventualAssert.That(() => int.Parse(testResults.Span(Find.ByClass("total", false)).InnerHtml), Is.GreaterThan(0));
 		}
 
 		[Then(@"I should see all tests pass")]
 		public void ThenIShouldSeeAllTestsPass()
 		{
-			EventualAssert.That(() => int.Parse(Browser.Current.Para("qunit-testresult").Span(Find.ByClass("failed", false)).InnerHtml), Is.EqualTo(0));
+			var testResults = Browser.Current.Para("qunit-testresult");
+			EventualAssert.That(() => testResults.Exists, Is.True);
+			EventualAssert.That(() => int.Parse(testResults.Span(Find.ByClass("failed", false)).InnerHtml), Is.EqualTo(0));
 		}
 
 	}
