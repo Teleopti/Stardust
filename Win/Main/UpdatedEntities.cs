65be438050b9417bac7578b6ef0b8743c4792c24
Talha Muhammad <talha.muhammad@teleopti.com>
2012-06-25 16:03:32 +0200

    Fixed Issue 19804: Crash happen when click delete option after open forecast for MultiSite skill
    --HG--
    branch : Release 7.2.362

---
+++ Win/Main/UpdatedEntities.cs
@@ -0,0 +1,11 @@
+﻿using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.MessageBroker.Events;
+
+namespace Teleopti.Ccc.Win.Main
+{
+    public class UpdatedEntities
+    {
+        public IAggregateRoot UpdatedEntity { get; set; }
+        public DomainUpdateType EntityStatus { get; set; }
+    }
+}
