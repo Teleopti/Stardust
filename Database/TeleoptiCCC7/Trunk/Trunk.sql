adb973c2256d95c070dc55710d2f6ce8f1354719
David Jonsson <david.jonsson@teleopti.com>
2012-06-27 21:49:15 +0200

    Make DB work, add missing column

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -15,3 +15,11 @@ UPDATE dbo.Contract
 SET EmploymentType = 0
 WHERE EmploymentType = 2
 GO
+
+----------------  
+--Name: David Jonsson
+--Date: 2012-06-27
+--Desc: Make DB-deploy work
+----------------  
+ALTER TABLE ReadModel.FindPerson
+ADD SearchValueId uniqueidentifier null
