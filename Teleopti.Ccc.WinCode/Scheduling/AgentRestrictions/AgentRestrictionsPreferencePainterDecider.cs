92eee45d65ee20316283e449013abd0ea12b9ce7
Claes Stenman <claes.stenman@teleopti.com>
2012-06-27 07:39:41 +0200

    FxCop...

--- Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsPreferencePainterDecider.cs
+++ Teleopti.Ccc.WinCode/Scheduling/AgentRestrictions/AgentRestrictionsPreferencePainterDecider.cs
@@ -1,71 +1,83 @@
-﻿using Teleopti.Ccc.WinCode.Scheduling.RestrictionSummary;
+﻿using System;
+using Teleopti.Ccc.WinCode.Scheduling.RestrictionSummary;
 
 namespace Teleopti.Ccc.WinCode.Scheduling.AgentRestrictions
 {
 	public class AgentRestrictionsPreferencePainterDecider
 	{
+		private readonly bool _shouldPaint;
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
+		public AgentRestrictionsPreferencePainterDecider()
+		{
+			_shouldPaint = true;
+		}
+		
 		public bool ShouldPaint(IPreferenceCellData preferenceCellData)
 		{
+			if(preferenceCellData == null) throw new ArgumentNullException("preferenceCellData");
+
 			if (!preferenceCellData.Enabled) return false;
 			if (preferenceCellData.SchedulingOption.UseScheduling && (preferenceCellData.HasFullDayAbsence || preferenceCellData.HasShift || preferenceCellData.HasDayOff)) return false;
 			if (!preferenceCellData.SchedulingOption.UsePreferences && !preferenceCellData.SchedulingOption.UseRotations && !preferenceCellData.SchedulingOption.UseAvailability && !preferenceCellData.SchedulingOption.UseStudentAvailability) return false;
-		
-			
-			return true;
+
+			return _shouldPaint;
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
 		public bool ShouldPaintPreferredDayOff(IPreferenceCellData preferenceCellData)
 		{
+			if (preferenceCellData == null) throw new ArgumentNullException("preferenceCellData");
+
 			if (preferenceCellData.EffectiveRestriction == null) return false;	
 			if (preferenceCellData.EffectiveRestriction.DayOffTemplate == null) return false;
 			if ((preferenceCellData.SchedulingOption.UsePreferences && preferenceCellData.SchedulingOption.UseRotations) && preferenceCellData.EffectiveRestriction.ShiftCategory != null) return false;
 
-			return true;
+			return _shouldPaint;
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
 		public bool ShouldPaintPreferredShiftCategory(IPreferenceCellData preferenceCellData)
 		{
+			if (preferenceCellData == null) throw new ArgumentNullException("preferenceCellData");
+
 			if (preferenceCellData.EffectiveRestriction == null) return false;	
 			if (preferenceCellData.EffectiveRestriction.ShiftCategory == null) return false;
 			if ((preferenceCellData.SchedulingOption.UsePreferences && preferenceCellData.SchedulingOption.UseRotations) && preferenceCellData.EffectiveRestriction.DayOffTemplate != null) return false;
 
-			return true;
+			return _shouldPaint;
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
 		public bool ShouldPaintPreferredAbsence(IPreferenceCellData preferenceCellData)
 		{
+			if (preferenceCellData == null) throw new ArgumentNullException("preferenceCellData");
+
 			if (preferenceCellData.EffectiveRestriction == null) return false;
 			if (preferenceCellData.EffectiveRestriction.Absence == null) return false;
 			if (preferenceCellData.HasAbsenceOnContractDayOff) return false;
 			if ((preferenceCellData.SchedulingOption.UsePreferences && preferenceCellData.SchedulingOption.UseRotations) && preferenceCellData.EffectiveRestriction.DayOffTemplate != null) return false;
 
-			return true;
+			return _shouldPaint;
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
 		public bool ShouldPaintPreferredAbsenceOnContractDayOff(IPreferenceCellData preferenceCellData)
 		{
+			if (preferenceCellData == null) throw new ArgumentNullException("preferenceCellData");
+
 			if (preferenceCellData.EffectiveRestriction == null) return false;
 			if (preferenceCellData.EffectiveRestriction.Absence == null) return false;
 			if (!preferenceCellData.HasAbsenceOnContractDayOff) return false;
 			if ((preferenceCellData.SchedulingOption.UsePreferences && preferenceCellData.SchedulingOption.UseRotations) && preferenceCellData.EffectiveRestriction.DayOffTemplate != null) return false;
 
-			return true;
+			return _shouldPaint;
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
 		public bool ShouldPaintPreferredExtended(IPreferenceCellData preferenceCellData)
 		{
+			if (preferenceCellData == null) throw new ArgumentNullException("preferenceCellData");
+
 			if (!preferenceCellData.SchedulingOption.UsePreferences) return false;
 			if (!preferenceCellData.HasExtendedPreference) return false;
 			if (preferenceCellData.SchedulingOption.UseScheduling) return false;
 
-			return true;
+			return _shouldPaint;
 		}
 	}
 }
