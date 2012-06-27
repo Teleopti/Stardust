0e5d3bd86fe5677f739a59fa2cf614197a640acd
David Jonsson <david.jonsson@teleopti.com>
2012-06-27 22:03:41 +0200

    Add another missing column

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -7,6 +7,15 @@
 
 ----------------  
 --Name: Tamas Balog
+--Date: 2012-06-19
+--Desc: Add PeriodTime colums to SchedulePeriod table
+----------------  
+ALTER TABLE dbo.SchedulePeriod ADD
+	PeriodTime bigint NULL
+GO
+
+----------------  
+--Name: Tamas Balog
 --Date: 2012-06-26
 --Desc: Change all FixedStaffPeriodWorkTime contract worktype to FixedStaffNormalWorkType
 ----------------  
