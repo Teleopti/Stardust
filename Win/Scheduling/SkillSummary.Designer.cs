2e4f24b4c7b21aea9bd89bd397fdc16b362187ea
micke@teleopti.com <micke@teleopti.com>
2012-06-29 15:27:54 +0200

    Bug 19898
    --HG--
    branch : Release 7.2.362

--- Win/Scheduling/SkillSummary.Designer.cs
+++ Win/Scheduling/SkillSummary.Designer.cs
@@ -35,15 +35,17 @@
 			this.checkedListBoxSkill = new System.Windows.Forms.CheckedListBox();
 			this.textBoxSummeryName = new System.Windows.Forms.TextBox();
 			this.ribbonControlAdv1 = new Syncfusion.Windows.Forms.Tools.RibbonControlAdvFixed();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			((System.ComponentModel.ISupportInitialize)(this.ribbonControlAdv1)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// buttonAdvOk
 			// 
 			this.buttonAdvOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonAdvOk.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Office2007;
-			this.buttonAdvOk.Location = new System.Drawing.Point(292, 280);
-			this.buttonAdvOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.buttonAdvOk.Location = new System.Drawing.Point(333, 285);
+			this.buttonAdvOk.Margin = new System.Windows.Forms.Padding(4);
 			this.buttonAdvOk.Name = "buttonAdvOk";
 			this.buttonAdvOk.Office2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Managed;
 			this.buttonAdvOk.Size = new System.Drawing.Size(100, 28);
@@ -57,8 +59,8 @@
 			this.buttonAdvCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonAdvCancel.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Office2007;
 			this.buttonAdvCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonAdvCancel.Location = new System.Drawing.Point(400, 280);
-			this.buttonAdvCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.buttonAdvCancel.Location = new System.Drawing.Point(441, 285);
+			this.buttonAdvCancel.Margin = new System.Windows.Forms.Padding(4);
 			this.buttonAdvCancel.Name = "buttonAdvCancel";
 			this.buttonAdvCancel.Office2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Managed;
 			this.buttonAdvCancel.Size = new System.Drawing.Size(100, 28);
@@ -69,50 +71,50 @@
 			// labelSkill
 			// 
 			this.labelSkill.AutoSize = true;
-			this.labelSkill.Location = new System.Drawing.Point(24, 85);
+			this.labelSkill.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.labelSkill.Location = new System.Drawing.Point(4, 34);
 			this.labelSkill.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.labelSkill.Name = "labelSkill";
-			this.labelSkill.Size = new System.Drawing.Size(52, 17);
+			this.labelSkill.Size = new System.Drawing.Size(192, 182);
 			this.labelSkill.TabIndex = 6;
 			this.labelSkill.Text = "xxSkills";
 			// 
 			// labelSummeryName
 			// 
 			this.labelSummeryName.AutoSize = true;
-			this.labelSummeryName.Location = new System.Drawing.Point(24, 60);
+			this.labelSummeryName.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.labelSummeryName.Location = new System.Drawing.Point(4, 0);
 			this.labelSummeryName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.labelSummeryName.Name = "labelSummeryName";
-			this.labelSummeryName.Size = new System.Drawing.Size(116, 17);
+			this.labelSummeryName.Size = new System.Drawing.Size(192, 34);
 			this.labelSummeryName.TabIndex = 5;
 			this.labelSummeryName.Text = "xxSummeryName";
+			this.labelSummeryName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// checkedListBoxSkill
 			// 
-			this.checkedListBoxSkill.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.checkedListBoxSkill.CheckOnClick = true;
+			this.checkedListBoxSkill.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.checkedListBoxSkill.FormattingEnabled = true;
-			this.checkedListBoxSkill.Location = new System.Drawing.Point(192, 85);
-			this.checkedListBoxSkill.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.checkedListBoxSkill.Location = new System.Drawing.Point(204, 38);
+			this.checkedListBoxSkill.Margin = new System.Windows.Forms.Padding(4);
 			this.checkedListBoxSkill.Name = "checkedListBoxSkill";
-			this.checkedListBoxSkill.Size = new System.Drawing.Size(308, 174);
+			this.checkedListBoxSkill.Size = new System.Drawing.Size(305, 174);
 			this.checkedListBoxSkill.TabIndex = 2;
 			// 
 			// textBoxSummeryName
 			// 
-			this.textBoxSummeryName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.textBoxSummeryName.Location = new System.Drawing.Point(191, 55);
-			this.textBoxSummeryName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBoxSummeryName.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBoxSummeryName.Location = new System.Drawing.Point(204, 4);
+			this.textBoxSummeryName.Margin = new System.Windows.Forms.Padding(4);
 			this.textBoxSummeryName.Name = "textBoxSummeryName";
-			this.textBoxSummeryName.Size = new System.Drawing.Size(308, 22);
+			this.textBoxSummeryName.Size = new System.Drawing.Size(305, 22);
 			this.textBoxSummeryName.TabIndex = 1;
 			// 
 			// ribbonControlAdv1
 			// 
 			this.ribbonControlAdv1.Location = new System.Drawing.Point(1, 0);
-			this.ribbonControlAdv1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.ribbonControlAdv1.Margin = new System.Windows.Forms.Padding(4);
 			this.ribbonControlAdv1.MenuButtonText = "";
 			this.ribbonControlAdv1.MenuButtonVisible = false;
 			this.ribbonControlAdv1.MinimumSize = new System.Drawing.Size(0, 41);
@@ -126,10 +128,31 @@
 			this.ribbonControlAdv1.SelectedTab = null;
 			this.ribbonControlAdv1.ShowLauncher = false;
 			this.ribbonControlAdv1.ShowMinimizeButton = false;
-			this.ribbonControlAdv1.Size = new System.Drawing.Size(513, 41);
+			this.ribbonControlAdv1.Size = new System.Drawing.Size(553, 41);
 			this.ribbonControlAdv1.SystemText.QuickAccessDialogDropDownName = "xxStart menu";
 			this.ribbonControlAdv1.TabIndex = 0;
 			this.ribbonControlAdv1.Text = "xxSkillSummery";
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.labelSummeryName, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.labelSkill, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.textBoxSummeryName, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.checkedListBoxSkill, 1, 1);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(24, 65);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(513, 199);
+			this.tableLayoutPanel1.TabIndex = 7;
 			// 
 			// SkillSummary
 			// 
@@ -137,21 +160,19 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.buttonAdvCancel;
-			this.ClientSize = new System.Drawing.Size(515, 318);
-			this.Controls.Add(this.labelSkill);
-			this.Controls.Add(this.labelSummeryName);
+			this.ClientSize = new System.Drawing.Size(555, 354);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Controls.Add(this.buttonAdvCancel);
 			this.Controls.Add(this.buttonAdvOk);
-			this.Controls.Add(this.checkedListBoxSkill);
-			this.Controls.Add(this.textBoxSummeryName);
 			this.Controls.Add(this.ribbonControlAdv1);
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.MinimumSize = new System.Drawing.Size(290, 60);
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MinimumSize = new System.Drawing.Size(290, 59);
 			this.Name = "SkillSummary";
 			this.Text = "xxSkillSummery";
 			((System.ComponentModel.ISupportInitialize)(this.ribbonControlAdv1)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
         }
 
@@ -164,5 +185,6 @@
         private Syncfusion.Windows.Forms.ButtonAdv buttonAdvCancel;
         private System.Windows.Forms.Label labelSummeryName;
         private System.Windows.Forms.Label labelSkill;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
