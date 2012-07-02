81d2eedd1530fe8ad35eeefcd4c3c127e99e894c
David Jonsson <david.jonsson@teleopti.com>
2012-07-02 10:27:33 +0200

    Remove old MessageBroker from Azure install

--- ccnet/Azure/TeleoptiCCC/bin/InstallService.cmd
+++ ccnet/Azure/TeleoptiCCC/bin/InstallService.cmd
@@ -15,19 +15,8 @@ SC QUERY TeleoptiETLService
 IF NOT ERRORLEVEL 1060 CALL :ServiceError
 "%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe" "..\Services\ETL\Service\Teleopti.Analytics.Etl.ServiceHost.exe" >> Install.log
 
-SC QUERY TeleoptiBrokerService
-IF NOT ERRORLEVEL 1060 CALL :ServiceError
-"%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe" "..\Services\MessageBroker\Teleopti.Messaging.Svc.exe" >> Install.log
-
 ECHO. >> Install.log
 ECHO Done install! >> Install.log
-
-::================
-::Open Firewall for TeleoptiBrokerService
-::================
-::ECHO. >> Install.log
-ECHO Open up local firewall rule for MsgBroker broker. >> Install.log
-netsh advfirewall firewall add rule name = TeleoptiBrokerService dir = in protocol = tcp action = allow localport = 10000 profile=PUBLIC
 
 ::================
 ::Install Report Viewer
@@ -44,17 +33,8 @@ ping 127.0.0.1 -n 5 > nul
 ECHO. >> Install.log
 ECHO Starting services... >> Install.log
 
-NET START TeleoptiBrokerService
-IF %ERRORLEVEL% NEQ 0 CALL :ServiceStartError TeleoptiBrokerService
-
 NET START AnalyticsEtlService
-IF %ERRORLEVEL% NEQ 0 CALL :ServiceStartError AnalyticsEtlService
-
-IF %ServiceStartError% EQU 0
-ECHO Services started successfully >> Install.log
-
-IF %ServiceStartError% NEQ 0
-ECHO Error: Some services could not be started!! >> Install.log
+IF %ERRORLEVEL% NEQ 0 ECHO Error: ETL services could not be started!! >> Install.log
 
 ECHO.
 ECHO Done!
