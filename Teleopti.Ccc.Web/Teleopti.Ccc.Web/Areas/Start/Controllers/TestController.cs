cc23e984f2542fa60098abbccab2e76205c464ea
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-07-03 12:32:32 +0200

    test controller Logon method

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Controllers/TestController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Controllers/TestController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using Teleopti.Ccc.Web.Areas.Start.Core.Authentication.DataProvider;
+using Teleopti.Ccc.Web.Areas.Start.Core.Authentication.Services;
 using Teleopti.Ccc.Web.Core.RequestContext;
 using Teleopti.Ccc.Web.Core.RequestContext.Cookie;
 using Teleopti.Interfaces.Domain;
@@ -10,16 +13,33 @@ namespace Teleopti.Ccc.Web.Areas.Start.Controllers
 	public class TestController : Controller
 	{
 		private readonly ISessionSpecificDataProvider _sessionSpecificDataProvider;
+		private readonly IAuthenticator _authenticator;
+		private readonly IWebLogOn _logon;
+		private readonly IBusinessUnitProvider _businessUnitProvider;
 
-		public TestController(ISessionSpecificDataProvider sessionSpecificDataProvider)
+		public TestController(ISessionSpecificDataProvider sessionSpecificDataProvider, IAuthenticator authenticator, IWebLogOn logon, IBusinessUnitProvider businessUnitProvider)
 		{
 			_sessionSpecificDataProvider = sessionSpecificDataProvider;
+			_authenticator = authenticator;
+			_logon = logon;
+			_businessUnitProvider = businessUnitProvider;
 		}
 
 		public ActionResult AfterScenario()
 		{
 			_sessionSpecificDataProvider.RemoveCookie();
 			return new FilePathResult(@"~\Areas\Start\Views\Test\AfterScenario.html", "text/html");
+		}
+
+		// "SignIn.DataSourceName={0}&SignIn.UserName={1}&SignIn.Password={2}&X-Requested-With=XMLHttpRequest";
+		// dataSourceName=TestData&businessUnitName=BusinessUnit&userName=1&password=1
+		public ActionResult Login(string dataSourceName, string businessUnitName, string userName, string password)
+		{
+			var result = _authenticator.AuthenticateApplicationUser(dataSourceName, userName, password);
+			var businessUnits = _businessUnitProvider.RetrieveBusinessUnitsForPerson(result.DataSource, result.Person);
+			var businessUnit = (from b in businessUnits where b.Name == businessUnitName select b).Single();
+			_logon.LogOn(businessUnit.Id.Value, dataSourceName, result.Person.Id.Value, AuthenticationTypeOption.Application);
+			return Content("Signed in as " + result.Person.Name.ToString(), "text/html");
 		}
 
 		public EmptyResult ExpireMyCookie()
