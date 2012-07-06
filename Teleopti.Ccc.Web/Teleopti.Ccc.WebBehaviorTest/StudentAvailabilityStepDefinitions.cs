a5bf3598493e1761b3d1cf15f2a6e1dd19e68024
Robin Karlsson <robin.karlsson@teleopti.com>
2012-07-06 19:40:38 +0200

    ...

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/StudentAvailabilityStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/StudentAvailabilityStepDefinitions.cs
@@ -227,7 +227,7 @@ namespace Teleopti.Ccc.WebBehaviorTest
 
 		private void cellShouldContainInputValues(DateTime date)
 		{
-			EventualAssert.WhenElementExists(_page.CalendarCellForDate(date), c => c.ClassName, Is.StringContaining("unvalidated"));
+			EventualAssert.WhenElementExists(_page.CalendarCellForDate(date), c => c.Parent.ClassName, Is.StringContaining("unvalidated"));
 			EventualAssert.That(() => _page.CalendarCellForDate(date).InnerHtml, Is.StringContaining("05:00"));
 			EventualAssert.That(() => _page.CalendarCellForDate(date).InnerHtml, Is.StringContaining("14:00"));
 		}
