869437ec37ae75c7147c99cf472ac017323d6fdf
Talha Muhammad <talha.muhammad@teleopti.com>
2012-07-10 16:58:48 +0200

    Fixed Fx-Cop Issues : 19974 - Only for Default branch

--- Teleopti.Ccc.WinCodeTest/AddAbsenceCommandTest.cs
+++ Teleopti.Ccc.WinCodeTest/AddAbsenceCommandTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using Rhino.Mocks;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.Domain.Time;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.WinCode.Common;
@@ -30,6 +31,7 @@ namespace Teleopti.Ccc.WinCodeTest
         private readonly DateTime _date = new DateTime(2012, 07, 16, 0, 0, 0, DateTimeKind.Utc);
         private IScheduleDay _schedulePart;
         private IScheduleRange _scheduleRange;
+        private DateOnlyPeriod _dateOnlyPeriod;
 
         [SetUp]
         public void Setup()
@@ -39,9 +41,10 @@ namespace Teleopti.Ccc.WinCodeTest
             _scenario = _mocks.DynamicMock<IScenario>();
             _dialog = _mocks.StrictMock<IAddLayerViewModel<IAbsence>>();
             _period = new DateTimePeriod(2012, 7, 16, 2012, 7, 16);
+            _dateOnlyPeriod = new DateOnlyPeriod(2012, 7, 16, 2012, 7, 16);
             _selectedSchedules = new List<IScheduleDay>();
             _schedulePart = _mocks.DynamicMock<IScheduleDay>();
-            _schedulerStateHolder = new SchedulerStateHolder(_scenario, _period, new List<IPerson>());
+            _schedulerStateHolder = new SchedulerStateHolder(_scenario, new DateOnlyPeriodAsDateTimePeriod(_dateOnlyPeriod, TeleoptiPrincipal.Current.Regional.TimeZone), new List<IPerson>());
             _selectedItem = _mocks.StrictMock<IAbsence>();
             _schedulePresenterBase = _mocks.DynamicMock<ISchedulePresenterBase>();
             _target = new AddAbsenceCommand(_schedulerStateHolder, _viewBase, _schedulePresenterBase, _selectedSchedules);
