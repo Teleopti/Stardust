ab2a1ea68d85c66d28a76b8b04cf01694e77f5d7
micke@teleopti.com <micke@teleopti.com>
2012-07-18 14:45:19 +0200

    New class to set specification

---
+++ Domain/Optimization/MainShiftOptimizeActivitySpecificationSetter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Optimization
+{
+	public interface IMainShiftOptimizeActivitySpecificationSetter
+	{
+		void SetSpecification(ISchedulingOptions schedulingOptions, IOptimizationPreferences optimizationPreferences, IMainShift mainShift, DateOnly viewDate);
+	}
+
+	public class MainShiftOptimizeActivitySpecificationSetter : IMainShiftOptimizeActivitySpecificationSetter
+	{
+		public void SetSpecification(ISchedulingOptions schedulingOptions, IOptimizationPreferences optimizationPreferences, IMainShift mainShift, DateOnly viewDate)
+		{
+			IOptimizerActivitiesPreferences optimizerActivitiesPreferences = new OptimizerActivitiesPreferences();
+			optimizerActivitiesPreferences.KeepShiftCategory = optimizationPreferences.Shifts.KeepShiftCategories;
+			optimizerActivitiesPreferences.KeepStartTime = optimizationPreferences.Shifts.KeepStartTimes;
+			optimizerActivitiesPreferences.KeepEndTime = optimizationPreferences.Shifts.KeepEndTimes;
+			//throw new NotImplementedException();
+			optimizerActivitiesPreferences.AllowAlterBetween = new TimePeriod(TimeSpan.FromHours(0), TimeSpan.FromHours(36));
+			optimizerActivitiesPreferences.SetDoNotMoveActivities(new List<IActivity>());
+
+			schedulingOptions.MainShiftOptimizeActivitySpecification = new MainShiftOptimizeActivitiesSpecification(optimizerActivitiesPreferences, mainShift, viewDate, StateHolderReader.Instance.StateReader.SessionScopeData.TimeZone);
+		}
+	}
+}
---
+++ DomainTest/Optimization/MainShiftOptimizeActivitySpecificationSetterTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Specification;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization
+{
+	[TestFixture]
+	public class MainShiftOptimizeActivitySpecificationSetterTest
+	{
+		private IMainShiftOptimizeActivitySpecificationSetter _target;
+		private ISchedulingOptionsCreator _schedulingOptionsCreator;
+		private IOptimizationPreferences _optimizationPreferences;
+		private IMainShift _mainShift;
+
+		[SetUp]
+		public void Setup()
+		{
+			_target = new MainShiftOptimizeActivitySpecificationSetter();
+			_schedulingOptionsCreator = new SchedulingOptionsCreator();
+			_optimizationPreferences = new OptimizationPreferences();
+			_mainShift = MainShiftFactory.CreateMainShiftWithThreeActivityLayers();
+		}
+
+		[Test]
+		public void ShouldSetCorrectValues()
+		{
+			_optimizationPreferences.Shifts.AlterBetween = true;
+			_optimizationPreferences.Shifts.KeepEndTimes = true;
+			_optimizationPreferences.Shifts.KeepShiftCategories = true;
+			_optimizationPreferences.Shifts.KeepStartTimes = true;
+			_optimizationPreferences.Shifts.SelectedActivitiesGuids = new List<Guid>();
+
+			ISchedulingOptions schedulingOptions = _schedulingOptionsCreator.CreateSchedulingOptions(_optimizationPreferences);
+
+			var minShiftOptimizeActivitySpecification = schedulingOptions.MainShiftOptimizeActivitySpecification;
+			Assert.AreEqual(typeof(All<IMainShift>), minShiftOptimizeActivitySpecification.GetType());
+
+			_target.SetSpecification(schedulingOptions, _optimizationPreferences, _mainShift, DateOnly.MinValue);
+
+			IOptimizerActivitiesPreferences optimizerActivitiesPreferences = new OptimizerActivitiesPreferences();
+			optimizerActivitiesPreferences.KeepShiftCategory = _optimizationPreferences.Shifts.KeepShiftCategories;
+			optimizerActivitiesPreferences.KeepStartTime = _optimizationPreferences.Shifts.KeepStartTimes;
+			optimizerActivitiesPreferences.KeepEndTime = _optimizationPreferences.Shifts.KeepEndTimes;
+			//throw new NotImplementedException();
+			optimizerActivitiesPreferences.AllowAlterBetween = new TimePeriod(TimeSpan.FromHours(0), TimeSpan.FromHours(36));
+			optimizerActivitiesPreferences.SetDoNotMoveActivities(new List<IActivity>());
+
+			minShiftOptimizeActivitySpecification = new MainShiftOptimizeActivitiesSpecification(optimizerActivitiesPreferences, _mainShift, DateOnly.MinValue, StateHolderReader.Instance.StateReader.SessionScopeData.TimeZone);
+
+			Assert.AreEqual(typeof(MainShiftOptimizeActivitiesSpecification), minShiftOptimizeActivitySpecification.GetType());
+		}
+	}
+}
