7645caef6681ed7f429d368b109d34d9cd087c03
Asad MIrza <asad.mirza@teleopti.com>
2012-08-06 10:24:16 +0200

    Fixed the setting to be saved in the Request Filter
    --HG--
    branch : Release 7.2.362

--- Teleopti.Ccc.WinCode/Common/FilterBoxAdvancedFilter.cs
+++ Teleopti.Ccc.WinCode/Common/FilterBoxAdvancedFilter.cs
@@ -5,17 +5,27 @@ using System.Text;
 
 namespace Teleopti.Ccc.WinCode.Common
 {
+    [Serializable]
     public class FilterBoxAdvancedFilter
     {
         private object _filterOn;
         private FilterAdvancedTupleItem _filterOperand;
         private FilterAdvancedTupleItem _filterCriteria;
+        private object _filterValue;
 
         public FilterBoxAdvancedFilter(object filterOn, FilterAdvancedTupleItem filterOperand, FilterAdvancedTupleItem filterCriteria)
         {
             _filterOn = filterOn;
             _filterOperand = filterOperand;
             _filterCriteria = filterCriteria;
+        }
+
+        public FilterBoxAdvancedFilter(object filterOn, FilterAdvancedTupleItem filterOperand, FilterAdvancedTupleItem filterCriteria, object filterValue)
+        {
+            _filterOn = filterOn;
+            _filterOperand = filterOperand;
+            _filterCriteria = filterCriteria;
+            _filterValue = filterValue;
         }
 
         public object FilterOn
@@ -31,6 +41,11 @@ namespace Teleopti.Ccc.WinCode.Common
         public FilterAdvancedTupleItem FilterCriteria
         {
             get { return _filterCriteria; }
+        }
+
+        public object FilterValue
+        {
+            get { return _filterValue; }
         }
     }
 }
