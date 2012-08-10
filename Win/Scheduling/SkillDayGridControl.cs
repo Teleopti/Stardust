6b9bfb3a3b8224c44d8e44a4aba1295fff023dc5
ola@teleopti.com <ola@teleopti.com>
2012-08-10 11:02:20 +0200

    fix after merge

--- Win/Scheduling/SkillDayGridControl.cs
+++ Win/Scheduling/SkillDayGridControl.cs
@@ -266,7 +266,7 @@ namespace Teleopti.Ccc.Win.Scheduling
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
 		public void SetDataSource(ISchedulerStateHolder stateHolder,ISkill skill)
 		{
-			var dateTimePeriods = stateHolder.RequestedPeriod.WholeDayCollection();
+			var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection();
 			_dates = dateTimePeriods.Select(d => new DateOnly(TimeZoneHelper.ConvertFromUtc(d.StartDateTime, stateHolder.TimeZoneInfo))).ToList();
 
 			// in ViewPoint
@@ -274,7 +274,7 @@ namespace Teleopti.Ccc.Win.Scheduling
 			// user timeZone
 			var userTimeZone = TeleoptiPrincipal.Current.Regional.TimeZone;
 			var diff = userTimeZone.BaseUtcOffset - timeZone.BaseUtcOffset;
-			var viewPointPeriod = stateHolder.RequestedPeriod.MovePeriod(diff);
+			var viewPointPeriod = stateHolder.RequestedPeriod.Period().MovePeriod(diff);
 
 			dateTimePeriods = viewPointPeriod.WholeDayCollection();
 			var dataSource = createDataSourceDictionary(dateTimePeriods, stateHolder, skill);
@@ -292,7 +292,7 @@ namespace Teleopti.Ccc.Win.Scheduling
                 Model.Options.MergeCellsMode = GridMergeCellsMode.OnDemandCalculation |
                                                GridMergeCellsMode.MergeColumnsInRow;
 
-                var dateTimePeriods = stateHolder.RequestedPeriod.WholeDayCollection();
+                var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection();
                 _dates = dateTimePeriods.Select(d => new DateOnly(TimeZoneHelper.ConvertFromUtc(d.StartDateTime, stateHolder.TimeZoneInfo))).ToList();
                 createGridRows(skill, _dates, stateHolder);
             	SetDataSource(stateHolder, skill);
