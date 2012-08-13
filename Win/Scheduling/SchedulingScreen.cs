b42f4dc5cbe1d5300a576b956e1addb6167fbcfb
ola@teleopti.com <ola@teleopti.com>
2012-08-13 09:42:32 +0200

    bug #20136
    --HG--
    branch : Release 7.2.362

--- Win/Scheduling/SchedulingScreen.cs
+++ Win/Scheduling/SchedulingScreen.cs
@@ -4774,6 +4774,7 @@ namespace Teleopti.Ccc.Win.Scheduling
                 if (_scheduleCounter >= _optimizerOriginalPreferences.SchedulingOptions.RefreshRate)
                 {
                     _grid.Invalidate();
+					refreshSummarySkillIfActive();
                     _skillIntradayGridControl.RefreshGrid();
                     _skillDayGridControl.RefreshGrid();
 					refreshChart();
