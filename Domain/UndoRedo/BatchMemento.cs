91b3363bc6b5797ee78825afc71da6182c88714a
Roger Kratz <roger.kratz@teleopti.com>
2012-08-15 10:35:22 +0200

    tabified undo stuff

--- Domain/UndoRedo/BatchMemento.cs
+++ Domain/UndoRedo/BatchMemento.cs
@@ -4,27 +4,27 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.UndoRedo
 {
-    public class BatchMemento : IMemento
-    {
-        public BatchMemento(string description)
-        {
-            MementoCollection = new List<IMemento>();
-            Description = description;
-            Time = DateTime.Now;
-        }
+	public class BatchMemento : IMemento
+	{
+		public BatchMemento(string description)
+		{
+			MementoCollection = new List<IMemento>();
+			Description = description;
+			Time = DateTime.Now;
+		}
 
-        public IList<IMemento> MementoCollection { get; private set; }
+		public IList<IMemento> MementoCollection { get; private set; }
 
-        public DateTime Time { get; private set; }
+		public DateTime Time { get; private set; }
 
-        public string Description { get; private set; }
+		public string Description { get; private set; }
 
-        public IMemento Restore()
-        {
-            var inverse = new BatchMemento(Description);
-            for (var i = MementoCollection.Count - 1; i >= 0; i--)
-                inverse.MementoCollection.Add(MementoCollection[i].Restore());
-            return inverse;
-        }
-    }
+		public IMemento Restore()
+		{
+			var inverse = new BatchMemento(Description);
+			for (var i = MementoCollection.Count - 1; i >= 0; i--)
+				inverse.MementoCollection.Add(MementoCollection[i].Restore());
+			return inverse;
+		}
+	}
 }
