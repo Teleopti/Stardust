0f22416406c9ee726a93db498be321b6246a2694
David Jonsson <david.jonsson@teleopti.com>
2012-08-16 16:15:17 +0200

    #20187 - MyTimeWeb Framework version

--- ccnet/Azure/TeleoptiCCC/bin/ChangeAppPoolVersion.ps1
+++ ccnet/Azure/TeleoptiCCC/bin/ChangeAppPoolVersion.ps1
@@ -1,65 +1,81 @@
 param ([string] $applicationName = $(throw "applicationName is required"))
-Write-Host $applicationName
-
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Web.Administration")
 
 $iis = New-Object Microsoft.Web.Administration.ServerManager 
-
 $attemptSiteCount = 0;
 $attemptAppCount = 0;
 $attemptPathCount = 0;
-$MaxAttemptCount = 20;
+$attemptSuccess = 0;
+$MaxAttemptCount = 10;
+$sleep = 30
 
+#----------
 # functions
+#----------
 function fnChangeAppPoolVersion {
 param ([System.Object]$arg0)
 	$appPoolName = $arg0.ApplicationPoolName;
-	Write-Output $("Set .NET v4.0 as ManagedRuntimeVersion on: $appPoolName")
+	Write-host $("Set .NET v4.0 as ManagedRuntimeVersion on: $appPoolName ")
 	$apppoolobject = $iis.ApplicationPools[$appPoolName]
 	$apppoolobject.ManagedRuntimeVersion = "v4.0";
 	$iis.CommitChanges();
 }
 
-# =======
+function fnLoopSiteApplication {
+    # wait for web site(s) to be created
+    do {
+    	if ($iis.sites)
+    	{
+    	break
+    	}
+        Write-host $("No sites found. Sleeping for $sleep secs");
+    	$attemptSiteCount++
+    	Start-Sleep -s $sleep
+    }
+    while ($attemptSiteCount -le $MaxAttemptCount)
+
+    foreach ($site_name in $iis.sites)
+    { 
+    	# wait for application(s) to be created
+    	do {
+    		if ($site_name.applications)
+    		{
+    		  break
+    		}
+            Write-host $("No applications found. Sleeping for $sleep secs ");
+    		$attemptAppCount++
+    		Start-Sleep -s $sleep
+    	}
+    	while ($attemptAppCount -le $MaxAttemptCount)
+
+    	do {
+    		foreach ($siteApplication in $site_name.applications)
+             {
+    		   if ($siteApplication.Path.EndsWith("/" + $applicationName))
+    		   {
+                    fnChangeAppPoolVersion($siteApplication);
+                    Return $True
+    			}
+              }
+            $attemptPathCount++
+            Write-host $("siteFound is empty. Sleeping for $sleep secs");
+            Start-Sleep -s $sleep
+    	}
+    	while ($attemptPathCount -le $MaxAttemptCount)
+    }
+    Return $False
+}
+
+#----------
 # Main
-# =======
-# wait for web site(s) to be created
+#----------
 do {
-	if ($iis.sites)
-	{
-	break
-	}
-	$attemptSiteCount++
-	Start-Sleep -s 30
+    If (fnLoopSiteApplication) {
+        write-host "Success!"
+        break
+    }else{
+    write-host "Could not find the correct application. Trying again ... $attemptSuccess"
+    $attemptSuccess++
+    }
 }
-while ($attemptSiteCount -le $MaxAttemptCount)
-
-foreach ($site_name in $iis.sites)
-{ 
-	# wait for application(s) to be created
-	do {
-		if ($site_name.applications)
-		{
-		  break
-		}
-		$attemptAppCount++
-		Start-Sleep -s 30
-	}
-	while ($attemptAppCount -le $MaxAttemptCount)
-
-	do {
-		foreach ($siteApplication in $site_name.applications)
-         {
-		   if ($siteApplication.Path.EndsWith("/" + $applicationName))
-		   {
-                $siteFound = $siteApplication.Path.EndsWith("/" + $applicationName);
-                fnChangeAppPoolVersion($siteApplication)
-				break
-			}
-		}
-			$attemptPathCount++
-			if (!$siteFound)
-            {Start-Sleep -s 30}
-	}
-	while ((!$siteFound) -and ($attemptPathCount -le $MaxAttemptCount))
-}
+while ($attemptSuccess -le $MaxAttemptCount)
