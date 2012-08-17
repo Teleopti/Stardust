7b08663efed2d7514ca1ab8e2a2e49e8255b9579
David Jonsson <david.jonsson@teleopti.com>
2012-08-17 09:59:28 +0200

    #20187 - simplify and more debug info

--- ccnet/Azure/TeleoptiCCC/bin/ChangeAppPoolVersion.ps1
+++ ccnet/Azure/TeleoptiCCC/bin/ChangeAppPoolVersion.ps1
@@ -4,92 +4,62 @@ param ([string] $applicationName = $(throw "applicationName is required"))
 #----------
 # functions
 #----------
+function fnLoopSiteApplication {
+    foreach ($site_name in $iis.sites)
+    { 
+        foreach ($siteApplication in $site_name.applications)
+        {
+            write-host "$siteApplication `r"
+            if ($siteApplication.Path.EndsWith("/" + $applicationName))
+            {
+                fnChangeAppPoolVersion($siteApplication);
+                Return $True
+            }
+        }
+    }
+    Return $False
+}
+
 function fnChangeAppPoolVersion {
 param ([System.Object]$arg0)
 	$appPoolName = $arg0.ApplicationPoolName;
-	Write-host $("Set .NET v4.0 as ManagedRuntimeVersion on: $appPoolName ")
 	$apppoolobject = $iis.ApplicationPools[$appPoolName]
 	$apppoolobject.ManagedRuntimeVersion = "v4.0";
 	$iis.CommitChanges();
 }
 
-function fnLoopSiteApplication {
-    # wait for web site(s) to be created
-    do {
-    	if ($iis.sites)
-    	{
-    	break
-    	}
-        Write-host $("No sites found. Sleeping for $sleep secs");
-    	$attemptSiteCount++
-    	Start-Sleep -s $sleep
-    }
-    while ($attemptSiteCount -le $MaxAttemptCount)
-
-    foreach ($site_name in $iis.sites)
-    { 
-    	# wait for application(s) to be created
-    	do {
-    		if ($site_name.applications)
-    		{
-    		  break
-    		}
-            Write-host $("No applications found. Sleeping for $sleep secs ");
-    		$attemptAppCount++
-    		Start-Sleep -s $sleep
-    	}
-    	while ($attemptAppCount -le $MaxAttemptCount)
-
-    	do {
-    		foreach ($siteApplication in $site_name.applications)
-             {
-    		   if ($siteApplication.Path.EndsWith("/" + $applicationName))
-    		   {
-                    fnChangeAppPoolVersion($siteApplication);
-                    Return $True
-    			}
-              }
-            $attemptPathCount++
-            Write-host $("siteFound is empty. Sleeping for $sleep secs");
-            Start-Sleep -s $sleep
-    	}
-    	while ($attemptPathCount -le $MaxAttemptCount)
-    }
-    Return $False
-}
-
 #----------
 # Main
 #----------
-$attemptSiteCount = 0;
-$attemptAppCount = 0;
-$attemptPathCount = 0;
 $attemptSuccess = 0;
 $MaxAttemptCount = 20;
 $sleep = 30
-
 do {
 
     try {
+        Write-host "$(get-date -format 'yyyy-MM-dd HH:mm:ss.ms')`r"
         $iis = New-Object Microsoft.Web.Administration.ServerManager 
         If (fnLoopSiteApplication) {
-            write-host "Success!"
+            Write-host "Success!"
             break
-        }else{
-        write-host "Could not find the correct application. Trying again ... $attemptSuccess"
+        }
+        else
+        {
+        Write-host "Could not find the correct application. Trying again ... $attemptSuccess `r"
         $attemptSuccess++
+        Start-Sleep -s $sleep
         }
     }
 
     Catch {
-     "Caught in a catch"
      $err=$Error[0]
-     $err
-     $err |Format-List *
+     Write-host "$err"
+     break
      }
  
    finally {
-    $iis.Dispose()
+    $iis.Dispose();
     }
 }
 while ($attemptSuccess -le $MaxAttemptCount)
+Write-host "$(get-date -format 'yyyy-MM-dd HH:mm:ss.ms')"
