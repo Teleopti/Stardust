e14410a1b69ad7bb6f64556fdc1e528e23e51713
Kunning Mao <kunning.mao@teleopti.com>
2012-08-23 11:48:10 +0200

    #20260 Night shifts display on the same day (not next day)

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/WeekSchedule/ViewModelFactory/PeriodViewModelFactory.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/WeekSchedule/ViewModelFactory/PeriodViewModelFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using Teleopti.Ccc.Domain.Helper;
@@ -8,47 +9,65 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Core.WeekSchedule.ViewModelFactory
 {
-    public class PeriodViewModelFactory : IPeriodViewModelFactory
-    {
-        public IEnumerable<PeriodViewModel> CreatePeriodViewModels(IVisualLayerCollection visualLayerCollection, TimePeriod minMaxTime)
-        {
-            foreach (var visualLayer in visualLayerCollection)
-            {
-                MeetingViewModel meetingModel = null;
-                
-                var meetingPayload = visualLayer.Payload as IMeetingPayload;
-                if (meetingPayload!=null)
-                {
-                    meetingModel = new MeetingViewModel
-                                       {
-                                           Location = meetingPayload.Meeting.GetLocation(new NoFormatting()),
-                                           Title = meetingPayload.Meeting.GetSubject(new NoFormatting())
-                                       };
-                }
-                
-                yield return
-                    new PeriodViewModel
-                        {
-                            Summary =
-                                TimeHelper.GetLongHourMinuteTimeString(visualLayer.Period.ElapsedTime(),
-                                                                       CultureInfo.CurrentUICulture),
-                            Title = visualLayer.DisplayDescription().Name,
-                            TimeSpan =
-                                visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).
-                                ToShortTimeString(CultureInfo.CurrentUICulture),
-                            StyleClassName = colorToString(visualLayer.DisplayColor()),
-                            Meeting = meetingModel,
-                            Color = visualLayer.DisplayColor().ToCSV(),
-                            StartPositionPercentage = (decimal)(visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).StartTime - minMaxTime.StartTime).Ticks / (minMaxTime.EndTime - minMaxTime.StartTime).Ticks,
-                            EndPositionPercentage = (decimal)(visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).EndTime - minMaxTime.StartTime).Ticks / (minMaxTime.EndTime - minMaxTime.StartTime).Ticks
-                        };
-            }
-        }
+	public class PeriodViewModelFactory : IPeriodViewModelFactory
+	{
+		public IEnumerable<PeriodViewModel> CreatePeriodViewModels(IVisualLayerCollection visualLayerCollection, TimePeriod minMaxTime)
+		{
+			foreach (var visualLayer in visualLayerCollection)
+			{
+				MeetingViewModel meetingModel = null;
 
-        private static string colorToString(Color color)
-        {
-            var colorCode = color.ToArgb();
-            return string.Concat("color_", ColorTranslator.ToHtml(Color.FromArgb(colorCode)).Replace("#", ""));
-        }
-    }
+				var meetingPayload = visualLayer.Payload as IMeetingPayload;
+				if (meetingPayload != null)
+				{
+					meetingModel = new MeetingViewModel
+					{
+						Location = meetingPayload.Meeting.GetLocation(new NoFormatting()),
+						Title = meetingPayload.Meeting.GetSubject(new NoFormatting())
+					};
+				}
+
+				var dateTimePeriod = visualLayer.Period;
+				var timePeriod = dateTimePeriod.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone);
+
+				var startTime = timePeriod.StartTime;
+				var endTime = timePeriod.EndTime;
+
+				// make sure the shifts over midnight are displayed on the same day
+				if (startTime < minMaxTime.StartTime)
+				{
+					startTime+=TimeSpan.FromDays(1);
+				}
+
+				if (endTime < minMaxTime.StartTime)
+				{
+					endTime += TimeSpan.FromDays(1);
+				}
+
+				yield return
+					new PeriodViewModel
+					{
+						Summary =
+							TimeHelper.GetLongHourMinuteTimeString(dateTimePeriod.ElapsedTime(),
+																   CultureInfo.CurrentUICulture),
+						Title = visualLayer.DisplayDescription().Name,
+						TimeSpan =
+							dateTimePeriod.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).
+							ToShortTimeString(CultureInfo.CurrentUICulture),
+						StyleClassName = colorToString(visualLayer.DisplayColor()),
+						Meeting = meetingModel,
+						Color = visualLayer.DisplayColor().ToCSV(),
+						StartPositionPercentage = (decimal)(startTime - minMaxTime.StartTime).Ticks / (minMaxTime.EndTime - minMaxTime.StartTime).Ticks,
+						EndPositionPercentage = (decimal)(endTime - minMaxTime.StartTime).Ticks / (minMaxTime.EndTime - minMaxTime.StartTime).Ticks
+					};
+			}
+		}
+
+
+		private static string colorToString(Color color)
+		{
+			var colorCode = color.ToArgb();
+			return string.Concat("color_", ColorTranslator.ToHtml(Color.FromArgb(colorCode)).Replace("#", ""));
+		}
+	}
 }
