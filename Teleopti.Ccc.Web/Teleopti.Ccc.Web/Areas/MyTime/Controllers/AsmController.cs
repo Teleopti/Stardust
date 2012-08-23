24315fe12034a11a8ccdcbbed936f3960e4bd8d4
Roger Kratz <roger.kratz@teleopti.com>
2012-08-23 16:20:59 +0200

    fixing tests

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/AsmController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/AsmController.cs
@@ -1,22 +1,23 @@
 ﻿using System.Web.Mvc;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Asm;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.LayoutBase;
-using Teleopti.Ccc.Web.Areas.MyTime.Models.LayoutBase;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 {
 	public class AsmController : Controller
 	{
 		private readonly IAsmViewModelFactory _asmModelFactory;
+		private readonly ILayoutBaseViewModelFactory _layoutBaseViewModelFactory;
 
-		public AsmController(IAsmViewModelFactory asmModelFactory)
+		public AsmController(IAsmViewModelFactory asmModelFactory, ILayoutBaseViewModelFactory layoutBaseViewModelFactory)
 		{
 			_asmModelFactory = asmModelFactory;
+			_layoutBaseViewModelFactory = layoutBaseViewModelFactory;
 		}
 
 		public ViewResult Index()
 		{
-			ViewBag.LayoutBase = new LayoutBaseViewModelFactory(new CultureSpecificViewModelFactory(), new DatePickerGlobalizationViewModelFactory()).CreateLayoutBaseViewModel();
+			ViewBag.LayoutBase = _layoutBaseViewModelFactory.CreateLayoutBaseViewModel();
 			return View(_asmModelFactory.CreateViewModel());
 		}
 	}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/AsmControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/AsmControllerTest.cs
@@ -4,7 +4,9 @@ using Rhino.Mocks;
 using SharpTestsEx;
 using Teleopti.Ccc.Web.Areas.MyTime.Controllers;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Asm;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.LayoutBase;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.Asm;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.LayoutBase;
 
 namespace Teleopti.Ccc.WebTest.Areas.MyTime.Controllers
 {
@@ -16,14 +18,30 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Controllers
 		{
 			const string payload = "phone";
 			var asmModelFactory = MockRepository.GenerateMock<IAsmViewModelFactory>();
+			var layoutFactory = MockRepository.GenerateMock<ILayoutBaseViewModelFactory>();
 			var asmViewModel = new AsmViewModel();
 			asmViewModel.Layers.Add(new AsmLayer {Payload = payload});
 			asmModelFactory.Expect(fac => fac.CreateViewModel()).Return(asmViewModel);
 
-			using (var controller = new AsmController(asmModelFactory))
+			using (var controller = new AsmController(asmModelFactory, layoutFactory))
 			{
 				var model = controller.Index().Model as AsmViewModel;
 				model.Layers.First().Payload.Should().Be.EqualTo(payload);
+			}
+		}
+
+		[Test]
+		public void ShouldSetPortalViewBag()
+		{
+			var asmModelFactory = MockRepository.GenerateMock<IAsmViewModelFactory>();
+			var layoutFactory = MockRepository.GenerateMock<ILayoutBaseViewModelFactory>();
+			var viewBag = new LayoutBaseViewModel();
+			layoutFactory.Expect(fac => fac.CreateLayoutBaseViewModel()).Return(viewBag);
+
+			using (var controller = new AsmController(asmModelFactory, layoutFactory))
+			{
+				((object)controller.Index().ViewBag.LayoutBase)
+					.Should().Be.SameInstanceAs(viewBag);
 			}
 		}
 	}
