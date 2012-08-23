29fc4e101ff9c532a2de4058a01293a5b26717d6
Roger Kratz <roger.kratz@teleopti.com>
2012-08-23 16:08:32 +0200

    using layoutuserbase

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/AsmController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/AsmController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Asm;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.LayoutBase;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.LayoutBase;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 {
@@ -14,6 +16,7 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 
 		public ViewResult Index()
 		{
+			ViewBag.LayoutBase = new LayoutBaseViewModelFactory(new CultureSpecificViewModelFactory(), new DatePickerGlobalizationViewModelFactory()).CreateLayoutBaseViewModel();
 			return View(_asmModelFactory.CreateViewModel());
 		}
 	}
