a3c7210745f4a8503837c0ea2e783f5c0db2de45
Kunning Mao <kunning.mao@teleopti.com>
2012-08-24 11:19:03 +0200

    localize text in anywhere mobile report login page

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Models/Authentication/SignInApplicationModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Models/Authentication/SignInApplicationModel.cs
@@ -4,20 +4,20 @@ using System.ComponentModel.DataAnnotations;
 namespace Teleopti.Ccc.Web.Areas.Start.Models.Authentication
 {
 	public class SignInApplicationModel
-	 {
+	{
 
-		  [Required(ErrorMessageResourceName = "DataSourceRequired", ErrorMessageResourceType = typeof(Resources))]
-		  [Display(Name = "PleaseChooseADatasource", ResourceType = typeof(Resources))]
-		  public string DataSourceName { get; set; }
+		[Required(ErrorMessageResourceName = "DataSourceRequired", ErrorMessageResourceType = typeof (Resources))]
+		[Display(Name = "PleaseChooseADatasource", ResourceType = typeof (Resources))]
+		public string DataSourceName { get; set; }
 
-		  [Required(ErrorMessageResourceName = "UserNameRequired", ErrorMessageResourceType = typeof(Resources))]
-		  [Display(Name = "User name")]
-		  public string UserName { get; set; }
+		[Required(ErrorMessageResourceName = "UserNameRequired", ErrorMessageResourceType = typeof (Resources))]
+		[Display(Name = "UserName", ResourceType = typeof (Resources))]
+		public string UserName { get; set; }
 
-		  [Required(ErrorMessageResourceName = "PasswordRequired", ErrorMessageResourceType = typeof(Resources))]
-		  [DataType(DataType.Password)]
-		  [Display(Name = "Password", Prompt = "Password")]
-		  public string Password { get; set; }
+		[Required(ErrorMessageResourceName = "PasswordRequired", ErrorMessageResourceType = typeof (Resources))]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password", Prompt = "Password", ResourceType = typeof (Resources))]
+		public string Password { get; set; }
 
-	 }
+	}
 }
