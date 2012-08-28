410654dd220695675f5a62187eebca64d3cfb169
David Jonsson <david.jonsson@teleopti.com>
2012-08-28 19:21:39 +0200

    #16901 - add CrossDbViews to Security .exe
    --HG--
    branch : Release 7.2.368

--- Teleopti.Support.Security/Program.cs
+++ Teleopti.Support.Security/Program.cs
@@ -6,15 +6,19 @@
         private static readonly ICommandLineCommand forecasterDateAdjustment = new ForecasterDateAdjustment();
         private static readonly ICommandLineCommand PersonFirstDayOfWeekSetter = new PersonFirstDayOfWeekSetter();
         private static readonly ICommandLineCommand LicenseStatusChecker = new LicenseStatusChecker();
+		private static readonly ICommandLineCommand CrossDatabaseViewUpdate = new CrossDatabaseViewUpdate();
 
         static void Main(string[] args)
         {
             var commandLineArgument = new CommandLineArgument(args);
+			if (!string.IsNullOrEmpty(commandLineArgument.AggDatabase))
+				CrossDatabaseViewUpdate.Execute(commandLineArgument);
 
             if (!commandLineArgument.ForecasterMode &&
                 !commandLineArgument.PersonUpdateMode &&
                 !commandLineArgument.PasswordEncryptionMode &&
-                !commandLineArgument.LicenseStatusMode)
+                !commandLineArgument.LicenseStatusMode &&
+				string.IsNullOrEmpty(commandLineArgument.AggDatabase))
             {
                 forecasterDateAdjustment.Execute(commandLineArgument);
                 passwordEncryption.Execute(commandLineArgument);
@@ -34,6 +38,8 @@
 
             if (commandLineArgument.LicenseStatusMode)
                 LicenseStatusChecker.Execute(commandLineArgument);
+
+
         }
     }
 }
