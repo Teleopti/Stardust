8de2f251faa8a53f05075d625d9467916259f9be
ola@teleopti.com <ola@teleopti.com>
2012-08-29 08:14:48 +0200

    test fix

--- InfrastructureTest/Repositories/ScheduleDayReadModelRepositoryTest.cs
+++ InfrastructureTest/Repositories/ScheduleDayReadModelRepositoryTest.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using NHibernate.Transform;
 using NUnit.Framework;
-using Rhino.Mocks;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Ccc.InfrastructureTest.Helper;
 using Teleopti.Interfaces.Domain;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.InfrastructureTest.Repositories
 {
@@ -17,15 +12,10 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories
 	{
 		private ScheduleDayReadModelRepository _target;
  
-		[SetUp]
-		public void Setup()
-		{
-			_target = new ScheduleDayReadModelRepository(UnitOfWorkFactory.Current);	
-		}
-
 		[Test]
 		public void ShouldReturnReadModelsForPerson()
 		{
+			_target = new ScheduleDayReadModelRepository(UnitOfWorkFactory.Current);	
 			var dateOnly = new DateOnly(2012, 8, 28);
 			var personId = Guid.NewGuid();
 			Assert.That(_target.ReadModelsOnPerson(dateOnly, dateOnly.AddDays(5),personId),Is.Not.Null);
