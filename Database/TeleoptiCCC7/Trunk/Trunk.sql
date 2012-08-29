e623820290351ecc1a0d830f58c2e90b94d4b063
Asad MIrza <asad.mirza@teleopti.com>
2012-08-29 11:55:58 +0200

    Added a new column in activty table

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -8,3 +8,15 @@ ALTER TABLE dbo.Contract ADD
 	IsWorkTimeFromContract int NOT NULL CONSTRAINT DF_Contract_IsWorkTimeFromContract DEFAULT 1,
 	IsWorkTimeFromSchedulePeriod int NOT NULL CONSTRAINT DF_Contract_IsWorkTimeFromSchedulePeriod DEFAULT 0
 GO
+
+----------------  
+--Name: Asad MIrza
+--Date: 2012-08-29
+--Desc: Added an extra column in activity to indicate if we can overwrite it or not
+---------------- 
+ALTER TABLE dbo.Activity ADD
+	AllowOverwrite bit NOT NULL CONSTRAINT DF_Activity_AllowOverwrite DEFAULT 1
+GO
+
+ALTER TABLE dbo.Activity SET (LOCK_ESCALATION = TABLE)
+GO
