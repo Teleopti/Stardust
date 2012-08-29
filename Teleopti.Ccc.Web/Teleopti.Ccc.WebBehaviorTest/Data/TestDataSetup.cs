7249a5f3fc3960827858c63bedc0b8c12c3bf51a
Kunning Mao <kunning.mao@teleopti.com>
2012-08-29 16:00:20 +0200

    mobile report localization

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/TestDataSetup.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/TestDataSetup.cs
@@ -17,7 +17,6 @@ using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
-using Teleopti.Ccc.WebBehaviorTest.Core.Extensions;
 using Teleopti.Ccc.WebBehaviorTest.Data.Setups.Common;
 using Teleopti.Ccc.WebBehaviorTest.Data.Setups.Specific;
 using Teleopti.Interfaces.Domain;
