ef7d4de81d2033681016a78b638677d57f717ed7
Roger Kratz <roger.kratz@teleopti.com>
2012-08-30 16:18:51 +0200

    moving (most of) js from view to separate file in asm

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="Teleopti.MyTimeWeb.Common.js"/>
+
+if (typeof (Teleopti) === 'undefined') {
+	Teleopti = {};
+	if (typeof (Teleopti.MyTimeWeb) === 'undefined') {
+		Teleopti.MyTimeWeb = {};
+	}
+}
+
+Teleopti.MyTimeWeb.Asm = (function () {
+	function _start(serverMsSince1970, pixelsPerHour) {
+		var refreshMilliSeconds = 200;
+		var slidingSchedules = $('.asm-sliding-schedules');
+
+		_moveTimeLineToNow(slidingSchedules, serverMsSince1970, pixelsPerHour);
+		slidingSchedules.show();
+
+
+		setInterval(function () {
+			_moveTimeLineToNow(slidingSchedules, serverMsSince1970, pixelsPerHour);
+			_updateInfoCanvas();
+		}, refreshMilliSeconds);
+	}
+
+	function _updateInfoCanvas() {
+		$('#asm-info-debug').text(new Date().getTeleoptiTime());
+	}
+
+	function _moveTimeLineToNow(slidingSchedules, serverMsSince1970, pixelsPerHour) {
+		var clientMsSince1970 = new Date().getTeleoptiTime();
+		var msSinceStart = clientMsSince1970 - serverMsSince1970;
+
+		var hoursSinceStart = msSinceStart / 1000 / 60 / 60;
+		var startPixel = -(pixelsPerHour * hoursSinceStart);
+		slidingSchedules.css('left', (startPixel) + 'px');
+	}
+
+	return {
+		Init: function (serverMsSince1970, pixelsPerHour) {
+			_start(serverMsSince1970, pixelsPerHour);
+		}
+	};
+
+})(jQuery);
