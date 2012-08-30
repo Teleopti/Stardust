cd40e657fc2e0f81764a9a668d75377a843c63d8
Maria Stein <maria.stein@teleopti.com>
2012-08-30 16:39:16 +0200

    Visualize night shift correctly in week schedule

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/WeekSchedule/ViewModelFactory/PeriodViewModelFactory.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/WeekSchedule/ViewModelFactory/PeriodViewModelFactory.cs
@@ -10,10 +10,15 @@ using Teleopti.Interfaces.Domain;
 namespace Teleopti.Ccc.Web.Areas.MyTime.Core.WeekSchedule.ViewModelFactory
 {
 	public class PeriodViewModelFactory : IPeriodViewModelFactory
-	{
-		public IEnumerable<PeriodViewModel> CreatePeriodViewModels(IVisualLayerCollection visualLayerCollection, TimePeriod minMaxTime)
+    {
+        public IEnumerable<PeriodViewModel> CreatePeriodViewModels(IEnumerable<IVisualLayer> visualLayerCollection, TimePeriod minMaxTime, DateTime localDate, ICccTimeZoneInfo timeZone)
         {
-            foreach (var visualLayer in visualLayerCollection)
+            var calendarDayExtractor = new VisualLayerCalendarDayExtractor();
+            var layerExtendedList = calendarDayExtractor.CreateVisualPeriods(localDate, visualLayerCollection, timeZone);
+
+            var newList = new List<PeriodViewModel>();
+            
+            foreach (var visualLayer in layerExtendedList)
             {
                 MeetingViewModel meetingModel = null;
                 
@@ -26,24 +31,33 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.WeekSchedule.ViewModelFactory
                                             Title = meetingPayload.Meeting.GetSubject(new NoFormatting())
                                         };
                 }
-                      
-                yield return
-                    new PeriodViewModel
-                        {
-                            Summary =
-                                TimeHelper.GetLongHourMinuteTimeString(visualLayer.Period.ElapsedTime(),
-                                                                        CultureInfo.CurrentUICulture),
-                            Title = visualLayer.DisplayDescription().Name,
-                            TimeSpan =
-                                visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).
-                                ToShortTimeString(CultureInfo.CurrentUICulture),
-                            StyleClassName = colorToString(visualLayer.DisplayColor()),
-                            Meeting = meetingModel,
-                            Color = visualLayer.DisplayColor().ToCSV(),
-                            StartPositionPercentage = (decimal)(visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).StartTime - minMaxTime.StartTime).Ticks / (minMaxTime.EndTime - minMaxTime.StartTime).Ticks,
-                            EndPositionPercentage = (decimal)(visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).EndTime - minMaxTime.StartTime).Ticks / (minMaxTime.EndTime - minMaxTime.StartTime).Ticks
-                        };
+
+                //yield return
+                newList.Add(new PeriodViewModel
+                                {
+                                    Summary =
+                                        TimeHelper.GetLongHourMinuteTimeString(visualLayer.Period.ElapsedTime(),
+                                                                               CultureInfo.CurrentUICulture),
+                                    Title = visualLayer.DisplayDescription().Name,
+                                    TimeSpan =
+                                        visualLayer.Period.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).
+                                        ToShortTimeString(CultureInfo.CurrentUICulture),
+                                    StyleClassName = colorToString(visualLayer.DisplayColor()),
+                                    Meeting = meetingModel,
+                                    Color = visualLayer.DisplayColor().ToCSV(),
+                                    StartPositionPercentage =
+                                        (decimal)
+                                        (visualLayer.VisualPeriod.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).
+                                             StartTime - minMaxTime.StartTime).Ticks/
+                                        (minMaxTime.EndTime - minMaxTime.StartTime).Ticks,
+                                    EndPositionPercentage =
+                                        (decimal)
+                                        (visualLayer.VisualPeriod.TimePeriod(TeleoptiPrincipal.Current.Regional.TimeZone).
+                                             EndTime - minMaxTime.StartTime).Ticks/
+                                        (minMaxTime.EndTime - minMaxTime.StartTime).Ticks
+                                });
             }
+            return newList;
         }
 
 
@@ -52,5 +66,6 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.WeekSchedule.ViewModelFactory
 			var colorCode = color.ToArgb();
 			return string.Concat("color_", ColorTranslator.ToHtml(Color.FromArgb(colorCode)).Replace("#", ""));
 		}
+
 	}
 }
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/WeekSchedule/ViewModelFactory/VisualLayerCalendarDayExtractor.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Core.WeekSchedule.ViewModelFactory
+{
+    public class VisualLayerCalendarDayExtractor
+    {
+        public IList<VisualLayerForWebDisplay> CreateVisualPeriods(DateTime date, IEnumerable<IVisualLayer> visualLayerCollection, ICccTimeZoneInfo timeZone)
+        {
+            var returnList = new List<VisualLayerForWebDisplay>();
+            
+            foreach (var visualLayer in visualLayerCollection)
+            {
+                var layer = visualLayer as VisualLayer;
+                if (layer == null)
+                    continue;
+
+                DateTimePeriod visualPeriod;
+                if (visualLayer.Period.StartDateTimeLocal(timeZone).Date < date)
+                {
+                    // Layer starts yesterday
+                    if (visualLayer.Period.EndDateTimeLocal(timeZone).Date == date)
+                    {
+                        // ...and ends today
+                        visualPeriod = new DateTimePeriod(TimeZoneHelper.ConvertToUtc(date.Date, timeZone), visualLayer.Period.EndDateTime);
+                    }
+                    else
+                    {
+                        // ...and ends yesterday
+                        continue;
+                    }
+                }
+                else if (visualLayer.Period.StartDateTimeLocal(timeZone).Date == date)
+                {
+                    // Layer starts today
+                    if (visualLayer.Period.EndDateTimeLocal(timeZone).Date > date)
+                    {
+                        // ...and ends tomorrow
+                        visualPeriod = new DateTimePeriod(visualLayer.Period.StartDateTime,
+                                                          TimeZoneHelper.ConvertToUtc(date.Date.Add(new TimeSpan(23, 59, 59)), timeZone));
+                    }
+                    else
+                    {
+                        // ...and ends today
+                        visualPeriod = new DateTimePeriod(visualLayer.Period.StartDateTime,
+                                                          visualLayer.Period.EndDateTime);
+                    }
+                }
+                else
+                {
+                    // Layer starts tomorrow
+                    continue;
+                }
+
+                var layerForDisplay = new VisualLayerForWebDisplay(visualLayer.Payload, visualLayer.Period, layer.HighestPriorityActivity, visualLayer.Person)
+                                          {
+                                              VisualPeriod = visualPeriod
+                                          };
+                returnList.Add(layerForDisplay);
+            }
+
+            return returnList;
+        }
+    }
+}
