b6bf8d63ed9bcbb5d925368d96c03f220197798c
Roger Kratz <roger.kratz@teleopti.com>
2012-09-03 10:27:01 +0100

    moving and refactoring js code for asm

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
@@ -9,24 +9,47 @@ if (typeof (Teleopti) === 'undefined') {
 
 Teleopti.MyTimeWeb.Asm = (function () {
 	function _start(serverMsSince1970, pixelsPerHour) {
-		var refreshMilliSeconds = 200;
-		var slidingSchedules = $('.asm-sliding-schedules');
+		var refreshSeconds = 20;
 
-		_moveTimeLineToNow(slidingSchedules, serverMsSince1970, pixelsPerHour);
-		slidingSchedules.show();
-
+		_refresh(serverMsSince1970, pixelsPerHour);
+		$('.asm-outer-canvas').show();
 
 		setInterval(function () {
-			_moveTimeLineToNow(slidingSchedules, serverMsSince1970, pixelsPerHour);
-			_updateInfoCanvas();
-		}, refreshMilliSeconds);
+			_refresh(serverMsSince1970, pixelsPerHour);
+		}, refreshSeconds * 1000);
+	}
+
+	function _refresh(serverMsSince1970, pixelsPerHour) {
+		_moveTimeLineToNow(serverMsSince1970, pixelsPerHour);
+		_updateInfoCanvas();
 	}
 
 	function _updateInfoCanvas() {
+		var currentStart;
+		var currentEnd;
+		var currentActivity = '';
+		var nu = new Date().getTeleoptiTime();
+		$('.asm-layer')
+			.each(function () {
+				var start = parseFloat($(this).data('asm-start-milliseconds'));
+				var end = parseFloat($(this).data('asm-end-milliseconds'));
+				if (nu >= start && nu < end) {
+					currentStart = $(this).data('asm-start-time');
+					currentEnd = $(this).data('asm-end-time');
+					currentActivity = $(this).data('asm-activity');
+					return false;
+				}
+			});
+		if (currentActivity != '') {
+			$('#asm-info-current-activity').text(currentActivity);
+			$('#asm-info-current-starttime').text(currentStart);
+			$('#asm-info-current-endtime').text(currentEnd);
+		}
 		$('#asm-info-debug').text(new Date().getTeleoptiTime());
 	}
 
-	function _moveTimeLineToNow(slidingSchedules, serverMsSince1970, pixelsPerHour) {
+	function _moveTimeLineToNow(serverMsSince1970, pixelsPerHour) {
+		var slidingSchedules = $('.asm-sliding-schedules');
 		var clientMsSince1970 = new Date().getTeleoptiTime();
 		var msSinceStart = clientMsSince1970 - serverMsSince1970;
 
