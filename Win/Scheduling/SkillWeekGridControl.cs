d625749a0c31a0791c60d901a39eb94ae2aac841
Claes Stenman <claes.stenman@teleopti.com>
2012-09-05 10:49:17 +0200

    Added a presenter for SkillWeekGridControl...

--- Win/Scheduling/SkillWeekGridControl.cs
+++ Win/Scheduling/SkillWeekGridControl.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing;
-using System.Globalization;
 using System.Linq;
-using System.Windows.Forms;
 using Syncfusion.Windows.Forms.Grid;
-using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Helper;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
@@ -17,27 +13,24 @@ using Teleopti.Ccc.Win.Forecasting.Forms;
 using Teleopti.Ccc.WinCode.Common;
 using Teleopti.Ccc.WinCode.Common.Chart;
 using Teleopti.Ccc.WinCode.Common.Rows;
+using Teleopti.Ccc.WinCode.Scheduling;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Ccc.Win.Common;
 using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Win.Scheduling
 {
-    public class SkillWeekGridControl : TeleoptiGridControl, ITaskOwnerGrid, IHelpContext
+	public class SkillWeekGridControl : TeleoptiGridControl, ITaskOwnerGrid, IHelpContext, ISkillWeekGridControl
     {
         private AbstractDetailView _owner;
         private const int RowHeaderWidth = 200;
-        //private const int CellWidth = 50;
         private const string SettingName = "SchedulerSkillDayGridAndChart";
 		private RowManagerScheduler<SkillWeekGridRow, IDictionary<DateOnlyPeriod, IList<ISkillStaffPeriod>>> _rowManager;
-        private static readonly Color ColorCells = Color.AntiqueWhite;
-
         private IList<IGridRow> _gridRows;
         private GridRow _currentSelectedGridRow;
-        private IList<DateOnly> _dates;
-    	private IList<DateOnlyPeriod> _weeks;
         private readonly ChartSettings _chartSettings;
         private readonly ChartSettings _defaultChartSettings = new ChartSettings();
+		private readonly SkillWeekGridControlPresenter _presenter;
 
         //constructor
         public SkillWeekGridControl()
@@ -45,11 +38,13 @@ namespace Teleopti.Ccc.Win.Scheduling
             initializeComponent();
             initializeGrid();
             setupChartDefault();
-            //flytta, i alla fall från konstruktor
-            using(IUnitOfWork uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+           
+            using(var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
             {
                 _chartSettings = new PersonalSettingDataRepository(uow).FindValueByKey(SettingName, _defaultChartSettings);
             }
+
+			_presenter = new SkillWeekGridControlPresenter(this);
         }
 
         //initialize component
@@ -97,85 +92,55 @@ namespace Teleopti.Ccc.Win.Scheduling
 
         private GridCellModelBase initializeCallPercentReadOnlyCell()
         {
-        	return new PercentReadOnlyCellModel(Model);// {NumberOfDecimals = 0};
-			//PercentReadOnlyCellModel cellModel = new PercentReadOnlyCellModel(Model);
-			//cellModel.NumberOfDecimals = 0;
-			//return cellModel;
+        	return new PercentReadOnlyCellModel(Model);
         }
 
         private GridCellModelBase initializeCallPercentReadOnlyPercentCell()
         {
-        	return new PercentFromPercentReadOnlyCellModel(Model); //{NumberOfDecimals = 0};
-			//PercentFromPercentReadOnlyCellModel cellModel = new PercentFromPercentReadOnlyCellModel(Model);
-			//cellModel.NumberOfDecimals = 0;
-			//return cellModel;
+        	return new PercentFromPercentReadOnlyCellModel(Model);
         }
 
-        //event col width
         private void gridSkillDataQueryColWidth(object sender, GridRowColSizeEventArgs e)
-        {
-            if (e.Index <= Cols.HeaderCount)
-            {
-                e.Size = RowHeaderWidth;
-            }
-
-            if (e.Index > Cols.HeaderCount)
-            {
-				e.Size = RowHeaderWidth;
-                e.Handled = true;
-            }
+        {	
+			e.Size = RowHeaderWidth;
+            e.Handled = true;   
         }
 
         private void gridSkillDataQueryCellInfo(object sender, GridQueryCellInfoEventArgs e)
         {
             if (e.ColIndex < 0 || e.RowIndex < 0) return;
             if (_gridRows == null) return;
-            if (e.ColIndex == 0 && e.RowIndex == 1)
-                e.Style.CellValue = " ";
+            if (e.ColIndex == 0 && e.RowIndex == 1) e.Style.CellValue = " ";
 
-            if(e.RowIndex < _gridRows.Count)
-                _gridRows[e.RowIndex].QueryCellInfo(GetCellInfo(e.Style,e.ColIndex,e.RowIndex));
-
-            if (e.ColIndex > 0)
-                e.Style.HorizontalAlignment = GridHorizontalAlignment.Center;
+            if(e.RowIndex < _gridRows.Count) _gridRows[e.RowIndex].QueryCellInfo(GetCellInfo(e.Style,e.ColIndex,e.RowIndex));
+            if (e.ColIndex > 0) e.Style.HorizontalAlignment = GridHorizontalAlignment.Center;
 
             e.Handled = true;
         }
 
         private IChartSeriesSetting configureSetting(string key)
         {
-            IChartSeriesSetting ret = _chartSettings.DefinedSetting(key, new ChartSettingsManager().ChartSettingsDefault);
+            var ret = _chartSettings.DefinedSetting(key, new ChartSettingsManager().ChartSettingsDefault);
             ret.Enabled = _chartSettings.SelectedRows.Contains(key);
             return ret;
         }
 
-        private void createGridRows(ISkill skill, IList<DateOnly> dates, ISchedulerStateHolder schedulerStateHolder)
+        public void CreateGridRows(ISkill skill, IList<DateOnly> dates, ISchedulerStateHolder schedulerStateHolder)
         {
+			if (skill == null || dates == null) return;
+
 			((PercentReadOnlyCellModel)CellModels["ReadOnlyPercentCell"]).NumberOfDecimals = 0;
 			((PercentFromPercentReadOnlyCellModel)CellModels["PercentCellModel"]).NumberOfDecimals = 0;
             ((NumericReadOnlyCellModel)CellModels["NumericReadOnlyCell"]).NumberOfDecimals = 2;
             DateOnly baseDate;
 
-            _gridRows = new List<IGridRow>();
-            _gridRows.Add(new DateHeaderGridRow(DateHeaderType.WeekDates, dates));
-            //_gridRows.Add(new DateHeaderGridRow(DateHeaderType.MonthDayNumber, dates));
-
-            if (dates.Count > 0)
-                baseDate = dates.First();
-            else
-                baseDate = DateOnly.MinValue;
+            _gridRows = new List<IGridRow> {new DateHeaderGridRow(DateHeaderType.WeekDates, dates)};
+        	baseDate = dates.Count > 0 ? dates.First() : DateOnly.MinValue;
 
             _rowManager = new RowManagerScheduler<SkillWeekGridRow, IDictionary<DateOnlyPeriod, IList<ISkillStaffPeriod>>>(this, new List<IntervalDefinition>(), 15, schedulerStateHolder);
             _rowManager.BaseDate = baseDate;
 
         	SkillWeekGridRow gridRow;
-
-			//if (skill.SkillType.ForecastSource == ForecastSource.MaxSeatSkill)
-			//{
-			//    gridRow = new SkillDayGridRowMaxSeatsIssues(_rowManager, "NumericReadOnlyCell", "MaxUsedSeats", UserTexts.Resources.MaxUsedSeats);
-			//    gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
-			//    _gridRows.Add(_rowManager.AddRow(gridRow));
-			//}
 
 			if (skill.SkillType.ForecastSource != ForecastSource.MaxSeatSkill)
 			{
@@ -202,35 +167,23 @@ namespace Teleopti.Ccc.Win.Scheduling
 
 				if (!skill.IsVirtual)
 				{
-					gridRow = new SkillWeekGridRowStaffingIssues(_rowManager, "ReadOnlyPercentCell", "RelativeDifference",
-																UserTexts.Resources.RelativeDifference, skill);
+					gridRow = new SkillWeekGridRowStaffingIssues(_rowManager, "ReadOnlyPercentCell", "RelativeDifference",UserTexts.Resources.RelativeDifference, skill);
 					gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
 					_gridRows.Add(_rowManager.AddRow(gridRow));
 				}
 				else
 				{
-					gridRow = new SkillWeekGridRowStaffingIssuesSummary(_rowManager, "ReadOnlyPercentCell", "RelativeDifference",
-																UserTexts.Resources.RelativeDifference);
+					gridRow = new SkillWeekGridRowStaffingIssuesSummary(_rowManager, "ReadOnlyPercentCell", "RelativeDifference",UserTexts.Resources.RelativeDifference);
 					gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
 					_gridRows.Add(_rowManager.AddRow(gridRow));
 				}
 
-				//gridRow = new SkillDayGridRowBoostedRelativeDifference(_rowManager, "NumericReadOnlyCell", "RelativeBoostedDifference", UserTexts.Resources.AdjustedDifference, skill);
-				//gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
-				//_gridRows.Add(_rowManager.AddRow(gridRow));
-
-				//gridRow = new SkillDayGridRow(_rowManager, "NumericReadOnlyCell", "RootMeanSquare", UserTexts.Resources.RMS);
-				//gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
-				//_gridRows.Add(_rowManager.AddRow(gridRow));
-
+				
 				gridRow = new SkillWeekGridRow(_rowManager, "NumericReadOnlyCell", "DailySmoothness", UserTexts.Resources.StandardDeviation);
 				gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
 				_gridRows.Add(_rowManager.AddRow(gridRow));
 
-				//gridRow = new SkillDayGridRow(_rowManager, "NumericReadOnlyCell", "HighestDeviationInPeriod", UserTexts.Resources.HighestStdev);
-				//gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
-				//_gridRows.Add(_rowManager.AddRow(gridRow));
-
+				
 				gridRow = new SkillWeekGridRow(_rowManager, "PercentCellModel", "EstimatedServiceLevel", UserTexts.Resources.ESL);
 				gridRow.ChartSeriesSettings = configureSetting(gridRow.DisplayMember);
 				_gridRows.Add(_rowManager.AddRow(gridRow));
@@ -261,102 +214,21 @@ namespace Teleopti.Ccc.Win.Scheduling
 		
 		public void SetDataSource(ISchedulerStateHolder stateHolder, ISkill skill)
 		{
-			if (skill == null) return;
-
-			_weeks = new List<DateOnlyPeriod>();
-			var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection();
-			_dates = dateTimePeriods.Select(d => new DateOnly(TimeZoneHelper.ConvertFromUtc(d.StartDateTime, stateHolder.TimeZoneInfo))).ToList();
-
-			foreach (var dateOnly in _dates)
-			{
-				var weekPeriod = DateHelper.GetWeekPeriod(dateOnly, CultureInfo.CurrentCulture);
-				if (!_weeks.Contains(weekPeriod)) 
-				{
-					var weekDays = weekPeriod.DayCollection();
-					var exists = weekDays.All(weekDateOnly => _dates.Contains(weekDateOnly));
-					if(exists) _weeks.Add(weekPeriod);
-				}
-			}
-
-			IDictionary<DateOnlyPeriod, IList<ISkillStaffPeriod>> skillWeekPeriods = new Dictionary<DateOnlyPeriod, IList<ISkillStaffPeriod>>();
-			IAggregateSkill aggregateSkillSkill = skill;
-
-			foreach (var dateOnlyPeriod in _weeks)
-			{
-				IList<ISkillStaffPeriod> periods = new List<ISkillStaffPeriod>();
-				if (aggregateSkillSkill.IsVirtual)
-				{
-					periods = stateHolder.SchedulingResultState.SkillStaffPeriodHolder.SkillStaffPeriodList(aggregateSkillSkill, dateOnlyPeriod.ToDateTimePeriod(stateHolder.TimeZoneInfo));
-				}
-				else
-				{
-					DateTimePeriod periodToFind = dateOnlyPeriod.ToDateTimePeriod(stateHolder.TimeZoneInfo);
-					periods =stateHolder.SchedulingResultState.SkillStaffPeriodHolder.SkillStaffPeriodList(new List<ISkill> { skill }, periodToFind);
-				}
-
-				
-				skillWeekPeriods.Add(dateOnlyPeriod, periods);
-				
-			}
-
+			var skillWeekPeriods = _presenter.CreateDataSource(stateHolder, skill);
+			if (skillWeekPeriods == null) return;
 			_rowManager.SetDataSource(new List<IDictionary<DateOnlyPeriod, IList<ISkillStaffPeriod>>> { skillWeekPeriods });
+		}
+
+		public void SetupGrid(int colCount)
+		{
+			ColCount = colCount;
+			RowCount = _gridRows.Count - 1;
+			ColWidths[0] = RowHeaderWidth;
 		}
 
         public void DrawDayGrid(ISchedulerStateHolder stateHolder,ISkill skill)
         {
-           
-            if (stateHolder == null || skill==null) return;
-
-            using (PerformanceOutput.ForOperation("DrawSkillDayGrid"))
-            {
-				//Model.Options.MergeCellsMode = GridMergeCellsMode.OnDemandCalculation |
-				//                               GridMergeCellsMode.MergeColumnsInRow;
-
-                var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection();
-                _dates = dateTimePeriods.Select(d => new DateOnly(TimeZoneHelper.ConvertFromUtc(d.StartDateTime, stateHolder.TimeZoneInfo))).ToList();
-
-            	var weekDates = new List<DateOnly>();
-				_weeks = new List<DateOnlyPeriod>();
-
-				foreach (var dateOnly in _dates)
-				{
-					var weekPeriod = DateHelper.GetWeekPeriod(dateOnly, CultureInfo.CurrentCulture);
-					if (!_weeks.Contains(weekPeriod)) _weeks.Add(weekPeriod);
-				}
-
-            	foreach (var dateOnlyPeriod in _weeks)
-            	{
-            		weekDates.Add(dateOnlyPeriod.StartDate);
-            	}
-
-                createGridRows(skill, weekDates, stateHolder);
-
-            	SetDataSource(stateHolder, skill);
-                ColCount = _weeks.Count;
-                RowCount = _gridRows.Count - 1;
-                ColWidths[0] = RowHeaderWidth;
-
-                //Model.MergeCells.DelayMergeCells(GridRangeInfo.Table());
-            }
-        }
-
-        public void WeekendOrWeekday(GridQueryCellInfoEventArgs e, DateTime date, bool backColor, bool textColor)
-        {
-			if(e == null) throw new ArgumentNullException("e");
-            if (DateHelper.IsWeekend(date, CultureInfo.CurrentCulture))
-            {
-                if (backColor)
-                    e.Style.BackColor = ColorHolidayCell;
-                if (textColor)
-                    e.Style.TextColor = ColorHolidayHeader;
-            }
-            else
-            {
-                if(backColor)
-                    e.Style.BackColor = ColorCells;
-                   
-                e.Style.TextColor = ForeColor;
-            }
+           _presenter.DrawWeekGrid(stateHolder, skill);	
         }
 
         //refresh grid
@@ -479,7 +351,7 @@ namespace Teleopti.Ccc.Win.Scheduling
 
         public bool HasColumns
         {
-            get { return _dates.Count > 0; }
+			get { return _presenter.Weeks.Count > 0; }
         }
 
         public GridRow CurrentSelectedGridRow
