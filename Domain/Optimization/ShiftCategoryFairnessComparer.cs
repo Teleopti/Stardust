b5f47280135f45108c05c9d65517e5518f9a59d6
ola@teleopti.com <ola@teleopti.com>
2012-09-11 14:49:48 +0200

    compare two fairness aggregates

---
+++ Domain/Optimization/ShiftCategoryFairnessComparer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Optimization
+{
+	public interface IShiftCategoryFairnessComparerResult
+	{
+		IShiftCategory ShiftCategory { get; set; }
+		double Original { get; set; }
+		double ComparedTo { get; set; }
+	}
+
+	public class ShiftCategoryFairnessComparerResult : IShiftCategoryFairnessComparerResult
+	{
+		public IShiftCategory ShiftCategory { get; set; }
+		public double Original { get; set; }
+		public double ComparedTo { get; set; }
+	}
+
+	public interface IShiftCategoryFairnessComparer
+	{
+		IList<ShiftCategoryFairnessComparerResult> Compare(IShiftCategoryFairness original, IShiftCategoryFairness compareTo, IList<IShiftCategory> shiftCategories);
+	}
+
+	public class ShiftCategoryFairnessComparer : IShiftCategoryFairnessComparer
+	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "1"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
+		public IList<ShiftCategoryFairnessComparerResult> Compare(IShiftCategoryFairness original, IShiftCategoryFairness compareTo, IList<IShiftCategory> shiftCategories)
+		{
+			var totalOriginal = original.ShiftCategoryFairnessDictionary.Values.Sum();
+			var totalCompare = compareTo.ShiftCategoryFairnessDictionary.Values.Sum();
+
+			var result = shiftCategories.Select(shiftCategory => new ShiftCategoryFairnessComparerResult {ShiftCategory = shiftCategory}).ToList();
+			foreach (var shiftCategoryFairnessComparerResult in result)
+			{
+				if (original.ShiftCategoryFairnessDictionary.ContainsKey(shiftCategoryFairnessComparerResult.ShiftCategory))
+				{
+					shiftCategoryFairnessComparerResult.Original =
+						(double)original.ShiftCategoryFairnessDictionary[shiftCategoryFairnessComparerResult.ShiftCategory]/totalOriginal;
+				}
+				if (compareTo.ShiftCategoryFairnessDictionary.ContainsKey(shiftCategoryFairnessComparerResult.ShiftCategory))
+				{
+					shiftCategoryFairnessComparerResult.ComparedTo =
+						(double)compareTo.ShiftCategoryFairnessDictionary[shiftCategoryFairnessComparerResult.ShiftCategory] / totalCompare;
+				}
+			}
+
+			return result;
+		}
+
+	}
+}
---
+++ DomainTest/Optimization/ShiftCategoryFairnessComparerTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization
+{
+	[TestFixture]
+	public class ShiftCategoryFairnessComparerTest
+	{
+		private ShiftCategoryFairnessComparer _target;
+
+		[SetUp]
+		public void Setup()
+		{
+			_target = new ShiftCategoryFairnessComparer();
+		}
+
+		[Test]
+		public void ShouldContainAllCategories()
+		{
+			var cat = ShiftCategoryFactory.CreateShiftCategory("Morning");
+			var cat2 = ShiftCategoryFactory.CreateShiftCategory("Day");
+			var cat3 = ShiftCategoryFactory.CreateShiftCategory("Late");
+
+			var catDic = new Dictionary<IShiftCategory, int> { { cat, 2 }, { cat2, 2 }};
+			var catDic2 = new Dictionary<IShiftCategory, int> { { cat2, 2 }, { cat3, 2 } };
+
+			var fairness = new ShiftCategoryFairness(catDic, new FairnessValueResult());
+			var fairness2 = new ShiftCategoryFairness(catDic2, new FairnessValueResult());
+
+			var result = _target.Compare(fairness, fairness2, new List<IShiftCategory>{cat,cat2,cat3});
+			Assert.That(result.Count,Is.EqualTo(3));
+			Assert.That(result[0].Original, Is.EqualTo(.50));	
+			Assert.That(result[1].Original, Is.EqualTo(.50));	
+			Assert.That(result[2].Original, Is.EqualTo(.0));
+
+			Assert.That(result[0].ComparedTo, Is.EqualTo(.0));
+			Assert.That(result[1].ComparedTo, Is.EqualTo(.50));
+			Assert.That(result[2].ComparedTo, Is.EqualTo(.50));
+		}
+	}
+
+	
+}
