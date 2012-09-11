2ae0d1022df412518075c3c569a596708e044113
ola@teleopti.com <ola@teleopti.com>
2012-09-11 11:54:23 +0200

    aggregate shiftcategoryfairness on a number of persons

---
+++ Domain/Optimization/ShiftCategoryFairnessAggregator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Optimization
+{
+	public interface IShiftCategoryFairnessAggregator
+	{
+		IShiftCategoryFairness GetShiftCategoryFairnessForPersons(IScheduleDictionary scheduleDictionary, IList<IPerson> persons);
+	}
+
+	public class ShiftCategoryFairnessAggregator : IShiftCategoryFairnessAggregator
+	{
+		public IShiftCategoryFairness GetShiftCategoryFairnessForPersons(IScheduleDictionary scheduleDictionary, IList<IPerson> persons)
+		{
+			IShiftCategoryFairness result = null;
+			foreach (var person in persons)
+			{
+				var schedule = scheduleDictionary[person];
+				if (result == null)
+					result = schedule.CachedShiftCategoryFairness();
+				else
+					result = result.Add(schedule.CachedShiftCategoryFairness());
+			}
+			return result;
+		}
+	}
+}
