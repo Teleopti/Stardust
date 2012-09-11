525165b7b9dee43003397723f6a31c1c59b64117
David Jonsson <david.jonsson@teleopti.com>
2012-09-11 14:59:22 +0200

    Backed out changeset: 7fa962abd2e9
    --HG--
    branch : Release 7.2.362

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -95,4 +95,12 @@ AND fix.Batch = @Batch
 -------------------
 IF (SELECT COUNT(1) FROM dbo.MainShiftActivityLayerFix306 WHERE Batch=@Batch)> 0
 PRINT 'Shifts have been updated'
+
+select * from dbo.MainShiftActivityLayerFix306 
+CREATE UNIQUE NONCLUSTERED INDEX [UIX_MainShiftActivityLayer_Parent_OrderIndex]
+ON [dbo].[MainShiftActivityLayer] 
+(
+	[Parent] ASC,
+	[OrderIndex] ASC
+)
 GO
