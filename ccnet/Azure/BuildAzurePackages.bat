62d594e08f17dcc90f6fe82481d6ff89427514b0
David Jonsson <david.jonsson@teleopti.com>
2012-09-13 10:06:46 +0200

    #19768 - adding on 368 to keep WISE binary in sync (default vs. 368)
    --HG--
    branch : Release 7.2.368

--- ccnet/Azure/BuildAzurePackages.bat
+++ ccnet/Azure/BuildAzurePackages.bat
@@ -53,6 +53,12 @@ for /f "tokens=1,2 delims=," %%g in (contentMapping.txt) do ROBOCOPY "%ContentSo
 Echo Getting Previous build. Done
 ECHO copy done
 
+::Get StartPage
+XCOPY /S /d /y "%ContentSource%\StartPage" "%ContentDest%\TeleoptiCCC"
+
+::Replace URL in index.html
+cscript replace.vbs "/TeleoptiCCC/" "/" "%ContentDest%\TeleoptiCCC\index.html"
+
 ::Get ReportViewer
 XCOPY /d /y "%Dependencies%\ReportViewer2010.exe" "%ContentDest%\TeleoptiCCC\bin"
 
