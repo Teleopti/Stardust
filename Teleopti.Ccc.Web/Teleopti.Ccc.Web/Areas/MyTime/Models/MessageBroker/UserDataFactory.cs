93d5f79a7e05f4073e99c9dd1d5415ab1ffe7014
Roger Kratz <roger.kratz@teleopti.com>
2012-09-19 14:13:27 +0200

    Test + impl for UserDataFactory.DataSourceName

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/MessageBroker/UserDataFactory.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/MessageBroker/UserDataFactory.cs
@@ -1,20 +1,27 @@
 ﻿using Teleopti.Ccc.Web.Core.RequestContext;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Models.MessageBroker
 {
 	public class UserDataFactory : IUserDataFactory
 	{
 		private readonly ICurrentBusinessUnitProvider _businessUnitProvider;
+		private readonly IDataSource _dataSource;
 
-		public UserDataFactory(ICurrentBusinessUnitProvider businessUnitProvider)
+		public UserDataFactory(ICurrentBusinessUnitProvider businessUnitProvider, 
+									IDataSource dataSource)
 		{
 			_businessUnitProvider = businessUnitProvider;
+			_dataSource = dataSource;
 		}
 
 		public UserData CreateViewModel()
 		{
+			var currentBu = _businessUnitProvider.CurrentBusinessUnit();
 			var ret = new UserData();
-			ret.BusinessUnitId = _businessUnitProvider.CurrentBusinessUnit().Id.Value;
+			if(currentBu!=null)
+				ret.BusinessUnitId = _businessUnitProvider.CurrentBusinessUnit().Id.Value;
+			ret.DataSourceName = _dataSource.DataSourceName;
 			return ret;
 		}
 	}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Models/MessageBroker/UserDataFactoryTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Models/MessageBroker/UserDataFactoryTest.cs
@@ -5,24 +5,45 @@ using SharpTestsEx;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.MessageBroker;
 using Teleopti.Ccc.Web.Core.RequestContext;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WebTest.Areas.MyTime.Models.MessageBroker
 {
 	[TestFixture]
 	public class UserDataFactoryTest
 	{
+		private ICurrentBusinessUnitProvider buProvider;
+		private IDataSource dataSource;
+		private IUserDataFactory target;
+
+		[SetUp]
+		public void Setup()
+		{
+			buProvider = MockRepository.GenerateMock<ICurrentBusinessUnitProvider>();
+			dataSource = MockRepository.GenerateMock<IDataSource>();
+			target = new UserDataFactory(buProvider, dataSource);			
+		}
+
 		[Test]
 		public void ShouldSetBusinessUnitId()
 		{
-			var buProvider = MockRepository.GenerateMock<ICurrentBusinessUnitProvider>();
-			var factory = new UserDataFactory(buProvider);
-
 			var expected = Guid.NewGuid();
 			var bu = new BusinessUnit("dd");
 			bu.SetId(expected);
 			buProvider.Expect(mock => mock.CurrentBusinessUnit()).Return(bu);
-			var result = factory.CreateViewModel();
+			var result = target.CreateViewModel();
 			result.BusinessUnitId.Should().Be.EqualTo(expected);
+		}
+
+		[Test]
+		public void ShouldSetDataSourceName()
+		{
+			var expected = "stoj och lek";
+
+			dataSource.Expect(mock => mock.DataSourceName).Return(expected);
+
+			var result = target.CreateViewModel();
+			result.DataSourceName.Should().Be.EqualTo(expected);
 		}
 	}
 }
