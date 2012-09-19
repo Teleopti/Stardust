44e02a168b53e4f7836a3eaad4b41b98a9d71970
Roger Kratz <roger.kratz@teleopti.com>
2012-09-19 13:39:54 +0200

    Added test + impl for messagebrokercontroller.fetchuserdata()

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/MessageBrokerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.MessageBroker;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
+{
+	public class MessageBrokerController : Controller
+	{
+		private readonly IUserDataFactory _userDataFactory;
+
+		public MessageBrokerController(IUserDataFactory userDataFactory)
+		{
+			_userDataFactory = userDataFactory;
+		}
+
+		[HttpGet]
+		public JsonResult FetchUserData()
+		{
+			return Json(_userDataFactory.CreateViewModel(), JsonRequestBehavior.AllowGet);
+		}
+	}
+}
