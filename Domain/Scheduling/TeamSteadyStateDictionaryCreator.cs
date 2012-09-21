b05e23436d785f06264c8cae75ea7be2bdb3d535
Claes Stenman <claes.stenman@teleopti.com>
2012-09-21 13:37:35 +0200

    Added threading...

--- Domain/Scheduling/TeamSteadyStateDictionaryCreator.cs
+++ Domain/Scheduling/TeamSteadyStateDictionaryCreator.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Teleopti.Interfaces.Domain;
 
@@ -10,6 +12,8 @@ namespace Teleopti.Ccc.Domain.Scheduling
 		private readonly ISchedulePeriodTargetTimeCalculator _schedulePeriodTargetTimeCalculator;
 		private readonly IList<IScheduleMatrixPro> _matrixList;
 
+		private delegate KeyValuePair<string, bool> TeamSteadyStateDelegate(IGroupPerson groupPerson, DateOnly dateOnly);
+
 		public TeamSteadyStateDictionaryCreator(IList<IGroupPerson> groupPersons, ISchedulePeriodTargetTimeCalculator schedulePeriodTargetTimeCalculator, IList<IScheduleMatrixPro> matrixList)
 		{
 			_groupPersons = groupPersons;
@@ -19,60 +23,32 @@ namespace Teleopti.Ccc.Domain.Scheduling
 
 		public IDictionary<string, bool> Create(DateOnly dateOnly)
 		{
-			var dictionary = new Dictionary<string, bool>();
+			var runnableList = new Dictionary<TeamSteadyStateDelegate, IAsyncResult>();
 
 			foreach (var groupPerson in _groupPersons)
 			{
-				var firstPerson = groupPerson.GroupMembers[0];
-				if (groupPerson.GroupMembers.Count == 0) continue;
-				var teamSteadyState = CreateTeamSteadyState(firstPerson, dateOnly);
-				if (teamSteadyState == null) continue;
-				var steadyState = true;
-
-				foreach (var person in groupPerson.GroupMembers)
-				{
-					if(!person.Equals(firstPerson))
-					{
-						var virtualSchedulePeriod = person.VirtualSchedulePeriod(dateOnly);
-						var scheduleMatrixPro = GetScheduleMatrixPro(virtualSchedulePeriod);
-						if (scheduleMatrixPro == null) continue;
-						var personPeriod = person.PersonPeriods(new DateOnlyPeriod(dateOnly, dateOnly)).SingleOrDefault();
-						if (personPeriod == null) continue;
-						
-						if(!teamSteadyState.SteadyState(virtualSchedulePeriod, scheduleMatrixPro, personPeriod))
-						{
-							steadyState = false;
-							break;
-						}
-					}
-				}
-
-				dictionary.Add(groupPerson.Name.FirstName, steadyState);
+				var runner = new TeamSteadyStateRunner(_matrixList, _schedulePeriodTargetTimeCalculator);
+				TeamSteadyStateDelegate toRun = runner.Run;
+				var result = toRun.BeginInvoke(groupPerson, dateOnly, null, null);
+				runnableList.Add(toRun,result);
 			}
 
-			return dictionary;
-		}
-
-		private ITeamSteadyState CreateTeamSteadyState(IPerson firstPerson, DateOnly dateOnly)
-		{
-			var firstPersonvirtualSchedulePeriod = firstPerson.VirtualSchedulePeriod(dateOnly);
-			var firstPersonScheduleMatrixPro = GetScheduleMatrixPro(firstPersonvirtualSchedulePeriod);
-			if (firstPersonScheduleMatrixPro == null) return null;
-			var firstPersonpersonPeriod = firstPerson.PersonPeriods(new DateOnlyPeriod(dateOnly, dateOnly)).SingleOrDefault();
-			if (firstPersonpersonPeriod == null) return null;
-			var firstPersonteamSteadyStateSchedulePeriod = new TeamSteadyStateSchedulePeriod(firstPersonvirtualSchedulePeriod, _schedulePeriodTargetTimeCalculator, firstPersonScheduleMatrixPro);
-			var firstPersonteamSteadyStatePersonPeriod = new TeamSteadyStatePersonPeriod(firstPersonpersonPeriod);
-			return new TeamSteadyState(firstPersonteamSteadyStatePersonPeriod, firstPersonteamSteadyStateSchedulePeriod);	
-		}
-
-		private IScheduleMatrixPro GetScheduleMatrixPro(IVirtualSchedulePeriod virtualSchedulePeriod)
-		{
-			foreach (var scheduleMatrixPro in _matrixList)
+			//Sync all threads
+			IList<KeyValuePair<string, bool>> results = new List<KeyValuePair<string, bool>>();
+			try
 			{
-				if (scheduleMatrixPro.Person.Equals(virtualSchedulePeriod.Person) && scheduleMatrixPro.SchedulePeriod.Equals(virtualSchedulePeriod)) return scheduleMatrixPro;
+				foreach (var thread in runnableList)
+				{
+					results.Add(thread.Key.EndInvoke(thread.Value));
+				}
+			}
+			catch (Exception e)
+			{
+				Trace.WriteLine(e.Message);
+				throw;
 			}
 
-			return null;
+			return results.ToDictionary(keyValuePair => keyValuePair.Key, keyValuePair => keyValuePair.Value);
 		}
 	}
 }
