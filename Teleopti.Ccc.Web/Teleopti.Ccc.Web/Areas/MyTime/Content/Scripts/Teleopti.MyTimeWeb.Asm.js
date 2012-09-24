7bc5bd14b465941c26418c8138d232f5dc5803d5
Roger Kratz <roger.kratz@teleopti.com>
2012-09-24 13:35:56 +0200

    fix for 20805
    --HG--
    branch : Release 7.2.370

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
@@ -130,7 +130,7 @@ Teleopti.MyTimeWeb.Asm = (function () {
 		};
 
 		Teleopti.MyTimeWeb.Ajax.Ajax({
-			url: '/MyTime/MessageBroker/FetchUserData',
+			url: 'MessageBroker/FetchUserData',
 			dataType: "json",
 			type: 'GET',
 			success: function (data) {
