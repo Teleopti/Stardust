3e4d91b1a6fd889080e22c8b04a2de37bc4ab81a
David Jonsson <david.jonsson@teleopti.com>
2012-09-26 20:45:20 +0200

    Merge strut in Trunk.sql
    --HG--
    branch : Release 7.2.370

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -7,26 +7,6 @@ set
 	FunctionCode='ResReportAdherencePerDay',
 	FunctionDescription='xxResReportAgentScheduleAdherance'
 where ForeignId='D1ADE4AC-284C-4925-AEDD-A193676DBD2F'
---Robin: Removing column IsDeleted as it isn't used anymore.
-
-IF EXISTS(SELECT * FROM sys.columns WHERE object_id=OBJECT_ID('dbo.StateGroupActivityAlarm','U') AND name='IsDeleted')
-	BEGIN
-		EXECUTE('DELETE FROM dbo.StateGroupActivityAlarm WHERE IsDeleted=1')
-	END
-GO
-
-IF EXISTS(SELECT * FROM sys.columns WHERE object_id=OBJECT_ID('dbo.StateGroupActivityAlarm','U') AND name='IsDeleted')
-	BEGIN
-		ALTER TABLE dbo.StateGroupActivityAlarm	DROP COLUMN IsDeleted
-	END
-	
-GO
-
---Anders: Adding possiblity to purge old messages cause it's been asked for
-if not exists (select 1 from PurgeSetting where [Key] = 'Message')
-begin
-	insert into PurgeSetting values ('Message', 10)
-end
 
 ----------------  
 --Name: David J
