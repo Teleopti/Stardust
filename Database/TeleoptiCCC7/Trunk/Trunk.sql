c26b094585bdb7f78cc4a8c955024f8f6c50c23b
David Jonsson <david.jonsson@teleopti.com>
2012-09-26 20:04:11 +0200

    #18771 - Allowance bug, need to truncate readModel.ScheduleProjectionReadOnly
    --HG--
    branch : Release 7.2.362

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -19,3 +19,19 @@ begin
 	insert into PurgeSetting values ('Message', 10)
 end
 
+----------------  
+--Name: David J
+--Date: 2012-09-26
+--Desc: #18771 - Allowance bug, need to truncate readModel.ScheduleProjectionReadOnly
+--		Since we are adding this on Trunk.sql => Add logic to truncate only once
+----------------  
+declare @bugId int
+set @bugId=-17881
+if not exists (select * from dbo.DatabaseVersion where BuildNumber=@bugId)
+begin
+	DECLARE @systemVersion varchar(100)
+	SELECT TOP 1 @systemVersion=systemVersion FROM dbo.DatabaseVersion order by BuildNumber desc
+	SELECT @systemVersion = @systemVersion + '.1'
+	TRUNCATE TABLE readModel.ScheduleProjectionReadOnly
+	INSERT INTO DatabaseVersion(BuildNumber, SystemVersion) VALUES (@bugId,@systemVersion)
+end
