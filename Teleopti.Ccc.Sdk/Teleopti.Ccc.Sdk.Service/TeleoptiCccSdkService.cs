d6f12a361aa25d2232cfb4786a0a92aeb94e71e5
Talha Muhammad <talha.muhammad@teleopti.com>
2012-09-26 16:47:13 +0200

    PBI20362

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Service/TeleoptiCccSdkService.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Service/TeleoptiCccSdkService.cs
@@ -509,6 +509,15 @@ namespace Teleopti.Ccc.Sdk.WcfService
 			}		
     	}
 
+    	public ICollection<DefinitionSetDto> GetMultiplicatorDefinitionSetByQuery(QueryDto queryDto)
+    	{
+			using (var inner = _lifetimeScope.BeginLifetimeScope())
+			{
+				var invoker = inner.Resolve<IInvokeQuery<ICollection<DefinitionSetDto>>>();
+				return invoker.Invoke(queryDto);
+			}	
+    	}
+
     	public IAsyncResult BeginCreateServerScheduleDistribution(PersonDto[] personList, DateOnlyDto startDate, DateOnlyDto endDate, string timeZoneId, AsyncCallback callback, object asyncState)
 		{
 			CreateScheduleDistributionAsyncResult asyncResult = new CreateScheduleDistributionAsyncResult(callback, asyncState);
