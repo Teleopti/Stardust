bb82cdad419d36a9341e7f93a4e03539f8149333
henry.greijer@teleopti.com <henry.greijer@teleopti.com>
2012-09-30 20:52:11 +0200

    Tweaked the icons

--- Teleopti.Support.Tool/MainForm.Designer.cs
+++ Teleopti.Support.Tool/MainForm.Designer.cs
@@ -30,15 +30,16 @@ namespace Teleopti.Support.Tool
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.PTracks = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.linkManageDBVersions = new Teleopti.Support.Tool.Controls.General.SmoothLink();
-            this.LLChangeDBConn = new Teleopti.Support.Tool.Controls.General.SmoothLink();
             this.BClose = new System.Windows.Forms.Button();
             this.pictureBoxBathingBall = new System.Windows.Forms.PictureBox();
             this.panelContent = new System.Windows.Forms.Panel();
             this.labelHeader = new Teleopti.Support.Tool.Controls.General.SmoothLabel();
+            this.linkManageDBVersions = new Teleopti.Support.Tool.Controls.General.SmoothLink();
+            this.LLChangeDBConn = new Teleopti.Support.Tool.Controls.General.SmoothLink();
             this.PTracks.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBathingBall)).BeginInit();
             this.panelContent.SuspendLayout();
@@ -74,34 +75,6 @@ namespace Teleopti.Support.Tool
             this.label1.TabIndex = 7;
             this.label1.Text = "Update database names and users in all connections and updates database reference" +
     "s in databases";
-            // 
-            // linkManageDBVersions
-            // 
-            this.linkManageDBVersions.AutoSize = true;
-            this.linkManageDBVersions.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkManageDBVersions.Location = new System.Drawing.Point(5, 82);
-            this.linkManageDBVersions.Margin = new System.Windows.Forms.Padding(5);
-            this.linkManageDBVersions.Name = "linkManageDBVersions";
-            this.linkManageDBVersions.Size = new System.Drawing.Size(231, 21);
-            this.linkManageDBVersions.TabIndex = 1;
-            this.linkManageDBVersions.TabStop = true;
-            this.linkManageDBVersions.Text = "Teleopti CCC Database Versions";
-            this.linkManageDBVersions.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            this.linkManageDBVersions.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
-            // 
-            // LLChangeDBConn
-            // 
-            this.LLChangeDBConn.AutoSize = true;
-            this.LLChangeDBConn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LLChangeDBConn.Location = new System.Drawing.Point(5, 26);
-            this.LLChangeDBConn.Margin = new System.Windows.Forms.Padding(5);
-            this.LLChangeDBConn.Name = "LLChangeDBConn";
-            this.LLChangeDBConn.Size = new System.Drawing.Size(221, 21);
-            this.LLChangeDBConn.TabIndex = 0;
-            this.LLChangeDBConn.TabStop = true;
-            this.LLChangeDBConn.Text = "Change Database Connections";
-            this.LLChangeDBConn.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            this.LLChangeDBConn.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLChangeDBConn_LinkClicked);
             // 
             // BClose
             // 
@@ -145,6 +118,34 @@ namespace Teleopti.Support.Tool
             this.labelHeader.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             this.labelHeader.UseCompatibleTextRendering = true;
             // 
+            // linkManageDBVersions
+            // 
+            this.linkManageDBVersions.AutoSize = true;
+            this.linkManageDBVersions.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkManageDBVersions.Location = new System.Drawing.Point(5, 82);
+            this.linkManageDBVersions.Margin = new System.Windows.Forms.Padding(5);
+            this.linkManageDBVersions.Name = "linkManageDBVersions";
+            this.linkManageDBVersions.Size = new System.Drawing.Size(231, 21);
+            this.linkManageDBVersions.TabIndex = 1;
+            this.linkManageDBVersions.TabStop = true;
+            this.linkManageDBVersions.Text = "Teleopti CCC Database Versions";
+            this.linkManageDBVersions.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.linkManageDBVersions.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // LLChangeDBConn
+            // 
+            this.LLChangeDBConn.AutoSize = true;
+            this.LLChangeDBConn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LLChangeDBConn.Location = new System.Drawing.Point(5, 26);
+            this.LLChangeDBConn.Margin = new System.Windows.Forms.Padding(5);
+            this.LLChangeDBConn.Name = "LLChangeDBConn";
+            this.LLChangeDBConn.Size = new System.Drawing.Size(221, 21);
+            this.LLChangeDBConn.TabIndex = 0;
+            this.LLChangeDBConn.TabStop = true;
+            this.LLChangeDBConn.Text = "Change Database Connections";
+            this.LLChangeDBConn.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.LLChangeDBConn.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLChangeDBConn_LinkClicked);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -156,8 +157,10 @@ namespace Teleopti.Support.Tool
             this.Controls.Add(this.BClose);
             this.Controls.Add(this.panelContent);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.Text = "Teleopti CCC Configuration Manager";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.PTracks.ResumeLayout(false);
             this.PTracks.PerformLayout();
