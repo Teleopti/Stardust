8566f9df2195adc2a1d51f1ba23870de21f23e8a
Asad MIrza <asad.mirza@teleopti.com>
2012-10-02 15:09:21 +0200

    Updated the xml comments

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IRemoveShiftCategoryBackToLegalService.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IRemoveShiftCategoryBackToLegalService.cs
@@ -16,6 +16,9 @@ namespace Teleopti.Interfaces.Domain
 		/// <returns></returns>
         IList<IScheduleDayPro> Execute(IShiftCategoryLimitation shiftCategoryLimitation, ISchedulingOptions schedulingOptions);
 
+        /// <summary>
+        /// Schedule matrix for a single person
+        /// </summary>
         IScheduleMatrixPro ScheduleMatrixPro { get; }
     }
 }
