f5438c793933b27f48941639feb362f59162a5ad
Erik Sundberg <erik.sundberg@teleopti.com>
2012-10-03 16:16:57 +0200

    Added RuleSet Checker

---
+++ Domain/Optimization/ShiftCategoryFairness/ShiftCategoryFairnessRuleSetChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Optimization.ShiftCategoryFairness
+{
+    public class ShiftCategoryFairnessRuleSetChecker
+    {
+        public bool Check(IPersonPeriod period1, IPersonPeriod period2)
+        {
+            return period1.RuleSetBag == period2.RuleSetBag || period1.RuleSetBag.RuleSetCollection.Intersect(period2.RuleSetBag.RuleSetCollection).Any();
+        }
+    }
+}
