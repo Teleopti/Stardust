691843a27c960c1f46eb7b575b21cf53739599de
tamasb <none@none>
2012-10-03 14:14:52 +0200

    add testclass

---
+++ Domain/Scheduling/Restrictions/PreferenceFulfilledChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.Restrictions
+{
+
+	/// <summary>
+	/// Checks if the preference is fullfilled for a given day
+	/// </summary>
+	public interface IPreferenceFulfilledChecker
+	{
+		bool IsPreferenceFulfilled(DateOnly theDay);
+	}
+
+
+	public class PreferenceFulfilledChecker : IPreferenceFulfilledChecker
+	{
+		public bool IsPreferenceFulfilled(DateOnly theDay)
+		{
+			return false;
+		}
+	}
+}
