c48a00c96dcf1548a8f5c83b9f3f493647eacb09
Mathias Stenbom <mathias.stenbom@teleopti.com>
2012-10-05 10:50:20 +0200

    Added configuration sanity check to behavior tests.

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/ConfigSanityCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Teleopti.Ccc.WebBehaviorTest
+{
+
+	[TestFixture]
+	public class ConfigSanityCheck
+	{
+		[Test]
+		public void ShouldLoadConfig()
+		{
+			Assert.That(ConfigurationManager.AppSettings["configSanityCheck"], 
+				Is.EqualTo("Yes, configuration is loaded!"), 
+				"No! Configuration is not loaded!!!");
+		}
+	}
+}
