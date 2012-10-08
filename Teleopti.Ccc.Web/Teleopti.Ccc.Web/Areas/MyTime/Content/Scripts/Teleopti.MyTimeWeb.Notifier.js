f11553fc8b656f18bed0da87562fffb37b86d796
Roger Kratz <roger.kratz@teleopti.com>
2012-10-08 13:08:22 +0200

    adding Noty lib and added MyTimeWeb.Notifier.js for this

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
@@ -0,0 +1,20 @@
+Teleopti.MyTimeWeb.Notifier = (function() {
+	function _notify(options) {
+		return noty({
+			text: options.text,
+			layout: 'top',
+			timeout: 5000,
+			animation: {
+				open: { height: 'toggle' },
+				close: { height: 'toggle' },
+				easing: 'swing',
+				speed: 500 // opening & closing animation speed
+			}
+		});
+	}
+	return {
+		Notify: function (options) {
+			_notify(options);
+		}
+	};
+})(jQuery)
