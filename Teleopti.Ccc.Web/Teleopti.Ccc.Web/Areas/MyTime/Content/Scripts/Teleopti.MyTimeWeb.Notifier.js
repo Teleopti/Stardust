d15e494c033c26984d1bbde6effc489cc4b229ed
Roger Kratz <roger.kratz@teleopti.com>
2012-10-08 16:23:07 +0200

    notifier on bottom instead

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
@@ -2,7 +2,7 @@ Teleopti.MyTimeWeb.Notifier = (function() {
 	function _notify(options) {
 		return noty({
 			text: options.text,
-			layout: 'top',
+			layout: 'bottom',
 			timeout: 5000,
 			animation: {
 				open: { height: 'toggle' },
