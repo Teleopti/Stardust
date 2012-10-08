2ec946f0d720d9b0fd815dcdf044de6141940263
Roger Kratz <roger.kratz@teleopti.com>
2012-10-08 17:40:54 +0200

    refactoring. now logic for nice web notifications (=chrome) is done.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
@@ -1,9 +1,18 @@
-Teleopti.MyTimeWeb.Notifier = (function() {
-	function _notify(options) {
+Teleopti.MyTimeWeb.Notifier = (function () {
+	var notifyText;
+	var timeout = 5000;
+
+	function _setOptions(options) {
+		notifyText = options.text;
+		if (options.timeout) {
+			timeout = options.timeout;
+		}
+	}
+	function _notify() {
 		return noty({
-			text: options.text,
+			text: notifyText,
 			layout: 'bottom',
-			timeout: 5000,
+			timeout: timeout,
 			animation: {
 				open: { height: 'toggle' },
 				close: { height: 'toggle' },
@@ -12,9 +21,25 @@ Teleopti.MyTimeWeb.Notifier = (function() {
 			}
 		});
 	}
+	function _webNotification() {
+		if (window.webkitNotifications) {
+			if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
+				var notification = window.webkitNotifications.createNotification('', notifyText, '');
+				notification.show();
+				setTimeout(function() {
+					notification.cancel();
+				}, timeout);
+			} else {
+				window.webkitNotifications.requestPermission();
+			}
+		}
+	}
+
 	return {
 		Notify: function (options) {
-			_notify(options);
+			_setOptions(options);
+			_notify();
+			_webNotification();
 		}
 	};
-})(jQuery)
+})(jQuery);
