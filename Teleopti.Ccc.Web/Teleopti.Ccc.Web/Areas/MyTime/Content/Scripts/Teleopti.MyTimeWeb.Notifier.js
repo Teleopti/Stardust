0c36f59fc4f9762d6071a617d441726f87df6a3b
Roger Kratz <roger.kratz@teleopti.com>
2012-10-09 13:45:10 +0200

    avoid duplicated web notifications if both asm and portal open

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
@@ -2,6 +2,8 @@ Teleopti.MyTimeWeb.Notifier = (function () {
 	var notifyText;
 	var timeout = 5000;
 	var baseUrl;
+	var header = '';
+	var webNotification = function () { return true; }; //default also send as web not if possible
 
 	function _setOptions(options) {
 		notifyText = options.notifyText;
@@ -12,6 +14,12 @@ Teleopti.MyTimeWeb.Notifier = (function () {
 			baseUrl = options.baseUrl;
 		} else {
 			console.warning('Missing base url for icon in notification!');
+		}
+		if (options.module) {
+			header = options.module;
+		}
+		if (options.webNotification) {
+			webNotification = options.webNotification;
 		}
 	}
 	function _notify() {
@@ -30,14 +38,14 @@ Teleopti.MyTimeWeb.Notifier = (function () {
 	function _webNotification() {
 		if (window.webkitNotifications) {
 			if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
-				var iconUrl = baseUrl + 'content/favicon.ico';
-				var notification = window.webkitNotifications.createNotification(iconUrl, notifyText, '');
-				notification.show();
-				setTimeout(function () {
-					notification.cancel();
-				}, timeout);
-			} else {
-				window.webkitNotifications.requestPermission();
+				if (webNotification()) {
+					var iconUrl = baseUrl + 'content/favicon.ico';
+					var notification = window.webkitNotifications.createNotification(iconUrl, header, notifyText);
+					notification.show();
+					setTimeout(function () {
+						notification.cancel();
+					}, timeout);
+				}
 			}
 		}
 	}
