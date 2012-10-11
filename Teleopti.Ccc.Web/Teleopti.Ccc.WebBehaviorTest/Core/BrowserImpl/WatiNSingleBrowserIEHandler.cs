f6d833dd368fcbca268fe5ea9ff69e6d7cc8021f
Kunning Mao <kunning.mao@teleopti.com>
2012-10-11 22:26:44 +0200

    minor

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Core/BrowserImpl/WatiNSingleBrowserIEHandler.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Core/BrowserImpl/WatiNSingleBrowserIEHandler.cs
@@ -16,7 +16,7 @@ namespace Teleopti.Ccc.WebBehaviorTest.Core.BrowserImpl
 		private static IDisposable BrowserLock { get; set; }
 		//private static IDisposable BrowserLock { get { return ScenarioContext.Current.Value<SystemLevelLock>(); } set { ScenarioContext.Current.Value((SystemLevelLock)value); } }
 
-		private static bool _closeByWatiNCloseNDisposeFailed = false;
+		//private static bool _closeByWatiNCloseNDisposeFailed = false;
 		private IE _browser;
 
 		[SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
