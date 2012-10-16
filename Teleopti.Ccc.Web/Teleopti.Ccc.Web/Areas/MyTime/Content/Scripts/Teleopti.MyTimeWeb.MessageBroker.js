af7470b477184620d8c7e39c93e0e3f8fd97061d
Henke <henrik.andersson@teleopti.com>
2012-10-16 14:18:29 +0200

    new syntax to support cors long polling (IE issue)

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.MessageBroker.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.MessageBroker.js
@@ -7,7 +7,7 @@ Teleopti.MyTimeWeb.MessageBroker = (function () {
 
 		hub.onEventMessage = options.callback;
 
-		$.connection.hub.start({ transport: 'longPolling', xdomain: true })
+		$.connection.hub.start({ jsonp: true })
 			.done(function () {
 				hub.addSubscription({
 					'DomainType': options.domainType,
