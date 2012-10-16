7314e3e1259effa87d946252041b4d250f6f4508
Robin Karlsson <robin.karlsson@teleopti.com>
2012-10-16 13:55:40 +0200

    Adding comments

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/TimeZoneInfoExtensions.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/TimeZoneInfoExtensions.cs
@@ -2,8 +2,17 @@ using System;
 
 namespace Teleopti.Interfaces.Domain
 {
+    /// <summary>
+    /// Extension methods for time zone info class
+    /// </summary>
     public static class TimeZoneInfoExtensions
     {
+		/// <summary>
+		/// Converts a time to UTC and handles invalid times by avoiding them.
+		/// </summary>
+		/// <param name="timeZoneInfo">The source time zone.</param>
+		/// <param name="dateTime">The datetime local to the time zone.</param>
+		/// <returns></returns>
         public static DateTime SafeConvertTimeToUtc(this TimeZoneInfo timeZoneInfo, DateTime dateTime)
         {
             var kindUnSpecifiedDateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified);
