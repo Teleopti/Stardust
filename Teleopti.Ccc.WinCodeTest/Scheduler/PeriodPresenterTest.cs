89fe80352e48cebb6d59c64b84dbd98bb3512d7e
micke@teleopti.com <micke@teleopti.com>
2012-10-17 14:21:15 +0200

    Bug #21041 Can not edit people after change schedule
    --HG--
    branch : Release 7.2.372

--- Teleopti.Ccc.WinCodeTest/Scheduler/PeriodPresenterTest.cs
+++ Teleopti.Ccc.WinCodeTest/Scheduler/PeriodPresenterTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Syncfusion.Windows.Forms.Grid;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Scheduling.Rules;
 using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
@@ -12,7 +13,7 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCodeTest.Scheduler
 {
-    [TestFixture]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable"), TestFixture]
     public class PeriodPresenterTest
     {
         private PeriodPresenter target;
@@ -24,27 +25,61 @@ namespace Teleopti.Ccc.WinCodeTest.Scheduler
         private SchedulerStateHolder schedulerState;
         private IOverriddenBusinessRulesHolder _overriddenBusinessRulesHolder;
         private IScheduleDayChangeCallback _scheduleDayChangeCallback;
+    	private GridControl _grid;
 
         [SetUp]
         public void Setup()
         {
             
             mocks = new MockRepository();
-            viewBase = mocks.StrictMock<IScheduleViewBase>();
+			_grid = new GridControl();
+			viewBase = mocks.StrictMock<IScheduleViewBase>();
             scenario = mocks.StrictMock<IScenario>();
             gridlockManager = new GridlockManager();
             clipHandlerSchedulePart = new ClipHandler<IScheduleDay>();
             schedulerState = new SchedulerStateHolder(scenario,new DateOnlyPeriodAsDateTimePeriod(new DateOnlyPeriod(), TeleoptiPrincipal.Current.Regional.TimeZone), new List<IPerson>());
             _overriddenBusinessRulesHolder = new OverriddenBusinessRulesHolder();
-            _scheduleDayChangeCallback = mocks.DynamicMock<IScheduleDayChangeCallback>();
+            _scheduleDayChangeCallback = mocks.StrictMock<IScheduleDayChangeCallback>();
             target = new PeriodPresenter(viewBase, schedulerState, gridlockManager, clipHandlerSchedulePart,
                                       SchedulePartFilter.None, _overriddenBusinessRulesHolder, _scheduleDayChangeCallback, NullScheduleTag.Instance);
         }
+
+		[TearDown]
+		public void Teardown()
+		{
+			_grid.Dispose();
+		}
 
         [Test]
         public void VerifyInstanceCreated()
         {
             Assert.IsNotNull(target);
         }
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope"), Test]
+		public void ShouldSetCellToStaticIfNotHeaderOrFixedColumn()
+		{
+			using (mocks.Record())
+			{
+				Expect.Call(viewBase.RowHeaders).Return(1).Repeat.AtLeastOnce();
+				Expect.Call(viewBase.TheGrid).Return(_grid).Repeat.AtLeastOnce();
+			}
+
+			using(mocks.Playback())
+			{
+				GridStyleInfo style = new GridStyleInfo();
+				style.CellType = "CheckBox";
+				GridQueryCellInfoEventArgs args = new GridQueryCellInfoEventArgs(0, (int)ColumnType.StartScheduleColumns - 2, style);
+				target.QueryCellInfo(this, args);
+				Assert.IsFalse(style.CellType == "Static");
+
+				style = new GridStyleInfo();
+				args = new GridQueryCellInfoEventArgs(2, (int)ColumnType.StartScheduleColumns, style);
+				target.QueryCellInfo(this, args);
+				Assert.IsTrue(style.CellType == "Static");
+
+				style.Dispose();
+			}
+		}
     }
 }
