998ee0a3b8df1235c627060b59acdad4818ad868
Johan Ryding <johan.ryding@teleopti.com>
2012-10-18 15:40:54 +0200

    Made trunk work in Azure
    --HG--
    branch : Release 7.2.362

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -43,6 +43,6 @@ end
 ----------------  
 IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PreferenceDay]') AND name = N'uq_preference_day_per_agent')
 BEGIN
-EXECUTE('DROP INDEX [uq_preference_day_per_agent] ON [dbo].[PreferenceDay] WITH ( ONLINE = OFF )')
+EXECUTE('DROP INDEX [uq_preference_day_per_agent] ON [dbo].[PreferenceDay]')
 END
 GO
