c4cde2f99f477819a778e5ae88077bf795ebc1dd
Roger Kratz <roger.kratz@teleopti.com>
2012-10-19 14:53:14 +0200

    fixing behavior test. can now persist schedule in a "when"

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/UserStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/UserStepDefinitions.cs
@@ -1,5 +1,6 @@
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Ccc.WebBehaviorTest.Data;
 using Teleopti.Ccc.WebBehaviorTest.Data.Setups.Generic;
 
@@ -23,11 +24,22 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic
 		}
 
 		[Given(@"there is a shift with")]
-		[When(@"there is a shift with")]
 		public void GivenThereIsAShiftWith(Table table)
 		{
 			var schedule = table.CreateInstance<ShiftConfigurable>();
 			UserFactory.User().Setup(schedule);
+		}
+
+		[When(@"there is a shift with")]
+		public void GivenThereIsAShiftWithWhen(Table table)
+		{
+			var schedule = table.CreateInstance<ShiftConfigurable>();
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			{
+				var user = UserFactory.User().Person;
+				schedule.Apply(uow, user, user.PermissionInformation.Culture());
+				uow.PersistAll();
+			}
 		}
 
 		[Given(@"I have a meeting scheduled")]
