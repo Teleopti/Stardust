9b25af2c48362856d69600f3804d34d594449961
Claes Stenman <claes.stenman@teleopti.com>
2012-10-22 14:19:20 +0200

    Refact schedulingscreen...

---
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/ClipboardItems.cs
@@ -0,0 +1,37 @@
+﻿namespace Teleopti.Interfaces.Domain
+{
+	/// <summary>
+	/// Clipboard items
+	/// </summary>
+	public enum ClipboardItems
+	{
+		/// <summary>
+		/// Shift
+		/// </summary>
+		Shift,
+		/// <summary>
+		/// Absence
+		/// </summary>
+		Absence,
+		/// <summary>
+		/// DayOff
+		/// </summary>
+		DayOff,
+		/// <summary>
+		/// PersonalShift
+		/// </summary>
+		PersonalShift,
+		/// <summary>
+		/// Special
+		/// </summary>
+		Special,
+		/// <summary>
+		/// Overtime
+		/// </summary>
+		Overtime,
+		/// <summary>
+		/// ShiftFromShifts
+		/// </summary>
+		ShiftFromShifts
+	}
+}
