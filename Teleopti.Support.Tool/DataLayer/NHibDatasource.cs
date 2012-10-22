dea057f1e14b986fe5103587c8d1e674b3e872c2
henry.greijer@teleopti.com <henry.greijer@teleopti.com>
2012-10-22 14:07:42 +0200

    Fix for missing file
    --HG--
    branch : Release 7.2.372

---
+++ Teleopti.Support.Tool/DataLayer/NHibDatasource.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+
+namespace Teleopti.Support.Tool.DataLayer
+{
+    public class NHibDataSource
+    {
+        private readonly string _connectionString;
+        private readonly string _databaseTextConstant;
+        private SqlConnectionStringBuilder _sqlConnectionStringBuilder;
+        private string _version;
+        public const string NotConnected = "Not connected";
+
+        public NHibDataSource(string connectionString, string databaseTextConstant)
+        {
+            _connectionString = connectionString;
+            _databaseTextConstant = databaseTextConstant;
+            _sqlConnectionStringBuilder = new SqlConnectionStringBuilder(_connectionString);
+        }
+
+        public string CccDatabaseType
+        {
+            get { return _databaseTextConstant; }
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+
+        public string DatabaseName
+        {
+            get { return _sqlConnectionStringBuilder.InitialCatalog; }
+        }
+
+        public string ServerName
+        {
+            get { return _sqlConnectionStringBuilder.DataSource; }
+        }
+
+        public string Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+    }
+}
