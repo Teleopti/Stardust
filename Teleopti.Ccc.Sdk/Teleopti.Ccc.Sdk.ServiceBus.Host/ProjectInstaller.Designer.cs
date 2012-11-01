6b4b5ce7ae492e5f45e0f1827f2483e32bc6c6f5
Robin Karlsson <robin.karlsson@teleopti.com>
2012-11-01 09:27:43 +0100

    Trying out with delayed start option to see if it works better.

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus.Host/ProjectInstaller.Designer.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus.Host/ProjectInstaller.Designer.cs
@@ -28,25 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
-            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
-            // 
-            // serviceInstaller1
-            // 
-            this.serviceInstaller1.Description = "Backend processing of longrunning tasks";
-            this.serviceInstaller1.DisplayName = "Teleopti Service Bus";
-            this.serviceInstaller1.ServiceName = "TeleoptiServiceBus";
-            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
-            // 
-            // serviceProcessInstaller1
-            // 
-            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
-            this.serviceProcessInstaller1.Password = null;
-            this.serviceProcessInstaller1.Username = null;
-            // 
-            // ProjectInstaller
-            // 
-            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+			this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+			this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+			// 
+			// serviceProcessInstaller1
+			// 
+			this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+			this.serviceProcessInstaller1.Password = null;
+			this.serviceProcessInstaller1.Username = null;
+			// 
+			// serviceInstaller1
+			// 
+			this.serviceInstaller1.DelayedAutoStart = true;
+			this.serviceInstaller1.Description = "Backend processing of longrunning tasks";
+			this.serviceInstaller1.DisplayName = "Teleopti Service Bus";
+			this.serviceInstaller1.ServiceName = "TeleoptiServiceBus";
+			this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+			// 
+			// ProjectInstaller
+			// 
+			this.Installers.AddRange(new System.Configuration.Install.Installer[] {
             this.serviceProcessInstaller1,
             this.serviceInstaller1});
 
