752a2118dc9e9d430d0ae5ba5bfae7c9015a33f6
Kunning Mao <kunning.mao@teleopti.com>
2012-11-06 15:50:30 +0100

    fix test

--- Domain/Security/UserDetail.cs
+++ Domain/Security/UserDetail.cs
@@ -4,62 +4,86 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Security
 {
-    public class UserDetail : SimpleAggregateRoot, IUserDetail
-    {
-        private readonly IPerson _person;
-		public DateTime LastPasswordChange { get; set; }
-		public int InvalidAttempts { get; set; }
-        private bool _isLocked;
-		public DateTime InvalidAttemptsSequenceStart { get; set; }
+	public class UserDetail : SimpleAggregateRoot, IUserDetail
+	{
+		private readonly IPerson _person;
+		private DateTime _lastPasswordChange;
+		private int _invalidAttempts;
+		private bool _isLocked;
+		private DateTime _invalidAttemptsSequenceStart;
 
-        protected UserDetail()
-        {
-        }
+		protected UserDetail()
+		{
+		}
 
-        public UserDetail(IPerson person) : this()
-        {
-            _person = person;
-			LastPasswordChange = DateTime.UtcNow;
-			InvalidAttemptsSequenceStart = DateTime.UtcNow;
-        }
+		public UserDetail(IPerson person) : this()
+		{
+			_person = person;
+			_lastPasswordChange = DateTime.UtcNow;
+			_invalidAttemptsSequenceStart = DateTime.UtcNow;
+		}
 
-        public virtual bool IsLocked
-        {
-            get {
-                return _isLocked;
-            }
-        }
+		public virtual DateTime LastPasswordChange
+		{
+			get {
+				return _lastPasswordChange;
+			}
+			set { _lastPasswordChange = value; }
+		}
 
-        public virtual IPerson Person
-        {
-            get {
-                return _person;
-            }
-        }
+		public virtual int InvalidAttempts
+		{
+			get {
+				return _invalidAttempts;
+			}
+			set { _invalidAttempts = value; }
+		}
 
-        public virtual void Lock()
-        {
-            _isLocked = true;
-        }
+		public virtual bool IsLocked
+		{
+			get {
+				return _isLocked;
+			}
+		}
 
-	    public virtual void RegisterInvalidAttempt(IPasswordPolicy passwordPolicy)
-        {
-            DateTime utcNow = DateTime.UtcNow;
-			if (utcNow > InvalidAttemptsSequenceStart.Add(passwordPolicy.InvalidAttemptWindow) ||
-                InvalidAttempts == 0)
-            {
-                //Start of new sequence
-				InvalidAttemptsSequenceStart = utcNow;
-				InvalidAttempts = 0;
-            }
-			InvalidAttempts++;
-        }
+		public virtual DateTime InvalidAttemptsSequenceStart
+		{
+			get {
+				return _invalidAttemptsSequenceStart;
+			}
+			set { _invalidAttemptsSequenceStart = value; }
+		}
 
-        public virtual void RegisterPasswordChange()
-        {
-			LastPasswordChange = DateTime.UtcNow;
-			InvalidAttempts = 0;
-            _isLocked = false;
-        }
-    }
+		public virtual IPerson Person
+		{
+			get {
+				return _person;
+			}
+		}
+
+		public virtual void Lock()
+		{
+			_isLocked = true;
+		}
+
+		public virtual void RegisterInvalidAttempt(IPasswordPolicy passwordPolicy)
+		{
+			DateTime utcNow = DateTime.UtcNow;
+			if (utcNow>_invalidAttemptsSequenceStart.Add(passwordPolicy.InvalidAttemptWindow) ||
+				_invalidAttempts == 0)
+			{
+				//Start of new sequence
+				_invalidAttemptsSequenceStart = utcNow;
+				_invalidAttempts = 0;
+			}
+			_invalidAttempts++;
+		}
+
+		public virtual void RegisterPasswordChange()
+		{
+			_lastPasswordChange = DateTime.UtcNow;
+			_invalidAttempts = 0;
+			_isLocked = false;
+		}
+	}
 }
