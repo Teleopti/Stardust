b92b8902ba08634717848c3ce67c33b7339596aa
Claes Stenman <claes.stenman@teleopti.com>
2012-11-06 14:40:41 +0100

    Refactoring TeamSteadyStateDictionaryCreator...

--- Domain/Scheduling/TeamSteadyStateRunner.cs
+++ Domain/Scheduling/TeamSteadyStateRunner.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Scheduling
 {
-	public class TeamSteadyStateRunner
+	public interface ITeamSteadyStateRunner
+	{
+		KeyValuePair<Guid, bool> Run(IGroupPerson groupPerson, DateOnly dateOnly);
+	}
+
+	public class TeamSteadyStateRunner : ITeamSteadyStateRunner
 	{
 		private readonly IList<IScheduleMatrixPro> _matrixList;
 		private readonly ISchedulePeriodTargetTimeCalculator _schedulePeriodTargetTimeCalculator;
+		private DateOnly _startDate = new DateOnly(DateTime.MinValue);
+		private DateOnly _endDate = new DateOnly(DateTime.MinValue);
 
 		public TeamSteadyStateRunner(IList<IScheduleMatrixPro> matrixList, ISchedulePeriodTargetTimeCalculator schedulePeriodTargetTimeCalculator)
 		{
@@ -17,12 +23,20 @@ namespace Teleopti.Ccc.Domain.Scheduling
 		}
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
-		public KeyValuePair<Guid, bool> Run(IGroupPerson groupPerson, DateOnly dateOnly)
+		public KeyValuePair<Guid, bool> Run(IGroupPerson groupPerson, DateOnly dateOnly)	
 		{
 				if(!groupPerson.Id.HasValue) throw new ArgumentNullException("groupPerson");
 				var steadyState = true;
 				var firstPerson = groupPerson.GroupMembers[0];
-				var teamSteadyState = CreateTeamSteadyState(firstPerson, dateOnly);
+				var firstPersonvirtualSchedulePeriod = firstPerson.VirtualSchedulePeriod(dateOnly);
+				var firstPersonScheduleMatrixPro = GetScheduleMatrixPro(firstPersonvirtualSchedulePeriod);
+
+				if (firstPersonScheduleMatrixPro == null)
+					return new KeyValuePair<Guid, bool>(groupPerson.Id.Value, false);
+			
+				_startDate = firstPersonScheduleMatrixPro.EffectivePeriodDays[0].Day;
+				_endDate = firstPersonScheduleMatrixPro.EffectivePeriodDays[firstPersonScheduleMatrixPro.EffectivePeriodDays.Count - 1].Day;
+				var teamSteadyState = CreateTeamSteadyState(firstPerson, dateOnly, firstPersonScheduleMatrixPro, firstPersonvirtualSchedulePeriod);
 				if (teamSteadyState == null)
 					steadyState = false;
 
@@ -39,7 +53,13 @@ namespace Teleopti.Ccc.Domain.Scheduling
 							break;	
 						}
 
-						var personPeriod = person.PersonPeriods(new DateOnlyPeriod(dateOnly, dateOnly)).SingleOrDefault();
+						if(!MatrixIsSteady(scheduleMatrixPro))
+						{
+							steadyState = false;
+							break;
+						}
+
+						var personPeriod = person.Period(dateOnly);
 
 						if (personPeriod == null)
 						{
@@ -58,12 +78,29 @@ namespace Teleopti.Ccc.Domain.Scheduling
 			return new KeyValuePair<Guid, bool>(groupPerson.Id.Value, steadyState);
 		}
 
-		private ITeamSteadyState CreateTeamSteadyState(IPerson firstPerson, DateOnly dateOnly)
+		private bool MatrixIsSteady(IScheduleMatrixPro matrix)
 		{
-			var firstPersonvirtualSchedulePeriod = firstPerson.VirtualSchedulePeriod(dateOnly);
-			var firstPersonScheduleMatrixPro = GetScheduleMatrixPro(firstPersonvirtualSchedulePeriod);
+			var matrixStart = matrix.EffectivePeriodDays[0].Day;
+			var matrixEnd = matrix.EffectivePeriodDays[matrix.EffectivePeriodDays.Count - 1].Day;
+
+			if (matrixStart != _startDate || matrixEnd != _endDate)
+			{
+				return false;
+			}
+		
+			if (matrix.Person.Period(matrixStart) != matrix.Person.Period(matrixEnd))
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private ITeamSteadyState CreateTeamSteadyState(IPerson firstPerson, DateOnly dateOnly, IScheduleMatrixPro firstPersonScheduleMatrixPro, IVirtualSchedulePeriod firstPersonvirtualSchedulePeriod)
+		{
 			if (firstPersonScheduleMatrixPro == null) return null;
-			var firstPersonpersonPeriod = firstPerson.PersonPeriods(new DateOnlyPeriod(dateOnly, dateOnly)).SingleOrDefault();
+			if (firstPersonScheduleMatrixPro.Person.Period(_startDate) != firstPersonScheduleMatrixPro.Person.Period(_endDate)) return null;
+			var firstPersonpersonPeriod = firstPerson.Period(dateOnly);
 			if (firstPersonpersonPeriod == null) return null;
 			var firstPersonteamSteadyStateSchedulePeriod = new TeamSteadyStateSchedulePeriod(firstPersonvirtualSchedulePeriod, _schedulePeriodTargetTimeCalculator, firstPersonScheduleMatrixPro);
 			var firstPersonteamSteadyStatePersonPeriod = new TeamSteadyStatePersonPeriod(firstPersonpersonPeriod);
