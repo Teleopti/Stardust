c1f64a7b434d6a2d80bb989e9c7c9364be9a90df
Kunning Mao <kunning.mao@teleopti.com>
2012-11-08 14:28:40 +0100

    integrate with login prototype step 1

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Content/Scripts/Teleopti.Start.LoginViewModel.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Content/Scripts/Teleopti.Start.LoginViewModel.js
@@ -2,10 +2,8 @@
 /// <reference path="~/Content/Scripts/jquery-1.8.2.js" />
 /// <reference path="~/Content/Scripts/MicrosoftMvcAjax.debug.js" />
 /// <reference path="~/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Ajax.js" />
-/// <reference path="~/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.js" />
-/// <reference path="~/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.DayViewModel.js" />
 
-var SignInViewModel = function () {
+Teleopti.Start.SignInViewModel = function () {
 	var self = this;
 
 	this.AvailableDataSources = ko.observableArray();
@@ -22,6 +20,13 @@ var SignInViewModel = function () {
 	this.WarningActive = ko.observable(false);
 	this.MustChangePasswordActive = ko.observable(false);
 	this.ChangingPasswordActive = ko.observable(false);
+	this.IsApplicationLogon = ko.computed(function () {
+		if (self.SelectedSource()) {
+			return self.SelectedSource().ApplicationAuthentication();
+		} else {
+			return true;
+		}
+	});
 
 	this.UserName = ko.observable();
 	this.Password = ko.observable();
@@ -31,6 +36,18 @@ var SignInViewModel = function () {
 			self.SelectedSource().Selected(false);
 		this.Selected(true);
 		self.SelectedSource(this);
+	};
+
+	this.LoadDataSources = function () {
+		ajax.Ajax({
+			url: "Authentication/LoadDataSources",
+			dataType: "json",
+			type: 'GET',
+			success: function (data, textStatus, jqXHR) {
+				
+//				self.AvailableDataSources(data);
+			}
+		});
 	};
 
 	this.SelectBusinessUnit = function () {
@@ -66,82 +83,82 @@ var SignInViewModel = function () {
 	};
 };
 
-var DataSourceViewModel = function () {
+Teleopti.Start.DataSourceViewModel = function () {
 	this.DataSourceName = "";
 	this.Selected = ko.observable(false);
 	this.ApplicationAuthentication = ko.observable(false);
 };
 
-var BusinessUnitViewModel = function () {
+Teleopti.Start.BusinessUnitViewModel = function () {
 	this.BusinessUnitName = "";
 	this.Selected = ko.observable(false);
 };
 
-var ApplicationViewModel = function () {
+Teleopti.Start.ApplicationViewModel = function () {
 	this.ApplicationName = "";
 	this.Selected = ko.observable(false);
 	this.ApplicationUrl = "";
 };
 
 
-var data1 = new DataSourceViewModel();
-data1.DataSourceName = "Teleopti CCC Main";
-data1.ApplicationAuthentication(true);
+//var data1 = new DataSourceViewModel();
+//data1.DataSourceName = "Teleopti CCC Main";
+//data1.ApplicationAuthentication(true);
 
-var data2 = new DataSourceViewModel();
-data2.DataSourceName = "Teleopti CCC Main (Windows)";
-data2.ApplicationAuthentication(false);
+//var data2 = new DataSourceViewModel();
+//data2.DataSourceName = "Teleopti CCC Main (Windows)";
+//data2.ApplicationAuthentication(false);
 
-var data3 = new DataSourceViewModel();
-data3.DataSourceName = "TestData";
-data3.ApplicationAuthentication(true);
+//var data3 = new DataSourceViewModel();
+//data3.DataSourceName = "TestData";
+//data3.ApplicationAuthentication(true);
 
-var data4 = new DataSourceViewModel();
-data4.DataSourceName = "372 Data";
-data4.ApplicationAuthentication(true);
+//var data4 = new DataSourceViewModel();
+//data4.DataSourceName = "372 Data";
+//data4.ApplicationAuthentication(true);
 
-var signinViewModel = new SignInViewModel();
-var datas = [data1, data2, data3, data4];
-signinViewModel.AvailableDataSources(datas);
-datas[0].Selected(true);
-signinViewModel.SelectedSource(datas[0]);
-ko.applyBindings(signinViewModel, $('#Login-container')[0]);
+//var signinViewModel = new SignInViewModel();
+//var datas = [data1, data2, data3, data4];
+//signinViewModel.AvailableDataSources(datas);
+//datas[0].Selected(true);
+//signinViewModel.SelectedSource(datas[0]);
+//ko.applyBindings(signinViewModel, $('#Login-container')[0]);
 
 
 
-$('#Login-button').click(function () {
-	if ($('#Username-input').val() == "1") {
-		signinViewModel.WarningActive(true);
-		signinViewModel.ChangingPasswordActive(false);
-	}
-	if ($('#Username-input').val() == "2") {
-		signinViewModel.MustChangePasswordActive(true);
-		signinViewModel.ChangingPasswordActive(true);
-		signinViewModel.DataSourceSelectionActive(false);
-		return;
-	}
-	signinViewModel.BusinessUnitSelectionActive(true);
-	signinViewModel.DataSourceSelectionActive(false);
-	var bu1 = new BusinessUnitViewModel();
-	bu1.BusinessUnitName = "BU1";
-	var bu2 = new BusinessUnitViewModel();
-	bu2.BusinessUnitName = "BU2";
-	var bu3 = new BusinessUnitViewModel();
-	bu3.BusinessUnitName = "BU3";
-	var bus = [bu1, bu2, bu3];
-	signinViewModel.AvailableBusinessUnits(bus);
-	bus[0].Selected(true);
-	signinViewModel.SelectedBusinessUnit(bus[0]);
+//$('#Login-button').click(function () {
+//	if ($('#Username-input').val() == "1") {
+//		signinViewModel.WarningActive(true);
+//		signinViewModel.ChangingPasswordActive(false);
+//	}
+//	if ($('#Username-input').val() == "2") {
+//		signinViewModel.MustChangePasswordActive(true);
+//		signinViewModel.ChangingPasswordActive(true);
+//		signinViewModel.DataSourceSelectionActive(false);
+//		return;
+//	}
+//	signinViewModel.BusinessUnitSelectionActive(true);
+//	signinViewModel.DataSourceSelectionActive(false);
+//	var bu1 = new BusinessUnitViewModel();
+//	bu1.BusinessUnitName = "BU1";
+//	var bu2 = new BusinessUnitViewModel();
+//	bu2.BusinessUnitName = "BU2";
+//	var bu3 = new BusinessUnitViewModel();
+//	bu3.BusinessUnitName = "BU3";
+//	var bus = [bu1, bu2, bu3];
+//	signinViewModel.AvailableBusinessUnits(bus);
+//	bus[0].Selected(true);
+//	signinViewModel.SelectedBusinessUnit(bus[0]);
 
-});
+//});
 
-$('#Change-password-button').click(function () {
-	signinViewModel.DataSourceSelectionActive(true);
-	signinViewModel.BusinessUnitSelectionActive(false);
-	signinViewModel.MustChangePasswordActive(false);
-	signinViewModel.ApplicationSelectionActive(false);
-	signinViewModel.WarningActive(false);
-});
+//$('#Change-password-button').click(function () {
+//	signinViewModel.DataSourceSelectionActive(true);
+//	signinViewModel.BusinessUnitSelectionActive(false);
+//	signinViewModel.MustChangePasswordActive(false);
+//	signinViewModel.ApplicationSelectionActive(false);
+//	signinViewModel.WarningActive(false);
+//});
 
 //$('#Businessunit-cancel-button').click(function () {
 //	signinViewModel.BusinessUnitSelectionActive(false);
