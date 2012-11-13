9d6608221357f0691bf9c8b1a54df3eb90cd9543
Roger Kratz <roger.kratz@teleopti.com>
2012-11-13 16:43:17 +0100

    trying to fix build (net 40)
    --HG--
    branch : Release 7.3.374

--- InfrastructureTest/UnitOfWork/NHibernateUnitOfWorkCommitCallbackTest.cs
+++ InfrastructureTest/UnitOfWork/NHibernateUnitOfWorkCommitCallbackTest.cs
@@ -5,70 +5,63 @@ using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Infrastructure.Foundation;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.InfrastructureTest.Helper;
 
 namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 {
 	[TestFixture]
-	public class NHibernateUnitOfWorkCommitCallbackTest
+	public class NHibernateUnitOfWorkCommitCallbackTest : DatabaseTest
 	{
 		[Test]
 		public void ShouldCallTxCallbackWhenSuccessfulCommit()
 		{
+			SkipRollback();
 			var isCalled = false;
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-			{
-				uow.AfterSuccessfulTx(() => isCalled = true);
-				uow.PersistAll();
-				isCalled.Should().Be.True();				
-			}
+			UnitOfWork.AfterSuccessfulTx(() => isCalled = true);
+			UnitOfWork.PersistAll();
+			isCalled.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldNotCallTxCallbackWhenRollbacked()
 		{
 			var isCalled = false;
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-			{
-				uow.AfterSuccessfulTx(() => isCalled = true);
-			}
+			UnitOfWork.AfterSuccessfulTx(() => isCalled = true);
 			isCalled.Should().Be.False();
 		}
 
 		[Test]
 		public void ShouldNotCallTxCallbackIfException()
 		{
+			SkipRollback();
 			var isCalled = false;
 			var person = new Person();
 			var correctEx = false;
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+
+			person.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Local);
+			new PersonRepository(UnitOfWork).Add(person);
+			UnitOfWork.PersistAll();
+
+			UnitOfWork.AfterSuccessfulTx(() => isCalled = true);
+			//too long email
+			var email = new string('c', 300);
+			person.Email = email;
+			try
 			{
-				person.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Local);
-				new PersonRepository(uow).Add(person);
-				uow.PersistAll();
+				UnitOfWork.PersistAll();
 			}
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			catch (DataSourceException)
 			{
-				uow.Reassociate(person);
-				uow.AfterSuccessfulTx(() => isCalled = true);
-				//too long email
-				var email = new string('c', 300);
-				person.Email = email;
-				try
-				{
-					uow.PersistAll();
-				}
-				catch (DataSourceException)
-				{
-					correctEx = true;
-				}
+				correctEx = true;
 			}
 
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
 			{
 				person.Email = "ok";
 				new PersonRepository(uow).Remove(person);
 				uow.PersistAll();
 			}
+
 			correctEx.Should().Be.True();
 			isCalled.Should().Be.False();
 		}
@@ -76,13 +69,11 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 		[Test]
 		public void ShouldWorkWithCurrentUnitOfWork()
 		{
+			SkipRollback();
 			var isCalled = false;
-			using (UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-			{
-				UnitOfWorkFactory.Current.CurrentUnitOfWork().AfterSuccessfulTx(() => isCalled = true);
-				UnitOfWorkFactory.Current.CurrentUnitOfWork().PersistAll();
-				isCalled.Should().Be.True();
-			}
+			UnitOfWorkFactory.Current.CurrentUnitOfWork().AfterSuccessfulTx(() => isCalled = true);
+			UnitOfWorkFactory.Current.CurrentUnitOfWork().PersistAll();
+			isCalled.Should().Be.True();
 		}
 	}
 }
