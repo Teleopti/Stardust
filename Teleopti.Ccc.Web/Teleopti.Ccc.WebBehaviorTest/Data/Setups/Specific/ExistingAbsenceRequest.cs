3695fb142763d073edd44d6e583d6575dd3be464
Henke <henrik.andersson@teleopti.com>
2012-11-28 10:45:07 +0100

    refact

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Specific/ExistingAbsenceRequest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Specific/ExistingAbsenceRequest.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using Teleopti.Ccc.Domain.AgentInfo.Requests;
 using Teleopti.Ccc.Infrastructure.Repositories;
@@ -75,26 +74,5 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Specific
 
 			requestRepository.Add(PersonRequest);
 		}
-	}
-
-	public class ExistingShiftTradeRequest : IUserDataSetup
-	{
-		public PersonRequest PersonRequest { get; set; }
-		public ShiftTradeRequest ShiftTradeRequest { get; set; }
-
-		public void Apply(IUnitOfWork uow, IPerson user, CultureInfo cultureInfo)
-		{
-			var today = DateTime.UtcNow.Date;
-			var tomorrow = today.AddDays(1);
-			var shiftTradeSwapDetail = new ShiftTradeSwapDetail(user, user, new DateOnly(today), new DateOnly(tomorrow));
-			ShiftTradeRequest = new ShiftTradeRequest(new List<IShiftTradeSwapDetail> { shiftTradeSwapDetail });
-			PersonRequest = new PersonRequest(user) {Subject = "I want to swap shift with myself"};
-			PersonRequest.Request = ShiftTradeRequest;
-			PersonRequest.TrySetMessage("This is a short text for the description of a shift trade request");
-
-			var requestRepository = new PersonRequestRepository(uow);
-			requestRepository.Add(PersonRequest);
-		}
-
 	}
 }
