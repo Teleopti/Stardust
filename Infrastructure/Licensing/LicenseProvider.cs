9fd392a21965722d75fe71687e563ad05bc26a19
Robin Karlsson <robin.karlsson@teleopti.com>
2012-11-28 12:37:02 +0100

    Suppress for now

--- Infrastructure/Licensing/LicenseProvider.cs
+++ Infrastructure/Licensing/LicenseProvider.cs
@@ -28,7 +28,8 @@ namespace Teleopti.Ccc.Infrastructure.Licensing
         /// Created by: Klas
         /// Created date: 2008-12-03
         /// </remarks>
-        public static ILicenseActivator GetLicenseActivator(ILicenseService licenseService)
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
+		public static ILicenseActivator GetLicenseActivator(ILicenseService licenseService)
         {
             if (licenseService == null) throw new ArgumentNullException("licenseService");
 
