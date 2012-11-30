4edb3afea8086e1c9616da8af37bc37c0805128c
Robin Karlsson <robin.karlsson@teleopti.com>
2012-11-30 11:11:49 +0100

    hmm...

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Team/Content/Scripts/main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Team/Content/Scripts/main.js
@@ -2,22 +2,22 @@
 require.config({
 
 	paths: {
-		jquery: '/Content/Scripts/jquery-1.8.2',
-		jqueryui: '/Content/jqueryui/jquery-ui-1.9.1.custom',
-		knockout: '/Content/Scripts/knockout-2.1.0',
-		modernizr: '/Content/modernizr/modernizr-2.6.2.min',
-		respond: '/Content/respondjs/respond.min',
-		moment: '/Content/moment/moment',
-		bootstrap: '/Content/bootstrap/bootstrap.min',
-		signals: '/Content/signals/signals',
-		crossroads: '/Content/crossroads/crossroads',
-		hasher: '/Content/hasher/hasher',
-		touchPunch: '/Content/jqueryui.touch-punch/jquery.ui.touch-punch',
-		swipeListener: '/Content/jquery-plugin/jquery.swipeListener',
+		jquery: '../../../../Content/Scripts/jquery-1.8.2',
+		jqueryui: '../../../../Content/jqueryui/jquery-ui-1.9.1.custom',
+		knockout: '../../../../Content/Scripts/knockout-2.1.0',
+		modernizr: '../../../../Content/modernizr/modernizr-2.6.2.min',
+		respond: '../../../../Content/respondjs/respond.min',
+		moment: '../../../../Content/moment/moment',
+		bootstrap: '../../../../Content/bootstrap/bootstrap.min',
+		signals: '../../../../Content/signals/signals',
+		crossroads: '../../../../Content/crossroads/crossroads',
+		hasher: '../../../../Content/hasher/hasher',
+		touchPunch: '../../../../Content/jqueryui.touch-punch/jquery.ui.touch-punch',
+		swipeListener: '../../../../Content/jquery-plugin/jquery.swipeListener',
 
 		templates: '../templates',
 
-		text: '/Content/require/text'
+		text: '../../../../Content/require/text'
 		
 	},
 
