5dca6e7cb539f3a36aa9f37d2750511cf48d9763
David Jonsson <david.jonsson@teleopti.com>
2012-12-05 14:34:10 +0100

    FixYourLocalConfig - Adding ETL-tool and debug mode

--- BuildArtifacts/RefreshConfig.bat
+++ BuildArtifacts/RefreshConfig.bat
@@ -3,9 +3,15 @@
 set Configuration=debug
 set CustomConfig=c:\CustomConfig.txt
 
-::Read config files in debug
+if "%Configuration%"=="debug" call:debugConfigSetup
+
+::del current config
 for /f "tokens=1,2 delims=;" %%g in (ConfigFiles-%Configuration%.txt) do if exist %%g del %%g
+
+::copy new config from BuildArtifacts
 for /f "tokens=1,2 delims=;" %%g in (ConfigFiles-%Configuration%.txt) do copy "%%h" "%%g"
+
+::update according to your custom config
 for /f "tokens=1,2 delims=;" %%g in (ConfigFiles-%Configuration%.txt) do call:Replace "%%g" "%CustomConfig%"
 
 goto:eof
@@ -19,3 +25,14 @@ for /f "tokens=1,2 delims=;" %%g in (%CustomConfig%) do cscript replace.vbs "%%g
 ENDLOCAL
 )
 goto:eof
+
+:debugConfigSetup
+taskkill /IM WebDev.WebServer40.EXE /F
+taskkill /IM WebDev.WebServer20.EXE /F
+if not exist "%CustomConfig%" (
+ECHO $^(CCC7DB^);main_Demoreg_TeleoptiCCC7>"%CustomConfig%"
+ECHO $^(AnalyticsDB^);main_Demoreg_TeleoptiAnalytics>>"%CustomConfig%"
+ECHO $^(SitePath^);c:\nhib>>"%CustomConfig%"
+)
+goto:eof
+
