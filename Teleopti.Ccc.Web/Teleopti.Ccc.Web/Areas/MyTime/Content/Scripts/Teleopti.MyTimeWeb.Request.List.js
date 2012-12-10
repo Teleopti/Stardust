24303067d6513ebff1b8f8b4ef3b02c1242b278b
tamasb <none@none>
2012-12-10 17:07:12 +0100

    Backed out changeset: bf709051e89b

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.List.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.List.js
@@ -10,272 +10,265 @@
 
 Teleopti.MyTimeWeb.Request.List = (function ($) {
 
-    var ajax = new Teleopti.MyTimeWeb.Ajax();
-    var readyForInteraction = function () { };
-    var completelyLoaded = function () { };
+	var ajax = new Teleopti.MyTimeWeb.Ajax();
+	var readyForInteraction = function () { };
+	var completelyLoaded = function () { };
 
-    function _initScrollPaging() {
-        _loadAPage();
-        $(window).scroll(_loadAPageIfRequired);
-    }
+	function _initScrollPaging() {
+		_loadAPage();
+		$(window).scroll(_loadAPageIfRequired);
+	}
 
-    function _loadAPageIfRequired() {
-        if (!_hasMoreToLoad())
-            return;
-        var jqWindow = $(window);
-        var jqDocument = $(window.document);
-        if (_isAtBottom(jqDocument, jqWindow)) {
-            _loadAPage();
-        }
-    }
+	function _loadAPageIfRequired() {
+		if (!_hasMoreToLoad())
+			return;
+		var jqWindow = $(window);
+		var jqDocument = $(window.document);
+		if (_isAtBottom(jqDocument, jqWindow)) {
+			_loadAPage();
+		}
+	}
 
-    function _isAtBottom(jqDocument, jqWindow) {
-        var totalContentHeight = jqDocument.height();
-        var inViewContentHeight = jqWindow.height();
-        var aboveViewContentHeight = jqWindow.scrollTop();
-        return totalContentHeight - inViewContentHeight - aboveViewContentHeight <= 0;
-    }
+	function _isAtBottom(jqDocument, jqWindow) {
+		var totalContentHeight = jqDocument.height();
+		var inViewContentHeight = jqWindow.height();
+		var aboveViewContentHeight = jqWindow.scrollTop();
+		return totalContentHeight - inViewContentHeight - aboveViewContentHeight <= 0;
+	}
 
-    function _loadAPage() {
-        var skip = $('#Requests-list li:not(.template)').length;
-        var take = 20;
-        ajax.Ajax({
-            url: "Requests/Requests",
-            dataType: "json",
-            type: 'GET',
-            beforeSend: _loading,
-            data: {
-                Take: take,
-                Skip: skip
-            },
-            success: function (data, textStatus, jqXHR) {
-                _drawRequests(data);
-                if (data.length == 0 || data.length < take) {
-                    _noMoreToLoad();
-                } else {
-                    _moreToLoad();
-                }
-                _showMessageIfNoRequests();
-            },
-            complete: function () {
-                if (readyForInteraction)
-                    readyForInteraction();
-                readyForInteraction = null;
-                if (completelyLoaded)
-                    completelyLoaded();
-                completelyLoaded = null;
-            }
-        });
-    }
+	function _loadAPage() {
+		var skip = $('#Requests-list li:not(.template)').length;
+		var take = 20;
+		ajax.Ajax({
+			url: "Requests/Requests",
+			dataType: "json",
+			type: 'GET',
+			beforeSend: _loading,
+			data: {
+				Take: take,
+				Skip: skip
+			},
+			success: function (data, textStatus, jqXHR) {
+				_drawRequests(data);
+				if (data.length == 0 || data.length < take) {
+					_noMoreToLoad();
+				} else {
+					_moreToLoad();
+				}
+				_showMessageIfNoRequests();
+			},
+			complete: function () {
+				if (readyForInteraction)
+					readyForInteraction();
+				readyForInteraction = null;
+				if (completelyLoaded)
+					completelyLoaded();
+				completelyLoaded = null;
+			}
+		});
+	}
 
-    function _hasMoreToLoad() {
-        return $('.request-list .arrow-down').is(':visible');
-    }
+	function _hasMoreToLoad() {
+		return $('.request-list .arrow-down').is(':visible');
+	}
 
-    function _loading() {
-        $('.request-list .arrow-down').hide();
-        $('.request-list .loading-gradient').show();
-    }
+	function _loading() {
+		$('.request-list .arrow-down').hide();
+		$('.request-list .loading-gradient').show();
+	}
 
-    function _noMoreToLoad() {
-        $('.request-list .arrow-down').hide();
-        $('.request-list .loading-gradient').hide();
-    }
+	function _noMoreToLoad() {
+		$('.request-list .arrow-down').hide();
+		$('.request-list .loading-gradient').hide();
+	}
 
-    function _moreToLoad() {
-        $('.request-list .arrow-down').show();
-        $('.request-list .loading-gradient').hide();
-    }
+	function _moreToLoad() {
+		$('.request-list .arrow-down').show();
+		$('.request-list .loading-gradient').hide();
+	}
 
-    function _showMessageIfNoRequests() {
-        if ($('.request-item').not('.template').is(':visible')) {
-            $('.friendly-message').hide();
-        } else {
-            $('.friendly-message').show();
-        }
-    }
+	function _showMessageIfNoRequests() {
+		if ($('.request-item').not('.template').is(':visible')) {
+			$('.friendly-message').hide();
+		} else {
+			$('.friendly-message').show();
+		}
+	}
 
-    function _removeRequest(requestOrListItem) {
-        var listItem;
-        if (requestOrListItem.Id)
-            listItem = $('#Requests-list li[data-mytime-requestid="' + requestOrListItem.Id + '"]');
-        else
-            listItem = requestOrListItem;
-        listItem
+	function _removeRequest(requestOrListItem) {
+		var listItem;
+		if (requestOrListItem.Id)
+			listItem = $('#Requests-list li[data-mytime-requestid="' + requestOrListItem.Id + '"]');
+		else
+			listItem = requestOrListItem;
+		listItem
 			.animate({
-			    'height': '0',
-			    'opacity': '0'
+				'height': '0',
+				'opacity': '0'
 			}, 'fast', function () {
-			    $(this).remove();
-			    _loadAPageIfRequired();
-			    _showMessageIfNoRequests();
+				$(this).remove();
+				_loadAPageIfRequired();
+				_showMessageIfNoRequests();
 			});
-    }
+	}
 
-    function _drawRequests(requests) {
-        for (var i = 0; i < requests.length; i++) {
-            var request = requests[i];
-            _drawRequest(request);
-        }
-    }
+	function _drawRequests(requests) {
+		for (var i = 0; i < requests.length; i++) {
+			var request = requests[i];
+			_drawRequest(request);
+		}
+	}
 
-    function _drawRequest(request) {
-        $('#Requests-list').append(_createRequestListItem(request));
-    }
+	function _drawRequest(request) {
+		$('#Requests-list').append(_createRequestListItem(request));
+	}
 
-    function _drawRequestAtTop(request) {
-        var request = _createRequestListItem(request)
+	function _drawRequestAtTop(request) {
+		var request = _createRequestListItem(request)
 			.hide()
 			;
-        $('#Requests-list')
+		$('#Requests-list')
 			.prepend(request)
 			;
-        request.slideDown();
-    }
+		request.slideDown();
+	}
 
-    function _createRequestListItem(request) {
-        var listItem = $('#Requests-list li.template')
+	function _createRequestListItem(request) {
+		var listItem = $('#Requests-list li.template')
 			.clone(true)
 			.removeClass('template')
 			;
-        listItem.attr('data-mytime-requestid', request.Id);
-        listItem.attr('data-mytime-link', request.Link.href);
-        listItem.find('.request-data-subject').text(request.Subject);
-        listItem.find('.request-data-date').text(request.Dates);
-        listItem.find('.request-data-updatedon').text(request.UpdatedOn);
-        listItem.find('.request-data-status').text(request.Status);
-        listItem.find('.request-data-text').text(request.Text);
+		listItem.attr('data-mytime-requestid', request.Id);
+		listItem.attr('data-mytime-link', request.Link.href);
+		listItem.find('.request-data-subject').text(request.Subject);
+		listItem.find('.request-data-date').text(request.Dates);
+		listItem.find('.request-data-updatedon').text(request.UpdatedOn);
+		listItem.find('.request-data-status').text(request.Status);
+		listItem.find('.request-data-text').text(request.Text);
 
-        var connector = listItem.find('.request-connector');
-        var deleteButton = listItem.find('.request-delete-button');
+		var connector = listItem.find('.request-connector');
+		var deleteButton = listItem.find('.request-delete-button');
 
-        if (request.Payload != '') {
-            listItem.find('.request-data-type').text(request.Type + ' \u2013 ' + request.Payload);
-        } else {
-            listItem.find('.request-data-type').text(request.Type);
-        }
-        connector.connector();
+		if (request.Payload != '') {
+			listItem.find('.request-data-type').text(request.Type + ' \u2013 ' + request.Payload);
+		} else {
+			listItem.find('.request-data-type').text(request.Type);
+		}
+		connector.connector();
 
-        if (request.Link.Methods.indexOf("DELETE") != -1) {
-            deleteButton
+		if (request.Link.Methods.indexOf("DELETE") != -1) {
+			deleteButton
 				.click(function (event) {
-				    $(this).prop('disabled', true);
-				    event.stopPropagation();
-				    _disconnectAll();
-				    Teleopti.MyTimeWeb.Request.RequestDetail.HideEditSection();
-				    _deleteRequest(listItem);
+					$(this).prop('disabled', true);
+					event.stopPropagation();
+					_disconnectAll();
+					Teleopti.MyTimeWeb.Request.RequestDetail.HideEditSection();
+					_deleteRequest(listItem);
 				})
-				.removeAttr('disabled', 'disabled');
-            // todo by tamasb: these two methods has to be somehow integrated in qtip library
-            deleteButton.mouseenter(function () {
-                var title = this.getAttribute('oldtitle');
-                deleteButton.qtip('show', title);
-            });
-            deleteButton.mouseleave(function () {
-                deleteButton.qtip('hide');
-            });
-            listItem.hover(function () {
-                deleteButton
+				.removeAttr('disabled', 'disabled')
+				;
+			listItem.hover(function () {
+				deleteButton
 					.stop(true, true)
 					.fadeToggle();
-            });
-        } else {
-            deleteButton.remove();
-        }
-        return listItem;
-    }
+			});
+		} else {
+			deleteButton.remove();
+		}
+		return listItem;
+	}
 
-    function _deleteRequest(listItem) {
-        var url = listItem.data('mytime-link');
-        ajax.Ajax({
-            url: url,
-            dataType: "json",
-            contentType: 'application/json; charset=utf-8',
-            type: "DELETE",
-            beforeSend: function () {
-                Teleopti.MyTimeWeb.Common.LoadingOverlay.Add(listItem);
+	function _deleteRequest(listItem) {
+		var url = listItem.data('mytime-link');
+		ajax.Ajax({
+			url: url,
+			dataType: "json",
+			contentType: 'application/json; charset=utf-8',
+			type: "DELETE",
+			beforeSend: function () {
+				Teleopti.MyTimeWeb.Common.LoadingOverlay.Add(listItem);
 
-            },
-            complete: function (jqXHR, textStatus) {
-                Teleopti.MyTimeWeb.Common.LoadingOverlay.Remove(listItem);
-            },
-            success: function (data, textStatus, jqXHR) {
-                _removeRequest(listItem);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                Teleopti.MyTimeWeb.Common.AjaxFailed(jqXHR, null, textStatus);
-            }
-        });
-    }
+			},
+			complete: function (jqXHR, textStatus) {
+				Teleopti.MyTimeWeb.Common.LoadingOverlay.Remove(listItem);
+			},
+			success: function (data, textStatus, jqXHR) {
+				_removeRequest(listItem);
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				Teleopti.MyTimeWeb.Common.AjaxFailed(jqXHR, null, textStatus);
+			}
+		});
+	}
 
-    function _initListClick() {
-        $('#Requests-list li')
+	function _initListClick() {
+		$('#Requests-list li')
 			.click(function () {
-			    _showRequest($(this));
+				_showRequest($(this));
 			}
 		);
-    }
+	}
 
-    function _showRequest(listItem) {
-        var url = listItem.attr('data-mytime-link');
-        var connector = listItem
+	function _showRequest(listItem) {
+		var url = listItem.attr('data-mytime-link');
+		var connector = listItem
 			.find('.request-connector')
 			;
-        var bindListItemClick = function _bindClick() {
-            listItem.bind('click', function () {
-                _showRequest($(this));
-            });
-        };
-        ajax.Ajax({
-            url: url,
-            dataType: "json",
-            type: 'GET',
-            beforeSend: function () {
-                listItem.unbind('click');
-                _disconnectAllOthers(listItem);
-                Teleopti.MyTimeWeb.Request.RequestDetail.FadeEditSection(bindListItemClick);
-                connector.connector("connecting");
-            },
-            success: function (data, textStatus, jqXHR) {
-                Teleopti.MyTimeWeb.Request.RequestDetail.ShowRequest(data, listItem.position().top - 30);
-                connector.connector("connect");
-            }
-        });
+		var bindListItemClick = function _bindClick() {
+			listItem.bind('click', function () {
+				_showRequest($(this));
+			});
+		};
+		ajax.Ajax({
+			url: url,
+			dataType: "json",
+			type: 'GET',
+			beforeSend: function () {
+				listItem.unbind('click');
+				_disconnectAllOthers(listItem);
+				Teleopti.MyTimeWeb.Request.RequestDetail.FadeEditSection(bindListItemClick);
+				connector.connector("connecting");
+			},
+			success: function (data, textStatus, jqXHR) {
+				Teleopti.MyTimeWeb.Request.RequestDetail.ShowRequest(data, listItem.position().top - 30);
+				connector.connector("connect");
+			}
+		});
 
-    }
+	}
 
-    function _disconnectAll() {
-        $('#Requests-list li:not(.template) .request-connector')
+	function _disconnectAll() {
+		$('#Requests-list li:not(.template) .request-connector')
 			.connector('disconnect')
 		;
-    }
+	}
 
-    function _disconnectAllOthers(listItem) {
-        listItem.siblings()
+	function _disconnectAllOthers(listItem) {
+		listItem.siblings()
 					.not('.template')
 					.data('connected', false)
 					.find('.request-connector')
 					.connector('disconnect')
 					;
-    }
+	}
 
-    return {
-        Init: function (readyForInteractionCallback, completelyLoadedCallback) {
-            readyForInteraction = readyForInteractionCallback;
-            completelyLoaded = completelyLoadedCallback;
-            _initScrollPaging();
-            _initListClick();
-        },
-        AddItemAtTop: function (request) {
-            _drawRequestAtTop(request);
-            _showMessageIfNoRequests();
-        },
-        RemoveItem: function (request) {
-            _removeRequest(request);
-        },
-        DisconnectAll: function () {
-            _disconnectAll();
-        }
-    };
+	return {
+		Init: function (readyForInteractionCallback, completelyLoadedCallback) {
+			readyForInteraction = readyForInteractionCallback;
+			completelyLoaded = completelyLoadedCallback;
+			_initScrollPaging();
+			_initListClick();
+		},
+		AddItemAtTop: function (request) {
+			_drawRequestAtTop(request);
+			_showMessageIfNoRequests();
+		},
+		RemoveItem: function (request) {
+			_removeRequest(request);
+		},
+		DisconnectAll: function () {
+			_disconnectAll();
+		}
+	};
 
 })(jQuery);
