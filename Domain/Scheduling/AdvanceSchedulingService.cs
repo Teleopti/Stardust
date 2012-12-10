a629bd8a2f499924e6660484fbd57bd1308e2463
Xianwei Shen <xianwei.shen@teleopti.com>
2012-12-10 14:45:27 +0100

    inject aggregated restriction into service.

--- Domain/Scheduling/AdvanceSchedulingService.cs
+++ Domain/Scheduling/AdvanceSchedulingService.cs
@@ -37,10 +37,11 @@ namespace Teleopti.Ccc.Domain.Scheduling
 			//call class that return the teamblock dates for a given date (problem if team members don't have same days off)
             var dateOnlyList =  _dynamicBlockFinder.ExtractBlockDays(new DateTime());
             //call class that returns the aggregated restrictions for the teamblock (is team member personal skills needed for this?)
-            
+            var effectiveRestriction = _restrictionAggregator.Aggregate(dateOnlyList, groupPerson);
 			//call class that returns the aggregated intraday dist based on teamblock dates
             var skillInternalDataList = _skillDayPeriodIntervalData.GetIntervalDistribution(dateOnlyList);
 			//call class that returns a filtered list of valid workshifts, this class will probably consists of a lot of subclasses (should we cover for max seats here?)
+
 			//call class that returns the workshift to use based on valid workshifts, the aggregated intraday dist and other things we need
 			//call class that schedules given date with given workshift on the complete team
 			//call class that schedules the unscheduled days for the teamblock using the same start time from the given shift, this class will handle steady state as well as individual
