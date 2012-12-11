2f995d0a612d6699f63ea7ddda323c1e3ce70092
Robin Karlsson <robin.karlsson@teleopti.com>
2012-12-11 14:09:15 +0100

    Clear collection on navigation back and forth.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Team/Content/Scripts/views/teamschedule/view.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Team/Content/Scripts/views/teamschedule/view.js
@@ -29,20 +29,14 @@ define([
 
 				options.renderHtml(view);
 
-				$('#date-selection').datepicker({
-					pullRight: true,
-					format: 'yyyy-mm-dd',
-					weekStart: 1
-				}).on('changeDate', function (ev) {
-					$('#date-selection').datepicker('hide');
-				});
-
 				var timeLine = new timeLineViewModel();
-
 				var teamSchedule = new teamScheduleViewModel(timeLine);
 
 				var schedule = $.connection.scheduleHub;
 				schedule.client.teamScheduleLoaded = function (schedules) {
+
+					teamSchedule.Agents.removeAll();
+
 					ko.utils.arrayForEach(schedules, function (s) {
 						var agent = new agentViewModel(timeLine, s);
 						teamSchedule.AddAgent(agent);
@@ -80,7 +74,7 @@ define([
 				navigation.GotoAgentSchedule($(this).data('agent-id'), $('#date-selection').attr('value'));
 				});
 				*/
-				
+
 				$('.team-schedule').swipeListener({
 					swipeLeft: function () {
 						var dateValue = $('#date-selection').attr('value');
@@ -92,6 +86,16 @@ define([
 						var date = moment(dateValue).add('d', 1);
 						$('#date-selection').attr('value', date.format('YYYY-MM-DD'));
 					}
+				});
+
+
+				$('#date-selection').datepicker({
+					pullRight: true,
+					format: 'yyyy-mm-dd',
+					weekStart: 1
+				}).on('changeDate', function (ev) {
+					$('#date-selection').datepicker('hide');
+					schedule.server.subscribeTeamSchedule('34590A63-6331-4921-BC9F-9B5E015AB495', $('#date-selection').val());
 				});
 			}
 		};
