de4453247dada00d3f586c2461b3ea3b6057b2b8
Robin Karlsson <robin.karlsson@teleopti.com>
2012-12-12 09:12:44 +0100

    Suppress for now

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Team/Content/Scripts/helpers.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Team/Content/Scripts/helpers.js
@@ -29,15 +29,6 @@ define(
 					code();
 					return setInterval(code, interval);
 				}
-			},
-
-			Guid: {
-				Create: function () {
-					return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g , function(c) {
-						var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-						return v.toString(16);
-					});
-				}
 			}
 		};
 
