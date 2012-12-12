8a1b5bb61594b158a966b3d38a36d90e19b4df9a
David Jonsson <david.jonsson@teleopti.com>
2012-12-12 13:08:37 +0100

    PBI21517 - remove wiki language helper
    --HG--
    branch : Release 7.3.374

--- Teleopti.Analytics/WebPortal/Teleopti.Analytics.Portal/Teleopti.Analytics.Portal/Utils/HelpLinkBuilder.cs
+++ Teleopti.Analytics/WebPortal/Teleopti.Analytics.Portal/Teleopti.Analytics.Portal/Utils/HelpLinkBuilder.cs
@@ -14,32 +14,5 @@ namespace Teleopti.Analytics.Portal.Utils
         {
 			return string.Format("{0}/Performance_Manager_Module", ConfigurationManager.AppSettings["HelpUrl"]);
         }
-
-        private static string getLanguage()
-        {
-            //Get culture, two letter Style
-            string helpLang = string.Empty;
-            string helpCulture = Thread.CurrentThread.CurrentUICulture.Name;
-
-            if (!string.IsNullOrEmpty(helpCulture)) helpLang = helpCulture.Substring(0, 2);
-
-            switch (helpLang)
-            {
-                case "sv":
-                    return helpLang;
-                case "de":
-                    return helpLang;
-                /* 
-                case "ru":
-                    _helpLang = "ru";
-                        break;
-                case "zh":
-                    _helpLang = "zh";
-                    break;                  
-                */
-                default:
-                    return "en";
-            }
-        }
     }
 }
