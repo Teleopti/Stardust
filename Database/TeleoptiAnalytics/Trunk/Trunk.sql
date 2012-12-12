91b72bad66736c62992fe9d3ed84933f22113647
David Jonsson <david.jonsson@teleopti.com>
2012-12-12 11:23:45 +0100

    PBI21517 - fix Analytics portal
    --HG--
    branch : Release 7.3.374

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -61,3 +61,20 @@ CREATE NONCLUSTERED INDEX IX_dimPerson_personCode_DateIncludes
 ON [mart].[dim_person] ([person_code])
 INCLUDE ([person_id],[valid_from_date_id],[valid_to_date_id],[business_unit_code])
 GO
+
+----------------  
+--Name: Karin and David
+--Date: 2012-12-12
+--Desc: PBI21517
+----------------  
+UPDATE mart.report
+SET help_key = REPLACE(help_key,'.html','')
+WHERE RIGHT(help_key,5) = '.html'
+
+UPDATE mart.report
+SET help_key = REPLACE(help_key,'f01_','f01:')
+WHERE LEFT(help_key,4) = 'f01_'
+
+UPDATE mart.report
+SET help_key = REPLACE(help_key,'_','+')
+WHERE SUBSTRING(help_key,11,1) = '_'
