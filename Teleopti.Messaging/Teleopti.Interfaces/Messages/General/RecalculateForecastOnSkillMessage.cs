2c8d6bd09652b4a84bcc50b65b4fc0390148f2a3
ola@teleopti.com <ola@teleopti.com>
2012-12-12 14:24:45 +0100

    added first stub of message to send recalc of forecast to service bus

---
+++ Teleopti.Messaging/Teleopti.Interfaces/Messages/General/RecalculateForecastOnSkillMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Interfaces.Messages.General
+{
+	///<summary>
+	/// Message with details to perform an recalc of forecasting data on a skill.
+	///</summary>
+	public class RecalculateForecastOnSkillMessage : RaptorDomainMessage
+	{
+		private readonly Guid _messageId = Guid.NewGuid();
+
+		/// <summary>
+		/// Gets the message identity.
+		/// </summary>
+		public override Guid Identity
+		{
+			get { return _messageId; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="skill"></param>
+		public RecalculateForecastOnSkillMessage(ISkill skill)
+		{
+			Skill = skill;
+		}
+
+		///<summary>
+		/// The period to recalculate in the skills time zone.
+		///</summary>
+		public DateOnlyPeriod Period { get; set; }
+
+		///<summary>
+		/// The skill to recalculate.
+		///</summary>
+		public ISkill Skill { get; set; }
+
+		/// <summary>
+		/// The owner of this action. can we use this to say that this person updated the forecast????
+		/// </summary>
+		public Guid OwnerPersonId { get; set; }
+
+
+		// add information how the forecast should change
+		// percent up/down or some other way
+	}
+}
