bd7e37e3f2de07478627b384acab90dac8ce7f5c
Erik Sundberg <erik.sundberg@teleopti.com>
2012-12-18 11:06:33 +0100

    Added test for Models and WizardPages

---
+++ Teleopti.Ccc.WinCodeTest/Intraday/ReforecastModelTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.WinCode.Intraday;
+
+namespace Teleopti.Ccc.WinCodeTest.Intraday
+{
+    [TestFixture]
+    public class ReforecastModelTest
+    {
+        private ReforecastModel _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new ReforecastModel();
+        }
+
+        [Test]
+        public void ShouldInitializeWorloadList()
+        {
+            Assert.That(_target.Workload, Is.Not.Null);
+        }
+    }
+
+    [TestFixture]
+    public class ReforecastModelCollectionTest
+    {
+        private ReforecastModelCollection _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new ReforecastModelCollection();
+        }
+
+        [Test]
+        public void ShouldInitializeWorloadList()
+        {
+            Assert.That(_target.ReforecastModels, Is.Not.Null);
+        }
+    }
+}
---
+++ Teleopti.Ccc.WinCodeTest/Intraday/ReforecastWizardPagesTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.WinCode.Intraday;
+
+namespace Teleopti.Ccc.WinCodeTest.Intraday
+{
+    [TestFixture]
+    public class ReforecastWizardPagesTest
+    {
+        private ReforecastWizardPages _target;
+        private MockRepository _mock;
+        private ReforecastModelCollection _modelColletion;
+        private ReforecastModel _model;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mock = new MockRepository();
+            _model = new ReforecastModel();
+            _modelColletion = new ReforecastModelCollection();
+            _modelColletion.ReforecastModels.Add(_model);
+            _target = new ReforecastWizardPages(_modelColletion);
+        }
+
+        [Test]
+        public void ShouldHaveName()
+        {
+            Assert.That(_target.Name, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldHaveWindowText()
+        {
+            Assert.That(_target.WindowText, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldReturnObject()
+        {
+            Assert.That(_target.CreateNewStateObj(), Is.Not.Null);
+        }
+    }
+}
