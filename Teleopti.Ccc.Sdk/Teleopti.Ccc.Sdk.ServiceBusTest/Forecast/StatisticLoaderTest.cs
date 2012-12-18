789650180336ab62152b4e1e77d2937bc1bdd8ea
ola@teleopti.com <ola@teleopti.com>
2012-12-18 10:44:34 +0100

    moe tests

---
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/Forecast/StatisticLoaderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Sdk.ServiceBus.Forecast;
+
+namespace Teleopti.Ccc.Sdk.ServiceBusTest.Forecast
+{
+	[TestFixture]
+	public class StatisticLoaderTest
+	{
+		private MockRepository _mocks;
+		private StatisticLoader _target;
+		private IStatisticRepository _statisticRepository;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mocks = new MockRepository();
+			_statisticRepository = _mocks.DynamicMock<IStatisticRepository>();
+			_target = new StatisticLoader(_statisticRepository);
+
+		}
+
+
+	}
+
+	
+}
