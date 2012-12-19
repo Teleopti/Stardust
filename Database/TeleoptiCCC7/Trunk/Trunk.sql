b8815b8728994e657d5aba794aa8935487e1d579
Jonas Nordh <jonas.nordh@teleopti.com>
2012-12-19 10:51:03 +0100

    Bug #21822 - Changing web start menu items
    --HG--
    branch : Release 7.3.376

---
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -0,0 +1,7 @@
+update ApplicationFunction
+set FunctionDescription = 'xxAnywhere'
+where ForeignId = '0080'
+
+update ApplicationFunction
+set FunctionDescription = 'xxMobileReports', FunctionCode = 'MobileReports'
+where ForeignId = '0074'
