2712c09798b42921de27867fae4cb78c3722b1c9
Henke <henrik.andersson@teleopti.com>
2013-01-08 11:26:10 +0100

    Fixed trunk (main - release + branch)

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -1,119 +1,3 @@
-
---#21539
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonPeriod]') AND name = N'IX_PersonPeriod_Team_Id_Parent')
-CREATE NONCLUSTERED INDEX IX_PersonPeriod_Team_Id_Parent
-ON [dbo].[PersonPeriod] ([Team])
-INCLUDE ([Id],[Parent])
-GO
-----------------  
---Name: Micke D
---Date: 2012-11-14
---Desc: Purge old personal settings
-----------------  
-  DELETE FROM dbo.PersonalSettingData
-  WHERE [Key]= 'AdvancedPreferencesPersonalSettings'
-  DELETE FROM dbo.PersonalSettingData
-  WHERE [Key]= 'SchedulingOptionsAdvancedSettings'
-GO
-
-----------------  
---Name: Jonas N
---Date: 2012-11-14  
---Desc: Add the following new application function ShiftTradeRequestsWeb
-----------------  
-SET NOCOUNT ON
-	
---declarations
-DECLARE @SuperUserId as uniqueidentifier
-DECLARE @FunctionId as uniqueidentifier
-DECLARE @ParentFunctionId as uniqueidentifier
-DECLARE @ForeignId as varchar(255)
-DECLARE @ParentForeignId as varchar(255)
-DECLARE @FunctionCode as varchar(255)
-DECLARE @FunctionDescription as varchar(255)
-DECLARE @ParentId as uniqueidentifier
-
---insert to super user if not exist
-SELECT	@SuperUserId = '3f0886ab-7b25-4e95-856a-0d726edc2a67'
-
--- check for the existence of super user role
-IF  (NOT EXISTS (SELECT id FROM [dbo].[Person] WHERE Id = @SuperUserId)) 
-INSERT [dbo].[Person]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Email], [Note], [EmploymentNumber], [TerminalDate], [FirstName], [LastName], [DefaultTimeZone], [IsDeleted], [BuiltIn], [FirstDayOfWeek])
-VALUES (@SuperUserId,1,@SuperUserId, @SuperUserId, getdate(), getdate(), '', '', '', NULL, '_Super User', '_Super User', 'UTC', 0, 1, 1)
-
---get parent level
-SELECT @ParentForeignId = '0065'	--Parent Foreign id that is hardcoded
-SELECT @ParentId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ParentForeignId + '%')
-	
---insert/modify application function
-SELECT @ForeignId = '0080' --Foreign id of the function > hardcoded	
-SELECT @FunctionCode = 'ShiftTradeRequests' --Name of the function > hardcoded
-SELECT @FunctionDescription = 'xxShiftTradeRequests' --Description of the function > hardcoded
-SELECT @ParentId = @ParentId
-
-IF  (NOT EXISTS (SELECT Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')))
-INSERT [dbo].[ApplicationFunction]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Parent], [FunctionCode], [FunctionDescription], [ForeignId], [ForeignSource], [IsDeleted])
-VALUES (newid(),1, @SuperUserId, @SuperUserId, getdate(), getdate(), @ParentId, @FunctionCode, @FunctionDescription, @ForeignId, 'Raptor', 0) 
-SELECT @FunctionId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
-UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentId WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
-
-SET NOCOUNT OFF
-GO
-
-----------------
---Name: Roger K
---Date: 2012-11-30
---Desc: Remove scenario.audittrail
-----------------
-alter table dbo.Scenario
-drop column audittrail
-
-
-
-----------------  
---Name: Robin K
---Date: 2012-12-03
---Desc: Add the following new application function> Export to file
-----------------  
-SET NOCOUNT ON
-	
---declarations
-DECLARE @SuperUserId as uniqueidentifier
-DECLARE @FunctionId as uniqueidentifier
-DECLARE @ParentFunctionId as uniqueidentifier
-DECLARE @ForeignId as varchar(255)
-DECLARE @ParentForeignId as varchar(255)
-DECLARE @FunctionCode as varchar(255)
-DECLARE @FunctionDescription as varchar(255)
-DECLARE @ParentId as uniqueidentifier
-
---insert to super user if not exist
-SELECT	@SuperUserId = '3f0886ab-7b25-4e95-856a-0d726edc2a67'
-
--- check for the existence of super user role
-IF  (NOT EXISTS (SELECT id FROM [dbo].[Person] WHERE Id = @SuperUserId)) 
-INSERT [dbo].[Person]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Email], [Note], [EmploymentNumber], [TerminalDate], [FirstName], [LastName], [DefaultTimeZone], [IsDeleted], [BuiltIn], [FirstDayOfWeek])
-VALUES (@SuperUserId,1,@SuperUserId, @SuperUserId, getdate(), getdate(), '', '', '', NULL, '_Super User', '_Super User', 'UTC', 0, 1, 1)
-
---get parent level
-SELECT @ParentForeignId = '0003'	--Parent Foreign id that is hardcoded
-SELECT @ParentId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ParentForeignId + '%')
-	
---insert/modify application function
-SELECT @ForeignId = '0082'
-SELECT @FunctionCode = 'ExportForecastFile'
-SELECT @FunctionDescription = 'xxExportToFile'
-SELECT @ParentId = @ParentId
-
-IF  (NOT EXISTS (SELECT Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')))
-INSERT [dbo].[ApplicationFunction]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Parent], [FunctionCode], [FunctionDescription], [ForeignId], [ForeignSource], [IsDeleted])
-VALUES (newid(),1, @SuperUserId, @SuperUserId, getdate(), getdate(), @ParentId, @FunctionCode, @FunctionDescription, @ForeignId, 'Raptor', 0) 
-SELECT @FunctionId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
-UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentId WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
-
-SET NOCOUNT OFF
-GO
-
 -----------------  
 --Name: TamasB
 --Date: 2012-12-17
@@ -178,3 +62,70 @@ ALTER TABLE [dbo].[StudentAvailabilityRestriction] CHECK CONSTRAINT [FK_StudentA
 GO
 
 
+----------------  
+--Name: David Jonsson
+--Date: 2013-01-08
+--Desc: Bug #21786. Remove dubplicates in PersonSkill
+----------------  
+--remove duplicates, but keep one.
+--note: We don't consider "Active" it will be random
+WITH Dubplicates AS
+(
+	select Id, Parent,Skill,ROW_NUMBER() OVER(PARTITION BY Parent,Skill ORDER BY Id) as rownumber
+	from PersonSkill
+) 
+DELETE ps
+FROM PersonSkill ps
+INNER JOIN Dubplicates d
+ON ps.Id = d.Id
+WHERE d.rownumber >1;
+GO
+
+--add constraint to block new duplicates
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonSkill]') AND name = N'UC_Parent_Skill')
+ALTER TABLE PersonSkill ADD CONSTRAINT UC_Parent_Skill UNIQUE (Parent,Skill)
+
+
+----------------  
+--Name: Jonas N
+--Date: 2012-11-14  
+--Desc: Add the following new application function ShiftTradeRequestsWeb
+----------------  
+SET NOCOUNT ON
+	
+--declarations
+DECLARE @SuperUserId as uniqueidentifier
+DECLARE @FunctionId as uniqueidentifier
+DECLARE @ParentFunctionId as uniqueidentifier
+DECLARE @ForeignId as varchar(255)
+DECLARE @ParentForeignId as varchar(255)
+DECLARE @FunctionCode as varchar(255)
+DECLARE @FunctionDescription as varchar(255)
+DECLARE @ParentId as uniqueidentifier
+
+--insert to super user if not exist
+SELECT	@SuperUserId = '3f0886ab-7b25-4e95-856a-0d726edc2a67'
+
+-- check for the existence of super user role
+IF  (NOT EXISTS (SELECT id FROM [dbo].[Person] WHERE Id = @SuperUserId)) 
+INSERT [dbo].[Person]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Email], [Note], [EmploymentNumber], [TerminalDate], [FirstName], [LastName], [DefaultTimeZone], [IsDeleted], [BuiltIn], [FirstDayOfWeek])
+VALUES (@SuperUserId,1,@SuperUserId, @SuperUserId, getdate(), getdate(), '', '', '', NULL, '_Super User', '_Super User', 'UTC', 0, 1, 1)
+
+--get parent level
+SELECT @ParentForeignId = '0065'	--Parent Foreign id that is hardcoded
+SELECT @ParentId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ParentForeignId + '%')
+	
+--insert/modify application function
+SELECT @ForeignId = '0080' --Foreign id of the function > hardcoded	
+SELECT @FunctionCode = 'ShiftTradeRequests' --Name of the function > hardcoded
+SELECT @FunctionDescription = 'xxShiftTradeRequests' --Description of the function > hardcoded
+SELECT @ParentId = @ParentId
+
+IF  (NOT EXISTS (SELECT Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')))
+INSERT [dbo].[ApplicationFunction]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Parent], [FunctionCode], [FunctionDescription], [ForeignId], [ForeignSource], [IsDeleted])
+VALUES (newid(),1, @SuperUserId, @SuperUserId, getdate(), getdate(), @ParentId, @FunctionCode, @FunctionDescription, @ForeignId, 'Raptor', 0) 
+SELECT @FunctionId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
+UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentId WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
+
+SET NOCOUNT OFF
+GO
