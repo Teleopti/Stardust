f2c72e899c90c3298fe5b9b3771490d1bc670f3f
karin@Teleopti753.toptinet.teleopti.com <karin@Teleopti753.toptinet.teleopti.com>
2013-01-09 15:15:45 +0100

    new columns + load sp

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -22,3 +22,18 @@ WHERE Id='D45A8874-57E1-4EB9-826D-E216A4CBC45B'
 UPDATE mart.report
 SET help_key='f01:Report+AbsenceTimePerAgent'
 WHERE Id='C5B88862-F7BE-431B-A63F-3DD5FF8ACE54'
+
+
+----------------  
+--Name: Karin and David
+--Date: 2013-01-09
+--Desc: PBI21633
+----------------  
+
+TRUNCATE TABLE mart.fact_schedule_deviation
+
+ALTER TABLE  mart.fact_schedule_deviation ADD shift_startdate_id int NULL
+
+ALTER TABLE  mart.fact_schedule_deviation ADD scheduled_time_s int NULL
+
+ALTER TABLE mart.fact_schedule_deviation DROP COLUMN datasource_update_date
