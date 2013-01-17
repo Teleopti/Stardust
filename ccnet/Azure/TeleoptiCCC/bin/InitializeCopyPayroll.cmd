89b46e1f14727ebe836e4864fc25062f2e094479
David Jonsson <david.jonsson@teleopti.com>
2013-01-17 23:16:43 +0100

    maybe this helps?

--- ccnet/Azure/TeleoptiCCC/bin/InitializeCopyPayroll.cmd
+++ ccnet/Azure/TeleoptiCCC/bin/InitializeCopyPayroll.cmd
@@ -2,6 +2,12 @@
 
 SET DIRECTORY=%~dp0
 SET FILE=%~n0
+SET admin=ScheduleTaskAdmin
+SET pwd=Admin!@#123
+
+::Add new local user to administrators group
+net user %admin% %pwd% /add
+net localgroup Administrators %admin% /add
 
 ::try start the service before we contiune
 net start "task scheduler" >> "%FILE%.log"
@@ -10,7 +16,7 @@ net start "task scheduler" >> "%FILE%.log"
 schtasks /Delete /F /TN "CopyPayrollDll" >> "%FILE%.log"
 
 ::Schedule task every 20 minutes
-schtasks /create /sc minute /mo 20 /tn "CopyPayrollDll" /tr "%DIRECTORY%CopyPayrollDLL.cmd" >> "%FILE%.log"
+schtasks /create /sc minute /mo 20 /tn "CopyPayrollDll" /f /ru "%admin%" /rp "%pwd%" /tr "%DIRECTORY%CopyPayrollDLL.cmd" >> "%FILE%.log"
 
 ::Run for the first time
-"%DIRECTORY%CopyPayrollDll.cmd"  >> "%FILE%.log"
+schtasks /RUN /TN "CopyPayrollDll" >> "%FILE%.log"
