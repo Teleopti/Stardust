b19646998b11b4b50024680f351ca9bedd0eb723
David Jonsson <david.jonsson@teleopti.com>
2013-01-22 14:25:43 +0100

    re-add user and pwd, but generate a random password for each instance

--- ccnet/Azure/TeleoptiCCC/bin/InitializeCopyPayroll.cmd
+++ ccnet/Azure/TeleoptiCCC/bin/InitializeCopyPayroll.cmd
@@ -2,16 +2,29 @@
 
 SET DIRECTORY=%~dp0
 SET FILE=%~n0
-SET TaskName=CopyPayrollDll
+SET admin=ScheduleTaskAdmin
+SET taskName=CopyPayrollDll
+
+::generate password
+net user administrator /random > %temp%\pwd.txt
+set /P pwd=< %temp%\pwd.txt
+set pwd=%pwd:~31,39%
+echo Temporary user is %admin% with password %pwd% >> "%FILE%.log"
+del %temp%\pwd.txt
+
+::Add new local user to administrators group
+net user %admin% %pwd% /del
+net user %admin% %pwd% /add
+net localgroup Administrators %admin% /add
 
 ::try start the service before we contiune
 net start "task scheduler" >> "%FILE%.log"
 
 ::Delete if exist
-schtasks /Delete /F /TN "%TaskName%" >> "%FILE%.log"
+schtasks /Delete /F /TN "%taskName%" >> "%FILE%.log"
 
 ::Schedule task every 20 minutes
-schtasks /create /sc minute /mo 20 /tn "%TaskName%" /f /tr "%DIRECTORY%CopyPayrollDLL.cmd" >> "%FILE%.log"
+schtasks /create /sc minute /mo 20 /tn "%taskName%" /f /ru "%admin%" /rp "%pwd%" /tr "%DIRECTORY%CopyPayrollDll.cmd" >> "%FILE%.log"
 
 ::Run for the first time
-schtasks /RUN /TN "CopyPayrollDll" >> "%FILE%.log"
+schtasks /RUN /TN "%taskName%" >> "%FILE%.log"
