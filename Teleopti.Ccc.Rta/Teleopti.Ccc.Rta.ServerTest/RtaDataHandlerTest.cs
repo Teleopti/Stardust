3a4bb1af2e87497590a9958df2d1e4c81bbe4e6f
Erik Sundberg <none@none>
2013-01-24 15:01:58 +0100

    New tests for RtaDataHandler

--- Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.ServerTest/RtaDataHandlerTest.cs
+++ Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.ServerTest/RtaDataHandlerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
+using System.Net.Sockets;
 using System.Threading;
 using Teleopti.Interfaces.Domain;
 using log4net;
@@ -13,100 +14,360 @@ using Teleopti.Messaging.Exceptions;
 
 namespace Teleopti.Ccc.Rta.ServerTest
 {
-    [TestFixture]
-    [Category("LongRunning")]
-    public class RtaDataHandlerTest
-    {
-        private MockRepository _mocks;
-        private ILog _loggingSvc;
-        private IMessageSender _messageSender;
-        private IDatabaseConnectionFactory _databaseConnectionFactory;
-        private const string ConnectionString = "connection";
-        private RtaDataHandlerForTest _target;
-        private IDataSourceResolver _dataSourceResolver;
-        private IPersonResolver _personResolver;
-    	private IStateResolver _stateResolver;
-    	private IRtaConsumer _rtaConsumer;
+	[TestFixture]
+	[Category("LongRunning")]
+	public class RtaDataHandlerTest
+	{
+		private MockRepository _mocks;
+		private ILog _loggingSvc;
+		private IMessageSender _messageSender;
+		private IDatabaseConnectionFactory _databaseConnectionFactory;
+		private const string ConnectionString = "connection";
+		private RtaDataHandlerForTest _target;
+		private IDataSourceResolver _dataSourceResolver;
+		private IPersonResolver _personResolver;
+		private IStateResolver _stateResolver;
+		private IRtaConsumer _rtaConsumer;
 
-    	[SetUp]
-        public void Setup()
-        {
-            _mocks = new MockRepository();
-            _loggingSvc = _mocks.DynamicMock<ILog>();
-            _messageSender = _mocks.StrictMock<IMessageSender>();
-            _databaseConnectionFactory = _mocks.StrictMock<IDatabaseConnectionFactory>();
-            _dataSourceResolver = _mocks.StrictMock<IDataSourceResolver>();
-        	_personResolver = _mocks.DynamicMock<IPersonResolver>();
-    		_stateResolver = _mocks.DynamicMock<IStateResolver>();
-        	_rtaConsumer = _mocks.DynamicMock<IRtaConsumer>();
-        }
+		private string _logOn;
+		private string _stateCode;
+		private TimeSpan _timeInState;
+		private DateTime _timestamp;
+		private Guid _platformTypeId;
+		private string _sourceId;
+		private DateTime _batchId;
+		private bool _isSnapshot;
 
-        [Test, ExpectedException(typeof(BrokerNotInstantiatedException))]
-        public void VerifyCreateInstanceUsingEmptyConstructorFailsBecauseNoConfigurationAvailable()
-        {
+
+		[SetUp]
+		public void Setup()
+		{
+			_mocks = new MockRepository();
+			_loggingSvc = _mocks.DynamicMock<ILog>();
+			_messageSender = _mocks.StrictMock<IMessageSender>();
+			_databaseConnectionFactory = _mocks.StrictMock<IDatabaseConnectionFactory>();
+			_dataSourceResolver = _mocks.StrictMock<IDataSourceResolver>();
+			_personResolver = _mocks.DynamicMock<IPersonResolver>();
+			_stateResolver = _mocks.DynamicMock<IStateResolver>();
+			_rtaConsumer = _mocks.StrictMock<IRtaConsumer>();
+
+			_logOn = "002";
+			_stateCode = "AUX2";
+			_timeInState = TimeSpan.FromSeconds(45);
+			_timestamp = DateTime.Now;
+			_platformTypeId = Guid.Empty;
+			_sourceId = "1";
+			_batchId = SqlDateTime.MinValue.Value;
+			_isSnapshot = false;
+		}
+
+		[Test, ExpectedException(typeof (BrokerNotInstantiatedException))]
+		public void VerifyCreateInstanceUsingEmptyConstructorFailsBecauseNoConfigurationAvailable()
+		{
 			new RtaDataHandler(_rtaConsumer);
-        }
+		}
 
-        [Test]
-        public void VerifyErrorWhenMessageBrokerNotInstantiated()
-        {
-            _loggingSvc.Error("", new BrokerNotInstantiatedException());
-            LastCall.IgnoreArguments();
-            _messageSender.InstantiateBrokerService();
-            LastCall.Throw(new BrokerNotInstantiatedException());
-            _mocks.ReplayAll();
-			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, ConnectionString, _databaseConnectionFactory, _dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
-            _mocks.VerifyAll();
-        }
+		[Test]
+		public void VerifyProtectedConstructorWorks()
+		{
+			_messageSender.Expect(e => e.InstantiateBrokerService());
+			_mocks.ReplayAll();
+			new RtaDataHandlerForTest(_loggingSvc, _messageSender, string.Empty, _databaseConnectionFactory, _dataSourceResolver,
+			                          _personResolver, _stateResolver);
+			_mocks.VerifyAll();
+		}
 
-        [Test]
-        public void VerifyProperties()
-        {
-            _messageSender.InstantiateBrokerService();
-            Expect.Call(_messageSender.IsAlive).Return(true);
-            _mocks.ReplayAll();
-			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, ConnectionString, _databaseConnectionFactory, _dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
-            Assert.IsTrue(_target.IsAlive);
-            _mocks.VerifyAll();
-        }
+		[Test]
+		public void VerifyProtectedConstructorCatchBrokerException()
+		{
+			_messageSender.Expect(e => e.InstantiateBrokerService()).Throw(new BrokerNotInstantiatedException());
+			_loggingSvc.Expect(l => l.Error("", new BrokerNotInstantiatedException())).IgnoreArguments();
+			_mocks.ReplayAll();
+			new RtaDataHandlerForTest(_loggingSvc, _messageSender, string.Empty, _databaseConnectionFactory, _dataSourceResolver,
+			                          _personResolver, _stateResolver);
+			_mocks.VerifyAll();
+		}
 
-        [Test]
-        public void VerifyWarningWithNoConnectionString()
-        {
-            DateTime timestamp = DateTime.UtcNow;
-			var autoreset = new AutoResetEvent(true);
-            int dataSourceAfterResolve;
-            Expect.Call(_dataSourceResolver.TryResolveId("1",
-                                                        out dataSourceAfterResolve)).Return(true).OutRef(1);
-            IEnumerable<PersonWithBusinessUnit> personListAfterResolve;
-            Expect.Call(_personResolver.TryResolveId(1, "002", out personListAfterResolve)).Return(true).
-				OutRef(new List<PersonWithBusinessUnit> { new PersonWithBusinessUnit() });
-        	_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, "002")).IgnoreArguments().Return(true);
-        	_rtaConsumer.Expect(
-        		r =>
-        		r.Consume(Guid.Empty, Guid.Empty, Guid.Empty, "002", DateTime.Now, TimeSpan.FromMinutes(1),
-						  autoreset)).IgnoreArguments().Return(new ActualAgentState());
+		[Test]
+		public void VerifyErrorWhenMessageBrokerNotInstantiated()
+		{
+			_loggingSvc.Error("", new BrokerNotInstantiatedException());
+			LastCall.IgnoreArguments();
+			_messageSender.InstantiateBrokerService();
+			LastCall.Throw(new BrokerNotInstantiatedException());
+			_mocks.ReplayAll();
+			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, ConnectionString, _databaseConnectionFactory,
+			                                    _dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
+			_mocks.VerifyAll();
+		}
 
-            _messageSender.InstantiateBrokerService();
-            _messageSender.SendRtaData(Guid.Empty,Guid.Empty, null);
-            LastCall.IgnoreArguments();
-            Expect.Call(_messageSender.IsAlive).Return(true);
-            _loggingSvc.Warn("No connection information available in configuration file.");
+		[Test]
+		public void VerifyIsAlive()
+		{
+			_messageSender.InstantiateBrokerService();
+			Expect.Call(_messageSender.IsAlive).Return(true);
+			_mocks.ReplayAll();
+			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, ConnectionString, _databaseConnectionFactory,
+			                                    _dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
+			Assert.IsTrue(_target.IsAlive);
+			_mocks.VerifyAll();
+		}
 
-            _mocks.ReplayAll();
-			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, string.Empty, _databaseConnectionFactory, _dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
-            _target.ProcessRtaData("002", "AUX2", TimeSpan.FromSeconds(45), timestamp, Guid.NewGuid(), "1",
-                                  SqlDateTime.MinValue.Value, false);
-            _mocks.VerifyAll();
+		// needed?
+		[Test]
+		public void VerifyWarningWithNoConnectionString()
+		{
+			var autoreset = new AutoResetEvent(false);
+			int dataSourceAfterResolve;
+			Expect.Call(_dataSourceResolver.TryResolveId("1",
+			                                             out dataSourceAfterResolve)).Return(true).OutRef(1);
+			IEnumerable<PersonWithBusinessUnit> personListAfterResolve;
+			Expect.Call(_personResolver.TryResolveId(1, "002", out personListAfterResolve)).Return(true).
+				OutRef(new List<PersonWithBusinessUnit> {new PersonWithBusinessUnit()});
+			_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, "002")).IgnoreArguments().Return(true);
+			_rtaConsumer.Expect(
+				r =>
+				r.Consume(Guid.Empty, Guid.Empty, Guid.Empty, "002", DateTime.Now, TimeSpan.FromMinutes(1),
+				          autoreset)).IgnoreArguments().Return(new ActualAgentState());
+
+			_messageSender.InstantiateBrokerService();
+			_messageSender.SendRtaData(Guid.Empty, Guid.Empty, null);
+			LastCall.IgnoreArguments();
+			Expect.Call(_messageSender.IsAlive).Return(true);
+			_loggingSvc.Warn("No connection information available in configuration file.");
+
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+			
 			autoreset.Dispose();
-        }
+		}
 		
-        private class RtaDataHandlerForTest : RtaDataHandler
-        {
-			public RtaDataHandlerForTest(ILog loggingSvc, IMessageSender messageSender, string connectionStringDataStore, IDatabaseConnectionFactory databaseConnectionFactory, IDataSourceResolver dataSourceResolver, IPersonResolver personResolver, IStateResolver stateResolver, IRtaConsumer rtaConsumer)
-				: base(loggingSvc, messageSender, connectionStringDataStore, databaseConnectionFactory, dataSourceResolver, personResolver, stateResolver, rtaConsumer)
-            {
-            }
-        }
-    }
+		[Test]
+		public void ShouldNotSendWhenWrongDataSource()
+		{
+			_messageSender.InstantiateBrokerService();
+			int dataSource;
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(false).OutRef(1);
+			
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+		}
+
+		[Test]
+		public void ShouldNotSendWhenWrongPerson()
+		{
+			int dataSource;
+			IEnumerable<PersonWithBusinessUnit> outPersonBusinessUnits;
+
+			_messageSender.InstantiateBrokerService();
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(true).OutRef(1);
+			_personResolver.Expect(p => p.TryResolveId(1, _logOn, out outPersonBusinessUnits)).Return(false).OutRef(new object[1]);
+			_messageSender.Expect(m => m.IsAlive).Return(true);
+			
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+		}
+
+		[Test]
+		public void ShouldNotSendWhenStateHaveNotChanged()
+		{
+			int dataSource;
+			IEnumerable<PersonWithBusinessUnit> outPersonBusinessUnits;
+			var retPersonBusinessUnits = new List<PersonWithBusinessUnit>
+			                             	{
+			                             		new PersonWithBusinessUnit
+			                             			{
+			                             				BusinessUnitId = Guid.Empty,
+			                             				PersonId = Guid.Empty
+			                             			}
+			                             	};
+			
+			_messageSender.InstantiateBrokerService();
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(true).OutRef(1);
+			_personResolver.Expect(p => p.TryResolveId(1, _logOn, out outPersonBusinessUnits)).Return(true).OutRef(
+				retPersonBusinessUnits);
+			_messageSender.Expect(m => m.IsAlive).Return(true);
+			_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, _stateCode)).Return(false);
+			
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+		}
+
+		[Test]
+		public void ShouldNotSendWhenAgentStateIsNull()
+		{
+			int dataSource;
+			IEnumerable<PersonWithBusinessUnit> outPersonBusinessUnits;
+			var retPersonBusinessUnits = new List<PersonWithBusinessUnit>
+			                             	{
+			                             		new PersonWithBusinessUnit
+			                             			{
+			                             				BusinessUnitId = Guid.Empty,
+			                             				PersonId = Guid.Empty
+			                             			}
+			                             	};
+			var autoReset = new AutoResetEvent(false);
+
+			_messageSender.InstantiateBrokerService();
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(true).OutRef(1);
+			_personResolver.Expect(p => p.TryResolveId(1, _logOn, out outPersonBusinessUnits)).Return(true).OutRef(
+				retPersonBusinessUnits);
+			_messageSender.Expect(m => m.IsAlive).Return(true);
+			_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, _stateCode)).Return(true);
+			_rtaConsumer.Expect(
+				r => r.Consume(Guid.Empty, Guid.Empty, _platformTypeId, _stateCode, _timestamp, _timeInState, autoReset)).
+				IgnoreArguments().Return(
+					null);
+			
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+			
+			autoReset.Dispose();
+		}
+
+		[Test]
+		public void ShouldSend()
+		{
+			int dataSource;
+			IEnumerable<PersonWithBusinessUnit> outPersonBusinessUnits;
+			var retPersonBusinessUnits = new List<PersonWithBusinessUnit>
+			                             	{
+			                             		new PersonWithBusinessUnit
+			                             			{
+			                             				BusinessUnitId = Guid.Empty,
+			                             				PersonId = Guid.Empty
+			                             			}
+			                             	};
+			var autoReset = new AutoResetEvent(false);
+			var agentState = new ActualAgentState();
+
+			_messageSender.InstantiateBrokerService();
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(true).OutRef(1);
+			_personResolver.Expect(p => p.TryResolveId(1, _logOn, out outPersonBusinessUnits)).Return(true).OutRef(
+				retPersonBusinessUnits);
+			_messageSender.Expect(m => m.IsAlive).Return(true);
+			_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, _stateCode)).Return(true);
+			_rtaConsumer.Expect(
+				r => r.Consume(Guid.Empty, Guid.Empty, _platformTypeId, _stateCode, _timestamp, _timeInState, autoReset)).
+				IgnoreArguments().Return(agentState);
+			_messageSender.Expect(m => m.SendRtaData(Guid.Empty, Guid.Empty, agentState));
+			
+			_mocks.ReplayAll();
+
+			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, "connectionStringDataStore", _databaseConnectionFactory,
+												_dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
+			var result = _target.ProcessRtaData(_logOn, _stateCode, _timeInState, _timestamp, _platformTypeId, _sourceId, _batchId,
+								   _isSnapshot);
+			Assert.That(result, Is.Not.Null);
+			_mocks.VerifyAll();
+			
+			autoReset.Dispose();
+		}
+
+		[Test]
+		public void ShouldCatchSocketException()
+		{
+			int dataSource;
+			IEnumerable<PersonWithBusinessUnit> outPersonBusinessUnits;
+			var retPersonBusinessUnits = new List<PersonWithBusinessUnit>
+			                             	{
+			                             		new PersonWithBusinessUnit
+			                             			{
+			                             				BusinessUnitId = Guid.Empty,
+			                             				PersonId = Guid.Empty
+			                             			}
+			                             	};
+			var autoReset = new AutoResetEvent(false);
+			var agentState = new ActualAgentState();
+
+			_messageSender.InstantiateBrokerService();
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(true).OutRef(1);
+			_personResolver.Expect(p => p.TryResolveId(1, _logOn, out outPersonBusinessUnits)).Return(true).OutRef(
+				retPersonBusinessUnits);
+			_messageSender.Expect(m => m.IsAlive).Return(true);
+			_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, _stateCode)).Return(true);
+			_rtaConsumer.Expect(
+				r => r.Consume(Guid.Empty, Guid.Empty, _platformTypeId, _stateCode, _timestamp, _timeInState, autoReset)).
+				IgnoreArguments().Return(agentState);
+			_messageSender.Expect(m => m.SendRtaData(Guid.Empty, Guid.Empty, agentState)).Throw(new SocketException());
+			_loggingSvc.Expect(l => l.Error("", new SocketException())).IgnoreArguments();
+
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+
+			autoReset.Dispose();
+		}
+
+		[Test]
+		public void ShouldCatchBrokerNotInstantiatedException()
+		{
+			int dataSource;
+			IEnumerable<PersonWithBusinessUnit> outPersonBusinessUnits;
+			var retPersonBusinessUnits = new List<PersonWithBusinessUnit>
+			                             	{
+			                             		new PersonWithBusinessUnit
+			                             			{
+			                             				BusinessUnitId = Guid.Empty,
+			                             				PersonId = Guid.Empty
+			                             			}
+			                             	};
+			var autoReset = new AutoResetEvent(false);
+			var agentState = new ActualAgentState();
+
+			_messageSender.InstantiateBrokerService();
+			_dataSourceResolver.Expect(d => d.TryResolveId("1", out dataSource)).Return(true).OutRef(1);
+			_personResolver.Expect(p => p.TryResolveId(1, _logOn, out outPersonBusinessUnits)).Return(true).OutRef(
+				retPersonBusinessUnits);
+			_messageSender.Expect(m => m.IsAlive).Return(true);
+			_stateResolver.Expect(s => s.HaveStateCodeChanged(Guid.Empty, _stateCode)).Return(true);
+			_rtaConsumer.Expect(
+				r => r.Consume(Guid.Empty, Guid.Empty, _platformTypeId, _stateCode, _timestamp, _timeInState, autoReset)).
+				IgnoreArguments().Return(agentState);
+			_messageSender.Expect(m => m.SendRtaData(Guid.Empty, Guid.Empty, agentState)).Throw(new BrokerNotInstantiatedException());
+			_loggingSvc.Expect(l => l.Error("", new SocketException())).IgnoreArguments();
+
+			_mocks.ReplayAll();
+			AssignTargetAndRun();
+			_mocks.VerifyAll();
+
+			autoReset.Dispose();
+		}
+
+		private void AssignTargetAndRun()
+		{
+			_target = new RtaDataHandlerForTest(_loggingSvc, _messageSender, string.Empty, _databaseConnectionFactory,
+			                                    _dataSourceResolver, _personResolver, _stateResolver, _rtaConsumer);
+			_target.ProcessRtaData(_logOn, _stateCode, _timeInState, _timestamp, _platformTypeId, _sourceId, _batchId,
+			                       _isSnapshot);
+		}
+
+		private class RtaDataHandlerForTest : RtaDataHandler
+		{
+			public RtaDataHandlerForTest(ILog loggingSvc, IMessageSender messageSender, string connectionStringDataStore,
+			                             IDatabaseConnectionFactory databaseConnectionFactory,
+			                             IDataSourceResolver dataSourceResolver, IPersonResolver personResolver,
+			                             IStateResolver stateResolver, IRtaConsumer rtaConsumer)
+				: base(
+					loggingSvc, messageSender, connectionStringDataStore, databaseConnectionFactory, dataSourceResolver, personResolver,
+					stateResolver, rtaConsumer)
+			{
+			}
+
+			public RtaDataHandlerForTest(ILog loggingSvc, IMessageSender messageSender, string connectionStringDataStore,
+			                             IDatabaseConnectionFactory databaseConnectionFactory,
+			                             IDataSourceResolver dataSourceResolver, IPersonResolver personResolver,
+			                             IStateResolver stateResolver)
+				: base(
+					loggingSvc, messageSender, connectionStringDataStore, databaseConnectionFactory, dataSourceResolver, personResolver,
+					stateResolver)
+			{
+			}
+		}
+	}
 }
