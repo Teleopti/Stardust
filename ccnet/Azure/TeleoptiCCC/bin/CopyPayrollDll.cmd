530a28ac4b4a24f2dd09eca69983388134c96a97
David Jonsson <david.jonsson@teleopti.com>
2013-01-24 09:01:28 +0100

    Adding errorlevel on exit so that Schedule Task fails on powershell error

--- ccnet/Azure/TeleoptiCCC/bin/CopyPayrollDll.cmd
+++ ccnet/Azure/TeleoptiCCC/bin/CopyPayrollDll.cmd
@@ -5,4 +5,5 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 ::execute
-powershell -File "%DIRECTORY%\CopyPayrollDll.ps1" "%DIRECTORY%"
+powershell "%DIRECTORY%\CopyPayrollDll.ps1" "%DIRECTORY%"
+exit %ERRORLEVEL%
