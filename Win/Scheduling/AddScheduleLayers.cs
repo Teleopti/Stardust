a652ef84bd8e6a2be2b8a1717b963fa6c5830c00
Talha Muhammad <talha.muhammad@teleopti.com>
2013-01-28 14:24:39 +0100

    Fixed issue 22099: Add Activity default to next day in this time zone case
    --HG--
    branch : Release 7.3.374

--- Win/Scheduling/AddScheduleLayers.cs
+++ Win/Scheduling/AddScheduleLayers.cs
@@ -18,12 +18,12 @@ namespace Teleopti.Ccc.Win.Scheduling
             : base(presenter)
         { }
 
-        public IAddLayerViewModel<IAbsence> CreateAddAbsenceViewModel(IList<IAbsence> bindingList, ISetupDateTimePeriod period)
+        public IAddLayerViewModel<IAbsence> CreateAddAbsenceViewModel(IList<IAbsence> bindingList, ISetupDateTimePeriod period, TimeZoneInfo timeZoneInfo)
         {
             var model = new AddAbsenceViewModel(bindingList, period, WorkingInterval);
             model.PeriodViewModel.Min = Presenter.SelectedPeriod.Period().StartDateTime;
             model.PeriodViewModel.Max = Presenter.SelectedPeriod.Period().EndDateTime.AddDays(2);
-            AddDialogComposer<AddAbsenceViewModel> composer = new AddDialogComposer<AddAbsenceViewModel>(model);
+            AddDialogComposer<AddAbsenceViewModel> composer = new AddDialogComposer<AddAbsenceViewModel>(model, timeZoneInfo);
             return composer.Result();
         }
 
@@ -31,31 +31,31 @@ namespace Teleopti.Ccc.Win.Scheduling
         {
             var model = new AddPersonalActivityViewModel(activities, period, WorkingInterval);
             SetBoundary(model);
-            AddDialogComposer<IAddLayerViewModel<IActivity>> composer = new AddDialogComposer<IAddLayerViewModel<IActivity>>(model);
+            AddDialogComposer<IAddLayerViewModel<IActivity>> composer = new AddDialogComposer<IAddLayerViewModel<IActivity>>(model, timeZoneInfo);
             return composer.Result();
         }
 
-        public IAddOvertimeViewModel CreateAddOvertimeViewModel(IScheduleDay selectedSchedule, IList<IActivity> activities, IList<IMultiplicatorDefinitionSet> definitionSets, IActivity defaultActivity, DateTimePeriod period)
+        public IAddOvertimeViewModel CreateAddOvertimeViewModel(IScheduleDay selectedSchedule, IList<IActivity> activities, IList<IMultiplicatorDefinitionSet> definitionSets, IActivity defaultActivity, DateTimePeriod period, TimeZoneInfo timeZoneInfo)
         {
             var dateTimePeriod = TimeZoneHelper.NewUtcDateTimePeriodFromLocalDateTime(period.LocalStartDateTime, period.LocalEndDateTime);
             var fallbackDefaultHours = new SetupDateTimePeriodDefaultLocalHoursForActivities(selectedSchedule, dateTimePeriod);
             var model = new AddOvertimeViewModel(activities, definitionSets, defaultActivity, fallbackDefaultHours, WorkingInterval);
             SetBoundary(model);
-            return new AddDialogComposer<AddOvertimeViewModel>(model).Result();
+            return new AddDialogComposer<AddOvertimeViewModel>(model, timeZoneInfo).Result();
         }
 
         public IAddLayerViewModel<IDayOffTemplate> CreateAddDayOffViewModel(IList<IDayOffTemplate> dayOffTemplates, TimeZoneInfo timeZoneInfo, DateTimePeriod period)
         {
             var model = new AddDayOffViewModel(dayOffTemplates, period);
             SetBoundary(model);
-            return new AddDialogComposer<AddDayOffViewModel>(model).Result();
+            return new AddDialogComposer<AddDayOffViewModel>(model, timeZoneInfo).Result();
         }
 
         public IAddActivityViewModel CreateAddActivityViewModel(IList<IActivity> activities, IList<IShiftCategory> shiftCategories, DateTimePeriod period, TimeZoneInfo timeZoneInfo)
         {
             var model = new AddActivityViewModel(activities, shiftCategories, period, WorkingInterval);
             SetBoundary(model);
-            return new AddDialogComposer<AddActivityViewModel>(model).Result();
+            return new AddDialogComposer<AddActivityViewModel>(model, timeZoneInfo).Result();
         }
 
         private static TimeSpan WorkingInterval
