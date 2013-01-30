4dd67c160a3791bd06f162c31f9050c284905885
David Jonsson <david.jonsson@teleopti.com>
2013-01-30 21:34:36 +0100

    #22139 - wrong path
    --HG--
    branch : Release 7.3.374

--- Teleopti.Support.Tool/StartStopSystem/StartSystem.bat
+++ Teleopti.Support.Tool/StartStopSystem/StartSystem.bat
@@ -1,15 +1,17 @@
 @echo off
-SET action=delayed-auto
 ECHO Starting all local Teleopti CCC 7 services and MsDtc
-::WinXp and 2003 can't handle start mode: "delayed-auto"
-cscript OsMajorVersionGet.vbs //NoLogo
-SET /a OsMajorVersion = %ERRORLEVEL%
-IF %OsMajorVersion% LEQ 5 SET action=auto
-
-ECHO Start mode will be set to "%action%"
-PING -n 4 127.0.0.1>nul
+PING -n 1 127.0.0.1>nul
 
 IF "%ROOTDIR%"=="" SET ROOTDIR=%~dp0
+
+::WinXp and 2003 can't handle start mode: "delayed-auto"
+SET action=delayed-auto
+cscript "%ROOTDIR%OsMajorVersionGet.vbs" //NoLogo
+SET /a OsMajorVersion = %ERRORLEVEL%
+IF %OsMajorVersion% LEQ 5 SET action=auto
+ECHO OsMajorVersion is: %OsMajorVersion%
+ECHO Start mode will be set to "%action%"
+ECHO.
 
 ::Set the list of services to manuipulate
 ::note: Order matters!!
@@ -24,11 +26,17 @@ echo trying to start iis 7.0 or 7.5 App Pools ...
 "%windir%\system32\inetsrv\AppCmd.exe" Set Apppool "Teleopti ASP.NET v4.0 Web" /autoStart:true
 "%windir%\system32\inetsrv\AppCmd.exe" Start Apppool "Teleopti ASP.NET v4.0 Broker"
 "%windir%\system32\inetsrv\AppCmd.exe" Set Apppool "Teleopti ASP.NET v4.0 Broker" /autoStart:true
+"%windir%\system32\inetsrv\AppCmd.exe" Start Apppool "Teleopti ASP.NET v4.0 SDK"
+"%windir%\system32\inetsrv\AppCmd.exe" Set Apppool "Teleopti ASP.NET v4.0 SDK" /autoStart:true
+"%windir%\system32\inetsrv\AppCmd.exe" Start Apppool "Teleopti ASP.NET v4.0 RTA"
+"%windir%\system32\inetsrv\AppCmd.exe" Set Apppool "Teleopti ASP.NET v4.0 RTA" /autoStart:true
 ) else (
 echo trying to start iis 5.1 or 6.0 App Pools ...
 CSCRIPT "%ROOTDIR%..\WiseIISConfig\adsutil.vbs" START_SERVER "W3SVC/AppPools/Teleopti ASP.NET v4.0"
 CSCRIPT "%ROOTDIR%..\WiseIISConfig\adsutil.vbs" START_SERVER "W3SVC/AppPools/Teleopti ASP.NET v4.0 Web"
 CSCRIPT "%ROOTDIR%..\WiseIISConfig\adsutil.vbs" START_SERVER "W3SVC/AppPools/Teleopti ASP.NET v4.0 Broker"
+CSCRIPT "%ROOTDIR%..\WiseIISConfig\adsutil.vbs" START_SERVER "W3SVC/AppPools/Teleopti ASP.NET v4.0 SDK"
+CSCRIPT "%ROOTDIR%..\WiseIISConfig\adsutil.vbs" START_SERVER "W3SVC/AppPools/Teleopti ASP.NET v4.0 RTA"
 )
 
 echo Start App Pools. Done
