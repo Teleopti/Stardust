984bca2a7be96e3eb598fdc449fc12890b4af50c
David Jonsson <david.jonsson@teleopti.com>
2013-01-31 16:23:53 +0100

    Neuptune must run port 80 and SQL auth for end users (until #22208 is fixed)

--- ccnet/SilentInstall/server/machine/Neptune.bat
+++ ccnet/SilentInstall/server/machine/Neptune.bat
@@ -3,13 +3,13 @@
 ::hard coded for each environment
 ::=====================
 SET INSTALLDIR=C:\Program Files (x86)\Teleopti\
-SET DBServerInstance=neptune.toptinet.teleopti.com
-SET AppServer=silentcccinstall.teleopti.com
+SET DBServerInstance=neptune
+SET AppServer=neptune
 SET DB_ANALYTICS=TeleoptiAnalytics_Demo
 SET DB_CCC7=TeleoptiCCC7_Demo
 SET DB_CCCAGG=TeleoptiCCC7Agg_Demo
-SET DB_WINGROUP=NEPTUNE\EverybodyAtTeleopti
-SET /A SSL=1
+SET DB_WINGROUP=
+SET /A SSL=0
 SET ADDLOCAL=Database,AgentPortalWeb,Analytics,ClickOnce,AgentPortal,AdminClient,SDK,ServiceBus,ETL,Service,Tool,RTA,MessageBroker,RestoreDemo
 
 SET DB_AdminAccess=WINAUTH
@@ -19,9 +19,9 @@ SET DB_AdminAccess=WINAUTH
 ::SET DB_ADMIN_SQLLOGIN
 ::SET DB_ADMIN_PWD
 
-SET DB_EndUserAccess=WINAUTH
+SET DB_EndUserAccess=SQL
 ::if you specify:
 ::SET DB_EndUserAccess=SQL
 ::you must also provide:
-::SET DB_ENDUSER_SQLLOGIN
-::SET DB_ENDUSER_PWD
+SET DB_ENDUSER_SQLLOGIN=TeleoptiDemoUser
+SET DB_ENDUSER_PWD=TeleoptiDemoPwd
