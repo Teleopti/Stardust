73694301a005e5bd425c8e38c6a61bfc4a734f01
Henke <henrik.andersson@teleopti.com>
2013-02-07 18:48:04 +0100

    refact moved to class

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/ShiftTradeSwapDetailsViewModelMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using AutoMapper;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.Requests;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.Mapping
+{
+	public class ShiftTradeSwapDetailsViewModelMappingProfile : Profile
+	{
+		protected override void Configure()
+		{
+			CreateMap<IShiftTradeRequest, ShiftTradeSwapDetailsViewModel>()
+				.ConvertUsing(s =>
+					              {
+						              var details = s.ShiftTradeSwapDetails.First();
+
+
+						              var viewModel = new ShiftTradeSwapDetailsViewModel()
+							                              {
+								                              DateFrom = details.DateFrom,
+								                              DateTo = details.DateTo
+							                              };
+						              return viewModel;
+					              });
+		}
+	}
+}
