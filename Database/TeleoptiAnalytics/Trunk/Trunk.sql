deafd1412ca81851588dc03a4bd6976974da3d5d
Claes Stenman <claes.stenman@teleopti.com>
2013-02-11 14:58:32 +0100

    PBI[22024]Treat a broken lunch preference as an unfulfilled preference

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -1,3 +1,15 @@
+----------------  
+--Name: CS
+--Date: 2013-02-11  
+--Desc: PBI #22024 
+----------------  
+
+-- Add column for activity preference
+
+ALTER TABLE stage.stg_schedule_preference ADD
+	activity_code uniqueidentifier NULL
+GO
+
 -- Robin Karlsson, 2013-01-17
 -- Drop everything related to the message broker
 --Stored Procedures
