422acdbb8f4fdb7d9158255c3930b0867db8d847
Robin Karlsson <robin.karlsson@teleopti.com>
2013-02-13 15:38:59 +0100

    Blocking duplicate skills in a few places and removing the unique constraint to avoid other issues.
    --HG--
    branch : Release 7.3.378

--- Domain/AgentInfo/PersonPeriod.cs
+++ Domain/AgentInfo/PersonPeriod.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using Iesi.Collections.Generic;
 using Teleopti.Ccc.Domain.Common.EntityBaseTypes;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.AgentInfo
 {
-
-	/// <summary>
-	/// Represents the concrete circumstances of a working period for a person (agent).
-	/// Defines where, in which team, under what conditions the person WILL, and also 
-	/// in what skills the person CAN work.  
-	/// </summary>
-	/// <remarks>
-	/// Created by: Dinesh Ranasinghe
-	/// Created date: 2008-01-09
-	/// </remarks>
 	public class PersonPeriod : AggregateEntity, IPersonPeriod
 	{
 		private IPersonContract _personContract;
 		private ITeam _team;
-		private IList<IPersonSkill> _personSkillCollection;
+		private Iesi.Collections.Generic.ISet<IPersonSkill> _personSkillCollection;
 		private readonly IList<IPersonSkill> _personMaxSeatSkillCollection  = new List<IPersonSkill>();
 		private IList<IExternalLogOn> _externalLogOnCollection;
 		private DateOnly _startDate;
@@ -30,27 +22,10 @@ namespace Teleopti.Ccc.Domain.AgentInfo
         //TODO remove new and map when saving to DB
 	    private readonly IList<IPersonSkill> _personNonBlendSkillCollection = new List<IPersonSkill>();
 
-	    /// <summary>
-		/// Default constructordate
-		/// </summary>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-01-09
-		/// </remarks>
 		protected PersonPeriod()
 		{
 		}
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="PersonPeriod"/> class.
-		/// </summary>
-		/// <param name="startDate"></param>
-		/// <param name="personContract"></param>
-		/// <param name="team"></param>
-		/// <remarks>
-		/// Created by: cs
-		/// Created date: 2008-03-04
-		/// </remarks>
 		public PersonPeriod(DateOnly startDate, IPersonContract personContract, ITeam team)
 			: this()
 		{
@@ -59,29 +34,18 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 
 			_personContract = personContract;
 			_team = team;
-			_personSkillCollection = new List<IPersonSkill>();
+			_personSkillCollection = new HashedSet<IPersonSkill>();
 			_personMaxSeatSkillCollection = new List<IPersonSkill>();
 			_externalLogOnCollection = new List<IExternalLogOn>();
 			_startDate = startDate;
 		}
 
-		/// <summary>
-		/// StartDate
-		/// </summary>
-		/// /// <remarks>
-		/// Created by: cs
-		/// Created date: 2008-03-08
-		/// </remarks>
 		public virtual DateOnly StartDate
 		{
 			get { return _startDate; }
 			set { _startDate = value; }
 		}
 
-		/// <summary>
-		/// Gets the period enddate
-		/// </summary>
-		/// <value>The end date.</value>
 		public virtual DateOnly EndDate()
 		{
 			var person = (IPerson)Parent;
@@ -93,13 +57,6 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 			return nextPeriod.StartDate.AddDays(-1);
 		}
 
-		/// <summary>
-		/// Gets the period
-		/// </summary>
-		/// <remarks>
-		/// Created by: cs
-		/// Created date: 2008-03-06
-		/// </remarks>
 		public virtual DateOnlyPeriod Period
 		{
 			get
@@ -109,13 +66,6 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 			}
 		}
 
-		/// <summary>
-		/// Represent Person contract
-		/// </summary>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-01-09
-		/// </remarks>
 		public virtual IPersonContract PersonContract
 		{
 			get
@@ -128,71 +78,32 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 			}
 		}
 
-		/// <summary>
-		/// Represent Team
-		/// </summary>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-01-09
-		/// </remarks>
 		public virtual ITeam Team
 		{
 			get { return _team; }
 			set { _team = value; }
 		}
 
-		/// <summary>
-		/// Gets or sets the person skill collection.
-		/// </summary>
-		/// <value>The person skill collection.</value>
-		/// <remarks>
-		/// Created by: sumeda herath
-		/// Created date: 2008-01-09
-		/// </remarks>
 		public virtual IList<IPersonSkill> PersonSkillCollection
 		{
 			get
 			{
-				return new ReadOnlyCollection<IPersonSkill>(_personSkillCollection);
+				return new ReadOnlyCollection<IPersonSkill>(_personSkillCollection.ToList());
 			}
 		}
 
-		/// <summary>
-		/// Gets or sets the rule set bag.
-		/// </summary>
-		/// <value>The rule set bag.</value>
-		/// <remarks>
-		/// Created by: rogerkr
-		/// Created date: 2008-03-27
-		/// </remarks>
 		public virtual IRuleSetBag RuleSetBag
 		{
 			get { return _ruleSetBag; }
 			set { _ruleSetBag = value; }
 		}
 
-		/// <summary>
-		/// Gets or sets the note.
-		/// </summary>
-		/// <value>The note.</value>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-10-08
-		/// </remarks>
 		public virtual string Note
 		{
 			get { return _note; }
 			set { _note = value; }
 		}
 
-		/// <summary>
-		/// Gets the ACD login collection.
-		/// </summary>
-		/// <value>The ACD login collection.</value>
-		/// <remarks>
-		/// Created by: robink
-		/// Created date: 2008-06-18
-		/// </remarks>
 		public virtual ReadOnlyCollection<IExternalLogOn> ExternalLogOnCollection
 		{
 			get { return new ReadOnlyCollection<IExternalLogOn>(_externalLogOnCollection); }
@@ -204,15 +115,6 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 			set { _budgetGroup = value; }
 		}
 
-		/// <summary>
-		/// Adds the person skill.
-		/// </summary>
-		/// <param name="personSkill">The person skill.</param>
-		/// <returns></returns>
-		/// <remarks>
-		/// Created by: sumeda herath
-		/// Created date: 2008-01-09
-		/// </remarks>
 		public virtual void AddPersonSkill(IPersonSkill personSkill)
 		{
 			InParameter.NotNull("personSkill", personSkill);
@@ -220,14 +122,6 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 			_personSkillCollection.Add(personSkill);
 		}
 
-		/// <summary>
-		/// Deletes the person skill.
-		/// </summary>
-		/// <param name="personSkill">The person skill.</param>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-04-02
-		/// </remarks>
 		public virtual void DeletePersonSkill(IPersonSkill personSkill)
 		{
 			InParameter.NotNull("personSkill", personSkill);
@@ -235,27 +129,11 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 			_personSkillCollection.Remove(personSkill);
 		}
 
-
-		/// <summary>
-		/// Resets the person skill.
-		/// </summary>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-08-05
-		/// </remarks>
 		public virtual void ResetPersonSkill()
 		{
 			_personSkillCollection.Clear();
 		}
 
-		/// <summary>
-		/// Adds the login.
-		/// </summary>
-		/// <param name="externalLogOn">The log on.</param>
-		/// <remarks>
-		/// Created by: robink
-		/// Created date: 2008-06-18
-		/// </remarks>
 		public virtual void AddExternalLogOn(IExternalLogOn externalLogOn)
 		{
 			InParameter.NotNull("logOn", externalLogOn);
@@ -264,18 +142,6 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 				_externalLogOnCollection.Add(externalLogOn);
 		}
 
-		/// <summary>
-		/// Removes the external log on.
-		/// </summary>
-		/// <param name="externalLogOn">The external log on.</param>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-08-18
-		/// </remarks>
-		/// <remarks>
-		/// Created by: Dinesh Ranasinghe
-		/// Created date: 2008-08-18
-		/// </remarks>
 		public virtual void RemoveExternalLogOn(IExternalLogOn externalLogOn)
 		{
 			InParameter.NotNull("externalLogOn", externalLogOn);
@@ -311,13 +177,6 @@ namespace Teleopti.Ccc.Domain.AgentInfo
             _personNonBlendSkillCollection.Add(personSkill);
 	    }
 
-	    /// <summary>
-		/// Resets the external log on.
-		/// </summary>
-		/// <remarks>
-		/// Created by: Muhamad Risath
-		/// Created date: 2008-08-15
-		/// </remarks>
 		public virtual void ResetExternalLogOn()
 		{
 			_externalLogOnCollection.Clear();
@@ -346,7 +205,7 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 		{
 			var retobj = (PersonPeriod)MemberwiseClone();
 			retobj.SetId(null);
-			retobj._personSkillCollection = new List<IPersonSkill>();
+			retobj._personSkillCollection = new HashedSet<IPersonSkill>();
 			retobj._externalLogOnCollection = new List<IExternalLogOn>(_externalLogOnCollection);
 
 			foreach (IPersonSkill personSkill in _personSkillCollection)
@@ -364,7 +223,7 @@ namespace Teleopti.Ccc.Domain.AgentInfo
 		public virtual IPersonPeriod EntityClone()
 		{
 			var retobj = (PersonPeriod)MemberwiseClone();
-			retobj._personSkillCollection = new List<IPersonSkill>();
+			retobj._personSkillCollection = new HashedSet<IPersonSkill>();
 			retobj._externalLogOnCollection = new List<IExternalLogOn>(_externalLogOnCollection);
 
 			foreach (IPersonSkill personSkill in _personSkillCollection)
