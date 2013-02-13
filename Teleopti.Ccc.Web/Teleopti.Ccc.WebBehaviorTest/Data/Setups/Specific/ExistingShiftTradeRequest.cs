9602bc394d9097f289b1131461efb8956f7f54a4
Henke <henrik.andersson@teleopti.com>
2013-02-13 16:29:37 +0100

    Added schedule to existing shifttrade

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Specific/ExistingShiftTradeRequest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Specific/ExistingShiftTradeRequest.cs
@@ -15,29 +15,31 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Specific
 		public string From { get; set; }
 		public string To { get; set; }
 		public string Subject { get; set; }
+		public DateTime? DateTo { get; set; }
+		public DateTime? DateFrom { get; set; }
 
-		public void Apply(IPerson user,IUnitOfWork uow)
+		public void Apply(IPerson user, IUnitOfWork uow)
 		{
-					var today = DateTime.UtcNow.Date;
-					var tomorrow = today.AddDays(1);
-					var sender = String.IsNullOrEmpty(From) ? user : GetOrCreatePerson(From, uow);
-					var reciever = String.IsNullOrEmpty(To) ? user : GetOrCreatePerson(To, uow);
+			var dateTimefrom = DateFrom ?? DateTime.UtcNow.Date;
+			var dateTimeTo = DateTo ?? dateTimefrom.AddDays(1);
+			var sender = String.IsNullOrEmpty(From) ? user : GetOrCreatePerson(From, uow);
+			var reciever = String.IsNullOrEmpty(To) ? user : GetOrCreatePerson(To, uow);
 
-					var shiftTradeSwapDetail = new ShiftTradeSwapDetail(sender, reciever, new DateOnly(today), new DateOnly(tomorrow));
-					var shiftTradeRequest = new ShiftTradeRequest(new List<IShiftTradeSwapDetail> { shiftTradeSwapDetail });
-					PersonRequest = new PersonRequest(reciever)
-					{
-						Subject = Subject == string.Empty ? "Swap shift with " + sender.Name : Subject
-					};
-					PersonRequest.Request = shiftTradeRequest;
-					PersonRequest.TrySetMessage("This is a short text for the description of a shift trade request");
+			var shiftTradeSwapDetail = new ShiftTradeSwapDetail(sender, reciever, new DateOnly(dateTimefrom), new DateOnly(dateTimeTo));
+			var shiftTradeRequest = new ShiftTradeRequest(new List<IShiftTradeSwapDetail> { shiftTradeSwapDetail });
+			PersonRequest = new PersonRequest(reciever)
+			{
+				Subject = Subject == string.Empty ? "Swap shift with " + sender.Name : Subject
+			};
+			PersonRequest.Request = shiftTradeRequest;
+			PersonRequest.TrySetMessage("This is a short text for the description of a shift trade request");
 
-					var setShiftTraderequestCheckSum = new ShiftTradeRequestSetChecksum(new ScenarioRepository(uow),
-					                                                                    new ScheduleRepository(uow));
+			var setShiftTraderequestCheckSum = new ShiftTradeRequestSetChecksum(new ScenarioRepository(uow),
+																									  new ScheduleRepository(uow));
 
-					setShiftTraderequestCheckSum.SetChecksum(shiftTradeRequest);
-					var requestRepository = new PersonRequestRepository(uow);
-					requestRepository.Add(PersonRequest);				
+			setShiftTraderequestCheckSum.SetChecksum(shiftTradeRequest);
+			var requestRepository = new PersonRequestRepository(uow);
+			requestRepository.Add(PersonRequest);
 		}
 
 		private static IPerson GetOrCreatePerson(string name, IUnitOfWork uow)
@@ -47,10 +49,10 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Specific
 			var people = personRepository.LoadAll();
 			var person = people.FirstOrDefault(p => p.Name == personName);
 			if (person == null)
-				{
-					person = PersonFactory.CreatePerson(personName);
-					personRepository.Add(person);
-				}
+			{
+				person = PersonFactory.CreatePerson(personName);
+				personRepository.Add(person);
+			}
 			return person;
 		}
 	}
