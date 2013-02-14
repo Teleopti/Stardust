1bd92f52663dfa020305ac8f5aeb6995f4693a03
Kunning Mao <none@none>
2013-02-14 17:05:22 +0100

    more clean up

--- Teleopti.Ccc.Web.IdentityProvider/Models/AccountModels.cs
+++ Teleopti.Ccc.Web.IdentityProvider/Models/AccountModels.cs
@@ -1,94 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Web.Mvc;
 using System.Web.Security;
 
 namespace Teleopti.Ccc.Web.IdentityProvider.Models
 {
 
-    #region Models
-    public class LogOnModel
-    {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
+	public class LogOnModel
+	{
+		[Required]
+		[Display(Name = "User name")]
+		public string UserName { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
-    }
+		[Display(Name = "Remember me?")]
+		public bool RememberMe { get; set; }
+	}
 
-    #endregion
+	// The FormsAuthentication type is sealed and contains static members, so it is difficult to
+	// unit test code that calls its members. The interface and helper class below demonstrate
+	// how to create an abstract wrapper around such a type in order to make the AccountController
+	// code unit testable.
 
-    #region Services
-    // The FormsAuthentication type is sealed and contains static members, so it is difficult to
-    // unit test code that calls its members. The interface and helper class below demonstrate
-    // how to create an abstract wrapper around such a type in order to make the AccountController
-    // code unit testable.
+	public interface IMembershipService
+	{
+		bool ValidateUser(string userName, string password);
+	}
 
-    public interface IMembershipService
-    {
-        int MinPasswordLength { get; }
+	public class AccountMembershipService : IMembershipService
+	{
+		//private readonly MembershipProvider _provider;
 
-        bool ValidateUser(string userName, string password);
-    }
+		public bool ValidateUser(string userName, string password)
+		{
+			return true;
+		}
+	}
 
-    public class AccountMembershipService : IMembershipService
-    {
-        private readonly MembershipProvider _provider;
+	public interface IFormsAuthenticationService
+	{
+		void SignIn(string userName, bool createPersistentCookie);
+		void SignOut();
+	}
 
-        public AccountMembershipService()
-            : this(null)
-        {
-        }
+	public class FormsAuthenticationService : IFormsAuthenticationService
+	{
+		public void SignIn(string userName, bool createPersistentCookie)
+		{
+			if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
 
-        public AccountMembershipService(MembershipProvider provider)
-        {
-            _provider = provider ?? Membership.Provider;
-        }
+			FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
+		}
 
-        public int MinPasswordLength
-        {
-            get
-            {
-                return _provider.MinRequiredPasswordLength;
-            }
-        }
-
-        public bool ValidateUser(string userName, string password)
-        {
-            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
-            if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
-
-            return _provider.ValidateUser(userName, password);
-        }
-    }
-
-    public interface IFormsAuthenticationService
-    {
-        void SignIn(string userName, bool createPersistentCookie);
-        void SignOut();
-    }
-
-    public class FormsAuthenticationService : IFormsAuthenticationService
-    {
-        public void SignIn(string userName, bool createPersistentCookie)
-        {
-            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
-
-            FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
-        }
-
-        public void SignOut()
-        {
-            FormsAuthentication.SignOut();
-        }
-    }
-    #endregion
+		public void SignOut()
+		{
+			FormsAuthentication.SignOut();
+		}
+	}
 }
