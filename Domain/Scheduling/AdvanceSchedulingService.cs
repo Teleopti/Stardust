d859644fce53b6e9ac8c0d4b2b34686a89967839
Xianwei Shen <xianwei.shen@teleopti.com>
2013-02-15 15:41:04 +0100

    reduce duplicate runs.

--- Domain/Scheduling/AdvanceSchedulingService.cs
+++ Domain/Scheduling/AdvanceSchedulingService.cs
@@ -104,17 +104,17 @@ namespace Teleopti.Ccc.Domain.Scheduling
 
             var startDate = StartDate(selectedPersonMatrixList, out dayOff, out effectiveDays, out unLockedDays);
 
-            var selectedPerson = selectedPersonMatrixList.Select(scheduleMatrixPro => scheduleMatrixPro.Person).ToList();
+            var selectedPerson = selectedPersonMatrixList.Select(scheduleMatrixPro => scheduleMatrixPro.Person).Distinct().ToList();
             
             while (startDate != DateOnly.MinValue )
             {
                 //call class that return the teamblock dates for a given date (problem if team members don't have same days off)
                 var dateOnlyList = _dynamicBlockFinder.ExtractBlockDays( startDate );
-                var allGroupPersonListOnStartDate = new List<IGroupPerson>();
+                var allGroupPersonListOnStartDate = new HashSet<IGroupPerson>();
                 
                 foreach (var person in selectedPerson)
                 {
-                    allGroupPersonListOnStartDate.Add(_groupPersonBuilderForOptimization.BuildGroupPerson(person,startDate ));
+					allGroupPersonListOnStartDate.Add(_groupPersonBuilderForOptimization.BuildGroupPerson(person, startDate));
                 }
                 
                 foreach (var fullGroupPerson in allGroupPersonListOnStartDate.GetRandom(allGroupPersonListOnStartDate.Count, true))
@@ -124,7 +124,7 @@ namespace Teleopti.Ccc.Domain.Scheduling
                     {
                         var groupMatrixList = GetScheduleMatrixProList(groupPerson, startDate, allPersonMatrixList);
                         //call class that returns the aggregated restrictions for the teamblock (is team member personal skills needed for this?)
-                        var restriction = _restrictionAggregator.Aggregate(dateOnlyList, groupPerson, _schedulingOptions);
+                        var restriction = _restrictionAggregator.Aggregate(dateOnlyList, groupPerson,groupMatrixList, _schedulingOptions);
 
                         //call class that returns the aggregated intraday dist based on teamblock dates ???? consider the priority and understaffing
                         var activityInternalData = _skillDayPeriodIntervalDataGenerator.Generate(fullGroupPerson, dateOnlyList);
