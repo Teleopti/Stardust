fec9c197d01037b4ff1ec9e11a910edf9dc98107
tamasb@teleopti696.toptinet.teleopti.com <tamasb@teleopti696.toptinet.teleopti.com>
2013-02-15 09:58:54 +0100

    fix of the bugfix 22336: better trunk file
    --HG--
    branch : Release 7.3.378

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -1,8 +1,69 @@
+
 ----------------  
 --Name: Tamasb
 --Date: 2012-11-26  
---Desc: Bugfix reAdd the following new application function: AdminWeb
+--Desc: Bugfix 22336 correct the following application function: AdminWeb
 --Note: The application function has been once added in v. 378.sql, but there the foreignid is already occupied, so the application function
+--      was not added at all. Adding the function again will correct that bug
+----------------  
+UPDATE [ApplicationFunction]
+SET [ForeignId] = '0083'
+WHERE [FunctionCode] = 'ShiftTradeRequests'
+AND [ForeignId] = '0080'
+
+----------------  
+--Name: tamasb
+--Date: 2012-11-28  
+--Desc: Bugfix 22336 : readd the following application function: ShiftTradeRequests
+--Note: The application function has been once added in v. 378.sql, but there the foreignid is already occupied, so the application function
+--      changed the other function by mistake. Adding the function again will correct that bug
+----------------  
+SET NOCOUNT ON
+	
+--declarations
+DECLARE @SuperUserId as uniqueidentifier
+DECLARE @FunctionId as uniqueidentifier
+DECLARE @ParentFunctionId as uniqueidentifier
+DECLARE @ForeignId as varchar(255)
+DECLARE @ParentForeignId as varchar(255)
+DECLARE @FunctionCode as varchar(255)
+DECLARE @FunctionDescription as varchar(255)
+DECLARE @ParentId as uniqueidentifier
+
+--insert to super user if not exist
+SELECT	@SuperUserId = '3f0886ab-7b25-4e95-856a-0d726edc2a67'
+
+-- check for the existence of super user role
+IF  (NOT EXISTS (SELECT id FROM [dbo].[Person] WHERE Id = @SuperUserId)) 
+INSERT [dbo].[Person]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Email], [Note], [EmploymentNumber], [TerminalDate], [FirstName], [LastName], [DefaultTimeZone], [IsDeleted], [BuiltIn], [FirstDayOfWeek])
+VALUES (@SuperUserId,1,@SuperUserId, @SuperUserId, getdate(), getdate(), '', '', '', NULL, '_Super User', '_Super User', 'UTC', 0, 1, 1)
+
+--get parent level
+SELECT @ParentForeignId = '0065'	--Parent Foreign id that is hardcoded
+SELECT @ParentId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ParentForeignId + '%')
+	
+--insert/modify application function
+SELECT @ForeignId = '0083' --Foreign id of the function > hardcoded	
+SELECT @FunctionCode = 'ShiftTradeRequests' --Name of the function > hardcoded
+SELECT @FunctionDescription = 'xxShiftTradeRequests' --Description of the function > hardcoded
+SELECT @ParentId = @ParentId
+
+IF  (NOT EXISTS (SELECT Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')))
+INSERT [dbo].[ApplicationFunction]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [UpdatedOn], [Parent], [FunctionCode], [FunctionDescription], [ForeignId], [ForeignSource], [IsDeleted])
+VALUES (newid(),1, @SuperUserId, @SuperUserId, getdate(), getdate(), @ParentId, @FunctionCode, @FunctionDescription, @ForeignId, 'Raptor', 0) 
+SELECT @FunctionId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
+UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentId WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')
+
+SET NOCOUNT OFF
+GO
+
+
+
+----------------  
+--Name: Tamasb
+--Date: 2012-11-26  
+--Desc: Bugfix 22336 readd the following application function: AdminWeb
+--Note: The application function has been once added in v. 375.sql, but there the foreignid is already occupied, so the application function
 --      was not added at all. Adding the function again will correct that bug
 ----------------  
 SET NOCOUNT ON
@@ -30,9 +91,9 @@ SELECT @ParentForeignId = '0001'	--Parent Foreign id that is hardcoded
 SELECT @ParentId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ParentForeignId + '%')
 	
 --insert/modify application function
-SELECT @ForeignId = '0083'
+SELECT @ForeignId = '0080'
 SELECT @FunctionCode = 'AdminWeb'
-SELECT @FunctionDescription = 'Web'
+SELECT @FunctionDescription = 'xxAnywhere'
 SELECT @ParentId = @ParentId
 
 IF  (NOT EXISTS (SELECT Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')))
@@ -48,9 +109,8 @@ GO
 ----------------  
 --Name: tamasb
 --Date: 2012-11-28  
---Desc: Add the following new application function to AdminWeb: Schedules
---Note: The application function has been once added in v. 378.sql, but there the parent's foreignid was already occupied, so the application function
---      was not added but with a wrong parent. Adding the function again will correct that bug
+--Desc: Bugfix 22336 : readd the following application function: SchedulesAdminWeb
+--Note: The application function has been once added in v. 375.sql, but later its parent was overriden by mistake by another function 
 ----------------  
 SET NOCOUNT ON
 	
@@ -73,13 +133,13 @@ INSERT [dbo].[Person]([Id], [Version], [CreatedBy], [UpdatedBy], [CreatedOn], [U
 VALUES (@SuperUserId,1,@SuperUserId, @SuperUserId, getdate(), getdate(), '', '', '', NULL, '_Super User', '_Super User', 'UTC', 0, 1, 1)
 
 --get parent level
-SELECT @ParentForeignId = '0083'	--Parent Foreign id that is hardcoded
+SELECT @ParentForeignId = '0080'	--Parent Foreign id that is hardcoded
 SELECT @ParentId = Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ParentForeignId + '%')
 	
 --insert/modify application function
 SELECT @ForeignId = '0081'
 SELECT @FunctionCode = 'SchedulesAdminWeb'
-SELECT @FunctionDescription = 'Schedules'
+SELECT @FunctionDescription = 'xxSchedules'
 SELECT @ParentId = @ParentId
 
 IF  (NOT EXISTS (SELECT Id FROM ApplicationFunction WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like(@ForeignId + '%')))
@@ -91,3 +151,4 @@ UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentI
 
 SET NOCOUNT OFF
 GO
+
