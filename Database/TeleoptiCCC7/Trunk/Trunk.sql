1cc108a17af9804738e91d467b99f06b528ca6c1
Robin Karlsson <robin.karlsson@teleopti.com>
2013-02-18 13:48:45 +0100

    Adding an extra drop in trunk.
    --HG--
    branch : Release 7.3.380

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -153,3 +153,12 @@ UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentI
 SET NOCOUNT OFF
 GO
 
+
+----------------  
+--Name: Robin Karlsson
+--Date: 2013-02-13
+--Desc: Bug #22194. Constraint violation error in PersonSkill
+----------------  
+IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonSkill]') AND name = N'UC_Parent_Skill')
+	EXEC('ALTER TABLE [dbo].[PersonSkill] DROP CONSTRAINT [UC_Parent_Skill]')
+GO
