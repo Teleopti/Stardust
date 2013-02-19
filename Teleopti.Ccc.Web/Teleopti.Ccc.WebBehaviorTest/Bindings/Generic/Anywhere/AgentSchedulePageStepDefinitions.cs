f4cf9b359de347e75238768636798e7b8be37fc0
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-02-19 11:09:35 +0100

    Added last agent schedule step definition

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Anywhere/AgentSchedulePageStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Anywhere/AgentSchedulePageStepDefinitions.cs
@@ -1,10 +1,12 @@
 using System;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 using Teleopti.Ccc.WebBehaviorTest.Core.Robustness;
 using Teleopti.Ccc.WebBehaviorTest.Data;
 using WatiN.Core;
 using Browser = Teleopti.Ccc.WebBehaviorTest.Core.Browser;
+using Table = TechTalk.SpecFlow.Table;
 
 namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.Anywhere
 {
@@ -38,5 +40,18 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.Anywhere
 			EventualAssert.That(() => Browser.Current.Element(Find.BySelector(".shift .layer")).Exists, Is.False);
 		}
 
+		[Then(@"I should see a shift layer with")]
+		public void ThenIShouldSeeAShiftLayerWith(Table table)
+		{
+			var shiftLayer = table.CreateInstance<ShiftLayerInfo>();
+			EventualAssert.That(() => Browser.Current.Element(Find.BySelector(".shift .layer")).GetAttributeValue("data-start-time"), Is.EqualTo(shiftLayer.StartTime));
+			EventualAssert.That(() => Browser.Current.Element(Find.BySelector(".shift .layer:last")).GetAttributeValue("data-end-time"), Is.EqualTo(shiftLayer.EndTime));
+		}
+
+		public class ShiftLayerInfo
+		{
+			public DateTime StartTime { get; set; }
+			public DateTime EndTime { get; set; }
+		}
 	}
 }
