708f70133b2119e2ba433d9ff1d4f2f0458c9613
Kunning Mao <none@none>
2013-02-19 11:10:19 +0100

    remove session dependency

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/SSO/Controllers/OpenIdController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/SSO/Controllers/OpenIdController.cs
@@ -2,10 +2,8 @@
 using System.Linq;
 using System.Web.Mvc;
 using DotNetOpenAuth.OpenId;
-using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
 using DotNetOpenAuth.OpenId.Provider;
 using DotNetOpenAuth.Messaging;
-using Teleopti.Ccc.Web.Areas.SSO.Core;
 
 namespace Teleopti.Ccc.Web.Areas.SSO.Controllers
 {
@@ -17,9 +15,8 @@ namespace Teleopti.Ccc.Web.Areas.SSO.Controllers
 		  {
 				if (Request.AcceptTypes != null && Request.AcceptTypes.Contains("application/xrds+xml"))
 				{
-					 return new TransferResult("~/OpenId/Xrds");
+					 return View("Xrds");
 				}
-
 				return View();
 		  }
 
@@ -35,60 +32,32 @@ namespace Teleopti.Ccc.Web.Areas.SSO.Controllers
 						  return OpenIdProvider.PrepareResponse(request).AsActionResult();
 					 }
 
-					 ProviderEndpoint.PendingRequest = (IHostProcessedRequest)request;
 					 var idrequest = request as IAuthenticationRequest;
 
 					 // Verify that RP discovery is successful.
 					 if (idrequest.IsReturnUrlDiscoverable(ProviderEndpoint.Provider.Channel.WebRequestHandler) != RelyingPartyDiscoveryResult.Success)
 					 {
 						 idrequest.IsAuthenticated = false;
-						 ProviderEndpoint.Provider.PrepareResponse(idrequest).AsActionResult();
+						 return ProviderEndpoint.Provider.PrepareResponse(idrequest).AsActionResult();
 					 }
 
 					 idrequest.LocalIdentifier = buildIdentityUrl();
 					 idrequest.IsAuthenticated = true;
-					 ProviderEndpoint.SendResponse();
-
-					 if (idrequest.IsAuthenticated != null && idrequest.IsAuthenticated.Value)
-					 {
-						 // add extension responses here.
-						 var fetchRequest = idrequest.GetExtension<FetchRequest>();
-						 if (fetchRequest != null)
-						 {
-							 var fetchResponse = new FetchResponse();
-							 //if (fetchRequest.Attributes.Contains(RolesAttribute))
-							 //{
-							 //    // Inform the RP what roles this user should fill
-							 //    // These roles would normally come out of the user database
-							 //    // or Windows security groups.
-							 //    fetchResponse.Attributes.Add(RolesAttribute, "Member", "Admin");
-							 //}
-							 idrequest.AddResponseExtension(fetchResponse);
-						 }
-					 }
+					 ProviderEndpoint.Provider.SendResponse(request);
 				}
 
 				return new EmptyResult();
 		  }
 
-
 		  private static Identifier buildIdentityUrl()
 		  {
-			  string username = System.Web.HttpContext.Current.User.Identity.Name;
-			  int slash = username.IndexOf('\\');
+			  var username = System.Web.HttpContext.Current.User.Identity.Name;
+			  var slash = username.IndexOf('\\');
 			  if (slash >= 0)
 			  {
 				  username = username.Substring(slash + 1);
 			  }
-			  return buildIdentityUrl(username);
-		  }
-
-		  private static Identifier buildIdentityUrl(string username)
-		  {
-			  // This sample Provider has a custom policy for normalizing URIs, which is that the whole
-			  // path of the URI be lowercase except for the first letter of the username.
-			  username = username.Substring(0, 1).ToUpperInvariant() + username.Substring(1).ToLowerInvariant();
-			  return new Uri(System.Web.HttpContext.Current.Request.Url, System.Web.HttpContext.Current.Response.ApplyAppPathModifier("~/OpenId/AskUser/" + username));
+			  return new Uri(System.Web.HttpContext.Current.Request.Url, System.Web.HttpContext.Current.Response.ApplyAppPathModifier("~/SSO/OpenId/AskUser/" + username));
 		  }
 
 		  public ActionResult AskUser()
