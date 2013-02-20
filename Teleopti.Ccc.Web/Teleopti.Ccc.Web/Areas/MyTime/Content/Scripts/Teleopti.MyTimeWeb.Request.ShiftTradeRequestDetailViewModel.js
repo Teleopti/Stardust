f4fa0444696c26d200e7c47ef43d3fa5d437910b
Henke <henrik.andersson@teleopti.com>
2013-02-20 10:51:41 +0100

    fixed timeline size for shifttrade details

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel.js
@@ -17,7 +17,7 @@ Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel = function (ajax) {
 		self.respondToRequest("Requests/ApproveShiftTrade/" + self.Id());
 		Teleopti.MyTimeWeb.Request.RequestDetail.FadeEditSection();
 	};
-	self.pixelPerMinute = ko.observable(60);
+	self.pixelPerMinute = ko.observable(0.3);
 	self.Deny = function () {
 		self.respondToRequest("Requests/DenyShiftTrade/" + self.Id());
 		Teleopti.MyTimeWeb.Request.RequestDetail.FadeEditSection();
@@ -41,7 +41,6 @@ Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel = function (ajax) {
 	self.hours = ko.observableArray();
 	self.hourWidth = ko.observable(10);
 	self.henkeTest = function () {
-		console.log('sdghjasdgj');
 		self.ajax.Ajax({
 			url: "Requests/ShiftTradeRequestSwapDetails/" + self.Id(),
 			dataType: "json",
@@ -53,7 +52,7 @@ Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel = function (ajax) {
 				self.hours.removeAll();
 				for (var i = 0; i < data.TimeLineHours.length; i++) {
 					console.log(data);
-					self.hours.push(new TimeLineHourViewModel(data,self));
+					self.hours.push(new TimeLineHourViewModel(data.TimeLineHours[i], self));
 				}
 			},
 			error: function (error) {
@@ -68,21 +67,16 @@ Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel = function (ajax) {
 //henke: separate class used by both addshifttrade and show shifttrade
 function TimeLineHourViewModel(hour, parentViewModel) {
 	var self = this;
-	var self.borderSize = 1;
+	 self.borderSize = 1;
 
-	self.hourText = hour.HourText;
+	 self.hourText = hour.HourText;
 	self.lengthInMinutes = hour.LengthInMinutesToDisplay;
 	self.leftPx = ko.observable('-8px');
 	
 	self.hourWidth = ko.computed(function () {
-		//return '20px';
 		return self.lengthInMinutes * parentViewModel.pixelPerMinute() - self.borderSize + 'px';
 	});
 
-	console.log(self.lengthInMinutes);
-	console.log(parentViewModel.pixelPerMinute());
-	console.log(self.borderSize);
-	console.log(self.hourWidth());
 }
 
 ko.utils.extend(Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel.prototype, {
