19c0c1978e905732a0dc66b4d27cdbe3476b3a07
Erik Sundberg <none@none>
2013-02-22 10:20:38 +0100

    Missed a closingbracket

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/RTA/UpdatedScheduleInfoConsumerTest.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/RTA/UpdatedScheduleInfoConsumerTest.cs
@@ -11,81 +11,82 @@ using Teleopti.Interfaces.Messages.Denormalize;
 
 namespace Teleopti.Ccc.Sdk.ServiceBusTest.Rta
 {
-    [TestFixture]
-    public class UpdatedScheduleInfoConsumerTest
-    {
-	//    private MockRepository mocks;
-	//    private IServiceBus serviceBus;
-	//    private IUnitOfWorkFactory unitOfWorkFactory;
-	//    private IUnitOfWork unitOfWork;
-	//    private IScheduleProjectionReadOnlyRepository scheduleProjectionReadOnlyRepository;
-	//    private UpdatedScheduleInfoConsumer target;
+	[TestFixture]
+	public class UpdatedScheduleInfoConsumerTest
+	{
+		//    private MockRepository mocks;
+		//    private IServiceBus serviceBus;
+		//    private IUnitOfWorkFactory unitOfWorkFactory;
+		//    private IUnitOfWork unitOfWork;
+		//    private IScheduleProjectionReadOnlyRepository scheduleProjectionReadOnlyRepository;
+		//    private UpdatedScheduleInfoConsumer target;
 
-	//    [SetUp]
-	//    public void Setup()
-	//    {
-	//        mocks = new MockRepository();
-	//        serviceBus = mocks.DynamicMock<IServiceBus>();
-	//        unitOfWorkFactory = mocks.DynamicMock<IUnitOfWorkFactory>();
-	//        unitOfWork = mocks.DynamicMock<IUnitOfWork>();
-	//        scheduleProjectionReadOnlyRepository = new ScheduleProjectionReadOnlyRepository(unitOfWorkFactory);
-	//        //mocks.DynamicMock<IScheduleProjectionReadOnlyRepository>();
+		//    [SetUp]
+		//    public void Setup()
+		//    {
+		//        mocks = new MockRepository();
+		//        serviceBus = mocks.DynamicMock<IServiceBus>();
+		//        unitOfWorkFactory = mocks.DynamicMock<IUnitOfWorkFactory>();
+		//        unitOfWork = mocks.DynamicMock<IUnitOfWork>();
+		//        scheduleProjectionReadOnlyRepository = new ScheduleProjectionReadOnlyRepository(unitOfWorkFactory);
+		//        //mocks.DynamicMock<IScheduleProjectionReadOnlyRepository>();
 
-	//        target = new UpdatedScheduleInfoConsumer(serviceBus, scheduleProjectionReadOnlyRepository, unitOfWorkFactory);
-	//    }
+		//        target = new UpdatedScheduleInfoConsumer(serviceBus, scheduleProjectionReadOnlyRepository, unitOfWorkFactory);
+		//    }
 
-	//    [Test]
-	//    public void IsPersonWithExternalLogOnConsumeSuccessfully()
-	//    {
-	//        //var person = PersonFactory.CreatePerson();
-	//        //person.SetId(Guid.NewGuid());
+		//    [Test]
+		//    public void IsPersonWithExternalLogOnConsumeSuccessfully()
+		//    {
+		//        //var person = PersonFactory.CreatePerson();
+		//        //person.SetId(Guid.NewGuid());
 
-	//        //var bussinessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit("TestBU");
-	//        //bussinessUnit.SetId(Guid.NewGuid());
-            
-	//        //var period = new DateTimePeriod(DateTime.UtcNow, DateTime.UtcNow);
+		//        //var bussinessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit("TestBU");
+		//        //bussinessUnit.SetId(Guid.NewGuid());
 
-	//        //var personInfoMessage = new PersonWithExternalLogOn();
-	//        //personInfoMessage.PersonId = person.Id.GetValueOrDefault();
-	//        //personInfoMessage.Timestamp = period.StartDateTime;
-	//        //personInfoMessage.BusinessUnitId = bussinessUnit.Id.GetValueOrDefault();
-	//        //personInfoMessage.Datasource = "DS";
+		//        //var period = new DateTimePeriod(DateTime.UtcNow, DateTime.UtcNow);
 
-	//        //Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+		//        //var personInfoMessage = new PersonWithExternalLogOn();
+		//        //personInfoMessage.PersonId = person.Id.GetValueOrDefault();
+		//        //personInfoMessage.Timestamp = period.StartDateTime;
+		//        //personInfoMessage.BusinessUnitId = bussinessUnit.Id.GetValueOrDefault();
+		//        //personInfoMessage.Datasource = "DS";
 
-	//        //mocks.ReplayAll();
-	//        //target.Consume(personInfoMessage);
-	//        //mocks.VerifyAll();
-	//    }
+		//        //Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
 
-	//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), Test]
-	//    public void DoNotSendDelayMessageIfThereIsNoNextActivity()
-	//    {
-            
-	//    }
+		//        //mocks.ReplayAll();
+		//        //target.Consume(personInfoMessage);
+		//        //mocks.VerifyAll();
+		//    }
 
-	//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), Test]
-	//    public void SendDelayMessageIfThereExistNextActivity()
-	//    {
-            
-	//    }
+		//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), Test]
+		//    public void DoNotSendDelayMessageIfThereIsNoNextActivity()
+		//    {
 
-	//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), Test]
-	//    public void IsUpdatedScheduleDayConsumeSuccessfully()
-	//    {
-           
-	//    }
+		//    }
 
-	//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
-	//    public void DoNotSendAnyMessageIfTheScheduleIsNotUpdatedWithinRange()
-	//    {
-            
-	//    }
+		//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), Test]
+		//    public void SendDelayMessageIfThereExistNextActivity()
+		//    {
 
-	//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
-	//    public void SendMessageIfTheScheduleIsNotUpdatedWithinRange()
-	//    {
-            
-	//    }
-	//}
+		//    }
+
+		//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic"), Test]
+		//    public void IsUpdatedScheduleDayConsumeSuccessfully()
+		//    {
+
+		//    }
+
+		//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+		//    public void DoNotSendAnyMessageIfTheScheduleIsNotUpdatedWithinRange()
+		//    {
+
+		//    }
+
+		//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+		//    public void SendMessageIfTheScheduleIsNotUpdatedWithinRange()
+		//    {
+
+		//    }
+		//}
+	}
 }
