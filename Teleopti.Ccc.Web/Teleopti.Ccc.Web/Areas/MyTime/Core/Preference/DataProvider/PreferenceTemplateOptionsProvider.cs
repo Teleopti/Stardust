b60482e409f9eb80e640320426ad3825e1517b0f
Kunning Mao <none@none>
2013-02-26 15:41:14 +0100

    add PreferenceTemplateOptionsProvider

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Preference/DataProvider/PreferenceTemplateOptionsProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Preference.DataProvider
+{
+	public class PreferenceTemplateOptionsProvider
+	{
+		private readonly ILoggedOnUser _loggedOnUser;
+		private readonly IExtendedPreferenceTemplateRepository _extendedPreferenceTemplateRepository;
+
+		public PreferenceTemplateOptionsProvider(ILoggedOnUser loggedOnUser, IExtendedPreferenceTemplateRepository extendedPreferenceTemplateRepository)
+		{
+			_loggedOnUser = loggedOnUser;
+			_extendedPreferenceTemplateRepository = extendedPreferenceTemplateRepository;
+		}
+
+		public IEnumerable<IExtendedPreferenceTemplate> RetrievePreferenceTemplateOptions()
+		{
+			var user = _loggedOnUser.CurrentUser();
+			return _extendedPreferenceTemplateRepository.FindByUser(user);
+		}
+	}
+}
