6850e0a788ede4f651928d6e9a3b37c214415baa
Xianwei Shen <xianwei.shen@teleopti.com>
2013-03-01 17:05:51 +0100

    refactor work shift fiters.

---
+++ Domain/Scheduling/WorkShiftFilters/TimeLimitationShiftFilter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.WorkShiftFilters
+{
+	public interface ITimeLimitationShiftFilter
+	{
+		IList<IShiftProjectionCache> Filter(IList<IShiftProjectionCache> shiftList, DateTimePeriod validPeriod, IWorkShiftFinderResult finderResult);
+	}
+
+	public class TimeLimitationShiftFilter : ITimeLimitationShiftFilter
+	{
+		public IList<IShiftProjectionCache> Filter(IList<IShiftProjectionCache> shiftList, DateTimePeriod validPeriod, IWorkShiftFinderResult finderResult)
+		{
+			var cntBefore = shiftList.Count;
+			IList<IShiftProjectionCache> workShiftsWithinPeriod = new List<IShiftProjectionCache>();
+			foreach (IShiftProjectionCache proj in shiftList)
+			{
+				var mainShiftPeriod = proj.TheMainShift.LayerCollection.Period();
+				if (mainShiftPeriod.HasValue)
+				{
+					if (validPeriod.Contains(mainShiftPeriod.Value))
+					{
+						workShiftsWithinPeriod.Add(proj);
+					}
+				}
+			}
+			finderResult.AddFilterResults(
+				new WorkShiftFilterResult(
+					string.Format(TeleoptiPrincipal.Current.Regional.Culture,
+								  UserTexts.Resources.FilterOnPersonalPeriodLimitationsWithParams,
+								  validPeriod.LocalStartDateTime, validPeriod.LocalEndDateTime), cntBefore,
+					workShiftsWithinPeriod.Count));
+
+			return workShiftsWithinPeriod;
+		}
+	}
+}
---
+++ DomainTest/Scheduling/WorkShiftFilters/ActivityRestrictionsShiftFilterTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Restriction;
+using Teleopti.Ccc.Domain.Scheduling.Restrictions;
+using Teleopti.Ccc.Domain.Scheduling.WorkShiftFilters;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Scheduling.WorkShiftFilters
+{
+	[TestFixture]
+	public class ActivityRestrictionsShiftFilterTest
+	{
+		private IActivityRestrictionsShiftFilter _target;
+		private MockRepository _mocks;
+		private IPersonalShiftMeetingTimeChecker _personalShiftMeetingTimeChecker;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mocks = new MockRepository();
+			_personalShiftMeetingTimeChecker = _mocks.StrictMock<IPersonalShiftMeetingTimeChecker>();
+			_target = new ActivityRestrictionsShiftFilter();
+		}
+
+		[Test]
+		public void ShouldFilterActivityRestriction()
+		{
+			var dateOnly = new DateOnly(2009, 2, 2);
+			var finderResult = new WorkShiftFinderResult(new Person(), new DateOnly(2009, 2, 3));
+			IActivity testActivity = ActivityFactory.CreateActivity("test");
+			IActivity breakActivity = ActivityFactory.CreateActivity("lunch");
+			var breakPeriod = new DateTimePeriod(new DateTime(1800, 1, 1, 12, 0, 0, DateTimeKind.Utc), new DateTime(2009, 2, 2, 13, 0, 0, DateTimeKind.Utc));
+
+
+			IWorkShift ws1 = WorkShiftFactory.CreateWorkShift(TimeSpan.FromHours(8), TimeSpan.FromHours(21), testActivity);
+			ws1.LayerCollection.Add(new WorkShiftActivityLayer(breakActivity, breakPeriod));
+			IWorkShift ws2 = WorkShiftFactory.CreateWorkShift(TimeSpan.FromHours(9), TimeSpan.FromHours(22), testActivity);
+			ws2.LayerCollection.Add(new WorkShiftActivityLayer(breakActivity, breakPeriod.MovePeriod(new TimeSpan(1, 0, 0))));
+			IList<IWorkShift> listOfWorkShifts = new List<IWorkShift> { ws1, ws2 };
+
+			var timeZoneInfo = (TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+			var casheList = new List<IShiftProjectionCache>();
+			foreach (IWorkShift shift in listOfWorkShifts)
+			{
+				var cache = new ShiftProjectionCache(shift, _personalShiftMeetingTimeChecker);
+				cache.SetDate(dateOnly, timeZoneInfo);
+				casheList.Add(cache);
+			}
+
+			IList<IActivityRestriction> activityRestrictions = new List<IActivityRestriction>();
+			IEffectiveRestriction effectiveRestriction = new EffectiveRestriction(new StartTimeLimitation(),
+																				  new EndTimeLimitation(),
+																				  new WorkTimeLimitation(), null, null, null,
+																				  activityRestrictions);
+			var ret = _target.Filter(dateOnly, timeZoneInfo, casheList, effectiveRestriction, finderResult);
+			Assert.AreEqual(2, ret.Count);
+
+			var activityRestriction = new ActivityRestriction(breakActivity)
+			{
+				StartTimeLimitation = new StartTimeLimitation(new TimeSpan(12, 0, 0), null),
+				EndTimeLimitation = new EndTimeLimitation(null, new TimeSpan(13, 0, 0))
+			};
+			activityRestrictions = new List<IActivityRestriction> { activityRestriction };
+			effectiveRestriction = new EffectiveRestriction(new StartTimeLimitation(),
+																				  new EndTimeLimitation(),
+																				  new WorkTimeLimitation(), null, null, null,
+																				  activityRestrictions);
+
+			ret = _target.Filter(dateOnly, timeZoneInfo, casheList, effectiveRestriction, finderResult);
+			Assert.AreEqual(0, ret.Count);
+		}
+	}
+}
