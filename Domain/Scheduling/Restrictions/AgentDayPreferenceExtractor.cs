b6ab933793e7026ec36691de8cd756c3b276de59
tamasb@teleopti696.toptinet.teleopti.com <tamasb@teleopti696.toptinet.teleopti.com>
2013-03-05 16:36:46 +0100

    Backed out changeset: 37150ba8a929
    --HG--
    branch : Release 7.3.374

---
+++ Domain/Scheduling/Restrictions/AgentDayPreferenceExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teleopti.Ccc.Domain.Scheduling.Restrictions
+{
+
+	/// <summary>
+	/// Extracts and creates AgentDayPreference class
+	/// </summary>
+	public interface IAgentDayPreferenceExtractor
+	{
+		AgentDayPreference Extract();
+	}
+
+	public class AgentDayPreferenceExtractor : IAgentDayPreferenceExtractor
+	{
+		public AgentDayPreference Extract()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
