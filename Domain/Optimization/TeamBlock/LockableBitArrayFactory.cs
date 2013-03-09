daf56df5d1e288c4b2ca478727c6144b0fe8e3f6
Micke <micke@teleopti.com>
2013-03-09 17:51:22 +0100

    Refact to new func

---
+++ Domain/Optimization/TeamBlock/LockableBitArrayFactory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Teleopti.Ccc.DayOffPlanning;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Optimization.TeamBlock
+{
+	public interface ILockableBitArrayFactory
+	{
+		ILockableBitArray ConvertFromMatrix(bool useWeekBefore, bool useWeekAfter, IScheduleMatrixPro matrix);
+	}
+
+	public class LockableBitArrayFactory : ILockableBitArrayFactory
+	{
+		public ILockableBitArray ConvertFromMatrix(bool useWeekBefore, bool useWeekAfter, IScheduleMatrixPro matrix)
+		{
+			if (!useWeekBefore && !useWeekAfter)
+				return arrayFromList(matrix.FullWeeksPeriodDays, false, false, matrix);
+
+			if (useWeekBefore && !useWeekAfter)
+				return arrayFromList(matrix.WeekBeforeOuterPeriodDays, true, false, matrix);
+
+			if (!useWeekBefore)
+				return arrayFromList(matrix.WeekAfterOuterPeriodDays, false, true, matrix);
+
+			return arrayFromList(matrix.OuterWeeksPeriodDays, true, true, matrix);
+		}
+
+		private ILockableBitArray arrayFromList(IList<IScheduleDayPro> list, bool useWeekBefore, bool useWeekAfter, IScheduleMatrixPro matrix)
+		{
+			ILockableBitArray ret = new LockableBitArray(list.Count, useWeekBefore, useWeekAfter, null);
+			int index = 0;
+			foreach (IScheduleDayPro scheduleDayPro in list)
+			{
+				SchedulePartView significant = scheduleDayPro.DaySchedulePart().SignificantPart();
+				ret.Set(index, ((significant == SchedulePartView.DayOff) || (significant == SchedulePartView.ContractDayOff)));
+				if (!matrix.UnlockedDays.Contains(scheduleDayPro))
+					ret.Lock(index, true);
+				if (scheduleDayPro.DaySchedulePart().SignificantPart() == SchedulePartView.FullDayAbsence)
+					ret.Lock(index, true);
+				index++;
+			}
+
+			int periodAreaStart = list.IndexOf(matrix.EffectivePeriodDays[0]);
+			int periodAreaEnd = list.IndexOf(matrix.EffectivePeriodDays[matrix.EffectivePeriodDays.Count - 1]);
+			ret.PeriodArea = new MinMax<int>(periodAreaStart, periodAreaEnd);
+
+			return ret;
+		}
+	}
+}
