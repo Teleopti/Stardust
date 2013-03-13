08c31d547e55c74e14b4226809cadcd23d69aac8
Roger Kratz <roger.kratz@teleopti.com>
2013-03-13 11:53:00 +0100

    failing test for 22600
    --HG--
    branch : Release 7.3.374

---
+++ InfrastructureTest/Bugs/Bug22600.cs
@@ -0,0 +1,102 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.InfrastructureTest.Helper;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.Bugs
+{
+	public class Bug22600 : DatabaseTest
+	{
+		private IGroupPage groupPage;
+		private IPerson personInRootGroup;
+		private IPerson personInChildGroup;
+
+		protected override void SetupForRepositoryTest()
+		{
+			personInRootGroup = PersonFactory.CreatePerson("Person1");
+			personInChildGroup = PersonFactory.CreatePerson("Person2");
+			groupPage = new GroupPage("Contract Basis Page");
+			var rootPersonGroup = new RootPersonGroup("unit1");
+			var childPersonGroup = new ChildPersonGroup("subUnit1");
+			groupPage.AddRootPersonGroup(rootPersonGroup);
+			rootPersonGroup.AddChildGroup(childPersonGroup);
+			rootPersonGroup.AddPerson(personInRootGroup);
+			childPersonGroup.AddPerson(personInChildGroup);
+
+			PersistAndRemoveFromUnitOfWork(personInRootGroup);
+			PersistAndRemoveFromUnitOfWork(personInChildGroup);
+			PersistAndRemoveFromUnitOfWork(groupPage);
+			UnitOfWork.PersistAll();
+		}
+
+		[Test]
+		public void MergeNoChangesShouldNotReturnAnyChanges()
+		{
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				groupPage = uow.Merge(groupPage);
+				uow.PersistAll().Should().Be.Empty();
+			}
+		}
+
+		[Test]
+		public void AddingPersonToRootGroupShouldGenerateOneChange()
+		{
+			groupPage.RootGroupCollection[0].AddPerson(personInChildGroup);
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				groupPage = uow.Merge(groupPage);
+				uow.PersistAll().Count().Should().Be.EqualTo(1);
+			}			
+		}
+
+		[Test]
+		public void AddingPersonToChildGroupShouldGenerateOneChange()
+		{
+			groupPage.RootGroupCollection[0].ChildGroupCollection[0].AddPerson(personInRootGroup);
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				groupPage = uow.Merge(groupPage);
+				uow.PersistAll().Count().Should().Be.EqualTo(1);
+			}
+		}
+
+		[Test]
+		public void RemovingPersonToRootGroupShouldGenerateOneChange()
+		{
+			groupPage.RootGroupCollection[0].RemovePerson(personInRootGroup);
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				groupPage = uow.Merge(groupPage);
+				uow.PersistAll().Count().Should().Be.EqualTo(1);
+			}
+		}
+
+		[Test]
+		public void RemovingPersonToChildGroupShouldGenerateOneChange()
+		{
+			groupPage.RootGroupCollection[0].ChildGroupCollection[0].RemovePerson(personInChildGroup);
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				groupPage = uow.Merge(groupPage);
+				uow.PersistAll().Count().Should().Be.EqualTo(1);
+			}
+		}
+
+		protected override void TeardownForRepositoryTest()
+		{
+			using (var uow = SetupFixtureForAssembly.DataSource.Application.CreateAndOpenUnitOfWork())
+			{
+				var rep = new Repository(uow);
+				rep.Remove(groupPage);
+				rep.Remove(personInChildGroup);
+				rep.Remove(personInRootGroup);
+				uow.PersistAll();
+			}
+		}
+	}
+}
