60156243aa508f66af3ad8a5e89204b3afc5f762
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-03-13 15:54:56 +0100

    fxcop

--- Infrastructure/ApplicationLayer/SyncCommandDispatcher.cs
+++ Infrastructure/ApplicationLayer/SyncCommandDispatcher.cs
@@ -9,6 +9,7 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 
 		public SyncCommandDispatcher(IComponentContext componentContext) { _componentContext = componentContext; }
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
 		public void Execute(object command)
 		{
 			var handlerType = typeof(IHandleCommand<>).MakeGenericType(new[] { command.GetType() });
