18159fc130aba42ee0cbe04483ef2d15973ef7a0
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-03-13 15:01:12 +0100

    added full day absence command handler

---
+++ Domain/ApplicationLayer/AddFullDayAbsenceCommandHandler.cs
@@ -0,0 +1,7 @@
+namespace Teleopti.Ccc.Domain.ApplicationLayer
+{
+	public class AddFullDayAbsenceCommandHandler : IHandleCommand<AddFullDayAbsenceCommand>
+	{
+		public void Handle(AddFullDayAbsenceCommand command) {  }
+	}
+}
---
+++ DomainTest/ApplicationLayer/AddFullDayAbsenceCommandHandlerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.ApplicationLayer;
+
+namespace Teleopti.Ccc.DomainTest.ApplicationLayer
+{
+	[TestFixture]
+	public class AddFullDayAbsenceCommandHandlerTest
+	{
+		[Test]
+		public void ShouldExist()
+		{
+			var target = new AddFullDayAbsenceCommandHandler();
+			target.Handle(new AddFullDayAbsenceCommand());
+		}
+	}
+}
