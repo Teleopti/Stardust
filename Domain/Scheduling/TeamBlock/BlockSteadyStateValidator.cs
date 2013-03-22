53be5dc8145a95b5ee192fdaaa9e9c57b70b5935
Asad MIrza <asad.mirza@teleopti.com>
2013-03-22 16:35:52 +0100

    Added new classes

---
+++ Domain/Scheduling/TeamBlock/BlockSteadyStateValidator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock
+{
+
+    public interface IBlockSteadyStateValidator
+    {
+        bool IsBlockInSteadyState(ITeamBlockInfo teamBlockInfo, ISchedulingOptions schedulingOptions);
+        bool IsBlockScheduled(ITeamBlockInfo teamBlockInfo);
+    }
+
+    public class BlockSteadyStateValidator : IBlockSteadyStateValidator
+    {
+        public bool IsBlockInSteadyState(ITeamBlockInfo teamBlockInfo, ISchedulingOptions schedulingOptions)
+        {
+            var dayList = teamBlockInfo.BlockInfo.BlockPeriod.DayCollection();
+            if (dayList.Count > 0)
+            {
+                var matrixList = teamBlockInfo.TeamInfo.MatrixesForGroupAndDate(dayList[0]).ToList();
+                if (matrixList.Any())
+                {
+                    var sampleScheduleDay = matrixList[0].GetScheduleDayByKey(dayList[0]).DaySchedulePart();
+                    if (schedulingOptions.UseLevellingSameStartTime)
+                        return verifySameStartTime(teamBlockInfo, dayList, sampleScheduleDay);
+                    if (schedulingOptions.UseLevellingSameShift)
+                        return verifySameShift(teamBlockInfo, dayList, sampleScheduleDay);
+                }
+            }
+            
+            return true;
+        }
+
+        public bool IsBlockScheduled(ITeamBlockInfo teamBlockInfo)
+        {
+            foreach (var day in teamBlockInfo.BlockInfo.BlockPeriod.DayCollection())
+                foreach (var matrix in teamBlockInfo.TeamInfo.MatrixesForGroupAndDate(day))
+                    if (!matrix.GetScheduleDayByKey(day).DaySchedulePart().IsScheduled())
+                        return false;
+            return true;
+        }
+
+        private bool verifySameStartTime(ITeamBlockInfo teamBlockInfo, IEnumerable<DateOnly> dayList, IScheduleDay sampleScheduleDay)
+        {
+            var dateTimePeriod = getShiftPeriod(sampleScheduleDay);
+            if (dateTimePeriod.HasValue)
+            {
+                var sampleStartTime = dateTimePeriod.Value.StartDateTimeLocal(sampleScheduleDay.TimeZone);
+                foreach (var day in dayList)
+                    foreach (var matrix in teamBlockInfo.TeamInfo.MatrixesForGroupAndDate(day))
+                    {
+                        var scheduleDay = matrix.GetScheduleDayByKey(day).DaySchedulePart();
+                        if (scheduleDay.IsScheduled())
+                        {
+                            var startDateTime = getStartTimeLocal(scheduleDay);
+                            if (startDateTime != sampleStartTime)
+                                return false;
+                        }
+
+                    }
+            }
+            
+            return true;
+        }
+
+        private DateTime getStartTimeLocal(IScheduleDay scheduleDay)
+        {
+            var dateTimePeriod = getShiftPeriod(scheduleDay);
+            if (dateTimePeriod.HasValue)
+            {
+                return dateTimePeriod.Value.StartDateTimeLocal(scheduleDay.TimeZone);
+            }
+            return DateTime.MinValue ;
+        }
+
+        private DateTimePeriod? getShiftPeriod(IScheduleDay scheduleDay)
+        {
+            var samplePersonAssignment = scheduleDay.AssignmentHighZOrder();
+            if (samplePersonAssignment != null && samplePersonAssignment.MainShift!=null)
+            {
+                return  samplePersonAssignment.MainShift.ProjectionService().CreateProjection().Period();
+            }
+            return null;
+        }
+
+        private bool verifySameShift(ITeamBlockInfo teamBlockInfo, IList<DateOnly> dayList, IScheduleDay sampleScheduleDay)
+        {
+            var equator = new ScheduleDayEquator();
+            foreach (var day in dayList)
+                foreach (var matrix in teamBlockInfo.TeamInfo.MatrixesForGroupAndDate(day))
+                {
+                    var scheduleDay = matrix.GetScheduleDayByKey(day).DaySchedulePart();
+                    //scheduleDay.AssignmentHighZOrder().MainShift.ProjectionService().CreateProjection().Period().Value.StartDateTimeLocal(schedulePart.TimeZone)
+                    if (scheduleDay.IsScheduled())
+                        if (!equator.MainShiftEquals(sampleScheduleDay, scheduleDay))
+                            return false;
+
+                }
+
+            return true;
+        }
+    }
+}
---
+++ DomainTest/Scheduling/TeamBlock/TeamBlockSteadyStateValidatorTest.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Scheduling.TeamBlock;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Scheduling.TeamBlock
+{
+    [TestFixture]
+    public class BlockSteadyStateValidatorTest
+    {
+        private MockRepository _mock;
+        private IBlockSteadyStateValidator _target;
+        private BaseLineData _baseLineData;
+        private ITeamInfo _teamInfo;
+        private IBlockInfo _blockInfo;
+        private IList<IScheduleMatrixPro> _matrixList;
+        private IScheduleMatrixPro _scheduleMatrixPro;
+        private IVirtualSchedulePeriod _virtualSchedulePeriod;
+        private DateOnly _today;
+        private IScheduleDayPro _todayScheduleDayPro;
+        private IScheduleDayPro _tomorrowScheduleDayPro;
+        private IScheduleDay _todayScheduleDay;
+        private IScheduleDay _tomorrowScheduleDay;
+        private DateOnly _tomorrow;
+        private DateOnly _dayAfterTomorrow;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mock = new MockRepository();
+            _target = new BlockSteadyStateValidator();
+            _baseLineData = new BaseLineData();
+            _matrixList = new List<IScheduleMatrixPro>();
+            _scheduleMatrixPro = _mock.StrictMock<IScheduleMatrixPro>();
+            _virtualSchedulePeriod = _mock.StrictMock<IVirtualSchedulePeriod>();
+            _todayScheduleDayPro = _mock.StrictMock<IScheduleDayPro>();
+            _tomorrowScheduleDayPro = _mock.StrictMock<IScheduleDayPro>();
+            _todayScheduleDay = _mock.StrictMock<IScheduleDay>();
+            _tomorrowScheduleDay = _mock.StrictMock<IScheduleDay>();
+            _today = new DateOnly();
+            _tomorrow = _today.AddDays(1);
+            _dayAfterTomorrow = _tomorrow.AddDays(1);
+        }
+
+        [Test]
+        public void ShouldReturnTrueForNonScheduledDays()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            var schedulingOptions =new SchedulingOptions();
+            schedulingOptions.UseLevellingSameShift = true;
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+
+                //day1
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(false);
+
+                //day2
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_tomorrow)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(false);
+
+                //day3
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_dayAfterTomorrow)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(false);
+
+            }
+            Assert.IsTrue(_target.IsBlockInSteadyState(teamBlockInfo,schedulingOptions));
+        }
+
+
+        [Test]
+        public void ShouldRerurnFalseIfAllAreNotSameStartTimeForSameStartTime()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            var schedulingOptions = new SchedulingOptions();
+            schedulingOptions.UseLevellingSameStartTime = true;
+            var now = DateTime.UtcNow;
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+
+                //day1
+                expectCallForDayForSameStartTime(_todayScheduleDay, _today, now, true);
+
+                //day2
+                expectCallForDayForSameStartTime(_tomorrowScheduleDay, _tomorrow, now.AddMinutes(30), true);
+
+                //day3
+                expectCallForDayForSameStartTime(_todayScheduleDay, _dayAfterTomorrow, now, true);
+
+            }
+            Assert.False(_target.IsBlockInSteadyState(teamBlockInfo,schedulingOptions));
+        }
+
+        [Test]
+        public void ShouldRerurnTrueIfAllAreSameShiftForSameShift()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            var schedulingOptions = new SchedulingOptions();
+            schedulingOptions.UseLevellingSameShift = true;
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+                //day1
+                expectCallForDayForSameShift(_todayScheduleDay, _today);
+
+                //day2
+                expectCallForDayForSameShift(_todayScheduleDay, _tomorrow);
+
+                //day3
+                expectCallForDayForSameShift(_todayScheduleDay, _dayAfterTomorrow);
+
+            }
+            Assert.IsTrue(_target.IsBlockInSteadyState(teamBlockInfo, schedulingOptions));
+        }
+
+        
+
+        [Test]
+        public void ShouldReturnTrueIfAllAreSameShiftForSameStartTime()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            var schedulingOptions = new SchedulingOptions();
+            schedulingOptions.UseLevellingSameStartTime  = true;
+            var now = DateTime.UtcNow;
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+
+                //day1
+                expectCallForDayForSameStartTime(_todayScheduleDay, _today, now, true);
+
+                //day2
+                expectCallForDayForSameStartTime(_todayScheduleDay, _tomorrow, now, true);
+
+                //day3
+                expectCallForDayForSameStartTime(_todayScheduleDay, _dayAfterTomorrow, now, true);
+
+            }
+            Assert.IsTrue(_target.IsBlockInSteadyState(teamBlockInfo, schedulingOptions));
+        }
+
+        [Test]
+        public void ShouldReturnTrueIfSomeAreSameShiftForSameStartTime()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            var schedulingOptions = new SchedulingOptions();
+            schedulingOptions.UseLevellingSameStartTime = true;
+            var now = DateTime.UtcNow;
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay);
+
+                //day1
+                expectCallForDayForSameStartTime(_todayScheduleDay, _today, now, true);
+
+                //day2
+                expectCallForDayForSameStartTime(_todayScheduleDay, _tomorrow, now,false);
+
+                //day3
+                expectCallForDayForSameStartTime(_todayScheduleDay, _dayAfterTomorrow, now, true);
+
+            }
+            Assert.IsTrue(_target.IsBlockInSteadyState(teamBlockInfo, schedulingOptions));
+        }
+
+        [Test]
+        public void ReturnTrueForScheduledBlock()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce().Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay).Repeat.AtLeastOnce();
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(true).Repeat.AtLeastOnce();
+                
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_tomorrow )).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay).Repeat.AtLeastOnce();
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(true).Repeat.AtLeastOnce();
+                
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_dayAfterTomorrow )).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay).Repeat.AtLeastOnce();
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(true).Repeat.AtLeastOnce();
+            }
+            Assert.IsTrue(_target.IsBlockScheduled(teamBlockInfo));
+        }
+
+        [Test]
+        public void ReturnFalseForSomeScheduledBlock()
+        {
+            _matrixList = new List<IScheduleMatrixPro>();
+            _matrixList.Add(_scheduleMatrixPro);
+            _teamInfo = new TeamInfo(_baseLineData.GroupPerson, new List<IList<IScheduleMatrixPro>> { _matrixList });
+            _blockInfo = new BlockInfo(new DateOnlyPeriod(_today, _today.AddDays(2)));
+            var teamBlockInfo = new TeamBlockInfo(_teamInfo, _blockInfo);
+            using (_mock.Record())
+            {
+                Expect.Call(_scheduleMatrixPro.SchedulePeriod).Return(_virtualSchedulePeriod).Repeat.AtLeastOnce();
+                Expect.Call(_virtualSchedulePeriod.DateOnlyPeriod)
+                      .Return(new DateOnlyPeriod(_today, _today.AddDays(2))).Repeat.AtLeastOnce().Repeat.AtLeastOnce();
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_today)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay).Repeat.AtLeastOnce();
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(true).Repeat.AtLeastOnce();
+
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_tomorrow)).Return(_tomorrowScheduleDayPro);
+                Expect.Call(_tomorrowScheduleDayPro.DaySchedulePart()).Return(_tomorrowScheduleDay).Repeat.AtLeastOnce();
+                Expect.Call(_tomorrowScheduleDay.IsScheduled()).Return(false).Repeat.AtLeastOnce();
+
+                Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(_dayAfterTomorrow)).Return(_todayScheduleDayPro);
+                Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(_todayScheduleDay).Repeat.AtLeastOnce();
+                Expect.Call(_todayScheduleDay.IsScheduled()).Return(true).Repeat.AtLeastOnce();
+            }
+            Assert.IsFalse(_target.IsBlockScheduled(teamBlockInfo));
+        }
+
+        private void expectCallForDayForSameShift(IScheduleDay scheduleDay, DateOnly dateOnly)
+        {
+            IList<IPersonDayOff> list = new List<IPersonDayOff>();
+            ReadOnlyCollection<IPersonDayOff> personDayOffCollection = new ReadOnlyCollection<IPersonDayOff>(list);
+            IList<IPersonAssignment> personAssignment = new List<IPersonAssignment>();
+            ReadOnlyCollection<IPersonAssignment> personAssignmentList = new ReadOnlyCollection<IPersonAssignment>(personAssignment);
+
+            Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(dateOnly)).Return(_todayScheduleDayPro);
+            Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(scheduleDay);
+            Expect.Call(scheduleDay.IsScheduled()).Return(true);
+
+            Expect.Call(scheduleDay.PersonDayOffCollection()).Return(personDayOffCollection).Repeat.AtLeastOnce();
+            Expect.Call(scheduleDay.SignificantPart()).Return(SchedulePartView.MainShift).Repeat.AtLeastOnce();
+            Expect.Call(scheduleDay.PersonAssignmentCollection()).Return(personAssignmentList);
+        }
+
+        private void expectCallForDayForSameStartTime(IScheduleDay scheduleDay, DateOnly dateOnly, DateTime startTime, bool isScheduled)
+        {
+            var mainShift = _mock.StrictMock<IMainShift>();
+            IProjectionService projectionService = _mock.StrictMock<IProjectionService>();
+            var personAssignment = _mock.StrictMock<IPersonAssignment>();
+            IVisualLayerCollection visualLayerCollection = _mock.StrictMock<IVisualLayerCollection>();
+
+            Expect.Call(scheduleDay.AssignmentHighZOrder()).Return(personAssignment).Repeat.AtLeastOnce();
+            Expect.Call(personAssignment.MainShift).Return(mainShift).Repeat.AtLeastOnce();
+            Expect.Call(mainShift.ProjectionService()).Return(projectionService).Repeat.AtLeastOnce();
+            Expect.Call(projectionService.CreateProjection()).Return(visualLayerCollection).Repeat.AtLeastOnce();
+            Expect.Call(visualLayerCollection.Period()).Return(new DateTimePeriod(startTime, startTime.AddHours(1))).Repeat.AtLeastOnce();
+            Expect.Call(scheduleDay.TimeZone).Return(TimeZoneInfo.Local).Repeat.AtLeastOnce();
+            Expect.Call(_scheduleMatrixPro.GetScheduleDayByKey(dateOnly)).Return(_todayScheduleDayPro);
+            Expect.Call(_todayScheduleDayPro.DaySchedulePart()).Return(scheduleDay);
+            Expect.Call(scheduleDay.IsScheduled()).Return(isScheduled);
+
+        }
+    }
+
+}
