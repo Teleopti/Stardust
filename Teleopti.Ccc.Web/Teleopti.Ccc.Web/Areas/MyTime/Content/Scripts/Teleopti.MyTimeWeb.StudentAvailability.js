24e232d3424d658c6ee02660c7dbca5a2894dffd
Robin Karlsson <robin.karlsson@teleopti.com>
2013-03-25 11:38:02 +0100

    Student availability almost working

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.StudentAvailability.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.StudentAvailability.js
@@ -77,60 +77,54 @@ Teleopti.MyTimeWeb.StudentAvailability = (function ($) {
 
 		editFormViewModel = new Teleopti.MyTimeWeb.StudentAvailability.EditFormViewModel();
 
+		editButton.click(function (e) { e.preventDefault(); });
 		studentAvailabilityToolTip = $('<div/>')
-			.qtip({
-				id: "edit-student-availability",
-				content: {
-					text: template,
-					title: {
-						text: '&nbsp;',
-						button: 'Close'
-					}
-				},
-				position: {
-					target: editButton,
-					my: "left top",
-					at: "left bottom",
-					adjust: {
-						x: 11,
-						y: 0
-					}
-				},
-				show: {
-					target: editButton,
-					event: 'click'
-				},
-				hide: {
-					target: editButton,
-					event: 'click'
-				},
-				style: {
-					def: false,
-					classes: 'ui-tooltip-custom ui-tooltip-rounded ui-tooltip-shadow',
-					tip: {
-						corner: "top left"
-					}
-				},
-				events: {
-					render: function () {
-						$('#Student-availability-reset')
-							.click(function () {
-								editFormViewModel.reset();
-							});
-						$('#Student-availability-apply')
-							.click(function () {
-								_setStudentAvailability(ko.toJS(editFormViewModel));
-							});
-						ko.applyBindings(editFormViewModel, template[0]);
-					}
-				}
-			});
+	        .qtip({
+	            id: "edit-student-availability",
+	            content: {
+	                text: template,
+	                title: {
+	                    text: '&nbsp;',
+	                    button: 'Close'
+	                }
+	            },
+	            position: {
+	                target: editButton,
+	                my: "left top",
+	                at: "left bottom",
+	                adjust: {
+	                    x: 11,
+	                    y: 0
+	                }
+	            },
+	            show: {
+	                target: editButton,
+	                event: 'mousedown'
+	            },
+	            hide: {
+	                target: editButton,
+	                event: 'mousedown'
+	            },
+	            style: {
+	                def: false,
+	                classes: 'ui-tooltip-custom ui-tooltip-rounded ui-tooltip-shadow',
+	                tip: {
+	                    corner: "top left"
+	                }
+	            },
+	            events: {
+	                render: function() {
+	                    ko.applyBindings(editFormViewModel, template[0]);
+	                }
+	            }
+	        });
 		editButton.removeAttr('disabled');
 
 		var deleteButton = $('#StudentAvailability-delete-button');
 		deleteButton.removeAttr('disabled');
-		deleteButton.click(function () {
-			_deleteStudentAvailability();
+		deleteButton.click(function (e) {
+		    e.preventDefault();
+		    _deleteStudentAvailability();
 		});
 	}
 
@@ -176,7 +170,6 @@ Teleopti.MyTimeWeb.StudentAvailability = (function ($) {
 
 	return {
 		Init: function () {
-			_initToolbarButtons();
 			Teleopti.MyTimeWeb.Portal.RegisterPartialCallBack(
 				'StudentAvailability/Index',
 				Teleopti.MyTimeWeb.StudentAvailability.StudentAvailabilityPartialInit,
@@ -187,6 +180,7 @@ Teleopti.MyTimeWeb.StudentAvailability = (function ($) {
 			if (!$('#StudentAvailability-body').length) {
 				return;
 			}
+			_initToolbarButtons();
 			_initPeriodSelection();
 			_initViewModels();
 			_activateSelectable();
@@ -194,6 +188,9 @@ Teleopti.MyTimeWeb.StudentAvailability = (function ($) {
 		StudentAvailabilityPartialDispose: function () {
 			studentAvailabilityToolTip.qtip('toggle', false);
 			ajax.AbortAll();
+		},
+		SetStudentAvailability: function(studentAvailabilityViewModel) {
+		    _setStudentAvailability(ko.toJS(studentAvailabilityViewModel));
 		}
 	};
 
