348266c361c08046f6c4bbb5c1915923ed20a656
Henke <henrik.andersson@teleopti.com>
2013-03-26 12:23:41 +0100

    fxcop

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Common/DataProvider/AllowanceRandomProvider.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Common/DataProvider/AllowanceRandomProvider.cs
@@ -7,11 +7,11 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Common.DataProvider
 {
 	public class AllowanceRandomProvider : IAllowanceProvider
 	{
-
-		IEnumerable<IAllowanceDay> IAllowanceProvider.GetAllowanceForPeriod(DateOnlyPeriod period)
+		public IEnumerable<IAllowanceDay> GetAllowanceForPeriod(DateOnlyPeriod period)
 		{
 			var random = new Random();
 			return period.DayCollection().Select(day => new AllowanceDay() { Date = day, Allowance = random.Next(1, 100) }).Cast<IAllowanceDay>().ToList();
 		}
+
 	}
 }
