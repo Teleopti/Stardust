12ac0d9cc64431400821d5fb1be6decb169d073e
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-03-29 11:43:26 +0100

    events from package is now send to bus aswell

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus/ApplicationLayer/EventsConsumer.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus/ApplicationLayer/EventsConsumer.cs
@@ -1,20 +1,31 @@
 ﻿using Rhino.ServiceBus;
 using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Sdk.ServiceBus.ApplicationLayer
 {
-	public class EventsConsumer : ConsumerOf<IEvent>
+	public class EventsConsumer : 
+		ConsumerOf<IEvent>,
+		ConsumerOf<EventsPackageMessage>
 	{
 		private readonly IEventPublisher _publisher;
+		private readonly IServiceBus _bus;
 
-		public EventsConsumer(IEventPublisher publisher) {
+		public EventsConsumer(IEventPublisher publisher, IServiceBus bus)
+		{
 			_publisher = publisher;
+			_bus = bus;
 		}
 
 		public void Consume(IEvent message)
 		{
 			_publisher.Publish(message);
+		}
+
+		public void Consume(EventsPackageMessage message)
+		{
+			_bus.Send(message.Events);
 		}
 	}
 }
--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/ApplicationLayer/EventsConsumerTest.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/ApplicationLayer/EventsConsumerTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using Rhino.Mocks;
 using Rhino.ServiceBus;
 using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Ccc.IocCommon.Configuration;
 using Teleopti.Ccc.Sdk.ServiceBus;
 using Teleopti.Ccc.Sdk.ServiceBus.ApplicationLayer;
@@ -20,12 +21,24 @@ namespace Teleopti.Ccc.Sdk.ServiceBusTest.ApplicationLayer
 		public void ShouldPublishEvents()
 		{
 			var publisher = MockRepository.GenerateMock<IEventPublisher>();
-			var target = new EventsConsumer(publisher);
+			var target = new EventsConsumer(publisher, null);
 			var @event = new Event();
 
 			target.Consume(@event);
 
 			publisher.AssertWasCalled(x => x.Publish(@event));
+		}
+
+		[Test]
+		public void ShouldSendEventsFromPackageMessage()
+		{
+			var bus = MockRepository.GenerateMock<IServiceBus>();
+			var target = new EventsConsumer(null, bus);
+			var message = new EventsPackageMessage {Events = new[] {new Event()}};
+
+			target.Consume(message);
+
+			bus.AssertWasCalled(x => x.Send(message.Events));
 		}
 
 	}
