29bdbe7590275b49b36f6f79d250772b5ecab766
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-03-29 22:48:30 +0100

    a test for coverage

---
+++ InfrastructureTest/ApplicationLayer/AutofacResolveTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Infrastructure.ApplicationLayer;
+
+namespace Teleopti.Ccc.InfrastructureTest.ApplicationLayer
+{
+	[TestFixture]
+	public class AutofacResolveTest
+	{
+		[Test]
+		public void ShouldBeCovered()
+		{
+			var target = new AutofacResolve(MockRepository.GenerateMock<IComponentContext>());
+			try
+			{
+				target.Resolve(typeof (object));
+			}
+			catch (Exception)
+			{
+				
+			}
+		}
+	}
+}
