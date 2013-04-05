acf2b64452d98823b34f6043365b56d2a51a6eda
Talha Muhammad <talha.muhammad@teleopti.com>
2013-04-05 11:21:33 +0200

    Fixed FxCop

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IBudgetGroupAllowanceCalculator.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IBudgetGroupAllowanceCalculator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Teleopti.Interfaces.Domain
+﻿namespace Teleopti.Interfaces.Domain
 {
     /// <summary>
     /// 
@@ -17,6 +12,11 @@ namespace Teleopti.Interfaces.Domain
         /// <returns></returns>
         string CheckBudgetGroup(IAbsenceRequest absenceRequest);
 
+        /// <summary>
+        /// This will return validation error for Budget Group Head Count option in WCS.
+        /// </summary>
+        /// <param name="absenceRequest"></param>
+        /// <returns></returns>
         string CheckHeadCountInBudgetGroup(IAbsenceRequest absenceRequest);
     }
 }
