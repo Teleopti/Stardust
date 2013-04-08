7f9ef733cf09ae0d39f587eefaab1554da4e7b3d
Claes Stenman <claes.stenman@teleopti.com>
2013-04-08 12:41:02 +0200

    Preference day creator...

--- Teleopti.Ccc.WinCode/Scheduling/AgentPreferenceDayCreator.cs
+++ Teleopti.Ccc.WinCode/Scheduling/AgentPreferenceDayCreator.cs
@@ -1,20 +1,137 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Teleopti.Ccc.Domain.Scheduling.Restriction;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCode.Scheduling
 {
 	public interface  IAgentPreferenceDayCreator
 	{
-		IAgentPreferenceCanCreateResult CanCreate(TimeSpan? minStart, TimeSpan? maxStart, TimeSpan? minEnd, TimeSpan? maxEnd, TimeSpan? minLength, TimeSpan? maxLength, bool endNextDay)
+		IPreferenceDay Create(IScheduleDay scheduleDay, TimeSpan? minStart, TimeSpan? maxStart, TimeSpan? minEnd, TimeSpan? maxEnd, TimeSpan? minLength, TimeSpan? maxLength, bool endNextDay);
+		IAgentPreferenceCanCreateResult CanCreate(TimeSpan? minStart, TimeSpan? maxStart, TimeSpan? minEnd, TimeSpan? maxEnd, TimeSpan? minLength, TimeSpan? maxLength, bool endNextDay);
 	}
 
 	public class AgentPreferenceDayCreator : IAgentPreferenceDayCreator
 	{
+		public IPreferenceDay Create(IScheduleDay scheduleDay, TimeSpan? minStart, TimeSpan? maxStart, TimeSpan? minEnd, TimeSpan? maxEnd, TimeSpan? minLength, TimeSpan? maxLength, bool endNextDay)
+		{
+			if(scheduleDay == null) throw new ArgumentNullException("scheduleDay");
+
+			var result = CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, endNextDay);
+			if (!result.Result) return null;
+
+			var restriction = new PreferenceRestriction();
+			restriction.StartTimeLimitation = new StartTimeLimitation(minStart, maxStart);
+			restriction.EndTimeLimitation = new EndTimeLimitation(minEnd, maxEnd);
+			restriction.WorkTimeLimitation = new WorkTimeLimitation(minLength, maxLength);
+			var preferenceDay = new PreferenceDay(scheduleDay.Person, scheduleDay.DateOnlyAsPeriod.DateOnly, restriction);
+
+			return preferenceDay;
+		}
+
 		public IAgentPreferenceCanCreateResult CanCreate(TimeSpan? minStart, TimeSpan? maxStart, TimeSpan? minEnd, TimeSpan? maxEnd, TimeSpan? minLength, TimeSpan? maxLength, bool endNextDay)
 		{
-			throw new NotImplementedException();
+			var result = new AgentPreferenceCanCreateResult();
+
+			if(endNextDay && maxEnd != null)
+				maxEnd = maxEnd.Value.Add(TimeSpan.FromDays(1));
+
+			if (minStart != null && maxStart != null)
+			{
+				if (minStart.Value > maxStart.Value)
+				{
+					result.Result = false;
+					result.StartTimeMinError = true;
+					return result;
+				}
+			}
+
+			if (minEnd != null && maxEnd != null)
+			{
+				if (minEnd.Value > maxEnd.Value)
+				{
+					result.Result = false;
+					result.EndTimeMinError = true;
+					return result;
+				}
+			}
+
+			if (minLength != null && maxLength != null)
+			{
+				if (minLength.Value > maxLength.Value)
+				{
+					result.Result = false;
+					result.LengthMinError = true;
+					return result;
+				}
+			}
+
+			if (minStart != null)
+			{
+				if (minEnd != null && minStart.Value >= minEnd.Value )
+				{
+					result.Result = false;
+					result.StartTimeMinError = true;
+					return result;	
+				}
+
+				if (maxEnd != null && minStart.Value >= maxEnd.Value)
+				{
+					result.Result = false;
+					result.StartTimeMinError = true;
+					return result;		
+				}
+			}
+
+			if (maxStart != null)
+			{
+				if (minEnd != null && maxStart.Value >= minEnd.Value)
+				{
+					result.Result = false;
+					result.StartTimeMaxError = true;
+					return result;
+				}
+
+				if (maxEnd != null && maxStart.Value >= maxEnd.Value)
+				{
+					result.Result = false;
+					result.StartTimeMaxError = true;
+					return result;
+				}
+			}
+
+			if (minLength != null)
+			{
+				if (minStart != null && maxEnd != null)
+				{
+					var maximumLength = maxEnd.Value.Subtract(minStart.Value);
+
+					if (minLength > maximumLength)
+					{
+						result.Result = false;
+						result.LengthMinError = true;
+						return result;
+					}
+				}				
+			}
+
+			if (maxLength != null)
+			{
+				if (maxStart != null && minEnd != null)
+				{
+					var minumumLength = minEnd.Value.Subtract(maxStart.Value);
+					{
+						if (maxLength < minumumLength)
+						{
+							result.Result = false;
+							result.LengthMaxError = true;
+							return result;
+						}
+					}
+				}
+			}
+
+			result.Result = true;
+			return result;
 		}
 	}
 }
--- Teleopti.Ccc.WinCodeTest/Scheduler/AgentPreferenceDayCreatorTest.cs
+++ Teleopti.Ccc.WinCodeTest/Scheduler/AgentPreferenceDayCreatorTest.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.WinCode.Scheduling;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCodeTest.Scheduler
 {
@@ -12,12 +11,75 @@ namespace Teleopti.Ccc.WinCodeTest.Scheduler
 	public class AgentPreferenceDayCreatorTest
 	{
 		private AgentPreferenceDayCreator _preferenceDayCreator;
+		private MockRepository _mock;
+		private IPerson _person;
+		private DateOnly _dateOnly;
+		private IScheduleDay _scheduleDay;
+		private IDateOnlyAsDateTimePeriod _dateOnlyAsDateTimePeriod;
 
 	
 		[SetUp]
 		public void Setup()
 		{
-			_preferenceDayCreator = new AgentPreferenceDayCreator();	
+			_mock = new MockRepository();
+			_scheduleDay = _mock.StrictMock<IScheduleDay>();
+			_preferenceDayCreator = new AgentPreferenceDayCreator();
+			_person = new Person();
+			_dateOnly = new DateOnly(2013, 1, 1);
+			_scheduleDay = _mock.StrictMock<IScheduleDay>();
+			_dateOnlyAsDateTimePeriod = _mock.StrictMock<IDateOnlyAsDateTimePeriod>();
+		}
+
+		[Test]
+		public void ShouldCreate()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			using (_mock.Record())
+			{
+				Expect.Call(_scheduleDay.Person).Return(_person);
+				Expect.Call(_scheduleDay.DateOnlyAsPeriod).Return(_dateOnlyAsDateTimePeriod);
+				Expect.Call(_dateOnlyAsDateTimePeriod.DateOnly).Return(_dateOnly);	
+			}
+
+			using (_mock.Playback())
+			{
+				var result = _preferenceDayCreator.Create(_scheduleDay, minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+				Assert.IsNotNull(result);
+				var restriction = result.Restriction;
+				Assert.IsNotNull(restriction);
+				Assert.AreEqual(minStart, restriction.StartTimeLimitation.StartTime);
+				Assert.AreEqual(maxStart,restriction.StartTimeLimitation.EndTime);
+				Assert.AreEqual(minEnd, restriction.EndTimeLimitation.StartTime);
+				Assert.AreEqual(maxEnd, restriction.EndTimeLimitation.EndTime);
+				Assert.AreEqual(minLength, restriction.WorkTimeLimitation.StartTime);
+				Assert.AreEqual(maxLength, restriction.WorkTimeLimitation.EndTime);
+			}	
+		}
+
+		[Test]
+		public void ShouldReturnNullWhenCantCreate()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(2);
+			TimeSpan? maxStart = TimeSpan.FromHours(1);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.Create(_scheduleDay, minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+			Assert.IsNull(result);
+		}
+
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void ShouldThrowExceptionOnEmptyDay()
+		{
+			_preferenceDayCreator.Create(null, null, null, null, null, null, null, false);	
 		}
 
 		[Test]
@@ -39,6 +101,389 @@ namespace Teleopti.Ccc.WinCodeTest.Scheduler
 			Assert.IsFalse(result.EndTimeMaxError);
 			Assert.IsFalse(result.LengthMinError);
 			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldHandleEmptyStartTimeMin()
+		{
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(null, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldHandleEmptyStartTimeMax()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, null, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldHandleEmptyStartTime()
+		{
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(null, null, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldDetectStartTimeMinGreaterThanStartTimeMax()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(2);
+			TimeSpan? maxStart = TimeSpan.FromHours(1);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsTrue(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldHandleEmptyEndTimeMin()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, null, maxEnd, minLength, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldHandleEmptyEndTimeMax()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, null, minLength, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldHandleEmptyEndTime()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, null, null, minLength, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldDetectEndTimeMinGreaterThanEndTimeMax()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(4);
+			TimeSpan? maxEnd = TimeSpan.FromHours(3);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsTrue(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldHandleEndTimeNextDay()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(4);
+			TimeSpan? maxEnd = TimeSpan.FromHours(3);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, true);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldHandleEmptyMinLength()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, null, maxLength, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldHandleEmptyMaxLength()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, null, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldHandleEmptyLength()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, null, null, false);
+
+			Assert.IsTrue(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldDetectMinLengthGreaterThanMaxLength()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(3);
+			TimeSpan? maxLength = TimeSpan.FromHours(2);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsTrue(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);	
+		}
+
+		[Test]
+		public void ShouldDetectStartTimeMinGreaterThanEndTimeMin()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(3);
+			TimeSpan? maxStart = TimeSpan.FromHours(4);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsTrue(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);		
+		}
+
+		[Test]
+		public void ShouldDetectStartTimeMinGreaterThanEndTimeMax()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(4);
+			TimeSpan? maxStart = TimeSpan.FromHours(5);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsTrue(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldDetectStartTimeMaxGreaterThanEndTimeMin()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(2);
+			TimeSpan? maxStart = TimeSpan.FromHours(4);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsTrue(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldDetectStartTimeMaxGreaterThanEndTimeMax()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(2);
+			TimeSpan? maxStart = TimeSpan.FromHours(4);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(2);
+			TimeSpan? maxLength = TimeSpan.FromHours(3);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, null, maxEnd, minLength, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsTrue(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);
+		}
+
+		[Test]
+		public void ShouldDetectCantFulfillMinLength()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(3);
+			TimeSpan? maxEnd = TimeSpan.FromHours(4);
+			TimeSpan? minLength = TimeSpan.FromHours(5);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, maxEnd, minLength, null, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsTrue(result.LengthMinError);
+			Assert.IsFalse(result.LengthMaxError);		
+		}
+
+		[Test]
+		public void ShouldDetectCantFulfillMaxLength()
+		{
+			TimeSpan? minStart = TimeSpan.FromHours(1);
+			TimeSpan? maxStart = TimeSpan.FromHours(2);
+			TimeSpan? minEnd = TimeSpan.FromHours(6);
+			TimeSpan? maxLength = TimeSpan.FromHours(1);
+
+			var result = _preferenceDayCreator.CanCreate(minStart, maxStart, minEnd, null, null, maxLength, false);
+
+			Assert.IsFalse(result.Result);
+			Assert.IsFalse(result.StartTimeMinError);
+			Assert.IsFalse(result.StartTimeMaxError);
+			Assert.IsFalse(result.EndTimeMinError);
+			Assert.IsFalse(result.EndTimeMaxError);
+			Assert.IsFalse(result.LengthMinError);
+			Assert.IsTrue(result.LengthMaxError);	
 		}
 	}
 }
