25eaa8e0a9161d3f5fdb7239bc01b7baa6f870a5
Roger Kratz <roger.kratz@teleopti.com>
2013-04-08 11:33:05 +0200

    22959: cleaning up

--- Infrastructure/Repositories/RepositoryNonGeneric.cs
+++ Infrastructure/Repositories/RepositoryNonGeneric.cs
@@ -7,93 +7,114 @@ using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Infrastructure.Repositories
 {
-    /// <summary>
-    /// Base class for all repositories. 
-    /// Non generic methods are here
-    /// </summary>
-    /// <remarks>
-    /// Created by: rogerkr
-    /// Created date: 2007-12-21
-    /// </remarks>
-    public class Repository : IRepository
-    {
-	    private readonly ICurrentUnitOfWork _currentUnitOfWork;
-	    private readonly IUnitOfWork _uow;
-        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+	/// <summary>
+	/// Base class for all repositories. 
+	/// Non generic methods are here
+	/// </summary>
+	/// <remarks>
+	/// Created by: rogerkr
+	/// Created date: 2007-12-21
+	/// </remarks>
+	public class Repository : IRepository
+	{
+		private readonly ICurrentUnitOfWork _currentUnitOfWork;
 
-        //old way - avoid using this one
-        public Repository(IUnitOfWork unitOfWork)
-        {
-            InParameter.NotNull("unitOfWork", unitOfWork);
-            _uow = unitOfWork;
-        }
+		//old way - avoid using this one
+		public Repository(IUnitOfWork unitOfWork)
+		{
+			InParameter.NotNull("unitOfWork", unitOfWork);
+			_currentUnitOfWork = new fixedCurrentUnitOfWork(unitOfWork);
+		}
+		private class fixedCurrentUnitOfWork : ICurrentUnitOfWork
+		{
+			private readonly IUnitOfWork _currentUnitOfWork;
 
-        //old way - don't use this one
-        public Repository(IUnitOfWorkFactory unitOfWorkFactory)
-        {
-            _unitOfWorkFactory = unitOfWorkFactory;
-        }
+			public fixedCurrentUnitOfWork(IUnitOfWork currentUnitOfWork)
+			{
+				_currentUnitOfWork = currentUnitOfWork;
+			}
 
-			//new, safe way
-	    public Repository(ICurrentUnitOfWork currentUnitOfWork)
-	    {
-		    _currentUnitOfWork = currentUnitOfWork;
-	    }
+			public IUnitOfWork Current()
+			{
+				return _currentUnitOfWork;
+			}
+		}
+
+		//old way - don't use this one
+		public Repository(IUnitOfWorkFactory unitOfWorkFactory)
+		{
+			_currentUnitOfWork = new justForBackwardCompability(unitOfWorkFactory);
+		}
+		private class justForBackwardCompability : ICurrentUnitOfWork
+		{
+			private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+			public justForBackwardCompability(IUnitOfWorkFactory unitOfWorkFactory)
+			{
+				_unitOfWorkFactory = unitOfWorkFactory;
+			}
+
+			public IUnitOfWork Current()
+			{
+				return _unitOfWorkFactory.CurrentUnitOfWork();
+			}
+		}
+
+		//new, safe way
+		public Repository(ICurrentUnitOfWork currentUnitOfWork)
+		{
+			_currentUnitOfWork = currentUnitOfWork;
+		}
 
 
-	    public IUnitOfWork UnitOfWork
-        {
-            get 
-            {
-							//fix this
-	            if (_uow != null)
-		            return _uow;
-	            if (_unitOfWorkFactory != null)
-		            return _unitOfWorkFactory.CurrentUnitOfWork();
-	            return _currentUnitOfWork.Current();
-            }
-        }
+		public IUnitOfWork UnitOfWork
+		{
+			get
+			{
+				return _currentUnitOfWork.Current();
+			}
+		}
 
-        public virtual void Add(IAggregateRoot root)
-        {
-            Session.SaveOrUpdate(root);
-        }
+		public virtual void Add(IAggregateRoot root)
+		{
+			Session.SaveOrUpdate(root);
+		}
 
-        public virtual void Remove(IAggregateRoot root)
-        {
-            IDeleteTag delRootInfo = root as IDeleteTag;
-            if (delRootInfo == null)
-            {
-                Session.Delete(root);
-            }
-            else
-            {
-                if (!UnitOfWork.Contains(root))
-                {
+		public virtual void Remove(IAggregateRoot root)
+		{
+			IDeleteTag delRootInfo = root as IDeleteTag;
+			if (delRootInfo == null)
+			{
+				Session.Delete(root);
+			}
+			else
+			{
+				if (!UnitOfWork.Contains(root))
+				{
 					//ouch! this creates a lot of problem
 					//don't dare to remove it though
-					UnitOfWork.Reassociate(root);                	
-                }
-                delRootInfo.SetDeleted();
-            }
-        }
+					UnitOfWork.Reassociate(root);
+				}
+				delRootInfo.SetDeleted();
+			}
+		}
 
 
-        protected virtual ISession Session
-        {
-            get
-            {
-                if (ValidateUserLoggedOn &&
-                    !StateHolder.Instance.StateReader.IsLoggedIn)
-                    throw new PermissionException("This repository is not available for non logged on users");
-                return ((NHibernateUnitOfWork)UnitOfWork).Session;
-            }
-        }
+		protected virtual ISession Session
+		{
+			get
+			{
+				if (ValidateUserLoggedOn &&
+						!StateHolder.Instance.StateReader.IsLoggedIn)
+					throw new PermissionException("This repository is not available for non logged on users");
+				return ((NHibernateUnitOfWork)UnitOfWork).Session;
+			}
+		}
 
-        public virtual bool ValidateUserLoggedOn
-        {
-            get { return true; }
-        }
+		public virtual bool ValidateUserLoggedOn
+		{
+			get { return true; }
+		}
 
-    }
+	}
 }
