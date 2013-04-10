7b041725231d8a9229a8b37100eb9e851bc26f61
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-10 11:30:41 +0200

    testfix

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/InitializeTaskTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/InitializeTaskTest.cs
@@ -34,13 +34,13 @@ namespace Teleopti.Ccc.WebTest.Core.Startup
 			{
 				Expect.Call(settings.nhibConfPath()).Return("path");
 				Expect.Call(physicalApplicationPath.Get()).Return("applicationPath");
-				initializeApplication.Start(null, null, null, null, true);
+				initializeApplication.Start(null, null, null, null, false);
 				LastCall.IgnoreArguments().Constraints(
 					Is.TypeOf(typeof (WebState)),
 					Is.Equal(Path.Combine("applicationPath", "path")),
 					Is.TypeOf(typeof (LoadPasswordPolicyService)),
 					Is.TypeOf(typeof (ConfigurationManagerWrapper)),
-					Is.Equal(true)
+					Is.Equal(false)
 					);
 			}
 			using (mocks.Playback())
