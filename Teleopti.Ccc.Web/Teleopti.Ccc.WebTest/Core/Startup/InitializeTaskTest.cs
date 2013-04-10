11bdb5c10dc074d321269e0ab36dcc0a601d91bc
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-10 11:27:24 +0200

    testfix

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/InitializeTaskTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/InitializeTaskTest.cs
@@ -39,7 +39,8 @@ namespace Teleopti.Ccc.WebTest.Core.Startup
 					Is.TypeOf(typeof (WebState)),
 					Is.Equal(Path.Combine("applicationPath", "path")),
 					Is.TypeOf(typeof (LoadPasswordPolicyService)),
-					Is.TypeOf(typeof (ConfigurationManagerWrapper))
+					Is.TypeOf(typeof (ConfigurationManagerWrapper)),
+					Is.Equal(true)
 					);
 			}
 			using (mocks.Playback())
