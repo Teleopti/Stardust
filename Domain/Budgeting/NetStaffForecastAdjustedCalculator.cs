08536a7f85c43331f4cbdd6222fd8db6fe5495ac
Erik Sundberg <none@none>
2013-04-11 14:46:22 +0200

    Calculate and restrict NetStaff FC Ajd to the max limit of the agents

--- Domain/Budgeting/NetStaffForecastAdjustedCalculator.cs
+++ Domain/Budgeting/NetStaffForecastAdjustedCalculator.cs
@@ -5,62 +5,56 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Budgeting
 {
-    public class NetStaffForecastAdjustCalculator : ICalculator
-    {
-        private readonly INetStaffCalculator _netStaffCalculator;
-        private readonly CultureInfo _cultureInfo;
+	public class NetStaffForecastAdjustCalculator : ICalculator
+	{
+		private readonly INetStaffCalculator _netStaffCalculator;
+		private readonly CultureInfo _cultureInfo;
 
-        public NetStaffForecastAdjustCalculator(INetStaffCalculator netStaffCalculator, CultureInfo cultureInfo)
-        {
-            _netStaffCalculator = netStaffCalculator;
-            _cultureInfo = cultureInfo;
-        }
+		public NetStaffForecastAdjustCalculator(INetStaffCalculator netStaffCalculator, CultureInfo cultureInfo)
+		{
+			_netStaffCalculator = netStaffCalculator;
+			_cultureInfo = cultureInfo;
+		}
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "1"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "2")]
-        public void Calculate(IBudgetDay budgetDay, IEnumerable<IBudgetDay> budgetDayList, BudgetCalculationResult budgetCalculationResult)
-        {
-            var week = DateHelper.GetWeekPeriod(budgetDay.Day, _cultureInfo);
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods",
+			MessageId = "1"),
+		 System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods",
+			 MessageId = "2")]
+		public void Calculate(IBudgetDay budgetDay, IEnumerable<IBudgetDay> budgetDayList,
+		                      BudgetCalculationResult budgetCalculationResult)
+		{
+			var week = DateHelper.GetWeekPeriod(budgetDay.Day, _cultureInfo);
 
-            var budgetDaysWithinWeek = new List<IBudgetDay>(7);
-            foreach (IBudgetDay day in budgetDayList)
-            {
-                if (day.Day > week.EndDate) break;
-                if (day.Day < week.StartDate) continue;
+			var budgetDaysWithinWeek = new List<IBudgetDay>(7);
+			budgetDaysWithinWeek.AddRange(
+				budgetDayList.TakeWhile(day => day.Day <= week.EndDate).Where(day => day.Day >= week.StartDate));
 
-                budgetDaysWithinWeek.Add(day);
-            }
+			var currentNetStaffWeekSum = getWeekSumNetStaff(budgetDaysWithinWeek);
+			var currentForecastedHoursWeekSum = getWeekSumForecastedHours(budgetDaysWithinWeek);
 
-            var currentNetStaffWeekSum = GetWeekSumNetStaff(budgetDaysWithinWeek);
-            var currentForecastedHoursWeekSum = GetWeekSumForecastedHours(budgetDaysWithinWeek);
+			double netStaffFcAdj = 0;
+			if (!currentForecastedHoursWeekSum.Equals(0))
+				netStaffFcAdj = (budgetDay.ForecastedHours/currentForecastedHoursWeekSum)*currentNetStaffWeekSum;
 
-            double netStaffFcAdj = 0;
-            if (currentForecastedHoursWeekSum != 0)
-            {
-                netStaffFcAdj = (budgetDay.ForecastedHours/currentForecastedHoursWeekSum) * currentNetStaffWeekSum;
-            }
+			var closedToInt = budgetDay.IsClosed ? 1 : 0;
+			var sumCustomShrinkage = budgetDay.CustomShrinkages.GetTotal();
+			var maxStaff = (1 - closedToInt)*(budgetCalculationResult.GrossStaff + budgetDay.Contractors)*
+			               (1 - (sumCustomShrinkage).Value);
 
-            budgetCalculationResult.NetStaffFcAdj = netStaffFcAdj;
-        }
+			budgetCalculationResult.NetStaffFcAdj = netStaffFcAdj > maxStaff ? maxStaff : netStaffFcAdj;
+		}
 
-        private double GetWeekSumNetStaff(IEnumerable<IBudgetDay> weekBudgetDays)
-        {
-            double weekNetStaff = 0;
-            foreach (var budgetDay in weekBudgetDays)
-            {
-                var calculatedResult = _netStaffCalculator.CalculatedResult(budgetDay);
-                weekNetStaff += calculatedResult.NetStaff;
-            }
-            return weekNetStaff;
-        }
+		private double getWeekSumNetStaff(IEnumerable<IBudgetDay> weekBudgetDays)
+		{
+			return
+				weekBudgetDays.Select(budgetDay => _netStaffCalculator.CalculatedResult(budgetDay))
+				              .Select(calculatedResult => calculatedResult.NetStaff)
+				              .Sum();
+		}
 
-        private static double GetWeekSumForecastedHours(IEnumerable<IBudgetDay> weekBudgetDays)
-        {
-            double weekForecastedHours = 0;
-            foreach (var budgetDay in weekBudgetDays)
-            {
-                weekForecastedHours += budgetDay.ForecastedHours;
-            }
-            return weekForecastedHours;
-        }
-    }
+		private static double getWeekSumForecastedHours(IEnumerable<IBudgetDay> weekBudgetDays)
+		{
+			return weekBudgetDays.Sum(budgetDay => budgetDay.ForecastedHours);
+		}
+	}
 }
