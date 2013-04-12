96ba461cff86e636b07ccda45a99074d4826b067
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-12 14:45:37 +0200

    wtf! logging fxcop

--- Infrastructure/ApplicationLayer/ServiceBusEventPublisher.cs
+++ Infrastructure/ApplicationLayer/ServiceBusEventPublisher.cs
@@ -4,8 +4,10 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 {
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "WTFDEBUG")]
 	public static class WTFDEBUG
 	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
 		public static void Log(string stuff)
 		{
 			try
@@ -48,7 +50,7 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 			_sender = sender;
 		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes")]
 		public void Publish(object @event)
 		{
 			WTFDEBUG.Log("ServiceBusEventPublisher " + @event.GetType().Name);
