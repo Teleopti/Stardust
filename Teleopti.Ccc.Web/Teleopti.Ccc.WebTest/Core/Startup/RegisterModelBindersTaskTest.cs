544f8199ca8fc70daafb6b6503ba5d721b51f3d1
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-13 22:00:40 +0200

    missing unit tests for message brokert bootstrap task

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/RegisterModelBindersTaskTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/RegisterModelBindersTaskTest.cs
@@ -1,13 +1,76 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 using NUnit.Framework;
+using Rhino.Mocks;
 using SharpTestsEx;
 using Teleopti.Ccc.Web.Areas.Start.Models.Authentication;
+using Teleopti.Ccc.Web.Core.RequestContext;
 using Teleopti.Ccc.Web.Core.Startup;
+using Teleopti.Ccc.Web.Core.Startup.InitializeApplication;
 using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.MessageBroker.Events;
 
 namespace Teleopti.Ccc.WebTest.Core.Startup
 {
+	[TestFixture]
+	public class MessageBrokerTaskTest
+	{
+		[Test]
+		public void ShouldConnectToItselfIfNoAppSetting()
+		{
+			var currentHttpContext = CurrentHttpContext("http://my.url.com", "/app/");
+			var settings = MockRepository.GenerateMock<ISettings>();
+			var messageBroker = MockRepository.GenerateMock<IMessageBroker>();
+			messageBroker.Stub(x => x.ConnectionString).PropertyBehavior();
+			var target = new MessageBrokerTask(messageBroker, currentHttpContext, settings);
+
+			target.Execute();
+
+			messageBroker.ConnectionString.Should().Be("http://my.url.com/app/");
+		}
+
+		[Test]
+		public void ShouldConnectToAppSetting()
+		{
+			var currentHttpContext = CurrentHttpContext("http://my.url.com", "/app/");
+			var settings = MockRepository.GenerateMock<ISettings>();
+			settings.Stub(x => x.MessageBroker()).Return("http://my.broker.com/path/");
+			var messageBroker = MockRepository.GenerateMock<IMessageBroker>();
+			messageBroker.Stub(x => x.ConnectionString).PropertyBehavior();
+			var target = new MessageBrokerTask(messageBroker, currentHttpContext, settings);
+
+			target.Execute();
+
+			messageBroker.ConnectionString.Should().Be(@"http://my.broker.com/path/");
+		}
+
+		[Test]
+		public void ShouldStartMessageBroker()
+		{
+			var currentHttpContext = CurrentHttpContext("http://localhost", "/");
+			var messageBroker = MockRepository.GenerateMock<IMessageBroker>();
+			var target = new MessageBrokerTask(messageBroker, currentHttpContext, MockRepository.GenerateMock<ISettings>());
+
+			Task.WaitAll(target.Execute());
+
+			messageBroker.AssertWasCalled(x => x.StartMessageBroker());
+		}
+
+		private static ICurrentHttpContext CurrentHttpContext(string url, string applicationPath)
+		{
+			var httpRequest = MockRepository.GenerateStub<HttpRequestBase>();
+			httpRequest.Stub(x => x.Url).Return(new Uri(url));
+			httpRequest.Stub(x => x.ApplicationPath).Return(applicationPath);
+			var httpContext = MockRepository.GenerateStub<HttpContextBase>();
+			httpContext.Stub(x => x.Request).Return(httpRequest);
+			var currentHttpContext = MockRepository.GenerateMock<ICurrentHttpContext>();
+			currentHttpContext.Stub(x => x.Current()).Return(httpContext);
+			return currentHttpContext;
+		}
+	}
+
 	[TestFixture]
 	public class RegisterModelBindersTaskTest
 	{
