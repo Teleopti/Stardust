88dae484c944d8b0b35162cc3ffb4b0d74845613
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-14 17:15:14 +0200

    coverage

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/TimeoutSettingsTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.Web.Broker;
+
+namespace Teleopti.Ccc.WebTest.Core.MessageBroker
+{
+	[TestFixture]
+	public class TimeoutSettingsTest
+	{
+		[Test]
+		public void ShouldLoad()
+		{
+			TimeoutSettings.Load();
+		}
+	}
+}
