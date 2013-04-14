e433b9dce8a44b817ff01a9b71cb93d5d28c5207
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-14 12:04:27 +0200

    coverage tests for SignalR - AutofacDependencyResolver

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Core/AutofacDependencyResolverTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Areas.Anywhere.Core;
+
+namespace Teleopti.Ccc.WebTest.Areas.Anywhere.Core
+{
+	[TestFixture]
+	public class AutofacDependencyResolverTest
+	{
+		[Test]
+		public void ShouldHaveCoverage()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterType<RegisteredService>();
+			builder.RegisterType<MultipleRegisteredServiceOne>().As<IMultipleRegisteredService>();
+			builder.RegisterType<MultipleRegisteredServiceTwo>().As<IMultipleRegisteredService>();
+			builder.RegisterType<RegisteredService>();
+			var container = builder.Build();
+
+			var target = new AutofacDependencyResolver(container);
+
+			target.GetService(typeof(UnregisteredService)).Should().Be.Null();
+			target.GetService(typeof(RegisteredService)).Should().Be.OfType<RegisteredService>();
+
+			target.GetServices(typeof (IMultipleRegisteredService)).Should().Have.Count.EqualTo(2);
+
+			// returns 0, a bug?
+			target.RegistrationsFor(new KeyedService("", typeof (IMultipleRegisteredService)), null);
+		}
+
+		private class RegisteredService
+		{
+			
+		}
+
+		private class MultipleRegisteredServiceOne : IMultipleRegisteredService
+		{
+
+		}
+
+		private class MultipleRegisteredServiceTwo : IMultipleRegisteredService
+		{
+
+		}
+
+		internal interface IMultipleRegisteredService
+		{
+		}
+
+		private class UnregisteredService
+		{
+
+		}
+	}
+
+}
