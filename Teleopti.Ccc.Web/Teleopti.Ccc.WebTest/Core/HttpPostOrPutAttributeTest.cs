8d9113d0224711e0f32487aa28eccbdb5465cf6c
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-04-14 17:03:44 +0200

    a test for coverage

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/HttpPostOrPutAttributeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Areas.MyTime.Core;
+
+namespace Teleopti.Ccc.WebTest.Core
+{
+	[TestFixture]
+	public class HttpPostOrPutAttributeTest
+	{
+		[Test]
+		public void ShouldHaveCoverage()
+		{
+			var target = new HttpPostOrPutAttribute();
+			var controller = new TestController();
+			var controllerBuilder = new TestControllerBuilder();
+			controllerBuilder.InitializeController(controller);
+			target.IsValidForRequest(controller.ControllerContext, typeof (TestController).GetMethod("Action"));
+		}
+
+		class TestController : Controller
+		{
+			[HttpPostOrPut]
+			public void Action()
+			{
+				
+			}
+		}
+
+	}
+}
