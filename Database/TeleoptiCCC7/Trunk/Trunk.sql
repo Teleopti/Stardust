c12577b9c2780e05581180dd4786315308f583e1
Roger Kratz <roger.kratz@teleopti.com>
2013-04-15 13:38:12 +0200

    adding datecolumn in mapping + trunk

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -41,3 +41,16 @@ UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentI
 
 SET NOCOUNT OFF
 GO
+
+
+----------------  
+--Name: Roger Kratz
+--Date: 2013-04-15
+--Desc: Adding date for person assignment. Hard coded to 1800-1-1. Will be replaced by .net code
+ALTER TABLE dbo.PersonAssignment
+ADD TheDate datetime
+GO
+
+update dbo.PersonAssignment
+set TheDate ='1800-1-1'
+Go
