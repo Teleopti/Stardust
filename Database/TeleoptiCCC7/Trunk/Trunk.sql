dc282600d8f146203b0e13e3184a5388601dbf68
Roger Kratz <roger.kratz@teleopti.com>
2013-04-15 16:49:42 +0200

    adding datecolumn to pa audit table

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -58,3 +58,15 @@ GO
 ALTER TABLE dbo.PersonAssignment
 ALTER COLUMN TheDate datetime not null
 GO
+
+----------------  
+--Name: Roger Kratz
+--Date: 2013-04-15
+--Desc: Adding date for person assignment audit table. Hard coded to 1800-1-1. Will be replaced by .net code
+ALTER TABLE Auditing.PersonAssignment_AUD
+ADD TheDate datetime
+GO
+
+update dbo.PersonAssignment
+set TheDate ='1800-1-1'
+GO
