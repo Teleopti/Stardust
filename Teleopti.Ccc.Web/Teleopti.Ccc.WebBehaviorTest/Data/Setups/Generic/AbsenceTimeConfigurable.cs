f9117cc09df2c0e622f01b88c1ca0f845b271263
Henke <henrik.andersson@teleopti.com>
2013-04-15 15:01:58 +0200

    Removed some unnecessary code

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Generic/AbsenceTimeConfigurable.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Generic/AbsenceTimeConfigurable.cs
@@ -1,17 +1,13 @@
 using System;
 using System.Drawing;
 using System.Linq;
-using System.Reflection;
-using NHibernate;
 using NUnit.Framework;
-using Rhino.Mocks;
 using Teleopti.Ccc.Domain.Budgeting;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Security.Principal;
-using Teleopti.Ccc.Infrastructure.Foundation;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
-using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.Infrastructure;
@@ -22,23 +18,11 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Generic
 	public class AbsenceTimeConfigurable : IPostSetup
 	{
 		public DateTime Date { get; set; }
-		public static ISession Session { get; set; }
-		private IState stateMock;
-		protected IPerson LoggedOnPerson { get; private set; }
 		protected IRepositoryFactory RepositoryFactory { get; private set; }
-		protected MockRepository Mocks { get; private set; }
-
-		protected virtual void SetupForRepositoryTest() { }
-
-		protected void PersistAndRemoveFromUnitOfWork(IEntity obj)
-		{
-			Session.SaveOrUpdate(obj);
-			Session.Flush();
-			Session.Evict(obj);
-		}
 
 		public void Apply(IPerson user, IUnitOfWork uow)
 		{
+
 			IScheduleProjectionReadOnlyRepository target;
 			IScenario scenario;
 			IPerson person;
@@ -49,76 +33,59 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Generic
 			BusinessUnitFactory.SetBusinessUnitUsedInTestToNull();
 			RepositoryFactory = new RepositoryFactory();
 
-			Mocks = new MockRepository();
-			stateMock = Mocks.StrictMock<IState>();
-
 			var buGuid = Guid.NewGuid();
 			BusinessUnitFactory.BusinessUnitUsedInTest.SetId(buGuid);
-			LoggedOnPerson = PersonFactory.CreatePersonWithBasicPermissionInfo(string.Concat("logOnName", Guid.NewGuid().ToString()), string.Empty);
 
-			StateHolderProxyHelper.ClearAndSetStateHolder(Mocks,
-													 LoggedOnPerson,
-													 BusinessUnitFactory.BusinessUnitUsedInTest,
-													 null,
-													 stateMock);
 
-			Session =
-				(ISession)
-				uow.GetType().GetProperty("Session", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(
-					uow, null);
-
-			((IDeleteTag)LoggedOnPerson).SetDeleted();
-			Session.Save(LoggedOnPerson);
-
-			//force a insert
-			BusinessUnitFactory.BusinessUnitUsedInTest.SetId(null);
-			Session.Save(BusinessUnitFactory.BusinessUnitUsedInTest, buGuid);
-			Session.Flush();
-
-			SetupForRepositoryTest();
-
-			//scenario = ScenarioFactory.CreateScenarioAggregate();
-			//scenario = GlobalDataContext.Data().Data<CommonScenario>().Scenario;
+			//henke: här skall vi nog hämta default?? 
 			var scenarioRepository = new ScenarioRepository(uow);
 			scenario = scenarioRepository.LoadAll().First();
-			PersistAndRemoveFromUnitOfWork(scenario);
 			scenarioId = scenario.Id.GetValueOrDefault();
 
+			//henke: varför behöver vi den här här??
+			//person = PersonFactory.CreatePerson();
 			person = PersonFactory.CreatePerson();
-			PersistAndRemoveFromUnitOfWork(person);
 
 			var absenceRepository = new AbsenceRepository(uow);
 			absence = absenceRepository.LoadAll().First();
-			//absence = AbsenceFactory.CreateAbsence("Vacation");
-			PersistAndRemoveFromUnitOfWork(absence);
 
 			budgetGroup = new BudgetGroup { Name = "My Budget", TimeZone = TeleoptiPrincipal.Current.Regional.TimeZone };
-			//var budgetGroupRepository = new BudgetGroupRepository(uow);
-			//budgetGroup = budgetGroupRepository.LoadAll().First();
 
 			var shrinkage = new CustomShrinkage("test", true);
 			shrinkage.AddAbsence(absence);
 			budgetGroup.AddCustomShrinkage(shrinkage);
-			PersistAndRemoveFromUnitOfWork(budgetGroup);
 
-			//var site = SiteFactory.CreateSimpleSite();
-			var siteRepository = new SiteRepository(uow);
-			var site = siteRepository.LoadAll().First();
-			PersistAndRemoveFromUnitOfWork(site);
+			var budgetGroupRepo = new BudgetGroupRepository(uow);
+			budgetGroupRepo.Add(budgetGroup);
 
-			//var team = TeamFactory.CreateSimpleTeam("team test");
 			var teamRepository = new TeamRepository(uow);
 			var team = teamRepository.LoadAll().First();
-			team.Site = site;
-			PersistAndRemoveFromUnitOfWork(team);
 
-			var personPeriod = PersonPeriodFactory.CreatePersonPeriod(new DateOnly(Date).AddDays(-1), team, budgetGroup);
+			var partTimePercentageRepository = new PartTimePercentageRepository(uow);
+			var partTimepercentage = new PartTimePercentage("some name");
+			partTimePercentageRepository.Add(partTimepercentage);
+
+			var contract = new Contract("the contract");
+			var contractRepository = new ContractRepository(uow);
+			contractRepository.Add(contract);
+
+			var contractSchedule = new ContractSchedule("contract schedule");
+			var contractSchedulerepository = new ContractScheduleRepository(uow);
+			contractSchedulerepository.Add(contractSchedule);
+
+			IPersonPeriod personPeriod = PersonPeriodFactory.CreatePersonPeriod(new DateOnly(Date).AddDays(-1), team, budgetGroup);
+
+			personPeriod.PersonContract.Contract = contract;
+			personPeriod.PersonContract.ContractSchedule = contractSchedule;
+			personPeriod.PersonContract.PartTimePercentage = partTimepercentage;
+		
 			person.AddPersonPeriod(personPeriod);
-			PersistAndRemoveFromUnitOfWork(personPeriod.PersonContract.Contract);
-			PersistAndRemoveFromUnitOfWork(personPeriod.PersonContract.ContractSchedule);
-			PersistAndRemoveFromUnitOfWork(personPeriod.PersonContract.PartTimePercentage);
-			PersistAndRemoveFromUnitOfWork(person);
-			//target = new ScheduleProjectionReadOnlyRepository(new CurrentUnitOfWork(GlobalUnitOfWorkState.CurrentUnitOfWorkFactory));
+
+			var personRepository = new PersonRepository(uow);
+			personRepository.Add(person);
+
+			uow.PersistAll();
+
 			target = new ScheduleProjectionReadOnlyRepository(new FixedCurrentUnitOfWork(uow));
 
 			var period =
@@ -137,8 +104,6 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Generic
 
 			target.AddProjectedLayer(new DateOnly(Date), scenarioId, person.Id.GetValueOrDefault(), layer);
 
-			//var result = target.AbsenceTimePerBudgetGroup(new DateOnlyPeriod(new DateOnly(Date).AddDays(-1), new DateOnly(Date).AddDays(1)),
-			//										 budgetGroup, scenario);
 
 			var usedAbsenceMinutes = TimeSpan.FromTicks(
 				   target.AbsenceTimePerBudgetGroup(new DateOnlyPeriod(new DateOnly(Date).AddDays(-1), new DateOnly(Date).AddDays(1)),
