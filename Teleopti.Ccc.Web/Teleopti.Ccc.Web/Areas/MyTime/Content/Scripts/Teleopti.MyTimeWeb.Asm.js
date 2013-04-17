3a8bcc222ecb4754e847a3344417db2d9614b3cf
Maria Stein <none@none>
2013-04-17 21:11:27 +0200

    Bug #23097 - Schedule update notification a bit difficult to understand
    --HG--
    branch : Release 7.3.374

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Asm.js
@@ -204,7 +204,7 @@ Teleopti.MyTimeWeb.Asm = (function () {
 		},
 		NotifyWhenScheduleChangedListener: function (notification) {
 			if (_validSchedulePeriod(notification)) {
-				var startDate = Teleopti.MyTimeWeb.MessageBroker.ConvertMbDateTimeToJsDate(notification.StartDate).toString('d');
+				var startDate = Teleopti.MyTimeWeb.MessageBroker.ConvertMbDateTimeToJsDate(notification.StartDate);
 				var notifyText = notifyOptions.notifyText.format(startDate);
 				Teleopti.MyTimeWeb.Notifier.Notify(notifyOptions, notifyText);
 			}
