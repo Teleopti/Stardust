deac308a3b5b3d0d63fb7943feee8e0778b801de
David Jonsson <david.jonsson@teleopti.com>
2013-04-24 21:25:24 +0200

    removed unused code/switches

--- Teleopti.Support.Security/Program.cs
+++ Teleopti.Support.Security/Program.cs
@@ -7,59 +7,39 @@ using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Support.Security
 {
-    class Program
-    {
-        private static readonly ICommandLineCommand PasswordEncryption = new PasswordEncryption();
+	class Program
+	{
+		private static readonly ICommandLineCommand PasswordEncryption = new PasswordEncryption();
         private static readonly ICommandLineCommand ForecasterDateAdjustment = new ForecasterDateAdjustment();
         private static readonly ICommandLineCommand PersonFirstDayOfWeekSetter = new PersonFirstDayOfWeekSetter();
         private static readonly ICommandLineCommand LicenseStatusChecker = new LicenseStatusChecker();
 		private static readonly ICommandLineCommand CrossDatabaseViewUpdate = new CrossDatabaseViewUpdate();
 
+		static void Main(string[] args)
+		{
+			var commandLineArgument = new CommandLineArgument(args);
+			var connectionStringBuilder = new SqlConnectionStringBuilder(commandLineArgument.DestinationConnectionString);
 
-        static void Main(string[] args)
-        {
-            var commandLineArgument = new CommandLineArgument(args);
-
-					var connectionStringBuilder = new SqlConnectionStringBuilder(commandLineArgument.DestinationConnectionString);
-	        var scheduleConverter =
-		        new ConvertSchedule(new IPersonAssignmentConverter[]
-			        {
-				        new PersonAssignmentAuditDateSetter(connectionStringBuilder),
-				        new PersonAssignmentDateSetter(connectionStringBuilder)
-			        });
-					scheduleConverter.ExecuteAllConverters();
-	
 			if (!string.IsNullOrEmpty(commandLineArgument.AggDatabase))
+			{
 				CrossDatabaseViewUpdate.Execute(commandLineArgument);
+			}
+			else
+			{
+				var scheduleConverter =
+				new ConvertSchedule(new IPersonAssignmentConverter[]
+				{
+					new PersonAssignmentAuditDateSetter(connectionStringBuilder),
+					new PersonAssignmentDateSetter(connectionStringBuilder)
+				});
 
-            if (!commandLineArgument.ForecasterMode &&
-                !commandLineArgument.PersonUpdateMode &&
-                !commandLineArgument.PasswordEncryptionMode &&
-                !commandLineArgument.LicenseStatusMode &&
-				string.IsNullOrEmpty(commandLineArgument.AggDatabase))
-            {
-                ForecasterDateAdjustment.Execute(commandLineArgument);
-                PasswordEncryption.Execute(commandLineArgument);
-                PersonFirstDayOfWeekSetter.Execute(commandLineArgument);
-                LicenseStatusChecker.Execute(commandLineArgument);
-                return;
-            }
-
-            if (commandLineArgument.ForecasterMode)
-                ForecasterDateAdjustment.Execute(commandLineArgument);
-
-            if (commandLineArgument.PersonUpdateMode)
-                PersonFirstDayOfWeekSetter.Execute(commandLineArgument);
-
-            if (commandLineArgument.PasswordEncryptionMode)
-                PasswordEncryption.Execute(commandLineArgument);
-
-            if (commandLineArgument.LicenseStatusMode)
-                LicenseStatusChecker.Execute(commandLineArgument);
-
-	        Environment.ExitCode = 0;
+				scheduleConverter.ExecuteAllConverters();
+				ForecasterDateAdjustment.Execute(commandLineArgument);
+				PersonFirstDayOfWeekSetter.Execute(commandLineArgument);
+				PasswordEncryption.Execute(commandLineArgument);
+				LicenseStatusChecker.Execute(commandLineArgument);
+			}
+			Environment.ExitCode = 0;
         }
-
-
     }
 }
