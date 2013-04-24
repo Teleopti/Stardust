719337e4994a82e85de87c830d79a9760908f730
Roger Kratz <roger.kratz@teleopti.com>
2013-04-24 16:06:20 +0200

    no longer depended on personassignmentcommon

--- Infrastructure/SystemCheck/AgentDayConverter/PersonAssignmentDateSetterBase.cs
+++ Infrastructure/SystemCheck/AgentDayConverter/PersonAssignmentDateSetterBase.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using Teleopti.Interfaces.Infrastructure;
+using System.Globalization;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.SystemCheck.AgentDayConverter
 {
 	public abstract class PersonAssignmentDateSetterBase : IPersonAssignmentConverter
 	{
-		private readonly IPersonAssignmentCommon _personAssignmentCommon = new PersonAssignmentCommon();
-
 		protected abstract string NumberOfNotConvertedCommand { get; }
 		protected abstract string ReadCommand { get; }
 		protected abstract string UpdateAssignmentDate { get; }
@@ -22,14 +21,14 @@ namespace Teleopti.Ccc.Infrastructure.SystemCheck.AgentDayConverter
 			{
 				connection.Open();
 
-				var rows = _personAssignmentCommon.ReadRows(connection, ReadCommand);
-				_personAssignmentCommon.SetFields(rows);
+				var rows = readRows(connection, ReadCommand);
+				setFields(rows);
 				updatePersonAssignmentRows(rows, connection);
 
 
 
 				//fix - throw error here if not 0
-				rows = _personAssignmentCommon.ReadRows(connection, NumberOfNotConvertedCommand);
+				rows = readRows(connection, NumberOfNotConvertedCommand);
 
 			}
 		}
@@ -49,6 +48,43 @@ namespace Teleopti.Ccc.Infrastructure.SystemCheck.AgentDayConverter
 					command.ExecuteNonQuery();
 				}
 			}
+		}
+
+		private static void setFields(IEnumerable<DataRow> rows)
+		{
+			foreach (var dataRow in rows)
+			{
+				var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(dataRow.Field<string>("DefaultTimeZone"));
+				var utcTime = new DateTime(dataRow.Field<DateTime>("Minimum").Ticks, DateTimeKind.Utc);
+				var localDate = timeZoneInfo.SafeConvertTimeToUtc(utcTime);
+				dataRow["TheDate"] = String.Format("{0:s}", localDate.Date);
+				var version = dataRow.Field<int>("Version");
+				dataRow["Version"] = version + 1;
+			}
+		}
+
+		private IEnumerable<DataRow> readRows(SqlConnection connection, string readCommand)
+		{
+			IList<DataRow> ret;
+			using (var command = new SqlCommand(readCommand, connection))
+			{
+				ret = new List<DataRow>();
+				using (var dataSet = new DataSet())
+				{
+					dataSet.Locale = CultureInfo.CurrentUICulture;
+					using (var sqlDataAdapter = new SqlDataAdapter(command))
+					{
+						sqlDataAdapter.Fill(dataSet, "Data");
+					}
+
+					foreach (DataRow row in dataSet.Tables[0].Rows)
+					{
+						ret.Add(row);
+					}
+				}
+			}
+
+			return ret;
 		}
 	}
 }
