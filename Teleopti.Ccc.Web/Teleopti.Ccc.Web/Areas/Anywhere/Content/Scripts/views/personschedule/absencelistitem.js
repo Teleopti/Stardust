9a2ad7f36f55c0c5135e06bab5da6dadc0061c6e
Tina Hammarstrom <none@none>
2013-04-25 13:56:22 +0200

    Minor

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/absencelistitem.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/absencelistitem.js
@@ -10,8 +10,6 @@ define([
 
     return function (data) {
 
-        var self = this;
-        
         this.StartTime = ko.observable(moment(data.StartTime).format("YYYY-MM-DD HH:mm"));
         this.EndTime = ko.observable(moment(data.EndTime).format("YYYY-MM-DD HH:mm"));
 
