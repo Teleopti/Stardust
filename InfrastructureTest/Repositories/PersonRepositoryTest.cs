5d8a89557a298be270bccb85d7b779a3e29ebf54
Roger Kratz <roger.kratz@teleopti.com>
2013-04-25 15:47:58 +0200

    no updates of person's timezone using personreposity anylonger

--- InfrastructureTest/Repositories/PersonRepositoryTest.cs
+++ InfrastructureTest/Repositories/PersonRepositoryTest.cs
@@ -1651,6 +1651,24 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories
 			Assert.That(testList[0].OptionalColumnValueCollection.Count,Is.EqualTo(1));
 		}
 
+		[Test]
+		public void ShouldNotBeAbleToAlterTimeZoneInDb()
+		{
+			//need to run stuff infrastructure\systemcheck\agentdayconverter instead! roger, micke & david knows more!
+
+			var pRep = new PersonRepository(UnitOfWork);
+			var snubbe = new Person();
+			snubbe.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Utc);
+			PersistAndRemoveFromUnitOfWork(snubbe);
+
+			var loadedSnubbe = pRep.Get(snubbe.Id.Value);
+			loadedSnubbe.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+			PersistAndRemoveFromUnitOfWork(loadedSnubbe);
+
+			pRep.Get(snubbe.Id.Value).PermissionInformation.DefaultTimeZone()
+			    .Should().Be.EqualTo(TimeZoneInfo.Utc);
+		}
+
 		private static void verifyPermissionInfoIsLazy(bool expected, IPerson userRetOk)
 		{
 			Assert.AreEqual(expected,
