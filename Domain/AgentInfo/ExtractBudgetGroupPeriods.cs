30c0014f7d6c8c2ec73fbaecdac47eb8cd5bca3c
Henke <henrik.andersson@teleopti.com>
2013-04-26 11:53:04 +0200

    Added class for getting the periods and budgetgroups Added namingrule for resharper methods

---
+++ Domain/AgentInfo/ExtractBudgetGroupPeriods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.AgentInfo
+{
+	public class ExtractBudgetGroupPeriods : IExtractBudgetGroupPeriods
+	{
+		public IEnumerable<Tuple<DateOnlyPeriod, IBudgetGroup>> BudgetGroupsForPeriod(IPerson person, DateOnlyPeriod period)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ DomainTest/AgentInfo/ExtractBudgetGroupPeriodsTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Teleopti.Ccc.DomainTest.AgentInfo
+{
+	[TestFixture]
+	public class ExtractBudgetGroupPeriodsTest
+	{
+		[Test]
+		public void Extract_WhenOnlyOneAffectedPersonPeriod_ShouldReturnTheBudgetGroupForThatPersonPeriod()
+		{
+			
+		}
+	}
+}
