1a57bb2f899f2510450867476dc8975de5ef787a
Erik Sundberg <none@none>
2013-04-26 15:58:22 +0200

    Added stable sorting to request view

--- Teleopti.Ccc.WpfControls/Controls/Requests/Views/HandlePersonRequestView.xaml.cs
+++ Teleopti.Ccc.WpfControls/Controls/Requests/Views/HandlePersonRequestView.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
-using Microsoft.Windows.Controls;
+using Microsoft.Windows.Controls.Primitives;
 using Teleopti.Ccc.WinCode.Common.GuiHelpers;
 using Teleopti.Ccc.WinCode.Scheduling.Requests;
+using DataGrid = Microsoft.Windows.Controls.DataGrid;
+using DataGridSortingEventArgs = Microsoft.Windows.Controls.DataGridSortingEventArgs;
+
 
 namespace Teleopti.Ccc.WpfControls.Controls.Requests.Views
 {
@@ -13,13 +19,12 @@ namespace Teleopti.Ccc.WpfControls.Controls.Requests.Views
     /// </summary>
     public partial class HandlePersonRequestView
     {
-
     	public HandlePersonRequestView()
         {
             InitializeComponent();
         }
-        
-        //Sorry Hank, had to put all this shit here to make this expand/collapse thing work Teleopti style
+
+	    //Sorry Hank, had to put all this shit here to make this expand/collapse thing work Teleopti style
         private void Expander_Expanded(object sender, RoutedEventArgs e)
         {
             requestGrid.RowDetailsVisibilityMode = Microsoft.Windows.Controls.DataGridRowDetailsVisibilityMode.VisibleWhenSelected;
@@ -54,7 +59,7 @@ namespace Teleopti.Ccc.WpfControls.Controls.Requests.Views
             var expander = sender as Expander;
             if (expander != null) expander.IsExpanded = false;
         }
-
+		
         private void requestGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var src = VisualTreeHelper.GetParent((DependencyObject)e.OriginalSource);
@@ -78,6 +83,36 @@ namespace Teleopti.Ccc.WpfControls.Controls.Requests.Views
         {
             requestGrid.SelectedCellsChanged -= requestGrid_SelectedCellsChanged;
             requestGrid.MouseDoubleClick -= requestGrid_MouseDoubleClick;
+	        requestGrid.Sorting -= RequestGrid_OnSorting;
+	        requestGrid.PreviewMouseDown -= RequestGrid_OnPreviewMouseDown;
         }
+
+	    private void RequestGrid_OnSorting(object sender, DataGridSortingEventArgs e)
+	    {
+		    e.Handled = true;
+	    }
+
+	    private void RequestGrid_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+	    {
+		    var dependencyObject = (DependencyObject) e.OriginalSource;
+		    while (dependencyObject != null && !(dependencyObject is DataGridColumnHeader))
+			    dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+		    if (dependencyObject == null || !(dependencyObject is DataGridColumnHeader) || !(sender is DataGrid)) return;
+
+		    var header = dependencyObject as DataGridColumnHeader;
+		    var view = CollectionViewSource.GetDefaultView((sender as DataGrid).ItemsSource);
+		    var direction = ListSortDirection.Ascending;
+		    if (view.SortDescriptions.Any(s => s.PropertyName == header.Column.SortMemberPath))
+		    {
+			    var previousSort = view.SortDescriptions.FirstOrDefault(s => s.PropertyName == header.Column.SortMemberPath);
+			    direction = previousSort.Direction == ListSortDirection.Ascending
+				                ? ListSortDirection.Descending
+				                : ListSortDirection.Ascending;
+			    view.SortDescriptions.Remove(previousSort);
+		    }
+
+		    var sortDescription = new SortDescription(header.Column.SortMemberPath, direction);
+		    view.SortDescriptions.Insert(0, sortDescription);
+	    }
     }
 }
