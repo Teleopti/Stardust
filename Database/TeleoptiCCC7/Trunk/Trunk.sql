3c117e6c4392bb36ecedbcbf611d63f0182bbae3
David Jonsson <david.jonsson@teleopti.com>
2013-04-29 16:24:13 +0200

    bug #23288 - remove unwanted TRUNCATE
    --HG--
    branch : Release 7.3.382

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -3,6 +3,6 @@
 --Date: 2013-04-24
 --Desc: Bug #23047. Empty read models to consider deleted schedule when done.
 ----------------  
-TRUNCATE TABLE [ReadModel].[ScheduleDay]
+--TRUNCATE TABLE [ReadModel].[ScheduleDay] nope! dont => #23288 //DavidJ
 TRUNCATE TABLE [ReadModel].[ScheduleProjectionReadOnly]
 GO
