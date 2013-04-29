5bf49d657c9cc00437961e4439b983c5ba2cbcdd
David Jonsson <david.jonsson@teleopti.com>
2013-04-29 14:32:34 +0200

    bug #23285 - Persist Purge
    --HG--
    branch : Release 7.3.374

--- Teleopti.Analytics/Teleopti.Analytics.Etl.TransformerInfrastructure/RaptorRepository.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.TransformerInfrastructure/RaptorRepository.cs
@@ -602,10 +602,11 @@ namespace Teleopti.Analytics.Etl.TransformerInfrastructure
 
 		public int PerformPurge()
 		{
-			using (UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
 			{
 				var purgeCommand = new PurgeApplicationData(UnitOfWorkFactory.Current);
 				purgeCommand.Execute();
+				uow.PersistAll();
 			}
 			return 1;
 		}
