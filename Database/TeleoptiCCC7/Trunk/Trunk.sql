cfed74b9da5cd668215430c3cc846e633248a48a
David Jonsson <david.jonsson@teleopti.com>
2013-05-04 01:23:05 +0200

    Bug #23349 Speed up rotation db query
    --HG--
    branch : Release 7.3.374

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -37,3 +37,14 @@ GO
 --add constraint to block new duplicates
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonSkill]') AND name = N'UC_Parent_Skill')
 ALTER TABLE PersonSkill ADD CONSTRAINT UC_Parent_Skill UNIQUE (Parent,Skill)
+GO
+
+----------------  
+--Name: David Jonsson
+--Date: 2013-05-04
+--Desc: Bug #23349 Speed up rotations
+----------------  
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonRotation]') AND name = N'IX_PersonRotation_Person_IsDeleted_BusinessUnit_StartDate')
+CREATE NONCLUSTERED INDEX [IX_PersonRotation_Person_IsDeleted_BusinessUnit_StartDate]
+ON [dbo].[PersonRotation] ([Person],[IsDeleted],[BusinessUnit],[StartDate])
+GO
