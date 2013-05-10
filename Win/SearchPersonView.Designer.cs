58d2ef5d1809cd7cb8bfd65e25dde326ac0d8a44
ola@teleopti.com <ola@teleopti.com>
2013-05-10 13:40:10 +0200

    bug #23414
    --HG--
    branch : Release 7.4.384

--- Win/SearchPersonView.Designer.cs
+++ Win/SearchPersonView.Designer.cs
@@ -30,7 +30,6 @@
         {
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.label1 = new System.Windows.Forms.Label();
 			this.gridListControl1 = new Syncfusion.Windows.Forms.Grid.GridListControl();
 			this.tableLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.gridListControl1)).BeginInit();
@@ -39,9 +38,11 @@
 			// textBox1
 			// 
 			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.textBox1.Location = new System.Drawing.Point(3, 23);
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.textBox1.Location = new System.Drawing.Point(3, 5);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(269, 20);
+			this.textBox1.Size = new System.Drawing.Size(388, 21);
 			this.textBox1.TabIndex = 1;
 			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
 			// 
@@ -49,31 +50,17 @@
 			// 
 			this.tableLayoutPanel1.ColumnCount = 1;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.gridListControl1, 0, 2);
 			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 3;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(274, 521);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(394, 521);
 			this.tableLayoutPanel1.TabIndex = 2;
-			// 
-			// label1
-			// 
-			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(3, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(269, 20);
-			this.label1.TabIndex = 0;
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// gridListControl1
 			// 
@@ -82,12 +69,12 @@
 			this.gridListControl1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.gridListControl1.GridVisualStyles = Syncfusion.Windows.Forms.GridVisualStyles.Office2007Blue;
 			this.gridListControl1.ItemHeight = 17;
-			this.gridListControl1.Location = new System.Drawing.Point(3, 43);
+			this.gridListControl1.Location = new System.Drawing.Point(3, 30);
 			this.gridListControl1.MultiColumn = false;
 			this.gridListControl1.Name = "gridListControl1";
 			this.gridListControl1.Properties.BackgroundColor = System.Drawing.SystemColors.Window;
 			this.gridListControl1.SelectedIndex = -1;
-			this.gridListControl1.Size = new System.Drawing.Size(269, 475);
+			this.gridListControl1.Size = new System.Drawing.Size(388, 488);
 			this.gridListControl1.TabIndex = 3;
 			this.gridListControl1.ThemesEnabled = true;
 			this.gridListControl1.TopIndex = 0;
@@ -98,7 +85,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "SearchPersonView";
-			this.Size = new System.Drawing.Size(274, 521);
+			this.Size = new System.Drawing.Size(394, 521);
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.gridListControl1)).EndInit();
@@ -109,8 +96,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private Syncfusion.Windows.Forms.Grid.GridListControl gridListControl1;
     }
 }
