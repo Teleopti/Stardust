190c413835be3b7efcbf259549b5c1b0f05df1dc
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-05-10 14:26:07 +0200

    rewrote some more step definitions the new way. that actually was unstable!

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/NotifyStepDefinition.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/NotifyStepDefinition.cs
@@ -1,9 +1,4 @@
-﻿using NUnit.Framework;
-using SharpTestsEx;
-using TechTalk.SpecFlow;
-using Teleopti.Ccc.WebBehaviorTest.Core;
-using Teleopti.Ccc.WebBehaviorTest.Core.Robustness;
-using WatiN.Core;
+﻿using TechTalk.SpecFlow;
 using Browser = Teleopti.Ccc.WebBehaviorTest.Core.Browser;
 
 namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic
@@ -14,16 +9,16 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic
 		[Then(@"I should see only one alert containing '(.*)'")]
 		public void ThenIShouldSeeOnlyOneAlertContaining(string activity)
 		{
-			EventualAssert.That(() =>
-					 Browser.Current.Elements.Filter(QuicklyFind.ByClass("notifyLoggerItem")).Exists(Find.ByText(t => t.Contains(activity))),
-					 Is.True);
-			Browser.Current.Elements.Filter(QuicklyFind.ByClass("notifyLoggerItem")).Count.Should().Be.EqualTo(1);
+			Browser.Interactions.AssertExists(string.Format("#notifyLogger .notifyLoggerItem:contains('{0}')", activity));
+			Browser.Interactions.AssertExists("#notifyLogger .notifyLoggerItem");
+			Browser.Interactions.AssertNotExists("#notifyLogger", ".notifyLoggerItem:nth-child(2)");
 		}
 
 		[Then(@"I should see one notify message")]
 		public void ThenIShouldSeeAnAlert()
 		{
-			Browser.Current.Elements.Filter(QuicklyFind.ByClass("notifyLoggerItem")).Count.Should().Be.EqualTo(1);
+			Browser.Interactions.AssertExists("#notifyLogger .notifyLoggerItem");
+			Browser.Interactions.AssertNotExists("#notifyLogger", ".notifyLoggerItem:nth-child(2)");
 		}
 	}
 }
