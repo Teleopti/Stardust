129297bd28d5cf1e128780ac8617374d4fd94e60
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-05-13 09:15:15 +0200

    merge fix

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/NotifyStepDefinition.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/NotifyStepDefinition.cs
@@ -6,14 +6,6 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic
 	[Binding]
 	public class NotifyStepDefinition
 	{
-		[Then(@"I should see only one alert containing '(.*)'")]
-		public void ThenIShouldSeeOnlyOneAlertContaining(string activity)
-		{
-			Browser.Interactions.AssertExists(string.Format("#notifyLogger .notifyLoggerItem:contains('{0}')", activity));
-			Browser.Interactions.AssertExists("#notifyLogger .notifyLoggerItem");
-			Browser.Interactions.AssertNotExists("#notifyLogger", ".notifyLoggerItem:nth-child(2)");
-		}
-
 		[Then(@"I should see one notify message")]
 		public void ThenIShouldSeeAnAlert()
 		{
