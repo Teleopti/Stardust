051068c60f7af41f7aab10a6bf6de0a96f2f466e
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-05-13 14:33:55 +0200

    lowered timeout to 10 seconds again.

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/EventDefinition.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/EventDefinition.cs
@@ -38,7 +38,7 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings
 				TestSiteConfigurationSetup.RecycleApplication();
 
 				log4net.Config.XmlConfigurator.Configure();
-				Timeouts.Set(TimeSpan.FromSeconds(20));
+				Timeouts.Set(TimeSpan.FromSeconds(10));
 
 				TestDataSetup.CreateDataSource();
 
