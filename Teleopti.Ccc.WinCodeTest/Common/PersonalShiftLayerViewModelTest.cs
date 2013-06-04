777e72b74c1741419fcb3de6b2f666c005a368ac
Claes Stenman <claes.stenman@teleopti.com>
2013-06-04 14:47:39 +0200

    PBI 22212 Add complex personal shifts

--- Teleopti.Ccc.WinCodeTest/Common/PersonalShiftLayerViewModelTest.cs
+++ Teleopti.Ccc.WinCodeTest/Common/PersonalShiftLayerViewModelTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.WinCode.Common;
 using Teleopti.Interfaces.Domain;
 
@@ -27,5 +29,32 @@ namespace Teleopti.Ccc.WinCodeTest.Common
         {
             get { return true; }
         }
+
+		[Test]
+		public void ShouldMoveUpAndDown()
+		{
+			var period = new DateTimePeriod(new DateTime(2000, 1, 1, 10, 0, 0, DateTimeKind.Utc), new DateTime(2001, 1, 1, 11, 0, 0, DateTimeKind.Utc));
+			var activity = ActivityFactory.CreateActivity("activity");
+			var layer = new PersonalShiftActivityLayer(activity, period);
+			var personalShift1 = PersonalShiftFactory.CreatePersonalShift(activity, period);
+			var personalShift2 = PersonalShiftFactory.CreatePersonalShift(activity, period);
+
+			var personAssignment = PersonAssignmentFactory.CreatePersonAssignmentEmpty();
+			personAssignment.AddPersonalShift(personalShift1);
+			personAssignment.AddPersonalShift(personalShift2);
+
+			Assert.IsTrue(personAssignment.PersonalShiftCollection.IndexOf(personalShift1) == 0);
+			Assert.IsTrue(personAssignment.PersonalShiftCollection.IndexOf(personalShift2) == 1);
+
+			var model = new PersonalShiftLayerViewModel(layer, personalShift2, null);
+			model.MoveUp();
+			
+			Assert.IsTrue(personAssignment.PersonalShiftCollection.IndexOf(personalShift1) == 1);
+			Assert.IsTrue(personAssignment.PersonalShiftCollection.IndexOf(personalShift2) == 0);
+
+			model.MoveDown();
+			Assert.IsTrue(personAssignment.PersonalShiftCollection.IndexOf(personalShift1) == 0);
+			Assert.IsTrue(personAssignment.PersonalShiftCollection.IndexOf(personalShift2) == 1);
+		}
     }
 }
