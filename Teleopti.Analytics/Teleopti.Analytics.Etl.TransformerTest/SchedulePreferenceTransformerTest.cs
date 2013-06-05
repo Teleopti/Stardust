c04560f605882c8bcffd1c687dd23c10570e37ab
Roger Kratz <roger.kratz@teleopti.com>
2013-06-05 17:18:38 +0200

    no need to use editableshift

--- Teleopti.Analytics/Teleopti.Analytics.Etl.TransformerTest/SchedulePreferenceTransformerTest.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.TransformerTest/SchedulePreferenceTransformerTest.cs
@@ -38,10 +38,8 @@ namespace Teleopti.Analytics.Etl.TransformerTest
             IPerson person = schedulePart.Person;
             IShiftCategory shiftCategory = new ShiftCategory("TopCat");
             shiftCategory.SetId(Guid.NewGuid());
-            var mainShift = new EditableShift(shiftCategory);
-            mainShift.LayerCollection.Add(new EditorActivityLayer(activity, schedulePart.Period));
 			IPersonAssignment assignment = new PersonAssignment(person, schedulePart.Scenario, new DateOnly(2000, 1, 1));
-            new EditableShiftMapper().SetMainShiftLayers(assignment, mainShift);
+			assignment.SetMainShiftLayers(new[] { new MainShiftActivityLayerNew(activity, schedulePart.Period) }, shiftCategory);
             IDayOffTemplate dayOffTemplate = new DayOffTemplate(new Description("WrongDayOff"));
             dayOffTemplate.SetId(Guid.NewGuid());
             IPreferenceRestriction dayRestriction = new PreferenceRestriction
