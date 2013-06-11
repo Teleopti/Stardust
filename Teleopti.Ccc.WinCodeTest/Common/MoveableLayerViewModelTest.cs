18bf0208be79dce6cf94a3d4d0c0e1d5811762b5
Henke <henrik.andersson@teleopti.com>
2013-06-11 15:18:16 +0200

    Refact MoveableLayer

--- Teleopti.Ccc.WinCodeTest/Common/MoveableLayerViewModelTest.cs
+++ Teleopti.Ccc.WinCodeTest/Common/MoveableLayerViewModelTest.cs
@@ -1,148 +1,386 @@
+using System;
 using System.Linq;
+using System.Reflection;
+using System.Windows;
 using Microsoft.Practices.Composite.Events;
 using NUnit.Framework;
 using Rhino.Mocks;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.WinCode.Common;
 using Teleopti.Ccc.WinCodeTest.Common.Commands;
+using Teleopti.Ccc.WinCodeTest.Helpers;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCodeTest.Common
 {
     [TestFixture]
-    public class MoveableLayerViewModelTest : LayerViewModelTest
+    public class MoveableLayerViewModelTest
     {
     	private readonly TesterForCommandModels _models = new TesterForCommandModels();
         private readonly MockRepository _mocks = new MockRepository();
         private readonly IEventAggregator _eventAggregator=new EventAggregator();
+	    private bool _expectMovePermitted;
+		private MainShiftLayerViewModel _target;
+		private ILayer _layerWithPayload;
+		private IPayload _payload;
+		private IScheduleDay _scheduleDay;
+		private CrossThreadTestRunner _testRunner;
+		private PropertyChangedListener _listener;
+		private IPerson _person;
+		private TesterForCommandModels _testerForCommandModels;
+		private DateTimePeriod _period;
 
-        protected override string LayerModelDescription
-        {
-            get { return UserTexts.Resources.Activity; }
-        }
+		[SetUp]
+		public void Setup()
+		{
+			_expectMovePermitted = true;
+			_listener = new PropertyChangedListener();
+			_testerForCommandModels = new TesterForCommandModels();
+			_layerWithPayload = _mocks.StrictMock<ILayer<IPayload>>();
+			_payload = ActivityFactory.CreateActivity("dfsdf");
+			_scheduleDay = _mocks.StrictMock<IScheduleDay>();
+			_person = PersonFactory.CreatePerson();
+			_period = DateTimeFactory.CreateDateTimePeriod(new DateTime(2008, 12, 5, 0, 0, 0, DateTimeKind.Utc), new DateTime(2008, 12, 6, 0, 0, 0, DateTimeKind.Utc));
+			Expect.Call(_layerWithPayload.Payload).Return(_payload).Repeat.Any();
+			Expect.Call(_layerWithPayload.Period).PropertyBehavior().Return(_period).IgnoreArguments().Repeat.Any();
+			Expect.Call(_scheduleDay.Person).Return(_person).Repeat.Any();
+			Expect.Call(_scheduleDay.DateOnlyAsPeriod).Return(new DateOnlyAsDateTimePeriod(new DateOnly(2008, 12, 5), TimeZoneHelper.CurrentSessionTimeZone)).Repeat.Any();
 
-        protected override LayerViewModel CreateTestInstance(ILayer layer)
-        {
-            return new MainShiftLayerViewModel(layer);
-        }
+			_mocks.ReplayAll();
 
-        protected override bool ExpectMovePermitted
-        {
-            get { return true; }
-        }
+			_layerWithPayload.Period = _period;
+			_target = new MainShiftLayerViewModel(_layerWithPayload);
 
-        protected override bool ExpectIsPayloadChangePermitted
-        {
-            get { return true; }
-        }
+			_testRunner = new CrossThreadTestRunner();
+		}
 
-        [Test]
-        public void VerifyMoveUpDownCommand()
-        {
-            //Check taht the layer actually moves and that the observer is called (for resorting etc.)
+		[Test]
+		public void VerifyMoveUpDownCommand()
+		{
+			//Check taht the layer actually moves and that the observer is called (for resorting etc.)
 
-            #region setup
-            ILayerViewModelObserver observer = _mocks.StrictMock<ILayerViewModelObserver>();
-	        var shift = MainShiftFactory.CreateMainShiftWithThreeActivityLayers();
-	        var ass = new PersonAssignment(new Person(), new Scenario("d"), new DateOnly());
+			#region setup
+			ILayerViewModelObserver observer = _mocks.StrictMock<ILayerViewModelObserver>();
+			var shift = MainShiftFactory.CreateMainShiftWithThreeActivityLayers();
+			var ass = new PersonAssignment(new Person(), new Scenario("d"), new DateOnly());
 #pragma warning disable 612,618
-					ass.SetMainShift(shift);
+			ass.SetMainShift(shift);
 #pragma warning restore 612,618
-            ILayer<IActivity> firstLayer =
-                (from l in shift.LayerCollection
-                 orderby l.OrderIndex
-                 select l).First();
-            MainShiftLayerViewModel model = new MainShiftLayerViewModel(observer, firstLayer, shift, _eventAggregator);
-            #endregion
+			ILayer<IActivity> firstLayer =
+				(from l in shift.LayerCollection
+				 orderby l.OrderIndex
+				 select l).First();
+			MainShiftLayerViewModel model = new MainShiftLayerViewModel(observer, firstLayer, shift, _eventAggregator);
+			#endregion
 
-            #region expectations
-            using (_mocks.Record())
-            {
-                Expect.Call(()=>observer.LayerMovedVertically(model)).Repeat.Twice();
-            }
-            #endregion
-            //Check texts
-            Assert.AreEqual(model.MoveUpCommand.Text, UserTexts.Resources.MoveUp);
-            Assert.AreEqual(model.MoveDownCommand.Text, UserTexts.Resources.MoveDown);
+			#region expectations
+			using (_mocks.Record())
+			{
+				Expect.Call(() => observer.LayerMovedVertically(model)).Repeat.Twice();
+			}
+			#endregion
+			//Check texts
+			Assert.AreEqual(model.MoveUpCommand.Text, UserTexts.Resources.MoveUp);
+			Assert.AreEqual(model.MoveDownCommand.Text, UserTexts.Resources.MoveDown);
 
-            using(_mocks.Playback())
-            {
-                //Execute MoveDown
-                _models.ExecuteCommandModel(model.MoveDownCommand);
+			using (_mocks.Playback())
+			{
+				//Execute MoveDown
+				_models.ExecuteCommandModel(model.MoveDownCommand);
 
-                //Execute MoveUp
-                _models.ExecuteCommandModel(model.MoveUpCommand);
-            }
-        }
+				//Execute MoveUp
+				_models.ExecuteCommandModel(model.MoveUpCommand);
+			}
+		}
 
-        [Test]
-        public void VerifyCanMoveUpDownCommand()
-        {
-            #region setup
+		[Test]
+		public void VerifyCanMoveUpDownCommand()
+		{
+			#region setup
 			var shift = MainShiftFactory.CreateMainShiftWithThreeActivityLayers();
 
-            IOrderedEnumerable<ILayer<IActivity>> layers =
-                from l in shift.LayerCollection
-                orderby l.OrderIndex
-                select l;
+			IOrderedEnumerable<ILayer<IActivity>> layers =
+				from l in shift.LayerCollection
+				orderby l.OrderIndex
+				select l;
 
-            MainShiftLayerViewModel highest = new MainShiftLayerViewModel(null, layers.First(), shift, _eventAggregator);
-            MainShiftLayerViewModel lowest = new MainShiftLayerViewModel(null, layers.Last(), shift, _eventAggregator);
-            #endregion
+			MainShiftLayerViewModel highest = new MainShiftLayerViewModel(null, layers.First(), shift, _eventAggregator);
+			MainShiftLayerViewModel lowest = new MainShiftLayerViewModel(null, layers.Last(), shift, _eventAggregator);
+			#endregion
 
-            Assert.IsTrue(_models.CanExecute(highest.MoveDownCommand), "Can move down because there are other layers below");
-            Assert.IsTrue(_models.CanExecute(lowest.MoveUpCommand), "Can  move up because there are other layers that are higher");
+			Assert.IsTrue(_models.CanExecute(highest.MoveDownCommand), "Can move down because there are other layers below");
+			Assert.IsTrue(_models.CanExecute(lowest.MoveUpCommand), "Can  move up because there are other layers that are higher");
 
-            Assert.IsFalse(_models.CanExecute(highest.MoveUpCommand), "Cannot move up layer because its the highest in the shift");
-            Assert.IsFalse(_models.CanExecute(lowest.MoveDownCommand), "Cannot move down layer because its the lowest in the shift");
-        }
+			Assert.IsFalse(_models.CanExecute(highest.MoveUpCommand), "Cannot move up layer because its the highest in the shift");
+			Assert.IsFalse(_models.CanExecute(lowest.MoveDownCommand), "Cannot move down layer because its the lowest in the shift");
+		}
 
-        [Test]
-        public void VerifyCannotMoveUpOrDownIfNoShift()
-        {
-            MainShiftLayerViewModel modelWithoutParent = new MainShiftLayerViewModel(new MainShiftActivityLayer(ActivityFactory.CreateActivity("test"), Period));
-            TesterForCommandModels testerForCommandModels = new TesterForCommandModels();
-            Assert.IsNull(modelWithoutParent.Parent);
-            Assert.IsFalse(testerForCommandModels.CanExecute(modelWithoutParent.MoveUpCommand));
-            Assert.IsFalse(testerForCommandModels.CanExecute(modelWithoutParent.MoveDownCommand));
-        }
+		[Test]
+		public void VerifyCannotMoveUpOrDownIfNoShift()
+		{
+			MainShiftLayerViewModel modelWithoutParent = new MainShiftLayerViewModel(new MainShiftActivityLayer(ActivityFactory.CreateActivity("test"), _period));
+			TesterForCommandModels testerForCommandModels = new TesterForCommandModels();
+			Assert.IsNull(modelWithoutParent.Parent);
+			Assert.IsFalse(testerForCommandModels.CanExecute(modelWithoutParent.MoveUpCommand));
+			Assert.IsFalse(testerForCommandModels.CanExecute(modelWithoutParent.MoveDownCommand));
+		}
 
-        [Test]
-        public void VerifyDeleteCommandCallsObserver()
-        {
-            #region setup
-            ILayerViewModelObserver observer = _mocks.StrictMock<ILayerViewModelObserver>();
-         
-            MainShift shift = MainShiftFactory.CreateMainShiftWithThreeActivityLayers();
-	        var ass = new PersonAssignment(new Person(), new Scenario("d"), new DateOnly());
+		[Test]
+		public void VerifyDeleteCommandCallsObserver()
+		{
+			#region setup
+			ILayerViewModelObserver observer = _mocks.StrictMock<ILayerViewModelObserver>();
+
+			MainShift shift = MainShiftFactory.CreateMainShiftWithThreeActivityLayers();
+			var ass = new PersonAssignment(new Person(), new Scenario("d"), new DateOnly());
 #pragma warning disable 612,618
-					ass.SetMainShift(shift);
+			ass.SetMainShift(shift);
 #pragma warning restore 612,618
 #pragma warning disable 612,618
-	        var ms = ass.ToMainShift();
+			var ms = ass.ToMainShift();
 #pragma warning restore 612,618
-					ILayer<IActivity> firstLayer =
-		(from l in shift.LayerCollection
-		 orderby l.OrderIndex
-		 select l).First();
-            MainShiftLayerViewModel model = new MainShiftLayerViewModel(observer, firstLayer, ms, _eventAggregator);
-            
+			ILayer<IActivity> firstLayer =
+(from l in shift.LayerCollection
+ orderby l.OrderIndex
+ select l).First();
+			MainShiftLayerViewModel model = new MainShiftLayerViewModel(observer, firstLayer, ms, _eventAggregator);
 
-            #endregion
-            #region expectations
-            using (_mocks.Record())
-            {
-                Expect.Call(()=>observer.RemoveActivity(model));
-            }
-            #endregion
-         
-            using (_mocks.Playback())
-            {
-                //Execute Delete
-                _models.ExecuteCommandModel(model.DeleteCommand);
-            }
-        }
+
+			#endregion
+			#region expectations
+			using (_mocks.Record())
+			{
+				Expect.Call(() => observer.RemoveActivity(model));
+			}
+			#endregion
+
+			using (_mocks.Playback())
+			{
+				//Execute Delete
+				_models.ExecuteCommandModel(model.DeleteCommand);
+			}
+		}
+
+		[Test]
+		public void VerifyCorrectDescription()
+		{
+			Assert.AreEqual(UserTexts.Resources.Activity, _target.LayerDescription);
+		}
+
+
+		[Test]
+		public void VerifyProperties()
+		{
+			_target.SchedulePart = _scheduleDay;
+
+			var payloadFromLayer = (IPayload)_layerWithPayload.Payload;
+
+			Assert.AreEqual(payloadFromLayer.ConfidentialDisplayColor(_person, new DateOnly(2008, 12, 5)), _target.DisplayColor);
+			Assert.AreEqual(payloadFromLayer.ConfidentialDescription(_person, new DateOnly(2008, 12, 5)).Name, _target.Description);
+			Assert.AreEqual(_layerWithPayload.Period, _target.Period);
+			Assert.AreEqual(TimeSpan.FromMinutes(15), _target.Interval);
+			Assert.IsNull(_target.Parent);
+			Assert.IsFalse(_target.IsChanged);
+			Assert.AreEqual(_layerWithPayload, _target.Layer);
+			Assert.AreSame(_scheduleDay, _target.SchedulePart);
+			Assert.IsFalse(_target.CanMoveAll);
+			Assert.AreEqual(false, _target.Opaque);
+			_mocks.BackToRecordAll();
+
+			ILayer testLayer = _mocks.StrictMock<ILayer>();
+
+			_mocks.ReplayAll();
+
+			_target.SchedulePart = _scheduleDay;
+			_target.Layer = testLayer;
+			_target.Interval = TimeSpan.FromHours(1);
+
+			Assert.AreEqual(_scheduleDay, _target.SchedulePart);
+			Assert.AreEqual(testLayer, _target.Layer);
+			Assert.AreEqual(TimeSpan.FromHours(1), _target.Interval);
+		}
+
+		[Test]
+		public void VerifyCanMoveAll()
+		{
+			bool succeeded = false;
+			_target.PropertyChanged += (x, y) =>
+			{
+				succeeded = true;
+				Assert.AreEqual("CanMoveAll", y.PropertyName);
+			};
+			_target.CanMoveAll = true;
+			Assert.IsTrue(succeeded);
+		}
+
+		[Test]
+		public void VerifyIsSelected()
+		{
+			Assert.IsFalse(_target.IsSelected);
+
+			_listener.ListenTo(_target);
+			_target.IsSelected = true;
+			Assert.IsTrue(_listener.HasFired("IsSelected"));
+			Assert.IsTrue(_target.IsSelected);
+
+		}
+
+		[Test]
+		public void VerifyCanSetIsChanged()
+		{
+			bool succeeded = false;
+			_target.PropertyChanged += (x, y) =>
+			{
+				succeeded = true;
+				Assert.AreEqual("IsChanged", y.PropertyName);
+			};
+			_target.IsChanged = true;
+			Assert.IsTrue(succeeded);
+		}
+
+		[Test]
+		public void VerifyCanSetPeriod()
+		{
+			_mocks.BackToRecord(_layerWithPayload);
+			_layerWithPayload.Period = _period.ChangeStartTime(TimeSpan.FromMinutes(-5));
+			_mocks.ReplayAll();
+
+			_listener.ListenTo(_target);
+
+			_target.Period = _period.ChangeStartTime(TimeSpan.FromMinutes(-5));
+			Assert.IsTrue(_listener.HasFired("Period"));
+		}
+
+		[Test]
+		public void VerifyUpdatePeriod()
+		{
+			_mocks.BackToRecord(_layerWithPayload);
+			_layerWithPayload.Period = _period.ChangeStartTime(TimeSpan.FromMinutes(-5));
+			LastCall.Repeat.Twice();
+			_mocks.ReplayAll();
+
+			_target.IsChanged = true;
+			_target.Period = _period.ChangeStartTime(TimeSpan.FromMinutes(-5));
+			_target.UpdatePeriod();
+			Assert.IsFalse(_target.IsChanged);
+		}
+
+		[Test]
+		public void VerifyStartTimeChangedWithSchedulePart()
+		{
+			_testRunner.RunInSTA(
+				delegate
+				{
+					DateTimePeriodPanel panel = getPanel();
+					_target.SchedulePart = _scheduleDay;
+					_target.StartTimeChanged(panel, 1);
+					Assert.AreEqual(_expectMovePermitted, _target.IsChanged);
+				});
+		}
+
+		private DateTimePeriodPanel getPanel()
+		{
+			var panel = new DateTimePeriodPanel();
+			var fieldSize = typeof(UIElement).GetField("_size", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+			fieldSize.SetValue(panel, new System.Windows.Size(10, 2));
+			DateTimePeriodPanel.SetClipPeriod(panel, false);
+			DateTimePeriodPanel.SetDateTimePeriod(panel, _period);
+			return panel;
+		}
+
+		[Test]
+		public void VerifyEndTimeChangedWithSchedulePart()
+		{
+			_testRunner.RunInSTA(
+				delegate
+				{
+					DateTimePeriodPanel panel = getPanel();
+					_target.SchedulePart = _scheduleDay;
+					_target.EndTimeChanged(panel, 1);
+					Assert.AreEqual(_expectMovePermitted, _target.IsChanged);
+				});
+		}
+
+		[Test]
+		public void VerifyTimeChangedWithSchedulePart()
+		{
+			_testRunner.RunInSTA(
+				delegate
+				{
+					DateTimePeriodPanel panel = getPanel();
+					_target.SchedulePart = _scheduleDay;
+					_target.TimeChanged(panel, 1);
+					Assert.AreEqual(_expectMovePermitted, _target.IsChanged);
+				});
+		}
+
+		[Test]
+		public void VerifyStartTimeChangedWithoutSchedulePart()
+		{
+			_testRunner.RunInSTA(
+				delegate
+				{
+					DateTimePeriodPanel panel = getPanel();
+					_target.StartTimeChanged(panel, 1);
+					Assert.AreEqual(_expectMovePermitted, _target.IsChanged);
+				});
+		}
+
+		[Test]
+		public void VerifyEndTimeChangedWithoutSchedulePart()
+		{
+			_testRunner.RunInSTA(
+				delegate
+				{
+					DateTimePeriodPanel panel = getPanel();
+					_target.EndTimeChanged(panel, 1);
+					Assert.AreEqual(_expectMovePermitted, _target.IsChanged);
+				});
+		}
+
+		[Test]
+		public void VerifyTimeChangedWithoutSchedulePart()
+		{
+			_testRunner.RunInSTA(
+				delegate
+				{
+					DateTimePeriodPanel panel = getPanel();
+					_target.TimeChanged(panel, 1);
+					Assert.AreEqual(_expectMovePermitted, _target.IsChanged);
+				});
+		}
+
+		[Test]
+		public void VerifyMoveUpDownReturnsFalseIfParentCollectionIsNull()
+		{
+			Assert.IsFalse(_target.CanMoveUp);
+			Assert.IsFalse(_target.CanMoveDown);
+		}
+
+		[Test]
+		public void VerifyCanExecuteDeleteCommand()
+		{
+			Assert.AreEqual(_testerForCommandModels.CanExecute(_target.DeleteCommand), _target.IsMovePermitted());
+			Assert.AreEqual(_target.DeleteCommand.Text, UserTexts.Resources.Delete);
+		}
+
+		[Test]
+		public void VerifyPeriodElapsedTime()
+		{
+			PropertyChangedListener listener = new PropertyChangedListener().ListenTo(_target);
+			_target.Period = _target.Period.MovePeriod(TimeSpan.FromHours(2));
+			Assert.IsTrue(listener.HasFired("ElapsedTime"));
+			Assert.AreEqual(_target.Period.ElapsedTime(), _target.ElapsedTime);
+
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			_mocks.VerifyAll();
+		}
     }
 }
