8393296261a05f24e548e4e56a04e85e20221eab
David Jonsson <david.jonsson@teleopti.com>
2013-06-11 11:47:31 +0200

    add pester wrapper

--- ccnet/pester/runInstallationTest.bat
+++ ccnet/pester/runInstallationTest.bat
@@ -4,10 +4,10 @@ set rootdir=%~dp0
 SET rootdir=%rootdir:~0,-1%
 set currentPester=2.0.3
 
-call "%rootdir%\runAllTests.bat" "%rootdir%\..\..\Teleopti.Support.Tool\WiseIISConfig\IISConfigCommands"
+call "%rootdir%\run.bat" "%rootdir%\..\..\Teleopti.Support.Tool\WiseIISConfig\IISConfigCommands"
 
 set outputFile=%rootdir%\Pester.%currentPester%\Test.xml
 copy "%outputFile%" "%rootdir%\..\..\%~n0.xml"
 
 ::For test by Mr.Ryding
-notepad "%rootdir%\..\..\nunit.%~n0n.PowerShell.xml"
+notepad "%rootdir%\..\..\nunit.%~n0.PowerShell.xml"
