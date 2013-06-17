5278c4f07bb95a4f246f8e91838a35bec96819fe
Roger Kratz <roger.kratz@teleopti.com>
2013-06-17 15:16:56 +0200

    move up/down now works again.

--- Domain/Scheduling/Assignment/MoveLayerVertical.cs
+++ Domain/Scheduling/Assignment/MoveLayerVertical.cs
@@ -1,17 +1,54 @@
-﻿using Teleopti.Interfaces.Domain;
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Scheduling.Assignment
 {
+	//one valid for mainshiftlayer and overtimeshiftlayer
+	//throw if layer not found?
 	public class MoveLayerVertical : IMoveLayerVertical
 	{
 		public void MoveUp(IPersonAssignment personAssignment, ILayer<IActivity> layer)
 		{
-			throw new System.NotImplementedException();
+			//no need to cast later when agentday is done
+			var msLayer = layer as IMainShiftActivityLayerNew;
+			if (msLayer != null)
+			{
+				var oldLayers = new List<IMainShiftActivityLayerNew>(personAssignment.MainShiftActivityLayers);
+				var indexOfMainShiftLayer = oldLayers.IndexOf(msLayer);
+				oldLayers.Remove(msLayer);
+				oldLayers.Insert(indexOfMainShiftLayer-1, msLayer);
+				personAssignment.SetMainShiftLayers(oldLayers, personAssignment.ShiftCategory);
+			}
+
+			//no need to cast later when agentday is done
+			var overLayer = layer as IOvertimeShiftActivityLayer;
+			if (overLayer != null)
+			{
+				var shift = (IOvertimeShift) overLayer.Parent;
+				shift.LayerCollection.MoveUpLayer(layer);
+			}
 		}
 
 		public void MoveDown(IPersonAssignment personAssignment, ILayer<IActivity> layer)
 		{
-			throw new System.NotImplementedException();
+			//no need to cast later when agentday is done
+			var msLayer = layer as IMainShiftActivityLayerNew;
+			if (msLayer != null)
+			{
+				var oldLayers = new List<IMainShiftActivityLayerNew>(personAssignment.MainShiftActivityLayers);
+				var indexOfMainShiftLayer = oldLayers.IndexOf(msLayer);
+				oldLayers.Remove(msLayer);
+				oldLayers.Insert(indexOfMainShiftLayer + 1, msLayer);
+				personAssignment.SetMainShiftLayers(oldLayers, personAssignment.ShiftCategory);
+			}
+
+			//no need to cast later when agentday is done
+			var overLayer = layer as IOvertimeShiftActivityLayer;
+			if (overLayer != null)
+			{
+				var shift = (IOvertimeShift)overLayer.Parent;
+				shift.LayerCollection.MoveDownLayer(layer);
+			}
 		}
 	}
 }
