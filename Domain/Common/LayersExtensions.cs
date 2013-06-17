7a5008ed6ab6bfc8e90fbe7c71412508f23e866f
Roger Kratz <roger.kratz@teleopti.com>
2013-06-17 17:10:26 +0200

    adding extension method IEnumerable<ILayer<IPayload>>.Period()

---
+++ Domain/Common/LayersExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Common
+{
+	public static class LayersExtensions
+	{
+		 public static DateTimePeriod? Period(this IEnumerable<ILayer<IPayload>> layers)
+		 {
+			 DateTimePeriod? ret = null;
+			 foreach (var layer in layers)
+			 {
+				 if (layer != null)
+				 {
+					 ret = DateTimePeriod.MaximumPeriod(ret, layer.Period);
+				 }
+			 }
+			 return ret;
+		 }
+	}
+}
