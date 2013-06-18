dec287ad9db4716c56000061bb78d0fd24bcbeb4
ola@teleopti.com <ola@teleopti.com>
2013-06-18 14:23:11 +0200

    bug #23883
    --HG--
    branch : Release 7.4.384

--- Database/TeleoptiAnalytics/Programmability/03StoredProcedures/stage.etl_stg_schedule_updated_special_load.sql
+++ Database/TeleoptiAnalytics/Programmability/03StoredProcedures/stage.etl_stg_schedule_updated_special_load.sql
@@ -9,33 +9,35 @@ BEGIN
 
 SET NOCOUNT ON;
 
---@variables used to create local dates
-declare @minutesPerInterval int
-declare @maxInterval int
-select @maxInterval=max(interval_id) from mart.dim_interval
-select @minutesPerInterval=1440/@maxInterval
-
 --create local person periods dates
 INSERT INTO Stage.stg_schedule_updated_personLocal
 SELECT DISTINCT
 person_id				= dp.person_id,
 time_zone_id			= dp.time_zone_id,
 person_code				= dp.person_code,
-valid_from_date_local	= dateadd(MINUTE,UpDown.conversion*btz.interval_id*@minutesPerInterval,dp.valid_from_date),
-valid_to_date_local		= dateadd(MINUTE,UpDown.conversion*btz.interval_id*@minutesPerInterval,dp.valid_to_date)
+valid_from_date_local	= dd.date_date,
+valid_to_date_local		= '2059-12-31'
 FROM mart.dim_date dd
 INNER JOIN mart.bridge_time_zone btz
 	ON	btz.local_date_id = dd.date_id
-INNER JOIN mart.v_time_zone_convertUpDown UpDown
-	ON UpDown.time_zone_id = btz.time_zone_id
-INNER JOIN mart.dim_interval di
-	ON btz.local_interval_id = di.interval_id 
 INNER JOIN mart.dim_person		dp
 	ON btz.time_zone_id =	dp.time_zone_id
 	AND dp.valid_from_date_id = btz.date_id
 	AND dp.valid_from_interval_id = btz.interval_id
 INNER JOIN Stage.stg_schedule_changed stg
 	ON stg.person_code = dp.person_code
+
+UPDATE Stage.stg_schedule_updated_personLocal 
+SET valid_to_date_local		= dd.date_date 
+FROM mart.dim_date dd
+INNER JOIN mart.bridge_time_zone btz
+	ON	btz.local_date_id = dd.date_id
+INNER JOIN mart.dim_person	dp
+	ON btz.time_zone_id =	dp.time_zone_id
+	AND dp.valid_to_date_id = btz.date_id
+	AND dp.valid_to_interval_id = btz.interval_id
+INNER JOIN Stage.stg_schedule_updated_personLocal stg
+	ON stg.person_id = dp.person_id	
 
 --create utc shift_date_id for local agent schedule day
 INSERT INTO Stage.stg_schedule_updated_ShiftStartDateUTC
