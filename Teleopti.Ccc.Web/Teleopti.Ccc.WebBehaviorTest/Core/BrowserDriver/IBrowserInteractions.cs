19a7a18e6ca26a54d4988c314edb1de430cd8492
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-06-18 09:50:53 +0200

    moving out JSEncode extension so it may be used in alternative interactions implementaions

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Core/BrowserDriver/IBrowserInteractions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Core/BrowserDriver/IBrowserInteractions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace Teleopti.Ccc.WebBehaviorTest.Core.BrowserDriver
 {
@@ -18,59 +17,5 @@ namespace Teleopti.Ccc.WebBehaviorTest.Core.BrowserDriver
 		void AssertJavascriptResultContains(string javascript, string text);
 		void DumpInfo(Action<string> writer);
 		void DumpUrl(Action<string> writer);
-	}
-
-	public static class BrowserInteractionsExtensions
-	{
-		public static void Javascript(this IBrowserInteractions interactions, string javascript, params object[] args)
-		{
-			interactions.Javascript(string.Format(javascript, args));
-		}
-
-		public static void Javascript(this IBrowserInteractions interactions, Action<Action<string>> javascript, params object[] args)
-		{
-			var builder = new StringBuilder();
-			javascript.Invoke(s => builder.Append(s));
-			interactions.Javascript(string.Format(builder.ToString(), args));
-		}
-
-		public static void AssertExists(this IBrowserInteractions interactions, string selector, params object[] args)
-		{
-			interactions.AssertExists(string.Format(selector, args));
-		}
-
-		public static void AssertInputValueUsingJQuery(this IBrowserInteractions interactions, string selector, string value)
-		{
-			interactions.AssertJavascriptResultContains("return $('" + selector + "').val();", value);
-		}
-
-		public static void AssertVisibleUsingJQuery(this IBrowserInteractions interactions, string selector)
-		{
-			var js = string.Format("return $('{0}').filter(\":visible\").length > 0 ? 'visible' : 'not visible';", selector.JSEncode());
-			interactions.AssertJavascriptResultContains(js, "visible");
-		}
-
-		public static void SelectOptionByTextUsingJQuery(this IBrowserInteractions interactions, string selector, string text)
-		{
-			var selectSelector = selector + ":enabled";
-			var optionSelector = string.Format(selectSelector + " option:contains('{0}')", text);
-			interactions.AssertExists(selectSelector);
-			interactions.AssertExists(optionSelector);
-			interactions.Javascript("$(\"{0}\").val($(\"{1}\").val());", selectSelector.JSEncode(), optionSelector.JSEncode());
-			interactions.Javascript("$(\"{0}\").change();", selectSelector.JSEncode());
-		}
-
-		public static void TypeTextIntoInputTextUsingJQuery(this IBrowserInteractions interactions, string selector, string text)
-		{
-			selector = selector + ":enabled";
-			interactions.AssertExists(selector);
-			interactions.Javascript("$(\"{0}\").val(\"{1}\");", selector.JSEncode(), text.JSEncode());
-			interactions.Javascript("$(\"{0}\").change();", selector.JSEncode());
-		}
-
-		private static string JSEncode(this string value)
-		{
-			return System.Web.HttpUtility.JavaScriptStringEncode(value);
-		}
 	}
 }
