2563b79c4bc9810ed4643037768eeef3ebfdd04d
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-06-18 13:09:03 +0200

    fast chrome, when disabling automatic proxy setting detection! and seperate profile for testrun...

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Core/BrowserDriver/CoypuImpl/CoypuBrowserActivator.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Core/BrowserDriver/CoypuImpl/CoypuBrowserActivator.cs
@@ -1,5 +1,8 @@
-﻿using Coypu;
+﻿using System;
+using Coypu;
 using Coypu.Drivers.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace Teleopti.Ccc.WebBehaviorTest.Core.BrowserDriver.CoypuImpl
 {
@@ -14,11 +17,14 @@ namespace Teleopti.Ccc.WebBehaviorTest.Core.BrowserDriver.CoypuImpl
 					AppHost = "about:blank",
 					Port = 80,
 					SSL = false,
-					Driver = typeof(SeleniumWebDriver),
+					Driver = typeof(ChromeWebDriverWithProfile),
+					//Driver = typeof(SeleniumWebDriver),
 					//Browser = Coypu.Drivers.Browser.InternetExplorer,
 					//Browser = Coypu.Drivers.Browser.Firefox,
-					Browser = Coypu.Drivers.Browser.Chrome
+					//Browser = Coypu.Drivers.Browser.Chrome
 				};
+			// to get chrome to be fast, disable automatic detection of proxy settings. odd, but it works:
+			// http://stackoverflow.com/questions/16179808/chromedriver-is-extremely-slow-on-a-specific-machine-using-selenium-grid-and-ne
 			_browser = new BrowserSession(sessionConfiguration);
 		}
 
@@ -47,4 +53,21 @@ namespace Teleopti.Ccc.WebBehaviorTest.Core.BrowserDriver.CoypuImpl
 			return new CoypuBrowserInteractions(_browser);
 		}
 	}
+
+	public class ChromeWebDriverWithProfile : SeleniumWebDriver
+	{
+		public ChromeWebDriverWithProfile(Coypu.Drivers.Browser browser)
+			: base(CustomProfileDriver(), browser)
+		{
+		}
+
+		private static ChromeDriver CustomProfileDriver()
+		{
+			var profilePath = System.IO.Path.Combine(Environment.CurrentDirectory, "ChromeWebDriverProfile");
+			var options = new ChromeOptions();
+			options.AddArgument(string.Format("--user-data-dir={0}", profilePath));
+			return new ChromeDriver(options);
+		}
+	}
+
 }
