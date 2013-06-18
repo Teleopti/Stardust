f0c5e263b26424c78a0d65d11fdd3579836a1719
Roger Kratz <roger.kratz@teleopti.com>
2013-06-18 12:53:41 +0200

    adding extension method to create projection from IEnumerable<IMainShiftActivityLayerNew>

--- Domain/Common/LayersExtensions.cs
+++ Domain/Common/LayersExtensions.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections.Generic;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Common
 {
 	public static class LayersExtensions
 	{
-		 public static DateTimePeriod? Period(this IEnumerable<ILayer<IPayload>> layers)
-		 {
-			 DateTimePeriod? ret = null;
-			 foreach (var layer in layers)
-			 {
-				 if (layer != null)
-				 {
-					 ret = DateTimePeriod.MaximumPeriod(ret, layer.Period);
-				 }
-			 }
-			 return ret;
-		 }
+		public static DateTimePeriod? Period(this IEnumerable<ILayer<IPayload>> layers)
+		{
+			DateTimePeriod? ret = null;
+			foreach (var layer in layers)
+			{
+				if (layer != null)
+				{
+					ret = DateTimePeriod.MaximumPeriod(ret, layer.Period);
+				}
+			}
+			return ret;
+		}
+
+		public static IVisualLayerCollection CreateProjection(this IMainShiftActivityLayerNew[] layers)
+		{
+			var projSvc = new VisualLayerProjectionService(null);
+			projSvc.Add(layers, new VisualLayerFactory());
+			return projSvc.CreateProjection();
+		}
+
+		public static IVisualLayerCollection CreateProjection(this IMainShiftActivityLayerNew layer)
+		{
+			return CreateProjection(new[] {layer});
+		}
 	}
 }
---
+++ DomainTest/Common/LayersExtensionsProjectionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Ccc.Domain.Common;
+
+namespace Teleopti.Ccc.DomainTest.Common
+{
+	[TestFixture]
+	public class LayersExtensionsProjectionTest
+	{
+		[Test]
+		public void ShouldReturnProjectionForOneLayer()
+		{
+			var proj = new MainShiftActivityLayerNew(new Activity("sdf"), new DateTimePeriod(2000, 1, 1, 2000, 1, 2)).CreateProjection();
+			proj.Single().Period.Should().Be.EqualTo(new DateTimePeriod(2000, 1, 1, 2000, 1, 2));
+		}
+
+		[Test]
+		public void ShouldReturnProjectionForMultipleLayers()
+		{
+			var start = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			var proj = new[]
+				{
+					new MainShiftActivityLayerNew(new Activity("1"), new DateTimePeriod(start, start.AddHours(8))),
+					new MainShiftActivityLayerNew(new Activity("2"), new DateTimePeriod(start.AddHours(4), start.AddHours(5)))
+				}.CreateProjection();
+			proj.Count().Should().Be.EqualTo(3);
+		}
+
+		[Test]
+		public void ShouldReturnEmptyProjectionForZeroLayers()
+		{
+			new IMainShiftActivityLayerNew[0].CreateProjection().Should().Be.Empty();
+		}
+	}
+}
