c7ff41db0371792b1749d2e7d5655ceea587d8d5
Roger Kratz <roger.kratz@teleopti.com>
2013-06-25 10:44:04 +0200

    removing personalshift stuff from databaseconverter

--- DataBaseConverterTest/EntityMapper/AgentAssignmentMapperTest.cs
+++ DataBaseConverterTest/EntityMapper/AgentAssignmentMapperTest.cs
@@ -105,11 +105,12 @@ namespace Teleopti.Ccc.DatabaseConverterTest.EntityMapper
         [Test]
         public void CanMapAgentPersonalAssignment()
         {
-            _agDay.AgentDayAssignment.AddFillup(_agdFactory.FillUpShift(), _agdFactory.ScheduleType);
+	        var fillup = _agdFactory.FillUpShift();
+            _agDay.AgentDayAssignment.AddFillup(fillup, _agdFactory.ScheduleType);
             AgentAssignmentMapper agAssMapper = new AgentAssignmentMapper(mappedObjectPair, (TimeZoneInfo.Utc));
             IPersonAssignment newAgAss = agAssMapper.Map(_agDay);
             Assert.IsNotNull(newAgAss.Person);
-            Assert.AreEqual(1, newAgAss.PersonalShiftCollection.Count);
+            Assert.AreEqual(fillup.LayerCollection.Count, newAgAss.PersonalLayers.Count());
         }
 
         /// <summary>
@@ -145,13 +146,15 @@ namespace Teleopti.Ccc.DatabaseConverterTest.EntityMapper
         [Test]
         public void CanMapAgentPersonalAssignmentWithOvertime()
         {
+					//ett övertidslager och två personalshiftlager
             SetOvertime();
-            _agDay.AgentDayAssignment.AddFillup(_agdFactory.FillUpShift(), _agdFactory.ScheduleType);
+	        var fillup = _agdFactory.FillUpShift();
+            _agDay.AgentDayAssignment.AddFillup(fillup, _agdFactory.ScheduleType);
             AgentAssignmentMapper agAssMapper = new AgentAssignmentMapper(mappedObjectPair, (TimeZoneInfo.Utc));
             IPersonAssignment newAgAss = agAssMapper.Map(_agDay);
             Assert.IsNotNull(newAgAss.Person);
-            Assert.AreEqual(1, newAgAss.PersonalShiftCollection.Count);
-            Assert.AreEqual(1, newAgAss.OvertimeShiftCollection.Count);
+            Assert.AreEqual(2, newAgAss.PersonalLayers.Count());
+            Assert.AreEqual(1, newAgAss.OvertimeShiftCollection.Single().LayerCollection.Count);
         }
     }
 }
