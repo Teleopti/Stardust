cc6738014bde70f351486bd64cc793eeb7f4a05c
tamasb@teleopti696.toptinet.teleopti.com <tamasb@teleopti696.toptinet.teleopti.com>
2013-06-28 08:11:37 +0200

    simple string encryption with form auth ticket

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/RequestContext/StringEncryption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Teleopti.Ccc.Web.Core.RequestContext
+{
+	public class StringEncryption
+	{
+
+	public string Encrypt(string stringToEncrypt)
+    {
+        FormsAuthenticationTicket tk = new FormsAuthenticationTicket(stringToEncrypt, false, 600);
+        // returns encrypted string
+        return FormsAuthentication.Encrypt(tk); 
+    }
+
+
+	public string Decrypt(string encryptedString)
+    {
+        FormsAuthenticationTicket tk= FormsAuthentication.Decrypt(encryptedString);
+        return tk.Name;
+    }
+}
