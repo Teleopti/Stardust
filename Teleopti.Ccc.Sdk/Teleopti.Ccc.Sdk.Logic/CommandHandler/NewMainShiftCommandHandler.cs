fb060d631983af1a89854a90705f9ea64f618f79
Roger Kratz <roger.kratz@teleopti.com>
2013-07-05 10:45:54 +0200

    made sdk compile

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/NewMainShiftCommandHandler.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/NewMainShiftCommandHandler.cs
@@ -60,11 +60,7 @@ namespace Teleopti.Ccc.Sdk.Logic.CommandHandler
 	            var mainShiftLayers = _mainActivityLayerAssembler.DtosToDomainEntities(command.LayerCollection);
                
 				IPersonAssignment currentAss = scheduleDay.AssignmentHighZOrder();
-			//unsure about this...
-	            foreach (var period in mainShiftLayers.PeriodBlocks())
-	            {
-								scheduleDay.MergePersonalShiftsToOneAssignment(period); 
-	            }
+
 				if (currentAss == null)
 				{
 					currentAss = new PersonAssignment(scheduleDay.Person, scheduleDay.Scenario, scheduleDay.DateOnlyAsPeriod.DateOnly);
