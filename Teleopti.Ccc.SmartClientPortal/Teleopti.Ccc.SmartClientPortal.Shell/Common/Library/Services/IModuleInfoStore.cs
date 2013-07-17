5b5cb647b332f1bb7c06536b8179c59fa6729c20
Robin Karlsson <robin.karlsson@teleopti.com>
2013-07-17 13:42:14 +0200

    Including required files in project instead of separate compiled version

---
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Common/Library/Services/IModuleInfoStore.cs
@@ -0,0 +1,7 @@
+﻿namespace Teleopti.Ccc.SmartClientPortal.Shell.Common.Library.Services
+{
+    public interface IModuleInfoStore
+    {
+        string GetModuleListXml();
+    }
+}
---
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Common/Library/Services/ProfileCatalogModuleInfoStore.cs
@@ -0,0 +1,10 @@
+﻿namespace Teleopti.Ccc.SmartClientPortal.Shell.Common.Library.Services
+{
+    public class ProfileCatalogModuleInfoStore : IModuleInfoStore
+    {
+        public string GetModuleListXml()
+        {
+            return null;
+        }
+    }
+}
