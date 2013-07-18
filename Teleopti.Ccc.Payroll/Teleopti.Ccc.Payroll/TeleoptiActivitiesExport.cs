04f47d07a10812440b3e61d9eb4c94bb7b8a1423
Talha Muhammad <talha.muhammad@teleopti.com>
2013-07-18 13:15:04 +0200

    Add Teleopti Activities Export in Payroll export Wizard

---
+++ Teleopti.Ccc.Payroll/Teleopti.Ccc.Payroll/TeleoptiActivitiesExport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using Teleopti.Ccc.Sdk.Common.Contracts;
+using Teleopti.Ccc.Sdk.Common.DataTransferObject;
+using log4net;
+
+namespace Teleopti.Ccc.Payroll
+{
+    public class TeleoptiActivitiesExport : IPayrollExportProcessorWithFeedback
+    {
+        private static readonly PayrollFormatDto Format = new PayrollFormatDto(new Guid("{0e531434-a463-4ab6-8bf1-4696ddc9b296}"), "Teleopti Activities Export");
+        public PayrollFormatDto PayrollFormat
+        {
+            get { return Format; }
+        }
+
+        public IXPathNavigable ProcessPayrollData(ITeleoptiSchedulingService schedulingService,
+                                                  ITeleoptiOrganizationService organizationService, PayrollExportDto payrollExport)
+        {
+            return null;
+        }
+
+        public IPayrollExportFeedback PayrollExportFeedback { get; set; }
+
+        
+    }
+}
