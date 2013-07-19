7c7f1656ccf71d05c959a71be497ab0103b80759
Xianwei Shen <xianwei.shen@teleopti.com>
2013-07-19 12:15:14 +0200

    fix merge.

--- Domain/Scheduling/TeamBlock/TeamBlockScheduler.cs
+++ Domain/Scheduling/TeamBlock/TeamBlockScheduler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling.TeamBlock.WorkShiftCalculation;
 using Teleopti.Interfaces.Domain;
