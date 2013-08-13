1dc3bcebe894358acaf18a112ea171180aceaf6b
Robin Karlsson <robin.karlsson@teleopti.com>
2013-08-13 11:08:51 +0200

    We don't have to load all people actually. Sweet!

--- Domain/Scheduling/ResourceCalculateSkillCommand.cs
+++ Domain/Scheduling/ResourceCalculateSkillCommand.cs
@@ -16,17 +16,15 @@ namespace Teleopti.Ccc.Domain.Scheduling
 	{
 		private readonly ISchedulingResultStateHolder _schedulingResultStateHolder;
 		private readonly ISkillLoaderDecider _skillLoaderDecider;
-		private readonly IPersonRepository _personRepository;
 		private readonly ISkillDayLoadHelper _skillDayLoadHelper;
 		private readonly ISkillRepository _skillRepository;
 		private readonly IWorkloadRepository _workloadRepository;
 		private readonly IScheduledResourcesReadModelStorage _storage;
 
-		public ResourceCalculateSkillCommand(IPersonRepository personRepository, ISkillRepository skillRepository, IWorkloadRepository workloadRepository, IScheduledResourcesReadModelStorage storage, ISchedulingResultStateHolder schedulingResultStateHolder, ISkillLoaderDecider skillLoaderDecider, ISkillDayLoadHelper skillDayLoadHelper)
+		public ResourceCalculateSkillCommand(ISkillRepository skillRepository, IWorkloadRepository workloadRepository, IScheduledResourcesReadModelStorage storage, ISchedulingResultStateHolder schedulingResultStateHolder, ISkillLoaderDecider skillLoaderDecider, ISkillDayLoadHelper skillDayLoadHelper)
 		{
 			_schedulingResultStateHolder = schedulingResultStateHolder;
 			_skillLoaderDecider = skillLoaderDecider;
-			_personRepository = personRepository;
 			_skillDayLoadHelper = skillDayLoadHelper;
 			_skillRepository = skillRepository;
 			_workloadRepository = workloadRepository;
@@ -37,14 +35,11 @@ namespace Teleopti.Ccc.Domain.Scheduling
 		{
 			var dateOnlyPeriod = period.ToDateOnlyPeriod(skill.TimeZone);
 
-			_schedulingResultStateHolder.PersonsInOrganization = _personRepository.FindPeopleInOrganization(dateOnlyPeriod, true);
-
 			var skills = _skillRepository.FindAllWithSkillDays(dateOnlyPeriod);
 			var allSkills = skills.ToList();
 			_workloadRepository.LoadAll();
 
 			_skillLoaderDecider.Execute(scenario, period, skill);
-			_skillLoaderDecider.FilterPeople(_schedulingResultStateHolder.PersonsInOrganization);
 			_skillLoaderDecider.FilterSkills(skills);
 
 			if (skills != null)
