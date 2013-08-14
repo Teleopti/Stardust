b5f420d42457d304d8381a1747899698b02cc6f3
tamasb@teleopti696.toptinet.teleopti.com <tamasb@teleopti696.toptinet.teleopti.com>
2013-08-14 13:49:35 +0200

    more

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Core/PublishedScheduleSpecification.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.PersonScheduleDayReadModel;
+using Teleopti.Ccc.Domain.Security.AuthorizationData;
+using Teleopti.Ccc.Domain.Specification;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Common.DataProvider;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.Areas.Anywhere.Core
+{
+	public class PublishedScheduleSpecification : Specification<PersonScheduleDayReadModel>
+	{
+		private readonly ISchedulePersonProvider _schedulePersonProvider;
+		private readonly DateTime _date;
+		private readonly IEnumerable<IPerson> _persons;
+
+		public PublishedScheduleSpecification(ISchedulePersonProvider schedulePersonProvider, Guid teamId, DateTime date)
+		{
+			_schedulePersonProvider = schedulePersonProvider;
+			_date = date;
+			_persons = _schedulePersonProvider.GetPermittedPersonsForTeam(new DateOnly(date), teamId,
+			                                                              DefinedRaptorApplicationFunctionPaths
+				                                                              .SchedulesAnywhere);
+		}
+
+		public override bool IsSatisfiedBy(PersonScheduleDayReadModel obj)
+		{
+			var person = (from p in _persons where (p.Id == obj.PersonId) select p).FirstOrDefault();
+
+			if (person != null && isSchedulePublished(_date, person))
+				return true;
+			return false;
+		}
+
+		private static bool isSchedulePublished(DateTime date, IPerson person)
+		{
+			var workflowControlSet = person.WorkflowControlSet;
+			return workflowControlSet.SchedulePublishedToDate.HasValue &&
+			       workflowControlSet.SchedulePublishedToDate.Value.AddDays(1) > date;
+		}
+	}
+}
