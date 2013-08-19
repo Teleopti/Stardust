249eb5c249fab3edabcb99aa2824b106caca98cc
Asad MIrza <asad.mirza@teleopti.com>
2013-08-19 14:20:57 +0200

    Added code to extract shift categroy information from schedule day

---
+++ Teleopti.Ccc.WinCodeTest/Scheduler/ShiftCategoryInfo/ScheduleDayToShiftCategoryMapperTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.WinCode.Scheduling.ShiftCategoryInfo;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCodeTest.Scheduler.ShiftCategoryInfo
+{
+    [TestFixture]
+    public class ScheduleDayToShiftCategoryMapperTest
+    {
+        private MockRepository _mocks;
+        private IScheduleDay _scheduleDay1;
+        private IScheduleDay _scheduleDay2;
+        private IPersonAssignment _personAssignment1;
+        private IPersonAssignment _personAssignment2;
+        private ShiftCategory _shiftCategory1;
+        private ShiftCategory _shiftCategory2;
+        private IPerson _person1;
+        private IPerson _person2;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocks = new MockRepository();
+
+            _scheduleDay1 = _mocks.StrictMock<IScheduleDay>();
+            _scheduleDay2 = _mocks.StrictMock<IScheduleDay>();
+            _personAssignment1 = _mocks.StrictMock<IPersonAssignment>();
+            _personAssignment2 = _mocks.StrictMock<IPersonAssignment>();
+            _shiftCategory1 = new ShiftCategory("Test1");
+            _shiftCategory2 = new ShiftCategory("Test2");
+            _person1 = PersonFactory.CreatePerson("asad");
+            _person2 = PersonFactory.CreatePerson("ali");
+
+        }
+
+        [Test]
+        public void CheckIfCorrectListIsMapped()
+        {
+            setUpData();
+
+            var returnList =
+                ScheduleDayToShiftCategoryMapper.MapScheduleDay(new List<IScheduleDay> {_scheduleDay1, _scheduleDay2});
+            
+            Assert.AreEqual(returnList.Count,2 );
+        }
+
+        [Test]
+        public void CheckIfShiftCategoryIsMapped()
+        {
+            setUpData();
+
+            var returnList =
+                ScheduleDayToShiftCategoryMapper.MapScheduleDay(new List<IScheduleDay> { _scheduleDay1, _scheduleDay2 });
+
+            Assert.AreEqual( returnList[0].ShiftCategoryValue.Description , _shiftCategory1.Description);
+            Assert.AreEqual(returnList[1].ShiftCategoryValue.Description, _shiftCategory2.Description);
+        }
+        
+        [Test]
+        public void CheckIfDateIsMapped()
+        {
+            setUpData();
+
+            var returnList =
+                ScheduleDayToShiftCategoryMapper.MapScheduleDay(new List<IScheduleDay> { _scheduleDay1, _scheduleDay2 });
+
+            Assert.AreEqual( returnList[0].DateOnlyValue  , DateOnly.Today );
+            Assert.AreEqual(returnList[1].DateOnlyValue, DateOnly.Today);
+        }
+        [Test]
+        public void CheckIfPersonIsMapped()
+        {
+            setUpData();
+
+            var returnList =
+                ScheduleDayToShiftCategoryMapper.MapScheduleDay(new List<IScheduleDay> { _scheduleDay1, _scheduleDay2 });
+
+            Assert.AreEqual( returnList[0].PersonValue   , _person1  );
+            Assert.AreEqual(returnList[1].PersonValue , _person2 );
+        }
+
+        private void  setUpData()
+        {
+            using (_mocks.Record())
+            {
+                expectCalls(_scheduleDay1, _personAssignment1, _shiftCategory1, _person1);
+                expectCalls(_scheduleDay2, _personAssignment2, _shiftCategory2, _person2);
+            }
+        }
+
+
+        private static void expectCalls(IScheduleDay scheduleDay1, IPersonAssignment personAssignment1,
+                                        IShiftCategory shiftCategory1, IPerson person)
+        {
+            Expect.Call(scheduleDay1.PersonAssignment()).Return(personAssignment1);
+            Expect.Call(personAssignment1.ShiftCategory).Return(shiftCategory1);
+            Expect.Call(scheduleDay1.DateOnlyAsPeriod)
+                  .Return(new DateOnlyAsDateTimePeriod(DateOnly.Today, TimeZoneInfo.Utc));
+            Expect.Call(scheduleDay1.Person).Return(person);
+        }
+    }
+}
---
+++ Teleopti.Ccc.WinCodeTest/Scheduler/ShiftCategoryInfo/ShiftCategoryStructureTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.WinCode.Scheduling.ShiftCategoryInfo;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCodeTest.Scheduler.ShiftCategoryInfo
+{
+    [TestFixture]
+    public class ShiftCategoryStructureTest
+    {
+        private ShiftCategoryStructure _target;
+        private IScheduleDay _scheduleDay;
+        private MockRepository _mocks;
+        private IPersonAssignment _personAssignment;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocks = new MockRepository();
+            _scheduleDay = _mocks.StrictMock<IScheduleDay>();
+            _personAssignment = _mocks.StrictMock<IPersonAssignment>();
+        }
+
+        [Test]
+        public void VerifyValueBeingSet()
+        {
+            var shiftCategory = new Domain.Scheduling.ShiftCategory("test");
+            var person = PersonFactory.CreatePerson("person1");
+            using (_mocks.Record())
+            {
+                Expect.Call(_scheduleDay.PersonAssignment()).Return(_personAssignment);
+                Expect.Call(_personAssignment.ShiftCategory).Return(shiftCategory);
+                Expect.Call(_scheduleDay.DateOnlyAsPeriod)
+                      .Return(new DateOnlyAsDateTimePeriod(DateOnly.Today, TimeZoneInfo.Utc));
+
+                Expect.Call(_scheduleDay.Person).Return(person);
+            }
+            _target = new ShiftCategoryStructure(_scheduleDay);
+            Assert.AreEqual(_target.ShiftCategoryValue.Description, shiftCategory.Description);
+            Assert.AreEqual(_target.DateOnlyValue, DateOnly.Today);
+            Assert.AreEqual(_target.PersonValue, person);
+
+        }
+    }
+}
