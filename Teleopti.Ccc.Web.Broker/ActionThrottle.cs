e8d2e4b327eda3a9828b03e52e3ec9b710ac39fd
Tina Hammarstrom <none@none>
2013-08-21 09:18:52 +0200

    Added actionscheduler stuff for throttling messages in message broker

---
+++ Teleopti.Ccc.Web.Broker/ActionThrottle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Teleopti.Ccc.Web.Broker
+{
+	public class ActionThrottle : IDisposable, IActionScheduler
+	{
+		private readonly BlockingCollection<Action> actions = new BlockingCollection<Action>();
+		private readonly int actionDelay;
+		private readonly CancellationTokenSource cancellation = new CancellationTokenSource();
+
+		public ActionThrottle(int actionsPerSecond)
+		{
+			actionDelay = 1000 / actionsPerSecond;
+		}
+
+		public void Do(Action action)
+		{
+			actions.Add(action);
+		}
+
+		public void Start()
+		{
+			Task.Factory.StartNew(() =>
+				{
+					foreach (var action in actions.GetConsumingEnumerable(cancellation.Token))
+					{
+						action();
+						Thread.Sleep(actionDelay);
+					}
+				});
+		}
+
+		public void Dispose()
+		{
+			cancellation.Cancel();
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/ActionImmediateTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Broker;
+
+namespace Teleopti.Ccc.WebTest.Core.MessageBroker
+{
+	[TestFixture]
+	public class ActionImmediateTest
+	{
+		[Test]
+		public void ShouldExecuteAction()
+		{
+			var executed = false;
+			var target = new ActionImmediate();
+
+			target.Do(() => { executed = true; });
+
+			executed.Should().Be.True();
+		}
+	}
+}
