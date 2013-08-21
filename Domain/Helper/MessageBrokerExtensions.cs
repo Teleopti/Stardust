848d88c14c5fafcec629f7bca25aece3d585ca17
Robin Karlsson <robin.karlsson@teleopti.com>
2013-08-21 09:25:30 +0200

    Removing unused methods in broker

--- Domain/Helper/MessageBrokerExtensions.cs
+++ Domain/Helper/MessageBrokerExtensions.cs
@@ -23,20 +23,6 @@ namespace Teleopti.Ccc.Domain.Helper
 		}
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
-		public static void RegisterEventSubscription(this IMessageBrokerListener broker, EventHandler<EventMessageArgs> eventMessageHandler, Guid domainObjectId, Type domainObjectType)
-		{
-			var detail = GetDetail();
-			broker.RegisterEventSubscription(detail.Datasource, detail.BusinessUnitId, eventMessageHandler, domainObjectId, domainObjectType);
-		}
-
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
-		public static void RegisterEventSubscription(this IMessageBrokerListener broker, EventHandler<EventMessageArgs> eventMessageHandler, Guid referenceObjectId, Type referenceObjectType, Type domainObjectType)
-		{
-			var detail = GetDetail();
-			broker.RegisterEventSubscription(detail.Datasource, detail.BusinessUnitId, eventMessageHandler, referenceObjectId, referenceObjectType, domainObjectType);
-		}
-
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
 		public static void RegisterEventSubscription(this IMessageBrokerListener broker, EventHandler<EventMessageArgs> eventMessageHandler, Type domainObjectType, DateTime startDate, DateTime endDate)
 		{
 			var detail = GetDetail();
