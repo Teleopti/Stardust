b9e0160ff3c22fed3883bf5b53f2b822bfbb7fcf
Asad MIrza <asad.mirza@teleopti.com>
2013-08-23 11:10:52 +0200

    Added the control of shift distribution analysis grid

---
+++ Win/Scheduling/PropertyPanel/ShiftDistributionAnalysisControl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Teleopti.Ccc.Win.Common;
+
+namespace Teleopti.Ccc.Win.Scheduling.PropertyPanel
+{
+    public class ShiftDistributionAnalysisControl : BaseUserControl
+    {
+        private TableLayoutPanel shiftDistributiontableLayoutPanel;
+        
+        public ShiftDistributionAnalysisControl()
+        {
+            InitializeComponent();
+            var shiftDistributionGrid = new ShiftDistributionGrid();
+            shiftDistributionGrid.Dock = DockStyle.Fill;
+            shiftDistributiontableLayoutPanel.Controls.Add(shiftDistributionGrid);
+        }
+
+        private void InitializeComponent()
+        {
+            this.shiftDistributiontableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // shiftDistributiontableLayoutPanel
+            // 
+            this.shiftDistributiontableLayoutPanel.ColumnCount = 1;
+            this.shiftDistributiontableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.shiftDistributiontableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.shiftDistributiontableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.shiftDistributiontableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.shiftDistributiontableLayoutPanel.Name = "shiftDistributiontableLayoutPanel";
+            this.shiftDistributiontableLayoutPanel.RowCount = 3;
+            this.shiftDistributiontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.shiftDistributiontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.shiftDistributiontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.shiftDistributiontableLayoutPanel.Size = new System.Drawing.Size(150, 150);
+            this.shiftDistributiontableLayoutPanel.TabIndex = 0;
+            // 
+            // ShiftDistributionAnalysisControl
+            // 
+            this.Controls.Add(this.shiftDistributiontableLayoutPanel);
+            this.Name = "ShiftDistributionAnalysisControl";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Win/Scheduling/PropertyPanel/ShiftDistributionGrid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Teleopti.Ccc.Win.Common;
+using Teleopti.Ccc.Win.Common.Controls;
+using Teleopti.Ccc.Win.Forecasting.Forms;
+using Teleopti.Ccc.WinCode.Common.Rows;
+
+namespace Teleopti.Ccc.Win.Scheduling.PropertyPanel
+{
+    public class ShiftDistributionGrid : TeleoptiGridControl, ITaskOwnerGrid, IHelpContext
+    {
+        public ShiftDistributionGrid()
+        {
+            initializeComponent();
+        }
+
+        //initialize component
+        private void initializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            ResumeLayout(false);
+        }
+        public bool HasColumns { get; private set; }
+        public void RefreshGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AbstractDetailView Owner { get; set; }
+        public void GoToDate(DateTime theDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLocalCurrentDate(int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<int, GridRow> EnabledChartGridRows { get; private set; }
+        public ReadOnlyCollection<GridRow> AllGridRows { get; private set; }
+        public int MainHeaderRow { get; private set; }
+        public IList<GridRow> EnabledChartGridRowsMicke65()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRowVisibility(string key, bool enabled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasHelp { get; private set; }
+        public string HelpId { get; private set; }
+    }
+}
