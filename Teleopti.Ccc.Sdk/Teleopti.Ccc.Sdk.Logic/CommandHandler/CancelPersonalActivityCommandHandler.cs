34acb98ba65f7974b046a4feb9a722d9f684e218
Roger Kratz <roger.kratz@teleopti.com>
2013-08-23 14:15:16 +0200

    removing some usage of personassignmentcollectiondonotuse

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/CancelPersonalActivityCommandHandler.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/CancelPersonalActivityCommandHandler.cs
@@ -52,13 +52,11 @@ namespace Teleopti.Ccc.Sdk.Logic.CommandHandler
 				var scheduleRange = scheduleDictionary[person];
 				var rules = _businessRulesForPersonalAccountUpdate.FromScheduleRange(scheduleRange);
 				var scheduleDay = scheduleRange.ScheduledDay(startDate);
-				var personAssignmentCollection = scheduleDay.PersonAssignmentCollectionDoNotUse();
-
-				foreach (var personAssignment in personAssignmentCollection)
+				var personAssignment = scheduleDay.PersonAssignment();
+				if (personAssignment != null)
 				{
-					cancelPersonalActivity(personAssignment, dateTimePeriod);
+					cancelPersonalActivity(personAssignment, dateTimePeriod);					
 				}
-
 				_saveSchedulePartService.Save(scheduleDay, rules);
 				using (_messageBrokerEnablerFactory.NewMessageBrokerEnabler())
 				{
