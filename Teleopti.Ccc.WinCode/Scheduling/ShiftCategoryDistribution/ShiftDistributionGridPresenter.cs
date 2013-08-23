b1d12b6abe34aebe7ed8d0edd2fac2cb46939f78
Claes Stenman <claes.stenman@teleopti.com>
2013-08-23 15:09:45 +0200

    Adding presenters for the grids...

---
+++ Teleopti.Ccc.WinCode/Scheduling/ShiftCategoryDistribution/ShiftDistributionGridPresenter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teleopti.Ccc.WinCode.Scheduling.ShiftCategoryDistribution
+{
+	public class ShiftDistributionGridPresenter
+	{
+	}
+}
---
+++ Teleopti.Ccc.WinCodeTest/Scheduler/ShiftCategoryDistribution/ShiftPerAgentGridPresenterTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Teleopti.Ccc.WinCode.Scheduling.ShiftCategoryDistribution;
+
+namespace Teleopti.Ccc.WinCodeTest.Scheduler.ShiftCategoryDistribution
+{
+	[TestFixture]
+	public class ShiftPerAgentGridPresenterTest
+	{
+		private ShiftPerAgentGridPresenter _target;
+	}
+}
