e316da97ddf34eeb9537fe67587e023180471fde
Claes Stenman <claes.stenman@teleopti.com>
2013-08-26 12:33:47 +0200

    Some test code to draw data...

--- Win/Scheduling/PropertyPanel/ShiftPerAgentGrid.cs
+++ Win/Scheduling/PropertyPanel/ShiftPerAgentGrid.cs
@@ -8,18 +8,83 @@ using Teleopti.Ccc.Win.Common.Controls;
 using Teleopti.Ccc.Win.Forecasting.Forms;
 using Teleopti.Ccc.WinCode.Common.Rows;
 using Teleopti.Ccc.WinCode.Scheduling.ShiftCategoryDistribution;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Win.Scheduling.PropertyPanel
 {
     public class ShiftPerAgentGrid : TeleoptiGridControl, ITaskOwnerGrid, IHelpContext, IShiftPerAgentGrid
     {
-	    private IDistributionInformationExtractor _model;
+	    private readonly IDistributionInformationExtractor _model;
 	    private ShiftPerAgentGridPresenter _presenter;
 
 		public ShiftPerAgentGrid(IDistributionInformationExtractor model)
 		{
 			_model = model;
+			initializeComponent();
+			
 			_presenter = new ShiftPerAgentGridPresenter(this);
+		}
+
+		private void initializeComponent()
+		{
+			QueryColCount += shiftPerAgentGridQueryColCount;
+			QueryRowCount += shiftPerAgentGridQueryRowCount;
+			QueryCellInfo += shiftPerAgentGridQueryCellInfo;
+
+			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+			ResumeLayout(false);
+		}
+
+		void shiftPerAgentGridQueryCellInfo(object sender, Syncfusion.Windows.Forms.Grid.GridQueryCellInfoEventArgs e)
+		{
+			if (e.ColIndex < 0 || e.RowIndex < 0) return;
+			if (e.ColIndex == 0 && e.RowIndex == 0) return;
+			if (e.ColIndex > _model.ShiftCategories.Count) return;
+			if (e.RowIndex > _model.PersonInvolved.Count) return;
+
+			if (e.ColIndex > 0 && e.RowIndex == 0)
+			{
+				e.Style.CellValue = _model.ShiftCategories[e.ColIndex - 1];
+				e.Style.Tag = _model.ShiftCategories[e.ColIndex - 1];
+			}
+
+			if (e.ColIndex == 0 && e.RowIndex > 0)
+			{
+				e.Style.CellValue = _model.PersonInvolved[e.RowIndex - 1].Name.FirstName;
+				e.Style.Tag = _model.PersonInvolved[e.RowIndex - 1];
+			}
+
+			if (e.ColIndex > 0 && e.RowIndex > 0)
+			{
+				var person = this[e.RowIndex - 1, 0].Tag as IPerson;
+				var shiftCategory = this[0, e.ColIndex - 1].Tag as string;
+
+				foreach (var shiftCategoryPerAgent in _model.GetShiftCategoryPerAgent())
+				{
+					if (shiftCategoryPerAgent.Person.Equals(person))
+					{
+						if (shiftCategoryPerAgent.ShiftCategoryName.Equals(shiftCategory))
+						{
+							e.Style.CellValue = shiftCategoryPerAgent.Count;
+							break;
+						}
+					}
+				}
+			}
+
+			e.Handled = true;
+		}
+
+		void shiftPerAgentGridQueryRowCount(object sender, Syncfusion.Windows.Forms.Grid.GridRowColCountEventArgs e)
+		{
+			e.Count = _model.PersonInvolved.Count;
+			e.Handled = true;
+		}
+
+		void shiftPerAgentGridQueryColCount(object sender, Syncfusion.Windows.Forms.Grid.GridRowColCountEventArgs e)
+		{
+			e.Count = _model.ShiftCategories.Count;
+			e.Handled = true;
 		}
 
         public bool HasColumns { get; private set; }
