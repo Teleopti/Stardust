023810fdbe62523fcd0c5646c7303a79dd2c9a1d
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-08-29 13:43:11 +0200

    Moving things into place

---
+++ Domain/ApplicationLayer/ScheduleChangedEventHandlers/ScheduleChangedNotifier.cs
@@ -0,0 +1,33 @@
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.MessageBroker.Events;
+
+namespace Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers
+{
+	public class ScheduleChangedNotifier : IHandleEvent<ScheduleChangedEvent>
+	{
+		private readonly IMessageBrokerSender _broker;
+
+		public ScheduleChangedNotifier(IMessageBrokerSender broker)
+		{
+			_broker = broker;
+		}
+
+		public void Handle(ScheduleChangedEvent @event)
+		{
+			_broker.SendEventMessage(
+				@event.Datasource,
+				@event.BusinessUnitId,
+				@event.StartDateTime,
+				@event.EndDateTime,
+				@event.InitiatorId,
+				@event.ScenarioId,
+				typeof (Scenario),
+				@event.PersonId,
+				typeof (IScheduleChangedEvent),
+				DomainUpdateType.NotApplicable,
+				null);
+		}
+	}
+}
--- DomainTest/ApplicationLayer/ScheduleChangedEventHandlers/ScheduleChangedNotifierTest.cs
+++ DomainTest/ApplicationLayer/ScheduleChangedEventHandlers/ScheduleChangedNotifierTest.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 using Rhino.Mocks;
-using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.MessageBroker.Events;
@@ -46,36 +43,5 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.ScheduleChangedEventHandlers
 				DomainUpdateType.NotApplicable,
 				null));
 		}
-	}
-
-	public class ScheduleChangedNotifier : IHandleEvent<ScheduleChangedEvent>
-	{
-		private readonly IMessageBrokerSender _broker;
-
-		public ScheduleChangedNotifier(IMessageBrokerSender broker)
-		{
-			_broker = broker;
-		}
-
-		public void Handle(ScheduleChangedEvent @event)
-		{
-			_broker.SendEventMessage(
-				@event.Datasource,
-				@event.BusinessUnitId,
-				@event.StartDateTime,
-				@event.EndDateTime,
-				@event.InitiatorId,
-				@event.ScenarioId,
-				typeof (Scenario),
-				@event.PersonId,
-				typeof (IScheduleChangedEvent),
-				DomainUpdateType.NotApplicable,
-				null);
-		}
-	}
-
-	public interface IScheduleChangedEvent
-	{
-
 	}
 }
