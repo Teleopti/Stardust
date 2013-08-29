0ba2f7e5b3a2ea499c58d67dfa8411c9cb8b05ce
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-08-29 11:30:36 +0200

    Test refactoring

--- InfrastructureTest/UnitOfWork/ScheduleMessageSenderTest.cs
+++ InfrastructureTest/UnitOfWork/ScheduleMessageSenderTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Rhino.Mocks;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Infrastructure.ApplicationLayer;
@@ -15,14 +16,12 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 	public class ScheduleMessageSenderTest
 	{
 		private IMessageSender target;
-		private MockRepository mocks;
 		private IServiceBusSender serviceBusSender;
 
 		[SetUp]
 		public void Setup()
 		{
-			mocks = new MockRepository();
-			serviceBusSender = mocks.DynamicMock<IServiceBusSender>();
+			serviceBusSender = MockRepository.GenerateMock<IServiceBusSender>();
 			target = new ScheduleMessageSender(serviceBusSender);
 		}
 
@@ -34,15 +33,11 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 			var personAssignment = PersonAssignmentFactory.CreatePersonAssignment(person, scenario);
 			IRootChangeInfo rootChangeInfo = new RootChangeInfo(personAssignment, DomainUpdateType.Insert);
 
-			using (mocks.Record())
-			{
-				Expect.Call(serviceBusSender.EnsureBus()).Return(true);
-				Expect.Call(()=>serviceBusSender.Send(null)).IgnoreArguments();
-			}
-			using (mocks.Playback())
-			{
-				target.Execute(null, new[] { rootChangeInfo });
-			}
+			serviceBusSender.Stub(x => x.EnsureBus()).Return(true);
+
+			target.Execute(null, new[] {rootChangeInfo});
+
+			serviceBusSender.AssertWasCalled(x => x.Send(null), o => o.IgnoreArguments());
 		}
 
 		[Test]
@@ -52,30 +47,20 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 			var personAssignment = PersonAssignmentFactory.CreatePersonAssignment(person, null);
 			IRootChangeInfo rootChangeInfo = new RootChangeInfo(personAssignment, DomainUpdateType.Insert);
 
-			using (mocks.Record())
-			{
-				Expect.Call(()=>serviceBusSender.Send(null)).IgnoreArguments().Repeat.Never();
-			}
-			using (mocks.Playback())
-			{
-				target.Execute(null, new[] { rootChangeInfo });
-			}
+			target.Execute(null, new[] {rootChangeInfo});
+
+			serviceBusSender.AssertWasNotCalled(x => x.Send(null), o => o.IgnoreArguments());
 		}
-		
+
 		[Test]
 		public void ShouldNotSendNotificationForOtherType()
 		{
 			var person = PersonFactory.CreatePerson();
 			IRootChangeInfo rootChangeInfo = new RootChangeInfo(person, DomainUpdateType.Insert);
 
-			using (mocks.Record())
-			{
-				Expect.Call(()=>serviceBusSender.Send(null)).IgnoreArguments().Repeat.Never();
-			}
-			using (mocks.Playback())
-			{
-				target.Execute(null, new[] { rootChangeInfo });
-			}
+			target.Execute(null, new[] {rootChangeInfo});
+			
+			serviceBusSender.AssertWasNotCalled(x => x.Send(null), o => o.IgnoreArguments());
 		}
 
 		[Test]
@@ -83,17 +68,12 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 		{
 			var person = PersonFactory.CreatePerson();
 			var scenario = ScenarioFactory.CreateScenarioAggregate();
-			var note = new Note(person,DateOnly.Today,scenario,"my note");
+			var note = new Note(person, DateOnly.Today, scenario, "my note");
 			IRootChangeInfo rootChangeInfo = new RootChangeInfo(note, DomainUpdateType.Insert);
 
-			using (mocks.Record())
-			{
-				Expect.Call(()=>serviceBusSender.Send(null)).IgnoreArguments().Repeat.Never();
-			}
-			using (mocks.Playback())
-			{
-				target.Execute(null, new[] { rootChangeInfo });
-			}
+			target.Execute(null, new[] {rootChangeInfo});
+
+			serviceBusSender.AssertWasNotCalled(x => x.Send(null), o => o.IgnoreArguments());
 		}
 
 		[Test]
@@ -104,14 +84,26 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
 			var note = new PublicNote(person, DateOnly.Today, scenario, "my note");
 			IRootChangeInfo rootChangeInfo = new RootChangeInfo(note, DomainUpdateType.Insert);
 
-			using (mocks.Record())
-			{
-				Expect.Call(()=>serviceBusSender.Send(null)).IgnoreArguments().Repeat.Never();
-			}
-			using (mocks.Playback())
-			{
-				target.Execute(null, new[] { rootChangeInfo });
-			}
+			target.Execute(null, new[] {rootChangeInfo});
+
+			serviceBusSender.AssertWasNotCalled(x => x.Send(null), o => o.IgnoreArguments());
 		}
+
+		[Test]
+		public void ShouldSendMessageBrokerIdentifierWithEvent()
+		{
+			var person = PersonFactory.CreatePerson();
+			var scenario = ScenarioFactory.CreateScenarioAggregate();
+			var personAssignment = PersonAssignmentFactory.CreatePersonAssignment(person, scenario);
+			IRootChangeInfo rootChangeInfo = new RootChangeInfo(personAssignment, DomainUpdateType.Insert);
+
+			var messageBrokerIdentifier = new FakeMessageBrokerIdentifier();
+			target.Execute(messageBrokerIdentifier, new[] { rootChangeInfo });
+		}
+	}
+
+	public class FakeMessageBrokerIdentifier : IMessageBrokerIdentifier
+	{
+		public Guid InstanceId { get; set; }
 	}
 }
