f527c51e28da1186fc4853bbe99775d4808507bc
Tina Hammarstrom <none@none>
2013-09-05 15:38:31 +0200

    Refactoring performance tool, should work now

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/addremovefulldayabsence/scenario.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/addremovefulldayabsence/scenario.js
@@ -1,185 +1,219 @@
 
 define([
-        'knockout',
-        'progressitem-count',
-        'addremovefulldayabsence/iteration',
-        'result',
-        'messagebroker'
-    ], function(
-        ko,
-        ProgressItemCountViewModel,
-        Iteration,
-        ResultViewModel,
-        messagebroker
-    ) {
-        
+		'knockout',
+		'progressitem-count',
+		'addremovefulldayabsence/iteration',
+		'result',
+		'messagebroker'
+	], function(
+		ko,
+		ProgressItemCountViewModel,
+		Iteration,
+		ResultViewModel,
+		messagebroker
+	) {
+		
 
-        return function(readModelName, isApplicableReadModelNotification) {
+		return function(readModelName, isApplicableReadModelNotification) {
 
-            var self = this;
+			var self = this;
+			var startPromise = messagebroker.start();
+			var readModelSubscription;
+			var personAbsenceSubscription;
+			var result;
+			
+			this.Name = "Add and remove full day absence -> " + readModelName;
 
-            var progressItemReadModel = new ProgressItemCountViewModel(readModelName);
+			this.Configuration = ko.observable();
 
-            this.Name = "Add and remove full day absence -> " + readModelName;
-            
-            this.ProgressItems = [
-                progressItemReadModel
-            ];
+			this.ConfigurationObject = ko.computed(function() {
+				try {
+					return JSON.parse(self.Configuration());
+				} catch(e) {
+					return undefined;
+				}
+			});
+			
+			this.Iterations = ko.computed(function() {
 
-            var iterations = [];
-            
-            this.IterationsExpected = ko.observable();
+				var configuration = self.ConfigurationObject();
+				if (!configuration)
+					return;
+				
+				var startDate = moment(configuration.DateRange.From);
+				var endDate = moment(configuration.DateRange.To);
+				var numberOfDays = endDate.diff(startDate, 'days') + 1;
+				var personIds = configuration.PersonIds;
 
-            var startPromise = messagebroker.start();
-            var readModelSubscription;
-            var personAbsenceSubscription;
-            var result;
+				var iterations = [];
 
-            this.LoadDefaultConfiguration = function (callback) {
-                var absenceId;
-                var personId;
+				for (var i = 0; i < personIds.length; i++) {
+					var personId = personIds[i];
+					var date = startDate.clone().subtract('days', 1);
 
-                var promise1 = $.ajax({
-                    url: 'Configuration/GetAAbsenceId',
-                    success: function (data, textStatus, jqXHR) {
-                        absenceId = data;
-                    }
-                });
+					for (var j = 0; j < numberOfDays; j++) {
+						date.add('days', 1);
 
-                var promise2 = $.ajax({
-                    url: 'Configuration/GetAPersonId',
-                    success: function (data, textStatus, jqXHR) {
-                        personId = data;
-                    }
-                });
+						iterations.push(new Iteration({
+							AbsenceId: configuration.AbsenceId,
+							PersonId: personId,
+							Date: date.clone(),
+							ReadModelUpdated: function() {
+								progressItemReadModel.Success();
+								calculateRunDone();
+							},
+							ReadModelUpdateFailed: function() {
+								progressItemReadModel.Failure();
+								calculateRunDone();
+							},
+							IsApplicableNotification: isApplicableReadModelNotification
+						}));
 
-                $.when(promise1, promise2).done(function () {
-                    var date = moment().format('YYYY-MM-DD');
-                    callback({
-                        AbsenceId: absenceId,
-                        PersonIds: [
-                            personId
-                        ],
-                        DateRange: {
-                            From: date,
-                            To: date
-                        }
-                    });
-                });
-            };
+						if (iterations.length > 2000)
+							return undefined;
+					}
+				}
 
-            this.ConfigurationChanged = function (configuration) {
-                var startDate = moment(configuration.DateRange.From);
-                var endDate = moment(configuration.DateRange.To);
-                var numberOfDays = endDate.diff(startDate, 'days') + 1;
-                var personIds = configuration.PersonIds;
+				return iterations;
+			});
 
-                iterations = [];
-                
-                for (var i = 0; i < personIds.length; i++) {
-                    var personId = personIds[i];
-                    var date = startDate.clone().subtract('days', 1);
-                    
-                    for (var j = 0; j < numberOfDays; j++) {
-                        date.add('days', 1);
+			this.IterationsExpected = ko.computed(function () {
+				var iterations = self.Iterations();
+				if (iterations)
+					return iterations.length;
+				else
+					return 0;
+			});
 
-                        iterations.push(new Iteration({
-                            AbsenceId: configuration.AbsenceId,
-                            PersonId: personId,
-                            Date: date.clone(),
-                            ReadModelUpdated: function() {
-                                progressItemReadModel.Success();
-                                calculateRunDone();
-                            },
-                            ReadModelUpdateFailed: function() {
-                                progressItemReadModel.Failure();
-                                calculateRunDone();
-                            },
-                            IsApplicableNotification: isApplicableReadModelNotification
-                        }));
-                        
-                        if (iterations.length > 2000) {
-                            self.IterationsExpected(undefined);
-                            progressItemReadModel.Target(undefined);
-                            throw "Too many combinations";
-                        }
-                    }
-                }
+			var progressItemReadModel = new ProgressItemCountViewModel(
+				readModelName,
+				ko.computed(function() {
+					return self.IterationsExpected() * 2;
+				})
+			);
+			
+			this.ProgressItems = [
+				progressItemReadModel
+			];
+			
+			this.ConfigurationError = ko.computed(function() {
+				if (!self.ConfigurationObject())
+					return "Could not parse configuration";
+				if (!self.Iterations())
+					return "Too many combinations found";
+				if (self.Iterations().length == 0)
+					return "No combinations found";
+				return undefined;
+			});
+			
+			var loadDefaultConfiguration = function (callback) {
+				var absenceId;
+				var personId;
 
-                self.IterationsExpected(iterations.length);
-                progressItemReadModel.Target(iterations.length * 2);
-            };
+				var promise1 = $.ajax({
+					url: 'Configuration/GetAAbsenceId',
+					success: function (data, textStatus, jqXHR) {
+						absenceId = data;
+					}
+				});
 
-            var calculateRunDone = function () {
-                var calculatedInterationsDone = progressItemReadModel.Count() / 2;
-                if (calculatedInterationsDone > result.IterationsDone()) {
-                    result.IterationsDone(calculatedInterationsDone);
-                    if (result.IterationsDone() >= self.IterationsExpected()) {
-                        result.RunDone(true);
-                        result = null;
-                        messagebroker.unsubscribe(readModelSubscription);
-                        readModelSubscription = null;
-                    }
-                }
-            };
-            
-            this.Run = function () {
-                
-                progressItemReadModel.Reset();
-                result = new ResultViewModel();
+				var promise2 = $.ajax({
+					url: 'Configuration/GetAPersonId',
+					success: function (data, textStatus, jqXHR) {
+						personId = data;
+					}
+				});
 
-                startPromise.done(function () {
-                    
-                    readModelSubscription = messagebroker.subscribe({
-                        domainType: 'I' + readModelName,
-                        callback: function (notification) {
-                            $.each(iterations, function (i, e) {
-                                if (e.NotifyReadModelChanged(notification))
-                                    return false;
-                            });
-                        }
-                    });
-                    
-                    personAbsenceSubscription = messagebroker.subscribe({
-                        domainType: 'IPersistableScheduleData',
-                        callback: function (notification) {
-                            
-                            if (notification.DomainUpdateType == 0) //Only insert
-                            {
-                                $.each(iterations, function(i, e) {
-                                    if (e.NotifyPersonAbsenceChanged(notification)) {
-                                        if (e == iterations[iterations.length - 1]) {
-                                            messagebroker.unsubscribe(personAbsenceSubscription);
-                                            personAbsenceSubscription = null;
-                                        }
-                                        return false;
-                                    }
-                                });
-                            }
-                        }
-                    });
+				$.when(promise1, promise2).done(function () {
+					var date = moment().format('YYYY-MM-DD');
+					var configurationObject = {
+						AbsenceId: absenceId,
+						PersonIds: [
+							personId
+						],
+						DateRange: {
+							From: date,
+							To: date
+						}
+					};
+					var configuration = JSON.stringify(configurationObject, null, 4);
+					self.Configuration(configuration);
+				});
+			};
+			loadDefaultConfiguration();
+			
+			var calculateRunDone = function () {
+				var calculatedInterationsDone = progressItemReadModel.Count() / 2;
+				if (calculatedInterationsDone > result.IterationsDone()) {
+					result.IterationsDone(calculatedInterationsDone);
+					if (result.IterationsDone() >= self.IterationsExpected()) {
+						result.RunDone(true);
+						result = null;
+						messagebroker.unsubscribe(readModelSubscription);
+						readModelSubscription = null;
+					}
+				}
+			};
+			
+			this.Run = function () {
 
-                    $.when(
-                        readModelSubscription.promise,
-                        personAbsenceSubscription.promise
-                    ).done(function() {
+				var iterations = self.Iterations();
+				
+				progressItemReadModel.Reset();
+				result = new ResultViewModel();
 
-                        $.each(iterations, function(i, e) {
-                            e.Start();
-                        });
+				startPromise.done(function () {
+					
+					readModelSubscription = messagebroker.subscribe({
+						domainType: 'I' + readModelName,
+						callback: function (notification) {
+							$.each(iterations, function (i, e) {
+								if (e.NotifyReadModelChanged(notification))
+									return false;
+							});
+						}
+					});
+					
+					personAbsenceSubscription = messagebroker.subscribe({
+						domainType: 'IPersistableScheduleData',
+						callback: function (notification) {
+							
+							if (notification.DomainUpdateType == 0) //Only insert
+							{
+								$.each(iterations, function(i, e) {
+									if (e.NotifyPersonAbsenceChanged(notification)) {
+										if (e == iterations[iterations.length - 1]) {
+											messagebroker.unsubscribe(personAbsenceSubscription);
+											personAbsenceSubscription = null;
+										}
+										return false;
+									}
+								});
+							}
+						}
+					});
 
-                        var commandsSentPromises = $.map(iterations, function (e) {
-                            return e.AllCommandsCompletedPromise;
-                        });
-                        $.when.apply($, commandsSentPromises).then(function () {
-                            result.CommandsDone(true);
-                        });
+					$.when(
+						readModelSubscription.promise,
+						personAbsenceSubscription.promise
+					).done(function() {
 
-                    });
+						$.each(iterations, function(i, e) {
+							e.Start();
+						});
 
-                });
+						var commandsSentPromises = $.map(iterations, function (e) {
+							return e.AllCommandsCompletedPromise;
+						});
+						$.when.apply($, commandsSentPromises).then(function () {
+							result.CommandsDone(true);
+						});
 
-                return result;
-            };
-        };
-    });
+					});
+
+				});
+
+				return result;
+			};
+		};
+	});
