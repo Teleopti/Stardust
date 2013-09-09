da2faa4b51a083b8af68e5f7dad113661d4b8305
tamasb@teleopti696.toptinet.teleopti.com <tamasb@teleopti696.toptinet.teleopti.com>
2013-09-09 13:35:12 +0200

    header changes

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IPersonAccountUpdater.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IPersonAccountUpdater.cs
@@ -1,5 +1,9 @@
 ﻿namespace Teleopti.Interfaces.Domain
 {
+
+	/// <summary>
+	/// Responsible to update all the person absence accounts
+	/// </summary>
 	public interface IPersonAccountUpdater
 	{
 		void UpdatePersonAccounts();
