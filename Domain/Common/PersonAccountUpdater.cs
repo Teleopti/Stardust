72814bc4a3cefb6c3d3f86574481eeb96eed1320
Talha Muhammad <talha.muhammad@teleopti.com>
2013-09-09 15:21:05 +0200

    Added PersonAccountUpdater class

---
+++ Domain/Common/PersonAccountUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Common
+{
+    public class PersonAccountUpdater : IPersonAccountUpdater
+    {
+        public void UpdatePersonAccounts()
+        {
+            // sort person account collection by starting datetime
+            // go though all of them
+            // set the account 
+            throw new System.NotImplementedException();
+        }
+    }
+}
