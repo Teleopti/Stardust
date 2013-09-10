3342ad3efeeb657264606d1ede11b1f6be748a0f
Asad MIrza <asad.mirza@teleopti.com>
2013-09-10 11:10:43 +0200

    Fixed minor bugs

--- Win/Scheduling/PropertyPanel/ShiftDistributionGrid.cs
+++ Win/Scheduling/PropertyPanel/ShiftDistributionGrid.cs
@@ -1,4 +1,7 @@
-﻿using Syncfusion.Windows.Forms.Grid;
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using Syncfusion.Windows.Forms.Grid;
 using Teleopti.Ccc.Win.Common.Controls;
 using Teleopti.Ccc.Win.Common.Controls.Cells;
 using Teleopti.Ccc.WinCode.Scheduling.ShiftCategoryDistribution;
@@ -10,6 +13,7 @@ namespace Teleopti.Ccc.Win.Scheduling.PropertyPanel
     {
         private IDistributionInformationExtractor _model;
         private ShiftDistributionGridPresenter _presenter;
+        private static readonly Color ColorCells = Color.AntiqueWhite;
 
         public ShiftDistributionGrid()
         {
@@ -73,11 +77,13 @@ namespace Teleopti.Ccc.Win.Scheduling.PropertyPanel
             {
                 e.Style.CellValue = _presenter.SortedDates() [e.RowIndex - 1].Date.ToShortDateString();
                 e.Style.Tag = _presenter.SortedDates()[e.RowIndex - 1];
+                WeekendOrWeekday(e, (DateOnly) e.Style.Tag, false, true);
             }
 
             if (e.ColIndex > 0 && e.RowIndex > 0)
             {
                 var date = (DateOnly) this[e.RowIndex, 0].Tag;
+               
                 var shiftCategory = this[0, e.ColIndex].Tag as IShiftCategory;
                 e.Style.CellType = "IntegerReadOnlyCell";
 
@@ -88,7 +94,27 @@ namespace Teleopti.Ccc.Win.Scheduling.PropertyPanel
                     e.Style.CellValue = 0;
             }
 
+                
+
             e.Handled = true;
+        }
+
+        public void WeekendOrWeekday(GridQueryCellInfoEventArgs e, DateOnly date, bool backColor, bool textColor)
+        {
+            if (DateHelper.IsWeekend(date, CultureInfo.CurrentCulture))
+            {
+                if (backColor)
+                    e.Style.BackColor = ColorHolidayCell;
+                if (textColor)
+                    e.Style.TextColor = ColorHolidayHeader;
+            }
+            else
+            {
+                if (backColor)
+                    e.Style.BackColor = ColorCells;
+
+                e.Style.TextColor = ForeColor;
+            }
         }
 
         private void shiftPerAgentGridQueryRowCount(object sender,
