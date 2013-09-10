060267495769d639f3fd9aa8279cfb72e0ccc0bc
Robin Karlsson <robin.karlsson@teleopti.com>
2013-09-10 12:15:28 +0200

    Cleaning up in some view models and fixing bug #24681
    --HG--
    branch : Release 7.4.388

--- Teleopti.Ccc.WinCode/Payroll/GridViewBaseModel.cs
+++ Teleopti.Ccc.WinCode/Payroll/GridViewBaseModel.cs
@@ -6,32 +6,19 @@ namespace Teleopti.Ccc.WinCode.Payroll
 {
     public class GridViewBaseModel<T> : ViewModel<T>, IGridViewBaseModel where T : IMultiplicatorDefinition
     {
-        #region Fields - Instance Members
+        private readonly T _containedEntity;
 
-        #region Fields - Instance Members - Private Fields
-
-        private T _containedEntity;
-
-        #endregion
-
-        #endregion
-
-        #region Properties -Instance Members
-
-        public IList<IMultiplicator> MultiplicatorCollection
+        protected GridViewBaseModel(T definition) : base(definition)
         {
-            get;
-            set;
+            _containedEntity = definition;
         }
+
+        public IList<IMultiplicator> MultiplicatorCollection { get; set; }
 
         /// <summary>
         /// Gets the multiplicator.
         /// </summary>
         /// <value>The multiplicator.</value>
-        /// <remarks>
-        /// Created by: robink
-        /// Created date: 2009-04-15
-        /// </remarks>
         public IMultiplicator Multiplicator
         {
             get
@@ -46,20 +33,5 @@ namespace Teleopti.Ccc.WinCode.Payroll
                 }
             }
         }
-        #endregion
-
-        #region Methods - Instance Members
-
-        #region Methods - Instance Members - Constructors
-
-        public GridViewBaseModel(T definition)
-            : base(definition)
-        {
-            _containedEntity = definition;
-        }
-        
-        #endregion
-
-        #endregion
     }
 }
