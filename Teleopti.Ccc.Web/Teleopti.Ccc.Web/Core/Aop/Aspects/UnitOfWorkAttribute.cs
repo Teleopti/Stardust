fe414f41f57ed37942d7e3f7a24041b4e60512c3
ola@teleopti.com <ola@teleopti.com>
2013-09-19 13:08:07 +0200

    sealed some Attribute classes

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Aop/Aspects/UnitOfWorkAttribute.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Aop/Aspects/UnitOfWorkAttribute.cs
@@ -2,7 +2,7 @@ using Teleopti.Ccc.Web.Core.Aop.Core;
 
 namespace Teleopti.Ccc.Web.Core.Aop.Aspects
 {
-	public class UnitOfWorkAttribute : ResolvedAspectAttribute
+    public sealed class UnitOfWorkAttribute : ResolvedAspectAttribute
 	{
 		public UnitOfWorkAttribute() : base(typeof(UnitOfWorkAspect)) { }
 	}
