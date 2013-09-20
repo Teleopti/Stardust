c32d2a9666df5a80ae98ddecb4c89df16dd63a52
Maria Stein <none@none>
2013-09-20 09:52:30 +0200

    Bug #24693 - It is not possible to scroll down or up to see the replies to an absence request in MyTimeWeb
    --HG--
    branch : Release 7.4.388

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/TextRequestStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/TextRequestStepDefinitions.cs
@@ -75,12 +75,14 @@ namespace Teleopti.Ccc.WebBehaviorTest
 		{
 			var request = UserFactory.User().UserData<ExistingTextRequest>();
 
+			var firstFiftyCharsOfMessage = request.PersonRequest.GetMessage(new NoFormatting()).Substring(0,50);
+
 			Browser.Interactions.AssertFirstContains(
 				string.Format(".request-body:nth-child({0}) .request-data-subject", position),
 				request.PersonRequest.GetSubject(new NoFormatting()));
 			Browser.Interactions.AssertFirstContains(
 				string.Format(".request-body:nth-child({0}) .request-data-message", position),
-				request.PersonRequest.GetMessage(new NoFormatting()));
+				firstFiftyCharsOfMessage);
 
 			Browser.Interactions.AssertFirstContains(
 				string.Format(".request-body:nth-child({0}) .request-data-date", position),
