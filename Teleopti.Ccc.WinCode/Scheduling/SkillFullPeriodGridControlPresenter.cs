3099dd798c200f1c66926607dc2705cd1b388887
Asad MIrza <asad.mirza@teleopti.com>
2013-09-23 14:12:23 +0200

    Added timezone to WholeDayCollection method
    --HG--
    branch : Release 7.4.388

--- Teleopti.Ccc.WinCode/Scheduling/SkillFullPeriodGridControlPresenter.cs
+++ Teleopti.Ccc.WinCode/Scheduling/SkillFullPeriodGridControlPresenter.cs
@@ -27,7 +27,7 @@ namespace Teleopti.Ccc.WinCode.Scheduling
 		{
 			if (stateHolder == null || skill == null) return;
 
-			var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection();
+            var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection(stateHolder.TimeZoneInfo);
 			_dates = dateTimePeriods.Select(d => new DateOnly(TimeZoneHelper.ConvertFromUtc(d.StartDateTime, stateHolder.TimeZoneInfo))).OrderBy(s => s.Date).ToList();
 			var startDate = _dates.FirstOrDefault();
 			var endDate = _dates.LastOrDefault();
@@ -52,7 +52,7 @@ namespace Teleopti.Ccc.WinCode.Scheduling
 		{
 			if (stateHolder == null) return;
 
-			var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection();
+			var dateTimePeriods = stateHolder.RequestedPeriod.Period().WholeDayCollection(stateHolder.TimeZoneInfo );
 			_dates = dateTimePeriods.Select(d => new DateOnly(TimeZoneHelper.ConvertFromUtc(d.StartDateTime, stateHolder.TimeZoneInfo))).OrderBy(s => s.Date).ToList();
 		}
 
