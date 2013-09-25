3cea67c6c08848f354b9604a94a9e9ec998227ed
David Jonsson <david.jonsson@teleopti.com>
2013-09-25 20:07:54 +0200

    Change settings.txt first then build and run

--- BuildArtifacts/FixMyConfig/RefreshConfig.bat
+++ BuildArtifacts/FixMyConfig/RefreshConfig.bat
@@ -6,7 +6,7 @@ SET masterSettings=%ROOTDIR%\..\settings.txt
 SET CCC7DB=%1
 SET AnalyticsDB=%2
 set MSBUILD="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-SET MySettings=%ROOTDIR%\..\..\Teleopti.Support.Code\bin\debug\settings.txt
+SET MySettings=%ROOTDIR%\..\..\Teleopti.Support.Code\settings.txt
  
 cd %ROOTDIR%
 
@@ -16,13 +16,13 @@ taskkill /IM WebDev.WebServer20.EXE /F
 ::get a fresh Settings.txt
 COPY "%masterSettings%" "%MySettings%"
 
-::Build Teleopti.Support.Tool.exe
-ECHO Building %ROOTDIR%\..\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj
-%MSBUILD% "%ROOTDIR%\..\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" > "%ROOTDIR%\Teleopti.Support.Tool.build.log"
-
 ::Replace some parameters according to current RestoreToLocal.bat
 cscript replace.vbs "TeleoptiAnalytics_Demo" "%AnalyticsDB%" "%MySettings%" > NUL
 cscript replace.vbs "TeleoptiCCC7_Demo" "%CCC7DB%" "%MySettings%" > NUL
+
+::Build Teleopti.Support.Tool.exe
+ECHO Building %ROOTDIR%\..\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj
+%MSBUILD% "%ROOTDIR%\..\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" > "%ROOTDIR%\Teleopti.Support.Tool.build.log"
 
 ::Run supportTool to replace all config
 "%ROOTDIR%\..\..\Teleopti.Support.Tool\bin\Debug\Teleopti.Support.Tool.exe" -MODebug
