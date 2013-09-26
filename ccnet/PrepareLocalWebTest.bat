61b24b08df6553e708c1af7dc502e863bb2efe33
David Jonsson <david.jonsson@teleopti.com>
2013-09-26 09:14:57 +0200

    Add a Developer Infratest.ini to target

--- ccnet/PrepareLocalWebTest.bat
+++ ccnet/PrepareLocalWebTest.bat
@@ -4,18 +4,10 @@ SET ROOTDIR=%ROOTDIR:~0,-7%
 SET MsbuildProjRaptor2=ccnet\Raptor2.proj
 SET CCNetWorkingDirectory=%ROOTDIR%
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+SET ImADeveloper=TRUE
 
-::register ncover licens locally ^(we only have two lics on Teleopti...^)
-CD "%CCNetWorkingDirectory%\ccnet\NCover64"
-NCover.Registration.exe //License NC3CMPLIC.lic
-
-::Get to root
-CD %CCNetWorkingDirectory%
-ECHO %CCNetWorkingDirectory%
-
-::Standard build
 ::Select build type
-SET CCNetProject=LocalWebTest
+SET CCNetProject=Infratest
 
 ::Run Build
 ECHO "%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProjRaptor2%" /t:FixConfigFiles
