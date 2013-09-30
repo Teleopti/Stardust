29d87f787c6aa28f8ccc0e45e42b5908f29dc4bb
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-09-30 20:19:10 +0200

    fixes

--- Infrastructure/Repositories/PersonScheduleDayReadModelFinder.cs
+++ Infrastructure/Repositories/PersonScheduleDayReadModelFinder.cs
@@ -29,7 +29,7 @@ namespace Teleopti.Ccc.Infrastructure.Repositories
 		public IEnumerable<PersonScheduleDayReadModel> ForPerson(DateOnly startDate, DateOnly endDate, Guid personId)
 		{
 			return _unitOfWork.Session().CreateSQLQuery(
-				"SELECT PersonId, TeamId, SiteId, BusinessUnitId, BelongsToDate AS Date, ShiftStart, ShiftEnd, Shift FROM ReadModel.PersonScheduleDay WHERE PersonId=:personid AND BelongsToDate Between :startdate AND :enddate")
+				"SELECT PersonId, TeamId, SiteId, BusinessUnitId, BelongsToDate AS Date, ShiftStart, ShiftEnd, Model FROM ReadModel.PersonScheduleDay WHERE PersonId=:personid AND BelongsToDate Between :startdate AND :enddate")
 			                  .AddScalar("PersonId", NHibernateUtil.Guid)
 			                  .AddScalar("TeamId", NHibernateUtil.Guid)
 			                  .AddScalar("SiteId", NHibernateUtil.Guid)
@@ -37,7 +37,7 @@ namespace Teleopti.Ccc.Infrastructure.Repositories
 			                  .AddScalar("Date", NHibernateUtil.DateTime)
 			                  .AddScalar("ShiftStart", NHibernateUtil.DateTime)
 			                  .AddScalar("ShiftEnd", NHibernateUtil.DateTime)
-			                  .AddScalar("Shift", NHibernateUtil.Custom(typeof (CompressedString)))
+			                  .AddScalar("Model", NHibernateUtil.Custom(typeof (CompressedString)))
 			                  .SetGuid("personid", personId)
 			                  .SetDateTime("startdate", startDate)
 			                  .SetDateTime("enddate", endDate)
