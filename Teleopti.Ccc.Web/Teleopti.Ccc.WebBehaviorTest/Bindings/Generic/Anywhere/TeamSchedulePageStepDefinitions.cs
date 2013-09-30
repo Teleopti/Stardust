10b884afeb3bd44fe94838f0afb4507f00f3ca1b
Tina Hammarstrom <none@none>
2013-09-30 10:25:46 +0200

    New step definition for team schedule

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Anywhere/TeamSchedulePageStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Anywhere/TeamSchedulePageStepDefinitions.cs
@@ -101,6 +101,12 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.Anywhere
 			Browser.Interactions.AssertExistsUsingJQuery(".person:contains('{0}') .dayoff", personName);
 		}
 
+		[Then(@"I should see '(.*)' before '(.*)'")]
+		public void ThenIShouldSeeBefore(string person1, string person2)
+		{
+			Browser.Interactions.AssertExistsUsingJQuery(".person:contains('{0}') + .person:contains('{1}')", person1, person2);
+		}
+
 		public class TeamInfo
 		{
 			public string Team { get; set; }
