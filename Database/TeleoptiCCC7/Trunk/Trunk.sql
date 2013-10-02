8a0b4327db212e66fd62b573fdef631747baf2be
David Jonsson <david.jonsson@teleopti.com>
2013-10-02 11:15:19 +0200

    Bug #24989 - Make database compatible with SQL 2005
    --HG--
    branch : Release 7.4.388

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -11,3 +11,11 @@ set earlystart = 0 where EarlyStart = 1
 update ActivityExtender
 set LateStart = 0 where LateStart = 1
 GO
+
+----------------  
+--Name: David Jonsson
+--Date: 2013-10-02
+--Desc: Bug #24989 - Make database compatible with SQL 2005
+---------------- 
+ALTER TABLE [dbo].[OvertimeAvailability]
+ALTER COLUMN [DateOfOvertime] [datetime] NOT NULL
