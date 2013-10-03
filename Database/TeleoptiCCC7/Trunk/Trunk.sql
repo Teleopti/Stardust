145e15be65e64d909f84152b925fcfbe87b38392
Erik Sundberg <none@none>
2013-10-03 16:42:32 +0200

    #25008 RTA could not differentiate between no alarm and alarm for no scheduled activity
    --HG--
    branch : Release 7.4.384

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -299,3 +299,12 @@ set earlystart = 0 where EarlyStart = 1
 update ActivityExtender
 set LateStart = 0 where LateStart = 1
 GO
+
+
+----------------  
+--Name: Erik Sundberg
+--Date: 2013-10-03
+--Desc: Bug #25008 - RTA does not differentiate between no scheduled activity and no alarm
+---------------- 
+ALTER TABLE dbo.StateGroupActivityAlarm ALTER COLUMN AlarmType uniqueidentifier NULL
+GO
