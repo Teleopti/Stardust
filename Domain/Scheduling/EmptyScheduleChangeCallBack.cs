879fc14a93fcd3318316478556b0115b3603d3bd
Micke <micke@teleopti.com>
2013-10-04 23:16:09 +0200

    Taking back removed class
    --HG--
    branch : Release 7.4.388

---
+++ Domain/Scheduling/EmptyScheduleChangeCallBack.cs
@@ -0,0 +1,15 @@
+﻿using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling
+{
+	public class EmptyScheduleDayChangeCallback : IScheduleDayChangeCallback
+	{
+		public void ScheduleDayChanging(IScheduleDay partBefore)
+		{
+		}
+
+		public void ScheduleDayChanged(IScheduleDay partAfter)
+		{
+		}
+	}
+}
