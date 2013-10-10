5aaaa35f33bd6153a27c5c974ba09a797073dce2
Mathias Stenbom <mathias.stenbom@teleopti.com>
2013-10-10 10:52:53 +0200

    layer refact step 1

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/shared/timeline-unit.js
@@ -0,0 +1,107 @@
+define([
+		'knockout',
+		'moment',
+		'resources!r'
+], function (
+	ko,
+	moment,
+	resources
+	) {
+
+	return function (timeline, data) {
+
+		var self = this;
+
+		var startMinutesToPixels = function (minutes) {
+			var start = minutes - timeline.StartMinutes();
+			var pixels = start * timeline.PixelsPerMinute();
+			return Math.round(pixels);
+		};
+
+		var lengthMinutesToPixels = function (minutes) {
+			var pixels = minutes * timeline.PixelsPerMinute();
+			return Math.round(pixels);
+		};
+
+
+
+		this.StartMinutes = function() {
+			var startTime = moment(data.Start, resources.FixedDateTimeFormatForMoment);
+			return startTime.diff(data.Date, 'minutes');
+		};
+
+		this.LengthMinutes = function() {
+			return data.Minutes;
+		};
+
+		this.EndMinutes = ko.computed(function () {
+			return self.StartMinutes() + self.LengthMinutes();
+		});
+
+		this.StartPixels = ko.computed(function () {
+			return startMinutesToPixels(self.StartMinutes());
+		});
+
+		this.LengthPixels = ko.computed(function () {
+			return lengthMinutesToPixels(self.LengthMinutes());
+		});
+
+
+
+		this.CutInsideTimeLineStartMinutes = ko.computed(function () {
+			if (self.StartMinutes() < timeline.StartMinutes())
+				return timeline.StartMinutes();
+			return self.StartMinutes();
+		});
+
+		this.CutInsideTimeLineLengthMinutes = ko.computed(function () {
+			if (timeline.EndMinutes() < self.StartMinutes() + self.LengthMinutes())
+				return timeline.EndMinutes() - self.CutInsideTimeLineStartMinutes();
+			return self.LengthMinutes();
+		});
+
+		this.CutInsideTimeLineStartPixels = ko.computed(function () {
+			return startMinutesToPixels(self.CutInsideTimeLineStartMinutes());
+		});
+
+		this.CutInsideTimeLineLengthPixels = ko.computed(function () {
+			return lengthMinutesToPixels(self.CutInsideTimeLineLengthMinutes());
+		});
+
+
+
+		this.CutInsideDayStartMinutes = ko.computed(function () {
+			if (self.StartMinutes() < 0)
+				return 0;
+			return self.StartMinutes();
+		});
+
+		this.CutInsideDayLengthMinutes = ko.computed(function () {
+			if (self.StartMinutes() < 0)
+				return self.LengthMinutes() - (self.StartMinutes() * -1);
+			return self.LengthMinutes();
+		});
+
+		this.CutInsideDayStartPixels = ko.computed(function () {
+			return startMinutesToPixels(self.CutInsideDayStartMinutes());
+		});
+
+		this.CutInsideDayLengthPixels = ko.computed(function () {
+			return lengthMinutesToPixels(self.CutInsideDayLengthMinutes());
+		});
+
+
+
+		this.OverlapsTimeLine = ko.computed(function () {
+			return self.CutInsideTimeLineLengthMinutes() > 0;
+		});
+		
+
+
+		this.StartTime = ko.computed(function () {
+			var time = moment().startOf('day').add('minutes', self.StartMinutes());
+			return time.format(resources.TimeFormatForMoment);
+		});
+
+	};
+});
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/teamschedule/dayoff.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/teamschedule/dayoff.js
@@ -1,46 +1,23 @@
 define([
 		'knockout',
 		'moment',
-		'resources!r'
+		'resources!r',
+		'shared/timeline-unit'
 ], function (
-		ko,
-		moment,
-		resources
+	ko,
+	moment,
+	resources,
+	unitViewModel
 	) {
 
 	return function (timeline, data) {
 
-		var startTime = moment(data.Start, resources.FixedDateTimeFormatForMoment);
-		var dayOffStartMinutes = startTime.diff(data.Date, 'minutes');
+		var unit = new unitViewModel(timeline, data);
+		
+		this.StartPixels = unit.CutInsideTimeLineStartPixels;
+		this.LengthPixels = unit.CutInsideTimeLineLengthPixels;
 
-		var startMinutes = ko.observable(dayOffStartMinutes);
-		var lengthMinutes = ko.observable(data.Minutes);
+		this.OverlapsTimeLine = unit.OverlapsTimeLine;
 
-		var displayStartMinute = ko.computed(function() {
-			if (startMinutes() < timeline.StartMinutes())
-				return timeline.StartMinutes();
-			return startMinutes();
-		});
-
-		var displayLength = ko.computed(function () {
-			if (timeline.EndMinutes() < startMinutes() + lengthMinutes())
-				return timeline.EndMinutes() - displayStartMinute();
-			return lengthMinutes();
-		});
-
-		this.StartPixels = ko.computed(function () {
-			var start = displayStartMinute() - timeline.StartMinutes();
-			var pixels = start * timeline.PixelsPerMinute();
-			return Math.round(pixels);
-		});
-
-		this.LengthPixels = ko.computed(function () {
-			var pixels = displayLength() * timeline.PixelsPerMinute();
-			return Math.round(pixels);
-		});
-
-		this.InTimeLine = ko.computed(function () {
-			return displayLength() > 0;
-		});
 	};
 });
