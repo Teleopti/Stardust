aec0a7266b70f7970bf032f4764980eae843d710
ola@teleopti.com <ola@teleopti.com>
2013-10-18 15:44:48 +0200

    basic test database created!

--- Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SetupFixtureForAssembly.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SetupFixtureForAssembly.cs
@@ -1,66 +1,49 @@
-﻿//using System;
-//using System.IO;
-//using NUnit.Framework;
-//using Teleopti.Ccc.Domain.Security;
-//using Teleopti.Ccc.Domain.Security.Principal;
-//using Teleopti.Ccc.Infrastructure.Foundation;
-//using Teleopti.Ccc.Infrastructure.Repositories;
-//using Teleopti.Ccc.Infrastructure.UnitOfWork;
-//using Teleopti.Ccc.TestCommon;
-//using Teleopti.Ccc.TestCommon.FakeData;
-//using Teleopti.Ccc.TestCommon.TestData.Core;
-//using Teleopti.Interfaces.Infrastructure;
+﻿using System;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Ccc.TestCommon.TestData.Setups;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
-//namespace Teleopti.Analytics.Etl.IntegrationTest
-//{
-//    [SetUpFixture]
-//    public class SetupFixtureForAssembly
-//    {
-//        private ICurrentUnitOfWorkFactory _unitOfWorkFactory;
+namespace Teleopti.Analytics.Etl.IntegrationTest
+{
+    [SetUpFixture]
+    public class SetupFixtureForAssembly
+    {
+        private ICurrentUnitOfWorkFactory _unitOfWorkFactory;
 
-//        private void UnitOfWorkAction(Action<IUnitOfWork> action)
-//        {
-//            using (var uow = _unitOfWorkFactory.LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork())
-//            {
-//                action(uow);
-//                uow.PersistAll();
-//            }
-//        }
+        private void UnitOfWorkAction(Action<IUnitOfWork> action)
+        {
+            using (var uow = _unitOfWorkFactory.LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork())
+            {
+                action(uow);
+                uow.PersistAll();
+            }
+        }
 
-//        //[SetUp]
-//        //public void Setup()
-//        //{
-//        //    var dataSource = DataSourceHelper.CreateDataSource(new IMessageSender[]{}, "TestData");
+        [SetUp]
+        public void Setup()
+        {
+            var dataSource = DataSourceHelper.CreateDataSource(new IMessageSender[] { }, "TestData");
 
-//        //    var personThatCreatesTestData = PersonFactory.CreatePersonWithBasicPermissionInfo("UserThatCreatesTestData", "password");
+            var personThatCreatesTestData = PersonFactory.CreatePersonWithBasicPermissionInfo("UserThatCreatesTestData", "password");
 
-//        //    var businessUnitFromFakeState = BusinessUnitFactory.CreateBusinessUnitWithSitesAndTeams();
-//        //    businessUnitFromFakeState.Name = "BusinessUnit";
+            var businessUnitFromFakeState = BusinessUnitFactory.CreateBusinessUnitWithSitesAndTeams();
+            businessUnitFromFakeState.Name = "BusinessUnit";
 
-//        //    StateHolderProxyHelper.SetupFakeState(dataSource, personThatCreatesTestData, businessUnitFromFakeState, new ThreadPrincipalContext(new TeleoptiPrincipalFactory()));
+            StateHolderProxyHelper.SetupFakeState(dataSource, personThatCreatesTestData, businessUnitFromFakeState, new ThreadPrincipalContext(new TeleoptiPrincipalFactory()));
 
-//        //    _unitOfWorkFactory = UnitOfWorkFactory.CurrentUnitOfWorkFactory();
+            _unitOfWorkFactory = UnitOfWorkFactory.CurrentUnitOfWorkFactory();
 
-//        //    //DataSourceHelper.PersistAuditSetting();// TODO: Remove, its done in DataSourceHelper.CreateDataSource();
-
-//        //    var dataFactory = new DataFactory(UnitOfWorkAction);
-
-//        //    UnitOfWorkAction(uow =>
-//        //        {
-
-//        //            var personRepository = new PersonRepository(uow);
-//        //            personRepository.Add(personThatCreatesTestData);
-
-//        //            var license = new License { XmlString = File.ReadAllText("License.xml") };
-//        //            var licenseRepository = new LicenseRepository(uow);
-//        //            licenseRepository.Add(license);
-
-//        //        });
-
-
-
-
-
-//        //}
-//    }
-//}
+            var dataFactory = new DataFactory(UnitOfWorkAction);
+            dataFactory.Apply(new PersonThatCreatesTestData(personThatCreatesTestData));
+            dataFactory.Apply(new LicenseFromFile());
+        }
+    }
+}
---
+++ Teleopti.Ccc.TestCommon/TestData/Setups/LicenseFromFile.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Teleopti.Ccc.Domain.Security;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.TestCommon.TestData.Setups
+{
+    public class LicenseFromFile : IDataSetup
+    {
+        public void Apply(IUnitOfWork uow)
+        {
+            var license = new License { XmlString = File.ReadAllText("License.xml") };
+            var licenseRepository = new LicenseRepository(uow);
+            licenseRepository.Add(license);
+        }
+    }
+}
