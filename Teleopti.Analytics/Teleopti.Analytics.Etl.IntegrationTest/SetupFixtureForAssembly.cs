e8301957c4bbcd6985bbc2882f9f78e13148976b
ola@teleopti.com <ola@teleopti.com>
2013-10-18 16:21:48 +0200

    created common data factory state for tests, and saw it working

--- Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SetupFixtureForAssembly.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SetupFixtureForAssembly.cs
@@ -1,49 +1,50 @@
-﻿using System;
-using NUnit.Framework;
-using Teleopti.Ccc.Domain.Common;
-using Teleopti.Ccc.Domain.Security.Principal;
-using Teleopti.Ccc.Infrastructure.Foundation;
-using Teleopti.Ccc.Infrastructure.UnitOfWork;
-using Teleopti.Ccc.TestCommon;
-using Teleopti.Ccc.TestCommon.FakeData;
-using Teleopti.Ccc.TestCommon.TestData.Core;
-using Teleopti.Ccc.TestCommon.TestData.Setups;
-using Teleopti.Interfaces.Domain;
-using Teleopti.Interfaces.Infrastructure;
+﻿//using System;
+//using NUnit.Framework;
+//using Teleopti.Ccc.Domain.Common;
+//using Teleopti.Ccc.Domain.Security.Principal;
+//using Teleopti.Ccc.Infrastructure.Foundation;
+//using Teleopti.Ccc.Infrastructure.UnitOfWork;
+//using Teleopti.Ccc.TestCommon;
+//using Teleopti.Ccc.TestCommon.FakeData;
+//using Teleopti.Ccc.TestCommon.TestData.Core;
+//using Teleopti.Ccc.TestCommon.TestData.Setups;
+//using Teleopti.Interfaces.Domain;
+//using Teleopti.Interfaces.Infrastructure;
 
-namespace Teleopti.Analytics.Etl.IntegrationTest
-{
-    [SetUpFixture]
-    public class SetupFixtureForAssembly
-    {
-        private ICurrentUnitOfWorkFactory _unitOfWorkFactory;
+//namespace Teleopti.Analytics.Etl.IntegrationTest
+//{
+//    [SetUpFixture]
+//    public class SetupFixtureForAssembly
+//    {
+//        private ICurrentUnitOfWorkFactory _unitOfWorkFactory;
 
-        private void UnitOfWorkAction(Action<IUnitOfWork> action)
-        {
-            using (var uow = _unitOfWorkFactory.LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork())
-            {
-                action(uow);
-                uow.PersistAll();
-            }
-        }
+//        private void UnitOfWorkAction(Action<IUnitOfWork> action)
+//        {
+//            using (var uow = _unitOfWorkFactory.LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork())
+//            {
+//                action(uow);
+//                uow.PersistAll();
+//            }
+//        }
 
-        [SetUp]
-        public void Setup()
-        {
-            var dataSource = DataSourceHelper.CreateDataSource(new IMessageSender[] { }, "TestData");
+//        [SetUp]
+//        public void Setup()
+//        {
+//            var dataSource = DataSourceHelper.CreateDataSource(new IMessageSender[] { }, "TestData");
 
-            var personThatCreatesTestData = PersonFactory.CreatePersonWithBasicPermissionInfo("UserThatCreatesTestData", "password");
+//            var personThatCreatesTestData = PersonFactory.CreatePersonWithBasicPermissionInfo("UserThatCreatesTestData", "password");
 
-            var businessUnitFromFakeState = BusinessUnitFactory.CreateBusinessUnitWithSitesAndTeams();
-            businessUnitFromFakeState.Name = "BusinessUnit";
+//            var businessUnitFromFakeState = BusinessUnitFactory.CreateBusinessUnitWithSitesAndTeams();
+//            businessUnitFromFakeState.Name = "BusinessUnit";
 
-            StateHolderProxyHelper.SetupFakeState(dataSource, personThatCreatesTestData, businessUnitFromFakeState, new ThreadPrincipalContext(new TeleoptiPrincipalFactory()));
+//            StateHolderProxyHelper.SetupFakeState(dataSource, personThatCreatesTestData, businessUnitFromFakeState, new ThreadPrincipalContext(new TeleoptiPrincipalFactory()));
 
-            _unitOfWorkFactory = UnitOfWorkFactory.CurrentUnitOfWorkFactory();
+//            _unitOfWorkFactory = UnitOfWorkFactory.CurrentUnitOfWorkFactory();
 
-            var dataFactory = new DataFactory(UnitOfWorkAction);
-            dataFactory.Apply(new PersonThatCreatesTestData(personThatCreatesTestData));
-            dataFactory.Apply(new LicenseFromFile());
-        }
-    }
-}
+//            DataFactoryState.DataFactory = new DataFactory(UnitOfWorkAction);
+
+//            Data.Apply(new PersonThatCreatesTestData(personThatCreatesTestData));
+//            Data.Apply(new LicenseFromFile());
+//        }
+//    }
+//}
