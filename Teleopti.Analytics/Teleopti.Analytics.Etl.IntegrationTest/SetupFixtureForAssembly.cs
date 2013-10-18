6a5d7dfa8fd820077532ea3cdb67d80284c7a5cd
ola@teleopti.com <ola@teleopti.com>
2013-10-18 12:56:59 +0200

    basic test project for etl integration tests

---
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SetupFixtureForAssembly.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Security;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Analytics.Etl.IntegrationTest
+{
+    [SetUpFixture]
+    public class SetupFixtureForAssembly
+    {
+        private ICurrentUnitOfWorkFactory _unitOfWorkFactory;
+
+        private void UnitOfWorkAction(Action<IUnitOfWork> action)
+        {
+            using (var uow = _unitOfWorkFactory.LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork())
+            {
+                action(uow);
+                uow.PersistAll();
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            return;
+
+            var dataSource = DataSourceHelper.CreateDataSource(new IMessageSender[]{}, "TestData");
+
+            var personThatCreatesTestData = PersonFactory.CreatePersonWithBasicPermissionInfo("UserThatCreatesTestData", "password");
+
+            var businessUnitFromFakeState = BusinessUnitFactory.CreateBusinessUnitWithSitesAndTeams();
+            businessUnitFromFakeState.Name = "BusinessUnit";
+
+            StateHolderProxyHelper.SetupFakeState(dataSource, personThatCreatesTestData, businessUnitFromFakeState, new ThreadPrincipalContext(new TeleoptiPrincipalFactory()));
+
+            _unitOfWorkFactory = UnitOfWorkFactory.CurrentUnitOfWorkFactory();
+
+            //DataSourceHelper.PersistAuditSetting();// TODO: Remove, its done in DataSourceHelper.CreateDataSource();
+
+            var dataFactory = new DataFactory(UnitOfWorkAction);
+
+            UnitOfWorkAction(uow =>
+                {
+
+                    var personRepository = new PersonRepository(uow);
+                    personRepository.Add(personThatCreatesTestData);
+
+                    var license = new License { XmlString = File.ReadAllText("License.xml") };
+                    var licenseRepository = new LicenseRepository(uow);
+                    licenseRepository.Add(license);
+
+                });
+
+
+
+
+
+        }
+    }
+}
