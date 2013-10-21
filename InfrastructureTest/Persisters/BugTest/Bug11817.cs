b394b4fa4e9b62e51525b65d994c57f483e3e3b2
Roger Kratz <roger.kratz@teleopti.com>
2013-10-21 13:20:16 +0200

    #23090: Deleting old intragration tests covered by new integration tests

--- InfrastructureTest/Persisters/BugTest/Bug11817.cs
+++ InfrastructureTest/Persisters/BugTest/Bug11817.cs
@@ -8,8 +8,6 @@ namespace Teleopti.Ccc.InfrastructureTest.Persisters.BugTest
 	[Category("LongRunning")]
 	public class Bug11817 : ScheduleScreenPersisterIntegrationTest
 	{
-		//private PersonAssignment _personAssignment;
-
 		protected override IPersistableScheduleData SetupScheduleData()
 		{
 			return null;
@@ -25,32 +23,11 @@ namespace Teleopti.Ccc.InfrastructureTest.Persisters.BugTest
 			ScheduleDictionary.TakeSnapshot();
 			ModifyPersonAbsenceAccountInMemory();
 			AddPersonAssignmentInMemory(FirstDayDateOnly);
-			//AddPersonAssignment();
 
 			var result = TryPersistScheduleScreen();
 			Assert.IsTrue(result.Saved);
 		}
 
-		//private void AddPersonAssignment()
-		//{
-		//	_personAssignment = new PersonAssignment(Person, Scenario, FirstDayDateOnly);
-		//	_personAssignment.SetMainShiftLayers(new[]{new MainShiftLayer(Activity, FirstDayDateTimePeriod)}, ShiftCategory);
-
-		//	var scheduleDay = ScheduleDictionary[Person].ScheduledDay(FirstDayDateOnly);
-		//	scheduleDay.Add(_personAssignment);
-		//	ScheduleDictionary.Modify(ScheduleModifier.Scheduler, scheduleDay, NewBusinessRuleCollection.Minimum(), new ResourceCalculationOnlyScheduleDayChangeCallback(), new ScheduleTagSetter(NullScheduleTag.Instance));
-		//}
-
-		//protected override void TeardownForRepositoryTest()
-		//{
-		//	using (var unitOfWork = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-		//	{
-		//		var repository = new Repository(unitOfWork);
-		//		var personAssignment = new PersonAssignmentRepository(unitOfWork).Get(_personAssignment.Id.Value);
-		//		repository.Remove(personAssignment);
-		//		unitOfWork.PersistAll();
-		//	}
-		//}
 
 		protected override IEnumerable<IAggregateRoot> TestDataToReassociate()
 		{
