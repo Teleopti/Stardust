ed6dd2b4228fbc9b59741445ec1c9c489f132d31
Roger Kratz <roger.kratz@teleopti.com>
2013-10-22 17:09:45 +0200

    #23090 persist tests for write protection

---
+++ Infrastructure/Persisters/WriteProtection/IWriteProtectionPersister.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Infrastructure.Persisters.WriteProtection
+{
+	public interface IWriteProtectionPersister
+	{
+		void Persist(ICollection<IPersonWriteProtectionInfo> writeProtections);
+	}
+}
---
+++ Infrastructure/Persisters/WriteProtection/WriteProtectionPersister.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.Infrastructure.Persisters.WriteProtection
+{
+	public class WriteProtectionPersister : IWriteProtectionPersister
+	{
+		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+		private readonly IWriteProtectionRepository _writeProtectionRepository;
+		private readonly IMessageBrokerIdentifier _messageBrokerIdentifier;
+
+		public WriteProtectionPersister(IUnitOfWorkFactory unitOfWorkFactory, 
+																	IWriteProtectionRepository writeProtectionRepository,
+																	IMessageBrokerIdentifier messageBrokerIdentifier)
+		{
+			_unitOfWorkFactory = unitOfWorkFactory;
+			_writeProtectionRepository = writeProtectionRepository;
+			_messageBrokerIdentifier = messageBrokerIdentifier;
+		}
+
+		public void Persist(ICollection<IPersonWriteProtectionInfo> writeProtections)
+		{
+			using (var unitOfWork = _unitOfWorkFactory.CreateAndOpenUnitOfWork())
+			{
+				_writeProtectionRepository.AddRange(writeProtections);
+				unitOfWork.PersistAll(_messageBrokerIdentifier);
+				writeProtections.Clear();
+			}
+		}
+	}
+}
