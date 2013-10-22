b0e9a8032227bf4cfc343d98aa623fb1d53e7b59
Erik Sundberg <none@none>
2013-10-22 15:37:01 +0200

    Sorting out merge

--- Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/Bug25131.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/Bug25131.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+using Teleopti.Analytics.Etl.Transformer.Job;
+using Teleopti.Analytics.Etl.Transformer.Job.Steps;
 using Teleopti.Ccc.TestCommon.TestData.Analytics;
 using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Ccc.TestCommon.TestData.Generic;
 using Teleopti.Ccc.TestCommon.TestData.Setups;
 using RequestType = Teleopti.Ccc.TestCommon.TestData.Analytics.RequestType;
 
@@ -28,48 +32,43 @@ namespace Teleopti.Analytics.Etl.IntegrationTest
 			var analyticsDataFactory = new AnalyticsDataFactory();
 			var dates = new CurrentWeekDates();
 			var datasource = new  ExistingDatasources(new UtcAndCetTimeZones());
-			var businesUnit = new Ccc.Domain.Common.BusinessUnit("bu");
-			businesUnit.SetId(Guid.NewGuid());
-			var businessUnit = new BusinessUnit(businesUnit, datasource);
+			var businessUnit = new BusinessUnit(TestState.BusinessUnit, datasource);
 			var requestType = new RequestType(1, "Absence Request", "absence");
 			var requestStatus = new RequestStatus(1, "Pending", "pending");
+			var person = TestState.TestDataFactory.Person("Ashley Andeen").Person;
 
 			analyticsDataFactory.Setup(new EternityAndNotDefinedDate());			
 			analyticsDataFactory.Setup(dates);
 			analyticsDataFactory.Setup(businessUnit);
 			analyticsDataFactory.Setup(requestType);
 			analyticsDataFactory.Setup(requestStatus);
-			analyticsDataFactory.Persist();
-			//Data.Apply(new AbsenceConfigurable{Color = "Red", Name = "Absence"});
-			//Data.Person("Ashley Andeen").Apply(new AbsenceRequestConfigurable
-			//    {
-			//        Absence = "Absence",
-			//        StartTime = DateTime.Parse("2013-10-22 00:00"),
-			//        EndTime = DateTime.Parse("2013-10-23 00:00")
-			//    });
-
-			Data.Person("Ashley Andeen").Apply(new StockholmTimeZone());
-			// sätt upp analytics data här
-			Data.Apply(new AbsenceConfigurable { Color = "Red", Name = "Absence" });
-			var analFakta = new AnalyticsDataFactory();
-			var person = TestState.TestDataFactory.Person("Ashley Andeen").Person;
-
-			var datasource = new  ExistingDatasources(new UtcAndCetTimeZones());
-			analFakta.Setup(new BusinessUnit(TestState.BusinessUnit, datasource));
-
-            //Valid from date id måste vi veta här när vi insertar så dom måste insertas först
-			analFakta.Setup(new Person(person, datasource, 0, new DateTime(2010, 1, 1),
+			
+			analyticsDataFactory.Setup(new Person(person, datasource, 0, new DateTime(2010, 1, 1),
 									   new DateTime(2059, 12, 31), 0, -2, 0, TestState.BusinessUnit.Id.Value,
 									   false));
-            analFakta.Setup(new Person(person, datasource, 1, new DateTime(2011, 1, 1),
+			analyticsDataFactory.Setup(new Person(person, datasource, 1, new DateTime(2011, 1, 1),
                                        new DateTime(2059, 12, 31), 0, -2, 0, TestState.BusinessUnit.Id.Value,
                                        true));
-			analFakta.Persist();
+			analyticsDataFactory.Persist();
+				
 
+			
+			// sätt upp analytics data här
+			Data.Person("Ashley Andeen").Apply(new StockholmTimeZone());
+			Data.Apply(new AbsenceConfigurable { Color = "Red", Name = "Absence" });
+			
+
+            //Valid from date id måste vi veta här när vi insertar så dom måste insertas först
+			
+			
 			//Act
 			// kör etl körning här
 			JobParameters parameters = null; //= new JobParameters()
 			var steps = new List<JobStepBase>
+				{
+					new IntradayStageRequestJobStep(parameters),
+					new FactRequestJobStep(parameters, true)
+				};
 
 			//Assert
 			// kolla att prylarna hamnade rätt i databasen här
