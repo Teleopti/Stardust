fd77625f6494ce2b7d140e9ed771897fd02c015c
Roger Kratz <roger.kratz@teleopti.com>
2013-10-23 13:46:05 +0200

    #23090 test + impl for scheduledictionarypersister

---
+++ Infrastructure/Persisters/Schedules/ScheduleDictionaryPersister.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Infrastructure.Persisters.Schedules
+{
+	public class ScheduleDictionaryPersister : IScheduleDictionaryPersister
+	{
+		private readonly IScheduleRangePersister _scheduleRangePersister;
+
+		public ScheduleDictionaryPersister(IScheduleRangePersister scheduleRangePersister)
+		{
+			_scheduleRangePersister = scheduleRangePersister;
+		}
+
+
+		public IEnumerable<PersistConflict> Persist(IScheduleDictionary scheduleDictionary)
+		{
+			var conflicts = new List<PersistConflict>();
+			foreach (var scheduleRange in scheduleDictionary.Values)
+			{
+				var res = _scheduleRangePersister.Persist(scheduleRange);
+				if (res != null)
+				{
+					conflicts.AddRange(res);					
+				}
+			}
+			return conflicts;
+		}
+	}
+}
