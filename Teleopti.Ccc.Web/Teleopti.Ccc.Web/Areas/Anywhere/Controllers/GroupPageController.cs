16b0db44425a3471c0139c66c4fa54025fee8e67
Tina Hammarstrom <none@none>
2013-10-23 13:19:14 +0200

    Adding groupPageController

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Controllers/GroupPageController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Web.Filters;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.Areas.Anywhere.Controllers
+{
+	public class GroupPageController : Controller
+	{
+		private readonly IGroupingReadOnlyRepository _groupingReadOnlyRepository;
+		private readonly ILoggedOnUser _loggedOnUser;
+
+		public GroupPageController(IGroupingReadOnlyRepository groupingReadOnlyRepository, ILoggedOnUser loggedOnUser)
+		{
+			_groupingReadOnlyRepository = groupingReadOnlyRepository;
+			_loggedOnUser = loggedOnUser;
+		}
+
+		[UnitOfWorkAction, HttpGet]
+		public JsonResult AvailableGroupPages(DateTime date)
+		{
+			var actualGroupPages =
+				_groupingReadOnlyRepository.AvailableGroupPages().Select(gp => new
+					{
+						Name = gp.PageName,
+						Groups = _groupingReadOnlyRepository.AvailableGroups(gp, new DateOnly(date)).Select(g => new
+							{
+								Name = g.GroupName,
+								Id = g.GroupId
+							}).Distinct().ToList()
+					});
+
+			var selectedGroupId = _loggedOnUser.CurrentUser().MyTeam(new DateOnly(date));
+
+			return Json(new {GroupPages = actualGroupPages.ToList(), SelectedGroupId = selectedGroupId}, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Controllers/GroupPageControllerTest.cs
@@ -0,0 +1,119 @@
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.Web.Areas.Anywhere.Controllers;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WebTest.Areas.Anywhere.Controllers
+{
+	public class GroupPageControllerTest
+	{
+		[Test]
+		public void ShouldReturnAvailableGroupPages()
+		{
+			var groupPage = new ReadOnlyGroupPage()
+				{
+					PageName = "Skill"
+				};
+			var group = new ReadOnlyGroupDetail()
+				{
+					GroupId = Guid.Empty,
+					GroupName = "Phone"
+				};
+
+			var target = new GroupPageController(new FakeGroupingReadOnlyRepository(groupPage, new List<ReadOnlyGroupDetail>(){ group }), new FakeLoggedOnUser());
+
+			dynamic result = target.AvailableGroupPages(DateTime.Now).Data;
+
+			dynamic gp = result.GroupPages[0];
+			((object)gp.Name).Should().Be.EqualTo(groupPage.PageName);
+			dynamic g = gp.Groups[0];
+			((object)g.Id).Should().Be.EqualTo(group.GroupId);
+			((object)g.Name).Should().Be.EqualTo(group.GroupName);
+		}
+
+		[Test]
+		public void ShouldReturnMyTeamAsDefaultGroup()
+		{
+			var site = SiteFactory.CreateSimpleSite(" ");
+			site.SetId(Guid.NewGuid());
+			var team = new Team() { Site = site, Description = new Description("Team red")};
+			team.SetId(Guid.NewGuid());
+			var person = PersonFactory.CreatePersonWithPersonPeriodFromTeam(DateOnly.Today, team);
+			var loggedOnUser = new FakeLoggedOnUser(person);
+
+			var firstGroup = new ReadOnlyGroupDetail()
+				{
+					GroupId = Guid.Empty,
+					GroupName = "Team green"
+				};
+
+			var secondGroup = new ReadOnlyGroupDetail()
+				{
+					GroupId = Guid.Empty,
+					GroupName = person.MyTeam(DateOnly.Today).Description.Name
+				};
+
+			var target = new GroupPageController(new FakeGroupingReadOnlyRepository(new ReadOnlyGroupPage(), new List<ReadOnlyGroupDetail>() { firstGroup, secondGroup }), loggedOnUser);
+
+			dynamic result = target.AvailableGroupPages(DateTime.Now).Data;
+
+			dynamic gp = result;
+			((object)gp.SelectedGroupId).Should().Be.EqualTo(loggedOnUser.CurrentUser().MyTeam(new DateOnly(DateTime.Now)));
+		}
+	}
+
+	public class FakeGroupingReadOnlyRepository : IGroupingReadOnlyRepository
+	{
+		public IList<ReadOnlyGroupPage> ReadOnlyGroupPages = new List<ReadOnlyGroupPage>();
+		public IList<ReadOnlyGroupDetail> ReadOnlyGroupDetails;
+
+		public FakeGroupingReadOnlyRepository(ReadOnlyGroupPage readOnlyGroupPage, List<ReadOnlyGroupDetail> readOnlyGroupDetails)
+		{
+			ReadOnlyGroupPages.Add(readOnlyGroupPage);
+			ReadOnlyGroupDetails = readOnlyGroupDetails;
+		}
+
+		public IEnumerable<ReadOnlyGroupPage> AvailableGroupPages()
+		{
+			return ReadOnlyGroupPages;
+		}
+
+		public IEnumerable<ReadOnlyGroupDetail> AvailableGroups(ReadOnlyGroupPage groupPage, DateOnly queryDate)
+		{
+			return ReadOnlyGroupDetails;
+		}
+
+		public IEnumerable<ReadOnlyGroupDetail> AvailableGroups(DateOnly queryDate)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<ReadOnlyGroupDetail> DetailsForGroup(Guid groupId, DateOnly queryDate)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateGroupingReadModel(ICollection<Guid> inputIds)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateGroupingReadModelGroupPage(ICollection<Guid> inputIds)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateGroupingReadModelData(ICollection<Guid> inputIds)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
