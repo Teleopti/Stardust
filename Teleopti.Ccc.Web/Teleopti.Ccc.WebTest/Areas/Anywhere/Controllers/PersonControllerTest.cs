2dc89e3fa9606e997b20d36ac63100d7453e2d65
Kunning Mao <none@none>
2013-10-24 16:03:31 +0200

    remove code not in use

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Controllers/PersonControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Controllers/PersonControllerTest.cs
@@ -18,71 +18,14 @@ namespace Teleopti.Ccc.WebTest.Areas.Anywhere.Controllers
 	public class PersonControllerTest
 	{
 		private PersonController target;
-		private ITeamProvider teamProvider;
 		private ISchedulePersonProvider schedulePersonProvider;
-		private ILoggedOnUser _loggedOnUser;
 
 		[SetUp]
 		public void Setup()
 		{
-			teamProvider = MockRepository.GenerateMock<ITeamProvider>();
 			schedulePersonProvider = MockRepository.GenerateMock<ISchedulePersonProvider>();
-			_loggedOnUser = MockRepository.GenerateMock<ILoggedOnUser>();
-			target = new PersonController(schedulePersonProvider, teamProvider, _loggedOnUser);
+			target = new PersonController(schedulePersonProvider);
 		}
-
-		//[Test]
-		//public void ShouldGetTeamsForDate()
-		//{
-		//	var date = new DateOnly(2012, 12, 01);
-		//	var team = TeamFactory.CreateTeam("Team", "Site");
-		//	team.SetId(Guid.NewGuid());
-
-		//	teamProvider.Stub(x => x.GetPermittedTeams(date, DefinedRaptorApplicationFunctionPaths.SchedulesAnywhere)).Return(
-		//		new[] {team});
-		//	target.ControllerContext = new ControllerContext(new FakeHttpContext("/"), new RouteData(), target);
-
-		//	dynamic result = target.AvailableTeams(date.Date).Data;
-		//	dynamic teamResult = result.Teams[0];
-		//	((object) teamResult.Id).Should().Be.EqualTo(team.Id);
-		//	((object) teamResult.SiteAndTeam).Should().Be.EqualTo(team.SiteAndTeam);
-		//}
-
-		//[Test]
-		//public void ShouldDefaultToMyTeamIfNoTeamAvailable()
-		//{
-		//	var date = new DateOnly(2012, 12, 01);
-		//	var team = TeamFactory.CreateTeam("Team", "Site");
-		//	team.SetId(Guid.NewGuid());
-		//	var person = MockRepository.GenerateMock<IPerson>();
-		//	person.Stub(x => x.MyTeam(date)).Return(team);
-		//	_loggedOnUser.Stub(x => x.CurrentUser()).Return(person);
-
-		//	teamProvider.Stub(x => x.GetPermittedTeams(date, DefinedRaptorApplicationFunctionPaths.SchedulesAnywhere)).Return(new ITeam[] { });
-		//	target.ControllerContext = new ControllerContext(new FakeHttpContext("/"), new RouteData(), target);
-
-		//	dynamic result = target.AvailableTeams(date.Date).Data;
-		//	dynamic teamResult = result.Teams[0];
-		//	((object)teamResult.Id).Should().Be.EqualTo(team.Id);
-		//	((object)teamResult.SiteAndTeam).Should().Be.EqualTo(team.SiteAndTeam);
-		//}
-
-		//[Test]
-		//public void ShouldReturnEmptyIfNoTeamAvailableAndNoMyTeam()
-		//{
-		//	var date = new DateOnly(2012, 12, 01);
-		//	var team = TeamFactory.CreateTeam("Team", "Site");
-		//	team.SetId(Guid.NewGuid());
-		//	var person = MockRepository.GenerateMock<IPerson>();
-		//	_loggedOnUser.Stub(x => x.CurrentUser()).Return(person);
-
-		//	teamProvider.Stub(x => x.GetPermittedTeams(date, DefinedRaptorApplicationFunctionPaths.SchedulesAnywhere)).Return(new ITeam[] { });
-		//	target.ControllerContext = new ControllerContext(new FakeHttpContext("/"), new RouteData(), target);
-
-		//	dynamic result = target.AvailableTeams(date.Date).Data;
-		//	dynamic teamsResult = result.Teams;
-		//	((IList)teamsResult).Count.Should().Be.EqualTo(0);
-		//}
 
 		[Test]
 		public void ShouldGetPeopleForDateAndGroup()
