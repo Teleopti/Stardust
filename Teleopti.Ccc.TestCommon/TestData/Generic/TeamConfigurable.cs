caf8ef1b564d163ef78f7f9a680a79d0fc30f6fd
Mathias Stenbom <mathias@stenbom.com>
2013-10-26 01:23:12 +0200

    fixing the build

--- Teleopti.Ccc.TestCommon/TestData/Generic/TeamConfigurable.cs
+++ Teleopti.Ccc.TestCommon/TestData/Generic/TeamConfigurable.cs
@@ -7,24 +7,24 @@ using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.TestCommon.TestData.Generic
 {
-    public class TeamConfigurable : IDataSetup
-    {
-        public string Name;
-        public string Site;
+	public class TeamConfigurable : IDataSetup
+	{
+		public string Name { get; set; }
+		public string Site { get; set; }
 
-        public Team Team { get; private set; }
+		public Team Team { get; private set; }
 
-        public void Apply(IUnitOfWork uow)
-        {
-            var siteRepository = new SiteRepository(uow);
-            var site = siteRepository.LoadAll().Single(c => c.Description.Name == Site);
-            Team = new Team
-            {
-                Description = new Description(Name),
-                Site = site
-            };
-            var teamRepository = new TeamRepository(uow);
-            teamRepository.Add(Team);
-        }
-    }
+		public void Apply(IUnitOfWork uow)
+		{
+			var siteRepository = new SiteRepository(uow);
+			var site = siteRepository.LoadAll().Single(c => c.Description.Name == Site);
+			Team = new Team
+			{
+				Description = new Description(Name),
+				Site = site
+			};
+			var teamRepository = new TeamRepository(uow);
+			teamRepository.Add(Team);
+		}
+	}
 }
