4defae43d6e86afb3904cf4bc2546324a7c63414
ola@teleopti.com <ola@teleopti.com>
2013-10-28 14:19:14 +0100

    tab

--- Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SanityTest.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.IntegrationTest/SanityTest.cs
@@ -3,22 +3,22 @@ using NUnit.Framework;
 
 namespace Teleopti.Analytics.Etl.IntegrationTest
 {
-    [TestFixture]
-    public class SanityTest
-    {
-        [Test]
-        public void ShouldBeSane()
-        {
-            Assert.That(true, Is.True);
-        }
+	[TestFixture]
+	public class SanityTest
+	{
+		[Test]
+		public void ShouldBeSane()
+		{
+			Assert.That(true, Is.True);
+		}
 
-        [Test]
-        public void ShouldLoadConfig()
-        {
+		[Test]
+		public void ShouldLoadConfig()
+		{
 			Assert.That(ConfigurationManager.AppSettings["configSanityCheck"],
-                Is.EqualTo("Yes, configuration is loaded!"),
-                "No! Configuration is not loaded!!!");
-        }
+				Is.EqualTo("Yes, configuration is loaded!"),
+				"No! Configuration is not loaded!!!");
+		}
 
-    }
+	}
 }
