0ef35c167ccf9acc4e34b9d715effe18f88a8e80
Asad MIrza <asad.mirza@teleopti.com>
2013-10-29 15:11:50 +0100

    Added effective restriction for team block

---
+++ Domain/Scheduling/TeamBlock/Restriction/TeamBlockEffectiveRestrcition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock.Restriction
+{
+    public class TeamBlockEffectiveRestrcition : IScheduleRestrictionStrategy
+    {
+        private readonly IEffectiveRestrictionCreator _effectiveRestrictionCreator;
+        private readonly IGroupPerson _groupPerson;
+        private readonly IScheduleDictionary _scheduleDictionary;
+        private readonly ISchedulingOptions _schedulingOptions;
+
+        public TeamBlockEffectiveRestrcition(IEffectiveRestrictionCreator effectiveRestrictionCreator,
+                                             IGroupPerson groupPerson, ISchedulingOptions schedulingOptions,
+                                             IScheduleDictionary scheduleDictionary)
+        {
+            _effectiveRestrictionCreator = effectiveRestrictionCreator;
+            _groupPerson = groupPerson;
+            _schedulingOptions = schedulingOptions;
+            _scheduleDictionary = scheduleDictionary;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="dateOnlyList"></param>
+        /// <param name="matrixList">Pass null as no matrix list is used here</param>
+        /// <returns></returns>
+        public IEffectiveRestriction ExtractRestriction(IList<DateOnly> dateOnlyList,
+                                                        IList<IScheduleMatrixPro> matrixList)
+        {
+            IEffectiveRestriction effectiveRestriction = null;
+            foreach (DateOnly dateOnly in dateOnlyList)
+            {
+                IEffectiveRestriction restriction =
+                    _effectiveRestrictionCreator.GetEffectiveRestriction(_groupPerson.GroupMembers,
+                                                                         dateOnly, _schedulingOptions,
+                                                                         _scheduleDictionary);
+                if (restriction == null)
+                    return null;
+                if (effectiveRestriction != null)
+                    effectiveRestriction = effectiveRestriction.Combine(restriction);
+                else
+                    effectiveRestriction = restriction;
+                if (effectiveRestriction == null)
+                    return null;
+            }
+            return effectiveRestriction;
+        }
+    }
+}
--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IScheduleRestrictionStrategy.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IScheduleRestrictionStrategy.cs
@@ -7,6 +7,13 @@ namespace Teleopti.Interfaces.Domain
 {
     public interface IScheduleRestrictionStrategy
     {
+        /// <summary>
+        /// Extracts the restrictions
+        /// some probable refactoring to pass on team block to get the restriction
+        /// </summary>
+        /// <param name="dateOnlyList"></param>
+        /// <param name="matrixList"></param>
+        /// <returns></returns>
         IEffectiveRestriction ExtractRestriction(IList<DateOnly> dateOnlyList, IList<IScheduleMatrixPro> matrixList);
     }
 }
