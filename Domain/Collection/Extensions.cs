5d24b7f64203528d9e6da8f76a51d7874adf09f1
Xianwei Shen <xianwei.shen@teleopti.com>
2013-10-31 13:24:50 +0100

    add is null or emtpy check extension for IEnumerable

--- Domain/Collection/Extensions.cs
+++ Domain/Collection/Extensions.cs
@@ -169,7 +169,11 @@ namespace Teleopti.Ccc.Domain.Collection
             return !source.Any();
         }
 
-
+		public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+		{
+			return source == null || !source.Any();
+		}
+		
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
         public static IEnumerable<T> CopyEnumerable<T>(this IEnumerable source)
         {
