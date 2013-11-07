f0aa15446e852481f7d0702d28f2d132f9adb7e0
Asad MIrza <asad.mirza@teleopti.com>
2013-11-07 16:00:26 +0100

    Bug 25613: Remove the unwanted iterations to schedule the white posts
    --HG--
    branch : Release 7.4.384

---
+++ Win/Scheduling/TeleoptiProgressChangeMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Teleopti.Ccc.Win.Scheduling
+{
+    class TeleoptiProgressChangeMessage
+    {
+        public string Message { get; set; }
+
+        public TeleoptiProgressChangeMessage(string message)
+        {
+            Message = message;
+        }
+    }
+}
