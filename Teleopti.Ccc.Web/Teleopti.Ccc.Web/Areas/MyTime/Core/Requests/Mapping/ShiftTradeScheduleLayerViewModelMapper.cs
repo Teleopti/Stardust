e2ad251c560b4846cdea88fcec83e07e139fb405
Jonas Nordh <jonas.nordh@teleopti.com>
2013-11-12 17:19:47 +0100

    layer tooltip in "add shift trade"

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/ShiftTradeScheduleLayerViewModelMapper.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/ShiftTradeScheduleLayerViewModelMapper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.PersonScheduleDayReadModel;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.Requests;
@@ -14,13 +15,15 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.Mapping
 
 		private static ShiftTradeScheduleLayerViewModel mapLayer(SimpleLayer sourceLayer)
 		{
+			var expectedTitle = string.Format(CultureInfo.CurrentCulture, "{0}\n{1}-{2}", sourceLayer.Description,
+											  sourceLayer.Start.ToShortTimeString(), sourceLayer.End.ToShortTimeString());
 			return new ShiftTradeScheduleLayerViewModel
 				{
 					Start = sourceLayer.Start,
 					End = sourceLayer.End,
 					LengthInMinutes = sourceLayer.Minutes,
 					Color = sourceLayer.Color,
-					Title = sourceLayer.Description,
+					Title = expectedTitle,
 					IsAbsenceConfidential = sourceLayer.IsAbsenceConfidential
 				};
 		}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Requests/Mapping/ShiftTradeScheduleLayerViewModelMapperTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Requests/Mapping/ShiftTradeScheduleLayerViewModelMapperTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
@@ -31,8 +32,45 @@ namespace Teleopti.Ccc.WebTest.Core.Requests.Mapping
 			mappedlayer.End.Should().Be.EqualTo(readModelLayer.End);
 			mappedlayer.LengthInMinutes.Should().Be.EqualTo(readModelLayer.Minutes);
 			mappedlayer.Color.Should().Be.EqualTo(readModelLayer.Color);
-			mappedlayer.Title.Should().Be.EqualTo(readModelLayer.Description);
 			mappedlayer.IsAbsenceConfidential.Should().Be.EqualTo(readModelLayer.IsAbsenceConfidential);
+		}
+
+		[Test, SetCulture("en-US")]
+		public void ShouldMapLayerTitleToUsCulture()
+		{
+			var startDateTime = DateTime.Now;
+			var readModelLayer = new SimpleLayer
+			{
+				Start = startDateTime,
+				End = startDateTime.AddHours(1),
+				Description = "Phone"
+			};
+
+			var target = new ShiftTradeScheduleLayerViewModelMapper();
+			var result = target.Map(new[] { readModelLayer });
+
+			var expectedTitle = string.Format(CultureInfo.CurrentCulture, "{0}\n{1}-{2}", readModelLayer.Description,
+			                                  readModelLayer.Start.ToShortTimeString(), readModelLayer.End.ToShortTimeString());
+			result.First().Title.Should().Be.EqualTo(expectedTitle);
+		}
+
+		[Test, SetCulture("sv-SE")]
+		public void ShouldMapLayerTitleToSwedishCulture()
+		{
+			var startDateTime = DateTime.Now;
+			var readModelLayer = new SimpleLayer
+			{
+				Start = startDateTime,
+				End = startDateTime.AddHours(1),
+				Description = "Phone"
+			};
+
+			var target = new ShiftTradeScheduleLayerViewModelMapper();
+			var result = target.Map(new[] { readModelLayer });
+
+			var expectedTitle = string.Format(CultureInfo.CurrentCulture, "{0}\n{1}-{2}", readModelLayer.Description,
+											  readModelLayer.Start.ToShortTimeString(), readModelLayer.End.ToShortTimeString());
+			result.First().Title.Should().Be.EqualTo(expectedTitle);
 		}
 	}
 }
