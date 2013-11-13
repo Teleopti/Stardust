0de9b6644417ee4dfc6fb2cfb766fd3c10f9055c
David Jonsson <david.jonsson@teleopti.com>
2013-11-13 13:37:35 +0100

    Left join to fetch unknown persons

--- Database/TeleoptiAnalytics/Programmability/01Views/mart.v_fact_agent_state_merge.sql
+++ Database/TeleoptiAnalytics/Programmability/01Views/mart.v_fact_agent_state_merge.sql
@@ -5,14 +5,14 @@ CREATE VIEW mart.v_fact_agent_state_merge
 AS
 SELECT 
 date_id			= d.date_id,
-person_id		= dp.person_id,
+person_id		= ISNULL(dp.person_id,-1),
 interval_id		= i.interval_id,
 state_group_id	= sg.state_group_id,
 time_in_state_s	= sum(stg.time_in_state_s),
 datasource_id	= 1,
 insert_date		= cast(getdate() as smalldatetime)
-FROM [stage].[stg_agent_state] stg WITH(TABLOCKX)
-INNER JOIN mart.dim_person dp
+FROM [stage].[stg_agent_state] stg WITH(TABLOCKX) --Lock the table for trigger insert during our data load and later truncate
+LEFT JOIN mart.dim_person dp
 	ON stg.person_code = dp.person_code
 	AND --trim
 		(
@@ -26,6 +26,5 @@ INNER JOIN mart.dim_date d
 	ON stg.[date] = d.date_date
 INNER JOIN mart.dim_interval i
 	ON stg.interval BETWEEN i.interval_start AND i.interval_end
-WHERE stg.time_in_state_s > 0
 GROUP BY date_id,person_id,interval_id,state_group_id
 GO
