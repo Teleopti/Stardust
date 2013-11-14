877faaab739bef534d2556bad77573ab7df312b9
Mathias Stenbom <none@none>
2013-11-14 09:17:35 +0100

    minor refact

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Anywhere/ApplicationPageStepDefinition.cs
@@ -0,0 +1,15 @@
+﻿using TechTalk.SpecFlow;
+using Teleopti.Ccc.WebBehaviorTest.Core;
+
+namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.Anywhere
+{
+	[Binding]
+	public class ApplicationPageStepDefinition
+	{
+		[Then(@"I should see Anywhere")]
+		public void ThenIShouldSeeAnywhere()
+		{
+			Browser.Interactions.AssertExists("#content-placeholder");
+		}
+	}
+}
