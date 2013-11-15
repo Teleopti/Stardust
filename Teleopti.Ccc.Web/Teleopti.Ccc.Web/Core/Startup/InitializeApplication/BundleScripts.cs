475020f23d114644fb6f9ebf2f9d185b91837f17
Roger Kratz <roger.kratz@teleopti.com>
2013-11-15 13:24:28 +0100

    Starting to bundle (and minimy) css for MyTimeWeb

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Startup/InitializeApplication/BundleScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Web.Optimization;
+using Teleopti.Ccc.Web.Core.Startup.Booter;
+
+namespace Teleopti.Ccc.Web.Core.Startup.InitializeApplication
+{
+	[TaskPriority(20)]
+	public class BundleScripts : IBootstrapperTask
+	{
+		public Task Execute()
+		{
+			var cssBundle = new StyleBundle("~/MyTimeCss");
+			cssBundle.IncludeDirectory("~/Areas/MyTime/Content/Css", "*.css");
+			BundleTable.Bundles.Add(cssBundle);
+			return null;
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Startup/BundleScriptsTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Core.Startup.InitializeApplication;
+
+namespace Teleopti.Ccc.WebTest.Core.Startup
+{
+	//can't really find a good way unit tests this without just repeating real code here...
+	//realy on web behavior tests 
+
+	[TestFixture]
+	public class BundleScriptsTest
+	{
+		 [Test]
+		 public void ShouldAddClientFiles()
+		 {
+			 BundleTable.Bundles.Clear();
+			 new BundleScripts().Execute();
+			 BundleTable.Bundles.Should().Not.Be.Empty();
+		 }
+	}
+}
