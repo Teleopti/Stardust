f8baca5a284a2c31336742f799beb28c178f7f82
Roger Kratz <roger.kratz@teleopti.com>
2013-11-15 13:34:23 +0100

    adding 3rd party css to mytimeccc bundle

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Startup/InitializeApplication/BundleScripts.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Startup/InitializeApplication/BundleScripts.cs
@@ -9,8 +9,17 @@ namespace Teleopti.Ccc.Web.Core.Startup.InitializeApplication
 	{
 		public Task Execute()
 		{
-			var cssBundle = new StyleBundle("~/MyTimeCss");
-			cssBundle.IncludeDirectory("~/Areas/MyTime/Content/Css", "*.css");
+			var cssBundle = new StyleBundle("~/MyTimeCss")
+				.IncludeDirectory("~/Areas/MyTime/Content/Css", "*.css")
+				.Include(
+					"~/Content/bootstrap/bootstrap.css",
+					"~/Content/moment-datepicker/datepicker.css",
+					"~/Content/jqueryui/smoothness/jquery-ui-1.10.2.custom.css",
+					"~/Content/Scripts/jquery.qtip.css",
+					"~/Content/select2/select2.css",
+					"~/Content/bootstrap-timepicker/css/bootstrap-timepicker.min.css",
+					"~/Content/Scripts/pinify/content/jquery.pinify.min.css"
+				);
 			BundleTable.Bundles.Add(cssBundle);
 			return null;
 		}
