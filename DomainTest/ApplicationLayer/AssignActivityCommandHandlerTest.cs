398c505f185ce56336ec4ea1dd56cfd704726e91
Mathias Stenbom <none@none>
2013-11-18 09:00:18 +0100

    putting stuff in its place

--- DomainTest/ApplicationLayer/AssignActivityCommandHandlerTest.cs
+++ DomainTest/ApplicationLayer/AssignActivityCommandHandlerTest.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
-using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Commands;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.Common;
@@ -117,40 +116,5 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 			addedLayer.Period.EndDateTime.Should().Be(TimeZoneHelper.ConvertToUtc(command.EndTime, TimeZoneInfoFactory.HawaiiTimeZoneInfo()));
 		}
 
-	}
-
-	public class AssignActivityCommandHandler : IHandleCommand<AssignActivityCommand>
-	{
-		private readonly IProxyForId<IActivity> _activityForId;
-		private readonly IWriteSideRepositoryTypedId<IPersonAssignment, PersonAssignmentKey> _personAssignmentRepository;
-		private readonly ICurrentScenario _currentScenario;
-		private readonly IProxyForId<IPerson> _personForId;
-		private readonly IUserTimeZone _timeZone;
-
-		public AssignActivityCommandHandler(
-			IWriteSideRepositoryTypedId<IPersonAssignment, PersonAssignmentKey> personAssignmentRepository,
-			ICurrentScenario currentScenario, IProxyForId<IActivity> activityForId, IProxyForId<IPerson> personForId,
-			IUserTimeZone timeZone)
-		{
-			_activityForId = activityForId;
-			_personAssignmentRepository = personAssignmentRepository;
-			_currentScenario = currentScenario;
-			_personForId = personForId;
-			_timeZone = timeZone;
-		}
-
-		public void Handle(AssignActivityCommand command)
-		{
-			var activity = _activityForId.Load(command.ActivityId);
-			var personAssignment = _personAssignmentRepository.LoadAggregate(new PersonAssignmentKey
-				{
-					Date = command.Date,
-					Scenario = _currentScenario.Current(),
-					Person = _personForId.Load(command.PersonId)
-				});
-			var period = new DateTimePeriod(TimeZoneHelper.ConvertToUtc(command.StartTime, _timeZone.TimeZone()), TimeZoneHelper.ConvertToUtc(command.EndTime, _timeZone.TimeZone()));
-
-			personAssignment.AssignActivity(activity, period);
-		}
 	}
 }
