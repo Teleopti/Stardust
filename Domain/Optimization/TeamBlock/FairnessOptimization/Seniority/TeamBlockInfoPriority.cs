8aed3da69379135aeb0d709410c00ede42468096
Claes Stenman <claes.stenman@teleopti.com>
2013-12-05 08:49:03 +0100

    Added test...

--- Domain/Optimization/TeamBlock/FairnessOptimization/Seniority/TeamBlockInfoPriority.cs
+++ Domain/Optimization/TeamBlock/FairnessOptimization/Seniority/TeamBlockInfoPriority.cs
@@ -2,17 +2,24 @@
 
 namespace Teleopti.Ccc.Domain.Optimization.TeamBlock.FairnessOptimization.Seniority
 {
-    public  class TeamBlockInfoPriority
+	public interface ITeamBlockInfoPriority
+	{
+		ITeamBlockInfo TeamBlockInfo { get; }
+		double Seniority { get; }
+		int ShiftCategoryPriority { get; }	
+	}
+
+	public class TeamBlockInfoPriority : ITeamBlockInfoPriority
     {
+		public ITeamBlockInfo TeamBlockInfo { get; private set; }
+		public double Seniority { get; private set; }
+		public int ShiftCategoryPriority { get; private set; }
+
         public TeamBlockInfoPriority(ITeamBlockInfo teamBlockInfo, double seniority, int shiftCategoryPriority)
         {
             TeamBlockInfo = teamBlockInfo;
             Seniority = seniority;
             ShiftCategoryPriority = shiftCategoryPriority;
-        }
-        
-        public ITeamBlockInfo TeamBlockInfo { get; set; }
-        public double Seniority { get; set; }
-        public int ShiftCategoryPriority { get; set; }
+        }    
     }
 }
