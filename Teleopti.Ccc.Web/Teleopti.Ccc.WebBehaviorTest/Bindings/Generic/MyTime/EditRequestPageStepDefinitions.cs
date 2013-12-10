143070989555cb5b9a3e091cc5509e85294d72a8
Xinfeng Li <xinfeng.li@teleopti.com>
2013-12-10 12:51:42 +0800

    Fix broken web feature test.
    --HG--
    branch : Release 7.4.388

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/MyTime/EditRequestPageStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/MyTime/EditRequestPageStepDefinitions.cs
@@ -67,7 +67,6 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.MyTime
 		public void ThenIShouldNotBeAbleToInputValuesForTextRequestAtPositionInTheList(int position)
 		{
 			Browser.Interactions.AssertNotVisibleUsingJQuery(string.Format(".request:nth-child({0}) .request-edit-subject", position));
-			Browser.Interactions.AssertNotExists(string.Format(".request:nth-child({0}) .request-non-edit-message", position), string.Format(".request:nth-child({0}) .request-edit-message", position));
  
 			Browser.Interactions.AssertExists(string.Format(".request:nth-child({0}) .request-edit-datefrom:not(:enabled)", position));
 			Browser.Interactions.AssertExists(string.Format(".request:nth-child({0}) .request-edit-timefrom:not(:enabled)", position));
