5b80372ef572909f379dd29cbb3c897b1c6150c4
David Jonsson <david.jonsson@teleopti.com>
2013-12-17 15:47:23 +0100

    bug #26204 - add supporting indexes to the stg_schedule view
    --HG--
    branch : Release 7.4.388

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -260,7 +260,7 @@ GO
 --Date: 2013-12-16
 --Desc: bug #26204 - make hard join on null guid instead of isnull
 ----------------
---dim
+--make sure we use a zero guid instead of dbNull
 update mart.dim_absence
 set absence_code='00000000-0000-0000-0000-000000000000'
 where absence_id=-1
@@ -276,3 +276,43 @@ where overtime_id=-1
 update mart.dim_shift_category
 set shift_category_code='00000000-0000-0000-0000-000000000000'
 where shift_category_id =-1
+
+--add supporting indexes to the stg_schedule view
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_activity]') AND name = N'IX_activity_code')
+CREATE NONCLUSTERED INDEX [IX_activity_code] ON [mart].[dim_activity]
+(
+	[activity_code] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_absence]') AND name = N'IX_absence_code')
+CREATE NONCLUSTERED INDEX [IX_absence_code] ON [mart].[dim_absence]
+(
+	[absence_code] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_scenario]') AND name = N'IX_scenario_code')
+CREATE NONCLUSTERED INDEX [IX_scenario_code] ON [mart].[dim_scenario]
+(
+	[scenario_code] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_shift_category]') AND name = N'IX_shift_category_code')
+CREATE NONCLUSTERED INDEX [IX_shift_category_code] ON [mart].[dim_shift_category]
+(
+	[shift_category_code] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_shift_length]') AND name = N'IX_shift_length_m')
+CREATE NONCLUSTERED INDEX [IX_shift_length_m] ON [mart].[dim_shift_length]
+(
+	[shift_length_m] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_overtime]') AND name = N'IX_dim_overtime_code')
+CREATE NONCLUSTERED INDEX [IX_dim_overtime_code] ON [mart].[dim_overtime]
+(
+	[overtime_code] ASC
+)
+
+GO
+
