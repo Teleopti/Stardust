212ea598a2dae8f4e6274a7f004e138f37b8dc0e
Mathias Stenbom <mathias@stenbom.com>
2014-01-07 09:20:11 +0100

    removing unused code

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/shift.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/shift.js
@@ -18,15 +18,10 @@ define([
 
 		this.Layers = ko.observableArray();
 
-		// refact
-		this.IsFullDayAbsence = ko.observable();
-		
 		this.AddLayers = function (data) {
 			var newItems = ko.utils.arrayMap(data.Projection, function (l) {
 				l.Offset = data.Offset;
 				l.IsFullDayAbsence = data.IsFullDayAbsence;
-				// refact
-				self.IsFullDayAbsence(data.IsFullDayAbsence);
 				return new layer(timeline, l);
 			});
 			self.Layers.push.apply(self.Layers, newItems);
