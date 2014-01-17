76af085989adfc251f0824a33243bdb677fdf0a0
Ola <none@none>
2014-01-17 17:11:38 +0100

    some layout things on dummy data

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.MyReport.js
@@ -0,0 +1,32 @@
+﻿//MyReport = (function() {
+	
+	function MyReportViewModel() {
+		var self = this;
+
+		self.Adherence = ko.observable();
+		self.AnsweredCalls = ko.observable();
+		self.AverageAfterWork = ko.observable();
+		self.AverageHandlingTime = ko.observable();
+		self.AverageTalkTime = ko.observable();
+		self.Readiness = ko.observable();
+		$.ajax({
+			url: 'MyTime/MyReport/OnDates',
+			dataType: 'json',
+			success: function(data) {
+				self.Adherence(data.Adherence);
+				self.AnsweredCalls(data.AnsweredCalls);
+				self.AverageAfterWork(data.AverageAfterWork);
+				self.AverageHandlingTime(data.AverageHandlingTime);
+				self.AverageTalkTime(data.AverageTalkTime);
+				self.Readiness(data.Readiness);
+			},
+			error: function(xhr, ajaxOptions, thrownError) {
+				alert(xhr.status);
+				alert(xhr.responseText);
+				alert(thrownError);
+			}
+		});
+	}
+
+	
+//})(jQuery)
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/MyReportController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/MyReportController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Filters;
 using Teleopti.Ccc.Web.Filters;
 
@@ -13,5 +14,48 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 			return View("MyReportPartial");
         }
 
+		//[HttpGet]
+		//public JsonResult OnDates(DateTime startDate, DateTime endDate)
+		//{
+		//	var model = new MyReportModel
+		//		{
+		//			Adherence = .86,
+		//			AnsweredCalls = 55,
+		//			AverageAfterWork = 15,
+		//			AverageHandlingTime = 135,
+		//			AverageTalkTime = 120,
+		//			Readiness = .88
+		//		};
+
+		//	return Json(model, JsonRequestBehavior.AllowGet);
+		//}
+
+		[HttpGet]
+		public JsonResult OnDates()
+		{
+			var model = new MyReportModel
+			{
+				Adherence = .86,
+				AnsweredCalls = 55,
+				AverageAfterWork = 15,
+				AverageHandlingTime = 135,
+				AverageTalkTime = 120,
+				Readiness = .88
+			};
+
+			return Json(model, JsonRequestBehavior.AllowGet);
+		}
     }
+
+	//temp here only
+	public class MyReportModel
+	{
+		public double Adherence { get; set; }
+		public double Readiness { get; set; }
+		public int AverageHandlingTime { get; set; }
+		public int AverageTalkTime { get; set; }
+		public int AverageAfterWork { get; set; }
+		public int AnsweredCalls { get; set; }
+	}
+
 }
