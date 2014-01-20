24df027c8302c5bf2e96e4ddcb2f8d22ee09237e
Claes Stenman <claes.stenman@teleopti.com>
2014-01-20 12:29:05 +0100

    Bugfix:#26591 Schedule Days off according to contract schedule does not work...
    --HG--
    branch : Release 7.4.384

--- Domain/Scheduling/DayOffScheduling/DayOffOnPeriod.cs
+++ Domain/Scheduling/DayOffScheduling/DayOffOnPeriod.cs
@@ -39,11 +39,18 @@ namespace Teleopti.Ccc.Domain.Scheduling.DayOffScheduling
 			var contractDayOffs = new List<IScheduleDay>();
 			IList<KeyValuePair<IScheduleDay, int>> bestSpotList = new List<KeyValuePair<IScheduleDay, int>>();
 
+			var allContractDayOffIsSatisfied = true;
+
 			foreach (var scheduleDay in _scheduleDays)
 			{
-				if(hasContractDayOffDefinition.IsDayOff(scheduleDay))
+				if (hasContractDayOffDefinition.IsDayOff(scheduleDay))
+				{
+					if (scheduleDay.PersonDayOffCollection().Count == 0) allContractDayOffIsSatisfied = false;
 					contractDayOffs.Add(scheduleDay);
+				}
 			}
+
+			if (allContractDayOffIsSatisfied) return null;
 
 			foreach (var scheduleDay in _scheduleDays)
 			{
