73503f7e53d7ee4c7f6130061939b0f7aff4ceca
Jonas Nordh <jonas.nordh@teleopti.com>
2014-01-21 14:45:38 +0100

    creating base class for web report tests

--- InfrastructureTest/WebReports/DailyMetricsForDay/SimpleAnsweredCallsTest.cs
+++ InfrastructureTest/WebReports/DailyMetricsForDay/SimpleAnsweredCallsTest.cs
@@ -1,71 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Globalization;
-using System.Linq;
-using NUnit.Framework;
-using Teleopti.Ccc.Infrastructure.UnitOfWork;
+﻿using NUnit.Framework;
 using Teleopti.Ccc.Infrastructure.WebReports;
-using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.TestData.Analytics;
 using Teleopti.Ccc.TestCommon.TestData.Core;
 using Teleopti.Interfaces.Domain;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.InfrastructureTest.WebReports.DailyMetricsForDay
 {
 	[TestFixture]
-	public class SimpleAnsweredCallsTest
+	public class SimpleAnsweredCallsTest : WebReportTest
 	{
-		private AnalyticsDataFactory _analyticsDataFactory;
-		private IUnitOfWork _unitOfWork;
+		private const int answeredCalls = 3;
 
-		[SetUp]
-		public void Setup()
+		protected override void InsertTestSpecificData(AnalyticsDataFactory analyticsDataFactory)
 		{
-			_unitOfWork = UnitOfWorkFactory.CurrentUnitOfWorkFactory().LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork();
-			_analyticsDataFactory = new AnalyticsDataFactory();
+			analyticsDataFactory.Setup(new FactAgentQueue(0, 1, 1, 1, 1, 1, answeredCalls, 1, Datasource.RaptorDefaultDatasourceId));
 		}
-
-		[TearDown]
-		public void Teardown()
-		{
-			//DataSourceHelper.ClearAnalyticsData();
-			_unitOfWork.Dispose();
-		}
-
-		
 
 		[Test]
 		public void ShouldReturnAnsweredCalls()
 		{
-			const int answeredCalls = 3;
-
-			var timeZones = new UtcAndCetTimeZones();
-			var dates = new TodayDate();
-			var intervals = new QuarterOfAnHourInterval();
-			var dataSource = new ExistingDatasources(timeZones);
-
-			var agent = new Person(SetupFixtureForAssembly.loggedOnPerson, dataSource, 0, new DateTime(2010, 1, 1),
-					   new DateTime(2059, 12, 31), 0, -2, 0, BusinessUnitFactory.BusinessUnitUsedInTest.Id.Value,
-					   false);
-			var scenario = Scenario.DefaultScenarioFor(1, BusinessUnitFactory.BusinessUnitUsedInTest.Id.Value);
-
-			_analyticsDataFactory.Setup(new EternityAndNotDefinedDate());
-			_analyticsDataFactory.Setup(timeZones);
-			_analyticsDataFactory.Setup(dates);
-			_analyticsDataFactory.Setup(intervals);
-			_analyticsDataFactory.Setup(dataSource);
-			_analyticsDataFactory.Setup(new FillBridgeTimeZoneFromData(dates, intervals, timeZones, dataSource));
-			_analyticsDataFactory.Setup(agent);
-			//acd lgin
-			_analyticsDataFactory.Setup(new FillBridgeAcdLoginPersonFromData(agent, 1));
-			_analyticsDataFactory.Setup(scenario);
-			_analyticsDataFactory.Setup(new FactAgentQueue(0, 1, 1, 1, 1, 1, answeredCalls, 1, dataSource.RaptorDefaultDatasourceId));
-
-			_analyticsDataFactory.Persist();
-
 			var result = new DailyMetricsForDayQuery().Execute(new DateOnlyPeriod(2000, 1, 1, 2020, 1, 1), 1, 1, SetupFixtureForAssembly.loggedOnPerson, BusinessUnitFactory.BusinessUnitUsedInTest);
 			Assert.That(result.AnsweredCalls,Is.EqualTo(answeredCalls));
 		}
---
+++ InfrastructureTest/WebReports/DailyMetricsForDay/WebReportTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.TestData.Analytics;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.InfrastructureTest.WebReports.DailyMetricsForDay
+{
+	[TestFixture]
+	public abstract class WebReportTest
+	{
+		private AnalyticsDataFactory _analyticsDataFactory;
+		private IUnitOfWork _unitOfWork;
+		protected ExistingDatasources Datasource;
+
+		[SetUp]
+		public void Setup()
+		{
+			_unitOfWork = UnitOfWorkFactory.CurrentUnitOfWorkFactory().LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork();
+			_analyticsDataFactory = new AnalyticsDataFactory();
+			insertCommonData();
+		}
+
+		private void insertCommonData()
+		{
+			var timeZones = new UtcAndCetTimeZones();
+			var dates = new TodayDate();
+			var intervals = new QuarterOfAnHourInterval();
+			Datasource = new ExistingDatasources(timeZones);
+
+			var agent = new Person(SetupFixtureForAssembly.loggedOnPerson, Datasource, 0, new DateTime(2010, 1, 1),
+					   new DateTime(2059, 12, 31), 0, -2, 0, BusinessUnitFactory.BusinessUnitUsedInTest.Id.Value,
+					   false);
+			var scenario = Scenario.DefaultScenarioFor(1, BusinessUnitFactory.BusinessUnitUsedInTest.Id.Value);
+
+			_analyticsDataFactory.Setup(new EternityAndNotDefinedDate());
+			_analyticsDataFactory.Setup(timeZones);
+			_analyticsDataFactory.Setup(dates);
+			_analyticsDataFactory.Setup(intervals);
+			_analyticsDataFactory.Setup(Datasource);
+			_analyticsDataFactory.Setup(new FillBridgeTimeZoneFromData(dates, intervals, timeZones, Datasource));
+			_analyticsDataFactory.Setup(agent);
+			//acd lgin
+			_analyticsDataFactory.Setup(new FillBridgeAcdLoginPersonFromData(agent, 1));
+			_analyticsDataFactory.Setup(scenario);
+
+			InsertTestSpecificData(_analyticsDataFactory);
+			_analyticsDataFactory.Persist();
+		}
+
+		protected abstract void InsertTestSpecificData(AnalyticsDataFactory analyticsDataFactory);
+
+		[TearDown]
+		public void Teardown()
+		{
+			//DataSourceHelper.ClearAnalyticsData();
+			_unitOfWork.Dispose();
+		} 
+	}
+}
