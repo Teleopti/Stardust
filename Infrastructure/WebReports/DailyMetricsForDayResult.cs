f6f1041f05a9bb87a23fa4a1f3474938f50d0df7
Roger Kratz <roger.kratz@teleopti.com>
2014-01-23 14:31:51 +0100

    Adding test + impl for No Data Available

--- Infrastructure/WebReports/DailyMetricsForDayResult.cs
+++ Infrastructure/WebReports/DailyMetricsForDayResult.cs
@@ -2,10 +2,16 @@
 {
 	public class DailyMetricsForDayResult
 	{
+		public DailyMetricsForDayResult()
+		{
+			DataAvailable = true;
+		}
+
 		public int AnsweredCalls { get; set; }
 		public int AfterCallWorkTime { get; set; }
 		public int TalkTime { get; set; }
 		public int HandlingTime { get; set; }
 		public int ReadyTimePerScheduledReadyTime { get; set; }
+		public bool DataAvailable { get; set; }
 	}
 }
---
+++ InfrastructureTest/WebReports/DailyMetricsForDay/NoDataAvailableTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.WebReports.DailyMetricsForDay
+{
+	[TestFixture]
+	public class NoDataAvailableTest : WebReportTest
+	{
+		protected override void InsertTestSpecificData(AnalyticsDataFactory analyticsDataFactory)
+		{
+		}
+
+		[Test]
+		public void ShouldHaveDataAvailableSetToFalse()
+		{
+			Target().Execute(new DateOnlyPeriod(2000, 1, 1, 2020, 1, 1), 1, 1, SetupFixtureForAssembly.loggedOnPerson)
+				.DataAvailable.Should().Be.False();
+		}
+	}
+}
