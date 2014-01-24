c9920c2a0693a81dc97d75b69fbb53153a392d72
Anders Forsberg <anders.forsberg@teleopti.com>
2014-01-24 20:46:04 +0100

    Feature #26164 - Add purge of old requests to purge routine
    --HG--
    branch : Release 7.5.390

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -12,3 +12,12 @@ CREATE NONCLUSTERED INDEX [IX_Team_Site] ON [dbo].[Team]
 	[Site] ASC
 )
 GO
+
+----------------  
+--Name: Anders Forsberg
+--Date: 2013-12-05
+--Desc: Feature #26164 - Add purge of old requests to purge routine
+---------------- 
+if not exists (select 1 from PurgeSetting where [Key] = 'MonthsToKeepRequests')
+	insert into PurgeSetting ([Key], [Value]) values('MonthsToKeepRequests', 120)
+GO
