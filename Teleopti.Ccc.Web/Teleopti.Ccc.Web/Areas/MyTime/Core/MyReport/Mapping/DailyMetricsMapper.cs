5f93f0985d075699f470885ac39350b27847d403
Maria Stein <none@none>
2014-01-28 14:04:34 +0100

    Mapping daily metrics

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/MyReport/Mapping/DailyMetricsMapper.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/MyReport/Mapping/DailyMetricsMapper.cs
@@ -1,4 +1,5 @@
-﻿using Teleopti.Ccc.Infrastructure.WebReports;
+﻿using System.Globalization;
+using Teleopti.Ccc.Infrastructure.WebReports;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.MyReport;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Core.MyReport.Mapping
@@ -7,7 +8,15 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.MyReport.Mapping
 	{
 		public DailyMetricsViewModel Map(DailyMetricsForDayResult dataModel)
 		{
-			throw new System.NotImplementedException();
+			return new DailyMetricsViewModel
+				{
+					AnsweredCalls = dataModel.AnsweredCalls,
+					AverageAfterCallWork = dataModel.AfterCallWorkTimeAverage.TotalSeconds.ToString(CultureInfo.InvariantCulture),
+					AverageTalkTime = dataModel.TalkTimeAverage.TotalSeconds.ToString(CultureInfo.InvariantCulture),
+					AverageHandlingTime = dataModel.HandlingTimeAverage.TotalSeconds.ToString(CultureInfo.InvariantCulture),
+					ReadyTimePerScheduledReadyTime = dataModel.ReadyTimePerScheduledReadyTime.Value.ToString(CultureInfo.InvariantCulture),
+					Adherence = dataModel.Adherence.Value.ToString(CultureInfo.InvariantCulture)
+				};
 		}
 	}
 }
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/MyReport/Mapping/DailyMetricsMapperTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/MyReport/Mapping/DailyMetricsMapperTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Infrastructure.WebReports;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.MyReport.Mapping;
@@ -8,12 +9,6 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.MyReport.Mapping
 	[TestFixture]
 	public class DailyMetricsMapperTest
 	{
-		[SetUp]
-		public void Setup()
-		{
-			
-		}
-
 		[Test]
 		public void ShouldMapAnsweredCalls()
 		{
@@ -21,7 +16,57 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.MyReport.Mapping
 			var dataModel = new DailyMetricsForDayResult {AnsweredCalls = 55};
 			var viewModel = target.Map(dataModel);
 
-			viewModel.AnsweredCalls.Should().Be.EqualTo(dataModel.AnsweredCalls)
+			viewModel.AnsweredCalls.Should().Be.EqualTo(dataModel.AnsweredCalls);
+		}
+
+		[Test]
+		public void ShouldMapAfterCallWorkTimeAverage()
+		{
+			var target = new DailyMetricsMapper();
+			var dataModel = new DailyMetricsForDayResult { AfterCallWorkTimeAverage = new TimeSpan(0,0,40) };
+			var viewModel = target.Map(dataModel);
+
+			viewModel.AverageAfterCallWork.Should().Be.EqualTo(dataModel.AfterCallWorkTimeAverage.TotalSeconds.ToString());
+		}
+
+		[Test]
+		public void ShouldMapTalkTimeAverage()
+		{
+			var target = new DailyMetricsMapper();
+			var dataModel = new DailyMetricsForDayResult { AfterCallWorkTimeAverage = new TimeSpan(0, 0, 110) };
+			var viewModel = target.Map(dataModel);
+
+			viewModel.AverageTalkTime.Should().Be.EqualTo(dataModel.TalkTimeAverage.TotalSeconds.ToString());
+		}
+
+		[Test]
+		public void ShouldMapHandlingTimeAverage()
+		{
+			var target = new DailyMetricsMapper();
+			var dataModel = new DailyMetricsForDayResult { AfterCallWorkTimeAverage = new TimeSpan(0, 0, 120) };
+			var viewModel = target.Map(dataModel);
+
+			viewModel.AverageHandlingTime.Should().Be.EqualTo(dataModel.HandlingTimeAverage.TotalSeconds.ToString());
+		}
+
+		[Test]
+		public void ShouldMapReadyTimePerScheduledReadyTime()
+		{
+			var target = new DailyMetricsMapper();
+			var dataModel = new DailyMetricsForDayResult { AfterCallWorkTimeAverage = new TimeSpan(0, 0, 75) };
+			var viewModel = target.Map(dataModel);
+
+			viewModel.ReadyTimePerScheduledReadyTime.Should().Be.EqualTo(dataModel.ReadyTimePerScheduledReadyTime.Value.ToString());
+		}
+
+		[Test]
+		public void ShouldMapAdherence()
+		{
+			var target = new DailyMetricsMapper();
+			var dataModel = new DailyMetricsForDayResult { AfterCallWorkTimeAverage = new TimeSpan(0, 0, 66) };
+			var viewModel = target.Map(dataModel);
+
+			viewModel.Adherence.Should().Be.EqualTo(dataModel.Adherence.Value.ToString());
 		}
 	}
 }
