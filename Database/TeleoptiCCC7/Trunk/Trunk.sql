1205ae6f7562894be2aec14c3b3c26ab3ebde4a6
Roger Kratz <roger.kratz@teleopti.com>
2014-02-10 10:27:00 +0100

    Bug #26664

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -64,3 +64,12 @@ GO
 if not exists (select 1 from PurgeSetting where [Key] = 'MonthsToKeepRequests')
 	insert into PurgeSetting ([Key], [Value]) values('MonthsToKeepRequests', 120)
 GO
+
+
+--drop old persondayoff tables
+IF OBJECT_ID('[Auditing].[PersonDayOff_AUD]', 'U') IS NOT NULL
+	DROP TABLE [Auditing].[PersonDayOff_AUD]
+GO
+IF OBJECT_ID('[dbo].[PersonDayOff]', 'U') IS NOT NULL
+	DROP TABLE [dbo].[PersonDayOff]
+GO
