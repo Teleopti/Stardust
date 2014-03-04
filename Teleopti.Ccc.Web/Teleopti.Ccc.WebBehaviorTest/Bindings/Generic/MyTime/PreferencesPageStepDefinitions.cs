e6885f2ef368d663fb309194e36ca35e5ab18311
Xinfeng Li <xinfeng.li@teleopti.com>
2014-03-04 09:43:57 +0800

    Backed out changeset: 4090976f3cd7

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/MyTime/PreferencesPageStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/MyTime/PreferencesPageStepDefinitions.cs
@@ -293,14 +293,12 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.MyTime
 			OpenExtendedTooltip(fields.Date);
 
 			var tooltip = ExtendedTooltip(fields.Date);
-			Console.WriteLine(@"tooltip: {0}", tooltip);
 			Browser.Interactions.AssertExistsUsingJQuery(tooltip);
 
 			if (fields.EndTimeMaximum != null) Browser.Interactions.AssertFirstContainsUsingJQuery(tooltip, fields.EndTimeMaximum);
 
 			// sometimes the tooltip will hide, and will lose the tooltip content
 			var resultHtml = Browser.Interactions.Javascript("return $(\"" + tooltip + "\").html()") as string;
-			Console.WriteLine(@"resultHtml: {0}", resultHtml);
 
 			if (fields.Preference != null) Assert.IsTrue(resultHtml.Contains(fields.Preference));
 			if (fields.StartTimeMinimum != null) Assert.IsTrue(resultHtml.Contains(fields.StartTimeMinimum)); 
