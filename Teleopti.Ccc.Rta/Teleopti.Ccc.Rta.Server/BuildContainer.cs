dbf6663a6f40cecd180f643d4ad78319dac244d3
Erik Sundberg <none@none>
2014-03-06 15:11:12 +0100

    Added IoC tests to rta

---
+++ Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.Server/BuildContainer.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using MbCache.Configuration;
+using Teleopti.Ccc.IocCommon.Configuration;
+using Teleopti.Ccc.Rta.LogClientProxy.TeleoptiRtaService;
+
+namespace Teleopti.Ccc.Rta.Server
+{
+	public static class BuildContainer
+	{
+		 public static IContainer Build()
+		 {
+			 var builder = new ContainerBuilder();
+
+			 var mbCacheModule = new MbCacheModule(new InMemoryCache(20), null);
+			 builder.RegisterType<TeleoptiRtaService>().SingleInstance();
+			 builder.RegisterModule(mbCacheModule);
+			 builder.RegisterModule(new RealTimeContainerInstaller(mbCacheModule));
+			 return builder.Build();
+		 }
+	}
+}
