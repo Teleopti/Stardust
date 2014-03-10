f4265567ee3e0288a656c159e776a758a0e72212
Roger Kratz <roger.kratz@teleopti.com>
2014-03-10 13:55:59 +0100

    fixing build adding test for fetching siteid for personid

--- InfrastructureTest/Rta/PersonOrganizationReaderTest.cs
+++ InfrastructureTest/Rta/PersonOrganizationReaderTest.cs
@@ -17,7 +17,7 @@ namespace Teleopti.Ccc.InfrastructureTest.Rta
 	public class PersonOrganizationReaderTest : DatabaseTestWithoutTransaction
 	{
 		[Test]
-		public void ShouldFetchTeamPerson()
+		public void ShouldFetchTeamForPerson()
 		{
 			var team = TeamFactory.CreateTeam(" ", " ");
 			var person = new Person();
@@ -34,7 +34,7 @@ namespace Teleopti.Ccc.InfrastructureTest.Rta
 			var personId = person.Id.Value;
 
 			var target = new PersonOrganizationReader(new Now());
-			var resItem = target.LoadAll().Single();
+			var resItem = target.LoadAll().Single(x => x.PersonId == personId);
 			resItem.TeamId.Should().Be.EqualTo(teamId);
 			resItem.PersonId.Should().Be.EqualTo(personId);
 
@@ -44,6 +44,44 @@ namespace Teleopti.Ccc.InfrastructureTest.Rta
 				var rep = new Repository(uow);
 				rep.Remove(team);
 				rep.Remove(team.Site);
+				rep.Remove(person);
+				rep.Remove(person.PersonPeriodCollection.Single().PersonContract.ContractSchedule);
+				rep.Remove(person.PersonPeriodCollection.Single().PersonContract.Contract);
+				rep.Remove(person.PersonPeriodCollection.Single().PersonContract.PartTimePercentage);
+				uow.PersistAll();
+			}
+		}
+
+
+		[Test]
+		public void ShouldFetchSiteForPerson()
+		{
+			var team = TeamFactory.CreateTeam(" ", " ");
+			var site = team.Site;
+			var person = new Person();
+			person.AddPersonPeriod(createPersonPeriodAndPersistDependencies(team));
+			person.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Local);
+
+			PersistAndRemoveFromUnitOfWork(site);
+			PersistAndRemoveFromUnitOfWork(team);
+			PersistAndRemoveFromUnitOfWork(person);
+
+			UnitOfWork.PersistAll();
+
+			var siteId = site.Id.Value;
+			var personId = person.Id.Value;
+	
+			var target = new PersonOrganizationReader(new Now());
+			var resItem = target.LoadAll().Single(x => x.PersonId == personId);
+			resItem.SiteId.Should().Be.EqualTo(siteId);
+			resItem.PersonId.Should().Be.EqualTo(personId);
+
+			//cleanup -remove later
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			{
+				var rep = new Repository(uow);
+				rep.Remove(team);
+				rep.Remove(site);
 				rep.Remove(person);
 				rep.Remove(person.PersonPeriodCollection.Single().PersonContract.ContractSchedule);
 				rep.Remove(person.PersonPeriodCollection.Single().PersonContract.Contract);
