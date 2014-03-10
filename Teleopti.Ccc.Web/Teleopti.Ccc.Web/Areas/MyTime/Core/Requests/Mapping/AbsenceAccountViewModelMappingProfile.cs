c7a85749ef0595d7a160280b8a35640d1898d848
Xinfeng Li <xinfeng.li@teleopti.com>
2014-03-10 12:12:11 +0800

    Fix name of 2 properties for AbsenceAccountViewModel.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/AbsenceAccountViewModelMappingProfile.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/AbsenceAccountViewModelMappingProfile.cs
@@ -35,8 +35,8 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.Mapping
 						}
 						return trackerType;
 					}))
-				.ForMember(d => d.PeriodStartUtc, o => o.MapFrom(m => TimeZoneInfo.ConvertTimeFromUtc(m.StartDate, _userTimeZone.Invoke().TimeZone())))
-				.ForMember(d => d.PeriodEndUtc, o => o.MapFrom(m => TimeZoneInfo.ConvertTimeFromUtc(m.Period().EndDate, _userTimeZone.Invoke().TimeZone())))
+				.ForMember(d => d.PeriodStart, o => o.MapFrom(m => TimeZoneInfo.ConvertTimeFromUtc(m.StartDate, _userTimeZone.Invoke().TimeZone())))
+				.ForMember(d => d.PeriodEnd, o => o.MapFrom(m => TimeZoneInfo.ConvertTimeFromUtc(m.Period().EndDate, _userTimeZone.Invoke().TimeZone())))
 				.ForMember(d => d.Accrued, o => o.MapFrom(m => convertTimeSpanToString(m.Accrued, m.Owner.Absence.Tracker)))
 				.ForMember(d => d.Used, o => o.MapFrom(m =>  convertTimeSpanToString(m.LatestCalculatedBalance, m.Owner.Absence.Tracker)))
 				.ForMember(d => d.Remaining, o => o.MapFrom(m => convertTimeSpanToString(m.Remaining, m.Owner.Absence.Tracker)));
@@ -49,7 +49,7 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.Mapping
 			var classTypeOfTracker = tracker.GetType();
 			if (classTypeOfTracker == Tracker.CreateDayTracker().GetType())
 			{
-				result = ts.TotalDays.ToString(CultureInfo.InvariantCulture);
+				result = ts.TotalDays.ToString(CultureInfo.CurrentCulture);
 			}
 			else if (classTypeOfTracker == Tracker.CreateTimeTracker().GetType())
 			{
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/Requests/AbsenceAccountViewModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/Requests/AbsenceAccountViewModel.cs
@@ -9,8 +9,8 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Models.Requests
 	{
 		public string AbsenceName { get; set; }
 		public string TrackerType { get; set; }
-		public DateTime PeriodStartUtc { get; set; }
-		public DateTime PeriodEndUtc { get; set; }
+		public DateTime PeriodStart { get; set; }
+		public DateTime PeriodEnd { get; set; }
 		public string Accrued { get; set; }
 		public string Used { get; set; }
 		public string Remaining { get; set; }
