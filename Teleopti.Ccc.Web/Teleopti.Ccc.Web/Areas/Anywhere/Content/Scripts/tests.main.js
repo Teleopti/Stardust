6bf85c949ccf9773d41f4bcf3ae815dbce3aea2d
Team Green <none@none>
2014-03-13 14:51:16 +0100

    buster tests is now running again, controlled by returning functions..

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
@@ -1,10 +1,11 @@
 require.config(requireconfiguration);
 
 require([
-        'tests',
-			'views/realtimeadherence/vm_test',
-			'views/realtimeadherencesite/vm_test'
-		
-    ], function() {
-
-    });
+		'tests',
+		'views/realtimeadherence/vm_test',
+		'views/realtimeadherencesite/vm_test'
+], function () {
+	for (var i = 0, j = arguments.length; i < j; i++) {
+		arguments[i]();
+	}
+});
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherence/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherence/vm_test.js
@@ -1,18 +1,22 @@
 ﻿define(['buster', 'views/realtimeadherence/vm'], function (buster, viewModel) {
+	return function () {
 
-	buster.testCase("real time adherence view model", {
-		"should have no sites if none filled": function() {
-			var vm = viewModel();
-			assert.equals(vm.sites(), []);
-		},
-		
-		"should fill sites data": function () {
-			var site1 = { name : 'Londin', Id : 'guid1' };
-			var site2 = { name : 'PAris', Id : 'guid2' };
-			var vm = viewModel();
-			vm.fill([site1, site2]);
+		buster.testCase("real time adherence view model", {
+			"should have no sites if none filled": function () {
+				var vm = viewModel();
+				assert.equals(vm.sites(), []);
+			},
 
-			assert.equals(vm.sites(), [site1, site2]);
-		}
-	});
+			"should fill sites data": function () {
+				var site1 = { name: 'Londin', Id: 'guid1' };
+				var site2 = { name: 'PAris', Id: 'guid2' };
+				var vm = viewModel();
+				vm.fill([site1, site2]);
+
+				assert.equals(vm.sites(), [site1, site2]);
+			},
+
+		});
+
+	};
 });
