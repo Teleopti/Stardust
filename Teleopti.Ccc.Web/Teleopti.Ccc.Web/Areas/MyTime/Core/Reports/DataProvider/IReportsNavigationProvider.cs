57a99087cbc216b27f1189edabebfa6b63667fb5
Ola <none@none>
2014-03-17 03:00:26 +0100

    interfaces in their own file and remove unnecessary filter

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Reports/DataProvider/IReportsNavigationProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.Portal;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider
+{
+	public interface IReportsNavigationProvider
+	{
+		IList<ReportNavigationItem> GetNavigationItems();
+	}
+}
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Reports/DataProvider/ReportsProvider.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Reports/DataProvider/ReportsProvider.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
 using Teleopti.Ccc.Domain.Security.Matrix;
 using Teleopti.Ccc.Domain.Security.Principal;
-using Teleopti.Ccc.UserTexts;
-using Teleopti.Ccc.Web.Areas.MobileReports.Core;
-using Teleopti.Ccc.Web.Areas.MobileReports.Core.Providers;
-using Teleopti.Ccc.Web.Areas.MyTime.Models.Portal;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider
@@ -24,22 +18,10 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider
 
 		public IEnumerable<IApplicationFunction> GetReports()
 		{
-			var externalApplicationFunctionSpecification =
-				new ExternalApplicationFunctionSpecification(DefinedForeignSourceNames.SourceMatrix);
-			var mobileReportFunctionSpecification = DefinedReports.ReportInformations;
-
-			var allPermittedReports = _principalAuthorization.GrantedFunctionsBySpecification(externalApplicationFunctionSpecification).ToList();
-			var query =
-				from c in allPermittedReports
-				where !(from o in mobileReportFunctionSpecification select o.FunctionCode)
-					.Contains(c.FunctionCode)
-				select c;
-			return query;
+			return
+				_principalAuthorization.GrantedFunctionsBySpecification(
+					new ExternalApplicationFunctionSpecification(DefinedForeignSourceNames.SourceMatrix)).ToList();
 		}
 	}
 
-	public interface IReportsProvider
-	{
-		IEnumerable<IApplicationFunction> GetReports();
-	}
 }
