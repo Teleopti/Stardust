9f66c95c36dab7809aa7d6eb750ba937da63fe2d
Mingdi Wu <mingdi.wu@teleopti.com>
2014-03-17 12:21:08 +0800

    implement real business unit ID and matrix web site url

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Reports/DataProvider/ReportsNavigationProvider.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Reports/DataProvider/ReportsNavigationProvider.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Globalization;
 using System.Linq;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
 using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.UserTexts;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.Portal;
+using Teleopti.Ccc.Web.Core.RequestContext.Cookie;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider
 {
@@ -13,18 +15,20 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider
 	{
 		private readonly IPrincipalAuthorization _principalAuthorization;
 		private readonly IReportsProvider _reportsProvider;
+		private readonly ISessionSpecificDataProvider _sessionSpecificDataProvider;
 
-		public ReportsNavigationProvider(IPrincipalAuthorization principalAuthorization, IReportsProvider reportsProvider)
+		public ReportsNavigationProvider(IPrincipalAuthorization principalAuthorization, IReportsProvider reportsProvider, ISessionSpecificDataProvider sessionSpecificDataProvider)
 		{
 			_principalAuthorization = principalAuthorization;
 			_reportsProvider = reportsProvider;
+			_sessionSpecificDataProvider = sessionSpecificDataProvider;
 		}
 
 		public IList<ReportNavigationItem> GetNavigationItems()
 		{
-			var fakeMatrixWebsiteUrl = "http://www.teleopti.com?ReportId={0}&ForceForms={1}&BuId={2}";
-			var fakeBu = Guid.NewGuid();
-			var forceFormsLogin = false;
+			var data = _sessionSpecificDataProvider.GrabFromCookie();
+			var matrixWebsiteUrl = ConfigurationManager.AppSettings["MatrixWebSiteUrl"];
+			var realBu = data.BusinessUnitId;
 			var reportsList = new List<ReportNavigationItem>();
 			if (_principalAuthorization.IsPermitted(DefinedRaptorApplicationFunctionPaths.MyReportWeb))
 				reportsList.Add(new ReportNavigationItem
@@ -42,9 +46,8 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider
 				});
 			foreach (var applicationFunction in otherReports)
 			{
-				var url = string.Format(CultureInfo.CurrentCulture, fakeMatrixWebsiteUrl,
-													 applicationFunction.ForeignId,
-													 forceFormsLogin, fakeBu);
+				var url = string.Format(CultureInfo.CurrentCulture, "{0}?ReportId={1}&BuId={2}",
+					matrixWebsiteUrl, applicationFunction.ForeignId, realBu);
 
 				reportsList.Add(new ReportNavigationItem
 				{
