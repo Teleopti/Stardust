505c400d59f6f1de9ffc2e53e89554fa9d7524f7
Team Green <none@none>
2014-03-17 16:15:00 +0100

    should fill data in team view model.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherencesite/vm.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherencesite/vm.js
@@ -1,16 +1,29 @@
 ﻿define([
-		'knockout'
+		'knockout',
+		'lazy',
+		'views/realtimeadherencesite/team'
 ],
-	function (ko) {
+	function (ko,
+		lazy,
+		team) {
 	return function () {
 
 		var that = {};
 		that.teams = ko.observableArray();
 		that.fill = function(data) {
-			that.teams(data);
+			for (var i = 0; i < data.length; i++) {
+				var newTeam = team();
+				newTeam.Id = data[i].Id;
+				newTeam.Name = data[i].Name;
+				newTeam.NumberOfAgents = data[i].NumberOfAgents;
+				that.teams.push(newTeam);
+			}
 		};
-		
+
+
 		return that;
 	};
+
+
 }
 );
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherencesite/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherencesite/vm_test.js
@@ -2,15 +2,28 @@
 	return function () {
 		
 		buster.testCase("real time adherence site view model", {
+			"should have no teams if none filled": function () {
+				var vm = viewModel();
+				assert.equals(vm.teams(), []);
+			},
+
 			"should fill site's team data": function () {
-				var team1 = { name: 'Green', Id: 'guid1' };
-				var team2 = { name: 'Red', Id: 'guid2' };
+				var team1 = { Name: 'Green', Id: 'guid1', NumberOfAgents:5};
+				var team2 = { Name: 'Red', Id: 'guid2', NumberOfAgents:6};
 				var vm = viewModel();
 				vm.fill([team1, team2]);
 
-				assert.equals(vm.teams(), [team1, team2]);
-			}
+				assert.equals(vm.teams()[0].Id, team1.Id);
+				assert.equals(vm.teams()[0].Name, team1.Name);
+				assert.equals(vm.teams()[0].NumberOfAgents, team1.NumberOfAgents);
+				assert.equals(vm.teams()[1].Id, team2.Id);
+				assert.equals(vm.teams()[1].Name, team2.Name);
+				assert.equals(vm.teams()[1].NumberOfAgents, team2.NumberOfAgents);
+			},
+
 		});
+
+
 		
 	};
 });
