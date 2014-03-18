f99026b59c0b7aa3c4705557226776c5dee6d6ad
Jovi <Jiajun.Qiu@teleopti.com>
2014-03-18 18:13:28 +0100

    create a method shift Nudge earlier

---
+++ DomainTest/Optimization/WeeklyRestSolver/ShiftNudgeEarlier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization.WeeklyRestSolver
+{
+  public class ShiftNudgeEarlier
+    {
+        private readonly Domain.Scheduling.Assignment.IDeleteAndResourceCalculateService _deleteAndResourceCalculateService;
+      private readonly IScheduleService _scheduleService;
+      private readonly IEffectiveRestrictionCreator _effectiveRestrictionCreator;
+
+      public ShiftNudgeEarlier(Domain.Scheduling.Assignment.IDeleteAndResourceCalculateService deleteAndResourceCalculateService, IScheduleService scheduleService, IEffectiveRestrictionCreator effectiveRestrictionCreator)
+        {
+            _deleteAndResourceCalculateService = deleteAndResourceCalculateService;
+            _scheduleService = scheduleService;
+          _effectiveRestrictionCreator = effectiveRestrictionCreator;
+        }
+
+      public bool Nudge(Interfaces.Domain.IScheduleDay scheduleDay, ISchedulePartModifyAndRollbackService rollbackService, ISchedulingOptions schedulingOptions)
+        {
+            var shiftEnd =
+                scheduleDay.PersonAssignment().ProjectionService().CreateProjection().Period().Value.EndDateTime;
+            var scheduleDayList = new List<IScheduleDay> {scheduleDay};
+            _deleteAndResourceCalculateService.DeleteWithResourceCalculation(scheduleDayList, rollbackService, true);
+          var effectiveRestriction = _effectiveRestrictionCreator.GetEffectiveRestriction(scheduleDay, schedulingOptions);
+          effectiveRestriction.EndTimeLimitation.EndTime
+            _scheduleService.SchedulePersonOnDay(scheduleDay,schedulingOptions)
+
+
+            return false;
+        }
+
+    }
+}
---
+++ DomainTest/Optimization/WeeklyRestSolver/ShiftNudgeEarlierTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization.WeeklyRestSolver
+{
+    [TestFixture]
+    public class ShiftNudgeEarlierTest
+    {
+        private ShiftNudgeEarlier _target;
+        private MockRepository _mock;
+        private IScheduleDay _scheduleDay;
+        private IDeleteAndResourceCalculateService _deleteAndResourceCalculateService;
+
+        [SetUp]
+        public void setUp()
+        {
+            
+            _mock = new MockRepository();
+            _deleteAndResourceCalculateService = _mock.StrictMock<IDeleteAndResourceCalculateService>();
+            _target = new ShiftNudgeEarlier(_deleteAndResourceCalculateService);
+        }
+
+        [Test]
+        public void ShouldNudge()
+        {
+            bool result = _target.Nudge(_scheduleDay);
+            Assert.IsTrue(result);
+        }
+
+    }
+}
