398f57efa33d1c7a812e4e891b550610316cd915
Team Green <none@none>
2014-03-19 09:44:16 +0100

    minor

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/navigation.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/navigation.js
@@ -1,42 +1,43 @@
 
 define([
         'moment'
-    ], function(
+], function (
         moment
     ) {
 
-        var toDateString = function(date) {
-            if (moment.isMoment(date))
-                date = date.format("YYYYMMDD");
-            return date;
-        };
+    var toDateString = function (date) {
+        if (moment.isMoment(date))
+            date = date.format("YYYYMMDD");
+        return date;
+    };
 
-        return {
-        	GotoPersonSchedule: function (groupid, personid, date) {
-        		window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date);
-            },
-        	GotoPersonScheduleWithoutHistory: function (groupid, personid, date) {
-            	window.location.replace('#personschedule/' + groupid + '/' + personid + '/' + toDateString(date));
-            },
-            
-            GotoPersonScheduleAddFullDayAbsenceForm: function (groupid, personid, date) {
-            	window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/addfulldayabsence';
-            },
+    return {
+        GotoPersonSchedule: function (groupid, personid, date) {
+            window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date);
+        },
 
-            GotoPersonScheduleAddActivityForm: function (groupid, personid, date) {
-            	window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/addactivity';
-            },
-            
-            GotoPersonScheduleAddIntradayAbsenceForm: function (groupid, personid, date) {
-            	window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/addintradayabsence';
-            },
-        	
-            GotoPersonScheduleWithAction: function (groupid, personid, date, action) {
-            	window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/' + action;
-            },
-            
-            GoToTeamSchedule: function (id, date, skill) {
-            	window.location.hash = 'teamschedule/' + id + '/' + toDateString(date) + ((skill) ? '/' + skill.Id : '');
-            }
-        };
-    });
+        GotoPersonScheduleWithoutHistory: function (groupid, personid, date) {
+            window.location.replace('#personschedule/' + groupid + '/' + personid + '/' + toDateString(date));
+        },
+
+        GotoPersonScheduleAddFullDayAbsenceForm: function (groupid, personid, date) {
+            window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/addfulldayabsence';
+        },
+
+        GotoPersonScheduleAddActivityForm: function (groupid, personid, date) {
+            window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/addactivity';
+        },
+
+        GotoPersonScheduleAddIntradayAbsenceForm: function (groupid, personid, date) {
+            window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/addintradayabsence';
+        },
+
+        GotoPersonScheduleWithAction: function (groupid, personid, date, action) {
+            window.location.hash = 'personschedule/' + groupid + '/' + personid + '/' + toDateString(date) + '/' + action;
+        },
+
+        GoToTeamSchedule: function (id, date, skill) {
+            window.location.hash = 'teamschedule/' + id + '/' + toDateString(date) + ((skill) ? '/' + skill.Id : '');
+        }
+    };
+});
