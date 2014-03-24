1bc5653bf6682d25da7c6f41171726c721ffad4f
Asad MIrza <asad.mirza@teleopti.com>
2014-03-24 17:06:08 +0100

    Refactored weekly rest service

---
+++ DomainTest/Optimization/WeeklyRestSolver/DeleteScheduleDayFromUnsolvedPersonWeekTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teleopti.Ccc.DomainTest.Optimization.WeeklyRestSolver
+{
+    public class DeleteScheduleDayFromUnsolvedPersonWeekTest
+    {
+    }
+}
