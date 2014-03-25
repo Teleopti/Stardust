3a37da257ef0d29dc2c0fa4aab2236cf111f59c7
Henke <henrik.andersson@teleopti.com>
2014-03-25 12:56:03 +0100

    Added diagnosis in anywhere

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/diagnosis/view.js
@@ -0,0 +1,28 @@
+﻿define([
+		'knockout',
+		'text!templates/diagnosis/view.html',
+		'views/diagnosis/vm'
+], function (
+		ko,
+		view,
+		diagnosisViewModel
+	) {
+
+	var viewModel;
+
+	return {
+		initialize: function (options) {
+
+			options.renderHtml(view);
+		},
+
+		display: function (options) {
+
+			viewModel = diagnosisViewModel();
+
+			ko.applyBindings(viewModel, options.bindingElement);
+
+		},
+	};
+});
+
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/diagnosis/vm.js
@@ -0,0 +1,20 @@
+﻿define([
+		'knockout',
+		'resources'
+],
+	function (
+		ko,
+		resources
+	) {
+		return function () {
+
+			var that = {};
+
+			that.resources = resources;
+
+			that.description = ko.observable('xxDiagnosis');
+
+			return that;
+		};
+	}
+);
