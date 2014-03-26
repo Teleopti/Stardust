1750c0e4e46fcad7e4814a7c40504e3186e934a4
Roger Kratz <roger.kratz@teleopti.com>
2014-03-26 15:36:41 +0100

    adding message repeater
    --HG--
    branch : Release 7.5.390

---
+++ Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.Server/Repeater/IMessageRepeaterTrigger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Teleopti.Ccc.Rta.Server.Repeater
+{
+	public interface IMessageRepeaterTrigger
+	{
+		void SetTriggerMethod(Action flush);
+	}
+}
---
+++ Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.Server/Repeater/MessageRepeater.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.MessageBroker.Client;
+
+namespace Teleopti.Ccc.Rta.Server.Repeater
+{
+	public class MessageRepeater
+	{
+		private readonly IMessageSender _messageSender;
+		private readonly ICreateNotification _createNotification;
+		private readonly IDictionary<Guid, IActualAgentState> _actualAgentStates;
+
+		public MessageRepeater(IMessageSender messageSender, 
+			IMessageRepeaterTrigger messageRepeaterTrigger,
+			ICreateNotification createNotification)
+		{
+			_actualAgentStates = new ConcurrentDictionary<Guid, IActualAgentState>();
+			_messageSender = messageSender;
+			_createNotification = createNotification;
+			
+			if (messageRepeaterTrigger != null)
+			{
+				messageRepeaterTrigger.SetTriggerMethod(flush);
+			}
+		}
+
+		public void Invoke(IActualAgentState message)
+		{
+			_actualAgentStates[message.PersonId] = message;
+		}
+
+		private void flush()
+		{
+			foreach (var actualAgentState in _actualAgentStates)
+			{
+				_messageSender.SendNotification(_createNotification.FromActualAgentState(actualAgentState.Value));
+			}
+			_actualAgentStates.Clear();
+		}
+	}
+}
