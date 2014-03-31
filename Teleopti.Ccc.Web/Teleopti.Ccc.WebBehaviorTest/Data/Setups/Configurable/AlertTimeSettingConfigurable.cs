bae4e020a97407d68c9dbfc09baf2ba223e54859
Chundan Xu <chundan.xu@teleopti.com>
2014-03-31 16:31:55 +0800

    finish step definition for setting.

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Configurable/AlertTimeSettingConfigurable.cs
@@ -0,0 +1,25 @@
+﻿using Teleopti.Ccc.Domain.SystemSetting.GlobalSetting;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Configurable
+{
+	public class AlertTimeSettingConfigurable : IDataSetup
+	{
+		public int AlertTime { get; set; }
+		public AlertTimeSettingConfigurable(int alertTime)
+		{
+			AlertTime = alertTime;
+		}
+
+		public void Apply(IUnitOfWork uow)
+		{
+			var globalSettingRep = new GlobalSettingDataRepository(uow);
+
+			var alertTimeSetting = globalSettingRep.FindValueByKey("AsmAlertTime", new AsmAlertTime());
+			alertTimeSetting.SecondsBeforeChange = AlertTime;
+			new GlobalSettingDataRepository(uow).PersistSettingValue(alertTimeSetting).GetValue(new AsmAlertTime());
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/MyTime/AsmActivityNotificationStepDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Xml;
+using TechTalk.SpecFlow;
+using Teleopti.Ccc.Domain.SystemSetting.GlobalSetting;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Ccc.UserTexts;
+using Teleopti.Ccc.WebBehaviorTest.Core.BrowserDriver;
+using Teleopti.Ccc.WebBehaviorTest.Data;
+using Teleopti.Ccc.WebBehaviorTest.Data.Setups.Configurable;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
+using Browser = Teleopti.Ccc.WebBehaviorTest.Core.Browser;
+
+namespace Teleopti.Ccc.WebBehaviorTest.MyTime
+{
+	[Binding]
+	public class AsmActivityNotificationStepDefinition
+	{
+		[Given(@"Alert Time setting is '(.*)' seconds")]
+		public void GivenAlertTimeSettingIs(int alertTime)
+		{
+			DataMaker.Data().Apply(new AlertTimeSettingConfigurable(alertTime));
+		}
+
+
+	}
+}
