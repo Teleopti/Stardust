269e95588d3741d87106683236892126a59e9287
Henke <henrik.andersson@teleopti.com>
2014-04-01 10:24:28 +0200

    removed old test

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
@@ -11,7 +11,6 @@ require([
 		'tests',
 		'views/realtimeadherencesites/vm_test',
 		'views/realtimeadherenceteams/vm_test',
-		'views/diagnosis/vm_test'
 ], function () {
 	for (var i = 0, j = arguments.length; i < j; i++) {
 		arguments[i]();
