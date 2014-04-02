ec308389d7e14c3efceeaba91829edfc1ecb5cd5
Roger Kratz <roger.kratz@teleopti.com>
2014-04-02 10:41:55 +0200

    can now pass in argument to msbuild from BuildOnLocalPC
    --HG--
    branch : Release 7.5.390

--- ccnet/BuildOnLocalPC.bat
+++ ccnet/BuildOnLocalPC.bat
@@ -1,9 +1,14 @@
 @ECHO off
+
+REM To run a specific target
+REM BuildOnLocalPC.bat /t:Compile
+
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-7%
 SET MsbuildProjRaptor2=ccnet\Raptor2.proj
 SET CCNetWorkingDirectory=%ROOTDIR%
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+
 
 ::register ncover licens locally ^(we only have two lics on Teleopti...^)
 CD "%CCNetWorkingDirectory%\ccnet\NCover64"
@@ -18,8 +23,8 @@ ECHO %CCNetWorkingDirectory%
 SET CCNetProject=RaptorMain
 
 ::Run Build
-ECHO "%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProjRaptor2%"
-"%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProjRaptor2%"
+ECHO "%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProjRaptor2%" %1
+"%MSBUILD%" /nologo /p:Configuration=Debug "%CCNetWorkingDirectory%\%MsbuildProjRaptor2%" %1
 
 ECHO.
 ECHO reverting updated checked in config files ...
@@ -31,6 +36,4 @@ hg revert -C "%ROOTDIR%\Teleopti.Ccc.Web\Teleopti.Ccc.WebBehaviorTest\App.config
 hg revert -C "%ROOTDIR%\Teleopti.Ccc.ApplicationConfigTest\Teleopti.Ccc.ApplicationConfigTest.dll.Config"
 
 ECHO Done!
-PAUSE
-PAUSE
 PAUSE
