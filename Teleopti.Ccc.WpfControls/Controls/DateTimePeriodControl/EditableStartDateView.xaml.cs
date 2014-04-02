06ccea63d843589877199c1069fb30ba22563ebe
Henke <henrik.andersson@teleopti.com>
2014-04-02 10:38:28 +0200

    Removed wpf toolkit from wpfcontrols
    --HG--
    branch : Release 7.5.390

--- Teleopti.Ccc.WpfControls/Controls/DateTimePeriodControl/EditableStartDateView.xaml.cs
+++ Teleopti.Ccc.WpfControls/Controls/DateTimePeriodControl/EditableStartDateView.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Controls;
-using Microsoft.Windows.Controls;
 using Teleopti.Ccc.WinCode.Scheduling;  
 
 namespace Teleopti.Ccc.WpfControls.Controls.DateTimePeriodControl
