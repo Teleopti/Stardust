dbfaa7e62cd15347097fad7432042b8f84f13858
Team Green <none@none>
2014-04-15 13:34:42 +0200

    refact

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/MyTime/WeekScheduleOnMobileStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/MyTime/WeekScheduleOnMobileStepDefinitions.cs
@@ -11,21 +11,17 @@ using Teleopti.Ccc.WebBehaviorTest.Data;
 using Teleopti.Ccc.WebBehaviorTest.Pages;
 using Teleopti.Interfaces.Domain;
 using Browser = Teleopti.Ccc.WebBehaviorTest.Core.Browser;
-using Table = TechTalk.SpecFlow.Table;
 
 namespace Teleopti.Ccc.WebBehaviorTest.MyTime
 {
     [Binding]
-    public class MobileWeekScheduleStepDefinitions
+    public class WeekScheduleOnMobileStepDefinitions
     {
-       
-
         [Then(@"I should see mobile view of the week with for date '(.*)'")]
         public void ThenIShouldSeeMobileViewOfTheWeekWithForDate(DateTime date)
         {
             AssertShowingWeekForDay(DateHelper.GetFirstDateInWeek(date.Date, DataMaker.Data().MyCulture));
         }
-
 
         private void AssertShowingWeekForDay(DateTime anyDayOfWeek)
         {
