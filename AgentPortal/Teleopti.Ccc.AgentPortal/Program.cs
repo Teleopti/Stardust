16ebeb0e0d78b793e5db7bd5110a2dc8376325d2
Robin Karlsson <robin.karlsson@teleopti.com>
2014-04-17 08:30:06 +0200

    Removed old single signon in MyTime (windows application) never used

--- AgentPortal/Teleopti.Ccc.AgentPortal/Program.cs
+++ AgentPortal/Teleopti.Ccc.AgentPortal/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 using Teleopti.Ccc.AgentPortal.Helper;
 using Teleopti.Ccc.AgentPortal.Main;
-using Teleopti.Ccc.Sdk.Common.DataTransferObject;
 
 namespace Teleopti.Ccc.AgentPortal
 {
@@ -19,77 +17,12 @@ namespace Teleopti.Ccc.AgentPortal
             Application.SetCompatibleTextRenderingDefault(false);
 
             var settings = AppConfigHelper.LoadSettings();
-            var logOnDetails = new LogOnDetails();
-            try
-            {
-                var token = string.Empty;
-                if (args.Length == 1)
-                {
-                    token = args[0];
-                }
-            	var activationArguments = AppDomain.CurrentDomain.SetupInformation.ActivationArguments;
-                if (string.IsNullOrEmpty(token) &&
-					activationArguments !=null &&
-                    activationArguments.ActivationData != null &&
-                    activationArguments.ActivationData.Length > 0)
-                {
-					if (activationArguments.ActivationData[0].IndexOf('?')!=-1)
-						token = activationArguments.ActivationData[0];
-                }
-            	
-                if (!string.IsNullOrEmpty(token))
-                {
-                    if (token.IndexOf('?') != -1)
-                        token = token.Substring(token.IndexOf('?') + 1);
-                    var clearToken = Encryption.DecryptStringFromBytes(Convert.FromBase64String(token),
-                                                                       Convert.FromBase64String(settings["SharedKey"]),
-                                                                       Convert.FromBase64String(settings["SharedIV"]));
-                    var clearTokenProperties = clearToken.Split(',');
-                    var tokenDictionary = new Dictionary<string, string>();
-                    foreach (var clearTokenProperty in clearTokenProperties)
-                    {
-                        tokenDictionary.Add(clearTokenProperty.Substring(0, clearTokenProperty.IndexOf('=')),
-                                            clearTokenProperty.Substring(clearTokenProperty.IndexOf('=') + 1));
-                    }
-
-                    logOnDetails.UserName = tokenDictionary["usr"];
-                    logOnDetails.Password = tokenDictionary["pwd"];
-                    logOnDetails.DataSource = new DataSourceDto
-                                                  {
-                                                      Name = tokenDictionary["datasource"],
-                                                      AuthenticationTypeOptionDto =
-                                                          AuthenticationTypeOptionDto.Application
-                                                  };
-
-                    var timestamp = tokenDictionary["time"];
-                    double time;
-                    if (!double.TryParse(timestamp, out time)) return;
-
-                    var tokenTimestamp = TimeSpan.FromMilliseconds(time);
-                    if (
-                        DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).Subtract(
-                            tokenTimestamp).TotalSeconds > 30)
-                    {
-                        logOnDetails = new LogOnDetails();
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                MessageBoxHelper.ShowErrorMessage(e.Message, string.Concat(UserTexts.Resources.LogOn, " - ", UserTexts.Resources.IllegalInput));
-            }
-            var logOnScreen = new LogOnScreen(logOnDetails,settings);
+            
+            var logOnScreen = new LogOnScreen(settings);
             if (logOnScreen.ShowDialog() == DialogResult.OK)
             {
                 Application.Run(new MainScreen());
             }
         }
-    }
-
-    public class LogOnDetails
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public DataSourceDto DataSource { get; set; }
     }
 }
