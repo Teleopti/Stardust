8649d19c9254ce49ea8a87aa6948a47eeab8ef9d
Xinfeng Li <xineng.li@teleopti.com>
2014-04-18 22:10:17 +0800

    Refactor step implementation

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/NotifyStepDefinition.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/NotifyStepDefinition.cs
@@ -6,26 +6,26 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic
 	[Binding]
 	public class NotifyStepDefinition
 	{
+		const string notifyTextSelector = "#noty_bottom_layout_container .noty_bar .noty_message .noty_text";
 		[Then(@"I should see one notify message")]
 		public void ThenIShouldSeeAnAlert()
 		{
-			Browser.Interactions.AssertExists("#noty_bottom_layout_container .noty_bar .noty_message .noty_text");
+			Browser.Interactions.AssertExists(notifyTextSelector);
 		}
 
 		[Then(@"I should see a notify message contains text (.*)")]
 		public void ThenIShouldSeeANotifyMessageContainsText(string content)
 		{
-			Browser.Interactions.AssertAnyContains("#noty_bottom_layout_container .noty_bar .noty_message .noty_text", content);
+			Browser.Interactions.AssertAnyContains(notifyTextSelector, content);
 		}
 
 		[Then(@"I should see a notify message contains all text")]
 		public void ThenIShouldSeeANotifyMessageContainsAllText(Table contentTable)
 		{
-			const string selector = "#noty_bottom_layout_container .noty_bar .noty_message .noty_text";
-			for(var i =0;i<contentTable.RowCount; i++)
+			for (var i = 0; i < contentTable.RowCount; i++)
 			{
 				var content = contentTable.Rows[i]["Content"].Trim();
-				Browser.Interactions.AssertAnyContains(selector, content);
+				Browser.Interactions.AssertAnyContains(notifyTextSelector, content);
 			}
 		}
 
