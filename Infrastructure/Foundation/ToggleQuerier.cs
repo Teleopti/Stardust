9830fdfe3949896841098da91c409f6aac2e1d1d
Roger Kratz <roger.kratz@gmail.com>
2014-04-24 14:59:33 +0200

    adding ToggleQuerier

---
+++ Infrastructure/Foundation/ToggleQuerier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace Teleopti.Ccc.Infrastructure.Foundation
+{
+	//integration tests from Toggle.feature scenario
+	public class ToggleQuerier 
+	{
+		private readonly string _url;
+
+		public ToggleQuerier(string url)
+		{
+			_url = url;
+		}
+
+		public bool IsEnabled(string flag)
+		{
+			var uriBuilder = new UriBuilder(_url);
+			var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+			query["toggle"] = flag;
+			uriBuilder.Query = query.ToString();
+			var request = WebRequest.Create(uriBuilder.ToString());
+			using (var reader = new StreamReader(request.GetResponse().GetResponseStream()))
+			{
+				return Convert.ToBoolean(reader.ReadToEnd());
+			}
+		}
+	}
+}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Toggle/Toggle.feature
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Toggle/Toggle.feature
@@ -3,6 +3,10 @@
 	As a manager
 	I want to be able to choose what features should reach customers
 
-Scenario: Enabled feature
-	When I query toggle service for 'EnabledFeature'
+Scenario: Enabled feature in process
+	When I query inprocess toggle service for 'EnabledFeature'
+	Then I should get 'true' back
+
+Scenario: Enabled feature out of process
+	When I query outofprocess toggle service for 'EnabledFeature'
 	Then I should get 'true' back
