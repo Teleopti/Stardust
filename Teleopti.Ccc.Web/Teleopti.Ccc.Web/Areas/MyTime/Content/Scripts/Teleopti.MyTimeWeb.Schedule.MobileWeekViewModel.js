040035e07b8a867f60a813f9f2b4258a3d69f922
Chundan Xu <chundan.xu@teleopti.com>
2014-04-24 15:48:29 +0800

    fix isDayoff and hasShift

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Schedule.MobileWeekViewModel.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Schedule.MobileWeekViewModel.js
@@ -30,26 +30,25 @@ Teleopti.MyTimeWeb.Schedule.MobileWeekViewModel = function() {
 
 	};
 };
-Teleopti.MyTimeWeb.Schedule.MobileDayViewModel = function (scheduleDay) {
-    var self = this;
-    self.summaryName = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.Title : null);
-    self.summaryTimeSpan = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.TimeSpan : null);
-    self.summaryColor = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.Color : null);
+Teleopti.MyTimeWeb.Schedule.MobileDayViewModel = function(scheduleDay) {
+	var self = this;
+	self.summaryName = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.Title : null);
+	self.summaryTimeSpan = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.TimeSpan : null);
+	self.summaryColor = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.Color : null);
 	self.fixedDate = ko.observable(scheduleDay.FixedDate);
 	self.weekDayHeaderTitle = ko.observable(scheduleDay.Header ? scheduleDay.Header.Title : null);
 	self.summaryStyleClassName = ko.observable(scheduleDay.Summary ? scheduleDay.Summary.StyleClassName : null);
-	self.isDayoff = function() {
-		if(self.summaryStyleClassName() != undefined && self.summaryStyleClassName() != null)
-		{
+	self.isDayoff = ko.computed(function() {
+		if (self.summaryStyleClassName() != undefined && self.summaryStyleClassName() != null) {
 			return self.summaryStyleClassName() == "dayoff striped";
 		}
 		return false;
-	};
-	self.hasShift = function() {
+	});
+	self.hasShift = ko.computed(function() {
 		if (scheduleDay.Periods != null && scheduleDay.Periods.length > 0)
 			return true;
 		return false;
-	};
+	});
 
     self.backgroundColor = scheduleDay.Summary ? scheduleDay.Summary.Color : null;
     self.summaryTextColor = ko.observable(self.backgroundColor ? Teleopti.MyTimeWeb.Common.GetTextColorBasedOnBackgroundColor(self.backgroundColor) : 'black');
