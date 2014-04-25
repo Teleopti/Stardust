a9b4226a246fb0a5a5e26bcfd0c3fd16a4e6c448
Johan Ryding <none@none>
2014-04-25 11:33:56 +0200

    Removed create new branch from .bat file

--- Database/Tools/CreateReleaseBranch.bat
+++ Database/Tools/CreateReleaseBranch.bat
@@ -8,20 +8,26 @@ SET ROOTDIR=%THISDIR:~0,-1%
 SET ROOTDIR=%ROOTDIR%\..\..
 
 ::Confirm
-ECHO This will create release db scripts and
-ECHO create a new named branch on server repository.
-ECHO For safety reasons - you need to push manually. Named branches cannot easily be deleted.
-ECHO When you push - you need the --new-branch flag to be set.
-ECHO If you're using TortoiseHg, remember to remove the "allow --new-branch" thing after you've pushed!
+REM ECHO This will create release db scripts and
+REM ECHO create a new named branch on server repository.
+REM ECHO For safety reasons - you need to push manually. Named branches cannot easily be deleted.
+REM ECHO When you push - you need the --new-branch flag to be set.
+REM ECHO If you're using TortoiseHg, remember to remove the "allow --new-branch" thing after you've pushed!
+REM CHOICE /M "Are you sure you want to continue"
+REM IF ERRORLEVEL 2 exit
+REM ECHO.
+
+ECHO This will generate a new database build! Re-init trunks!
 CHOICE /M "Are you sure you want to continue"
 IF ERRORLEVEL 2 exit
 ECHO.
 
+
 :: Call VersionBuild.bat
 call "%THISDIR%\versionbuild.bat"
 
-set releaseBranchName="Release %SYSTEMVERSION%"
-echo using releasebranchname %releaseBranchName%
+REM set releaseBranchName="Release %SYSTEMVERSION%"
+REM echo using releasebranchname %releaseBranchName%
 
 if %myError% NEQ 0 (
 echo something wrong in versionbuild.bat. Ending...
@@ -29,11 +35,11 @@ exit
 )
 
 :: create new release branch
-echo Creating branch %releaseBranchName%
-hg pull
-hg update default
-hg branch %releaseBranchName%
-hg ci -m "Creating release %SYSTEMVERSION%"
+REM echo Creating branch %releaseBranchName%
+REM hg pull
+REM hg update default
+REM hg branch %releaseBranchName%
+REM hg ci -m "Creating release %SYSTEMVERSION%"
 
 :: Bump version on default
 hg update default
