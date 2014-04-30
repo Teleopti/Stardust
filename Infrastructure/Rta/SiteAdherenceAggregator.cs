d2fc64faf31cb05a758b507a636b60051472eab8
Xianwei Shen <xianwei.shen@teleopti.com>
2014-04-30 10:57:11 +0200

    add site adherence aggregator

---
+++ Infrastructure/Rta/SiteAdherenceAggregator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Teleopti.Ccc.Domain.Repositories;
+
+namespace Teleopti.Ccc.Infrastructure.Rta
+{
+	public interface ISiteAdherenceAggregator
+	{
+	}
+	
+	public class SiteAdherenceAggregator : ISiteAdherenceAggregator
+	{
+		private readonly IStatisticRepository _statisticRepository;
+		private readonly IPersonOrganizationReader _personOrganization;
+
+		public SiteAdherenceAggregator(IStatisticRepository statisticRepository, IPersonOrganizationReader personOrganization)
+		{
+			_statisticRepository = statisticRepository;
+			_personOrganization = personOrganization;
+		}
+
+		public int Aggregate(Guid siteId)
+		{
+			var personIds = _personOrganization.LoadAll().Where(x => x.SiteId == siteId).Select(x => x.PersonId).ToList();
+			var lastStates = _statisticRepository.LoadLastAgentState(personIds);
+			return lastStates.Count(x => x.StaffingEffect > 0);
+		}
+	}
+}
---
+++ InfrastructureTest/Rta/SiteAdherenceAggregatorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Infrastructure.Rta;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.Rta
+{
+	public class SiteAdherenceAggregatorTest
+	{
+		[Test]
+		public void ShouldAggregateOutOfAdherenceOnPositiveStaffingEffectForOneSite()
+		{
+			var siteId = Guid.NewGuid();
+			var personId1 = Guid.NewGuid();
+			var personId2 = Guid.NewGuid();
+			var personId3 = Guid.NewGuid();
+			var inAdherence1 = new ActualAgentState { StaffingEffect = 0 };
+			var inAdherence2 = new ActualAgentState { StaffingEffect = 0 };
+			var outOfAdherence = new ActualAgentState { StaffingEffect = 1 };
+
+			var statisticRepository = MockRepository.GenerateMock<IStatisticRepository>();
+			statisticRepository.Stub(x => x.LoadLastAgentState(new[] { personId1, personId2, personId3 }))
+				.Return(new List<IActualAgentState> { inAdherence1, inAdherence2, outOfAdherence });
+
+			var personOrganization = MockRepository.GenerateMock<IPersonOrganizationReader>();
+			personOrganization.Stub(x => x.LoadAll()).Return(new[]
+			{
+				new PersonOrganizationData {PersonId = personId1, SiteId = siteId},
+				new PersonOrganizationData {PersonId = personId2, SiteId = siteId},
+				new PersonOrganizationData {PersonId = personId3, SiteId = siteId},
+			});
+
+			var target = new SiteAdherenceAggregator(statisticRepository, personOrganization);
+
+			var result = target.Aggregate(siteId);
+
+			result.Should().Be(1);
+		}
+	}
+}
