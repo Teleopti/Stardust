eec4bb2e98405f5de94f0c66198672f076489d3d
Xianwei Shen <xianwei.shen@teleopti.com>
2014-05-05 14:16:25 +0200

    add team adherence aggregator

---
+++ Infrastructure/Rta/TeamAdherenceAggregator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.Common.Time;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Infrastructure.Rta
+{
+	public class TeamAdherenceAggregator
+	{
+		private readonly IStatisticRepository _statisticRepository;
+		private readonly ITeamRepository _teamRepository;
+		private readonly IPersonRepository _personRepository;
+		private readonly INow _now;
+
+		public TeamAdherenceAggregator(IStatisticRepository statisticRepository, ITeamRepository teamRepository, IPersonRepository personRepository, INow now)
+		{
+			_statisticRepository = statisticRepository;
+			_teamRepository = teamRepository;
+			_personRepository = personRepository;
+			_now = now;
+		}
+
+		public int Aggregate(Guid teamId)
+		{
+			var team = _teamRepository.Get(teamId);
+			var personIds = new List<Guid>();
+			var today = _now.LocalDateOnly();
+			var timePeriod = new DateOnlyPeriod(today, today);
+			personIds.AddRange(_personRepository.FindPeopleBelongTeam(team, timePeriod).Select(x => x.Id.GetValueOrDefault()));
+			var lastStates = _statisticRepository.LoadLastAgentState(personIds);
+			return lastStates.Count(x => !x.StaffingEffect.Equals(0));
+		}
+	}
+}
