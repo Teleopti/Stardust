6813d5e58d534c6d80ae0eb5dda0f75182ccd874
Roger Kratz <roger.kratz@gmail.com>
2014-05-06 17:00:50 +0200

    supports enable/disable toggles by user.
    syntax
    
    myfeature = user
    myfeature.user.ids=guid1, guid2

---
+++ InfrastructureTest/FeatureFlags/ToggleUserProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Infrastructure.FeatureFlags;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.FeatureFlags
+{
+	public class ToggleUserProviderTest
+	{
+		[Test]
+		public void ShouldGetCurrentUserIdAsString()
+		{
+			var currentUser = new Person();
+			currentUser.SetId(Guid.NewGuid());
+			var loggedOnUser = MockRepository.GenerateStub<ILoggedOnUser>();
+			loggedOnUser.Expect(x => x.CurrentUser()).Return(currentUser);
+
+			var target = new ToggleUserProvider(loggedOnUser);
+			target.CurrentUser()
+				.Should().Be.EqualTo(currentUser.Id.Value.ToString());
+		}
+	}
+}
--- Teleopti.Ccc.IocCommon/Configuration/ToggleNetModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/ToggleNetModule.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Autofac;
 using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.Security.Authentication;
+using Teleopti.Ccc.Infrastructure.FeatureFlags;
 using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Interfaces.Domain;
 using Toggle.Net;
 using Toggle.Net.Configuration;
-using Toggle.Net.Internal;
 using Toggle.Net.Providers.TextFile;
 
 namespace Teleopti.Ccc.IocCommon.Configuration
@@ -36,11 +39,16 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 				}
 				else
 				{
-					var toggleConfiguration =
-						new ToggleConfiguration(new FileProviderFactory(new FileReader(_pathToToggleFile),
-							new DefaultSpecificationMappings()));
-					var toggleChecker = toggleConfiguration.Create();
-					builder.Register(_ => new toggleCheckerWrapper(toggleChecker))
+					builder.Register(c =>
+					{
+						var toggleConfiguration =
+							new ToggleConfiguration(new FileProviderFactory(new FileReader(_pathToToggleFile),
+								new DefaultSpecificationMappings()));
+						toggleConfiguration.SetUserProvider(new ToggleUserProvider(c.Resolve<ILoggedOnUser>()));
+						var toggleChecker = toggleConfiguration.Create();
+						return new toggleCheckerWrapper(toggleChecker);
+					}
+						)
 						.SingleInstance()
 						.As<IToggleManager>();
 				}
