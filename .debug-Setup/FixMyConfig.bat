998a606b658c4849d295fe0706ddc546c9537267
David Jonsson <david.jonsson@teleopti.com>
2014-05-07 11:32:57 +0200

    #26969 #27753 - Make local debug work after test run

--- .debug-Setup/FixMyConfig.bat
+++ .debug-Setup/FixMyConfig.bat
@@ -7,7 +7,10 @@ SET CCC7DB=%1
 SET AnalyticsDB=%2
 set MSBUILD="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 SET MySettings=%ROOTDIR%\..\Teleopti.Support.Code\settings.txt
- 
+
+SET validationKey=754534E815EF6164CE788E521F845BA4953509FA45E321715FDF5B92C5BD30759C1669B4F0DABA17AC7BBF729749CE3E3203606AB49F20C19D342A078A3903D1
+SET decryptionKey=3E1AD56713339011EB29E98D1DF3DBE1BADCF353938C3429
+
 cd %ROOTDIR%
 
 if "%1" == "" (
@@ -28,6 +31,10 @@ cscript .\common\replace.vbs "TeleoptiCCC7_Demo" "%CCC7DB%" "%MySettings%" > NUL
 ::Build Teleopti.Support.Tool.exe
 ECHO Building %ROOTDIR%\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj
 %MSBUILD% /t:rebuild "%ROOTDIR%\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" > "%ROOTDIR%\Teleopti.Support.Tool.build.log"
+
+::add fixed encryption/decryption keys to match the ones used in WebTest
+ECHO %validationKey%>"%ROOTDIR%\..\Teleopti.Support.Tool\bin\Debug\validation.key"
+ECHO %decryptionKey%>"%ROOTDIR%\..\Teleopti.Support.Tool\bin\Debug\decryption.key"
 
 ::Run supportTool to replace all config
 "%ROOTDIR%\..\Teleopti.Support.Tool\bin\Debug\Teleopti.Support.Tool.exe" -MODebug
