3052607a1bdd518e1a4f64c7688760e7e3283477
Henke <henrik.andersson@teleopti.com>
2014-05-13 14:54:40 +0200

    Added vm and js test for realtimeadherenceagents

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
@@ -11,6 +11,7 @@ require([
 		'tests',
 		'views/realtimeadherencesites/vm_test',
 		'views/realtimeadherenceteams/vm_test',
+		'views/realtimeadherenceagents/vm_test'
 ], function () {
 	for (var i = 0, j = arguments.length; i < j; i++) {
 		arguments[i]();
