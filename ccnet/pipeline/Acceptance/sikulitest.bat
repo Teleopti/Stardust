8c6d35de8430101d3c4acfb9677da153cb364eb0
David Jonsson <david.jonsson@teleopti.com>
2014-05-13 14:02:36 +0200

    Adding Sikuli Acceptance test to Pipeline

---
+++ ccnet/pipeline/Acceptance/sikulitest.bat
@@ -0,0 +1,20 @@
+::Test msbuild targets on webscenarios.msbuild
+:: usage
+:: sikulitest.bat [CCNetLabel] [destinationFolder] [targets]
+
+@ECHO off
+
+SET ROOTDIR=%~dp0
+SET ROOTDIR=%ROOTDIR:~0,-27%
+SET CCNetWorkingDirectory=%ROOTDIR%
+SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+
+set CCNetLabel=%1
+set DestinationFolder=%2
+set targets=%3
+
+echo %rootdir%
+
+%msbuild% %CCNetWorkingDirectory%\ccnet\pipeline\acceptance\sikulitest.msbuild /t:%targets%
+
+echo %ROOTDIR%
