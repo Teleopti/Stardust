737817c2ee0d7a5c289dae8c3f7e06e7257b3556
David Jonsson <david.jonsson@teleopti.com>
2014-05-13 14:46:22 +0200

    390 Demo backup special - only for 390
    --HG--
    branch : Release 7.5.390

--- WISE/CompileAll.bat
+++ WISE/CompileAll.bat
@@ -147,9 +147,9 @@ XCOPY "%DEPENDENCIESSRC%\ccc7_forecast\SQLEXPR.EXE" "%WISESOURCEFILE%\Wise\ccc7_
 XCOPY "%DEPENDENCIESSRC%\ccc7_forecast\ForecastDatabase\TeleoptiCCC_Forecasts.BAK" "%WISESOURCEFILE%\ForecastDatabase\" /D /Y
 
 ::CCC_server
-XCOPY "%DEPENDENCIESSRC%\ccc7_server\DemoDatabase\TeleoptiAnalytics_Demo.bak" "%WISESOURCEFILE%\DemoDatabase\" /D /Y
-XCOPY "%DEPENDENCIESSRC%\ccc7_server\DemoDatabase\TeleoptiCCC7_Demo.bak" "%WISESOURCEFILE%\DemoDatabase\" /D /Y
-XCOPY "%DEPENDENCIESSRC%\ccc7_server\DemoDatabase\TeleoptiCCC7Agg_Demo.BAK" "%WISESOURCEFILE%\DemoDatabase\" /D /Y
+XCOPY "%DEPENDENCIESSRC%\ccc7_server\DemoDatabase\390\TeleoptiAnalytics_Demo.bak" "%WISESOURCEFILE%\DemoDatabase\" /D /Y
+XCOPY "%DEPENDENCIESSRC%\ccc7_server\DemoDatabase\390\TeleoptiCCC7_Demo.bak" "%WISESOURCEFILE%\DemoDatabase\" /D /Y
+XCOPY "%DEPENDENCIESSRC%\ccc7_server\DemoDatabase\390\TeleoptiCCC7Agg_Demo.BAK" "%WISESOURCEFILE%\DemoDatabase\" /D /Y
 XCOPY "%DEPENDENCIESSRC%\ccc7_server\ReportViewer2010.exe" "%WISESOURCEFILE%\Wise\ccc7_server\" /D /Y
 XCOPY "%DEPENDENCIESSRC%\ccc7_server\RegisterEventLogSource.exe" "%WISESOURCEFILE%\Wise\ccc7_server\Logs\" /D /Y
 XCOPY "%DEPENDENCIESSRC%\ccc7_server\ntrights.exe" "%WISESOURCEFILE%\Wise\ccc7_server\Logs\" /D /Y
