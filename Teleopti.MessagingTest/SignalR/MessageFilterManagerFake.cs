f81cb04c3b2d3e2dcc08b46b820b2d49b11f8e6f
Erik Sundberg <none@none>
2014-05-13 13:54:45 +0200

    moving classes to separate files
    --HG--
    branch : Release 7.5.390

---
+++ Teleopti.MessagingTest/SignalR/MessageFilterManagerFake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Teleopti.Interfaces.MessageBroker.Core;
+
+namespace Teleopti.MessagingTest.SignalR
+{
+	public class MessageFilterManagerFake : IMessageFilterManager
+	{
+		public bool HasType(Type type)
+		{
+			return true;
+		}
+
+		public string LookupTypeToSend(Type domainObjectType)
+		{
+			return "string";
+		}
+
+		public Type LookupType(Type domainObjectType)
+		{
+			return typeof(string);
+		}
+	}
+}
