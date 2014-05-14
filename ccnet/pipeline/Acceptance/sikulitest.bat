5275c35aaf15e82d051bfca5f075cf10301c7222
David Jonsson <david.jonsson@teleopti.com>
2014-05-14 10:01:10 +0200

    Add Custom MSBuild task

--- ccnet/pipeline/Acceptance/sikulitest.bat
+++ ccnet/pipeline/Acceptance/sikulitest.bat
@@ -1,6 +1,6 @@
 ::Test msbuild targets on webscenarios.msbuild
 :: usage
-:: sikulitest.bat [CCNetLabel] [destinationFolder] [targets] [SikuliPath]
+:: sikulitest.bat [CCNetLabel] [destinationFolder] [SikuliPath] [targets]
 :: sikulitest.bat 138 c:\Temp\Pipeline RunSikuliTests C:\Sikuli
 
 @ECHO off
@@ -12,10 +12,11 @@ SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 
 set CCNetLabel=%1
 set DestinationFolder=%2
-set targets=%3
+set SikuliPath=%3
+set targets=%4
 
 echo %rootdir%
-
+echo %msbuild% %CCNetWorkingDirectory%\ccnet\pipeline\acceptance\sikulitest.msbuild /t:%targets%
 %msbuild% %CCNetWorkingDirectory%\ccnet\pipeline\acceptance\sikulitest.msbuild /t:%targets%
 
 echo %ROOTDIR%
