31de8c5de32bd97f12dcc53c4a64253186c75d0d
Henke <henrik.andersson@teleopti.com>
2014-05-15 21:27:12 +0200

    #27954 Added AgentStateReader to controller

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Controllers/AgentsControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Controllers/AgentsControllerTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using Rhino.Mocks;
 using SharpTestsEx;
@@ -6,6 +8,8 @@ using Teleopti.Ccc.Domain.AgentInfo;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
+using Teleopti.Ccc.Infrastructure;
+using Teleopti.Ccc.Infrastructure.Rta;
 using Teleopti.Ccc.Web.Areas.Anywhere.Controllers;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Portal.DataProvider;
 
@@ -32,6 +36,55 @@ namespace Teleopti.Ccc.WebTest.Areas.Anywhere.Controllers
 			{
 				target.GetStates(teamId);
 				target.Response.StatusCode.Should().Be(403);
+			}
+		}
+
+		[Test]
+		public void GetStates_ShouldGetAllTheStatesForTeamFromReader()
+		{
+			var teamId = Guid.NewGuid();
+			var expected = new AgentAdherenceStateInfo()
+			               {
+				               Name = "Ashley Andeen",
+				               State = "out of adherence",
+				               Activity = "Phone",
+				               NextActivity = "Lunch",
+				               NextActivityStartTime = new DateTime(2001, 1, 1, 12, 3, 0),
+				               Alarm = "Alarma!",
+				               AlarmTime = new DateTime(2001, 1, 1, 12, 0, 0)
+			               };
+
+			var dataReader = new fakeStateReader(new List<AgentAdherenceStateInfo>()
+			                                     {
+				                                     expected,
+													 expected
+			                                     });
+
+			using (var target = new StubbingControllerBuilder().CreateController<AgentsController>(new FakePermissionProvider(), MockRepository.GenerateStub<ITeamRepository>(), new Now(), dataReader))
+			{
+			
+				var result = target.GetStates(teamId).Data as IEnumerable<AgentAdherenceStateInfo>;
+
+				result.Count().Should().Be(2);
+
+				Assert.That(result.First(),Is.EqualTo(expected));
+				Assert.That(result.Last(),Is.EqualTo(expected));
+			}
+		
+		}
+
+		private class fakeStateReader : IAgentStateReader
+		{
+			private readonly IEnumerable<AgentAdherenceStateInfo> _statesForAnyTeam;
+
+			public fakeStateReader(IEnumerable<AgentAdherenceStateInfo> statesForAnyTeam)
+			{
+				_statesForAnyTeam = statesForAnyTeam;
+			}
+
+			public IEnumerable<AgentAdherenceStateInfo> GetLatestStatesForTeam(Guid teamId)
+			{
+				return _statesForAnyTeam;
 			}
 		}
 	}
