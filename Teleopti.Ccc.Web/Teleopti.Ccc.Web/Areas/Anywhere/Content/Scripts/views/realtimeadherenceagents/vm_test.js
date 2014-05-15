f55fcc46b2c261be0e880067a17525e01bbef4ea
Xianwei Shen <xianwei.shen@teleopti.com>
2014-05-15 09:57:34 +0200

    fill agent states in view model

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/vm_test.js
@@ -6,6 +6,44 @@
 				var vm = viewModel();
 				assert.equals(vm.agents(), []);
 			},
+
+			"should fill agent state data": function() {
+				var state1 = {
+					Name: 'Pierre Baldi',
+					State: 'Ready',
+					Activity: 'Phone',
+					NextActivity: 'Lunch',
+					NextActivityStartTime: '2014-01-21 13:00',
+					Alarm: 'Adhering',
+					AlarmTime: '2014-01-21 12:15'
+				};
+				var state2 = {
+					Name: 'Ashley Andeen',
+					State: 'Pause',
+					Activity: 'Phone',
+					NextActivity: 'Lunch',
+					NextActivityStartTime: '2014-01-21 13:00',
+					Alarm: 'Not Adhering',
+					AlarmTime: '2014-01-21 12:15'
+				};
+				var vm = viewModel();
+				vm.fill([state1, state2]);
+
+				assert.equals(vm.agents()[0].Name, state1.Name);
+				assert.equals(vm.agents()[0].State, state1.State);
+				assert.equals(vm.agents()[0].Activity, state1.Activity);
+				assert.equals(vm.agents()[0].NextActivity, state1.NextActivity);
+				assert.equals(vm.agents()[0].NextActivityStartTime, state1.NextActivityStartTime);
+				assert.equals(vm.agents()[0].Alarm, state1.Alarm);
+				assert.equals(vm.agents()[0].AlarmTime, state1.AlarmTime);
+				assert.equals(vm.agents()[1].Name, state2.Name);
+				assert.equals(vm.agents()[1].State, state2.State);
+				assert.equals(vm.agents()[1].Activity, state2.Activity);
+				assert.equals(vm.agents()[1].NextActivity, state2.NextActivity);
+				assert.equals(vm.agents()[1].NextActivityStartTime, state2.NextActivityStartTime);
+				assert.equals(vm.agents()[1].Alarm, state2.Alarm);
+				assert.equals(vm.agents()[1].AlarmTime, state2.AlarmTime);
+			}
 		});		
 	};
 });
