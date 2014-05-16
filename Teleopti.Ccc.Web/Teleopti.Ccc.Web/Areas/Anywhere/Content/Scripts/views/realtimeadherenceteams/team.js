b6d370ed4ff89e00e350a36908fa63de9f6b5305
blandine <none@none>
2014-05-16 09:39:57 +0200

    Merge

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceteams/team.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceteams/team.js
@@ -7,6 +7,18 @@
 			var that = {};
 			that.OutOfAdherence = ko.observable();
 			that.hasBeenUpdated = ko.observable(false);
+
+			that.fill = function (data) {
+				that.Id = data.Id;
+				that.Name = data.Name;
+				that.NumberOfAgents = data.NumberOfAgents;
+			};
+
+			// needs id from the vm
+			that.openTeam = function () {
+				navigation.GotoRealTimeAdherenceTeamDetails(that.Id);
+			};
+
 			return that;
 		};
 	}
