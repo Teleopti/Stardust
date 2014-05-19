c71f072498abc88932ae129fc4f0849f6da56344
David Jonsson <david.jonsson@teleopti.com>
2014-05-19 15:36:30 +0200

    bug #27986 - share with Karin. Not done

--- Teleopti.Support.Security/TeleoptiAnalyticsDataUpdate.cs
+++ Teleopti.Support.Security/TeleoptiAnalyticsDataUpdate.cs
@@ -20,56 +20,45 @@ namespace Teleopti.Support.Security
 			const int buildNumber = -2798601;
 			const string systemVersion = "fact_schedule";
 			
-		//Select database version 
 			SqlTransaction transaction = null;
 			using (SqlConnection connection = new SqlConnection(commandLineArgument.DestinationConnectionString))
-		{
-
-			connection.Open();
-
-			//Check version
-			SqlCommand command;
-			using (command = connection.CreateCommand())
 			{
-				command.CommandText = string.Format(System.Globalization.CultureInfo.InvariantCulture,
-					"SELECT COUNT(*) FROM dbo.DatabaseVersion WHERE BuildNumber={0} AND SystemVersion='{1}'",
-					buildNumber, systemVersion);
-				var versionCount = (int) command.ExecuteScalar();
-				if (versionCount > 0)
-				{
+				connection.Open();
+
+				if (patchApplied(connection, systemVersion, buildNumber))
 					return 1;
-				}
-			}
 
-			log.Debug("re-order columns in table mart.fact_schedule ...");
-			using (command = connection.CreateCommand())
-			{
-				command.CommandText = "select count(*) from mart.fact_schedule_old";
-				var rowsLeft = (Int32)command.ExecuteScalar();
-				log.Debug("Total number of rows to update: " + rowsLeft);
-				var rows = 1;
+				SqlCommand command;
 
-				command.CommandText = "Exec mart.sp_dummy";
+				log.Debug("re-order columns in table mart.fact_schedule ...");
+				using (command = connection.CreateCommand())
 				{
-					while (rows > 0)
+					command.CommandText = "select count(*) from mart.fact_schedule_old";
+					var rowsLeft = (Int32)command.ExecuteScalar();
+					log.Debug("Total number of rows to update: " + rowsLeft);
+					var rows = 1;
+
+					command.CommandText = "Exec mart.sp_dummy";
 					{
-						using (transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+						while (rows > 0)
 						{
-							command.Transaction = transaction;
-							rows = command.ExecuteNonQuery();
-							transaction.Commit();
+							using (transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+							{
+								command.Transaction = transaction;
+								rows = command.ExecuteNonQuery();
+								transaction.Commit();
+							}
+							rowsLeft = rowsLeft - rows;
+							log.Debug("\tRows left to update: " + rowsLeft);
 						}
-						rowsLeft = rowsLeft - rows;
-						log.Debug("\tRows left to update: " + rowsLeft);
+						log.Debug("No more rows that can be moved. Left overs are: " + rowsLeft);
 					}
-					log.Debug("No more rows that can be moved. Left overs are: " + rowsLeft);
 				}
-			}
 			updateDatabaseVersion(connection, systemVersion, buildNumber);
-		}
+			}
 		log.Debug("re-order columns in table mart.fact_schedule. Done!");
 		return 0;
-	}
+		}
 
 		private void updateDatabaseVersion(SqlConnection connection, string systemVersion, int buildNumber)
 		{
@@ -82,6 +71,24 @@ namespace Teleopti.Support.Security
 				Environment.UserName);
 			command.ExecuteNonQuery();
 			log.Debug("Done updating database version.");
+		}
+
+		private bool patchApplied(SqlConnection connection, string systemVersion, int buildNumber)
+		{
+			SqlCommand command;
+			using (command = connection.CreateCommand())
+			{
+				command.CommandText = string.Format(System.Globalization.CultureInfo.InvariantCulture,
+					"SELECT COUNT(*) FROM dbo.DatabaseVersion WHERE BuildNumber={0} AND SystemVersion='{1}'",
+					buildNumber, systemVersion);
+				var versionCount = (int) command.ExecuteScalar();
+				if (versionCount > 0)
+				{
+					return true;
+				}
+				else
+					return false;
+			}
 		}
 	}
 }
