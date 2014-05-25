b03dfbe08d620050e24c484ec543f960d11dae3c
Xianwei Shen <xianwei.shen@teleopti.com>
2014-05-25 22:09:39 +0200

    fix randomly failed test

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/vm_test.js
@@ -8,7 +8,7 @@
 			},
 			
 			"should fill agents info": function () {
-				var agent = { PersonId: "guid", Name: "Bill", SiteId: "gui1", SiteName: "site", TeamId: "guid2", TeamName: "team", TimeZoneOffset: -600 };
+				var agent = { PersonId: "guid", Name: "Bill", SiteId: "gui1", SiteName: "site", TeamId: "guid2", TeamName: "team", TimeZoneOffsetMinutes: -600 };
 				var vm = viewModel();
 				vm.fillAgents([agent]);
 
@@ -18,7 +18,7 @@
 				assert.equals(vm.agents[0].SiteName, agent.SiteName);
 				assert.equals(vm.agents[0].TeamId, agent.TeamId);
 				assert.equals(vm.agents[0].TeamName, agent.TeamName);
-				assert.equals(vm.agents[0].TimeZoneOffset, agent.TimeZoneOffset);
+				assert.equals(vm.agents[0].TimeZoneOffset, agent.TimeZoneOffsetMinutes);
 			},
 
 			"should fill agent state data": function() {
@@ -29,6 +29,7 @@
 					NextActivity: 'Lunch',
 					NextActivityStartTime: moment('2014-01-21 13:00').format(resources.DateTimeFormatForMoment),
 					Alarm: 'Adhering',
+					AlarmColor: '#333',
 					AlarmTime: moment('2014-01-21 12:15').format(resources.DateTimeFormatForMoment)
 				};
 				var state2 = {
@@ -38,11 +39,12 @@
 					NextActivity: 'Lunch',
 					NextActivityStartTime: moment('2014-01-21 13:00').format(resources.DateTimeFormatForMoment),
 					Alarm: 'Not Adhering',
+					AlarmColor: '#fff',
 					AlarmTime: moment('2014-01-21 12:15').format(resources.DateTimeFormatForMoment)
 				};
 				var vm = viewModel();
-				vm.fillAgents([{ PersonId: "guid1", Name: "Bill", SiteId: "gui1", SiteName: "site", TeamId: "guid2", TeamName: "team", TimeZoneOffset: -600 },
-					{ PersonId: "guid2", Name: "John", SiteId: "gui1", SiteName: "site", TeamId: "guid2", TeamName: "team", TimeZoneOffset: -600 }]);
+				vm.fillAgents([{ PersonId: "guid1", Name: "Bill", SiteId: "gui1", SiteName: "site", TeamId: "guid2", TeamName: "team", TimeZoneOffsetMinutes: -600 },
+					{ PersonId: "guid2", Name: "John", SiteId: "gui1", SiteName: "site", TeamId: "guid2", TeamName: "team", TimeZoneOffsetMinutes: -600 }]);
 				vm.fillAgentsStates([state1, state2]);
 
 				assert.equals(vm.agentStates()[0].PersonId, state1.PersonId);
@@ -51,6 +53,7 @@
 				assert.equals(vm.agentStates()[0].NextActivity, state1.NextActivity);
 				assert.equals(vm.agentStates()[0].NextActivityStartTime, moment(state1.NextActivityStartTime, resources.DateTimeFormatForMoment).add(-600, 'minutes').format(resources.DateTimeFormatForMoment));
 				assert.equals(vm.agentStates()[0].Alarm, state1.Alarm);
+				assert.equals(vm.agentStates()[0].AlarmColor, state1.AlarmColor);
 				assert.equals(vm.agentStates()[0].AlarmTime, moment(state1.AlarmTime, resources.DateTimeFormatForMoment).add(-600, 'minutes').format(resources.DateTimeFormatForMoment));
 				assert.equals(vm.agentStates()[1].PersonId, state2.PersonId);
 				assert.equals(vm.agentStates()[1].State, state2.State);
@@ -58,6 +61,7 @@
 				assert.equals(vm.agentStates()[1].NextActivity, state2.NextActivity);
 				assert.equals(vm.agentStates()[1].NextActivityStartTime, moment(state2.NextActivityStartTime, resources.DateTimeFormatForMoment).add(-600, 'minutes').format(resources.DateTimeFormatForMoment));
 				assert.equals(vm.agentStates()[1].Alarm, state2.Alarm);
+				assert.equals(vm.agentStates()[1].AlarmColor, state2.AlarmColor);
 				assert.equals(vm.agentStates()[1].AlarmTime, moment(state2.AlarmTime, resources.DateTimeFormatForMoment).add(-600, 'minutes').format(resources.DateTimeFormatForMoment));
 			}
 		});		
