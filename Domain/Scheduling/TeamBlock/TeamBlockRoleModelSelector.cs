5a25c74a0b9a1698c023a5a3e1d6584932dc077d
Asad MIrza <asad.mirza@teleopti.com>
2014-05-27 12:53:46 +0200

    PBI #23419: added the control code and toggle for max seat calculation

--- Domain/Scheduling/TeamBlock/TeamBlockRoleModelSelector.cs
+++ Domain/Scheduling/TeamBlock/TeamBlockRoleModelSelector.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling.TeamBlock.Restriction;
 using Teleopti.Ccc.Domain.Scheduling.TeamBlock.Specification;
@@ -21,13 +24,15 @@ namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock
 		private readonly IWorkShiftSelector _workShiftSelector;
 		private readonly ISchedulingResultStateHolder _schedulingResultStateHolder;
 		private readonly IActivityIntervalDataCreator _activityIntervalDataCreator;
+		private readonly IMaxSeatInformationGeneratorBasedOnIntervals _maxSeatInformationGeneratorBasedOnIntervals;
+		private readonly IToggleManager  _toggleManager;
 
 		public TeamBlockRoleModelSelector(ITeamBlockRestrictionAggregator teamBlockRestrictionAggregator,
 										  IWorkShiftFilterService workShiftFilterService,
 										  ISameOpenHoursInTeamBlockSpecification sameOpenHoursInTeamBlockSpecification,
 										  IWorkShiftSelector workShiftSelector,
 											ISchedulingResultStateHolder schedulingResultStateHolder,
-											IActivityIntervalDataCreator activityIntervalDataCreator)
+											IActivityIntervalDataCreator activityIntervalDataCreator, IMaxSeatInformationGeneratorBasedOnIntervals maxSeatInformationGeneratorBasedOnIntervals, IToggleManager toggleManager)
 		{
 			_teamBlockRestrictionAggregator = teamBlockRestrictionAggregator;
 			_workShiftFilterService = workShiftFilterService;
@@ -35,6 +40,8 @@ namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock
 			_workShiftSelector = workShiftSelector;
 			_schedulingResultStateHolder = schedulingResultStateHolder;
 			_activityIntervalDataCreator = activityIntervalDataCreator;
+			_maxSeatInformationGeneratorBasedOnIntervals = maxSeatInformationGeneratorBasedOnIntervals;
+			_toggleManager = toggleManager;
 		}
 
         public IShiftProjectionCache Select(ITeamBlockInfo teamBlockInfo, DateOnly datePointer, IPerson person,
@@ -59,6 +66,13 @@ namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock
 			        .UseMinimumPersons,
 		        schedulingOptions
 			        .UseMaximumPersons, MaxSeatsFeatureOptions.DoNotConsiderMaxSeats);
+			  IDictionary<DateTime, bool> maxSeatInfo = new Dictionary<DateTime, bool>();
+			  if (_toggleManager.IsEnabled(Toggles.Scheduler_TeamBlockAdhereWithMaxSeatRule_23419))
+			  {
+				  maxSeatInfo = _maxSeatInformationGeneratorBasedOnIntervals.GetMaxSeatInfo(teamBlockInfo, datePointer ,
+					  _schedulingResultStateHolder);
+			  }
+	        parameters.MaxSeatInfoPerInterval = maxSeatInfo;
 			var roleModel = _workShiftSelector.SelectShiftProjectionCache(shifts, activityInternalData,
 				parameters, TimeZoneGuard.Instance.TimeZone);
 			return roleModel;
---
+++ Domain/Scheduling/TeamBlock/WorkShiftCalculation/MaxSeatInformationGeneratorBasedOnIntervals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock.WorkShiftCalculation
+{
+	public interface IMaxSeatInformationGeneratorBasedOnIntervals
+	{
+		IDictionary<DateTime, bool> GetMaxSeatInfo(ITeamBlockInfo teamBlockInfo, DateOnly datePointer, ISchedulingResultStateHolder  schedulingResultStateHolder);
+	}
+
+	public class MaxSeatInformationGeneratorBasedOnIntervals : IMaxSeatInformationGeneratorBasedOnIntervals
+	{
+		private readonly IMaxSeatSkillAggregator _maxSeatSkillAggregator;
+
+		public MaxSeatInformationGeneratorBasedOnIntervals(IMaxSeatSkillAggregator maxSeatSkillAggregator)
+		{
+			_maxSeatSkillAggregator = maxSeatSkillAggregator;
+		}
+
+		public IDictionary<DateTime, bool> GetMaxSeatInfo(ITeamBlockInfo teamBlockInfo, DateOnly datePointer, ISchedulingResultStateHolder  schedulingResultStateHolder)
+		{
+			var skills = _maxSeatSkillAggregator.GetAggregatedSkills(teamBlockInfo.TeamInfo.GroupMembers.ToList(), datePointer ).ToList();
+			var skillDaysOnDatePointer = schedulingResultStateHolder.SkillDaysOnDateOnly(new List<DateOnly> { datePointer });
+			var skillDaysHavingMaxSeatInfo = new List<ISkillDay>();
+			foreach (var skillDay in skillDaysOnDatePointer)
+			{
+				if (skills.Contains(skillDay.Skill))
+					skillDaysHavingMaxSeatInfo.Add(skillDay);
+			}
+			//is this check really valid?
+			var maxSeatInfoOnEachIntervalDic = new Dictionary<DateTime, bool>();
+			if (skillDaysHavingMaxSeatInfo.Any( ))
+			{
+				//skillStaffPeriodCollection = skillDaysHavingMaxSeatInfo[0].SkillStaffPeriodCollection;
+				//call Jovi code here and return the result
+			}
+			//is this really valid
+			return maxSeatInfoOnEachIntervalDic;
+		}
+	}
+}
