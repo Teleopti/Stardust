0b2e6c66cffbf28f8a0b1d12dfd949463bfa77b5
Chundan Xu <chundan.xu@teleopti.com>
2014-05-28 12:49:17 +0800

    PBI #25635 js unit tests for week view model

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.WeekViewModel.Tests.js
@@ -0,0 +1,37 @@
+﻿
+
+$(document).ready(function () {
+
+	 module("Teleopti.MyTimeWeb.Preference.WeekViewModel");
+
+	test("should summarize possible weekly contract time", function () {
+		var viewModelDay1 = new Teleopti.MyTimeWeb.Preference.DayViewModel();
+		var viewModelDay2 = new Teleopti.MyTimeWeb.Preference.DayViewModel();
+		var weekViewModel = new Teleopti.MyTimeWeb.Preference.WeekViewModel();
+
+		viewModelDay1.PossibleContractTimeMinutesLower(6 * 60);
+		viewModelDay1.PossibleContractTimeMinutesUpper(10 * 60);
+		viewModelDay2.PossibleContractTimeMinutesLower(6 * 60);
+		viewModelDay2.PossibleContractTimeMinutesUpper(10 * 60);
+
+		weekViewModel.DayViewModels.push(viewModelDay1);
+		weekViewModel.DayViewModels.push(viewModelDay2);
+
+		expect(2);
+		equal(weekViewModel.PossibleResultWeeklyContractTimeMinutesLower(), 12 * 60);
+		equal(weekViewModel.PossibleResultWeeklyContractTimeMinutesUpper(), 20 * 60);
+	});
+
+	test("should format possible weekly contract time", function () {
+		var viewModelDay = new Teleopti.MyTimeWeb.Preference.DayViewModel();
+		var weekViewModel = new Teleopti.MyTimeWeb.Preference.WeekViewModel();
+		viewModelDay.PossibleContractTimeMinutesLower(100 * 60 + 30);
+		viewModelDay.PossibleContractTimeMinutesUpper(160 * 60 + 5);
+		weekViewModel.DayViewModels.push(viewModelDay);
+
+		expect(2);
+		equal(weekViewModel.PossibleResultWeeklyContractTimeLower(), "100:30");
+		equal(weekViewModel.PossibleResultWeeklyContractTimeUpper(), "160:05");
+	});
+
+});
