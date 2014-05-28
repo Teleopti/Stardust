c9ee1bc4bf6ec3b894342d1f2b393f3fd2d3f85d
Chundan Xu <chundan.xu@teleopti.com>
2014-05-28 14:17:41 +0800

    PBI #25635 modify the calculation of weely work time according to schedule and period

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.WeekViewModel.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.WeekViewModel.js
@@ -19,32 +19,38 @@ if (typeof (Teleopti) === 'undefined') {
 Teleopti.MyTimeWeb.Preference.WeekViewModel = function () {
 	var self = this;
 
-    this.DayViewModels = ko.observableArray();
+	this.DayViewModels = ko.observableArray();
 
 	self.PossibleResultWeeklyContractTimeMinutesLower = ko.computed(function() {
 		var sum = 0;
-		$.each(self.DayViewModels(), function(index, day) {
-			var value = day.PossibleContractTimeMinutesLower();
-			if (value)
-				sum += parseInt(value);
-			var absenceValue = day.AbsenceContractTimeMinutes();
-			if (absenceValue)
-				sum += parseInt(absenceValue);
-			sum += day.ContractTimeMinutes();
+		$.each(self.DayViewModels(), function (index, day) {
+			 if (!(day.EditableHasNoSchedule()) || (day.EditableHasNoSchedule() && day.EditableIsInOpenPeriod() && day.DisplayFeedback())) {
+			 	 var value = day.PossibleContractTimeMinutesLower();
+			 	 if (value)
+			 	 	 sum += parseInt(value);
+			 	 var absenceValue = day.AbsenceContractTimeMinutes();
+			 	 if (absenceValue)
+			 	 	 sum += parseInt(absenceValue);
+			 	 sum += day.ContractTimeMinutes();
+			 }
+
 		});
 		return sum;
 	});
 
 	self.PossibleResultWeeklyContractTimeMinutesUpper = ko.computed(function() {
 		var sum = 0;
-		$.each(self.DayViewModels(), function(index, day) {
-			var value = day.PossibleContractTimeMinutesUpper();
-			if (value)
-				sum += parseInt(value);
-			var absenceValue = day.AbsenceContractTimeMinutes();
-			if (absenceValue)
-				sum += parseInt(absenceValue);
-			sum += day.ContractTimeMinutes();
+		$.each(self.DayViewModels(), function (index, day) {
+			 if (!(day.EditableHasNoSchedule()) || (day.EditableHasNoSchedule() && day.EditableIsInOpenPeriod() && day.DisplayFeedback())) {
+				 var value = day.PossibleContractTimeMinutesUpper();
+				 if (value)
+					 sum += parseInt(value);
+				 var absenceValue = day.AbsenceContractTimeMinutes();
+				 if (absenceValue)
+					 sum += parseInt(absenceValue);
+				 sum += day.ContractTimeMinutes();
+			 }
+			
 		});
 		return sum;
 	});
@@ -54,6 +60,12 @@ Teleopti.MyTimeWeb.Preference.WeekViewModel = function () {
 	});
 	self.PossibleResultWeeklyContractTimeUpper = ko.computed(function() {
 		return Teleopti.MyTimeWeb.Preference.formatTimeSpan(self.PossibleResultWeeklyContractTimeMinutesUpper());
+	});
+
+	self.IsWeeklyWorkTimeVisible = ko.computed(function() {
+		if (self.PossibleResultWeeklyContractTimeMinutesLower() > 0)
+			return true;
+		return false;
 	});
 
 };
