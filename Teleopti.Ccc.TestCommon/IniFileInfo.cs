089179f45bee6f7fff8eec62f102b84cbde69fa3
Roger Kratz <roger.kratz@gmail.com>
2014-05-28 15:55:25 +0200

    no need to set toggle file in tests

--- Teleopti.Ccc.TestCommon/IniFileInfo.cs
+++ Teleopti.Ccc.TestCommon/IniFileInfo.cs
@@ -23,7 +23,6 @@ namespace Teleopti.Ccc.TestCommon
 		public static string SQL_PASSWORD { get; private set; }
 		public static string AGENTPORTALWEB_nhibConfPath { get; private set; }
 		public static bool ServiceBus { get; private set; }
-		public static string FeatureToggle { get; private set; }
 
 		private static void readIniFile()
 		{
@@ -36,7 +35,6 @@ namespace Teleopti.Ccc.TestCommon
 			WEB_BROKER_BACKPLANE = ConfigurationManager.AppSettings["WEB_BROKER_BACKPLANE"];
 			AGENTPORTALWEB_nhibConfPath = ConfigurationManager.AppSettings["AGENTPORTALWEB_nhibConfPath"];
 			ServiceBus = ConfigurationManager.AppSettings["servicebus"] == "true";
-			FeatureToggle = ConfigurationManager.AppSettings["FeatureToggle"];
 			buildConnectionString();
 		}
 
