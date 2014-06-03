4c3fa25908b7ec5578466584be905debbfe9b550
Asad MIrza <asad.mirza@teleopti.com>
2014-06-03 12:44:00 +0200

    Bug #28143: Fixed the bug to not consider opening hours on a day off
    --HG--
    branch : Release 7.5.390

--- Domain/Scheduling/TeamBlock/Specification/SameOpenHoursInTeamBlockSpecification.cs
+++ Domain/Scheduling/TeamBlock/Specification/SameOpenHoursInTeamBlockSpecification.cs
@@ -5,47 +5,49 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock.Specification
 {
-    public interface ISameOpenHoursInTeamBlockSpecification
-    {
-        bool IsSatisfiedBy(ITeamBlockInfo skillDays);
-    }
+	public interface ISameOpenHoursInTeamBlockSpecification
+	{
+		bool IsSatisfiedBy(ITeamBlockInfo skillDays);
+	}
 
-    public class SameOpenHoursInTeamBlockSpecification : Specification<ITeamBlockInfo>, ISameOpenHoursInTeamBlockSpecification
-    {
-	    private readonly IOpenHourForDate _openHourForDate;
-	    private readonly ICreateSkillIntervalDataPerDateAndActivity _createSkillIntervalDataPerDateAndActivity;
-        private readonly ISchedulingResultStateHolder _schedulingResultStateHolder;
+	public class SameOpenHoursInTeamBlockSpecification : Specification<ITeamBlockInfo>,
+		ISameOpenHoursInTeamBlockSpecification
+	{
+		private readonly IOpenHourForDate _openHourForDate;
+		private readonly ICreateSkillIntervalDataPerDateAndActivity _createSkillIntervalDataPerDateAndActivity;
+		private readonly ISchedulingResultStateHolder _schedulingResultStateHolder;
 
-	    public SameOpenHoursInTeamBlockSpecification(IOpenHourForDate openHourForDate,
-	                                                 ICreateSkillIntervalDataPerDateAndActivity
-		                                                 createSkillIntervalDataPerDateAndActivity,
-	                                                 ISchedulingResultStateHolder schedulingResultStateHolder)
-	    {
-		    _openHourForDate = openHourForDate;
-		    _createSkillIntervalDataPerDateAndActivity = createSkillIntervalDataPerDateAndActivity;
-		    _schedulingResultStateHolder = schedulingResultStateHolder;
-	    }
+		public SameOpenHoursInTeamBlockSpecification(IOpenHourForDate openHourForDate,
+			ICreateSkillIntervalDataPerDateAndActivity
+				createSkillIntervalDataPerDateAndActivity,
+			ISchedulingResultStateHolder schedulingResultStateHolder)
+		{
+			_openHourForDate = openHourForDate;
+			_createSkillIntervalDataPerDateAndActivity = createSkillIntervalDataPerDateAndActivity;
+			_schedulingResultStateHolder = schedulingResultStateHolder;
+		}
 
-	    public override bool IsSatisfiedBy(ITeamBlockInfo  teamBlockInfo)
-        {
-	        var skillIntervalDataPerDateAndActivity = _createSkillIntervalDataPerDateAndActivity.CreateFor(teamBlockInfo,
-	                                                                                                       _schedulingResultStateHolder);
+		public override bool IsSatisfiedBy(ITeamBlockInfo teamBlockInfo)
+		{
+			var skillIntervalDataPerDateAndActivity = _createSkillIntervalDataPerDateAndActivity.CreateFor(teamBlockInfo,
+				_schedulingResultStateHolder);
 
-	        var dates = skillIntervalDataPerDateAndActivity.Keys;
-	        var sampleDate = dates.First();
-	        var sampleOpenHour = _openHourForDate.OpenHours(sampleDate, skillIntervalDataPerDateAndActivity[sampleDate]);
-		    var blockPeriod = teamBlockInfo.BlockInfo.BlockPeriod;
-	        foreach (var dateOnly in dates)
-	        {
+			var dates = skillIntervalDataPerDateAndActivity.Keys;
+			var sampleDate = dates.First();
+			var sampleOpenHour = _openHourForDate.OpenHours(sampleDate, skillIntervalDataPerDateAndActivity[sampleDate]);
+			var blockPeriod = teamBlockInfo.BlockInfo.BlockPeriod;
+			foreach (var dateOnly in dates)
+			{
 				if (!blockPeriod.Contains(dateOnly))
 					continue;
 
-		        var compareOpenHour = _openHourForDate.OpenHours(dateOnly, skillIntervalDataPerDateAndActivity[dateOnly]);
-		        if (compareOpenHour != sampleOpenHour)
-			        return false;
-	        }
+				var compareOpenHour = _openHourForDate.OpenHours(dateOnly, skillIntervalDataPerDateAndActivity[dateOnly]);
+				if (compareOpenHour == null) continue;
+				if (compareOpenHour != sampleOpenHour)
+					return false;
+			}
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
