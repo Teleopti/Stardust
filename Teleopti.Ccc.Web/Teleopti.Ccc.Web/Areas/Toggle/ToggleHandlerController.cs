3d08383ef4dccf8cfdd683f04a2943b7223a83bf
Roger Kratz <roger.kratz@gmail.com>
2014-06-05 15:53:56 +0200

    adding test method to list all toggles in web app. for debug purpose only!

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Toggle/ToggleHandlerController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Toggle/ToggleHandlerController.cs
@@ -1,20 +1,46 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Infrastructure.Foundation;
 
 namespace Teleopti.Ccc.Web.Areas.Toggle
 {
 	public class ToggleHandlerController : Controller
 	{
 		private readonly IToggleManager _toggleManager;
+		private readonly ITogglesActive _togglesActive;
 
-		public ToggleHandlerController(IToggleManager toggleManager)
+		public ToggleHandlerController(IToggleManager toggleManager, ITogglesActive togglesActive)
 		{
 			_toggleManager = toggleManager;
+			_togglesActive = togglesActive;
 		}
 
+		/// <summary>
+		/// Url
+		/// <![CDATA[
+		/// [web]/ToggleHandler/IsEnabled?toggle=[yourToggle]&datasource=[your datasource]
+		/// ]]>
+		/// </summary>
 		public bool IsEnabled(Toggles toggle)
 		{
 			return _toggleManager.IsEnabled(toggle);
+		}
+
+		/// <summary>
+		/// Not used in code. Debug usage only!
+		/// Url
+		/// <![CDATA[
+		/// [web]/ToggleHandler/IsEnabled?toggle=[yourToggle]&datasource=[your datasource]
+		/// ]]>
+		/// </summary>
+		[HttpGet]
+		public JsonResult AllToggles()
+		{
+			var dic = _togglesActive.AllActiveToggles()
+				.ToDictionary(toggleInfo => toggleInfo.Key.ToString(), toggleInfo => toggleInfo.Value);
+
+			return Json(dic, JsonRequestBehavior.AllowGet);
 		}
 	}
 }
