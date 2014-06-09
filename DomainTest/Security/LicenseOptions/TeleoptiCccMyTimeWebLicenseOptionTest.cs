27a58f86ebd2fc8bcf3128a746b9ede103c97c49
Chundan Xu <chundan.xu@teleopti.com>
2014-06-09 14:46:03 +0800

    fix failed unit test for permission 22254

--- DomainTest/Security/LicenseOptions/TeleoptiCccMyTimeWebLicenseOptionTest.cs
+++ DomainTest/Security/LicenseOptions/TeleoptiCccMyTimeWebLicenseOptionTest.cs
@@ -17,7 +17,7 @@ namespace Teleopti.Ccc.DomainTest.Security.LicenseOptions
 			target.EnableApplicationFunctions(new List<IApplicationFunction>());
 			var result = target.EnabledApplicationFunctions;
 			
-			Assert.AreEqual(6, result.Count);
+			Assert.AreEqual(7, result.Count);
 		}
 
 	}
