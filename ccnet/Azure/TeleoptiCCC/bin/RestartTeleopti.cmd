bed01b4ae9ee33017e6d26355621054e8b334c88
David Jonsson <david.jonsson@teleopti.com>
2014-06-10 23:09:10 +0200

    bug #28359 - move to SupportTool. Used as one common file
    --HG--
    branch : Release 7.5.390

--- ccnet/Azure/TeleoptiCCC/bin/RestartTeleopti.cmd
+++ ccnet/Azure/TeleoptiCCC/bin/RestartTeleopti.cmd
@@ -2,4 +2,4 @@ SET FILE=%~n0
 SET DIRECTORY=%~dp0
 SET DIRECTORY=%DIRECTORY:~0,-1%
 powershell set-executionpolicy unrestricted
-powershell ". %DIRECTORY%\RestartTeleopti.ps1; main"
+powershell ". %DIRECTORY%\..\Tools\SupportTools\RestartTeleopti.ps1; main"
