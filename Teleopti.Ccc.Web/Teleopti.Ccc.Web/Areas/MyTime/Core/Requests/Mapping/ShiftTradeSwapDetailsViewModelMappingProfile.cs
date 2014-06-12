1c7640c9b5a960c12c312e344d6f15da84120501
Chundan Xu <chundan.xu@teleopti.com>
2014-06-12 17:44:09 +0800

    clean up

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/ShiftTradeSwapDetailsViewModelMappingProfile.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/Mapping/ShiftTradeSwapDetailsViewModelMappingProfile.cs
@@ -217,20 +217,4 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.Mapping
 		}
 	}
 
-    //internal class ShiftTradePersonDayData
-    //{
-    //    public string Name { get; set; }
-
-    //    public IEnumerable<IVisualLayer> ScheduleLayers { get; set; }
-
-    //    public string DayOffText { get; set; }
-
-    //    public TimeZoneInfo PersonTimeZone { get; set; }
-
-    //    public CultureInfo PersonCulture { get; set; }
-
-    //    public SchedulePartView SignificantPartForDisplay { get; set; }
-
-    //    public Guid PersonId { get; set; }
-    //}
 }
