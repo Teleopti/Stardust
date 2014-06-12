99467c40c18fcff8dd635d533d4a3d7138e04760
Erik Sundberg <none@none>
2014-06-12 10:15:55 +0200

    Found a cooler way to get branch name
    --HG--
    branch : Release 7.5.390

--- .debug-Setup/FixMyConfigFlow.bat
+++ .debug-Setup/FixMyConfigFlow.bat
@@ -3,11 +3,15 @@ SETLOCAL
 
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
-SET REPODIR=%ROOTDIR:\.debug-Setup=%
-FOR %%f in (%REPODIR%) DO SET REPONAME=%%~nxf
-SET CCC7DB=%REPONAME%_DemoSales_TeleoptiCCC7
-SET AnalyticsDB=%REPONAME%_DemoSales_TeleoptiAnalytics
+SET CD=%ROOTDIR%\..
+CALL :Branch "%CD%"
+SET CCC7DB=%Branch%_DemoSales_TeleoptiCCC7
+SET AnalyticsDB=%Branch%_DemoSales_TeleoptiAnalytics
 
 CALL FixMyConfig.bat %CCC7DB% %AnalyticsDB%
+
+:Branch
+SET BRANCH=%~n1
+SET BRANCH=%BRANCH%%~x1
 
 ENDLOCAL
