b6155fad2a06fbc835b2cfd52d9ceac561e534c1
Roger Kratz <roger.kratz@gmail.com>
2014-06-12 18:01:54 +0200

    vafan.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
@@ -12,8 +12,7 @@ require([
 		'views/realtimeadherencesites/vm_test',
 		'views/realtimeadherenceteams/vm_test',
 		'views/realtimeadherenceagents/vm_test',
-		'views/realtimeadherenceteams/team_test',
-		'feature_test'
+		'views/realtimeadherenceteams/team_test'
 ], function () {
 	for (var i = 0, j = arguments.length; i < j; i++) {
 		arguments[i]();
