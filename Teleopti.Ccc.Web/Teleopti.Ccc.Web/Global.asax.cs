1c0d9b719325228b7938d6c90dc351c76717e965
Erik Sundberg <none@none>
2014-06-12 14:15:38 +0200

    remove stuff from global asax, new approach to ajax

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Global.asax.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Global.asax.cs
@@ -57,15 +57,7 @@ namespace Teleopti.Ccc.Web
 
 		protected void Application_BeginRequest(object sender, EventArgs e)
 		{
-			HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-			if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
-			{
-				//These headers are handling the "pre-flight" OPTIONS call sent by the browser
-				HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
-				HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
-				HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
-				HttpContext.Current.Response.End();
-			}
+
 		}
 
 		protected void Application_AuthenticateRequest(object sender, EventArgs e)
