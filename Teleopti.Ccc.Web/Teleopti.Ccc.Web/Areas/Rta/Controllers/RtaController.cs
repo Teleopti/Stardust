47d91b19df5f454dca81a677c8b7a483505d7f5a
Erik Sundberg <none@none>
2014-06-13 13:26:58 +0200

    Green tests on new RtaController

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Teleopti.Ccc.Rta.Interfaces;
+using Newtonsoft.Json;
 
 namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 {
@@ -17,10 +14,44 @@ namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 	    }
 
 	    public int SaveExternalUserState(string externalState)
-		{
-		//	_teleoptiRtaService.SaveExternalUserState()
-		    return -1;
-		}
+	    {
+		    var state = JsonConvert.DeserializeObject<ajaxUserState>(externalState);
+		    return _teleoptiRtaService.SaveExternalUserState(state.AuthenticationKey, 
+				state.UserCode, 
+				state.StateCode,
+			    state.StateDescription, 
+				state.IsLoggedOn, 
+				state.SecondsInState, 
+				state.Timestamp, 
+				state.PlatformTypeId,
+			    state.SourceId, 
+				state.BatchId, 
+				state.IsSnapshot);
+	    }
 
+	    private class ajaxUserState
+	    {
+			public string AuthenticationKey { get; set; }
+
+		    public string UserCode { get; set; }
+
+		    public string StateCode { get; set; }
+
+		    public string StateDescription { get; set; }
+
+		    public bool IsLoggedOn { get; set; }
+
+		    public int SecondsInState { get; set; }
+
+		    public DateTime Timestamp { get; set; }
+
+			public string PlatformTypeId { get; set; }
+
+			public string SourceId { get; set; }
+
+		    public DateTime BatchId { get; set; }
+
+		    public bool IsSnapshot { get; set; }
+	    }
     }
 }
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Rta/Controllers/RtaControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Rta/Controllers/RtaControllerTest.cs
@@ -1,14 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Web.Areas.Rta;
 using Teleopti.Ccc.Web.Areas.Rta.Controllers;
 
 namespace Teleopti.Ccc.WebTest.Areas.Rta.Controllers
 {
 	public class RtaControllerTest
 	{
-		private RtaController
+		[Test]
+		public void ShouldCallRtaWebService()
+		{
+			const string externalState =
+				@"{
+'authenticationKey':'!#¤atAbgT%',
+'userCode':2001,
+'stateCode':'OFF',
+'isLoggedOn':'true',
+'timestamp':'2014-06-13T10:54:27',
+'platformTypeId':'00000000-0000-0000-0000-000000000000',
+'sourceId':1,
+'isSnapshot':'false'}";
+			var rtaWebService = MockRepository.GenerateMock<ITeleoptiRtaService>();
+			var target = new RtaController(rtaWebService);
 
+			target.SaveExternalUserState(externalState);
+
+			rtaWebService.AssertWasCalled(
+				x => x.SaveExternalUserState("!#¤atAbgT%",
+				"2001",
+				"OFF",
+				null,
+				true,
+				0,
+				new DateTime(2014, 06, 13, 10, 54, 27),
+				Guid.Empty.ToString(),
+				"1",
+				DateTime.MinValue,
+				false));
+		}
 	}
 }
