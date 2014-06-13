df105da1cd9091dc69035d3ad42bb9422b86f9c0
Erik Sundberg <none@none>
2014-06-13 16:36:09 +0200

    Cleaning up

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Core/IoC/RtaAreaModule.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Core/IoC/RtaAreaModule.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Security.Policy;
-using System.Security.Principal;
-using Autofac;
-using Autofac.Integration.Wcf;
-using log4net;
-using MbCache.Configuration;
+﻿using Autofac;
 using Teleopti.Ccc.IocCommon.Configuration;
-using Teleopti.Ccc.Rta.Interfaces;
 using Teleopti.Ccc.Rta.Server;
-using Teleopti.Ccc.Rta.Server.Adherence;
 
 namespace Teleopti.Ccc.Web.Areas.Rta.Core.IoC
 {
@@ -24,42 +16,7 @@ namespace Teleopti.Ccc.Web.Areas.Rta.Core.IoC
 		protected override void Load(ContainerBuilder builder)
 		{
 			builder.RegisterModule(new RealTimeContainerInstaller(_mbCacheModule));
-			//builder.RegisterModule(new DateAndTimeModule());
-			builder.RegisterType<TeleoptiRtaService>().As<ITeleoptiRtaService>().SingleInstance();
-
-			//var container = buildIoc();
-			//AutofacHostFactory.Container = container;
-
-			//container.Resolve<IRtaDataHandler>();
-			//container.Resolve<AdherenceAggregatorInitializor>().Initialize();
-			//setDefaultGenericPrincipal();
+			builder.RegisterType<TeleoptiRtaService>().AsSelf().As<ITeleoptiRtaService>().SingleInstance();
 		}
-
-		//private static IContainer buildIoc()
-		//{
-		//	//var builder = new ContainerBuilder();
-
-
-		//	//var mbCacheModule = new MbCacheModule(new InMemoryCache(20), null);
-		//	//builder.RegisterModule(mbCacheModule);
-		//	//builder.RegisterModule(new RealTimeContainerInstaller(mbCacheModule));
-		//	//builder.RegisterModule(new DateAndTimeModule());
-		//	//builder.RegisterType<TeleoptiRtaService>().As<ITeleoptiRtaService>().SingleInstance();
-		//	//return builder.Build();
-		//}
-
-		//private static void setDefaultGenericPrincipal()
-		//{
-		//	try
-		//	{
-		//		Logger.Debug("Trying to set default generic principal.");
-		//		AppDomain.CurrentDomain.SetThreadPrincipal(new GenericPrincipal(new GenericIdentity("Anonymous"), new string[] { }));
-		//	}
-		//	catch (PolicyException policyException)
-		//	{
-		//		Logger.Debug("Failed to set thread principal for app domain, because it was already set.", policyException);
-		//	}
-		//}
-
 	}
 }
