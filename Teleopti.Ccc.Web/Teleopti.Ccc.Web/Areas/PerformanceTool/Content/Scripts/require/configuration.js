2bbbdfcc0027eb7d9b93460983f4c3c1c4cda782
Erik Sundberg <none@none>
2014-06-16 15:07:24 +0200

    test for iterations (added ko to require config to make buster run)

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/require/configuration.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/require/configuration.js
@@ -1,6 +1,7 @@
 ﻿var requireconfiguration = {
 	paths: {
 		buster: '../../../../Content/busterjs/buster-test',
+		knockout: '../../../../Content/Scripts/knockout-2.2.1',
 	},
 	
 	// dependencies that requires loading order
