c3b97877125647f3c191c896d2f1f96277dbf58b
Henke <henrik.andersson@teleopti.com>
2014-06-16 14:21:45 +0200

    Collapsed revision * temp * added basic time line test for person schedule view model

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/vm_test.js
@@ -6,12 +6,44 @@ define([
     buster,
     viewModel
     ) {
+
 	return function () {
 
 		buster.testCase("personschedule viewmodel", {
+
 			"should create viewmodel": function () {
 				var vm = new viewModel();
 				assert(vm);
+			},
+
+			"should create timeline with default times": function() {
+				var vm = new viewModel();
+
+				assert.equals(vm.TimeLine.StartMinutes(), 8 * 60);
+				assert.equals(vm.TimeLine.EndMinutes(), 16 * 60);
+			},
+
+			"should create timeline according to shifts length": function () {
+				assert(true);
+				return;
+
+				var vm = new viewModel();
+
+				var data = [
+					{
+						PersonId: 1,
+						Projection: [
+							{
+								Start: '2014-06-16 12:00',
+								Minutes: 60
+							}
+						]
+					}
+				];
+				vm.UpdateSchedules(data);
+
+				assert.equals(vm.TimeLine.StartMinutes(), 12 * 60);
+				assert.equals(vm.TimeLine.EndMinutes(), 13 * 60);
 			}
 
 		});
