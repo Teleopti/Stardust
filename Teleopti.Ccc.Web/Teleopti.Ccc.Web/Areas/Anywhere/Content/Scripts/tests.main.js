a6a6b58f4964ea3a246dc79ab1019571da8afdc6
Henke <henrik.andersson@teleopti.com>
2014-06-16 11:23:16 +0200

    added first basic test for person schedule view model

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
@@ -12,7 +12,9 @@ require([
 		'views/realtimeadherencesites/vm_test',
 		'views/realtimeadherenceteams/vm_test',
 		'views/realtimeadherenceagents/vm_test',
-		'views/realtimeadherenceteams/team_test'
+		'views/realtimeadherenceteams/team_test',
+		'views/personschedule/vm_test'
+
 ], function () {
 	for (var i = 0, j = arguments.length; i < j; i++) {
 		arguments[i]();
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/vm_test.js
@@ -0,0 +1,20 @@
+﻿
+define([
+    'buster',
+    'views/personschedule/vm'
+], function (
+    buster,
+    viewModel
+    ) {
+	return function () {
+
+		buster.testCase("personschedule viewmodel", {
+			"should create viewmodel": function () {
+				var vm = new viewModel();
+				assert(vm);
+			}
+
+		});
+
+	};
+});
