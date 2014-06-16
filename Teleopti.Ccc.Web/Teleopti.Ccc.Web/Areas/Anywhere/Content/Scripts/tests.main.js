ef7363056f66cdc18919f157f8db5d56ab62980e
Henke <henrik.andersson@teleopti.com>
2014-06-16 14:52:54 +0200

    added ability to use buster js test fixtures query string to select which tests to run

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/tests.main.js
@@ -7,18 +7,21 @@ define('window', {
 	locationReplace: function () { },
 });
 
-require([
-		'tests',
+var testCases = [
+	'tests',
 		'views/personschedule/vm_test',
-		'views/realtimeadherencesites/vm_test',
-		'views/realtimeadherenceteams/vm_test',
-		'views/realtimeadherenceagents/vm_test',
-		'views/realtimeadherenceteams/team_test',
+	'views/realtimeadherencesites/vm_test',
+	'views/realtimeadherenceteams/vm_test',
+	'views/realtimeadherenceagents/vm_test',
+	'views/realtimeadherenceteams/team_test',
 		'views/personschedule/vm_test',
+];
 		'helpers_test'
 
-], function () {
+require(testCases, function () {
+	var query = location.search.substring(1);
 	for (var i = 0, j = arguments.length; i < j; i++) {
-		arguments[i]();
+		if (query == '' || testCases[i].indexOf(query) > 0)
+			arguments[i]();
 	}
 });
