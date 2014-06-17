a444498ed3e3dc16115f8815909d09b4d845d744
Henke <henrik.andersson@teleopti.com>
2014-06-17 15:07:15 +0200

    #28367 added test to expose bug

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/personschedule/vm_test.js
@@ -51,6 +51,48 @@
 					done();
 				},2);
 			},
+
+			"should not consider nightshifts from yesterday when creating timeline" : function(done) {
+				//ignored until we can use TimeFormatForMoment: "HH:mm",
+				done();
+				assert(true);
+				return;
+				var vm = new viewModel();
+
+				vm.SetViewOptions({
+					id: 1,
+					date: '20140616'
+				});
+				var data = [
+					{
+						PersonId: 1,
+						Projection: [
+							{
+								Start: '2014-06-16 12:00',
+								Minutes: 60
+							}
+						]
+					},
+					{
+						PersonId: 1,
+						Projection: [
+							{
+								Start: '2014-06-15 22:00',
+								Minutes: 180
+							}
+						]
+					}
+				];
+
+				vm.UpdateSchedules(data);
+
+				setTimeout(function () {
+					assert.equals(vm.TimeLine.StartTime(), "12:00");
+					assert.equals(vm.TimeLine.EndTime(), "13:00");
+					done();
+				}, 2);
+
+			},
 			
 			"should get the selected layer from url": function () {
 
