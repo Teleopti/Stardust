468ea97e2fad37e563258e5eb5584623f30a2ae0
Roger Kratz <roger.kratz@gmail.com>
2014-06-25 11:28:30 +0200

    simplified integration with autofac and mbcache
    starting with rulesetmodule

--- Teleopti.Ccc.IocCommon/Configuration/AutofacMbCacheIntegration.cs
+++ Teleopti.Ccc.IocCommon/Configuration/AutofacMbCacheIntegration.cs
@@ -14,6 +14,16 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 		public static IRegistrationBuilder<T, ConcreteReflectionActivatorData, SingleRegistrationStyle> IntegrateWithMbCache<T>(this IRegistrationBuilder<T, ConcreteReflectionActivatorData, SingleRegistrationStyle> builder) where T : class
 		{
 			return builder.OnActivating(e => e.ReplaceInstance(e.Context.Resolve<IMbCacheFactory>().ToCachedComponent(e.Instance)));
-		} 
+		}
+
+
+		public static IRegistrationBuilder<TInterface, SimpleActivatorData, SingleRegistrationStyle> RegisterMbCacheComponent<TClass, TInterface>(this ContainerBuilder containerBuilder)
+			where TInterface : class
+			where TClass : TInterface
+		{
+			containerBuilder.RegisterType<TClass>().AsSelf();
+			return containerBuilder.Register<TInterface>(c => c.Resolve<TClass>())
+				.OnActivating(e => e.ReplaceInstance(e.Context.Resolve<IMbCacheFactory>().ToCachedComponent(e.Instance)));
+		}
 	}
 }
