3a6451e025abb43311b2428ddb900d56535075eb
Erik Sundberg <none@none>
2014-06-27 11:24:38 +0200

    fixing buster for performance tool and js tests

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/main.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/main.js
@@ -1,29 +1,5 @@
 
-require.config({
-
-	paths: {
-	    jquery: '../../../../Content/jquery/jquery-1.10.2',
-		knockout: '../../../../Content/Scripts/knockout-2.2.1',
-		modernizr: '../../../../Content/modernizr/modernizr-2.6.2.min',
-		moment: '../../../../Content/moment/moment',
-		bootstrap: '../../../../Content/bootstrap/Scripts/bootstrap.min',
-		signalr: '../../../../Content/signalr/jquery.signalR-1.2.0.min',
-		
-		noext: '../../../../Content/require/noext',
-		signalrrr: 'require/signalrrr',
-		
-		templates: '../templates',
-		
-		text: '../../../../Content/require/text'
-
-	},
-
-	// dependencies that requires loading order
-	shim: {
-		'bootstrap': ['jquery'],
-		'signalr': ['jquery']
-	}
-});
+require.config(requireconfiguration);
 
 require([
         'jquery',
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/require/configuration.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/PerformanceTool/Content/Scripts/require/configuration.js
@@ -1,15 +1,27 @@
 ﻿var requireconfiguration = {
 	paths: {
 		jquery: '../../../../Content/jquery/jquery-1.10.2',
-		buster: '../../../../Content/busterjs/buster-test',
 		knockout: '../../../../Content/Scripts/knockout-2.2.1',
+		modernizr: '../../../../Content/modernizr/modernizr-2.6.2.min',
+		moment: '../../../../Content/moment/moment',
+		bootstrap: '../../../../Content/bootstrap/Scripts/bootstrap.min',
+		signalr: '../../../../Content/signalr/jquery.signalR-1.2.0.min',
+		buster: '../../../../Content/busterjs/buster-test',
+
+		noext: '../../../../Content/require/noext',
+		signalrrr: 'require/signalrrr',
+
+		templates: '../templates',
+
+		text: '../../../../Content/require/text',
 	},
-	
+
 	// dependencies that requires loading order
 	shim: {
+		'bootstrap': ['jquery'],
+		'signalr': ['jquery'],
 		'buster': {
 			exports: 'buster'
-		},
-		'knockout': ['jquery'],
+		}
 	}
 };
