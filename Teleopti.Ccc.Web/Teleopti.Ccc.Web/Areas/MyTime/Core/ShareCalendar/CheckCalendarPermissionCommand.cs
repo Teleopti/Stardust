516f93bed0d125d965d47b22227a91aee8c0621c
Robin Karlsson <robin.karlsson@teleopti.com>
2014-06-30 11:53:36 +0200

    Getting the current unit of work for call to role to principal command
    --HG--
    branch : Release 7.5.390

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/ShareCalendar/CheckCalendarPermissionCommand.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/ShareCalendar/CheckCalendarPermissionCommand.cs
@@ -22,7 +22,7 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.ShareCalendar
         public void Execute(IDataSource dataSource, IPerson person, IPersonRepository personRepository)
         {
             var principal = _principalFactory.MakePrincipal(person, dataSource, null);
-            _roleToPrincipalCommand.Execute(principal, dataSource.Application, personRepository);
+            _roleToPrincipalCommand.Execute(principal, dataSource.Application.CurrentUnitOfWork(), personRepository);
             var permission = _principalAuthorizationFactory.FromPrincipal(principal);
 
             if (!permission.IsPermitted(DefinedRaptorApplicationFunctionPaths.ShareCalendar))
