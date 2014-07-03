769a4d32df00dcf123d844c41ac6406a878742a3
Roger Kratz <roger.kratz@gmail.com>
2014-07-03 17:34:14 +0200

    adding toggleQuerier

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/toggleQuerier.js
@@ -0,0 +1,16 @@
+﻿define([
+	'ajax'
+], function (ajax) {
+	return function (toggleName, enabledCallback, disabledCallback) {
+		ajax.ajax({
+			url: 'ToggleHandler/IsEnabled?toggle=' + toggleName,
+			success: function (data) {
+				if (data.IsEnabled) {
+					enabledCallback();
+				} else {
+					disabledCallback();
+				}
+			}
+		});
+	}
+});
