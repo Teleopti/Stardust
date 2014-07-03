e5bfb09fe25d3c04feda8fa527d3c822c7c97b0e
David Jonsson <david.jonsson@teleopti.com>
2014-07-03 09:59:29 +0200

    bug #28912 - add logging to .txt-file
    --HG--
    branch : Release 7.5.390

--- ccnet/Azure/TeleoptiCCC/bin/RestartTeleopti.cmd
+++ ccnet/Azure/TeleoptiCCC/bin/RestartTeleopti.cmd
@@ -2,4 +2,4 @@ SET FILE=%~n0
 SET DIRECTORY=%~dp0
 SET DIRECTORY=%DIRECTORY:~0,-1%
 powershell set-executionpolicy unrestricted
-powershell ". %DIRECTORY%\..\Tools\SupportTools\StartStopSystem\RestartTeleopti.ps1; main"
+powershell ". %DIRECTORY%\..\Tools\SupportTools\StartStopSystem\RestartTeleopti.ps1; main" >"%DIRECTORY%\RestartTeleopti.log"
