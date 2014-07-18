0378cf6dfc1c94ee52f71763c018f98b533a700f
Tamas Balog <tamas.balogh@teleopti.com>
2014-07-18 11:46:39 +0200

    add test addtional info view

---
+++ Win/TestAdditionalInfoView.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Teleopti.Ccc.Win.Scheduling
+{
+	partial class TestAdditionalInfoView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1302:DoNotHardcodeLocaleSpecificStrings", MessageId = "Start menu")]
+		private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestAdditionalInfoView));
+			this.ribbonControlAdv1 = new Syncfusion.Windows.Forms.Tools.RibbonControlAdvFixed();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.labelTestInfoHeader = new System.Windows.Forms.Label();
+			this.textBoxTestInfoText = new System.Windows.Forms.TextBox();
+			this.buttonOk = new Syncfusion.Windows.Forms.ButtonAdv();
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControlAdv1)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// ribbonControlAdv1
+			// 
+			this.ribbonControlAdv1.HideMenuButtonToolTip = false;
+			this.ribbonControlAdv1.Location = new System.Drawing.Point(1, 0);
+			this.ribbonControlAdv1.MaximizeToolTip = "Maximize Ribbon";
+			this.ribbonControlAdv1.MenuButtonEnabled = true;
+			this.ribbonControlAdv1.MenuButtonFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ribbonControlAdv1.MenuButtonText = "";
+			this.ribbonControlAdv1.MenuButtonVisible = false;
+			this.ribbonControlAdv1.MenuColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(114)))), ((int)(((byte)(198)))));
+			this.ribbonControlAdv1.MinimizeToolTip = "Minimize Ribbon";
+			this.ribbonControlAdv1.Name = "ribbonControlAdv1";
+			this.ribbonControlAdv1.Office2013ColorScheme = Syncfusion.Windows.Forms.Tools.Office2013ColorScheme.White;
+			// 
+			// ribbonControlAdv1.OfficeMenu
+			// 
+			this.ribbonControlAdv1.OfficeMenu.Name = "OfficeMenu";
+			this.ribbonControlAdv1.OfficeMenu.Size = new System.Drawing.Size(12, 65);
+			this.ribbonControlAdv1.OverFlowButtonToolTip = "Show DropDown";
+			this.ribbonControlAdv1.QuickPanelImageLayout = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ribbonControlAdv1.QuickPanelVisible = false;
+			this.ribbonControlAdv1.RibbonHeaderImage = Syncfusion.Windows.Forms.Tools.RibbonHeaderImage.None;
+			this.ribbonControlAdv1.SelectedTab = null;
+			this.ribbonControlAdv1.Show2010CustomizeQuickItemDialog = false;
+			this.ribbonControlAdv1.ShowRibbonDisplayOptionButton = true;
+			this.ribbonControlAdv1.Size = new System.Drawing.Size(656, 33);
+			this.ribbonControlAdv1.SystemText.QuickAccessDialogDropDownName = "Start menu";
+			this.ribbonControlAdv1.TabIndex = 29;
+			this.ribbonControlAdv1.TitleColor = System.Drawing.Color.Black;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.buttonOk, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.labelTestInfoHeader, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.textBoxTestInfoText, 0, 1);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(170, 164);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 251);
+			this.tableLayoutPanel1.TabIndex = 30;
+			// 
+			// labelTestInfoHeader
+			// 
+			this.labelTestInfoHeader.AutoSize = true;
+			this.labelTestInfoHeader.Location = new System.Drawing.Point(3, 0);
+			this.labelTestInfoHeader.Name = "labelTestInfoHeader";
+			this.labelTestInfoHeader.Size = new System.Drawing.Size(35, 13);
+			this.labelTestInfoHeader.TabIndex = 0;
+			this.labelTestInfoHeader.Text = "label1";
+			// 
+			// textBoxTestInfoText
+			// 
+			this.textBoxTestInfoText.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBoxTestInfoText.Location = new System.Drawing.Point(3, 53);
+			this.textBoxTestInfoText.Multiline = true;
+			this.textBoxTestInfoText.Name = "textBoxTestInfoText";
+			this.textBoxTestInfoText.Size = new System.Drawing.Size(194, 157);
+			this.textBoxTestInfoText.TabIndex = 1;
+			// 
+			// buttonOk
+			// 
+			this.buttonOk.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
+			this.buttonOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+			this.buttonOk.BeforeTouchSize = new System.Drawing.Size(75, 23);
+			this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.buttonOk.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonOk.ForeColor = System.Drawing.Color.White;
+			this.buttonOk.IsBackStageButton = false;
+			this.buttonOk.Location = new System.Drawing.Point(3, 216);
+			this.buttonOk.Name = "buttonOk";
+			this.buttonOk.Office2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Managed;
+			this.buttonOk.Size = new System.Drawing.Size(75, 23);
+			this.buttonOk.TabIndex = 31;
+			this.buttonOk.Text = "xxOk";
+			this.buttonOk.UseVisualStyle = true;
+			// 
+			// TestAdditionalInfoView
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(658, 527);
+			this.ControlBox = false;
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.ribbonControlAdv1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "TestAdditionalInfoView";
+			this.Text = "Additional testing information";
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControlAdv1)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+		private Syncfusion.Windows.Forms.Tools.RibbonControlAdvFixed ribbonControlAdv1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label labelTestInfoHeader;
+		private System.Windows.Forms.TextBox textBoxTestInfoText;
+		private Syncfusion.Windows.Forms.ButtonAdv buttonOk;
+
+    }
+}
---
+++ Win/TestAdditionalInfoView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Syncfusion.Drawing;
+using Teleopti.Ccc.Win.Common;
+using Teleopti.Ccc.WinCode.Common.GuiHelpers;
+using Teleopti.Ccc.WinCode.Scheduling;
+
+namespace Teleopti.Ccc.Win.Scheduling
+{
+	public partial class TestAdditionalInfoView : BaseRibbonForm
+	{
+
+		public TestAdditionalInfoView()
+		{
+			InitializeComponent();
+			if (!DesignMode)
+				SetTexts();
+		}
+
+		public void CloseForm()
+		{
+			Close();
+		}
+	}
+}
