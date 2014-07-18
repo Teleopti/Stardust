a667a4c4cd9158630bcf2eb4b18a6209e820882f
blandine <none@none>
2014-07-18 14:35:25 +0200

    Fix #28886 : Agents row should be colored with alarm color or leave white.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/helpers.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/helpers.js
@@ -7,36 +7,36 @@ define(
         resources
     ) {
 
-    	return {
-    		TextColor: {
-    			BasedOnBackgroundColor: function (backgroundColor) {
-    				backgroundColor = backgroundColor.slice(backgroundColor.indexOf('(') + 1, backgroundColor.indexOf(')'));
+        return {
+            TextColor: {
+                BasedOnBackgroundColor: function (backgroundColor) {
+                    backgroundColor = backgroundColor.slice(backgroundColor.indexOf('(') + 1, backgroundColor.indexOf(')'));
 
-    				var backgroundColorArr = backgroundColor.split(',');
+                    var backgroundColorArr = backgroundColor.split(',');
 
-    				var brightness = backgroundColorArr[0] * 0.299 + backgroundColorArr[1] * 0.587 + backgroundColorArr[2] * 0.114;
+                    var brightness = backgroundColorArr[0] * 0.299 + backgroundColorArr[1] * 0.587 + backgroundColorArr[2] * 0.114;
 
-    				return brightness < 100 ? 'rgb(255,255,255)' : 'rgb(0,0,0)';
-    			},
-    			HexToRgb: function(hex) {
-    				var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    				var rgb= result ? {
-    					r: parseInt(result[1], 16),
-    					g: parseInt(result[2], 16),
-    					b: parseInt(result[3], 16)
-    				} : null;
-    				if (rgb)
-    					return "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
-    				return rgb;
-    			}
-    		},
-    		Minutes: {
-            	FromHours: function (hours) {
+                    return brightness < 100 ? 'rgb(255,255,255)' : 'rgb(0,0,0)';
+                },
+                HexToRgb: function(hex) {
+                    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+                    var rgb= result ? {
+                        r: parseInt(result[1], 16),
+                        g: parseInt(result[2], 16),
+                        b: parseInt(result[3], 16)
+                    } : null;
+                    if (rgb)
+                        return "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
+                    return rgb;
+                }
+            },
+            Minutes: {
+                FromHours: function (hours) {
                     return hours * 60;
                 },
 
                 ForHourOfDay: function (hour) {
-                	return hour * 60;
+                    return hour * 60;
                 },
 
                 StartOfHour: function(minutes) {
@@ -44,8 +44,8 @@ define(
                 },
 
                 EndOfHour: function (minutes) {
-	                var reminder = (minutes % 60);
-	                if (reminder == 0)
+                    var reminder = (minutes % 60);
+                    if (reminder == 0)
                         return minutes;
                     return minutes + (60 - reminder);
                 },
@@ -77,8 +77,8 @@ define(
                     return moment(date);
                 }
             },
-					timeFormatForUrl: function(time) {
-						return moment(time, "HH:mm").format("HHmm").toString();
-					}
-    	};
+                    timeFormatForUrl: function(time) {
+                        return moment(time, "HH:mm").format("HHmm").toString();
+                    }
+        };
     });
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/agentstate.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/agentstate.js
@@ -1,101 +1,105 @@
 ﻿define([
-	'resources',
-	'knockout'
+    'resources',
+    'knockout',
+    'helpers'
 ],
-    function (resources, ko
-		) {
-    	return function () {
+    function (resources, ko, helpers
+        ) {
+        return function () {
 
-    		var that = {};
-    		that.TimeInState = ko.observable();
-    		that.AlarmTime = ko.observable();
-    		that.State = ko.observable();
-    		that.Activity = ko.observable();
-    		that.NextActivity = ko.observable();
-    		that.NextActivityStartTime = ko.observable();
+            var that = {};
+            that.TimeInState = ko.observable();
+            that.AlarmTime = ko.observable();
+            that.State = ko.observable();
+            that.Activity = ko.observable();
+            that.NextActivity = ko.observable();
+            that.NextActivityStartTime = ko.observable();
 
-		    that.HaveNewAlarm = false;
-    		that.NextAlarm = '';
-		    that.NextAlarmColor = '';
+            that.HaveNewAlarm = false;
+            that.NextAlarm = '';
+            that.NextAlarmColor = '';
 
-		    that.Alarm = ko.observable();
-    		that.AlarmColor = ko.observable();
-    		that.AlarmStart = ko.observable();
+            that.Alarm = ko.observable();
+            that.AlarmColor = ko.observable();
+            that.AlarmStart = ko.observable();
 
-    		that.EnteredCurrentAlarm = ko.observable();
-    		that.TextWeight = ko.observable();
+            that.EnteredCurrentAlarm = ko.observable();
+            that.TextWeight = ko.observable();
+            that.TextColor = ko.observable();
 
-    		that.fill = function (data, name, offset, teamName) {
-    			that.PersonId = data.PersonId;
-			    that.Name = name;
-			    that.TeamName = teamName;
-    			that.State(data.State);
-				that.Activity(data.Activity);
-				that.NextActivity(data.NextActivity);
-				that.NextActivityStartTime(data.NextActivity && data.NextActivityStartTime ? that.getDateTimeFormat(moment.utc(data.NextActivityStartTime).add(offset, 'minutes')) : '');
+            that.fill = function (data, name, offset, teamName) {
+                that.PersonId = data.PersonId;
+                that.Name = name;
+                that.TeamName = teamName;
+                that.State(data.State);
+                that.Activity(data.Activity);
+                that.NextActivity(data.NextActivity);
+                that.NextActivityStartTime(data.NextActivity && data.NextActivityStartTime ? that.getDateTimeFormat(moment.utc(data.NextActivityStartTime).add(offset, 'minutes')) : '');
 
-				that.EnteredCurrentAlarm(data.StateStart ? moment.utc(data.StateStart).add(offset, 'minutes').format(resources.FixedDateTimeWithSecondsFormatForMoment) : '');
-				that.refreshAlarmTime();
+                that.EnteredCurrentAlarm(data.StateStart ? moment.utc(data.StateStart).add(offset, 'minutes').format(resources.FixedDateTimeWithSecondsFormatForMoment) : '');
+                that.refreshAlarmTime();
 
-				that.AlarmStart(data.AlarmStart ? moment.utc(data.AlarmStart).add(offset, 'minutes').format(resources.FixedDateTimeWithSecondsFormatForMoment) : '');
+                that.AlarmStart(data.AlarmStart ? moment.utc(data.AlarmStart).add(offset, 'minutes').format(resources.FixedDateTimeWithSecondsFormatForMoment) : '');
 
-				if (that.shouldWaitWithUpdatingAlarm()) {
-					that.HaveNewAlarm = true;
-			    	that.NextAlarm = data.Alarm;
-				    that.NextAlarmColor = data.AlarmColor;
-				    return;
-			    }
-    			that.Alarm(data.Alarm);
-				that.refreshColor(data.AlarmColor);
-    		};
-			
-    		that.refreshAlarmTime = function() {
-			    if (!that.EnteredCurrentAlarm()) return;
-			    var duration = moment.duration(((new Date).getTime() - moment(that.EnteredCurrentAlarm(), resources.FixedDateTimeWithSecondsFormatForMoment).toDate()));
-			    that.AlarmTime(Math.floor(duration.asHours()) + moment(duration.asMilliseconds()).format(":mm:ss"));
+                if (that.shouldWaitWithUpdatingAlarm()) {
+                    that.HaveNewAlarm = true;
+                    that.NextAlarm = data.Alarm;
+                    that.NextAlarmColor = data.AlarmColor;
+                    return;
+                }
+                that.Alarm(data.Alarm);
+                that.refreshColor(data.AlarmColor);
+            };
+            
+            that.refreshAlarmTime = function() {
+                if (!that.EnteredCurrentAlarm()) return;
+                var duration = moment.duration(((new Date).getTime() - moment(that.EnteredCurrentAlarm(), resources.FixedDateTimeWithSecondsFormatForMoment).toDate()));
+                that.AlarmTime(Math.floor(duration.asHours()) + moment(duration.asMilliseconds()).format(":mm:ss"));
 
-			    if (!that.HaveNewAlarm
-					|| that.shouldWaitWithUpdatingAlarm()) return;
-			    that.Alarm(that.NextAlarm);
-			    that.refreshColor(that.NextAlarmColor);
-			    that.HaveNewAlarm = false;
-    		}
+                if (!that.HaveNewAlarm
+                    || that.shouldWaitWithUpdatingAlarm()) return;
+                that.Alarm(that.NextAlarm);
+                that.refreshColor(that.NextAlarmColor);
+                that.HaveNewAlarm = false;
+            }
 
-    		that.shouldWaitWithUpdatingAlarm = function () {
-    			return that.AlarmStart() && moment.utc().isBefore(that.AlarmStart());
-    		}
+            that.shouldWaitWithUpdatingAlarm = function () {
+                return that.AlarmStart() && moment.utc().isBefore(that.AlarmStart());
+            }
 
-		    that.refreshColor = function (newColor) {
-				if (that.color !== newColor) {
-					that.AlarmColor('rgba(' + that.hexToRgb(newColor) + ', 0.6)');
-					that.TextWeight(700);
-					setTimeout(function () {
-						that.TextWeight(500);
-					}, 4000);
-					that.color = newColor;
-				}
-				
-			}
+            that.refreshColor = function (newColor) {
+            	if (newColor !== undefined && newColor !== "#000000" && that.color !== newColor) {
+                    var rgb = that.hexToRgb(newColor);
+                    that.AlarmColor('rgba(' + rgb + ', 0.6)');
+                    that.TextColor(helpers.TextColor.BasedOnBackgroundColor('(' + rgb + ')'));
+                    that.TextWeight(700);
+                    setTimeout(function () {
+                        that.TextWeight(500);
+                    }, 4000);
+                    that.color = newColor;
+                }
+                
+            }
 
-    		that.hexToRgb = function (hex) {
-				
-			    hex = hex ? hex.substring(1) : 'ffffff';
-    			var bigint = parseInt(hex, 16);
-    			var r = (bigint >> 16) & 255;
-    			var g = (bigint >> 8) & 255;
-    			var b = bigint & 255;
+            that.hexToRgb = function (hex) {
+                
+                hex = hex ? hex.substring(1) : 'ffffff';
+                var bigint = parseInt(hex, 16);
+                var r = (bigint >> 16) & 255;
+                var g = (bigint >> 8) & 255;
+                var b = bigint & 255;
 
-    			return r + "," + g + "," + b;
-    		}
+                return r + "," + g + "," + b;
+            }
 
-			that.getDateTimeFormat = function(time) {
-				if (time.date() > moment().date()) {
-					return time.format(resources.FixedDateTimeWithSecondsFormatForMoment);
-				}
-				return time.format(resources.TimeFormatForMoment);
-			}
+            that.getDateTimeFormat = function(time) {
+                if (time.date() > moment().date()) {
+                    return time.format(resources.FixedDateTimeWithSecondsFormatForMoment);
+                }
+                return time.format(resources.TimeFormatForMoment);
+            }
 
-    		return that;
-    	};
+            return that;
+        };
     }
 );
