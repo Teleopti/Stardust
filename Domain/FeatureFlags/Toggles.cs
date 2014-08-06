fa5219eae483e8332790c2a67eefda2e86f3a8d5
Xianwei Shen <xianwei.shen@teleopti.com>
2014-08-06 13:31:10 +0200

    add new toggle for monitor multiple bu

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -20,6 +20,7 @@
 
 		RTA_RtaLastStatesOverview_27789,
 		RTA_DrilldownToAllAgentsInOneTeam_25234,
+		RTA_MonitorMultipleBusinessUnits_28348,
 
 		MyTeam_MoveActivity_25206,
 		MyTeam_StaffingMetrics_25562,
