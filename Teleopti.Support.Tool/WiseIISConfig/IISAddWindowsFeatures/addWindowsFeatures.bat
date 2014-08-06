12b78e0614079320a719f3d137bf1ba117d4fc52
David Jonsson <davidj@teleopti.com>
2014-08-06 16:53:02 +0200

    bug #29271 - Make work on Win2008R2 + Win7. not very well tested ...

--- Teleopti.Support.Tool/WiseIISConfig/IISAddWindowsFeatures/addWindowsFeatures.bat
+++ Teleopti.Support.Tool/WiseIISConfig/IISAddWindowsFeatures/addWindowsFeatures.bat
@@ -2,10 +2,6 @@
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
 
-set /A WindowsVista_WindowsServer2008=260
-set /A Windows7=261
-
-
 For /f "tokens=2 delims=[]" %%G in ('ver') Do (set version=%%G)
 For /f "tokens=2,3,4 delims=. " %%G in ('echo %version%') Do (set /A major=%%G & set minor=%%H & set build=%%I) 
 
@@ -15,13 +11,16 @@ if %major% GEQ 6 (
 	if %minor% GEQ 2 call :configure_Win8_Win2012Server
 )
 
-GOTO :EOF
+GOTO :Finish
+
+:configure_Win7_Win2008ServerR2
+call "%ROOTDIR%\common\configure_Win7_Win2008ServerR2.bat"
+exit /b
 
 :configure_Win8_Win2012Server
 powershell set-executionpolicy -scope CurrentUser unrestricted
 powershell -file "%ROOTDIR%\common\configure_Win8_Win2012Server.ps1" 
 exit /b
 
-:configure_Win7_Win2008ServerR2
-"%ROOTDIR%\common\configure_Win7_Win2008ServerR2.bat"
-exit /b
+:Finish
+ping 127.0.0.1 -n 3 > NUL
