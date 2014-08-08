7a9dfa5cb9cf2584a2bd26d270feac9abec1e79c
Ola <none@none>
2014-08-08 09:02:15 +0200

    oops

--- Win/Scheduling/SkillSummary.Designer.cs
+++ Win/Scheduling/SkillSummary.Designer.cs
@@ -47,31 +47,33 @@
 			this.buttonAdvOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonAdvOk.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
 			this.buttonAdvOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-			this.buttonAdvOk.BeforeTouchSize = new System.Drawing.Size(87, 20);
+			this.buttonAdvOk.BeforeTouchSize = new System.Drawing.Size(87, 27);
 			this.buttonAdvOk.ForeColor = System.Drawing.Color.White;
 			this.buttonAdvOk.IsBackStageButton = false;
-			this.buttonAdvOk.Location = new System.Drawing.Point(119, 5);
+			this.buttonAdvOk.Location = new System.Drawing.Point(116, 13);
+			this.buttonAdvOk.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
 			this.buttonAdvOk.Name = "buttonAdvOk";
 			this.buttonAdvOk.Office2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Managed;
-			this.buttonAdvOk.Size = new System.Drawing.Size(87, 20);
+			this.buttonAdvOk.Size = new System.Drawing.Size(87, 27);
 			this.buttonAdvOk.TabIndex = 3;
 			this.buttonAdvOk.Text = "xxOk";
 			this.buttonAdvOk.UseVisualStyle = true;
-			this.buttonAdvOk.Click += new System.EventHandler(this.buttonAdvOk_Click);
+			this.buttonAdvOk.Click += new System.EventHandler(this.buttonAdvOkClick);
 			// 
 			// buttonAdvCancel
 			// 
 			this.buttonAdvCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonAdvCancel.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
 			this.buttonAdvCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-			this.buttonAdvCancel.BeforeTouchSize = new System.Drawing.Size(87, 20);
+			this.buttonAdvCancel.BeforeTouchSize = new System.Drawing.Size(87, 27);
 			this.buttonAdvCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.buttonAdvCancel.ForeColor = System.Drawing.Color.White;
 			this.buttonAdvCancel.IsBackStageButton = false;
-			this.buttonAdvCancel.Location = new System.Drawing.Point(223, 5);
+			this.buttonAdvCancel.Location = new System.Drawing.Point(222, 13);
+			this.buttonAdvCancel.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
 			this.buttonAdvCancel.Name = "buttonAdvCancel";
 			this.buttonAdvCancel.Office2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Managed;
-			this.buttonAdvCancel.Size = new System.Drawing.Size(87, 20);
+			this.buttonAdvCancel.Size = new System.Drawing.Size(87, 27);
 			this.buttonAdvCancel.TabIndex = 4;
 			this.buttonAdvCancel.Text = "xxCancel";
 			this.buttonAdvCancel.UseVisualStyle = true;
@@ -80,9 +82,9 @@
 			// 
 			this.labelSkill.AutoSize = true;
 			this.labelSkill.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.labelSkill.Location = new System.Drawing.Point(3, 39);
+			this.labelSkill.Location = new System.Drawing.Point(3, 40);
 			this.labelSkill.Name = "labelSkill";
-			this.labelSkill.Size = new System.Drawing.Size(160, 200);
+			this.labelSkill.Size = new System.Drawing.Size(160, 177);
 			this.labelSkill.TabIndex = 6;
 			this.labelSkill.Text = "xxSkills";
 			// 
@@ -92,7 +94,7 @@
 			this.labelSummeryName.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.labelSummeryName.Location = new System.Drawing.Point(3, 0);
 			this.labelSummeryName.Name = "labelSummeryName";
-			this.labelSummeryName.Size = new System.Drawing.Size(160, 39);
+			this.labelSummeryName.Size = new System.Drawing.Size(160, 40);
 			this.labelSummeryName.TabIndex = 5;
 			this.labelSummeryName.Text = "xxSummeryName";
 			this.labelSummeryName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -102,15 +104,15 @@
 			this.checkedListBoxSkill.CheckOnClick = true;
 			this.checkedListBoxSkill.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.checkedListBoxSkill.FormattingEnabled = true;
-			this.checkedListBoxSkill.Location = new System.Drawing.Point(169, 42);
+			this.checkedListBoxSkill.Location = new System.Drawing.Point(169, 43);
 			this.checkedListBoxSkill.Name = "checkedListBoxSkill";
-			this.checkedListBoxSkill.Size = new System.Drawing.Size(313, 194);
+			this.checkedListBoxSkill.Size = new System.Drawing.Size(313, 171);
 			this.checkedListBoxSkill.TabIndex = 2;
 			// 
 			// textBoxSummeryName
 			// 
 			this.textBoxSummeryName.BackColor = System.Drawing.Color.White;
-			this.textBoxSummeryName.BeforeTouchSize = new System.Drawing.Size(313, 22);
+			this.textBoxSummeryName.BeforeTouchSize = new System.Drawing.Size(313, 23);
 			this.textBoxSummeryName.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
 			this.textBoxSummeryName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.textBoxSummeryName.Cursor = System.Windows.Forms.Cursors.IBeam;
@@ -119,7 +121,7 @@
 			this.textBoxSummeryName.MaxLength = 45;
 			this.textBoxSummeryName.Metrocolor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
 			this.textBoxSummeryName.Name = "textBoxSummeryName";
-			this.textBoxSummeryName.Size = new System.Drawing.Size(313, 22);
+			this.textBoxSummeryName.Size = new System.Drawing.Size(313, 23);
 			this.textBoxSummeryName.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Metro;
 			this.textBoxSummeryName.TabIndex = 1;
 			// 
@@ -133,14 +135,16 @@
 			this.tableLayoutPanel2.Controls.Add(this.textBoxSummeryName, 1, 0);
 			this.tableLayoutPanel2.Controls.Add(this.checkedListBoxSkill, 1, 1);
 			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 1, 2);
-			this.tableLayoutPanel2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(18, 55);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 3;
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.38418F));
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.61582F));
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(485, 273);
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(485, 267);
 			this.tableLayoutPanel2.TabIndex = 8;
 			// 
 			// tableLayoutPanel1
@@ -151,12 +155,13 @@
 			this.tableLayoutPanel1.Controls.Add(this.buttonAdvOk, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.buttonAdvCancel, 1, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(169, 242);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(166, 217);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 1;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(313, 28);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(319, 50);
 			this.tableLayoutPanel1.TabIndex = 7;
 			// 
 			// SkillSummary
@@ -165,11 +170,17 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
+			this.BorderColor = System.Drawing.Color.Blue;
 			this.CancelButton = this.buttonAdvCancel;
-			this.ClientSize = new System.Drawing.Size(634, 450);
+			this.CaptionFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ClientSize = new System.Drawing.Size(485, 267);
 			this.Controls.Add(this.tableLayoutPanel2);
 			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.HelpButton = false;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "SkillSummary";
+			this.ShowIcon = false;
 			this.Text = "xxSkillSummery";
 			((System.ComponentModel.ISupportInitialize)(this.textBoxSummeryName)).EndInit();
 			this.tableLayoutPanel2.ResumeLayout(false);
