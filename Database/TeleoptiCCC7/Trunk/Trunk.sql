aad8c07518b4334da48638476017fc21d62c2518
Chundan Xu <chundan.xu@teleopti.com>
2014-08-11 10:12:11 +0800

    PBI #28913 fix merge problem: add new column LastCalculatedDate to badge table by altering.

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -1,2 +1,12 @@
-	[BadgeType] [int] NOT NULL,
-	[LastCalculatedDate] [datetime] NOT NULL
+--Name: Xinfeng, Chundan
+--Date: 2014-08-11  
+--Desc: Add new column for agent badges
+----------------  
+ALTER TABLE [dbo].[AgentBadge] ADD [LastCalculatedDate] [datetime] NULL
+GO
+UPDATE [dbo].[AgentBadge]
+SET [LastCalculatedDate] = '2000-01-01'
+GO
+ALTER TABLE [dbo].[AgentBadge] ALTER COLUMN [LastCalculatedDate] [datetime] NOT NULL
+GO
+
