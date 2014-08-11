f0db098c2ecd9698eb699bab1fad3bf0cb7a3cbf
Xianwei Shen <xianwei.shen@teleopti.com>
2014-08-11 09:55:54 +0200

    added multiple business units aspect

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Aop/Aspects/MultipleBusinessUnitsAspect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Web.Core.Aop.Core;
+using Teleopti.Ccc.Web.Core.RequestContext;
+
+namespace Teleopti.Ccc.Web.Core.Aop.Aspects
+{
+    public class MultipleBusinessUnitsAspect : IAspect
+    {
+        private readonly IBusinessUnitFilterOverrider _overrider;
+        private readonly ICurrentHttpContext _context;
+        private IDisposable _disposable;
+
+        public MultipleBusinessUnitsAspect(IBusinessUnitFilterOverrider overrider, ICurrentHttpContext context)
+        {
+            _overrider = overrider;
+            _context = context;
+        }
+
+        public void OnBeforeInvokation()
+        {
+            var value = _context.Current().Request.Headers["x-override-business-unit-filter"];
+            var id = Guid.Parse(value);
+            _disposable = _overrider.OverrideWith(id);
+        }
+
+        public void OnAfterInvokation()
+        {
+            _disposable.Dispose();
+            _disposable = null;
+        }
+
+    }
+
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Aop/Aspects/MultipleBusinessUnitsAspectTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Web;
+using MvcContrib.TestHelper.Fakes;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Web.Core.Aop.Aspects;
+using Teleopti.Ccc.Web.Core.RequestContext;
+
+namespace Teleopti.Ccc.WebTest.Core.Aop.Aspects
+{
+    [TestFixture]
+    public class MultipleBusinessUnitsAspectTest
+    {
+        [Test]
+        public void ShouldHaveAttribute()
+        {
+            var target = new MultipleBusinessUnitsAttribute();
+            target.Should().Be.AssignableTo<Attribute>();
+            target.AspectType.Should().Be(typeof (MultipleBusinessUnitsAspect));
+        }
+
+        [Test]
+        public void ShouldChangeBusinessUnitIdFromHttpContextBeforeInvokation()
+        {
+            var businessUnitFilterOverrider = MockRepository.GenerateMock<IBusinessUnitFilterOverrider>();
+            var httpContext = new FakeHttpContext("http://example.com", null);
+            var request = MockRepository.GenerateStub<FakeHttpRequest>("/", new Uri("http://localhost/"), new Uri("http://localhost/"));
+            var guid = Guid.NewGuid();
+            request.Stub(x => x.Headers).Return(new NameValueCollection() { { "x-override-business-unit-filter", guid.ToString() } });
+            httpContext.SetRequest(request);
+            var currentHttpContext = new FakeCurrentHttpContext(httpContext);
+            var target = new MultipleBusinessUnitsAspect(businessUnitFilterOverrider, currentHttpContext);
+
+            target.OnBeforeInvokation();
+
+            businessUnitFilterOverrider.AssertWasCalled(x => x.OverrideWith(guid));
+        }
+    }
+}
