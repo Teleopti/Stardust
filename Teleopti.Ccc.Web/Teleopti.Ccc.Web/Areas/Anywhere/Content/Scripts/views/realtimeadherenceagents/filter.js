9470e05375bfedc11f25f1d251f9f31916fba150
Erik Sundberg <none@none>
2014-08-13 09:19:02 +0200

    PBI #29358 Sane useage of negate and qoutes

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/filter.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/filter.js
@@ -1,54 +1,63 @@
 ﻿define([
 		'knockout'
 ],
-    function (ko) {
-	    return function() {
-		    var that = {};
+	function(ko) {
+		return function() {
+			var that = {};
 
-		    that.match = function(items, filter) {
-			    var matchedItems = 0;
-			    var filterWords = mapOutFilterWords(filter);
-			    if (!filterWords) {
-				    return false;
-			    }
+			that.match = function(items, filter) {
+				var matchedItems = 0;
+				var filterWords = mapOutFilterWords(filter);
+				if (!filterWords) {
+					return false;
+				}
 
-			    for (var i = 0; i < items.length; i++) {
-				    var item = items[i];
-				    if (!item) {
-					    continue;
-				    }
-				    for (var wordIter = 0; wordIter < filterWords.length; wordIter++) {
-					    var filterWord = filterWords[wordIter];
+				for (var i = 0; i < items.length; i++) {
+					var item = items[i];
+					if (!item) {
+						continue;
+					}
 
-					    if (shouldNegate(filterWord) && stringContains(item, filterWord.slice(1))) {
-						    return false;
-					    }
-					    if (stringContains(item, filterWord)) {
-						    matchedItems++;
-					    }
-					    if (shouldMatchExact(filterWord) && item.toUpperCase() === removeQuotes(filterWord).toUpperCase()) {
-						    matchedItems++;
-					    }
-				    }
+					for (var wordIter = 0; wordIter < filterWords.length; wordIter++) {
+						var filterWord = filterWords[wordIter];
 
-			    }
-			    var unNegatedFilterWords = ko.utils.arrayFilter(filterWords, function(word) { return !shouldNegate(word); }).length;
-			    if (matchedItems === unNegatedFilterWords) {
-				    return true;
-			    }
-			    return false;
-		    };
+						if (shouldNegate(filterWord)) {
+							var filterWordWithoutExlamationMark = filterWord.slice(1);
+							if (shouldMatchExact(filterWordWithoutExlamationMark) &&
+								stringContains(item, removeQuotes(filterWordWithoutExlamationMark))) {
+								return false;
+							}
 
-		    var mapOutFilterWords = function(rawInput) { return rawInput.match(/([!]?\w+)|(?:["'](.*?)["'])/g); }
-		    var shouldNegate = function(word) { return word.indexOf("!") === 0; }
-		    var shouldMatchExact = function(word) { return word.indexOf("'") === 0; }
-		    var removeQuotes = function(word) { return word.slice(1, word.length - 1); }
+							if (stringContains(item, filterWord.slice(1))) {
+								return false;
+							}
+						}
+						if (shouldMatchExact(filterWord) && item.toUpperCase() === removeQuotes(filterWord).toUpperCase()) {
+							matchedItems++;
+						}
+						if (stringContains(item, filterWord)) {
+							matchedItems++;
+						}
+					}
 
-		    var stringContains = function(item, filter) {
-			    return item.toUpperCase().indexOf(filter.toUpperCase()) > -1;
-		    }
+				}
+				var unNegatedFilterWords = ko.utils.arrayFilter(filterWords, function(word) { return !shouldNegate(word); }).length;
+				if (matchedItems === unNegatedFilterWords) {
+					return true;
+				}
+				return false;
+			};
 
-		    return that;
-	    };
-    }
+			var mapOutFilterWords = function(rawInput) { return rawInput.match(/([!]*\w+)|(?:[!"']{1,2}(.*?)["'])/g); }
+			var shouldNegate = function(word) { return word.indexOf("!") === 0; }
+			var shouldMatchExact = function(word) { return word.indexOf("'") === 0; }
+			var removeQuotes = function(word) { return word.slice(1, word.length - 1); }
+
+			var stringContains = function(item, filter) {
+				return item.toUpperCase().indexOf(filter.toUpperCase()) > -1;
+			}
+
+			return that;
+		};
+	}
 );
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/vm_test.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/realtimeadherenceagents/vm_test.js
@@ -204,6 +204,19 @@
 				vm.filter("'Ready'");
 				assert.equals(vm.filteredAgents().length, 1);
 				assert.equals(vm.filteredAgents()[0].State(), "Ready");
+			},
+			"should only display agents that does not match negating filter with quoted searchwords": function() {
+				var agent1 = { PersonId: "guid1", Name: "Kurt", TimeZoneOffsetMinutes: 0 },
+					agent2 = { PersonId: "guid2", Name: "Glen", TimeZoneOffsetMinutes: 0 },
+					agent1State = { PersonId: "guid1", Alarm: "Positive" },
+					agent2State = { PersonId: "guid2", Alarm: "In adherence" };
+
+				var vm = viewModel();
+				vm.fillAgents([agent1, agent2]);
+				vm.fillAgentsStates([agent1State, agent2State]);
+				vm.filter("!'In adherence'");
+				assert.equals(vm.filteredAgents().length, 1);
+				assert.equals(vm.filteredAgents()[0].Name, "Kurt");
 			}
 		});
 	};
