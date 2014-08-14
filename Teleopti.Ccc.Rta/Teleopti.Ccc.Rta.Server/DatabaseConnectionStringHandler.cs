a1e62c5fc91bc2c0edc64699186ad51d032351cc
Erik Sundberg <none@none>
2014-08-14 16:18:30 +0200

    Fixing build

--- Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.Server/DatabaseConnectionStringHandler.cs
+++ Teleopti.Ccc.Rta/Teleopti.Ccc.Rta.Server/DatabaseConnectionStringHandler.cs
@@ -7,12 +7,16 @@ namespace Teleopti.Ccc.Rta.Server
 	{
 		public string AppConnectionString()
 		{
-			return ConfigurationManager.ConnectionStrings["RtaApplication"].ConnectionString;
+			return ConfigurationManager.ConnectionStrings["RtaApplication"] != null
+				? ConfigurationManager.ConnectionStrings["RtaApplication"].ConnectionString
+				: "";
 		}
 
 		public string DataStoreConnectionString()
 		{
-			return ConfigurationManager.ConnectionStrings["RtaAnalytics"].ConnectionString;
+			return ConfigurationManager.ConnectionStrings["RtaAnalytics"] != null
+				? ConfigurationManager.ConnectionStrings["RtaAnalytics"].ConnectionString
+				: "";
 		}
 	}
 }
