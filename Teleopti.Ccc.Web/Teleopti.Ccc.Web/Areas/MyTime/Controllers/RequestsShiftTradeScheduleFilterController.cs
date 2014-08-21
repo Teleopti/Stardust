bdf51e4223a31c583a59de97077860dfef828d74
Mingdi Wu <mingdi.wu@teleopti.com>
2014-08-21 12:32:51 +0800

    PBI #24560 make time format according to user culture setting

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/RequestsShiftTradeScheduleFilterController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/RequestsShiftTradeScheduleFilterController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Common.Mapping;
 using Teleopti.Ccc.Web.Filters;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 {
@@ -35,19 +37,31 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 	public class RequestsShiftTradeScheduleFilterViewModelFactory : IRequestsShiftTradeScheduleFilterViewModelFactory
 	{
 		private readonly IDayOffTemplateRepository _dayOffTemplateRepository;
+		private readonly ICreateHourText _createHourText;
+		private readonly IUserTimeZone _userTimeZone;
 
-		public RequestsShiftTradeScheduleFilterViewModelFactory(IDayOffTemplateRepository dayOffTemplateRepository)
+		public RequestsShiftTradeScheduleFilterViewModelFactory(IDayOffTemplateRepository dayOffTemplateRepository, 
+																			ICreateHourText createHourText, IUserTimeZone timeZone)
 		{
 			_dayOffTemplateRepository = dayOffTemplateRepository;
+			_createHourText = createHourText;
+			_userTimeZone = timeZone;
 		}
 
 		public RequestsShiftTradeScheduleFilterViewModel ViewModel()
 		{
+			IList<DateTime> hours = new List<DateTime>();
+			for (var i = 0; i <= 24; ++i)
+			{
+				hours.Add(TimeZoneHelper.ConvertToUtc(DateTime.Today, _userTimeZone.TimeZone()).AddHours(i));
+			}
 			var ret = new RequestsShiftTradeScheduleFilterViewModel
 			{
 				DayOffShortNames =
-					from t in _dayOffTemplateRepository.FindAllDayOffsSortByDescription()
-					select t.Description.ShortName
+					from dayoff in _dayOffTemplateRepository.FindAllDayOffsSortByDescription()
+					select dayoff.Description.ShortName,
+				HourTexts = from hour in hours
+								select _createHourText.CreateText(hour)
 			};
 
 			return ret;
@@ -57,5 +71,6 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 	public class RequestsShiftTradeScheduleFilterViewModel
 	{
 		public IEnumerable<string> DayOffShortNames { get; set; }
+		public IEnumerable<string> HourTexts { get; set; } 
 	}
 }
