7e0a2c5409254c0f2aa089accecf3c0b28eb13ca
Ola <none@none>
2014-09-04 16:09:46 +0200

    bug - system test issue #29994

--- Win/Intraday/IntradayViewContent.Designer.cs
+++ Win/Intraday/IntradayViewContent.Designer.cs
@@ -62,16 +62,13 @@ namespace Teleopti.Ccc.Win.Intraday
 			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbpanelSkillChart = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
 			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbpanelSkillGrid = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
 			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbelementHostShiftEditor = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
-			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbelementHostDayLayerView = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
 			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbelementHostPinnedLayerView = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
+			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbelementHostDayLayerView = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
 			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbelementHostAgentState = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
 			Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbelementHostStaffingEffect = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
-			Syncfusion.Windows.Forms.Chart.ChartSeries chartSeries1 = new Syncfusion.Windows.Forms.Chart.ChartSeries();
-			Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo chartCustomShapeInfo1 = new Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo();
-			Syncfusion.Windows.Forms.Chart.ChartLineInfo chartLineInfo1 = new Syncfusion.Windows.Forms.Chart.ChartLineInfo();
+			this.chartControlSkillData = new Syncfusion.Windows.Forms.Chart.ChartControl();
 			this.dockingManager1 = new Syncfusion.Windows.Forms.Tools.DockingManager(this.components);
 			this.panelSkillChart = new System.Windows.Forms.Panel();
-			this.chartControlSkillData = new Syncfusion.Windows.Forms.Chart.ChartControl();
 			this.panelSkillGrid = new System.Windows.Forms.Panel();
 			this.tabSkillData = new Syncfusion.Windows.Forms.Tools.TabControlAdv();
 			this.tabPageAdv1 = new Syncfusion.Windows.Forms.Tools.TabPageAdv();
@@ -86,7 +83,6 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.timerRefreshSchedule = new System.Windows.Forms.Timer(this.components);
 			this.backgroundWorkerFetchData = new System.ComponentModel.BackgroundWorker();
 			((System.ComponentModel.ISupportInitialize)(this.dockingManager1)).BeginInit();
-			this.panelSkillChart.SuspendLayout();
 			this.panelSkillGrid.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.tabSkillData)).BeginInit();
 			this.tabSkillData.SuspendLayout();
@@ -94,6 +90,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			// 
 			// dockingManager1
 			// 
+			this.dockingManager1.ActiveCaptionBackground = new Syncfusion.Drawing.BrushInfo(System.Drawing.SystemColors.ActiveCaption);
 			this.dockingManager1.ActiveCaptionFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
 			this.dockingManager1.AutoHideInterval = 5;
 			this.dockingManager1.AutoHideTabFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
@@ -134,15 +131,15 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.dockingManager1.SetEnableDocking(this.elementHostShiftEditor, true);
 			ccbelementHostShiftEditor.MergeWith(this.dockingManager1.CaptionButtons, false);
 			this.dockingManager1.SetCustomCaptionButtons(this.elementHostShiftEditor, ccbelementHostShiftEditor);
-			this.dockingManager1.SetDockLabel(this.elementHostDayLayerView, "elementHostDayLayerView");
-			this.dockingManager1.SetEnableDocking(this.elementHostDayLayerView, true);
-			ccbelementHostDayLayerView.MergeWith(this.dockingManager1.CaptionButtons, false);
-			this.dockingManager1.SetCustomCaptionButtons(this.elementHostDayLayerView, ccbelementHostDayLayerView);
 			this.dockingManager1.SetDockLabel(this.elementHostPinnedLayerView, "elementHostPinnedLayerView");
 			this.dockingManager1.SetEnableDocking(this.elementHostPinnedLayerView, true);
 			ccbelementHostPinnedLayerView.MergeWith(this.dockingManager1.CaptionButtons, false);
 			this.dockingManager1.SetCustomCaptionButtons(this.elementHostPinnedLayerView, ccbelementHostPinnedLayerView);
-			this.dockingManager1.SetDockLabel(this.elementHostAgentState, "elementHost1");
+			this.dockingManager1.SetDockLabel(this.elementHostDayLayerView, "elementHostDayLayerView");
+			this.dockingManager1.SetEnableDocking(this.elementHostDayLayerView, true);
+			ccbelementHostDayLayerView.MergeWith(this.dockingManager1.CaptionButtons, false);
+			this.dockingManager1.SetCustomCaptionButtons(this.elementHostDayLayerView, ccbelementHostDayLayerView);
+			this.dockingManager1.SetDockLabel(this.elementHostAgentState, "elementHostAgentState");
 			this.dockingManager1.SetEnableDocking(this.elementHostAgentState, true);
 			ccbelementHostAgentState.MergeWith(this.dockingManager1.CaptionButtons, false);
 			this.dockingManager1.SetCustomCaptionButtons(this.elementHostAgentState, ccbelementHostAgentState);
@@ -152,12 +149,12 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.dockingManager1.SetCustomCaptionButtons(this.elementHostStaffingEffect, ccbelementHostStaffingEffect);
 			// 
 			// panelSkillChart
-			// 
+			//
 			this.panelSkillChart.Controls.Add(this.chartControlSkillData);
 			this.panelSkillChart.Location = new System.Drawing.Point(1, 24);
 			this.panelSkillChart.Margin = new System.Windows.Forms.Padding(0);
 			this.panelSkillChart.Name = "panelSkillChart";
-			this.panelSkillChart.Size = new System.Drawing.Size(802, 272);
+			this.panelSkillChart.Size = new System.Drawing.Size(847, 206);
 			this.panelSkillChart.TabIndex = 19;
 			// 
 			// chartControlSkillData
@@ -173,7 +170,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			// 
 			// 
 			// 
-			this.chartControlSkillData.Legend.Location = new System.Drawing.Point(698, 75);
+			this.chartControlSkillData.Legend.Location = new System.Drawing.Point(743, 75);
 			this.chartControlSkillData.Localize = null;
 			this.chartControlSkillData.Location = new System.Drawing.Point(0, 0);
 			this.chartControlSkillData.Name = "chartControlSkillData";
@@ -182,32 +179,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.chartControlSkillData.PrimaryYAxis.Crossing = double.NaN;
 			this.chartControlSkillData.PrimaryYAxis.ForceZero = true;
 			this.chartControlSkillData.PrimaryYAxis.Margin = true;
-			chartSeries1.FancyToolTip.ResizeInsideSymbol = true;
-			chartSeries1.Name = "Default";
-			chartSeries1.Points.Add(0D, ((double)(54D)), ((double)(319D)), ((double)(249D)), ((double)(127D)));
-			chartSeries1.Points.Add(1D, ((double)(68D)), ((double)(305D)), ((double)(159D)), ((double)(222D)));
-			chartSeries1.Points.Add(2D, ((double)(94D)), ((double)(194D)), ((double)(138D)), ((double)(128D)));
-			chartSeries1.Points.Add(3D, ((double)(60D)), ((double)(113D)), ((double)(82D)), ((double)(110D)));
-			chartSeries1.Points.Add(4D, ((double)(85D)), ((double)(94D)), ((double)(86D)), ((double)(88D)));
-			chartSeries1.Resolution = 0D;
-			chartSeries1.StackingGroup = "Default Group";
-			chartSeries1.Style.AltTagFormat = "";
-			chartSeries1.Style.Border.Width = 2F;
-			chartSeries1.Style.DisplayShadow = true;
-			chartSeries1.Style.DrawTextShape = false;
-			chartLineInfo1.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
-			chartLineInfo1.Color = System.Drawing.SystemColors.ControlText;
-			chartLineInfo1.DashPattern = null;
-			chartLineInfo1.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
-			chartLineInfo1.Width = 1F;
-			chartCustomShapeInfo1.Border = chartLineInfo1;
-			chartCustomShapeInfo1.Color = System.Drawing.SystemColors.HighlightText;
-			chartCustomShapeInfo1.Type = Syncfusion.Windows.Forms.Chart.ChartCustomShape.Square;
-			chartSeries1.Style.TextShape = chartCustomShapeInfo1;
-			chartSeries1.Text = "Default";
-			chartSeries1.Type = Syncfusion.Windows.Forms.Chart.ChartSeriesType.Line;
-			this.chartControlSkillData.Series.Add(chartSeries1);
-			this.chartControlSkillData.Size = new System.Drawing.Size(802, 272);
+			this.chartControlSkillData.Size = new System.Drawing.Size(847, 206);
 			this.chartControlSkillData.TabIndex = 0;
 			this.chartControlSkillData.Text = "Skill";
 			// 
@@ -226,7 +198,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.panelSkillGrid.Location = new System.Drawing.Point(1, 24);
 			this.panelSkillGrid.Margin = new System.Windows.Forms.Padding(0);
 			this.panelSkillGrid.Name = "panelSkillGrid";
-			this.panelSkillGrid.Size = new System.Drawing.Size(802, 281);
+			this.panelSkillGrid.Size = new System.Drawing.Size(847, 151);
 			this.panelSkillGrid.TabIndex = 18;
 			// 
 			// tabSkillData
@@ -234,13 +206,13 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.tabSkillData.ActiveTabColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
 			this.tabSkillData.ActiveTabFont = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
 			this.tabSkillData.BackColor = System.Drawing.Color.White;
-			this.tabSkillData.BeforeTouchSize = new System.Drawing.Size(802, 281);
+			this.tabSkillData.BeforeTouchSize = new System.Drawing.Size(847, 151);
 			this.tabSkillData.Controls.Add(this.tabPageAdv1);
 			this.tabSkillData.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tabSkillData.InactiveTabColor = System.Drawing.Color.White;
 			this.tabSkillData.Location = new System.Drawing.Point(0, 0);
 			this.tabSkillData.Name = "tabSkillData";
-			this.tabSkillData.Size = new System.Drawing.Size(802, 281);
+			this.tabSkillData.Size = new System.Drawing.Size(847, 151);
 			this.tabSkillData.TabIndex = 6;
 			this.tabSkillData.TabPanelBackColor = System.Drawing.Color.White;
 			this.tabSkillData.TabStyle = typeof(Syncfusion.Windows.Forms.Tools.TabRendererMetro);
@@ -254,7 +226,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.tabPageAdv1.Location = new System.Drawing.Point(1, 24);
 			this.tabPageAdv1.Name = "tabPageAdv1";
 			this.tabPageAdv1.ShowCloseButton = true;
-			this.tabPageAdv1.Size = new System.Drawing.Size(799, 255);
+			this.tabPageAdv1.Size = new System.Drawing.Size(844, 125);
 			this.tabPageAdv1.TabBackColor = System.Drawing.Color.White;
 			this.tabPageAdv1.TabIndex = 1;
 			this.tabPageAdv1.ThemesEnabled = false;
@@ -265,7 +237,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.elementHostShiftEditor.BackColorTransparent = true;
 			this.elementHostShiftEditor.Location = new System.Drawing.Point(1, 24);
 			this.elementHostShiftEditor.Name = "elementHostShiftEditor";
-			this.elementHostShiftEditor.Size = new System.Drawing.Size(802, 85);
+			this.elementHostShiftEditor.Size = new System.Drawing.Size(847, 99);
 			this.elementHostShiftEditor.TabIndex = 13;
 			this.elementHostShiftEditor.Text = "yyelementHost1";
 			this.elementHostShiftEditor.Child = null;
@@ -275,7 +247,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.elementHostDayLayerView.BackColor = System.Drawing.Color.Transparent;
 			this.elementHostDayLayerView.Location = new System.Drawing.Point(1, 24);
 			this.elementHostDayLayerView.Name = "elementHostDayLayerView";
-			this.elementHostDayLayerView.Size = new System.Drawing.Size(690, 191);
+			this.elementHostDayLayerView.Size = new System.Drawing.Size(847, 154);
 			this.elementHostDayLayerView.TabIndex = 44;
 			this.elementHostDayLayerView.Text = "elementHost1";
 			this.elementHostDayLayerView.Child = this.dayLayerView1;
@@ -286,7 +258,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.elementHostPinnedLayerView.BackColorTransparent = true;
 			this.elementHostPinnedLayerView.Location = new System.Drawing.Point(1, 24);
 			this.elementHostPinnedLayerView.Name = "elementHostPinnedLayerView";
-			this.elementHostPinnedLayerView.Size = new System.Drawing.Size(273, 694);
+			this.elementHostPinnedLayerView.Size = new System.Drawing.Size(229, 232);
 			this.elementHostPinnedLayerView.TabIndex = 49;
 			this.elementHostPinnedLayerView.Text = "elementHostPinnedLayerView";
 			this.elementHostPinnedLayerView.Child = null;
@@ -296,7 +268,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.elementHostAgentState.BackColor = System.Drawing.Color.Transparent;
 			this.elementHostAgentState.Location = new System.Drawing.Point(1, 24);
 			this.elementHostAgentState.Name = "elementHostAgentState";
-			this.elementHostAgentState.Size = new System.Drawing.Size(231, 159);
+			this.elementHostAgentState.Size = new System.Drawing.Size(229, 230);
 			this.elementHostAgentState.TabIndex = 49;
 			this.elementHostAgentState.Text = "elementHost1";
 			this.elementHostAgentState.Child = this.agentStateLayerView1;
@@ -306,7 +278,7 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.elementHostStaffingEffect.BackColor = System.Drawing.Color.Transparent;
 			this.elementHostStaffingEffect.Location = new System.Drawing.Point(1, 24);
 			this.elementHostStaffingEffect.Name = "elementHostStaffingEffect";
-			this.elementHostStaffingEffect.Size = new System.Drawing.Size(231, 167);
+			this.elementHostStaffingEffect.Size = new System.Drawing.Size(229, 176);
 			this.elementHostStaffingEffect.TabIndex = 51;
 			this.elementHostStaffingEffect.Text = "xxStaffingEffect";
 			this.elementHostStaffingEffect.Child = this.staffingEffectView1;
@@ -331,7 +303,6 @@ namespace Teleopti.Ccc.Win.Intraday
 			this.Size = new System.Drawing.Size(1083, 719);
 			this.Load += new System.EventHandler(this.intradayViewContentLoad);
 			((System.ComponentModel.ISupportInitialize)(this.dockingManager1)).EndInit();
-			this.panelSkillChart.ResumeLayout(false);
 			this.panelSkillGrid.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.tabSkillData)).EndInit();
 			this.tabSkillData.ResumeLayout(false);
