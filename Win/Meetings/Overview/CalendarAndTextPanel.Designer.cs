a409f5338cb7525edb57bdc08ce8e8b49c36283f
Ola <none@none>
2014-09-05 16:23:14 +0200

    bug #29606 ( atry at least) and some design things

--- Win/Meetings/Overview/CalendarAndTextPanel.Designer.cs
+++ Win/Meetings/Overview/CalendarAndTextPanel.Designer.cs
@@ -53,18 +53,18 @@
 			// textBox1
 			// 
 			this.textBox1.BackColor = System.Drawing.Color.White;
-			this.textBox1.BeforeTouchSize = new System.Drawing.Size(100, 20);
+			this.textBox1.BeforeTouchSize = new System.Drawing.Size(228, 421);
 			this.textBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
 			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
 			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.textBox1.Location = new System.Drawing.Point(0, 211);
+			this.textBox1.Location = new System.Drawing.Point(0, 183);
 			this.textBox1.Metrocolor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
 			this.textBox1.ReadOnly = true;
 			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textBox1.Size = new System.Drawing.Size(228, 393);
+			this.textBox1.Size = new System.Drawing.Size(228, 421);
 			this.textBox1.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Metro;
 			this.textBox1.TabIndex = 2;
 			// 
@@ -72,32 +72,33 @@
 			// 
 			this.monthCalendarAdv1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
 			this.monthCalendarAdv1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.monthCalendarAdv1.BottomHeight = 29;
+			this.monthCalendarAdv1.BottomHeight = 25;
 			this.monthCalendarAdv1.Culture = new System.Globalization.CultureInfo("sv-SE");
 			this.monthCalendarAdv1.DayNamesColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-			this.monthCalendarAdv1.DayNamesFont = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.monthCalendarAdv1.DayNamesFont = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
 			this.monthCalendarAdv1.DaysFont = new System.Drawing.Font("Segoe UI", 9F);
 			this.monthCalendarAdv1.DaysHeaderInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.SystemColors.Window);
 			this.monthCalendarAdv1.Dock = System.Windows.Forms.DockStyle.Top;
 			this.monthCalendarAdv1.GridLines = Syncfusion.Windows.Forms.Grid.GridBorderStyle.None;
-			this.monthCalendarAdv1.HeaderHeight = 39;
+			this.monthCalendarAdv1.HeaderHeight = 34;
 			this.monthCalendarAdv1.HeaderStartColor = System.Drawing.Color.White;
 			this.monthCalendarAdv1.HighlightColor = System.Drawing.Color.Black;
 			this.monthCalendarAdv1.Iso8601CalenderFormat = false;
 			this.monthCalendarAdv1.Location = new System.Drawing.Point(0, 0);
-			this.monthCalendarAdv1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+			this.monthCalendarAdv1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
 			this.monthCalendarAdv1.MouseDragMultiselect = true;
 			this.monthCalendarAdv1.Name = "monthCalendarAdv1";
-			this.monthCalendarAdv1.ScrollButtonSize = new System.Drawing.Size(28, 28);
+			this.monthCalendarAdv1.ScrollButtonSize = new System.Drawing.Size(24, 24);
 			this.monthCalendarAdv1.SelectedDates = new System.DateTime[] {
-        new System.DateTime(2014, 8, 27, 0, 0, 0, 0)};
+        new System.DateTime(2014, 9, 5, 0, 0, 0, 0)};
 			this.monthCalendarAdv1.ShowWeekNumbers = true;
-			this.monthCalendarAdv1.Size = new System.Drawing.Size(228, 211);
+			this.monthCalendarAdv1.Size = new System.Drawing.Size(228, 183);
 			this.monthCalendarAdv1.Style = Syncfusion.Windows.Forms.VisualStyle.Metro;
 			this.monthCalendarAdv1.TabIndex = 1;
 			this.monthCalendarAdv1.ThemedEnabledGrid = true;
-			this.monthCalendarAdv1.WeekFont = new System.Drawing.Font("Verdana", 8F);
-			this.monthCalendarAdv1.WeekInterior = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.Vertical, System.Drawing.Color.PeachPuff, System.Drawing.Color.AntiqueWhite);
+			this.monthCalendarAdv1.WeekFont = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.monthCalendarAdv1.WeekInterior = new Syncfusion.Drawing.BrushInfo();
+			this.monthCalendarAdv1.WeekTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(51)))));
 			// 
 			// 
 			// 
@@ -106,8 +107,8 @@
 			this.monthCalendarAdv1.NoneButton.BeforeTouchSize = new System.Drawing.Size(75, 23);
 			this.monthCalendarAdv1.NoneButton.ForeColor = System.Drawing.Color.White;
 			this.monthCalendarAdv1.NoneButton.IsBackStageButton = false;
-			this.monthCalendarAdv1.NoneButton.Location = new System.Drawing.Point(164, 0);
-			this.monthCalendarAdv1.NoneButton.Size = new System.Drawing.Size(84, 29);
+			this.monthCalendarAdv1.NoneButton.Location = new System.Drawing.Point(129, 0);
+			this.monthCalendarAdv1.NoneButton.Size = new System.Drawing.Size(84, 25);
 			this.monthCalendarAdv1.NoneButton.Text = "None";
 			this.monthCalendarAdv1.NoneButton.UseVisualStyle = true;
 			this.monthCalendarAdv1.NoneButton.Visible = false;
@@ -120,7 +121,7 @@
 			this.monthCalendarAdv1.TodayButton.ForeColor = System.Drawing.Color.White;
 			this.monthCalendarAdv1.TodayButton.IsBackStageButton = false;
 			this.monthCalendarAdv1.TodayButton.Location = new System.Drawing.Point(0, 0);
-			this.monthCalendarAdv1.TodayButton.Size = new System.Drawing.Size(248, 29);
+			this.monthCalendarAdv1.TodayButton.Size = new System.Drawing.Size(213, 25);
 			this.monthCalendarAdv1.TodayButton.Text = "Today";
 			this.monthCalendarAdv1.TodayButton.UseVisualStyle = true;
 			this.monthCalendarAdv1.TodayButton.Visible = false;
