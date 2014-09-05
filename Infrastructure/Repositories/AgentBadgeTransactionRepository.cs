694160a97619e347e3b9b9fec9d20f8a6fa256f0
Xinfeng Li <xinfeng.li@teleopti.com>
2014-09-05 15:06:43 +0800

    PBI #28913 Add repository for badge transaction.

---
+++ Infrastructure/Repositories/AgentBadgeTransactionRepository.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using NHibernate.Criterion;
+using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.Infrastructure.Repositories
+{
+	public class AgentBadgeTransactionRepository : Repository<IAgentBadgeTransaction>, IAgentBadgeTransactionRepository
+	{
+		public AgentBadgeTransactionRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+		{
+		}
+
+		public AgentBadgeTransactionRepository(IUnitOfWorkFactory unitOfWorkFactory) : base(unitOfWorkFactory)
+		{
+		}
+
+		public AgentBadgeTransactionRepository(ICurrentUnitOfWork currentUnitOfWork) : base(currentUnitOfWork)
+		{
+		}
+
+		public ICollection<IAgentBadgeTransaction> Find(IPerson person)
+		{
+			InParameter.NotNull("person", person);
+
+			var result = Session.CreateCriteria(typeof(IAgentBadgeTransaction), "badge")
+				.Add(Restrictions.Eq("Person", person))
+				.List<IAgentBadgeTransaction>();
+			return result;
+		}
+
+		public ICollection<IAgentBadgeTransaction> Find(IPerson person, BadgeType badgeType)
+		{
+			InParameter.NotNull("person", person);
+			InParameter.NotNull("badgeType", badgeType);
+
+			var result = Session.CreateCriteria(typeof(IAgentBadgeTransaction), "badge")
+				.Add(Restrictions.Eq("Person", person))
+				.Add(Restrictions.Eq("BadgeType", (int)badgeType))
+				.List<IAgentBadgeTransaction>();
+			return result;
+		}
+
+		public IAgentBadgeTransaction Find(IPerson person, BadgeType badgeType, DateOnly calculateDate)
+		{
+			InParameter.NotNull("person", person);
+			InParameter.NotNull("badgeType", badgeType);
+			InParameter.NotNull("calculateDate", calculateDate);
+
+			var result = Session.CreateCriteria(typeof(IAgentBadgeTransaction), "badge")
+				.Add(Restrictions.Eq("Person", person))
+				.Add(Restrictions.Eq("BadgeType", (int)badgeType))
+				.Add(Restrictions.Eq("CalculatedDate", calculateDate))
+				.UniqueResult<IAgentBadgeTransaction>();
+			return result;
+		}
+	}
+}
