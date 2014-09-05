a50cd65b92602c4c6e1db98d456fe76acfc38aca
Mathias Stenbom <mathias@stenbom.com>
2014-09-05 10:40:15 +0200

    #29205 2 tests implementes in message broker controller

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/MessageBrokerControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/MessageBrokerControllerTest.cs
@@ -2,37 +2,71 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using Castle.Core.Internal;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
 using SharpTestsEx;
+using Teleopti.Ccc.Web.Broker;
 using Teleopti.Ccc.WebTest.Areas.Anywhere.Hubs;
 using Teleopti.Interfaces.MessageBroker;
+using Extensions = Teleopti.Ccc.Domain.Collection.Extensions;
 
 namespace Teleopti.Ccc.WebTest.Core.MessageBroker
 {
 	public class MessageBrokerControllerTest
 	{
-		[Test, Ignore]
+		[Test]
 		public void ShouldNotifyClients()
 		{
-			var notified = false;
 			var hubContext = MockRepository.GenerateStub<IHubContext>();
-			var clients = MockRepository.GenerateMock<IHubConnectionContext>();
-			hubContext.Stub(x => x.Clients).Return(clients);
-			var client = new FakeClientBuilder().Make("onEventMessage",
-				new Action<Notification, string>((n, r) =>
-				{
-					notified = true;
-				}));
-			clients.Stub(x => x.Group(null)).IgnoreArguments().Return(client);
+			var clientsContext = MockRepository.GenerateMock<IHubConnectionContext>();
+			hubContext.Stub(x => x.Clients).Return(clientsContext);
+			var client = MockRepository.GenerateMock<IOnEventMessageClient>();
+			clientsContext.Stub(x => x.Group(null)).IgnoreArguments().Return(client);
 			var target = new MessageBrokerController(() => hubContext);
 
 			target.NotifyClients(new Notification());
 			
-			notified.Should().Be.True();
+			client.AssertWasCalled(x => x.onEventMessage(null, null), o => o.IgnoreArguments());
+		}
+
+		[Test]
+		public void ShouldNotifyAllGroupsForNotification()
+		{
+			var notification = new Notification();
+			var hubContext = MockRepository.GenerateStub<IHubContext>();
+			var clientsContext = MockRepository.GenerateMock<IHubConnectionContext>();
+			hubContext.Stub(x => x.Clients).Return(clientsContext);
+
+			var expects = notification.Routes()
+				.Select(route =>
+				{
+					var client = MockRepository.GenerateMock<IOnEventMessageClient>();
+					clientsContext.Stub(x => x.Group(MessageBrokerHub.RouteToGroupName(route))).Return(client);
+					return new
+					{
+						client,
+						route
+					};
+				})
+				.ToArray();
+			var target = new MessageBrokerController(() => hubContext);
+			
+			target.NotifyClients(notification);
+
+			expects.ForEach(c =>
+				c.client.AssertWasCalled(x => x.onEventMessage(notification, c.route))
+				);
+		}
+
+		public interface IOnEventMessageClient
+		{
+			void onEventMessage(Notification notification, string route);
 		}
 	}
 
@@ -47,6 +81,9 @@ namespace Teleopti.Ccc.WebTest.Core.MessageBroker
 
 		public void NotifyClients(Notification notification)
 		{
+			notification.Routes().ForEach(r =>
+				_hubContext().Clients.Group(MessageBrokerHub.RouteToGroupName(r)).onEventMessage(notification, r)
+				);
 		}
 	}
 }
