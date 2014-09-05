3e4cdec32619d8ef047e0f38687f3c62b41946f0
Ola <none@none>
2014-09-05 16:23:38 +0200

    why are these static?

--- Teleopti.Analytics/WebPortal/Teleopti.Analytics.Portal/Teleopti.Analytics.Parameter/Selector.cs
+++ Teleopti.Analytics/WebPortal/Teleopti.Analytics.Portal/Teleopti.Analytics.Parameter/Selector.cs
@@ -27,9 +27,9 @@ namespace Teleopti.Analytics.Parameters
         private bool _isReportPermissionGranted;
         private Guid _groupPageCode;
 
-        internal static Guid CurrentUserCode;
-        internal static Guid BuCode;
-        internal static int LangId;
+		  private Guid CurrentUserCode;
+		  private Guid BuCode;
+		  private int LangId;
 
 		internal static string _ConnectionString = ConfigurationManager.ConnectionStrings["LocalSqlServer"].ConnectionString;
         //public static string _Database;
