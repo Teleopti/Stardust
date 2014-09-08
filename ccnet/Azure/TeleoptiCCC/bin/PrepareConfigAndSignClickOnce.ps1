eced8e5ca7e7f120d0eba0276ea31a84f877f1bb
David Jonsson <davidj@teleopti.com>
2014-09-09 00:30:02 +0200

    manual merge

--- ccnet/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
+++ ccnet/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
@@ -1,8 +1,3 @@
 param([string]$ScriptPath)
 write-host $ScriptPath
 powershell -File "$ScriptPath" 2>&1 | more
-	Add-Content "$fullPathsettingsFile" "`$(URL)|https://$DataSourceName.teleopticloud.com/Web/"
-    Add-Content "$fullPathsettingsFile" "`$(DEFAULT_IDENTITY_PROVIDER)|Teleopti"
-    Add-Content "$fullPathsettingsFile" "`$(WindowsClaimProvider)|"
-    Add-Content "$fullPathsettingsFile" "`$(TeleoptiClaimProvider)|<add identifier=`"urn:Teleopti`" displayName=`"Teleopti`" url=`"https://$DataSourceName.teleopticloud.com/Web/sso/`" protocolHandler=`"OpenIdHandler`" />"
-    Add-Content "$fullPathsettingsFile" "`$(UrlAuthenticationBridge)|https://$DataSourceName.teleopticloud.com/AuthenticationBridge/"
