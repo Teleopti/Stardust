a44a07b6a93d21c106fd5c7270ae83d4aecf0a29
Team Green <greenmachine@teleopti.com>
2014-09-09 15:26:48 +0200

    fixing build

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/Container/ContainerConfigurationTest.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/Container/ContainerConfigurationTest.cs
@@ -1,12 +1,19 @@
 ﻿using Autofac;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Rhino.ServiceBus;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Infrastructure.Toggle;
+using Teleopti.Ccc.Sdk.ServiceBus.ApplicationLayer;
 using Teleopti.Ccc.Sdk.ServiceBus.Container;
+using Teleopti.Ccc.Sdk.ServiceBus.Forecast;
+using Teleopti.Ccc.Sdk.ServiceBus.Payroll;
+using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.MessageBroker.Client;
 using Teleopti.Interfaces.MessageBroker.Client.Composite;
+using Teleopti.Interfaces.Messages.General;
+using Teleopti.Interfaces.Messages.Payroll;
 using Teleopti.Messaging.Client.Http;
 using Teleopti.Messaging.Client.SignalR;
 
@@ -69,5 +76,93 @@ namespace Teleopti.Ccc.Sdk.ServiceBusTest.Container
 			}
 		}
 
+		[Test]
+		public void ShouldResolveEventsConsumer()
+		{
+			var builder = new ContainerBuilder();
+			fakeInternalBusRegistrations(builder);
+			using (var container = builder.Build())
+			{
+				var containerConfiguration = new ContainerConfiguration(container);
+				containerConfiguration.Configure();
+				container.Resolve<ConsumerOf<IEvent>>().Should().Not.Be.Null();
+			}
+		}
+
+		[Test]
+		public void ShouldResolveExportMultisiteSkillsToSkillConsumer()
+		{
+			var builder = new ContainerBuilder();
+			fakeInternalBusRegistrations(builder);
+			using (var container = builder.Build())
+			{
+				var containerConfiguration = new ContainerConfiguration(container);
+				containerConfiguration.Configure();
+				container.Resolve<ConsumerOf<ExportMultisiteSkillsToSkill>>().Should().Not.Be.Null();
+			}
+		}
+
+		[Test]
+		public void ShouldResolveExportMultisiteSkillToSkillConsumer()
+		{
+			var builder = new ContainerBuilder();
+			fakeInternalBusRegistrations(builder);
+			using (var container = builder.Build())
+			{
+				var containerConfiguration = new ContainerConfiguration(container);
+				containerConfiguration.Configure();
+				container.Resolve<ConsumerOf<ExportMultisiteSkillToSkill>>().Should().Not.Be.Null();
+			}
+		}
+
+		[Test]
+		public void ShouldResolveImportForecastsFileToSkillConsumer()
+		{
+			var builder = new ContainerBuilder();
+			fakeInternalBusRegistrations(builder);
+			using (var container = builder.Build())
+			{
+				var containerConfiguration = new ContainerConfiguration(container);
+				containerConfiguration.Configure();
+				container.Resolve<ConsumerOf<ImportForecastsFileToSkill>>().Should().Not.Be.Null();
+			}
+		}
+
+		[Test]
+		public void ShouldResolveImportForecastsToSkillConsumer()
+		{
+			var builder = new ContainerBuilder();
+			fakeInternalBusRegistrations(builder);
+			using (var container = builder.Build())
+			{
+				var containerConfiguration = new ContainerConfiguration(container);
+				containerConfiguration.Configure();
+				container.Resolve<ConsumerOf<ImportForecastsToSkill>>().Should().Not.Be.Null();
+			}
+		}
+
+		[Test]
+		public void ShouldResolvePayrollExportConsumer()
+		{
+			var builder = new ContainerBuilder();
+			fakeInternalBusRegistrations(builder);
+			using (var container = builder.Build())
+			{
+				var containerConfiguration = new ContainerConfiguration(container);
+				containerConfiguration.Configure();
+				container.Resolve<ConsumerOf<RunPayrollExport>>().Should().Not.Be.Null();
+			}
+		}
+
+		private static void fakeInternalBusRegistrations(ContainerBuilder builder)
+		{
+			builder.Register(x => MockRepository.GenerateMock<IServiceBus>()).As<IServiceBus>();
+			builder.RegisterType<EventsConsumer>().As<ConsumerOf<IEvent>>();
+			builder.RegisterType<ExportMultisiteSkillsToSkillConsumer>().As<ConsumerOf<ExportMultisiteSkillsToSkill>>();
+			builder.RegisterType<ExportMultisiteSkillToSkillConsumer>().As<ConsumerOf<ExportMultisiteSkillToSkill>>();
+			builder.RegisterType<ImportForecastsFileToSkillConsumer>().As<ConsumerOf<ImportForecastsFileToSkill>>();
+			builder.RegisterType<ImportForecastsToSkillConsumer>().As<ConsumerOf<ImportForecastsToSkill>>();
+			builder.RegisterType<PayrollExportConsumer>().As<ConsumerOf<RunPayrollExport>>();
+		}
 	}
 }
