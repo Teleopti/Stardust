68acb4ef26aa9a2bc20e6fc648a93e552f2b3e48
Tamas Balog <tamas.balogh@teleopti.com>
2014-09-10 23:14:54 +0200

    add contract time and day offs validation

--- Win/Sikuli/Validators/OptimizeWithinDaysValidator.cs
+++ Win/Sikuli/Validators/OptimizeWithinDaysValidator.cs
@@ -1,4 +1,8 @@
-﻿using Teleopti.Ccc.WinCode.Common;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.WinCode.Common;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Win.Sikuli.Validators
@@ -16,15 +20,61 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators
 
 		public SikuliValidationResult Validate()
 		{
+			bool stdDevSumResult;
+			bool contractTimeResult = true;
+			bool daysOffResult = true;
+
 			SikuliValidationResult result = new SikuliValidationResult(true);
 			var std = ValidatorHelper.GetDailySumOfStandardDeviationsFullPeriod(_schedulerState, _totalSkill);
+			
+			stdDevSumResult = std > 4.6;
+			
+			IList<IPerson> persons = _schedulerState.FilteredPersonDictionary.Values.ToList();
+			
+			foreach (var person in persons)
+			{
+				var range = _schedulerState.Schedules[person];
+				var currentContractTime = range.CalculatedContractTimeHolder;
+				var targetContractTime = range.CalculatedTargetTimeHolder;
+
+				if (currentContractTime != targetContractTime)
+					contractTimeResult = false;
+
+				var currentDaysOff = range.CalculatedScheduleDaysOff;
+				var targetDayOffs = range.CalculatedTargetScheduleDaysOff;
+
+				if (currentDaysOff != targetDayOffs)
+					daysOffResult = false;
+
+			}
+
+			result.Result = stdDevSumResult && contractTimeResult && daysOffResult;
+
 			result.Details.AppendLine("Details:");
 			result.AppendLimitValueLine("Period daily StdDev sum", "4.6", std.ToString());
-			if (std > 4.6)
-			{
-				result.Result = false;
-			}
+			result.Details.AppendLine(contractTimeResult ? "Contract time : OK" : "Contract time : FAIL");
+			result.Details.AppendLine(daysOffResult ? "Day offs : OK" : "Contract time : FAIL");
+
 			return result;
+		}
+
+		public static TimeSpan GetCurrentContractTime(IScheduleRange wholeRange, DateOnlyPeriod period)
+		{
+			//if (!wholeRange.CalculatedContractTimeHolder.HasValue)
+			//{
+			//	TimeSpan contractTime = TimeSpan.Zero;
+			//	foreach (var scheduleDay in wholeRange.ScheduledDayCollection(period))
+			//	{
+			//		DateOnly dateOnly = scheduleDay.DateOnlyAsPeriod.DateOnly;
+			//		IPerson person = scheduleDay.Person;
+			//		if (!person.IsAgent(dateOnly))
+			//			continue;
+
+			//		contractTime = contractTime.Add(scheduleDay.ProjectionService().CreateProjection().ContractTime());
+			//	}
+			//	wholeRange.CalculatedContractTimeHolder = contractTime;
+			//}
+			return wholeRange.CalculatedContractTimeHolder.Value;
 		}
 	}
 }
