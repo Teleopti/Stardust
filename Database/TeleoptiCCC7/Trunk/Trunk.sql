69d4ca3045655e38276d9c4693805fabbf498ebf
David Jonsson <david.jonsson@teleopti.com>
2014-09-25 10:03:56 +0200

    Fix Trunk

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -4,8 +4,11 @@
 --Desc: Add Fairness type to WorkFlowControlSet table as part of the PBI #28317: Hide the points fairness system (Version 8)
 ----------------
 ALTER TABLE dbo.WorkflowControlSet ADD
-	FairnessType tinyint NOT NULL CONSTRAINT DF_WorkflowControlSet_FairnessType DEFAULT 1
+	FairnessType tinyint NULL CONSTRAINT DF_WorkflowControlSet_FairnessType DEFAULT 1
 GO
 UPDATE dbo.WorkflowControlSet
-	SET dbo.FairnessType = dbo.UseShiftCategoryFairness 
+	SET FairnessType = UseShiftCategoryFairness 
+GO
+ALTER TABLE dbo.WorkflowControlSet
+ALTER COLUMN FairnessType tinyint NOT NULL
 GO
