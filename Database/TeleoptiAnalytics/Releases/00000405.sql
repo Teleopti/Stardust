5bb59a29d8961ab2fadb8dcdc09fe94f21e205d8
David Jonsson <david.jonsson@teleopti.com>
2014-09-25 10:04:40 +0200

    Automated database build: 8.1.405

---
+++ Database/TeleoptiAnalytics/Releases/00000405.sql
@@ -0,0 +1,5 @@
+
+GO
+
+PRINT 'Adding build number to database' 
+INSERT INTO DatabaseVersion(BuildNumber, SystemVersion) VALUES (405,'8.1.405') 
---
+++ Database/TeleoptiCCC7/Releases/00000405.sql
@@ -0,0 +1,19 @@
+----------------  
+--Name: Tamas Balog
+--Date: 2014-09-25 
+--Desc: Add Fairness type to WorkFlowControlSet table as part of the PBI #28317: Hide the points fairness system (Version 8)
+----------------
+ALTER TABLE dbo.WorkflowControlSet ADD
+	FairnessType tinyint NULL CONSTRAINT DF_WorkflowControlSet_FairnessType DEFAULT 1
+GO
+UPDATE dbo.WorkflowControlSet
+	SET FairnessType = UseShiftCategoryFairness 
+GO
+ALTER TABLE dbo.WorkflowControlSet
+ALTER COLUMN FairnessType tinyint NOT NULL
+GO
+
+GO
+
+PRINT 'Adding build number to database' 
+INSERT INTO DatabaseVersion(BuildNumber, SystemVersion) VALUES (405,'8.1.405') 
