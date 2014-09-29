dbf159252a705fe355f47ed0df275fb4679e7702
Roger Kratz <roger.kratz@teleopti.com>
2014-09-29 13:58:57 +0200

    Backed out changeset: 63c65fe9f7cc
    Doesn't work as expected and breaks old catches of DataSourceException in eg
    web client.
    
    --HG--
    branch : Release 7.5.390

--- Infrastructure/NHibernateConfiguration/SqlServerExceptionConverter.cs
+++ Infrastructure/NHibernateConfiguration/SqlServerExceptionConverter.cs
@@ -2,32 +2,29 @@
 using System.Data.SqlClient;
 using NHibernate.Exceptions;
 using Teleopti.Ccc.Infrastructure.Foundation;
-using ConstraintViolationException=Teleopti.Ccc.Infrastructure.Foundation.ConstraintViolationException;
+using ConstraintViolationException = Teleopti.Ccc.Infrastructure.Foundation.ConstraintViolationException;
 
 
 namespace Teleopti.Ccc.Infrastructure.NHibernateConfiguration
 {
-    public class SqlServerExceptionConverter : ISQLExceptionConverter
-    {
-        public Exception Convert(AdoExceptionContextInfo adoExceptionContextInfo)
-        {
-            var sqle = ADOExceptionHelper.ExtractDbException(adoExceptionContextInfo.SqlException) as SqlException;
-            if (sqle != null)
-            {
-                switch (sqle.Number)
-                {
-					case -2:
-						return new DataSourceException(adoExceptionContextInfo.Message, sqle);
-                    case 2627:
-                        return new ConstraintViolationException(adoExceptionContextInfo.Message, sqle, adoExceptionContextInfo.Sql);
+	public class SqlServerExceptionConverter : ISQLExceptionConverter
+	{
+		public Exception Convert(AdoExceptionContextInfo adoExceptionContextInfo)
+		{
+			var sqle = ADOExceptionHelper.ExtractDbException(adoExceptionContextInfo.SqlException) as SqlException;
+			if (sqle != null)
+			{
+				switch (sqle.Number)
+				{
+					case 2627:
+						return new ConstraintViolationException(adoExceptionContextInfo.Message, sqle, adoExceptionContextInfo.Sql);
 					case 547:
-                		return new ForeignKeyException(adoExceptionContextInfo.Message, sqle, adoExceptionContextInfo.Sql);
-                }
-
-				return new Exception(adoExceptionContextInfo.Message, sqle);
-            }
+						return new ForeignKeyException(adoExceptionContextInfo.Message, sqle, adoExceptionContextInfo.Sql);
+				}
+				return new DataSourceException(adoExceptionContextInfo.Message, sqle);
+			}
 			var nhEx = SQLStateConverter.HandledNonSpecificException(adoExceptionContextInfo.SqlException, adoExceptionContextInfo.Message, adoExceptionContextInfo.Sql);
 			return new DataSourceException(nhEx.Message, nhEx);
-        }
-    }
+		}
+	}
 }
