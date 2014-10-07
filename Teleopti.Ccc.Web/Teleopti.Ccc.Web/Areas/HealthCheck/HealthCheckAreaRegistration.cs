8997efe39e740cfc758c1a82f24731bfe058dabf
Robin Karlsson <robin.karlsson@teleopti.com>
2014-10-07 10:35:13 +0200

    Making the authentication kind of work

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/HealthCheck/HealthCheckAreaRegistration.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/HealthCheck/HealthCheckAreaRegistration.cs
@@ -14,6 +14,14 @@ namespace Teleopti.Ccc.Web.Areas.HealthCheck
 
 		public override void RegisterArea(AreaRegistrationContext context)
 		{
+			var mapRoute = context.MapRoute(
+				"HealthCheck-authentication",
+				"HealthCheck/Authentication/{action}",
+				new { controller = "Authentication", action = "SignIn", area = "Start", origin = "HealthCheck" },
+				null,
+				new[] { "Teleopti.Ccc.Web.Areas.Start.*" });
+			mapRoute.DataTokens["area"] = "Start";
+
 			context.MapRoute(
 				"HealthCheck_default",
 				"HealthCheck/{controller}/{action}/{id}",
