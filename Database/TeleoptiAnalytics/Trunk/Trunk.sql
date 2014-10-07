cc9e7224a1657398f46f6182731b1b62a431df42
Team Green <greenmachine@teleopti.com>
2014-10-07 12:56:13 +0200

    PBI: #30783 Removed the ActualAgentState_History table

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -1,6 +1,17 @@
+
+
 -- 2014-10-06 RK: Adding new view for log object details to make troubleshooting easier
 
 SET IDENTITY_INSERT [mart].[sys_crossdatabaseview] ON
 INSERT [mart].[sys_crossdatabaseview] ([view_id], [view_name], [view_definition], [target_id]) VALUES (7, N'v_log_object_detail', N'SELECT * FROM [$$$target$$$].dbo.log_object_detail', 4)
 SET IDENTITY_INSERT [mart].[sys_crossdatabaseview] OFF
+GO
+
+--Name: Team Green
+--Date: 2014-10-07
+--Desc: Removed the ActualAgentState history table
+IF OBJECT_ID('RTA.ActualAgentState_History') IS NOT NULL
+BEGIN
+	DROP TABLE RTA.ActualAgentState_History
+END
 GO
