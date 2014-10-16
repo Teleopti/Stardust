87579ea1705b54cec74a331398dd5450cf9aa161
Xinfeng Li <xinfeng.li@teleopti.com>
2014-10-16 09:25:48 +0800

    Update test case of BadgeSettingProvider.

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Portal/DataProvider/BadgeSettingProviderTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Portal/DataProvider/BadgeSettingProviderTest.cs
@@ -1,9 +1,11 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Rhino.Mocks;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.SystemSetting.GlobalSetting;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Portal.DataProvider;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WebTest.Core.Portal.DataProvider
 {
@@ -11,21 +13,41 @@ namespace Teleopti.Ccc.WebTest.Core.Portal.DataProvider
 	class BadgeSettingProviderTest
 	{
 		[Test]
-		public void ShouldGetAgentBadgeSettingsByDefaultValue()
+		public void ShouldGetAgentBadgeSettings()
 		{
+			var settings = new AgentBadgeSettings
+			{
+				BadgeEnabled = true,
+				AnsweredCallsBadgeEnabled = true,
+				AnsweredCallsThreshold = 99,
+				AdherenceBadgeEnabled = true,
+				AdherenceThreshold = new Percent(0.55),
+				AHTBadgeEnabled = true,
+				AHTThreshold = new TimeSpan(0, 3, 30),
+				GoldToSilverBadgeRate = 3,
+				SilverToBronzeBadgeRate = 2
+			};
+
 			var repository = MockRepository.GenerateMock<IAgentBadgeSettingsRepository>();
-			repository.Stub(x => x.GetSettings()).IgnoreArguments().Return(new AgentBadgeSettings());
+			repository.Stub(x => x.GetSettings()).IgnoreArguments().Return(settings);
 
 			var target = new BadgeSettingProvider(repository);
 
 			var result = target.GetBadgeSettings();
 
-			result.BadgeEnabled.Should().Be(false);
-			result.AnsweredCallsThreshold.Should().Be(100);
-			result.AHTThreshold.Minutes.Should().Be(5);
-			result.AdherenceThreshold.ValueAsPercent().Should().Be(75);
-			result.SilverToBronzeBadgeRate.Should().Be(2);
-			result.GoldToSilverBadgeRate.Should().Be(5);
+			result.BadgeEnabled.Should().Be(settings.BadgeEnabled);
+			
+			result.AnsweredCallsBadgeEnabled.Should().Be(settings.AnsweredCallsBadgeEnabled);
+			result.AnsweredCallsThreshold.Should().Be(settings.AnsweredCallsThreshold);
+
+			result.AdherenceBadgeEnabled.Should().Be(settings.AdherenceBadgeEnabled);
+			result.AdherenceThreshold.ValueAsPercent().Should().Be(settings.AdherenceThreshold.ValueAsPercent());
+
+			result.AHTBadgeEnabled.Should().Be(settings.AHTBadgeEnabled);
+			result.AHTThreshold.Ticks.Should().Be(settings.AHTThreshold.Ticks);
+
+			result.SilverToBronzeBadgeRate.Should().Be(result.SilverToBronzeBadgeRate);
+			result.GoldToSilverBadgeRate.Should().Be(result.GoldToSilverBadgeRate);
 		}
 	}
 }
