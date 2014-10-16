054eb21dc2230dfa8430d5dfb611ec4fb11d8696
Mathias Stenbom <mathias@stenbom.com>
2014-10-16 10:36:21 +0200

    PBI: #30783 refact: oops

--- InfrastructureTest/Rta/PersonOrganizationReaderTest.cs
+++ InfrastructureTest/Rta/PersonOrganizationReaderTest.cs
@@ -34,7 +34,7 @@ namespace Teleopti.Ccc.InfrastructureTest.Rta
 			var personId = person.Id.Value;
 
 			var target = new PersonOrganizationReader(new Now(), Session.Connection.ConnectionString);
-			var resItem = target.LoadAll().Single(x => x.PersonId == personId);
+			var resItem = target.PersonOrganizationData().Single(x => x.PersonId == personId);
 			resItem.TeamId.Should().Be.EqualTo(teamId);
 			resItem.PersonId.Should().Be.EqualTo(personId);
 
@@ -72,7 +72,7 @@ namespace Teleopti.Ccc.InfrastructureTest.Rta
 			var personId = person.Id.Value;
 	
 			var target = new PersonOrganizationReader(new Now(), Session.Connection.ConnectionString);
-			var resItem = target.LoadAll().Single(x => x.PersonId == personId);
+			var resItem = target.PersonOrganizationData().Single(x => x.PersonId == personId);
 			resItem.SiteId.Should().Be.EqualTo(siteId);
 			resItem.PersonId.Should().Be.EqualTo(personId);
 
