c0c6d822c3cc493f4bf5c369c36b286799de2ceb
Robin Karlsson <robin.karlsson@teleopti.com>
2014-10-19 11:00:22 +0200

    Making the build pass again

--- Teleopti.Runtime.Environment/Teleopti.Runtime.TestServer/Global.asax.cs
+++ Teleopti.Runtime.Environment/Teleopti.Runtime.TestServer/Global.asax.cs
@@ -16,7 +16,6 @@ namespace Teleopti.Runtime.TestServer
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
