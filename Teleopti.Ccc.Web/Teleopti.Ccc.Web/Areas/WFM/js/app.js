df01d20a90c8f4062a7959a3367e3ca2c2c409db
blandine <none@none>
2014-10-21 09:46:38 +0200

    wfm web - states with ui-router

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/js/app.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/js/app.js
@@ -1,14 +1,55 @@
 'use strict';
 
-angular.module('wfm', [
-    'ngRoute',
+var wfm = angular.module('wfm', [
+    'ui.router',
     'wfmCtrls'
-]).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
+]);
+wfm.config([          '$stateProvider', '$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    //debugger;
+    $stateProvider.state('main', {
+        url:'/',
         templateUrl: 'html/main.html',
         controller: 'MainCtrl'
-    }).when('/forecasting', {
+    }).state('forecasting', {
+        url:'/forecasting',
         templateUrl: 'html/forecasting.html',
         controller: 'ForecastingCtrl'
-    }).otherwise({redirectTo: '/'});
+    }).state('forecasting.run', {
+        params:{period: {}},
+        templateUrl: 'html/forecasting-run.html',
+        controller: 'ForecastingRunCtrl'
+    });
 }]);
+/*
+var wfm = angular.module('wfm', ["ui.router"]);
+wfm.config([          '$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider){
+
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/route1")
+
+    $stateProvider
+        .state('route1', {
+            url: "/route1",
+            templateUrl: "route1.html"
+        })
+        .state('route1.list', {
+            url: "/list",
+            templateUrl: "route1.list.html",
+            controller: function($scope){
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+
+        .state('route2', {
+            url: "/route2",
+            templateUrl: "route2.html"
+        })
+        .state('route2.list', {
+            url: "/list",
+            templateUrl: "route2.list.html",
+            controller: function($scope){
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        })
+}]);*/
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/js/wfmCtrls.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/js/wfmCtrls.js
@@ -8,8 +8,18 @@ wfmCtrls.controller('MainCtrl',
     }
 );
 
-wfmCtrls.controller('ForecastingCtrl',
-    function ($scope) {
+wfmCtrls.controller('ForecastingCtrl', ['$scope', '$state',
+    function ($scope, $state) {
+       $scope.startPeriod = function (period){
+           $state.go('forecasting.run', {period: period}); //there's probably a better way to do that
+        }
+    }]
+);
 
-    }
+wfmCtrls.controller('ForecastingRunCtrl',[ '$scope','$stateParams',
+    function ($scope, $stateParams) {
+        $scope.period = $stateParams.period;
+       $scope.states =['done'];
+
+    }]
 );
