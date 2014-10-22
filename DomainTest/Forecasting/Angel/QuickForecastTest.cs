72faf382ad6f1defb8c20b047599a9d429db0559
Roger Kratz <roger.kratz@teleopti.com>
2014-10-22 16:54:04 +0200

    minor - just restructure code

--- DomainTest/Forecasting/Angel/QuickForecastTest.cs
+++ DomainTest/Forecasting/Angel/QuickForecastTest.cs
@@ -14,7 +14,6 @@ using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.Infrastructure;
 
-
 namespace Teleopti.Ccc.DomainTest.Forecasting.Angel
 {
 	public abstract class QuickForecastTest
@@ -25,48 +24,6 @@ namespace Teleopti.Ccc.DomainTest.Forecasting.Angel
 		{
 			DefaultScenario = new Scenario("default scenario") {DefaultScenario = true};
 		}
-
-		protected IScenario DefaultScenario { get; private set; }
-
-		protected virtual DateOnlyPeriod HistoricalPeriod
-		{
-			get { return new DateOnlyPeriod(2000, 1, 1, 2000, 1, 1); }
-		}
-
-		protected virtual DateOnlyPeriod FuturePeriod
-		{
-			get { return new DateOnlyPeriod(HistoricalPeriod.StartDate.AddDays(7), HistoricalPeriod.EndDate.AddDays(7)); }
-		}
-
-		protected virtual IWorkload Workload
-		{
-			get 
-			{
-				return _workload ?? (_workload = WorkloadFactory.CreateWorkloadWithFullOpenHours(SkillFactory.CreateSkill("_")));
-			}
-		}
-
-		protected virtual IEnumerable<StatisticTask> StatisticTasks()
-		{
-			return Enumerable.Empty<StatisticTask>();
-		}
-
-		protected virtual IEnumerable<IValidatedVolumeDay> ValidatedVolumeDays()
-		{
-			return Enumerable.Empty<IValidatedVolumeDay>();
-		}
-
-		protected virtual ICollection<ISkillDay> CurrentSkillDays()
-		{
-			return new List<ISkillDay>();
-		}
-
-		protected TimeZoneInfo SkillTimeZoneInfo()
-		{
-			return Workload.Skill.TimeZone;
-		}
-
-		protected abstract void Assert(IEnumerable<ISkillDay> modifiedSkillDays);
 
 		[Test]
 		public void DoTheTest()
@@ -94,5 +51,45 @@ namespace Teleopti.Ccc.DomainTest.Forecasting.Angel
 
 			Assert(skillDays);
 		}
+
+
+		protected IScenario DefaultScenario { get; private set; }
+
+		protected virtual DateOnlyPeriod HistoricalPeriod
+		{
+			get { return new DateOnlyPeriod(2000, 1, 1, 2000, 1, 1); }
+		}
+
+		protected virtual DateOnlyPeriod FuturePeriod
+		{
+			get { return new DateOnlyPeriod(HistoricalPeriod.StartDate.AddDays(7), HistoricalPeriod.EndDate.AddDays(7)); }
+		}
+
+		protected virtual IWorkload Workload
+		{
+			get { return _workload ?? (_workload = WorkloadFactory.CreateWorkloadWithFullOpenHours(SkillFactory.CreateSkill("_"))); }
+		}
+
+		protected virtual IEnumerable<StatisticTask> StatisticTasks()
+		{
+			return Enumerable.Empty<StatisticTask>();
+		}
+
+		protected virtual IEnumerable<IValidatedVolumeDay> ValidatedVolumeDays()
+		{
+			return Enumerable.Empty<IValidatedVolumeDay>();
+		}
+
+		protected virtual ICollection<ISkillDay> CurrentSkillDays()
+		{
+			return new List<ISkillDay>();
+		}
+
+		protected TimeZoneInfo SkillTimeZoneInfo()
+		{
+			return Workload.Skill.TimeZone;
+		}
+
+		protected abstract void Assert(IEnumerable<ISkillDay> modifiedSkillDays);
 	}
 }
