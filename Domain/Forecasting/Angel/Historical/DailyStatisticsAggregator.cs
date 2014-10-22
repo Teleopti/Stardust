0ba0700049e39aa62bd8fb51dc3d3c3878571d8b
Robin Karlsson <robin.karlsson@teleopti.com>
2014-10-22 11:37:05 +0200

    Making the aggregator consider time zone, midnight break and call calculations

--- Domain/Forecasting/Angel/Historical/DailyStatisticsAggregator.cs
+++ Domain/Forecasting/Angel/Historical/DailyStatisticsAggregator.cs
@@ -18,11 +18,18 @@ namespace Teleopti.Ccc.Domain.Forecasting.Angel.Historical
 		public IEnumerable<DailyStatistic> LoadDailyStatistics(IWorkload workload, DateOnlyPeriod dateRange)
 		{
 			var statisticTasks = _statisticRepository.LoadSpecificDates(workload.QueueSourceCollection,
-				dateRange.ToDateTimePeriod(TimeZoneInfo.Utc));
+				dateRange.ToDateTimePeriod(workload.Skill.TimeZone));
 
+			var calculator = new QueueStatisticsCalculator(workload.QueueAdjustments);
 			var result = from t in statisticTasks
-						 group t by t.Interval.Date into g
-						select new DailyStatistic(new DateOnly(g.Key),(int)g.Sum(k => k.StatAnsweredTasks));
+				group t by
+					TimeZoneHelper.ConvertFromUtc(t.Interval, workload.Skill.TimeZone).Subtract(workload.Skill.MidnightBreakOffset).Date
+				into g
+				select new DailyStatistic(new DateOnly(g.Key), (int) g.Sum(k =>
+				{
+					calculator.Calculate(k);
+					return k.StatCalculatedTasks;
+				}));
 			return result;
 		}
 	}
--- DomainTest/Forecasting/Angel/Historical/DailyStatisticsAggregatorTest.cs
+++ DomainTest/Forecasting/Angel/Historical/DailyStatisticsAggregatorTest.cs
@@ -25,14 +25,15 @@ namespace Teleopti.Ccc.DomainTest.Forecasting.Angel.Historical
 				x => x.LoadSpecificDates(workload.QueueSourceCollection, new DateTimePeriod(2001, 1, 1, 2001, 1, 2)))
 				.Return(new List<IStatisticTask>
 				{
-					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatAnsweredTasks = 6},
-					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 30, 0, DateTimeKind.Utc), StatAnsweredTasks = 7},
+					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 6},
+					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 30, 0, DateTimeKind.Utc), StatOfferedTasks = 7},
 				});
 			var target = new DailyStatisticsAggregator(statisticRepository);
 			var result = target.LoadDailyStatistics(workload, dateRange);
 
-			result.Single().Date.Should().Be.EqualTo(dateRange.StartDate);
-			result.Single().CalculatedTasks.Should().Be.EqualTo(13);
+			var onlyDayInResult = result.Single();
+			onlyDayInResult.Date.Should().Be.EqualTo(dateRange.StartDate);
+			onlyDayInResult.CalculatedTasks.Should().Be.EqualTo(13);
 		}
 
 		[Test]
@@ -46,16 +47,94 @@ namespace Teleopti.Ccc.DomainTest.Forecasting.Angel.Historical
 				x => x.LoadSpecificDates(workload.QueueSourceCollection, new DateTimePeriod(2001, 1, 1, 2001, 1, 3)))
 				.Return(new List<IStatisticTask>
 				{
-					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatAnsweredTasks = 6},
-					new StatisticTask {Interval = new DateTime(2001, 1, 2, 11, 15, 0, DateTimeKind.Utc), StatAnsweredTasks = 7},
+					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 6},
+					new StatisticTask {Interval = new DateTime(2001, 1, 2, 11, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 7},
 				});
 			var target = new DailyStatisticsAggregator(statisticRepository);
 			var result = target.LoadDailyStatistics(workload, dateRange);
 
-			result.First().Date.Should().Be.EqualTo(dateRange.StartDate);
-			result.First().CalculatedTasks.Should().Be.EqualTo(6);
-			result.Last().Date.Should().Be.EqualTo(dateRange.EndDate);
-			result.Last().CalculatedTasks.Should().Be.EqualTo(7);
+			var firstDayInResult = result.First();
+			var lastDayInResult = result.Last();
+			
+			firstDayInResult.Date.Should().Be.EqualTo(dateRange.StartDate);
+			firstDayInResult.CalculatedTasks.Should().Be.EqualTo(6);
+			lastDayInResult.Date.Should().Be.EqualTo(dateRange.EndDate);
+			lastDayInResult.CalculatedTasks.Should().Be.EqualTo(7);
+		}
+
+		[Test]
+		public void ShouldAggregateStatisticTasksForSkillLocalDay()
+		{
+			var workload = new Workload(SkillFactory.CreateSkill("Direct sales"));
+			var localTimeZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+			workload.Skill.TimeZone = localTimeZone;
+			var dateRange = new DateOnlyPeriod(2001, 1, 1, 2001, 1, 1);
+
+			var statisticRepository = MockRepository.GenerateStub<IStatisticRepository>();
+			statisticRepository.Stub(
+				x => x.LoadSpecificDates(workload.QueueSourceCollection, dateRange.ToDateTimePeriod(localTimeZone)))
+				.Return(new List<IStatisticTask>
+				{
+					new StatisticTask {Interval = new DateTime(2000, 12, 31, 23, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 6},
+					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 7},
+				});
+			var target = new DailyStatisticsAggregator(statisticRepository);
+			var result = target.LoadDailyStatistics(workload, dateRange);
+
+			var onlyDayInResult = result.Single();
+			onlyDayInResult.Date.Should().Be.EqualTo(dateRange.StartDate);
+			onlyDayInResult.CalculatedTasks.Should().Be.EqualTo(13);
+		}
+
+		[Test]
+		public void ShouldAggregateStatisticTasksForSkillLocalDayConsideringSkillMidnightBreak()
+		{
+			var workload = new Workload(SkillFactory.CreateSkill("Direct sales"));
+			var localTimeZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+			workload.Skill.TimeZone = localTimeZone;
+			workload.Skill.MidnightBreakOffset = TimeSpan.FromHours(2);
+			var dateRange = new DateOnlyPeriod(2001, 1, 1, 2001, 1, 1);
+
+			var statisticRepository = MockRepository.GenerateStub<IStatisticRepository>();
+			statisticRepository.Stub(
+				x => x.LoadSpecificDates(workload.QueueSourceCollection, dateRange.ToDateTimePeriod(localTimeZone)))
+				.Return(new List<IStatisticTask>
+				{
+					new StatisticTask {Interval = new DateTime(2000, 12, 31, 23, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 6},
+					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 7},
+				});
+			var target = new DailyStatisticsAggregator(statisticRepository);
+			var result = target.LoadDailyStatistics(workload, dateRange);
+
+			var firstDayInResult = result.First();
+			var lastDayInResult = result.Last();
+
+			firstDayInResult.Date.Should().Be.EqualTo(dateRange.StartDate.AddDays(-1));
+			firstDayInResult.CalculatedTasks.Should().Be.EqualTo(6);
+			lastDayInResult.Date.Should().Be.EqualTo(dateRange.StartDate);
+			lastDayInResult.CalculatedTasks.Should().Be.EqualTo(7);
+		}
+
+		[Test]
+		public void ShouldApplyCalculationRulesFromWorkloadForStatistics()
+		{
+			var workload = new Workload(SkillFactory.CreateSkill("Direct sales"));
+			workload.QueueAdjustments = new QueueAdjustment {OfferedTasks = new Percent(0.5)};
+			var dateRange = new DateOnlyPeriod(2001, 1, 1, 2001, 1, 1);
+
+			var statisticRepository = MockRepository.GenerateStub<IStatisticRepository>();
+			statisticRepository.Stub(
+				x => x.LoadSpecificDates(workload.QueueSourceCollection, new DateTimePeriod(2001, 1, 1, 2001, 1, 2)))
+				.Return(new List<IStatisticTask>
+				{
+					new StatisticTask {Interval = new DateTime(2001, 1, 1, 11, 15, 0, DateTimeKind.Utc), StatOfferedTasks = 6},
+				});
+			var target = new DailyStatisticsAggregator(statisticRepository);
+			var result = target.LoadDailyStatistics(workload, dateRange);
+
+			var onlyDayInResult = result.Single();
+			onlyDayInResult.Date.Should().Be.EqualTo(dateRange.StartDate);
+			onlyDayInResult.CalculatedTasks.Should().Be.EqualTo(3);
 		}
 	}
 }
