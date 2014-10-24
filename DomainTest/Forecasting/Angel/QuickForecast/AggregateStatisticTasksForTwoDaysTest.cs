a6d7d021f81b86ce0ef6a1f27a83140046319dae
Roger Kratz <roger.kratz@teleopti.com>
2014-10-24 13:50:45 +0200

    converted last two statistic test to run against outer api instead. these set to ignored ATM

---
+++ DomainTest/Forecasting/Angel/QuickForecast/AggregateStatisticTasksForTwoDaysTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Forecasting.Angel.QuickForecast
+{
+	[Ignore("Fails because statistics will be put as an average on each skillday (they both contain 6.5 below)")]
+	public class AggregateStatisticTasksForTwoDaysTest : QuickForecastTest
+	{
+		protected override DateOnlyPeriod HistoricalPeriod
+		{
+			get { return new DateOnlyPeriod(2000,1,1,2000,1,2); }
+		}
+
+		protected override IEnumerable<StatisticTask> StatisticTasks()
+		{
+			var startDateOnHistoricalPeriod = HistoricalPeriod.ToDateTimePeriod(SkillTimeZoneInfo()).StartDateTime.AddHours(12);
+			return new[]
+			{
+				new StatisticTask {Interval = startDateOnHistoricalPeriod, StatOfferedTasks = 6},
+				new StatisticTask {Interval = startDateOnHistoricalPeriod.AddDays(1), StatOfferedTasks = 7}
+			};
+		}
+
+		protected override void Assert(IEnumerable<ISkillDay> modifiedSkillDays)
+		{
+			var firstDay = modifiedSkillDays.Single(x => x.CurrentDate == FuturePeriod.StartDate);
+			var lastDay = modifiedSkillDays.Single(x => x.CurrentDate == FuturePeriod.StartDate.AddDays(1));
+			Convert.ToInt32(firstDay.Tasks)
+				.Should().Be.EqualTo(6);
+			Convert.ToInt32(lastDay.Tasks)
+				.Should().Be.EqualTo(7);
+		}
+	}
+}
