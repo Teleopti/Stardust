bc5af330f50d37b468b1f433a2b320ef8ea1372c
Henke <henrik.andersson@teleopti.com>
2014-10-27 10:17:20 +0100

    #30783 Start calculating historical adherence

---
+++ Domain/ApplicationLayer/Rta/HistoricalAdherence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta
+{
+	public interface IHistoricalAdherence
+	{
+		Percent ForDay(AdherencePercentageReadModel data);
+	}
+
+	public class HistoricalAdherence : IHistoricalAdherence
+	{
+		public Percent ForDay(AdherencePercentageReadModel data)
+		{
+			var minIn =Convert.ToDouble(data.MinutesInAdherence);
+			var total = data.MinutesInAdherence + data.MinutesOutOfAdherence;
+			return new Percent(minIn / total);
+		}
+	}
+}
