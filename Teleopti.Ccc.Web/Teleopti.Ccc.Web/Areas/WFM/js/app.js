d9427c4024ca76c8e371abb77cb1d724879e1d78
Robin Karlsson <robin.karlsson@teleopti.com>
2014-10-29 15:36:26 +0100

    Initial authentication for Angel

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/js/app.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/js/app.js
@@ -4,7 +4,7 @@ var wfm = angular.module('wfm', [
     'ui.router',
     'wfmCtrls'
 ]);
-wfm.config([          '$stateProvider', '$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
+wfm.config(['$stateProvider', '$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/");
     //debugger;
     $stateProvider.state('main', {
@@ -20,4 +20,11 @@ wfm.config([          '$stateProvider', '$urlRouterProvider',function ($statePro
         templateUrl: 'html/forecasting-run.html',
         controller: 'ForecastingRunCtrl'
     });
-}]);
+}]).run(['$rootScope', '$http', '$q', '$location'], function ($rootScope, $http, $q, $location) {
+	var context = $http.get('../../Anywhere/Application/NavigationContent');
+	$rootScope.$on('$routeChangeStart', function (event, next, current) {
+		$q.all(context);
+		context.error(function() { $location.path('../../'); });
+		context.success(function(data) {});
+	});
+});
