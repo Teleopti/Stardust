072c6fc87473f6cdf6cbb1c681b4675fdd30da4b
Ola <none@none>
2014-10-30 16:05:50 +0100

    bug #29497 just taking one if more, it is just random which it will take but it is the same in nightly and in GUI (randomly that is, not the same as)
    --HG--
    branch : Release 7.5.390

--- Teleopti.Analytics/Teleopti.Analytics.Etl.Transformer/IntradaySchedulePreferenceTransformer.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.Transformer/IntradaySchedulePreferenceTransformer.cs
@@ -8,34 +8,61 @@ using Teleopti.Interfaces.Domain;
 namespace Teleopti.Analytics.Etl.Transformer
 {
 	public class IntradaySchedulePreferenceTransformer : IIntradaySchedulePreferenceTransformer
-    {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "1"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
-        public void Transform(IEnumerable<IPreferenceDay> rootList, DataTable table, ICommonStateHolder stateHolder, IScenario scenario)
-        {
-            InParameter.NotNull("rootList", rootList);
-            InParameter.NotNull("table", table);
-
-            foreach (var preferenceDay in rootList)
-            {
-	            if (!CheckIfPreferenceIsValid(preferenceDay.Restriction)) 
+	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "1"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
+		public void Transform(IEnumerable<IPreferenceDay> rootList, DataTable table, ICommonStateHolder stateHolder, IScenario scenario)
+		{
+			InParameter.NotNull("rootList", rootList);
+			InParameter.NotNull("table", table);
+			var preferenceDayAndPersonList = new HashSet<PreferenceDayAndPerson>();
+			foreach (var preferenceDay in rootList)
+			{
+				if (!CheckIfPreferenceIsValid(preferenceDay.Restriction))
 					continue;
 
-	            var persons = stateHolder.PersonsWithIds(new List<Guid> {preferenceDay.Person.Id.GetValueOrDefault()});
-	            if (!persons.Any()) 
+				var persons = stateHolder.PersonsWithIds(new List<Guid> { preferenceDay.Person.Id.GetValueOrDefault() });
+				if (!persons.Any())
 					return;
 
-	            var newDataRow = table.NewRow();
-	            var schedulePart = stateHolder.GetSchedulePartOnPersonAndDate(persons[0], preferenceDay.RestrictionDate, scenario);
+				var schedulePart = stateHolder.GetSchedulePartOnPersonAndDate(persons[0], preferenceDay.RestrictionDate, scenario);
+				var preferenceDayAndPerson = new PreferenceDayAndPerson
+				{
+					PersonId = schedulePart.Person.Id.GetValueOrDefault(),
+					PreferenceDate = preferenceDay.RestrictionDate
+				};
+				if (preferenceDayAndPersonList.Contains(preferenceDayAndPerson))
+					continue;
+				var newDataRow = table.NewRow();
+
 				newDataRow = SchedulePreferenceTransformerHelper.FillDataRow(newDataRow, preferenceDay.Restriction, schedulePart);
-	            table.Rows.Add(newDataRow);
-            }
-        }
+				table.Rows.Add(newDataRow);
+				preferenceDayAndPersonList.Add(preferenceDayAndPerson);
+			}
+		}
 
 		public bool CheckIfPreferenceIsValid(IPreferenceRestriction preferenceRestriction)
 		{
 			return SchedulePreferenceTransformerHelper.CheckIfPreferenceIsValid(preferenceRestriction);
-        }
+		}
 
-        
-    }
+		internal class PreferenceDayAndPerson
+		{
+			public DateOnly PreferenceDate { get; set; }
+			public Guid PersonId { get; set; }
+			public override bool Equals(Object obj)
+			{
+				if (!(obj is PreferenceDayAndPerson)) return false;
+
+				var p = (PreferenceDayAndPerson)obj;
+				return GetHashCode().Equals(p.GetHashCode());
+			}
+			public override int GetHashCode()
+			{
+				var idHash = PersonId.GetHashCode();
+				var dateHash = PreferenceDate.GetHashCode();
+
+				return idHash ^ dateHash;
+			}
+		}
+	}
 }
