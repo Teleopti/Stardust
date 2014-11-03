b3edfe0699ed11e77fe7ab37d4ebfb3f104b29b3
Ola <none@none>
2014-11-03 13:36:09 +0100

    add a test class for the keybased authentication

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/KeyBasedAuthenticationAttribute.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/KeyBasedAuthenticationAttribute.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Ajax.Utilities;
 using Microsoft.IdentityModel.Claims;
 using Teleopti.Ccc.Infrastructure.Foundation;
 using Claim = System.Security.Claims.Claim;
@@ -14,21 +15,20 @@ namespace Teleopti.Ccc.Web.Filters
 {
 	public class KeyBasedAuthenticationAttribute : BasicAuthenticationAttribute
 	{
-#pragma warning disable 1998
+
 		protected override async Task<IPrincipal> AuthenticateAsync(string userName, string password, CancellationToken cancellationToken)
-#pragma warning restore 1998
 		{
 			var key = ConfigurationManager.AppSettings["AuthenticationKey"];
 			if (!password.Equals(key))
 				return null;
+
 			var claims = new List<Claim>
 			{
 				new Claim(System.IdentityModel.Claims.ClaimTypes.NameIdentifier, userName )
 			};
 			var claimsIdentity = new ClaimsIdentity(claims, "Basic");
-			
-			
-			return new ClaimsPrincipal(claimsIdentity);
+			return await Task.FromResult(new ClaimsPrincipal(claimsIdentity));
+
 		}
 
 	}
