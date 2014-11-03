2cf2441de7430281d3ee18b45a8f226302be112e
Ola <none@none>
2014-11-03 09:24:09 +0100

    utf8 encoding to handle the authenticationkey

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/BasicAuthenticationAttribute.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/BasicAuthenticationAttribute.cs
@@ -84,7 +84,7 @@ namespace Teleopti.Ccc.Web.Filters
 			// The currently approved HTTP 1.1 specification says characters here are ISO-8859-1.
 			// However, the current draft updated specification for HTTP 1.1 indicates this encoding is infrequently
 			// used in practice and defines behavior only for ASCII.
-			Encoding encoding = Encoding.ASCII;
+			Encoding encoding = Encoding.UTF8;
 			// Make a writable copy of the encoding to enable setting a decoder fallback.
 			encoding = (Encoding)encoding.Clone();
 			// Fail on invalid bytes rather than silently replacing and continuing.
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/KeyBasedAuthenticationAttribute.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/KeyBasedAuthenticationAttribute.cs
@@ -1,8 +1,14 @@
 ﻿using System;
-using System.Security.Claims;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Claims;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Claim = System.Security.Claims.Claim;
+using ClaimsIdentity = System.Security.Claims.ClaimsIdentity;
+using ClaimsPrincipal = System.Security.Claims.ClaimsPrincipal;
 
 namespace Teleopti.Ccc.Web.Filters
 {
@@ -10,9 +16,17 @@ namespace Teleopti.Ccc.Web.Filters
 	{
 		protected override async Task<IPrincipal> AuthenticateAsync(string userName, string password, CancellationToken cancellationToken)
 		{
-
-			var identity = new ClaimsIdentity();
-			return new ClaimsPrincipal(identity);
+			var key = ConfigurationManager.AppSettings["AuthenticationKey"];
+			if (!password.Equals(key))
+				return null;
+			var claims = new List<Claim>
+			{
+				new Claim(System.IdentityModel.Claims.ClaimTypes.NameIdentifier, userName )
+			};
+			var claimsIdentity = new ClaimsIdentity(claims, "Basic");
+			
+			
+			return new ClaimsPrincipal(claimsIdentity);
 		}
 
 	}
