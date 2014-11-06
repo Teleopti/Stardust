d442587e12144257ca595bc7f85c2a125adf1304
Mingdi Wu <mingdi.wu@teleopti.com>
2014-11-06 14:13:22 +0800

    PBI #31070 UT case for reports controller

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Controllers/ReportsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Areas.Anywhere.Controllers;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider;
+using Teleopti.Ccc.Web.Core.RequestContext.Cookie;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WebTest.Areas.Anywhere.Controllers
+{
+	[TestFixture]
+	public class ReportsControllerTest
+	{
+
+		[Test]
+		public void ShouldGetReports()
+		{
+			var sessionSpecificDataProvider = MockRepository.GenerateMock<ISessionSpecificDataProvider>();
+			sessionSpecificDataProvider.Stub(x => x.GrabFromCookie())
+				.Return(new SessionSpecificData(Guid.NewGuid(), Guid.NewGuid().ToString(), Guid.NewGuid()));
+			var reportsProvider = MockRepository.GenerateMock<IReportsProvider>();
+			reportsProvider.Stub(x => x.GetReports()).Return(new List<IApplicationFunction>());
+
+			var target = new ReportsController(reportsProvider, sessionSpecificDataProvider);
+			
+			var result = target.GetReports();
+
+			result.Should().Be(new List<ReportItem>());
+		}
+	}
+}
