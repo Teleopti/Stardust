43b25342dc97959b6be09a406dfa1e1ccdf9995f
Tamas Balog <tamas.balogh@teleopti.com>
2014-11-06 09:23:48 +0100

    bugfix 30981 in 390: The filter should navigate correct after pressing a letter on keyboard
    --HG--
    branch : Release 7.5.390

--- Win/Grouping/PersonSelectorView.cs
+++ Win/Grouping/PersonSelectorView.cs
@@ -44,7 +44,7 @@ namespace Teleopti.Ccc.Win.Grouping
 			treeViewAdvMainTabTree.SortWithChildNodes = true;
 			treeViewAdvMainTabTree.Root.SortType = TreeNodeAdvSortType.Text;
 			treeViewAdvMainTabTree.Root.SortOrder = SortOrder.Ascending;
-	        treeViewAdvMainTabTree.AllowKeyboardSearch = false;
+	        treeViewAdvMainTabTree.AllowKeyboardSearch = true;
         }
 
 	    private void makeSureDropdownButtonWorksInRightToLeftCultures()
