f3f8c4e1604f04c844acf9772a6c6e3555636ad0
Roger Kratz <roger.kratz@teleopti.com>
2014-11-07 14:13:26 +0100

    fixing #31277 Don't know if the assert/logic is correct. Robin - please have a look!

--- Domain/Forecasting/Angel/Future/ForecastVolumeApplier.cs
+++ Domain/Forecasting/Angel/Future/ForecastVolumeApplier.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Forecasting.Angel.Future
@@ -7,6 +8,9 @@ namespace Teleopti.Ccc.Domain.Forecasting.Angel.Future
 	{
 		public void Apply(IWorkload workload, ITaskOwnerPeriod taskOwnerPeriod, IEnumerable<ITaskOwner> futureWorkloadDays)
 		{
+			if (!taskOwnerPeriod.TaskOwnerDayCollection.Any())
+				return;
+
 			var totalVolume = new TotalVolume();
 			var indexes = new IVolumeYear[]
 			{
@@ -14,7 +18,7 @@ namespace Teleopti.Ccc.Domain.Forecasting.Angel.Future
 				new WeekOfMonth(taskOwnerPeriod, new WeekOfMonthCreator()),
 				new DayOfWeeks(taskOwnerPeriod, new DaysOfWeekCreator())
 			};
-			totalVolume.Create(taskOwnerPeriod, futureWorkloadDays, indexes, new IOutlier[] {}, 0, 0, false, workload);
+			totalVolume.Create(taskOwnerPeriod, futureWorkloadDays, indexes, new IOutlier[] { }, 0, 0, false, workload);
 		}
 	}
 }
