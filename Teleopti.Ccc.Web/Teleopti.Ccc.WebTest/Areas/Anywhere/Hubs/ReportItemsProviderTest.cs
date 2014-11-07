d4c65889fcf3ed76db273ed4fc4147bad46f76bb
Mingdi Wu <mingdi.wu@teleopti.com>
2014-11-07 13:49:42 +0800

    PBI #31070 test ReportItemProvider

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Anywhere/Hubs/ReportItemsProviderTest.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Security.AuthorizationEntities;
+using Teleopti.Ccc.Web.Areas.Anywhere.Core;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Reports.DataProvider;
+using Teleopti.Ccc.Web.Core.RequestContext.Cookie;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WebTest.Areas.Anywhere.Hubs
+{
+	[TestFixture]
+	public class ReportItemProviderTest
+	{
+		private IReportsProvider _reportsProvider;
+		private ISessionSpecificDataProvider _sessionSpecificDataProvider;
+		private Guid _guid;
+
+		[SetUp]
+		public void Setup()
+		{
+			_guid = Guid.NewGuid();
+			_reportsProvider = MockRepository.GenerateMock<IReportsProvider>();
+			_sessionSpecificDataProvider = MockRepository.GenerateMock<ISessionSpecificDataProvider>();
+
+			_sessionSpecificDataProvider.Stub(x => x.GrabFromCookie())
+												 .Return(new SessionSpecificData(_guid, "DataSourceName", _guid));
+		}
+
+		[Test]
+		public void ShouldGetReportItems()
+		{
+			_reportsProvider.Stub(x => x.GetReports()).Return(new List<IApplicationFunction>() {new ApplicationFunction()});
+
+			var target = new ReportItemsProvider(_reportsProvider, _sessionSpecificDataProvider);
+			var result = target.GetReportItems();
+
+			result.Count.Should().Be(1);
+		}
+
+		[Test]
+		public void ShouldGetReportItemNameAndUrl()
+		{
+			const string localizedFunctionDescription = "report1";
+
+			var applicationFunciton = MockRepository.GenerateMock<IApplicationFunction>();
+			applicationFunciton.Stub(x => x.LocalizedFunctionDescription).Return(localizedFunctionDescription);
+			applicationFunciton.Stub(y => y.ForeignId).Return("foreignId");
+			_reportsProvider.Stub(x => x.GetReports()).Return(new List<IApplicationFunction>() { applicationFunciton });
+
+			var target = new ReportItemsProvider(_reportsProvider, _sessionSpecificDataProvider);
+			var result = target.GetReportItems();
+
+			result[0].Name.Should().Be.EqualTo(localizedFunctionDescription);
+			result[0].Url.Should().Be.EqualTo("Selection.aspx?ReportId=foreignId&BuId=" + _guid.ToString());
+		}
+
+		[Test]
+		public void ShouldSortReportItemsByName()
+		{
+			const string localizedFunctionDescription1 = "report1";
+			const string localizedFunctionDescription2 = "report2";
+
+			var applicationFunciton1 = MockRepository.GenerateMock<IApplicationFunction>();
+			applicationFunciton1.Stub(x => x.LocalizedFunctionDescription).Return(localizedFunctionDescription1);			
+			var applicationFunciton2 = MockRepository.GenerateMock<IApplicationFunction>();
+			applicationFunciton2.Stub(x => x.LocalizedFunctionDescription).Return(localizedFunctionDescription2);
+
+			var applicationFunctions = new List<IApplicationFunction>();
+			applicationFunctions.Add(applicationFunciton2);
+			applicationFunctions.Add(applicationFunciton1);
+			_reportsProvider.Stub(x => x.GetReports()).Return(applicationFunctions);
+
+			var target = new ReportItemsProvider(_reportsProvider, _sessionSpecificDataProvider);
+			var result = target.GetReportItems();
+
+			result[0].Name.Should().Be.EqualTo(localizedFunctionDescription1);
+			result[1].Name.Should().Be.EqualTo(localizedFunctionDescription2);
+		}
+	}
+}
