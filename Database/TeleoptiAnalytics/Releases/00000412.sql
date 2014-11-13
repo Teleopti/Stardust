347f473856cf6a875884ed12854ce9ccd2238dc0
Anders Forsberg <anders.forsberg@teleopti.com>
2014-11-13 14:20:28 +0100

    Automated database build: 8.1.412

---
+++ Database/TeleoptiAnalytics/Releases/00000412.sql
@@ -0,0 +1,16 @@
+----------------  
+--Name: Mathias Stenbom
+--Date: 2014-11-13
+--Desc: changes to actual agent state
+----------------  
+
+ALTER TABLE [RTA].[ActualAgentState] ALTER COLUMN [OriginalDataSourceId] NVARCHAR(50) NULL
+GO
+
+ALTER TABLE [RTA].[ActualAgentState] ADD [BusinessUnitId] uniqueidentifier NULL
+GO
+
+GO
+
+PRINT 'Adding build number to database' 
+INSERT INTO DatabaseVersion(BuildNumber, SystemVersion) VALUES (412,'8.1.412') 
