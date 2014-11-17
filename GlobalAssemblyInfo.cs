33651ff7b368d3da89f5f4cc52f652c3c2aeff3e
Roger Kratz <roger.kratz@teleopti.com>
2014-11-17 13:02:15 +0100

    Teleopti info in globalassemblyinfo

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,7 +2,17 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+/*
+ * Shared assembly info file
+ * Watch out if you change this one - affect lots of assemblies! 
+ */
+
 [assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Teleopti")]
+[assembly: AssemblyProduct("Teleopti WFM")]
+[assembly: AssemblyDescription("Teleopti WFM")]
+[assembly: AssemblyCopyright("Copyright © Teleopti AB 2014")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
