e64d4a8da301ea87679a603b792594d16ef1b27e
Claes Stenman <claes.stenman@teleopti.com>
2014-11-17 10:50:59 +0100

    PBI:#29846 only show intra interval issues on phone + chat...

--- Win/Scheduling/SkillResult/SkillDayGridControl.cs
+++ Win/Scheduling/SkillResult/SkillDayGridControl.cs
@@ -192,7 +192,7 @@ namespace Teleopti.Ccc.Win.Scheduling.SkillResult
 				gridRow.ChartSeriesSettings = ConfigureSetting(gridRow.DisplayMember);
 				GridRows.Add(_rowManager.AddRow(gridRow));
 
-				if (ToggleManager.IsEnabled(Toggles.Scheduler_IntraIntervalSolver_29845))
+				if (ToggleManager.IsEnabled(Toggles.Scheduler_IntraIntervalSolver_29845) && (skill.SkillType.ForecastSource == ForecastSource.InboundTelephony || skill.SkillType.ForecastSource == ForecastSource.Chat))
 				{
 					gridRow = new SkillDayGridIntervalIssues(_rowManager, "ReadOnlyPercentCell", "HighestDeviationInPeriod", Resources.LowestIntraIntervalBalance);
 				}
