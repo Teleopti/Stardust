f4be0e0964673b545c2ada711a8eb4c41b670894
Tamas Balog <tamas.balogh@teleopti.com>
2014-11-18 12:36:40 +0100

    finalize intra interval sikuli validators

--- Win/Sikuli/Validators/IntervalBalanceAfterValidator.cs
+++ Win/Sikuli/Validators/IntervalBalanceAfterValidator.cs
@@ -18,8 +18,13 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators
 		public SikuliValidationResult Validate()
 		{
 			var result = new SikuliValidationResult(true);
-			var lowestIntervalBalances = ValidatorHelper.GetDailyLowestIntraIntervalBalanceForPeriod(_schedulerState, _totalSkill);
-			var checkResult = lowestIntervalBalances.All(c => c > 0.8);
+			var lowestIntervalBalances = ValidatorHelper.GetDailyLowestIntraIntervalBalanceForPeriod(_schedulerState, _totalSkill.AggregateSkills[1]);
+			if (lowestIntervalBalances == null)
+			{
+				result.Result = false;
+				result.Details.AppendLine("Validator failure");
+			}
+			var checkResult = lowestIntervalBalances != null && lowestIntervalBalances.All(c => c >= 0.8);
 			result.Details.AppendLine("Details:");
 			if (checkResult)
 				result.Details.AppendLine("Lowest intra interval balance: OK");
--- Win/Sikuli/Validators/IntervalBalanceBeforeValidator.cs
+++ Win/Sikuli/Validators/IntervalBalanceBeforeValidator.cs
@@ -21,9 +21,15 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators
 
 		public SikuliValidationResult Validate()
 		{
+
 			var result = new SikuliValidationResult(true);
-			var lowestIntervalBalances = ValidatorHelper.GetDailyLowestIntraIntervalBalanceForPeriod(_schedulerState, _totalSkill);
-			var checkResult = lowestIntervalBalances.Any(c => c > 0.8);
+			var lowestIntervalBalances = ValidatorHelper.GetDailyLowestIntraIntervalBalanceForPeriod(_schedulerState, _totalSkill.AggregateSkills[1]);
+			if (lowestIntervalBalances == null)
+			{
+				result.Result = false;
+				result.Details.AppendLine("Validator failure");
+			}
+			var checkResult = lowestIntervalBalances != null && lowestIntervalBalances.Any(c => c > 0 && c < 0.8);
 			result.Details.AppendLine("Details:");
 			if (checkResult)
 				result.Details.AppendLine("Lowest intra interva balance: OK");
