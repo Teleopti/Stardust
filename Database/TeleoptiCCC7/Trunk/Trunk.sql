18480adaebb1a12e24451fcbde0793e1b47833d9
Team Real-time <greenmachine@teleopti.com>
2014-11-20 11:54:34 +0100

    PBI #31285 added unique index

--- Database/TeleoptiCCC7/Trunk/Trunk.sql
+++ Database/TeleoptiCCC7/Trunk/Trunk.sql
@@ -41,3 +41,30 @@ UPDATE [dbo].[ApplicationFunction] SET [ForeignId]=@ForeignId, [Parent]=@ParentI
 
 SET NOCOUNT OFF
 GO
+
+----------------  
+--Name: Xianwei Shen
+--Date: 2014-11-20
+--Desc: Add new table for adherence details
+---------------- 
+DROP TABLE [ReadModel].[AdherenceDetails];
+GO
+CREATE TABLE [ReadModel].[AdherenceDetails](
+	[PersonId] [uniqueidentifier] NOT NULL,
+	[BelongsToDate] [smalldatetime] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[StartTime] [datetime] NULL,
+	[ActualStartTime] [datetime] NULL,
+	[LastStateChangedTime] [datetime] NULL,
+	[IsInAdherence] [bit] NULL,
+	[TimeInAdherence] [bigint] NULL,
+	[TimeOutOfAdherence] [bigint] NULL,
+)
+GO
+CREATE UNIQUE CLUSTERED INDEX [UCI_AdherenceDetails] ON [ReadModel].[AdherenceDetails]
+(
+	[PersonId] ASC,
+	[BelongsToDate] ASC,
+	[StartTime] ASC
+)
+GO
