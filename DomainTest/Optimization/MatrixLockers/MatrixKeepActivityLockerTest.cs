5ab706fc3d6998f6c269e0537f33f4c7da9a6769
Claes Stenman <claes.stenman@teleopti.com>
2014-11-21 14:11:17 +0100

    Bugfix:#31342: Optimization sometimes moves activity set as "Do not move"
    --HG--
    branch : Release 7.5.390

---
+++ DomainTest/Optimization/MatrixLockers/MatrixKeepActivityLockerTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Optimization.MatrixLockers;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization.MatrixLockers
+{
+	[TestFixture]
+	public class MatrixKeepActivityLockerTest
+	{
+		private MatrixKeepActivityLocker _target;
+		private MockRepository _mock;
+		private IScheduleMatrixPro _scheduleMatrixPro;
+		private IList<IScheduleMatrixPro> _scheduleMatrixPros;
+		private IActivity _activity;
+		private IList<IActivity> _activities;
+		private DateOnly _dateOnly;
+		private IScheduleDayPro _scheduleDayPro;
+		private ReadOnlyCollection<IScheduleDayPro> _unlockedScheduleDayPros;
+		private IScheduleDay _scheduleDay;
+		private IPerson _person;
+		private DateTimePeriod _period;
+		private IProjectionService _projectionService;
+		private IVisualLayerCollection _visualLayerCollection;
+		private IVisualLayer _visualLayer;
+
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mock = new MockRepository();
+			_scheduleMatrixPro = _mock.StrictMock<IScheduleMatrixPro>();
+			_scheduleMatrixPros = new List<IScheduleMatrixPro>{_scheduleMatrixPro};
+			_activity = new Activity("activity");
+			_activities = new List<IActivity>{_activity};
+			_target = new MatrixKeepActivityLocker(_scheduleMatrixPros, _activities);
+			_dateOnly = new DateOnly(2014, 1, 1);
+			_scheduleDayPro = _mock.StrictMock<IScheduleDayPro>();
+			_unlockedScheduleDayPros = new ReadOnlyCollection<IScheduleDayPro>(new List<IScheduleDayPro>{_scheduleDayPro});
+			_scheduleDay = _mock.StrictMock<IScheduleDay>();
+			_person = new Person();
+			_period = new DateTimePeriod(new DateTime(2014, 1, 1, 0, 0, 0, DateTimeKind.Utc), new DateTime(2014, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+			_projectionService = _mock.StrictMock<IProjectionService>();
+			_visualLayer = new VisualLayer(_activity, _period, _activity,_person);
+			_visualLayerCollection = new VisualLayerCollection(_person, new List<IVisualLayer>{_visualLayer}, new ProjectionPayloadMerger());
+		}
+
+		[Test]
+		public void ShouldLockMatrixesHavingKeepActivity()
+		{
+			using (_mock.Record())
+			{
+				Expect.Call(_scheduleMatrixPro.UnlockedDays).Return(_unlockedScheduleDayPros);
+				Expect.Call(_scheduleDayPro.DaySchedulePart()).Return(_scheduleDay);
+				Expect.Call(_scheduleDay.HasProjection()).Return(true);
+				Expect.Call(_scheduleDay.ProjectionService()).Return(_projectionService);
+				Expect.Call(_projectionService.CreateProjection()).Return(_visualLayerCollection);
+				Expect.Call(_scheduleDayPro.Day).Return(_dateOnly);
+				Expect.Call(() => _scheduleMatrixPro.LockPeriod(new DateOnlyPeriod(_dateOnly, _dateOnly)));
+			}
+
+			using (_mock.Playback())
+			{
+				_target.Execute();
+			}
+		}
+	}
+}
