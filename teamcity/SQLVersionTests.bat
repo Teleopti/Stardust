51d17859934906d35a1f519090a78c29eabc3b75
David Jonsson <david.jonsson@teleopti.com>
2014-11-25 13:00:00 +0100

    Add Properties from calling batchfile

--- teamcity/SQLVersionTests.bat
+++ teamcity/SQLVersionTests.bat
@@ -8,7 +8,10 @@ CD "%WorkingDirectory%"
 .nuget\nuget.exe install .nuget\packages.config -o packages -source "http://hestia/nuget";"https://nuget.org/api/v2"
 
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
-"%MSBUILD%" "%~dp0SQLVersionTests.msbuild"
+"%MSBUILD%" /property:Configuration=debug "%WorkingDirectory%\Teleopti.Support.Security\Teleopti.Support.Security.csproj"
+"%MSBUILD%" /property:Configuration=debug "%WorkingDirectory%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager.csproj"
+
+"%MSBUILD%" /property:WorkingDirectory=%WorkingDirectory%;SQLServerInstance=%COMPUTERNAME% "%~dp0SQLVersionTests.msbuild"
 CD %~dp0
 PAUSE
 
