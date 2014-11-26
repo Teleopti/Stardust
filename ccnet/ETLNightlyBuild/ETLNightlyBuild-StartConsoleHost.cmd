8567e5b38495d4a60706b01ae172291a3bb1db34
Kunning Mao <none@none>
2014-11-26 10:37:14 +0100

    kill the process correctly

--- ccnet/ETLNightlyBuild/ETLNightlyBuild-StartConsoleHost.cmd
+++ ccnet/ETLNightlyBuild/ETLNightlyBuild-StartConsoleHost.cmd
@@ -1,8 +1,8 @@
 @ECHO off
 SET CCNetWorkDir=%~1
 SET Config=%~2
-tasklist /FI "IMAGENAME eq Teleopti.Analytics.Etl.ServiceConsoleHost.exe" 2>NUL | find /I /N "Teleopti.Analytics.Etl.ServiceConsoleHost.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-	taskkill /IM Teleopti.Analytics.Etl.ServiceConsoleHost.exe
+tasklist /FI "IMAGENAME eq Teleopti.Analytics.Etl.ServiceConsoleHost.exe" |find ":" > nul
+if "%ERRORLEVEL%"=="1" (
+	taskkill /F /IM Teleopti.Analytics.Etl.ServiceConsoleHost.exe
 )
 start %CCNetWorkDir%\Teleopti.Analytics.Etl.ServiceConsoleHost\bin\%Config%\Teleopti.Analytics.Etl.ServiceConsoleHost.exe
