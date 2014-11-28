9801a09ea18108387c06d5ac6320609d73d1f29f
David Jonsson <david.jonsson@teleopti.com>
2014-11-28 07:02:01 +0100

    Dont bother about sys schema

--- Database/Tools/NoHeapsCheck.sql
+++ Database/Tools/NoHeapsCheck.sql
@@ -16,6 +16,7 @@ SET @ErrorMessage = ''
 	LEFT OUTER JOIN sys.dm_db_index_usage_stats ius ON i.object_id = ius.object_id AND i.index_id = ius.index_id
 	WHERE i.type_desc = 'HEAP'
 	AND object_name(o.object_id) <> 'sysfiles1'
+	AND OBJECT_NAME(s.schema_id) <> 'sys'
 	ORDER BY s.name,object_name(o.object_id);
 	
 	OPEN cur;
