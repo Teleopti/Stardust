8cf760fdf0c7a494cbb76efec322a9831617c924
Tamas Balog <tamas.balogh@teleopti.com>
2014-12-01 12:30:51 +0100

    remove details header

--- Win/Sikuli/SikuliHelper.cs
+++ Win/Sikuli/SikuliHelper.cs
@@ -91,10 +91,7 @@ namespace Teleopti.Ccc.Win.Sikuli
 				validationResult.Details.AppendLine(string.Format("Duration = {0}", _timer.GetDuration().ToString(@"mm\:ss")));
 				_timer = null;
 			}
-			var details = validationResult.Details.ToString();
-			if (!string.IsNullOrWhiteSpace(details))
-				details = details.Insert(0, "Details:\n");
-			var testView = new SikuliResultView { Header = "Task Done", Result = validationResult.Result, Details = details };
+			var testView = new SikuliResultView { Header = "Task Done", Result = validationResult.Result, Details = validationResult.Details.ToString() };
 			testView.ShowDialog(owner);
 			CurrentValidator = SikuliValidatorRegister.None;
 		}
