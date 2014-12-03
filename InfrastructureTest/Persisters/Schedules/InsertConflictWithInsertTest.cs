495809dcc79d9acdfe3fceffdbaddd689ecea3f1
Roger Kratz <roger.kratz@teleopti.com>
2014-12-03 15:20:41 +0100

    #31611: no need to obfuscate infrastructure.dll any longer

--- InfrastructureTest/Persisters/Schedules/InsertConflictWithInsertTest.cs
+++ InfrastructureTest/Persisters/Schedules/InsertConflictWithInsertTest.cs
@@ -2,11 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using SharpTestsEx;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Infrastructure.Persisters.Schedules;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.InfrastructureTest.Persisters.Schedules
 {
+	public class Foo : ScheduleRangeConflictTest
+	{
+		private readonly DateOnly date = new DateOnly(2000, 1, 1);
+		private readonly DateTime start = new DateTime(2000, 1, 1, 10, 0, 0, DateTimeKind.Utc);
+
+		protected override IEnumerable<IPersistableScheduleData> Given()
+		{
+			var ass = new PersonAssignment(Person, Scenario, date);
+			ass.AddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)));
+			return new[] {ass};
+		}
+
+		protected override void WhenOtherHasChanged(IScheduleRange othersScheduleRange)
+		{
+			var day = othersScheduleRange.ScheduledDay(date);
+			day.PersonAssignment().Clear();
+			DoModify(day);
+		}
+
+		protected override void WhenImChanging(IScheduleRange myScheduleRange)
+		{
+			var day = myScheduleRange.ScheduledDay(date);
+			day.CreateAndAddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)), ShiftCategory);
+			day.CreateAndAddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)), ShiftCategory);
+			day.CreateAndAddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)), ShiftCategory);
+			day.CreateAndAddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)), ShiftCategory);
+			day.CreateAndAddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)), ShiftCategory);
+			day.CreateAndAddActivity(Activity, new DateTimePeriod(start, start.AddHours(2)), ShiftCategory);
+			DoModify(day);
+		}
+
+		protected override void Then(IEnumerable<PersistConflict> conflicts)
+		{
+		}
+
+		protected override void Then(IScheduleRange myScheduleRange)
+		{
+			Target.Persist(myScheduleRange);
+		}
+	}
+
 	public class InsertConflictWithInsertTest : ScheduleRangeConflictTest
 	{
 		private readonly DateOnly date = new DateOnly(2000, 1, 1);
