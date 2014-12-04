158311656830cb0f8e9f0768fa494ebbed386b7e
Mathias Stenbom <mathias@stenbom.com>
2014-12-04 11:42:31 +0100

    PBI: #31237 added hangfire

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Hangfire/HangfireServerStartupTask.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Hangfire/HangfireServerStartupTask.cs
@@ -1,35 +1,40 @@
 using System;
 using System.Threading.Tasks;
 using Autofac;
+using Hangfire;
+using Hangfire.SqlServer;
 using Owin;
 using Teleopti.Ccc.Domain;
 using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Web.Core.Startup.Booter;
-using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Web.Core.Hangfire
 {
 	[UseOnToggle(Toggles.RTA_HangfireEventProcessing_31593)]
+	[TaskPriority(100)]
 	public class HangfireServerStartupTask : IBootstrapperTask
 	{
-		private readonly Func<IApplicationData> _applicationData;
 		private readonly ILifetimeScope _lifetimeScope;
+		private readonly IConfigReader _config;
 
-		public HangfireServerStartupTask(Func<IApplicationData> applicationData, ILifetimeScope lifetimeScope)
+		public HangfireServerStartupTask(ILifetimeScope lifetimeScope, IConfigReader config)
 		{
-			_applicationData = applicationData;
 			_lifetimeScope = lifetimeScope;
+			_config = config;
 		}
 
 		public Task Execute(IAppBuilder application)
 		{
-			//application.UseHangfire(c =>
-			//{
-			//	var connectionString = _applicationData.Invoke().RegisteredDataSourceCollection.Single().Statistic.ConnectionString;
-			//	c.UseSqlServerStorage(connectionString, new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) });
-			//	c.UseAutofacActivator(_lifetimeScope);
-			//	c.UseServer();
-			//});
+			application.UseHangfire(c =>
+			{
+				c.UseSqlServerStorage(
+					_config.ConnectionStrings["Hangfire"].ConnectionString,
+					new SqlServerStorageOptions {QueuePollInterval = TimeSpan.FromSeconds(1)}
+					);
+				c.UseAutofacActivator(_lifetimeScope);
+				c.UseServer();
+			});
 			return null;
 		}
 	}
