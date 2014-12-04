44c9e6c396b7d5fb8ca2abf7aec0a6759223562e
Mathias Stenbom <mathias@stenbom.com>
2014-12-04 13:07:40 +0100

    PBI: #31237 making bootstrap tasks not require a priority

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Startup/Booter/Bootstrapper.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/Startup/Booter/Bootstrapper.cs
@@ -12,8 +12,8 @@ namespace Teleopti.Ccc.Web.Core.Startup.Booter
 				.OrderBy(t =>
 					{
 						var taskType = t.GetType();
-						var attributePrio = (TaskPriorityAttribute) taskType.GetCustomAttributes(typeof (TaskPriorityAttribute), false).Single();
-						return attributePrio.Priority;
+						var attributePrio = (TaskPriorityAttribute)taskType.GetCustomAttributes(typeof(TaskPriorityAttribute), false).FirstOrDefault();
+						return attributePrio != null ? attributePrio.Priority : int.MaxValue;
 					})
 				.ToArray();
 			return bootstrapperTasks.Select(t => t.Execute()).Where(x => x != null).ToArray();
