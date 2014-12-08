fde58a8d35737dff58fb93cfcb126f764f43d383
Xianwei Shen <xianwei.shen@teleopti.com>
2014-12-08 15:08:38 +0100

    notify when read model updates due to event stream

--- Infrastructure/LiteUnitOfWork/ReadModelUnitOfWorkAspect.cs
+++ Infrastructure/LiteUnitOfWork/ReadModelUnitOfWorkAspect.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using Newtonsoft.Json;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.Common;
+using Teleopti.Interfaces.MessageBroker;
+using Teleopti.Interfaces.MessageBroker.Client;
 
 namespace Teleopti.Ccc.Infrastructure.LiteUnitOfWork
 {
 	public class ReadModelUnitOfWorkAspect : IReadModelUnitOfWorkAspect
 	{
 		private readonly ICurrentDataSource _currentDataSource;
+		private readonly IMessageSender _messageSender;
+		private readonly ICurrentIdentity _currentIdentity;
 
-		public ReadModelUnitOfWorkAspect(ICurrentDataSource currentDataSource)
+		public ReadModelUnitOfWorkAspect(ICurrentDataSource currentDataSource, IMessageSender messageSender, ICurrentIdentity currentIdentity)
 		{
 			_currentDataSource = currentDataSource;
+			_messageSender = messageSender;
+			_currentIdentity = currentIdentity;
 		}
 
 		public void OnBeforeInvokation()
@@ -23,6 +30,20 @@ namespace Teleopti.Ccc.Infrastructure.LiteUnitOfWork
 		{
 			var factory = _currentDataSource.Current().ReadModel;
 			factory.EndUnitOfWork(exception);
+			if (exception == null)
+			{
+				_messageSender.Send(new Notification
+				{
+					DomainType = "ReadModelUpdatedMessage",
+					BinaryData = JsonConvert.SerializeObject(new ReadModelUpdatedMessage()),
+					DataSource = _currentDataSource.Current().DataSourceName,
+					BusinessUnitId = _currentIdentity.Current().BusinessUnit.Id.GetValueOrDefault().ToString()
+				});
+			}
 		}
+	}
+
+	public class ReadModelUpdatedMessage
+	{
 	}
 }
---
+++ InfrastructureTest/Rta/ReadModelUnitOfWorkAspectTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Security.Principal;
+using Teleopti.Ccc.Infrastructure.LiteUnitOfWork;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Infrastructure;
+using IMessageSender = Teleopti.Interfaces.MessageBroker.Client.IMessageSender;
+
+namespace Teleopti.Ccc.InfrastructureTest.Rta
+{
+	[TestFixture]
+	public class ReadModelUnitOfWorkAspectTest
+	{
+		[Test]
+		public void ShouldNotifyIfEventLeadsACommit()
+		{
+			var readModelFactory = MockRepository.GenerateMock<IReadModelUnitOfWorkFactory>();
+			var uowFactory = MockRepository.GenerateMock<IUnitOfWorkFactory>();
+			var identity = MockRepository.GenerateMock<ICurrentIdentity>();
+			identity.Stub(x => x.Current()).Return(new TeleoptiIdentity("", null, new BusinessUnit("bu").WithId(), null, null));
+			var datasource = MockRepository.GenerateMock<ICurrentDataSource>();
+			datasource.Stub(x => x.Current()).Return(new DataSource(uowFactory, null, readModelFactory, null));
+			var messageSender = MockRepository.GenerateMock<IMessageSender>();
+			var target = new ReadModelUnitOfWorkAspect(datasource, messageSender, identity);
+
+			messageSender.Expect(x => x.Send(null)).IgnoreArguments();
+
+			target.OnAfterInvokation(null);
+		}
+	}
+}
