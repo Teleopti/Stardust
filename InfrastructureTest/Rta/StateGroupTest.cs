3d4575c3ce3d7f1e15dff3e66b6b6f71da97ea3f
Erik Sundberg <erik.sundberg@teleopti.com>
2014-12-09 10:11:25 +0100

    BUG: #31511 Fix build

--- InfrastructureTest/Rta/StateGroupTest.cs
+++ InfrastructureTest/Rta/StateGroupTest.cs
@@ -3,80 +3,28 @@ using System.Data.SqlClient;
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
-using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.RealTimeAdherence;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.Rta;
-using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.InfrastructureTest.Helper;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.InfrastructureTest.Rta
 {
 	[TestFixture]
-	[StateGroupTest]
-	public class StateGroupTest 
+	public class StateGroupTest : DatabaseTestWithoutTransaction
 	{
-		[Test, Ignore]
-		public void ShouldAddStateCodeToDefaultStateGroup()
+		private void createDefaultStateGroup()
 		{
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-			{
-				var target = new DatabaseWriter(new DatabaseConnectionFactory(), new FakeDatabaseConnectionStringHandler());
-				
-				var defaultStateGroup = target.AddAndGetNewRtaState("phone", Guid.Empty, getBusinessUnitId(uow));
-
-				new RtaStateGroupRepository(uow).Get(defaultStateGroup.StateGroupId)
-					.StateCollection.Single()
-					.Name
-					.Should().Be.EqualTo("phone");
-			}
+			var stateGroup = new RtaStateGroup("default", true, true);
+			PersistAndRemoveFromUnitOfWork(stateGroup);
 		}
 
-		[Test, Ignore]
-		public void ShouldNotAddEmptyStateCode()
-		{
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-			{
-				var target = new DatabaseWriter(new DatabaseConnectionFactory(), new FakeDatabaseConnectionStringHandler());
-
-				var defaultStateGroup = target.AddAndGetNewRtaState("", Guid.Empty, getBusinessUnitId(uow));
-
-				new RtaStateGroupRepository(uow).Get(defaultStateGroup.StateGroupId)
-					.StateCollection
-					.Should().Be.Empty();
-			}
-		}
-
-		private static Guid getBusinessUnitId(IUnitOfWork uow)
-		{
-			return new BusinessUnitRepository(uow).LoadAll().Single().Id.Value;
-		}
-	}
-	
-
-	public class StateGroupTestAttribute : Attribute, ITestAction
-	{
-		private BusinessUnit _businessUnit;
-
-		public ActionTargets Targets { get { return ActionTargets.Test; } }
-
-		public void BeforeTest(TestDetails testDetails)
-		{
-			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
-			{
-				new BusinessUnitRepository(uow).Add(_businessUnit = BusinessUnitFactory.CreateSimpleBusinessUnit());
-				new RtaStateGroupRepository(uow).Add(StateGroupFactory.CreateDefaultStateGroup(_businessUnit));
-				
-				uow.PersistAll();
-			}
-		}
-
-		public void AfterTest(TestDetails testDetails)
+		private static void removeDefaultStateGroup()
 		{
 			applySql("DELETE FROM dbo.RtaState");
 			applySql("DELETE FROM dbo.RtaStateGroup");
-			applySql(string.Format("DELETE FROM dbo.BusinessUnit WHERE Id = '{0}'", _businessUnit.Id.Value));
 		}
 
 		private static void applySql(string sql)
@@ -89,6 +37,37 @@ namespace Teleopti.Ccc.InfrastructureTest.Rta
 					command.ExecuteNonQuery();
 				}
 			}
+		}
+
+		[Test]
+		public void ShouldAddStateCodeToDefaultStateGroup()
+		{
+			createDefaultStateGroup();
+			var target = new DatabaseWriter(new DatabaseConnectionFactory(), new FakeDatabaseConnectionStringHandler());
+
+			var defaultStateGroup = target.AddAndGetNewRtaState("phone", Guid.Empty, BusinessUnitFactory.BusinessUnitUsedInTest.Id.Value);
+
+			new RtaStateGroupRepository(UnitOfWork).Get(defaultStateGroup.StateGroupId)
+				.StateCollection.Single()
+				.Name
+				.Should().Be.EqualTo("phone");
+
+			removeDefaultStateGroup();
+		}
+
+		[Test]
+		public void ShouldNotAddEmptyStateCode()
+		{
+			createDefaultStateGroup();
+			var target = new DatabaseWriter(new DatabaseConnectionFactory(), new FakeDatabaseConnectionStringHandler());
+
+			var defaultStateGroup = target.AddAndGetNewRtaState("", Guid.Empty, BusinessUnitFactory.BusinessUnitUsedInTest.Id.Value);
+
+			new RtaStateGroupRepository(UnitOfWork).Get(defaultStateGroup.StateGroupId)
+				.StateCollection
+				.Should().Be.Empty();
+
+			removeDefaultStateGroup();
 		}
 	}
 }
