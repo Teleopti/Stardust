3af0692b179e1b3a7fb58b6aa644c1b302536629
Xinfeng Li <xinfeng.li@teleopti.com>
2014-12-12 13:58:28 +0800

    PBI #31654: Add new test case for StudentAvailabilityRestriction.

--- DomainTest/Scheduling/Restrictions/EffectiveRestrictionForDisplayCreatorTest.cs
+++ DomainTest/Scheduling/Restrictions/EffectiveRestrictionForDisplayCreatorTest.cs
@@ -22,7 +22,8 @@ namespace Teleopti.Ccc.DomainTest.Scheduling.Restrictions
 			var restrictionCollection = new[] {preference};
 			scheduleDay.Stub(x => x.RestrictionCollection()).Return(restrictionCollection);
 
-			var target = new EffectiveRestrictionForDisplayCreator(new RestrictionRetrievalOperation(), new RestrictionCombiner());
+			var target = new EffectiveRestrictionForDisplayCreator(new RestrictionRetrievalOperation(),
+				new RestrictionCombiner());
 			var result = target.MakeEffectiveRestriction(scheduleDay, effectiveRestrictionOptions);
 
 			result.ShiftCategory.Should().Be.EqualTo(preference.ShiftCategory);
@@ -45,12 +46,39 @@ namespace Teleopti.Ccc.DomainTest.Scheduling.Restrictions
 			var restrictionCollection = new[] { availability };
 			scheduleDay.Stub(x => x.RestrictionCollection()).Return(restrictionCollection);
 
-			var target = new EffectiveRestrictionForDisplayCreator(new RestrictionRetrievalOperation(), new RestrictionCombiner());
+			var target = new EffectiveRestrictionForDisplayCreator(new RestrictionRetrievalOperation(),
+				new RestrictionCombiner());
 			var result = target.MakeEffectiveRestriction(scheduleDay, effectiveRestrictionOptions);
 
 			result.StartTimeLimitation.Should().Be.EqualTo(availability.StartTimeLimitation);
 			result.EndTimeLimitation.Should().Be.EqualTo(availability.EndTimeLimitation);
 			result.WorkTimeLimitation.Should().Be.EqualTo(availability.WorkTimeLimitation);
+			result.IsAvailabilityDay.Should().Be.True();
+			result.IsRestriction.Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldCreateEffectiveRestrictionBasedOnStudentAvailability()
+		{
+			var studentAvailability = new StudentAvailabilityRestriction
+			{
+				StartTimeLimitation = new StartTimeLimitation(new TimeSpan(8, 0, 0), new TimeSpan(17, 0, 0)),
+				EndTimeLimitation = new EndTimeLimitation(new TimeSpan(8, 0, 0), new TimeSpan(17, 0, 0)),
+				WorkTimeLimitation = new WorkTimeLimitation(new TimeSpan(6, 0, 0), new TimeSpan(9, 0, 0))
+			};
+			var effectiveRestrictionOptions = new EffectiveRestrictionOptions { UseStudentAvailability = true };
+
+			var scheduleDay = MockRepository.GenerateMock<IScheduleDay>();
+			var restrictionCollection = new[] { studentAvailability };
+			scheduleDay.Stub(x => x.RestrictionCollection()).Return(restrictionCollection);
+
+			var target = new EffectiveRestrictionForDisplayCreator(new RestrictionRetrievalOperation(),
+				new RestrictionCombiner());
+			var result = target.MakeEffectiveRestriction(scheduleDay, effectiveRestrictionOptions);
+
+			result.StartTimeLimitation.Should().Be.EqualTo(studentAvailability.StartTimeLimitation);
+			result.EndTimeLimitation.Should().Be.EqualTo(studentAvailability.EndTimeLimitation);
+			result.WorkTimeLimitation.Should().Be.EqualTo(studentAvailability.WorkTimeLimitation);
 			result.IsAvailabilityDay.Should().Be.True();
 			result.IsRestriction.Should().Be.True();
 		}
