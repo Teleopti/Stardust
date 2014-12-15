d1e894cb5f9f7af673191473e7126b9e43dd38eb
Karin Jeppsson <karin.jeppsson@teleopti.com>
2014-12-15 10:13:04 +0100

    #30094 trunk
    --HG--
    branch : Release 7.5.390

--- Database/TeleoptiAnalytics/Trunk/Trunk.sql
+++ Database/TeleoptiAnalytics/Trunk/Trunk.sql
@@ -256,3 +256,10 @@ BEGIN
 	ALTER TABLE [mart].[sys_crossdatabaseview_custom] CHECK CONSTRAINT [FK_sys_crossdatabaseview_custom_sys_crossdatabaseview_target]
 END
 GO
+----------------  
+--Name: KJ
+--Date: 2014-12-12 
+--Desc: Remove timezone as a parameter from Agent Skill report
+----------------  
+DELETE FROM mart.report_control_collection WHERE collection_id=46 and control_collection_id=495
+GO
