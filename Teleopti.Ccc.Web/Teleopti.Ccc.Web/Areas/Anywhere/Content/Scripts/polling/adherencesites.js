555eeee734a3d2fcd3219c73706b6b7e77ee7aa3
Erik Sundberg <erik.sundberg@teleopti.com>
2014-12-15 14:31:16 +0100

    PBI: #31237 Adding js polling for site adherence based of toggleflag

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/polling/adherencesites.js
@@ -0,0 +1,49 @@
+define([
+	'jquery',
+	'ajax'
+], function (
+	$,
+	ajax
+	) {
+	var siteAdherencePoller = null;
+	var unsubscribeAdherence = function () {
+		if (!siteAdherencePoller)
+			return;
+		clearInterval(siteAdherencePoller);
+		siteAdherencePoller = null;
+	};
+
+	var mapToNotificationStyle = function(data) {
+		return {
+			DomainId: data.Id,
+			BinaryData: JSON.stringify(data)
+		}
+	}
+
+	return {
+		start: function () {
+			return $.Deferred().resolve();
+		},
+
+		subscribeAdherence: function (callback, businessUnitId, subscriptionDone) {
+			unsubscribeAdherence();
+
+			siteAdherencePoller = setInterval(function() {
+				ajax.ajax({
+					headers: { 'X-Business-Unit-Filter': businessUnitId },
+					url: "Sites/GetOutOfAdherenceForAllSites",
+					success: function (data) {
+						for (var i = 0; i < data.length; i++) {
+							callback(mapToNotificationStyle(data[i]));
+						}
+					}
+				});
+			}, 5000);
+
+			subscriptionDone();
+		},
+
+		unsubscribeAdherence: unsubscribeAdherence
+	};
+
+});
