46ff96f36dd49359f6523141880f7413a78f8d5f
Henke <henrik.andersson@teleopti.com>
2014-12-19 20:08:04 +0100

    sql

--- Database/TeleoptiCCC7/Releases/00000505.sql
+++ Database/TeleoptiCCC7/Releases/00000505.sql
@@ -4,14 +4,14 @@
 --Desc: Add new table for team adherence 
 ---------------- 
 
-DROP TABLE [ReadModel].[TeamAdherence];
+DROP TABLE [ReadModel].[TeamAdherence]
 GO
 CREATE TABLE [ReadModel].[TeamAdherence](
          [TeamId] [uniqueidentifier] NOT NULL,
          [AgentsOutOfAdherence] [int] NOT NULL,
 CONSTRAINT [PK_TeamAdherence] PRIMARY KEY CLUSTERED 
 (
-         [TeamId] ASC,   
+         [TeamId] ASC   
 )
 )
 GO
