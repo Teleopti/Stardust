f4c50af4460a39ba8bca3dbae77d03497cace92c
Kunning Mao <none@none>
2014-12-26 06:10:03 +0100

    #31909 Notify agent, Server Error in '/TeleoptiWFM/Web' Application.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Messages/MessagesAreaRegistration.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Messages/MessagesAreaRegistration.cs
@@ -14,6 +14,14 @@ namespace Teleopti.Ccc.Web.Areas.Messages
 
 		public override void RegisterArea(AreaRegistrationContext context) 
 		{
+			var mapRoute = context.MapRoute(
+				"Messages-authentication",
+				"Messages/Authentication/{action}",
+				new { controller = "Authentication", action = "SignIn", area = "Start", origin = "Messages" },
+				null,
+				new[] { "Teleopti.Ccc.Web.Areas.Start.*" });
+			mapRoute.DataTokens["area"] = "Start";
+
 			context.MapRoute(
 				"Messages_default",
 				"Messages/{controller}/{action}/{id}",
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Messages/MessagesAreaRegistrationTest.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Areas.Messages;
+
+namespace Teleopti.Ccc.WebTest.Areas.Messages
+{
+	[TestFixture]
+	public class MessagesAreaRegistrationTest
+	{
+
+		[Test]
+		public void MessagesShouldRouteMessagesArea()
+		{
+			var routes = new RouteCollection();
+			var target = new MessagesAreaRegistration();
+			var areaRegistrationContext = new AreaRegistrationContext(target.AreaName, routes);
+
+			target.RegisterArea(areaRegistrationContext);
+
+			var httpContext = MockRepository.GenerateMock<HttpContextBase>();
+			httpContext.Stub(c => c.Request.AppRelativeCurrentExecutionFilePath).Return("~/Messages/Controller/Action");
+			var routeData = routes.GetRouteData(httpContext);
+
+			routeData.Values["controller"].Should().Be("Controller");
+			routeData.Values["action"].Should().Be("Action");
+		}
+
+		[Test]
+		public void ShouldRouteMessagesAuthenticationToStartArea()
+		{
+			var routes = new RouteCollection();
+			var target = new MessagesAreaRegistration();
+			var areaRegistrationContext = new AreaRegistrationContext(target.AreaName, routes);
+
+			target.RegisterArea(areaRegistrationContext);
+
+			var httpContext = MockRepository.GenerateMock<HttpContextBase>();
+			httpContext.Stub(c => c.Request.AppRelativeCurrentExecutionFilePath).Return("~/Messages/Authentication/SignIn");
+			var routeData = routes.GetRouteData(httpContext);
+
+			var namespaces = routeData.DataTokens["namespaces"] as IEnumerable<string>;
+			namespaces.Should().Have.SameValuesAs("Teleopti.Ccc.Web.Areas.Start.*");
+			routeData.Values["origin"].Should().Be("Messages");
+			routeData.DataTokens["area"].Should().Be("Start");
+		}
+	}
+}
