0f331c0dc353a98e288de8bf060ba883ae427424
David Jonsson <david.jonsson@teleopti.com>
2014-12-29 17:47:16 +0100

    fix build

--- Database/Create/IndexMaintenance/dbo.IndexMaintenance_Azure_v11.sql
+++ Database/Create/IndexMaintenance/dbo.IndexMaintenance_Azure_v11.sql
@@ -15,21 +15,56 @@ DECLARE @ReturnCode int
 DECLARE @EndMessage nvarchar(max)
 DECLARE @DatabaseMessage nvarchar(max)
 DECLARE @ErrorMessage nvarchar(max)
-
+DECLARE @CurrentExtendedInfo xml
+DECLARE @db_id int
 
 SET @DatabaseName = db_name()
+SET @db_id = db_id()
+
 SET @CurrentCommand = ''
 SET @Error = 0
 SET @ReturnCode = 0
 
-
 DECLARE @avg_fragmentation_in_percent smallint
 SET @avg_fragmentation_in_percent = 10
+
+-- DROP TABLE #dm_db_index_physical_stats_tmp
+CREATE TABLE #dm_db_index_physical_stats_tmp(
+	[database_id] [smallint] NULL,
+	[object_id] [int] NULL,
+	[index_id] [int] NULL,
+	[partition_number] [int] NULL,
+	[index_type_desc] [nvarchar](60) NULL,
+	[alloc_unit_type_desc] [nvarchar](60) NULL,
+	[index_depth] [tinyint] NULL,
+	[index_level] [tinyint] NULL,
+	[avg_fragmentation_in_percent] [float] NULL,
+	[fragment_count] [bigint] NULL,
+	[avg_fragment_size_in_pages] [float] NULL,
+	[page_count] [bigint] NULL,
+	[avg_page_space_used_in_percent] [float] NULL,
+	[record_count] [bigint] NULL,
+	[ghost_record_count] [bigint] NULL,
+	[version_ghost_record_count] [bigint] NULL,
+	[min_record_size_in_bytes] [int] NULL,
+	[max_record_size_in_bytes] [int] NULL,
+	[avg_record_size_in_bytes] [float] NULL,
+	[forwarded_record_count] [bigint] NULL,
+	[compressed_page_count] [bigint] NULL
+)
+
+--get frag figures for all indexes
+INSERT INTO #dm_db_index_physical_stats_tmp
+SELECT database_id, object_id, index_id, partition_number, index_type_desc, alloc_unit_type_desc, index_depth, index_level, avg_fragmentation_in_percent, fragment_count, avg_fragment_size_in_pages, page_count, avg_page_space_used_in_percent, record_count, ghost_record_count, version_ghost_record_count, min_record_size_in_bytes, max_record_size_in_bytes, avg_record_size_in_bytes, forwarded_record_count, compressed_page_count
+FROM sys.dm_db_index_physical_stats(@db_id, null, null, null, 'LIMITED') ips
+WHERE avg_fragmentation_in_percent > 10
+AND alloc_unit_type_desc = 'IN_ROW_DATA'
+AND index_level = 0
 
 --Get tables with fragmentation on table or its index worse than 10%
 DECLARE TableCursor CURSOR FOR
 (
-	SELECT
+	SELECT DISTINCT
 		sc.name AS SchemaName,
 		OBJECT_NAME(ps.object_id) AS TableName
 	 FROM sys.dm_db_partition_stats ps
@@ -40,12 +75,10 @@ DECLARE TableCursor CURSOR FOR
 	 INNER JOIN sys.indexes i
 	 ON ps.object_id = i.object_id
 	 AND ps.index_id = i.index_id
-	 CROSS APPLY sys.dm_db_index_physical_stats(DB_ID(), ps.object_id, ps.index_id, null, 'LIMITED') ips
+	 CROSS APPLY #dm_db_index_physical_stats_tmp ips
 	 GROUP BY
 		sc.name,
 		OBJECT_NAME(ps.object_id)
-	 HAVING max(ips.avg_fragmentation_in_percent) > @avg_fragmentation_in_percent
-
 )
 
 --Rebuild
