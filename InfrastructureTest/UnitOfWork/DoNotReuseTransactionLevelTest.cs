a71d734ce27e9cee2ea14631068c989f9685b3e6
Roger Kratz <roger.kratz@teleopti.com>
2015-01-12 14:50:12 +0100

    #29571 Verified it with test. Might not work in older sql servers though... We need to fix #31662 first!

---
+++ InfrastructureTest/UnitOfWork/DoNotReuseTransactionLevelTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.InfrastructureTest.Helper;
+using Teleopti.Interfaces.Infrastructure;
+
+namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork
+{
+	public class DoNotReuseTransactionLevelTest : DatabaseTest
+	{
+		[Test]
+		public void ShouldBeResetAfterExplcitlySetInEarlierTransaction()
+		{
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork(TransactionIsolationLevel.Serializable))
+			{
+				startTransactionByMakingDummyCall(uow);
+			}
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			{
+				serverTransactionLevel(uow)
+					.Should().Be.EqualTo(2);
+			}
+		}
+
+		private static void startTransactionByMakingDummyCall(IUnitOfWork uow)
+		{
+			uow.FetchSession().CreateSQLQuery("select 1").UniqueResult<int>();
+		}
+
+		/// <returns>
+		/// 0 = Unspecified
+		/// 1 = ReadUncomitted
+		/// 2 = ReadCommitted
+		/// 3 = Repeatable
+		/// 4 = Serializable
+		/// 5 = Snapshot
+		/// </returns>
+		private static int serverTransactionLevel(IUnitOfWork uow)
+		{
+			const string sql = "select transaction_isolation_level from sys.dm_exec_sessions where session_id=@@spid";
+			return uow.FetchSession().CreateSQLQuery(sql).UniqueResult<short>();
+		}
+	}
+}
