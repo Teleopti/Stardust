1f882092e43b6c1c1d2f11362593395de1aca296
Roger Kratz <roger.kratz@teleopti.com>
2015-01-15 14:32:28 +0100

    First little stub for application authentication

---
+++ Infrastructure/Authentication/IApplicationAuthentication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Teleopti.Ccc.Infrastructure.Authentication
+{
+	public interface IApplicationAuthentication
+	{
+		IdentificationResult TryAuthenticate(string userName, string password);
+	}
+
+	public class IdentificationResult
+	{
+		public bool Success { get; private set; }
+		public Guid PersonId { get; private set; }
+		public string Tennant { get; private set; }
+	}
+}
