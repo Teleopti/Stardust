77c02f82436959e4a17f854c9124701b884db1c4
Roger Kratz <roger.kratz@teleopti.com>
2015-01-16 10:44:49 +0100

    Stub for Ola

---
+++ Domain/Security/MultiTenancyAuthentication/AuthenticationQueryResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Teleopti.Ccc.Domain.Security.MultiTenancyAuthentication
+{
+	public class AuthenticationQueryResult
+	{
+		public bool Success { get; set; }
+		public Guid PersonId { get; set; }
+		public string Tennant { get; set; }
+	}
+}
