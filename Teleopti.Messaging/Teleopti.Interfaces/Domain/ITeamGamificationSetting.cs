82f33e39bbc8c469536a65d308c011bd7701612c
Zhiping Lan <zhiping.lan@teleopti.com>
2015-01-20 14:03:15 +0800

    PBI #31318 add domain interface

---
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/ITeamGamificationSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teleopti.Interfaces.Domain
+{
+	public interface ITeamGamificationSetting : IAggregateRoot, IBelongsToBusinessUnit, ICloneableEntity<ITeamGamificationSetting>
+	{
+		ITeam Team { get; set; }
+		IGamificationSetting GamificationSetting { get; set; }
+	}
+}
