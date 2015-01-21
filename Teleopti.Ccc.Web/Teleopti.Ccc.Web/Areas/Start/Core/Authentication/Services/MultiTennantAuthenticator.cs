f7d4db20368d87e05f1db2b5b9d27930e8cc83ea
Roger Kratz <roger.kratz@teleopti.com>
2015-01-21 14:29:15 +0100

    Removing TennantUnitOfWork from service. To be placed on controller later.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Core/Authentication/Services/MultiTennantAuthenticator.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Core/Authentication/Services/MultiTennantAuthenticator.cs
@@ -1,5 +1,4 @@
 ﻿using Teleopti.Ccc.Domain.Repositories;
-using Teleopti.Ccc.Infrastructure.MultiTenancy.NHibernate;
 using Teleopti.Ccc.Web.Areas.Start.Core.Authentication.DataProvider;
 using Teleopti.Ccc.Web.Areas.Tennant.Core;
 using Teleopti.Interfaces.Domain;
@@ -14,7 +13,6 @@ namespace Teleopti.Ccc.Web.Areas.Start.Core.Authentication.Services
 		private readonly IApplicationAuthentication _applicationAuthentication;
 		private readonly IIdentityAuthentication _identityAuthentication;
 
-
 		public MultiTennantAuthenticator(IDataSourcesProvider dataSourceProvider,
 			ITokenIdentityProvider tokenIdentityProvider,
 			IRepositoryFactory repositoryFactory,
@@ -28,7 +26,6 @@ namespace Teleopti.Ccc.Web.Areas.Start.Core.Authentication.Services
 			_identityAuthentication = identityAuthentication;
 		}
 
-		[TennantUnitOfWork]
 		public AuthenticateResult AuthenticateWindowsUser(string dataSourceName)
 		{
 			var winAccount = _tokenIdentityProvider.RetrieveToken();
@@ -65,7 +62,6 @@ namespace Teleopti.Ccc.Web.Areas.Start.Core.Authentication.Services
 			return null;
 		}
 
-		[TennantUnitOfWork]
 		public AuthenticateResult AuthenticateApplicationUser(string dataSourceName, string userName, string password)
 		{
 			var result = _applicationAuthentication.Logon(userName, password);
@@ -77,12 +73,10 @@ namespace Teleopti.Ccc.Web.Areas.Start.Core.Authentication.Services
 				{
 					var person = _repositoryFactory.CreatePersonRepository(uow).LoadOne(result.PersonId);
 					return new AuthenticateResult { DataSource = dataSource, Person = person, Successful = true, HasMessage = !string.IsNullOrEmpty(result.FailReason), Message = result.FailReason, PasswordExpired = false };
-
 				}
 			}
 
 			return new AuthenticateResult { DataSource = null, Person = null, Successful = false, HasMessage = !string.IsNullOrEmpty(result.FailReason), Message = result.FailReason, PasswordExpired = false };
-			
 		}
 
 		public void SaveAuthenticateResult(string userName, AuthenticateResult result)
