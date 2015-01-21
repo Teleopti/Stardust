bfb2de13da7fe96c33d11621529c811ecf215c88
Roger Kratz <roger.kratz@teleopti.com>
2015-01-21 12:21:00 +0100

    changing copyright string in assembly info.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Teleopti")]
 [assembly: AssemblyProduct("Teleopti WFM")]
 [assembly: AssemblyDescription("Teleopti WFM")]
-[assembly: AssemblyCopyright("Copyright © Teleopti AB 2014")]
+[assembly: AssemblyCopyright("Copyright © Teleopti AB 2009–2015")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
