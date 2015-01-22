cbe13ec5ca0b2f1bae75c32090d1fcb101994ea6
Roger Kratz <roger.kratz@teleopti.com>
2015-01-22 10:22:29 +0100

    #32084 trying to get rid of hard coded paths
    --HG--
    branch : Release 7.5.390

--- WISE/CompileAll.bat
+++ WISE/CompileAll.bat
@@ -8,6 +8,7 @@ SET /A ERRORLEV=0
 SET Version=%1
 SET ProductId=%2
 SET OUTDIR=%3
+SET DEPLOYSHARE=%4
 
 ::Get the number of input params
 SET /a argc=0
@@ -92,8 +93,6 @@ ECHO WISEPROJFILE: %WISEPROJFILE%
 ECHO DYNAMICCONTENT: %DYNAMICCONTENT%
 SET WISEEXE=C:\Program Files (x86)\Altiris\Wise\Windows Installer Editor\WfWI.exe
 SET WISESCRIPT=C:\Program Files (x86)\Altiris\Wise\WiseScript Package Editor\Wise32.exe
-SET DEPLOYSHARE=D:\Installation\PreviousBuilds
-SET AZUREOUTDIR=D:\Installation\Azure
 
 ::Set Product folders. note: Folder namd must correspond to .wsi-file name. See: sub batch file: CompileWise.bat
 SET ccc7_server=ccc7_server
