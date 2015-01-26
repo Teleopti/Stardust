5b12f355000179dc2d341b28aa521c5bd78a23fa
Mathias Stenbom <mathias.stenbom@teleopti.com>
2015-01-26 11:59:19 +0100

    PBI: #31237 should not open 100000 instances of iisexpress.exe when BeforeTestRun is retried after failure any more.

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/EventDefinition.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/EventDefinition.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using TechTalk.SpecFlow;
+using Teleopti.Ccc.Domain;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.WebBehaviorTest.Core;
 using Teleopti.Ccc.WebBehaviorTest.Data;
@@ -11,28 +12,53 @@ using Teleopti.Ccc.WebBehaviorTest.Toggle;
 
 namespace Teleopti.Ccc.WebBehaviorTest.Bindings
 {
+	public class BeforeTestRunException : Exception
+	{
+		public BeforeTestRunException(Exception innerException)
+			: base("Exception occurred in BeforeTestRun", innerException)
+		{
+		}
+	}
+
 	[Binding]
 	public class EventDefinition
 	{
 		private static readonly ILog log = LogManager.GetLogger(typeof(EventDefinition));
+		private static Exception _beforeTestRunException;
 
+		// THIS METHOD WILL BE RETRIED WHEN FAILED!
+		// So we catch all exceptions...
+		// .. so that it wont be retried
+		// .. and rethrow in BeforeScenario so all tests will fail with the same exception
+		// .. and dont run AfterScenario at all
+		// .. and let AfterTestRun clean up
 		[BeforeTestRun]
 		public static void BeforeTestRun()
 		{
-			log4net.Config.XmlConfigurator.Configure();
+			try
+			{
+				log4net.Config.XmlConfigurator.Configure();
 
-			log.Debug("Preparing for test run");
+				log.Debug("Preparing for test run");
 
-			Browser.SetDefaultTimeouts(TimeSpan.FromSeconds(20), TimeSpan.FromMilliseconds(25));
-			TestSiteConfigurationSetup.Setup();
-			TestDataSetup.CreateDataSourceAndStartWebApp();
+				Browser.SetDefaultTimeouts(TimeSpan.FromSeconds(20), TimeSpan.FromMilliseconds(25));
+				TestSiteConfigurationSetup.Setup();
+				TestDataSetup.CreateDataSourceAndStartWebApp();
 
-			log.Debug("Starting test run");
+				log.Debug("Starting test run");
+			}
+			catch (Exception e)
+			{
+				_beforeTestRunException = e;
+			}
 		}
 		
 		[BeforeScenario]
 		public static void BeforeScenario()
 		{
+			if (_beforeTestRunException != null)
+				throw new BeforeTestRunException(_beforeTestRunException);
+
 			log.Debug("Preparing for scenario " + ScenarioContext.Current.ScenarioInfo.Title);
 			Browser.SelectBrowserByTag();
 
@@ -60,7 +86,7 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings
 
 		private static readonly string targetTestDataNHibFile = Path.Combine(Paths.WebBinPath(), "TestData2.nhib.xml");
 
-        private static void addExtraDataSource()
+		private static void addExtraDataSource()
         {
             if (!ScenarioContext.Current.ScenarioInfo.Tags.Contains("ExtraDataSource")) return;
             FileConfigurator.ConfigureByTags(
@@ -73,6 +99,9 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings
 		[AfterScenario]
 		public static void AfterScenario()
 		{
+			if (_beforeTestRunException != null)
+				return;
+
 			log.Debug("Cleaning up after scenario " + ScenarioContext.Current.ScenarioInfo.Title);
             
             Browser.Interactions.GoTo("about:blank");
