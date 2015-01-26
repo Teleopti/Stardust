1031400a281c1e79ab505dc78734156d31adc357
Ola <none@none>
2015-01-26 10:14:14 +0100

    first test of loading the nhib confs

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Tennant/Core/NHibernateConfigurationsHandler.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Tennant/Core/NHibernateConfigurationsHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using Teleopti.Ccc.Domain.Security;
+using Teleopti.Ccc.Web.Core.Startup.InitializeApplication;
 using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Web.Areas.Tennant.Core
@@ -12,45 +15,43 @@ namespace Teleopti.Ccc.Web.Areas.Tennant.Core
 
 	public class NHibernateConfigurationsHandler : INHibernateConfigurationsHandler
 	{
+		private readonly ISettings _settings;
 		readonly IList<IDataSourceHolder> _dataSources = new List<IDataSourceHolder>();
-		private string nhibConf = @"<datasource>
-	<hibernate-configuration xmlns='urn:nhibernate-configuration-2.2'>
-		<session-factory name='Teleopti WFM'>
-			<property name='connection.connection_string'>
-		
-        Data Source=.;Integrated Security=SSPI;Initial Catalog=main_clone_DemoSales_TeleoptiCCC7;Current Language=us_english
-      </property>
-			<property name='command_timeout'>60</property>
-		</session-factory>
-	</hibernate-configuration>
-	<matrix name='MatrixDatamartDemo'>
-		<connectionString>
-		<!--WISEMETA: default='[SQL_AUTH_STRING];Initial Catalog=[DB_ANALYTICS];Current Language=us_english'-->
-		Data Source=.;Integrated Security=SSPI;Initial Catalog=main_clone_DemoSales_TeleoptiAnalytics;Current Language=us_english
-    </connectionString>
-	</matrix>
-	<authentication>
-		<logonMode>mix</logonMode>
-		<!--  win or mix -->
-	</authentication>
-</datasource>";
 
-		public NHibernateConfigurationsHandler()
+		public NHibernateConfigurationsHandler(ISettings settings)
 		{
-			//behöver veta nhibkatalogen sedan
-
-			_dataSources.Add(new DataSourceHolder{DataSourceName = "Teleopti WFM", DataSourceConfig = Encryption.EncryptStringToBase64(nhibConf,
-																		  EncryptionConstants.Image1,
-																		  EncryptionConstants.Image2)});
-			
+			_settings = settings;
 		}
+
 		public string GetConfigForName(string dataSourceName)
 		{
+			if (!_dataSources.Any())
+				loadDataSources();
 			foreach (var dataSourceHolder in _dataSources.Where(dataSourceHolder => dataSourceHolder.DataSourceName.Equals(dataSourceName)))
 			{
 				return dataSourceHolder.DataSourceConfig;
 			}
 			return "";
+		}
+
+		private void loadDataSources()
+		{
+			var nhibPath = _settings.nhibConfPath();
+
+			foreach (string file in Directory.GetFiles(nhibPath, "*.nhib.xml"))
+			{
+				XElement element = XElement.Load(file);
+				if (element.Name != "datasource")
+					continue;
+
+				_dataSources.Add(new DataSourceHolder
+				{
+					DataSourceName = element.Descendants().ElementAt(1).Attribute("name").Value,
+					DataSourceConfig = Encryption.EncryptStringToBase64(element.ToString(),
+						EncryptionConstants.Image1,
+						EncryptionConstants.Image2)
+				});
+			}
 		}
 	}
 
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Tennant/Core/NHibernateConfigurationsHandlerTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SharpTestsEx;
+using Teleopti.Ccc.Web.Areas.Tennant.Core;
+using Teleopti.Ccc.Web.Core.Startup.InitializeApplication;
+
+namespace Teleopti.Ccc.WebTest.Areas.Tennant.Core
+{
+	public class NHibernateConfigurationsHandlerTest
+	{
+		private const string nhibConf = @"<?xml version='1.0' encoding='utf-8'?>
+		<datasource>
+			<hibernate-configuration xmlns='urn:nhibernate-configuration-2.2'>
+				<session-factory name='Teleopti WFM'>
+					<property name='connection.connection_string'>
+				
+		        Data Source=.;Integrated Security=SSPI;Initial Catalog=main_clone_DemoSales_TeleoptiCCC7;Current Language=us_english
+		      </property>
+					<property name='command_timeout'>60</property>
+				</session-factory>
+			</hibernate-configuration>
+			<matrix name='MatrixDatamartDemo'>
+				<connectionString>
+				<!--WISEMETA: default='[SQL_AUTH_STRING];Initial Catalog=[DB_ANALYTICS];Current Language=us_english'-->
+				Data Source=.;Integrated Security=SSPI;Initial Catalog=main_clone_DemoSales_TeleoptiAnalytics;Current Language=us_english
+		    </connectionString>
+			</matrix>
+			<authentication>
+				<logonMode>mix</logonMode>
+				<!--  win or mix -->
+			</authentication>
+		</datasource>";
+
+		private INHibernateConfigurationsHandler _target;
+		private ISettings _settings;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_settings = MockRepository.GenerateMock<ISettings>();
+		}
+
+		[Test]
+		public void ShouldLoadDataSources()
+		{
+			
+			var path = Path.GetTempPath();
+			var filename = Guid.NewGuid() + ".nhib.xml";
+			var writer = new StreamWriter(path + filename);
+
+			writer.Write(nhibConf);
+			writer.Close();
+			_settings.Stub(x => x.nhibConfPath()).Return(path);
+
+			_target = new NHibernateConfigurationsHandler(_settings);
+			var result = _target.GetConfigForName("Teleopti WFM");
+			result.Should().Not.Be.EqualTo("");
+			File.Delete(path + filename);
+		}
+	}
+}
