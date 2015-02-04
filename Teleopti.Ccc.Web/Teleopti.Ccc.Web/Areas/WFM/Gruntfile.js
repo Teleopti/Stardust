0e28412bf6bdf9e30159155a03e1651873a9eb8e
Carl.Rockman@teleopti.com <Carl.Rockman@teleopti.com>
2015-02-04 13:00:08 +0100

    wfm web - grunt and scss

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/Gruntfile.js
@@ -0,0 +1,30 @@
+﻿module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		sass: {
+			dist: {
+				files: {
+					'css/main.css': 'css/main.scss'
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['css/*.scss'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+	});
+
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s).
+	grunt.registerTask('default', ['sass','watch']);
+
+};
