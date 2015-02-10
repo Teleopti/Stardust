69fbe7c0e3ec1473f3d0ea2a2424f0b6a5f83dd4
Ola <none@none>
2015-02-10 15:55:56 +0100

    fix some routing

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Reporting/ReportingAreaRegistration.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Reporting/ReportingAreaRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using Microsoft.ReportingServices.Interfaces;
 
 namespace Teleopti.Ccc.Web.Areas.Reporting
 {
@@ -14,15 +15,17 @@ namespace Teleopti.Ccc.Web.Areas.Reporting
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
-			  //context.Routes.MapPageRoute("reports",
-			  // "Reporting/{controller}/{action}/{query}",
-			  // "http://localhost:52858/Areas/Reporting/Index.aspx{query}"
-			  // );
-
 			  context.MapRoute(
 				"Reporting_default",
+				 "Reporting/{controller}/{id}",
+				 new { Controller = "Report", action = "Index", id = UrlParameter.Optional }
+
+			  );
+
+			  context.MapRoute(
+				"Reporting_index",
 				 "Reporting/{controller}/{action}/{id}",
-				 new { action = "Index", id = UrlParameter.Optional }
+				 new { Controller = "Report", action = "Index", id = UrlParameter.Optional }
 
 			  );
 			  
