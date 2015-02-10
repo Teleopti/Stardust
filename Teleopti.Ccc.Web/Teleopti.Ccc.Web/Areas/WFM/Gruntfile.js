86fdd6f7ed7165343f9656f914f3407ccd32ff90
blandine descamps <blandine.descamps@teleopti.com>
2015-02-10 17:00:23 +0100

    wfm web - test build js with grunt as a build step

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/Gruntfile.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/WFM/Gruntfile.js
@@ -24,17 +24,17 @@
 				separator: '\r\n'
 			},
 			dist: {
-				src: ['node_modules/angular/angular.js',
+				src: ['node_modules/angular/angular.min.js',
 					'node_modules/angular-ui-router/release/angular-ui-router.min.js',
 					'node_modules/angular-resource/angular-resource.min.js',
 					'vendor/ui-bootstrap.min.js',
 					'node_modules/moment/min/moment-with-locales.min.js',
-					'node_modules/angular-moment/angular-moment.js',
+					'node_modules/angular-moment/angular-moment.min.js',
 					'vendor/angular-ui-tree/angular-ui-tree.min.js',
-					'vendor/angular-aria/angular-aria.js',
-					'vendor/angular-animate/angular-animate.js',
-					'vendor/hammerjs/hammer.js',
-					'vendor/angular-material/angular-material.js',
+					'vendor/angular-aria/angular-aria.min.js',
+					'vendor/angular-animate/angular-animate.min.js',
+					'vendor/hammerjs/hammer.min.js',
+					'vendor/angular-material/angular-material.min.js',
 					'js/**/*.js'],
 				dest: 'dist/main.js'
 			}
@@ -42,7 +42,8 @@
 
 		uglify: {
 			options: {
-				banner: '/*! <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+				banner: '/*! <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+				sourceMap: true
 			},
 			dist: {
 				files: {
@@ -61,5 +62,5 @@
 
 	// Default task(s).
 	grunt.registerTask('default', ['sass', 'watch']); // this task watchs
-	grunt.registerTask('dist', ['concat']); // this task is kind of package
+	grunt.registerTask('dist', ['concat', 'uglify']); // this task is kind of package
 };
