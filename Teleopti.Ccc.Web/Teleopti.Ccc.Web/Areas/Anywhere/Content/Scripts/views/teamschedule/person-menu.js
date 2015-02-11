4747bb656037c6f0b589fe4e8264fe19992bf835
Chundan Xu <chundan.xu@teleopti.com>
2015-02-11 11:03:39 +0800

    PBI #31897 add all other menus except for moving activity to Person Menu.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/teamschedule/person-menu.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Anywhere/Content/Scripts/views/teamschedule/person-menu.js
@@ -10,8 +10,14 @@ define([
 		this.urlForAddFullDayAbsence = function () {
 			return navigation.UrlForPersonScheduleAddFullDayAbsence(self.BusinessUnitId, self.GroupId, self.PersonId, self.Date);
 		}
+		this.urlForAddIntradayAbsence = function () {
+			return navigation.UrlForPersonScheduleAddIntradayAbsence(self.BusinessUnitId, self.GroupId, self.PersonId, self.Date);
+		};
 		this.urlForAddActivity = function () {
 			return navigation.UrlForPersonScheduleAddActivity(self.BusinessUnitId, self.GroupId, self.PersonId, self.Date);
 		}
+		this.urlForRemoveAbsence = function () {
+			return navigation.UrlForPersonSchedule(self.BusinessUnitId, self.GroupId, self.PersonId, self.Date);
+		};
 	};
 });
