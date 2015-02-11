a0aac7fa212b797b0e2a7915b7c41d14910c4e2d
Kunning Mao <none@none>
2015-02-11 11:37:55 +0100

    #31115 add a ping to azure script

---
+++ teamcity/Azure/AzureDeployTest/Ping.js
@@ -0,0 +1,20 @@
+var request = require("request");
+
+var startTime = new Date();
+var refreshIntervalId = setInterval(function () {
+	request(process.env.UrlToTest, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(process.env.UrlToTest + " is up and running.");
+			clearInterval(refreshIntervalId);
+		} else {
+			var currentTime = new Date();
+			var diff = (currentTime - startTime) / 1000;
+			console.log(process.env.UrlToTest + " isn't available for " + diff + " seconds.");
+			
+			if (diff > 1200) {
+				clearInterval(refreshIntervalId);
+				throw new Error(process.env.UrlToTest + " isn't available for 20 minutes.");
+			}
+		}
+	})
+}, 10000);
