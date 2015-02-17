b8dc571c9b8e5ccc5926e67bbfe955be458478cb
Ola <none@none>
2015-02-17 15:21:37 +0100

    removed one use of RegisteredDataSourceCollection

--- Domain/Security/Authentication/AvailableDataSourcesProvider.cs
+++ Domain/Security/Authentication/AvailableDataSourcesProvider.cs
@@ -5,60 +5,60 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Security.Authentication
 {
-    public class AvailableDataSourcesProvider : IAvailableDataSourcesProvider
-    {
+	public class AvailableDataSourcesProvider : IAvailableDataSourcesProvider
+	{
 		private readonly ICurrentApplicationData _applicationData;
-        private bool _initialized;
-        private readonly IList<IDataSource> _availableDataSources = new List<IDataSource>();
-        private readonly IList<IDataSource> _unavailableDataSources = new List<IDataSource>();
-        private readonly object _initializeLock = new object();
+		private bool _initialized;
+		private readonly IList<IDataSource> _availableDataSources = new List<IDataSource>();
+		private readonly IList<IDataSource> _unavailableDataSources = new List<IDataSource>();
+		private readonly object _initializeLock = new object();
 
-        public AvailableDataSourcesProvider(ICurrentApplicationData applicationData)
-        {
-            _applicationData = applicationData;
-        }
+		public AvailableDataSourcesProvider(ICurrentApplicationData applicationData)
+		{
+			_applicationData = applicationData;
+		}
 
-        public IEnumerable<IDataSource> AvailableDataSources()
-        {
-            VerifyInitialize();
-            return _availableDataSources;
-        }
+		public IEnumerable<IDataSource> AvailableDataSources()
+		{
+			VerifyInitialize();
+			return _availableDataSources;
+		}
 
-        private void VerifyInitialize()
-        {
-            lock (_initializeLock)
-            {
-                if (!_initialized)
-                {
-                    foreach (IDataSource dataSource in _applicationData.Current().RegisteredDataSourceCollection)
-                    {
-                        try
-                        {
-									using (dataSource.Application.CreateAndOpenUnitOfWork())
-                            {
-                                //Must probably do something to get a connection opened
-                            }
-                            _availableDataSources.Add(dataSource);
-                        }
-                        catch (Exception ex)
-                        {
-                            SqlException sqlEx = ex.InnerException as SqlException;
-                            if (sqlEx != null &&
-                                (sqlEx.Number == 4060 || sqlEx.Number == 53 || sqlEx.Number == -1 || sqlEx.Number == -2))
-                                _unavailableDataSources.Add(dataSource);
-                            else
-                                throw;
-                        }
-                    }
-                    _initialized = true;
-                }
-            }
-        }
+		private void VerifyInitialize()
+		{
+			lock (_initializeLock)
+			{
+				if (!_initialized)
+				{
+					//temp for to be able to remove access to the list
+					IDataSource dataSource = _applicationData.Current().DataSource("Teleopti WFM");
+					try
+					{
+						using (dataSource.Application.CreateAndOpenUnitOfWork())
+						{
+							//Must probably do something to get a connection opened
+						}
+						_availableDataSources.Add(dataSource);
+					}
+					catch (Exception ex)
+					{
+						var sqlEx = ex.InnerException as SqlException;
+						if (sqlEx != null &&
+							 (sqlEx.Number == 4060 || sqlEx.Number == 53 || sqlEx.Number == -1 || sqlEx.Number == -2))
+							_unavailableDataSources.Add(dataSource);
+						else
+							throw;
+					}
 
-        public IEnumerable<IDataSource> UnavailableDataSources()
-        {
-            VerifyInitialize();
-            return _unavailableDataSources;
-        }
-    }
+					_initialized = true;
+				}
+			}
+		}
+
+		public IEnumerable<IDataSource> UnavailableDataSources()
+		{
+			VerifyInitialize();
+			return _unavailableDataSources;
+		}
+	}
 }
--- DomainTest/Security/Authentication/AvailableDataSourcesProviderTest.cs
+++ DomainTest/Security/Authentication/AvailableDataSourcesProviderTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Rhino.Mocks;
@@ -15,57 +14,45 @@ namespace Teleopti.Ccc.DomainTest.Security.Authentication
 	public class AvailableDataSourcesProviderTest
 	{
 		private IAvailableDataSourcesProvider target;
-		private MockRepository mocks;
 		private IApplicationData applicationData;
 
 		[SetUp]
 		public void Setup()
 		{
-			mocks = new MockRepository();
-			applicationData = mocks.StrictMock<IApplicationData>();
+			applicationData = MockRepository.GenerateMock<IApplicationData>();
 			target = new AvailableDataSourcesProvider(new ThisApplicationData(applicationData));
 		}
 
 		[Test]
 		public void VerifyCanGetAvailableDataSources()
 		{
-			var dataSource = mocks.StrictMock<IDataSource>();
-			IList<IDataSource> dataSources = new List<IDataSource> { dataSource };
-			IUnitOfWorkFactory unitOfWorkFactory = mocks.StrictMock<IUnitOfWorkFactory>();
-			IUnitOfWork unitOfWork = mocks.StrictMock<IUnitOfWork>();
-			using (mocks.Record())
-			{
-				Expect.Call(applicationData.RegisteredDataSourceCollection).Return(dataSources);
-				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
-				Expect.Call(dataSource.Application).Return(unitOfWorkFactory);
-				Expect.Call(unitOfWork.Dispose);
-			}
-			using (mocks.Playback())
-			{
-				var result = target.AvailableDataSources();
-				Assert.AreEqual(1, result.Count());
-			}
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var unitOfWorkFactory =  MockRepository.GenerateMock<IUnitOfWorkFactory>();
+			var unitOfWork = MockRepository.GenerateMock<IUnitOfWork>();
+			
+			applicationData.Stub(x => x.DataSource("Teleopti WFM")).Return(dataSource);
+			unitOfWorkFactory.Stub(x => x.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			dataSource.Stub(x => x.Application).Return(unitOfWorkFactory);
+			unitOfWork.Stub(x => x.Dispose());
+			
+			var result = target.AvailableDataSources();
+			Assert.AreEqual(1, result.Count());
 		}
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes"), Test]
 		public void VerifyCanGetUnavailableDataSources()
 		{
-			var dataSource = mocks.StrictMock<IDataSource>();
-			IList<IDataSource> dataSources = new List<IDataSource> { dataSource };
-			IUnitOfWorkFactory unitOfWorkFactory = mocks.StrictMock<IUnitOfWorkFactory>();
-			using (mocks.Record())
-			{
-				Expect.Call(applicationData.RegisteredDataSourceCollection).Return(dataSources);
-				Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Throw(new Exception("test",
-																							 SqlExceptionConstructor.
-																								CreateSqlException("Error message", 4060)));
-				Expect.Call(dataSource.Application).Return(unitOfWorkFactory);
-			}
-			using (mocks.Playback())
-			{
-				var result = target.UnavailableDataSources();
-				Assert.AreEqual(1, result.Count());
-			}
+			var dataSource = MockRepository.GenerateMock<IDataSource>();
+			var unitOfWorkFactory = MockRepository.GenerateMock<IUnitOfWorkFactory>();
+
+			applicationData.Stub(x => x.DataSource("Teleopti WFM")).Return(dataSource);
+			unitOfWorkFactory.Stub(x => x.CreateAndOpenUnitOfWork()).Throw(new Exception("test",
+																			SqlExceptionConstructor.
+																			CreateSqlException("Error message", 4060)));
+			dataSource.Stub(x => x.Application).Return(unitOfWorkFactory);
+
+			var result = target.UnavailableDataSources();
+			Assert.AreEqual(1, result.Count());
 		}
 	}
 }
