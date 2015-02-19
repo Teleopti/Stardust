888f97343e7064da52b415a557bbc0a23bae90f5
Roger Kratz <roger.kratz@teleopti.com>
2015-02-19 11:02:43 +0100

    removing one usage of data source list in sdk (forgot to remove this method while refactoring yesterday)

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus/DataSourceFactory.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus/DataSourceFactory.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Globalization;
-using System.Linq;
 using log4net;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Security;
-using Teleopti.Ccc.Domain.Security.Authentication;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Sdk.ServiceBus
@@ -33,28 +31,6 @@ namespace Teleopti.Ccc.Sdk.ServiceBus
 					ret.DataSource = dataSource;
 					return ret;
 				}
-
-        public static DataSourceContainer GetDesiredDataSource(IDataSourceProvider dataSourceProvider, string dataSource, IRepositoryFactory repositoryFactory)
-        {
-            DataSourceContainer dataSourceContainer =
-                dataSourceProvider.DataSourceList().FirstOrDefault(d => d.DataSource.Application.Name == dataSource);
-            
-            if (dataSourceContainer == null)
-            {
-                Logger.ErrorFormat("No datasource matching the name {0} was found.",dataSource);
-                throw new ArgumentNullException("dataSource",
-                                                string.Format(CultureInfo.InvariantCulture,
-                                                              "No datasource matching the name {0} was found.",
-                                                              dataSource));
-            }
-
-	        using (var uow = dataSourceContainer.DataSource.Application.CreateAndOpenUnitOfWork())
-	        {
-		        var systemId = SuperUser.Id_AvoidUsing_This;
-		        dataSourceContainer.SetUser(repositoryFactory.CreatePersonRepository(uow).LoadOne(systemId));
-	        }
-	        return dataSourceContainer;
-        }
     }
 
 	public class SdkDataSourceResult
