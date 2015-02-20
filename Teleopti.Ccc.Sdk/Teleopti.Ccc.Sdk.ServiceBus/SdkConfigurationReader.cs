26417180d37126fc0f14b6038f892657b0ade0cd
Ola <none@none>
2015-02-20 14:07:53 +0100

    read nhib from disc instead of sdk in servicebus

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus/SdkConfigurationReader.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBus/SdkConfigurationReader.cs
@@ -21,70 +21,73 @@ using Teleopti.Messaging.Client;
 
 namespace Teleopti.Ccc.Sdk.ServiceBus
 {
-    public class SdkConfigurationReader : IConfigurationReader
-    {
-        private static readonly ILog Logger = LogManager.GetLogger(typeof(SdkConfigurationReader));
-        private static readonly object LockObject = new object();
+	public class SdkConfigurationReader : IConfigurationReader
+	{
+		private static readonly ILog Logger = LogManager.GetLogger(typeof(SdkConfigurationReader));
+		private static readonly object LockObject = new object();
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "NHibernate"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "NHibernate"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
 		public void ReadConfiguration(MessageSenderCreator creator, Func<IMessageBrokerComposite> messageBroker)
-        {
-            lock (LockObject)
-            {
-                if (StateHolderReader.IsInitialized)
-                {
-                    Logger.Info("StateHolder already initialized. This step is skipped.");
-                    return;
-                }
+		{
+			var wrapper = new ConfigurationManagerWrapper();
+			var path = wrapper.AppSettings["NhibPath"];
+			lock (LockObject)
+			{
+				if (StateHolderReader.IsInitialized)
+				{
+					Logger.Info("StateHolder already initialized. This step is skipped.");
+					return;
+				}
 
-                //This should probably go into a host of some kind instead
-                ICollection<string> encryptedNHibConfigs;
-                IDictionary<string, string> encryptedAppSettings;
-                using (var proxy = new Proxy())
-                {
-                    try
-                    {
-                        encryptedNHibConfigs = proxy.GetHibernateConfigurationInternal();
-                        encryptedAppSettings = proxy.GetAppSettingsInternal();
+				//This should probably go into a host of some kind instead
+				IDictionary<string, string> encryptedAppSettings;
+				using (var proxy = new Proxy())
+				{
+					try
+					{
+						encryptedAppSettings = proxy.GetAppSettingsInternal();
 
-                    }
-                    catch (CommunicationException exception)
-                    {
-                        Logger.Error("Configuration could not be retrieved from server.", exception);
-                        encryptedAppSettings = new Dictionary<string, string>(0);
-                        encryptedNHibConfigs = new List<string>(0);
-                    }
-                }
-                if (encryptedNHibConfigs.Count == 0)
-                    throw new DataSourceException(
-                        "No NHibernate configurations received. Verify that the SDK is up and running.");
+					}
+					catch (CommunicationException exception)
+					{
+						Logger.Error("Configuration could not be retrieved from server.", exception);
+						encryptedAppSettings = new Dictionary<string, string>(0);
+					}
+				}
 
-            	
-                encryptedAppSettings.DecryptDictionary(EncryptionConstants.Image1, EncryptionConstants.Image2);
-            	var application =
-            		new InitializeApplication(
-						new DataSourcesFactory(new EnversConfiguration(), creator.Create(), DataSourceConfigurationSetter.ForServiceBus(), new CurrentHttpContext()),
-						messageBroker());
-                application.Start(new BasicState(), encryptedAppSettings,
-                                  encryptedNHibConfigs.DecryptList(EncryptionConstants.Image1,
-                                                                   EncryptionConstants.Image2), null);
+				encryptedAppSettings.DecryptDictionary(EncryptionConstants.Image1, EncryptionConstants.Image2);
+				var application =
+					new InitializeApplication(
+					new DataSourcesFactory(new EnversConfiguration(), creator.Create(), DataSourceConfigurationSetter.ForServiceBus(), new CurrentHttpContext()),
+					messageBroker());
 
-                Logger.Info("Initialized application");
-            }
-        }
-    }
+				application.Start(new BasicState(), path, null, new ConfigurationManagerWrapperInternal(encryptedAppSettings), true);
+
+				Logger.Info("Initialized application");
+			}
+		}
+	}
+
+	class ConfigurationManagerWrapperInternal : IConfigurationWrapper
+	{
+		public ConfigurationManagerWrapperInternal(IDictionary<string, string> appSettings)
+		{
+			AppSettings = appSettings;
+		}
+		public IDictionary<string, string> AppSettings { get; private set; }
+	}
 
 	public class InternalServiceBusSender : IServiceBusSender
 	{
 		private readonly Func<IServiceBus> _serviceBus;
-        
+
 		public InternalServiceBusSender(Func<IServiceBus> serviceBus)
 		{
-		    _serviceBus = serviceBus;
+			_serviceBus = serviceBus;
 		}
 
-	    public void Dispose()
+		public void Dispose()
 		{
 		}
 
@@ -142,7 +145,7 @@ namespace Teleopti.Ccc.Sdk.ServiceBus
 			{
 				xmlPath = AppDomain.CurrentDomain.BaseDirectory;
 			}
-			
+
 			return new FileConfigurationReader(xmlPath);
 		}
 	}
@@ -189,8 +192,8 @@ namespace Teleopti.Ccc.Sdk.ServiceBus
 		}
 	}
 
-    public interface IConfigurationReader
-    {
+	public interface IConfigurationReader
+	{
 		void ReadConfiguration(MessageSenderCreator creator, Func<IMessageBrokerComposite> messageBroker);
-    }
+	}
 }
