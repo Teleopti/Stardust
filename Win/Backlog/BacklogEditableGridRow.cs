cf1e2757fee7f3c78353336b8f6cb84dc64a1fc9
Micke <micke@teleopti.com>
2015-02-23 14:19:20 +0100

    Fixing the build

--- Win/Backlog/BacklogEditableGridRow.cs
+++ Win/Backlog/BacklogEditableGridRow.cs
@@ -6,17 +6,51 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Win.Backlog
 {
-	public class BacklogEditableGridRow : BacklogGridRow, IBacklogGridRow
+	public class BacklogEditableGridRow : IBacklogGridRow
 	{
+		private readonly string _cellType;
+		private readonly string _rowHeaderText;
+		private readonly Func<int, ISkill, TimeSpan?> _dataSource;
+		private readonly BacklogModel _model;
+
 		public BacklogEditableGridRow(BacklogCategory category, string cellType, string rowHeaderText,
 			Func<int, ISkill, TimeSpan?> dataSource, BacklogModel model)
-			: base(category, cellType, rowHeaderText, dataSource, model)
 		{
+			_cellType = cellType;
+			_rowHeaderText = rowHeaderText;
+			_dataSource = dataSource;
+			_model = model;
+			Category = category;
 		}
+
+		public BacklogCategory Category { get; private set; }
 
 		public void SetCell(ISkill skill, GridQueryCellInfoEventArgs cellInfo, DateOnly planningStartDate)
 		{
-			base.SetCell(skill, cellInfo,planningStartDate);
+			if (cellInfo.ColIndex == -1)
+				return;
+
+			if (cellInfo.ColIndex == 0)
+			{
+				cellInfo.Style.CellValue = _rowHeaderText;
+				return;
+			}
+
+			cellInfo.Style.CellType = _cellType;
+			var time = _dataSource.Invoke(cellInfo.ColIndex, skill);
+			cellInfo.Style.CellValue = time;
+
+			var date = _model.GetDateOnIndex(cellInfo.ColIndex);
+			if (Category == BacklogCategory.ProductionPlan && date < planningStartDate)
+				cellInfo.Style.BackColor = Color.LightGray;
+
+			if (Category == BacklogCategory.Scheduled && date >= planningStartDate)
+				cellInfo.Style.BackColor = Color.LightGray;
+
+			if (_model.IsClosedOnIndex(cellInfo.ColIndex, skill))
+				cellInfo.Style.BackColor = Color.LightSalmon;
+
+
 			cellInfo.Style.BackColor = Color.Khaki;
 		}
 	}
