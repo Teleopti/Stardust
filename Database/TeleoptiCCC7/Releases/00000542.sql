d42beb8840d6697e9b148dae8fed48ed29a987de
asadm <asadm@teleopti.com>
2015-02-26 11:39:10 +0100

    PBI #31236 Added script to clear the read models.

---
+++ Database/TeleoptiCCC7/Releases/00000542.sql
@@ -0,0 +1,11 @@
+-- =============================================
+-- Author: Asad Mirza
+-- Description:	Cleared the models
+-- =============================================
+delete from [ReadModel].[AdherenceDetails]
+
+delete from [ReadModel].[AdherencePercentage]
+
+delete from [ReadModel].[SiteOutOfAdherence]
+
+delete from [ReadModel].[TeamOutOfAdherence]
