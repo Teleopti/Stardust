dd7ad922e7921db0e56cf1ea04a9438669e44ef3
Roger Kratz <roger.kratz@teleopti.com>
2015-02-27 13:33:43 +0100

    No need to keep Teleopti.*.Authentication.DataSourceViewModel.js

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/SSO/Content/Scripts/Teleopti.SSO.Authentication.DataSourceViewModel.js
+++
@@ -1,5 +0,0 @@
-﻿
-//TODO: tenant - remove!
-Teleopti.SSO.Authentication.DataSourceViewModel = function (name) {
-	this.Name = name;
-};
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Content/Scripts/Teleopti.Start.Authentication.DataSourceViewModel.js
+++
@@ -1,7 +0,0 @@
-﻿
-//TODO: tenant - should be removed
-Teleopti.Start.Authentication.DataSourceViewModel = function (name, type) {
-	this.Name = name;
-	this.Type = type;
-	this.IsWindows = (type == 'windows');
-};
