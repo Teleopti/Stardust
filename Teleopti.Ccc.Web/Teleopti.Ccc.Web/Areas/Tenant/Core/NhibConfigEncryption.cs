4aa55cce0f7914c81ea0f642c3a57bbb4b53b22a
Ola <none@none>
2015-03-03 15:29:40 +0100

    got encrypted several times if you logon more then once on the same web "session"

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Tenant/Core/NhibConfigEncryption.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Tenant/Core/NhibConfigEncryption.cs
@@ -14,9 +14,14 @@ namespace Teleopti.Ccc.Web.Areas.Tenant.Core
 		public DataSourceConfiguration EncryptConfig(DataSourceConfiguration dataSourceConfig)
 		{
 			var dic = dataSourceConfig.ApplicationNHibernateConfig.Keys.ToDictionary(key => key, key => Encryption.EncryptStringToBase64(dataSourceConfig.ApplicationNHibernateConfig[key], EncryptionConstants.Image1, EncryptionConstants.Image2));
-			dataSourceConfig.ApplicationNHibernateConfig = dic;
-			dataSourceConfig.AnalyticsConnectionString = Encryption.EncryptStringToBase64(dataSourceConfig.AnalyticsConnectionString, EncryptionConstants.Image1, EncryptionConstants.Image2);
-			return dataSourceConfig;
+			var ret = new DataSourceConfiguration
+			{
+				ApplicationNHibernateConfig = dic,
+				AnalyticsConnectionString =
+					Encryption.EncryptStringToBase64(dataSourceConfig.AnalyticsConnectionString, EncryptionConstants.Image1,
+						EncryptionConstants.Image2)
+			};
+			return ret;
 		}
 
 		public DataSourceConfiguration DecryptConfig(DataSourceConfiguration dataSourceConfig)
