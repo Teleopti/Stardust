8177c17c77a5b4f020f9765d82959837fcd0dbf4
Zhiping Lan <zhiping.lan@teleopti.com>
2015-03-04 18:30:54 +0800

    PBI #32092 Added filter-by-group in new team schedule.

--- Database/TeleoptiCCC7/Programmability/03StoredProcedures/ReadModel.LoadPersonForScheduleSearch.sql
+++ Database/TeleoptiCCC7/Programmability/03StoredProcedures/ReadModel.LoadPersonForScheduleSearch.sql
@@ -1,14 +1,12 @@
-
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[ReadModel].[LoadPersonForScheduleSearch]') AND type in (N'P', N'PC'))
 DROP PROCEDURE  [ReadModel].[LoadPersonForScheduleSearch]
+
 GO
-
-
-/****** Object:  StoredProcedure [ReadModel].[LoadPersonForScheduleSearch]    Script Date: 2/25/2015 3:29:59 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		Fan Zhang, Zhiping Lan, Yanyi Wan
 -- Create date: 2015-02-05
@@ -18,6 +16,7 @@ CREATE PROCEDURE [ReadModel].[LoadPersonForScheduleSearch]
 	-- teamIdList should be comma separated uids for team.	
 	@scheduleDate smalldatetime, 
 	@teamIdList varchar(max),
+	@businessUnitId uniqueidentifier,
 	@name nvarchar(max)
 AS
 BEGIN
@@ -45,4 +44,15 @@ BEGIN
 	WHERE p.WorkflowControlSet IS NOT NULL		
 		AND (@scheduleDate BETWEEN StartDate AND EndDate) 	
 		AND ((@namesearch is null or @namesearch = '') or ((p.LastName + p.FirstName) like @namesearch) or ((p.FirstName + p.LastName) like @namesearch))	
+	UNION 
+	SELECT gr.PersonId as PersonId,gr.TeamId as TeamId, gr.SiteId as SiteId, gr.BusinessUnitId as BusinessUnitId
+	FROM ReadModel.groupingreadonly gr
+		INNER JOIN @teamids tids ON tids.Team =gr.groupId
+		INNER JOIN Person p ON gr.PersonId = p.Id
+	WHERE gr.Businessunitid = @businessUnitId 
+		AND @scheduleDate BETWEEN gr.StartDate and isnull(gr.EndDate,'2059-12-31') 
+		AND (gr.LeavingDate >= @scheduleDate OR gr.LeavingDate IS NULL)
+		AND p.WorkflowControlSet IS NOT NULL		
+		AND ((@namesearch is null or @namesearch = '') or ((p.LastName + p.FirstName) like @namesearch) or ((p.FirstName + p.LastName) like @namesearch))
+				 
 END
--- Infrastructure/Repositories/PersonForShiftTradeRepository.cs
+++ Infrastructure/Repositories/PersonForShiftTradeRepository.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using NHibernate.Impl;
 using NHibernate.Transform;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.Infrastructure;
@@ -24,14 +26,25 @@ namespace Teleopti.Ccc.Infrastructure.Repositories
 			_unitOfWork = new FixedCurrentUnitOfWork(unitOfWork);
 		}
 
+		private Guid getBusinessUnitId()
+		{
+			var filter = (FilterImpl)_unitOfWork.Session().GetEnabledFilter("businessUnitFilter");
+			object businessUnitId;
+			if (!filter.Parameters.TryGetValue("businessUnitParameter", out businessUnitId))
+			{
+				businessUnitId = ((ITeleoptiIdentity)TeleoptiPrincipal.Current.Identity).BusinessUnit.Id.GetValueOrDefault();
+			}
+			return Guid.Parse(businessUnitId.ToString());
+		}
 
 		public IList<IAuthorizeOrganisationDetail> GetPersonForShiftTrade(DateOnly shiftTradeDate, IList<Guid> teamIdList , string name)
 		{
 			return ((NHibernateUnitOfWork)_unitOfWork.Current()).Session.CreateSQLQuery(
-				"exec ReadModel.LoadPersonForScheduleSearch @scheduleDate=:scheduleDate, @teamIdList=:teamIdList, @name=:name")
+				"exec ReadModel.LoadPersonForScheduleSearch @scheduleDate=:scheduleDate, @teamIdList=:teamIdList,@businessUnitId=:businessUnitId, @name=:name ")
 													  .SetDateTime("scheduleDate", shiftTradeDate)
 													  .SetString("teamIdList", string.Join(",", teamIdList))
 													  .SetString("name", name)
+													  .SetGuid("businessUnitId", getBusinessUnitId())
 													  .SetResultTransformer(Transformers.AliasToBean(typeof(PersonSelectorShiftTrade)))
 													  .SetReadOnly(true)
 													  .List<IAuthorizeOrganisationDetail>();
