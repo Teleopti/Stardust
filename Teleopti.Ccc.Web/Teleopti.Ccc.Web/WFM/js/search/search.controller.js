c9f3e31af01f1b294e88188869131141c07a46ce
asadm <asadm@teleopti.com>
2015-03-10 16:07:33 +0100

    PBI #32418 parsing the search result

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/search/search.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/search/search.controller.js
@@ -2,14 +2,17 @@
 
 var search = angular.module('wfm.searching', []);
 search.controller('SearchCtrl', [
-	'$scope', '$filter', 'SearchSvrc',
-	function ($scope, $filter, SearchSvrc) {
+	'$scope', '$filter', '$state', 'SearchSvrc',
+	function ($scope, $filter, $state, SearchSvrc) {
 		$scope.searchResult = [];
 		$scope.keyword = '';
 		$scope.searchKeyword = function () {
 			SearchSvrc.search.query({ keyword: $scope.keyword }).$promise.then(function(result) {
 				$scope.searchResult = result;
+				$scope.first = result[0];
+				console.log($scope.searchResult);
 			});
 		};
+
 	}
 ]);
