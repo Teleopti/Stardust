c39739b6b9f30075d271693601c9d0c34ecf676c
Roger Kratz <roger.kratz@teleopti.com>
2015-03-11 15:52:00 +0100

    add "robin script" to run karma tests as file instead.

---
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -0,0 +1,8 @@
+call ..\.node\npm install -g karma --save-dev
+call ..\.node\npm install -g karma-teamcity-reporter
+call ..\.node\npm install -g karma-chrome-launcher
+call ..\.node\npm install -g karma-jasmine
+call ..\.node\npm install -g karma-cli
+
+for /f "tokens=1" %%i in ('..\.node\npm bin -g') do set output=%%i
+call %output%\karma start --reporters teamcity --single-run
