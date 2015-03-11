b3ef24444715d2692614b2da6bcb3099d5d02fa2
Robin Karlsson <robin.karlsson@teleopti.com>
2015-03-11 16:45:51 +0100

    Space in path... Tralalalala

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -7,6 +7,6 @@ call ..\.node\npm install -g karma-jasmine
 call ..\.node\npm install -g karma-cli
 
 for /f "tokens=1" %%i in ('..\.node\npm bin -g') do set output=%%i
-echo %output%
+echo "%output%"
 
-%output%\karma start --reporters teamcity --single-run
+"%output%\karma" start --reporters teamcity --single-run
