26a0daead1f32b54be585a7201a7ca5c659cce4f
Roger Kratz <roger.kratz@teleopti.com>
2015-03-12 11:04:59 +0100

    Adding FindTenantByNameQuery

---
+++ Infrastructure/MultiTenancy/Server/FindTenantByNameQuery.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate.Criterion;
+using Teleopti.Ccc.Infrastructure.MultiTenancy.Server.NHibernate;
+
+namespace Teleopti.Ccc.Infrastructure.MultiTenancy.Server
+{
+	public class FindTenantByNameQuery : IFindTenantByNameQuery
+	{
+		private readonly ICurrentTenantSession _tenantUnitOfWorkManager;
+
+		public FindTenantByNameQuery(ICurrentTenantSession tenantUnitOfWorkManager)
+		{
+			_tenantUnitOfWorkManager = tenantUnitOfWorkManager;
+		}
+
+		public Tenant Find(string name)
+		{
+			return _tenantUnitOfWorkManager.CurrentSession()
+				.CreateCriteria<Tenant>()
+				.Add(Restrictions.Eq("Name", name))
+				.UniqueResult<Tenant>();
+		}
+	}
+}
---
+++ InfrastructureTest/MultiTenancy/Server/FindTenantByNameQueryTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Infrastructure.MultiTenancy.Server;
+using Teleopti.Ccc.Infrastructure.MultiTenancy.Server.NHibernate;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.TestData;
+
+namespace Teleopti.Ccc.InfrastructureTest.MultiTenancy.Server
+{
+	[RestoreDatabaseAfterTest]
+	public class FindTenantByNameQueryTest
+	{
+		private Tenant tenantPresentInDatabase;
+		private IFindTenantByNameQuery target;
+
+		[Test]
+		public void ShouldFindTenant()
+		{
+			target.Find(tenantPresentInDatabase.Name)
+				.Should().Be.EqualTo(tenantPresentInDatabase);
+		}
+
+		[Test]
+		public void ShouldReturnNullIfNotExist()
+		{
+			target.Find(tenantPresentInDatabase.Name + "something else")
+				.Should().Be.Null();
+		}
+
+		[SetUp]
+		public void InsertPreState()
+		{
+			var tenantUnitOfWorkManager = TenantUnitOfWorkManager.CreateInstanceForTest(ConnectionStringHelper.ConnectionStringUsedInTests);
+			target = new FindTenantByNameQuery(tenantUnitOfWorkManager);
+
+			tenantPresentInDatabase = new Tenant(RandomName.Make());
+			tenantUnitOfWorkManager.CurrentSession().Save(tenantPresentInDatabase);
+		}
+	}
+}
