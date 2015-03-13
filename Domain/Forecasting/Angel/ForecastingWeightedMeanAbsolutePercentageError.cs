a44da4fa3369416a9ccff4ab35cfe91e4fb63f00
Kunning Mao <none@none>
2015-03-13 09:29:44 +0100

    #32569 accuracy result seems not correct, revert to previous one, and comment out the tree ui for now

--- Domain/Forecasting/Angel/ForecastingWeightedMeanAbsolutePercentageError.cs
+++ Domain/Forecasting/Angel/ForecastingWeightedMeanAbsolutePercentageError.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Forecasting.Angel
@@ -12,34 +11,28 @@ namespace Teleopti.Ccc.Domain.Forecasting.Angel
 		{
 			var diffSum = 0d;
 			var tasksSum = 0d;
-
-			var first = historicalDataForLastYear.Select(x => new {x.TotalStatisticCalculatedTasks, x.CurrentDate, Tasks = 0d});
-			first = first.Concat(forecastingForLastYear.Select(x => new {TotalStatisticCalculatedTasks = 0d, x.CurrentDate, x.Tasks}));
-			var grouped =
-				first.GroupBy(x => x.CurrentDate)
-					.Select(
-						x =>
-							new
-							{
-								x.Key,
-								Tasks = x.Sum(t => t.Tasks),
-								TotalStatisticCalculatedTasks = x.Sum(t => t.TotalStatisticCalculatedTasks)
-							});
-			foreach (var day in grouped)
+			foreach (var day in historicalDataForLastYear)
 			{
-				double diff;
-				if (Math.Abs(day.TotalStatisticCalculatedTasks) < 0.000001 && Math.Abs(day.Tasks) < 0.000001)
+				var diff = 0d;
+				foreach (var forecastingDay in forecastingForLastYear)
 				{
-					diff = 0d;
+					if (forecastingDay.CurrentDate == day.CurrentDate)
+					{
+						if (Math.Abs(day.TotalStatisticCalculatedTasks) < 0.000001 && Math.Abs(forecastingDay.Tasks) < 0.000001)
+						{
+							diff = 0d;
+						}
+						else
+						{
+							diff = Math.Abs(day.TotalStatisticCalculatedTasks - forecastingDay.Tasks);
+							tasksSum += day.TotalStatisticCalculatedTasks;
+						}
+						break;
+					}
 				}
-				else
-				{
-					diff = Math.Abs(day.TotalStatisticCalculatedTasks - day.Tasks);
-					tasksSum += day.TotalStatisticCalculatedTasks;
-				}
+
 				diffSum += diff;
 			}
-
 			var wmape = Math.Abs(tasksSum) < 0.000001 ? 0 : diffSum / tasksSum;
 			return Math.Max(0, 100 - Math.Round(wmape * 100, 3));
 		}
