a61871e11f5ffc65843f14142ca7c4e6f261b9d6
asadm <asadm@teleopti.com>
2015-03-19 13:51:07 +0100

    PBI #32659 minor refactoring

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/notifications/notifications.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/notifications/notifications.controller.js
@@ -1,12 +1,12 @@
 ﻿'use strict';
 
-var search = angular.module('wfm.notifications', []);
-search.controller('NotificationsCtrl', [
+var notification = angular.module('wfm.notifications', []);
+notification.controller('NotificationsCtrl', [
 	'$scope','NotificationsSvrc',
 	function ($scope, NotificationsSvrc) {
-		$scope.searchResult = [];
+		$scope.notificationResult = [];
 		NotificationsSvrc.getNotifications.query({ }).$promise.then(function (result) {
-			$scope.searchResult = result;
+			$scope.notificationResult = result;
 			console.log(result);
 		});
 	}
