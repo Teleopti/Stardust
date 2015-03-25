ae2221cab6d579da7da3104bc8634e77d5e03137
Roger Kratz <roger.kratz@teleopti.com>
2015-03-25 13:47:09 +0100

    #32796 adding overrideconfigreader

---
+++ Teleopti.Ccc.IocCommon/MultipleConfig/OverrideConfigReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Teleopti.Ccc.IocCommon.MultipleConfig
+{
+	public class OverrideConfigReader
+	{
+		private readonly string _directory;
+
+		public OverrideConfigReader(string directory)
+		{
+			_directory = directory;
+		}
+
+		public IDictionary<string, string> Overrides()
+		{
+			const string overrideConfigFilePatter = "*.override.config";
+
+			var ret = new Dictionary<string, string>();
+			foreach (var overrideFile in Directory.EnumerateFiles(_directory, overrideConfigFilePatter))
+			{
+				ret[Path.GetFileName(overrideFile).Replace(".override.config", string.Empty)] = overrideFile;
+			}
+
+			return ret;
+		}
+	}
+}
---
+++ Teleopti.Ccc.IocCommonTest/MultipleConfig/OverrideConfigReaderTest.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.IocCommon.MultipleConfig;
+using Teleopti.Ccc.TestCommon.TestData;
+
+namespace Teleopti.Ccc.IocCommonTest.MultipleConfig
+{
+	public class OverrideConfigReaderTest
+	{
+		[Test]
+		public void ShouldFindOneFile()
+		{
+			var dir = Directory.GetCurrentDirectory();
+			var displayName = RandomName.Make();
+			var filename = Path.Combine(dir, displayName + ".override.config");
+			try
+			{
+				File.WriteAllText(filename, "it doesn't matter");
+
+				var target = new OverrideConfigReader(dir);
+				target.Overrides()[displayName]
+					.Should().Be.EqualTo(filename);
+			}
+			finally
+			{
+				File.Delete(filename);
+			}
+		}
+
+		[Test]
+		public void ShouldNotFindFileWithWrongPatter()
+		{
+			var dir = Directory.GetCurrentDirectory();
+			var displayName = RandomName.Make();
+			var filename = Path.Combine(dir, displayName + ".config");
+			try
+			{
+				File.WriteAllText(filename, "it doesn't matter");
+
+				var target = new OverrideConfigReader(dir);
+				target.Overrides().ContainsKey(displayName)
+					.Should().Be.False();
+			}
+			finally
+			{
+				File.Delete(filename);
+			}
+		}
+
+		[Test]
+		public void ShouldReadMultipleFiles()
+		{
+			var dir = Directory.GetCurrentDirectory();
+			var filename1 = Path.Combine(dir, RandomName.Make() + ".override.config");
+			var filename2 = Path.Combine(dir, RandomName.Make() + ".override.config");
+			try
+			{
+				File.WriteAllText(filename1, "it doesn't matter");
+				File.WriteAllText(filename2, "it doesn't matter");
+				var target = new OverrideConfigReader(dir);
+				target.Overrides().Count()
+					.Should().Be.EqualTo(2);
+			}
+			finally
+			{
+				File.Delete(filename1);
+				File.Delete(filename2);
+			}
+		}
+	}
+}
