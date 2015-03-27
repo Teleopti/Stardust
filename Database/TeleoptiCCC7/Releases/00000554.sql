0890191ab20919779cc228a57c27131277605493
Ola <none@none>
2015-03-27 06:36:40 +0100

    fix db build

--- Database/TeleoptiCCC7/Releases/00000554.sql
+++ Database/TeleoptiCCC7/Releases/00000554.sql
@@ -3,7 +3,7 @@ CREATE TABLE PayrollFormat(
 	[Id] [uniqueidentifier] NOT NULL,
 	FormatId [uniqueidentifier] NOT NULL,
 	Name nvarchar(500) NOT NULL,
-	PRIMARY KEY CLUSTERED 
+	CONSTRAINT [PK_PayrollFormat]  PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )
