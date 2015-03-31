b5438e257b97f3606e265c4b45ce6eb9a744670f
Team Real-time <greenmachine@teleopti.com>
2015-03-31 09:14:11 +0200

    a comment

--- Domain/Common/Time/INowExtensions.cs
+++ Domain/Common/Time/INowExtensions.cs
@@ -5,11 +5,21 @@ namespace Teleopti.Ccc.Domain.Common.Time
 {
 	public static class INowExtensions
 	{
+		/// <summary>
+		/// when ever(!) is the servers timezone a good idea to use?
+		/// </summary>
+		/// <param name="now"></param>
+		/// <returns>a probably incorrect value for your use</returns>
 		public static DateTime LocalDateTime(this INow now)
 		{
 			return now.UtcDateTime().ToLocalTime();
 		}
 
+		/// <summary>
+		/// when ever(!) is the servers timezone a good idea to use?
+		/// </summary>
+		/// <param name="now"></param>
+		/// <returns>a probably incorrect value for your use</returns>
 		public static DateOnly LocalDateOnly(this INow now)
 		{
 			return new DateOnly(now.LocalDateTime());
