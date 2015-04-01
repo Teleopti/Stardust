f6cb06a9bd3eb0a0a138c3df57de914fafe453d1
Team Real-time <greenmachine@teleopti.com>
2015-04-01 13:42:22 +0200

    Extending alarm configuratin from scenarios

--- Teleopti.Ccc.TestCommon/TestData/Setups/Configurable/AlarmConfigurable.cs
+++ Teleopti.Ccc.TestCommon/TestData/Setups/Configurable/AlarmConfigurable.cs
@@ -26,7 +26,21 @@ namespace Teleopti.Ccc.TestCommon.TestData.Setups.Configurable
 
 			var activityRepository = new ActivityRepository(uow);
 
-			var stateGroup = new RtaStateGroup(PhoneState, false, true);
+			IRtaStateGroup stateGroup = null;
+			if (PhoneState != null)
+			{
+				var stateGroupRepository = new RtaStateGroupRepository(uow);
+				stateGroup = (from g in stateGroupRepository.LoadAll()
+					from s in g.StateCollection
+					where s.StateCode == PhoneState
+					select g).SingleOrDefault();
+				if (stateGroup == null)
+				{
+					stateGroup = new RtaStateGroup(PhoneState, false, true);
+					stateGroup.AddState(PhoneState, PhoneState, Guid.Empty);
+					stateGroupRepository.Add(stateGroup);
+				}
+			}
 
 			IActivity activity = null;
 			if (Activity != null)
@@ -34,25 +48,21 @@ namespace Teleopti.Ccc.TestCommon.TestData.Setups.Configurable
 
 			var stateGroupActivityAlarm = new StateGroupActivityAlarm(stateGroup, activity) { AlarmType = alarmType };
 
-			stateGroup.AddState(PhoneState, PhoneState, Guid.Empty);
-
 			if (!string.IsNullOrEmpty(BusinessUnit))
 			{
 				var businessUnit = new BusinessUnitRepository(uow).LoadAll().Single(b => b.Name == BusinessUnit);
-				stateGroup.SetBusinessUnit(businessUnit);
+				if (stateGroup != null)
+					stateGroup.SetBusinessUnit(businessUnit);
 				stateGroupActivityAlarm.SetBusinessUnit(businessUnit);
 			}
+
 			if (!string.IsNullOrWhiteSpace(Adherence))
 			{
 				Adherence adherence;
 				if(Enum.TryParse(Adherence, true, out adherence))
 				alarmType.Adherence = adherence;
 			}
-				
-
-			var stateGroupRepository = new RtaStateGroupRepository(uow);
-			stateGroupRepository.Add(stateGroup);
-
+			
 			var alarmTypeRepository = new AlarmTypeRepository(uow);
 			alarmTypeRepository.Add(alarmType);
 
