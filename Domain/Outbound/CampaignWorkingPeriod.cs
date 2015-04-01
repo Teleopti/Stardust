e3ccd00a7d1c9cdf6ad0e29f1f1feaa5befdb717
Mingdi Wu <mingdi.wu@teleopti.com>
2015-04-01 13:01:51 +0800

    fix build

--- Domain/Outbound/CampaignWorkingPeriod.cs
+++ Domain/Outbound/CampaignWorkingPeriod.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
+﻿using System.Collections.Generic;
 using Teleopti.Ccc.Domain.Common.EntityBaseTypes;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Outbound
 {
