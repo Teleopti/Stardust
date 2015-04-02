5f2b89f3e5ddd86d1af24092d2b15e8500728182
Chundan Xu <chundan.xu@teleopti.com>
2015-04-02 09:51:58 +0200

    PBI #32697: restructure the js code a little.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/people/people.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/people/people.controller.js
@@ -1,14 +1,45 @@
-﻿'use strict';
+﻿(function() {
+	'use strict';
 
 
-angular.module('wfm.people', ['peopleService', 'peopleSearchService'])
-	.controller('PeopleCtrl', [
-	'$scope', '$filter', '$state', 'PeopleSearch',
-	function ($scope, $filter, $state, SearchSvrc) {
+	angular.module('wfm.people', ['peopleService', 'peopleSearchService'])
+		.controller('PeopleCtrl', [
+			'$scope', '$filter', '$state', 'PeopleSearch',PeopleController
+		])
+		.directive('modalDialog', function() {
+			return {
+				restrict: 'E',
+				scope: {
+					show: '='
+				},
+				replace: true, // Replace with the template below
+				transclude: true, // we want to insert custom content inside the directive
+				link: function(scope, element, attrs) {
+					scope.dialogStyle = {};
+					if (attrs.width)
+						scope.dialogStyle.width = attrs.width;
+					if (attrs.height)
+						scope.dialogStyle.height = attrs.height;
+					scope.hideModal = function() {
+						scope.show = false;
+					};
+				},
+				template: "<div class='ng-modal' ng-show='show'>" +
+					"<div class='ng-modal-overlay' ng-click='hideModal()'>" +
+					"</div>" +
+					"<div class='ng-modal-dialog' ng-style='dialogStyle'>" +
+					"<div class='ng-modal-close' ng-click='hideModal()'>x</div>" +
+					"<div class='ng-modal-dialog-content' ng-transclude></div>" +
+					"</div>" +
+					"</div>"
+			};
+		});
+
+	function PeopleController($scope, $filter, $state, SearchSvrc) {
 		$scope.searchResult = [];
 		$scope.keyword = '';
-		$scope.searchKeyword = function () {
-			SearchSvrc.search.query({ keyword: $scope.keyword }).$promise.then(function (result) {
+		$scope.searchKeyword = function() {
+			SearchSvrc.search.query({ keyword: $scope.keyword }).$promise.then(function(result) {
 				$scope.searchResult = result;
 				console.log($scope.searchResult);
 			});
@@ -18,32 +49,6 @@ angular.module('wfm.people', ['peopleService', 'peopleSearchService'])
 		$scope.toggleModal = function() {
 			$scope.modalShown = !$scope.modalShown;
 		};
-	}])
-	.directive('modalDialog', function () {
-	return {
-		restrict: 'E',
-		scope: {
-			show: '='
-		},
-		replace: true, // Replace with the template below
-		transclude: true, // we want to insert custom content inside the directive
-		link: function (scope, element, attrs) {
-			scope.dialogStyle = {};
-			if (attrs.width)
-				scope.dialogStyle.width = attrs.width;
-			if (attrs.height)
-				scope.dialogStyle.height = attrs.height;
-			scope.hideModal = function () {
-				scope.show = false;
-			};
-		},
-		template: "<div class='ng-modal' ng-show='show'>" +
-			"<div class='ng-modal-overlay' ng-click='hideModal()'>" +
-			"</div>" +
-			"<div class='ng-modal-dialog' ng-style='dialogStyle'>" +
-			"<div class='ng-modal-close' ng-click='hideModal()'>x</div>" +
-			"<div class='ng-modal-dialog-content' ng-transclude></div>" +
-			"</div>" +
-			"</div>"
-	};
-});
+	}
+
+})();
