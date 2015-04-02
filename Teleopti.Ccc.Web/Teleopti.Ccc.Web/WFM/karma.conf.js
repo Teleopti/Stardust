f202edfb43357cd8c2af3b4e095aab4ae462250f
Rob Wright <robert.wright@teleopti.com>
2015-04-02 08:16:07 +0800

    Fix Build: Update Karma config to reference new vendor libraries

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.js
@@ -4,81 +4,82 @@
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+	// base path that will be used to resolve all patterns (eg. files, exclude)
+	basePath: '',
 
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+	// frameworks to use
+	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+	frameworks: ['jasmine'],
 
 
-    // list of files / patterns to load in the browser
-    files: [
-	  'node_modules/angular/angular.min.js',
-	  'node_modules/angular-mocks/angular-mocks.js',
-	  'node_modules/angular-ui-router/release/angular-ui-router.min.js',
-	  'node_modules/angular-resource/angular-resource.min.js',
+	// list of files / patterns to load in the browser
+	files: [
+		'node_modules/angular/angular.min.js',
+		'node_modules/angular-mocks/angular-mocks.js',
+		'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+		'node_modules/angular-resource/angular-resource.min.js',
 		'node_modules/angular-translate/dist/angular-translate.min.js',
 		'node_modules/angular-translate/dist/angular-translate-loader-url/angular-translate-loader-url.min.js',
-	  'vendor/ui-bootstrap.min.js',
-	  'node_modules/angular-moment/angular-moment.min.js',
-	  'vendor/angular-ui-tree/angular-ui-tree.min.js',
-	  'vendor/angular-aria/angular-aria.min.js',
-	  'vendor/angular-animate/angular-animate.min.js',
-	  'vendor/angular-growl.js',
-	  'vendor/hammerjs/hammer.min.js',
-	  'vendor/angular-material/angular-material.min.js',
-	  
-	  'js/ABmetrics.min.js',
-	  'dist/main.min.js',
-      'tests/**/*.js',
-      'tests/*.js'
-    ],
+		'vendor/ui-bootstrap.min.js',
+		'node_modules/angular-moment/angular-moment.min.js',
+		'vendor/angular-ui-tree/angular-ui-tree.min.js',
+		'vendor/angular-aria/angular-aria.min.js',
+		'vendor/angular-animate/angular-animate.min.js',
+		'vendor/angular-growl.js',
+		'vendor/hammerjs/hammer.min.js',
+		'vendor/angular-material/angular-material.min.js',
+		'vendor/fabricjs/fabric.min.js',
+		'vendor/fabricjs/fabricjs_viewport.js',
+		'js/ABmetrics.min.js',
+		'dist/main.min.js',
+		'tests/**/*.js',
+		'tests/*.js'
+	],
 
 
-    // list of files to exclude
-    exclude: [
+	// list of files to exclude
+	exclude: [
 		'**/Gruntfile.js'
-    ],
+	],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+	// preprocess matching files before serving them to the browser
+	// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	preprocessors: {
+	},
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+	// test results reporter to use
+	// possible values: 'dots', 'progress'
+	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+	reporters: ['progress'],
 
 
-    // web server port
-    port: 9876,
+	// web server port
+	port: 9876,
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+	// enable / disable colors in the output (reporters and logs)
+	colors: true,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+	// level of logging
+	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+	// enable / disable watching file and executing tests whenever any file changes
+	autoWatch: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+	// start these browsers
+	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	browsers: ['Chrome'],
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+	// Continuous Integration mode
+	// if true, Karma captures browsers, runs the tests and exits
+	singleRun: false
   });
 };
