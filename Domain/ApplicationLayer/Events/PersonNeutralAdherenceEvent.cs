5fbd9e0713069c0f4f6c7e49eb23a6e10df2e13b
Xianwei Shen <xianwei.shen@teleopti.com>
2015-04-07 13:23:30 +0200

    PBI #30930 process PersonNeutralAdherenceEvent in hangfire

--- Domain/ApplicationLayer/Events/PersonNeutralAdherenceEvent.cs
+++ Domain/ApplicationLayer/Events/PersonNeutralAdherenceEvent.cs
@@ -4,7 +4,7 @@ using Teleopti.Interfaces.Messages;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Events
 {
-	public class PersonNeutralAdherenceEvent : IEvent, ILogOnInfo
+	public class PersonNeutralAdherenceEvent : IEvent, ILogOnInfo, IGoToHangfire
 	{
 		public Guid PersonId { get; set; }
 		public DateOnly? BelongsToDate { get; set; }
