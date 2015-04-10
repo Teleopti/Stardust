fbaa953ba932e0660e156a43f726404b21ecbf3c
Tamas Balog <tamas.balogh@teleopti.com>
2015-04-10 12:56:07 +0200

    change limit on block team same start time sikuli

--- Win/Sikuli/Validators/RootValidators/OptimizeBlockTeamSameStartTimeValidator.cs
+++ Win/Sikuli/Validators/RootValidators/OptimizeBlockTeamSameStartTimeValidator.cs
@@ -20,7 +20,7 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators.RootValidators
 
 		public override SikuliValidationResult Validate(ITestDuration duration)
 		{
-			const double periodStandardDeviationLimit = 0.07d;
+			const double periodStandardDeviationLimit = 0.08d;
 			AtomicValidators.Add(new PeriodStandardDeviationValidator(_schedulerState, _totalSkill, periodStandardDeviationLimit));
 			AtomicValidators.Add(new DurationValidator(TimeSpan.FromMinutes(1).Add(TimeSpan.FromSeconds(25)), duration));
 			return ValidateAtomicValidators(AtomicValidators);
