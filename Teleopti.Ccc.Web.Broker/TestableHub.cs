4b68ad2b08d16e45d95493bd4bb37465184deb4d
Team Real-time <greenmachine@teleopti.com>
2015-04-14 13:59:02 +0200

    #32732 fix build

--- Teleopti.Ccc.Web.Broker/TestableHub.cs
+++ Teleopti.Ccc.Web.Broker/TestableHub.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using Microsoft.AspNet.SignalR;
 
 namespace Teleopti.Ccc.Web.Broker
 {
+	[CLSCompliant(false)]
 	// makes the hub back portable to 390
 	public class TestableHub : Hub
 	{
