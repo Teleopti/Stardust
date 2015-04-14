8fd323ebb15f14f455b8126b6e1eaecd4cceb532
Robin Karlsson <robin.karlsson@teleopti.com>
2015-04-14 16:22:34 +0200

    Flattening structure from correct place

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -5,8 +5,10 @@ call ..\.node\npm install -g karma-teamcity-reporter
 call ..\.node\npm install -g karma-chrome-launcher
 call ..\.node\npm install -g karma-jasmine
 call ..\.node\npm install -g karma-cli
+call ..\.node\npm install -g flatten-packages
 
 for /f "tokens=1" %%i in ('..\.node\npm bin -g') do set output=%%i
 echo "%output%"
 
+call "%output%\flatten-packages.cmd" "%output%"
 "%output%\karma" start --reporters teamcity --single-run
