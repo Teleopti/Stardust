d21e369fac84caef2dd7730fad9e40430c8b5da8
Ola <none@none>
2015-04-16 13:27:46 +0200

    clean up

--- Teleopti.Ccc.WinCodeTest/Configuration/ChangePasswordPresenterTest.cs
+++ Teleopti.Ccc.WinCodeTest/Configuration/ChangePasswordPresenterTest.cs
@@ -12,205 +12,172 @@ using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.WinCodeTest.Configuration
 {
-    [TestFixture]
-    public class ChangePasswordPresenterTest
-    {
-        private ChangePasswordPresenter target;
-        private MockRepository mocks;
-        private IUnitOfWorkFactory unitOfWorkFactory;
-        private IRepositoryFactory repositoryFactory;
-        private IUnitOfWork unitOfWork;
-        private IPerson person;
-        private Guid personId;
-        private IChangePasswordView view;
-        private IOneWayEncryption oneWayEncryption;
-        private IApplicationAuthenticationInfo applicationAuthenticationInfo;
-        private IPasswordPolicy passwordPolicy;
-        private IPersonRepository personRepository;
-	    private IChangeUserPassword changePw;
+	[TestFixture]
+	public class ChangePasswordPresenterTest
+	{
+		private ChangePasswordPresenter target;
+		private MockRepository mocks;
+		private IUnitOfWorkFactory unitOfWorkFactory;
+		private IRepositoryFactory repositoryFactory;
+		private IUnitOfWork unitOfWork;
+		private IPerson person;
+		private Guid personId;
+		private IChangePasswordView view;
+		private IOneWayEncryption oneWayEncryption;
+		private IApplicationAuthenticationInfo applicationAuthenticationInfo;
+		private IPasswordPolicy passwordPolicy;
+		private IPersonRepository personRepository;
+		private IChangeUserPassword changePw;
 
-	    [SetUp]
-        public void Setup()
-        {
-            mocks = new MockRepository();
-            unitOfWorkFactory = mocks.StrictMock<IUnitOfWorkFactory>();
-            repositoryFactory = mocks.StrictMock<IRepositoryFactory>();
-            unitOfWork = mocks.StrictMock<IUnitOfWork>();
-            person = mocks.StrictMock<IPerson>();
-            personId = Guid.Empty;
-            view = mocks.StrictMock<IChangePasswordView>();
-            passwordPolicy = mocks.StrictMock<IPasswordPolicy>();
-            oneWayEncryption = mocks.StrictMock<IOneWayEncryption>();
-            applicationAuthenticationInfo = mocks.StrictMock<IApplicationAuthenticationInfo>();
-            personRepository = mocks.StrictMock<IPersonRepository>();
-	        changePw = mocks.DynamicMock<IChangeUserPassword>();
-			  target = new ChangePasswordPresenter(view, passwordPolicy, unitOfWorkFactory, repositoryFactory, oneWayEncryption, changePw);
-        }
+		[SetUp]
+		public void Setup()
+		{
+			mocks = new MockRepository();
+			unitOfWorkFactory = MockRepository.GenerateMock<IUnitOfWorkFactory>();
+			repositoryFactory = MockRepository.GenerateMock<IRepositoryFactory>();
+			unitOfWork = MockRepository.GenerateMock<IUnitOfWork>();
+			person = MockRepository.GenerateMock<IPerson>();
+			personId = Guid.Empty;
+			view = MockRepository.GenerateMock<IChangePasswordView>();
+			passwordPolicy = MockRepository.GenerateMock<IPasswordPolicy>();
+			oneWayEncryption = MockRepository.GenerateMock<IOneWayEncryption>();
+			applicationAuthenticationInfo = MockRepository.GenerateMock<IApplicationAuthenticationInfo>();
+			personRepository = MockRepository.GenerateMock<IPersonRepository>();
+			changePw = MockRepository.GenerateMock<IChangeUserPassword>();
+			target = new ChangePasswordPresenter(view, passwordPolicy, unitOfWorkFactory, repositoryFactory, oneWayEncryption, changePw);
+		}
 
-        private void initializeExpectation()
-        {
-            Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
-            Expect.Call(repositoryFactory.CreatePersonRepository(unitOfWork)).Return(personRepository);
-            Expect.Call(personRepository.Get(personId)).Return(person);
-            Expect.Call(person.ApplicationAuthenticationInfo).Return(applicationAuthenticationInfo);
-            Expect.Call(applicationAuthenticationInfo.Password).Return("currentEncryptedPassword");
-            Expect.Call(unitOfWork.Dispose);
-            Expect.Call(view.SetInputFocus);
-        }
+		private void initializeExpectation()
+		{
+			unitOfWorkFactory.Stub(x => x.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			repositoryFactory.Stub(x => x.CreatePersonRepository(unitOfWork)).Return(personRepository);
+			personRepository.Stub(x => x.Get(personId)).Return(person);
+			person.Stub(x => x.ApplicationAuthenticationInfo).Return(applicationAuthenticationInfo);
+			applicationAuthenticationInfo.Stub(x => x.Password).Return("currentEncryptedPassword");
+			unitOfWork.Stub(x => x.Dispose());
+			view.Stub(x => x.SetInputFocus());
+		}
 
-        [Test]
-        public void VerifyOldPasswordMustMatch()
-        {
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(()=>view.SetOldPasswordValid(false));
-                Expect.Call(oneWayEncryption.EncryptString("currentNotEncryptedPassword2")).Return(
-                    "currentEncryptedPassword2");
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.SetOldPassword("currentNotEncryptedPassword2");
-            }
-        }
+		[Test]
+		public void VerifyOldPasswordMustMatch()
+		{
 
-        [Test]
-        public void VerifyNewPasswordMustPassPolicyCheck()
-        {
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(() => view.SetNewPasswordValid(false));
-                Expect.Call(passwordPolicy.CheckPasswordStrength("newNotEncryptedPassword2")).Return(false);
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.SetNewPassword("newNotEncryptedPassword2");
-            }
-        }
+			initializeExpectation();
+			view.Stub(x => x.SetOldPasswordValid(false));
+			oneWayEncryption.Stub(x => x.EncryptString("currentNotEncryptedPassword2")).Return(
+				"currentEncryptedPassword2");
 
-        [Test]
-        public void VerifyNewPasswordsWhenMatch()
-        {
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(() => view.SetNewPasswordValid(true));
-                Expect.Call(passwordPolicy.CheckPasswordStrength("newNotEncryptedPassword2")).Return(true);
-                Expect.Call(() => view.SetConfirmPasswordValid(true));
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.SetNewPassword("newNotEncryptedPassword2");
-                target.SetConfirmNewPassword("newNotEncryptedPassword2");
-            }
-        }
+			target.Initialize();
+			target.SetOldPassword("currentNotEncryptedPassword2");
+		}
 
-        [Test]
-        public void VerifySaveNewPasswordsWhenMatch()
-        {
-            var userDetailRepository = mocks.StrictMock<IUserDetailRepository>();
-            var userDetail = mocks.StrictMock<IUserDetail>();
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
-                Expect.Call(repositoryFactory.CreatePersonRepository(unitOfWork)).Return(personRepository);
-                Expect.Call(repositoryFactory.CreateUserDetailRepository(unitOfWork)).Return(userDetailRepository);
-                Expect.Call(personRepository.Get(personId)).Return(person);
-                Expect.Call(userDetailRepository.FindByUser(person)).Return(userDetail);
-                Expect.Call(person.ChangePassword("newNotEncryptedPassword2",
-                                                  StateHolderReader.Instance.StateReader.ApplicationScopeData.
-                                                      LoadPasswordPolicyService, userDetail)).Return(true);
-                Expect.Call(passwordPolicy.CheckPasswordStrength("newNotEncryptedPassword2")).Return(true);
-                Expect.Call(unitOfWork.PersistAll()).Return(new List<IRootChangeInfo>(0));
-                Expect.Call(unitOfWork.Dispose);
-                Expect.Call(view.Close);
-	            Expect.Call(changePw.SetNewPassword(null))
-		            .IgnoreArguments()
-		            .Return(new ChangeUserPasswordResult {Success = true});
+		[Test]
+		public void VerifyNewPasswordMustPassPolicyCheck()
+		{
+			initializeExpectation();
+			view.Stub(x => x.SetNewPasswordValid(false));
+			passwordPolicy.Stub(x => x.CheckPasswordStrength("newNotEncryptedPassword2")).Return(false);
 
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.Model.OldEnteredEncryptedPassword = target.Model.OldEncryptedPassword;
-                target.Model.NewPassword = "newNotEncryptedPassword2";
-                target.Model.ConfirmPassword = "newNotEncryptedPassword2";
-                target.Save();
-                Assert.AreEqual("newNotEncryptedPassword2",
-                                ((IUnsafePerson)TeleoptiPrincipal.CurrentPrincipal).Person.ApplicationAuthenticationInfo.Password);
-            }
-        }
+			target.Initialize();
+			target.SetNewPassword("newNotEncryptedPassword2");
+		}
 
-        [Test]
-        public void VerifyWhenPasswordCouldNotBeChanged()
-        {
-            var userDetailRepository = mocks.StrictMock<IUserDetailRepository>();
-            var userDetail = mocks.StrictMock<IUserDetail>();
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(unitOfWorkFactory.CreateAndOpenUnitOfWork()).Return(unitOfWork);
-                Expect.Call(repositoryFactory.CreatePersonRepository(unitOfWork)).Return(personRepository);
-                Expect.Call(repositoryFactory.CreateUserDetailRepository(unitOfWork)).Return(userDetailRepository);
-                Expect.Call(personRepository.Get(personId)).Return(person);
-                Expect.Call(userDetailRepository.FindByUser(person)).Return(userDetail);
-                Expect.Call(person.ChangePassword("newNotEncryptedPassword2",
-                                                  StateHolderReader.Instance.StateReader.ApplicationScopeData.
-                                                      LoadPasswordPolicyService, userDetail)).Return(false);
-                Expect.Call(passwordPolicy.CheckPasswordStrength("newNotEncryptedPassword2")).Return(true);
-                Expect.Call(unitOfWork.Dispose);
-                Expect.Call(view.ShowValidationError);
+		[Test]
+		public void VerifyNewPasswordsWhenMatch()
+		{
+			initializeExpectation();
+			view.Stub(x => x.SetNewPasswordValid(true));
+			passwordPolicy.Stub(x => x.CheckPasswordStrength("newNotEncryptedPassword2")).Return(true);
+			view.Stub(x => x.SetConfirmPasswordValid(true));
 
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.Model.OldEnteredEncryptedPassword = target.Model.OldEncryptedPassword;
-                target.Model.NewPassword = "newNotEncryptedPassword2";
-                target.Model.ConfirmPassword = "newNotEncryptedPassword2";
-                target.Save();
-            }
-        }
+			target.Initialize();
+			target.SetNewPassword("newNotEncryptedPassword2");
+			target.SetConfirmNewPassword("newNotEncryptedPassword2");
+		}
 
-        [Test]
-        public void VerifySaveWithErrorInModel()
-        {
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(view.ShowValidationError);
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.Model.NewPassword = "newNotEncryptedPassword2";
-                target.Model.ConfirmPassword = "newNotEncryptedPassword3";
-                target.Save();
-            }
-        }
+		[Test]
+		public void VerifySaveNewPasswordsWhenMatch()
+		{
+			var userDetailRepository = mocks.StrictMock<IUserDetailRepository>();
+			var userDetail = mocks.StrictMock<IUserDetail>();
 
-        [Test]
-        public void VerifySaveWithErrorWhenNoNewPassword()
-        {
-            using (mocks.Record())
-            {
-                initializeExpectation();
-                Expect.Call(view.ShowValidationError);
-            }
-            using (mocks.Playback())
-            {
-                target.Initialize();
-                target.Model.OldEncryptedPassword = "encrypted!";
-                target.Model.OldEnteredEncryptedPassword = "encrypted!";
-                target.Model.NewPassword = "";
-                target.Model.ConfirmPassword = "";
-                target.Save();
-            }
-        }
-    }
+			initializeExpectation();
+			unitOfWorkFactory.Stub(x => x.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			repositoryFactory.Stub(x => x.CreatePersonRepository(unitOfWork)).Return(personRepository);
+			repositoryFactory.Stub(x => x.CreateUserDetailRepository(unitOfWork)).Return(userDetailRepository);
+			personRepository.Stub(x => x.Get(personId)).Return(person);
+			userDetailRepository.Stub(x => x.FindByUser(person)).Return(userDetail);
+			person.Stub(x => x.ChangePassword("newNotEncryptedPassword2",
+														StateHolderReader.Instance.StateReader.ApplicationScopeData.
+															LoadPasswordPolicyService, userDetail)).Return(true).IgnoreArguments();
+			passwordPolicy.Stub(x => x.CheckPasswordStrength("newNotEncryptedPassword2")).Return(true);
+			unitOfWork.Stub(x => x.PersistAll()).Return(new List<IRootChangeInfo>(0));
+			unitOfWork.Stub(x => x.Dispose());
+			view.Stub(x => x.Close());
+			changePw.Stub(x => x.SetNewPassword(null))
+				.IgnoreArguments()
+				.Return(new ChangeUserPasswordResult { Success = true });
+
+
+			target.Initialize();
+			target.Model.OldEnteredEncryptedPassword = target.Model.OldEncryptedPassword;
+			target.Model.NewPassword = "newNotEncryptedPassword2";
+			target.Model.ConfirmPassword = "newNotEncryptedPassword2";
+			target.Save();
+			Assert.AreEqual("newNotEncryptedPassword2",
+								((IUnsafePerson)TeleoptiPrincipal.CurrentPrincipal).Person.ApplicationAuthenticationInfo.Password);
+		}
+
+		[Test]
+		public void VerifyWhenPasswordCouldNotBeChanged()
+		{
+			var userDetailRepository = mocks.StrictMock<IUserDetailRepository>();
+			var userDetail = mocks.StrictMock<IUserDetail>();
+
+			initializeExpectation();
+			unitOfWorkFactory.Stub(x => x.CreateAndOpenUnitOfWork()).Return(unitOfWork);
+			repositoryFactory.Stub(x => x.CreatePersonRepository(unitOfWork)).Return(personRepository);
+			repositoryFactory.Stub(x => x.CreateUserDetailRepository(unitOfWork)).Return(userDetailRepository);
+			personRepository.Stub(x => x.Get(personId)).Return(person);
+			userDetailRepository.Stub(x => x.FindByUser(person)).Return(userDetail);
+			person.Stub(x => x.ChangePassword("newNotEncryptedPassword2",
+														StateHolderReader.Instance.StateReader.ApplicationScopeData.
+															LoadPasswordPolicyService, userDetail)).Return(false);
+			passwordPolicy.Stub(x => x.CheckPasswordStrength("newNotEncryptedPassword2")).Return(true);
+			unitOfWork.Stub(x => x.Dispose());
+			view.Stub(x => x.ShowValidationError());
+
+
+			target.Initialize();
+			target.Model.OldEnteredEncryptedPassword = target.Model.OldEncryptedPassword;
+			target.Model.NewPassword = "newNotEncryptedPassword2";
+			target.Model.ConfirmPassword = "newNotEncryptedPassword2";
+			target.Save();
+		}
+
+		[Test]
+		public void VerifySaveWithErrorInModel()
+		{
+			initializeExpectation();
+			view.Stub(x => x.ShowValidationError());
+
+			target.Initialize();
+			target.Model.NewPassword = "newNotEncryptedPassword2";
+			target.Model.ConfirmPassword = "newNotEncryptedPassword3";
+			target.Save();
+		}
+
+		[Test]
+		public void VerifySaveWithErrorWhenNoNewPassword()
+		{
+			initializeExpectation();
+
+			target.Initialize();
+			target.Model.OldEncryptedPassword = "encrypted!";
+			target.Model.OldEnteredEncryptedPassword = "encrypted!";
+			target.Model.NewPassword = "";
+			target.Model.ConfirmPassword = "";
+			target.Save();
+		}
+	}
 }
