7812ecf5b801c44b113b1ca52b0003dd2f76fee1
Robin Karlsson <robin.karlsson@teleopti.com>
2015-04-17 16:12:57 +0200

    Any difference now?

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -1,6 +1,6 @@
 :: Runs from [repo]\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM
 
-call ..\.node\npm install -g karma --save-dev
+call ..\.node\npm install -g karma
 call ..\.node\npm install -g karma-teamcity-reporter
 call ..\.node\npm install -g karma-chrome-launcher
 call ..\.node\npm install -g karma-jasmine
@@ -11,4 +11,4 @@ for /f "tokens=1" %%i in ('..\.node\npm bin -g') do set output=%%i
 echo "%output%"
 
 call "%output%\flatten-packages.cmd" "%output%"
-"%output%\karma" start --reporters teamcity --single-run
+call "%output%\karma" start --reporters teamcity --single-run
