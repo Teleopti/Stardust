b65a60ad7c9036406f30781c263781511f47501d
Roger Kratz <roger.kratz@teleopti.com>
2015-04-28 13:04:45 +0200

    remove dead code

--- Domain/Security/ISystemUserPasswordSpecification.cs
+++
@@ -1,9 +0,0 @@
-using Teleopti.Interfaces.Domain;
-
-namespace Teleopti.Ccc.Domain.Security
-{
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces")]
-    public interface ISystemUserPasswordSpecification : ISpecification<string>
-    {
-    }
-}
