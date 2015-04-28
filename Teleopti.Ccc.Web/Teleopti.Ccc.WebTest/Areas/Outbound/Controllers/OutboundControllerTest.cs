f2a4607a49c8a1cea1fa14e9c543531c64a1c621
Jianguang Fang <jianguang.fang@teleopti.com>
2015-04-28 11:29:37 +0800

    code cleaning

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Outbound/Controllers/OutboundControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Outbound/Controllers/OutboundControllerTest.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Web.Http;
 using System.Web.Http.Results;
-using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using Rhino.Mocks;
 using SharpTestsEx;
