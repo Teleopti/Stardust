2df8cafd5e62fcd85d6568f78812f6aa08c55c28
Tamas Balog <tamas.balogh@teleopti.com>
2015-04-28 16:55:17 +0200

    refactor optimize interval balance validator

--- Win/Sikuli/Validators/RootValidators/OptimizeIntervalBalanceAfterValidator.cs
+++ Win/Sikuli/Validators/RootValidators/OptimizeIntervalBalanceAfterValidator.cs
@@ -13,15 +13,25 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators.RootValidators
 		private readonly ISchedulerStateHolder _schedulerState;
 		private readonly IAggregateSkill _totalSkill;
 
+		private const double limit = 0.8d;
+		private const int maxRuleBreaks = 3;
+		private TimeSpan durationLimit;
+
 		public OptimizeIntervalBalanceAfterValidator(ISchedulerStateHolder schedulerState, IAggregateSkill totalSkill)
 		{
 			_schedulerState = schedulerState;
 			_totalSkill = totalSkill;
+			durationLimit = TimeSpan.FromMinutes(1);
 		}
 
 		public string Description
 		{
-			get { return "Only two 'lowest intra interval balance' value can be under 0.75. Duration must be under 1 minute."; }
+			get
+			{
+				return string.Format(
+					"Only {0} 'lowest intra interval balance' value can be under {1}. Duration must be under {2}.",
+					maxRuleBreaks, limit, durationLimit.ToString(@"mm\:ss"));
+			}
 		}
 
 		public SikuliValidationResult Validate(ITestDuration duration)
@@ -60,7 +70,6 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators.RootValidators
 				result.Result = SikuliValidationResult.ResultValue.Warn;
 				result.Details.AppendLine(string.Format("Broken rules: {0}", ruleBreaks));
 			}
-			const int maxRuleBreaks = 2;
 			if (ruleBreaks > maxRuleBreaks)
 				result.Result = SikuliValidationResult.ResultValue.Fail;
 			result.Details.AppendLine(string.Format("Lowest intra interval balance: {0}", result.Result));
@@ -68,7 +77,6 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators.RootValidators
 
 		private int checkInternalBalanceRuleBreaks(IEnumerable<double?> intervalBalances)
 		{
-			const double limit = 0.75d;
 			int numberOfRuleBreaks = intervalBalances.Count(intervalBalance => intervalBalance < limit);
 			return numberOfRuleBreaks;
 		}
