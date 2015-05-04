313a9ea467f0ec0431b9bf7d745e1bd261555003
Roger Kratz <roger.kratz@teleopti.com>
2015-05-04 10:55:50 +0200

    Adding FindPersonInfo

---
+++ Infrastructure/MultiTenancy/Server/FindPersonInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Teleopti.Ccc.Infrastructure.MultiTenancy.Server.NHibernate;
+
+namespace Teleopti.Ccc.Infrastructure.MultiTenancy.Server
+{
+	public class FindPersonInfo : IFindPersonInfo
+	{
+		private readonly ICurrentTenantSession _currentTenantSession;
+
+		public FindPersonInfo(ICurrentTenantSession currentTenantSession)
+		{
+			_currentTenantSession = currentTenantSession;
+		}
+
+		public PersonInfo GetById(Guid id)
+		{
+			return _currentTenantSession.CurrentSession()
+				.Get<PersonInfo>(id);
+		}
+	}
+}
---
+++ Infrastructure/MultiTenancy/Server/IFindPersonInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Teleopti.Ccc.Infrastructure.MultiTenancy.Server
+{
+	public interface IFindPersonInfo
+	{
+		PersonInfo GetById(Guid id);
+	}
+}
