30a5199c1aecd970547fe4110a46724488c6163a
Kunning Mao <none@none>
2015-05-04 11:10:32 +0200

    try to fix #33243 SMS  becomes ? in the message

--- Domain/Notification/NotificationWebClient.cs
+++ Domain/Notification/NotificationWebClient.cs
@@ -15,6 +15,7 @@ namespace Teleopti.Ccc.Domain.Notification
             _url = url;
             _client = new WebClient();
             _client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+			_client.Headers.Add("Accept-Language", "en-US,en;q=0.8,sv-SE;q=0.7,sv;q=0.5");
         }
 
         public void Dispose()
