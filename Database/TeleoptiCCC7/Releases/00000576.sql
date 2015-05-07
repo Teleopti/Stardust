c66d598146c1aa97396a63f0a4895c6191a4820e
Anders Forsberg <anders.forsberg@teleopti.com>
2015-05-07 14:27:17 +0200

    Dummy to kick build pipe...

--- Database/TeleoptiCCC7/Releases/00000576.sql
+++ Database/TeleoptiCCC7/Releases/00000576.sql
@@ -2,4 +2,4 @@
 --Name: David
 --Desc: bug #33307 - database CPU and IO load is too high. Table scan
 ---------------- 
-/* AF: Ouch, does not work on Azure... Will fix in next release script */
+/* AF: Ouch, does not work on Azure... Will fix in next release script  */
