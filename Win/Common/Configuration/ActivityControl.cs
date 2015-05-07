d46b946c4feb898d11862f237a5e3fcf31b585fa
Claes Stenman <claes.stenman@teleopti.com>
2015-05-07 11:13:18 +0200

    Bugfix:#33291: Crash when clicking "Scheduling" in Options, #33301: Crash when click on Activity on Option

--- Win/Common/Configuration/ActivityControl.cs
+++ Win/Common/Configuration/ActivityControl.cs
@@ -242,7 +242,7 @@ namespace Teleopti.Ccc.Win.Common.Configuration
 		{
 			_sourceList.Clear();
 			var activityRepository = new ActivityRepository(uow);
-			_sourceList.Add(GridType.Activity, activityRepository.LoadAllWithUpdatedBy().Where(a => a.IsOutboundActivity == false));
+			_sourceList.Add(GridType.Activity, activityRepository.LoadAllWithUpdatedBy().Where(a => a.IsOutboundActivity == false).ToList());
 		}
 
 		private List<T> getSource<T>(GridType gridType)
