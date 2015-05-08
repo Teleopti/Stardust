a08496381e9599bdab6db15c8da4281bc704ace0
Ola <none@none>
2015-05-08 11:24:47 +0200

    strange works for me

--- Database/TeleoptiCCC7/Releases/00000579.sql
+++ Database/TeleoptiCCC7/Releases/00000579.sql
@@ -1,32 +1,32 @@
+-- works locally
+  --CREATE table #double(foreignid nvarchar(100) COLLATE database_default )
 
-  CREATE table #double(foreignid nvarchar(100) COLLATE database_default )
+  ---- doubles
+  --INSERT INTO #double
+  --select  ForeignId FROM ApplicationFunction f
+  --INNER JOIN ApplicationFunctionInRole fr ON f.Id = fr.ApplicationFunction
+  --where ForeignSource = 'Matrix'
+  --GROUP BY ForeignId, fr.ApplicationRole
+  -- HAVING count(*) > 1
 
-  -- doubles
-  INSERT INTO #double
-  select  ForeignId FROM ApplicationFunction f
-  INNER JOIN ApplicationFunctionInRole fr ON f.Id = fr.ApplicationFunction
-  where ForeignSource = 'Matrix'
-  GROUP BY ForeignId, fr.ApplicationRole
-   HAVING count(*) > 1
+  --  CREATE table #minid(id uniqueidentifier, foreignid nvarchar(100) COLLATE database_default   )
+  --          INSERT INTO #minid
+  --          SELECT min(convert(varchar(100),id)), d.foreignid  FROM ApplicationFunction f
+  --          INNER JOIN #double d ON f.ForeignId = d.foreignid
+  --          GROUP BY d.foreignid
 
-    CREATE table #minid(id uniqueidentifier, foreignid nvarchar(100) COLLATE database_default   )
-            INSERT INTO #minid
-            SELECT min(convert(varchar(100),id)), d.foreignid  FROM ApplicationFunction f
-            INNER JOIN #double d ON f.ForeignId = d.foreignid
-            GROUP BY d.foreignid
+  -- --all id with those foreign
+  -- CREATE table #tochange(id uniqueidentifier, foreignid nvarchar(100 ) COLLATE database_default , thenewid uniqueidentifier)
+  -- INSERT INTO #tochange
+  -- SELECT f.id, f.ForeignId, m.id FROM ApplicationFunction f
+  -- INNER JOIN #minid m ON m.foreignid = f.ForeignId
 
-   --all id with those foreign
-   CREATE table #tochange(id uniqueidentifier, foreignid nvarchar(100 ) COLLATE database_default , thenewid uniqueidentifier)
-   INSERT INTO #tochange
-   SELECT f.id, f.ForeignId, m.id FROM ApplicationFunction f
-   INNER JOIN #minid m ON m.foreignid = f.ForeignId
+  --DELETE FROM ApplicationFunctionInRole 
+  --from ApplicationFunctionInRole a 
+  --INNER JOIN #tochange c
+  --ON a.ApplicationFunction = c.id
+  --WHERE id <> thenewid
 
-  DELETE FROM ApplicationFunctionInRole 
-  from ApplicationFunctionInRole a 
-  INNER JOIN #tochange c
-  ON a.ApplicationFunction = c.id
-  WHERE id <> thenewid
-
-  DELETE FROM ApplicationFunction
-  where ForeignSource = 'Matrix'
-  AND Id not in(SELECT ApplicationFunction FROM ApplicationFunctionInRole)
+  --DELETE FROM ApplicationFunction
+  --where ForeignSource = 'Matrix'
+  --AND Id not in(SELECT ApplicationFunction FROM ApplicationFunctionInRole)
