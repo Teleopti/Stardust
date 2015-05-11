af468e67200d0f198a71d9eec50c5d29f9b733f5
Mathias Stenbom <mathias.stenbom@teleopti.com>
2015-05-11 11:47:43 +0200

    #32733 some more ioc tests for message broker server

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/MessageBrokerServerTestAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.Web.Broker;
+
+namespace Teleopti.Ccc.WebTest.Core.MessageBroker
+{
+	public class MessageBrokerServerTestAttribute : IoCTestAttribute
+	{
+		protected override void RegisterInContainer(ContainerBuilder builder, IIocConfiguration configuration)
+		{
+			builder.RegisterModule<MessageBrokerWebModule>();
+			builder.RegisterModule<MessageBrokerServerModule>();
+			builder.RegisterInstance(new FakeSignalR()).AsSelf().As<ISignalR>();
+			builder.RegisterInstance(new ActionImmediate()).As<IActionScheduler>();
+		}
+	}
+}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/SignalRPublishTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/MessageBroker/SignalRPublishTest.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
 using NUnit.Framework;
-using Rhino.Mocks.Constraints;
 using SharpTestsEx;
-using Teleopti.Ccc.IocCommon;
-using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Ccc.Web.Broker;
 using Teleopti.Interfaces.MessageBroker;
 
@@ -16,17 +10,9 @@ namespace Teleopti.Ccc.WebTest.Core.MessageBroker
 {
 	[CLSCompliant(false)]
 	[TestFixture]
-	[IoCTest]
-	public class SignalRPublishTest : IRegisterInContainer
+	[MessageBrokerServerTest]
+	public class SignalRPublishTest
 	{
-		public void RegisterInContainer(ContainerBuilder builder, IIocConfiguration configuration)
-		{
-			builder.RegisterModule<MessageBrokerWebModule>();
-			builder.RegisterModule<MessageBrokerServerModule>();
-			builder.RegisterInstance(new FakeSignalR()).AsSelf().As<ISignalR>();
-			builder.RegisterInstance(new ActionImmediate()).As<IActionScheduler>();
-		}
-
 		public IMessageBrokerServer Server;
 		public FakeSignalR SignalR;
 
@@ -37,19 +23,46 @@ namespace Teleopti.Ccc.WebTest.Core.MessageBroker
 			Server.NotifyClients(notification);
 			SignalR.SentMessage.Should().Be(notification);
 		}
+
+		[Test]
+		public void ShouldPublishToAllRoutes()
+		{
+			var notification = new Notification();
+
+			Server.NotifyClients(notification);
+
+			SignalR.SentToGroups.Should().Have.SameValuesAs(notification.Routes().Select(MessageBrokerServer.RouteToGroupName));
+			SignalR.SentRoutes.Should().Have.SameValuesAs(notification.Routes());
+		}
+
+		[Test]
+		public void ShouldPublishMultipleNotifications()
+		{
+			var notifications = new[] { new Notification(), new Notification() };
+			Server.NotifyClientsMultiple(notifications);
+			SignalR.SentMessages.Should().Have.SameValuesAs(notifications);
+		}
 	}
 
 	public class FakeSignalR : ISignalR
 	{
 		public Notification SentMessage;
-		public string CalledGroup;
-		public string CalledRoute;
+		public string SentToGroup;
+		public string SentRoute;
+
+		public IList<string> SentToGroups = new List<string>();
+		public IList<string> SentRoutes = new List<string>();
+		public IList<Notification> SentMessages = new List<Notification>();
 
 		public void CallOnEventMessage(string groupName, string route, Notification notification)
 		{
-			CalledGroup = groupName;
-			CalledRoute = route;
+			SentToGroup = groupName;
+			SentRoute = route;
 			SentMessage = notification;
+
+			SentToGroups.Add(groupName);
+			SentRoutes.Add(route);
+			SentMessages.Add(notification);
 		}
 	}
 }
