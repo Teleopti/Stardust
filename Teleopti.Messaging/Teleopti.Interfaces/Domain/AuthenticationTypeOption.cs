0e0564097bfc0074626ec0eb3dc09f2acf18ca3b
Roger Kratz <roger.kratz@teleopti.com>
2015-05-12 10:41:03 +0200

    simplified AuthenticationTypeOption

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/AuthenticationTypeOption.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/AuthenticationTypeOption.cs
@@ -1,31 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Teleopti.Interfaces.Domain
 {
-    ///<summary>
-    /// Authentication type enumeration. 
-    ///</summary>
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1714:FlagsEnumsShouldHavePluralNames"), Flags]
 	public enum AuthenticationTypeOption
 	{
-
-		/// <summary>
-		/// Authentication type is unknown or not logged on
-		/// </summary>
-		Unknown = 1,
-
-		/// <summary>
-		/// Authentication type is application: via username - password
-		/// </summary>
-		Application = 2,
-
-		/// <summary>
-		/// Authentication type is windows: via username in domain
-		/// </summary>
-		Windows = 4
+		Application,
+		Windows
 	}
-
-	
 }
