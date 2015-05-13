fc4e10d954b771a4dc56546d24bff52375d94168
Zhiping Lan <zhiping.lan@teleopti.com>
2015-05-13 12:46:33 +0800

    clean code

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/TeamSchedule/Mapping/AgentScheduleViewModelReworkedMapperTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/TeamSchedule/Mapping/AgentScheduleViewModelReworkedMapperTest.cs
@@ -17,6 +17,7 @@ using Teleopti.Ccc.Web.Areas.MyTime.Core.TeamSchedule.DataProvider;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.TeamSchedule.Mapping;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.TeamSchedule;
 using Teleopti.Ccc.Web.Core;
+using Teleopti.Ccc.WebTest.Core.TeamSchedule.Mapping;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.TeamSchedule.Mapping
@@ -41,42 +42,7 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.TeamSchedule.Mapping
 		}
 	}
 
-	public class FakePersonScheduleDayReadModelFinder : IPersonScheduleDayReadModelFinder
-	{
-		public IEnumerable<PersonScheduleDayReadModel> ForPerson(DateOnly startDate, DateOnly endDate, Guid personId)
-		{
-			throw new NotImplementedException();
-		}
-
-		public PersonScheduleDayReadModel ForPerson(DateOnly date, Guid personId)
-		{
-			throw new NotImplementedException();
-		}
-
-		public IEnumerable<PersonScheduleDayReadModel> ForPeople(DateTimePeriod period, IEnumerable<Guid> personIds)
-		{
-			throw new NotImplementedException();
-		}
-
-		public IEnumerable<PersonScheduleDayReadModel> ForBulletinPersons(IEnumerable<string> shiftExchangeOfferIdList, Paging paging)
-		{
-			throw new NotImplementedException();
-		}
-
-		public IEnumerable<PersonScheduleDayReadModel> ForPersons(DateOnly date, IEnumerable<Guid> personIdList, Paging paging, TimeFilterInfo filterInfo = null,
-			string timeSortOrder = "")
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	public class FakeTeamSchedulePersonsProvider : ITeamSchedulePersonsProvider
-	{
-		public IEnumerable<Guid> RetrievePersons(TeamScheduleViewModelData data)
-		{
-			throw new NotImplementedException();
-		}
-	}
+	
 
 
 	[TestFixture]
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/TeamSchedule/Mapping/TeamScheduleViewModelReworkedMapperTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/TeamSchedule/Mapping/TeamScheduleViewModelReworkedMapperTest.cs
@@ -1,7 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using SharpTestsEx;
+using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.PersonScheduleDayReadModel;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.TeamSchedule.DataProvider;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.TeamSchedule.Mapping;
 using Teleopti.Ccc.WebTest.Areas.MyTime.Core.TeamSchedule.Mapping;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WebTest.Core.TeamSchedule.Mapping
 {
@@ -10,11 +16,49 @@ namespace Teleopti.Ccc.WebTest.Core.TeamSchedule.Mapping
 	public class TeamScheduleViewModelReworkedMapperTest
 	{
 		public ITeamScheduleViewModelReworkedMapper Mapper;
+		
 		[Test]
 		public void ShouldMap()
 		{
 			var target = Mapper.Map(new TeamScheduleViewModelData());
 			target.Should().Not.Be.Null();
+		}
+	}
+
+	public class FakePersonScheduleDayReadModelFinder : IPersonScheduleDayReadModelFinder
+	{
+		public IEnumerable<PersonScheduleDayReadModel> ForPerson(DateOnly startDate, DateOnly endDate, Guid personId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public PersonScheduleDayReadModel ForPerson(DateOnly date, Guid personId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<PersonScheduleDayReadModel> ForPeople(DateTimePeriod period, IEnumerable<Guid> personIds)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<PersonScheduleDayReadModel> ForBulletinPersons(IEnumerable<string> shiftExchangeOfferIdList, Paging paging)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<PersonScheduleDayReadModel> ForPersons(DateOnly date, IEnumerable<Guid> personIdList, Paging paging, TimeFilterInfo filterInfo = null,
+			string timeSortOrder = "")
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class FakeTeamSchedulePersonsProvider : ITeamSchedulePersonsProvider
+	{
+		public IEnumerable<Guid> RetrievePersons(TeamScheduleViewModelData data)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
