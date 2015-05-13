8c53160504c0714444a91b7eede892f4576a9276
Robin Karlsson <robin.karlsson@teleopti.com>
2015-05-13 11:13:33 +0200

    Attempt with older chrome launcher version

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -7,7 +7,7 @@ call ..\.node\npm uninstall karma-cli
 
 call ..\.node\npm install -g karma
 call ..\.node\npm install -g karma-teamcity-reporter
-call ..\.node\npm install -g karma-chrome-launcher
+call ..\.node\npm install -g karma-chrome-launcher@0.1.10
 call ..\.node\npm install -g karma-jasmine
 call ..\.node\npm install -g karma-cli
 call ..\.node\npm install -g flatten-packages
