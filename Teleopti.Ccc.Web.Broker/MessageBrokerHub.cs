d333846ac1092ba407ddcab9a4acfdf564adef36
Mathias Stenbom <mathias.stenbom@teleopti.com>
2015-05-13 15:54:12 +0200

    Backed out changeset: 696cf735fde6

--- Teleopti.Ccc.Web.Broker/MessageBrokerHub.cs
+++ Teleopti.Ccc.Web.Broker/MessageBrokerHub.cs
@@ -60,6 +60,13 @@ namespace Teleopti.Ccc.Web.Broker
 			}
 		}
 
+		public void Ping(int expectedNumberOfSentMessages, int messagesPerSecond)
+		{
+			_actionScheduler = new ActionThrottle(messagesPerSecond);
+			((ActionThrottle)_actionScheduler).Start();
+
+			Ping(expectedNumberOfSentMessages);
+		}
 	}
 
 }
