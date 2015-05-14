aaccf5e664bf5a852d1eda1f60972cbd2e0233d5
Ola <none@none>
2015-05-14 17:25:44 +0200

    need the one that starts with "Agent" first

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Default/DefaultMatrixApplicationFunctions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Default/DefaultMatrixApplicationFunctions.cs
@@ -11,6 +11,7 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Default
 	{
 		private readonly string[] names =
 		{
+			"ResReportAgentQueueMetrics",
 			"ResReportAbandonmentAndSpeedOfAnswer", 
 			"ResReportForecastvsActualWorkload", 
 			"ResReportServiceLevelAndAgentsReady", 
@@ -19,12 +20,15 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Default
 		// guids from mart.report with these names
 		private readonly string[] guids =
 		{
+			"479809D8-4DAE-4852-BF67-C98C3744918D",
 			"C232D751-AEC5-4FD7-A274-7C56B99E8DEC", 
 			"8D8544E4-6B24-4C1C-8083-CBE7522DD0E0", 
 			"AE758403-C16B-40B0-B6B2-E8F6043B6E04", 
 			"8DE1AB0F-32C2-4619-A2B2-97385BE4C49C"
 		};
 
+			
+//xxResReportScheduledOvertimePerAgent	EB977F5B-86C6-4D98-BEDF-B79DC562987B
 		private string reportGuid(string name)
 		{
 			var index = Array.IndexOf(names, name);
