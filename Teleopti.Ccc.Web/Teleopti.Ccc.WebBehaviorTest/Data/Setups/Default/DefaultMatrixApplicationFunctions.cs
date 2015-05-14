02e10e2c2fb45ed6b1c3d5b35c94e581e9d27267
Ola <none@none>
2015-05-14 12:26:02 +0200

    correct guids from mart.report

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Default/DefaultMatrixApplicationFunctions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/Default/DefaultMatrixApplicationFunctions.cs
@@ -16,14 +16,28 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.Default
 			"ResReportServiceLevelAndAgentsReady", 
 			"ResReportRequestsPerAgent"
 		};
+		// guids from mart.report with these names
+		private readonly string[] guids =
+		{
+			"C232D751-AEC5-4FD7-A274-7C56B99E8DEC", 
+			"8D8544E4-6B24-4C1C-8083-CBE7522DD0E0", 
+			"AE758403-C16B-40B0-B6B2-E8F6043B6E04", 
+			"8DE1AB0F-32C2-4619-A2B2-97385BE4C49C"
+		};
+
+		private string reportGuid(string name)
+		{
+			var index = Array.IndexOf(names, name);
+			return guids[index];
+		}
 
 		public void Apply(IUnitOfWork uow)
 		{
 			var applicationFunctionRepository = new ApplicationFunctionRepository(uow);
 			var matrixReportsParent = applicationFunctionRepository.LoadAll().First(x => x.FunctionCode == "Reports");
-			
+
 			applicationFunctionRepository.AddRange(
-				names.Select(n => new ApplicationFunction(n, matrixReportsParent) { ForeignSource = DefinedForeignSourceNames.SourceMatrix, ForeignId = Guid.NewGuid().ToString()}));
+				names.Select(n => new ApplicationFunction(n, matrixReportsParent) { ForeignSource = DefinedForeignSourceNames.SourceMatrix, ForeignId = reportGuid(n) }));
 		}
 
 		public int HashValue()
