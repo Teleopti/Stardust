5d1d084b07b473f8d2be3d86f521c7928c22b4bb
johand <johand@teleopti.com>
2015-05-20 10:26:00 +0200

    #33206 new ui for scheduling report

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/resourceplanner/Resourceplanner.report.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/resourceplanner/Resourceplanner.report.controller.js
@@ -3,21 +3,16 @@
 	angular.module('wfm.resourceplanner.report', [])
 		.controller('ResourceplannerReportCtrl', [
 			'$scope', '$stateParams', function ($scope, $stateParams) {
-				$scope.issues = $stateParams;
-				$scope.issuePreview = $scope.issues.result.BusinessRulesValidationResults;
+				$scope.issues = $stateParams.result.BusinessRulesValidationResults;
 				$scope.scheduledAgents = $stateParams.result.ScheduledAgentsCount;
-
 				$scope.gridOptions = {
-					selectionRowHeaderWidth: 35,
 					columnDefs: [
-						{ name: 'Agent', field: 'Name' },
-						{ name: 'Detail', field: 'Message' },
-						{ name: 'Issue-type', field: 'BusinessRuleCategory' }
+						{ name: 'Agent', field: 'Name', enableColumnMenu: false},
+						{ name: 'Detail', field: 'Message', enableColumnMenu: false },
+						{ name: 'Issue-type', field: 'BusinessRuleCategory', enableColumnMenu: false }
 					],
-
-					data: $scope.issues.result.BusinessRulesValidationResults
+					data: $scope.issues
 				};
-
 			}
 		]);
 })();
