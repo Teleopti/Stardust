826a472bcfe3a6d1a3764175c3d1e61f9864dc77
Erik Sundberg <erik.sundberg@teleopti.com>
2015-05-21 15:38:25 +0200

    #33453 Fix script

--- Database/TeleoptiCCC7/Releases/00000588.sql
+++ Database/TeleoptiCCC7/Releases/00000588.sql
@@ -4,6 +4,14 @@ INNER JOIN dbo.RtaStateGroup g
 ON g.Id = s.Parent
 WHERE g.IsDeleted = 1
 
+UPDATE dbo.StateGroupActivityAlarm
+SET StateGroup = NULL
+FROM dbo.StateGroupActivityAlarm m
+INNER JOIN dbo.RtaStateGroup g
+ON m.StateGroup = g.Id
+WHERE g.IsDeleted = 1
+
+
 DELETE FROM dbo.RtaStateGroup
 WHERE IsDeleted = 1
 
