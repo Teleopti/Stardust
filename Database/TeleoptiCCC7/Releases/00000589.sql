d03f2c8a49f69dae390188f733803b6cda405ac9
MingdiWu <ellen.wu@hotmail.com>
2015-05-22 15:34:43 +0800

    bug fix #33443 & #33451

---
+++ Database/TeleoptiCCC7/Releases/00000589.sql
@@ -0,0 +1,11 @@
+----------------  
+--Name: Mingdi Wu
+--Date: 2015-05-22  
+--Desc: delete Outbound and SeatPLanner in permission setting since it is not ready for release to customer
+---------------- 
+
+UPDATE [dbo].[ApplicationFunction] SET IsDeleted = 1
+WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like('0120%') AND FunctionCode='Outbound'
+
+UPDATE [dbo].[ApplicationFunction] SET IsDeleted = 1
+WHERE ForeignSource='Raptor' AND IsDeleted='False' AND ForeignId Like('0107%') AND FunctionCode='SeatPlanner'
