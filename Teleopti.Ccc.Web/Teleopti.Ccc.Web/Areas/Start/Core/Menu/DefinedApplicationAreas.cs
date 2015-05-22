f1a01bdff957c4feb8a5e7b77ad33505a4fc44ff
Asad Mirza <asadm@teleopti.com>
2015-05-22 11:02:34 +0200

    Removed the unwanted WFM menu

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Core/Menu/DefinedApplicationAreas.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Core/Menu/DefinedApplicationAreas.cs
@@ -27,14 +27,7 @@ namespace Teleopti.Ccc.Web.Areas.Start.Core.Menu
 						.Anywhere,
 				Area = "Anywhere",
 				Name = GetMenuText(DefinedRaptorApplicationFunctionPaths.Anywhere)
-			},
-			new ApplicationArea
-			{
-				ApplicationFunctionPath = DefinedRaptorApplicationFunctionPaths.OpenForecasterPage,
-				Area = "WFM",
-				Name = "WFM"
 			}
-
 		};
 
 		private static string GetMenuText(string applicationFunctionPath)
