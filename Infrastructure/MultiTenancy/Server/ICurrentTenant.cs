7e30b709011df9685e84873dc1d995377464097d
Roger Kratz <roger.kratz@teleopti.com>
2015-05-22 14:48:38 +0200

    split currenttenant types to seperate files.

--- Infrastructure/MultiTenancy/Server/ICurrentTenant.cs
+++ Infrastructure/MultiTenancy/Server/ICurrentTenant.cs
@@ -4,19 +4,4 @@
 	{
 		Tenant Current();
 	}
-
-	public class CurrentTenant : ICurrentTenant
-	{
-		private readonly ICurrentTenantUser _currentTenantUser;
-
-		public CurrentTenant(ICurrentTenantUser currentTenantUser)
-		{
-			_currentTenantUser = currentTenantUser;
-		}
-
-		public Tenant Current()
-		{
-			return _currentTenantUser.CurrentUser().Tenant;
-		}
-	}
 }
