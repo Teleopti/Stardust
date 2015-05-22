50de2c901c48498bed89c23925271e554687f285
johand <johand@teleopti.com>
2015-05-22 15:15:14 +0200

    #33206 added unittest for controller

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/resourceplanner/Resourceplanner.report.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/resourceplanner/Resourceplanner.report.controller.js
@@ -3,16 +3,18 @@
 	angular.module('wfm.resourceplanner.report', [])
 		.controller('ResourceplannerReportCtrl', [
 			'$scope', '$stateParams', function ($scope, $stateParams) {
-				$scope.issues = $stateParams.result.BusinessRulesValidationResults;
-				$scope.scheduledAgents = $stateParams.result.ScheduledAgentsCount;
-				$scope.gridOptions = {
-					columnDefs: [
-						{ name: 'Agent', field: 'Name', enableColumnMenu: false},
-						{ name: 'Detail', field: 'Message', enableColumnMenu: false },
-						{ name: 'Issue-type', field: 'BusinessRuleCategoryText', enableColumnMenu: false }
-					],
-					data: $scope.issues
-				};
+			$scope.issues = $stateParams.result.BusinessRulesValidationResults;
+			$scope.hasIssues = $scope.issues.length > 0;
+
+			$scope.scheduledAgents = $stateParams.result.ScheduledAgentsCount;
+			$scope.gridOptions = {
+				columnDefs: [
+					{ name: 'Agent', field: 'Name', enableColumnMenu: false},
+					{ name: 'Detail', field: 'Message', enableColumnMenu: false },
+					{ name: 'Issue-type', field: 'BusinessRuleCategoryText', enableColumnMenu: false }
+				],
+				data: $scope.issues
+			};
 			}
 		]);
 })();
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/resourceplanner/resourceplanner.report.controller.spec.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+describe('ResourceplannerReportCtrl', function () {
+	var $q,
+	$rootScope,
+	$httpBackend;;
+
+	beforeEach(module('wfm'));
+	beforeEach(inject(function (_$httpBackend_, _$q_, _$rootScope_) {
+		$q = _$q_;
+		$rootScope = _$rootScope_;
+		$httpBackend = _$httpBackend_;
+		$httpBackend.expectGET("../api/Global/Language?lang=en").respond(200, 'mock');
+		$httpBackend.expectGET("../api/Global/User/CurrentUser").respond(200, 'mock');
+	}));
+
+
+	it('no details are displayed when 0 issues', inject(function ($controller) {
+		var scope = $rootScope.$new();
+		var stateParams = {result:{BusinessRulesValidationResults:[]}};
+
+		$controller('ResourceplannerReportCtrl', { $scope: scope, $stateParams: stateParams });
+		expect(scope.issues.length).toEqual(0);
+		expect(scope.hasIssues).toEqual(false);
+	}));
+
+
+});
