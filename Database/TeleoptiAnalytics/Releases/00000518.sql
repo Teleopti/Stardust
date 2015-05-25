af33b3bb9606f409ae87fbacc9cf7d699f7c65d9
Erik Sundberg <erik.sundberg@teleopti.com>
2015-05-25 15:26:50 +0200

    #32733 Fix build

--- Database/TeleoptiAnalytics/Releases/00000518.sql
+++ Database/TeleoptiAnalytics/Releases/00000518.sql
@@ -4,13 +4,13 @@ CREATE TABLE [dbo].[Mailbox](
  CONSTRAINT [PK_Mailbox] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+)
+)
 
 CREATE TABLE [dbo].[Notification](
 	[Parent] [uniqueidentifier] NOT NULL,
 	[Message] [varchar](max) NOT NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+)
 
 ALTER TABLE [dbo].[Notification]  WITH CHECK ADD  CONSTRAINT [FK_Notification_Mailbox] FOREIGN KEY([Parent])
 REFERENCES [dbo].[Mailbox] ([Id])
