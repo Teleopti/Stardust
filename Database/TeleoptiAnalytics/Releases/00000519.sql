e852379a061ed1b4f368287a5492aa5b37d3e1a1
Erik Sundberg <erik.sundberg@teleopti.com>
2015-05-26 12:47:15 +0200

    #33557 Fix build

--- Database/TeleoptiAnalytics/Releases/00000519.sql
+++ Database/TeleoptiAnalytics/Releases/00000519.sql
@@ -5,3 +5,31 @@ ALTER SCHEMA msg
 
 ALTER SCHEMA msg 
     TRANSFER dbo.[Notification];
+
+
+DROP TABLE [msg].[Notification]
+DROP TABLE [msg].[Mailbox]
+
+CREATE TABLE [msg].[Mailbox](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Route] [varchar](max) NOT NULL,
+ CONSTRAINT [PK_Mailbox] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+)
+CREATE TABLE [msg].[Notification](
+	[Id] [uniqueidentifier] NOT NULL default NEWID(),
+	[Parent] [uniqueidentifier] NOT NULL,
+	[Message] [varchar](max) NOT NULL
+
+CONSTRAINT [PK_Notification] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+)
+
+ALTER TABLE [msg].[Notification]  WITH CHECK ADD  CONSTRAINT [FK_Notification_Mailbox] FOREIGN KEY([Parent])
+REFERENCES [msg].[Mailbox] ([Id])
+ALTER TABLE [msg].[Notification] CHECK CONSTRAINT [FK_Notification_Mailbox]
+
