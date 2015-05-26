bd6cc28e889df93d45b99f51e2f538f9f580e77c
Zhiping Lan <zhiping.lan@teleopti.com>
2015-05-26 15:19:28 +0800

    bug #33105 added tests by fake for MyTime/ShiftTradeRequest

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Requests/Mapping/ShiftTradeScheduleViewModelMapperByFakeTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Requests/Mapping/ShiftTradeScheduleViewModelMapperByFakeTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.AgentInfo;
@@ -36,15 +37,15 @@ namespace Teleopti.Ccc.WebTest.Core.Requests.Mapping
 			_loggedOnUser = loggedOnUser;
 		}
 
-
 		protected void SetUp()
 		{
 			var businessUnit = BusinessUnitFactory.CreateWithId("Teleopti");
 			_businessUnitRepository.Add(businessUnit);
 
 			var person1 = PersonFactory.CreatePersonWithGuid("person", "1");
-			var person2 = PersonFactory.CreatePersonWithGuid("Unpublished", "2");
-			var person3 = PersonFactory.CreatePersonWithGuid("NoShiftTrade", "3");
+			var person2 = PersonFactory.CreatePersonWithGuid("person", "2");
+			var person3 = PersonFactory.CreatePersonWithGuid("Unpublished", "3");
+			var person4 = PersonFactory.CreatePersonWithGuid("NoShiftTrade", "4");
 
 			ITeam team = TeamFactory.CreateTeamWithId("team1");
 			_teamRepository.Add(team);
@@ -52,31 +53,31 @@ namespace Teleopti.Ccc.WebTest.Core.Requests.Mapping
 			person1.AddPersonPeriod(new PersonPeriod(new DateOnly(2011, 1, 1), PersonContractFactory.CreatePersonContract(), team));
 			person2.AddPersonPeriod(new PersonPeriod(new DateOnly(2011, 1, 1), PersonContractFactory.CreatePersonContract(), team));
 			person3.AddPersonPeriod(new PersonPeriod(new DateOnly(2011, 1, 1), PersonContractFactory.CreatePersonContract(), team));
+			person4.AddPersonPeriod(new PersonPeriod(new DateOnly(2011, 1, 1), PersonContractFactory.CreatePersonContract(), team));
 
 			_personRepository.Add(person1);
 			_personRepository.Add(person2);
 			_personRepository.Add(person3);
+			_personRepository.Add(person4);
 			_personRepository.Add(_loggedOnUser.CurrentUser());
 
 
 			var person1Assignment_1 = PersonAssignmentFactory.CreatePersonAssignmentWithId(person1, new DateOnly(2015, 5, 19));
-			person1Assignment_1.AddActivity(ActivityFactory.CreateActivity("Phone"),
-				new DateTimePeriod(2015, 5, 19, 8, 2015, 5, 19, 14));
+			person1Assignment_1.AddActivity(ActivityFactory.CreateActivity("Phone"), new DateTimePeriod(2015, 5, 19, 8, 2015, 5, 19, 14));
 			var person1Assignment_2 = PersonAssignmentFactory.CreatePersonAssignmentWithId(person1, new DateOnly(2015, 5, 21));
-			person1Assignment_2.AddActivity(ActivityFactory.CreateActivity("Phone"),
-				new DateTimePeriod(2015, 5, 21, 10, 2015, 5, 21, 16));
+			person1Assignment_2.AddActivity(ActivityFactory.CreateActivity("Phone"), new DateTimePeriod(2015, 5, 21, 10, 2015, 5, 21, 16));
 
 
 			var person2Assignment_1 = PersonAssignmentFactory.CreatePersonAssignmentWithId(person2, new DateOnly(2015, 5, 19));
-			person2Assignment_1.AddActivity(ActivityFactory.CreateActivity("Phone"),
-				new DateTimePeriod(2015, 5, 19, 11, 2015, 5, 19, 20));
-
+			person2Assignment_1.AddActivity(ActivityFactory.CreateActivity("Phone"), new DateTimePeriod(2015, 5, 19, 11, 2015, 5, 19, 20));
 			var person2Assignment_2 = PersonAssignmentFactory.CreatePersonAssignmentWithId(person2, new DateOnly(2015, 5, 23));
 			person2Assignment_2.SetDayOff(new DayOffTemplate(new Description("Day Off", "DO")));
 
 			var person3Assignment = PersonAssignmentFactory.CreatePersonAssignmentWithId(person3, new DateOnly(2015, 5, 19));
-			person3Assignment.AddActivity(ActivityFactory.CreateActivity("Phone"),
-				new DateTimePeriod(2015, 5, 19, 12, 2015, 5, 19, 15));
+			person3Assignment.AddActivity(ActivityFactory.CreateActivity("Phone"), new DateTimePeriod(2015, 5, 19, 12, 2015, 5, 19, 15));
+			
+			var person4Assignment = PersonAssignmentFactory.CreatePersonAssignmentWithId(person4, new DateOnly(2015, 5, 19));
+			person4Assignment.AddActivity(ActivityFactory.CreateActivity("Phone"), new DateTimePeriod(2015, 5, 19, 12, 2015, 5, 19, 15));
 
 			_personAssignmentRepository.Add(person1Assignment_1);
 			_personAssignmentRepository.Add(person1Assignment_2);
@@ -98,11 +99,7 @@ namespace Teleopti.Ccc.WebTest.Core.Requests.Mapping
 			SetUp();
 			var data = new ShiftTradeScheduleViewModelData
 			{
-				Paging = new Paging
-				{
-					Skip = 0,
-					Take = 10,
-				},
+				Paging = new Paging { Skip = 0, Take = 10,},
 				ShiftTradeDate = new DateOnly(2015, 5, 19),
 				TeamIdList = _teamRepository.LoadAll().Select(t => t.Id.Value).ToList(),
 				SearchNameText = "",
@@ -110,7 +107,25 @@ namespace Teleopti.Ccc.WebTest.Core.Requests.Mapping
 
 			var result = _mapper.Map(data);
 
-			result.PossibleTradeSchedules.Single().Name.Should().Be("1 person");
+			result.PossibleTradeSchedules.Count().Should().Be(2);
+			result.PossibleTradeSchedules.First().Name.Should().Be("1 person");
+		}
+
+		[Test]
+		public void TeamScheduleControllerShouldReturnCorrectTimeLine()
+		{
+			SetUp();
+
+			var result = _mapper.Map(new ShiftTradeScheduleViewModelData
+			{
+				ShiftTradeDate = new DateOnly(2015, 5, 19),
+				TeamIdList = _teamRepository.LoadAll().Select(x => x.Id.Value).ToList(),
+				Paging = new Paging { Take = 20, Skip = 0 },
+				SearchNameText = ""
+			});
+
+			result.TimeLineHours.Max(t => t.EndTime).Should().Be.EqualTo(new DateTime(2015, 5, 19, 20, 15, 0));
+			result.TimeLineHours.Min(t => t.StartTime).Should().Be.EqualTo(new DateTime(2015, 5, 19, 7, 45, 0));
 		}
 	}
 }
