df59c285489dac7991340cf8867756e84d60612c
David Jonsson <david.jonsson@teleopti.com>
2015-05-27 20:27:20 +0200

    make script work at T-Mobile Croatia

--- Database/TeleoptiAnalytics/Releases/00000520.sql
+++ Database/TeleoptiAnalytics/Releases/00000520.sql
@@ -1,3 +1,5 @@
+--already addded with one customer
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_person]') AND name = N'IX_dim_person_personPeriodCode_IncludePersonId_businessUnitId')
 CREATE NONCLUSTERED INDEX [IX_dim_person_personPeriodCode_IncludePersonId_businessUnitId] ON [mart].[dim_person]
 (
 	[person_period_code] ASC
