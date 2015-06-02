3e70ff65c2a8e0be33f2ec40fddc75e0f8db5796
Roger Kratz <roger.kratz@teleopti.com>
2015-06-02 16:36:57 +0200

    #32766 Cannot upgrade from very old version directly.
    First an older upgrade needs to occur so old password will be encrypted.

--- Teleopti.Support.Security/PasswordEncryption.cs
+++ Teleopti.Support.Security/PasswordEncryption.cs
@@ -38,76 +38,7 @@ namespace Teleopti.Support.Security
                     return 0;
                 }
 
-                //Open transaction
-                SqlTransaction transaction = null;
-                try
-                {
-                    transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted);
-
-
-					using (SqlDataAdapter daPerson = new SqlDataAdapter("SELECT * FROM dbo.ApplicationAuthenticationInfo WHERE Password<>''", connection))
-                    {
-                        daPerson.SelectCommand.Transaction = transaction;
-                        using (SqlCommandBuilder builder = new SqlCommandBuilder(daPerson))
-                        {
-                            daPerson.UpdateCommand = builder.GetUpdateCommand();
-                            daPerson.DeleteCommand = builder.GetDeleteCommand();
-                            daPerson.InsertCommand = builder.GetInsertCommand();
-
-                            //Update passwords
-                            using (DataSet ds = new DataSet())
-                            {
-                                ds.Locale = System.Globalization.CultureInfo.InvariantCulture;
-                                daPerson.Fill(ds);
-
-                                var specification = new IsPasswordEncryptedSpecification();
-                                var encryption = new OneWayEncryption();
-                                foreach (DataRow row in ds.Tables[0].Rows)
-                                {
-                                    object passwordObj = row["Password"];
-                                    if (passwordObj != DBNull.Value)
-                                    {
-                                        string password = (string)passwordObj;
-                                        if (!specification.IsSatisfiedBy(password))
-                                        {
-                                            //set new password
-											row["Password"] = encryption.EncryptString(password);
-
-											//update root object
-											command = connection.CreateCommand();
-											command.CommandText = string.Format(System.Globalization.CultureInfo.InvariantCulture,
-																				"UPDATE dbo.Person SET Version=Version+1,UpdatedOn = GetUtcDate() WHERE Id='{0}'",
-																				row["Person"]);
-											command.Transaction = transaction;
-											command.ExecuteNonQuery();
-                                        }
-                                    }
-                                }
-                                daPerson.Update(ds);
-                            }
-                        }
-                        //Update databaseversion
-                        command = connection.CreateCommand();
-                        command.CommandText = string.Format(System.Globalization.CultureInfo.InvariantCulture,
-                                                            "INSERT INTO dbo.DatabaseVersion (BuildNumber,SystemVersion,AddedDate,AddedBy) VALUES ('-290','7.1.290.1',GetDate(),'{0}')",
-                                                            Environment.UserName);
-                        command.Transaction = transaction;
-                        command.ExecuteNonQuery();
-
-                        //Commit!
-                        transaction.Commit();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (transaction != null)
-                        transaction.Rollback();
-                    log.Debug("Something went wrong! Error message: " + ex.Message);
-	                return 1;
-                }
-            }
-
-	        return 0;
-        }
+							log.Error("Cannot upgrade directly to this version. Please upgrade to latest version before 8.2.427.35416 first!");
+	            return 1;
     }
 }
