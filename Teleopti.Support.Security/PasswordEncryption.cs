81f774dfd024cff6e73e3e674953e75f374960d7
Ola <none@none>
2015-06-02 17:17:50 +0200

    fix build

--- Teleopti.Support.Security/PasswordEncryption.cs
+++ Teleopti.Support.Security/PasswordEncryption.cs
@@ -1,44 +1,45 @@
 ﻿using System;
-using System.Data;
 using System.Data.SqlClient;
-using Teleopti.Ccc.Domain.Security;
 using log4net;
 
 namespace Teleopti.Support.Security
 {
 	//TODO: tenant Can (probably?) be removed when removing old schema
-    internal class PasswordEncryption : ICommandLineCommand
-    {
+	internal class PasswordEncryption : ICommandLineCommand
+	{
 		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
-        public int Execute(CommandLineArgument commandLineArgument)
-        {
-            //Select database version 
-            using (SqlConnection connection = new SqlConnection(commandLineArgument.DestinationConnectionString))
-            {
-                try
-                {
-                    connection.Open();
-                }
-                catch (SqlException ex)
-                {
-                    log.Debug("Could not open Sql Connection. Error message: " + ex.Message);
-                    return 1;
-                }
-                catch (InvalidOperationException ex)
-                {
-                    log.Debug("Could not open Sql Connection. Error message: " + ex.Message);
-                    return 1;
-                }
-                //Check version
-                var command = connection.CreateCommand();
-                command.CommandText = "SELECT COUNT(*) FROM dbo.DatabaseVersion WHERE BuildNumber=('-290')";
-                var versionCount = (int)command.ExecuteScalar();
-                if (versionCount > 0)
-                {
-                    return 0;
-                }
 
-							log.Error("Cannot upgrade directly to this version. Please upgrade to latest version before 8.2.427.35416 first!");
-	            return 1;
-    }
+		public int Execute(CommandLineArgument commandLineArgument)
+		{
+			//Select database version 
+			using (SqlConnection connection = new SqlConnection(commandLineArgument.DestinationConnectionString))
+			{
+				try
+				{
+					connection.Open();
+				}
+				catch (SqlException ex)
+				{
+					log.Debug("Could not open Sql Connection. Error message: " + ex.Message);
+					return 1;
+				}
+				catch (InvalidOperationException ex)
+				{
+					log.Debug("Could not open Sql Connection. Error message: " + ex.Message);
+					return 1;
+				}
+				//Check version
+				var command = connection.CreateCommand();
+				command.CommandText = "SELECT COUNT(*) FROM dbo.DatabaseVersion WHERE BuildNumber=('-290')";
+				var versionCount = (int)command.ExecuteScalar();
+				if (versionCount > 0)
+				{
+					return 0;
+				}
+
+				log.Error("Cannot upgrade directly to this version. Please upgrade to latest version before 8.2.427.35416 first!");
+				return 1;
+			}
+		}
+	}
 }
