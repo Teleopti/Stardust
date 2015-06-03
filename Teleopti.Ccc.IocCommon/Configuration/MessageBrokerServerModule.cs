fd88c799d35e7e5dacf13fb9f254a2fca30f883c
Mathias Stenbom <mathias.stenbom@teleopti.com>
2015-06-03 12:44:45 +0200

    #32733 hopefully temp fix this build

--- Teleopti.Ccc.IocCommon/Configuration/MessageBrokerServerModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/MessageBrokerServerModule.cs
@@ -1,6 +1,10 @@
-﻿using Autofac;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
 using Teleopti.Ccc.Domain.MessageBroker;
 using Teleopti.Ccc.Infrastructure.MessageBroker;
+using Teleopti.Interfaces.MessageBroker;
 
 namespace Teleopti.Ccc.IocCommon.Configuration
 {
@@ -10,7 +14,25 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 		{
 			builder.RegisterType<SubscriptionFiller>().As<IBeforeSubscribe>().SingleInstance();
 			builder.RegisterType<MessageBrokerServer>().As<IMessageBrokerServer>().SingleInstance();
-			builder.RegisterType<MailboxRepository>().As<IMailboxRepository>().SingleInstance();
+			builder.RegisterType<EmtpyMailboxRepository>().As<IMailboxRepository>().SingleInstance();
+			//builder.RegisterType<MailboxRepository>().As<IMailboxRepository>().SingleInstance();
+		}
+	}
+
+	public class EmtpyMailboxRepository : IMailboxRepository
+	{
+		public void Persist(Mailbox mailbox)
+		{
+		}
+
+		public Mailbox Load(Guid id)
+		{
+			return new Mailbox();
+		}
+
+		public IEnumerable<Mailbox> Load(string[] routes)
+		{
+			return Enumerable.Empty<Mailbox>();
 		}
 	}
 }
