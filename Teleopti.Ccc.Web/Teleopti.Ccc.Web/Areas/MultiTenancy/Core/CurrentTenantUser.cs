16d44fcfd962cd045e18d23d3042cc0e7b39f4ad
Roger Kratz <roger.kratz@teleopti.com>
2015-06-09 14:16:55 +0200

    #33645 Implementing ICurrentUser for web client

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MultiTenancy/Core/CurrentTenantUser.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MultiTenancy/Core/CurrentTenantUser.cs
@@ -1,20 +1,33 @@
 ﻿using Teleopti.Ccc.Infrastructure.MultiTenancy.Server;
 using Teleopti.Ccc.Infrastructure.Web;
+using Teleopti.Ccc.Web.Core.RequestContext.Cookie;
 
 namespace Teleopti.Ccc.Web.Areas.MultiTenancy.Core
 {
+	//would like two different impl here but hard now when web + tenant is same process...
 	public class CurrentTenantUser : ICurrentTenantUser
 	{
 		private readonly ICurrentHttpContext _currentHttpContext;
+		private readonly ISessionSpecificDataProvider _sessionDataProvider;
+		private readonly IFindPersonInfoByCredentials _findPersonInfoByCredentials;
 
-		public CurrentTenantUser(ICurrentHttpContext currentHttpContext)
+		public CurrentTenantUser(ICurrentHttpContext currentHttpContext, 
+														ISessionSpecificDataProvider sessionDataProvider, 
+														IFindPersonInfoByCredentials findPersonInfoByCredentials)
 		{
 			_currentHttpContext = currentHttpContext;
+			_sessionDataProvider = sessionDataProvider;
+			_findPersonInfoByCredentials = findPersonInfoByCredentials;
 		}
 
 		public PersonInfo CurrentUser()
 		{
-			return _currentHttpContext.Current().Items[TenantAuthentication.PersonInfoKey] as PersonInfo;
+			var personInfoSetFromExternalCall = _currentHttpContext.Current().Items[TenantAuthentication.PersonInfoKey] as PersonInfo;
+			if (personInfoSetFromExternalCall != null)
+				return personInfoSetFromExternalCall;
+
+			var sessionData = _sessionDataProvider.GrabFromCookie();
+			return sessionData == null ? null : _findPersonInfoByCredentials.Find(sessionData.PersonId, sessionData.TenantPassword);
 		}
 	}
 }
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MultiTenancy/Core/CurrentTenantUserTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MultiTenancy/Core/CurrentTenantUserTest.cs
@@ -1,29 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
 using SharpTestsEx;
 using Teleopti.Ccc.Infrastructure.MultiTenancy.Server;
+using Teleopti.Ccc.TestCommon.TestData;
 using Teleopti.Ccc.TestCommon.Web;
 using Teleopti.Ccc.Web.Areas.MultiTenancy.Core;
+using Teleopti.Ccc.Web.Core.RequestContext.Cookie;
 
 namespace Teleopti.Ccc.WebTest.Areas.MultiTenancy.Core
 {
 	public class CurrentTenantUserTest
 	{
 		[Test]
-		public void ShouldReturnNullIfNotExists()
+		public void ShouldReturnNullIfNotExist()
 		{
-			new CurrentTenantUser(new FakeCurrentHttpContext(new FakeHttpContext())).CurrentUser()
+			var sessionDataProvider = MockRepository.GenerateStub<ISessionSpecificDataProvider>();
+			sessionDataProvider.Expect(x => x.GrabFromCookie()).Return(null);
+			new CurrentTenantUser(new FakeCurrentHttpContext(new FakeHttpContext()), sessionDataProvider, null).CurrentUser()
 				.Should().Be.Null();
 		}
 
 		[Test]
-		public void ShouldGetPersonInfo()
+		public void ShouldGetPersonInfo_ExternalCalls()
 		{
 			var expected = new PersonInfo();
 			var httpContext = new FakeHttpContext();
 			httpContext.Items[TenantAuthentication.PersonInfoKey] = expected;
 
-			new CurrentTenantUser(new FakeCurrentHttpContext(httpContext)).CurrentUser()
+			new CurrentTenantUser(new FakeCurrentHttpContext(httpContext), null, null).CurrentUser()
 					.Should().Be.SameInstanceAs(expected);
+		}
+
+		[Test]
+		public void ShouldGetPersonInfo_InternalWebCalls()
+		{
+			var expected = new PersonInfo();
+			var sessionData = new SessionSpecificData(Guid.NewGuid(), RandomName.Make(), Guid.NewGuid(), RandomName.Make());
+			var sessionDataProvider = MockRepository.GenerateStub<ISessionSpecificDataProvider>();
+			sessionDataProvider.Expect(x => x.GrabFromCookie()).Return(sessionData);
+			var findPersonInfoByCredentials = MockRepository.GenerateMock<IFindPersonInfoByCredentials>();
+			findPersonInfoByCredentials.Expect(x => x.Find(sessionData.PersonId, sessionData.TenantPassword)).Return(expected);
+			var target = new CurrentTenantUser(new FakeCurrentHttpContext(new FakeHttpContext()), sessionDataProvider, findPersonInfoByCredentials);
+			target.CurrentUser()
+				.Should().Be.SameInstanceAs(expected);
 		}
 	}
 }
