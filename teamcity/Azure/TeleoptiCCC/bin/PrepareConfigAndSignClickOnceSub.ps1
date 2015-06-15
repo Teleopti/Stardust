bfdb1a21f2fc1ac74c4bf5e5e9d2f4f66154a0e3
David Jonsson <david.jonsson@teleopti.com>
2015-06-15 17:23:35 +0200

    #33740 - remove backplane for Azure as it does not work and cost unwanted network traffic

--- teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnceSub.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnceSub.ps1
@@ -288,7 +288,7 @@ Try
     Add-Content "$fullPathsettingsFile" "`$(RTA_STATE_CODE)|ACW,ADMIN,EMAIL,IDLE,InCall,LOGGED ON,OFF,Ready,WEB"
     Add-Content "$fullPathsettingsFile" "`$(RTA_QUEUE_ID)|2001,2002,0063,2000,0019,0068,0085,0202,0238,2003"
     Add-Content "$fullPathsettingsFile" "`$(WEB_BROKER_FOR_WEB)|https://$DataSourceName.teleopticloud.com/Web"
-    Add-Content "$fullPathsettingsFile" "`$(WEB_BROKER_BACKPLANE)|https://$DataSourceName.teleopticloud.com/Broker.backplane/backplane"
+    Add-Content "$fullPathsettingsFile" "`$(WEB_BROKER_BACKPLANE)|"
 	Add-Content "$fullPathsettingsFile" "`$(WEB_BROKER)|https://$DataSourceName.teleopticloud.com/Web/"
     Add-Content "$fullPathsettingsFile" "`$(PM_ASMX)|NotImplemented"
     Add-Content "$fullPathsettingsFile" "`$(PM_SERVICE|NotImplemented"
