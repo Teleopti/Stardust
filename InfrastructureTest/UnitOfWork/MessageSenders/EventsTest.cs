ae87d0ace955175c7d0ad7ffe023edb1a675345c
Mathias Stenbom <mathias.stenbom@teleopti.com>
2015-06-16 14:11:08 +0200

    #32733 test refact

--- InfrastructureTest/UnitOfWork/MessageSenders/EventsTest.cs
+++ InfrastructureTest/UnitOfWork/MessageSenders/EventsTest.cs
@@ -1,80 +1,49 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
-using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
-using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
-using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork.MessageSenders
 {
 	[TestFixture]
 	[PrincipalAndStateTest]
-	public class EventsTest : ISetup
+	public class EventsTest
 	{
-		public void Setup(ISystem system, IIocConfiguration configuration)
-		{
-			system.AddService<TheServiceImpl>();
-		}
-
-		public TheServiceImpl TheService;
+		public ICurrentUnitOfWorkFactory Uow;
 		public FakeEventPublisher EventsPublisher;
-
-		public class TheServiceImpl
-		{
-			public readonly IScenarioRepository ScenarioRepository;
-			public readonly IPersonRepository PersonRepository;
-			public readonly IPersonAssignmentRepository PersonAssignmentRepository;
-			public readonly IActivityRepository ActivityRepository;
-
-			public TheServiceImpl(
-				IScenarioRepository scenarioRepository, 
-				IPersonRepository personRepository, 
-				IPersonAssignmentRepository personAssignmentRepository,
-				IActivityRepository activityRepository
-				)
-			{
-				ScenarioRepository = scenarioRepository;
-				PersonRepository = personRepository;
-				PersonAssignmentRepository = personAssignmentRepository;
-				ActivityRepository = activityRepository;
-			}
-
-			[UnitOfWork]
-			public virtual void Does(Action<TheServiceImpl> action)
-			{
-				action(this);
-			}
-
-		}
+		public IScenarioRepository ScenarioRepository;
+		public IPersonRepository PersonRepository;
+		public IPersonAssignmentRepository PersonAssignmentRepository;
+		public IActivityRepository ActivityRepository;
 
 		[Test]
 		public void ShouldPublishEvents()
 		{
-			TheService.Does(service =>
+			using (var uow = Uow.LoggedOnUnitOfWorkFactory().CreateAndOpenUnitOfWork())
 			{
 				var scenario = ScenarioFactory.CreateScenario(".", true, false);
-				service.ScenarioRepository.Add(scenario);
+				ScenarioRepository.Add(scenario);
 
 				var person = PersonFactory.CreatePerson();
-				service.PersonRepository.Add(person);
+				PersonRepository.Add(person);
 
 				var activity = new Activity(".");
-				service.ActivityRepository.Add(activity);
+				ActivityRepository.Add(activity);
 
 				var personAssignment = new PersonAssignment(person, scenario, new DateOnly(2015, 6, 15));
 				personAssignment.AddActivity(activity, new DateTimePeriod(2015, 6, 15, 8, 2015, 6, 15, 17));
 
-				service.PersonAssignmentRepository.Add(personAssignment);
+				PersonAssignmentRepository.Add(personAssignment);
 
-			});
+				uow.PersistAll();
+			}
 
 			EventsPublisher.PublishedEvents.OfType<ActivityAddedEvent>()
 				.Should().Have.Count.GreaterThan(0);
