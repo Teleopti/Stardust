492ef718ff7571f5627367288e7d1ed17070361f
Asad Mirza <asadm@teleopti.com>
2015-06-17 13:19:46 +0200

    PBI #33033: Removed unwanted code

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/permissions/permissions.controller.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/permissions/permissions.controller.spec.js
@@ -77,10 +77,7 @@ describe('PermissionsCtrl', function () {
 		$httpBackend.expectGET("../api/Global/Language?lang=en").respond(200, 'mock');
 		$httpBackend.expectGET("../api/Global/User/CurrentUser").respond(200, 'mock');
 	}));
-
 	
-
-
 
 	it('creates a role in the list', inject(function($controller) {
 		var scope = $rootScope.$new();
@@ -89,7 +86,7 @@ describe('PermissionsCtrl', function () {
 
 		scope.roleName = 'name';
 		scope.createRole();
-		scope.$digest(); // this is needed to resolve the promise
+		scope.$digest(); 
 		
 		expect(scope.roles.length).toEqual(2); 
 	}));
@@ -100,115 +97,8 @@ describe('PermissionsCtrl', function () {
 		rolesList = [];
 		$controller('PermissionsCtrl', { $scope: scope, PermissionsService: mockPermissionsService });
 
-		scope.$digest(); // this is needed to resolve the promise
+		scope.$digest(); 
 
 		expect(scope.roles.length).toEqual(0);
 	}));
-/*
-	it('selects a function for a role', inject(function($controller) {
-		var functionNode = {
-			$modelValue: { selected: false },
-			$parentNodeScope: {
-				$modelValue: { hasSelectedChildren: 0 }
-			}
-	};
-		var scope = $rootScope.$new();
-		$controller('PermissionsCtrl', { $scope: scope, Permissions: mockPermissionsService });
-
-		scope.toggleFunctionForRole(functionNode);
-		scope.$digest();
-
-		expect(functionNode.$modelValue.selected).toBe(true);
-	}));
-
-	it('updates functions list when selected functions for a role is updated from the service ', inject(function ($controller) {
-
-		var scope = $rootScope.$new();
-		$controller('PermissionsCtrl', { $scope: scope, Permissions: mockPermissionsService });
-
-		scope.showRole("roleId");
-		scope.$digest();
-
-		var functionSelected;
-		scope.functionsDisplayed.forEach(function(item) {
-			if (item.selected) { //FIXME
-				functionSelected = item;
-			}
-		});
-		expect(functionSelected).not.toBe(null);
-		expect(functionSelected.FunctionId).toBe(1);
-	}));
-
-	it('checks if a function has children selected', inject(function ($controller) {
-		var parentFunction = {};
-		var scope = $rootScope.$new();
-
-		var mockPermissionsService2 = {
-			roles: {
-				post: function () {
-					var queryDeferred = $q.defer();
-					queryDeferred.resolve({ Id: 1, DescriptionText: 'text' });
-					return { $promise: queryDeferred.promise };
-				},
-				get: function () {
-					return [];
-				}
-			},
-			applicationFunctions: {
-				query: function () {
-					var queryDeferred = $q.defer();
-					queryDeferred.resolve([
-					{
-						FunctionDescription: "desc",
-						LocalizedFunctionDescription: "",
-						FunctionCode: "",
-						IsDisabled: false,
-						FunctionId: 1,
-						ChildFunctions: [
-							{
-								FunctionDescription: "desc",
-								LocalizedFunctionDescription: "",
-								FunctionCode: "",
-								IsDisabled: false,
-								FunctionId: 2,
-								ChildFunctions: []
-							}
-						]
-					}]);
-					return { $promise: queryDeferred.promise };
-				}
-			},
-			organizationSelections: {
-				query: function () {
-					var queryDeferred = $q.defer();
-					return { $promise: queryDeferred.promise };
-				}
-			},
-			rolesPermissions: {
-				query: function () {
-					var result = {
-						AvailableFunctions: [
-						{
-							Id: "2", FunctionCode: "", FunctionPath: "", LocalizedFunctionDescription: ""
-						}],
-						AvailableBusinessUnits: [],
-						AvailableSites: [],
-						AvailableTeams: []
-					};
-					var queryDeferred = $q.defer();
-					queryDeferred.resolve(result);
-					return { $promise: queryDeferred.promise };
-				}
-			}
-		};
-
-
-		$controller('PermissionsCtrl', { $scope: scope, Permissions: mockPermissionsService2 });
-		scope.showRole("roleId");
-
-		scope.$digest();
-		
-		expect(scope.functionsDisplayed[0].nmbSelectedChildren).toEqual(1);
-	}));
-*/
 });
