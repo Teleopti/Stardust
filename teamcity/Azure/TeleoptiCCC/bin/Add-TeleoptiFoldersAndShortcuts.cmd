5c6ab37278832889124a26df0db0d93ef95d7f36
David Jonsson <davidj@teleopti.com>
2015-06-17 15:17:50 -0600

    #33780 - try push without log4net

--- teamcity/Azure/TeleoptiCCC/bin/Add-TeleoptiFoldersAndShortcuts.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/Add-TeleoptiFoldersAndShortcuts.cmd
@@ -5,6 +5,13 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 
+::Try copy Log4Net .dll
+set srcFile=E:\approot\Services\ETL\Tool\log4net.dll
+set targetFile=%DIRECTORY%\log4Net\log4net.dll
+COPY "%srcFile%" "%targetFile%" /Y
+
 ::execute
+if exist "%targetFile%" (
 powershell . .\PrepareConfigAndSignClickOnce.ps1 -ScriptPath "%DIRECTORY%\Add-TeleoptiFoldersAndShortcuts.ps1" >nul
+)
 exit %ERRORLEVEL%
