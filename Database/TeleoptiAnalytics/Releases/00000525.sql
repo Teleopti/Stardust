e111f37bef6d7f09cef569da8ff0995646de272f
David Jonsson <david.jonsson@teleopti.com>
2015-06-24 13:08:26 +0200

    bug #33855 - thx Roger!

--- Database/TeleoptiAnalytics/Releases/00000525.sql
+++ Database/TeleoptiAnalytics/Releases/00000525.sql
@@ -8,7 +8,6 @@ INCLUDE ([acd_login_id])
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[mart].[dim_person]') AND name = N'IX_dim_person_SkillSet')
-DROP INDEX [IX_dim_person_SkillSet] ON [mart].[dim_person]
 CREATE NONCLUSTERED INDEX [IX_dim_person_SkillSet]
 ON [mart].[dim_person] ([skillset_id],[valid_from_date],[valid_to_date])
 INCLUDE ([person_id])
