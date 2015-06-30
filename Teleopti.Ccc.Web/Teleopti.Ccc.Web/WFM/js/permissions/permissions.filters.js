e392b3754ce5a79f25c240ef6fa72576460fc750
Felix <none@none>
2015-06-30 14:32:22 +0200

    BUG #33004 Improve functionality for tree selection.
    BUG #33005
    Change toggle switches to radio buttons. Add better labeling.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/permissions/permissions.filters.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/permissions/permissions.filters.js
@@ -3,37 +3,75 @@
 	var permissionsFilters = angular.module('wfm.permissions');
 	permissionsFilters.filter('selectedFunctions', [
 		function() {
-			return function(nodes, selectedFunctionToggle) {
-				if (!selectedFunctionToggle) return nodes;
+		    return function (nodes, selectedFunctionToggle) {
+		        if (!selectedFunctionToggle) return nodes;
 
-				var filteredNodes = [];
-				nodes.forEach(function(node) {
-					if (node.selected || node.nmbSelectedChildren > 0) {
-						filteredNodes.push(node);
-					}
-				});
-				return filteredNodes;
-			}
+		        var filteredNodes = [];
+		        nodes.forEach(function (node) {
+		            if (node.selected || node.nmbSelectedChildren > 0) {
+		                filteredNodes.push(node);
+		            }
+		        });
+		        return filteredNodes;
+		    }
 		}
 	]);
 
 	permissionsFilters.filter('unselectedFunctions', [
 		function() {
-			return function(nodes, unselectedFunctionToggle) {
-				if (!unselectedFunctionToggle) return nodes;
-				var filteredNodes = [];
-				nodes.forEach(function (node) {
-					var hasUnselectedChildren = false;
-					if (node.ChildFunctions && node.ChildFunctions.length > 0
+		    return function (nodes, unselectedFunctionToggle) {
+		        if (!unselectedFunctionToggle) return nodes;
+		        var filteredNodes = [];
+		        nodes.forEach(function (node) {
+		            var hasUnselectedChildren = false;
+		            if (node.ChildFunctions && node.ChildFunctions.length > 0
 						&& node.nmbSelectedChildren < node.ChildFunctions.length)
-						hasUnselectedChildren = true;
+		                hasUnselectedChildren = true;
 
-					if (!node.selected || hasUnselectedChildren) {
-						filteredNodes.push(node);
-					}
-				});
-				return filteredNodes;
-			}
+		            if (!node.selected || hasUnselectedChildren) {
+		                filteredNodes.push(node);
+		            }
+		        });
+		        return filteredNodes;
+		    }
+		}
+	]);
+
+	permissionsFilters.filter('selectedData', [
+    function () {
+        return function (nodes, selectedDataToggle) {
+            if (!selectedDataToggle) return nodes;
+
+            var filteredNodes = [];
+            nodes.forEach(function (node) {
+                if (node.selected || node.nmbSelectedChildren > 0) {
+                    filteredNodes.push(node);
+                }
+            });
+            return filteredNodes;
+        }
+    }
+	]);
+
+	permissionsFilters.filter('unselectedData', [
+		function () {
+		    return function (nodes, unselectedDataToggle) {
+		        if (!unselectedDataToggle) return nodes;
+		        var filteredNodes = [];
+		        nodes.forEach(function (node) {
+		            var hasUnselectedChildren = false;
+		            if (node.ChildNodes && node.ChildNodes.length > 0
+						&& node.nmbSelectedChildren < node.ChildNodes.length)
+		                hasUnselectedChildren = true;
+
+		            if (node.nmbSelectedChildren > 0) { node.selected }
+
+		            if (!node.selected || hasUnselectedChildren) {
+		                filteredNodes.push(node);
+		            }
+		        });
+		        return filteredNodes;
+		    }
 		}
 	]);
 })();
