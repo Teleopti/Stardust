a8b9703a32d1401f10c6616c145559a18b899ece
David Jonsson <davidj@teleopti.com>
2015-07-01 14:00:22 +0200

    bug #33951 - add Auto as part of new sc.exe command

--- teamcity/Azure/TeleoptiCCC/bin/InstallService.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/InstallService.cmd
@@ -28,6 +28,8 @@ IF NOT ERRORLEVEL 1060 (
 
 )
 sc create %ETLService% binPath= "E:\approot\Services\ETL\Service\Teleopti.Analytics.Etl.ServiceHost.exe" DisplayName= "%ETLService%" >> Install.log
+sc config %ETLService% start=Auto
+
 ECHO Service status after install [%ETLService%] >> Install.log
 SC QUERY %ETLService%  >> Install.log
 ECHO --------------------- >> Install.log
@@ -41,6 +43,7 @@ IF NOT ERRORLEVEL 1060 (
 	IF NOT ERRORLEVEL 1060 CALL :ServiceError
 )
 sc create %ServiceBus% binPath= "E:\approot\Services\ServiceBus\Teleopti.CCC.Sdk.ServiceBus.Host.exe" DisplayName= "%ServiceBus%" >> Install.log
+sc config %ServiceBus% start=Auto
 ECHO Service status after install [%ServiceBus%] >> Install.log
 SC QUERY %ServiceBus%  >> Install.log
 
