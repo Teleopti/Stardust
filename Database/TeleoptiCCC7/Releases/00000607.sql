4a4837356c44cbde3527093b3f38ae19a858280d
MingdiWu <mingdi.wu@teleopti.com>
2015-07-01 14:38:36 +0800

    bug #33857 clean redoundant records before make constraint unique

--- Database/TeleoptiCCC7/Releases/00000607.sql
+++ Database/TeleoptiCCC7/Releases/00000607.sql
@@ -9,6 +9,19 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+
+DELETE dbo.ShiftTradeSwapDetail 
+FROM dbo.ShiftTradeSwapDetail
+LEFT OUTER JOIN (
+   SELECT MIN(id) as RowId, Parent, PersonFrom, PersonTo, DateFrom, DateTo
+   FROM dbo.ShiftTradeSwapDetail 
+   GROUP BY Parent, PersonFrom, PersonTo, DateFrom, DateTo
+) as KeepRows ON
+   dbo.ShiftTradeSwapDetail.Id = KeepRows.RowId
+WHERE
+   KeepRows.RowId IS NULL
+
+
 ALTER TABLE dbo.ShiftTradeSwapDetail
 ADD CONSTRAINT [PK_ShiftTradeSwapDetails] UNIQUE (Parent, PersonFrom, PersonTo, DateFrom, DateTo)
 GO
