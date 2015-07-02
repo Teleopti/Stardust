51460f4bb1e3c4fd94ed00b96784d6aa8daf3b31
MingdiWu <mingdi.wu@teleopti.com>
2015-07-02 10:04:26 +0800

    bug #33857 clean duplicate records before constraint unique

--- Database/TeleoptiCCC7/Releases/00000607.sql
+++ Database/TeleoptiCCC7/Releases/00000607.sql
@@ -9,6 +9,18 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+
+DELETE dbo.ShiftTradeSwapDetail 
+FROM dbo.ShiftTradeSwapDetail
+LEFT OUTER JOIN (
+   SELECT CONVERT(uniqueidentifier, MIN(CONVERT(char(36), id))) as RowId, Parent, PersonFrom, PersonTo, DateFrom, DateTo
+   FROM dbo.ShiftTradeSwapDetail 
+   GROUP BY Parent, PersonFrom, PersonTo, DateFrom, DateTo
+) as KeepRows ON
+   dbo.ShiftTradeSwapDetail.Id = KeepRows.RowId
+WHERE
+   KeepRows.RowId IS NULL
+
 ALTER TABLE dbo.ShiftTradeSwapDetail
 ADD CONSTRAINT [PK_ShiftTradeSwapDetails] UNIQUE (Parent, PersonFrom, PersonTo, DateFrom, DateTo)
 GO
