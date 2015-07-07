09b89c4d9f48c44736303d8ec137aced0056f3ef
Kunning Mao <none@none>
2015-07-07 15:40:57 +0200

    #33938 missing shortcut - "Restart" in Azure doesn't work

--- teamcity/Azure/TeleoptiCCC/bin/RestartTeleopti.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/RestartTeleopti.cmd
@@ -2,5 +2,5 @@ SET FILE=%~n0
 SET DIRECTORY=%~dp0
 SET DIRECTORY=%DIRECTORY:~0,-1%
 powershell set-executionpolicy unrestricted
-powershell . .\RestartTeleopti.ps1 -ScriptPath "%DIRECTORY%\..\Tools\SupportTools\StartStopSystem\RestartTeleopti.ps1" >> RestartTeleopti.log
+powershell . "%DIRECTORY%\RestartTeleopti.ps1" -ScriptPath "%DIRECTORY%\..\Tools\SupportTools\StartStopSystem\RestartTeleopti.ps1" >> RestartTeleopti.log
  
