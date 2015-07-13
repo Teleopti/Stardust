1d3c24b1a8001ab8929bf91d9da8c239d098e74f
Li Xue <li.xue@teleopti.com>
2015-07-13 08:47:36 +0800

    #33700 campaign list presentation

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.controller.js
@@ -23,6 +23,17 @@
 		$scope.gotoCreateCampaign = function () {
 			$state.go('outbound-create');
 		};
+		$scope.generateChart = function() {
+			var chart = c3.generate({
+				bindto: '#chart',
+				data: {
+					columns: [
+					  ['data1', 30, 200, 100, 300, 150, 250],
+					  ['data2', 50, 20, 10, 40, 15, 25]
+					]
+				}
+			});
+		};
 
 		$scope.show = function (campaign) {
 			if (angular.isDefined(campaign)) $scope.currentCampaignId = campaign.Id;
@@ -120,11 +131,11 @@
 	function createCtrl($scope, $state, outboundService, outboundNotificationService) {
 		
 		init();
-
 		$scope.addCampaign = addCampaign;
 		$scope.init = init;		
 		$scope.backToList = backToList;
 		
+
 		function addCampaign() {
 			if (!$scope.isInputValid()) {
 				$scope.flashErrorIcons();
