e42496c48352a8a8d62ae0b4c0755c31f830a6d8
Hongzhao Gan <none@none>
2015-07-20 16:06:48 +0800

    move advance search to directive

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/people/people.advanceSearch.directive .js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+(function () {
+	var directive = function () {
+		return {
+			templateUrl: "js/people/template/advancedPeopleSearch.html"
+		};
+	};
+	angular.module('wfm.people').directive('advanceSearch', directive);
+}());
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/people/people.importPeople.directive.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+(function () {
+	var directive = function () {
+		return {
+			templateUrl: "js/people/template/importPeople.html"
+		};
+	};
+	angular.module('wfm.people').directive('importPeople', directive);
+}());
