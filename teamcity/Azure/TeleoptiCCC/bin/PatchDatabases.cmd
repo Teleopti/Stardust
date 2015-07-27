7f811ebdbf6cc3b71fee3b583ef25c2055d364f0
Kunning Mao <none@none>
2015-07-27 12:05:24 +0200

    add logging for PatchDatabases.ps1

--- teamcity/Azure/TeleoptiCCC/bin/PatchDatabases.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/PatchDatabases.cmd
@@ -5,6 +5,5 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 ::execute
-ECHO powershell . .\PatchDatabases.ps1 -ScriptPath "%DIRECTORY%\PatchDatabasesSub.ps1" > PatchDatabases.log
-powershell . .\PatchDatabases.ps1 -ScriptPath "%DIRECTORY%\PatchDatabasesSub.ps1" >> PatchDatabases.log
+powershell . .\PatchDatabases.ps1  >nul
 exit %ERRORLEVEL%
