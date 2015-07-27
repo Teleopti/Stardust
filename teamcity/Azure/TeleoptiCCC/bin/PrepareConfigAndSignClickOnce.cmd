19000bb1ea4c4d3af490de869c99c3bd8389175a
Kunning Mao <none@none>
2015-07-27 12:32:03 +0200

    add logging for PrepareConfigAndSignClickOnce.ps1

--- teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.cmd
@@ -5,6 +5,5 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 ::execute
-ECHO powershell . .\PrepareConfigAndSignClickOnce.ps1 -ScriptPath "%DIRECTORY%\PrepareConfigAndSignClickOnceSub.ps1" > PrepareConfigAndSignClickOnce.log
-powershell . .\PrepareConfigAndSignClickOnce.ps1 -ScriptPath "%DIRECTORY%\PrepareConfigAndSignClickOnceSub.ps1" >> PrepareConfigAndSignClickOnce.log
+powershell . .\PrepareConfigAndSignClickOnce.ps1 >nul
 exit %ERRORLEVEL%
