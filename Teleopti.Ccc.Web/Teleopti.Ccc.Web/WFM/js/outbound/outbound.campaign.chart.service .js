4cdeb5a9243aa85ff9e0591ec7e8b344e97e8af9
yanyiw <none@none>
2015-07-29 09:37:09 +0800

    PBI #33701 Adjusted tooltip style.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.campaign.chart.service .js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.campaign.chart.service .js
@@ -63,14 +63,14 @@
 
 			return {
 				dates: ['x'].concat(dates),
-				rawBacklogs: ['backlog'].concat(rawBacklogs),
-				calculatedBacklogs: ['backlog (calculated)'].concat(calculatedBacklogs),
-				plans: ['planned'].concat(plans),
-				unscheduledPlans: ['planned'].concat(unscheduledPlans),
-				schedules: ['scheduled'].concat(schedules),
-				underDiffs: ['underscheduled'].concat(underDiffs),
-				overDiffs: ['overscheduled'].concat(overDiffs),
-				statusLine: ['statusLine'].concat(rawBacklogs)
+				rawBacklogs: ['Backlog'].concat(rawBacklogs),
+				calculatedBacklogs: ['Backlog '].concat(calculatedBacklogs),
+				plans: ['Planned'].concat(plans),
+				unscheduledPlans: ['Planned'].concat(unscheduledPlans),
+				schedules: ['Scheduled'].concat(schedules),
+				underDiffs: ['Underscheduled'].concat(underDiffs),
+				overDiffs: ['Overscheduled'].concat(overDiffs),
+				statusLine: ['Progress Line'].concat(rawBacklogs)
 			};
 		}
 
@@ -110,22 +110,22 @@
 		function makeGraph(graph, graphId, viewScheduleDiffToggle, graphData, plannedPhase, warningInfo) {
 			var currentLabelGroups = selectDataGroups(viewScheduleDiffToggle, plannedPhase).map(function (name) { return graphData[name][0]; });
 			var previousLabelGroups = selectDataGroups(!viewScheduleDiffToggle, plannedPhase).map(function (name) { return graphData[name][0]; });
+			
+			var dataColor = getDataColor();
+			warningInfo.forEach(function (e) {
+			    if (e.TypeOfRule == 'OutboundUnderSLARule') {			       
+			        dataColor.statusLine = '#9467BD';
+			    }
+			    if (e.TypeOfRule == 'OutboundOverstaffRule') {			       
+			        dataColor.statusLine = '#f44336';
+			    }
+			});
 
 			var colorMap = {};
-			var dataColor = getDataColor();
 			for (var name in dataColor) {
 				colorMap[graphData[name][0]] = dataColor[name];
 			}
-			warningInfo.forEach(function (e) {
-				if (e.TypeOfRule == 'OutboundUnderSLARule') {
-					colorMap.statusLine = '#9467BD';
-				}
-				if (e.TypeOfRule == 'OutboundOverstaffRule') {
-					colorMap.statusLine = '#f44336';
-				}
-
-			});
-
+		  
 			if (graph) {
 				graph.load({
 					columns: selectDataGroups(viewScheduleDiffToggle, plannedPhase).map(function (name) { return graphData[name]; }),
@@ -139,7 +139,7 @@
 						columns: [graphData.dates].concat(selectDataGroups(viewScheduleDiffToggle, plannedPhase).map(function (name) { return graphData[name]; })),
 						type: 'bar',
 						types: {
-							statusLine: 'line'
+						    'Progress Line': 'line'
 						},
 						groups: [
 							currentLabelGroups,
@@ -163,6 +163,35 @@
 								text: 'Person hours (h)'								
 							}
 						}
+					},
+					tooltip: {
+					    contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+					        var $$ = this, config = $$.config,
+                                titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                                nameFormat = config.tooltip_format_name || function (name) { return name; },
+                                valueFormat = config.tooltip_format_value || defaultValueFormat,
+                                text, i, title, value, name, bgcolor;
+					        for (i = d.length - 1 ; i >= 0; i--) {
+					            if (!(d[i] && (d[i].value))) { continue; }
+
+					            if (!text) {
+					                title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+					                text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+					            }
+
+					            name = nameFormat(d[i].name);
+					            value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+					            bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+
+					            text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+					            text += "<td class='name' style='text-align: left' ><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+					            text += "<td class='value'>" + value + "</td>";
+					            text += "</tr>";
+					        }
+					        if (text)
+					            return text + "</table>";
+					        else return '';
+					    }
 					}
 				});
 
