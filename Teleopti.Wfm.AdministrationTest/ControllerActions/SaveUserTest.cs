11e1a74dde6626c40ac693dba76a6721c5b28f52
Ola <none@none>
2015-07-30 12:17:41 +0200

    test save of user

--- Teleopti.Wfm.AdministrationTest/ControllerActions/SaveUserTest.cs
+++ Teleopti.Wfm.AdministrationTest/ControllerActions/SaveUserTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Infrastructure.MultiTenancy.Admin;
 using Teleopti.Ccc.Infrastructure.MultiTenancy.Server;
@@ -15,24 +16,32 @@ namespace Teleopti.Wfm.AdministrationTest.ControllerActions
 	{
 		public AccountController Target;
 		public ITenantUnitOfWork TenantUnitOfWork;
-		//public ICurrentTenantSession CurrentTenantSession;
+		public ICurrentTenantSession CurrentTenantSession;
 
 		[Test]
 		public void ShouldUpdate()
 		{
 			//new fresh
 			DataSourceHelper.CreateDataSource(new NoMessageSenders(), "TestData");
+			int id;
 			using (TenantUnitOfWork.Start())
 			{
-				var addUserModel = new AddUserModel { ConfirmPassword = "passadej", Email = "ola@teleopti.com", Name = "Ola", Password = "passadej" };
-				Target.AddUser(addUserModel);
+				var user = new TenantAdminUser {AccessToken = "dummy",Email = "ola@teleopti.com", Name = "Ola", Password = "vadsomhelst"};
+				CurrentTenantSession.CurrentSession().Save(user);
+				id = user.Id;
 			}
-			//using (TenantUnitOfWork.Start())
-			//{
-			//	var result = Target.Login(model).Content;
-			//	result.Success.Should().Be.False();
-			//	result.Message.Should().Be.EqualTo("No user with that Email.");
-			//}
+			using (TenantUnitOfWork.Start())
+			{
+				var loaded = Target.GetOneUser(id);
+				loaded.Content.Email.Should().Be.EqualTo("ola@teleopti.com");
+				Target.SaveUser(new UpdateUserModel {Email = "olle@teleopti.com", Id = id, Name = "Olle"});
+			}
+
+			using (TenantUnitOfWork.Start())
+			{
+				var loaded = Target.GetOneUser(id);
+				loaded.Content.Email.Should().Be.EqualTo("olle@teleopti.com");
+			}
 		}
 
 	}
