a92ec2099fdd0884433430c747a658df846e06f0
Kunning Mao <none@none>
2015-07-31 16:25:42 +0200

    Backed out changeset: 58df52e77b09

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Content/Scripts/Teleopti.Start.Authentication.AuthenticationState.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Content/Scripts/Teleopti.Start.Authentication.AuthenticationState.js
@@ -162,7 +162,7 @@ Teleopti.Start.Authentication.AuthenticationState = function (data) {
 				$.extend(options, {
 					success: function (applicationsData, textState, jqXHR) {
 
-						var areas = ["MyTime", "Anywhere", "Reporting"];
+						var areas = ["MyTime", "Anywhere", "SeatPlanner", "Messages", "Reporting", "WFM"];
 						var areaToGo = ko.utils.arrayFirst(areas, function (a) {
 							var url = "/" + a + "/";
 							return window.location.href.toUpperCase().indexOf(url.toUpperCase()) !== -1;
