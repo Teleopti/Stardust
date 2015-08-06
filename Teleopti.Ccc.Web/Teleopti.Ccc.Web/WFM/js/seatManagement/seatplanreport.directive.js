f943b187926be8ec9fcbfc38f75bcfa586d4521f
jianfeng.zhu@teleopti.com <jianfeng.zhu@teleopti.com>
2015-08-06 12:10:43 +0800

    #33991 - Implement Seat Allocation Report  - Add filters functionality of front end

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/seatManagement/seatplanreport.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/seatManagement/seatplanreport.directive.js
@@ -4,39 +4,110 @@
 
 	angular.module('wfm.seatPlan').controller('seatPlanReportCtrl', seatPlanReportCtrl);
 
-	seatPlanReportCtrl.$inject = ['seatPlanService', '$window'];
+	seatPlanReportCtrl.$inject = ['seatPlanService', 'seatplanTeamAndLocationService'];
 
-	function seatPlanReportCtrl(seatPlanService, $window) {
+	function seatPlanReportCtrl(seatPlanService, seatplanTeamAndLocationService) {
 
 		var vm = this;
+
+		function getSeatBookings(options) {
+			vm.isLoadingReport = true;
+			var seatBookingReportParams = {
+				startDate: options.startDate != null ? moment(options.startDate).format('YYYY-MM-DD') : moment().format('YYYY-MM-DD'),
+				endDate: options.endDate != null ? moment(options.endDate).format('YYYY-MM-DD') : moment().format('YYYY-MM-DD'),
+				teams: options.teams != null ? options.teams : [],
+				locations: options.locations != null ? options.locations : [],
+				skip: options.skip != null ? options.skip : 0,
+				take: options.take != null ? options.take : 20
+			};
+			seatPlanService.seatBookingReport.get(seatBookingReportParams).$promise.then(function (data) {
+				vm.seatBookings = data.SeatBookingsByDate;
+				vm.isLoadingReport = false;
+			});
+		};
+
+
+		vm.locations = [];
+		vm.teams = [];
+		vm.isDatePickerOpened = true;
+		vm.selectedPeriod = vm.selectedPeriod == undefined ? { StartDate: '2015-01-01', EndDate: '2015-01-01' } : vm.selectedPeriod;
+
+		vm.toggleFilter = function (tabName) {
+			vm.isDatePickerOpened = false;
+			vm.isTeamPickerOpened = false;
+			vm.isLocationPickerOpened = false;
+
+			if (tabName == 'date') {
+				vm.isDatePickerOpened = true;
+			}
+			if (tabName == 'team') {
+				vm.isTeamPickerOpened = true;
+			}
+			if (tabName == 'location') {
+				vm.isLocationPickerOpened = true;
+			}
+		};
 
 		vm.getDisplayTimeString = function (startTime, endTime) {
 			return moment(startTime).format('HH:mm') + ' - ' + moment(endTime).format('HH:mm');
 		};
 
-		vm.getDisplayDateString = function (data) {
-			return moment(data).format('L');
+		vm.getDisplayDateString = function(date) {
+			return moment(date).format('L');
 		};
 
-		var seatBookingReportParams = {
-			startDate: moment('2015-08-17').format('YYYY-MM-DD'),
-			endDate: moment('2015-09-13').format('YYYY-MM-DD'),
-			//teams: []
-			//locations: []
-			skip: 0,
-			take : 20
+		vm.applyFilter = function () {
+			getSeatBookings({
+				startDate: vm.selectedPeriod.StartDate,
+				endDate: vm.selectedPeriod.EndDate,
+				teams: seatplanTeamAndLocationService.GetSelectedTeamsFromTeamList(vm.teams),
+				locations: seatplanTeamAndLocationService.GetSelectedLocationsFromLocationList(vm.locations)
+			});
+		};
+		vm.setRangeClass = function (date, mode) {
+			if (mode === 'day') {
+				var dayToCheck = new Date(date).setHours(12, 0, 0, 0);
+				var startDay = new Date(vm.selectedPeriod.StartDate).setHours(12, 0, 0, 0);
+				var endDay = new Date(vm.selectedPeriod.EndDate).setHours(12, 0, 0, 0);
+				if (dayToCheck >= startDay && dayToCheck <= endDay) {
+					return 'seatplan-status-success';
+				}
+			}
+			return '';
 		};
 
-		seatPlanService.seatBookingReport.get(seatBookingReportParams).$promise.then(function (data) {
-			vm.seatBookings = data.SeatBookingsByDate;
+		vm.selectTeam = seatplanTeamAndLocationService.SelectTeam;
 
+		vm.selectLocation = seatplanTeamAndLocationService.SelectLocation;
+
+		vm.getTeamDisplayText = seatplanTeamAndLocationService.GetTeamDisplayText;
+
+		vm.getLocationDisplayText = seatplanTeamAndLocationService.GetLocationDisplayText;
+
+
+		getSeatBookings({
+			startDate: vm.selectedPeriod == null ? null : vm.selectedPeriod.StartDate,
+			endDate: vm.selectedPeriod == null ? null : vm.selectedPeriod.EndDate,
+			take: 50
+
+	});
+
+		seatPlanService.locations.get().$promise.then(function (locations) {
+			locations.show = true;
+			vm.locations.push(locations);
 		});
-	};
 
+		seatPlanService.teams.get().$promise.then(function (teams) {
+			teams.show = true;
+			vm.teams.push(teams);
+		});
+
+	};
 })();
 
 
 (function () {
+
 	angular.module('wfm.seatPlan').directive('seatPlanReport', seatPlanReport);
 
 	function seatPlanReport() {
@@ -50,5 +121,3 @@
 		};
 	};
 })();
-
-
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/seatManagement/seatplanteamandlocation.service.js
@@ -0,0 +1,113 @@
+﻿'use strict';
+
+(function() {
+
+	angular.module('wfm.seatPlan').service('seatplanTeamAndLocationService', seatplanTeamAndLocationService);
+
+	seatplanTeamAndLocationService.$inject = ['seatPlanTranslateFactory'];
+
+	function seatplanTeamAndLocationService(seatPlanTranslateFactory) {
+
+		var getLocationDisplayText = function (location) {
+			if (location.Name == undefined) {
+				return seatPlanTranslateFactory.TranslatedStrings["NoLocationsAvailable"];
+			}
+			return location.Name + " (" + seatPlanTranslateFactory.TranslatedStrings["SeatCountTitle"] + ": {0})".replace("{0}", location.Seats.length);
+		};
+
+		var getTeamDisplayText = function (teamHierarchyNode) {
+			if (teamHierarchyNode.NumberOfAgents) {
+				return teamHierarchyNode.Name + " (" + seatPlanTranslateFactory.TranslatedStrings["AgentCountTitle"] +
+					": {0})".replace("{0}", teamHierarchyNode.NumberOfAgents);
+			} else {
+				return teamHierarchyNode.Name;
+			}
+
+		};
+
+		var selectTeam = function (team) {
+			team.selected = team.NumberOfAgents && team.NumberOfAgents > 0 ? !team.selected : team.selected;
+		};
+
+		var selectLocation = function (location) {
+			location.selected = location.Seats && location.Seats.length > 0 ? !location.selected : location.selected;
+		};
+
+		var getSelectedTeamsFromTeamList = function(teamList) {
+		
+			function getSelectedTeams(teams, result) {
+
+				teams.forEach(function (team) {
+					if (team.selected) {
+						result.push(team.Id);
+					}
+					if (team.Children != null && team.Children.length > 0) {
+						getSelectedTeams(team.Children, result);
+					}
+				});
+			}
+
+			var selectedTeams = [];
+			getSelectedTeams(teamList, selectedTeams);
+			return selectedTeams;
+		};
+		
+		var getSelectedLocationsFromLocationList = function(locationList) {
+
+			function getSelectedLocations(locations,result) {
+				locations.forEach(function (location) {
+					if (location.selected) {
+						result.push(location.Id);
+					}
+					if (location.Children != null && location.Children.length > 0) {
+						getSelectedLocations(location.Children, result);
+					}
+				});
+			}
+
+			var selectedLocations = [];
+			getSelectedLocations(locationList, selectedLocations);
+			return selectedLocations;
+		};
+
+		return {
+			SelectTeam: selectTeam,
+			SelectLocation: selectLocation,
+			GetSelectedTeamsFromTeamList: getSelectedTeamsFromTeamList,
+			GetSelectedLocationsFromLocationList: getSelectedLocationsFromLocationList,
+			GetLocationDisplayText: getLocationDisplayText,
+			GetTeamDisplayText: getTeamDisplayText
+		};
+
+	};
+}());
+
+
+(function() {
+
+	angular.module('wfm.seatPlan').factory('seatPlanTranslateFactory', seatPlanTranslateFactory);
+
+	seatPlanTranslateFactory.$inject = ['$translate'];
+
+	function seatPlanTranslateFactory(translate) {
+
+		var translatedStrings = {};
+
+		var setupTranslatedString = function (key) {
+			translate(key).then(function (result) {
+				translatedStrings[key] = result;
+			});
+		};
+
+		setupTranslatedString("NoLocationsAvailable");
+		setupTranslatedString("SeatCountTitle");
+		setupTranslatedString("AgentCountTitle");
+		setupTranslatedString("TeamsOrLocationsAreUnselected");
+		setupTranslatedString("SeatPlanSubmittedOK");
+
+		return {
+			TranslatedStrings: translatedStrings
+		}
+	};
+
+}());
