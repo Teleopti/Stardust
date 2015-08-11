007d7adc94fdc7f96d88f8d8fab8b4f4cc1f8cfd
Chundan Xu <chundan.xu@teleopti.com>
2015-08-11 11:44:26 +0800

    Bug #34219 When open for the 2nd time, whatever you input will let search filter close

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/people/people.outsideClick.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/people/people.outsideClick.directive.js
@@ -11,11 +11,18 @@
 
 		function linkFunction(scope, element, attrs) {
 			var outsideClickHandler = $parse(attrs.outsideClick);
-			angular.element($window).on('click', function (event) {
+			var clickEventHandler = function(event) {
 				if (element[0].contains(event.target)) return;
 				outsideClickHandler(scope, { $event: event });
 				scope.$apply();
-			});
+			}
+			angular.element($window).on('click', clickEventHandler);
+
+			var cleanUp = function () {
+				window.angular.element($window).off('click', clickEventHandler);
+			};
+
+			scope.$on('$destroy', cleanUp);
 		}
 	}]);
 
