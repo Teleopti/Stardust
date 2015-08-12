c001c095f1abd54a6ec2ec022aaad3589e5e12ce
Roger Kratz <roger.kratz@teleopti.com>
2015-08-12 14:27:12 +0200

    #33808 remove other (non existing) nhib file usages

--- teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
@@ -251,10 +251,7 @@ Try
 	
 
     $DataSourceName = TeleoptiDriveMapProperty-get -name "DataSourceName"
-    
-	#Remove *.nhib.xml files that still resides in public facing directories
-	$WebRootFolder="$directory\..\..\sitesroot\*"
-	get-childitem $WebRootFolder -include *.nhib.xml -recurse | foreach ($_) {remove-item $_.fullname}
+
 	
 	Remove-Item "$fullPathsettingsFile"
 	if (Test-Path "$fullPathsettingsFile") {
