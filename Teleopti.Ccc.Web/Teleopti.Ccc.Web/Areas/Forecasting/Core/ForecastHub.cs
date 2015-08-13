2964013908b4f73e282aa30d6a48de23172da766
Kunning Mao <none@none>
2015-08-13 11:54:21 +0200

    fix red build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Forecasting/Core/ForecastHub.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Forecasting/Core/ForecastHub.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Teleopti.Ccc.Web.Areas.Forecasting.Core
 {
 	[HubName("forecastHub")]
+	[CLSCompliant(false)]
 	public class ForecastHub : Hub
 	{
 	}
