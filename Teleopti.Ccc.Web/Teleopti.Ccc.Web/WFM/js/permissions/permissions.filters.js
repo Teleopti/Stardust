21d3c843ef8aa18efcb882c7d97db5da508f2ef9
Felix <none@none>
2015-08-13 15:51:05 +0200

    Refactoring in permissions filters.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/permissions/permissions.filters.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/permissions/permissions.filters.js
@@ -4,100 +4,19 @@
 
 	permissionsFilters.filter('selectedFunctions', [
 		function() {
-		    return function (nodes, selectedFunctionToggle) {
+		    return function(nodes, selectedFunctionToggle) {
 		        if (!selectedFunctionToggle) return nodes;
 
 		        var filteredNodes = [];
-		        nodes.forEach(function (node) {
+		        nodes.forEach(function(node) {
 		            if (node.selected || node.nmbSelectedChildren > 0) {
 		                filteredNodes.push(node);
 		            }
 		        });
 		        return filteredNodes;
-		    }
-		}
-	]);
-
-	permissionsFilters.filter('unselectedFunctions', [
-		function() {
-		    return function (nodes, unselectedFunctionToggle) {
-		        if (!unselectedFunctionToggle) return nodes;
-		        var filteredNodes = [];
-		        nodes.forEach(function (node) {
-		            var hasUnselectedChildren = false;
-		            if (node.ChildFunctions && node.ChildFunctions.length > 0
-						&& node.nmbSelectedChildren < node.ChildFunctions.length)
-		                hasUnselectedChildren = true;
-
-		            if (!node.selected || hasUnselectedChildren) {
-		                filteredNodes.push(node);
-		            }
-		        });
-		        return filteredNodes;
-		    }
-		}
-	]);
-
-    var getChildren = function(child, filterType) {
-        if (filterType === 'descriptionFilter') {
-            return child.ChildFunctions;
-        } else {
-            return child.ChildNodes;
-        }
-    }
-
-    var matchChild = function (node, reg, filterType) {
-        if (filterType === 'descriptionFilter') {
-            return node.LocalizedFunctionDescription.match(reg);
-        } else {
-            return node.Name.match(reg);
-        }
-    }
-
-	var checkChild = function (child, name, filterType) {
-	    var hasChildWithName = 0;
-	    var reg = new RegExp(name, "i");
-	    var thosechildren = getChildren(child, filterType);
-	    if (thosechildren && thosechildren.length > 0) {
-	        thosechildren.forEach(function (node) {
-	            if (checkChild(node, name, filterType))
-	                hasChildWithName++;
-	        });
-	        return (hasChildWithName > 0 || matchChild(child,reg, filterType));
-	    } else {
-	        return matchChild(child, reg, filterType);
-	    }
-	};
-
-    var filterNodes= function(nodes, name, filterType) {
-        var filteredNodes = [];
-
-        nodes.forEach(function (node) {
-            if (checkChild(node, name, filterType)) {
-                filteredNodes.push(node);
-            }
-        });
-
-        return filteredNodes;
-    }
-
-    permissionsFilters.filter('descriptionFilter', [
-		function () {
-		    return function (nodes, name) {
-		        if (!name) return nodes;
-		        return filterNodes(nodes, name, 'descriptionFilter');
 		    };
 		}
-    ]);
-
-    permissionsFilters.filter('nameFilter', [
-		function () {
-		    return function (nodes, name) {
-		        if (!name) return nodes;
-		        return filterNodes(nodes, name, 'nameFilter');
-		    };
-		}
-    ]);
+	]);
 
     permissionsFilters.filter('selectedData', [
     function () {
@@ -133,29 +52,155 @@
 		    return filteredNodes;
 	    };
     }
-	]);
+    ]);
+
+
+	//permissionsFilters.filter('unselectedFunctions', [
+	//	function() {
+	//	    return function (nodes, unselectedFunctionToggle) {
+	//	        if (!unselectedFunctionToggle) return nodes;
+	//	        var filteredNodes = [];
+	//	        nodes.forEach(function (node) {
+	//	            var hasUnselectedChildren = false;
+	//	            if (node.ChildFunctions && node.ChildFunctions.length > 0
+	//					&& node.nmbSelectedChildren < node.ChildFunctions.length)
+	//	                hasUnselectedChildren = true;
+	//	            console.log("CHILD DDE TOIOLETTE ", getChild(node));
+	//	            if (!node.selected || hasUnselectedChildren) {
+	//	                filteredNodes.push(node);
+	//	            }
+	//	        });
+	//	        return filteredNodes;
+	//	    }
+	//	}
+	//]);
+
+    //permissionsFilters.filter('unselectedData', [
+	//	function () {
+	//		return function(nodes, unselectedDataToggle) {
+	//			if (!unselectedDataToggle) return nodes;
+	//			var filteredNodes = [];
+	//			nodes.forEach(function(node) {
+	//				var hasUnselectedChildren = false;
+	//				if (node.ChildNodes && node.ChildNodes.length > 0
+	//					&& node.nmbSelectedChildren < node.ChildNodes.length)
+	//					hasUnselectedChildren = true;
+
+	//				if (node.nmbSelectedChildren > 0) {
+	//					node.selected
+	//				}
+
+	//				if (!node.selected || hasUnselectedChildren) {
+	//					filteredNodes.push(node);
+	//				}
+	//			});
+	//			return filteredNodes;
+	//		};
+	//	}
+    //]);
+
+    var getChildren = function(child, filterType) {
+        if (filterType === 'descriptionFilter') {
+            return child.ChildFunctions;
+        } else {
+            return child.ChildNodes;
+        }
+    }
+
+    var matchChild = function (node, reg, filterType) {
+        if (filterType === 'descriptionFilter') {
+            return node.LocalizedFunctionDescription.match(reg);
+        } else {
+            return node.Name.match(reg);
+        }
+    }
+    
+    var checkChild = function (child, name, filterType) {
+	    var hasChildWithName = 0;
+	    var reg = new RegExp(name, "i");
+	    var thoseChildren = getChildren(child, filterType);
+	    if (thoseChildren && thoseChildren.length > 0) {
+	        thoseChildren.forEach(function (node) {
+	            if (checkChild(node, name, filterType))
+	                hasChildWithName++;
+	        });
+	        return (hasChildWithName > 0 || matchChild(child, reg, filterType));
+	    } else {
+	        return matchChild(child, reg, filterType);
+	    }
+	};
+
+    var filterNodes = function (nodes, name, filterType) {
+        var filteredNodes = [];
+
+        nodes.forEach(function (node) {
+            if (checkChild(node, name, filterType)) {
+                filteredNodes.push(node);
+            }
+        });
+
+        return filteredNodes;
+    }
+
+    permissionsFilters.filter('descriptionFilter', [
+		function () {
+		    return function (nodes, name) {
+		        if (!name) return nodes;
+		        return filterNodes(nodes, name, 'descriptionFilter');
+		    };
+		}
+    ]);
+
+    permissionsFilters.filter('nameFilter', [
+		function () {
+		    return function (nodes, name) {
+		        if (!name) return nodes;
+		        return filterNodes(nodes, name, 'nameFilter');
+		    };
+		}
+    ]);
+
+    //
+
+
+
+    var unselect = function(nodes, toggle, getChild) {
+        if (!toggle) return nodes;
+        var filteredNodes = [];
+        nodes.forEach(function (node) {
+            var hasUnselectedChildren = false;
+            if (getChild(node) && getChild(node).length > 0
+                && node.nmbSelectedChildren < getChild(node).length){
+                console.log("NODE ", node);
+                hasUnselectedChildren = true;
+                }
+            console.log("CHILD DDE TOIOLETTE ", getChild(node));
+            if (!node.selected || hasUnselectedChildren) {
+                filteredNodes.push(node);
+            }
+        });
+        return filteredNodes;
+    };
+   
+    permissionsFilters.filter('unselectedFunctions', [
+        function() {   
+            return function (nodes, unselectedFunctionsToggle) {
+                return unselect(nodes, unselectedFunctionsToggle, function (node) {
+                    console.log("AHHOHOHO CHILD BAGUETTE ", node.ChildFunctions);
+                    return node.ChildFunctions;
+                });
+            };
+        }
+    ]);
 
     permissionsFilters.filter('unselectedData', [
-		function () {
-			return function(nodes, unselectedDataToggle) {
-				if (!unselectedDataToggle) return nodes;
-				var filteredNodes = [];
-				nodes.forEach(function(node) {
-					var hasUnselectedChildren = false;
-					if (node.ChildNodes && node.ChildNodes.length > 0
-						&& node.nmbSelectedChildren < node.ChildNodes.length)
-						hasUnselectedChildren = true;
+        function () {
+            return function (nodes, unselectedDataToggle) {
+               return unselect(nodes, unselectedDataToggle, function (node) {
+                    return node.ChildNodes;
+                });
+            };
+        }
+    ]);
 
-					if (node.nmbSelectedChildren > 0) {
-						node.selected
-					}
-
-					if (!node.selected || hasUnselectedChildren) {
-						filteredNodes.push(node);
-					}
-				});
-				return filteredNodes;
-			};
-		}
-	]);
 })();
