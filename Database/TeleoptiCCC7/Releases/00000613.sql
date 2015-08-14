bb94733fb263d4983491affd910d3eb1c80419b9
Chundan Xu <chundan.xu@teleopti.com>
2015-08-14 11:35:46 +0800

    Bug #34255 Gamification crash which also potentially affects MyTime

--- Database/TeleoptiCCC7/Releases/00000613.sql
+++ Database/TeleoptiCCC7/Releases/00000613.sql
@@ -1,7 +1,18 @@
 ----------------  
 --Name: Chundan Xu
---Desc: 2015-08-13 Added unique index for teamGamificationSetting. 
+--Desc: 2015-08-13 Remove duplicated setting for the same team 
+--                 Added unique index for teamGamificationSetting. 
 ---------------- 
+Delete from [dbo] .[TeamGamificationSetting]
+where Id in (
+select dupl. id
+from    [dbo]. [TeamGamificationSetting]   orig ,
+       [dbo] . [TeamGamificationSetting]   dupl
+where   orig. Team   =    dupl . Team
+and     orig. id     <    dupl . id
+)
+Go
+
 CREATE UNIQUE NONCLUSTERED INDEX [UQ_Key_Team_TeamGamificationSetting]
 ON [dbo].[TeamGamificationSetting] 
 (
