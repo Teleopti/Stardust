4e69985308c85cf4328a97c7e024d4c98ead2191
Robin Karlsson <robin.karlsson@teleopti.com>
2015-08-18 11:44:59 +0200

    Ouch, not in shape yet I think. (#34292)

--- DomainTest/ApplicationLayer/PersonChangedMessageLegacyHandlerTest.cs
+++ DomainTest/ApplicationLayer/PersonChangedMessageLegacyHandlerTest.cs
@@ -1,7 +1,7 @@
 using NUnit.Framework;
 using Rhino.Mocks;
 using Teleopti.Ccc.Domain.ApplicationLayer.PersonCollectionChangedHandlers;
-using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Messages.Denormalize;
 
 namespace Teleopti.Ccc.DomainTest.ApplicationLayer
@@ -12,7 +12,7 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		[Test]
 		public void ShouldHandleLegacyMessage()
 		{
-			var repository = MockRepository.GenerateMock<PersonFinderReadOnlyRepository>();
+			var repository = MockRepository.GenerateMock<IPersonFinderReadOnlyRepository>();
 			var handler = new UpdateFindPersonConsumer(repository);
 			var legacyMessage = new PersonChangedMessage();
 
