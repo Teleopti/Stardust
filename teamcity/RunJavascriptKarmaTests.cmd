26721c434f70811c8cf5c4d41fe6689716ee7dc4
Zheng Li <zheng.li@teleopti.com>
2015-08-27 11:28:10 +0800

    **TRY** use "npm test" as test command fix build errors

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -1,19 +1,3 @@
 :: Runs from [repo]\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM
 
-call ..\.node\npm uninstall karma
-call ..\.node\npm uninstall karma-chrome-launcher
-call ..\.node\npm uninstall karma-jasmine
-call ..\.node\npm uninstall grunt-sass
-
-call ..\.node\npm install -g karma@0.12.31
-call ..\.node\npm install -g karma-teamcity-reporter
-call ..\.node\npm install -g karma-chrome-launcher@0.1.10
-call ..\.node\npm install -g karma-jasmine
-call ..\.node\npm install -g karma-cli
-call ..\.node\npm install -g flatten-packages
-
-for /f "tokens=1" %%i in ('..\.node\npm bin -g') do set output=%%i
-echo "%output%"
-
-call "%output%\flatten-packages.cmd" "%output%"
-call "%output%\karma" start --reporters teamcity --single-run
+call ..\.node\npm test
