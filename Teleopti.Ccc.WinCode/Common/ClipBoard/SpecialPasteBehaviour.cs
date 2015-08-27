28763ab880f75bea8ad2fdca1faead940a9d35c2
tamasb <tamasb@teleopti.com>
2015-08-27 14:20:28 +0200

    Add SpecialPasteBehaviour and test as part of fix for copying short absences. #34379: Copy and paste special does not work for 2 different absences on same day when they together cover the entire shift CRM 20150804:017

---
+++ Teleopti.Ccc.WinCode/Common/ClipBoard/SpecialPasteBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCode.Common.Clipboard
+{
+	public class SpecialPasteBehaviour
+	{
+		public IScheduleDay DoPaste(IScheduleDay source)
+		{
+			//if (source.PersistableScheduleDataCollection().Any(scheduleData => scheduleData.Id.HasValue))
+			//{
+			//	throw new ArgumentException("Schedule day is not a copy");
+			//}
+
+			foreach (var personAbsence in source.PersonAbsenceCollection(true))
+			{
+				reduceAbsenceAtStart(personAbsence, source);
+				reduceAbsenceAtEnd(personAbsence, source);
+			}
+
+			return source;
+		}
+
+		private void reduceAbsenceAtStart(IPersonAbsence personAbsence, IScheduleDay source)
+		{
+			var scheduleDayStartDateTime = source.DateOnlyAsPeriod.Period().StartDateTime;
+			var personAbsenceStartDateTime = personAbsence.Period.StartDateTime;
+
+			if (personAbsenceStartDateTime < scheduleDayStartDateTime)
+			{
+				var newPeriod = personAbsence.Period.ChangeStartTime(scheduleDayStartDateTime.Subtract(personAbsenceStartDateTime));
+				personAbsence.ReplaceLayer(personAbsence.Layer.Payload, newPeriod);
+			}
+		}
+
+
+		private void reduceAbsenceAtEnd(IPersonAbsence personAbsence, IScheduleDay source)
+		{
+			var scheduleDayEndDateTime = source.DateOnlyAsPeriod.Period().EndDateTime;
+			var personAbsenceEndDateTime = personAbsence.Period.EndDateTime;
+
+			if (personAbsenceEndDateTime > scheduleDayEndDateTime)
+			{
+				var newPeriod = personAbsence.Period.ChangeEndTime(scheduleDayEndDateTime.Subtract(personAbsenceEndDateTime));
+				personAbsence.ReplaceLayer(personAbsence.Layer.Payload, newPeriod);
+			}
+		}
+
+	}
+}
---
+++ Teleopti.Ccc.WinCodeTest/Common/ClipBoard/SpecialPasteBehaviourTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.WinCode.Common.Clipboard;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCodeTest.Common.Clipboard
+{
+	[TestFixture]
+	public class SpecialPasteBehaviourTest
+	{
+		private SpecialPasteBehaviour _target;
+		private Person _person;
+		private DateOnly _dateOnly;
+		private IScheduleDay _source;
+
+
+		[SetUp]
+		public void Setup()
+		{
+			_person = new Person();
+			_dateOnly = new DateOnly(2015, 08, 27);
+			_source = ScheduleDayFactory.Create(_dateOnly, _person);
+			_target = new SpecialPasteBehaviour();
+		}
+
+		//[Test]
+		//[ExpectedException(typeof(ArgumentException))]
+		//public void SourceShouldBeClone()
+		//{
+		//	_source.CreateAndAddAbsence(new AbsenceLayer(new Absence(), new DateTimePeriod(2015, 08, 27, 2015, 08, 28)));
+		//	var personAbsence = _source.PersistableScheduleDataCollection().First();
+		//	personAbsence.SetId(Guid.NewGuid());
+		//	IScheduleDay result = _target.DoPaste(_source);
+		//}
+
+		[Test]
+		public void ShouldCutLongAbsenceAtStartOfDay()
+		{
+			_source.CreateAndAddAbsence(new AbsenceLayer(new Absence(), new DateTimePeriod(2015, 08, 26, 2015, 08, 28)));
+
+			IScheduleDay result = _target.DoPaste(_source);
+
+			var personAbsence = result.PersonAbsenceCollection(true).First();
+
+			var scheduleDayStartDateTime = _source.DateOnlyAsPeriod.Period().StartDateTime;
+			Assert.IsTrue(personAbsence.Period.StartDateTime==scheduleDayStartDateTime);
+		}
+
+		[Test]
+		public void ShouldPreserveShortAbsence()
+		{
+			_source.CreateAndAddAbsence(new AbsenceLayer(new Absence(), new DateTimePeriod(2015, 08, 27, 08, 2015, 08, 27, 12)));
+
+			IScheduleDay result = _target.DoPaste(_source);
+
+			var personAbsence = result.PersonAbsenceCollection(true).First();
+
+			Assert.IsTrue(personAbsence.Period.StartDateTime == new DateTime(2015, 08, 27, 08, 00, 00));
+			Assert.IsTrue(personAbsence.Period.EndDateTime == new DateTime(2015, 08, 27, 12, 00, 00));
+		}
+
+		[Test]
+		public void ShouldCutLongAbsenceAtEndOfDay()
+		{
+			_source.CreateAndAddAbsence(new AbsenceLayer(new Absence(), new DateTimePeriod(2015, 08, 27, 2015, 08, 29)));
+
+			IScheduleDay result = _target.DoPaste(_source);
+
+			var personAbsence = result.PersonAbsenceCollection(true).First();
+
+			var scheduleDayEndDateTime = _source.DateOnlyAsPeriod.Period().EndDateTime;
+			Assert.IsTrue(personAbsence.Period.EndDateTime == scheduleDayEndDateTime);
+		}
+
+	}
+}
