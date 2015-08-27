b4fc041196e573c18c7854548e36066828d828fb
Roger Kratz <roger.kratz@teleopti.com>
2015-08-27 15:53:00 +0200

    removing dead code

--- Domain/Outbound/OutboundAssignedStaffProvider.cs
+++ Domain/Outbound/OutboundAssignedStaffProvider.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Teleopti.Ccc.Domain.AgentInfo;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Domain.Outbound
 {
--- Domain/Outbound/OutboundSkillTypeProvider.cs
+++ Domain/Outbound/OutboundSkillTypeProvider.cs
@@ -1,12 +1,9 @@
-﻿
-
-using System.Linq;
+﻿using System.Linq;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Outbound
 {
-
 	public class OutboundSkillTypeProvider : IOutboundSkillTypeProvider
 	{
 		private readonly ISkillTypeRepository _skillTypeRepository;
