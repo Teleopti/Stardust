1961eb2eab756ee428cd2363274f093cd2ff0619
Mathias Stenbom <mathias.stenbom@teleopti.com>
2015-08-31 10:32:59 +0200

    #32539 cant repro warning locally

--- InfrastructureTest/MultiTenancy/Server/Queries/CountTenantsTest.cs
+++ InfrastructureTest/MultiTenancy/Server/Queries/CountTenantsTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Castle.Core.Internal;
 using NUnit.Framework;
 using SharpTestsEx;
@@ -21,7 +22,7 @@ namespace Teleopti.Ccc.InfrastructureTest.MultiTenancy.Server.Queries
 		public ILoadAllTenants LoadAll;
 
 		[Test]
-		public void ShouldCountTenants([Values(0, 1, 3, 7)] int expected)
+		public void ShouldCountTenants([Values(1, 3, 7)] int expected)
 		{
 			using (TenantUnitOfWork.Start())
 				LoadAll.Tenants().ForEach(Delete.Delete);
