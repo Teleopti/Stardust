b4ae8774a73a365de63488b7335ddf5fc93c5884
Yanyi Wan <none@none>
2015-09-06 15:59:33 +0800

    PBI #34653 Improve timepicker
    - based on angular ui bootstrap
    - locale-aware time format
    - locale-aware meridian input

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.directive.timepicker.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.directive.timepicker.js
@@ -1,362 +1,55 @@
 ﻿(function() {
-    'use strict';
+	'use strict';
 
-    angular.module('wfm.outbound')
-		.directive('wfmTimepicker', timepickerDirective)
-		.directive('minuteInput', minuteInputDirective)
-		.directive('hourInput', hourInputDirective);
+	angular.module('wfm.outbound')
+		.directive('timepickerWrap', timepickerWrapDirective);
 
-    function timepickerDirective() {
-        return {
-            restrict: 'E',
-            require: ["wfmTimepicker", "ngModel"],
-            template: timepickerTemplate,
-            scope: {
-                'useMeridian': '=?',
-                'hourStep': '@?',
-                'minuteStep': '@?',
-                'maxTime': '=?',
-                'minTime': '=?'
-            },
-            controller: ["$scope", "$element", "$attrs", "$filter", "$log", timepickerCtrl],
-            link: postLink
-        };
+	function timepickerWrapDirective() {
 
-        function timepickerCtrl($scope, $element, $attrs, $filter, $log) {
-            var self = this;
+		var meridianInfo = getMeridiemInfoFromMoment();
 
-            self.updateRange = function (useMeridian, hourStep, minuteStep) {
-                var minHour = useMeridian ? 1 : 0;
-                var maxHour = useMeridian ? 12 : 23;
-                var minMinute = 0;
-                var maxMinute = 59;
+		return {
+			template: '<timepicker></timepicker>',
+			controller: ['$scope', timepickerWrapCtrl],
+			compile: compileFn
+		};
 
-                hourStep = hourStep ? parseInt(hourStep) : 1;
-                minuteStep = minuteStep ? parseInt(minuteStep) : 15;
+		function compileFn(tElement, tAttributes) {
+			var binding = tAttributes.ngModel;
+			tElement.addClass('wfm-timepicker-wrap');
 
-                self.range = {
-                    hour: { min: minHour, max: maxHour, step: hourStep },
-                    minute: { min: minMinute, max: maxMinute, step: minuteStep }
-                };
-            };
-            self.updateRange();
+			var cellElement = tElement.find('timepicker');
+			cellElement.attr('ng-model', binding);
+			cellElement.attr('show-meridian', 'showMeridian');
+			cellElement.attr('minute-step', 'minuteStep');
 
+			if (meridianInfo.showMeridian) {
+				cellElement.attr('meridians', 'meridians');
+			}
+		}
 
-            self.readTime = function (timeValue, useMeridian) {
-                if (!timeValue) return null;
-                var timeText = self.getTimeText(timeValue, useMeridian);
-                var pieces = timeText.split(':');
+		function timepickerWrapCtrl($scope) {
+			$scope.showMeridian = meridianInfo.showMeridian;
+			$scope.minuteStep = 5;
 
-                var timeModel = {
-                    hour: parseInt(pieces[0]),
-                    minute: parseInt(pieces[1])
-                };
-                if (useMeridian) {
-                    timeModel.meridian = (pieces[2] == 'AM') ? 0 : 1;
-                }
-                return timeModel;
-            };
+			if (meridianInfo.showMeridian) {
+				$scope.meridians = [meridianInfo.am, meridianInfo.pm];
+			}
+		}
+	}
 
-            self.changeByKeyPress = function (evt, ngModel, ceiling) {
-                var action;
-                if (evt.which === 38) {
-                    evt.preventDefault();
-                    action = increment;
-                } else if (evt.which === 40) {
-                    evt.preventDefault();
-                    action = decrement;
-                }
-                if (action) {
-                    ngModel.$setViewValue(action(ngModel.$modelValue, ceiling));
-                    ngModel.$render();
-                }
-            };
+	function getMeridiemInfoFromMoment() {
+		var timeFormat = moment.localeData()._longDateFormat.LT;
+		var info = {};
 
-            self.changeByMouseWheel = function (evt, ngModel, ceiling) {
-                evt.preventDefault();
-                var isScrollingUp = function (e) {
-                    if (e.originalEvent) {
-                        e = e.originalEvent;
-                    }
-                    var delta = (e.wheelDelta) ? e.wheelDelta : -e.deltaY;
-                    return (e.detail || delta > 0);
-                };
-                var action = isScrollingUp(evt) ? increment : decrement;
-                if (action) {
-                    ngModel.$setViewValue(action(ngModel.$modelValue, ceiling));
-                    ngModel.$render();
-                }
-            };
-
-            self.getTimeText = function (timeValue, useMeridian) {
-                if (!timeValue) return null;
-                return $filter('date')(timeValue, useMeridian ? 'h:m:a' : 'H:m');
-            };
-
-            self.logTimeValue = function (timeValue) {
-                $log.log($filter('date')(timeValue, 'HH:mm'));
-            };
-
-            function increment(value, range) {
-                if (value == null) return range.min;
-                var overflow = value + range.step - range.max;
-                return (overflow > 0) ? range.min + overflow - 1 : value + range.step;
-            }
-
-            function decrement(value, range) {
-                if (value == null) return range.max;
-                var overflow = value - range.step - range.min;
-                return (overflow < 0) ? range.max + overflow + 1 : value - range.step;
-            }
-
-        }
-
-        function postLink(scope, elem, attrs, ctrls) {
-            var timepicker = ctrls[0];
-            var ngModel = ctrls[1];
-            var debug = angular.isDefined(attrs['debug']);
-
-            elem.attr('layout', 'row');
-            scope.timeValue = { hour: null, minute: null, meridian: null };
-
-            scope.$watch('useMeridian', function (newValue, oldValue) {
-                if (newValue == oldValue) return;
-                timepicker.updateRange(newValue, scope.hourStep, scope.minuteStep);
-                if (ngModel.$modelValue) {
-                    setViewValue(timepicker.readTime(ngModel.$modelValue, newValue));
-                }
-                ngModel.$render();
-            });
-
-            scope.$watch('hourStep', function (newValue, oldValue) {
-                if (newValue == oldValue) return;
-                timepicker.updateRange(scope.useMeridian, newValue, scope.minuteStep);
-            });
-
-            scope.$watch('minuteStep', function (newValue, oldValue) {
-                if (newValue == oldValue) return;
-                timepicker.updateRange(scope.useMeridian, scope.hourStep, newValue);
-            });
-
-            scope.$watch('timeValue', setViewValue, true);
-            scope.$watch(function () {
-                return ngModel.$dirty && ngModel.$invalid;
-            }, function (newValue) {
-                if (newValue) {
-                    var errMsg;
-                    if (angular.isDefined(ngModel.$error["minTime"])) {
-                        errMsg = "Min-time: " + timepicker.getTimeText(scope.minTime);
-                        scope.$broadcast('timepicker.notification.invalid', errMsg);
-                    }
-
-                    if (angular.isDefined(ngModel.$error['maxTime'])) {
-                        errMsg = "Max-time: " + timepicker.getTimeText(scope.maxTime);
-                        scope.$broadcast('timepicker.notification.invalid', errMsg);
-                    }
-                }
-            });
-
-            ngModel.$formatters.push(formatter);
-            ngModel.$parsers.push(parser);
-            ngModel.$render = renderer;
-
-            ngModel.$validators.setHourAndMinute = function (modelValue, viewValue) {
-                if (modelValue == null && viewValue == null) return true;
-                if (viewValue.hour != null && viewValue.minute != null) {
-                    return !scope.useMeridian || viewValue.meridian != null;
-                } else {
-                    return false;
-                }
-            };
-
-            ngModel.$validators.minTime = function (modelValue, viewValue) {
-                if (modelValue == null) return true;
-                return isNaN(scope.minTime) ||
-				new Date(modelValue.getTime()).setFullYear(1970, 0, 1) >=
-				new Date(scope.minTime.getTime()).setFullYear(1970, 0, 1);
-            };
-
-            ngModel.$validators.maxTime = function (modelValue, viewValue) {
-                if (modelValue == null) return true;
-                return isNaN(scope.maxTime) ||
-				new Date(modelValue.getTime()).setFullYear(1970, 0, 1) <=
-				new Date(scope.maxTime.getTime()).setFullYear(1970, 0, 1);
-            };
-
-
-            function setViewValue(newValue, oldValue) {
-
-                if (newValue == oldValue) return;
-                ngModel.$setViewValue(angular.copy(newValue));
-            };
-
-            function renderer() {
-                scope.timeValue = ngModel.$viewValue || {};
-                if (scope.useMeridian && scope.timeValue.meridian == null) {
-                    scope.timeValue.meridian = 0;
-                }
-            }
-
-            function parser(viewValue) {
-                var date = new Date();
-                var meridianAdjustment = 0;
-                if (viewValue.meridian != null) {
-                    meridianAdjustment = viewValue.meridian * 12;
-                    if (viewValue.hour == 12) {
-                        meridianAdjustment -= 12;
-                    }
-                }
-                date.setHours(viewValue.hour + meridianAdjustment);
-                date.setMinutes(viewValue.minute);
-
-                if (debug) {
-                    timepicker.logTimeValue(date);
-                }
-
-                return date;
-            }
-
-            function formatter(modelValue) {
-                return timepicker.readTime(modelValue, scope.useMeridian);
-            }
-        }
-    }
-
-
-    function hourInputDirective() {
-        return {
-            require: ['ngModel', '^wfmTimepicker'],
-            restrict: 'A',
-            link: postLink
-        };
-
-        function postLink(scope, elem, attrs, ctrls) {
-            var ngModel = ctrls[0];
-            var wfmTimepicker = ctrls[1];
-
-            elem.attr('placeholder', wfmTimepicker.showMeridian ? 'hh' : 'HH');
-            elem.bind('focus', onFocus);
-            elem.bind('blur', onBlur);
-
-
-            scope.$on('timepicker.notification.invalid', function () {
-                ngModel.$setValidity('range', false);
-            });
-
-            ngModel.$validators.number = function (modelValue, viewValue) {
-                if (viewValue != null && (angular.isString(viewValue) && viewValue)) {
-                    return isFinite(viewValue);
-                } else {
-                    return true;
-                }
-            };
-
-            ngModel.$validators.range = function (modelValue, viewValue) {
-                if (modelValue != null) {
-                    return modelValue >= wfmTimepicker.range.hour.min &&
-							  modelValue <= wfmTimepicker.range.hour.max;
-                } else {
-                    return true;
-                }
-            };
-
-            ngModel.$parsers.push(function (viewValue) {
-                if (viewValue != null) {
-                    var parsed = parseInt(viewValue);
-                    if (isFinite(parsed)) return parsed;
-                }
-                return null;
-            });
-
-            function changeByKeyPress(evt) {
-                wfmTimepicker.changeByKeyPress(evt, ngModel, wfmTimepicker.range.hour);
-            }
-
-            function changeByMouseWheel(evt) {
-                wfmTimepicker.changeByMouseWheel(evt, ngModel, wfmTimepicker.range.hour);
-            }
-
-            function onFocus(evt) {
-                elem.bind('keydown', changeByKeyPress);
-                elem.bind('mousewheel wheel', changeByMouseWheel);
-            }
-
-            function onBlur(evt) {
-                elem.unbind('keydown', changeByKeyPress);
-                elem.unbind('mousewheel wheel', changeByMouseWheel);
-            }
-        }
-    }
-
-    function minuteInputDirective() {
-        return {
-            restrict: 'A',
-            require: ['ngModel', '^wfmTimepicker'],
-            link: postLink
-        };
-
-        function postLink(scope, elem, attrs, ctrls) {
-            var ngModel = ctrls[0];
-            var wfmTimepicker = ctrls[1];
-
-            elem.attr('placeholder', 'mm');
-            elem.bind('focus', onFocus);
-            elem.bind('blur', onBlur);
-
-            scope.$on('timepicker.notification.invalid', function () {
-                ngModel.$setValidity('range', false);
-            });
-
-
-            ngModel.$validators.number = function (modelValue, viewValue) {
-                if (viewValue != null && (angular.isString(viewValue) && viewValue)) {
-                    return isFinite(viewValue);
-                } else {
-                    return true;
-                }
-            };
-
-            ngModel.$validators.range = function (modelValue, viewValue) {
-                if (modelValue != null) {
-                    return modelValue >= wfmTimepicker.range.minute.min &&
-							  modelValue <= wfmTimepicker.range.minute.max;
-                } else {
-                    return true;
-                }
-            };
-
-            ngModel.$parsers.push(function (viewValue) {
-                if (viewValue != null) {
-                    var parsed = parseInt(viewValue);
-                    if (isFinite(parsed)) return parsed;
-                }
-                return null;
-            });
-
-            function changeByKeyPress(evt) {
-                wfmTimepicker.changeByKeyPress(evt, ngModel, wfmTimepicker.range.minute);
-            }
-
-            function changeByMouseWheel(evt) {
-                wfmTimepicker.changeByMouseWheel(evt, ngModel, wfmTimepicker.range.minute);
-            }
-
-            function onFocus(evt) {
-                elem.bind('keydown', changeByKeyPress);
-                elem.bind('mousewheel wheel', changeByMouseWheel);
-            }
-
-            function onBlur(evt) {
-                elem.unbind('keydown', changeByKeyPress);
-                elem.unbind('mousewheel wheel', changeByMouseWheel);
-            }
-        }
-    }
-
-    function timepickerTemplate() {
-        return '<input name="hourValueInput" type="text" ng-model="timeValue.hour" hour-input flex tabindex="1" />:<input name="minuteValueInput" type="text" ng-model="timeValue.minute" minute-input flex tabindex="1"/>' +
-		  '<select name="meridianValueInput" ng-model="timeValue.meridian" ng-if="useMeridian" flex ><option value="0">AM</md-option><option value="1">PM</option></select>';
-    }
-
+		if (/h/.test(timeFormat)) {
+			info.showMeridian = true;
+			info.am = moment.localeData().meridiem(9, 0);
+			info.pm = moment.localeData().meridiem(15, 0);
+		} else {
+			info.showMeridian = false;
+		}
+		return info;
+	}
 
 })();
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
@@ -0,0 +1,32 @@
+﻿(function() {
+	'use strict';
+
+	angular.module('ui.bootstrap.timepicker')
+		.run([
+			'$templateCache', function($templateCache) {
+				$templateCache.put("template/timepicker/timepicker.html",
+				
+					"<table>" +
+					"  <tbody>" +
+					"    <tr>" +
+					"      <td class=\"form-group\" ng-class=\"{'has-error': invalidHours}\">" +
+					"          <input type=\"text\" ng-model=\"hours\" ng-change=\"updateHours()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\"/>" +
+					"	" +
+					"      </td>" +
+					"      <td>:</td>" +
+					"      <td class=\"form-group\" ng-class=\"{'has-error': invalidMinutes}\">" +
+					"          <input type=\"text\" ng-model=\"minutes\" ng-change=\"updateMinutes()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\"/>" +
+					"      </td>" +
+					"      <td ng-show=\"showMeridian\">" +
+					"	<md-button ng-disabled=\"noToggleMeridian()\" ng-click=\"toggleMeridian()\">{{meridian}}</md-button>" +
+					"      </td>" +
+					"    </tr>" +
+					"  </tbody>" +
+					"</table>" 
+					
+				);
+
+			}
+		]);
+
+})();
