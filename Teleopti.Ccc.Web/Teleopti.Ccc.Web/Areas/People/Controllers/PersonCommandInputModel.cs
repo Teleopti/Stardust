c478064428ca34684b12e242ffb94002a9e86af6
Chundan Xu <chundan.xu@teleopti.com>
2015-09-10 11:01:33 +0200

    PBI #34138 fix build.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/People/Controllers/PersonCommandInputModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/People/Controllers/PersonCommandInputModel.cs
@@ -6,8 +6,6 @@ namespace Teleopti.Ccc.Web.Areas.People.Controllers
 	public class PersonCommandInputModel
 	{
 		public Guid PersonId { get; set; }
-		public IList<Guid> SkillIdList { get; set; }
-		public Guid? ShiftBagId { get; set; }
 	}
 	public class SkillUpdateCommandInputModel : PersonCommandInputModel
 	{
