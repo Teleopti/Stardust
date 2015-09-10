5216a9235775d354a705f982d3204b7b099df884
Roger Kratz <roger.kratz@teleopti.com>
2015-09-10 14:24:54 +0200

    #34699 Adding a (ignored) webscenario

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/MultiTenancy/MultiTenancyStepDefinition.cs
@@ -0,0 +1,21 @@
+﻿using TechTalk.SpecFlow;
+using Teleopti.Ccc.Infrastructure.MultiTenancy.Server;
+using Teleopti.Ccc.Infrastructure.MultiTenancy.Server.Queries;
+using Teleopti.Ccc.WebBehaviorTest.Data;
+
+namespace Teleopti.Ccc.WebBehaviorTest.MultiTenancy
+{
+	[Binding]
+	public class MultiTenancyStepDefinition
+	{
+		[Given(@"There is a tenant called '(.*)'")]
+		public void GivenThereIsATenantCalled(string tenantName)
+		{
+			TenantUnitOfWorkState.TenantUnitOfWorkAction(tenantSession =>
+			{
+				var tenant = new Tenant(tenantName);
+				new PersistTenant(tenantSession).Persist(tenant);
+			});
+		}
+	}
+}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/MultiTenancy/Tenants.feature
@@ -0,0 +1,19 @@
+﻿Feature: Tenants
+In order to host multiple customers on a single installation
+As a application owner
+I want to simpler life
+
+@Ignore
+Scenario: Log on to newly added scenario
+	Given There is a tenant called 'NewTenant'
+	And I have user credential with
+	| Field    | Value       |
+	| UserName | theUser     |
+	| Password | thePassword |
+	| Tenant   | NewTenant   |
+		When I try to sign in with
+	| Field    | Value       |
+	| UserName | theUser     |
+	| Password | thePassword |
+	Then I should be signed in
+
