e090983801a2300561546c6340d756e013c02a9e
Robin Karlsson <robin.karlsson@teleopti.com>
2015-09-14 11:04:52 +0200

    Adding dummy event invocators

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/BootstrapperResolveTest.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.ServiceBusTest/BootstrapperResolveTest.cs
@@ -73,15 +73,84 @@ namespace Teleopti.Ccc.Sdk.ServiceBusTest
 			public int ThreadCount { get; private set; }
 			public CurrentMessageInformation CurrentMessageInformation { get; private set; }
 			public event Action<CurrentMessageInformation> MessageSent;
+
+			protected virtual void OnMessageSent(CurrentMessageInformation obj)
+			{
+				var handler = MessageSent;
+				if (handler != null) handler(obj);
+			}
+
 			public event Func<CurrentMessageInformation, bool> AdministrativeMessageArrived;
+
+			protected virtual void OnAdministrativeMessageArrived(CurrentMessageInformation arg)
+			{
+				var handler = AdministrativeMessageArrived;
+				if (handler != null) handler(arg);
+			}
+
 			public event Func<CurrentMessageInformation, bool> MessageArrived;
+
+			protected virtual void OnMessageArrived(CurrentMessageInformation arg)
+			{
+				var handler = MessageArrived;
+				if (handler != null) handler(arg);
+			}
+
 			public event Action<CurrentMessageInformation, Exception> MessageSerializationException;
+
+			protected virtual void OnMessageSerializationException(CurrentMessageInformation arg1, Exception arg2)
+			{
+				var handler = MessageSerializationException;
+				if (handler != null) handler(arg1, arg2);
+			}
+
 			public event Action<CurrentMessageInformation, Exception> MessageProcessingFailure;
+
+			protected virtual void OnMessageProcessingFailure(CurrentMessageInformation arg1, Exception arg2)
+			{
+				var handler = MessageProcessingFailure;
+				if (handler != null) handler(arg1, arg2);
+			}
+
 			public event Action<CurrentMessageInformation, Exception> MessageProcessingCompleted;
+
+			protected virtual void OnMessageProcessingCompleted(CurrentMessageInformation arg1, Exception arg2)
+			{
+				var handler = MessageProcessingCompleted;
+				if (handler != null) handler(arg1, arg2);
+			}
+
 			public event Action<CurrentMessageInformation> BeforeMessageTransactionRollback;
+
+			protected virtual void OnBeforeMessageTransactionRollback(CurrentMessageInformation obj)
+			{
+				var handler = BeforeMessageTransactionRollback;
+				if (handler != null) handler(obj);
+			}
+
 			public event Action<CurrentMessageInformation> BeforeMessageTransactionCommit;
+
+			protected virtual void OnBeforeMessageTransactionCommit(CurrentMessageInformation obj)
+			{
+				var handler = BeforeMessageTransactionCommit;
+				if (handler != null) handler(obj);
+			}
+
 			public event Action<CurrentMessageInformation, Exception> AdministrativeMessageProcessingCompleted;
+
+			protected virtual void OnAdministrativeMessageProcessingCompleted(CurrentMessageInformation arg1, Exception arg2)
+			{
+				var handler = AdministrativeMessageProcessingCompleted;
+				if (handler != null) handler(arg1, arg2);
+			}
+
 			public event Action Started;
+
+			protected virtual void OnStarted()
+			{
+				var handler = Started;
+				if (handler != null) handler();
+			}
 		}
 
 		private class fakeSubscriptionStorage : ISubscriptionStorage
@@ -122,6 +191,12 @@ namespace Teleopti.Ccc.Sdk.ServiceBusTest
 			}
 
 			public event Action SubscriptionChanged;
+
+			protected virtual void OnSubscriptionChanged()
+			{
+				var handler = SubscriptionChanged;
+				if (handler != null) handler();
+			}
 		}
 	}
 }
