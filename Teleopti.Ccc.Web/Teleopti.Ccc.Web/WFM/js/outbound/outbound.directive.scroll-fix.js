c527c0a4ee90af6144d9f91aa1e621e76b369652
Li Xue <li.xue@teleopti.com>
2015-09-17 10:45:15 +0800

    #34259 scrollfix directive, fix gantt header to window when scroll past it

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/outbound.directive.scroll-fix.js
@@ -0,0 +1,45 @@
+﻿(function() {
+	'use strict';
+	/**
+	 * Add a 'scrollfix' class to the element when the page scrolls past it's position.Go crazy with this class.
+	 * pass a number to override the detected offset.
+	 * for exemple scrollfix="+10"
+	 */
+	angular.module('wfm.outbound').directive('scrollfix', [scrollfixCtrl]);
+
+	function scrollfixCtrl() {
+		return {
+			link: postLink
+		};
+
+		function postLink(scope, elm, attrs, scrollfixTarget) {
+			var top = elm[0].offsetTop,
+				$target = angular.element(document.querySelector('#materialcontainer'));
+			if (!attrs.scrollfix) {
+				attrs.scrollfix = top;
+			} else if (typeof (attrs.scrollfix) === 'string') {
+				if (attrs.scrollfix.charAt(0) === '-') {
+					attrs.scrollfix = top - parseFloat(attrs.scrollfix.substr(1));
+				} else if (attrs.scrollfix.charAt(0) === '+') {
+					attrs.scrollfix = top + parseFloat(attrs.scrollfix.substr(1));
+				}
+			}
+
+			function onScroll() {
+				var offset = $target[0].scrollTop;
+				if (!elm.hasClass('scrollfix') && offset > attrs.scrollfix) {
+					elm.addClass('scrollfix');
+				} else if (elm.hasClass('scrollfix') && offset <= attrs.scrollfix) {
+					elm.removeClass('scrollfix');
+				}
+			}
+
+			$target.on('scroll', onScroll);
+
+			scope.$on('$destroy', function() {
+				$target.off('scroll', onScroll);
+			});
+		}
+	}
+})();
+
