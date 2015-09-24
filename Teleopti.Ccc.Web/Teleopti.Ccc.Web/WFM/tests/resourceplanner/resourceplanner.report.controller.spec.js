16856dfcfc600eeca47904f3d6cbdef24eeb70eb
johand <johand@teleopti.com>
2015-09-24 14:20:19 +0200

    refactored tests

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/resourceplanner/resourceplanner.report.controller.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/resourceplanner/resourceplanner.report.controller.spec.js
@@ -12,57 +12,70 @@ describe('ResourceplannerReportCtrl', function () {
 		$httpBackend.expectGET("../api/Global/Language?lang=en").respond(200, 'mock');
 		$httpBackend.expectGET("../api/Global/User/CurrentUser").respond(200, 'mock');
 	}));
-    var mockStateParams ={result:{BusinessRulesValidationResults:[]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-14'}}]}]}};
+	var result = {};
+	result.withNoIssues = function(){
+		return {result:{BusinessRulesValidationResults:[]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-14'}}]}]}};
+	};
+	result.withIssues = function(){
+		return {result:{BusinessRulesValidationResults:[[]]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-14'}}]}]}};
+	};
+	result.withNoWeekends = function(){
+		return {result:{BusinessRulesValidationResults:[[]]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-12'}}]}]}};
+	};
+	result.withPeriodId = function(){
+		return {planningperiod:{id:0},result:{BusinessRulesValidationResults:[[]]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-12'}}]}]}}
+	};
 
-    it ('should only get the number of days until next monday',inject(function($controller){
-       var scope = $rootScope.$new();
-        var day = new Date(2015,10,10);
-        $controller('ResourceplannerReportCtrl', { $scope: scope,  $stateParams: mockStateParams});
-
-        var result = scope.getdaysuntilnextmonday(day);
-
-        expect(result).toEqual(2);
-    }));
-
-	it('no details are displayed when 0 issues', inject(function ($controller) {
+	it('should not detect any issues', inject(function ($controller) {
 		var scope = $rootScope.$new();
 
-		$controller('ResourceplannerReportCtrl', { $scope: scope,$stateParams: mockStateParams});
+		$controller('ResourceplannerReportCtrl', { $scope: scope,$stateParams: result.withNoIssues()});
 
 		expect(scope.issues.length).toEqual(0);
 		expect(scope.hasIssues).toEqual(false);
 	}));
-
-    it('details are displayed when issues', inject(function ($controller) {
+    it('should detect and process issues', inject(function ($controller) {
         var scope = $rootScope.$new();
-         var mockStateParams = {result:{BusinessRulesValidationResults:[[]]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-14'}}]}]}};
-        $controller('ResourceplannerReportCtrl', { $scope: scope,$stateParams: mockStateParams });
+        $controller('ResourceplannerReportCtrl', { $scope: scope,$stateParams: result.withIssues() });
 
         expect(scope.issues.length).toEqual(1);
         expect(scope.hasIssues).toEqual(true);
     }));
-    it('weekends are found', inject(function ($controller) {
+    it('should find weekends', inject(function ($controller) {
         var scope = $rootScope.$new();
-        $controller('ResourceplannerReportCtrl', { $scope: scope,$stateParams: mockStateParams });
+        $controller('ResourceplannerReportCtrl', { $scope: scope,$stateParams:  result.withNoIssues() });
 
         expect(scope.dayNodes[0].SkillDetails[0].weekend).toBe(true);
     }));
-    it('non-weekend are found and ignored', inject(function ($controller) {
+    it('should find and ignore weekends', inject(function ($controller) {
         var scope = $rootScope.$new();
-        var mockStateParams = {result:{BusinessRulesValidationResults:[[]]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-12'}}]}]}};
-
-        $controller('ResourceplannerReportCtrl', { $scope: scope, $stateParams: mockStateParams });
+        $controller('ResourceplannerReportCtrl', { $scope: scope, $stateParams: result.withNoWeekends() });
 
         expect(scope.dayNodes[0].SkillDetails[0].weekend).toBe(undefined);
     }));
-	it('should be possible to publish a schudule', inject(function ($controller) {
-		var scope = $rootScope.$new();
-		var mockStateParams = {planningperiod:{id:0},result:{BusinessRulesValidationResults:[[]]},interResult:{_skillResultList:[{SkillName:'test',SkillDetails:[{Date:{Date:'2015-11-12'}}]}]}};
+	it ('should  get the number of days until next monday',inject(function($controller){
+	   var scope = $rootScope.$new();
+	   var day = new Date(2015,10,10);
 
-		$controller('ResourceplannerReportCtrl', { $scope: scope, $stateParams: mockStateParams });
+		$controller('ResourceplannerReportCtrl', {$scope: scope, $stateParams: result.withNoIssues() });
+		var week = scope.getdaysuntilnextmonday(day);
+
+		expect(week).toEqual(2);
+	}));
+	it('should be possible to publish a schedule', inject(function ($controller) {
+		var scope = $rootScope.$new();
+		$controller('ResourceplannerReportCtrl', { $scope: scope, $stateParams: result.withPeriodId() });
 
 		scope.publishSchedule()
 		expect(scope.publishedClicked).toBe(true);
 	}));
+	it('should be possible to publish a schedule only once', inject(function ($controller) {
+		var scope = $rootScope.$new();
+		$controller('ResourceplannerReportCtrl', { $scope: scope, $stateParams: result.withPeriodId() });
+
+		scope.publishSchedule()
+		expect(scope.publishedClicked).toBe(true);
+	}));
+
 
 });
