e20cfb35f05cc2206794e90f475187e3b6c59ba7
Tamas Balog <tamas.balogh@teleopti.com>
2015-09-25 14:38:55 +0200

    add new sikulitest config file

---
+++ .debug-Setup/NewSikulitestConfig.bat
@@ -0,0 +1,49 @@
+@ECHO off
+SETLOCAL
+SET ROOTDIR=%~dp0
+SET ROOTDIR=%ROOTDIR:~0,-14%
+SET masterSettings=%ROOTDIR%\.debug-setup\config\settingsSikuliTest.txt
+SET CCC7DB=%~1
+SET AnalyticsDB=%~2
+SET configuration=%3
+SET MSBUILD="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+SET logFile=%ROOTDIR%\.debug-setup\SikuliConfig.log
+
+SET MySettings=%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\settings.txt
+
+::Build Teleopti.Support.Tool.exe if source files are available (they aren't in pipeline)
+if exist "%ROOTDIR%\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" %MSBUILD% /t:build "%ROOTDIR%\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" /p:Configuration=%configuration%
+
+::get a fresh Settings.txt
+COPY "%masterSettings%" "%MySettings%"
+ECHO. >> "%MySettings%"
+ECHO $(DB_CCC7)^|%CCC7DB%>>"%MySettings%"
+ECHO $(DB_ANALYTICS)^|%AnalyticsDB%>>"%MySettings%"
+ECHO $(AS_DATABASE)^|%AnalyticsDB%>>"%MySettings%"
+
+::telling what files to modify
+SET configFilesFolder=%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\ConfigFiles
+SET buildServerConfigFiles="%configFilesFolder%\BuildServerConfigFiles.txt"
+if not exist "%configFilesFolder%" mkdir "%configFilesFolder%"
+echo ..\..\..\Teleopti.Ccc.SmartClientPortal\Teleopti.Ccc.SmartClientPortal.Shell\bin\%configuration%\Teleopti.Ccc.SmartClientPortal.Shell.exe.config,BuildArtifacts\AppRaptor.config>%buildServerConfigFiles%
+
+::Run supportTool to replace all config
+"%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\Teleopti.Support.Tool.exe" -MOTest
+ECHO RC>"c:\nhib\Toggles.txt"
+
+ECHO Set WFM path in Tenant table ...
+SQLCMD -S. -E -d"%CCC7DB%" -i"%ROOTDIR%\.debug-Setup\database\tsql\SetPath.sql"
+
+ECHO fix config files ...
+cd %ROOTDIR%\.debug-setup\
+call FixMyConfig.bat %CCC7DB% %AnalyticsDB% %configuration%
+
+ECHO Restart iis express ...
+timeout 5
+taskkill /F /IM iisexpress.exe
+timeout 5
+cd "c:\Program Files\IIS Express\"
+start iisexpress /AppPool:Clr4IntegratedAppPool
+
+ENDLOCAL
+GOTO:EOF
