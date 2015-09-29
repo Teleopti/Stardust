95a4553a95e42195a1df86374fd8eff2b2c342de
Anders Forsberg <anders.forsberg@teleopti.com>
2015-09-29 19:15:08 +0200

    Fix Azure build

--- Database/TeleoptiCCC7/Releases/00000625.sql
+++ Database/TeleoptiCCC7/Releases/00000625.sql
@@ -1,14 +1,14 @@
 IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonAssignment]') AND name = N'IX_PersonAssignment_Scenario_UpdatedBy_UpdatedOn')
-DROP INDEX [IX_PersonAssignment_Scenario_UpdatedBy_UpdatedOn] ON [dbo].[PersonAssignment] WITH ( ONLINE = OFF )
+DROP INDEX [IX_PersonAssignment_Scenario_UpdatedBy_UpdatedOn] ON [dbo].[PersonAssignment]
 
 IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonAssignment]') AND name = N'IX_PersonAssignment_Date')
-DROP INDEX [IX_PersonAssignment_Date] ON [dbo].[PersonAssignment] WITH ( ONLINE = OFF )
+DROP INDEX [IX_PersonAssignment_Date] ON [dbo].[PersonAssignment]
 
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UQ_PersonAssignment_Date_Scenario_Person]') AND type_desc LIKE 'UNIQUE_CONSTRAINT')
 ALTER TABLE [dbo].[PersonAssignment] DROP CONSTRAINT [UQ_PersonAssignment_Date_Scenario_Person]
 
 IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonAssignment]') AND name = N'CIX_PersonAssignment_PersonDate_Scenario')
-DROP INDEX [CIX_PersonAssignment_PersonDate_Scenario] ON [dbo].[PersonAssignment] WITH ( ONLINE = OFF )
+DROP INDEX [CIX_PersonAssignment_PersonDate_Scenario] ON [dbo].[PersonAssignment]
 
 CREATE UNIQUE CLUSTERED INDEX [CIX_PersonAssignment_PersonDate_Scenario] ON [dbo].[PersonAssignment]
 (
