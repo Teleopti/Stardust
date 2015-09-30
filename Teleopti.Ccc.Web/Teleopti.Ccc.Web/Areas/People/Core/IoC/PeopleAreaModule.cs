76f5a684aa7bce29833ed04eeb91411439a1c3f6
Chundan Xu <chundan.xu@teleopti.com>
2015-09-30 02:50:50 +0200

    PBI #33665 small refact.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/People/Core/IoC/PeopleAreaModule.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/People/Core/IoC/PeopleAreaModule.cs
@@ -1,14 +1,7 @@
 ﻿using Autofac;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Teleopti.Ccc.Domain.Security.MultiTenancyAuthentication;
 using Teleopti.Ccc.Infrastructure.Aop;
-using Teleopti.Ccc.Infrastructure.MultiTenancy.Client;
 using Teleopti.Ccc.Web.Areas.People.Core.Persisters;
 using Teleopti.Ccc.Web.Areas.People.Core.Providers;
-using Teleopti.Ccc.Web.Areas.Search.Controllers;
 
 namespace Teleopti.Ccc.Web.Areas.People.Core.IoC
 {
@@ -16,11 +9,11 @@ namespace Teleopti.Ccc.Web.Areas.People.Core.IoC
 	{
 		protected override void Load(ContainerBuilder builder)
 		{
-			builder.RegisterType<PeopleSearchProvider>().As<IPeopleSearchProvider>();
+			builder.RegisterType<PeopleSearchProvider>().As<IPeopleSearchProvider>().SingleInstance();
 			builder.RegisterType<PeoplePersister>().As<IPeoplePersister>().SingleInstance().ApplyAspects();
-			builder.RegisterType<UserValidator>().As<IUserValidator>();
-			builder.RegisterType<PersonDataProvider>().As<IPersonDataProvider>();
-			builder.RegisterType<PersonInfoUpdater>().As<IPersonInfoUpdater>();
+			builder.RegisterType<UserValidator>().As<IUserValidator>().SingleInstance();
+			builder.RegisterType<PersonDataProvider>().As<IPersonDataProvider>().SingleInstance();
+			builder.RegisterType<PersonInfoUpdater>().As<IPersonInfoUpdater>().SingleInstance();
 		}
 	}
 }
