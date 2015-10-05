31091f722c9b846214e70e241a21a37bf3bdac93
Roger Kratz <roger.kratz@teleopti.com>
2015-10-05 16:31:24 +0200

    Removing more dead files

--- Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/FakeData/LogFactory.cs
+++
@@ -1,6 +0,0 @@
-namespace Teleopti.Analytics.Etl.CommonTest.FakeData
-{
-    public class LogFactory
-    {
-    }
-}
--- Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Constants/EventTopicNames.cs
+++
@@ -1,9 +0,0 @@
-namespace Teleopti.Ccc.SmartClientPortal.Shell.Constants
-{
-    /// <summary>
-    /// Constants for event topic names.
-    /// </summary>
-    public class EventTopicNames : Teleopti.Ccc.SmartClientPortal.Common.Constants.EventTopicNames
-    {
-    }
-}
