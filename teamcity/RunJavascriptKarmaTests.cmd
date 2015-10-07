2f43e2f5733f11716269f35f52c6df4f9e5dfd68
blandine descamps <blandine.descamps@teleopti.com>
2015-10-07 15:26:51 +0200

    fix rimraf

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -3,6 +3,6 @@
 call ..\.node\npm run continuous
 
 :: Clear all dev dependencies and only install what we need for production
-call ..\.node\npm -g install rimraf
-call rimraf node_modules
+call ..\.node\npm install rimraf
+call ..\.node\node node_modules\bin.js node_modules
 call ..\.node\npm install --production
