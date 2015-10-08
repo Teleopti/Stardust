0c0166de86353ee82dabe9b692d08f7989fa36b4
Yanyi Wan <none@none>
2015-10-08 11:57:26 +0800

    BUG 34987: interprete 00:00 as 00:00+1 for the end time in the time-range-picker.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/timerangepicker/wfm-time-range-picker.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/timerangepicker/wfm-time-range-picker.directive.js
@@ -37,9 +37,26 @@
 
 			$scope.nextDay = isOnDifferentDays();
 			$scope.isInvalid = isInvalid;
+			$scope.toggleNextDay = toggleNextDay;
+			$scope.nextDayToggleText = '+ 0';
 
 			this.setDateValue = setDateValue;
 			this.checkValidity = checkValidity;
+			this.setNextDayToggleText = setNextDayToggleText;
+
+			function toggleNextDay() {
+				if (!$scope.disableNextDay) {
+					$scope.nextDay = ! $scope.nextDay;
+				}
+			}
+
+			function setNextDayToggleText(nextDay) {
+				if (nextDay) {
+					$scope.nextDayToggleText = '+ 1';
+				} else {
+					$scope.nextDayToggleText = '+ 0';
+				}
+			}
 
 			function setDateValue(nextDay) {
 				if (!nextDay) {
@@ -91,6 +108,7 @@
 
 			scope.$watch('nextDay', function (newValue, oldValue) {
 				ctrl.setDateValue(newValue);
+				ctrl.setNextDayToggleText(newValue);
 			});
 
 			scope.$watch(function () {
@@ -100,7 +118,17 @@
 					e: scope.endTime ? $filter('date')(scope.endTime, 'HH:mm') : ''
 				}
 			}, function (newValue, oldValue) {
+				if (scope.disableNextDay) {
+					if (newValue.e == '00:00' ) {
+						scope.$evalAsync(function() { scope.nextDay = true; });
+						
+					} else if (scope.nextDay) {
+						scope.nextDay = false;
+					}					
+				}
+				
 				ctrl.checkValidity();
+								
 			}, true);
 
 			scope.showMessage = !angular.isDefined(attrs.hideMessage);
@@ -151,20 +179,15 @@
           "     <td> <i class=\"mdi mdi-minus\"> </i> </td> " +
           "     <td><timepicker-wrap ng-model=\"endTime\"></timepicker></td> " +
           "     <td> " +
-          "       <div class=\"wfm-switch\"  ng-hide=\"disableNextDay\" >" +
-          " 	<input type=\"checkbox\" id=\"NextDaySwitch\" ng-model=\"nextDay\"/> " +
-          " 	<label for=\"NextDaySwitch\"> " +
-          " 	  <span class=\"wfm-switch-label\">Next Day</span> " +
-          " 	  <span class=\"wfm-switch-toggle\"></span> " +
-          " 	</label> " +
+          "       <div class=\"next-day-toggle\"  ng-show=\"!disableNextDay || nextDay\" >" +
+          " 	      <button class=\"wfm-btn wfm-btn-invis-default\" ng-class=\"{'wfm-btn-invis-disabled': disableNextDay }\" ng-click=\"toggleNextDay()\">{{nextDayToggleText}}</button> " +
           "       </div>       " +
           "     </td> " +
           "   </tr> " +
           " </table> " +
-          "</div>" +
-          "<div class=\"error-msg-container ng-invalid-order alert-error notice-spacer\" ng-if=\"showMessage\"><i class='mdi mdi-alert-octagon'></i> <span translate>EndTimeMustBeGreaterOrEqualToStartTime</span></div> " +
-	  "<div class=\"error-msg-container ng-invalid-empty alert-error notice-spacer\" ng-if=\"showMessage\"><i class='mdi mdi-alert-octagon'></i> <span translate>StartTimeAndEndTimeMustBeSet</span></div>";
-
+          "</div>" +		
+          "<div class=\"error-msg-container ng-invalid-order alert-error notice-spacer\" ><i class='mdi mdi-alert-octagon'></i> <span translate>EndTimeMustBeGreaterOrEqualToStartTime</span></div> " +
+	      "<div class=\"error-msg-container ng-invalid-empty alert-error notice-spacer\" ><i class='mdi mdi-alert-octagon'></i> <span translate>StartTimeAndEndTimeMustBeSet</span></div>";        
 	}
 
 
