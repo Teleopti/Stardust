edae30b4d0b693e86fdb470b9dbb5ef9fee96166
micke <micke@teleopti.com>
2015-10-09 15:46:53 +0200

    Backed out changeset: 0a4058848102

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/ScheduleControllerPrerequisites.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/ScheduleControllerPrerequisites.cs
@@ -6,20 +6,17 @@ namespace Teleopti.Ccc.Web.Areas.ResourcePlanner
 	{
 		private readonly IActivityRepository _activityRepository;
 		private readonly IDayOffTemplateRepository _dayOffTemplateRepository;
-		private readonly IPartTimePercentageRepository _partTimePercentageRepository;
 
-		public ScheduleControllerPrerequisites(IActivityRepository activityRepository, IDayOffTemplateRepository dayOffTemplateRepository, IPartTimePercentageRepository partTimePercentageRepository)
+		public ScheduleControllerPrerequisites(IActivityRepository activityRepository, IDayOffTemplateRepository dayOffTemplateRepository)
 		{
 			_activityRepository = activityRepository;
 			_dayOffTemplateRepository = dayOffTemplateRepository;
-			_partTimePercentageRepository = partTimePercentageRepository;
 		}
 
 		public void MakeSureLoaded()
 		{
 			_activityRepository.LoadAll();
 			_dayOffTemplateRepository.LoadAll();
-			_partTimePercentageRepository.LoadAll();
 		}
 	}
 }
