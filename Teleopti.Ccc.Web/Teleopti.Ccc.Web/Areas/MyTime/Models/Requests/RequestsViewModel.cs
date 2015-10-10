65cd63fb5bcb09369143bd10cde9e230e4a58627
Xinfeng Li <xinfeng.li@teleopti.com>
2015-10-10 13:32:04 +0400

    Fix bug #34958: Shift Trade Posts displayed in wrong time zone in Requests View when for UK GMT Time Zone

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/Requests/RequestsViewModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/Requests/RequestsViewModel.cs
@@ -24,9 +24,9 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Models.Requests
 		public RequestType TypeEnum;
 		public string UpdatedOn;
 
-		public DateTime DateTimeFrom;
-		public DateTime DateTimeTo;
-		public DateTime? UpdatedOnDateTime;
+		public string DateTimeFrom;
+		public string DateTimeTo;
+		public string UpdatedOnDateTime;
 
 		public string Status;
 		public string Payload;
