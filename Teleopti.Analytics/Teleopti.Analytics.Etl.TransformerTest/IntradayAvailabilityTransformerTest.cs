03f7a73869451b918fe0e1bbce26fbca5b017385
Karin Jeppsson <karin.jeppsson@teleopti.com>
2015-10-12 09:49:06 +0200

    #35171:ETL Intraday may fails on stg_hourly_availability
    --HG--
    branch : Release 7.5.390

--- Teleopti.Analytics/Teleopti.Analytics.Etl.TransformerTest/IntradayAvailabilityTransformerTest.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.TransformerTest/IntradayAvailabilityTransformerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using NUnit.Framework;
 using Rhino.Mocks;
+using SharpTestsEx;
 using Teleopti.Analytics.Etl.Interfaces.Transformer;
 using Teleopti.Analytics.Etl.Transformer;
 using Teleopti.Analytics.Etl.TransformerInfrastructure.DataTableDefinition;
@@ -12,25 +13,23 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Analytics.Etl.TransformerTest
 {
-    [TestFixture]
+	[TestFixture]
 	public class IntradayAvailabilityTransformerTest
-    {
-        private IIntradayAvailabilityTransformer _target;
-	    private MockRepository _mocks;
-	    private IScenario _scenario;
-	    private ICommonStateHolder _stateHolder;
+	{
+		private IIntradayAvailabilityTransformer _target;
+		private IScenario _scenario;
+		private ICommonStateHolder _stateHolder;
 
-	    [SetUp]
-        public void Setup()
-        {
+		[SetUp]
+		public void Setup()
+		{
 			_target = new IntradayHourlyAvailabilityTransformer();
-	        _mocks = new MockRepository();
-		    _scenario = new Scenario("name");
+			_scenario = new Scenario("name");
 			_scenario.SetId(Guid.NewGuid());
-		    _stateHolder = _mocks.DynamicMock<ICommonStateHolder>();
-        }
+			_stateHolder = MockRepository.GenerateMock<ICommonStateHolder>();
+		}
 
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "studDay"), Test]
+		[Test]
 		public void ShouldTransformAvailability()
 		{
 			var person = new Person();
@@ -39,28 +38,45 @@ namespace Teleopti.Analytics.Etl.TransformerTest
 			scenario.SetId(Guid.NewGuid());
 			var table = new DataTable();
 			HourlyAvailabilityInfrastructure.AddColumnsToDataTable(table);
-			var schedDay = _mocks.DynamicMock<IScheduleDay>();
+			var schedDay = MockRepository.GenerateMock<IScheduleDay>();
 			var studRestriction = new StudentAvailabilityRestriction();
-			var studDay = new StudentAvailabilityDay(person, new DateOnly(2013, 5, 15), new List<IStudentAvailabilityRestriction> { studRestriction });
+			var date = new DateOnly(2013, 5, 15);
+			var studDay = new StudentAvailabilityDay(person, date, new List<IStudentAvailabilityRestriction> { studRestriction });
+			var schedDic = MockRepository.GenerateMock<IScheduleDictionary>();
+			var range = MockRepository.GenerateMock<IScheduleRange>();
+			var dic = new Dictionary<DateOnly, IScheduleDictionary> { { date, schedDic } };
 
-			var projService = _mocks.DynamicMock<IProjectionService>();
-			var layers = _mocks.DynamicMock<IVisualLayerCollection>();
+			var projService = MockRepository.GenerateMock<IProjectionService>();
+			var layers = MockRepository.GenerateMock<IVisualLayerCollection>();
 
+			_stateHolder.Stub(x => x.GetSchedules(Arg<HashSet<IStudentAvailabilityDay>>.Is.Anything, Arg<IScenario>.Is.Same(scenario))).Return(dic);
+			_stateHolder.Stub(x => x.PersonsWithIds(new List<Guid>())).IgnoreArguments().Return(new List<IPerson> { person });
+			schedDic.Stub(x => x.ContainsKey(person)).Return(true);
+			schedDic.Stub(x => x[person]).Return(range);
 
-			Expect.Call(_stateHolder.PersonsWithIds(new List<Guid>())).IgnoreArguments().Return(new List<IPerson> { person });
-			Expect.Call(_stateHolder.GetSchedulePartOnPersonAndDate(person, studDay.RestrictionDate, scenario)).Return(schedDay);
+			range.Stub(x => x.ScheduledDay(date)).Return(schedDay);
+			schedDay.Stub(x => x.Scenario).Return(scenario);
+			schedDay.Stub(x => x.Person).Return(person);
+			schedDay.Stub(x => x.IsScheduled()).Return(true);
+			schedDay.Stub(x => x.ProjectionService()).Return(projService);
+			projService.Stub(x => x.CreateProjection()).Return(layers);
+			layers.Stub(x => x.HasLayers).Return(true);
+			layers.Stub(x => x.WorkTime()).Return(TimeSpan.FromHours(5));
 
-			Expect.Call(schedDay.Scenario).Return(scenario);
-			Expect.Call(schedDay.Person).Return(person);
-			Expect.Call(schedDay.IsScheduled()).Return(true);
-			Expect.Call(schedDay.ProjectionService()).Return(projService);
-			Expect.Call(projService.CreateProjection()).Return(layers);
-			Expect.Call(layers.HasLayers).Return(true);
-			Expect.Call(layers.WorkTime()).Return(TimeSpan.FromHours(5));
-			_mocks.ReplayAll();
 			_target.Transform(new List<IStudentAvailabilityDay> { studDay }, table, _stateHolder, scenario);
 			Assert.That(table.Rows.Count, Is.EqualTo(1));
-			_mocks.VerifyAll();
-		}       
-    }
+		}
+
+		[Test]
+		public void ShouldOnlyUseUniqeDays()
+		{
+			var person = new Person();
+			person.SetId(Guid.NewGuid());
+
+			var studDay1 = new StudentAvailabilityDay(person, new DateOnly(2013, 5, 15), new List<IStudentAvailabilityRestriction>());
+			var studDay2 = new StudentAvailabilityDay(person, new DateOnly(2013, 5, 15), new List<IStudentAvailabilityRestriction>());
+			var uniqueDays = new HashSet<IStudentAvailabilityDay> { studDay1, studDay2 };
+			uniqueDays.Count.Should().Be.EqualTo(1);
+		}
+	}
 }
