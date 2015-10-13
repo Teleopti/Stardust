e37d84df5bb32b7d8a12dbdecc7c7ceca8b6bed1
Karin Jeppsson <karin.jeppsson@teleopti.com>
2015-10-13 10:30:17 +0200

    Backed out changeset: eb64a81878c6
    --HG--
    branch : Release 7.5.390

--- Teleopti.Analytics/Teleopti.Analytics.Etl.Interfaces/Transformer/ICommonStateHolder.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.Interfaces/Transformer/ICommonStateHolder.cs
@@ -31,8 +31,6 @@ namespace Teleopti.Analytics.Etl.Interfaces.Transformer
 	    IDictionary<DateTimePeriod, IScheduleDictionary> GetSchedules(IList<IScheduleChangedReadModel> changed,
 																	IScenario scenario);
 
-	    IDictionary<DateOnly, IScheduleDictionary> GetSchedules(HashSet<IStudentAvailabilityDay> days,
-		    IScenario scenario);
 	    IDictionary<DateTimePeriod, IScheduleDictionary> GetScheduleCashe();
 
 	    IList<IScheduleDay> GetSchedulePartPerPersonAndDate(
