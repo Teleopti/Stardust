efed83826936ddd22922df065c35ce4bebf913e5
tamasb <tamasb@teleopti.com>
2015-10-14 21:30:14 +0200

    make sikuli validator classes created by reflection form the name id

--- Win/Sikuli/SikuliValidatorFactory.cs
+++ Win/Sikuli/SikuliValidatorFactory.cs
@@ -1,5 +1,7 @@
-﻿using Teleopti.Ccc.Win.Sikuli.Validators.RootValidators;
-using Teleopti.Ccc.Win.Sikuli.Validators.SelftestValidators;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Teleopti.Ccc.Win.Sikuli.Validators.RootValidators;
 
 namespace Teleopti.Ccc.Win.Sikuli
 {
@@ -7,106 +9,121 @@ namespace Teleopti.Ccc.Win.Sikuli
 	{
 		internal static class Scheduler
 		{
+
+			private static object createInstanceByReflection(string className)
+			{
+				var assembly = Assembly.GetExecutingAssembly();
+
+				var type = assembly.GetTypes()
+					.First(t => t.Name == className);
+
+				return Activator.CreateInstance(type);
+			}
+
 			public static IRootValidator CreateValidator(string validatorName)
 			{
-				switch (validatorName)
-				{
-					case SikuliValidatorRegister.DeleteAll:
-						return new DeleteAllValidator();
+				var validatorClassName = validatorName + "Validator";
 
-					case SikuliValidatorRegister.OptimizeDaysOff:
-						return new OptimizeDaysOffValidator();
+				return createInstanceByReflection(validatorClassName) as IRootValidator;
 
-					case SikuliValidatorRegister.OptimizeBetweenDays:
-						return new OptimizeBetweenDaysValidator();
+				//switch (validatorName)
+				//{
+				//	case SikuliValidatorRegister.DeleteAll:
+				//		return new DeleteAllValidator();
 
-					case SikuliValidatorRegister.OptimizeWithinDays:
-						return new OptimizeWithinDaysValidator();
+				//	case SikuliValidatorRegister.OptimizeDaysOff:
+				//		return new OptimizeDaysOffValidator();
 
-					case SikuliValidatorRegister.OptimizeIntervalBalanceBefore:
-						return new OptimizeIntervalBalanceBeforeValidator();
+				//	case SikuliValidatorRegister.OptimizeBetweenDays:
+				//		return new OptimizeBetweenDaysValidator();
 
-					case SikuliValidatorRegister.OptimizeIntervalBalanceAfter:
-						return new OptimizeIntervalBalanceAfterValidator();
+				//	case SikuliValidatorRegister.OptimizeWithinDays:
+				//		return new OptimizeWithinDaysValidator();
 
-					case SikuliValidatorRegister.ScheduleAllOff:
-						return new ScheduleAllOptionsOffValidator();
+				//	case SikuliValidatorRegister.OptimizeIntervalBalanceBefore:
+				//		return new OptimizeIntervalBalanceBeforeValidator();
 
-					case SikuliValidatorRegister.ScheduleOvertimePeriod:
-						return new ScheduleOvertimePeriodValidator();
+				//	case SikuliValidatorRegister.OptimizeIntervalBalanceAfter:
+				//		return new OptimizeIntervalBalanceAfterValidator();
 
-					case SikuliValidatorRegister.ScheduleBlockSameShiftCategory :
-						return new ScheduleBlockSameShiftCategoryValidator();
+				//	case SikuliValidatorRegister.ScheduleAllOff:
+				//		return new ScheduleAllOptionsOffValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockSameShiftCategory:
-						return new OptimizeBlockSameShiftCategoryValidator();
+				//	case SikuliValidatorRegister.ScheduleOvertimePeriod:
+				//		return new ScheduleOvertimePeriodValidator();
 
-					case SikuliValidatorRegister.ScheduleBlockSameStartTime:
-						return new ScheduleBlockSameStartTimeValidator();
+				//	case SikuliValidatorRegister.ScheduleBlockSameShiftCategory :
+				//		return new ScheduleBlockSameShiftCategoryValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockSameStartTime:
-						return new OptimizeBlockSameStartTimeValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockSameShiftCategory:
+				//		return new OptimizeBlockSameShiftCategoryValidator();
 
-					case SikuliValidatorRegister.ScheduleBlockSameShift:
-						return new ScheduleBlockSameShiftValidator();
+				//	case SikuliValidatorRegister.ScheduleBlockSameStartTime:
+				//		return new ScheduleBlockSameStartTimeValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockSameShift:
-						return new OptimizeBlockSameShiftValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockSameStartTime:
+				//		return new OptimizeBlockSameStartTimeValidator();
 
-					case SikuliValidatorRegister.ScheduleTeamSameShiftCategory:
-						return new ScheduleTeamSameShiftCategoryValidator();
+				//	case SikuliValidatorRegister.ScheduleBlockSameShift:
+				//		return new ScheduleBlockSameShiftValidator();
 
-					case SikuliValidatorRegister.OptimizeTeamSameShiftCategory:
-						return new OptimizeTeamSameShiftCategoryValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockSameShift:
+				//		return new OptimizeBlockSameShiftValidator();
 
-					case SikuliValidatorRegister.ScheduleTeamSameStartTime:
-						return new ScheduleTeamSameStartTimeValidator();
+				//	case SikuliValidatorRegister.ScheduleTeamSameShiftCategory:
+				//		return new ScheduleTeamSameShiftCategoryValidator();
 
-					case SikuliValidatorRegister.OptimizeTeamSameStartTime:
-						return new OptimizeTeamSameStartTimeValidator();
+				//	case SikuliValidatorRegister.OptimizeTeamSameShiftCategory:
+				//		return new OptimizeTeamSameShiftCategoryValidator();
 
-					case SikuliValidatorRegister.ScheduleTeamSameEndTime:
-						return new ScheduleTeamSameEndTimeValidator();
+				//	case SikuliValidatorRegister.ScheduleTeamSameStartTime:
+				//		return new ScheduleTeamSameStartTimeValidator();
 
-					case SikuliValidatorRegister.OptimizeTeamSameEndTime:
-						return new OptimizeTeamSameEndTimeValidator();
+				//	case SikuliValidatorRegister.OptimizeTeamSameStartTime:
+				//		return new OptimizeTeamSameStartTimeValidator();
 
-					case SikuliValidatorRegister.ScheduleBlockTeamSameShiftCategory:
-						return new ScheduleBlockTeamSameShiftCategoryValidator();
+				//	case SikuliValidatorRegister.ScheduleTeamSameEndTime:
+				//		return new ScheduleTeamSameEndTimeValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockTeamSameShiftCategoryDaysOff:
-						return new OptimizeBlockTeamSameShiftCategoryDaysOffValidator();
+				//	case SikuliValidatorRegister.OptimizeTeamSameEndTime:
+				//		return new OptimizeTeamSameEndTimeValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockTeamSameShiftCategoryBetweenDays:
-						return new OptimizeBlockTeamSameShiftCategoryBetweenDaysValidator();
+				//	case SikuliValidatorRegister.ScheduleBlockTeamSameShiftCategory:
+				//		return new ScheduleBlockTeamSameShiftCategoryValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockTeamSameShiftCategoryWithinDays:
-						return new OptimizeBlockTeamSameShiftCategoryWithinDaysValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockTeamSameShiftCategoryDaysOff:
+				//		return new OptimizeBlockTeamSameShiftCategoryDaysOffValidator();
 
-					case SikuliValidatorRegister.ScheduleBlockTeamSameStartTime:
-						return new ScheduleBlockTeamSameStartTimeValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockTeamSameShiftCategoryBetweenDays:
+				//		return new OptimizeBlockTeamSameShiftCategoryBetweenDaysValidator();
 
-					case SikuliValidatorRegister.OptimizeBlockTeamSameStartTime:
-						return new OptimizeBlockTeamSameStartTimeValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockTeamSameShiftCategoryWithinDays:
+				//		return new OptimizeBlockTeamSameShiftCategoryWithinDaysValidator();
 
-					case SikuliValidatorRegister.MemoryUsage:
-						return new MemoryUsageValidator();
+				//	case SikuliValidatorRegister.ScheduleBlockTeamSameStartTime:
+				//		return new ScheduleBlockTeamSameStartTimeValidator();
 
-					case SikuliValidatorRegister.TestPass:
-						return new SelftestPassValidator();
+				//	case SikuliValidatorRegister.OptimizeBlockTeamSameStartTime:
+				//		return new OptimizeBlockTeamSameStartTimeValidator();
 
-					case SikuliValidatorRegister.TestWarn:
-						return new SelftestWarnValidator();
+				//	case SikuliValidatorRegister.MemoryUsage:
+				//		return new MemoryUsageValidator();
 
-					case SikuliValidatorRegister.TestFail:
-						return new SelftestFailValidator();
+				//	case SikuliValidatorRegister.TestPass:
+				//		return new SelftestPassValidator();
 
-					case SikuliValidatorRegister.TestRoot:
-						return new SelftestRootValidator();
+				//	case SikuliValidatorRegister.TestWarn:
+				//		return new SelftestWarnValidator();
 
-					default:
-						return null;
-				}
+				//	case SikuliValidatorRegister.TestFail:
+				//		return new SelftestFailValidator();
+
+				//	case SikuliValidatorRegister.TestRoot:
+				//		return new SelftestRootValidator();
+
+				//	default:
+				//		return null;
+			
 			}
 		}
 	}
