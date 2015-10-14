365fe4ac8028bde4285ec6a617e66a0bd8f4ddf7
Roger Kratz <roger.kratz@teleopti.com>
2015-10-14 11:19:13 +0200

    #34946 trying to impl "real" fake impl of scheduledicpersister

--- Teleopti.Ccc.TestCommon/FakeData/FakeScheduleDictionaryPersister.cs
+++ Teleopti.Ccc.TestCommon/FakeData/FakeScheduleDictionaryPersister.cs
@@ -1,15 +1,42 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.TestCommon.FakeData
 {
-	//TODO: fake this one or something "lower"?
+	//this thing might need some more hugs and knådning
 	public class FakeScheduleDictionaryPersister : IScheduleDictionaryPersister
 	{
+		private readonly IScheduleRepository _scheduleRepository;
+
+		public FakeScheduleDictionaryPersister(IScheduleRepository scheduleRepository)
+		{
+			_scheduleRepository = scheduleRepository;
+		}
+
 		public IEnumerable<PersistConflict> Persist(IScheduleDictionary scheduleDictionary)
 		{
+			var diffSvc = new DifferenceEntityCollectionService<IPersistableScheduleData>();
+			foreach (var scheduleRange in scheduleDictionary.Values)
+			{
+        var diff = scheduleRange.DifferenceSinceSnapshot(diffSvc);
+				foreach (var scheduleChange in diff)
+				{
+					switch (scheduleChange.Status)
+					{
+						case DifferenceStatus.Added:
+							_scheduleRepository.Add(scheduleChange.CurrentItem);
+							break;
+						case DifferenceStatus.Deleted:
+							_scheduleRepository.Remove(scheduleChange.OriginalItem);
+							break;
+						case DifferenceStatus.Modified:
+							break;
+					}
+				}
+			}
 			return Enumerable.Empty<PersistConflict>();
 		}
 	}
