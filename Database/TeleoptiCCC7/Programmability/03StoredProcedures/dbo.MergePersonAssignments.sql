aa1a80cea3c351030d4b9e106872096eb1489f3d
David Jonsson <davidj@teleopti.com>
2015-10-21 16:16:27 -0600

    bug #35191 - again. previous fix messed up the PA merge logic. Move the table design into the merge SP. Any future changes on dbo.PersonAssignment depends on Security.exe and string "-35191"

--- Database/TeleoptiCCC7/Programmability/03StoredProcedures/dbo.MergePersonAssignments.sql
+++ Database/TeleoptiCCC7/Programmability/03StoredProcedures/dbo.MergePersonAssignments.sql
@@ -11,12 +11,29 @@ GO
 -- =============================================
 -- yyyy-mm-dd	Mrx			Because
 -- =============================================
+--EXEC [dbo].[MergePersonAssignments]
 CREATE PROCEDURE [dbo].[MergePersonAssignments]
 AS
 
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[PersonAssignment]') AND name = N'UQ_PersonAssignment_Date_Scenario_Person')
-BEGIN
+--our marker
+DECLARE @bugNumber int
+SET @bugNumber = -35191
 
+--If "Done" then return
+IF EXISTS (SELECT 1 FROM dbo.databaseVersion WHERE BuildNumber=@bugNumber)
+RETURN 0
+
+--Check if Realease 625 has been applied. If so we know the PA merge is OK => The unique key from early versions of Release 000000625.sql did work.
+--but we never marked it as "Done!" at the time
+DECLARE @ClusteredKeyIsUnique bit
+SELECT @ClusteredKeyIsUnique=is_unique
+FROM sys.indexes
+WHERE object_id = OBJECT_ID(N'[dbo].[PersonAssignment]')
+AND type_desc = 'CLUSTERED'
+
+--if the Clustered key is not yet unique, apply merge and re-design the clustered key
+IF @ClusteredKeyIsUnique=0
+BEGIN
 	--temp tables
 	CREATE TABLE #mergeUs (KeepMe uniqueidentifier, MergeUs uniqueidentifier)
 	CREATE TABLE #PersonAssignment (Id uniqueidentifier not null, Person uniqueidentifier not null,[Date] datetime not null, Scenario uniqueidentifier not null, ShiftCategory uniqueidentifier,DayOffTemplate uniqueidentifier)
@@ -108,15 +125,86 @@ BEGIN
 	INNER JOIN Dubplicates d
 	ON sl.Id = d.Id;
 
-	--And finally add Unique constraint
-	ALTER TABLE [dbo].[PersonAssignment] ADD  CONSTRAINT [UQ_PersonAssignment_Date_Scenario_Person] UNIQUE NONCLUSTERED 
+	--re-design the table
+	ALTER TABLE [dbo].[PersonAssignment] DROP CONSTRAINT [FK_PersonAssignment_DayOffTemplate]
+	ALTER TABLE [dbo].[PersonAssignment] DROP CONSTRAINT [FK_PersonAssignment_Person]
+	ALTER TABLE [dbo].[PersonAssignment] DROP CONSTRAINT [FK_PersonAssignment_Person_UpdatedBy]
+	ALTER TABLE [dbo].[PersonAssignment] DROP CONSTRAINT [FK_PersonAssignment_Scenario]
+	ALTER TABLE [dbo].[PersonAssignment] DROP CONSTRAINT [FK_PersonAssignment_ShiftCategory]
+	ALTER TABLE [dbo].[ShiftLayer] DROP CONSTRAINT [FK_ShiftLayer_PersonAssignment]
+	--GO
+
+	--create new table and clustered key
+	CREATE TABLE [dbo].[PersonAssignment_new](
+		[Id] [uniqueidentifier] NOT NULL,
+		[Version] [int] NOT NULL,
+		[UpdatedBy] [uniqueidentifier] NOT NULL,
+		[UpdatedOn] [datetime] NOT NULL,
+		[Person] [uniqueidentifier] NOT NULL,
+		[Scenario] [uniqueidentifier] NOT NULL,
+		[Date] [datetime] NOT NULL,
+		[ShiftCategory] [uniqueidentifier] NULL,
+		[DayOffTemplate] [uniqueidentifier] NULL,
+	 CONSTRAINT [PK_PersonAssignment_new] PRIMARY KEY NONCLUSTERED 
 	(
-		[Date] ASC,
-		[Scenario] ASC,
-		[Person] ASC
+		[Id] ASC
+	)
 	)
 
+	CREATE UNIQUE CLUSTERED INDEX [CIX_PersonAssignment_PersonDate_Scenario] ON [dbo].[PersonAssignment_new]
+	(
+					[Person] ASC,
+					[Date] ASC,
+					[Scenario] ASC
+	)
+
+	--get data into new table
+	INSERT INTO [dbo].[PersonAssignment_new]
+	SELECT  Id, Version, UpdatedBy, UpdatedOn, Person, Scenario, Date, ShiftCategory, DayOffTemplate
+	FROM [dbo].[PersonAssignment]
+
+	--rename tables
+	EXEC dbo.sp_rename @objname = N'[dbo].[PersonAssignment]', @newname = N'PersonAssignment_old', @objtype = N'OBJECT'
+	EXEC sp_rename N'[dbo].[PersonAssignment_old].[PK_PersonAssignment]', N'PK_PersonAssignment_old', N'INDEX'
+	EXEC dbo.sp_rename @objname = N'[dbo].[PersonAssignment_new]', @newname = N'PersonAssignment', @objtype = N'OBJECT'
+	EXEC sp_rename N'[dbo].[PersonAssignment].[PK_PersonAssignment_new]', N'PK_PersonAssignment', N'INDEX'
+
+	--re-create FKs
+	ALTER TABLE [dbo].[PersonAssignment]  WITH CHECK ADD  CONSTRAINT [FK_PersonAssignment_DayOffTemplate] FOREIGN KEY([DayOffTemplate])
+	REFERENCES [dbo].[DayOffTemplate] ([Id])
+	ALTER TABLE [dbo].[PersonAssignment]  CHECK CONSTRAINT [FK_PersonAssignment_DayOffTemplate]
+
+	ALTER TABLE [dbo].[PersonAssignment]  WITH CHECK ADD  CONSTRAINT [FK_PersonAssignment_Person] FOREIGN KEY([Person])
+	REFERENCES [dbo].[Person] ([Id])
+	ALTER TABLE [dbo].[PersonAssignment]  CHECK CONSTRAINT [FK_PersonAssignment_Person]
+
+	ALTER TABLE [dbo].[PersonAssignment]  WITH CHECK ADD  CONSTRAINT [FK_PersonAssignment_Person_UpdatedBy] FOREIGN KEY([UpdatedBy])
+	REFERENCES [dbo].[Person] ([Id])
+	ALTER TABLE [dbo].[PersonAssignment]  CHECK CONSTRAINT [FK_PersonAssignment_Person_UpdatedBy]
+
+	ALTER TABLE [dbo].[PersonAssignment]  WITH CHECK ADD  CONSTRAINT [FK_PersonAssignment_Scenario] FOREIGN KEY([Scenario])
+	REFERENCES [dbo].[Scenario] ([Id])
+	ALTER TABLE [dbo].[PersonAssignment]  CHECK CONSTRAINT [FK_PersonAssignment_Scenario]
+
+	ALTER TABLE [dbo].[PersonAssignment]  WITH CHECK ADD  CONSTRAINT [FK_PersonAssignment_ShiftCategory] FOREIGN KEY([ShiftCategory])
+	REFERENCES [dbo].[ShiftCategory] ([Id])
+	ALTER TABLE [dbo].[PersonAssignment]  CHECK CONSTRAINT [FK_PersonAssignment_ShiftCategory]
+
+	ALTER TABLE [dbo].[ShiftLayer]  WITH CHECK ADD  CONSTRAINT [FK_ShiftLayer_PersonAssignment] FOREIGN KEY([Parent])
+	REFERENCES [dbo].[PersonAssignment] ([Id])
+	ON DELETE CASCADE
+	ALTER TABLE [dbo].[ShiftLayer] CHECK CONSTRAINT [FK_ShiftLayer_PersonAssignment]
+
+	DECLARE @SQLString nvarchar(500)
+	SET @SQLString=N'DROP TABLE [dbo].[PersonAssignment_old]'
+	EXECUTE sp_executesql @SQLString
+
 END
+
+--Mark as "done!"
+INSERT INTO dbo.databaseVersion (BuildNumber,SystemVersion)
+SELECT @bugNumber,'Merge of PA'
+
 
 GO
 
