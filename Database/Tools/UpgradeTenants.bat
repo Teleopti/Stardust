654d9b920a8535bfd275b40d4334cb5ab0f36af5
Ola <ola@teleopti.com>
2015-10-26 11:16:11 +0100

    move the upgrade of tenants to powershell completely

--- Database/Tools/UpgradeTenants.bat
+++ Database/Tools/UpgradeTenants.bat
@@ -3,51 +3,13 @@
 @ECHO off
 
 SET INTEGRATED=%~1
-SET SUSER=%~2
-SET SPASS=%~3
-SET URL=%~4
-SET WinAuth=0
+SET SERVER=%~2
+SET DB=%~3 
+SET SUSER=%~4
+SET SPASS=%~5
 
-IF  "%INTEGRATED%"=="" (
-	GOTO :WinAuth
-)
-
-:Auth
-IF  "%INTEGRATED%"=="1" (
-	SET WinAuth=1
-	GOTO :Url
-)
-:User
-IF  "%SUSER%"=="" (
-SET /P SUSER=SQL Admin user:
-)
-
-IF "%SPASS%"=="" (
-SET /P SPASS=SQL Admin password:
-)
-:Url
-IF "%URL%"=="" (
-SET /P URL=Url to admin site:
-) 
-::ECHO "%Kommandompigg%"
-::ECHO "%WinAuth%"
-::ECHO "%SUSER%"
-::ECHO "%SPASS%"
-::ECHO "%URL%"
-::PAUSE
 
 set WORKING_DIRECTORY=%~dp0
 SET Kommandompigg=%WORKING_DIRECTORY%UpgradeTenants.ps1
-powershell.exe -ExecutionPolicy Bypass -File "%Kommandompigg%" "%WinAuth%" "%SUSER%" "%SPASS%" "%URL%"
+powershell.exe -ExecutionPolicy Bypass -File "%Kommandompigg%" "%INTEGRATED%" "%SERVER%" "%DB%" "%SUSER%" "%SPASS%"
 ::PAUSE
-GOTO :end
-
-:WinAuth
-SET INTEGRATED=0
-CHOICE /C yn /M "Do you want to use WinAuth?"
-IF %ERRORLEVEL% EQU 1 (
-SET INTEGRATED=1
-)
-GOTO :Auth
-
-:end
--- Database/Tools/UpgradeTenants.ps1
+++ Database/Tools/UpgradeTenants.ps1
@@ -4,19 +4,129 @@ if($args[0] -eq "1")
 {
     $integrated = "true"
 }
-
-$user=$args[1]
-$pwd=$args[2]
-$url=$args[3]
-$url= $url + "UpgradeAllTenants"
-
-$body = @{
-    "Tenant" = ""
-    "AdminUserName" = $user
-    "AdminPassword" = $pwd
-    "UseIntegratedSecurity" = $integrated
+else
+{
+    $PATCHUSER=$args[3]
+    $PATCHPWD=$args[4]
 }
+$SQLServer=$args[1]
+$CCC7DB=$args[2]
+
+
+#log-info "Get databases to patch"
+     # Create SqlConnection object and define connection string
+    $con = New-Object System.Data.SqlClient.SqlConnection
+    $con.ConnectionString = "Server=$SQLServer;Database=$CCC7DB;User ID=$PATCHUSER;Password=$PATCHPWD;Integrated Security=$integrated"
+    $con.open()
+   # log-info "opened " $con.ConnectionString 
+
+    # Create SqlCommand object, define command text, and set the connection
+    $cmd = New-Object System.Data.SqlClient.SqlCommand
+    $cmd.CommandText = "SELECT ApplicationConnectionString, AnalyticsConnectionString, AggregationConnectionString FROM Tenant.Tenant"
+    $cmd.Connection = $con
+
+    # Create SqlDataReader
+    $dr = $cmd.ExecuteReader()
+    
+    $SECSQLServer="-DS"+$SQLServer
+    if($integrated  -eq "true")
+    {
+        $SECPATCHUSER = "-EE"
+    }
+    else
+    {    
+        $SECPATCHUSER = "-DU"+$PATCHUSER
+        $SECPATCHPWD= "-DP"+$PATCHPWD
+    }
+
+    $SQLServer="-S"+$SQLServer
+    if($integrated  -eq "true")
+    {
+        $PATCHUSER = "-E"
+    }
+    else
+    {
+        $PATCHUSER = "-U"+$PATCHUSER
+        $PATCHPWD= "-P"+$PATCHPWD
+    }
+    
+
+    If ($dr.HasRows)
+    {
+        #log-info "Found databases"
+      While ($dr.Read())
+      {
+        $array = ($dr["ApplicationConnectionString"] -split ";")
+        for ($i=0; $i -lt $array.length; $i++) {
+	        if ($array[$i] -like '*Initial Catalog*') { 
+                $cat = ($array[$i] -split "=")
+                $appDb = $cat[1]
+            }
+            if ($array[$i] -like '*User ID*') { 
+                $cat = ($array[$i] -split "=")
+                $SQLUser = $cat[1]
+            }
+            if ($array[$i] -like '*Password*') { 
+                $cat = ($array[$i] -split "=")
+                $SQLPwd = $cat[1]
+            }
+        }
+        $array = ($dr["AnalyticsConnectionString"] -split ";")
+        for ($i=0; $i -lt $array.length; $i++) {
+	        if ($array[$i] -like '*Initial Catalog*') { 
+                $cat = ($array[$i] -split "=")
+                $analDb = $cat[1]
+            }
+        }
+        $array = ($dr["AggregationConnectionString"] -split ";")
+        for ($i=0; $i -lt $array.length; $i++) {
+	        if ($array[$i] -like '*Initial Catalog*') { 
+                $cat = ($array[$i] -split "=")
+                $aggDb = $cat[1]
+            }
+        }
+        if($appDb.Trim() -ne $CCC7DB.Trim()) #don't patch the first database again
+        {
+            $SECappDb = "-AP"+$appDb
+            $SECanalDb =  "-AN"+$analDb
+            $SECloggDb =  "-CD"+$aggDb
+            $colon = ":"
+            $SQLUserPwd = "-L$SQLUser$colon$SQLPwd"
+            if($SQLUserPwd -eq "-L:")
+            {
+                $SQLUserPwd = "-W" + [Environment]::UserDomainName + "\" + [Environment]::UserName
+            }
+            $CONNSTRINGBASE="Data Source=$SQLServer;User Id=$SQLUser;Password=$SQLPwd"
+
+            #log-info "Patch databases: $appDb $analDb."
+
+            $appDb = "-D"+$appDb
+            $analDb =  "-D"+$analDb
+            $aggDb =  "-D"+$aggDb
+        
+            $command = $PSScriptRoot + "\..\DBManager.exe"
+        
+            &"$command" $SQLServer $appDb "-OTeleoptiCCC7" $PATCHUSER $PATCHPWD "-T" "-R" $SQLUserPwd
+            &"$command" $SQLServer $analDb "-OTeleoptiAnalytics" $PATCHUSER $PATCHPWD "-T" "-R" $SQLUserPwd
+            &"$command" $SQLServer $aggDb "-OTeleoptiCCCAgg" $PATCHUSER $PATCHPWD "-T" "-R" $SQLUserPwd
+        
+            $command = $PSScriptRoot + "\..\Enrypted\Teleopti.Support.Security.exe"
+            &"$command" $SECSQLServer $SECappDb $SECanalDb $SECloggDb $CONNSTRINGBASE $SECPATCHUSER $SECPATCHPWD
+        }
+        
+      }
+    }
+# Close the data reader and the connection
+$dr.Close()
+$con.Close()
+
+#$body = @{
+#    "Tenant" = ""
+#    "AdminUserName" = $user
+#    "AdminPassword" = $pwd
+#    "UseIntegratedSecurity" = $integrated
+#}
 
 #write $integrated
-Invoke-RestMethod -Method Post $url -Body ($body) -timeout 300
+#Invoke-RestMethod -Method Post $url -Body ($body) -timeout 300
 
