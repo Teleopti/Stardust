b8cb23084fdabe48724d845ef57f553afde220d2
Felix <none@none>
2015-10-27 16:51:28 +0100

    WIP - Create HTML page to display result pt 2 - #35453

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.controller.js
@@ -4,7 +4,19 @@
 		.controller('IntradayCtrl', [
 		'$scope', '$state', 'IntradayService',
 		function ($scope, $state, IntradayService) {
+			//console.log(IntradayService.skillList);
+			$scope.skillList = IntradayService.skillList.query();
+			$scope.selectedSkill = $scope.skillList[0];
 
-            }
-        ]);
+			
+			$scope.skillChange = function(skill) {
+				$scope.name = skill.Measures[0].Name;
+				$scope.value = skill.Measures[0].Value;
+				$scope.stringValue = skill.Measures[0].StringValue;
+			}
+			//$scope.skillChange($scope.skillList[0]);
+
+
+		}
+	]);
 })()
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.service.js
@@ -4,35 +4,36 @@
 	var intradayService = angular.module('wfm.intraday');
 	intradayService.service('IntradayService', [
 		'$resource', function ($resource) {
-			console.log("intraday service test");
-			var intradayService = {};
 
-			intradayService.skillList = [
-				{
-					"SkillName": "McLaren Honda Support",
-					"Severity": "5",
-					"Measures": [
-				{
-					"Name": "Calls",
-					"Value": "3506",
-					"StringValue": "It is not looking good."
-				}
-					]
-				},
-				{
-					"SkillName": "Williams Martini Racing Support",
-					"Severity": "1",
-					"Measures": [
-				{
-					"Name": "Calls",
-					"Value": "15",
-					"StringValue": "It is looking okay."
-				}
-					]
-				}
-			];
+			this.skillList = $resource('../api/intraday/skillstatus', {}, {
+				query: { method: 'GET', params: {}, isArray: true }
+			});
 
-			return intradayService;
+
+			//intradayService.skillList = [
+			//	{
+			//		"SkillName": "McLaren Honda Support",
+			//		"Severity": "5",
+			//		"Measures": [
+			//	{
+			//		"Name": "Calls",
+			//		"Value": "3506",
+			//		"StringValue": "It is not looking good."
+			//	}
+			//		]
+			//	},
+			//	{
+			//		"SkillName": "Williams Martini Racing Support",
+			//		"Severity": "1",
+			//		"Measures": [
+			//	{
+			//		"Name": "Calls",
+			//		"Value": "15",
+			//		"StringValue": "It is looking okay."
+			//	}
+			//		]
+			//	}
+			//];
 		}
 	]);
 })();
