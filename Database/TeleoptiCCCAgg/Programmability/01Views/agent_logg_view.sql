394a6aab34c22ac9822672bbf0a2f9cd040c3a13
Maria Stein <none@none>
2015-10-29 11:40:46 +0100

    use default collate for agent logg view

--- Database/TeleoptiCCCAgg/Programmability/01Views/agent_logg_view.sql
+++ Database/TeleoptiCCCAgg/Programmability/01Views/agent_logg_view.sql
@@ -4,11 +4,11 @@ DROP VIEW [dbo].[agent_logg_view]
 GO
 CREATE VIEW [dbo].[agent_logg_view]
 AS
-SELECT     queue, date_from, interval, agent_id, agent_name, avail_dur, tot_work_dur, talking_call_dur, pause_dur, wait_dur, wrap_up_dur, answ_call_cnt, 
+SELECT     queue, date_from, interval, agent_id, agent_name COLLATE database_default as agent_name, avail_dur, tot_work_dur, talking_call_dur, pause_dur, wait_dur, wrap_up_dur, answ_call_cnt, 
                       direct_out_call_cnt, direct_out_call_dur, direct_in_call_cnt, direct_in_call_dur, transfer_out_call_cnt, admin_dur
 FROM         dbo.agent_logg_intraday WITH (NOLOCK)
 UNION
-SELECT     queue, date_from, interval, agent_id, agent_name, avail_dur, tot_work_dur, talking_call_dur, pause_dur, wait_dur, wrap_up_dur, answ_call_cnt, 
+SELECT     queue, date_from, interval, agent_id, agent_name COLLATE database_default as agent_name, avail_dur, tot_work_dur, talking_call_dur, pause_dur, wait_dur, wrap_up_dur, answ_call_cnt, 
                       direct_out_call_cnt, direct_out_call_dur, direct_in_call_cnt, direct_in_call_dur, transfer_out_call_cnt, admin_dur
 FROM         dbo.agent_logg WITH (NOLOCK)
 GO
