8695449f766b123966983b1879b3ba506b21aa3e
Li Xue <li.xue@teleopti.com>
2015-11-04 15:19:07 +0800

    #33041 add a empty view

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/CiscoWidgetController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Teleopti.Ccc.UserTexts;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.LayoutBase;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
+{
+	public class CiscoWidgetController : Controller
+	{
+		private readonly ILayoutBaseViewModelFactory _layoutBaseViewModelFactory;
+		public CiscoWidgetController(ILayoutBaseViewModelFactory layoutBaseViewModelFactory)
+		{
+			_layoutBaseViewModelFactory = layoutBaseViewModelFactory;
+		}
+
+		public ViewResult Index()
+		{
+			var layoutViewModel = _layoutBaseViewModelFactory.CreateLayoutBaseViewModel(Resources.AgentScheduleMessenger);
+			layoutViewModel.CultureSpecific.Rtl = false; //for now - asm is always displayed "western style" for now
+			ViewBag.LayoutBase = layoutViewModel;
+			return View();
+		}
+	}
+}
