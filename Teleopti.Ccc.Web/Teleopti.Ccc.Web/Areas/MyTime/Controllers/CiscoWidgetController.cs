8f76ca728894e0d42396bf816f9140ed6e306f97
Li Xue <li.xue@teleopti.com>
2015-11-04 15:39:18 +0800

    #33401 add logo and insert ASM

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/CiscoWidgetController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/CiscoWidgetController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Teleopti.Ccc.UserTexts;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Asm.ViewModelFactory;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.LayoutBase;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
@@ -11,9 +12,11 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 	public class CiscoWidgetController : Controller
 	{
 		private readonly ILayoutBaseViewModelFactory _layoutBaseViewModelFactory;
-		public CiscoWidgetController(ILayoutBaseViewModelFactory layoutBaseViewModelFactory)
+		private readonly IAsmViewModelFactory _asmModelFactory;
+		public CiscoWidgetController(ILayoutBaseViewModelFactory layoutBaseViewModelFactory, IAsmViewModelFactory asmModelFactory)
 		{
 			_layoutBaseViewModelFactory = layoutBaseViewModelFactory;
+			_asmModelFactory = asmModelFactory;
 		}
 
 		public ViewResult Index()
@@ -21,6 +24,8 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 			var layoutViewModel = _layoutBaseViewModelFactory.CreateLayoutBaseViewModel(Resources.AgentScheduleMessenger);
 			layoutViewModel.CultureSpecific.Rtl = false; //for now - asm is always displayed "western style" for now
 			ViewBag.LayoutBase = layoutViewModel;
+
+			ViewBag.HasAsmPermission = _asmModelFactory.HasAsmPermission();
 			return View();
 		}
 	}
