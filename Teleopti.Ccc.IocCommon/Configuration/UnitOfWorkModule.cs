e8b85470fb6e8311595fccff5f9b4d4a962ab4a0
Xinfeng Li <xinfeng.li@teleopti.com>
2015-11-06 18:33:15 +0800

    Remove duplicate Autofac registration to fix bug #35366: Schedules does not show all absences added in Teleopti WFM MyTeam

--- Teleopti.Ccc.IocCommon/Configuration/UnitOfWorkModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/UnitOfWorkModule.cs
@@ -35,14 +35,11 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 
 			builder.RegisterType<WithUnitOfWork>().SingleInstance();
 
-			builder.RegisterType<CurrentPersistCallbacks>()
-				.As<ICurrentPersistCallbacks>()
-				.SingleInstance();
-
 			builder.RegisterType<MessageSenderCreator>().SingleInstance();
 			builder.Register(c => c.Resolve<MessageSenderCreator>().Create())
-				.As<IMessageSendersScope>()
-				.SingleInstance();
+			 .As<ICurrentPersistCallbacks>()
+			 .As<IMessageSendersScope>()
+			 .SingleInstance();
 
 			builder.RegisterType<EventsMessageSender>().As<IPersistCallback>().SingleInstance();
 			if (_configuration.Toggle(Toggles.MessageBroker_SchedulingScreenMailbox_32733))
