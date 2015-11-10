041f78bc968af0ef84f15164b393e10b14bd340e
Maria Stein <none@none>
2015-11-10 14:28:19 +0100

    #35383 - persist override tasks using pattern when 0 calls

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Forecasting/Controllers/OverrideTasksPersister.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Forecasting/Controllers/OverrideTasksPersister.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using Teleopti.Ccc.Domain.Forecasting.Angel;
 using Teleopti.Ccc.Domain.Forecasting.Angel.Future;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
@@ -9,11 +13,15 @@ namespace Teleopti.Ccc.Web.Areas.Forecasting.Controllers
 	{
 		private readonly ISkillDayRepository _skillDayRepository;
 		private readonly IFutureData _futureData;
+		private readonly IIntradayForecaster _intradayForecaster;
+		private readonly IHistoricalPeriodProvider _historicalPeriodProvider;
 
-		public OverrideTasksPersister(ISkillDayRepository skillDayRepository, IFutureData futureData)
+		public OverrideTasksPersister(ISkillDayRepository skillDayRepository, IFutureData futureData, IIntradayForecaster intradayForecaster, IHistoricalPeriodProvider historicalPeriodProvider)
 		{
 			_skillDayRepository = skillDayRepository;
 			_futureData = futureData;
+			_intradayForecaster = intradayForecaster;
+			_historicalPeriodProvider = historicalPeriodProvider;
 		}
 
 		public void Persist(IScenario scenario, IWorkload workload, ModifiedDay[] days, double overrideTasks)
@@ -22,11 +30,28 @@ namespace Teleopti.Ccc.Web.Areas.Forecasting.Controllers
 			var max = days.Max(x => x.Date);
 			var futurePeriod = new DateOnlyPeriod(new DateOnly(min), new DateOnly(max));
 			var skillDays = _skillDayRepository.FindRange(futurePeriod, workload.Skill, scenario);
-			var workloadDays = _futureData.Fetch(workload, skillDays, futurePeriod);
+			var workloadDays = _futureData.Fetch(workload, skillDays, futurePeriod).ToList();
+			IDictionary<DayOfWeek, IEnumerable<ITemplateTaskPeriod>> intradayPattern = new Dictionary<DayOfWeek, IEnumerable<ITemplateTaskPeriod>>();
+
+			var allDaysHaveTasks = workloadDays.All(x => Math.Abs(x.Tasks) > 0);
+			if (!allDaysHaveTasks)
+			{
+				var statsPeriod = _historicalPeriodProvider.AvailableIntradayTemplatePeriod(workload);
+				if (statsPeriod.HasValue)
+					intradayPattern = _intradayForecaster.CalculatePattern(workload, statsPeriod.Value);
+			}
 
 			foreach (var workloadDay in days.Select(day => workloadDays.Single(x => x.CurrentDate == new DateOnly(day.Date))).Where(workloadDay => workloadDay.OpenForWork.IsOpen))
 			{
-				workloadDay.SetOverrideTasks(overrideTasks, null); 
+				IEnumerable<ITemplateTaskPeriod> weekdayPattern;
+				if (workloadDay.Tasks > 0d || !intradayPattern.TryGetValue(workloadDay.CurrentDate.DayOfWeek, out weekdayPattern))
+				{
+					workloadDay.SetOverrideTasks(overrideTasks, null); 	
+				}
+				else
+				{
+					workloadDay.SetOverrideTasks(overrideTasks, weekdayPattern); 
+				}
 			}
 		}
 	}
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Forecasting/Controllers/OverrideTasksPersisterTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/Forecasting/Controllers/OverrideTasksPersisterTest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Rhino.Mocks;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.Forecasting.Angel;
 using Teleopti.Ccc.Domain.Forecasting.Angel.Future;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.TestCommon.FakeData;
@@ -15,7 +17,7 @@ namespace Teleopti.Ccc.WebTest.Areas.Forecasting.Controllers
 	public class OverrideTasksPersisterTest
 	{
 		[Test]
-		public void ShouldPersist()
+		public void ShouldPersistADayThatHasCalls()
 		{
 			var workload = WorkloadFactory.CreateWorkload(SkillFactory.CreateSkill("skill1"));
 			var scenario = new Scenario("scenario1");
@@ -26,23 +28,100 @@ namespace Teleopti.Ccc.WebTest.Areas.Forecasting.Controllers
 			var skillDays = new[] { new SkillDay() };
 			var futurePeriod = new DateOnlyPeriod(dateOnly, dateOnly);
 			var skillDayRepository = MockRepository.GenerateMock<ISkillDayRepository>();
-			skillDayRepository.Stub(
-				x => x.FindRange(futurePeriod, workload.Skill, scenario))
-				.Return(skillDays);
+			skillDayRepository.Stub(x => x.FindRange(futurePeriod, workload.Skill, scenario)).Return(skillDays);
 			var futureData = MockRepository.GenerateMock<IFutureData>();
-			futureData.Stub(x => x.Fetch(workload, skillDays, futurePeriod))
-				.Return(new[] { workloadDay });
+			futureData.Stub(x => x.Fetch(workload, skillDays, futurePeriod)).Return(new[] { workloadDay });
 
-			var target = new OverrideTasksPersister(skillDayRepository, futureData);
+			var intradayForecaster = MockRepository.GenerateMock<IIntradayForecaster>();
+			var historicalPeriodProvider = MockRepository.GenerateMock<IHistoricalPeriodProvider>();
+			var target = new OverrideTasksPersister(skillDayRepository, futureData, intradayForecaster, historicalPeriodProvider);
 			workloadDay.OverrideTasks.Should().Be.EqualTo(null);
 			target.Persist(scenario, workload, new[]
 			{
-				new ModifiedDay()
+				new ModifiedDay
 				{
 					Date = dateOnly.Date
 				}
 			}, 300);
 			Math.Round(workloadDay.OverrideTasks.Value, 2).Should().Be.EqualTo(300d);
+			historicalPeriodProvider.AssertWasNotCalled(x=>x.AvailableIntradayTemplatePeriod(workload));
+			intradayForecaster.AssertWasNotCalled(x=>x.CalculatePattern(Arg<IWorkload>.Is.Anything, Arg<DateOnlyPeriod>.Is.Anything));
+		}
+
+		[Test]
+		public void ShouldNotProvidePatternWhenNoHistoricalData()
+		{
+			var workload = WorkloadFactory.CreateWorkload(SkillFactory.CreateSkill("skill1"));
+			var scenario = new Scenario("scenario1");
+			var dateOnly = new DateOnly();
+			var workloadDay = MockRepository.GenerateMock<IWorkloadDay>();
+			workloadDay.Stub(x => x.CurrentDate).Return(dateOnly);
+			workloadDay.Stub(x => x.OpenForWork).Return(new OpenForWork(true, true));
+
+			var skillDays = new SkillDay[] { };
+			var futurePeriod = new DateOnlyPeriod(dateOnly, dateOnly);
+			var skillDayRepository = MockRepository.GenerateMock<ISkillDayRepository>();
+			skillDayRepository.Stub(x => x.FindRange(futurePeriod, workload.Skill, scenario)).Return(skillDays);
+			var futureData = MockRepository.GenerateMock<IFutureData>();
+			futureData.Stub(x => x.Fetch(workload, skillDays, futurePeriod)).Return(new[] { workloadDay });
+
+			var intradayForecaster = MockRepository.GenerateMock<IIntradayForecaster>();
+			var historicalPeriodProvider = MockRepository.GenerateMock<IHistoricalPeriodProvider>();
+
+			historicalPeriodProvider.Stub(x => x.AvailableIntradayTemplatePeriod(workload)).Return(null);
+			
+			var target = new OverrideTasksPersister(skillDayRepository, futureData, intradayForecaster, historicalPeriodProvider);
+			target.Persist(scenario, workload, new[]
+			{
+				new ModifiedDay
+				{
+					Date = dateOnly.Date
+				}
+			}, 300);
+			workloadDay.AssertWasCalled(x => x.SetOverrideTasks(300d, null));
+			intradayForecaster.AssertWasNotCalled(x => x.CalculatePattern(Arg<IWorkload>.Is.Anything, Arg<DateOnlyPeriod>.Is.Anything));
+		}
+
+		[Test]
+		public void ShouldProvidePatternWhenADayThatHasZeroCalls()
+		{
+			var workload = WorkloadFactory.CreateWorkload(SkillFactory.CreateSkill("skill1"));
+			var scenario = new Scenario("scenario1");
+			var dateOnly = new DateOnly();
+			var workloadDay = MockRepository.GenerateMock<IWorkloadDay>();
+			workloadDay.Stub(x => x.CurrentDate).Return(dateOnly);
+			workloadDay.Stub(x => x.OpenForWork).Return(new OpenForWork(true, true));
+
+			var skillDays = new SkillDay[] {};
+			var futurePeriod = new DateOnlyPeriod(dateOnly, dateOnly);
+			var skillDayRepository = MockRepository.GenerateMock<ISkillDayRepository>();
+			skillDayRepository.Stub(x => x.FindRange(futurePeriod, workload.Skill, scenario)).Return(skillDays);
+			var futureData = MockRepository.GenerateMock<IFutureData>();
+			futureData.Stub(x => x.Fetch(workload, skillDays, futurePeriod)).Return(new[] { workloadDay });
+
+			var intradayForecaster = MockRepository.GenerateMock<IIntradayForecaster>();
+			var historicalPeriodProvider = MockRepository.GenerateMock<IHistoricalPeriodProvider>();
+			var patternPeriod = new DateOnlyPeriod();
+
+			historicalPeriodProvider.Stub(x => x.AvailableIntradayTemplatePeriod(workload)).Return(patternPeriod);
+			var mondayPattern = new List<ITemplateTaskPeriod>();
+			intradayForecaster.Stub(x => x.CalculatePattern(workload, patternPeriod))
+				.Return(new Dictionary<DayOfWeek, IEnumerable<ITemplateTaskPeriod>>
+				{
+					{DayOfWeek.Monday, mondayPattern}
+				});
+
+			var target = new OverrideTasksPersister(skillDayRepository, futureData, intradayForecaster, historicalPeriodProvider);
+			target.Persist(scenario, workload, new[]
+			{
+				new ModifiedDay
+				{
+					Date = dateOnly.Date
+				}
+			}, 300);
+
+			workloadDay.AssertWasCalled(x => x.SetOverrideTasks(300d, mondayPattern));
+
 		}
 	}
 }
