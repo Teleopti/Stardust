d1ad35516f84928c674fc62ad69ff665e671068e
Kunning Mao <none@none>
2015-11-11 16:16:09 +0100

    fix red build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/AuthorizeTeleoptiAttribute.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Filters/AuthorizeTeleoptiAttribute.cs
@@ -8,6 +8,7 @@ using Teleopti.Ccc.Domain.Security.Principal;
 
 namespace Teleopti.Ccc.Web.Filters
 {
+	[CLSCompliant(false)]
 	public class AuthorizeTeleoptiAttribute : AuthorizeAttribute
 	{
 		private readonly IEnumerable<Type> _excludeControllerTypes;
