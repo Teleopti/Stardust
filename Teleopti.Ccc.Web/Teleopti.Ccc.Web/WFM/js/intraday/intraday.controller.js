9e4278649952e4c5361db16ece201f0e38a8f809
Felix <none@none>
2015-11-12 14:13:00 +0100

    STI #35704: Date has no value when date is zero

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.controller.js
@@ -1,37 +1,27 @@
-﻿(function() {
+﻿(function () {
 	'use strict';
 	angular.module('wfm.intraday')
 		.controller('IntradayCtrl', [
 		'$scope', '$state', 'IntradayService',
 		function ($scope, $state, IntradayService) {
-
 			IntradayService.skillList.query().$promise.then(function (result) {
 				$scope.skillList = result;
 				$scope.selectedSkill = $scope.skillList[0];
 				$scope.skillChange($scope.selectedSkill);
 			});
-
 			$scope.skillChange = function (skill) {
 				$scope.name = skill.Measures[0].Name;
 				$scope.value = skill.Measures[0].Value;
+				$scope.offeredCalls = skill.Measures[0].ActualCalls;
+				$scope.forecastedCalls = skill.Measures[0].ForecastedCalls;
 				$scope.stringValue = skill.Measures[0].StringValue;
-				if (skill.Measures[0].LatestDate = new Date()) {
-					$scope.latestDate = "--:--:--";
-				} else {
-					$scope.latestDate = skill.Measures[0].LatestDate;
-				}
-				if ($scope.value == 0) {
-					$scope.value = "-";
-				} else {
-					$scope.value = skill.Measures[0].Value;
-				}
+				$scope.latestDate = skill.Measures[0].LatestDate;
+				$scope.format = IntradayService.formatDateTime;
 			}
-			
 			$scope.reload = setInterval(function () {
 				IntradayService.skillList.query().$promise.then(function (result) {
 					result.forEach(function (resultItem) {
-						$scope.skillList.forEach(function (skillItem)
-						{
+						$scope.skillList.forEach(function (skillItem) {
 							if (resultItem.SkillName == skillItem.SkillName) {
 								skillItem.Severity = resultItem.Severity;
 								skillItem.Measures = resultItem.Measures;
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.service.js
@@ -1,6 +1,5 @@
 ﻿(function() {
 	'use strict';
-
 	var intradayService = angular.module('wfm.intraday');
 	intradayService.service('IntradayService', [
 		'$resource', function ($resource) {
@@ -8,6 +7,17 @@
 			this.skillList = $resource('../api/intraday/skillstatus', {}, {
 				query: { method: 'GET', params: {}, isArray: true }
 			});
+
+			this.formatDateTime = function (time) {
+				if (time === null || time === undefined || time === '') return '--:--:--';
+				console.log(time);
+				var momentTime = moment.utc(time);
+				if (momentTime.format("YYYY") > moment("1970").format("YYYY")) {
+					return momentTime.format('HH:mm:ss');
+				} else {
+					return '--:--:--';
+				}
+			};
 		}
 	]);
 })();
