78c446592e14ee3be4ab9806d59df7254c4ad3aa
Roger Kratz <roger.kratz@teleopti.com>
2015-11-12 15:07:49 +0100

    #35380 Adding controller action to find filters by a string

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/FilterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.Optimization.Filters;
+
+namespace Teleopti.Ccc.Web.Areas.ResourcePlanner
+{
+	public class FilterController : ApiController
+	{
+		private readonly FindFilter _findFilter;
+
+		public FilterController(FindFilter findFilter)
+		{
+			_findFilter = findFilter;
+		}
+
+		[HttpGet, Route("api/filters/{searchString}"), UnitOfWork]
+		public virtual IHttpActionResult FindFilters(string searchString)
+		{
+			return Ok(_findFilter.Search(searchString));
+		}
+  }
+}
