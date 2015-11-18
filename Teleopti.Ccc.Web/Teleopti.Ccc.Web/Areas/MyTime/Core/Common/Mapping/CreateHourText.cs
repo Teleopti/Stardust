61b4a487efc51adcd6b6842c41098a0795dfce52
MingdiWu <mingdi.wu@teleopti.com>
2015-11-18 15:51:47 +0800

    bug fix #35732 should show minutes in time line

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Common/Mapping/CreateHourText.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Common/Mapping/CreateHourText.cs
@@ -22,12 +22,7 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Common.Mapping
 			var localTime = TimeZoneHelper.ConvertFromUtc(time, timeZone);
 			var hourString = string.Format(culture, localTime.ToShortTimeString());
 
-			const string regex = "(\\:.*\\ )";
-			var output = Regex.Replace(hourString, regex, " ");
-			//if (output.Contains(":"))
-			//	output = localTime.Hour.ToString();
-
-			return output;
+			return hourString;
 		}
 
 	}
