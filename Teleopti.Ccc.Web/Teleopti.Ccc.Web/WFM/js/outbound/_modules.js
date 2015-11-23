9e0c7c608d5badd8bd58598408ee6f63e5c148b3
Yanyi Wan <none@none>
2015-11-23 12:21:29 +0800

    Refactor numeric-value input decorator into a global directive.
    - Must be used with <input type="text"/>
    - Provide parsing check based on user's locale, e.g. it will permit ',' as decimal separator in Swedish culture, '.' for group separator in Norweign culture, etc.
    - Provide range check through `min` and `max` attributes
    
    Ref: BUG 35314

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/_modules.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/_modules.js
@@ -12,19 +12,18 @@
 			'toggleService',
 			'gantt',
 			'gantt.table',
-			'gantt.tooltips'
+			'gantt.tooltips',
+			'wfm.numericValue'
 		]
 	);
 	angular.module('outboundServiceModule', [ 'ngResource', 'pascalprecht.translate']);
 
 	outbound.run([
 		'$rootScope', '$state', '$location', function($rootScope, $state, $location) {
-
 			$rootScope.$on('$stateChangeSuccess',
 				function(event, toState) {
 					if ($location.url() == $state.current.url && toState.name == 'outbound') $state.go('outbound.summary');
 				});
-
 		}
 	]);
 })();
