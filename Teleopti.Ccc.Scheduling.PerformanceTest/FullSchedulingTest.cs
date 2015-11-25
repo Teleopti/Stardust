372c9f20639fb73e09cbd955b83f3747aba7eec4
Roger Kratz <roger.kratz@teleopti.com>
2015-11-25 16:30:07 +0100

    #36020 Adding performance test assembly. Just a dummy, explicit test for now.

---
+++ Teleopti.Ccc.Scheduling.PerformanceTest/FullSchedulingTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.TestCommon.Web.StartWeb;
+
+namespace Teleopti.Ccc.Scheduling.PerformanceTest
+{
+	public class FullSchedulingTest
+	{
+		[SetUp]
+		public void Setup()
+		{
+			TestSiteConfigurationSetup.Setup();
+		}
+
+		[Test, Explicit]
+		public void MeasurePerformance()
+		{
+			
+		}
+
+		[TearDown]
+		public void CleanUp()
+		{
+			
+		}
+	}
+}
