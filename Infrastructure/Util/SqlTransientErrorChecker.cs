431f7ef0a6398abc4e1b07910c17e7098850c642
Robin Karlsson <robin.karlsson@teleopti.com>
2015-11-25 20:26:56 +0100

    Reordering sql errors to treat as transient, and treat 42019 as transient

--- Infrastructure/Util/SqlTransientErrorChecker.cs
+++ Infrastructure/Util/SqlTransientErrorChecker.cs
@@ -17,27 +17,28 @@ namespace Teleopti.Ccc.Infrastructure.Util
 					{
 						switch (err.Number)
 						{
+							case -2: //Timeout!
+							case 64:
+							case 233:
 							case 4060:
-							case 40501:
-							case 49918:
-							case 49919:
-							case 49920:
-							case 10928:
-							case 10929:
 							case 10053:
 							case 10054:
 							case 10060:
+							case 10928:
+							case 10929:
+							case 40501:
+							case 40143:
 							case 40197:
 							case 40540:
 							case 40613:
-							case 40143:
-							case 40671:
 							case 40648:
+							case 40671:
+							case 42019:
 							case 45168:
 							case 45169:
-							case 233:
-							case 64:
-							case -2: //Timeout!
+							case 49918:
+							case 49919:
+							case 49920:
 								return true;
 						}
 					}
