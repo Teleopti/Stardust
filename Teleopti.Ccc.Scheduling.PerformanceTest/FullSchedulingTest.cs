b5e71946bccfc80068f0203b569cc65a29c883ba
Roger Kratz <roger.kratz@teleopti.com>
2015-11-26 15:51:51 +0100

    #36020 Now perf test actually does a scheduling Still wrong assert though

--- Teleopti.Ccc.Scheduling.PerformanceTest/FullSchedulingTest.cs
+++ Teleopti.Ccc.Scheduling.PerformanceTest/FullSchedulingTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using Teleopti.Ccc.TestCommon.Web.WebInteractions;
+using Teleopti.Ccc.TestCommon.Web.WebInteractions.BrowserDriver;
 using Teleopti.Ccc.TestCommon.Web.WebInteractions.BrowserDriver.CoypuImpl;
 
 namespace Teleopti.Ccc.Scheduling.PerformanceTest
@@ -16,13 +17,16 @@ namespace Teleopti.Ccc.Scheduling.PerformanceTest
 		[Test, Explicit]
 		public void MeasurePerformance()
 		{
+			// Move this to app.config!
 			var userName = "demo";
 			var password = "demo";
 			var businessUnitName = "Teleopti WFM Demo";
+			var planningPeriodId = "32f17285-f7ad-4c76-a050-a5590100ca00";
+			///////////////////////////
 
 			using (var browserActivator = new CoypuChromeActivator())
 			{
-				browserActivator.Start(TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(5));
+				browserActivator.Start(TimeSpan.FromSeconds(10), TimeSpan.FromMilliseconds(100));
 				var browserInteractions = browserActivator.GetInteractions();
 
 				//Logon
@@ -30,6 +34,18 @@ namespace Teleopti.Ccc.Scheduling.PerformanceTest
 					TestSiteConfigurationSetup.URL, 
 					"Test/Logon", 
 					string.Format("?businessUnitName={0}&userName={1}&password={2}", businessUnitName, userName, password)));
+
+				//Goto scheduling page
+				browserInteractions.GoTo(string.Concat(TestSiteConfigurationSetup.URL, "wfm/#/resourceplanner/planningperiod/", planningPeriodId));
+
+				//click schedule button
+				browserInteractions.Click(".schedule-button");
+
+				//wrong assert here for now - should check
+				//1. Show report page -> ok
+				//2. If "server busy -> fail
+				//3. If error is written -> fail
+				browserInteractions.AssertExists(".schedule-report");
 			}
 		}
 
