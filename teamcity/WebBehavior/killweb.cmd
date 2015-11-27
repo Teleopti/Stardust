613d76d1d70e3c94bff5fb27850d160177dad625
Roger Kratz <roger.kratz@teleopti.com>
2015-11-27 15:11:52 +0100

    #36020 Adding script to kill web tests. To be used in both webscenarios and perf tests

---
+++ teamcity/WebBehavior/killweb.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+taskkill.exe /IM chromedriver.exe /F /FI "memusage gt 2" /t
+taskkill.exe /IM iisexpress.exe /F /FI "memusage gt 2" /t
+taskkill.exe /IM chrome.exe /F /FI "memusage gt 2" /t
+taskkill.exe /IM iisexpresstray.exe /F /FI "memusage gt 2" /t
