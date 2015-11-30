7768c0113ec4385fe5a51c7d68cde6fb0ba0b68e
Roger Kratz <roger.kratz@teleopti.com>
2015-11-30 14:21:03 +0100

    #36020 Need to be able to set server name in script (on agent, default instance is not used)

--- teamcity/PerfTest/restoreAppDb.bat
+++ teamcity/PerfTest/restoreAppDb.bat
@@ -7,33 +7,40 @@ set destinationBakFile=%repoRoot%\db.bak
 set appDb=PerfApp
 set analDb=PerfAnal
 set aggDb=PerfAgg
-set sourceFolder=%1
-IF [%1]==[] (
-	echo Pass in a path to a folder containing "ccc7.bak" and "app.config"
-	pause
-	exit
-)
+set dbServer=%1
+set sourceFolder=%2
+IF [%1]==[] goto wrongInput
+if [%2]==[] goto wrongInput
+
 
 ::copy bak file
 COPY "%sourceFolder%\ccc7.bak" "%destinationBakFile%" /Y
 
 ::restore db
-SQLCMD -S. -E -dmaster -i"%RepoRoot%\.debug-Setup\database\tsql\DemoDatabase\RestoreDatabase.sql" -v BAKFILE="%destinationBakFile%" DATAFOLDER="%RepoRoot%" -v DATABASENAME="%appDb%"
+SQLCMD -S%dbServer% -E -dmaster -i"%RepoRoot%\.debug-Setup\database\tsql\DemoDatabase\RestoreDatabase.sql" -v BAKFILE="%destinationBakFile%" DATAFOLDER="%RepoRoot%" -v DATABASENAME="%appDb%"
 
 ::Skapa agg + analytics
-SQLCMD -S. -E -Q "alter database [%analDb%] set single_user with rollback immediate"
-SQLCMD -S. -E -Q "if exists(select 1 from sys.databases where name=""%analDb%"") drop database [%analDb%]"
-SQLCMD -S. -E -Q "alter database [%aggDb%] set single_user with rollback immediate"
-SQLCMD -S. -E -Q "if exists(select 1 from sys.databases where name=""%aggDb%"") drop database [%aggDb%]"
-%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\debug\DBManager.exe -S. -D%analDb% -E -OTeleoptiAnalytics -F"%RepoRoot%\Database" -C
-%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\debug\DBManager.exe -S. -D%aggDb% -E -OTeleoptiCCCAgg -F"%RepoRoot%\Database" -C
+SQLCMD -S%dbServer% -E -Q "alter database [%analDb%] set single_user with rollback immediate"
+SQLCMD -S%dbServer% -E -Q "if exists(select 1 from sys.databases where name=""%analDb%"") drop database [%analDb%]"
+SQLCMD -S%dbServer% -E -Q "alter database [%aggDb%] set single_user with rollback immediate"
+SQLCMD -S%dbServer% -E -Q "if exists(select 1 from sys.databases where name=""%aggDb%"") drop database [%aggDb%]"
+%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\debug\DBManager.exe -S%dbServer% -D%analDb% -E -OTeleoptiAnalytics -F"%RepoRoot%\Database" -C
+%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\debug\DBManager.exe -S%dbServer% -D%aggDb% -E -OTeleoptiCCCAgg -F"%RepoRoot%\Database" -C
 
 ::upgrade appdb
-%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\debug\DBManager.exe -S. -D"%appDb%" -E -OTeleoptiCCC7 -F"%RepoRoot%\Database"
-%RepoRoot%\Teleopti.Support.Security\bin\debug\Teleopti.Support.Security.exe -DS. -AP"%appDb%" -AN"%analDb%" -CD"%aggDb%" -EE 
+%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\debug\DBManager.exe -S%dbServer% -D"%appDb%" -E -OTeleoptiCCC7 -F"%RepoRoot%\Database"
+%RepoRoot%\Teleopti.Support.Security\bin\debug\Teleopti.Support.Security.exe -DS%dbServer% -AP"%appDb%" -AN"%analDb%" -CD"%aggDb%" -EE 
 
 
 ::copy app.config
 COPY "%sourceFolder%\app.config" "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\app.config" /Y
 COPY "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\app.config" "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\bin\debug\Teleopti.Ccc.Scheduling.PerformanceTest.dll.config" /Y
 COPY "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\app.config" "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\bin\release\Teleopti.Ccc.Scheduling.PerformanceTest.dll.config" /Y
+
+exit
+
+:wrongInput
+echo Two arguments are needed. Pass in server name and a path to a folder containing "ccc7.bak" and "app.config". Eg,
+echo restoreAppDb.bat . \\devbuild01\perftests\demosales
+echo.
+pause
