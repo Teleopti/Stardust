6fec3890746a07ef7520605d401e8b6b6602f384
blandine descamps <blandine.descamps@teleopti.com>
2015-12-01 09:25:09 +0100

    fix #35876 less error 401 during authentication

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/http.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/http.js
@@ -3,16 +3,34 @@
 
 	angular
         .module('wfm.http', ['currentUserInfoService'])
-        .factory('httpInterceptor', ['$q', 'growl', '$injector', httpInterceptor]);
+        .factory('httpInterceptor', ['$q', 'growl', '$injector', '$timeout', httpInterceptor]);
 
-	function httpInterceptor($q, growl, $injector) {
+	function httpInterceptor($q, growl, $injector, $timeout) {
+		var connected = true;
 		var service = {
-			'responseError': reject
+			'responseError': reject,
+			'request': request
 		};
 		return service;
 
+		
+
+		function request(config) {
+			if (!connected) {
+				var q = $q.defer();
+				$timeout(function() {
+					q.reject();
+				}, 2000);
+				return q.promise;
+			}
+			return config;
+		};
+
+
+
 		function reject(rejection) {
 			if (rejection.status === 401) {
+				connected = false;
 				var CurrentUserInfo = $injector.get('CurrentUserInfo');
 				CurrentUserInfo.resetContext();
 			} 
