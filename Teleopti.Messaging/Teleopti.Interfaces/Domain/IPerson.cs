b470250e7de69d03742844f491891366f283c40e
Robin Karlsson <robin.karlsson@teleopti.com>
2015-12-01 14:58:05 +0100

    Going from struct to class as we now have a reference to a lazy class (#36040 + #36083)

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IPerson.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IPerson.cs
@@ -279,27 +279,4 @@ namespace Teleopti.Interfaces.Domain
 	    bool IsTerminated();
 	    PersonWorkDay[] AverageWorkTimes(DateOnlyPeriod period);
     }
-
-	public struct PersonWorkDay
-	{
-		public PersonWorkDay(DateOnly date, bool workDay = false)
-			: this(date,new Lazy<TimeSpan>(()=>TimeSpan.Zero), WorkTimeSource.FromSchedulePeriod, new Percent(1), workDay)
-		{
-		}
-
-		public PersonWorkDay(DateOnly date, Lazy<TimeSpan> averageWorkTime, WorkTimeSource workTimeSource, Percent partTimePercentage, bool isWorkDay = false) : this()
-		{
-			IsWorkDay = isWorkDay;
-			AverageWorkTime = averageWorkTime;
-			WorkTimeSource = workTimeSource;
-			Date = date;
-			PartTimePercentage = partTimePercentage;
-		}
-
-		public DateOnly Date { get; private set; }
-		public Lazy<TimeSpan> AverageWorkTime { get; private set; }
-		public WorkTimeSource WorkTimeSource { get; private set; }
-		public Percent PartTimePercentage { get; private set; }
-		public bool IsWorkDay { get; private set; }
-	}
 }
---
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/PersonWorkDay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Teleopti.Interfaces.Domain
+{
+	public class PersonWorkDay
+	{
+		public PersonWorkDay(DateOnly date, bool workDay = false)
+			: this(date,new Lazy<TimeSpan>(()=>TimeSpan.Zero), WorkTimeSource.FromSchedulePeriod, new Percent(1), workDay)
+		{
+		}
+
+		public PersonWorkDay(DateOnly date, Lazy<TimeSpan> averageWorkTime, WorkTimeSource workTimeSource, Percent partTimePercentage, bool isWorkDay = false)
+		{
+			IsWorkDay = isWorkDay;
+			AverageWorkTime = averageWorkTime;
+			WorkTimeSource = workTimeSource;
+			Date = date;
+			PartTimePercentage = partTimePercentage;
+		}
+
+		public DateOnly Date { get; private set; }
+		public Lazy<TimeSpan> AverageWorkTime { get; private set; }
+		public WorkTimeSource WorkTimeSource { get; private set; }
+		public Percent PartTimePercentage { get; private set; }
+		public bool IsWorkDay { get; private set; }
+	}
+}
