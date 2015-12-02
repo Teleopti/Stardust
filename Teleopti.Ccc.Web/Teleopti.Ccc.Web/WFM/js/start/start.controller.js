05b05b28d5ba6fbbbf05bcaa2a64e9c36c1d7113
Carl.Rockman@teleopti.com <Carl.Rockman@teleopti.com>
2015-12-02 14:57:07 +0100

    removed unused feed parser

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/start/start.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/start/start.controller.js
@@ -3,25 +3,8 @@
 	var start = angular.module('wfm.start', []);
 
 	start.controller("FeedCtrl", [
-	'$scope', 'FeedService', function ($scope, Feed) {
-		$scope.feedSrc = 'http://blog.teleopti.com/feed/';
-			$scope.loadFeed = function(e) {
-				Feed.parseFeed($scope.feedSrc).then(function(res) {
-					$scope.feeds = res.data.responseData.feed.entries;
+	'$scope', function ($scope) {
 
-				});
-			};
-		}
+	}
 	]);
-
-	start.factory('FeedService', [
-		'$http', function ($http) {
-			return {
-				parseFeed: function(url) {
-					return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
-				}
-			};
-		}
-	]);
-
 })();
