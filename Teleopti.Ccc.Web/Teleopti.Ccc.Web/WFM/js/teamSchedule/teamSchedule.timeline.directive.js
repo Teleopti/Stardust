e7c1ec4892995f014a7354290c5c7e10da0c3c71
Chundan Xu <chundan.xu@teleopti.com>
2015-12-04 15:02:19 +0800

    PBI 35995: make check box look better.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.timeline.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.timeline.directive.js
@@ -1,25 +1,18 @@
 'use strict';
 
 (function () {
-	var timeLineCtrl = function($scope, toggleSvc) {
+	var timeLineCtrl = function ($scope) {
 		var vm = this;
 		vm.height = 0;
-		$scope.$watch(function() {
+		$scope.$watch(function () {
 			return vm.scheduleCount;
-		}, function(newValue) {
+		}, function (newValue) {
 			if (newValue > 0) {
-				toggleSvc.isFeatureEnabled
-					.query({ toggle: 'WfmTeamSchedule_AbsenceReporting_35995' })
-					.$promise
-					.then(function(result) {
-						var withCheckbox = result.IsEnabled;
+				var headerHeight = 17; //angular.element($("#time-line-container"))[0].offsetHeight
+				var labelHeight = 12; //angular.element($(".label-info"))[0].offsetHeight;
+				var rowHeight = 32;
 
-						var headerHeight = 17; //angular.element($("#time-line-container"))[0].offsetHeight
-						var labelHeight = 12; //angular.element($(".label-info"))[0].offsetHeight;
-						var rowHeight = withCheckbox ? 42 : 32;
-
-						vm.height = rowHeight * (vm.scheduleCount) + headerHeight + labelHeight;
-					});
+				vm.height = rowHeight * (vm.scheduleCount) + headerHeight + labelHeight;
 			}
 		});
 	}
@@ -38,7 +31,7 @@
 	};
 	angular.module('wfm.teamSchedule')
 		.directive('timeLine', directive)
-		.controller('TimeLineCtrl', ['$scope', 'Toggle', timeLineCtrl]);
+		.controller('TimeLineCtrl', ['$scope', timeLineCtrl]);
 
 	function linkFunction(scope, element, attributes, vm) {
 	};
