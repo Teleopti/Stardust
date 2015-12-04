65a43d705fb7740ba50e12c273259a7f630e5151
Xinfeng Li <xinfeng.li@teleopti.com>
2015-12-04 15:28:47 +0800

    PBI #35995: Change timepicker to same style as datepicker.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
@@ -5,28 +5,23 @@
 		.run([
 			'$templateCache', function($templateCache) {
 				$templateCache.put("template/timepicker/timepicker.html",
-				
 					"<table class=\"timepicker\">" +
 					"  <tbody>" +
 					"    <tr>" +
 					"      <td class=\"form-group\" ng-class=\"{'has-error': invalidHours}\">" +
-					"          <input type=\"text\" ng-model=\"hours\" ng-change=\"updateHours()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\"/>" +
-					"	" +
+					"          <input type=\"text\" class=\"form-control\" ng-model=\"hours\" ng-change=\"updateHours()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\"/>" +
 					"      </td>" +
 					"      <td>:</td>" +
 					"      <td class=\"form-group\" ng-class=\"{'has-error': invalidMinutes}\">" +
-					"          <input type=\"text\" ng-model=\"minutes\" ng-change=\"updateMinutes()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\"/>" +
+					"          <input type=\"text\" class=\"form-control\" ng-model=\"minutes\" ng-change=\"updateMinutes()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\"/>" +
 					"      </td>" +
 					"      <td ng-show=\"showMeridian\">" +
-					"	<md-button type=\"button\" ng-disabled=\"noToggleMeridian()\" ng-click=\"toggleMeridian()\" tabindex=\"1\">{{meridian}}</md-button>" +
+					"          <md-button type=\"button\" ng-disabled=\"noToggleMeridian()\" ng-click=\"toggleMeridian()\" tabindex=\"1\">{{meridian}}</md-button>" +
 					"      </td>" +
 					"    </tr>" +
 					"  </tbody>" +
 					"</table>" 
-					
 				);
-
 			}
 		]);
-
 })();
