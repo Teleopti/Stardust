f811dc673727aab1ac04b248bfb2c85fe86d7823
Xinfeng Li <xinfeng.li@teleopti.com>
2015-12-07 16:06:54 +0800

    PBI #35995: Change date picker and time picker to same row.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
@@ -9,11 +9,11 @@
 					"  <tbody>" +
 					"    <tr>" +
 					"      <td class=\"form-group\" ng-class=\"{'has-error': invalidHours}\">" +
-					"          <input type=\"text\" ng-model=\"hours\" ng-change=\"updateHours()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\"/>" +
+					"          <input type=\"text\" ng-model=\"hours\" style=\"text-align: right;\" ng-change=\"updateHours()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\" />" +
 					"      </td>" +
 					"      <td>:</td>" +
 					"      <td class=\"form-group\" ng-class=\"{'has-error': invalidMinutes}\">" +
-					"          <input type=\"text\" ng-model=\"minutes\" ng-change=\"updateMinutes()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\"/>" +
+					"          <input type=\"text\" ng-model=\"minutes\" ng-change=\"updateMinutes()\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"1\" />" +
 					"      </td>" +
 					"      <td ng-show=\"showMeridian\">" +
 					"          <md-button type=\"button\" ng-disabled=\"noToggleMeridian()\" ng-click=\"toggleMeridian()\" tabindex=\"1\">{{meridian}}</md-button>" +
