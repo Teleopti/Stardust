1d75b5a60c8e23dba7c8ee5fa11d5fa2237ea9ca
Robin Karlsson <robin.karlsson@teleopti.com>
2015-12-09 16:18:41 +0100

    Ok, worked better locally

--- Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/Wrappers/IHubConnectionWrapper.cs
+++ Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/Wrappers/IHubConnectionWrapper.cs
@@ -6,16 +6,13 @@ using Microsoft.AspNet.SignalR.Client.Transports;
 
 namespace Teleopti.Messaging.Client.SignalR.Wrappers
 {
+	[CLSCompliant(false)]
 	public interface IHubConnectionWrapper
 	{
-#pragma warning disable CS3003 // Type is not CLS-compliant
 		ConnectionState State { get; }
-#pragma warning restore CS3003 // Type is not CLS-compliant
 		ICredentials Credentials { get; set; }
 		Task Start();
-#pragma warning disable CS3001 // Argument type is not CLS-compliant
 		Task Start(IClientTransport transport);
-#pragma warning restore CS3001 // Argument type is not CLS-compliant
 		void Stop();
 		event Action Closed;
 		event Action Reconnected;
