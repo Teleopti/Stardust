09064c03d9a5224e70cf22e6b3af66d25a2e04b3
Robin Karlsson <robin.karlsson@teleopti.com>
2015-12-11 00:02:02 +0100

    Backed out changeset: 8fbaa72d905a

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/rta/rta.agents.controller.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/rta/rta.agents.controller.spec.js
@@ -65,7 +65,7 @@ describe('RtaAgentsCtrl', function() {
 		$sessionStorage = _$sessionStorage_;
 		$httpBackend = _$httpBackend_;
 
-		$httpBackend.whenGET("../api/Adherence/ForToday?personId=11610fe4-0130-4568-97de-9b5e015b2564")
+		$httpBackend.whenGET("../Adherence/ForToday?personId=11610fe4-0130-4568-97de-9b5e015b2564")
 			.respond(function() {
 				return [200, adherence];
 			});
