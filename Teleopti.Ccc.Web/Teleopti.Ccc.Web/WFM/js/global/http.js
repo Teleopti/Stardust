0e38600a4bfac5eb279e94e0dc46d38b7c0fcc08
johan Damm <none@none>
2015-12-14 16:17:18 +0100

    #34402 displaying rest errors in the ui

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/http.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/http.js
@@ -1,19 +1,17 @@
-﻿(function () {
+(function() {
 	'use strict';
 
 	angular
-        .module('wfm.http', ['currentUserInfoService'])
-        .factory('httpInterceptor', ['$q', 'growl', '$injector', '$timeout', httpInterceptor]);
+		.module('wfm.http', ['currentUserInfoService', 'angular-growl', 'pascalprecht.translate'])
+		.factory('httpInterceptor', ['$q', 'growl', '$injector', '$timeout', '$translate', httpInterceptor]);
 
-	function httpInterceptor($q, growl, $injector, $timeout) {
+	function httpInterceptor($q, growl, $injector, $timeout, $translate) {
 		var connected = true;
 		var service = {
 			'responseError': reject,
 			'request': request
 		};
 		return service;
-
-		
 
 		function request(config) {
 			if (!connected) {
@@ -26,14 +24,18 @@
 			return config;
 		};
 
-
-
 		function reject(rejection) {
 			if (rejection.status === 401) {
 				connected = false;
 				var CurrentUserInfo = $injector.get('CurrentUserInfo');
 				CurrentUserInfo.resetContext();
-			} 
+			}
+			if (rejection.status > 401 && rejection.status < 600 || rejection.status === 0) {
+				growl.error("<i class='mdi mdi-alert'></i>" + $translate.instant('InternalErrorMessage'), {
+					ttl: 0,
+					disableCountDown: true
+				});
+			}
 			return $q.reject(rejection);
 		};
 	}
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/tests/global/http.spec.js
@@ -0,0 +1,81 @@
+'use strict';
+describe('HttpTest', function() {
+	var $q,
+		$rootScope,
+		$httpBackend,
+		$http,
+		httpInterceptor,
+		growl;
+
+	beforeEach(function() {
+		module('wfm.http');
+		module(function($provide) {
+			$provide.service('growl', function() {
+				return {
+					error: function() {}
+				};
+			});
+		});
+	});
+	beforeEach(inject(function(_$httpBackend_, _$q_, _$rootScope_, _$http_, _httpInterceptor_, _growl_) {
+		$q = _$q_;
+		$rootScope = _$rootScope_;
+		$httpBackend = _$httpBackend_;
+		$http = _$http_;
+		httpInterceptor = _httpInterceptor_;
+		growl = _growl_;
+	}));
+
+	it('Should react to http error 500', function(done) {
+		var scope = $rootScope.$new();
+		spyOn(growl, 'error');
+		var rejection = {
+			status: 500
+		};
+		var response = httpInterceptor.responseError(rejection);
+		var successCallback = function() {};
+		var errorCallback = function(resolved) {
+			expect(growl.error).toHaveBeenCalled();
+			done();
+		};
+
+		response.then(successCallback, errorCallback);
+		scope.$digest();
+	});
+
+	it('Should react to all 4.XX http errors', function(done) {
+		var scope = $rootScope.$new();
+		spyOn(growl, 'error');
+		var rejection = {
+			status: 418
+		};
+		var response = httpInterceptor.responseError(rejection);
+		var successCallback = function() {};
+		var errorCallback = function(resolved) {
+			expect(growl.error).toHaveBeenCalled();
+			done();
+		};
+
+		response.then(successCallback, errorCallback);
+		scope.$digest();
+	});
+
+	fit('Should react to http error 0', function(done) {
+		var scope = $rootScope.$new();
+		spyOn(growl, 'error');
+		var rejection = {
+			status: 0
+		};
+		var response = httpInterceptor.responseError(rejection);
+		var successCallback = function() {};
+		var errorCallback = function(resolved) {
+			expect(growl.error).toHaveBeenCalled();
+			done();
+		};
+
+		response.then(successCallback, errorCallback);
+		scope.$digest();
+	});
+
+
+});
