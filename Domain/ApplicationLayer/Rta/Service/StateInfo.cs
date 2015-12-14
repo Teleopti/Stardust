a754cb3929830c36523e61aaf976b5ace114bf74
Xianwei Shen <xianwei.shen@teleopti.com>
2015-12-14 12:25:23 +0100

    lambda expression for readonly property is not ready yet

--- Domain/ApplicationLayer/Rta/Service/StateInfo.cs
+++ Domain/ApplicationLayer/Rta/Service/StateInfo.cs
@@ -25,17 +25,38 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 			Adherence = new AdherenceInfo(Input, Person, Stored, State, Schedule, appliedAdherence, stateMapper);
 		}
 
-		private Guid platformTypeId => string.IsNullOrEmpty(Input.PlatformTypeId) ?  Stored.PlatformTypeId() : Input.ParsedPlatformTypeId();
+		private Guid platformTypeId
+		{
+		    get
+		    {
+		        return string.IsNullOrEmpty(Input.PlatformTypeId) ? Stored.PlatformTypeId() : Input.ParsedPlatformTypeId();
+		    }
+		}
 
-		private string stateCode => Input.StateCode ?? Stored.StateCode();
+	    private string stateCode
+	    {
+	        get { return Input.StateCode ?? Stored.StateCode(); }
+	    }
 
-		private DateTime? stateStartTime => State.StateGroupChanged() ? CurrentTime : Stored.StateStartTime;
+	    private DateTime? stateStartTime
+	    {
+	        get { return State.StateGroupChanged() ? CurrentTime : Stored.StateStartTime; }
+	    }
 
-		private DateTime? adherenceStartTime => (Stored == null || State.Adherence() != Stored.Adherence) ? CurrentTime : Stored.AdherenceStartTime;
+	    private DateTime? adherenceStartTime
+	    {
+	        get
+	        {
+	            return (Stored == null || State.Adherence() != Stored.Adherence) ? CurrentTime : Stored.AdherenceStartTime;
+	        }
+	    }
 
-		private DateTime? batchId => Input.IsSnapshot ? Input.BatchId : Stored.BatchId();
+	    private DateTime? batchId
+	    {
+	        get { return Input.IsSnapshot ? Input.BatchId : Stored.BatchId(); }
+	    }
 
-		private DateTime? alarmStartTime
+	    private DateTime? alarmStartTime
 		{
 			get
 			{
@@ -45,12 +66,12 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 		}
 
 		public ExternalUserStateInputModel Input { get; set; }
-		public PersonOrganizationData Person { get; }
-		public StateAlarmInfo State { get; }
-		public StoredStateInfo Stored { get; }
-		public ScheduleInfo Schedule { get; }
-		public AdherenceInfo Adherence { get; }
-		public DateTime CurrentTime { get; }
+		public PersonOrganizationData Person { get; private set; }
+	    public StateAlarmInfo State { get; private set; }
+		public StoredStateInfo Stored { get; private set; }
+		public ScheduleInfo Schedule { get; private set; }
+		public AdherenceInfo Adherence { get; private set; }
+		public DateTime CurrentTime { get; private set; }
 
 		public AgentStateReadModel MakeAgentStateReadModel()
 		{
@@ -83,9 +104,12 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 			};
 		}
 		
-		public EventAdherence AggregatorAdherence => Adherence.AdherenceForNewStateAndCurrentActivity();
+		public EventAdherence AggregatorAdherence
+		{
+		    get { return Adherence.AdherenceForNewStateAndCurrentActivity(); }
+		}
 
-		public bool Send
+	    public bool Send
 		{
 			get
 			{
