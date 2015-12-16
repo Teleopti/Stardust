12b66df952671492fe133c0ae67fbe62f973a130
Li Xue <li.xue@teleopti.com>
2015-12-16 09:50:09 +0800

    #36294 use descriptive template name

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/requests/requests.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/requests/requests.controller.js
@@ -15,7 +15,7 @@
 			vm.isRequestsEnabled = toggles.isRequestsEnabled();			
 			vm.isPeopleSearchEnabled = toggles.isPeopleSearchEnabled();
 			vm.period = { startDate: new Date(), endDate: new Date() };
-			vm.templateType = "new";
+			vm.templateType = "dropdown";
 			vm.agentSearchOptions = {
 				keyword: "",
 				isAdvancedSearchEnabled: true,
