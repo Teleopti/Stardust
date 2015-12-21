668512a086266f291636b50fb691808c24e9695d
Roger Kratz <roger.kratz@teleopti.com>
2015-12-21 11:09:22 +0100

    Don't use interceptor before it is declared (moving function to declaration) Long shot trying to get rid of problem with "disappeared" http requests.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/http.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/http.js
@@ -3,40 +3,40 @@
 
 	angular
 		.module('wfm.http', ['currentUserInfoService', 'angular-growl', 'pascalprecht.translate'])
-		.factory('httpInterceptor', ['$q', 'growl', '$injector', '$timeout', '$translate', httpInterceptor]);
+		.factory('httpInterceptor', [
+			'$q', 'growl', '$injector', '$timeout', '$translate', function($q, growl, $injector, $timeout, $translate) {
+				var connected = true;
+				var service = {
+					'responseError': reject,
+					'request': request
+				};
+				return service;
 
-	function httpInterceptor($q, growl, $injector, $timeout, $translate) {
-		var connected = true;
-		var service = {
-			'responseError': reject,
-			'request': request
-		};
-		return service;
+				function request(config) {
+					if (!connected) {
+						var q = $q.defer();
+						$timeout(function() {
+							q.reject();
+						}, 2000);
+						return q.promise;
+					}
+					return config;
+				}
 
-		function request(config) {
-			if (!connected) {
-				var q = $q.defer();
-				$timeout(function() {
-					q.reject();
-				}, 2000);
-				return q.promise;
+				function reject(rejection) {
+					if (rejection.status === 401) {
+						connected = false;
+						var CurrentUserInfo = $injector.get('CurrentUserInfo');
+						CurrentUserInfo.resetContext();
+					}
+					if (rejection.status > 401 && rejection.status < 600 || rejection.status === 0) {
+						growl.error("<i class='mdi mdi-alert'></i>" + $translate.instant('InternalErrorMessage'), {
+							ttl: 0,
+							disableCountDown: true
+						});
+					}
+					return $q.reject(rejection);
+				}
 			}
-			return config;
-		};
-
-		function reject(rejection) {
-			if (rejection.status === 401) {
-				connected = false;
-				var CurrentUserInfo = $injector.get('CurrentUserInfo');
-				CurrentUserInfo.resetContext();
-			}
-			if (rejection.status > 401 && rejection.status < 600 || rejection.status === 0) {
-				growl.error("<i class='mdi mdi-alert'></i>" + $translate.instant('InternalErrorMessage'), {
-					ttl: 0,
-					disableCountDown: true
-				});
-			}
-			return $q.reject(rejection);
-		};
-	}
+		]);
 })();
