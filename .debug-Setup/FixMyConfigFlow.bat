1c277d854964a5d20f77966606e55586a7c3e744
Erik Sundberg <erik.sundberg@teleopti.com>
2015-12-22 09:31:17 +0100

    refact fixMyConfigFlow

--- .debug-Setup/FixMyConfigFlow.bat
+++ .debug-Setup/FixMyConfigFlow.bat
@@ -3,12 +3,12 @@ SETLOCAL
 
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
-SET CD=%ROOTDIR%\..
-CALL :Branch "%CD%"
+
+CALL :Branch %ROOTDIR%
 SET CCC7DB=%Branch%_DemoSales_TeleoptiCCC7
 SET AnalyticsDB=%Branch%_DemoSales_TeleoptiAnalytics
 
-CALL FixMyConfig.bat %CCC7DB% %AnalyticsDB%
+CALL %ROOTDIR%\FixMyConfig.bat %CCC7DB% %AnalyticsDB%
 
 :Branch
 SET BRANCH=%~n1
