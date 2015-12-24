3fc1f8ef9f830c8995748576d9799542ef927d1a
MingdiWu <mingdi.wu@teleopti.com>
2015-12-24 11:39:28 +0800

    PBI #36491 separate add absence step 1

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.directive.addabsence.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+
+(function () {
+
+	angular.module('wfm.teamSchedule').directive('addAbsence', ['$locale', absencePanel]);
+
+	function absencePanel($locale) {
+		return {
+			templateUrl: 'js/teamSchedule/html/addabsencepanel.html',
+			scope: {
+				selectedDate: '&',
+				permissions: '='
+			},
+			controller: ['$timeout', addAbsenceCtrl],
+			controllerAs: 'vm',
+			bindToController: true,
+			link: function (scope, element, attr) {
+				//scope.vm.shortDateFormat = $locale.DATETIME_FORMATS.shortDate;
+				//scope.$on('$localeChangeSuccess', function () {
+				//	scope.vm.shortDateFormat = $locale.DATETIME_FORMATS.shortDate;
+				//});
+				//scope.vm.isMiniMode = 'mini' in attr;
+			}
+		};
+	};
+
+
+	function addAbsenceCtrl($timeout) {
+		var vm = this;
+		vm.selectedAbsenceStartDate = vm.selectedDate();
+		vm.selectedAbsenceEndDate = vm.selectedDate();
+		//vm.absencePermissions = vm.permissions;
+		//	{
+		//	IsAddIntradayAbsenceAvailable: vm.permissions.IsAddIntradayAbsenceAvailable,
+		//	IsAddFullDayAbsenceAvailable: vm.permissions.IsAddFullDayAbsenceAvailable
+		//};
+	};
+
+})();
