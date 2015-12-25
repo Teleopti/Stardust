63608a9690f457141627d3978a789bbf5adc04d0
MingdiWu <mingdi.wu@teleopti.com>
2015-12-25 12:47:46 +0800

    fix build

--- DomainTest/Security/LicenseOptions/TeleoptiCccMyTeamLicenseOptionTest.cs
+++ DomainTest/Security/LicenseOptions/TeleoptiCccMyTeamLicenseOptionTest.cs
@@ -17,7 +17,7 @@ namespace Teleopti.Ccc.DomainTest.Security.LicenseOptions
 
 			target.EnableApplicationFunctions(inputList);
 			IList<IApplicationFunction> resultList = target.EnabledApplicationFunctions;
-			Assert.AreEqual(6, resultList.Count);
+			Assert.AreEqual(7, resultList.Count);
 		}
 	}
 }
