81e8c1bffe5724cef52b2f370edd2936ed298b93
blandine descamps <blandine.descamps@teleopti.com>
2016-01-05 09:43:58 +0100

    #35547 gantt chart issue on IE 11

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/angular-gantt.ext.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/outbound/angular-gantt.ext.js
@@ -2,6 +2,30 @@
 
 	'use strict';
 
+	angular.module('gantt').config(['$provide', function ($provide) {
+
+		$provide.decorator('GanttScroll', [
+			'$delegate', function ($delegate) {
+				$delegate.prototype.getBordersWidth = function() {
+					if (this.$element === undefined) {
+						return undefined;
+					}
+
+					if (this.$element[0].clientWidth) {
+						return this.$element[0].offsetWidth - this.$element[0].clientWidth;
+					} else {
+						//fix for IE11
+						var borderLeft = window.getComputedStyle(this.$element[0]).getPropertyValue('border-left-width') ? window.getComputedStyle(this.$element[0]).getPropertyValue('border-left-width').match(/\d+/)[0] : 0;
+						var borderRight = window.getComputedStyle(this.$element[0]).getPropertyValue('border-right-width') ? window.getComputedStyle(this.$element[0]).getPropertyValue('border-right-width').match(/\d+/)[0] : 0;
+
+						return parseInt(borderLeft) + parseInt(borderRight);
+					}
+				};
+				return $delegate;
+			}
+		]);
+	}]);
+
 	// gantt-column-header-content
 	angular.module('gantt').directive('ganttColumnHeaderContent', ganttColumnHeaderContent);
 
