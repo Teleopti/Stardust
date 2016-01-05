8eed9b1caeac7680d9ec136cbe92c47d97fcfa6c
Kunning Mao <none@none>
2016-01-05 09:11:43 +0100

    fix red build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/forecasting/forecasting.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/forecasting/forecasting.service.js
@@ -4,13 +4,17 @@
 	angular.module('wfm.forecasting')
 		.service('Forecasting', [
 			'$resource', function($resource) {
-				this.skills = $resource('../api/Forecasting/Skills', {}, {});
+				this.skills = $resource('../api/Forecasting/Skills', {}, {
+					query: { method: 'GET', params: {}, isArray: false }
+				});
 
 				this.status = $resource('../api/Status/Forecasting', {}, {
 					get: { method: 'GET', params: {}, isArray: false }
 				});
 
-				this.scenarios = $resource('../api/Forecasting/Scenarios', {}, {});
+				this.scenarios = $resource('../api/Forecasting/Scenarios', {}, {
+					query: { method: 'GET', params: {}, isArray: true }
+				});
 			}
 		]);
 })();
