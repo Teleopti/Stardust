ce63a7b64b3041b6ec50d27725f9e8df4b2b8978
johan Damm <none@none>
2016-01-07 11:21:54 +0100

    #36452 fixed tests

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/resourceplanner/_modules.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/resourceplanner/_modules.js
@@ -1,3 +1,3 @@
 ﻿(function() {
-	angular.module('wfm.resourceplanner', ['restResourcePlannerService', 'restPlanningPeriodService', 'ui.router']);
+	angular.module('wfm.resourceplanner', ['restResourcePlannerService', 'restPlanningPeriodService', 'ui.router','pascalprecht.translate']);
 })();
