ce1410623bd020568b32cd2f62aacb1b932615a8
Roger Kratz <roger.kratz@teleopti.com>
2016-01-08 16:48:26 +0100

    Clearer LogTime output.

--- Domain/Common/TimeLogger/LogTimeAspect.cs
+++ Domain/Common/TimeLogger/LogTimeAspect.cs
@@ -7,7 +7,7 @@ namespace Teleopti.Ccc.Domain.Common.TimeLogger
 {
 	public class LogTimeAspect : IAspect
 	{
-		private const string output = "Method '{0}' on type '{1}' took {2} seconds to run.";
+		private const string output = "Time spent in method '{0}' on type '{1}': {2}";
 		private const string loggerName = "Teleopti.LogTime";
 		private readonly ILogManagerWrapper _logger;
 		private readonly Stopwatch _stopwatch;
@@ -26,7 +26,7 @@ namespace Teleopti.Ccc.Domain.Common.TimeLogger
 		public void OnAfterInvocation(Exception exception, IInvocationInfo invocation)
 		{
 			_logger.GetLogger(loggerName)
-				.DebugFormat(output, invocation.Method.Name, invocation.Method.DeclaringType, _stopwatch.Elapsed.TotalSeconds);
+				.DebugFormat(output, invocation.Method.Name, invocation.Method.DeclaringType, _stopwatch.Elapsed.ToString(@"hh\:mm\:ss"));
 		}
 	}
 }
