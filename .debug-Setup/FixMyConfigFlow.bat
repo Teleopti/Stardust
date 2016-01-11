9e7bcc094393dc77979adb39d59a8cca27ce9d3c
Erik Sundberg <erik.sundberg@teleopti.com>
2016-01-11 17:18:47 +0100

    FixMyConfigFlow should work now

--- .debug-Setup/FixMyConfigFlow.bat
+++ .debug-Setup/FixMyConfigFlow.bat
@@ -4,7 +4,7 @@ SETLOCAL
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
 
-CALL :Branch %ROOTDIR%
+CALL :Branch %~dp0..
 SET CCC7DB=%Branch%_DemoSales_TeleoptiCCC7
 SET AnalyticsDB=%Branch%_DemoSales_TeleoptiAnalytics
 
@@ -13,5 +13,6 @@ CALL %ROOTDIR%\FixMyConfig.bat %CCC7DB% %AnalyticsDB%
 :Branch
 SET BRANCH=%~n1
 SET BRANCH=%BRANCH%%~x1
+GOTO :EOF
 
 ENDLOCAL
