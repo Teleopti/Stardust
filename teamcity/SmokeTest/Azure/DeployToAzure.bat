1d2963b201dd21aeae9756fd88b7c63ea9e09c4f
jimmy Broman <none@none>
2016-01-20 15:05:42 +0100

    Making script more dynamic

--- teamcity/SmokeTest/Azure/DeployToAzure.bat
+++ teamcity/SmokeTest/Azure/DeployToAzure.bat
@@ -2,11 +2,22 @@
 
 ::Deploy latest Azure package to Azure (teleoptirnd)
 SET ThisScriptsDirectory=%~dp0
+SET rootdir=%ThisScriptsDirectory:~0,-1%
+SET CloudServiceName=%1
 SET PowerShellScriptPath=%ThisScriptsDirectory%DeployToAzure.ps1
-SET AzurePackagePath=%~1
+SET AzurePackagePath=%ThisScriptsDirectory%AzurePackages
 IF "%AzurePackagePath%"=="" GOTO :NoAzurePackage
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' '%ThisScriptsDirectory%' 'Teleopti CCC Azure' 'teleoptirnd' '%AzurePackagePath%' '%ThisScriptsDirectory%AzureRelease\teleoptirnd.cscfg' 'production' '%ThisScriptsDirectory%AzureDemo.publishsettings'";
+@ECHO ***************************************************************
+@ECHO CloudServiceName			=	%CloudServiceName%
+@ECHO AzurePackagePath			=	%AzurePackagePath%
+@ECHO ThisScriptsDirectory		=	%ThisScriptsDirectory%
+@ECHO VersionedName				=	%VersionedName%
+@ECHO ***************************************************************
+
+FOR /F %%G IN ('DIR "%AzurePackagePath%\*.cspkg" /B') DO SET VersionedName=%%G
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' '%ThisScriptsDirectory%' 'Teleopti CCC Azure' '%CloudServiceName%' '%AzurePackagePath%\%VersionedName%' '%AzurePackagePath%\%CloudServiceName%.cscfg' 'production' '%ThisScriptsDirectory%AzureDemo.publishsettings'";
 EXIT %ERRORLEVEL%
 
 :NoAzurePackage
