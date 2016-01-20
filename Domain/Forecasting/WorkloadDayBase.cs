9d087a15480acc0310b58cae7c4a3cb88a29f9c4
Maria Stein <none@none>
2016-01-20 14:38:29 +0100

    Bug #36780: Campaign percentage increases after apply of templates

--- Domain/Forecasting/WorkloadDayBase.cs
+++ Domain/Forecasting/WorkloadDayBase.cs
@@ -1341,8 +1341,7 @@ namespace Teleopti.Ccc.Domain.Forecasting
                 _turnOffInternalRecalc = true;
                 if (_tasks > 0)
                 {
-	                double totalCamapaignTasks = _taskPeriodList.Sum(t => t.Task.Tasks * t.CampaignTasks.Value);
-	                _campaignTasks = new Percent(totalCamapaignTasks/_tasks);
+                    _campaignTasks = new Percent((_totalTasks / _tasks) - 1d);
                 }
                 _turnOffInternalRecalc = false;
 
@@ -1373,16 +1372,12 @@ namespace Teleopti.Ccc.Domain.Forecasting
             {
                 _turnOffInternalRecalc = true;
                 if (_averageTaskTime != TimeSpan.Zero)
-                {
-	                double totalCampaignTaskTime = _taskPeriodList.Sum(t => t.AverageTaskTime.Ticks * (1 + t.CampaignTaskTime.Value));
-						 double sumOfTaskTime = _taskPeriodList.Count * _averageTaskTime.Ticks;
-						 _campaignTaskTime = new Percent((totalCampaignTaskTime / sumOfTaskTime) - 1d);
+				{
+					_campaignTaskTime = new Percent((_totalAverageTaskTime.Ticks / (double)_averageTaskTime.Ticks) - 1d);
                 }
                 if (_averageAfterTaskTime != TimeSpan.Zero)
-					 {
-						 double totalCampaignAfterTaskTime = _taskPeriodList.Sum(t => t.AverageAfterTaskTime.Ticks * (1 + t.CampaignAfterTaskTime.Value));
-						 double sumOfAfterTaskTime = _taskPeriodList.Count * _averageAfterTaskTime.Ticks;
-						 _campaignAfterTaskTime = new Percent((totalCampaignAfterTaskTime / sumOfAfterTaskTime) - 1d);
+				{
+					_campaignAfterTaskTime = new Percent((_totalAverageAfterTaskTime.Ticks / (double)_averageAfterTaskTime.Ticks) - 1d);
                 }
                 _turnOffInternalRecalc = false;
 
@@ -1606,7 +1601,7 @@ namespace Teleopti.Ccc.Domain.Forecasting
                         ITemplateTaskPeriod newTemplateTaskPeriod = new TemplateTaskPeriod(
                             new Task(),
                             new DateTimePeriod(t, t.Add(resolutionAsTimeSpan)));
-                        if (IsEmailWorkload || IsWithinOpenHours(newTemplateTaskPeriod))
+                       if (IsEmailWorkload || IsWithinOpenHours(newTemplateTaskPeriod))
                         {
                             baseCollection.Add(newTemplateTaskPeriod);
                             newTemplateTaskPeriod.SetParent(this);
