0da9ed5c44fa31d1d8fc38d826606c4f5c5c75b8
Claes Stenman <claes.stenman@teleopti.com>
2016-01-22 13:58:49 +0100

    Failing test for #36617

---
+++ DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.Optimization.ScheduleOptimizationTests
+{
+	[DomainTest]
+	public class IntradayOptimizationTest
+	{
+		public FakeSkillRepository SkillRepository;
+		public FakePersonRepository PersonRepository;
+		public FakeSkillDayRepository SkillDayRepository;
+		public FakeScenarioRepository ScenarioRepository;
+		public FakePersonAssignmentRepository PersonAssignmentRepository;
+		public FakePlanningPeriodRepository PlanningPeriodRepository;
+
+		[Test, Ignore("To be fixed... #36617")]
+		public void ShouldMoveLunchToLowerDemandInterval()
+		{
+			//skifts med phone 8 - 17, lunch varje timme
+			//utgångsläge lunch 11 - 12
+			//efter lunch 10 -11
+			var phoneActivity = ActivityFactory.CreateActivity("phone");
+			var lunchActivity = ActivityFactory.CreateActivity("lunch");	
+			var skill = SkillRepository.Has("skill", phoneActivity);
+			var dateOnly = new DateOnly(2015, 10, 12);
+			var dateTime = TimeZoneHelper.ConvertToUtc(dateOnly.Date, skill.TimeZone);
+			var planningPeriod = PlanningPeriodRepository.Has(dateOnly, 1);
+			var scenario = ScenarioRepository.Has("some name");
+			var schedulePeriod = new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1);
+			var agent = PersonRepository.Has(new Contract("_"), new ContractSchedule("_"), new PartTimePercentage("_"), new Team { Site = new Site("site") }, schedulePeriod, skill);
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(17, 0, 17, 0, 15), shiftCategory));
+			ruleSet.AddExtender(new ActivityAbsoluteStartExtender(lunchActivity, new TimePeriodWithSegment(1, 0, 1, 0, 60), new TimePeriodWithSegment(8, 0, 16, 0, 60)));
+			
+			agent.Period(dateOnly).RuleSetBag = new RuleSetBag(ruleSet);
+	
+			SkillDayRepository.Has(new List<ISkillDay>
+				{
+					skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(10), new Tuple<int, TimeSpan>(10, TimeSpan.FromHours(5)))
+				});
+
+	
+			var assignment = new PersonAssignment(agent, scenario, dateOnly);
+			assignment.AddActivity(phoneActivity, new DateTimePeriod(dateTime.AddHours(8), dateTime.AddHours(17)));
+			assignment.AddActivity(lunchActivity, new DateTimePeriod(dateTime.AddHours(11), dateTime.AddHours(12)));
+			PersonAssignmentRepository.Add(assignment);
+
+			//Target.Doit(planning);
+
+			PersonAssignmentRepository.GetSingle(dateOnly).MainActivities().Single(x => x.Payload.Equals(lunchActivity)).Period
+				.Should().Be.EqualTo(new DateTimePeriod(dateTime.AddHours(10), dateTime.AddHours(11)));
+
+		}
+	}
+}
--- Teleopti.Ccc.TestCommon/SkillExtensions.cs
+++ Teleopti.Ccc.TestCommon/SkillExtensions.cs
@@ -8,6 +8,33 @@ namespace Teleopti.Ccc.TestCommon
 {
 	public static class SkillExtensions
 	{
+		public static ISkillDay CreateSkillDayWithDemandPerHour(this ISkill skill, IScenario scenario, DateOnly dateOnly, TimeSpan defaultDemand, Tuple<int, TimeSpan> specificHourDemand)
+		{
+			var skillDataPeriods = new List<ISkillDataPeriod>();
+			for (var hour = 0; hour < 24; hour++)
+			{
+				var dateTime = TimeZoneHelper.ConvertToUtc(dateOnly.Date, skill.TimeZone);
+				var period = new DateTimePeriod(dateTime.AddHours(hour), dateTime.AddHours(hour + 1));
+				var demand = specificHourDemand.Item1 == hour ? 
+					specificHourDemand.Item2 : 
+					defaultDemand;
+				skillDataPeriods.Add(new SkillDataPeriod(ServiceAgreement.DefaultValues(), new SkillPersonData(),period) {ManualAgents = demand.TotalHours});
+
+			}
+
+			var workloadDays = new List<IWorkloadDay>();
+			var workloadDay = new WorkloadDay();
+			var workload = skill.WorkloadCollection.First();
+			workloadDay.CreateFromTemplate(dateOnly, workload, (IWorkloadDayTemplate)workload.GetTemplate(TemplateTarget.Workload, dateOnly.DayOfWeek));
+			workloadDays.Add(workloadDay);
+
+			var skillDay = new SkillDay(dateOnly, skill, scenario, workloadDays, skillDataPeriods);
+
+			skillDay.SkillDayCalculator = new SkillDayCalculator(skill, new List<ISkillDay> { skillDay }, new DateOnlyPeriod(dateOnly, dateOnly));
+			return skillDay;
+		}
+
+
 		public static ISkillDay CreateSkillDayWithDemand(this ISkill skill, IScenario scenario, DateOnly dateOnly, TimeSpan demand)
 		{
 			var skillDataPeriods = new List<ISkillDataPeriod>();
