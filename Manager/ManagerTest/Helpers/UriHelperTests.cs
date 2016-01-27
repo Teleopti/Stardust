8e81170fd5951f09c44f63a38ebb3375d09a57b3
antonskripec <anton.skripec@hotmail.com>
2016-01-27 13:03:50 +0100

    Uri helper.

---
+++ Manager/ManagerTest/Helpers/UriHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Stardust.Manager.Helpers;
+
+namespace ManagerTest.Helpers
+{
+    [TestFixture()]
+    public class UriHelperTests
+    {
+        [Test()]
+        public void CreateCorrectUriTest()
+        {
+            Uri uri = UriHelper.CreateCorrectUri("localhost",
+                                                 null);
+
+            Assert.IsNotNull(uri);
+        }
+    }
+}
