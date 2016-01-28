ef8f3f3db46cf6cb08084315f2b9b1b8a5f86cbf
antonskripec <anton.skripec@hotmail.com>
2016-01-28 12:31:45 +0100

    Added more files.

---
+++ Manager/Manager/Helpers/NodeUriBuilderHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Stardust.Manager.Constants;
+
+namespace Stardust.Manager.Helpers
+{
+    public class NodeUriBuilderHelper
+    {
+        private UriBuilder UriBuilder { get; set; }
+
+        private Uri LocationUri { get; set; }
+
+        public NodeUriBuilderHelper(Uri locationUri) : this(locationUri.ToString())
+        {
+        }
+
+        public NodeUriBuilderHelper(string locationUri)
+        {
+            LocationUri = new Uri(locationUri);
+
+            UriBuilder = new UriBuilder
+            {
+                Host = LocationUri.Host,
+                Port = LocationUri.Port,
+                Scheme = LocationUri.Scheme
+            };
+        }
+
+        public Uri GetIsAliveTemplateUri()
+        {
+            return CreateUri(NodeRouteConstants.IsAlive);
+        }
+
+        public Uri GetJobTemplateUri()
+        {
+            return CreateUri(NodeRouteConstants.Job);
+        }
+
+        public Uri GetCancelJobTemplateUri()
+        {
+            return CreateUri(NodeRouteConstants.CancelJob);
+        }
+
+        public Uri GetCancelJobUri(Guid guid)
+        {
+            var path = NodeRouteConstants.CancelJob.Replace(NodeRouteConstants.JobIdOptionalParameter,
+                                                            guid.ToString());
+
+            return CreateUri(path);
+        }
+
+        public Uri CreateUri(string path)
+        {
+            UriBuilder.Path = path;
+
+            return UriBuilder.Uri;
+        }
+
+        public string GetHostName()
+        {
+            return UriBuilder.Host;
+        }
+
+        public int GetPort()
+        {
+            return UriBuilder.Port;
+        }
+
+        public string GetScheme()
+        {
+            return UriBuilder.Scheme;
+        }
+
+        public Uri GetLocationUri()
+        {
+            return UriBuilder.Uri;
+        }
+    }
+}
