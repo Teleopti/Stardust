f468310b45fe5e646a0321aa3ab1de643871387b
Claes Stenman <claes.stenman@teleopti.com>
2016-02-05 11:04:41 +0100

    PBI:#36999: - dont use SkillDayIntraIntervalFinder on all resourcecalculations, Added ignored test

--- DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationTest.cs
+++ DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationTest.cs
@@ -236,5 +236,43 @@ namespace Teleopti.Ccc.DomainTest.Optimization.ScheduleOptimizationTests
 
 			hasResource.Should().Be.True();
 		}
+
+		[Test, Ignore]
+		public void ShouldNotCalculateIntraIntervalIssuses()
+		{
+			var phoneActivity = ActivityFactory.CreateActivity("phone");
+			phoneActivity.RequiresSkill = true;
+			var skill = SkillRepository.Has("skill", phoneActivity);
+			var dateOnly = new DateOnly(2015, 10, 12);
+			var planningPeriod = PlanningPeriodRepository.Has(dateOnly.AddDays(-6), 1);
+			var scenario = ScenarioRepository.Has("some name");
+			var schedulePeriod = new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1);
+			var worktimeDirective = new WorkTimeDirective(TimeSpan.FromHours(36), TimeSpan.FromHours(63), TimeSpan.FromHours(11), TimeSpan.FromHours(36));
+			var contract = new Contract("contract") { WorkTimeDirective = worktimeDirective, PositivePeriodWorkTimeTolerance = TimeSpan.FromHours(9) };
+			var agent = PersonRepository.Has(contract, new ContractSchedule("_"), new PartTimePercentage("_"), new Team { Site = new Site("site") }, schedulePeriod, skill);
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(8, 15, 8, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), shiftCategory));
+			agent.Period(dateOnly).RuleSetBag = new RuleSetBag(ruleSet);
+
+			SkillDayRepository.Has(new List<ISkillDay>
+				{
+					skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(60), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(360)))
+				});
+
+			var dateTime = TimeZoneHelper.ConvertToUtc(dateOnly.Date, agent.PermissionInformation.DefaultTimeZone());
+			var assignment = new PersonAssignment(agent, scenario, dateOnly);
+			assignment.SetShiftCategory(shiftCategory);
+			assignment.AddActivity(phoneActivity, new DateTimePeriod(dateTime.AddHours(8), dateTime.AddHours(8).AddMinutes(10)));
+			PersonAssignmentRepository.Add(assignment);
+
+			Target.Optimize(planningPeriod.Id.Value);
+
+			var skillDays = SkillDayRepository.FindReadOnlyRange(new DateOnlyPeriod(dateOnly, dateOnly), new List<ISkill> { skill }, scenario);
+			var skillStaffPeriods = skillDays.First().SkillStaffPeriodCollection;
+			foreach (var skillStaffPeriod in skillStaffPeriods)
+			{
+				skillStaffPeriod.HasIntraIntervalIssue.Should().Be.False();
+			}	
+		}
 	}
 }
