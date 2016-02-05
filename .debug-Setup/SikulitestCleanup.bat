c24dfd28f603d9949abe985a1cceffb57a88b886
jimmy Broman <none@none>
2016-02-05 14:40:10 +0100

    added if exist %folder%

--- .debug-Setup/SikulitestCleanup.bat
+++ .debug-Setup/SikulitestCleanup.bat
@@ -1,5 +1,7 @@
 taskkill /F /IM iisexpress.exe
 
 set folder="C:\temp\AdminClient"
+IF EXIST "%folder%" (
 cd /d %folder%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+)
