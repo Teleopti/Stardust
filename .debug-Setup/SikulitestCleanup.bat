b60212db568077860cc4771b0d35d2f82d0f6826
Roger Kratz <roger.kratz@teleopti.com>
2016-02-05 17:42:57 +0100

    Rolling back Jimmys changes for now to see if sikuli starts to work again

--- .debug-Setup/SikulitestCleanup.bat
+++ .debug-Setup/SikulitestCleanup.bat
@@ -1,9 +1,1 @@
 taskkill /F /IM iisexpress.exe
-
-ping 127.0.0.1 -n 11 > nul
-
-set folder="C:\temp\AdminClient"
-IF EXIST "%folder%" (
-cd /d %folder%
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-)
