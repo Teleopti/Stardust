8bca8026f9ab6851e27f71d82b7d0263f306bf55
antonskripec <anton.skripec@hotmail.com>
2016-02-08 09:41:06 +0100

    Added more statuses.

--- Manager.Integration/Manager.Integration.Test.WPF/ViewModels/MainWindowViewModel.cs
+++ Manager.Integration/Manager.Integration.Test.WPF/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,23 @@ namespace Manager.Integration.Test.WPF.ViewModels
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private const string WorkerNodeHeaderConstant = "Worker Node";
+        private const string LoggingHeaderConstant = "Logging";
+        private const string JobHistoryHeaderConstant = "Job History";
+        private const string JobHistoryDetailHeaderConstant = "Job History Detail";
+        private const string JobDefinitionHeaderConstant = "Job Definition";
+
+        public string WorkerNodeHeader
+        {
+            get { return _workerNodeHeader; }
+            set
+            {
+                _workerNodeHeader = value;
+                OnPropertyChanged();
+
+            }
+        }
+
         private string _status;
 
         public string Status
@@ -69,6 +86,11 @@ namespace Manager.Integration.Test.WPF.ViewModels
         private List<JobHistoryDetail> _jobHistoryDetailData;
         private List<JobDefinition> _jobDefinitionData;
         private List<WorkerNode> _workerNodesData;
+        private string _workerNodeHeader;
+        private string _loggingHeader;
+        private string _jobHistoryHeader;
+        private string _jobHistoryDetailHeader;
+        private string _jobDefinitionDataHeader;
 
         public List<Logging> LoggingData
         {
@@ -92,25 +114,78 @@ namespace Manager.Integration.Test.WPF.ViewModels
                     managerDbEntities.Loggings.OrderByDescending(logging => logging.Id)
                         .ToList();
 
+                LoggingHeader = LoggingHeaderConstant + " ( " + LoggingData.Count + " )";
+
                 JobHistoryData =
                     managerDbEntities.JobHistories.OrderByDescending(history => history.Created)
                         .ToList();
 
+                JobHistoryHeader = JobHistoryHeaderConstant + " ( " + JobHistoryData.Count + " )";
+                
                 JobHistoryDetailData =
                     managerDbEntities.JobHistoryDetails.OrderByDescending(history => history.Created)
                         .ToList();
+
+                JobHistoryDetailHeader = JobHistoryDetailHeaderConstant + " ( " + JobHistoryDetailData.Count + " )";
 
                 JobDefinitionData =
                     managerDbEntities.JobDefinitions
                         .ToList();
 
+                JobDefinitionDataHeader = JobDefinitionHeaderConstant + " ( " + JobDefinitionData.Count + " )";
+
                 WorkerNodesData  =
                     managerDbEntities.WorkerNodes
                         .ToList();
 
+                WorkerNodeHeader = WorkerNodeHeaderConstant + " ( " + WorkerNodesData.Count + " )";
             }
 
             Status = "Refresh finished.";
+        }
+
+        public string JobDefinitionDataHeader
+        {
+            get { return _jobDefinitionDataHeader; }
+            set
+            {
+                _jobDefinitionDataHeader = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string JobHistoryDetailHeader
+        {
+            get { return _jobHistoryDetailHeader; }
+            set
+            {
+                _jobHistoryDetailHeader = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string JobHistoryHeader
+        {
+            get { return _jobHistoryHeader; }
+            set
+            {
+                _jobHistoryHeader = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string LoggingHeader
+        {
+            get { return _loggingHeader; }
+            set
+            {
+                _loggingHeader = value;
+
+                OnPropertyChanged();
+            }
         }
 
         public List<WorkerNode> WorkerNodesData
@@ -157,5 +232,6 @@ namespace Manager.Integration.Test.WPF.ViewModels
                 OnPropertyChanged();
             }
         }
+
     }
 }
