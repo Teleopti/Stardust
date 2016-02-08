aeb3a59aff3a85992e8d87e4a5d91981940f7753
antonskripec <anton.skripec@hotmail.com>
2016-02-08 11:13:38 +0100

    Added controller.

---
+++ Manager.Integration/Manager.Integration.Tests.Console.Host/ManagerIntegrationTestcontroller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using log4net;
+
+namespace Manager.IntegrationTest.Console.Host
+{
+    public class ManagerIntegrationTestController : ApiController
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof (ManagerIntegrationTestController));
+
+        public string WhoAmI { get; private set; }
+
+        public ManagerIntegrationTestController()
+        {
+            WhoAmI = "[MANAGER INTEGRATION TEST CONTROLLER, " + Environment.MachineName.ToUpper() + "]";
+        }
+
+        [HttpGet, Route("appdomains")]
+        public IEnumerable<AppDomain> GetAllAppDomains()
+        {
+            return Program.AppDomains.Values;
+        }
+    }
+}
