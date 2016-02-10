8a0135b7e9c3255e1946cecdbf30c89ed556a178
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-02-10 14:04:53 +0100

    Refact cleaning config setup

--- .debug-Setup/FixMyConfig.bat
+++ .debug-Setup/FixMyConfig.bat
@@ -2,12 +2,10 @@
 SETLOCAL
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
-SET masterSettings=%ROOTDIR%\config\settings.txt
 SET CCC7DB=%1
 SET AnalyticsDB=%2
 SET Configuration=%3
 SET MSBUILD="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-SET MySettings=%ROOTDIR%\..\Teleopti.Support.Tool\settings.txt
 
 SET validationKey=754534E815EF6164CE788E521F845BA4953509FA45E321715FDF5B92C5BD30759C1669B4F0DABA17AC7BBF729749CE3E3203606AB49F20C19D342A078A3903D1
 SET decryptionKey=3E1AD56713339011EB29E98D1DF3DBE1BADCF353938C3429
@@ -23,20 +21,20 @@ if "%2" == "" (
 SET /P AnalyticsDB=AnalyticsDB?
 )
 
-::get a fresh Settings.txt
-COPY "%masterSettings%" "%MySettings%"
+SET SourceSettings=%ROOTDIR%\config\settings.txt
+SET AppliedSettings=%ROOTDIR%\..\Teleopti.Support.Tool\bin\%Configuration%\settings.txt
+
+COPY "%SourceSettings%" "%AppliedSettings%"
 
 ::Replace some parameters according to current RestoreToLocal.bat
-cscript .\common\replace.vbs "TeleoptiAnalytics_Demo" "%AnalyticsDB%" "%MySettings%" > NUL
-cscript .\common\replace.vbs "TeleoptiApp_Demo" "%CCC7DB%" "%MySettings%" > NUL
-
+cscript .\common\replace.vbs "TeleoptiAnalytics_Demo" "%AnalyticsDB%" "%AppliedSettings%" > NUL
+cscript .\common\replace.vbs "TeleoptiApp_Demo" "%CCC7DB%" "%AppliedSettings%" > NUL
 
 IF NOT EXIST "%ROOTDIR%\..\Teleopti.Support.Tool\bin\%Configuration%\Teleopti.Support.Tool.exe" (
 	::Build Teleopti.Support.Tool.exe
 	ECHO Building %ROOTDIR%\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj
 	IF EXIST "%ROOTDIR%\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" %MSBUILD% /property:Configuration=%Configuration% /t:rebuild "%ROOTDIR%\..\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" > "%ROOTDIR%\Teleopti.Support.Tool.build.log"
 )
-COPY "%MySettings%" "%ROOTDIR%\..\Teleopti.Support.Tool\bin\%Configuration%\settings.txt"
 
 ::add fixed encryption/decryption keys to match the ones used in WebTest
 ECHO %validationKey%>"%ROOTDIR%\..\Teleopti.Support.Tool\bin\%Configuration%\validation.key"
--- .debug-Setup/InfratestConfig.bat
+++ .debug-Setup/InfratestConfig.bat
@@ -2,7 +2,6 @@
 SETLOCAL
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-14%
-SET masterSettings=%ROOTDIR%\.debug-setup\config\settingsInfraTest.txt
 SET CCC7DB=%~1
 SET AnalyticsDB=%~2
 SET ToggleMode=%~3
@@ -28,32 +27,23 @@ IF ERRORLEVEL 2 SET ToggleMode=RC
 IF ERRORLEVEL 3 SET ToggleMode=R
 )
 
-SET MySettings=%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\settings.txt
+SET SourceSettings=%ROOTDIR%\.debug-setup\config\settingsInfraTest.txt
+SET AppliedSettings=%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\settings.txt
 
 ::Build Teleopti.Support.Tool.exe if source files are available (they aren't in pipeline)
 if exist "%ROOTDIR%\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" %MSBUILD% /t:build "%ROOTDIR%\Teleopti.Support.Tool\Teleopti.Support.Tool.csproj" /p:Configuration=%configuration%
 
 ::get a fresh Settings.txt
-COPY "%masterSettings%" "%MySettings%"
-ECHO. >> "%MySettings%"
-ECHO $(DB_CCC7)^|%CCC7DB%>>"%MySettings%"
-ECHO $(DB_ANALYTICS)^|%AnalyticsDB%>>"%MySettings%"
-ECHO $(AS_DATABASE)^|%AnalyticsDB%>>"%MySettings%"
-ECHO $(TOGGLE_MODE)^|%ToggleMode%>>"%MySettings%"
-
-::telling what config to modify
-set configFilesFolder=%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\ConfigFiles
-set buildServerConfigFiles="%configFilesFolder%\BuildServerConfigFiles.txt"
-if not exist "%configFilesFolder%" mkdir "%configFilesFolder%"
-echo ..\..\..\Teleopti.Analytics\Teleopti.Analytics.Etl.IntegrationTest\App.config,BuildArtifacts\Teleopti.Ccc.TestCommon.App.config>%buildServerConfigFiles%
-echo ..\..\..\InfrastructureTest\App.config,BuildArtifacts\Teleopti.Ccc.TestCommon.App.config>>%buildServerConfigFiles%
-echo ..\..\..\Teleopti.Ccc.ApplicationConfigTest\App.config,BuildArtifacts\Teleopti.Ccc.TestCommon.App.config>>%buildServerConfigFiles%
-echo ..\..\..\Teleopti.Ccc.Web\Teleopti.Ccc.WebBehaviorTest\App.config,BuildArtifacts\Teleopti.Ccc.WebBehaviorTest.config>>%buildServerConfigFiles%
-echo ..\..\..\Teleopti.Ccc.Web\Teleopti.Ccc.WebBehaviorTest\bin\%configuration%\Teleopti.Ccc.WebBehaviorTest.dll.config,BuildArtifacts\Teleopti.Ccc.WebBehaviorTest.config>>%buildServerConfigFiles%
-echo ..\..\..\Teleopti.Wfm.AdministrationTest\App.config,BuildArtifacts\Teleopti.Ccc.TestCommon.App.config>>%buildServerConfigFiles%
+COPY "%SourceSettings%" "%AppliedSettings%"
+ECHO. >> "%AppliedSettings%"
+ECHO $(DB_CCC7)^|%CCC7DB%>>"%AppliedSettings%"
+ECHO $(DB_ANALYTICS)^|%AnalyticsDB%>>"%AppliedSettings%"
+ECHO $(AS_DATABASE)^|%AnalyticsDB%>>"%AppliedSettings%"
+ECHO $(TOGGLE_MODE)^|%ToggleMode%>>"%AppliedSettings%"
 
 ::Run supportTool to replace all config
 "%ROOTDIR%\Teleopti.Support.Tool\bin\%configuration%\Teleopti.Support.Tool.exe" -MOTEST
 
 ENDLOCAL
 goto:eof
+
