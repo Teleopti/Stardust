93ddc15fce48407c6f8b8018c08b6772d08b8568
Carl.Rockman@teleopti.com <Carl.Rockman@teleopti.com>
2016-02-11 10:49:06 +0100

    basic intraday overview layout

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/intraday/intraday.controller.js
@@ -22,6 +22,47 @@
 				$state.go('intraday.config', {});
 			};
 
+			$scope.modalShown = false;
+			$scope.toggleModal = function() {
+				$scope.modalShown = !$scope.modalShown;
+			};
+
+			$scope.demos = [
+				{id: 'Skill name1'},
+				{id: 'Skill name2'},
+				{id: 'Skill name3'},
+				{id: 'Skill name4'},
+				{id: 'Skill name5'},
+				{id: 'Skill name6'},
+				{id: 'Skill name7'},
+				{id: 'Skill name8'},
+				{id: 'Skill name9'},
+				{id: 'Skill name10'},
+				{id: 'Skill name11'},
+				{id: 'Skill name12'},
+				{id: 'Skill name13'},
+				{id: 'Skill name14'},
+				{id: 'Skill name15'},
+				{id: 'Skill name16'},
+				{id: 'Skill name17'},
+				{id: 'Skill name18'},
+				{id: 'Skill name19'},
+				{id: 'Skill name20'},
+				{id: 'Skill name21'},
+				{id: 'Skill name22'},
+				{id: 'Skill name23'},
+				{id: 'Skill name24'},
+				{id: 'Skill name25'},
+				{id: 'Skill name26'},
+				{id: 'Skill name27'}
+			];
+
+			$scope.demos2 = [
+				{id: 'This area'},
+				{id: 'Another area'},
+				{id: 'My area'},
+				{id: 'Your area'}
+			];
 
 			$scope.reload = setInterval(function () {
 				intradayService.skillList.query().$promise.then(function (result) {
