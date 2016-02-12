8c38e62d87d64ba2b724a4871f0bf775eff85f3d
Roger Kratz <roger.kratz@teleopti.com>
2016-02-12 14:33:20 +0100

    #37048 Adding test + impl for unactive skills and stuff

--- Domain/Scheduling/Assignment/ResourceCalculateAfterDeleteDecider.cs
+++ Domain/Scheduling/Assignment/ResourceCalculateAfterDeleteDecider.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Teleopti.Ccc.Domain.DayOffPlanning;
 using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Domain.Scheduling.Assignment
 {
@@ -18,11 +19,34 @@ namespace Teleopti.Ccc.Domain.Scheduling.Assignment
 
 		public bool DoCalculation(IPerson agent, DateOnly date)
 		{
-			var virtualSkillGroupsCreator = new VirtualSkillGroupsCreator();
-			var result = virtualSkillGroupsCreator.GroupOnDate(date, _scheduleResultStateHolder().PersonsInOrganization);
-			var persons =  result.GetPersonsForSkillGroupKey(agent.Period(date).PersonSkillCollection.First().Skill.Id.ToString());
+			var count = 0;
+			var agentPeriod = agent.Period(date);
+			if (agentPeriod == null)
+				return false;
 
-			return persons.Count() < _limitForNoResourceCalculation.NumberOfAgents;
+			foreach (var person in _scheduleResultStateHolder().PersonsInOrganization)
+			{
+				var otherPeriod = person.Period(date);
+				if(otherPeriod == null) continue;
+				if (hasSameSkills(otherPeriod, agentPeriod))
+					count++;
+			}
+
+			return count < _limitForNoResourceCalculation.NumberOfAgents;
+		}
+
+		private bool hasSameSkills(IPersonPeriod personPeriod1, IPersonPeriod personPeriod2)
+		{
+			var skills1 = personPeriod1.PersonSkillCollection.Where(x => x.Active).Select(x => x.Skill).Where(x => !((IDeleteTag)x).IsDeleted);
+			var skills2 = personPeriod2.PersonSkillCollection.Where(x => x.Active).Select(x => x.Skill).Where(x => !((IDeleteTag)x).IsDeleted);
+			if (skills1.Count() != skills2.Count())
+				return false;
+			foreach (var skill in skills1)
+			{
+				if (!skills2.Contains(skill))
+					return false;
+			}
+			return true;
 		}
 	}
 }
--- DomainTest/Scheduling/Assignment/ResourceCalculateAfterDeleteDeciderTest.cs
+++ DomainTest/Scheduling/Assignment/ResourceCalculateAfterDeleteDeciderTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.AgentInfo;
@@ -90,6 +91,103 @@ namespace Teleopti.Ccc.DomainTest.Scheduling.Assignment
 			SchedulingResultStateHolder.PersonsInOrganization = new[] { agent1, agent2 };
 
 			Target.DoCalculation(agent1, date)
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldDoCalculationIfAgentHasOnlyPartOfOthersSkill()
+		{
+			LimitForNoResourceCalculation.SetFromTest(2);
+
+			var date = new DateOnly(2000, 1, 1);
+			var me = new Person();
+			var otherAgent1 = new Person();
+			var otherAgent2= new Person();
+			me.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			otherAgent1.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			otherAgent2.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			var commonSkill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony)).WithId();
+			var myExtraSkill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony)).WithId();
+
+			me.AddSkill(commonSkill, date);
+			me.AddSkill(myExtraSkill, date);
+			otherAgent1.AddSkill(commonSkill, date);
+			otherAgent2.AddSkill(commonSkill, date);
+			SchedulingResultStateHolder.PersonsInOrganization = new[] { me, otherAgent1, otherAgent2 };
+
+			Target.DoCalculation(me, date)
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldNotIncludeDeletedSkills()
+		{
+			LimitForNoResourceCalculation.SetFromTest(2);
+
+			var date = new DateOnly(2000, 1, 1);
+			var agent1 = new Person();
+			var agent2 = new Person();
+			agent1.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			agent2.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			var skill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony));
+			var deletedSkill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony));
+			deletedSkill.SetDeleted();
+			agent1.AddSkill(skill, date);
+			agent2.AddSkill(skill, date);
+			agent2.AddSkill(deletedSkill, date);
+			SchedulingResultStateHolder.PersonsInOrganization = new[] { agent1, agent2 };
+
+			Target.DoCalculation(agent1, date)
+				.Should().Be.False();
+		}
+
+		[Test]
+		public void ShouldNotIncludeUnAcitivePersonSkills()
+		{
+			LimitForNoResourceCalculation.SetFromTest(2);
+
+			var date = new DateOnly(2000, 1, 1);
+			var agent1 = new Person();
+			var agent2 = new Person();
+			agent1.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			agent2.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			var skill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony));
+			var inActiveSkill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony));
+			
+			agent1.AddSkill(skill, date);
+			agent2.AddSkill(skill, date);
+			agent2.AddSkill(inActiveSkill, date);
+			((PersonSkill)agent2.Period(date).PersonSkillCollection.Last()).Active = false;
+			SchedulingResultStateHolder.PersonsInOrganization = new[] { agent1, agent2 };
+
+			Target.DoCalculation(agent1, date)
+				.Should().Be.False();
+		}
+
+		[Test]
+		public void ShouldNotCalculateIfAgentHasNoCurrentPersonPeriod()
+		{ 
+			var date = new DateOnly(2000, 11, 11);
+			var person = new Person();
+			person.AddPersonPeriod(new PersonPeriod(date.AddDays(10), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+
+			SchedulingResultStateHolder.PersonsInOrganization = new[] { person };
+
+			Target.DoCalculation(person, date)
+				.Should().Be.False();
+		}
+
+		[Test]
+		public void ShouldCalculateIfOtherAgentsHasNoPersonPeriod()
+		{
+			var date = new DateOnly(2000, 1, 1);
+			var me = new Person();
+			me.AddPersonPeriod(new PersonPeriod(new DateOnly(1900, 1, 1), new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team()));
+			var skill = new Skill("_", "", Color.Empty, 1, new SkillTypePhone(new Description("_"), ForecastSource.OutboundTelephony)).WithId();
+			me.AddSkill(skill, date);
+			SchedulingResultStateHolder.PersonsInOrganization = new[] { me, new Person(), new Person(), new Person(), new Person() };
+
+			Target.DoCalculation(me, date)
 				.Should().Be.True();
 		}
 
