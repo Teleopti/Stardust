3517fd6889221260301c8da81c879e0fc02f725d
Ola <ola@teleopti.com>
2016-02-13 08:32:48 +0100

    use some more unusual ports

--- .debug-Setup/SetUrlAcl.ps1
+++ .debug-Setup/SetUrlAcl.ps1
@@ -1,17 +1,17 @@
 ﻿
-$global:value = (netsh http show urlacl url=http://+:9000/)
+$global:value = (netsh http show urlacl url=http://+:14000/)
 
-if(!($global:value  -like '*http://+:9000/*'))
+if(!($global:value  -like '*http://+:14000/*'))
 {
-    $global:value = (netsh http add urlacl url=http://+:9000/ user=Everyone listen=yes)
+    $global:value = (netsh http add urlacl url=http://+:14000/ user=Everyone listen=yes)
 
-    if($global:value  -like '*5*')
+    if($global:value  -like '*Error: 5*')
     {
         Write-host 'You must run this as administrator to add permissions to listen on ports'
         pause
         Exit   
     }
-    netsh http add urlacl url=http://+:9001/ user=Everyone listen=yes
+    netsh http add urlacl url=http://+:14100/ user=Everyone listen=yes
 }
 
 
