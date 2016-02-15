e0adc7fa29e3f264c2b959e6a5b3cb4637e38baa
jimmy Broman <none@none>
2016-02-15 13:43:09 +0100

    Added a elevated right step in the beginning

--- .debug-Setup/SetUrlAcl.ps1
+++ .debug-Setup/SetUrlAcl.ps1
@@ -1,4 +1,37 @@
-﻿
+﻿# Check id and sec principal of current user
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+# Get sec principal for the Administrator
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   # If we are running as admin, change background colour to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + '(Elevated)'
+   $Host.UI.RawUI.BackgroundColor = 'DarkGreen'
+   clear-host
+   }
+else
+   {
+   # Relaunch as admin if needed
+   # Create a new process and start Powershell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+   
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+   
+   # Exit from the current, unelevated, process
+   exit
+   }
+
 $global:value = (netsh http show urlacl url=http://+:14000/)
 
 if(!($global:value  -like '*http://+:14000/*'))
