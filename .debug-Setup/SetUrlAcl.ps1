b683c558cf7e2ec3f07b81917333fd960e09419b
jimmy Broman <none@none>
2016-02-15 09:42:16 +0100

    Pause causing win scenario to fail. Added a short wait time instead

--- .debug-Setup/SetUrlAcl.ps1
+++ .debug-Setup/SetUrlAcl.ps1
@@ -8,7 +8,7 @@ if(!($global:value  -like '*http://+:14000/*'))
     if($global:value  -like '*Error: 5*')
     {
         Write-host 'You must run this as administrator to add permissions to listen on ports'
-        pause
+        start-sleep -seconds 5
         Exit   
     }
     netsh http add urlacl url=http://+:14100/ user=Everyone listen=yes
