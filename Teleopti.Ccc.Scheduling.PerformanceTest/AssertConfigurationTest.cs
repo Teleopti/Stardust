78b368a9543636927f4aa12a2949d6811f99fa8d
Roger Kratz <roger.kratz@teleopti.com>
2016-02-15 16:12:30 +0100

    #37048 Adding test making sure perf tests crashes if not run in DEV mode.
    Also, update "template" config to correct settings.
    
    Needs to update app.config on devbuild01 according to these changes.

---
+++ Teleopti.Ccc.Scheduling.PerformanceTest/AssertConfigurationTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Infrastructure.Toggle;
+using Teleopti.Ccc.TestCommon.Web.WebInteractions;
+
+namespace Teleopti.Ccc.Scheduling.PerformanceTest
+{
+	public class AssertConfigurationTest
+	{
+		[Test]
+		public void ShouldRunInToggleDevMode()
+		{
+			var toggleQuerier = new ToggleQuerier(TestSiteConfigurationSetup.URL.ToString());
+			toggleQuerier.IsEnabled(Toggles.TestToggle).Should().Be.True();
+		}
+	}
+}
