f3a930007c151cd26946b1df803886adc042ceaf
Roger Kratz <roger.kratz@teleopti.com>
2016-02-17 11:15:19 +0100

    Making more components singleinstance

--- Domain/Optimization/TeamBlock/FairnessOptimization/Seniority/PersonStartDateFromPersonPeriod.cs
+++ Domain/Optimization/TeamBlock/FairnessOptimization/Seniority/PersonStartDateFromPersonPeriod.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Teleopti.Interfaces.Domain;
+﻿using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Optimization.TeamBlock.FairnessOptimization.Seniority
 {
--- Domain/Optimization/TeamBlock/FairnessOptimization/SeniorityDaysOff/TeamBlockLocatorWithHigestPoints.cs
+++ Domain/Optimization/TeamBlock/FairnessOptimization/SeniorityDaysOff/TeamBlockLocatorWithHigestPoints.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Teleopti.Ccc.Domain.Scheduling.TeamBlock;
 
 namespace Teleopti.Ccc.Domain.Optimization.TeamBlock.FairnessOptimization.SeniorityDaysOff
