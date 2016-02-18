8fe395a83c365990ffae6b32e71fba9743837469
jimmy Broman <none@none>
2016-02-18 15:30:15 +0100

    Needed on WFMSmoke for Sikuli to be able to download client

---
+++ teamcity/SmokeTest/Set-AnonAuthTrue.ps1
@@ -0,0 +1,11 @@
+﻿$Username = 'toptinet\tfsintegration'
+$Password = 'm8kemew0rk'
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+
+invoke-command -ComputerName WFMSmoke -Credential $cred -ScriptBlock {
+
+Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/AnonymousAuthentication" `
+                             -Name Enabled `
+                             -Value True `
+                             -PSPath "IIS:\Sites\Default Web Site\TeleoptiWFM\Client" }
