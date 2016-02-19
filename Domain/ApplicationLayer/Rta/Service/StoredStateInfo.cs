921e05927224a0c7f932541bf7e653fa3c799c89
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-02-19 11:08:40 +0100

    #35329 Rename refacts, to make the rule vs alarm clearer

--- Domain/ApplicationLayer/Rta/Service/StoredStateInfo.cs
+++ Domain/ApplicationLayer/Rta/Service/StoredStateInfo.cs
@@ -18,17 +18,21 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 			PlatformTypeId = fromStorage.PlatformTypeId;
 			SourceId = fromStorage.OriginalDataSourceId;
 			ReceivedTime = fromStorage.ReceivedTime;
+
 			StateCode = fromStorage.StateCode;
 			StateGroupId = fromStorage.StateId;
+			StateStartTime = fromStorage.StateStartTime;
+
 			ActivityId = fromStorage.ScheduledId;
 			NextActivityId = fromStorage.ScheduledNextId;
 			NextActivityStartTime = fromStorage.NextStart;
+
 			RuleId = fromStorage.RuleId;
-			AlarmStartTime = fromStorage.AlarmStartTime;
 			RuleStartTime = fromStorage.RuleStartTime;
-			StateStartTime = fromStorage.StateStartTime;
 			StaffingEffect = fromStorage.StaffingEffect;
 			Adherence = (Adherence?)fromStorage.Adherence;
+
+			AlarmStartTime = fromStorage.AlarmStartTime;
 		}
 
 		public DateTime? BatchId { get; set; }
@@ -39,44 +43,35 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 		public DateTime ReceivedTime { get; set; }
 		public string StateCode { get; set; }
 		public Guid? StateGroupId { get; set; }
-		public Guid? ActivityId { get; set; }
+		public DateTime? StateStartTime { get; set; }
 
+		public Guid? ActivityId { get; set; }
 		public Guid? NextActivityId { get; set; }
 		public DateTime? NextActivityStartTime { get; set; }
 
 		public Guid? RuleId { get; set; }
 		public DateTime? RuleStartTime { get; set; }
-		public DateTime? StateStartTime { get; set; }
 		public double? StaffingEffect { get; set; }
 		public Adherence? Adherence { get; set; }
+
 		public DateTime? AlarmStartTime { get; set; }
 	}
 
 	public static class StoredExtensions
 	{
-		public static Guid PlatformTypeId(this StoredStateInfo stored)
-		{
-			return stored.get(s => s.PlatformTypeId);
-		}
-
-		public static Guid? ActivityId(this StoredStateInfo stored)
-		{
-			return stored.get(s => s.ActivityId);
-		}
-
-		public static string StateCode(this StoredStateInfo stored)
-		{
-			return stored.get(s => s.StateCode);
-		}
-
 		public static DateTime? BatchId(this StoredStateInfo stored)
 		{
 			return stored.get(s => s.BatchId);
 		}
 
-		public static Guid? RuleId(this StoredStateInfo stored)
+		public static Guid PlatformTypeId(this StoredStateInfo stored)
 		{
-			return stored.get(s => s.RuleId);
+			return stored.get(s => s.PlatformTypeId);
+		}
+
+		public static string SourceId(this StoredStateInfo stored)
+		{
+			return stored.get(s => s.SourceId);
 		}
 
 		public static DateTime ReceivedTime(this StoredStateInfo stored)
@@ -84,24 +79,40 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 			return stored.get(s => s.ReceivedTime);
 		}
 
+
+
+		public static string StateCode(this StoredStateInfo stored)
+		{
+			return stored.get(s => s.StateCode);
+		}
+
+		public static Guid? StateGroupId(this StoredStateInfo stored)
+		{
+			return stored.get(s => s.StateGroupId);
+		}
+
+
+
+		public static Guid? ActivityId(this StoredStateInfo stored)
+		{
+			return stored.get(s => s.ActivityId);
+		}
+
 		public static DateTime? NextActivityStartTime(this StoredStateInfo stored)
 		{
 			return stored.get(s => s.NextActivityStartTime);
 		}
-		public static string SourceId(this StoredStateInfo stored)
-		{
-			return stored.get(s => s.SourceId);
-		}
-		
 
 		public static Guid? NextActivityId(this StoredStateInfo stored)
 		{
 			return stored.get(s => s.NextActivityId);
 		}
-		
-		public static Guid? StateGroupId(this StoredStateInfo stored)
+
+
+
+		public static Guid? RuleId(this StoredStateInfo stored)
 		{
-			return stored.get(s => s.StateGroupId);
+			return stored.get(s => s.RuleId);
 		}
 
 		
