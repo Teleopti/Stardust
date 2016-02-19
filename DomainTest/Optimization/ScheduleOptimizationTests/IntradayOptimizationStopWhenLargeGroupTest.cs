8144c5da3c2bfd9697ef77bd745ea2fafb6cd1fc
Roger Kratz <roger.kratz@teleopti.com>
2016-02-19 13:20:00 +0100

    #37049 Removing incorrect test

--- DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationStopWhenLargeGroupTest.cs
+++ DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationStopWhenLargeGroupTest.cs
@@ -102,29 +102,6 @@ namespace Teleopti.Ccc.DomainTest.Optimization.ScheduleOptimizationTests
 				.Should().Be.EqualTo(2);
 		}
 
-
-		[Test, Ignore("not fixed yet")]
-		public void ShouldOptimizeHalfGroupWhenTwoAgentsAndTwoDaysIf50PercentShouldBeOptimized()
-		{
-			IntradayOptmizerLimiter.SetFromTest(new Percent(0.01), 3);
-			var phoneActivity = ActivityFactory.CreateActivity("phone");
-			var skill = SkillRepository.Has("skill", phoneActivity);
-			var scenario = ScenarioRepository.Has("some name");
-			var dateOnly = new DateOnly(2015, 10, 12);
-			var planningPeriod = PlanningPeriodRepository.Has(dateOnly, 1);
-			var agent1 = PersonRepository.Has(new Contract("_"), new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1), skill);
-			var agent2 = PersonRepository.Has(new Contract("_"), new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1), skill);
-			PersonAssignmentRepository.Add(new PersonAssignment(agent1, scenario, dateOnly));
-			PersonAssignmentRepository.Add(new PersonAssignment(agent1, scenario, dateOnly.AddDays(1)));
-			PersonAssignmentRepository.Add(new PersonAssignment(agent2, scenario, dateOnly));
-			PersonAssignmentRepository.Add(new PersonAssignment(agent2, scenario, dateOnly.AddDays(1)));
-
-			Target.Optimize(planningPeriod.Id.Value);
-
-			TrackOptimizeDaysForAgents.NumberOfOptimizationsFor(dateOnly).Should().Be.EqualTo(1);
-			TrackOptimizeDaysForAgents.NumberOfOptimizationsFor(dateOnly.AddDays(1)).Should().Be.EqualTo(1);
-		}
-
 		public void Setup(ISystem system, IIocConfiguration configuration)
 		{
 			system.UseTestDouble<TrackOptimizeDaysForAgents>().For<IIntradayOptimizeOneDayCallback>();
