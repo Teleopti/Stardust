4f899cc6f835d4fe90cd2f64adac39bf087918d1
jimmy Broman <none@none>
2016-02-22 08:46:04 +0100

    Validate in pipeline if Jenkinsdeploy is success or not

---
+++ teamcity/SmokeTest/Get-JenkinsStatus.ps1
@@ -0,0 +1,32 @@
+﻿param
+(
+  [String]
+  [Parameter(Mandatory=$true)]
+  $ServerName
+ )
+
+#$job_status = invoke-webrequest "http://tritonweb:8080/job/TritonWeb-AutoDeploy/lastBuild/api/json" | Select-String -pattern "`"result`":`"SUCCESS`""
+
+$ServerURL = "$ServerName" + ':8080'
+
+$Check_webstatus = invoke-webrequest "http://$ServerURL/job/$ServerName-AutoDeploy/lastBuild/api/json"
+
+$Job_status = ConvertFrom-Json $Check_webstatus
+
+$Job_status.result
+
+if ($Job_status.result -eq "SUCCESS") {
+
+Write-Host ""
+Write-Host "Jenkins deploy successfully completed!" -ForegroundColor Green
+Write-Host ""
+
+}
+
+else {
+
+Write-Host ""
+Write-Host "Jenkins deploy failure! Check http://tritonweb:8080/job/TritonWeb-AutoDeploy for more information!" -ForegroundColor Red
+Write-Host ""
+Exit
+}
