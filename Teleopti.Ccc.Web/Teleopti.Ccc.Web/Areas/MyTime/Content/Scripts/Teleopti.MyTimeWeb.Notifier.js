ad265575c92c7a333cfe1d7d56be6304c0947d47
Chundan Xu <chundan.xu@teleopti.com>
2016-02-23 04:00:15 +0100

    Bug #37034

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Notifier.js
@@ -1,18 +1,18 @@
-﻿Teleopti.MyTimeWeb.Notifier = (function () {
+﻿Teleopti.MyTimeWeb.Notifier = (function() {
 	var originalDocumentTitle;
 	var baseUrl;
 	var header = '';
 	var autoCloseTimeout = false;
 	var blinkTitleTimer;
-	var webNotification = function () { return true; }; //default also send as web notification if possible
+	var webNotification = function() { return true; }; //default also send as web notification if possible
 	var webNotifications = new Array();
 
 	function _setOptions(options) {
-		if(options && options.originalDocumentTitle)
-		originalDocumentTitle = options.originalDocumentTitle;
+		if (options && options.originalDocumentTitle)
+			originalDocumentTitle = options.originalDocumentTitle;
 		if (options && options.baseUrl) {
 			baseUrl = options.baseUrl;
-		} 
+		}
 		if (options && options.module) {
 			header = options.module;
 		}
@@ -44,39 +44,47 @@
 			}
 		});
 	}
-	
+
 	function isShowing(notifyText) {
-		var res = webNotifications.filter(function (item) {
+		var res = webNotifications.filter(function(item) {
 			return item.text == notifyText;
 		});
 		return res.length > 0;
 	}
 
 	function _webNotification(notifyText) {
-		if (window.webkitNotifications) {
-			if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
-				if (webNotification() && !isShowing(notifyText)) {
-					var timeout = 5000;
-					var iconUrl = baseUrl + 'content/favicon.ico?v=2';
-					var decodedText = htmlDecode(notifyText);
-					var notification = window.webkitNotifications.createNotification(iconUrl, header, decodedText);
-					notification.show();
-					webNotifications.push({ notification: notification, text: notifyText });
-					setTimeout(function () {
-						for (var i = 0; i < webNotifications.length; i++) {
-							if (webNotifications[i].notification == notification) {
-								notification.cancel();
-								webNotifications.splice(i, 1);
-							}
-						}
-
-						notification.cancel();
-						
-					}, timeout);
+		if (window.Notification && Notification.permission !== "granted") {
+			Notification.requestPermission(function(status) {
+				if (Notification.permission !== status) {
+					Notification.permission = status;
 				}
+			});
+		}
+		if (window.Notification && Notification.permission === "granted") {
+			if (webNotification() && !isShowing(notifyText)) {
+				var timeout = 5000;
+				var iconUrl = baseUrl + 'content/favicon.ico?v=2';
+				var decodedText = htmlDecode(notifyText);
+				var options = {
+					body: decodedText,
+					icon: iconUrl
+				};
+				var notification = new Notification(header, options);
+				webNotifications.push({ notification: notification, text: notifyText });
+				setTimeout(function() {
+					for (var i = 0; i < webNotifications.length; i++) {
+						if (webNotifications[i].notification == notification) {
+							notification.close.bind(notification);
+							webNotifications.splice(i, 1);
+						}
+					}
+					notification.close.bind(notification);
+				}, timeout);
 			}
 		}
+
 	}
+
 	function _messageClosed() {
 		$.pinify.clearOverlay();
 		_stopBlinkDocumentTitle();
