04f53177a942ed5a79d1716dd99213e7b7d358df
micke <micke@teleopti.com>
2016-02-23 15:43:58 +0100

    Bug #36942: ESL incorrect for some intervals on one chat skill - 20160129:014

--- Domain/Forecasting/StaffingCalculatorServiceFacade.cs
+++ Domain/Forecasting/StaffingCalculatorServiceFacade.cs
@@ -22,9 +22,36 @@ namespace Teleopti.Ccc.Domain.Forecasting
 			return _secretService.AgentsUseOccupancy(serviceLevel, serviceTime, tasks/maxParallelTasks, aht, periodLength, minOcc, maxOcc, 1);
 		}
 
+		private static double slope(double x1, double x2, double y1, double y2)
+		{
+			var dX = x2 - x1;
+			var dY = y2 - y1;
+			return dY/dX;
+		}
+
+		private static double baseFromSlope(double slope, double x2, double y2)
+		{
+			return y2 - (x2 * slope);
+		}
+
+		public double LinearEsl(double forecastedAgents, double agents, double sla)
+		{
+			if (Math.Abs(forecastedAgents) < 0.001)
+				return 0;
+
+			var slope = StaffingCalculatorServiceFacade.slope(forecastedAgents, forecastedAgents/2, sla, 0);
+			var baseY = baseFromSlope(slope, forecastedAgents, sla);
+			var esl = (agents*slope) + baseY;
+			return esl < 0 ? 0 : esl > 1 ? 1 : esl;
+		}
+
 		public double ServiceLevelAchievedOcc(double agents, double serviceTime, double calls, double aht, TimeSpan intervalLength, double sla, double forecastedAgents, int maxParellelTasks)
 		{
-			return _serviceLevelAchivedOcc.ServiceLevelAchived(forecastedAgents, agents, sla, (int) serviceTime, calls, TimeSpan.FromSeconds(aht), intervalLength);
+			if (calls/maxParellelTasks <= 2)
+			{
+				return LinearEsl(forecastedAgents, agents, sla);
+			}
+			return _serviceLevelAchivedOcc.ServiceLevelAchived(forecastedAgents, agents, sla, (int) serviceTime, calls/maxParellelTasks, TimeSpan.FromSeconds(aht), intervalLength);
 		}
 
 		public double Utilization(double demandWithoutEfficiency, double tasks, double aht, TimeSpan periodLength, int maxParellelTasks)
--- DomainTest/Forecasting/StaffingCalculatorServiceFacadeTest.cs
+++ DomainTest/Forecasting/StaffingCalculatorServiceFacadeTest.cs
@@ -23,5 +23,57 @@ namespace Teleopti.Ccc.DomainTest.Forecasting
 			var calculatedOcc = _target.Utilization(agents, 5d, 550, TimeSpan.FromMinutes(15), 2);
 			calculatedOcc.Should().Be.IncludedIn(.89, .91);
 		}
+
+		[Test]
+		public void BetterEslCalculationOnSmallVolumeChatUseLinearIfTaskDivMaxParallelIsTwoOrLess()
+		{
+			var tasks = 4d;
+			var agents = _target.AgentsUseOccupancy(.8, 20, tasks, 550, TimeSpan.FromMinutes(15), 0, 1, 3);
+			var esl = _target.ServiceLevelAchievedOcc(agents, 20, tasks, 550, TimeSpan.FromMinutes(15), .8, agents, 3);
+			esl.Should().Be.IncludedIn(.79, .81);
+		}
+
+		[Test]
+		public void EslCalculationOnLargerVolymesShouldNotUseLinear()
+		{
+			var tasks = 40d;
+			var agents = _target.AgentsUseOccupancy(.8, 20, tasks, 550, TimeSpan.FromMinutes(15), 0, 1, 3);
+			var esl = _target.ServiceLevelAchievedOcc(agents, 20, tasks, 550, TimeSpan.FromMinutes(15), .8, agents, 3);
+			esl.Should().Be.IncludedIn(.79, .81);
+		}
+
+		[Test]
+		public void ShouldCalculateLinearEslToMatchSlaWhenPerfectlyStaffed()
+		{
+			var tasks = 6d;
+			var agents = _target.AgentsUseOccupancy(.8, 20, tasks, 550, TimeSpan.FromMinutes(15), 0, 1, 3);
+			var esl = _target.LinearEsl(agents, agents, .8);
+			esl.Should().Be.IncludedIn(.79, .81);
+		}
+
+		[Test]
+		public void LinearEslCanNotBeNegative()
+		{
+			var tasks = 6d;
+			var agents = _target.AgentsUseOccupancy(.8, 20, tasks, 550, TimeSpan.FromMinutes(15), 0, 1, 3);
+			var esl = _target.LinearEsl(agents, 0, .8);
+			esl.Should().Be.EqualTo(0);
+		}
+
+		[Test]
+		public void LinearEslCanNotBeMoreThan100Percent()
+		{
+			var tasks = 6d;
+			var agents = _target.AgentsUseOccupancy(.8, 20, tasks, 550, TimeSpan.FromMinutes(15), 0, 1, 3);
+			var esl = _target.LinearEsl(agents, agents*10, .8);
+			esl.Should().Be.EqualTo(1);
+		}
+
+		[Test]
+		public void LinearEslShouldHandleZeroForecast()
+		{
+			var esl = _target.LinearEsl(0, 0, .8);
+			esl.Should().Be.EqualTo(0);
+		}
 	}
 }
