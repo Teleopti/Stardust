d4c4ccc60b7ad8196319af4e99d4019beb46e49d
Ola <ola@teleopti.com>
2016-02-24 09:05:32 +0100

    fix script

--- Teleopti.Wfm.Administration/Scripts/StardustApp/detailsController.js
+++ Teleopti.Wfm.Administration/Scripts/StardustApp/detailsController.js
@@ -9,7 +9,7 @@
 		/* jshint validthis:true */
 		var vm = this;
 		vm.JobId = $routeParams.jobId;
-		$http.get("../Stardust/JobHistoryDetails/" + vm.JobId).success(function (data) {
+		$http.get("./Stardust/JobHistoryDetails/" + vm.JobId).success(function (data) {
 			vm.JobDetails = data;
 		}).error(function (xhr, ajaxOptions, thrownError) {
 			console.log(xhr.Message + ': ' + xhr.ExceptionMessage);
--- Teleopti.Wfm.Administration/Scripts/StardustApp/listJobController.js
+++ Teleopti.Wfm.Administration/Scripts/StardustApp/listJobController.js
@@ -10,7 +10,7 @@
         var vm = this;
         vm.title = 'Stardust Manager - List Jobs';
 
-    	$http.get("../Stardust/JobHistoryList").success(function (data) {
+    	$http.get("./Stardust/JobHistoryList").success(function (data) {
         		vm.Jobs = data;
         	}).error(function (xhr, ajaxOptions, thrownError) {
         		console.log(xhr.Message + ': ' + xhr.ExceptionMessage);
