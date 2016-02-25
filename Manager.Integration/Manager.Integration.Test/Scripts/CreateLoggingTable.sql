d306c51e96817455511e275eae1ae8c34dc1671e
Amanda <amanda.jonasson@teleopti.com>
2016-02-25 08:31:29 +0100

    add fields to workernode db

--- Manager.Integration/Manager.Integration.Test/Scripts/CreateLoggingTable.sql
+++ Manager.Integration/Manager.Integration.Test/Scripts/CreateLoggingTable.sql
@@ -1,4 +1,5 @@
-﻿CREATE TABLE [Stardust].[Logging] (
+﻿IF OBJECT_ID(' [Stardust].[Logging]', 'U') IS NULL 
+  CREATE TABLE [Stardust].[Logging] (
     [Id] [int] IDENTITY (1, 1) NOT NULL,
     [Date] [datetime] NOT NULL,
     [Thread] [varchar] (255) NOT NULL,
