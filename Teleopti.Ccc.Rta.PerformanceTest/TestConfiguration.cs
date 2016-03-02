6a9c91bbc7b70e561cedfc977da50302cdab0df8
Erik Sundberg <erik.sundberg@teleopti.com>
2016-03-02 09:29:01 +0100

    #36979 Beefing up the rta perf tests

--- Teleopti.Ccc.Rta.PerformanceTest/TestConfiguration.cs
+++ Teleopti.Ccc.Rta.PerformanceTest/TestConfiguration.cs
@@ -2,7 +2,7 @@ namespace Teleopti.Ccc.Rta.PerformanceTest
 {
 	public class TestConfiguration
 	{
-		public int NumberOfAgents = 100;
+		public int NumberOfAgents = 10000;
 
 		// this is the switch id
 		// and is sent with the state
