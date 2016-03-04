fffe3017efe1422d164c975689ad809f7fabd1f7
Kunning Mao <none@none>
2016-03-04 11:13:33 +0100

    #36919 just reformat

--- Teleopti.Support.Security/CommandLineArgument.cs
+++ Teleopti.Support.Security/CommandLineArgument.cs
@@ -5,18 +5,18 @@ namespace Teleopti.Support.Security
 {
 	public class CommandLineArgument
 	{
-        private string _destinationServer;
-        private string _destinationUserName;
-        private string _destinationPassword;
-        private bool _useIntegratedSecurity;
+		private string _destinationServer;
+		private string _destinationUserName;
+		private string _destinationPassword;
+		private bool _useIntegratedSecurity;
 		private string _destinationAppDbDatabase;
 		private string _destinationAnalDbDatabase;
 		private string _baseConnstring;
 
 		public CommandLineArgument(string[] argumentCollection)
-        {
-						readArguments(argumentCollection);
-        }
+		{
+			readArguments(argumentCollection);
+		}
 
 		public IDatabaseArguments GetDatabaseArguments()
 		{
@@ -86,41 +86,41 @@ namespace Teleopti.Support.Security
 			return sqlConnectionStringBuilder.ConnectionString;
 		}
 
-				private void readArguments(string[] argumentCollection)
-        {
-					foreach (string s in argumentCollection)
-            {
-                string switchType = s.Substring(0, 3).ToUpper(CultureInfo.CurrentCulture);
-                string switchValue = s.Remove(0, 3);
+		private void readArguments(string[] argumentCollection)
+		{
+			foreach (string s in argumentCollection)
+			{
+				string switchType = s.Substring(0, 3).ToUpper(CultureInfo.CurrentCulture);
+				string switchValue = s.Remove(0, 3);
 
-                switch (switchType)
-                {
-                    case "-DS":   // Destination Server Name.
-                        _destinationServer = switchValue;
-                        break;
-									case "-AP": // Application db database name
-		                _destinationAppDbDatabase = switchValue;
-										break;
-									case "-AN": // Analytics db database name
-		                _destinationAnalDbDatabase = switchValue;
-		                break;
-                    case "-DU":   // Destination User Name.
-                        _destinationUserName = switchValue;
-                        break;
-                    case "-DP":   // Destination Password.
-                        _destinationPassword = switchValue;
-                        break;
-                    case "-EE":
-                        _useIntegratedSecurity = true;
-                        break;
-					case "-CD":   // Cross Db Name
+				switch (switchType)
+				{
+					case "-DS": // Destination Server Name.
+						_destinationServer = switchValue;
+						break;
+					case "-AP": // Application db database name
+						_destinationAppDbDatabase = switchValue;
+						break;
+					case "-AN": // Analytics db database name
+						_destinationAnalDbDatabase = switchValue;
+						break;
+					case "-DU": // Destination User Name.
+						_destinationUserName = switchValue;
+						break;
+					case "-DP": // Destination Password.
+						_destinationPassword = switchValue;
+						break;
+					case "-EE":
+						_useIntegratedSecurity = true;
+						break;
+					case "-CD": // Cross Db Name
 						AggDatabase = switchValue;
 						break;
-									case "-CS": //Used by WISE to set conn string, _without_ initial catalog
-		                _baseConnstring = switchValue;
-		                break;
-                }
-            }
-        }
-    }
+					case "-CS": //Used by WISE to set conn string, _without_ initial catalog
+						_baseConnstring = switchValue;
+						break;
+				}
+			}
+		}
+	}
 }
