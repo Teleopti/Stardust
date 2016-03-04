9aa1628b18bb875ea9bc1ab079da8282f2e5b8bc
Roger Kratz <roger.kratz@teleopti.com>
2016-03-04 11:13:36 +0100

    IoCSetup cleaning 4: Moving testDoubleFor into SystemImpl

--- Teleopti.Ccc.TestCommon/IoC/ISetup.cs
+++ Teleopti.Ccc.TestCommon/IoC/ISetup.cs
@@ -25,17 +25,17 @@ namespace Teleopti.Ccc.TestCommon.IoC
 
 		public virtual ITestDoubleFor UseTestDouble<TTestDouble>() where TTestDouble : class
 		{
-			return new TestDoubleFor<TTestDouble>(_builder, _testDoubles, null);
+			return new testDoubleFor<TTestDouble>(_builder, _testDoubles, null);
 		}
 
 		public virtual ITestDoubleFor UseTestDouble<TTestDouble>(TTestDouble instance) where TTestDouble : class
 		{
-			return new TestDoubleFor<TTestDouble>(_builder, _testDoubles, instance);
+			return new testDoubleFor<TTestDouble>(_builder, _testDoubles, instance);
 		}
 
 		public virtual ITestDoubleFor UseTestDoubleForType(Type type)
 		{
-			return new TestDoubleFor<object>(_builder, _testDoubles, type);
+			return new testDoubleFor<object>(_builder, _testDoubles, type);
 		}
 
 		public void AddService<TService>()
@@ -60,6 +60,124 @@ namespace Teleopti.Ccc.TestCommon.IoC
 		public void AddModule(Module module)
 		{
 			_builder.RegisterModule(module);
+		}
+
+		private class testDoubleFor<TTestDouble> : ITestDoubleFor where TTestDouble : class
+		{
+			private readonly ContainerBuilder _builder;
+			private readonly TestDoubles _testDoubles;
+			private readonly Type _type;
+			private readonly object _instance;
+
+			public testDoubleFor(ContainerBuilder builder, TestDoubles testDoubles, object instance)
+			{
+				_builder = builder;
+				_testDoubles = testDoubles;
+				_instance = instance;
+			}
+
+			public testDoubleFor(ContainerBuilder builder, TestDoubles testDoubles, Type type)
+			{
+				_builder = builder;
+				_testDoubles = testDoubles;
+				_type = type;
+			}
+
+			public void For<T>()
+			{
+				register(typeof(T));
+			}
+
+			public void For<T1, T2>()
+			{
+				register(typeof(T1), typeof(T2));
+			}
+
+			public void For<T1, T2, T3>()
+			{
+				register(typeof(T1), typeof(T2), typeof(T3));
+			}
+
+			public void For<T1, T2, T3, T4>()
+			{
+				register(typeof(T1), typeof(T2), typeof(T3), typeof(T4));
+			}
+
+			private void register(params Type[] asTypes)
+			{
+				if (_instance == null)
+				{
+					if (_type != null)
+					{
+						var registration = _testDoubles.Register(b =>
+						{
+							b.RegisterType(_type)
+								.SingleInstance()
+								.AsSelf()
+								.As(asTypes);
+						});
+						_builder
+							.RegisterType(_type)
+							.SingleInstance()
+							.AsSelf()
+							.As(asTypes)
+							.ExternallyOwned()
+							.OnActivated(c =>
+							{
+								registration.Action = b =>
+								{
+									b.RegisterInstance(c.Instance)
+										.SingleInstance()
+										.AsSelf()
+										.As(asTypes);
+								};
+							});
+					}
+					else
+					{
+						var registration = _testDoubles.Register(b =>
+						{
+							b.RegisterType<TTestDouble>()
+								.SingleInstance()
+								.AsSelf()
+								.As(asTypes);
+						});
+						_builder
+							.RegisterType<TTestDouble>()
+							.SingleInstance()
+							.AsSelf()
+							.As(asTypes)
+							.ExternallyOwned()
+							.OnActivated(c =>
+							{
+								registration.Action = b =>
+								{
+									b.RegisterInstance(c.Instance)
+										.SingleInstance()
+										.AsSelf()
+										.As(asTypes);
+								};
+							});
+					}
+				}
+				else
+				{
+					_testDoubles.Register(b =>
+					{
+						b.RegisterInstance(_instance)
+							.SingleInstance()
+							.AsSelf()
+							.As(asTypes);
+					});
+					_builder
+						.RegisterInstance(_instance)
+						.SingleInstance()
+						.AsSelf()
+						.As(asTypes)
+						.ExternallyOwned()
+						;
+				}
+			}
 		}
 	}
 
@@ -91,123 +209,4 @@ namespace Teleopti.Ccc.TestCommon.IoC
 			});
 		}
 	}
-
-	public class TestDoubleFor<TTestDouble> : ITestDoubleFor where TTestDouble : class
-	{
-		private readonly ContainerBuilder _builder;
-		private readonly TestDoubles _testDoubles;
-		private readonly Type _type;
-		private readonly object _instance;
-
-		public TestDoubleFor(ContainerBuilder builder, TestDoubles testDoubles, object instance)
-		{
-			_builder = builder;
-			_testDoubles = testDoubles;
-			_instance = instance;
-		}
-
-		public TestDoubleFor(ContainerBuilder builder, TestDoubles testDoubles, Type type)
-		{
-			_builder = builder;
-			_testDoubles = testDoubles;
-			_type = type;
-		}
-
-		public void For<T>()
-		{
-			register(typeof(T));
-		}
-
-		public void For<T1, T2>()
-		{
-			register(typeof(T1), typeof(T2));
-		}
-
-		public void For<T1, T2, T3>()
-		{
-			register(typeof(T1), typeof(T2), typeof(T3));
-		}
-
-		public void For<T1, T2, T3, T4>()
-		{
-			register(typeof(T1), typeof(T2), typeof(T3), typeof(T4));
-		}
-
-		private void register(params Type[] asTypes)
-		{
-			if (_instance == null)
-			{
-				if (_type != null)
-				{
-					var registration = _testDoubles.Register(b =>
-					{
-						b.RegisterType(_type)
-							.SingleInstance()
-							.AsSelf()
-							.As(asTypes);
-					});
-					_builder
-						.RegisterType(_type)
-						.SingleInstance()
-						.AsSelf()
-						.As(asTypes)
-						.ExternallyOwned()
-						.OnActivated(c =>
-						{
-							registration.Action = b =>
-							{
-								b.RegisterInstance(c.Instance)
-									.SingleInstance()
-									.AsSelf()
-									.As(asTypes);
-							};
-						});
-				}
-				else
-				{
-					var registration = _testDoubles.Register(b =>
-					{
-						b.RegisterType<TTestDouble>()
-							.SingleInstance()
-							.AsSelf()
-							.As(asTypes);
-					});
-					_builder
-						.RegisterType<TTestDouble>()
-						.SingleInstance()
-						.AsSelf()
-						.As(asTypes)
-						.ExternallyOwned()
-						.OnActivated(c =>
-						{
-							registration.Action = b =>
-							{
-								b.RegisterInstance(c.Instance)
-									.SingleInstance()
-									.AsSelf()
-									.As(asTypes);
-							};
-						});
-				}
-			}
-			else
-			{
-				_testDoubles.Register(b =>
-				{
-					b.RegisterInstance(_instance)
-						.SingleInstance()
-						.AsSelf()
-						.As(asTypes);
-				});
-				_builder
-					.RegisterInstance(_instance)
-					.SingleInstance()
-					.AsSelf()
-					.As(asTypes)
-					.ExternallyOwned()
-					;
-			}
-		}
-	}
-
 }
