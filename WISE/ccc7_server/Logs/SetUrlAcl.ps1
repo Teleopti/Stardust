b637bc7a2360bd257fd1738aeb5f023ad7cd01fd
Ola <ola@teleopti.com>
2016-03-10 16:03:55 +0100

    change way to set urlacl step one

---
+++ WISE/ccc7_server/Logs/SetUrlAcl.ps1
@@ -0,0 +1,23 @@
+﻿$url = $args[0]
+
+if($url -like '*http://*')
+{
+    $global:value = (netsh http show urlacl url=https://+:14100/)
+    if($global:value  -like '*https://+:14100/*')
+    {
+        $global:value = (netsh http delete urlacl url=https://+:14100/)
+    }
+    $global:value = (netsh http add urlacl url=http://+:14100/ user=Everyone listen=yes)
+}
+
+if($url -like '*https://*')
+{
+    $global:value = (netsh http show urlacl url=http://+:14100/)
+    Write-Host $url;
+    if($global:value  -like '*http://+:14100/*')
+    {
+        $global:value = (netsh http delete urlacl url=http://+:14100/)
+    }
+    $global:value = (netsh http add urlacl url=https://+:14100/ user=Everyone listen=yes)
+}
+
