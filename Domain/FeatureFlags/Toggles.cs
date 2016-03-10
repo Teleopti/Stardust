7f71e385ad86cc393f4b175475a6da1c89223bdc
Pontus Piazzolla <pontus.piazzolla@teleopti.com>
2016-03-10 15:51:49 +0100

    #37439 added toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -86,10 +86,11 @@
 
 		ResourcePlanner_SkillGroupDeleteAfterCalculation_37048,
 		ResourcePlanner_JumpOutWhenLargeGroupIsHalfOptimized_37049,
-        ETL_SpeedUpPersonPeriodIntraday_37162,
-        WfmGlobalLayout_personalOptions_37114,
+		ETL_SpeedUpPersonPeriodIntraday_37162,
+		ETL_SpeedUpPersonPeriodIntradayAcdLogin_37439,
+		WfmGlobalLayout_personalOptions_37114,
 
-				ResourcePlanner_IntradayIslands_36939
-        // ReSharper restore InconsistentNaming
-    }
+		ResourcePlanner_IntradayIslands_36939
+		// ReSharper restore InconsistentNaming
+	}
 }
