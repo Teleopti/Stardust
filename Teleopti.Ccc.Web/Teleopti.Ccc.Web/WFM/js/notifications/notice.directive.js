dfcd5f260ad32b0e4ea2957c4f62f9e26629a352
Erik Sundberg <erik.sundberg@teleopti.com>
2016-03-11 13:58:52 +0100

    Fix build, Backed out changeset: c1c9267f9970

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/notifications/notice.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/notifications/notice.directive.js
@@ -1,47 +1,20 @@
 'use strict';
 (function() {
-	var wfmNotice = angular.module('wfm.notice', ['angular-growl', 'ngAnimate']);
-	wfmNotice.controller('wfmNoticeCtrl', wfmNoticeCtrl);
-	wfmNoticeCtrl.$inject = ['$scope', 'growl'];
+var wfmNotice = angular.module('wfm.notice');
+wfmNotice.controller('wfmNoticeCtrl', wfmNoticeCtrl);
+wfmNoticeCtrl.$inject = [];
+ function wfmNoticeCtrl () {
+  var vm = this;
 
-	function wfmNoticeCtrl($scope, growl) {
+ }
 
-		$(function() {
-			var mainPageContainerElement = $('#materialcontainer');
-			var notificationBarElement = $('#notice');
+ wfmNotice.directive('wfmNotice', wfmNoticeDirective);
+ function wfmNoticeDirective () {
+  return {
+   controller: 'wfmNoticeCtrl',
+   controllerAs: 'vm',
+   template: "<div growl></div>"
+ }
 
-			mainPageContainerElement.scroll(function() {
-				var uiHeight = $('[ui-view]').height();
-				var containerHeight = mainPageContainerElement.height();
-				var scrollTop = mainPageContainerElement.scrollTop();
-
-				if (uiHeight > containerHeight) {
-					notificationBarElement.css({
-						bottom: -scrollTop
-					});
-				} else {
-					notificationBarElement.css({
-						bottom: 0
-					});
-				}
-			});
-
-			$(window).resize(function() {
-				if ($('.growl-container>.growl-item').length > 0) {
-					mainPageContainerElement.scrollTop(0);
-				}
-			});
-		});
-	}
-
-	wfmNotice.directive('wfmNotice', wfmNoticeDirective);
-
-	function wfmNoticeDirective() {
-		return {
-			controller: 'wfmNoticeCtrl',
-			template: "<div growl></div>",
-			scope: true
-		}
-	}
-
+ }
 })();
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/notifications/notice.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/notifications/notice.service.js
@@ -1,29 +1,16 @@
-(function() {
-	'use strict';
-	angular.module('wfm.notice')
-		.service('NoticeService', ['$rootScope', 'growl', function($rootScope, growl) {
-			var service = {};
-			var message;
-			service.warning = function(text, ttl, disableCountDown, beDeleted) {
-				message = growl.warning(text, {
-					ttl: ttl,
-					disableCountDown: disableCountDown
-				});
-				message.beDeleted = beDeleted;
-				destroyOnStateChange(message);
-				return message;
-			};
+(function(){
+'use strict';
+ angular.module('wfm.notice')
+ .service('NoticeService', ['$rootScope', function($rootScope){
+  var service = {};
 
-			 function destroyOnStateChange (message) {
-				$rootScope.$on('$stateChangeSuccess', function() {
-					if (message !== undefined && message.beDeleted) {
-						message.destroy();
-					} else {
-						return;
-					}
-				});
-			};
+  service.destroyOnStateChange = function (message) {
+   $rootScope.$on('$stateChangeSuccess', function(){
+    if (message !== undefined)
+     message.destroy();
+   });
+  };
 
-			return service;
-		}]);
+  return service;
+ }]);
 })();
