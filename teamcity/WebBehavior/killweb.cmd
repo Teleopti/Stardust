93a257600c37a774ae95830027adfb16ea4b1147
jimmy Broman <none@none>
2016-03-11 15:10:13 +0100

    Trying to get rid of "Process exited with code 255" error that happens occasionally

--- teamcity/WebBehavior/killweb.cmd
+++ teamcity/WebBehavior/killweb.cmd
@@ -3,4 +3,4 @@
 taskkill.exe /IM chromedriver.exe /F /FI "memusage gt 2" /t
 taskkill.exe /IM iisexpress.exe /F /FI "memusage gt 2" /t
 taskkill.exe /IM chrome.exe /F /FI "memusage gt 2" /t
-taskkill.exe /IM iisexpresstray.exe /F /FI "memusage gt 2" /t
+taskkill.exe /IM iisexpresstray.exe /F /FI "memusage gt 2" /T 2>&1 | exit /B 0
