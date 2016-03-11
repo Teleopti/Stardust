bf0f922eadb64d34246880b6cec9070469b0ceef
Erik Sundberg <erik.sundberg@teleopti.com>
2016-03-11 08:34:04 +0100

    Backing out of exploration Backed out changeset: bacb9be7fcec

--- Teleopti.Wfm.Administration/App_Start/WebApiConfig.cs
+++ Teleopti.Wfm.Administration/App_Start/WebApiConfig.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Http;
-using Mindscape.Raygun4Net.WebApi;
 
 namespace Teleopti.Wfm.Administration
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-		{
-			RaygunWebApiClient.Attach(config);
-			// Web API configuration and services
+        {
+            // Web API configuration and services
 
-			// Web API routes
-			config.MapHttpAttributeRoutes();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
