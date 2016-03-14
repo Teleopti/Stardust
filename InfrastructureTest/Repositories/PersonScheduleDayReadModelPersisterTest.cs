204dce238ff5ab8a7ececd9bff8ccdd4ccbd45ae
Yanyi Wan <none@none>
2016-03-14 15:26:59 +0800

    Backed out changeset: 0434d0551539

--- InfrastructureTest/Repositories/PersonScheduleDayReadModelPersisterTest.cs
+++ InfrastructureTest/Repositories/PersonScheduleDayReadModelPersisterTest.cs
@@ -19,35 +19,39 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories
 	[TestFixture, Category("LongRunning")]
 	public class PersonScheduleDayReadModelPersisterTest : DatabaseTest
 	{
+
 		[Test]
+		[Ignore]
 		public void ShouldIndicateIfInitializedOrNot()
-		{
-			clearReadModel();
-			var target = new PersonScheduleDayReadModelPersister(CurrUnitOfWork,
-				MockRepository.GenerateMock<IMessageBrokerComposite>(), null);
+		{			
+			var target = new PersonScheduleDayReadModelPersister(CurrentUnitOfWork.Make(), MockRepository.GenerateMock<IMessageBrokerComposite>(), null);
 			var personId = Guid.NewGuid();
 			var businessUnitId = Guid.NewGuid();
-			var teamId = Guid.NewGuid();
-			
-			var date = new DateTime(2012, 8, 29);
-			var model = new PersonScheduleDayReadModel
+			var teamId = Guid.NewGuid();	
+
+			using (UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
 			{
-				Date = date,
-				TeamId = teamId,
-				BusinessUnitId = businessUnitId,
-				PersonId = personId,
-				Start = date.AddHours(10),
-				End = date.AddHours(18),
-				Model = "{shift: blablabla}",
-				ScheduleLoadTimestamp = DateTime.UtcNow
-			};
+				Assert.That(target.IsInitialized(), Is.False);
 
-			Assert.That(target.IsInitialized(), Is.False);
+				var date = new DateTime(2012, 8, 29);
+				var model = new PersonScheduleDayReadModel
+				{
+					Date = date,
+					TeamId = teamId,
+					BusinessUnitId = businessUnitId,
+					PersonId = personId,
+					Start = date.AddHours(10),
+					End = date.AddHours(18),
+					Model = "{shift: blablabla}",
+					ScheduleLoadTimestamp = DateTime.UtcNow
+				};
 
-			target.UpdateReadModels(new DateOnlyPeriod(new DateOnly(date), new DateOnly(date)), personId, businessUnitId,
-				new[] {model}, false);
+				Assert.That(target.IsInitialized(), Is.False);
 
-			Assert.That(target.IsInitialized(), Is.True);
+				target.UpdateReadModels(new DateOnlyPeriod(new DateOnly(date), new DateOnly(date)), personId, businessUnitId, new[] { model }, false);
+							
+				Assert.That(target.IsInitialized(), Is.True);
+			}
 		}
 
 		[Test]
@@ -323,11 +327,6 @@ d\':\'2012-01-12T15:14:00Z\',\'Minutes\':9,\'Title\':\'??????? / ????? ???????\'
 			uow.Current().PersistAll();
 			messageBroker.GetSendInvokedCount().Should().Be.EqualTo(1);
 			CleanUpAfterTest();
-		}
-		private void clearReadModel()
-		{
-			Session.CreateSQLQuery("TRUNCATE TABLE ReadModel.PersonScheduleDay")
-				.ExecuteUpdate();
 		}
 	}
 
