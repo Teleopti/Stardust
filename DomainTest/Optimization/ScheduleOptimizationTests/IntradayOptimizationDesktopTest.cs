ce4a33772019c44f2c6edd8cdf7b8fb92ac5f68a
Claes Stenman <claes.stenman@teleopti.com>
2016-03-15 13:37:50 +0100

    PBI:#36939 Added ignored test, should be able to undo

--- DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationDesktopTest.cs
+++ DomainTest/Optimization/ScheduleOptimizationTests/IntradayOptimizationDesktopTest.cs
@@ -12,6 +12,7 @@ using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
 using Teleopti.Ccc.Domain.Scheduling.WebLegacy;
+using Teleopti.Ccc.Domain.UndoRedo;
 using Teleopti.Ccc.Infrastructure.ApplicationLayer;
 using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
@@ -106,6 +107,50 @@ namespace Teleopti.Ccc.DomainTest.Optimization.ScheduleOptimizationTests
 
 			schedulerStateHolderFrom.Schedules[agent].ScheduledDay(dateOnly).PersonAssignment().Period.StartDateTime.Minute
 				.Should().Be.EqualTo(15);
+		}
+
+		[Test, Ignore]
+		public void ShouldBeAbleToUndo()
+		{
+			var undoRedoContainer = new UndoRedoContainer(int.MaxValue);
+
+			var scenario = new Scenario("_");
+			var phoneActivity = ActivityFactory.CreateActivity("_");
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var dateOnly = new DateOnly(2010, 1, 1);
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(8, 15, 8, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), shiftCategory));
+			var contract = new Contract("_")
+			{
+				WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(36), TimeSpan.FromHours(63), TimeSpan.FromHours(11), TimeSpan.FromHours(36)),
+				PositivePeriodWorkTimeTolerance = TimeSpan.FromHours(9)
+			};
+			var skill = SkillFactory.CreateSkill("_").WithId();
+			skill.Activity = phoneActivity;
+			WorkloadFactory.CreateWorkloadWithFullOpenHours(skill);
+			var skillDay = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(60), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(360)));
+			var agent = new Person().WithId();
+			agent.AddPeriodWithSkill(new PersonPeriod(dateOnly, new PersonContract(contract, new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), skill);
+			agent.AddSchedulePeriod(new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1));
+			agent.Period(dateOnly).RuleSetBag = new RuleSetBag(ruleSet);
+			var ass = new PersonAssignment(agent, scenario, dateOnly);
+			ass.AddActivity(phoneActivity, new TimePeriod(8, 0, 17, 0));
+			ass.SetShiftCategory(new ShiftCategory("_").WithId());
+			var schedulerStateHolderFrom = SchedulerStateHolderFrom();
+			schedulerStateHolderFrom.SchedulingResultState.Schedules = new ScheduleDictionary(scenario, new ScheduleDateTimePeriod(new DateTimePeriod(2010, 1, 1, 2010, 1, 2)));
+			schedulerStateHolderFrom.AllPermittedPersons.Add(agent);
+			schedulerStateHolderFrom.SchedulingResultState.PersonsInOrganization.Add(agent);
+			var scheduleDay = ExtractedSchedule.CreateScheduleDay(schedulerStateHolderFrom.SchedulingResultState.Schedules, agent, dateOnly);
+			scheduleDay.AddMainShift(ass);
+			schedulerStateHolderFrom.SchedulingResultState.Schedules.Modify(scheduleDay);
+			schedulerStateHolderFrom.SchedulingResultState.SkillDays = new Dictionary<ISkill, IList<ISkillDay>> { { skill, new List<ISkillDay> { skillDay } } };
+			schedulerStateHolderFrom.SchedulingResultState.Schedules.SetUndoRedoContainer(undoRedoContainer);
+
+			Target.Optimize(new[] { scheduleDay }, new OptimizationPreferences(), new DateOnlyPeriod(dateOnly, dateOnly), new FixedDayOffOptimizationPreferenceProvider(new DaysOffPreferences()), new NoSchedulingProgress());
+
+			undoRedoContainer.UndoAll();
+			
+			schedulerStateHolderFrom.Schedules[agent].ScheduledDay(dateOnly).PersonAssignment().Period.StartDateTime.Minute
+				.Should().Be.EqualTo(0);
 		}
 
 		[Test, Explicit]
