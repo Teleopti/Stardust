ba3827a99820c2b69a4fd8d764e0cba0348a4ada
johan Damm <none@none>
2016-03-16 08:55:22 +0100

    #37114 recoupled dist for alt style, added gui elements

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/personalize.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/personalize.controller.js
@@ -5,7 +5,8 @@
   personalize.controller('personalizeController', [
     '$scope','Toggle', function ($scope,toggleService) {
 
-      $scope.showOverlay=false;
+      $scope.showOverlay = false;
+	  $scope.darkTheme = false;
 
       toggleService.togglesLoaded.then(function() {
         $scope.personalizeToggle = toggleService.WfmGlobalLayout_personalOptions_37114;
@@ -14,11 +15,26 @@
       var focusMenu = function(){
         document.getElementById("personalizeMenu").focus();
       }
-
-      $scope.toggleOverlayFocus = function () {
-        $scope.showOverlay=!$scope.showOverlay;
+	  var themeToggleFromTo = function(to, from){
+		  console.log(to,from);
+			  document.getElementById(to+'Modules').removeAttribute('disabled');
+			  document.getElementById(to+'Style').removeAttribute('disabled');
+			  document.getElementById(from+'Modules').setAttribute('disabled', true);
+			  document.getElementById(from+'Style').setAttribute('disabled',true);
+	  }
+      $scope.toggleOverlay = function () {
+        $scope.showOverlay =! $scope.showOverlay;
         focusMenu();
       };
+	  $scope.toggleDarkTheme = function(){
+		  $scope.darkTheme =! $scope.darkTheme;
+		  focusMenu();
+		  if ($scope.darkTheme) {
+			  themeToggleFromTo('dark','classic')
+		  }else {
+			  themeToggleFromTo('classic','dark');
+		  }
+	  }
     }
   ]);
 })();
