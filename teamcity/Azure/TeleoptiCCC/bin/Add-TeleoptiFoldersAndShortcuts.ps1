7615e37e889ef1c0687460673b45493a42eff922
Ola <ola@teleopti.com>
2016-03-16 15:37:13 +0100

    not needed any longer

--- teamcity/Azure/TeleoptiCCC/bin/Add-TeleoptiFoldersAndShortcuts.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/Add-TeleoptiFoldersAndShortcuts.ps1
@@ -268,13 +268,7 @@ else
 
 Try
 {
-    #add permissions to listen on port for Stardust Node
-    $global:value = (netsh http show urlacl url=http://+:14100/)
-
-    if(!($global:value  -like '*http://+:14100/*'))
-    {
-        $global:value = (netsh http add urlacl url=http://+:14100/ user=Everyone listen=yes) 
-    }
+   
     [Reflection.Assembly]::LoadWithPartialName("Microsoft.WindowsAzure.ServiceRuntime")
 
     #Get local path
