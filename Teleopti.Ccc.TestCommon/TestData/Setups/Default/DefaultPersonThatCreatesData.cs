7fb60058880c7e6b99f5f357a89d2a94dbd89fc9
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-03-17 09:12:11 +0100

    #36979 Better hashcode for DefaultPersonThatCreatesData

--- Teleopti.Ccc.TestCommon/TestData/Setups/Default/DefaultPersonThatCreatesData.cs
+++ Teleopti.Ccc.TestCommon/TestData/Setups/Default/DefaultPersonThatCreatesData.cs
@@ -1,4 +1,6 @@
-﻿using log4net;
+﻿using System;
+using System.Globalization;
+using log4net;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Interfaces.Domain;
@@ -10,8 +12,16 @@ namespace Teleopti.Ccc.TestCommon.TestData.Setups.Default
 	{
 		private static readonly ILog log = LogManager.GetLogger(typeof (DefaultPersonThatCreatesData));
 
-		public static IPerson PersonThatCreatesDbData =
-			PersonFactory.CreatePerson("UserThatCreatesTestData");
+		public static IPerson PersonThatCreatesDbData = makePersonThatCreatesDbData();
+
+		private static IPerson makePersonThatCreatesDbData()
+		{
+			var person = PersonFactory.CreatePerson("UserThatCreatesTestData");
+			person.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Utc);
+			person.PermissionInformation.SetCulture(CultureInfoFactory.CreateSwedishCulture());
+			person.PermissionInformation.SetUICulture(CultureInfoFactory.CreateSwedishCulture());
+			return person;
+		}
 
 		public void Apply(ICurrentUnitOfWork currentUnitOfWork)
 		{
@@ -21,9 +31,11 @@ namespace Teleopti.Ccc.TestCommon.TestData.Setups.Default
 
 		public int HashValue()
 		{
-			log.Debug("PersonThatCreatesDbData.Name.GetHashCode() " + PersonThatCreatesDbData.Name.GetHashCode());
+			log.Debug("PersonThatCreatesDbData.Name.FirstName.GetHashCode() " + PersonThatCreatesDbData.Name.FirstName.GetHashCode());
+			log.Debug("PersonThatCreatesDbData.Name.LastName.GetHashCode() " + PersonThatCreatesDbData.Name.LastName.GetHashCode());
 			log.Debug("PersonThatCreatesDbData.PermissionInformation.Culture().DisplayName.GetHashCode() " + PersonThatCreatesDbData.PermissionInformation.Culture().DisplayName.GetHashCode());
-			return PersonThatCreatesDbData.Name.GetHashCode() ^
+			return PersonThatCreatesDbData.Name.FirstName.GetHashCode() ^
+				   PersonThatCreatesDbData.Name.LastName.GetHashCode() ^
 				   PersonThatCreatesDbData.PermissionInformation.Culture().DisplayName.GetHashCode();
 		}
 	}
