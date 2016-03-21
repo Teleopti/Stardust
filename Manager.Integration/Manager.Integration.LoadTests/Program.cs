ee31c6b5e6433f51df28573fbe3d5ab5e2ee72b3
antonskripec <anton.skripec@hotmail.com>
2016-03-21 13:00:11 +0100

    Performance test.

--- Manager.Integration/Manager.Integration.LoadTests/Program.cs
+++ Manager.Integration/Manager.Integration.LoadTests/Program.cs
@@ -8,13 +8,20 @@ namespace Manager.Integration.LoadTests
 	{
 		private static void Main(string[] args)
 		{
+			//------------------------------------
+			// Do NOT change order of execution.
+			//------------------------------------
+
 			var loadTests = new OneManagerAndOneNodeLoadTests();
 
+			// Set up test.
 			loadTests.TestFixtureSetUp();
 			loadTests.SetUp();
 
+			// Execute test.
 			loadTests.YourTestGoesHere();
 
+			// Tear down test.
 			loadTests.TearDown();
 			loadTests.TestFixtureTearDown();
 		}
