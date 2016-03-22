e386d0504b0c26e1a9a4a5501ecf6c69190e29fe
johan Damm <none@none>
2016-03-22 13:13:11 +0100

    Fix the build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/Gruntfile.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/Gruntfile.js
@@ -3,7 +3,7 @@
 	grunt.initConfig({
 		watch: {
 			dev: {
-				files: ['css/*.scss','node_modules/teleopti-styleguide/css/*.css','index.tpl.html','js/**/*.html','html/**/*.html','js/**/*.js'],
+				files: ['css/*.scss','index.tpl.html','js/**/*.html','html/**/*.html','js/**/*.js'],
 				tasks: ['devBuild'],
 				options: {
 					spawn: false,
@@ -247,9 +247,9 @@
 	grunt.registerTask('default', ['devBuild','test','watch:dev']); // this task run the main task and then watch for file changes
 	grunt.registerTask('test', ['ngtemplates', 'karma:unit']);
 	grunt.registerTask('devTest', ['ngtemplates','karma:dev']);
-	grunt.registerTask('devBuild', ['ngtemplates', 'sass', 'cssmin', 'concat:distJs', 'concat:distCss','concat:distDarkCss', 'uglify:dev', 'generateIndex']);
+	grunt.registerTask('devBuild', ['ngtemplates', 'sass','concat:distJs', 'concat:distCss','concat:distDarkCss', 'cssmin', 'uglify:dev', 'generateIndex']);
 	grunt.registerTask('test:continuous', ['ngtemplates', 'karma:continuous']);
-	grunt.registerTask('dist', ['ngtemplates', 'sass', 'cssmin', 'concat:distJs', 'concat:distCss','concat:distDarkCss', 'uglify:dist', 'generateIndex']); // this task should only be used by the build. It's kind of packaging for production.
+	grunt.registerTask('dist', ['ngtemplates', 'sass','concat:distJs', 'concat:distCss','concat:distDarkCss','cssmin',  'uglify:dist', 'generateIndex']); // this task should only be used by the build. It's kind of packaging for production.
 	grunt.registerTask('nova', ['devBuild','iisexpress:authBridge','iisexpress:web', 'watch:dev']); // this task run the main task and then watch for file changes
 	grunt.registerTask('build', ['msbuild:build']); // build the solution
 	grunt.registerTask('generateIndex', ['processhtml', 'cacheBust']);
