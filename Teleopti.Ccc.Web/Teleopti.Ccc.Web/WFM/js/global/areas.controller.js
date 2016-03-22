c39d975339181393677406755573c939ee3f03dd
Rockman <none@none>
2016-03-22 10:10:36 +0100

    updated unauth modal to directive

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/areas.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/areas.controller.js
@@ -2,7 +2,7 @@
 
 var notification = angular.module('wfm.areas', ['restAreasService']);
 notification.controller('AreasCtrl', [
-	'$scope', 'AreasSvrc', 
+	'$scope', 'AreasSvrc',
 	function ($scope, AreasSvrc) {
 		$scope.areas = [];
 		$scope.filters = [];
@@ -18,10 +18,6 @@ notification.controller('AreasCtrl', [
 		};
 
 		$scope.unauthModal = true;
-
-		$scope.toggleModal = function () {
-		    $scope.unauthModal = !$scope.unauthModal;
-		};
 
 		$scope.dismissUnauthModal = function () {
 		    window.history.back();
