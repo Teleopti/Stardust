471ccae3561b4036530abee4bcc4884a52745caf
Pontus Piazzolla <pontus.piazzolla@teleopti.com>
2016-03-24 11:02:03 +0100

    #37509 removed steps from nightly and statistics

--- Teleopti.Analytics/Teleopti.Analytics.Etl.Common/Transformer/Job/Jobs/AgentStatisticsJobCollection.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.Common/Transformer/Job/Jobs/AgentStatisticsJobCollection.cs
@@ -13,7 +13,6 @@ namespace Teleopti.Analytics.Etl.Common.Transformer.Job.Jobs
 			Add(new StagePersonJobStep(jobParameters));
 			Add(new StageAgentSkillJobStep(jobParameters));
 			Add(new StageScorecardJobStep(jobParameters));
-			Add(new StageStateGroupJobStep(jobParameters));
 			Add(new DimDateJobStep(jobParameters));
 			Add(new DimBusinessUnitJobStep(jobParameters));
 			Add(new DimScorecardJobStep(jobParameters));
@@ -23,14 +22,12 @@ namespace Teleopti.Analytics.Etl.Common.Transformer.Job.Jobs
 			Add(new DimPersonJobStep(jobParameters));
 			Add(new DimQueueJobStep(jobParameters));
 			Add(new DimAcdLogOnJobStep(jobParameters));
-			Add(new DimStateGroupJobStep(jobParameters));
 			Add(new DimQualityQuestLoadJobStep(jobParameters));
 			Add(new BridgeAcdLogOnPersonJobStep(jobParameters));
 			Add(new FactAgentJobStep(jobParameters));
 			Add(new FactScheduleDeviationJobStep(jobParameters));
 			Add(new FactAgentQueueJobStep(jobParameters));
 			Add(new FactQualityLoadJobStep(jobParameters));
-			Add(new FactAgentStateJobStep(jobParameters));
 		}
 	}
 }
--- Teleopti.Analytics/Teleopti.Analytics.Etl.Common/Transformer/Job/Jobs/NightlyJobCollection.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.Common/Transformer/Job/Jobs/NightlyJobCollection.cs
@@ -31,7 +31,6 @@ namespace Teleopti.Analytics.Etl.Common.Transformer.Job.Jobs
 			Add(new RaptorAgentLogOnSynchronizationStep(jobParameters));
 			Add(new StagePersonJobStep(jobParameters));
 			Add(new StageAgentSkillJobStep(jobParameters));
-			Add(new StageStateGroupJobStep(jobParameters));
 			Add(new StageActivityJobStep(jobParameters));
 			Add(new StageAbsenceJobStep(jobParameters));
 			Add(new StageScenarioJobStep(jobParameters));
@@ -62,7 +61,6 @@ namespace Teleopti.Analytics.Etl.Common.Transformer.Job.Jobs
 			Add(new DimTeamJobStep(jobParameters));
 			Add(new DimSkillJobStep(jobParameters));
 			Add(new DimSkillSetJobStep(jobParameters));
-			Add(new DimStateGroupJobStep(jobParameters));
 			Add(new DimPersonJobStep(jobParameters));
 			Add(new DimActivityJobStep(jobParameters));
 			Add(new DimAbsenceJobStep(jobParameters));
@@ -90,14 +88,12 @@ namespace Teleopti.Analytics.Etl.Common.Transformer.Job.Jobs
 			Add(new StatisticsUpdateNotificationJobStep(jobParameters));                   // BU independent
 			Add(new FactAgentQueueJobStep(jobParameters));              // BU independent
 			Add(new FactQualityLoadJobStep(jobParameters));             // BU independent
-			Add(new FactAgentStateJobStep(jobParameters));
 			Add(new FactForecastWorkloadJobStep(jobParameters));
 			Add(new FactScheduleDeviationJobStep(jobParameters));
 			Add(new FactKpiTargetTeamJobStep(jobParameters));
 			Add(new FactRequestJobStep(jobParameters));
 			Add(new FactRequestedDaysJobStep(jobParameters));
 			Add(new FactAgentSkillJobStep(jobParameters));
-			Add(new FactAgentStateJobStep(jobParameters));
 			Add(new PermissionReportJobStep(jobParameters));
 
 			// If PM is installed then show PM job steps
