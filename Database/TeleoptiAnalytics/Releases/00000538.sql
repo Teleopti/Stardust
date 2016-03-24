44a023a671d74f9310a50f5e31f09b075ef887f6
Karin Jeppsson <karin.jeppsson@teleopti.com>
2016-03-24 13:27:14 +0100

    #37509 added if exists for fresh install

--- Database/TeleoptiAnalytics/Releases/00000538.sql
+++ Database/TeleoptiAnalytics/Releases/00000538.sql
@@ -1,20 +1,28 @@
 --sps
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[etl_fact_agent_state_load]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [mart].[etl_fact_agent_state_load]
 GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[etl_dim_state_group_load]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [mart].[etl_dim_state_group_load]
 GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[etl_dim_state_group_load_livefeed') AND type in (N'P', N'PC'))
 DROP PROCEDURE [mart].[etl_dim_state_group_load_livefeed]
 GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[report_control_twolist_state_group_get]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [mart].[report_control_twolist_state_group_get]
 GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[report_data_time_in_state_per_agent]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [mart].[report_data_time_in_state_per_agent]
 GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[etl_stg_state_group_delete]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [stage].[etl_stg_state_group_delete]
 GO
 
 --views
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[stage].[v_stg_agent_state_split_midnight]'))
 DROP VIEW [stage].[v_stg_agent_state_split_midnight]
 GO
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[stage].[v_stg_agent_state_sum]'))
 DROP VIEW [stage].[v_stg_agent_state_sum]
 GO
 
@@ -29,7 +37,7 @@ DROP TABLE [mart].[fact_agent_state]
 GO
 DROP TABLE [mart].[dim_state_group]
 GO
-
+IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[RTA].[tr_ActualAgentState_update]'))
 DROP TRIGGER [RTA].[tr_ActualAgentState_update]
 GO
 
