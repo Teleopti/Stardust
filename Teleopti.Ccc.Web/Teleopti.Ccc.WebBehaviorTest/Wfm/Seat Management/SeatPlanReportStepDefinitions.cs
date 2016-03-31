eeb5445bc5bba9fd782d78ca14c53fa04013ebb3
Xinfeng Li <xinfeng.li@teleopti.com>
2016-03-31 17:19:36 +0800

    Try fix #37790: random failing SeatPlanReportFeature.ShouldShowPagingBarWhenThereIsOnlyOnePage

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Seat Management/SeatPlanReportStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Seat Management/SeatPlanReportStepDefinitions.cs
@@ -10,7 +10,7 @@ namespace Teleopti.Ccc.WebBehaviorTest.Wfm.Seat_Management
 		[When(@"I click SeatPlanReport button")]
 		public void WhenIClickSeatPlanReportButton()
 		{
-			Browser.Interactions.Click(".view-header .seatplan-report-btn");
+			Browser.Interactions.Click("#seatplan-report-btn");
 		}
 
 		[Then(@"I should be able to view the report content")]
