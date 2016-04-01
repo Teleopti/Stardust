494bb1ee91514316754204d70ff1ce59c3a0b329
micke <micke@teleopti.com>
2016-04-01 16:25:58 +0200

    bug #37665 prevoius fix revealed an endless loop

--- Domain/Optimization/WeeklyRestSolver/ShiftNudgeEarlier.cs
+++ Domain/Optimization/WeeklyRestSolver/ShiftNudgeEarlier.cs
@@ -39,17 +39,25 @@ namespace Teleopti.Ccc.Domain.Optimization.WeeklyRestSolver
 			ITeamBlockInfo teamBlockInfo, ISchedulingResultStateHolder schedulingResultStateHolder,
 			IOptimizationPreferences optimizationPreferences, bool firstNudge)
 		{
+			//var personAssignment = scheduleDay.PersonAssignment();
+			//var projection = personAssignment.ProjectionService().CreateProjection();
+			//var projectionPeriod = projection.Period().Value;
+			//var shiftStartDate = projectionPeriod.StartDateTime.Date;
+			//var shiftEnd = projectionPeriod.EndDateTime;
+			//var adjustedEnd = shiftEnd.Add(TimeSpan.FromMinutes(-15)); //allways adjust 15 minutes regardless of interval length
+			//var dateOffset = (int) adjustedEnd.Date.Subtract(shiftStartDate).TotalDays;
+			//var timeZone = scheduleDay.TimeZone;
+			//var shiftEndUserLocalDateTime = TimeZoneHelper.ConvertFromUtc(adjustedEnd, timeZone);
+			//var latestEndTime = shiftEndUserLocalDateTime.TimeOfDay.Add(TimeSpan.FromDays(dateOffset));
+			//var shiftDate = personAssignment.Date;
+
 			var personAssignment = scheduleDay.PersonAssignment();
-			var projection = personAssignment.ProjectionService().CreateProjection();
-			var projectionPeriod = projection.Period().Value;
-			var shiftStartDate = projectionPeriod.StartDateTime.Date;
-			var shiftEnd = projectionPeriod.EndDateTime;
-			var adjustedEnd = shiftEnd.Add(TimeSpan.FromMinutes(-15)); //allways adjust 15 minutes regardless of interval length
-			var dateOffset = (int) adjustedEnd.Date.Subtract(shiftStartDate).TotalDays;
-			var timeZone = scheduleDay.TimeZone;
-			var shiftEndUserLocalDateTime = TimeZoneHelper.ConvertFromUtc(adjustedEnd, timeZone);
-			var latestEndTime = shiftEndUserLocalDateTime.TimeOfDay.Add(TimeSpan.FromDays(dateOffset));
+			var projectionPeriodUtc = personAssignment.ProjectionService().CreateProjection().Period().Value;
+			var shiftEndUserLocalDateTime = projectionPeriodUtc.EndDateTimeLocal(scheduleDay.TimeZone);
+			var latestStartDateTime = shiftEndUserLocalDateTime.AddMinutes(-15); //allways adjust 15 minutes regardless of interval length
 			var shiftDate = personAssignment.Date;
+			
+			var latestEndTime = latestStartDateTime.TimeOfDay;
 
 			if (optimizationPreferences != null)
 			{
