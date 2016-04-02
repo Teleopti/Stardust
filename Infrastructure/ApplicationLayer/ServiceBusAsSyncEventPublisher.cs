fd08db673824268a86bb368e3633596399be41ab
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-04-02 15:09:01 +0200

    #35703 Refact into a common event processor with a common context step #7

--- Infrastructure/ApplicationLayer/ServiceBusAsSyncEventPublisher.cs
+++ Infrastructure/ApplicationLayer/ServiceBusAsSyncEventPublisher.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using Teleopti.Ccc.Domain;
-using Teleopti.Ccc.Domain.ApplicationLayer;
+﻿using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Interfaces.Domain;
 
@@ -9,32 +7,21 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 	public class ServiceBusAsSyncEventPublisher : IEventPublisher
 	{
 		private readonly ResolveEventHandlers _resolver;
+		private readonly CommonEventProcessor _processor;
 
-		public ServiceBusAsSyncEventPublisher(ResolveEventHandlers resolver)
+		public ServiceBusAsSyncEventPublisher(
+			ResolveEventHandlers resolver, 
+			CommonEventProcessor processor)
 		{
 			_resolver = resolver;
+			_processor = processor;
 		}
 
 		public void Publish(params IEvent[] events)
 		{
 			foreach (var @event in events)
-			{
-				var handlerTypes = _resolver.HandlerTypesFor<IRunOnServiceBus>(@event);
-				foreach (var handlerType in handlerTypes)
-				{
-					var handler = _resolver.HandlerFor(handlerType);
-					var method = _resolver.HandleMethodFor(handlerType, @event);
-					try
-					{
-						method.Invoke(handler, new[] {@event});
-					}
-					catch (TargetInvocationException e)
-					{
-						PreserveStack.ForInnerOf(e);
-						throw e;
-					}
-				}
-			}
+				foreach (var handlerType in _resolver.HandlerTypesFor<IRunOnServiceBus>(@event))
+					_processor.Process(@event, handlerType);
 		}
 	}
 }
