77b6a8555dba2d195d974de82d0f1607fc7c68a3
Roger Kratz <roger.kratz@teleopti.com>
2016-04-06 16:03:50 +0200

    Moving DoNothingScheduleDayChangeCallback from testcommon to domain

--- Domain/Scheduling/DoNothingScheduleDayChangeCallBack.cs
+++ Domain/Scheduling/DoNothingScheduleDayChangeCallBack.cs
@@ -1,17 +1,15 @@
 ﻿using Teleopti.Interfaces.Domain;
 
-namespace Teleopti.Ccc.TestCommon
+namespace Teleopti.Ccc.Domain.Scheduling
 {
 	public class DoNothingScheduleDayChangeCallBack : IScheduleDayChangeCallback
 	{
 		public void ScheduleDayChanging(IScheduleDay partBefore)
 		{
-			//do nothing
 		}
 
 		public void ScheduleDayChanged(IScheduleDay partAfter)
 		{
-			//do nothing
 		}
 	}
 }
