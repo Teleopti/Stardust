3a30e54954d54604df2afb1c1d96620fae68c93a
Erik Nordenhök <erik.nordenhok@teleopti.com>
2016-04-08 09:25:48 +0200

    #33584 Saved the sp...

--- Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_permission_report_insert.sql
+++ Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_permission_report_insert.sql
@@ -1,21 +1,40 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[etl_bridge_group_page_person_insert]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [mart].[etl_bridge_group_page_person_insert]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[mart].[etl_permission_report_insert]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [mart].[etl_permission_report_insert]
 GO
 
 -- =============================================
--- Description:	Insert bridge group page person
+-- Description:	Inserts a permission for a user
 -- =============================================
-CREATE PROCEDURE [mart].[etl_bridge_group_page_person_insert]
-@person_codes nvarchar(max),
-@group_page_code uniqueidentifier
-
+CREATE PROCEDURE [mart].[etl_permission_report_insert]
+@person_code uniqueidentifier,
+@team_code uniqueidentifier,
+@my_own bit,
+@business_unit_code uniqueidentifier,
+@report_id uniqueidentifier
 AS
 BEGIN
 
-  declare @group_page_id int = (select group_page_id from [mart].[dim_group_page] where group_code = @group_page_code)
-	insert into [mart].[bridge_group_page_person]
-	select @group_page_id, p.person_id, 1, GETUTCDATE() 
-	from mart.SplitStringGuid(@person_codes) join [mart].dim_person p on p.person_code = id
+  declare @team_id int = (select team_id from mart.dim_team
+  where team_code = @team_code)
+
+  declare @business_unit_id int = (select business_unit_id from mart.dim_business_unit
+  where business_unit_code = @business_unit_code)
+
+  insert into mart.permission_report
+	([person_code]
+	,[team_id]
+	,[my_own]
+	,[business_unit_id]
+	,[datasource_id]
+	,[datasource_update_date]
+	,[ReportId])
+  values (@person_code
+	  ,@team_id
+	  ,@my_own
+	  ,@business_unit_id
+	  ,1
+	  ,GETUTCDATE()
+	  ,@report_id)
 END
 
 GO
