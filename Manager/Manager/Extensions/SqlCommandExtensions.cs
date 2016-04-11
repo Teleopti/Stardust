8e42516d3d8202cb53e2c5616a4ec4dd4472e522
antonskripec <anton.skripec@hotmail.com>
2016-04-11 08:21:37 +0200

    Changes.

---
+++ Manager/Manager/Extensions/SqlCommandExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Stardust.Manager.Extensions
+{
+	public static class SqlCommandExtensions
+	{
+		public static string GetCommandToExecute(this SqlCommand command)
+		{
+			var commandTextToExecute = command.CommandText;
+
+			foreach (SqlParameter p in command.Parameters)
+			{
+				string parameterValue = p.Value.ToString();
+
+				if (p.SqlDbType == SqlDbType.NVarChar ||
+					p.SqlDbType == SqlDbType.UniqueIdentifier ||
+					p.SqlDbType == SqlDbType.DateTime)
+				{
+					parameterValue = "'" + parameterValue + "'";
+				}
+
+				commandTextToExecute = commandTextToExecute.Replace(p.ParameterName, parameterValue);
+			}
+
+			return commandTextToExecute;
+		}
+	}
+}
