09c440fff45bb1d26dd712015ec57ee0b08ff56d
Pontus Piazzolla <pontus.piazzolla@teleopti.com>
2016-04-12 14:22:08 +0200

    #38019 event based update of fact schedule for all scenarios.

---
+++ Domain/ApplicationLayer/ScheduleChangedEventHandlers/Analytics/AnalyticsScheduleChangeForAllReportableScenariosFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.Repositories;
+
+namespace Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.Analytics
+{
+	public class AnalyticsScheduleChangeForAllReportableScenariosFilter : IAnalyticsScheduleChangeUpdaterFilter
+	{
+		private readonly IScenarioRepository _scenarioRepository;
+
+		public AnalyticsScheduleChangeForAllReportableScenariosFilter(IScenarioRepository scenarioRepository)
+		{
+			_scenarioRepository = scenarioRepository;
+		}
+
+		public bool ContinueProcessingEvent(ProjectionChangedEvent @event)
+		{
+			if (@event.IsDefaultScenario)
+				return true;
+
+			return _scenarioRepository.FindEnabledForReportingSorted().Any(a => a.Id.Equals(@event.ScenarioId));
+		}
+	}
+}
---
+++ Domain/ApplicationLayer/ScheduleChangedEventHandlers/Analytics/AnalyticsScheduleChangeForDefaultScenarioFilter.cs
@@ -0,0 +1,12 @@
+﻿using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+
+namespace Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.Analytics
+{
+	public class AnalyticsScheduleChangeForDefaultScenarioFilter : IAnalyticsScheduleChangeUpdaterFilter
+	{
+		public bool ContinueProcessingEvent(ProjectionChangedEvent @event)
+		{
+			return @event.IsDefaultScenario;
+		}
+	}
+}
