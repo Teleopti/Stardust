d06f94c638f8853b2b5c88bb23196eed3210b119
Claes Stenman <claes.stenman@teleopti.com>
2016-04-13 09:17:36 +0200

    PBI:#37998 Added toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -95,6 +95,7 @@
 
 		ResourcePlanner_IntradayIslands_36939,
 		ResourcePlanner_CascadingSkillsPOC_37679,
+		ResourcePlanner_TeamBlockDayOffForIndividuals_37998,
 
 		Wfm_QuickForecastOnHangfire_35845,
 		ETL_SpeedUpPermissionReport_33584,
