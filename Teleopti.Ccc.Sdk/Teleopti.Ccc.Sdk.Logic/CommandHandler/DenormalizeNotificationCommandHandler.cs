23a40204da285705ef00defcde7dedc8df37d9aa
amandaj <none@none>
2016-04-18 11:19:15 +0200

    Remove ProcessDenormalizerQueue message, consumer, repo and tests

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/DenormalizeNotificationCommandHandler.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/DenormalizeNotificationCommandHandler.cs
@@ -1,26 +1,12 @@
-﻿using System;
-using Teleopti.Ccc.Domain.ApplicationLayer;
-using Teleopti.Ccc.Infrastructure.ApplicationLayer;
+﻿using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Sdk.Common.DataTransferObject.Commands;
-using Teleopti.Interfaces.Messages.Denormalize;
 
 namespace Teleopti.Ccc.Sdk.Logic.CommandHandler
 {
 	public class DenormalizeNotificationCommandHandler : IHandleCommand<DenormalizeNotificationCommandDto>
 	{
-		private readonly IMessagePopulatingServiceBusSender _busSender;
-
-		public DenormalizeNotificationCommandHandler(IMessagePopulatingServiceBusSender busSender)
-		{
-			_busSender = busSender;
-		}
-
 		public void Handle(DenormalizeNotificationCommandDto command)
 		{
-			var message = new ProcessDenormalizeQueue();
-			
-            _busSender.Send(message, true);
-			command.Result = new CommandResultDto { AffectedId = Guid.Empty, AffectedItems = 1 };
 		}
 	}
 }
