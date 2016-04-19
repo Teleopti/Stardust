5c8de1404edac130e569577e710efd31b4f485a2
amandaj <none@none>
2016-04-19 16:29:57 +0200

    PBI #38207: + UpdateFindPersonDataHandler

---
+++ Domain/ApplicationLayer/SettingsForPersonPeriodChangedEventHandlers/UpdateFindPersonDataHandler.cs
@@ -0,0 +1,40 @@
+﻿using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.Repositories;
+
+namespace Teleopti.Ccc.Domain.ApplicationLayer.SettingsForPersonPeriodChangedEventHandlers
+{
+	[UseOnToggle(Toggles.SettingsForPersonPeriodChanged_ToHangfire_38207)]
+	public class UpdateFindPersonDataHandlerHangfire : IHandleEvent<SettingsForPersonPeriodChangedEvent>, IRunOnHangfire
+	{
+		private readonly IPersonFinderReadOnlyRepository _personFinderReadOnlyRepository;
+
+		public UpdateFindPersonDataHandlerHangfire(IPersonFinderReadOnlyRepository personFinderReadOnlyRepository)
+		{
+			_personFinderReadOnlyRepository = personFinderReadOnlyRepository;
+		}
+		
+		[UnitOfWork]
+		public virtual void Handle(SettingsForPersonPeriodChangedEvent @event)
+		{
+			_personFinderReadOnlyRepository.UpdateFindPersonData(@event.IdCollection);
+		}
+	}
+
+	[UseNotOnToggle(Toggles.SettingsForPersonPeriodChanged_ToHangfire_38207)]
+	public class UpdateFindPersonDataHandlerServiceBus : IHandleEvent<SettingsForPersonPeriodChangedEvent>, IRunOnServiceBus
+	{
+		private readonly IPersonFinderReadOnlyRepository _personFinderReadOnlyRepository;
+
+		public UpdateFindPersonDataHandlerServiceBus(IPersonFinderReadOnlyRepository personFinderReadOnlyRepository)
+		{
+			_personFinderReadOnlyRepository = personFinderReadOnlyRepository;
+		}
+		
+		public void Handle(SettingsForPersonPeriodChangedEvent @event)
+		{
+			_personFinderReadOnlyRepository.UpdateFindPersonData(@event.IdCollection);
+		}
+	}
+}
---
+++ DomainTest/ApplicationLayer/SettingsForPersonPeriodChangedEventHandlers/UpdateFindPersonDataHandlerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.ApplicationLayer.SettingsForPersonPeriodChangedEventHandlers;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.TestCommon.FakeData;
+
+namespace Teleopti.Ccc.DomainTest.ApplicationLayer.SettingsForPersonPeriodChangedEventHandlers
+{
+    [TestFixture]
+    public class UpdateFindPersonDataHandlerTest
+    {
+        private MockRepository _mocks;
+        private UpdateFindPersonDataHandlerHangfire _target;
+        private IPersonFinderReadOnlyRepository _finderReadOnlyRep;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocks = new MockRepository();
+            _finderReadOnlyRep = _mocks.StrictMock<IPersonFinderReadOnlyRepository>();
+            _target = new UpdateFindPersonDataHandlerHangfire(_finderReadOnlyRep);
+        }
+
+        [Test]
+        public void ShouldDenormalizeNotPersons()
+        {
+            var skill = SkillFactory.CreateSkill("TestID");
+            var tempGuid = Guid.NewGuid();
+            skill.SetId(tempGuid);
+
+            var ids = new[] {tempGuid};
+            var @event = new SettingsForPersonPeriodChangedEvent();
+			@event.SetIdCollection(ids);
+
+            using (_mocks.Record())
+            {
+                Expect.Call(() => _finderReadOnlyRep.UpdateFindPersonData(ids));
+            }
+            using (_mocks.Playback())
+            {
+                _target.Handle(@event);
+            }
+        }
+    }
+}
