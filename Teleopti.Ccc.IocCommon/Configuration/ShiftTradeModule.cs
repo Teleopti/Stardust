32f32ec6d0618a3ef1446a6dfaa93f8501ea45f8
Ola <none@none>
2016-04-19 14:08:58 +0200

    remove double registrations

--- Teleopti.Ccc.IocCommon/Configuration/ShiftTradeModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/ShiftTradeModule.cs
@@ -1,9 +1,5 @@
 ﻿using Autofac;
 using Teleopti.Ccc.Domain.AgentInfo.Requests;
-using Teleopti.Ccc.Domain.Optimization;
-using Teleopti.Ccc.Domain.Optimization.MatrixLockers;
-using Teleopti.Ccc.Domain.Scheduling;
-using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.WorkflowControl.ShiftTrades;
 using Teleopti.Interfaces.Domain;
 
@@ -13,29 +9,15 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 	{
 		protected override void Load(ContainerBuilder builder)
 		{
-			builder.RegisterType<SchedulePeriodTargetTimeCalculator>().As<ISchedulePeriodTargetTimeCalculator>().SingleInstance();
-			//builder.RegisterType<SchedulerStateHolder>()
-			//	.As<ISchedulerStateHolder>()
-			//	.InstancePerLifetimeScope();
-			builder.RegisterType<MatrixUserLockLocker>().As<IMatrixUserLockLocker>().InstancePerLifetimeScope();
-			builder.RegisterType<GridlockManager>().As<IGridlockManager>().InstancePerLifetimeScope();
-			builder.RegisterType<MatrixNotPermittedLocker>().As<IMatrixNotPermittedLocker>().InstancePerLifetimeScope();
-			builder.RegisterType<PersonListExtractorFromScheduleParts>().As<IPersonListExtractorFromScheduleParts>().InstancePerLifetimeScope();
-			builder.RegisterType<PeriodExtractorFromScheduleParts>().As<PeriodExtractorFromScheduleParts>().InstancePerLifetimeScope();
-			builder.RegisterType<MatrixListFactory>().As<IMatrixListFactory>().InstancePerLifetimeScope();
-
 			builder.RegisterType<ShiftTradeSkillSpecification>().As<IShiftTradeLightSpecification>();
 			builder.RegisterType<OpenShiftTradePeriodSpecification>().As<IShiftTradeLightSpecification>();
-
 			builder.RegisterType<ShiftTradeDateSpecification>().As<IShiftTradeSpecification>();
 			builder.RegisterType<ShiftTradeTargetTimeSpecification>().As<IShiftTradeSpecification>();
 			builder.RegisterType<ShiftTradeAbsenceSpecification>().As<IShiftTradeSpecification>();
 			builder.RegisterType<ShiftTradePersonalActivitySpecification>().As<IShiftTradeSpecification>();
 			builder.RegisterType<ShiftTradeMeetingSpecification>().As<IShiftTradeSpecification>();
-
 			builder.RegisterType<ShiftTradeValidator>().As<IShiftTradeValidator>();
 			builder.RegisterType<ShiftTradeLightValidator>().As<IShiftTradeLightValidator>();
-
 			builder.RegisterType<ShiftTradeRequestSetChecksum>().As<IShiftTradeRequestSetChecksum>().SingleInstance();
 			builder.RegisterType<ShiftTradeRequestStatusChecker>().As<IShiftTradeRequestStatusChecker>().SingleInstance();
 		}
