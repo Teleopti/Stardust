0fa6924e35b0644bbb9f79993add455158878a83
amandaj <none@none>
2016-04-20 10:03:36 +0200

    SyncAllEventPublisher publish with temporary fix

--- Infrastructure/ApplicationLayer/SyncAllEventPublisher.cs
+++ Infrastructure/ApplicationLayer/SyncAllEventPublisher.cs
@@ -1,6 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Teleopti.Ccc.Domain;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
@@ -9,25 +14,77 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 	{
 		private readonly ResolveEventHandlers _resolver;
 		private readonly CommonEventProcessor _processor;
+		private readonly ServiceBusEventProcessor _serviceBusEventProcessor;
+
 
 		public SyncAllEventPublisher(
 			ResolveEventHandlers resolver,
-			CommonEventProcessor processor)
+			CommonEventProcessor processor,
+			ServiceBusEventProcessor serviceBusEventProcessor)
 		{
 			_resolver = resolver;
 			_processor = processor;
+			_serviceBusEventProcessor = serviceBusEventProcessor;
 		}
 
 		public void Publish(params IEvent[] events)
 		{
 			foreach (var @event in events)
 			{
-				var handlerTypes = _resolver.HandlerTypesFor<IRunOnServiceBus>(@event)
-					.Concat(_resolver.HandlerTypesFor<IRunOnHangfire>(@event));
+				var handlerTypes = _resolver.HandlerTypesFor<IRunOnHangfire>(@event);
 				foreach (var handlerType in handlerTypes)
-					_processor.Process(@event, handlerType);
+				{
+					Exception exception = null;
+					var thread = new Thread(() =>
+					{
+						try
+						{
+							_processor.Process(@event, handlerType);
+						}
+						catch (Exception ex)
+						{
+							PreserveStack.For(ex);
+							exception = ex;
+						}
+					});
+					thread.Start();
+					thread.Join();
+					if (exception != null)
+						throw exception;
+					
+				}
 			}
+			
+
+			events.Where(e => _resolver.HandlerTypesFor<IRunOnServiceBus>(e).Any())
+				.ForEach(@event =>
+				{
+					Exception exception = null;
+					var thread = new Thread(() =>
+					{
+						try
+						{
+							ProcessLikeTheBus(@event);
+						}
+						catch (Exception ex)
+						{
+							PreserveStack.For(ex);
+							exception = ex;
+						}
+					});
+					thread.Start();
+					thread.Join();
+					if (exception != null)
+						throw exception;
+				});
 		}
 
+		[AsSystem]
+		protected virtual void ProcessLikeTheBus(IEvent @event)
+		{
+			_serviceBusEventProcessor.Process(@event);
+		}
 	}
+
+
 }
