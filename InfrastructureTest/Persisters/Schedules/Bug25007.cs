a6faef2ba27ee65490a77e3d446a1a712bef2a6f
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-04-20 12:14:39 +0200

    #35703 Minor

--- InfrastructureTest/Persisters/Schedules/Bug25007.cs
+++ InfrastructureTest/Persisters/Schedules/Bug25007.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.Scheduling;
-using Teleopti.Ccc.Infrastructure.Persisters.Schedules;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.InfrastructureTest.Persisters.Schedules
