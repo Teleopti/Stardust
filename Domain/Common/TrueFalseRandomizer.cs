96b009a9ed40d456aa0ae7993d51c2447e77ac4e
Roger Kratz <roger.kratz@teleopti.com>
2016-04-21 17:27:06 +0200

    #38174 Random instance is not thread safe

--- Domain/Common/TrueFalseRandomizer.cs
+++ Domain/Common/TrueFalseRandomizer.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Common
 {
 	public class TrueFalseRandomizer : ITrueFalseRandomizer
 	{
-		private readonly Random random = new Random();
+		private static readonly Random random = new Random();
 
+		[MethodImpl(MethodImplOptions.Synchronized)]
 		public bool Randomize()
 		{
 			return random.NextDouble() > 0.5;
