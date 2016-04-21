d170cf8aad8842292b3990a660d07d8417003a2b
Roger Kratz <roger.kratz@teleopti.com>
2016-04-21 13:59:15 +0200

    Removing unused method on PainterHelper

--- Teleopti.Ccc.WinCode/Scheduling/RestrictionSummary/PainterHelper.cs
+++ Teleopti.Ccc.WinCode/Scheduling/RestrictionSummary/PainterHelper.cs
@@ -27,11 +27,6 @@ namespace Teleopti.Ccc.WinCode.Scheduling.RestrictionSummary
             return (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasFullDayAbsence && !_cellValue.HasDayOff && _cellValue.HasAbsenceOnContractDayOff);
         }
 
-        public bool CanPaintAbsence()
-        {
-            return (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasFullDayAbsence && !_cellValue.HasDayOff);
-        }
-
         public bool CanPaintDisabled()
         {
             return (!_cellValue.Enabled);
@@ -46,41 +41,6 @@ namespace Teleopti.Ccc.WinCode.Scheduling.RestrictionSummary
         public bool CanPaintEffectiveRestrictionRightToLeft(bool rightToLeft)
         {
             return ((_cellValue.EffectiveRestriction != null) && !(_cellValue.HasShift || _cellValue.HasDayOff || _cellValue.HasFullDayAbsence) && rightToLeft);
-
-        }
-
-        public bool CanPaintPreferredDayOff()
-        {
-            bool scheduleShouldOverwrite = (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasFullDayAbsence) || (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasShift) || (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasDayOff);
-            if (scheduleShouldOverwrite)
-                return false;
-            if ((_cellValue.SchedulingOption.UsePreferences && _cellValue.SchedulingOption.UseRotations) && _cellValue.EffectiveRestriction != null
-                && (_cellValue.EffectiveRestriction.DayOffTemplate != null && _cellValue.EffectiveRestriction.ShiftCategory != null))
-                return false;
-            return ((_cellValue.SchedulingOption.UsePreferences || _cellValue.SchedulingOption.UseRotations ||
-                    _cellValue.SchedulingOption.UseAvailability || _cellValue.SchedulingOption.UseStudentAvailability) && (_cellValue.EffectiveRestriction != null && _cellValue.EffectiveRestriction.DayOffTemplate != null));
-
-
-        }
-
-        public bool CanPaintPreferredExtended()
-        {
-            bool canPaint = _cellValue.SchedulingOption.UsePreferences;
-            if (!canPaint)
-                return false;
-            return (((!_cellValue.HasDayOff && !_cellValue.HasShift && !_cellValue.HasAbsence && _cellValue.SchedulingOption.UseScheduling) || (!_cellValue.SchedulingOption.UseScheduling)) && _cellValue.HasExtendedPreference);
-        }
-
-        public bool CanPaintPreferredShiftCategory()
-        {
-            bool scheduleShouldOverwrite = (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasFullDayAbsence) || (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasShift) || (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasDayOff);
-            if (scheduleShouldOverwrite)
-                return false;
-            if ((_cellValue.SchedulingOption.UsePreferences && _cellValue.SchedulingOption.UseRotations) && _cellValue.EffectiveRestriction != null
-                && (_cellValue.EffectiveRestriction.DayOffTemplate != null && _cellValue.EffectiveRestriction.ShiftCategory != null))
-                return false;
-            return ((_cellValue.SchedulingOption.UsePreferences || _cellValue.SchedulingOption.UseRotations ||
-                    _cellValue.SchedulingOption.UseAvailability || _cellValue.SchedulingOption.UseStudentAvailability) && (_cellValue.EffectiveRestriction != null && _cellValue.EffectiveRestriction.ShiftCategory != null));
 
         }
 
@@ -111,19 +71,6 @@ namespace Teleopti.Ccc.WinCode.Scheduling.RestrictionSummary
                 return false;
             return ((_cellValue.SchedulingOption.UsePreferences || _cellValue.SchedulingOption.UseRotations ||
                     _cellValue.SchedulingOption.UseAvailability || _cellValue.SchedulingOption.UseStudentAvailability) && (_cellValue.EffectiveRestriction != null && _cellValue.EffectiveRestriction.Absence != null));
-
-        }
-
-        public bool CanPaintScheduledDayOff()
-        {
-            return (_cellValue.SchedulingOption.UseScheduling && _cellValue.HasDayOff);
-        }
-
-        public bool CanPaintScheduledShift()
-        {
-            if (_cellValue.SchedulingOption.UseRotations && _cellValue.HasFullDayAbsence)
-                return false;
-            return (_cellValue.SchedulingOption.UseScheduling && !_cellValue.HasFullDayAbsence && !_cellValue.HasDayOff && _cellValue.HasShift);
 
         }
     }
