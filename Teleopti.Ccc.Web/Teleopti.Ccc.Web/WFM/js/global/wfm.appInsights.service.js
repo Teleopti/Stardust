12e8c24817a08f2edf2f0e9beb58370085a90ecf
Alina Morariu <none@none>
2016-04-22 08:28:29 +0200

    #38074 service for metrics

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/wfm.appInsights.service.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+    angular.module('appInsightsService', []).service('appInsights', [function () {
+
+        var config = appInsights.config;
+                config.enableDebug = true;
+                config.verboseLogging = true;
+                appInsights.config = config;
+
+                function trackPageView(pageName, url) {
+                appInsights.trackPageView(pageName, url);
+            }
+
+            function trackEvent(event) {
+                appInsights.trackEvent(event);
+            }
+
+            return {
+                trackPageView: trackPageView,
+                trackEvent: trackEvent
+            }
+
+        }
+    ]);
+})();
