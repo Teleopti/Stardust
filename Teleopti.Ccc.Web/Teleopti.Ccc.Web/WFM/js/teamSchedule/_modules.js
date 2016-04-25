4b5a508918b941eeae13f0fdce6ccf2e3880f8ac
Zulong Chen <none@none>
2016-04-25 15:55:37 +0800

    Change primary color in line with Styleguide

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/_modules.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/_modules.js
@@ -8,8 +8,15 @@
 				'500': 'FFA726',
 				'600': 'FB8C00',
 			});
+			var teleoptiStyleguidePrimaryMap = $mdThemingProvider.extendPalette('blue', {
+				'100': '99D6FF',
+				'300': '66C2FF',
+				'500': '0099FF',
+			});
 			$mdThemingProvider.definePalette('teleoptiStyleguideWarn', teleoptiStyleguideWarnMap);
+			$mdThemingProvider.definePalette('teleoptiStyleguidePrimary', teleoptiStyleguidePrimaryMap);
 			$mdThemingProvider.theme('default')
+				.primaryPalette('teleoptiStyleguidePrimary')
 				.warnPalette('teleoptiStyleguideWarn', {
 					'default': '400'
 				});
