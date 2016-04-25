8271577bc3626db20e528c89a0691a9a064eb3ab
Amanda <amanda.jonasson@teleopti.com>
2016-04-25 12:34:47 +0200

    Upgrade to 4.6.1

--- Manager.Integration/Manager.Integration.Test.WPF/Model1.Designer.cs
+++ Manager.Integration/Manager.Integration.Test.WPF/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\antons\Documents\Stardust\Manager.Integration\Manager.Integration.Test.WPF\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Development\Stardust\Manager.Integration\Manager.Integration.Test.WPF\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
