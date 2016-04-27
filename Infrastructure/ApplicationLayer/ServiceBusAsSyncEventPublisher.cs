6fc29565c335a620b1c4f06180cb82270be66762
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-04-27 14:51:13 +0200

    #35703 Fixed simulated service bus publishing event processing order

--- Infrastructure/ApplicationLayer/ServiceBusAsSyncEventPublisher.cs
+++ Infrastructure/ApplicationLayer/ServiceBusAsSyncEventPublisher.cs
@@ -1,29 +1,78 @@
-﻿using Teleopti.Ccc.Domain.ApplicationLayer;
-using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NHibernate.Util;
+using Teleopti.Ccc.Domain;
+using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 {
 	public class ServiceBusAsSyncEventPublisher : IEventPublisher
 	{
-		private readonly ResolveEventHandlers _resolver;
-		private readonly CommonEventProcessor _processor;
+		private readonly ServiceBusEventProcessor _processor;
+		private readonly Queue<IEvent> _queue = new Queue<IEvent>();
+		private readonly object processLock = new object();
 
-		public ServiceBusAsSyncEventPublisher(
-			ResolveEventHandlers resolver, 
-			CommonEventProcessor processor)
+		public ServiceBusAsSyncEventPublisher(ServiceBusEventProcessor processor)
 		{
-			_resolver = resolver;
 			_processor = processor;
 		}
 
+#pragma warning disable 618
+
 		public void Publish(params IEvent[] events)
 		{
-			foreach (var @event in events)
-#pragma warning disable 618
-				foreach (var handlerType in _resolver.HandlerTypesFor<IRunOnServiceBus>(@event))
-#pragma warning restore 618
-					_processor.Process(@event, handlerType);
+			events.ForEach(_queue.Enqueue);
+
+			if (!Monitor.TryEnter(processLock, 0)) return;
+
+			try
+			{
+				var exceptions = new List<Exception>();
+				var thread = new Thread(() =>
+				{
+					try
+					{
+						while (_queue.Count > 0)
+						{
+							try
+							{
+								ProcessLikeTheBus(_queue.Dequeue());
+							}
+							catch (Exception e)
+							{
+								PreserveStack.For(e);
+								exceptions.Add(e);
+							}
+						}
+					}
+					catch (Exception e)
+					{
+						PreserveStack.For(e);
+						exceptions.Add(e);
+					}
+				});
+				thread.Start();
+				thread.Join();
+				if (exceptions.Any())
+					throw new AggregateException(exceptions);
+			}
+			finally
+			{
+				Monitor.Exit(processLock);
+			}
+
 		}
+
+		[AsSystem]
+		protected virtual void ProcessLikeTheBus(IEvent @event)
+		{
+			_processor.Process(@event);
+		}
+
+#pragma warning restore 618
+
 	}
 }
