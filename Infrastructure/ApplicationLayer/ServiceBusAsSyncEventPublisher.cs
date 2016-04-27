a1d0ebfb50f5faa0bf35b46f6f2b68443805ff25
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-04-27 16:30:33 +0200

    #35703 Should work

--- Infrastructure/ApplicationLayer/ServiceBusAsSyncEventPublisher.cs
+++ Infrastructure/ApplicationLayer/ServiceBusAsSyncEventPublisher.cs
@@ -4,27 +4,40 @@ using System.Threading;
 using NHibernate.Util;
 using Teleopti.Ccc.Domain;
 using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Interfaces.Domain;
+using System.Linq;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 {
+#pragma warning disable 618
+
 	public class ServiceBusAsSyncEventPublisher : IEventPublisher
 	{
 		private readonly ServiceBusEventProcessor _processor;
+		private readonly ResolveEventHandlers _resolver;
 		private readonly Queue<IEvent> _queue = new Queue<IEvent>();
 		private readonly object processLock = new object();
 
-		public ServiceBusAsSyncEventPublisher(ServiceBusEventProcessor processor)
+		public ServiceBusAsSyncEventPublisher(
+			ServiceBusEventProcessor processor, 
+			ResolveEventHandlers resolver)
 		{
 			_processor = processor;
+			_resolver = resolver;
 		}
 
 		public void Publish(params IEvent[] events)
 		{
-			events.ForEach(_queue.Enqueue);
+			events.ForEach(e =>
+			{
+				if (!_resolver.HandlerTypesFor<IRunOnServiceBus>(e).Any())
+					return;
+				_queue.Enqueue(e);
+			});
 
-			if (Monitor.TryEnter(processLock, 0))
+			if (_queue.Any() && Monitor.TryEnter(processLock, 0))
 				process();
 		}
 
@@ -33,20 +46,7 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 			try
 			{
 				var exceptions = new List<Exception>();
-				var thread = new Thread(() =>
-				{
-					try
-					{
-						processQueue(exceptions);
-					}
-					catch (Exception e)
-					{
-						PreserveStack.For(e);
-						exceptions.Add(e);
-					}
-				});
-				thread.Start();
-				thread.Join();
+				startProcessingThread(() => processQueue(exceptions));
 				if (exceptions.Any())
 					throw new AggregateException(exceptions);
 			}
@@ -54,6 +54,13 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 			{
 				Monitor.Exit(processLock);
 			}
+		}
+
+		private static void startProcessingThread(ThreadStart run)
+		{
+			var thread = new Thread(run);
+			thread.Start();
+			thread.Join();
 		}
 
 		private void processQueue(ICollection<Exception> exceptions)
@@ -79,4 +86,6 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 		}
 
 	}
+
+#pragma warning restore 618
 }
