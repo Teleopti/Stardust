85703d7b0c4f9ff8368726c9922997e2eed07efc
Chundan Xu <chundan.xu@teleopti.com>
2016-05-04 12:58:19 +0800

    Backed out changeset: 513fb10c273f

--- Domain/Scheduling/Assignment/PersistableScheduleDataPermissionChecker.cs
+++ Domain/Scheduling/Assignment/PersistableScheduleDataPermissionChecker.cs
@@ -31,6 +31,32 @@ namespace Teleopti.Ccc.Domain.Scheduling.Assignment
 		}
 	}
 
+	public class ByPassPersistableScheduleDataPermissionChecker : IPersistableScheduleDataPermissionChecker
+	{
+		public IList<IPersistableScheduleData> GetPermittedData(IEnumerable<IPersistableScheduleData> persistableScheduleData)
+		{
+			var authorization = PrincipalAuthorization.Instance();
+			var permittedData = persistableScheduleData.Where(d =>
+			{
+				var forAuthorization =
+					new PersistableScheduleDataForAuthorization(d);
+				if (forAuthorization.FunctionPath != DefinedRaptorApplicationFunctionPaths.ModifyPersonAbsence &&
+					forAuthorization.FunctionPath != DefinedRaptorApplicationFunctionPaths.ModifyPersonAssignment)
+					return authorization.IsPermitted(
+						forAuthorization.FunctionPath,
+						forAuthorization.DateOnly,
+						forAuthorization.Person);
+				return true;
+			}).ToList();
+			return permittedData;
+		}
+
+		public bool IsModifyPersonAssPermitted(DateOnly dateOnly, IPerson person)
+		{
+			return true;
+		}
+	}
+
 	public interface IPersistableScheduleDataPermissionChecker
 	{
 		IList<IPersistableScheduleData> GetPermittedData(
