b2ec6663c686307de7b7c6f18ade7a5507f99939
Claes Stenman <claes.stenman@teleopti.com>
2016-05-10 12:47:20 +0200

    PBI:#38524 more view for cascading skills

--- Win/Forecasting/Forms/CascadingSkillsView.Designer.cs
+++ Win/Forecasting/Forms/CascadingSkillsView.Designer.cs
@@ -33,15 +33,16 @@
 			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
 			this.buttonAdvOk = new Syncfusion.Windows.Forms.ButtonAdv();
 			this.buttonAdvCancel = new Syncfusion.Windows.Forms.ButtonAdv();
-			this.labelSourceWorkload = new System.Windows.Forms.Label();
 			this.listBoxCascading = new System.Windows.Forms.ListBox();
 			this.listBoxNonCascading = new System.Windows.Forms.ListBox();
 			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-			this.buttonAdvMakeCascading = new Syncfusion.Windows.Forms.ButtonAdv();
 			this.buttonAdvMakeNonCascading = new Syncfusion.Windows.Forms.ButtonAdv();
+			this.buttonAdvMakeCascading = new Syncfusion.Windows.Forms.ButtonAdv();
 			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
-			this.buttonAdvMoveUp = new Syncfusion.Windows.Forms.ButtonAdv();
 			this.buttonAdvMoveDown = new Syncfusion.Windows.Forms.ButtonAdv();
+			this.buttonAdvMoveUp = new Syncfusion.Windows.Forms.ButtonAdv();
+			this.labelCascadingSkills = new System.Windows.Forms.Label();
+			this.labelNonCascadingSkills = new System.Windows.Forms.Label();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.tableLayoutPanel2.SuspendLayout();
 			this.tableLayoutPanel3.SuspendLayout();
@@ -57,9 +58,10 @@
 			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 5);
 			this.tableLayoutPanel1.Controls.Add(this.listBoxCascading, 0, 1);
 			this.tableLayoutPanel1.Controls.Add(this.listBoxNonCascading, 2, 1);
-			this.tableLayoutPanel1.Controls.Add(this.labelSourceWorkload, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
 			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.labelCascadingSkills, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.labelNonCascadingSkills, 2, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
@@ -73,7 +75,6 @@
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
 			this.tableLayoutPanel1.Size = new System.Drawing.Size(727, 771);
 			this.tableLayoutPanel1.TabIndex = 0;
-			
 			// 
 			// tableLayoutPanel2
 			// 
@@ -93,7 +94,6 @@
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel2.Size = new System.Drawing.Size(727, 50);
 			this.tableLayoutPanel2.TabIndex = 3;
-			
 			// 
 			// buttonAdvOk
 			// 
@@ -129,16 +129,6 @@
 			this.buttonAdvCancel.Text = "xxCancel";
 			this.buttonAdvCancel.UseVisualStyle = true;
 			// 
-			// labelSourceWorkload
-			// 
-			this.labelSourceWorkload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.labelSourceWorkload.AutoSize = true;
-			this.labelSourceWorkload.Location = new System.Drawing.Point(3, 8);
-			this.labelSourceWorkload.Name = "labelSourceWorkload";
-			this.labelSourceWorkload.Size = new System.Drawing.Size(317, 15);
-			this.labelSourceWorkload.TabIndex = 1;
-			this.labelSourceWorkload.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			// 
 			// listBoxCascading
 			// 
 			this.listBoxCascading.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -146,6 +136,7 @@
 			this.listBoxCascading.ItemHeight = 15;
 			this.listBoxCascading.Location = new System.Drawing.Point(3, 35);
 			this.listBoxCascading.Name = "listBoxCascading";
+			this.listBoxCascading.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
 			this.listBoxCascading.Size = new System.Drawing.Size(317, 563);
 			this.listBoxCascading.TabIndex = 4;
 			// 
@@ -156,6 +147,7 @@
 			this.listBoxNonCascading.ItemHeight = 15;
 			this.listBoxNonCascading.Location = new System.Drawing.Point(406, 35);
 			this.listBoxNonCascading.Name = "listBoxNonCascading";
+			this.listBoxNonCascading.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
 			this.listBoxNonCascading.Size = new System.Drawing.Size(318, 563);
 			this.listBoxNonCascading.TabIndex = 5;
 			// 
@@ -174,29 +166,11 @@
 			this.tableLayoutPanel3.Size = new System.Drawing.Size(74, 85);
 			this.tableLayoutPanel3.TabIndex = 6;
 			// 
-			// buttonAdvMakeCascading
-			// 
-			this.buttonAdvMakeCascading.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.buttonAdvMakeCascading.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
-			this.buttonAdvMakeCascading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-			this.buttonAdvMakeCascading.BeforeTouchSize = new System.Drawing.Size(61, 27);
-			this.buttonAdvMakeCascading.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonAdvMakeCascading.ForeColor = System.Drawing.Color.White;
-			this.buttonAdvMakeCascading.IsBackStageButton = false;
-			this.buttonAdvMakeCascading.Location = new System.Drawing.Point(3, 4);
-			this.buttonAdvMakeCascading.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
-			this.buttonAdvMakeCascading.Name = "buttonAdvMakeCascading";
-			this.buttonAdvMakeCascading.Size = new System.Drawing.Size(61, 27);
-			this.buttonAdvMakeCascading.TabIndex = 7;
-			this.buttonAdvMakeCascading.Text = "<";
-			this.buttonAdvMakeCascading.UseVisualStyle = true;
-			// 
 			// buttonAdvMakeNonCascading
 			// 
 			this.buttonAdvMakeNonCascading.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
 			this.buttonAdvMakeNonCascading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
 			this.buttonAdvMakeNonCascading.BeforeTouchSize = new System.Drawing.Size(61, 27);
-			this.buttonAdvMakeNonCascading.DialogResult = System.Windows.Forms.DialogResult.OK;
 			this.buttonAdvMakeNonCascading.ForeColor = System.Drawing.Color.White;
 			this.buttonAdvMakeNonCascading.IsBackStageButton = false;
 			this.buttonAdvMakeNonCascading.Location = new System.Drawing.Point(3, 45);
@@ -206,6 +180,24 @@
 			this.buttonAdvMakeNonCascading.TabIndex = 8;
 			this.buttonAdvMakeNonCascading.Text = ">";
 			this.buttonAdvMakeNonCascading.UseVisualStyle = true;
+			this.buttonAdvMakeNonCascading.Click += new System.EventHandler(this.buttonAdvMakeNonCascadingClick);
+			// 
+			// buttonAdvMakeCascading
+			// 
+			this.buttonAdvMakeCascading.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.buttonAdvMakeCascading.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
+			this.buttonAdvMakeCascading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.buttonAdvMakeCascading.BeforeTouchSize = new System.Drawing.Size(61, 27);
+			this.buttonAdvMakeCascading.ForeColor = System.Drawing.Color.White;
+			this.buttonAdvMakeCascading.IsBackStageButton = false;
+			this.buttonAdvMakeCascading.Location = new System.Drawing.Point(3, 4);
+			this.buttonAdvMakeCascading.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
+			this.buttonAdvMakeCascading.Name = "buttonAdvMakeCascading";
+			this.buttonAdvMakeCascading.Size = new System.Drawing.Size(61, 27);
+			this.buttonAdvMakeCascading.TabIndex = 7;
+			this.buttonAdvMakeCascading.Text = "<";
+			this.buttonAdvMakeCascading.UseVisualStyle = true;
+			this.buttonAdvMakeCascading.Click += new System.EventHandler(this.buttonAdvMakeCascadingClick);
 			// 
 			// tableLayoutPanel4
 			// 
@@ -223,30 +215,12 @@
 			this.tableLayoutPanel4.Size = new System.Drawing.Size(317, 39);
 			this.tableLayoutPanel4.TabIndex = 7;
 			// 
-			// buttonAdvMoveUp
-			// 
-			this.buttonAdvMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-			this.buttonAdvMoveUp.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
-			this.buttonAdvMoveUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-			this.buttonAdvMoveUp.BeforeTouchSize = new System.Drawing.Size(87, 26);
-			this.buttonAdvMoveUp.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonAdvMoveUp.ForeColor = System.Drawing.Color.White;
-			this.buttonAdvMoveUp.IsBackStageButton = false;
-			this.buttonAdvMoveUp.Location = new System.Drawing.Point(32, 3);
-			this.buttonAdvMoveUp.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
-			this.buttonAdvMoveUp.Name = "buttonAdvMoveUp";
-			this.buttonAdvMoveUp.Size = new System.Drawing.Size(87, 26);
-			this.buttonAdvMoveUp.TabIndex = 7;
-			this.buttonAdvMoveUp.Text = "xxMoveUp";
-			this.buttonAdvMoveUp.UseVisualStyle = true;
-			// 
 			// buttonAdvMoveDown
 			// 
 			this.buttonAdvMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
 			this.buttonAdvMoveDown.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
 			this.buttonAdvMoveDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
 			this.buttonAdvMoveDown.BeforeTouchSize = new System.Drawing.Size(87, 26);
-			this.buttonAdvMoveDown.DialogResult = System.Windows.Forms.DialogResult.OK;
 			this.buttonAdvMoveDown.ForeColor = System.Drawing.Color.White;
 			this.buttonAdvMoveDown.IsBackStageButton = false;
 			this.buttonAdvMoveDown.Location = new System.Drawing.Point(190, 3);
@@ -256,6 +230,42 @@
 			this.buttonAdvMoveDown.TabIndex = 8;
 			this.buttonAdvMoveDown.Text = "xxMoveDown";
 			this.buttonAdvMoveDown.UseVisualStyle = true;
+			this.buttonAdvMoveDown.Click += new System.EventHandler(this.buttonAdvMoveDownClick);
+			// 
+			// buttonAdvMoveUp
+			// 
+			this.buttonAdvMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+			this.buttonAdvMoveUp.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro;
+			this.buttonAdvMoveUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+			this.buttonAdvMoveUp.BeforeTouchSize = new System.Drawing.Size(87, 26);
+			this.buttonAdvMoveUp.ForeColor = System.Drawing.Color.White;
+			this.buttonAdvMoveUp.IsBackStageButton = false;
+			this.buttonAdvMoveUp.Location = new System.Drawing.Point(32, 3);
+			this.buttonAdvMoveUp.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
+			this.buttonAdvMoveUp.Name = "buttonAdvMoveUp";
+			this.buttonAdvMoveUp.Size = new System.Drawing.Size(87, 26);
+			this.buttonAdvMoveUp.TabIndex = 7;
+			this.buttonAdvMoveUp.Text = "xxMoveUp";
+			this.buttonAdvMoveUp.UseVisualStyle = true;
+			this.buttonAdvMoveUp.Click += new System.EventHandler(this.buttonAdvMoveUpClick);
+			// 
+			// labelCascadingSkills
+			// 
+			this.labelCascadingSkills.AutoSize = true;
+			this.labelCascadingSkills.Location = new System.Drawing.Point(3, 0);
+			this.labelCascadingSkills.Name = "labelCascadingSkills";
+			this.labelCascadingSkills.Size = new System.Drawing.Size(98, 15);
+			this.labelCascadingSkills.TabIndex = 8;
+			this.labelCascadingSkills.Text = "xxCascadingSkills";
+			// 
+			// labelNonCascadingSkills
+			// 
+			this.labelNonCascadingSkills.AutoSize = true;
+			this.labelNonCascadingSkills.Location = new System.Drawing.Point(406, 0);
+			this.labelNonCascadingSkills.Name = "labelNonCascadingSkills";
+			this.labelNonCascadingSkills.Size = new System.Drawing.Size(121, 15);
+			this.labelNonCascadingSkills.TabIndex = 9;
+			this.labelNonCascadingSkills.Text = "xxNonCascadingSkills";
 			// 
 			// CascadingSkillsView
 			// 
@@ -268,7 +278,7 @@
 			this.ClientSize = new System.Drawing.Size(727, 771);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Font = new System.Drawing.Font("Segoe UI", 9F);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
 			this.HelpButton = false;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
@@ -290,7 +300,6 @@
         private Syncfusion.Windows.Forms.ButtonAdv buttonAdvOk;
         private Syncfusion.Windows.Forms.ButtonAdv buttonAdvCancel;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-		private System.Windows.Forms.Label labelSourceWorkload;
 		private System.Windows.Forms.ListBox listBoxCascading;
 		private System.Windows.Forms.ListBox listBoxNonCascading;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
@@ -299,5 +308,7 @@
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
 		private Syncfusion.Windows.Forms.ButtonAdv buttonAdvMoveDown;
 		private Syncfusion.Windows.Forms.ButtonAdv buttonAdvMoveUp;
+		private System.Windows.Forms.Label labelCascadingSkills;
+		private System.Windows.Forms.Label labelNonCascadingSkills;
 	}
 }
--- Win/Forecasting/Forms/CascadingSkillsView.cs
+++ Win/Forecasting/Forms/CascadingSkillsView.cs
@@ -1,44 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using Teleopti.Ccc.Infrastructure.Repositories;
-using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using System.Linq;
+using System.Windows.Forms;
 using Teleopti.Ccc.Win.Common;
-using Teleopti.Ccc.Win.Common.Controls;
-using Teleopti.Ccc.Win.Main;
-using Teleopti.Ccc.WinCode.Forecasting;
+using Teleopti.Ccc.WinCode.Forecasting.Cascading;
 using Teleopti.Interfaces.Domain;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Win.Forecasting.Forms
 {
     public partial class CascadingSkillsView : BaseDialogForm
     {
-        
-        public CascadingSkillsView(CopyToSkillModel model)
+	    private readonly CascadingSkillModel _model;
+	    private BindingSource _bindingSourceNonCascading;
+	    private BindingSource _bindingSourceCascading;
+
+		public CascadingSkillsView(CascadingSkillModel model)
         {
-            InitializeComponent();
+		    _model = model;
+		    InitializeComponent();
 				
             if (DesignMode) return;
-						var currentUnitOfWork = new FromFactory(() => UnitOfWorkFactory.Current);
-
-            //Presenter = new CopyToSkillPresenter(this, model,
-            //                                     new CopyToSkillCommand(this, model,
-            //                                                            new WorkloadRepository(currentUnitOfWork),
-            //                                                            UnitOfWorkFactory.Current),
-            //                                     new SkillRepository(currentUnitOfWork),
-            //                                     UnitOfWorkFactory.Current);
             SetTexts();
         }
 
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-            if (DesignMode)return;   
+            if (DesignMode)return;
+			
+			_model.Init();
+
+	        _bindingSourceNonCascading = new BindingSource {DataSource = _model.NonCascadingSkills};
+			_bindingSourceCascading = new BindingSource { DataSource = _model.CascadingSkills};
+
+			listBoxNonCascading.DataSource = _bindingSourceNonCascading;
+	        listBoxNonCascading.DisplayMember = "Name";
+
+	        listBoxCascading.DataSource = _bindingSourceCascading;
+	        listBoxCascading.DisplayMember = "Name";
         }
 
-        public void SetCopyFromText(string sourceWorkloadName)
-        {
-            labelSourceWorkload.Text = sourceWorkloadName;
-        }
-    }
+		private void buttonAdvMoveUpClick(object sender, EventArgs e)
+		{
+			var selectedIndices = listBoxCascading.SelectedIndices.Cast<int>().ToList();
+
+			if (selectedIndices.Count > 0 &&  selectedIndices[0] > 0)
+			{
+				foreach (var selectedItem in listBoxCascading.SelectedItems)
+				{ 
+					var skill = selectedItem as ISkill;
+					_model.MoveUpCascadingSkill(skill);
+				}
+
+				_bindingSourceCascading.ResetBindings(false);
+				listBoxCascading.SelectedItems.Clear();
+
+				foreach (var selectedIndex in selectedIndices)
+				{
+					var index = ((int) selectedIndex) - 1;
+					listBoxCascading.SetSelected(index, true);
+				}
+			}
+		}
+
+		private void buttonAdvMoveDownClick(object sender, EventArgs e)
+		{
+			var selectedIndices = listBoxCascading.SelectedIndices.Cast<int>().ToList();
+			var selectedItems = listBoxCascading.SelectedItems.Cast<ISkill>().ToList();
+
+			if(selectedIndices.Count > 0 && selectedIndices[selectedIndices.Count - 1] < _model.CascadingSkills.Count() - 1)
+			{
+				foreach (var skill in selectedItems.Reverse<ISkill>())
+				{
+					_model.MoveDownCascadingSkill(skill);
+				}
+
+				_bindingSourceCascading.ResetBindings(false);
+				listBoxCascading.SelectedItems.Clear();
+
+				foreach (var selectedIndex in selectedIndices)
+				{
+					var index = selectedIndex + 1;
+					listBoxCascading.SetSelected(index, true);
+				}
+			}
+		}
+
+		private void buttonAdvMakeCascadingClick(object sender, EventArgs e)
+		{
+			foreach (var selectedItem in listBoxNonCascading.SelectedItems)
+			{
+				var skill = selectedItem as ISkill;
+				_model.MakeCascading(skill);
+			}
+
+			_bindingSourceCascading.ResetBindings(false);
+			_bindingSourceNonCascading.ResetBindings(false);
+		}
+
+		private void buttonAdvMakeNonCascadingClick(object sender, EventArgs e)
+		{
+			foreach (var selectedItem in listBoxCascading.SelectedItems)
+			{
+				var skill = selectedItem as ISkill;
+				_model.MakeNonCascading(skill);
+			}
+
+			_bindingSourceCascading.ResetBindings(false);
+			_bindingSourceNonCascading.ResetBindings(false);
+		}
+	}
 }
