c2aa7aaf94e8e453efa1722ac04891c610c5ae09
Erik Nordenhök <erik.nordenhok@teleopti.com>
2016-05-11 14:16:40 +0200

    #38213 Fixed a problem with duplicate events for day off which could result in duplicate entries in analytics db.

--- Domain/Scheduling/DayOffTemplate.cs
+++ Domain/Scheduling/DayOffTemplate.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Teleopti.Ccc.Domain.Common.EntityBaseTypes;
 using Teleopti.Interfaces.Domain;
 using System.Drawing;
+using System.Linq;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Interfaces.Infrastructure;
 
@@ -24,173 +26,185 @@ namespace Teleopti.Ccc.Domain.Scheduling
         private bool _isDeleted;
     	private string _payrollCode;
 
-    	/// <summary>
-        /// Gets or sets the description
-        /// </summary>
-        /// <remarks>
-        /// Created by: shirang
-        /// Created date: 2008-10-28
-        /// </remarks>
-        public virtual Description Description
-        {
-            get { return _description; }
-        }
+	    public override IEnumerable<IEvent> PopAllEvents(INow now, DomainUpdateType? operation = null)
+	    {
+		    var events = base.PopAllEvents(now, operation).ToList();
+		    if (operation.HasValue)
+		    {
+			    switch (operation.Value)
+			    {
+				    case DomainUpdateType.Update:
+						events.Add(new DayOffTemplateChangedEvent
+						{
+							DayOffTemplateId = Id.GetValueOrDefault(),
+							DayOffName = Description.Name,
+							DayOffShortName = Description.ShortName,
+							DatasourceUpdateDate = UpdatedOn ?? now.UtcDateTime()
+						});
+						break;
+			    }
+		    }
 
-        public virtual void ChangeDescription(string name, string shortName)
-        {
-            _description = new Description(name, shortName);
+		    return events;
+	    }
 
-            AddEvent(() => new DayOffTemplateChangedEvent
-            {
-                DayOffTemplateId = Id.GetValueOrDefault(),
-                DayOffName = Description.Name,
-                DayOffShortName = Description.ShortName,
-                DatasourceUpdateDate = UpdatedOn ?? DateTime.UtcNow,
-                LogOnBusinessUnitId = BusinessUnit.Id.GetValueOrDefault(),
-                Timestamp = DateTime.UtcNow
-            });
-        }
+	    /// <summary>
+	    /// Gets or sets the description
+	    /// </summary>
+	    /// <remarks>
+	    /// Created by: shirang
+	    /// Created date: 2008-10-28
+	    /// </remarks>
+	    public virtual Description Description
+	    {
+		    get { return _description; }
+	    }
 
-        /// <summary>
-        /// Gets or sets the flexibility.
-        /// </summary>
-        /// <remarks>
-        /// Created by: shirang
-        /// Created date: 2008-10-28
-        /// </remarks>
-        public virtual TimeSpan Flexibility
-        {
-            get { return _flexibility; }
-            protected set
-            {
-                if (value.TotalMinutes > _targetLength.TotalMinutes / 2d)
-                    value = TimeSpan.FromMinutes(_targetLength.TotalMinutes / 2);
-                _flexibility = value;
-            }
-        }
+	    public virtual void ChangeDescription(string name, string shortName)
+	    {
+		    _description = new Description(name, shortName);
+	    }
 
-        /// <summary>
-        /// Gets or sets the length (duration).
-        /// </summary>
-        /// <remarks>
-        /// Created by: shirang
-        /// Created date: 2008-10-28
-        /// </remarks>
-        public virtual TimeSpan TargetLength
-        {
-            get { return _targetLength; }
-            protected set
-            {
-                _targetLength = value;
-                Flexibility = _flexibility;
-            }
-        }
+	    /// <summary>
+	    /// Gets or sets the flexibility.
+	    /// </summary>
+	    /// <remarks>
+	    /// Created by: shirang
+	    /// Created date: 2008-10-28
+	    /// </remarks>
+	    public virtual TimeSpan Flexibility
+	    {
+		    get { return _flexibility; }
+		    protected set
+		    {
+			    if (value.TotalMinutes > _targetLength.TotalMinutes/2d)
+				    value = TimeSpan.FromMinutes(_targetLength.TotalMinutes/2);
+			    _flexibility = value;
+		    }
+	    }
 
-        /// <summary>
-        /// Gets or sets the anchor point relative to the start of the parent interval.
-        /// </summary>
-        /// <remarks>
-        /// Created by: shirang
-        /// Created date: 2008-10-28
-        /// </remarks>
-        public virtual TimeSpan Anchor
-        {
-            get { return _anchor; }
-            set { _anchor = value; }
-        }
+	    /// <summary>
+	    /// Gets or sets the length (duration).
+	    /// </summary>
+	    /// <remarks>
+	    /// Created by: shirang
+	    /// Created date: 2008-10-28
+	    /// </remarks>
+	    public virtual TimeSpan TargetLength
+	    {
+		    get { return _targetLength; }
+		    protected set
+		    {
+			    _targetLength = value;
+			    Flexibility = _flexibility;
+		    }
+	    }
 
-        /// <summary>
-        /// Gets or sets the display color
-        /// </summary>
-        public virtual Color DisplayColor
-        {
-            get { return _displayColor; }
-            set { _displayColor = value; }
-        }
+	    /// <summary>
+	    /// Gets or sets the anchor point relative to the start of the parent interval.
+	    /// </summary>
+	    /// <remarks>
+	    /// Created by: shirang
+	    /// Created date: 2008-10-28
+	    /// </remarks>
+	    public virtual TimeSpan Anchor
+	    {
+		    get { return _anchor; }
+		    set { _anchor = value; }
+	    }
 
-    	public virtual string PayrollCode
-    	{
-			get { return _payrollCode; }
-			set { _payrollCode = value; }
-    	}
+	    /// <summary>
+	    /// Gets or sets the display color
+	    /// </summary>
+	    public virtual Color DisplayColor
+	    {
+		    get { return _displayColor; }
+		    set { _displayColor = value; }
+	    }
 
-    	public virtual bool IsDeleted
-        {
-            get { return _isDeleted; }
-        }
+	    public virtual string PayrollCode
+	    {
+		    get { return _payrollCode; }
+		    set { _payrollCode = value; }
+	    }
 
-        public DayOffTemplate(Description description)
-        {
-            _description = description;
-        }
+	    public virtual bool IsDeleted
+	    {
+		    get { return _isDeleted; }
+	    }
 
-        public DayOffTemplate()
-        {
-        }
+	    public DayOffTemplate(Description description)
+	    {
+		    _description = description;
+	    }
 
-        /// <summary>
-        /// Overloaded ToString method to return the Description's name
-        /// </summary>
-        /// <returns>The name of the DayOff</returns>
-        /// <remarks>
-        /// Created by: shirang
-        /// Created date: 2008-10-28
-        /// </remarks>
-        public override string ToString()
-        {
-            return _description.Name;
-        }
+	    public DayOffTemplate()
+	    {
+	    }
 
-        /// <summary>
-        /// Creates a new object that is a copy of the current instance.
-        /// </summary>
-        /// <returns>
-        /// A new object that is a copy of this instance.
-        /// </returns>
-        /// <filterpriority>2</filterpriority>
-        public virtual object Clone()
-        {
-            return EntityClone();
-        }
+	    /// <summary>
+	    /// Overloaded ToString method to return the Description's name
+	    /// </summary>
+	    /// <returns>The name of the DayOff</returns>
+	    /// <remarks>
+	    /// Created by: shirang
+	    /// Created date: 2008-10-28
+	    /// </remarks>
+	    public override string ToString()
+	    {
+		    return _description.Name;
+	    }
 
-        public virtual void SetTargetAndFlexibility(TimeSpan targetLength, TimeSpan flexibility)
-        {
-            TargetLength = targetLength;
-            Flexibility = flexibility;
-        }
+	    /// <summary>
+	    /// Creates a new object that is a copy of the current instance.
+	    /// </summary>
+	    /// <returns>
+	    /// A new object that is a copy of this instance.
+	    /// </returns>
+	    /// <filterpriority>2</filterpriority>
+	    public virtual object Clone()
+	    {
+		    return EntityClone();
+	    }
 
-        public virtual void SetDeleted()
-        {
-            _isDeleted = true;
-        }
+	    public virtual void SetTargetAndFlexibility(TimeSpan targetLength, TimeSpan flexibility)
+	    {
+		    TargetLength = targetLength;
+		    Flexibility = flexibility;
+	    }
 
-        /// <summary>
-        /// Returns a clone of this T with IEntitiy.Id set to null.
-        /// </summary>
-        /// <returns></returns>
-        /// <remarks>
-        /// Created by: micke
-        /// Created date: 2008-05-27
-        /// </remarks>
-        public virtual DayOffTemplate NoneEntityClone()
-        {
-            var clone = (DayOffTemplate)MemberwiseClone();
-            clone.SetId(null);
+	    public virtual void SetDeleted()
+	    {
+		    _isDeleted = true;
+	    }
 
-            return clone;
-        }
+	    /// <summary>
+	    /// Returns a clone of this T with IEntitiy.Id set to null.
+	    /// </summary>
+	    /// <returns></returns>
+	    /// <remarks>
+	    /// Created by: micke
+	    /// Created date: 2008-05-27
+	    /// </remarks>
+	    public virtual DayOffTemplate NoneEntityClone()
+	    {
+		    var clone = (DayOffTemplate) MemberwiseClone();
+		    clone.SetId(null);
 
-        /// <summary>
-        /// Returns a clone of this T with IEntitiy.Id as this T.
-        /// </summary>
-        /// <returns></returns>
-        /// <remarks>
-        /// Created by: micke
-        /// Created date: 2008-05-27
-        /// </remarks>
-        public virtual DayOffTemplate EntityClone()
-        {
-            return (DayOffTemplate)MemberwiseClone();
-        }
+		    return clone;
+	    }
+
+	    /// <summary>
+	    /// Returns a clone of this T with IEntitiy.Id as this T.
+	    /// </summary>
+	    /// <returns></returns>
+	    /// <remarks>
+	    /// Created by: micke
+	    /// Created date: 2008-05-27
+	    /// </remarks>
+	    public virtual DayOffTemplate EntityClone()
+	    {
+		    return (DayOffTemplate) MemberwiseClone();
+	    }
     }
 }
