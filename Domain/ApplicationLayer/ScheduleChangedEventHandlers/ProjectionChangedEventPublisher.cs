7db75800653b3cce32ccf28384a13b6d6b754e11
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-05-12 14:33:11 +0200

    Backed out: #35703 Moving ProjectionChangedEventPublisher to hangfire off the bus

--- Domain/ApplicationLayer/ScheduleChangedEventHandlers/ProjectionChangedEventPublisher.cs
+++ Domain/ApplicationLayer/ScheduleChangedEventHandlers/ProjectionChangedEventPublisher.cs
@@ -1,44 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
 using log4net;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.Helper;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers
 {
+#pragma warning disable 618
 	public class ProjectionChangedEventPublisher : 
 		IHandleEvent<ScheduleChangedEvent>, 
 		IHandleEvent<ScheduleInitializeTriggeredEventForScheduleProjection>,
 		IHandleEvent<ScheduleInitializeTriggeredEventForScheduleDay>,
 		IHandleEvent<ScheduleInitializeTriggeredEventForPersonScheduleDay>,
-		IRunOnHangfire
+		IRunOnServiceBus
+#pragma warning restore 618
 	{
-		private static readonly ILog Logger = LogManager.GetLogger(typeof(ProjectionChangedEventPublisher));
-
 		private readonly IEventPublisher _publisher;
+	    private static readonly ILog Logger = LogManager.GetLogger(typeof (ProjectionChangedEventPublisher));
 		private readonly IScenarioRepository _scenarioRepository;
 		private readonly IPersonRepository _personRepository;
 		private readonly IScheduleStorage _scheduleStorage;
 		private readonly IProjectionChangedEventBuilder _projectionChangedEventBuilder;
+		private readonly INow _now;
 		private readonly IProjectionVersionPersister _projectionVersionPersister;
 
-		public ProjectionChangedEventPublisher(
-			IEventPublisher publisher, 
-			IScenarioRepository scenarioRepository, 
-			IPersonRepository personRepository, 
-			IScheduleStorage scheduleStorage, 
-			IProjectionChangedEventBuilder projectionChangedEventBuilder, 
-			IProjectionVersionPersister projectionVersionPersister)
+		public ProjectionChangedEventPublisher(IEventPublisher publisher, IScenarioRepository scenarioRepository, IPersonRepository personRepository, IScheduleStorage scheduleStorage, IProjectionChangedEventBuilder projectionChangedEventBuilder, INow now, IProjectionVersionPersister projectionVersionPersister)
 		{
 			_publisher = publisher;
 			_scenarioRepository = scenarioRepository;
 			_personRepository = personRepository;
 			_scheduleStorage = scheduleStorage;
 			_projectionChangedEventBuilder = projectionChangedEventBuilder;
+			_now = now;
 			_projectionVersionPersister = projectionVersionPersister;
 		}
 		
