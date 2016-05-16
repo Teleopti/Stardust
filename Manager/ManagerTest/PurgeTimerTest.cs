b5092f67bdb18b6356b41b4a8b731075d40e7ddc
Amanda <amanda.jonasson@teleopti.com>
2016-05-16 10:56:32 +0200

    tests for Purging

---
+++ Manager/ManagerTest/PurgeTimerTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using ManagerTest.Database;
+using NUnit.Framework;
+using SharpTestsEx;
+using Stardust.Manager;
+
+namespace ManagerTest
+{
+	[TestFixture]
+	class PurgeTimerTest
+	{
+		private readonly string _managerConnectionString = ConfigurationManager.ConnectionStrings["ManagerConnectionString"].ConnectionString;
+		private readonly DatabaseHelper _databaseHelper = new DatabaseHelper();
+
+		[SetUp]
+		public void SetUp()
+		{
+			_databaseHelper.TryClearDatabase();
+
+			// add a new job that should never be purged
+			InsertJobRecords(1, 0); 
+		}
+
+		[Test]
+		public void ShouldDeleteOldJobOnPurge()
+		{
+			InsertJobRecords(1, 2); //two hour old job
+			PurgeTimer purgeTimer = new PurgeTimer(new RetryPolicyProvider(), new ManagerConfiguration(_managerConnectionString, "Route", 10, 10, 1, 1, 1));
+
+			NumberOfJobsInDatabase().Should().Be.EqualTo(1);
+			purgeTimer.Dispose();
+		}
+
+		[Test]
+		public void ShouldDeleteOldJobDetailOnPurge()
+		{
+			InsertJobRecords(1, 2); //two hour old job
+			PurgeTimer purgeTimer = new PurgeTimer(new RetryPolicyProvider(), new ManagerConfiguration(_managerConnectionString, "Route", 10, 10, 1, 1, 1));
+
+			NumberOfDetailsInDatabase().Should().Be.EqualTo(1);
+			purgeTimer.Dispose();
+		}
+
+		[Test]
+		public void ShouldOnlyDeleteBadgeSizeNumberOfJobs()
+		{
+			InsertJobRecords(2, 2); //two hour old job
+			PurgeTimer purgeTimer = new PurgeTimer(new RetryPolicyProvider(), new ManagerConfiguration(_managerConnectionString, "Route", 10, 10, 1, 1, 1));
+
+			NumberOfDetailsInDatabase().Should().Be.EqualTo(2);
+			purgeTimer.Dispose();
+		}
+
+
+		private void InsertJobRecords(int numberOfJobs, int hoursAgoJobWasCreated)
+		{
+			using (var connection = new SqlConnection(_managerConnectionString))
+			{
+				string insertJobCommandText = @"INSERT INTO [Stardust].[Job] ([JobId]
+								   ,[Name]
+								   ,[Created]
+								   ,[CreatedBy]
+								   ,[Started]
+								   ,[Ended]
+								   ,[Serialized]
+								   ,[Type]
+								   ,[SentToWorkerNodeUri]
+								   ,[Result])
+							 VALUES
+								   (@jobId
+								   ,'Name'
+								   ,DATEADD(HOUR, -@hours, GETDATE())
+								   ,'CreatedBy'
+								   ,NULL
+								   ,NULL
+								   ,'Serialized'
+								   ,'Type'
+								   ,'SentToWorkerNodeUri'
+								   ,'Result')";
+
+				string insertDetailCommandText = @"INSERT INTO [Stardust].[JobDetail]
+																([JobId]
+																,[Created]
+																,[Detail])
+															VALUES
+																(@jobId
+																, DATEADD(HOUR, -@hours, GETDATE())
+																, 'Detail')";
+
+				connection.Open();
+
+				while (numberOfJobs > 0)
+				{
+					var jobId = Guid.NewGuid();
+					using (var insertCommand = new SqlCommand(insertJobCommandText, connection))
+					{
+						insertCommand.Parameters.AddWithValue("@jobId", jobId);
+						insertCommand.Parameters.AddWithValue("@hours", hoursAgoJobWasCreated);
+						insertCommand.ExecuteNonQuery();
+					}
+					using (var insertCommand = new SqlCommand(insertDetailCommandText, connection))
+					{
+						insertCommand.Parameters.AddWithValue("@jobId", jobId);
+						insertCommand.Parameters.AddWithValue("@hours", hoursAgoJobWasCreated);
+						insertCommand.ExecuteNonQuery();
+					}
+					numberOfJobs--;
+				}
+			}
+		}
+
+		private int NumberOfJobsInDatabase()
+		{
+			var count = 0;
+			using (var connection = new SqlConnection(_managerConnectionString))
+			{
+				string countJobsCommandText = @"Select count(*) from [Stardust].[Job]";
+
+				connection.Open();
+				using (var countCommand = new SqlCommand(countJobsCommandText, connection))
+				{
+					count = (int) countCommand.ExecuteScalar();
+				}
+			}
+			return count;
+		}
+
+		private int NumberOfDetailsInDatabase()
+		{
+			var count = 0;
+			using (var connection = new SqlConnection(_managerConnectionString))
+			{
+				string countJobsCommandText = @"Select count(*) from [Stardust].[JobDetail]";
+
+				connection.Open();
+				using (var countCommand = new SqlCommand(countJobsCommandText, connection))
+				{
+					count = (int)countCommand.ExecuteScalar();
+				}
+			}
+			return count;
+		}
+	}
+}
