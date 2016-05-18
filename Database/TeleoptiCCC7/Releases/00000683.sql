2ae5ae3dbd011fa601e4c0d37469a895d7b60f67
amandaj <none@none>
2016-05-18 17:08:07 +0200

    Bug #38688 Delete corrupted data if any

--- Database/TeleoptiCCC7/Releases/00000683.sql
+++ Database/TeleoptiCCC7/Releases/00000683.sql
@@ -1,3 +1,7 @@
+--delete corrupted data
+DELETE FROM [Stardust].[JobDetail] WHERE JobId 
+NOT IN (SELECT JobId FROM [Stardust].[Job])
+
 ALTER TABLE [Stardust].[JobDetail] ADD CONSTRAINT FK_JobId FOREIGN KEY (JobId)
 REFERENCES [Stardust].[Job](JobId) ON DELETE CASCADE
 GO
