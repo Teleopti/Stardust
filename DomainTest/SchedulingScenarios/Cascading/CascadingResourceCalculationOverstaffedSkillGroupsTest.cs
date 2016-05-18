efe3121c139b4cbf5d0bd7be0ad3b18202b4317c
Claes Stenman <claes.stenman@teleopti.com>
2016-05-18 09:04:14 +0200

    PBI:#38026 Adding ignored test

--- DomainTest/SchedulingScenarios/Cascading/CascadingResourceCalculationOverstaffedSkillGroupsTest.cs
+++ DomainTest/SchedulingScenarios/Cascading/CascadingResourceCalculationOverstaffedSkillGroupsTest.cs
@@ -23,7 +23,7 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Cascading
 		public CascadingResourceCalculation Target;
 		public Func<ISchedulerStateHolder> SchedulerStateHolder;
 
-		[Test]
+		[Test, Ignore]
 		public void ShouldMoveResourceOnlyWithinSkillGroup()
 		{
 			var scenario = new Scenario("_");
@@ -61,6 +61,59 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Cascading
 				.Should().Be.EqualTo(0);
 			skillBDay.SkillStaffPeriodCollection.First().AbsoluteDifference
 				.Should().Be.EqualTo(-1);
+			skillCDay.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(0);
+		}
+
+		[Test]
+		public void ShouldMoveResourceFromLowestSkillGroupFirst()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var skillA = new Skill("A", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA.SetCascadingIndex_UseFromTestOnly(1);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA, new TimePeriod(8, 0, 9, 0));
+			var skillADay = skillA.CreateSkillDayWithDemand(scenario, dateOnly, 0.5);
+			var skillB = new Skill("B", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB.SetCascadingIndex_UseFromTestOnly(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB, new TimePeriod(8, 0, 9, 0));
+			var skillBDay = skillB.CreateSkillDayWithDemand(scenario, dateOnly, 0.5);
+			var skillC = new Skill("C", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillC.SetCascadingIndex_UseFromTestOnly(3);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillC, new TimePeriod(8, 0, 9, 0));
+			var skillCDay = skillC.CreateSkillDayWithDemand(scenario, dateOnly, 0.5);
+			var agent1 = new Person();
+			agent1.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Utc);
+			agent1.AddPeriodWithSkills(new PersonPeriod(DateOnly.MinValue, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }),
+				new[] { skillA, skillC });
+			var ass1 = new PersonAssignment(agent1, scenario, dateOnly);
+			ass1.AddActivity(activity, new TimePeriod(5, 0, 10, 0));
+			var agent2 = new Person();
+			agent2.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Utc);
+			agent2.AddPeriodWithSkills(new PersonPeriod(DateOnly.MinValue, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }),
+				new[] { skillB, skillC });
+			var ass2 = new PersonAssignment(agent2, scenario, dateOnly);
+			ass2.AddActivity(activity, new TimePeriod(5, 0, 10, 0));
+
+			SchedulerStateHolder.Fill(scenario, new DateOnlyPeriod(dateOnly, dateOnly), new[] { agent1, agent2 }, new[] { ass1, ass2 }, new[] { skillADay, skillBDay, skillCDay });
+
+			Target.ForDay(dateOnly);
+
+			//innan skovling
+			//A = 0.5 från skillgrupp A C
+			//B = 0.5 från skillgrupp B C
+			//C = -0.5
+
+			//efter skovling
+			//A = 0.5 Ska inte flyttas
+			//B = 0 flyttat till C
+			//C = 0
+
+			skillADay.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(0.5);
+			skillBDay.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(0);
 			skillCDay.SkillStaffPeriodCollection.First().AbsoluteDifference
 				.Should().Be.EqualTo(0);
 		}
