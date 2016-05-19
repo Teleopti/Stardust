0c0ca0864c8524ca75097cb7d218ca5cfe96d831
Erik Sundberg <erik.sundberg@teleopti.com>
2016-05-19 11:12:25 +0200

    #38698 Fix

--- Database/TeleoptiCCC7/Releases/00000685.sql
+++ Database/TeleoptiCCC7/Releases/00000685.sql
@@ -1,2 +1,6 @@
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[v_RtaMapping]'))
+DROP VIEW [dbo].[v_RtaMapping]
+GO
+
 ALTER TABLE dbo.RtaStateGroup
 DROP COLUMN IsLogOutState
