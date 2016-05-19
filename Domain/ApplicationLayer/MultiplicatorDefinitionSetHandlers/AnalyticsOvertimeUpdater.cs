b7037a96705e5053baadced6249eef41940af7e1
Erik Nordenhök <erik.nordenhok@teleopti.com>
2016-05-19 16:24:30 +0200

    #38304 Added some debuglogging

--- Domain/ApplicationLayer/MultiplicatorDefinitionSetHandlers/AnalyticsOvertimeUpdater.cs
+++ Domain/ApplicationLayer/MultiplicatorDefinitionSetHandlers/AnalyticsOvertimeUpdater.cs
@@ -1,4 +1,5 @@
-﻿using Teleopti.Ccc.Domain.Analytics;
+﻿using log4net;
+using Teleopti.Ccc.Domain.Analytics;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.FeatureFlags;
@@ -14,6 +15,8 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandler
 		IHandleEvent<MultiplicatorDefinitionSetChanged>,
 		IHandleEvent<MultiplicatorDefinitionSetDeleted>
 	{
+		private readonly static ILog logger = LogManager.GetLogger(typeof(AnalyticsOvertimeUpdater));
+
 		private readonly IAnalyticsOvertimeRepository _analyticsOvertimeRepository;
 		private readonly IAnalyticsBusinessUnitRepository _analyticsBusinessUnitRepository;
 
@@ -21,29 +24,38 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandler
 		{
 			_analyticsOvertimeRepository = analyticsOvertimeRepository;
 			_analyticsBusinessUnitRepository = analyticsBusinessUnitRepository;
+			logger.Debug($"New instance of {nameof(AnalyticsOvertimeUpdater)} was created");
 		}
 
 		[AnalyticsUnitOfWork]
 		public virtual void Handle(MultiplicatorDefinitionSetCreated @event)
 		{
+			logger.Debug($"Consuming {nameof(MultiplicatorDefinitionSetCreated)} for event id = {@event.MultiplicatorDefinitionSetId}.");
 			handle(@event);
 		}
 
 		[AnalyticsUnitOfWork]
 		public virtual void Handle(MultiplicatorDefinitionSetChanged @event)
 		{
+			logger.Debug($"Consuming {nameof(MultiplicatorDefinitionSetChanged)} for event id = {@event.MultiplicatorDefinitionSetId}.");
 			handle(@event);
 		}
 
 		[AnalyticsUnitOfWork]
 		public virtual void Handle(MultiplicatorDefinitionSetDeleted @event)
 		{
+			logger.Debug($"Consuming {nameof(MultiplicatorDefinitionSetDeleted)} for event id = {@event.MultiplicatorDefinitionSetId}.");
 			handle(@event);
 		}
 
 		private void handle(MultiplicatorDefinitionSetChangedBase @event)
 		{
-			if (@event.MultiplicatorType != MultiplicatorType.Overtime) return;
+
+			if (@event.MultiplicatorType != MultiplicatorType.Overtime)
+			{
+				logger.Debug($"Not {nameof(MultiplicatorType.Overtime)}, ending processing.");
+				return;
+			}
 
 			var businessUnit = _analyticsBusinessUnitRepository.Get(@event.LogOnBusinessUnitId);
 
@@ -56,6 +68,5 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandler
 				IsDeleted = @event.IsDeleted
 			});
 		}
-
 	}
 }
