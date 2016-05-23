39ccde4fb6f87e40e1d4f00d2c6cae608342d2df
Roger Kratz <roger.kratz@teleopti.com>
2016-05-23 15:39:42 +0200

    #38026 Removing TODO comment in code (see 5ada20a682f5)

--- Domain/Cascading/SkillGroupPerActivityProvider.cs
+++ Domain/Cascading/SkillGroupPerActivityProvider.cs
@@ -17,7 +17,6 @@ namespace Teleopti.Ccc.Domain.Cascading
 
 		public IEnumerable<CascadingSkillGroup> FetchOrdered(IActivity activity, DateTimePeriod period)
 		{
-			//TODO: it seems that AffectedResources gives back skillgroups with skills using different activities -> incorrect resources below probably...
 			var affectedSkills = ResourceCalculationContext.Fetch().AffectedResources(activity, period).Values;
 			//perf thingy - don't do this every time - look if necessary
 			var cascadingSkills = _stateHolder().SchedulingResultState.CascadingSkills().Where(x => x.Activity.Equals(activity)).ToArray();
