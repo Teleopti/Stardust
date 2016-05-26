d10850e2a75debde7c14417717ce328dee38fb8f
Erik Nordenhök <erik.nordenhok@teleopti.com>
2016-05-26 16:04:48 +0200

    #38133 Made Overtime analytics updates a bit more robust (get changes from app database)

--- Domain/ApplicationLayer/MultiplicatorDefinitionSetHandlers/AnalyticsOvertimeUpdater.cs
+++ Domain/ApplicationLayer/MultiplicatorDefinitionSetHandlers/AnalyticsOvertimeUpdater.cs
@@ -1,10 +1,12 @@
-﻿using log4net;
+﻿using System;
+using log4net;
 using Teleopti.Ccc.Domain.Analytics;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandlers
 {
@@ -19,11 +21,13 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandler
 
 		private readonly IAnalyticsOvertimeRepository _analyticsOvertimeRepository;
 		private readonly IAnalyticsBusinessUnitRepository _analyticsBusinessUnitRepository;
+		private readonly IMultiplicatorDefinitionSetRepository _multiplicatorDefinitionSetRepository;
 
-		public AnalyticsOvertimeUpdater(IAnalyticsOvertimeRepository analyticsOvertimeRepository, IAnalyticsBusinessUnitRepository analyticsBusinessUnitRepository)
+		public AnalyticsOvertimeUpdater(IAnalyticsOvertimeRepository analyticsOvertimeRepository, IAnalyticsBusinessUnitRepository analyticsBusinessUnitRepository, IMultiplicatorDefinitionSetRepository multiplicatorDefinitionSetRepository)
 		{
 			_analyticsOvertimeRepository = analyticsOvertimeRepository;
 			_analyticsBusinessUnitRepository = analyticsBusinessUnitRepository;
+			_multiplicatorDefinitionSetRepository = multiplicatorDefinitionSetRepository;
 			logger.Debug($"New instance of {nameof(AnalyticsOvertimeUpdater)} was created");
 		}
 
@@ -50,22 +54,21 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandler
 
 		private void handle(MultiplicatorDefinitionSetChangedBase @event)
 		{
-
 			if (@event.MultiplicatorType != MultiplicatorType.Overtime)
 			{
 				logger.Debug($"Not {nameof(MultiplicatorType.Overtime)}, ending processing.");
 				return;
 			}
-
+			var multiplicatorDefinitionSet = _multiplicatorDefinitionSetRepository.Get(@event.MultiplicatorDefinitionSetId);
 			var businessUnit = _analyticsBusinessUnitRepository.Get(@event.LogOnBusinessUnitId);
 
 			_analyticsOvertimeRepository.AddOrUpdate(new AnalyticsOvertime
 			{
 				BusinessUnitId = businessUnit.BusinessUnitId,
 				OvertimeCode = @event.MultiplicatorDefinitionSetId,
-				OvertimeName = @event.MultiplicatorDefinitionSetName,
-				DatasourceUpdateDate = @event.DatasourceUpdateDate,
-				IsDeleted = @event.IsDeleted
+				OvertimeName = @multiplicatorDefinitionSet.Name,
+				DatasourceUpdateDate = multiplicatorDefinitionSet.UpdatedOn ?? DateTime.UtcNow,
+				IsDeleted = multiplicatorDefinitionSet.IsDeleted
 			});
 		}
 	}
--- DomainTest/ApplicationLayer/MultiplicatorDefinitionSetHandlers/AnalyticsOvertimeUpdaterTest.cs
+++ DomainTest/ApplicationLayer/MultiplicatorDefinitionSetHandlers/AnalyticsOvertimeUpdaterTest.cs
@@ -5,8 +5,10 @@ using Teleopti.Ccc.Domain.Analytics;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.ApplicationLayer.MultiplicatorDefinitionSetHandlers;
 using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
 using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.DomainTest.ApplicationLayer.MultiplicatorDefinitionSetHandlers
 {
@@ -17,31 +19,43 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.MultiplicatorDefinitionSetHan
 
 		private FakeAnalyticsBusinessUnitRepository _analyticsBusinessUnitRepository;
 		private IAnalyticsOvertimeRepository _analyticsOvertimeRepository;
+		private IMultiplicatorDefinitionSetRepository _multiplicatorDefinitionSetRepository;
+
+		private MultiplicatorDefinitionSet multiplicatorDefinitionSet;
 
 		[SetUp]
 		public void Setup()
 		{
 			_analyticsBusinessUnitRepository = new FakeAnalyticsBusinessUnitRepository();
 			_analyticsOvertimeRepository = MockRepository.GenerateMock<IAnalyticsOvertimeRepository>();
+			_multiplicatorDefinitionSetRepository = MockRepository.GenerateMock<IMultiplicatorDefinitionSetRepository>();
 
-			_target = new AnalyticsOvertimeUpdater(_analyticsOvertimeRepository, _analyticsBusinessUnitRepository);
+			multiplicatorDefinitionSet = new MultiplicatorDefinitionSet("Test", MultiplicatorType.Overtime);
+			multiplicatorDefinitionSet.SetId(Guid.NewGuid());
+			_multiplicatorDefinitionSetRepository.Stub(x => x.Get(multiplicatorDefinitionSet.Id.GetValueOrDefault()))
+				.Return(multiplicatorDefinitionSet);
+
+			_target = new AnalyticsOvertimeUpdater(_analyticsOvertimeRepository, _analyticsBusinessUnitRepository, _multiplicatorDefinitionSetRepository);
 		}
 
 		[Test, TestCaseSource("OvertimeEvents")]
 		public void ShouldAddOrUpdate(MultiplicatorDefinitionSetChangedBase @event)
 		{
+			@event.MultiplicatorDefinitionSetId = multiplicatorDefinitionSet.Id.GetValueOrDefault();
 			_target.Handle((dynamic)@event);
 
 			_analyticsOvertimeRepository.AssertWasCalled(r =>
 					r.AddOrUpdate(Arg<AnalyticsOvertime>.Matches(ao =>
-								ao.IsDeleted == @event.IsDeleted &&
-								ao.OvertimeCode == @event.MultiplicatorDefinitionSetId &&
-								ao.OvertimeName == @event.MultiplicatorDefinitionSetName)));
+								ao.IsDeleted == multiplicatorDefinitionSet.IsDeleted &&
+								ao.OvertimeCode == multiplicatorDefinitionSet.Id.GetValueOrDefault() &&
+								ao.OvertimeName == multiplicatorDefinitionSet.Name)));
 		}
 
 		[Test, TestCaseSource("NonOvertimeEvents")]
 		public void ShouldDoNothingWhenWrongTypeOfMultiplicatorDefinitionSet(MultiplicatorDefinitionSetChangedBase @event)
 		{
+			@event.MultiplicatorDefinitionSetId = multiplicatorDefinitionSet.Id.GetValueOrDefault();
+
 			_target.Handle((dynamic)@event);
 
 			_analyticsOvertimeRepository.AssertWasNotCalled(r => r.AddOrUpdate(Arg<AnalyticsOvertime>.Is.Anything));
@@ -61,6 +75,8 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.MultiplicatorDefinitionSetHan
 			new object[] {CreateEvent<MultiplicatorDefinitionSetChanged>(MultiplicatorType.OBTime) },
 			new object[] {CreateEvent<MultiplicatorDefinitionSetDeleted>(MultiplicatorType.OBTime) }
 		};
+
+		
 
 		private static T CreateEvent<T>(MultiplicatorType type) where T : MultiplicatorDefinitionSetChangedBase, new()
 		{
