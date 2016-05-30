e425d934afc0e6d35a6afa1bac64feeab5b71944
Kunning Mao <none@none>
2016-05-30 14:52:43 +0200

    #38674 default backplane tpye for cloud installation is azure service bus

--- teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
@@ -232,7 +232,7 @@ function SetDefaultSettings{
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(DEFAULT_IDENTITY_PROVIDER)" -content "`$(DEFAULT_IDENTITY_PROVIDER)|Teleopti"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(WindowsClaimProvider)" -content "`$(WindowsClaimProvider)|"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(TeleoptiClaimProvider)" -content "`$(TeleoptiClaimProvider)|<add identifier=`"urn:Teleopti`" displayName=`"Teleopti`" url=`"https://$DataSourceName.teleopticloud.com/Web/sso/`" protocolHandler=`"RelativeOpenIdHandler`" />"
-	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(SIGNALR_BACKPLANE_TYPE)" -content "`$(SIGNALR_BACKPLANE_TYPE)|"
+	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(SIGNALR_BACKPLANE_TYPE)" -content "`$(SIGNALR_BACKPLANE_TYPE)|azureservicebus"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(SIGNALR_BACKPLANE_PREFIX)" -content "`$(SIGNALR_BACKPLANE_PREFIX)|$DataSourceName"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(SDK_CRED_PROT)" -content "`$(SDK_CRED_PROT)|None"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(MATRIX_WEB_SITE_URL)" -content "`$(MATRIX_WEB_SITE_URL)|https://$DataSourceName.teleopticloud.com/Analytics"
