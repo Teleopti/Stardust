826b9d8a944488819b580c2a0a138ed3b73d1871
Erik Sundberg <erik.sundberg@teleopti.com>
2016-06-08 15:47:21 +0200

    #31235 Mark readmodel as nightshift

--- Domain/ApplicationLayer/Rta/ReadModelUpdaters/AdherencePercentageReadModel.cs
+++ Domain/ApplicationLayer/Rta/ReadModelUpdaters/AdherencePercentageReadModel.cs
@@ -19,6 +19,7 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.ReadModelUpdaters
 		public bool ShiftHasEnded { get; set; }
 
 		public IEnumerable<AdherencePercentageReadModelState> State { get; set; }
+		public bool? Nightshift { get; set; }
 	}
 
 	public class AdherencePercentageReadModelState
--- Domain/ApplicationLayer/Rta/ReadModelUpdaters/AdherencePercentageReadModelUpdater.cs
+++ Domain/ApplicationLayer/Rta/ReadModelUpdaters/AdherencePercentageReadModelUpdater.cs
@@ -41,7 +41,7 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.ReadModelUpdaters
 					Timestamp = @event.ShiftStartTime,
 					ShiftStarted = true
 				},
-				m => { });
+				m => { m.Nightshift = @event.Nightshift; });
 		}
 
 		[ReadModelUnitOfWork]
