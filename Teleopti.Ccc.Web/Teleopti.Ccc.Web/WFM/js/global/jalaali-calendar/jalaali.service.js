2339557f8898632adcbd9d52ed7f3e1213bd76ec
herman.dahlen@teleopti.com <herman.dahlen@teleopti.com>
2016-06-10 11:13:50 +0200

    Added service to get jalaali date

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/jalaali-calendar/jalaali.service.js
@@ -0,0 +1,17 @@
+﻿(function () {
+	'use strict';
+	angular
+	  .module('wfm.jalaali', [])
+	  .service('JalaaliService', ['$resource', function ($resource) {
+​
+	  	this.getJalaaliDate = function (data) {
+	  		return $resource('../api/Schedule/FetchData', {}, {
+	  			query: {
+	  				method: 'GET',
+	  				isArray: false
+	  			}
+	  		}).query().$promise;
+	  	};
+​
+	  }]);
+})();
