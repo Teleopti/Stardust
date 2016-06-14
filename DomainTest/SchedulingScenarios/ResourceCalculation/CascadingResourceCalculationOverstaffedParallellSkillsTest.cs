b82a54184388e60a5350f67b404871e5cfa406aa
Roger Kratz <roger.kratz@teleopti.com>
2016-06-14 16:55:59 +0200

    #39102 Adding some ignored tests. Need some more info from Anders before fixing them.

--- DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationOverstaffedParallellSkillsTest.cs
+++ DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationOverstaffedParallellSkillsTest.cs
@@ -58,5 +58,82 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
 			skillDayB2.SkillStaffPeriodCollection.First().AbsoluteDifference
 				.Should().Be.EqualTo(-0.75);
 		}
+
+		[Test, Ignore(
+@"2 B fixed. 
+Need info from Anders what to assert/expect.
+Also - when this test is fixed, we need some similar test when B1 or B2 doesn't have 'enough' understaffing.
+")]
+		public void ShouldMoveResourcesToTwoSkillsWithDifferentDemand()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var skillA = new Skill("A", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA.SetCascadingIndex_UseFromTestOnly(1);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA, new TimePeriod(8, 0, 9, 0));
+			var skillDayA = skillA.CreateSkillDayWithDemand(scenario, dateOnly, 0.5);
+			var skillB1 = new Skill("B1", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB1.SetCascadingIndex_UseFromTestOnly(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB1, new TimePeriod(8, 0, 9, 0));
+			var skillDayB1 = skillB1.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var skillB2 = new Skill("B2", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB2.SetCascadingIndex_UseFromTestOnly(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB2, new TimePeriod(8, 0, 9, 0));
+			var skillDayB2 = skillB2.CreateSkillDayWithDemand(scenario, dateOnly, 2);
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc);
+			agent.AddPeriodWithSkills(new PersonPeriod(DateOnly.MinValue, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), new[] { skillA, skillB1, skillB2 });
+			var ass = new PersonAssignment(agent, scenario, dateOnly);
+			ass.AddActivity(activity, new TimePeriod(5, 0, 10, 0));
+			SchedulerStateHolder.Fill(scenario, new DateOnlyPeriod(dateOnly, dateOnly), new[] { agent }, new[] { ass }, new[] { skillDayA, skillDayB1, skillDayB2 });
+
+			Target.ForDay(dateOnly);
+
+			//should we expect.... (=put everything on B2 because it will still be more understaffed than B1 after the shoveling
+			skillDayA.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(0);
+			skillDayB1.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(-1);
+			skillDayB2.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(-1.5);
+			//...or should we expect.... (=put based on % - twice as much on B2 than B1 because the demand is twice as high)
+			var b1Understaffed = -skillDayB1.SkillStaffPeriodCollection.First().AbsoluteDifference;
+			var b2Understaffed = -skillDayB2.SkillStaffPeriodCollection.First().AbsoluteDifference;
+			(Math.Abs(b1Understaffed*2 - b2Understaffed) < 0.0001).Should().Be.True();
+		}
+
+		[Test, Ignore("2 B fixed. Probably easiest to wait fixing this until we know how to handle ShouldMoveResourcesToTwoSkillsWithDifferentDemand above (impl to fix this one will probably differ)")]
+		public void ShouldMoveAllResourcesToOneSkillsIfLastSubSkillHasNoDemand()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var skillA = new Skill("A", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA.SetCascadingIndex_UseFromTestOnly(1);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA, new TimePeriod(8, 0, 9, 0));
+			var skillDayA = skillA.CreateSkillDayWithDemand(scenario, dateOnly, 0.5);
+			var skillB1 = new Skill("B1", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB1.SetCascadingIndex_UseFromTestOnly(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB1, new TimePeriod(8, 0, 9, 0));
+			var skillDayB1 = skillB1.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var skillB2 = new Skill("B2", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB2.SetCascadingIndex_UseFromTestOnly(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB2, new TimePeriod(8, 0, 9, 0));
+			var skillDayB2 = skillB2.CreateSkillDayWithDemand(scenario, dateOnly, 0);
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc);
+			agent.AddPeriodWithSkills(new PersonPeriod(DateOnly.MinValue, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), new[] { skillA, skillB1, skillB2 });
+			var ass = new PersonAssignment(agent, scenario, dateOnly);
+			ass.AddActivity(activity, new TimePeriod(5, 0, 10, 0));
+			SchedulerStateHolder.Fill(scenario, new DateOnlyPeriod(dateOnly, dateOnly), new[] { agent }, new[] { ass }, new[] { skillDayA, skillDayB1, skillDayB2 });
+
+			Target.ForDay(dateOnly);
+
+			skillDayA.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(0);
+			skillDayB1.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(-0.5);
+			skillDayB2.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.EqualTo(0);
+		}
 	}
 }
