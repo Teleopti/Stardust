59da8f0959f00aa7a654489312e8f5fc1e6ce438
Rob Wright <robert.wright@teleopti.com>
2016-06-14 10:11:17 +0800

    Fix local build error, possibly due to hidden control characters in text of file.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/jalaali-calendar/jalaali.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/jalaali-calendar/jalaali.service.js
@@ -1,9 +1,8 @@
 ﻿(function () {
 	'use strict';
-	angular
-	  .module('wfm.jalaali', [])
+
+	angular.module('wfm.jalaali', [])
 	  .service('JalaaliService', ['$resource', function ($resource) {
-​
 	  	this.getJalaaliDate = function (data) {
 	  		return $resource('../api/Schedule/FetchData', {}, {
 	  			query: {
@@ -12,6 +11,5 @@
 	  			}
 	  		}).query().$promise;
 	  	};
-​
 	  }]);
 })();
