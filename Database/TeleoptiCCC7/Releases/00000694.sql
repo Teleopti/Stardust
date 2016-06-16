ddd7235ae213a1c5780491a447f2c5dcdf112542
Xianwei Shen <xianwei.shen@teleopti.com>
2016-06-16 18:13:30 +0200

    #39081 fix build

--- Database/TeleoptiCCC7/Releases/00000694.sql
+++ Database/TeleoptiCCC7/Releases/00000694.sql
@@ -1,6 +1,12 @@
-CREATE TABLE ReadModel.PersonSkills
+CREATE TABLE [ReadModel].[PersonSkills]
 	(
 	PersonId uniqueidentifier NOT NULL,
 	SkillId uniqueidentifier NOT NULL
-	)  ON [PRIMARY]
-GO
+	)
+ON [PRIMARY]
+ALTER TABLE  [ReadModel].[PersonSkills] ADD CONSTRAINT [UQ_Person_Skill] UNIQUE NONCLUSTERED 
+(
+	[PersonId] ASC,
+	[SkillId] ASC
+)
+ON [PRIMARY]
