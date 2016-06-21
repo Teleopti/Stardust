63668206cf46c33230824b52e4305dce2097cfd8
Ola <none@none>
2016-06-21 08:30:00 +0200

    added wfm routing to tenant admin info

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MultiTenancy/MultiTenancyAreaRegistration.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MultiTenancy/MultiTenancyAreaRegistration.cs
@@ -14,6 +14,15 @@ namespace Teleopti.Ccc.Web.Areas.MultiTenancy
 
 		public override void RegisterArea(AreaRegistrationContext context)
 		{
+			var mapRoute = context.MapRoute(
+				"MultiTenancy_Wfm",
+				"Wfm/MultiTenancy/TenantAdminInfo/{action}",
+				new { controller = "TenantAdminInfo", action = "Index", area = "MultiTenancy", origin = "WFM" },
+				null,
+				new[] { "Teleopti.Ccc.Web.Areas.MultiTenancy.*" });
+
+			mapRoute.DataTokens["area"] = "MultiTenancy";
+
 			context.MapRoute(
 				"MultiTenancy",
 				"MultiTenancy/TenantAdminInfo/{action}",
