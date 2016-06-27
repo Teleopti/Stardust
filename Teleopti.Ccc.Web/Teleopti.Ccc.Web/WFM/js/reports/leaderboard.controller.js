2ca98cbd1791526b780d877150df38840a387244
Chundan Xu <chundan.xu@teleopti.com>
2016-06-27 16:08:23 +0800

    PBI #39440 now front end service works with the real data.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/reports/leaderboard.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/reports/leaderboard.controller.js
@@ -8,12 +8,10 @@
 	function LeaderBoardCtrl(LeaderBoardSvc, ToggleSvc) {
 		var vm = this;
 
-		vm.fakeData = { "Agents": [{ "AgentName": "John Smith", "Gold": 10, "Silver": 6, "Bronze": 3 }, { "AgentName": "Jon Kleinsmith", "Gold": 10, "Silver": 3, "Bronze": 4 }, { "AgentName": "Stephen Bay", "Gold": 4, "Silver": 6, "Bronze": 4 }, { "AgentName": "Robert Klashner", "Gold": 4, "Silver": 2, "Bronze": 2 }, { "AgentName": "Carlos Oliveira", "Gold": 3, "Silver": 0, "Bronze": 0 }, { "AgentName": "Pierre Baldi", "Gold": 2, "Silver": 3, "Bronze": 5 }, { "AgentName": "Juancho Banaag", "Gold": 2, "Silver": 2, "Bronze": 6 }, { "AgentName": "Prashant Arora", "Gold": 2, "Silver": 1, "Bronze": 5 }, { "AgentName": "Kari Nies", "Gold": 2, "Silver": 0, "Bronze": 0 }, { "AgentName": "Daniel Billsus", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Michael Kantor", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Bill Gates", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Candy Mamer", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Tim McMahon", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Sharad Mehrotra", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "George Lueker", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Steve Novack", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Dmitry Pavlov", "Gold": 0, "Silver": 3, "Bronze": 7 }, { "AgentName": "Ashley Andeen", "Gold": 0, "Silver": 1, "Bronze": 1 }, { "AgentName": "John Smith", "Gold": 10, "Silver": 6, "Bronze": 3 }, { "AgentName": "Jon Kleinsmith", "Gold": 10, "Silver": 3, "Bronze": 4 }, { "AgentName": "Stephen Bay", "Gold": 4, "Silver": 6, "Bronze": 4 }, { "AgentName": "Robert Klashner", "Gold": 4, "Silver": 2, "Bronze": 2 }, { "AgentName": "Carlos Oliveira", "Gold": 3, "Silver": 0, "Bronze": 0 }, { "AgentName": "Pierre Baldi", "Gold": 2, "Silver": 3, "Bronze": 5 }, { "AgentName": "Juancho Banaag", "Gold": 2, "Silver": 2, "Bronze": 6 }, { "AgentName": "Prashant Arora", "Gold": 2, "Silver": 1, "Bronze": 5 }, { "AgentName": "Kari Nies", "Gold": 2, "Silver": 0, "Bronze": 0 }, { "AgentName": "Daniel Billsus", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Michael Kantor", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Bill Gates", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Candy Mamer", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Tim McMahon", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Sharad Mehrotra", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "George Lueker", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Steve Novack", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Dmitry Pavlov", "Gold": 0, "Silver": 3, "Bronze": 7 }, { "AgentName": "Ashley Andeen", "Gold": 0, "Silver": 1, "Bronze": 1 }, { "AgentName": "Carlos Oliveira", "Gold": 3, "Silver": 0, "Bronze": 0 }, { "AgentName": "Pierre Baldi", "Gold": 2, "Silver": 3, "Bronze": 5 }, { "AgentName": "Juancho Banaag", "Gold": 2, "Silver": 2, "Bronze": 6 }, { "AgentName": "Prashant Arora", "Gold": 2, "Silver": 1, "Bronze": 5 }, { "AgentName": "Kari Nies", "Gold": 2, "Silver": 0, "Bronze": 0 }, { "AgentName": "Daniel Billsus", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Michael Kantor", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Bill Gates", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Candy Mamer", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Tim McMahon", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Sharad Mehrotra", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "George Lueker", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Steve Novack", "Gold": 1, "Silver": 0, "Bronze": 0 }, { "AgentName": "Dmitry Pavlov", "Gold": 0, "Silver": 3, "Bronze": 7 }, { "AgentName": "Ashley Andeen", "Gold": 0, "Silver": 1, "Bronze": 1 }] };
-
 		vm.onKeyWordInSearchInputChanged = function () {
-			LeaderBoardSvc.getLeaderBoardData().then(function () {
-				//temp fake data
-				vm.leaderBoardTableList = vm.fakeData.Agents;
+			LeaderBoardSvc.getLeaderBoardData(vm.searchOptions.keyword).then(function (data) {
+				vm.searchOptions.keyword = data.Keyword;
+				vm.leaderBoardTableList = data.AgentBadges;
 			});
 		};
 
@@ -25,9 +23,9 @@
 				searchKeywordChanged: false
 			};
 
-			LeaderBoardSvc.getLeaderBoardData().then(function () {
-				//temp fake data
-				vm.leaderBoardTableList = vm.fakeData.Agents;
+			LeaderBoardSvc.getLeaderBoardData(vm.searchOptions.keyword).then(function (data) {
+				vm.searchOptions.keyword = data.Keyword;
+				vm.leaderBoardTableList = data.AgentBadges;
 			});
 		};
 
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/reports/leaderboard.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/reports/leaderboard.service.js
@@ -5,12 +5,18 @@
 	LeaderBoardService.$inject = ['$http','$q'];
 
 	function LeaderBoardService($http, $q) {
-		var url = '../api/Reports/GetLeaderBoardData';
+		var url = '../api/Reports/SearchLeaderboard';
 		this.getLeaderBoardData = getLeaderBoardData;
 
-		function getLeaderBoardData() {
+		function getLeaderBoardData(keyword) {
 			var deferred = $q.defer();
-			$http.get(url).success(function (data) {
+			$http.get(url,
+				{
+					params: {
+						keyword: keyword
+					}
+				}
+				).success(function (data) {
 				deferred.resolve(data);
 			});
 			return deferred.promise;
