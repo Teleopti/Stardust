6b8c77791515dbbb9385b6b7db8c47621652b76c
jimmy Broman <none@none>
2016-06-28 15:09:27 +0200

    Small text fixes + making build fail if any error is found

--- teamcity/SmokeTest/Get-SikuliErrors.ps1
+++ teamcity/SmokeTest/Get-SikuliErrors.ps1
@@ -10,30 +10,57 @@ $cred = new-object -typename System.Management.Automation.PSCredential -argument
 $command = {Get-EventLog -LogName Application -After (Get-Date).AddHours("-1") |
 Where-Object {$_.EntryType -like 'Error' -and $_.Source -like 'Teleopti*' -and $_.Message -notlike "*product activation*"}}
 
-$Eventlog = Invoke-Command -ComputerName $servername -ScriptBlock $command -Credential $cred | ft -autosize
+$Eventlog = Invoke-Command -ComputerName $servername -ScriptBlock $command -Credential $cred | ft -Property EntryType, Source, Message -autosize
 
     If ($Eventlog) 
     {
+        Write-Output 'Searching Eventlog/Application for Teleopti Errors:'
         Write-Output $Eventlog 
     }
   
     Else 
     {
-        Write-Host "There is no Teleopti application errors found"
+        Write-Host "There are no Teleopti application errors found"
     }
 
-
+   
 # Search Teleopti.Support.Security.txt for FATAL errors
 $TXTFile = 'C:\Program Files (x86)\Teleopti\DatabaseInstaller\Enrypted\Teleopti.Support.Security.txt'
-Invoke-Command -computername $servername -scriptblock {Select-String -Path $args[0] -pattern FATAL} -Credential $cred -ArgumentList $TXTFile | ft -autosize
+$ReturnTxt = Invoke-Command -computername $servername -scriptblock {Select-String -Path $args[0] -pattern FATAL} -Credential $cred -ArgumentList $TXTFile | ft -Property Line -AutoSize -HideTableHeaders
 
+    If ($ReturnTxt) 
+    {
+        Write-Output "Searching '$TXTFile' for FATAL errors:"
+        Write-Output $ReturnTxt 
+    }
+  
+    Else 
+    {
+        Write-Host "There are no FATAL errors found in $TXTFile"
+    }
 
 # Search Last 3 DBManagerLibrary*.Log for errors
 $path = 'C:\Program Files (x86)\Teleopti\DatabaseInstaller'
-Invoke-Command -ComputerName $servername -ScriptBlock {Get-ChildItem -Path $args[0] -Filter "DBManagerLibrary*.log" |  
+$ReturnLog = Invoke-Command -ComputerName $servername -ScriptBlock {Get-ChildItem -Path $args[0] -Filter "DBManagerLibrary*.log" |  
     where-object { -not $_.PSIsContainer } |
 	sort-object -Property $_.CreationTime |
 	select-object -last 3 | 
     Select-String -Pattern 'error'} -Credential $cred -ArgumentList $path |
-    ft -autosize |
-    Write-Output
+    ft -Property Line -autosize -HideTableHeaders
+  
+    If ($ReturnLog) 
+    {
+        Write-Output "Searching 'DBManagerLibrary*.Log' for errors:"
+        Write-Output $ReturnLog
+    }
+  
+    Else 
+    {
+        Write-Host "There are no errors found in DBManagerLibrary*.Log"
+    }  
+
+ # Make TC fail build if any Error
+ If ($Eventlog -or $ReturnTxt -or $ReturnLog) 
+ {
+ Exit 1
+ }
