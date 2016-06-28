51b51459fbe423865bc8719aed719760cc4476c0
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-06-28 10:30:18 +0200

    #39464 Using distributed lock in minutely recurring activity change processor, so 2 workers wont run it in parallel

--- Domain/ApplicationLayer/Rta/Service/ActivityChangeProcessor.cs
+++ Domain/ApplicationLayer/Rta/Service/ActivityChangeProcessor.cs
@@ -1,25 +1,31 @@
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.DistributedLock;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 {
-	public class ActivityChangeProcessor : IHandleEvent<TenantMinuteTickEvent>, IRunOnHangfire
+	public class ActivityChangeProcessor : 
+		IHandleEvent<TenantMinuteTickEvent>, 
+		IRunOnHangfire
 	{
 		private readonly ContextLoader _contextLoader;
 		private readonly RtaProcessor _processor;
+		private readonly IDistributedLockAcquirer _distributedLock;
 
 		public ActivityChangeProcessor(
 			ContextLoader contextLoader,
-			RtaProcessor processor
+			RtaProcessor processor,
+			IDistributedLockAcquirer distributedLock
 			)
 		{
 			_contextLoader = contextLoader;
 			_processor = processor;
+			_distributedLock = distributedLock;
 		}
 
 		[RecurringJob]
 		public void Handle(TenantMinuteTickEvent @event)
 		{
-			CheckForActivityChanges();
+			_distributedLock.TryLockForTypeOf(this, CheckForActivityChanges);
 		}
 
 		public void CheckForActivityChanges()
