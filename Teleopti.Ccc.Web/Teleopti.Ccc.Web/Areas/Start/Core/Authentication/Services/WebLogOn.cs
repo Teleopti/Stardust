721dfa4e3828daf6c734eaa30d71417455f0329e
Ola <none@none>
2016-06-28 10:27:54 +0200

    bug #39507

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Core/Authentication/Services/WebLogOn.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Start/Core/Authentication/Services/WebLogOn.cs
@@ -58,7 +58,8 @@ namespace Teleopti.Ccc.Web.Areas.Start.Core.Authentication.Services
 
 				var allowed =	_authorization.IsPermitted(DefinedRaptorApplicationFunctionPaths.MyTimeWeb) ||
 								_authorization.IsPermitted(DefinedRaptorApplicationFunctionPaths.Anywhere) ||
-								_authorization.IsPermitted(DefinedRaptorApplicationFunctionPaths.AccessToReports);
+								_authorization.IsPermitted(DefinedRaptorApplicationFunctionPaths.AccessToReports) ||
+								_authorization.IsPermitted(DefinedRaptorApplicationFunctionPaths.OpenPermissionPage);
 
 				if (!allowed)
 					throw new PermissionException("You (" + person.Name + ") don't have permission to access the web portal.");
