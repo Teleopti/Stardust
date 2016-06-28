22dae4cd40be2fcceb07339fca3baa3b4da3815a
Xianwei Shen <xianwei.shen@teleopti.com>
2016-06-28 10:22:42 +0200

    #39519 sort skills alphabetically according to user language

--- Domain/ApplicationLayer/Rta/ViewModels/SkillViewModelBuilder.cs
+++ Domain/ApplicationLayer/Rta/ViewModels/SkillViewModelBuilder.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.ViewModels
 {
 	public class SkillViewModelBuilder
 	{
 		private readonly ISkillRepository _skillRepository;
+		private readonly IUserUiCulture _uiCulture;
 
-		public SkillViewModelBuilder(ISkillRepository skillRepository)
+		public SkillViewModelBuilder(ISkillRepository skillRepository, IUserUiCulture uiCulture)
 		{
 			_skillRepository = skillRepository;
+			_uiCulture = uiCulture;
 		}
 
 		public IEnumerable<SkillViewModel> Build()
@@ -19,7 +23,7 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.ViewModels
 			{
 				Id = x.Id.Value.ToString(),
 				Name = x.Name
-			});
+			}).OrderBy(x => x.Name, StringComparer.Create(_uiCulture.GetUiCulture(), false));
 		}
 	}
 
--- DomainTest/ApplicationLayer/Rta/ViewModels/SkillViewModelBuilderTest.cs
+++ DomainTest/ApplicationLayer/Rta/ViewModels/SkillViewModelBuilderTest.cs
@@ -2,17 +2,25 @@
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta.ViewModels;
+using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.DomainTest.ApplicationLayer.Rta.ViewModels
 {
 	[DomainTest]
-	public class SkillViewModelBuilderTest
+	public class SkillViewModelBuilderTest : ISetup
 	{
 		public FakeSkillRepository SkillRepository;
 		public SkillViewModelBuilder Target;
+		public FakeUserUiCulture UiCulture;
+
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.UseTestDouble(new FakeUserUiCulture(CultureInfoFactory.CreateSwedishCulture())).For<IUserUiCulture>();
+		}
 
 		[Test]
 		public void ShouldGetSkills()
@@ -24,6 +32,37 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.Rta.ViewModels
 
 			result.Id.Should().Be(skill.Id.ToString());
 			result.Name.Should().Be("Sales");
+		}
+
+		[Test]
+		public void ShouldSortByName()
+		{
+			var skill1 = SkillFactory.CreateSkill("C").WithId();
+			var skill2 = SkillFactory.CreateSkill("A").WithId();
+			var skill3 = SkillFactory.CreateSkill("B").WithId();
+			SkillRepository.Has(skill1);
+			SkillRepository.Has(skill2);
+			SkillRepository.Has(skill3);
+
+			var result = Target.Build();
+
+			result.Select(x => x.Name).Should().Have.SameSequenceAs(new[] {"A", "B", "C"});
+		}
+
+		[Test]
+		public void ShouldSortBySwedishName()
+		{
+			var skill1 = SkillFactory.CreateSkill("Ä").WithId();
+			var skill2 = SkillFactory.CreateSkill("A").WithId();
+			var skill3 = SkillFactory.CreateSkill("Å").WithId();
+			SkillRepository.Has(skill1);
+			SkillRepository.Has(skill2);
+			SkillRepository.Has(skill3);
+			UiCulture.IsSwedish();
+
+			var result = Target.Build();
+
+			result.Select(x => x.Name).Should().Have.SameSequenceAs(new[] {"A", "Å", "Ä"});
 		}
 	}
 }
