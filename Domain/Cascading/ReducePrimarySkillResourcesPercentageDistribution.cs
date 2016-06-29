99a40db70364141f58780d32429647dd8d00f642
Roger Kratz <roger.kratz@teleopti.com>
2016-06-29 13:52:31 +0200

    #39463 Fixing issue when reducing resources from understaffed primary skill

--- Domain/Cascading/ReducePrimarySkillResourcesPercentageDistribution.cs
+++ Domain/Cascading/ReducePrimarySkillResourcesPercentageDistribution.cs
@@ -1,24 +1,30 @@
 using System.Collections.Generic;
-using System.Linq;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Cascading
 {
-	//TODO: remove me when #39463 is released/confirmed to work
 	public class ReducePrimarySkillResourcesPercentageDistribution
 	{
 		public void Execute(ISkillStaffPeriodHolder skillStaffPeriodHolder, IEnumerable<ISkill> primarySkills, DateTimePeriod interval, double resourcesMoved)
 		{
-			var primarySkillOverstaff = primarySkills.Sum(primarySkill => skillStaffPeriodHolder.SkillStaffPeriodOrDefault(primarySkill, interval, 0).AbsoluteDifference);
-			if (primarySkillOverstaff.IsZero())
-				return;
-
+			var totalPrimarySkillOverstaff = 0d;
+			var overstaffedPrimarySkills = new List<ISkill>();
 			foreach (var primarySkill in primarySkills)
+			{
+				var absDiff = skillStaffPeriodHolder.SkillStaffPeriodOrDefault(primarySkill, interval, 0).AbsoluteDifference;
+				if (absDiff.IsOverstaffed())
+				{
+					totalPrimarySkillOverstaff += absDiff;
+					overstaffedPrimarySkills.Add(primarySkill);
+				}
+			}
+
+			foreach (var primarySkill in overstaffedPrimarySkills)
 			{
 				var skillStaffPeriod = skillStaffPeriodHolder.SkillStaffPeriodOrDefault(primarySkill, interval, 0);
 				var overstaffForSkill = skillStaffPeriod.AbsoluteDifference;
-				var percentageOverstaff = overstaffForSkill / primarySkillOverstaff;
+				var percentageOverstaff = overstaffForSkill / totalPrimarySkillOverstaff;
 				var resourceToSubtract = resourcesMoved * percentageOverstaff;
 				skillStaffPeriod.AddResources(-resourceToSubtract);
 			}
--- DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationOverstaffedParallellSkillsTest.cs
+++ DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationOverstaffedParallellSkillsTest.cs
@@ -137,6 +137,44 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
 		}
 
 		[Test]
+		public void ShouldNotMoveResourcesFromUnderstaffedPrimarySkill()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var skillA1 = new Skill("skillA1", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA1.SetCascadingIndex(1);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA1, new TimePeriod(8, 0, 9, 0));
+			var skillDayA1 = skillA1.CreateSkillDayWithDemand(scenario, dateOnly, 0); //-> lite mer än 1 överbemannat
+			var skillA2 = new Skill("skillA2", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA2.SetCascadingIndex(1);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA2, new TimePeriod(8, 0, 9, 0));
+			var skillDayA2 = skillA2.CreateSkillDayWithDemand(scenario, dateOnly, 2); //-> lite mer än 1 underbemannat
+			var skillB = new Skill("skillB", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB.SetCascadingIndex(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB, new TimePeriod(8, 0, 9, 0));
+			var skillDayB = skillB.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var multiSkilledAgent = new Person().InTimeZone(TimeZoneInfo.Utc);
+			multiSkilledAgent.AddPeriodWithSkills(new PersonPeriod(DateOnly.MinValue, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), new[] { skillA1, skillA2, skillB });
+			var multiSkilledAss = new PersonAssignment(multiSkilledAgent, scenario, dateOnly);
+			multiSkilledAss.AddActivity(activity, new TimePeriod(8, 0, 9, 0));
+			var agentA1 = new Person().InTimeZone(TimeZoneInfo.Utc);
+			agentA1.AddPeriodWithSkills(new PersonPeriod(DateOnly.MinValue, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), new[] { skillA1 });
+			var a1Ass = new PersonAssignment(agentA1, scenario, dateOnly);
+			a1Ass.AddActivity(activity, new TimePeriod(8, 0, 9, 0));
+
+			Target.ResourceCalculate(dateOnly, ResourceCalculationDataCreator.WithData(scenario, dateOnly, new [] {multiSkilledAss, a1Ass}, new[] { skillDayA1, skillDayA2, skillDayB }, false, false));
+
+			skillDayA1.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.GreaterThanOrEqualTo(1); // should be exactly 1 but fix that in later test. This assert verifies that single skilled guy isn't removed
+			skillDayA2.SkillStaffPeriodCollection.First().AbsoluteDifference
+				.Should().Be.LessThanOrEqualTo(-1);
+			skillDayB.SkillStaffPeriodCollection.First().AbsoluteDifference
+					.Should().Be.IncludedIn(-1, -0.99); // -1 + some resources från A1 that comes from multi skilled guy
+		}
+
+
+		[Test]
 		public void ShouldMoveResourcesFromParallelPrimarySkillsWithDifferentDemand()
 		{
 			var scenario = new Scenario("_");
