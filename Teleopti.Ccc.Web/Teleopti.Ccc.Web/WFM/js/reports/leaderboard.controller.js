b9289fd13d4bebb03c384849e7661c1b4a9b856d
Chundan Xu <chundan.xu@teleopti.com>
2016-06-30 13:29:09 +0800

    PBI #39440 add loading sign.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/reports/leaderboard.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/reports/leaderboard.controller.js
@@ -7,16 +7,19 @@
 
 	function LeaderBoardCtrl(LeaderBoardSvc, ToggleSvc, VMFactory) {
 		var vm = this;
+		vm.isLoading = false;
 
 		vm.onKeyWordInSearchInputChanged = function () {
+			vm.isLoading = true;
 			LeaderBoardSvc.getLeaderBoardData(vm.searchOptions.keyword).then(function (data) {
 				vm.searchOptions.keyword = data.Keyword;
 				vm.leaderBoardTableList = VMFactory.Create(data.AgentBadges);
+				vm.isLoading = false;
 			});
 		};
 
 		vm.init = function () {
-
+			vm.isLoading = true;
 			vm.searchOptions = {
 				keyword: '',
 				isAdvancedSearchEnabled: ToggleSvc.WfmPeople_AdvancedSearch_32973,
@@ -26,6 +29,7 @@
 			LeaderBoardSvc.getLeaderBoardData(vm.searchOptions.keyword).then(function (data) {
 				vm.searchOptions.keyword = data.Keyword;
 				vm.leaderBoardTableList = VMFactory.Create(data.AgentBadges);
+				vm.isLoading = false;
 			});
 		};
 
