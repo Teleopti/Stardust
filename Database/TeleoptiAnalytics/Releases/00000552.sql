f7690aba571526871b4f95d9bf4bb0132c2a9410
Rob Wright <robert.wright@teleopti.com>
2016-06-30 14:12:29 +0800

    #39477: ETL step Stg_Request fails when havin the absence status Cancelled - ensure requests display in request report.

---
+++ Database/TeleoptiAnalytics/Releases/00000552.sql
@@ -0,0 +1,3 @@
+INSERT INTO mart.dim_request_status(request_status_id,request_status_name,resource_key) VALUES (3,'Cancelled','ResRequestStatusCancelled')
+INSERT INTO mart.dim_request_status(request_status_id,request_status_name,resource_key) VALUES (4,'Waitlisted','ResRequestStatusWaitlisted')
+GO 
