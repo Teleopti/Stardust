d827737999826b8937b24161a08963704ff27bc5
Asad Mirza <asad.mirza@teleopti.com>
2016-07-01 11:13:03 +0200

    Bug: #39594 Added a ugly solution

--- Domain/ApplicationLayer/Intraday/ResourceCalculateReadModelUpdater.cs
+++ Domain/ApplicationLayer/Intraday/ResourceCalculateReadModelUpdater.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Linq;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.Logon;
-using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Interfaces.Domain;
+using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Intraday
 {
@@ -14,18 +13,24 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Intraday
 	public class ResourceCalculateReadModelUpdater : IHandleEvent<UpdateResourceCalculateReadModelEvent>, IRunOnStardust
 	{
 		private readonly CalculateForReadModel _calculateForReadModel;
+		private readonly ICurrentUnitOfWorkFactory _currentFactory;
 
-		public ResourceCalculateReadModelUpdater(CalculateForReadModel calculateForReadModel)
+		public ResourceCalculateReadModelUpdater(CalculateForReadModel calculateForReadModel, ICurrentUnitOfWorkFactory current)
 		{
 			_calculateForReadModel = calculateForReadModel;
+			_currentFactory = current;
 		}
 
 		[AsSystem]
 		[UnitOfWork]
 		public virtual void Handle(UpdateResourceCalculateReadModelEvent @event)
 		{
-			var period = new DateOnlyPeriod( new DateOnly( @event.StartDateTime),new DateOnly(@event.EndDateTime));
+			var period = new DateOnlyPeriod(new DateOnly(@event.StartDateTime), new DateOnly(@event.EndDateTime));
 			_calculateForReadModel.ResourceCalculatePeriod(period);
+			var current = _currentFactory.Current().CurrentUnitOfWork();
+			//an ugly solution for bug 39594
+			if (current.IsDirty())
+				current.Clear();
 		}
 	}
 
