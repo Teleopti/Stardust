30373bbeaf6fa55214716e073f3218e152fcc698
junwen.luo <none@none>
2016-07-01 17:11:17 +0800

    PBI #39473: Add toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -92,6 +92,7 @@
 		Wfm_Requests_PrepareForRelease_38771,
 		Wfm_Requests_ApproveDeny_ShiftTrade_38494,
 		Wfm_Requests_Default_Status_Filter_39472,
+		Wfm_Requests_Show_Deny_Reasons_39473,
 
 		Wfm_Intraday_38074,
 
