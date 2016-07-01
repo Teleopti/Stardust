3db380de19475439426bb87fbc3d964d716ea3da
Roger Kratz <roger.kratz@teleopti.com>
2016-07-01 16:19:01 +0200

    #39587 Fixing issue with random failing test due to double NaN

--- Domain/Cascading/ReducePrimarySkillResourcesPercentageDistribution.cs
+++ Domain/Cascading/ReducePrimarySkillResourcesPercentageDistribution.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Interfaces.Domain;
@@ -8,17 +9,24 @@ namespace Teleopti.Ccc.Domain.Cascading
 	{
 		public void Execute(ISkillStaffPeriodHolder skillStaffPeriodHolder, IEnumerable<ISkill> primarySkills, DateTimePeriod interval, ShovelResourcesState state)
 		{
-			var totalPrimarySkillOverstaff = state.TotalResourcesAtStart;
+			if (jumpOutEarly(state))
+				return;
 
 			foreach (var keyValueForPrimarySkillAndResources in state.ResourcesAvailableForPrimarySkill())
 			{
 				var primarySkill = keyValueForPrimarySkillAndResources.Key;
 				var skillStaffPeriod = skillStaffPeriodHolder.SkillStaffPeriodOrDefault(primarySkill, interval);
 				var overstaffForSkill = keyValueForPrimarySkillAndResources.Value;
-				var percentageOverstaff = overstaffForSkill / totalPrimarySkillOverstaff;
+				var percentageOverstaff = overstaffForSkill / state.TotalResourcesAtStart;
 				var resourceToSubtract = state.ResourcesMoved * percentageOverstaff;
 				skillStaffPeriod.AddResources(-resourceToSubtract);
 			}
+		}
+
+		private bool jumpOutEarly(ShovelResourcesState state)
+		{
+			const double tolerance = 0.0000000001;
+			return Math.Abs(state.ResourcesMoved) < tolerance || Math.Abs(state.TotalResourcesAtStart) < tolerance;
 		}
 	}
 }
