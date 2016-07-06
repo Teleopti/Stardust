16a4a973344e700e95bec5a251abe0dbb23ed165
Xinfeng Li <xinfeng.li@teleopti.com>
2016-07-06 14:06:34 +0800

    Fix the problem that SqlException throwned after applied PatchDatabase-Internal-flow.bat

--- .debug-Setup/PatchDatabase-Internal-flow.bat
+++ .debug-Setup/PatchDatabase-Internal-flow.bat
@@ -5,7 +5,7 @@ SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
 SET CD=%ROOTDIR%\..
 CALL :Branch "%CD%"
-SET SqlInstance=localhost
+SET SqlInstance=.
 SET CCC7DB=%Branch%_DemoSales_TeleoptiCCC7
 SET AnalyticsDB=%Branch%_DemoSales_TeleoptiAnalytics
 SET AggDB=%Branch%_DemoSales_TeleoptiCCCAgg
