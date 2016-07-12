d8483d1d8a2840d3d3930f5d85319dee458a7817
Jianfeng <none@none>
2016-07-12 14:53:43 +0800

    PBI #39620: add toggle (remove this duplicated one when leader board is stable)

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -79,6 +79,7 @@
 
 		WfmReportPortal_Basic_38825,
 		WfmReportPortal_LeaderBoard_39440,
+		WfmReportPortal_LeaderBoardByPeriod_39620,
 
 		Wfm_Requests_Basic_35986,
 		Wfm_Requests_People_Search_36294,
