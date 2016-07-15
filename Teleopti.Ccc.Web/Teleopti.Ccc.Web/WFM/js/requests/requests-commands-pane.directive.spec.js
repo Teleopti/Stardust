a09536cf6f9572538db7e55abdbcfd29eaf50a71
junwen.luo <none@none>
2016-07-15 10:28:14 +0800

    PBI #39726: Fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/requests/requests-commands-pane.directive.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/requests/requests-commands-pane.directive.spec.js
@@ -228,7 +228,7 @@ describe('RequestsControllerTests', function () {
 			}
 		}
 		this.notifySubmitApproveWithValidatorsSucess = function () {
-			_notificationResult = "SubmitApproveWithValidatorsSucess";
+			_notificationResult = "SubmitApproveWithValidatorsSuccess";
 		}
 	}
 
