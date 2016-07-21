aca1533426d10a093880dca2c79433dbf45b8c70
Yanyi Wan <none@none>
2016-07-21 15:27:06 +0800

    BUG 39693 Replace team/site/bu/info in LoadPossibleShiftTradeSchedulesIncludeEmptyDays

--- Database/TeleoptiCCC7/Programmability/03StoredProcedures/ReadModel.LoadPossibleShiftTradeSchedulesIncludeEmptyDays.sql
+++ Database/TeleoptiCCC7/Programmability/03StoredProcedures/ReadModel.LoadPossibleShiftTradeSchedulesIncludeEmptyDays.sql
@@ -48,9 +48,9 @@ AS
 	         *,
 		     ROW_NUMBER() OVER (ORDER BY IsEmptySchedule, DayOffFlag, Start) AS 'RowNumber'
         FROM (select p.Person as PersonId,
-			 isnull(sd.TeamId, t.Id) as TeamID, 
-			 isnull(sd.SiteId, s.Id) as SiteId, 
-			 isnull(sd.BusinessUnitId, s.BusinessUnit) as BusinessUnitId, 
+			 t.Id as TeamID, 
+			 s.Id as SiteId, 
+			 s.BusinessUnit as BusinessUnitId, 
 			 isnull(sd.BelongsToDate, @shiftTradeDate) as BelongsToDate, 
 			 sd.Start, sd.[End], sd.Model,
 			 CASE DATEDIFF(MINUTE, sd.Start, sd.[End])
@@ -59,13 +59,9 @@ AS
 								 END AS DayOffFlag,
 			 CASE WHEN Start IS NULL THEN 1 ELSE 0 END As IsEmptySchedule
 			FROM @TempList p left join (select * from ReadModel.PersonScheduleDay where [BelongsToDate] = @shiftTradeDate) sd on sd.PersonId=p.Person
-			join PersonPeriod pp on pp.Parent = p.Person 
+			join PersonPeriod pp on pp.Parent = p.Person AND pp.StartDate <=  @shiftTradeDate AND  @shiftTradeDate < pp.EndDate
 			join Team t on t.Id = pp.Team
-			join [Site] s on s.Id = t.Site
-			where  pp.StartDate = (select max(startdate) from PersonPeriod
-									where startdate <= @shiftTradeDate 
-									and Parent = p.Person
-									group by parent)
+			join [Site] s on s.Id = t.[Site]		
 		) as s
 		ORDER BY IsEmptySchedule, DayOffFlag, Start
 	) 
