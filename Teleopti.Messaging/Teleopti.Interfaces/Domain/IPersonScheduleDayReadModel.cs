18b8c2687b1ede1db18b61448f35780f8ec002e8
Rob Wright <robert.wright@teleopti.com>
2016-07-28 11:45:25 +0800

    Remove noise...

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/IPersonScheduleDayReadModel.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/IPersonScheduleDayReadModel.cs
@@ -2,39 +2,18 @@ using System;
 
 namespace Teleopti.Interfaces.Domain
 {
-	/// <summary>
-	/// Use your english skills and resharper to understand what I am
-	/// </summary>
 	public interface IPersonScheduleDayReadModel
 	{
-		/// <summary>
-		/// Use your english skills and resharper to understand what I am
-		/// </summary>
+	
 		Guid PersonId { get; set; }
-		/// <summary>
-		/// Use your english skills and resharper to understand what I am
-		/// </summary>
+		
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Date")]
 		DateTime Date { get; set; }
-		/// <summary>
-		/// Use your english skills and resharper to understand what I am
-		/// </summary>
 		DateOnly BelongsToDate { get; }
-		/// <summary>
-		/// Use your english skills and resharper to understand what I am
-		/// </summary>
 		DateTime? Start { get; set; }
-		/// <summary>
-		/// Use your english skills and resharper to understand what I am
-		/// </summary>
 		DateTime? End { get; set; }
-		/// <summary>
-		/// Use your english skills and resharper to understand what I am
-		/// </summary>
+
 		string Model { get; set; }
-		/// <summary>
-		/// when request come from a bulletin board, it reference the offer id
-		/// </summary>
 		Guid? ShiftExchangeOffer { get; set; }
 
 		bool IsDayOff { get; set; }
