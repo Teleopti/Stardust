c86b1b84d60aa3efe55ef15a21046e88c42e3227
Jianfeng <none@none>
2016-08-01 14:10:42 +0800

    PBI #39800:add toggle check

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/TeamSchedule/Controllers/TeamScheduleDataController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/TeamSchedule/Controllers/TeamScheduleDataController.cs
@@ -44,6 +44,10 @@ namespace Teleopti.Ccc.Web.Areas.TeamSchedule.Controllers
 				ruleFlags |= BusinessRuleFlags.MinWeekWorkTimeRule;
 				ruleFlags |= BusinessRuleFlags.NewMaxWeekWorkTimeRule;
 			}
+			if (_toggleManager.IsEnabled(Toggles.WfmTeamSchedule_ShowWeeklyRestTimeWarning_39880))
+			{
+				ruleFlags |= BusinessRuleFlags.MinWeeklyRestRule;
+			}
 			if (_toggleManager.IsEnabled(Toggles.WfmTeamSchedule_ShowDayOffWarning_39801))
 			{
 				ruleFlags |= BusinessRuleFlags.NewDayOffRule;
