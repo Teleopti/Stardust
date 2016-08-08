541571e2b69def5e94d4fff41fc888fba354aa5f
Roger Kratz <roger.kratz@teleopti.com>
2016-08-08 10:36:32 +0200

    Rolling back (incorrect?) changes Micke did on 36472a335c30
    Will check but I guess these changes shouldn't been done in the first place.

--- DomainTest/SchedulingScenarios/OvertimeScheduling/OvertimeOnScheduleDaysTest.cs
+++ DomainTest/SchedulingScenarios/OvertimeScheduling/OvertimeOnScheduleDaysTest.cs
@@ -9,6 +9,7 @@ using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.Overtime;
 using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
 using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
@@ -45,6 +46,8 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.OvertimeScheduling
 			contract.AddMultiplicatorDefinitionSetCollection(definitionSet);
 			var agent = PersonRepository.Has(contract, new ContractSchedule("_"), new PartTimePercentage("_"), new Team { Site = new Site("site") }, schedulePeriod, skill);
 			var shiftCategory = new ShiftCategory("_").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(8, 15, 8, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), shiftCategory));
+			agent.Period(dateOnly).RuleSetBag = new RuleSetBag(ruleSet);
 			var skillDay = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(60), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(360)));
 			SkillDayRepository.Has(new List<ISkillDay>{skillDay});
 			PersonAssignmentRepository.Has(agent, scenario, phoneActivity, shiftCategory, dateOnly, new TimePeriod(8, 0, 16, 0));
