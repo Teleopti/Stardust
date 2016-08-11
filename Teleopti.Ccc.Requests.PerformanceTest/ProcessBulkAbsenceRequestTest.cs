a6980534c151a1e738e9b0f76a77b9c439c87a73
amandaj <none@none>
2016-08-11 17:12:04 +0200

    more people, don't care if they are absent already. Use date in march when many are working

--- Teleopti.Ccc.Requests.PerformanceTest/ProcessBulkAbsenceRequestTest.cs
+++ Teleopti.Ccc.Requests.PerformanceTest/ProcessBulkAbsenceRequestTest.cs
@@ -39,17 +39,24 @@ namespace Teleopti.Ccc.Requests.PerformanceTest
 		{
 
 			IEnumerable<Guid> personIds = new List<Guid>
-			{
+			{ // people from team 'FL_Sup_Sun7_71631' and 'FL_Sup_Sun6_00333'
+				new Guid("55E3A133-6305-4C9A-8AEA-A1410113C47D"),
+				new Guid("87E6CEF0-388A-4365-94FA-A1410113C47D"),
 				new Guid("391DB822-3936-4C4D-9634-A1410113C47D"),
-				//new Guid("C922055A-B4D0-4C06-B9AD-A1410113C47D"),
-				//new Guid("47721DE4-A0CB-45EE-A123-A1410113C47D"),
-				//new Guid("CADD42C6-5419-48DD-8514-A25B009AD59D"),
-				//new Guid("DCF2EA04-3031-4436-A229-A1410113C47D"),
-				//new Guid("AE476FA3-7A6C-4948-89C2-A3BF00D0577C"),
-				//new Guid("4886AEDD-E30F-416C-B5E8-A1410113C47D"),
-				//new Guid("E4C7A7A7-8D2C-4591-ACA4-A53D00F82C88"),
-				//new Guid("6069902E-5760-4DF4-B733-A5E00105B099"),
-				//new Guid("BF50C741-A780-4930-A64B-A5E00105F325")
+				new Guid("47721DE4-A0CB-45EE-A123-A1410113C47D"),
+				new Guid("DCF2EA04-3031-4436-A229-A1410113C47D"),
+				new Guid("4886AEDD-E30F-416C-B5E8-A1410113C47D"),
+				new Guid("C922055A-B4D0-4C06-B9AD-A1410113C47D"),
+				new Guid("CADD42C6-5419-48DD-8514-A25B009AD59D"),
+				new Guid("A35F2179-9A4B-40C9-BF7B-A27400997C79"),
+				new Guid("42394840-F905-4B22-915F-A332008A5288"),
+				new Guid("F25262A1-E3C3-4A54-B202-A33200B2E94F"),
+				new Guid("AE476FA3-7A6C-4948-89C2-A3BF00D0577C"),
+				new Guid("1EBFEE75-CE35-40FB-975E-A3BF00D0577C"),
+				new Guid("E4C7A7A7-8D2C-4591-ACA4-A53D00F82C88"),
+				new Guid("12728761-0ED6-422B-B2B5-A5E001051F1E"),
+				new Guid("6069902E-5760-4DF4-B733-A5E00105B099"),
+				new Guid("BF50C741-A780-4930-A64B-A5E00105F325")
 			};
 
 			//Guid businessUnitId;
@@ -66,7 +73,7 @@ namespace Teleopti.Ccc.Requests.PerformanceTest
 				var wfcs = WorkflowControlSetRepository.Get(new Guid("E97BC114-8939-4A70-AE37-A338010FFF19"));
 				foreach (var period in wfcs.AbsenceRequestOpenPeriods)
 				{
-					period.OpenForRequestsPeriod = new DateOnlyPeriod(new DateOnly(2016, 5, 30), new DateOnly(2099, 5, 30));
+					period.OpenForRequestsPeriod = new DateOnlyPeriod(new DateOnly(2016, 3, 1), new DateOnly(2099, 5, 30));
 					period.StaffingThresholdValidator = new StaffingThresholdValidator();
 					period.AbsenceRequestProcess = new GrantAbsenceRequest();
 					var datePeriod = period as AbsenceRequestOpenDatePeriod;
@@ -100,7 +107,7 @@ namespace Teleopti.Ccc.Requests.PerformanceTest
 							PersonRequestId = pReq.Id.Value,
 							InitiatorId = new Guid("00000000-0000-0000-0000-000000000000"),
 							LogOnBusinessUnitId = new Guid("1fa1f97c-ebff-4379-b5f9-a11c00f0f02b"),
-							LogOnDatasource = "Telia",
+							LogOnDatasource = "Teleopti WFM",
 							Timestamp = DateTime.Parse("2016-08-08T11:06:00.7366909Z")
 						}
 						);
@@ -116,8 +123,8 @@ namespace Teleopti.Ccc.Requests.PerformanceTest
 		private IPersonRequest createAbsenceRequest(IPerson person, IAbsence absence)
 		{
 			var req = new AbsenceRequest(absence,
-				new DateTimePeriod(new DateTime(2016, 6, 30, 8, 0, 0, DateTimeKind.Utc),
-					new DateTime(2016, 6, 30, 18, 0, 0, DateTimeKind.Utc)));
+				new DateTimePeriod(new DateTime(2016, 3, 10, 8, 0, 0, DateTimeKind.Utc),
+					new DateTime(2016, 3, 10, 18, 0, 0, DateTimeKind.Utc)));
 			return new PersonRequest(person) {Request = req};
 		}
 
