25ab24720a0551dd5e0cfc10bb91d90d2c3a740a
Yanyi Wan <none@none>
2016-08-16 16:37:05 +0800

    PBI 39870 Allow selecting different page sizes.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.controller.week.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.controller.week.js
@@ -3,8 +3,8 @@
 
 	angular.module('wfm.teamSchedule').controller('TeamScheduleWeeklyCtrl', TeamScheduleWeeklyCtrl);
 
-	TeamScheduleWeeklyCtrl.$inject = ['$stateParams', '$locale', '$filter', 'PersonScheduleWeekViewCreator', 'UtilityService', 'weekViewScheduleSvc'];
-	function TeamScheduleWeeklyCtrl(params, $locale, $filter, WeekViewCreator, Util, weekViewScheduleSvc) {
+	TeamScheduleWeeklyCtrl.$inject = ['$stateParams', '$locale', '$filter', 'PersonScheduleWeekViewCreator', 'UtilityService', 'weekViewScheduleSvc', 'Toggle', 'TeamSchedule'];
+	function TeamScheduleWeeklyCtrl(params, $locale, $filter, WeekViewCreator, Util, weekViewScheduleSvc, toggleSvc, teamScheduleSvc) {
 		var vm = this;
 		vm.searchOptions = {
 			keyword: angular.isDefined(params.keyword) && params.keyword !== '' ? params.keyword : '',
@@ -12,8 +12,20 @@
 		};
 
 		vm.isLoading = false;
+		vm.agentsPerPageSelection = [20, 50, 100, 500];
 
 		vm.onKeyWordInSearchInputChanged = function() {
+			vm.loadSchedules();
+		};
+
+		vm.selectorChanged = function () {
+			teamScheduleSvc.updateAgentsPerPageSetting.post({ agents: vm.paginationOptions.pageSize }).$promise.then(function () {
+				vm.resetSchedulePage();
+			});
+		};
+
+		vm.resetSchedulePage = function () {
+			vm.paginationOptions.pageNumber = 1;			
 			vm.loadSchedules();
 		};
 		
@@ -48,6 +60,23 @@
 		init();
 
 		function init() {
+			vm.toggles = {
+				AddActivityEnabled: toggleSvc.WfmTeamSchedule_AddActivity_37541,
+				RemoveActivityEnabled: toggleSvc.WfmTeamSchedule_RemoveActivity_37743,
+				AbsenceReportingEnabled: toggleSvc.WfmTeamSchedule_AbsenceReporting_35995,
+				RemoveAbsenceEnabled: toggleSvc.WfmTeamSchedule_RemoveAbsence_36705,
+				SeeScheduleChangesByOthers: toggleSvc.WfmTeamSchedule_SeeScheduleChangesByOthers_36303,
+				SelectAgentsPerPageEnabled: toggleSvc.WfmTeamSchedule_SetAgentsPerPage_36230,
+				SwapShiftEnabled: toggleSvc.WfmTeamSchedule_SwapShifts_36231,
+				MoveActivityEnabled: toggleSvc.WfmTeamSchedule_MoveActivity_37744,
+				AddPersonalActivityEnabled: toggleSvc.WfmTeamSchedule_AddPersonalActivity_37742,
+				ShowNightlyRestWarningEnabled: toggleSvc.WfmTeamSchedule_ShowNightlyRestWarning_39619,
+				ModifyShiftCategoryEnabled: toggleSvc.WfmTeamSchedule_ModifyShiftCategory_39797,
+				UndoScheduleEnabled: toggleSvc.WfmTeamSchedule_RevertToPreviousSchedule_39002,
+				CheckOverlappingCertainActivitiesEnabled: toggleSvc.WfmTeamSchedule_ShowWarningForOverlappingCertainActivities_39938,
+				WeekViewEnabled: toggleSvc.WfmTeamSchedule_WeekView_39870
+			};
+
 			vm.weekDays = Util.getWeekdays();
 			vm.paginationOptions.totalPages = 1;
 			vm.loadSchedules();			
