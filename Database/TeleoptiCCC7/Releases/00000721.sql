e16788e05eb4edec8be0ec2a3c5e2fce686eaf2d
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-08-16 15:32:52 +0200

    #40070 Optimize clustered index with PK for rta reads

---
+++ Database/TeleoptiCCC7/Releases/00000721.sql
@@ -0,0 +1,20 @@
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[ReadModel].[v_ScheduleProjectionReadOnlyRTA]'))
+DROP VIEW [ReadModel].[v_ScheduleProjectionReadOnlyRTA]
+GO
+
+DROP INDEX [CIX_ScheduleProjectionReadOnly] ON [ReadModel].[ScheduleProjectionReadOnly] WITH ( ONLINE = OFF )
+GO
+
+ALTER TABLE [ReadModel].[ScheduleProjectionReadOnly] DROP CONSTRAINT [PK_ScheduleProjectionReadOnly]
+GO
+ALTER TABLE [ReadModel].[ScheduleProjectionReadOnly] DROP CONSTRAINT [DF_ScheduleProjectionReadOnly_Id]
+GO
+ALTER TABLE [ReadModel].[ScheduleProjectionReadOnly] DROP COLUMN [Id]
+GO
+
+ALTER TABLE [ReadModel].[ScheduleProjectionReadOnly]
+ADD CONSTRAINT [PK_ScheduleProjectionReadOnly] PRIMARY KEY CLUSTERED (
+	[PersonId] ASC,
+	[BelongsToDate] ASC
+);  
+GO  
