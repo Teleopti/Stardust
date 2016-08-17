51a603bf9fb72f1dde2fb381c2202bb70eff38c2
Ola <none@none>
2016-08-17 14:55:52 +0200

    remove the mapping until I get it right

--- Domain/AbsenceWaitlisting/QueuedAbsenceRequest.cs
+++ Domain/AbsenceWaitlisting/QueuedAbsenceRequest.cs
@@ -6,10 +6,40 @@ namespace Teleopti.Ccc.Domain.AbsenceWaitlisting
 {
 	public class QueuedAbsenceRequest : Entity, IQueuedAbsenceRequest
 	{
-		public virtual IPersonRequest PersonRequest { get; set; }
-		public virtual DateTime Created { get; set; }
-		public virtual DateTime StartDateTime { get; set; }
-		public virtual DateTime EndDateTime { get; set; }
-		public virtual IBusinessUnit BusinessUnit { get; set; }
+		private IPersonRequest _personRequest;
+		private DateTime _created;
+		private DateTime _startDateTime;
+		private DateTime _endDateTime;
+		private IBusinessUnit _businessUnit;
+
+		public virtual IPersonRequest PersonRequest
+		{
+			get { return _personRequest; }
+			set { _personRequest = value; }
+		}
+
+		public virtual DateTime Created
+		{
+			get { return _created; }
+			set { _created = value; }
+		}
+
+		public virtual DateTime StartDateTime
+		{
+			get { return _startDateTime; }
+			set { _startDateTime = value; }
+		}
+
+		public virtual DateTime EndDateTime
+		{
+			get { return _endDateTime; }
+			set { _endDateTime = value; }
+		}
+
+		public virtual IBusinessUnit BusinessUnit
+		{
+			get { return _businessUnit; }
+			set { _businessUnit = value; }
+		}
 	}
 }
