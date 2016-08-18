d6a4b742fec26258917603f81bc2a52acd72f933
Jianfeng <none@none>
2016-08-18 16:18:57 +0800

    PBI #39870: use contrast font color

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.factory.personWeekView.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.factory.personWeekView.js
@@ -5,7 +5,7 @@
 	PersonScheduleWeekViewCreator.$inject = [];
 
 	function PersonScheduleWeekViewCreator() {
-		function createPersonWeekViewModel(personWeek) {			
+		function createPersonWeekViewModel(personWeek) {
 			var days = [];
 			angular.forEach(personWeek.DaySchedules, function(day) {
 				var dayViewModel = {
@@ -14,7 +14,8 @@
 					summeryTimeSpan: day.TimeSpan,
 					isDayOff: day.IsDayOff,
 					isTerminated: day.IsTerminated,
-					color: day.Color
+					color: day.Color,
+					contrastFontColor: getContrastColor(day.Color)
 				};
 				days.push(dayViewModel);
 			});
@@ -26,8 +27,34 @@
 			return personWeekViewModel;
 		}
 
+		function rgbToHexColor(rgbcolor){
+			var rgb =  rgbcolor.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+
+			return (rgb && rgb.length === 4) ? "#" +
+				("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+				("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+				("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+		}
+
+		function getContrastColor(rgbcolor) {
+			if(!rgbcolor) return 'black';
+
+			var hexcolor = rgbToHexColor(rgbcolor);
+
+			if (hexcolor[0] === '#') {
+				hexcolor = hexcolor.substring(1);
+			}
+
+			var r = parseInt(hexcolor.substr(0, 2), 16);
+			var g = parseInt(hexcolor.substr(2, 2), 16);
+			var b = parseInt(hexcolor.substr(4, 2), 16);
+			var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+			return (yiq >= 128) ? 'black' : 'white';
+		}
+
 		function createGroupWeeks(personWeeks) {
-			
+
 			var groupWeeks = [];
 			angular.forEach(personWeeks, function(personWeek) {
 				var personWeekViewModel = createPersonWeekViewModel(personWeek);
