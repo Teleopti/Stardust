0f0f7f1a1767e5ec3206d2b24569fe89b48f7e8b
jimmy Broman <none@none>
2016-08-18 11:21:50 +0200

    test

--- WISE/CreateMSI.ps1
+++ WISE/CreateMSI.ps1
@@ -7,7 +7,7 @@ properties {
     #$source_dir = "$base_dir\src"
     
     #TC Properties
-    $BuildVCSNumber = %dep.TeleoptiWFM_WfmMain.build.vcs.number.TeleoptiWFM_TeleoptiWFM%
+    $BuildVCSNumber = "%dep.TeleoptiWFM_WfmMain.build.vcs.number.TeleoptiWFM_TeleoptiWFM%"
 	$DEPENDENCIESSRC = "$env:DEPENDENCIESSRC"
     $MountKDirectory = "$env:MountKDirectory"
     $ProductVersion = "$env:CccVersion"
