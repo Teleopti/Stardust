255b82270da89f5720004f1032b8411fae8c4f4c
Claes Stenman <claes.stenman@teleopti.com>
2016-08-18 14:43:25 +0200

    PBI:#38872 Added ignore test for cascading schedule in desktop

---
+++ DomainTest/SchedulingScenarios/Scheduling/SchedulingCascadingDesktopTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Scheduling
+{
+	[DomainTest]
+	[Toggle(Toggles.ResourcePlanner_CascadingSkills_38524)]
+	public class SchedulingCascadingDesktopTest
+	{
+		public FullScheduling Target;
+		public Func<ISchedulerStateHolder> SchedulerStateHolderFrom;
+
+		[Test, Ignore]
+		public void ShouldBaseBestShiftOnNonShoveledResourceCalculation()
+		{
+			const int numberOfAgents = 100;
+			var earlyInterval = new TimePeriod(7, 45, 8, 0);
+			var lateInterval = new TimePeriod(15, 45, 16, 0);
+			var date = DateOnly.Today;
+
+			var activity = new Activity("_")
+			{
+				InWorkTime = true,
+				InContractTime = true,
+				RequiresSkill = true	
+			};
+
+			activity.SetId(Guid.NewGuid());
+			var scenario = new Scenario("_");
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var skillA = new Skill("A", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA.SetCascadingIndex(1);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA, new TimePeriod(7, 45, 16, 0));
+			var skillDayA = skillA.CreateSkillDayWithDemand(scenario, date, 1);
+			var skillB = new Skill("B", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillB.SetCascadingIndex(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB, new TimePeriod(7, 45, 16, 0));
+			var skillDayB = skillB.CreateSkillDayWithDemandOnInterval(scenario, date, 1, new Tuple<TimePeriod, double>(lateInterval, 1000)); //should not shovel resources here when deciding what shift to choose		
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(earlyInterval, TimeSpan.FromMinutes(15)), new TimePeriodWithSegment(lateInterval, TimeSpan.FromMinutes(15)), shiftCategory));
+			var agents = new List<IPerson>();
+		
+			for (var i = 0; i < numberOfAgents; i++)
+			{
+				var agent = new Person().WithId().InTimeZone(TimeZoneInfo.Utc);
+				agent.AddPeriodWithSkills(new PersonPeriod(date, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), new[] { skillA, skillB });
+				agent.AddSchedulePeriod(new SchedulePeriod(date, SchedulePeriodType.Day, 1));
+				agent.Period(date).RuleSetBag = new RuleSetBag(ruleSet);
+				agents.Add(agent);
+			}
+
+			var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(date, date), agents, Enumerable.Empty<IPersonAssignment>(), new[] { skillDayA, skillDayB });
+			
+			Target.DoScheduling(date.ToDateOnlyPeriod());
+		
+			var allAssignmentsStartTime = schedulerStateHolderFrom.Schedules.Select(keyValuePair => keyValuePair.Value).
+				Select(range => range.ScheduledDay(date).PersonAssignment()).
+				Select(x => x.Period.StartDateTime.TimeOfDay);
+
+			allAssignmentsStartTime.Count().Should().Be.EqualTo(numberOfAgents);
+			allAssignmentsStartTime.Count(x => x == new TimeSpan(7, 45, 0))
+					.Should().Be.EqualTo(numberOfAgents / 2);
+			allAssignmentsStartTime.Count(x => x == new TimeSpan(8, 0, 0))
+					.Should().Be.EqualTo(numberOfAgents / 2);
+		}
+	}
+}
