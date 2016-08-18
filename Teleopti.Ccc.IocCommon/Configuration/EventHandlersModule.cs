72a407c373d94a1fe8596e20024f37b24839a768
Erik Sundberg <erik.sundberg@teleopti.com>
2016-08-18 15:30:12 +0200

    #40069 Read from agentstate readmodel for sites

--- Teleopti.Ccc.IocCommon/Configuration/EventHandlersModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/EventHandlersModule.cs
@@ -149,12 +149,21 @@ namespace Teleopti.Ccc.IocCommon.Configuration
                 .As<ITeamOutOfAdherenceReadModelPersister>()
                 .As<ITeamOutOfAdherenceReadModelReader>()
                 .SingleInstance();
-            builder.RegisterType<SiteOutOfAdherenceReadModelPersister>()
-                .As<ISiteOutOfAdherenceReadModelPersister>()
+			
+	        builder.RegisterType<SiteOutOfAdherenceReadModelPersister>()
+		        .As<ISiteOutOfAdherenceReadModelPersister>()
+		        .SingleInstance();
+
+			if (_config.Toggle(Toggles.RTA_RemoveSiteTeamOutOfAdherenceReadModels_40069))
+				builder.RegisterType<SiteInAlarmFromAgentStatesReadModelReader>()
+				.As<ISiteOutOfAdherenceReadModelReader>()
+				.SingleInstance();
+			else
+				 builder.RegisterType<SiteOutOfAdherenceReadModelPersister>()
                 .As<ISiteOutOfAdherenceReadModelReader>()
                 .SingleInstance();
 
-            builder.RegisterType<IntervalLengthFetcher>().As<IIntervalLengthFetcher>().SingleInstance();
+			builder.RegisterType<IntervalLengthFetcher>().As<IIntervalLengthFetcher>().SingleInstance();
             builder.RegisterType<AnalyticsFactScheduleTimeHandler>().As<IAnalyticsFactScheduleTimeHandler>().SingleInstance();
             builder.RegisterType<AnalyticsFactScheduleDateHandler>().As<IAnalyticsFactScheduleDateHandler>().SingleInstance();
             builder.RegisterType<AnalyticsFactSchedulePersonHandler>().As<IAnalyticsFactSchedulePersonHandler>().SingleInstance();
