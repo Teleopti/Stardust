b611a2349359ed0268c8c9511aa4e1ea14970e93
Karin Jeppsson <karin.jeppsson@teleopti.com>
2016-08-22 09:46:09 +0200

    Bug #40156: Multiple person periods in mart.dim_person, fix for faster delete

--- Database/TeleoptiAnalytics/Releases/00000558.sql
+++ Database/TeleoptiAnalytics/Releases/00000558.sql
@@ -68,6 +68,7 @@ FROM mart.fact_hourly_availability AS fact
   INNER JOIN #dupl_persons AS dupl
     ON fact.person_id = dupl.remove_person_id
 
+--just in case
 --let the mart.etl_dim_person_delete in Nightly handle the delete from dim_person, but we want to be able to add constraint on person_period_code so reset
 UPDATE mart.dim_person
 SET to_be_deleted= 1,
@@ -78,7 +79,44 @@ FROM mart.dim_person p
  INNER JOIN #dupl_persons AS dupl
 ON p.person_id = dupl.remove_person_id
 
+---drop the FKs to speed up delete
+ALTER TABLE [mart].[fact_schedule] DROP CONSTRAINT [FK_fact_schedule_dim_person]
+
+ALTER TABLE [mart].[fact_schedule_deviation] DROP CONSTRAINT [FK_fact_schedule_deviation_dim_person]
+
+ALTER TABLE [mart].[fact_schedule_day_count] DROP CONSTRAINT [FK_fact_schedule_day_count_dim_person]
+
+ALTER TABLE [mart].[fact_schedule_preference] DROP CONSTRAINT [FK_fact_schedule_preference_dim_person]
+
+
+-- Delete  duplicates from dim_person
+DELETE mart.dim_person
+FROM mart.dim_person p
+INNER JOIN #dupl_persons AS dupl
+ON p.person_id = dupl.remove_person_id
+
 DROP TABLE #dupl_persons
+
 --finally add constraint
 ALTER TABLE [mart].[dim_person] ADD CONSTRAINT AK_person_period_code UNIQUE (person_period_code)
 
+--readd the FKs again
+ALTER TABLE [mart].[fact_schedule]  WITH NOCHECK ADD  CONSTRAINT [FK_fact_schedule_dim_person] FOREIGN KEY([person_id])
+REFERENCES [mart].[dim_person] ([person_id])
+
+ALTER TABLE [mart].[fact_schedule] CHECK CONSTRAINT [FK_fact_schedule_dim_person]
+
+ALTER TABLE [mart].[fact_schedule_deviation]  WITH NOCHECK ADD  CONSTRAINT [FK_fact_schedule_deviation_dim_person] FOREIGN KEY([person_id])
+REFERENCES [mart].[dim_person] ([person_id])
+
+ALTER TABLE [mart].[fact_schedule_deviation] CHECK CONSTRAINT [FK_fact_schedule_deviation_dim_person]
+
+ALTER TABLE [mart].[fact_schedule_day_count]  WITH NOCHECK ADD  CONSTRAINT [FK_fact_schedule_day_count_dim_person] FOREIGN KEY([person_id])
+REFERENCES [mart].[dim_person] ([person_id])
+
+ALTER TABLE [mart].[fact_schedule_day_count] CHECK CONSTRAINT [FK_fact_schedule_day_count_dim_person]
+
+ALTER TABLE [mart].[fact_schedule_preference]  WITH CHECK ADD  CONSTRAINT [FK_fact_schedule_preference_dim_person] FOREIGN KEY([person_id])
+REFERENCES [mart].[dim_person] ([person_id])
+
+ALTER TABLE [mart].[fact_schedule_preference] CHECK CONSTRAINT [FK_fact_schedule_preference_dim_person]
