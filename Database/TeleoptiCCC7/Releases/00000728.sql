44384a19539d6b4ce01690f1cfbdc3802bcd32dc
Anders Forsberg <anders.forsberg@teleopti.com>
2016-08-25 15:44:34 +0200

    Backed out changeset: 5911942d8eeb

--- Database/TeleoptiCCC7/Releases/00000728.sql
+++
@@ -1,9 +0,0 @@
---Enable feature 37931
-DELETE FROM Tenant.AdminUser WHERE Email = 'admin@company.com'
-
-DECLARE @admins int
-SELECT @admins = count(*) FROM Tenant.AdminUser
-IF @admins = 0
-BEGIN
-            UPDATE Tenant.Tenant SET Active = 0
-END
