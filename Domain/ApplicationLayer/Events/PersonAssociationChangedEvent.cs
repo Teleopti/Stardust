1eca86da407785c37a953ffd259c5ab0634d9004
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-08-26 11:27:24 +0200

    #40069 minor

--- Domain/ApplicationLayer/Events/PersonAssociationChangedEvent.cs
+++ Domain/ApplicationLayer/Events/PersonAssociationChangedEvent.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Events
@@ -14,6 +15,7 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Events
 
 		public int? Version { get; set; }
 
+		[RemoveMeWithToggle(Toggles.RTA_RemoveSiteTeamOutOfAdherenceReadModels_40069)]
 		public IEnumerable<Association> PreviousAssociation { get; set; }
 	}
 
