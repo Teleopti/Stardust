e5e4249a66fbb2c2d73cc4a043d24988e7490341
Roger Kratz <roger.kratz@teleopti.com>
2016-08-30 13:45:52 +0200

    #38983 Making "classic with teamblock toggle" more like normal teamblock mode.

--- Domain/Optimization/DayOffOptimizationDesktopTeamBlock.cs
+++ Domain/Optimization/DayOffOptimizationDesktopTeamBlock.cs
@@ -7,72 +7,84 @@ using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.TeamBlock;
+using Teleopti.Ccc.UserTexts;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Optimization
 {
-	public class DayOffOptimizationDesktopTeamBlock : DayOffOptimizationDesktop
+	public class DayOffOptimizationDesktopTeamBlock : IDayOffOptimizationDesktop
 	{
 		private readonly IResourceOptimizationHelper _resourceOptimizationHelper;
 		private readonly IGroupPersonBuilderWrapper _groupPersonBuilderWrapper;
 		private readonly ITeamBlockDayOffOptimizerService _teamBlockDayOffOptimizerService;
 		private readonly Func<ISchedulingResultStateHolder> _schedulingResultStateHolder;
+		private readonly IMatrixListFactory _matrixListFactory;
+		private readonly IOptimizerHelperHelper _optimizerHelperHelper;
+		private readonly Func<ISchedulerStateHolder> _schedulerStateHolder;
+		private readonly IScheduleDayEquator _scheduleDayEquator;
+		private readonly IResourceOptimizationHelperExtended _resouceOptimizationHelperExtended;
+		private readonly IResourceCalculationContextFactory _resourceCalculationContextFactory;
 
-		public DayOffOptimizationDesktopTeamBlock(
-								IResourceOptimizationHelper resourceOptimizationHelper,
+		public DayOffOptimizationDesktopTeamBlock(IResourceOptimizationHelper resourceOptimizationHelper,
 								IGroupPersonBuilderWrapper groupPersonBuilderWrapper,
 								ITeamBlockDayOffOptimizerService teamBlockDayOffOptimizerService,
 								Func<ISchedulingResultStateHolder> schedulingResultStateHolder,
 								IMatrixListFactory matrixListFactory,
 								IOptimizerHelperHelper optimizerHelperHelper,
 								Func<ISchedulerStateHolder> schedulerStateHolder,
-								DaysOffBackToLegalState daysOffBackToLegalState,
-								Func<IScheduleDayChangeCallback> scheduleDayChangeCallback,
 								IScheduleDayEquator scheduleDayEquator,
 								IResourceOptimizationHelperExtended resouceOptimizationHelperExtended,
-								WorkShiftBackToLegalStateServiceProFactory workShiftBackToLegalStateServiceProFactory,
-								ScheduleBlankSpots scheduleBlankSpots,
-								IResourceCalculationContextFactory resourceCalculationContextFactory) :
-			base(matrixListFactory,
-								optimizerHelperHelper,
-								schedulerStateHolder,
-								daysOffBackToLegalState,
-								scheduleDayChangeCallback,
-								scheduleDayEquator,
-								resouceOptimizationHelperExtended,
-								workShiftBackToLegalStateServiceProFactory,
-								scheduleBlankSpots,
-								resourceCalculationContextFactory)
+								IResourceCalculationContextFactory resourceCalculationContextFactory)
 		{
 			_resourceOptimizationHelper = resourceOptimizationHelper;
 			_groupPersonBuilderWrapper = groupPersonBuilderWrapper;
 			_teamBlockDayOffOptimizerService = teamBlockDayOffOptimizerService;
 			_schedulingResultStateHolder = schedulingResultStateHolder;
+			_matrixListFactory = matrixListFactory;
+			_optimizerHelperHelper = optimizerHelperHelper;
+			_schedulerStateHolder = schedulerStateHolder;
+			_scheduleDayEquator = scheduleDayEquator;
+			_resouceOptimizationHelperExtended = resouceOptimizationHelperExtended;
+			_resourceCalculationContextFactory = resourceCalculationContextFactory;
 		}
 
-		protected override void Optimize(IList<IScheduleMatrixOriginalStateContainer> matrixOriginalStateContainerListForDayOffOptimization,
-													DateOnlyPeriod selectedPeriod,
-													ISchedulingProgress backgroundWorker,
-													IOptimizationPreferences optimizationPreferences,
-													IDayOffOptimizationPreferenceProvider dayOffOptimizationPreferenceProvider)
+		public void Execute(DateOnlyPeriod selectedPeriod, IEnumerable<IScheduleDay> selectedDays, ISchedulingProgress backgroundWorker,
+			IOptimizationPreferences optimizationPreferences,
+			IDayOffOptimizationPreferenceProvider dayOffOptimizationPreferenceProvider, Func<IWorkShiftFinderResultHolder> workShiftFinderResultHolder,
+			Action<object, ResourceOptimizerProgressEventArgs> resourceOptimizerPersonOptimized)
 		{
-			var schedulingOptions = new SchedulingOptionsCreator().CreateSchedulingOptions(optimizationPreferences);
-			var matrixListForDayOffOptimization = matrixOriginalStateContainerListForDayOffOptimization.Select(container => container.ScheduleMatrix).ToList();
-			var selectedPersons = matrixListForDayOffOptimization.Select(matrixList => matrixList.Person).Distinct().ToList();
+			var stateHolder = _schedulerStateHolder();
 
-			var resourceCalculateDelayer = new ResourceCalculateDelayer(_resourceOptimizationHelper, 1, schedulingOptions.ConsiderShortBreaks, _schedulingResultStateHolder());
-			_groupPersonBuilderWrapper.SetSingleAgentTeam();
-			var teamInfoFactory = new TeamInfoFactory(_groupPersonBuilderWrapper);
+			resourceOptimizerPersonOptimized(this, new ResourceOptimizerProgressEventArgs(0, 0, Resources.Rescheduling + Resources.ThreeDots));
 
-			_teamBlockDayOffOptimizerService.OptimizeDaysOff(matrixListForDayOffOptimization,
-				selectedPeriod,
-				selectedPersons,
-				optimizationPreferences,
-				schedulingOptions,
-				resourceCalculateDelayer,
-				dayOffOptimizationPreferenceProvider,
-				teamInfoFactory,
-				backgroundWorker);
+			using (_resourceCalculationContextFactory.Create(stateHolder.Schedules, stateHolder.SchedulingResultState.Skills))
+			{
+				ResourceCalculationContext.Fetch().PrimarySkillMode = true;
+
+				var matrixListForSelection = _matrixListFactory.CreateMatrixListForSelection(selectedDays);
+				var matrixContainerList = matrixListForSelection.Select(matrixPro => new ScheduleMatrixOriginalStateContainer(matrixPro, _scheduleDayEquator))
+					.Cast<IScheduleMatrixOriginalStateContainer>().ToList();
+				var matrixList = matrixContainerList.Select(container => container.ScheduleMatrix).ToList();
+
+				_optimizerHelperHelper.LockDaysForDayOffOptimization(matrixList, optimizationPreferences, selectedPeriod);
+				_resouceOptimizationHelperExtended.ResourceCalculateAllDays(backgroundWorker, false);
+				var schedulingOptions = new SchedulingOptionsCreator().CreateSchedulingOptions(optimizationPreferences);
+				var selectedPersons = matrixList.Select(x => x.Person).Distinct().ToList();
+
+				var resourceCalculateDelayer = new ResourceCalculateDelayer(_resourceOptimizationHelper, 1, schedulingOptions.ConsiderShortBreaks, _schedulingResultStateHolder());
+				_groupPersonBuilderWrapper.SetSingleAgentTeam();
+				var teamInfoFactory = new TeamInfoFactory(_groupPersonBuilderWrapper);
+
+				_teamBlockDayOffOptimizerService.OptimizeDaysOff(matrixList,
+					selectedPeriod,
+					selectedPersons,
+					optimizationPreferences,
+					schedulingOptions,
+					resourceCalculateDelayer,
+					dayOffOptimizationPreferenceProvider,
+					teamInfoFactory,
+					backgroundWorker);
+			}
 		}
 	}
 }
