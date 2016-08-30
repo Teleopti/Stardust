95af25b03b6ff02e2133937bf8eb5503cd98b481
jimmy Broman <none@none>
2016-08-30 14:23:25 +0200

    Killing sikuli client when build fails so it wont interfere with next build

--- .debug-Setup/SikulitestCleanup.bat
+++ .debug-Setup/SikulitestCleanup.bat
@@ -1,2 +1,3 @@
 taskkill.exe /IM iisexpress.exe /F /FI "memusage gt 2" /T 2>&1 | exit /B 0
 taskkill.exe /IM iisexpresstray.exe /F /FI "memusage gt 2" /T 2>&1 | exit /B 0
+taskkill.exe /IM Teleopti.Ccc.SmartClientPortal.Shell.exe /F /FI "memusage gt 2" /T 2>&1 | exit /B 0
