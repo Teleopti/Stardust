45c390d09c919dabc6ffaefe813d7f99b8b3f1e0
Mathias Stenbom <mathias.stenbom@teleopti.com>
2016-09-02 14:12:09 +0200

    #40007 Dropping column dependencies

--- Database/TeleoptiCCC7/Releases/00000731.sql
+++ Database/TeleoptiCCC7/Releases/00000731.sql
@@ -2,4 +2,15 @@
 UPDATE [dbo].[RtaRule] SET ThresholdTime = ThresholdTime / 10000000
 GO
 
+-- dropping column dependencies, will be recreated later when Programmability is applied, if those files still exist
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[LoadRtaMappingsFor]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[LoadRtaMappingsFor] 
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[LoadAllRtaMappings]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[LoadAllRtaMappings]
+GO
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[v_RtaMapping]'))
+DROP VIEW [dbo].[v_RtaMapping]
+GO
 ALTER TABLE [dbo].[RtaRule] ALTER COLUMN [ThresholdTime] int NOT NULL
+GO
