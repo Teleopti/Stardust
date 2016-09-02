0c7ca34cd2281e3b9c9f749becd8691e3938887d
Kunning Mao <none@none>
2016-09-02 15:15:10 +0200

    #40008 another try

--- teamcity/SmokeTest/test/AzureAdTest.js
+++ teamcity/SmokeTest/test/AzureAdTest.js
@@ -6,7 +6,7 @@ var MicrosoftLoginPage = require('./pages/MicrosoftLogin.page');
 describe('Azure AD', function() {
 	this.timeout(360 * 1000); // Set global timeout for this test to 6 minutes
 	
-	before(function() {
+    it('should be able to sign in with AD user', function () {
 		// Given that we are showing the IdentityProviders selection page
 		MytimePage.open();
 		if (MytimePage.isCurrentPage()) {
@@ -17,9 +17,6 @@ describe('Azure AD', function() {
 			MytimePage.signout();
 		}
 		IdentityProvidersPage.azureadProvider.waitForExist(10 * 1000);
-    }, 2);
-	
-    it('should be able to sign in with AD user', function () {
 		IdentityProvidersPage.azureadProvider.click();
 		MicrosoftLoginPage.signin();
 		MytimePage.usernameLabel.waitForExist(60 * 1000);
