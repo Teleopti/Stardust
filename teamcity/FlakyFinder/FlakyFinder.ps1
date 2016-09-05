fea1c94d4acbe5ad140cbc6b421d955b2706643b
jimmy Broman <none@none>
2016-09-05 11:27:07 +0200

    Script improvement, functions and parameterized

--- teamcity/FlakyFinder/FlakyFinder.ps1
+++ teamcity/FlakyFinder/FlakyFinder.ps1
@@ -5,56 +5,93 @@
 
 )
 
-#TeleoptiWFM_WebScenarioAll or TeleoptiWFM_WebScenarioCustomer
+#Properties
 $username = "toptinet\tfsintegration"
 $password = "m8kemew0rk"
-$client = New-Object System.Net.WebClient
-$client.Credentials = New-Object System.Net.NetworkCredential $username, $password
+$server = "http://devbuild01"
+$buildTypeMain = "TeleoptiWFM_WfmMain"
+$secpasswd = ConvertTo-SecureString "m8kemew0rk" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($username,$secpasswd)
 
-$latestBuildUrl = "http://devbuild01/httpAuth/app/rest/builds/?locator=buildType:$BuildType,count:1,status:SUCCESS"
-[xml]$latestBuild = $client.DownloadString($latestBuildUrl)
-$latestBuildNumber = $latestBuild.builds.build.number
 
-$BuildNumber = $latestBuildNumber.split('-')[1].split(' ')
+function Get-LatestBuildNumberFromTC() {
+    param(
+        [string] $server,
+        [string] $buildType,
+        [string] $username,
+        [string] $password
+    )
 
-Write-Output ''
-Write-Output "Latest successful build number from $BuildType is: $BuildNumber"
-Write-Output ''
+    $client = New-Object System.Net.WebClient
+    $client.Credentials = New-Object System.Net.NetworkCredential $username, $password
 
-#TeleoptiWFM_WfmMain
-$BuildTypeMain = "TeleoptiWFM_WfmMain"
-$latestBuildUrl = "http://devbuild01/httpAuth/app/rest/builds/?locator=buildType:$BuildTypeMain,count:20,status:SUCCESS"
-[xml]$latestMainBuilds = $client.DownloadString($latestBuildUrl)
+    $latestBuildUrl = [System.String]::Format("{0}/httpAuth/app/rest/builds/?locator=buildType:{1},count:1,status:SUCCESS", $server, $buildType);
+    [xml]$latestBuild = $client.DownloadString($latestBuildUrl)
+    $latestBuildNumber = $latestBuild.builds.build.number
+    
+    [string] $Script:BuildNumber = $latestBuildNumber.split('-')[1].split(' ');
 
-$MainBuilds = $latestMainBuilds.builds.build.number
+    return $Script:BuildNumber
 
-$DependencyBuildMain = $MainBuilds -match $BuildNumber
+}
 
-Write-Output "This is the Dependency WFMMain build: $DependencyBuildMain"
-Write-Output ''
 
-#Setting correct dependency from TeleoptiWFM_WfmMain
-$Build = {<build>
+function Get-DependentBuildFromMain() {
+    param(
+        [string] $server,
+        [string] $buildTypeMain,
+        [string] $username,
+        [string] $password
+    )
+
+    $latestBuildUrl = [System.String]::Format("{0}/httpAuth/app/rest/builds/?locator=buildType:{1},count:20,status:SUCCESS", $server, $buildTypeMain);
+    $client = New-Object System.Net.WebClient
+    $client.Credentials = New-Object System.Net.NetworkCredential $username, $password
+    [xml]$latestMainBuilds = $client.DownloadString($latestBuildUrl)
+
+    $MainBuilds = $latestMainBuilds.builds.build.number
+
+    $Script:DependencyBuildMain = $MainBuilds -match $BuildNumber
+
+}
+
+
+function Set-CorrectBodytoPost() {
+    param(
+        [string]$DependencyBuildMain,
+        [string]$FlakyProjectName
+    )
+
+    $Build = {<build>
     <triggeringOptions queueAtTop="true"/>
     <buildType id="FLAKYPROJECTNAME"/>
     <comment><text>FlakyFinder</text></comment>
     <properties>
-        <property name="FlakyBuild" value="BUILDNUMBER"/>
+    <property name="FlakyBuild" value="BUILDNUMBER"/>
     </properties>
-</build>}
+    </build>}
 
-#Replacing BUILDNUMBER & FLAKYPROJECT name in Scriptblock $Build
-$ScriptBlockString = $Build.ToString()
-$ScriptBlockString = $ScriptBlockString.Replace("BUILDNUMBER", "$DependencyBuildMain")
-$ScriptBlockString = $ScriptBlockString.Replace("FLAKYPROJECTNAME", "$FlakyProjectName")
+    $ScriptBlockString = $Build.ToString()
+    $ScriptBlockString = $ScriptBlockString.Replace("BUILDNUMBER", "$DependencyBuildMain")
+    $ScriptBlockString = $ScriptBlockString.Replace("FLAKYPROJECTNAME", "$FlakyProjectName")
 
-$Build = [ScriptBlock]::Create($ScriptBlockString)
+    $Script:Build = [ScriptBlock]::Create($ScriptBlockString)
 
-Write-Output "This 'Body' will be POSTed to Teamcity host: `n $Build"
-Write-Output ''
+}
 
-#Used for accessing Teamcity
-$secpasswd = ConvertTo-SecureString "m8kemew0rk" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential($username,$secpasswd)
 
-Invoke-WebRequest -Uri http://devbuild01/httpAuth/app/rest/buildQueue -Method POST -ContentType application/xml -Body $Build -Credential $cred
+#Main
+
+Get-LatestBuildNumberFromTC $server $buildType $username $password
+Write-Output "Latest successful build number from $buildType is: $Script:BuildNumber"
+
+Get-DependentBuildFromMain $server $buildTypeMain $username $password
+Write-Output "This is the Dependency WFMMain build: $Script:DependencyBuildMain"
+
+Set-CorrectBodytoPost $Script:DependencyBuildMain $FlakyProjectName
+Write-Output "This 'Body' will be POSTed to Teamcity host: `n $Script:Build"
+
+
+Invoke-WebRequest -Uri $server/httpAuth/app/rest/buildQueue -Method POST -ContentType application/xml -Body $Script:Build -Credential $cred
+
+
