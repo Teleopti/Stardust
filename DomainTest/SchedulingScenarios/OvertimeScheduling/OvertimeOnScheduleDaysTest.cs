c38ea1e3830868059367ce2d1b9c4122fdcda6e3
Claes Stenman <claes.stenman@teleopti.com>
2016-09-05 12:47:32 +0200

    Bug #40092: ignored test

--- DomainTest/SchedulingScenarios/OvertimeScheduling/OvertimeOnScheduleDaysTest.cs
+++ DomainTest/SchedulingScenarios/OvertimeScheduling/OvertimeOnScheduleDaysTest.cs
@@ -11,6 +11,7 @@ using Teleopti.Ccc.Domain.Scheduling.Overtime;
 using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
 using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
+using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
@@ -31,6 +32,7 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.OvertimeScheduling
 		public FakeScenarioRepository ScenarioRepository;
 		public FakePlanningPeriodRepository PlanningPeriodRepository;
 		public IGridlockManager LockManager;
+		public FakeTimeZoneGuard TimeZoneGuard;
 
 		[Test]
 		public void ShouldSkipLockedDaysWithAssignmentWithLayers()
@@ -66,6 +68,50 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.OvertimeScheduling
 			Target.Execute(overtimePreference, new NoSchedulingProgress(), new[] { stateHolder.Schedules[agent].ScheduledDay(dateOnly) });
 			
 			stateHolder.Schedules[agent].ScheduledDay(dateOnly).PersonAssignment(true).OvertimeActivities().Should().Be.Empty();
+		}
+
+		[Test, Ignore]
+		public void ShouldPlaceOvertimeWhenViewerAndAgentTimeZonesAreFarAway()
+		{
+			var definitionSet = new MultiplicatorDefinitionSet("overtime", MultiplicatorType.Overtime);
+			var phoneActivity = ActivityFactory.CreateActivity("phone");
+			var skill = SkillRepository.Has("skill", phoneActivity);
+			var dateOnly = new DateOnly(2015, 10, 12);
+			var scenario = ScenarioRepository.Has("some name");
+			var schedulePeriod = new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1);
+			var worktimeDirective = new WorkTimeDirective(TimeSpan.FromHours(36), TimeSpan.FromHours(63), TimeSpan.FromHours(11), TimeSpan.FromHours(36));
+			var contract = new Contract("contract") { WorkTimeDirective = worktimeDirective, PositivePeriodWorkTimeTolerance = TimeSpan.FromHours(9) };
+			contract.AddMultiplicatorDefinitionSetCollection(definitionSet);
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(23, 15, 23, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), shiftCategory));
+			var agentUserTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time");
+			var endUserTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time");
+			
+			var agent = PersonRepository.Has(contract, new ContractSchedule("_"), 
+											new PartTimePercentage("_"), 
+											new Team { Site = new Site("site") }, schedulePeriod, 
+											ruleSet, skill)
+											.InTimeZone(agentUserTimeZone);
+
+			var skillDay = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(60), new Tuple<int, TimeSpan>(22, TimeSpan.FromMinutes(360)));
+			SkillDayRepository.Has(new List<ISkillDay> { skillDay });
+			PersonAssignmentRepository.Has(agent, scenario, phoneActivity, shiftCategory, dateOnly, new TimePeriod(23, 15, 31, 15));
+			var ass = PersonAssignmentRepository.GetSingle(dateOnly);
+			var stateHolder = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(dateOnly, dateOnly), new[] { agent }, new[] { ass }, skillDay);
+			var overtimePreference = new OvertimePreferences
+			{
+				OvertimeType = definitionSet,
+				ScheduleTag = new ScheduleTag(),
+				SelectedSpecificTimePeriod = new TimePeriod(0, 0, 30, 0),
+				SelectedTimePeriod = new TimePeriod(1, 0, 1, 0),
+				SkillActivity = phoneActivity
+			};
+
+			TimeZoneGuard.SetTimeZone(endUserTimeZone);
+
+			Target.Execute(overtimePreference, new NoSchedulingProgress(), new[] { stateHolder.Schedules[agent].ScheduledDay(dateOnly) });
+
+			stateHolder.Schedules[agent].ScheduledDay(dateOnly).PersonAssignment(true).OvertimeActivities().Should().Not.Be.Empty();
 		}
 	}
 }
