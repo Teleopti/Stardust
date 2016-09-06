5a5f5a22367c4cb6aa4464ffcd34800c38c9bbde
Erik Nordenhök <erik.nordenhok@teleopti.com>
2016-09-06 09:37:36 +0200

    Marked exception as serializable to get rid of warnings in build output

--- Infrastructure/DistributedLock/DistributedLockException.cs
+++ Infrastructure/DistributedLock/DistributedLockException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Teleopti.Ccc.Infrastructure.DistributedLock
 {
+	[Serializable]
 	public class DistributedLockException : Exception
 	{
 		public DistributedLockException(string message) : base(message)
