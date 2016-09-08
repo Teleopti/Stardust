38d691b47b5f6f0e420c69609270722e2a20a168
Kunning Mao <none@none>
2016-09-08 15:23:06 +0200

    update comment

--- Teleopti.Support.Tool/StartStopSystem/RestartTeleopti.ps1
+++ Teleopti.Support.Tool/StartStopSystem/RestartTeleopti.ps1
@@ -254,7 +254,7 @@ Try
 	$BaseUrl = BaseUrl-get $isAzure
 	TeleoptiWindowsServices-Stop
 	IIS-Restart
-	write-host "sleep 5 seconds for IIS to restart ..."
+	write-host "sleep 30 seconds for IIS to restart ..."
 	Start-Sleep -Seconds 30      
 	AppPools-Start $isAzure
 	TeleoptiWindowsServices-Start
