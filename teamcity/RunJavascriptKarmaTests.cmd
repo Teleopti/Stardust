b6487e509862c5888f3edc64e962cb9deeb1996a
Kunning Mao <none@none>
2016-09-14 13:22:09 +0200

    build should be red if js tests don't run

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -1,8 +1,26 @@
+@ECHO off
+SET /A ERRORLEV=0
 :: Runs from [repo]\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM
-
 call ..\.node\npm run continuous
-
+IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=1 & GOTO :error
 :: Clear all dev dependencies and only install what we need for production
 call ..\.node\npm install rimraf
+IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=2 & GOTO :error
 call ..\.node\node node_modules\rimraf\bin.js node_modules
+IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=3 & GOTO :error
 call ..\.node\npm install --production
+IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=4 & GOTO :error
+echo %errorlevel%
+
+GOTO :EOF
+
+:Error
+COLOR C
+ECHO.
+ECHO --------
+IF %ERRORLEV% NEQ 0 ECHO Errors found!
+
+ECHO.
+ECHO --------
+PAUSE
+GOTO :EOF
