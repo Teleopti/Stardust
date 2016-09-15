2fcc3aa86e93dc006b1c5a7ca1d097048a3a41dc
Rockman <none@none>
2016-09-15 14:40:21 +0200

    #40061

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/wfm.shortcuts.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/global/wfm.shortcuts.service.js
@@ -40,26 +40,6 @@
 				service.isTabbedClicked = false;
 		};
 
-
-		var goToState = function(state) {
-			$state.go(state);
-		};
-
-		AreasSvrc.getAreas().then(function(result){
-			registerGlobalShortcuts(result);
-		});
-
-		function registerGlobalShortcuts(result) {
-			var keys = [keyCodes.SHIFT];
-			var j = keyCodes.ONE;
-			var state;
-			for (var i = 0; i < result.length && j <= keyCodes.NINE; i++) {
-				state = result[i].InternalName;
-				ShortCuts.registerKeySequence(j, keys, goToState.bind(this, state));
-				j++;
-			}
-		};
-
 		return service;
 	}]);
 })();
