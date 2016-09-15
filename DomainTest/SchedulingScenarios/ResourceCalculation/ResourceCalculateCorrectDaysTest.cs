dba934c054ee8c663de729b33c6623da80fc280b
Roger Kratz <roger.kratz@teleopti.com>
2016-09-15 16:02:00 +0200

    #40646 First test for timezone issue (calculating too little)

---
+++ DomainTest/SchedulingScenarios/ResourceCalculation/ResourceCalculateCorrectDaysTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
+{
+	[DomainTest]
+	public class ResourceCalculateCorrectDaysTest
+	{
+		public Func<ISchedulerStateHolder> SchedulerStateHolder;
+		public IScheduleDayChangeCallback ScheduleDayChangeCallback;
+		public FakeTimeZoneGuard FakeTimeZoneGuard;
+
+		[Test, Ignore("40646")]
+		public void ShouldMarkDayBeforeIfShiftStartsDayBeforeInUsersTimeZone()
+		{
+			FakeTimeZoneGuard.SetTimeZone(TimeZoneInfo.Utc);
+			var date = new DateOnly(2015, 10, 12); 
+			var period = new DateOnlyPeriod(date, date.AddWeeks(1));
+			var activity = new Activity("_");
+			var scenario = new Scenario("_");
+			var agent = new Person().WithId().InTimeZone(TimeZoneInfoFactory.SingaporeTimeZoneInfo());
+			var ass = new PersonAssignment(agent, scenario, date);
+			ass.AddActivity(activity, new TimePeriod(0, 0, 1, 0));
+			var stateHolder = SchedulerStateHolder.Fill(scenario, period, new[] { agent }, new[] {ass}, Enumerable.Empty<ISkillDay>());
+			var schedule = stateHolder.Schedules[agent].ScheduledDay(date);
+
+			schedule.DeleteMainShift();
+			stateHolder.Schedules.Modify(schedule, ScheduleDayChangeCallback);
+
+			stateHolder.DaysToRecalculate.Should().Contain(date.AddDays(-1));
+		}
+	}
+}
