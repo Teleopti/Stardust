7a5ef0f712e5816a6467c7f046b987255f85e2d4
Roger Kratz <roger.kratz@teleopti.com>
2016-09-23 13:02:26 +0200

    #40242 Moving test class to separate file

--- DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationIslandDesktopTest.cs
+++ DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationIslandDesktopTest.cs
@@ -10,8 +10,6 @@ using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.DayOffPlanning;
 using Teleopti.Ccc.Domain.Forecasting;
 using Teleopti.Ccc.Domain.Optimization;
-using Teleopti.Ccc.Domain.ResourceCalculation;
-using Teleopti.Ccc.Domain.ResourceCalculation.GroupScheduling;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
@@ -25,7 +23,6 @@ using Teleopti.Ccc.Infrastructure.ApplicationLayer;
 using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
-using Teleopti.Ccc.TestCommon.FakeRepositories;
 using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
 
@@ -778,56 +775,6 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
 		public void Setup(ISystem system, IIocConfiguration configuration)
 		{
 			system.UseTestDouble<DesktopOptimizationContext>().For<IFillSchedulerStateHolder, ISynchronizeIntradayOptimizationResult, IOptimizationPreferencesProvider, IPeopleInOrganization>();
-		}
-	}
-	
-	[DomainTest]
-	public class IntradayOptimizationTeamTest
-	{
-		public IOptimizationCommand Target;
-		public Func<ISchedulerStateHolder> SchedulerStateHolderFrom;
-		public FakeBusinessUnitRepository BusinessUnitRepository;
-
-		[Test, Ignore("Need a reason since NUnit 3.")]
-		public void ShouldNotCrashWhenUsingKeepExistingDaysOff()
-		{
-			BusinessUnitRepository.Has(ServiceLocatorForEntity.CurrentBusinessUnit.Current());
-			var scenario = new Scenario("_");
-			var phoneActivity = ActivityFactory.CreateActivity("_");
-			var dateOnly = new DateOnly(2010, 1, 1);
-			var contract = new Contract("_")
-			{
-				WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(36), TimeSpan.FromHours(63), TimeSpan.FromHours(11), TimeSpan.FromHours(36)),
-				PositivePeriodWorkTimeTolerance = TimeSpan.FromHours(9)
-			};
-			var skill = new Skill("_", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = phoneActivity, TimeZone = TimeZoneInfo.Utc }.WithId();
-			WorkloadFactory.CreateWorkloadWithFullOpenHours(skill);
-			var skillDay = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(60), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(360)));
-			var agent = new Person().WithId().InTimeZone(TimeZoneInfo.Utc);
-			agent.AddPeriodWithSkill(new PersonPeriod(dateOnly, new PersonContract(contract, new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), skill);
-			agent.AddSchedulePeriod(new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1));
-			agent.Period(dateOnly).RuleSetBag = new RuleSetBag(new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(8, 15, 8, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), new ShiftCategory("_").WithId())));
-			var ass = new PersonAssignment(agent, scenario, dateOnly);
-			ass.AddActivity(phoneActivity, new TimePeriod(8, 0, 17, 0));
-			var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, dateOnly, new[] { agent }, new[] { ass }, skillDay);
-			var optimizationPreferences = new OptimizationPreferencesDefaultValueProvider().Fetch();
-			optimizationPreferences.General.OptimizationStepShiftsWithinDay = true;
-			optimizationPreferences.Extra.UseTeams = true;
-			var daysOffPreferences = new DaysOffPreferences {UseKeepExistingDaysOff = true};
-
-			Assert.DoesNotThrow(() =>
-			{
-				Target.Execute(null,
-					new NoSchedulingProgress(),
-					schedulerStateHolderFrom,
-					new List<IScheduleDay> { schedulerStateHolderFrom.Schedules[agent].ScheduledDay(dateOnly) },
-					null,
-					null,
-					optimizationPreferences,
-					false,
-					daysOffPreferences,
-					new FixedDayOffOptimizationPreferenceProvider(daysOffPreferences));
-			});
 		}
 	}
 }
---
+++ DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationTeamBlockDesktopTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
+{
+	[DomainTest]
+	public class IntradayOptimizationTeamBlockDesktopTest
+	{
+		public IOptimizationCommand Target;
+		public Func<ISchedulerStateHolder> SchedulerStateHolderFrom;
+		public FakeBusinessUnitRepository BusinessUnitRepository;
+
+		[Test, Ignore("#40242")]
+		public void ShouldNotCrashWhenUsingKeepExistingDaysOff()
+		{
+			BusinessUnitRepository.Has(ServiceLocatorForEntity.CurrentBusinessUnit.Current());
+			var scenario = new Scenario("_");
+			var phoneActivity = ActivityFactory.CreateActivity("_");
+			var dateOnly = new DateOnly(2010, 1, 1);
+			var contract = new Contract("_")
+			{
+				WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(36), TimeSpan.FromHours(63), TimeSpan.FromHours(11), TimeSpan.FromHours(36)),
+				PositivePeriodWorkTimeTolerance = TimeSpan.FromHours(9)
+			};
+			var skill = new Skill("_", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = phoneActivity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			WorkloadFactory.CreateWorkloadWithFullOpenHours(skill);
+			var skillDay = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(60), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(360)));
+			var agent = new Person().WithId().InTimeZone(TimeZoneInfo.Utc);
+			agent.AddPeriodWithSkill(new PersonPeriod(dateOnly, new PersonContract(contract, new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), skill);
+			agent.AddSchedulePeriod(new SchedulePeriod(dateOnly, SchedulePeriodType.Week, 1));
+			agent.Period(dateOnly).RuleSetBag = new RuleSetBag(new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phoneActivity, new TimePeriodWithSegment(8, 15, 8, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), new ShiftCategory("_").WithId())));
+			var ass = new PersonAssignment(agent, scenario, dateOnly);
+			ass.AddActivity(phoneActivity, new TimePeriod(8, 0, 17, 0));
+			var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, dateOnly, new[] { agent }, new[] { ass }, skillDay);
+			var optimizationPreferences = new OptimizationPreferencesDefaultValueProvider().Fetch();
+			optimizationPreferences.General.OptimizationStepShiftsWithinDay = true;
+			optimizationPreferences.Extra.UseTeams = true;
+			var daysOffPreferences = new DaysOffPreferences {UseKeepExistingDaysOff = true};
+
+			Assert.DoesNotThrow(() =>
+			{
+				Target.Execute(null,
+					new NoSchedulingProgress(),
+					schedulerStateHolderFrom,
+					new List<IScheduleDay> { schedulerStateHolderFrom.Schedules[agent].ScheduledDay(dateOnly) },
+					null,
+					null,
+					optimizationPreferences,
+					false,
+					daysOffPreferences,
+					new FixedDayOffOptimizationPreferenceProvider(daysOffPreferences));
+			});
+		}
+	}
+}
