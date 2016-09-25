14c81677884fe928007fd116193e7dfb51f04d31
David Jonsson <davidj@teleopti.com>
2016-09-24 21:27:54 -0600

    #40795: delete potential duplicates. Keep latest InsertedOn

--- Database/TeleoptiCCC7/Releases/00000721.sql
+++ Database/TeleoptiCCC7/Releases/00000721.sql
@@ -12,6 +12,15 @@ GO
 ALTER TABLE [ReadModel].[ScheduleProjectionReadOnly] DROP COLUMN [Id]
 GO
 
+--#40795: delete potential duplicates. Keep latest InsertedOn
+WITH cte as(
+  SELECT ROW_NUMBER() OVER (PARTITION BY BelongsToDate,PersonId,StartDateTime
+                            ORDER BY  InsertedOn DESC ) RN
+  FROM   [ReadModel].[ScheduleProjectionReadOnly]
+  )
+delete from cte where RN>1
+GO
+
 ALTER TABLE [ReadModel].[ScheduleProjectionReadOnly]
 ADD CONSTRAINT [PK_ScheduleProjectionReadOnly] PRIMARY KEY CLUSTERED (
 	[PersonId] ASC,
