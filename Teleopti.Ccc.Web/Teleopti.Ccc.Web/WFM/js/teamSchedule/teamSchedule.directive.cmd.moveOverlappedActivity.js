fda39983e9a266f06cfc2de265befb947c35dcd6
Zulong Chen <none@none>
2016-09-26 14:36:50 +0800

    PBI #40688 refactor (using Angular 1.5.x component style)

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.directive.cmd.moveOverlappedActivity.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/teamSchedule/teamSchedule.directive.cmd.moveOverlappedActivity.js
@@ -1,66 +1,48 @@
 (function () {
 	'use strict';
 
-	angular.module('wfm.teamSchedule').directive('moveOverlappedActivity', moveOverlappedActivityDirective);
+	angular.module('wfm.teamSchedule').component('moveOverlappedActivity', {
+		require: {
+			CmdContainerCtrl: '^teamscheduleCommandContainer'
+		},
+		controller: MoveOverlappedActivityCtrl
+	});
 
-	moveOverlappedActivityCtrl.$inject = ['PersonSelection', 'ActivityService', 'teamScheduleNotificationService'];
+	MoveOverlappedActivityCtrl.$inject = ['PersonSelection', 'ActivityService', 'teamScheduleNotificationService'];
 
-	function moveOverlappedActivityCtrl(PersonSelection, ActivityService, notification) {
-		var vm = this;
-		vm.label = 'MoveInvalidOverlappedActivity';
-		vm.init = function(){
-			moveOverlappedActivity();
-		}
+	function MoveOverlappedActivityCtrl(PersonSelection, ActivityService, notification) {
+		this.$onInit = function () {
+			var CmdContainerCtrl = this.CmdContainerCtrl;
+			var cmdName = 'MoveInvalidOverlappedActivity';
+			var date = CmdContainerCtrl.getDate();
+			var trackId = CmdContainerCtrl.getTrackId();
 
-		function moveOverlappedActivity(){
-			var selectedPersonInfo = PersonSelection.getCheckedPersonInfoList();
-			var selectedPersonIds = selectedPersonInfo.map(function (p) {
+			var personInfo = PersonSelection.getCheckedPersonInfoList();
+			var personIds = personInfo.map(function (p) {
 				return p.PersonId;
 			});
 			var cmdInput = {
-				PersonIds: selectedPersonIds,
-				Date: vm.selectedDate(),
-				TrackedCommandInfo: { TrackId: vm.trackId }
-			}
+				PersonIds: personIds,
+				Date: date,
+				TrackedCommandInfo: { TrackId: trackId }
+			};
+
 			ActivityService.moveInvalidOverlappedActivity(cmdInput).then(function (response) {
-				if (vm.getActionCb(vm.label)) {
-					vm.getActionCb(vm.label)(vm.trackId, selectedPersonIds);
+				if (CmdContainerCtrl.getActionCb(cmdName)) {
+					CmdContainerCtrl.getActionCb(cmdName)(trackId, personIds);
 				}
 
 				notification.reportActionResult({
-					"success": 'MoveInvalidOverlappedActivitySuccess',
-					"warning": 'MoveInvalidOverlappedActivityWarning'
-				}, selectedPersonInfo.map(function (x) {
+					'success': 'MoveInvalidOverlappedActivitySuccess',
+					'warning': 'MoveInvalidOverlappedActivityWarning'
+				}, personInfo.map(function (x) {
 					return {
 						PersonId: x.PersonId,
 						Name: x.Name
-					}
+					};
 				}), response.data);
-			})
-		}
+			});
+		};
 	}
 
-	function moveOverlappedActivityDirective() {
-		return {
-			restrict: 'E',
-			scope: {},
-			controller: moveOverlappedActivityCtrl,
-			controllerAs: 'vm',
-			bindToController: true,
-			require: ['^teamscheduleCommandContainer', 'moveOverlappedActivity'],
-			link: postlink
-		}
-
-		function postlink(scope, elem, attrs, ctrls) {
-			var containerCtrl = ctrls[0],
-				selfCtrl = ctrls[1];
-
-			scope.vm.selectedDate = containerCtrl.getDate;
-			scope.vm.trackId = containerCtrl.getTrackId();
-			scope.vm.getActionCb = containerCtrl.getActionCb;
-			scope.vm.resetActiveCmd = containerCtrl.resetActiveCmd;
-
-			selfCtrl.init();
-		}
-	}
 })();
