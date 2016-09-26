8e8fc13bb3e2521a2c5caa58ab077d44153c53d0
Rockman <none@none>
2016-09-26 13:43:16 +0200

    #40679 part 1 change routing for forecaster

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/app_desktop_client.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/js/app_desktop_client.js
@@ -55,24 +55,6 @@ wfm.config([
 		$stateProvider.state('main', {
 			url: '/',
 			templateUrl: 'html/main.html'
-		}).state('forecasting', {
-			url: '/forecasting',
-			templateUrl: 'js/forecasting/html/forecasting.html',
-			controller: 'ForecastingDefaultCtrl'
-		}).state('forecasting.start', {
-			params: {
-				workloadId: undefined
-			},
-			templateUrl: 'js/forecasting/html/forecasting-overview.html',
-			controller: 'ForecastingStartCtrl'
-		}).state('forecasting.advanced', {
-			url: '/advanced/:workloadId',
-			templateUrl: 'js/forecasting/html/forecasting-advanced.html',
-			controller: 'ForecastingAdvancedCtrl'
-		}).state('forecasting.skillcreate', {
-			url: '/skill/create',
-			templateUrl: 'js/forecasting/html/skill-create.html',
-			controller: 'ForecastingSkillCreateCtrl'
 		}).state('resourceplanner', {
 			url: '/resourceplanner',
 			templateUrl: 'js/resourceplanner/resourceplanner.html',
