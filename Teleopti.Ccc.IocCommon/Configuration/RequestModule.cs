77cd7138971fa2cba5ca3f577efe2ad85a4d7cc9
amandaj <none@none>
2016-09-27 12:50:00 +0200

    #40754 Registration

--- Teleopti.Ccc.IocCommon/Configuration/RequestModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/RequestModule.cs
@@ -53,7 +53,7 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 
 			builder.RegisterType<AbsenceRequestStrategyProcessor>().As<IAbsenceRequestStrategyProcessor>().SingleInstance();
 			builder.RegisterType<ArrangeRequestsByProcessOrder>().As<ArrangeRequestsByProcessOrder>().SingleInstance();
-			builder.RegisterType<IntradayRequestProcessor>().As<IntradayRequestProcessor>().SingleInstance();
+			builder.RegisterType<IntradayRequestProcessor>().As<IIntradayRequestProcessor>().SingleInstance();
 			
 
 			if (_configuration.Toggle (Toggles.Wfm_Requests_Show_Pending_Reasons_39473))
