2427735ca42785998038f6cbe054861b647fc41e
lin.sang.mode@teleopti.com <lin.sang.mode@teleopti.com>
2016-09-28 15:15:40 +0200

    Refactor maincontroller as vm

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/wfm.main.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/wfm.main.controller.js
@@ -1,61 +1,65 @@
 (function() {
-	'use strict';
-	var global = angular.module('wfm');
+    'use strict';
 
-	global.controller('mainController', [
-		'$scope', 'Toggle', '$rootScope', 'ThemeService', '$q',
-		function($scope, Toggle, $rootScope, ThemeService, $q) {
-			ThemeService.getTheme().then(function(result){
-				$scope.currentStyle = result.data.Name;
-			});
+    angular
+        .module('wfm')
+        .controller('MainController', MainController);
 
-			$rootScope.setTheme = function(theme) {
-				var darkThemeElement = document.getElementById('darkTheme');
-				if (darkThemeElement) {
-					darkThemeElement.checked = (theme === "dark");
-				}
-				$scope.style = theme;
-				if (checkCurrentTheme() != theme) {
-					modifyDOMHeader(theme);
-				}
+    MainController.$inject = ['$scope', 'Toggle', '$rootScope', 'ThemeService', '$q'];
 
-			};
+    /* @ngInject */
+    function MainController($scope, Toggle, $rootScope, ThemeService, $q) {
+        var vm = this;
 
-			var modifyDOMHeader = function(theme) {
-				var styleElements = ["Modules", "Style"];
-				styleElements.forEach(function(element) {
-					var themeComponent = document.getElementById('theme' + element);
-					if (themeComponent) {
-						var hash = extractHash(themeComponent);
-						themeComponent.setAttribute('href', 'dist/' + element.toLowerCase() + '_' + theme + '.min.css' + hash);
-						themeComponent.setAttribute('class',theme);
-						if (element === "Modules") {
-							themeComponent.onload = function() {
-								$scope.$apply(function() {
-									$scope.styleIsFullyLoaded = true;
-								});
-							};
-						}
-					}
-				});
-			};
-			var checkCurrentTheme = function(){
-				var currentStyle;
-				if (document.getElementById('themeStyle').className) {
-					currentStyle = document.getElementById('themeStyle').className
-				}
-				return currentStyle;
-			}
+        ThemeService.getTheme().then(function(result){
+  				vm.currentStyle = result.data.Name;
+  			});
 
-			var extractHash = function(element) {
-				var hashvalue = element.href.match("\\?(.*)"); //[^\\?]*$
-				var returnValue = "";
-				if (hashvalue) {
-					returnValue = hashvalue[0];
-				}
-				return returnValue;
-			};
+        $rootScope.setTheme = function(theme) {
+  				var darkThemeElement = document.getElementById('darkTheme');
+  				if (darkThemeElement) {
+  					darkThemeElement.checked = (theme === "dark");
+  				}
+  				vm.style = theme;
+  				if (checkCurrentTheme() != theme) {
+  					modifyDOMHeader(theme);
+  				}
+  			};
 
-		}
-	]);
+        function modifyDOMHeader (theme) {
+  				var styleElements = ["Modules", "Style"];
+  				styleElements.forEach(function(element) {
+  					var themeComponent = document.getElementById('theme' + element);
+  					if (themeComponent) {
+  						var hash = extractHash(themeComponent);
+  						themeComponent.setAttribute('href', 'dist/' + element.toLowerCase() + '_' + theme + '.min.css' + hash);
+  						themeComponent.setAttribute('class',theme);
+  						if (element === "Modules") {
+  							themeComponent.onload = function() {
+  								$scope.$apply(function() {
+  									vm.styleIsFullyLoaded = true;
+  								});
+  							};
+  						}
+  					}
+  				});
+  			}
+
+        function checkCurrentTheme(){
+  				var currentStyle;
+  				if (document.getElementById('themeStyle').className) {
+  					currentStyle = document.getElementById('themeStyle').className;
+  				}
+  				return currentStyle;
+  			}
+
+        function extractHash(element) {
+  				var hashvalue = element.href.match("\\?(.*)"); //[^\\?]*$
+  				var returnValue = "";
+  				if (hashvalue) {
+  					returnValue = hashvalue[0];
+  				}
+  				return returnValue;
+  			}
+    }
 })();
