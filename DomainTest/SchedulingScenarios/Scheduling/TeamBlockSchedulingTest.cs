7d841d8a6b419d7c8395e08c8fe5abefbfc44807
Roger Kratz <roger.kratz@teleopti.com>
2016-09-28 13:29:44 +0200

    Removing test that doesn't assert anything

--- DomainTest/SchedulingScenarios/Scheduling/TeamBlockSchedulingTest.cs
+++ DomainTest/SchedulingScenarios/Scheduling/TeamBlockSchedulingTest.cs
@@ -222,54 +222,5 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Scheduling
 			}
 			foundRestrictedAssignments.Should().Be.EqualTo(1);
 		}
-
-		[Test]
-		public void ShouldBeAbleToScheduleTeamWithAllMembersLoadedButOneMemberFilteredOut()
-		{
-			//two agents in the same team, everything equal
-			//both agents in PersonsInOrganization
-			//one agent in filteredPersons
-			var firstDay = new DateOnly(2015, 10, 12);
-			var period = new DateOnlyPeriod(firstDay, firstDay.AddWeeks(1)); //12 to 18
-			var activity = ActivityRepository.Has("_");
-			var skill = SkillRepository.Has("skill", activity);
-			var scenario = ScenarioRepository.Has("some name");
-			var team = new Team { Description = new Description("team") };
-			BusinessUnitRepository.Has(ServiceLocatorForEntity.CurrentBusinessUnit.Current());
-			var contract = new Contract("_");
-			var contractSchedule = ContractScheduleFactory.CreateWorkingWeekContractSchedule();
-			var agent1 = PersonRepository.Has(contract, contractSchedule, new PartTimePercentage("_"), team, new SchedulePeriod(firstDay, SchedulePeriodType.Week, 1), skill);
-			var agent2 = PersonRepository.Has(contract, contractSchedule, new PartTimePercentage("_"), team, new SchedulePeriod(firstDay, SchedulePeriodType.Week, 1), skill);
-			var shiftCategory = new ShiftCategory("_").WithId();
-			var restrictedRuleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 16, 0, 15), shiftCategory));
-			var normalRuleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(12, 0, 12, 0, 15), new TimePeriodWithSegment(20, 0, 20, 0, 15), shiftCategory));
-			restrictedRuleSet.OnlyForRestrictions = true;
-			var ruleSetBag = new RuleSetBag(restrictedRuleSet);
-			ruleSetBag.AddRuleSet(normalRuleSet);
-			agent1.Period(firstDay).RuleSetBag = ruleSetBag;
-			agent2.Period(firstDay).RuleSetBag = ruleSetBag;
-
-			SkillDayRepository.Has(skill.CreateSkillDaysWithDemandOnConsecutiveDays(scenario, firstDay,
-				1, 1, 1, 1, 1, 1, 1));
-
-			var dayOffTemplate = new DayOffTemplate(new Description("_default")).WithId();
-			DayOffTemplateRepository.Add(dayOffTemplate);
-
-			SchedulingOptionsProvider.SetFromTest(new SchedulingOptions
-			{
-				UseAvailability = true,
-				UsePreferences = true,
-				UseRotations = true,
-				UseStudentAvailability = false,
-				DayOffTemplate = dayOffTemplate,
-				ScheduleEmploymentType = ScheduleEmploymentType.FixedStaff,
-				GroupOnGroupPageForTeamBlockPer = new GroupPageLight(UserTexts.Resources.Main, GroupPageType.Hierarchy),
-				UseTeam = true,
-				TeamSameShiftCategory = true,
-				TagToUseOnScheduling = NullScheduleTag.Instance
-			});
-
-			Target.DoScheduling(period);
-		}
 	}
 }
