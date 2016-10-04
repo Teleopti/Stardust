467043a7bb9f7c4293d5386d74fea227f0588dcc
Ola <none@none>
2016-10-04 14:02:24 +0200

    fix build, didn't work

--- .debug-Setup/database/tsql/DemoDatabase/CreateLoginDropUsers.sql
+++ .debug-Setup/database/tsql/DemoDatabase/CreateLoginDropUsers.sql
@@ -26,13 +26,6 @@ IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$(SQLLogin)')
 DROP USER [$(SQLLogin)]
 GO
 
-DELETE FROM Tenant.AdminUser WHERE Name = 'FirstAdmin' AND [Password] = '###2B2E73BBB3BEE5EC6C159C0FB4E5B9A2570CD8EE###'
-IF NOT EXISTS (SELECT * FROM Tenant.AdminUser WHERE Name = 'FirstAdmin')
-BEGIN
-	INSERT INTO Tenant.AdminUser (Name, Email, Password, AccessToken)
-	VALUES ('FirstAdmin', 'first@admin.is', '###70D74A6BBA33B5972EADAD9DD449D273E1F4961D###', 'andaDummyToken')
-END
-
 USE $(TELEOPTIANALYTICS)
 IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$(SQLLogin)')
 DROP USER [$(SQLLogin)]
