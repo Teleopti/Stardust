10f2b9d9d12ac714eb64ee175b2e73b929484fb8
johan Damm <none@none>
2016-10-07 13:04:21 +0200

    added generate Web Bat and check for no node_modules

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/CheckPackageJsonIsModified.ps1
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/CheckPackageJsonIsModified.ps1
@@ -3,17 +3,23 @@ $fileExists = Test-Path PackageJsonHash.txt
 $isModified = "False"
 
 if ($fileExists){
-$latestKnownHash = get-content -Path ./PackageJsonHash.txt
-   $currentHash = Get-FileHash $file | Format-List
-       if($latestKnownHash -ne (Get-FileHash $file).hash){
-         $hash = (Get-FileHash $file).hash | Format-List | Out-file PackageJsonHash.txt
-         $isModified = "True"
-         return $isModified
-         exit
-       }else{
-         return $isModified
-         exit
-       }
+$checkModulelFolders = Test-Path ..\WFM\node_modules
+    if($checkModulelFolders -ne $True){
+        $isModified = "True"
+        return $isModified
+        exit
+    }
+    $latestKnownHash = get-content -Path ./PackageJsonHash.txt
+        $currentHash = Get-FileHash $file | Format-List
+            if($latestKnownHash -ne (Get-FileHash $file).hash){
+                $hash = (Get-FileHash $file).hash | Format-List | Out-file PackageJsonHash.txt
+                $isModified = "True"
+                return $isModified
+                exit
+            }else{
+                return $isModified
+                exit
+            }
 
 }else{
  $hash = (Get-FileHash $file).hash | Format-List | Out-file PackageJsonHash.txt
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/generateWeb.bat
@@ -0,0 +1,2 @@
+cd ..\WFM
+..\.node\grunt.cmd
