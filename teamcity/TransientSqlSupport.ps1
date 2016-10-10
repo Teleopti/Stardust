38b2774a48cdfc507644624697389df5b74a5bb2
jimmy Broman <none@none>
2016-10-10 11:00:40 +0200

    Corrected path to exe file #2 FUBAR way of calling an exe

--- teamcity/TransientSqlSupport.ps1
+++ teamcity/TransientSqlSupport.ps1
@@ -1,6 +1,6 @@
 ﻿$path = (get-item $PSScriptroot ).parent.Fullname
 $NugetPath = "$WorkingDirectory\..\packages"
-$NugetExe = "$WorkingDirectory\..\.nuget\nuget.exe"
+$NugetExe = "$WorkingDirectory.\\..\.nuget\nuget.exe"
 
 . $NugetExe install EnterpriseLibrary.TransientFaultHandling.Configuration -o $NugetPath -Source "https://www.nuget.org/api/v2/" -ExcludeVersion
 
