aac957366343808adf4571064f7e0ebe47adb6ff
Kunning Mao <none@none>
2016-10-10 13:38:57 +0200

    #40199 add support for dotnet framework 4.6.2

--- Teleopti.Support.PreReqsCheck/Checks/WebCheck.cs
+++ Teleopti.Support.PreReqsCheck/Checks/WebCheck.cs
@@ -89,14 +89,16 @@ namespace CheckPreRequisites.Checks
         }
         private void Get461FromRegistry()
         {
-            var lineNumber = _form1.printNewFeature(".NET Framework", "System", "Installed", "4.6.1");
+            var lineNumber = _form1.printNewFeature(".NET Framework", "System", "Installed", "4.6.1+");
             using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
             {
                 if (ndpKey?.GetValue("Release") != null)
                 {
                     var installed = (int)ndpKey.GetValue("Release");
-                    if (installed == 394271 || installed == 394254)
-                        _form1.printFeatureStatus(true, ".Net framework 4.6.1 is installed", lineNumber);
+	                if (installed == 394271 || installed == 394254)
+		                _form1.printFeatureStatus(true, ".Net framework 4.6.1 is installed", lineNumber);
+					else if (installed == 394802 || installed == 394806)
+						_form1.printFeatureStatus(true, ".Net framework 4.6.2 is installed", lineNumber);
                     else
                         _form1.printFeatureStatus(false, ".Net framework 4.6.1 is not installed", lineNumber);
                 }
