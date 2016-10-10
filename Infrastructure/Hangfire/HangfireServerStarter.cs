2fd425bf974df636fa941fa12e186ead45063e3f
amandaj <none@none>
2016-10-10 14:50:36 +0200

    Use app setting for Hangfire workers and default to 8 instead of 4

--- Infrastructure/Hangfire/HangfireServerStarter.cs
+++ Infrastructure/Hangfire/HangfireServerStarter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Hangfire;
 using Owin;
@@ -19,22 +20,24 @@ namespace Teleopti.Ccc.Infrastructure.Hangfire
 			_config = config;
 		}
 
-		const int setThisToOneAndErikWillHuntYouDownAndKillYouSlowlyAndPainfully = 4;
-		// But really.. using one worker will:
-		// - reduce performance of RTA too much
-		// - will be a one-way street where the code will never handle the concurrency
-		// - still wont handle the fact that some customers have more than one server running, 
-		//   so you will still have more than one worker
-
 		public void Start(IAppBuilder app)
 		{
 			_starter.Start(_config.ConnectionString("Hangfire"));
-
 			app.UseHangfireServer(new BackgroundJobServerOptions
 			{
-				WorkerCount = setThisToOneAndErikWillHuntYouDownAndKillYouSlowlyAndPainfully,
+				WorkerCount = getNumberOfHangfireWorkers(),
 				Queues = Queues.OrderOfPriority().ToArray()
 			});
+		}
+
+		private int getNumberOfHangfireWorkers()
+		{
+			var workerCountSetting = 8;
+			if(_config.AppConfig("HangFireWorkerCount") != null && int.Parse(_config.AppConfig("HangFireWorkerCount")) > 0)
+			{
+				workerCountSetting = int.Parse(_config.AppConfig("HangFireWorkerCount"));
+			}
+			return workerCountSetting;
 		}
 	}
 }
