6e0ecb5e7a48c5b6bf2fde83811efa388e478846
Hongzhao Gan <none@none>
2016-10-11 08:57:28 +0800

    Fix #40507: Calendar can not be displyed completely after clicked on the calendar icon on Absence request and Text request page

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Content/moment-datepicker/moment-datepicker-ext.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Content/moment-datepicker/moment-datepicker-ext.js
@@ -4,9 +4,14 @@ datepicker.place = function() {
     var sourceItem = this.component ? this.component : this.element;
     var offset = sourceItem.offset();
     if (this.calendarPlacement === 'left') {
+        var leftValue = offset.left + sourceItem[0].offsetWidth - this.picker[0].offsetWidth;
+
+        if (leftValue < 1) {
+            leftValue = offset.left + sourceItem[0].offsetWidth - sourceItem[0].parentElement.parentElement.offsetWidth;
+        }
         this.picker.css({
             top: offset.top + this.height,
-            left: offset.left + sourceItem[0].offsetWidth - this.picker[0].offsetWidth
+            left: leftValue
         });
     } else {
         this.picker.css({
