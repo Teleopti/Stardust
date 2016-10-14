c17b8bd98fcd6bf92205c3de3382cfb1712f8756
Justin! <justin@teleopti.com>
2016-10-14 15:55:34 +0200

    #41185 #41186 #40536 Failing test!

--- DomainTest/ApplicationLayer/Rta/Service/UpdateAgentStateReadModelShiftAbsenceTest.cs
+++ DomainTest/ApplicationLayer/Rta/Service/UpdateAgentStateReadModelShiftAbsenceTest.cs
@@ -39,7 +39,28 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.Rta.Service
 			shift.EndTime.Should().Be("2016-10-14 17:00".Utc());
 			shift.Name.Should("vacation");
 		}
-		
+
+		[Test]
+		[Ignore("WIP")]
+		public void ShouldPersistConfidentialAbsence()
+		{
+			var person = Guid.NewGuid();
+			var phone = Guid.NewGuid();
+			Now.Is("2016-10-14 07:00");
+			Database
+				.WithAgent("usercode", person)
+				.WithSchedule(person, phone, "2016-10-14 09:00", "2016-10-14 17:00")
+				.WithConfidentialAbsence()
+				.WithPersonAbsence("2016-10-14 00:00", "2016-10-15 00:00")
+				;
+
+			Target.CheckForActivityChanges(Database.TenantName());
+
+			var shift = Database.PersistedReadModel.Shift.Single();
+			shift.StartTime.Should().Be("2016-10-14 09:00".Utc());
+			shift.EndTime.Should().Be("2016-10-14 17:00".Utc());
+		}
+
 	}
 
 }
