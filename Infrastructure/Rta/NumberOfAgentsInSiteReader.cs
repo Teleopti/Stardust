646bbdacb26fe490eba2293dd17a1ea09925056d
Erik Sundberg <erik.sundberg@teleopti.com>
2016-10-18 10:45:16 +0200

    #40817 Refact

--- Infrastructure/Rta/NumberOfAgentsInSiteReader.cs
+++ Infrastructure/Rta/NumberOfAgentsInSiteReader.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using NHibernate.Mapping;
 using NHibernate.Transform;
-using Teleopti.Ccc.Domain.ApplicationLayer.Rta.ReadModelUpdaters;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta.ViewModels;
-using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.Infrastructure;
@@ -17,7 +13,14 @@ namespace Teleopti.Ccc.Infrastructure.Rta
 	{
 		private readonly ICurrentUnitOfWork _currentUnitOfWork;
 		private readonly INow _now;
-		private const string sqlQuery = @"
+		
+		public NumberOfAgentsInSiteReader(ICurrentUnitOfWork currentUnitOfWork, INow now)
+		{
+			_currentUnitOfWork = currentUnitOfWork;
+			_now = now;
+		}
+
+		private const string agentsForSites = @"
 SELECT
 	a.Site as 'SiteId',
 	count(a.Parent) as 'NumberOfAgents'
@@ -37,41 +40,63 @@ FROM
 inner join person p on 
 a.parent = p.id
 
-{0} a.is_current=1
+WHERE a.is_current=1
 and a.Site in (:sites)
 and (p.TerminalDate is null or p.TerminalDate > :now)
 group by a.Site";
-
-
-		public NumberOfAgentsInSiteReader(ICurrentUnitOfWork currentUnitOfWork, INow now)
-		{
-			_currentUnitOfWork = currentUnitOfWork;
-			_now = now;
-		}
-
 		public IDictionary<Guid, int> FetchNumberOfAgents(IEnumerable<Guid> siteIds)
 		{
 			var models =
-				_currentUnitOfWork.Session().CreateSQLQuery(string.Format(sqlQuery, " WHERE "))
+				_currentUnitOfWork.Session().CreateSQLQuery(agentsForSites)
 					.SetDateTime("now", _now.UtcDateTime())
 					.SetParameterList("sites", siteIds)
 					.SetResultTransformer(Transformers.AliasToBean(typeof(siteViewModel)))
 					.List()
 					.Cast<siteViewModel>();
-			return initializeAndConcat(siteIds, models);
+
+			var initializedSites =
+				from siteId in siteIds
+				where !models.Select(x => x.SiteId).Contains(siteId)
+				select new siteViewModel { SiteId = siteId, NumberOfAgents = 0 };
+
+			return initializedSites.Concat(models).ToDictionary(x => x.SiteId, y => y.NumberOfAgents);
 		}
 
+		private const string agentsForSkillQuery = @"
+SELECT
+	a.Site as 'SiteId',
+	count(a.Parent) as 'NumberOfAgents'
+FROM
+(
+	SELECT
+	pp.StartDate,
+	pp.Parent,
+	pp.PersonPeriod,
+	pp.BusinessUnit,
+	pp.Site,
+	pp.Team,
+	ROW_NUMBER()OVER(PARTITION BY pp.Parent ORDER BY pp.StartDate DESC) as is_current
+	FROM dbo.v_PersonPeriodTeamSiteBu pp WITH(NOEXPAND)
+	WHERE pp.StartDate <= :now 
+) a
+inner join person p on 
+a.parent = p.id
+
+INNER JOIN ReadModel.GroupingReadOnly AS g
+ON p.Id = g.PersonId					
+WHERE g.GroupId IN (:skillIds)
+AND g.PageId = :skillGroupingPageId
+						
+AND a.is_current=1
+and a.Site in (:sites)
+and (p.TerminalDate is null or p.TerminalDate > :now)
+group by a.Site
+";
 		public IDictionary<Guid, int> ForSkills(IEnumerable<Guid> siteIds, IEnumerable<Guid> skillIds)
 		{
 			var models =
 				_currentUnitOfWork.Session()
-					.CreateSQLQuery(
-						string.Format(sqlQuery, @"
-						INNER JOIN ReadModel.GroupingReadOnly AS g
-						ON p.Id = g.PersonId					
-						WHERE g.GroupId IN (:skillIds)
-						AND g.PageId = :skillGroupingPageId
-						AND "))
+					.CreateSQLQuery(agentsForSkillQuery)
 					.SetDateTime("now", _now.UtcDateTime())
 					.SetParameter("skillGroupingPageId", HardcodedSkillGroupingPageId.Get)
 					.SetParameterList("sites", siteIds)
@@ -83,18 +108,7 @@ group by a.Site";
 
 			return models.ToDictionary(x => x.SiteId, y => y.NumberOfAgents);
 		}
-
-		private IDictionary<Guid, int> initializeAndConcat(IEnumerable<Guid> siteIds, IEnumerable<siteViewModel> models)
-		{
-			var initializedSites =
-				from siteId in siteIds
-				where !models.Select(x => x.SiteId).Contains(siteId)
-				select new siteViewModel {SiteId = siteId, NumberOfAgents = 0};
-
-			return  initializedSites.Concat(models).ToDictionary(x => x.SiteId, y => y.NumberOfAgents);
-		}
-
-
+		
 		private class siteViewModel
 		{
 			public Guid SiteId { get; set; }
