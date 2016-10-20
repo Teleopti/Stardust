6633f59eb9dfe2c1d4812840f186a8aed4cae61b
Behrooz Aghakhanian <behrooz.aghakhanian@teleopti.com>
2016-10-20 16:54:06 +0200

    Increased optimizesameshift validator

--- Win/Sikuli/Validators/RootValidators/OptimizeBlockSameShiftValidator.cs
+++ Win/Sikuli/Validators/RootValidators/OptimizeBlockSameShiftValidator.cs
@@ -8,7 +8,7 @@ namespace Teleopti.Ccc.Win.Sikuli.Validators.RootValidators
 	{
 		public OptimizeBlockSameShiftValidator()
 		{
-			AtomicValidators.Add(new DurationValidator(TimeSpan.FromMinutes(2).Add(TimeSpan.FromSeconds(10))));
+			AtomicValidators.Add(new DurationValidator(TimeSpan.FromMinutes(3).Add(TimeSpan.FromSeconds(10))));
 		}
 
 		protected override SikuliValidationResult Validate(SchedulerTestData data)
