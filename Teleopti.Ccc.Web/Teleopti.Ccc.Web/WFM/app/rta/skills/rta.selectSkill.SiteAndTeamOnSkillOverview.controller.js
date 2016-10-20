9ed89cbfedae68c7e8b7a1bbf343ec72948ddb23
Sabbir Rahman <none@none>
2016-10-20 09:00:05 +0200

    #40817 remove console log in js

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/skills/rta.selectSkill.SiteAndTeamOnSkillOverview.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/skills/rta.selectSkill.SiteAndTeamOnSkillOverview.controller.js
@@ -153,7 +153,6 @@
 				}
 
 				function getSkillIdsFromSkillArea(skillAreaId){
-					console.log($scope.skillAreas);
 					return $scope.skillAreas.find(function(skillArea){
 						return skillArea.Id === skillAreaId;
 					})
