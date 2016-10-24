0388c6befb2928e6c32ede910e4bb5b46b5b97fd
LinM <none@none>
2016-10-24 11:32:34 +0200

    #40986 add test of areas and businessunits

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/businessunits/businessunits.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/businessunits/businessunits.controller.js
@@ -5,13 +5,11 @@
         .module('wfm.businessunits')
         .controller('BusinessUnitsCtrl', BusinessUnitsCtrl);
 
-    BusinessUnitsCtrl.$inject = ['$scope', '$resource', '$http', '$filter', '$state', '$sessionStorage', '$window', 'BusinessUnitsService'];
+    BusinessUnitsCtrl.$inject = [ '$filter', '$window', 'BusinessUnitsService'];
 
-    function BusinessUnitsCtrl($scope, $resource, $http, $filter, $state, $sessionStorage, $window, BusinessUnitsService) {
+    function BusinessUnitsCtrl($filter,  $window, BusinessUnitsService) {
         var vm = this;
-
 				vm.show = false;
-
 				vm.data = {
 					selectedBu: null
 				};
@@ -21,16 +19,19 @@
 					$window.location.reload();
 				};
 
-				BusinessUnitsService.getAllBusinessUnits().then(function(result) {
-					vm.data.businessUnits = result;
-					vm.show = (result.length > 1);
-					var buid = BusinessUnitsService.getBusinessUnitFromSessionStorage();
-					if (buid) {
-						var businessUnit = $filter('filter')(result, function(d) { return d.Id === buid; })[0];
-						vm.data.selectedBu = businessUnit;
-					} else {
-						vm.data.selectedBu = result[0];
-					}
-				});
+        vm.loadBusniessUnit = function(){
+          BusinessUnitsService.getAllBusinessUnits().then(function(result) {
+            vm.data.businessUnits = result;
+            vm.show = (result.length > 1);
+            var buid = BusinessUnitsService.getBusinessUnitFromSessionStorage();
+            if (buid) {
+              var businessUnit = $filter('filter')(result, function(d) { return d.Id === buid; })[0];
+              vm.data.selectedBu = businessUnit;
+            } else {
+              vm.data.selectedBu = result[0];
+
+            }
+          });
+        };
     }
 })();
