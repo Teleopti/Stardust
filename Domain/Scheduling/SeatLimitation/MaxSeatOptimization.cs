eeb49185d7a9de650faf820fb070ea32da431207
Roger Kratz <roger.kratz@teleopti.com>
2016-10-25 12:30:21 +0200

    #40939 MaxSeatOptimization no longer depended on StateHolder.SkillDays.

--- Domain/Scheduling/SeatLimitation/MaxSeatOptimization.cs
+++ Domain/Scheduling/SeatLimitation/MaxSeatOptimization.cs
@@ -70,7 +70,6 @@ namespace Teleopti.Ccc.Domain.Scheduling.SeatLimitation
 			((SchedulerStateHolder)_stateHolder()).SetLoadedPeriod_UseOnlyFromTest_ShouldProbablyBePutOnScheduleDictionaryInsteadIfNeededAtAll(loadedPeriod); //needed to build groups
 			allAgents.ForEach(x => _stateHolder().SchedulingResultState.PersonsInOrganization.Add(x)); //needed to build groups
 			allAgents.ForEach(x => _stateHolder().AllPermittedPersons.Add(x)); //needed to build groups
-			_stateHolder().SchedulingResultState.SkillDays = maxSeatData.AllMaxSeatSkillDaysPerSkill(); //needed for SameOpenHoursInTeamBlock
 			_stateHolder().SchedulingResultState.Schedules = schedules;
 			//////////////////
 
