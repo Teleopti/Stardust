6e15eeab3f0479d45f719f09f1c98b62df9abf48
Roger Kratz <roger.kratz@teleopti.com>
2016-10-25 17:10:28 +0200

    minor - comment to ourselfs

--- Domain/Scheduling/SeatLimitation/MaxSeatOptimization.cs
+++ Domain/Scheduling/SeatLimitation/MaxSeatOptimization.cs
@@ -55,6 +55,7 @@ namespace Teleopti.Ccc.Domain.Scheduling.SeatLimitation
 			_groupPersonBuilderForOptimizationFactory = groupPersonBuilderForOptimizationFactory;
 		}
 
+		//TODO: ska vi verkligen skicka in optimizationpreferences? Vi ska väl bara stödja team + GroupPageType.Hierarchy i alla fall?
 		public void Optimize(DateOnlyPeriod period, IEnumerable<IPerson> agentsToOptimize, IScheduleDictionary schedules, IScenario scenario, IOptimizationPreferences optimizationPreferences)
 		{
 			var allAgents = schedules.Select(schedule => schedule.Key);
