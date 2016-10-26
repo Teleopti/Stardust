aa78296c8e2b41936d7baa66a85b3acb4d442968
Yanyi Wan <none@none>
2016-10-26 17:20:28 +0800

    PBI #41305 Add belongs-to-date decider in front-end.

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.service.belongsToDateDecider.js
@@ -0,0 +1,86 @@
+﻿(function() {
+	'use strict';
+
+	angular.module('wfm.teamSchedule').service('belongsToDateDecider', belongsToDateDecider);
+
+	belongsToDateDecider.$inject = ['$filter'];
+
+	function belongsToDateDecider($filter) {
+
+		var self = this;
+
+		self.decideBelongsToDate = decideBelongsToDate;
+		self.normalizePersonScheduleVm = normalizePersonScheduleVm;
+
+
+		function decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray) {
+			var intersectedShiftDays = normalizedScheduleDataArray.filter(function(day) {
+				return day.shiftRange && timeRangeIntersect(day.shiftRange, targetTimeRange);
+			});
+
+			if (intersectedShiftDays.length > 1) return null;
+			if (intersectedShiftDays.length === 1) {
+				if (mergedRangeIsNotTooLong(targetTimeRange, intersectedShiftDays[0].shiftRange) 
+					&& !rangeStartsBeforeDay(targetTimeRange, intersectedShiftDays[0].timeRange)) {
+					return intersectedShiftDays[0].date;
+				} else {
+					return null;
+				}				
+			}
+
+			var startInEmptyDays = normalizedScheduleDataArray.filter(function(day) {
+				return !day.shiftRange && timeInTimeRange(targetTimeRange.startTime, day.timeRange);
+			});
+
+			if (startInEmptyDays.length !== 1) return null;
+			return startInEmptyDays[0].date;
+		}
+
+		function normalizePersonScheduleVm(personScheduleVm, currentTimezone) {
+			var result = {
+				date: personScheduleVm.Date
+			};
+
+			var currentDayStart = moment(personScheduleVm.Date).startOf('day');
+			var currentDayEnd = moment(personScheduleVm.Date).add(24, 'hour');
+
+			result.timeRange = {
+				startTime: moment($filter('timezone')(currentDayStart.format('YYYY-MM-DD hh:mm'), currentTimezone, personScheduleVm.Timezone)),
+				endTime: moment($filter('timezone')(currentDayEnd.format('YYYY-MM-DD hh:mm'), currentTimezone, personScheduleVm.Timezone))
+			}
+
+			if (personScheduleVm.Shifts && personScheduleVm.Shifts.length > 0) {
+				result.shiftRange = {
+					startTime: moment(personScheduleVm.ScheduleStartTime),
+					endTime: moment(personScheduleVm.ScheduleEndTime)
+				};
+			} else {
+				result.shiftRange = null;
+			}
+
+			return result;			
+		}
+
+		function mergedRangeIsNotTooLong(timeRangeA, timeRangeB) {
+			var allowShiftTotalMinutes = 36 * 60;
+			var startTime = timeRangeA.startTime < timeRangeB.startTime ? timeRangeA.startTime : timeRangeB.startTime;
+			var endTime = timeRangeA.endTime < timeRangeB.endTime ? timeRangeB.endTime : timeRangeA.endTime;
+			return endTime.diff(startTime, 'minute') <= allowShiftTotalMinutes;
+		}
+
+		function rangeStartsBeforeDay(targetTimeRange, dayTimeRange) {
+			return targetTimeRange.startTime < dayTimeRange.startTime;
+		}
+
+		function timeRangeIntersect(timeRangeA, timeRangeB) {
+			return (timeRangeA.startTime <= timeRangeB.startTime && timeRangeA.endTime >= timeRangeB.startTime) ||
+				(timeRangeB.startTime <= timeRangeA.startTime && timeRangeB.endTime >= timeRangeA.startTime);
+		}
+
+		function timeInTimeRange(time, timeRange) {
+			return timeRange.startTime <= time && time < timeRange.endTime;
+		}
+
+	}
+
+})();
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.service.belongsToDateDecider.spec.js
@@ -0,0 +1,266 @@
+﻿describe('belongs to date decider service tests', function() {
+
+	var target;
+
+	beforeEach(module('wfm.teamSchedule'));
+
+	beforeEach(inject(function(_belongsToDateDecider_) {
+		target = _belongsToDateDecider_;
+	}));
+
+	it('should decide the correct date if the target range intersects with the existing shift', function() {
+		var normalizedScheduleDataArray = [
+			{
+				date: '2016-07-01',
+				timeRange: {
+					startTime: moment('2016-07-01 00:00'),
+					endTime: moment('2016-07-02 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-01 23:00'),
+					endTime: moment('2016-07-02 03:00')
+				}
+			},
+			{
+				date: '2016-07-02',
+				timeRange: {
+					startTime: moment('2016-07-02 00:00'),
+					endTime: moment('2016-07-03 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-02 08:00'),
+					endTime: moment('2016-07-02 16:00')
+				}
+			},
+			{
+				date: '2016-07-03',
+				timeRange: {
+					startTime: moment('2016-07-03 00:00'),
+					endTime: moment('2016-07-04 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-03 08:00'),
+					endTime: moment('2016-07-03 16:00')
+				}
+			}
+		];
+
+		var targetTimeRange = { startTime: moment('2016-07-02 04:00'), endTime: moment('2016-07-02 08:00') };
+
+		var result = target.decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray);
+
+		expect(result).toEqual('2016-07-02');
+
+	});
+
+	it('should decide the correct date if the target range start from day without shift', function () {
+		var normalizedScheduleDataArray = [
+			{
+				date: '2016-07-01',
+				timeRange: {
+					startTime: moment('2016-07-01 00:00'),
+					endTime: moment('2016-07-02 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-01 08:00'),
+					endTime: moment('2016-07-01 16:00')
+				}
+			},
+			{
+				date: '2016-07-02',
+				timeRange: {
+					startTime: moment('2016-07-02 00:00'),
+					endTime: moment('2016-07-03 00:00')
+				},
+				shiftRange: null
+			},
+			{
+				date: '2016-07-03',
+				timeRange: {
+					startTime: moment('2016-07-03 00:00'),
+					endTime: moment('2016-07-04 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-03 08:00'),
+					endTime: moment('2016-07-03 16:00')
+				}
+			}
+		];
+
+		var targetTimeRange = { startTime: moment('2016-07-02 22:00'), endTime: moment('2016-07-03 04:00') };
+
+		var result = target.decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray);
+
+		expect(result).toEqual('2016-07-02');
+
+	});
+
+	it('should fail to decide if target range intersects with shifts in different days', function () {
+		var normalizedScheduleDataArray = [
+			{
+				date: '2016-07-01',
+				timeRange: {
+					startTime: moment('2016-07-01 00:00'),
+					endTime: moment('2016-07-02 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-01 23:00'),
+					endTime: moment('2016-07-02 06:00')
+				}
+			},
+			{
+				date: '2016-07-02',
+				timeRange: {
+					startTime: moment('2016-07-02 00:00'),
+					endTime: moment('2016-07-03 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-02 08:00'),
+					endTime: moment('2016-07-02 16:00')
+				}
+			},
+			{
+				date: '2016-07-03',
+				timeRange: {
+					startTime: moment('2016-07-03 00:00'),
+					endTime: moment('2016-07-04 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-03 08:00'),
+					endTime: moment('2016-07-03 16:00')
+				}
+			}
+		];
+
+		var targetTimeRange = { startTime: moment('2016-07-02 04:00'), endTime: moment('2016-07-02 09:00') };
+
+		var result = target.decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray);
+
+		expect(result).toBeNull();
+
+	});
+
+	it('should fail to decide if target range start from a day without shift but does not intersect it', function () {
+		var normalizedScheduleDataArray = [
+			{
+				date: '2016-07-01',
+				timeRange: {
+					startTime: moment('2016-07-01 00:00'),
+					endTime: moment('2016-07-02 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-01 23:00'),
+					endTime: moment('2016-07-02 03:00')
+				}
+			},
+			{
+				date: '2016-07-02',
+				timeRange: {
+					startTime: moment('2016-07-02 00:00'),
+					endTime: moment('2016-07-03 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-02 08:00'),
+					endTime: moment('2016-07-02 16:00')
+				}
+			},
+			{
+				date: '2016-07-03',
+				timeRange: {
+					startTime: moment('2016-07-03 00:00'),
+					endTime: moment('2016-07-04 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-03 08:00'),
+					endTime: moment('2016-07-03 16:00')
+				}
+			}
+		];
+
+		var targetTimeRange = { startTime: moment('2016-07-02 04:00'), endTime: moment('2016-07-02 05:00') };
+
+		var result = target.decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray);
+
+		expect(result).toBeNull();
+
+	});
+
+	it('should fail to decide if the target range merged with the existing shift becomes too long', function () {
+		var normalizedScheduleDataArray = [
+			{
+				date: '2016-07-01',
+				timeRange: {
+					startTime: moment('2016-07-01 00:00'),
+					endTime: moment('2016-07-02 00:00')
+				},
+				shiftRange: null
+			},
+			{
+				date: '2016-07-02',
+				timeRange: {
+					startTime: moment('2016-07-02 00:00'),
+					endTime: moment('2016-07-03 00:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-02 01:00'),
+					endTime: moment('2016-07-02 23:00')
+				}
+			},
+			{
+				date: '2016-07-03',
+				timeRange: {
+					startTime: moment('2016-07-03 00:00'),
+					endTime: moment('2016-07-04 00:00')
+				},
+				shiftRange: null
+			}
+		];
+
+		var targetTimeRange = { startTime: moment('2016-07-02 22:00'), endTime: moment('2016-07-03 14:00') };
+
+		var result = target.decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray);
+
+		expect(result).toBeNull();
+
+	});
+
+	it('should fail to decide if the target range merged with the existing shift starts before the belongs-to-date', function () {
+		var normalizedScheduleDataArray = [
+			{
+				date: '2016-07-01',
+				timeRange: {
+					startTime: moment('2016-07-01 10:00'),
+					endTime: moment('2016-07-02 10:00')
+				},
+				shiftRange: null
+			},
+			{
+				date: '2016-07-02',
+				timeRange: {
+					startTime: moment('2016-07-02 10:00'),
+					endTime: moment('2016-07-03 10:00')
+				},
+				shiftRange: {
+					startTime: moment('2016-07-02 13:00'),
+					endTime: moment('2016-07-03 00:00')
+				}
+			},
+			{
+				date: '2016-07-03',
+				timeRange: {
+					startTime: moment('2016-07-03 10:00'),
+					endTime: moment('2016-07-04 10:00')
+				},
+				shiftRange: null
+			}
+		];
+
+		var targetTimeRange = { startTime: moment('2016-07-02 08:00'), endTime: moment('2016-07-02 14:00') };
+
+		var result = target.decideBelongsToDate(targetTimeRange, normalizedScheduleDataArray);
+
+		expect(result).toBeNull();
+
+	});
+
+});
