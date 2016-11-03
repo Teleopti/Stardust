de6dfa2689a78a271b61e08fc516da6182bb96e1
Claes Stenman <claes.stenman@teleopti.com>
2016-11-03 11:07:58 +0100

    #40939 Add ShouldFixMaxSeatIfIssueIsNotOnFirstDay

--- DomainTest/SchedulingScenarios/MaxSeat/MaxSeatOptimizationTest.cs
+++ DomainTest/SchedulingScenarios/MaxSeat/MaxSeatOptimizationTest.cs
@@ -630,6 +630,38 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.MaxSeat
 				.Be.EqualTo(TimeSpan.FromHours(9));
 		}
 
+		[Test, Ignore("40939")]
+		public void ShouldFixMaxSeatIfIssueIsNotOnFirstDay()
+		{
+			var site = new Site("_") { MaxSeats = 1 }.WithId();
+			var team = new Team { Description = new Description("_"), Site = site };
+			GroupScheduleGroupPageDataProvider.SetBusinessUnit_UseFromTestOnly(BusinessUnitFactory.CreateBusinessUnitAndAppend(team));
+			var activity = new Activity("_") { RequiresSeat = true }.WithId();
+			var dateOnly = new DateOnly(2016, 10, 25);
+			var period = new DateOnlyPeriod(dateOnly, dateOnly.AddDays(1));
+			var scenario = new Scenario("_");
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(8, 0, 8, 0, 60), new TimePeriodWithSegment(16, 0, 16, 0, 60), new ShiftCategory("_").WithId()));
+			var agentDataOneHour = MaxSeatDataFactory.CreateAgentWithAssignment(period.EndDate, team, new RuleSetBag(ruleSet), scenario, activity, new TimePeriod(16, 0, 17, 0));
+			var agentDatas = MaxSeatDataFactory.CreateAgentWithAssignments(period, team, new RuleSetBag(ruleSet), scenario, activity, new TimePeriod(9, 0, 17, 0));
+
+			var assignments = new List<IPersonAssignment>();
+			foreach (var maxSeatData in agentDatas)
+			{
+				assignments.Add(maxSeatData.Assignment);
+			}
+			assignments.Add(agentDataOneHour.Assignment);
+
+			var schedules = ScheduleDictionaryCreator.WithData(scenario, period, assignments);
+			var optPreferences = CreateOptimizationPreferences();
+
+			Target.Optimize(period, new[] { agentDatas.First().Agent }, schedules, optPreferences, 15);
+
+			schedules[agentDatas.First().Agent].ScheduledDay(period.EndDate)
+				.PersonAssignment()
+				.Period.StartDateTime.TimeOfDay.Should()
+				.Be.EqualTo(TimeSpan.FromHours(8));
+		}
+
 		protected abstract OptimizationPreferences CreateOptimizationPreferences();
 	}
 }
