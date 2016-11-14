a4c5b9dbaedb838a2e6ebd638572801823462d0a
Kunning Mao <none@none>
2016-11-14 15:51:08 +0100

    #41652 added a test for wsfed (which is used in most of ADFS)

--- teamcity/SmokeTest/test/AzureAdTest.js
+++ teamcity/SmokeTest/test/AzureAdTest.js
@@ -6,7 +6,23 @@ var MicrosoftLoginPage = require('./pages/MicrosoftLogin.page');
 describe('Azure AD', function() {
 	this.timeout(360 * 1000); // Set global timeout for this test to 6 minutes
 	
-    it('should be able to sign in with AD user', function () {
+    it('should be able to sign in with AD user using OAuth', function () {
+		// Given that we are showing the IdentityProviders selection page
+		MytimePage.open();
+		if (MytimePage.isCurrentPage()) {
+			MytimePage.signout();
+		}
+		if (!IdentityProvidersPage.isCurrentPage()) {
+			MytimePage.signin();
+			MytimePage.signout();
+		}
+		IdentityProvidersPage.IdentityServerProvider.waitForExist(10 * 1000);
+		IdentityProvidersPage.IdentityServerProvider.click();
+		MicrosoftLoginPage.signin();
+		MytimePage.usernameLabel.waitForExist(60 * 1000);
+    }, 2);
+	
+	it('should be able to sign in with AD user using WsFed', function () {
 		// Given that we are showing the IdentityProviders selection page
 		MytimePage.open();
 		if (MytimePage.isCurrentPage()) {
--- teamcity/SmokeTest/test/pages/IdentityProviders.page.js
+++ teamcity/SmokeTest/test/pages/IdentityProviders.page.js
@@ -4,6 +4,7 @@ var IdentityProvidersPage = Object.create(Page, {
 	title: { get: function () { return 'Teleopti Authentication Bridge'; } },
 	
     azureadProvider: { get: function () { return browser.element('.azuread'); } },
+    IdentityServerProvider: { get: function () { return browser.element('.IdentityServer'); } },
     googleProvider: { get: function () { return browser.element('.google'); } },
 	teleoptiProvider: { get: function () { return browser.element('.teleopti'); } },
 	
