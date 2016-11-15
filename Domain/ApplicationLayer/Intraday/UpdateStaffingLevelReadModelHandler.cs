e73dd6069dfa9ef10612bd380e3f1edc61acdcf2
amandaj <none@none>
2016-11-15 14:51:17 +0100

    #41468 Remove unused Toggle

--- Domain/ApplicationLayer/Intraday/UpdateStaffingLevelReadModelHandler.cs
+++ Domain/ApplicationLayer/Intraday/UpdateStaffingLevelReadModelHandler.cs
@@ -8,7 +8,6 @@ using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Intraday
 {
-	//[EnabledBy(Toggles.Intraday_ResourceCalculateReadModel_39200)]
 	[EnabledBy(Toggles.AbsenceRequests_SpeedupIntradayRequests_40754, Toggles.AbsenceRequests_UseMultiRequestProcessing_39960)]
 	public class UpdateStaffingLevelReadModelHandler : IHandleEvent<UpdateStaffingLevelReadModelEvent>, IRunOnStardust
 	{
