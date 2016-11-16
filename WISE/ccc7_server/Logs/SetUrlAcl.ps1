57c6cb434c62041954c4c9e4016386233743e85e
Ola <none@none>
2016-11-16 10:07:19 +0100

    add access to multiple ports för Stardust

--- WISE/ccc7_server/Logs/SetUrlAcl.ps1
+++ WISE/ccc7_server/Logs/SetUrlAcl.ps1
@@ -1,8 +1,12 @@
-﻿$global:value = (netsh http show urlacl url=http://+:14100/)
-
-if(!($global:value  -like '*http://+:14100/*'))
-{
-    $global:value = (netsh http add urlacl url=http://+:14100/ user=Everyone listen=yes)
-}
-    
-
+﻿$numberOfNodes  = Get-WmiObject -Class Win32_ComputerSystem -ComputerName . | Select-Object -Property NumberOfLogicalProcessors
+$port = 14100
+for($i=1 
+    $i -le $numberOfNodes.NumberOfLogicalProcessors
+    $i++){
+            $global:value = (netsh http show urlacl url=http://+:$port/)
+            if(!($global:value  -like '*http://+:' + $port + '/*'))
+            {
+		        $global:value = (netsh http add urlacl url=http://+:$port/ user=Everyone listen=yes)
+	        }
+            $port++
+       }
