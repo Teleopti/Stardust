43a61b507875df8b9d9471fc012f119bd4a6ddc5
Kunning Mao <none@none>
2016-11-16 08:52:13 +0100

    fix the azure installation test, as okta changed their login page

--- teamcity/SmokeTest/test/pages/Okta.page.js
+++ teamcity/SmokeTest/test/pages/Okta.page.js
@@ -2,9 +2,9 @@ var Page = require('./Page.page')
 
 var OktaPage = Object.create(Page, {
 
-    username: { get: function () { return browser.element('#user-signin'); } },
-    password: { get: function () { return browser.element('#pass-signin'); } },
-    signinButton:     { get: function () { return browser.element('#signin-button'); } },
+    username: { get: function () { return browser.element('input[name="username"]'); } },
+    password: { get: function () { return browser.element('input[name="password"]'); } },
+    signinButton:     { get: function () { return browser.element('input[type="submit"]'); } },
 	
 	open: { value: function() {
 		var webUrl = 'https://teleopti.oktapreview.com/home/teleoptidev811147_teleoptirnd_1/0oa802gjr9ldcyH5M0h7/aln8049ac0l5gX1JO0h7?fromHome=true';
