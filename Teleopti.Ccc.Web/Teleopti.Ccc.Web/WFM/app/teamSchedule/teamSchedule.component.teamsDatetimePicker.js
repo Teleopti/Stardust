b3944fe60e703776d2fd7e3e9854dd48cc028ad4
Zulong Chen <none@none>
2016-11-18 10:26:51 +0800

    PBI #41696 add datetime component

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.component.teamsDatetimePicker.js
@@ -0,0 +1,32 @@
+﻿(function() {
+	'use strict';
+	angular.module('wfm.teamSchedule')
+		.component('teamsDatetimePicker',
+		{
+			templateUrl: 'app/teamSchedule/html/teamsDatetimePicker.tpl.html',
+			controller: TeamsDatetimePickerCtrl,
+			bindings: {
+				datetime: '<',
+				onUpdate: '&'
+			}
+		});
+
+	TeamsDatetimePickerCtrl.$inject = ['$locale'];
+
+	function TeamsDatetimePickerCtrl($locale) {
+		var ctrl = this;
+
+		ctrl.$onInit = function () {
+			ctrl.showMeridian = /h:/.test($locale.DATETIME_FORMATS.shortTime);
+			ctrl.meridians = $locale.DATETIME_FORMATS.AMPMS;
+		};
+
+		ctrl.onDateChange = function () {
+			ctrl.onUpdate({datetime: ctrl.datetime});
+		};
+
+		ctrl.onTimeChange = function () {
+			ctrl.onUpdate({datetime: ctrl.datetime});
+		};
+	}
+})();
