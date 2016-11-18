5a7c3ae5f899005e23312e2d8947fc5d22b162d5
Yanyi Wan <none@none>
2016-11-18 11:22:48 +0800

    delete NotificationsTimeToStay in web.config after PBI#38348: Move Notification Setting in from Config file to Option implemented

--- teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.ps1
@@ -263,7 +263,6 @@ function SetDefaultSettings{
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(UrlAuthenticationBridge)" -content "`$(UrlAuthenticationBridge)|https://$DataSourceName.teleopticloud.com/AuthenticationBridge/"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(WEB_DEPLOY)" -content "`$(WEB_DEPLOY)|true"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(DNS_ALIAS)" -content "`$(DNS_ALIAS)|https://$DataSourceName.teleopticloud.com/"
-	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(NEXT_ACTIVITY_TIME_TO_STAY)" -content "`$(NEXT_ACTIVITY_TIME_TO_STAY)|30"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(HSTS_HEADER)" -content "`$(HSTS_HEADER)|<add name=`"Strict-Transport-Security`" value=`"max-age=31536000`"/>"
 	AddIfNotExists -fullPathsettingsFile "$fullPathsettingsFile" -variableName "`$(CSP_PARENT_URL)" -content "`$(CSP_PARENT_URL)|"
 }
