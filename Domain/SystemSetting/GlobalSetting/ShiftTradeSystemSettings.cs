d21104f40fee0936a4abca7286d6e3cf0f87ee6d
Xinfeng Li <xinfeng.li@teleopti.com>
2016-11-21 17:11:48 +0800

    PBI #40770: Add business rule config for shift trade setting

--- Domain/SystemSetting/GlobalSetting/ShiftTradeSystemSettings.cs
+++ Domain/SystemSetting/GlobalSetting/ShiftTradeSystemSettings.cs
@@ -2,16 +2,31 @@ using System;
 
 namespace Teleopti.Ccc.Domain.SystemSetting.GlobalSetting
 {
+	public enum RequestHandleType
+	{
+		AutoDeny = 0,
+		SetToPending = 1
+	}
+
+	[Serializable]
+	public class ShiftTradeBusinessRuleConfig
+	{
+		// Full name of business rule class, use string since Type could not be serialized
+		public string BusinessRuleType { get; set; }
+
+		public string FriendlyName { get; set; }
+
+		public bool Enabled { get; set; }
+		public RequestHandleType HandleTypeOnBroken { get; set; }
+	}
+
 	[Serializable]
 	public class ShiftTradeSettings : SettingValue
 	{
-
 		public const string SettingsKey = "ShiftTradeSettings";
-
 
 		private int _maxSeatsValidationSegmentLength = 15;
 		private bool _maxSeatsValidationEnabled = false;
-
 
 		public bool MaxSeatsValidationEnabled
 		{
@@ -25,8 +40,6 @@ namespace Teleopti.Ccc.Domain.SystemSetting.GlobalSetting
 			set { _maxSeatsValidationSegmentLength = value; }
 		}
 
-
-		
-
+		public ShiftTradeBusinessRuleConfig[] BusinessRuleConfigs { get; set; }
 	}
 }
---
+++ DomainTest/SystemSetting/GlobalSetting/ShiftTradeSettingsTest.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.Scheduling.Rules;
+using Teleopti.Ccc.Domain.SystemSetting.GlobalSetting;
+
+namespace Teleopti.Ccc.DomainTest.SystemSetting.GlobalSetting
+{
+	[TestFixture]
+	public class ShiftTradeBusinessRuleConfigTest
+	{
+		[Test]
+		public void ShouldBeDeserializedCorrectly()
+		{
+			var originalSetting = new ShiftTradeSettings
+			{
+				MaxSeatsValidationEnabled = true,
+				MaxSeatsValidationSegmentLength = 23,
+				BusinessRuleConfigs = new[]
+				{
+					new ShiftTradeBusinessRuleConfig
+					{
+						FriendlyName = "Data Part Of Agent Day Rule",
+						BusinessRuleType = typeof(DataPartOfAgentDay).FullName,
+						Enabled = true,
+						HandleTypeOnBroken = RequestHandleType.AutoDeny
+					},
+					new ShiftTradeBusinessRuleConfig
+					{
+						FriendlyName = "Min Weekly Rest Rule",
+						BusinessRuleType = typeof(MinWeeklyRestRule).FullName,
+						Enabled = true,
+						HandleTypeOnBroken = RequestHandleType.SetToPending
+					},
+					new ShiftTradeBusinessRuleConfig
+					{
+						FriendlyName = "Min Week Work Time Rule",
+						BusinessRuleType = typeof(MinWeekWorkTimeRule).FullName,
+						Enabled = false,
+						HandleTypeOnBroken = RequestHandleType.SetToPending
+					}
+				}
+			};
+
+			var xmlSerializer = new XmlSerializer(typeof(ShiftTradeSettings));
+
+			string serializedValue;
+			using (var stringWriter = new StringWriter())
+			{
+				using (var xmlWriter = new XmlTextWriter(stringWriter))
+				{
+					xmlSerializer.Serialize(xmlWriter, originalSetting);
+					serializedValue = stringWriter.ToString();
+				}
+			}
+
+			ShiftTradeSettings deserializedSetting;
+			using (TextReader reader = new StringReader(serializedValue))
+			{
+				deserializedSetting = (ShiftTradeSettings)xmlSerializer.Deserialize(reader);
+			}
+
+			Assert.AreEqual(originalSetting.MaxSeatsValidationEnabled, deserializedSetting.MaxSeatsValidationEnabled);
+			Assert.AreEqual(originalSetting.MaxSeatsValidationSegmentLength, deserializedSetting.MaxSeatsValidationSegmentLength);
+
+			Assert.AreEqual(originalSetting.BusinessRuleConfigs.Length, deserializedSetting.BusinessRuleConfigs.Length);
+			for (var i = 0; i < originalSetting.BusinessRuleConfigs.Length; i++)
+			{
+				var originalRuleConfig = originalSetting.BusinessRuleConfigs[i];
+				var deserializedRuleConfig = deserializedSetting.BusinessRuleConfigs[i];
+				Assert.AreEqual(originalRuleConfig.FriendlyName, deserializedRuleConfig.FriendlyName);
+				Assert.AreEqual(originalRuleConfig.BusinessRuleType, deserializedRuleConfig.BusinessRuleType);
+				Assert.AreEqual(originalRuleConfig.Enabled, deserializedRuleConfig.Enabled);
+				Assert.AreEqual(originalRuleConfig.HandleTypeOnBroken, deserializedRuleConfig.HandleTypeOnBroken);
+			}
+		}
+	}
+}
