b1912e3d55da0342c2669f869f56b25d8531e521
Anders Forsberg <anders.forsberg@teleopti.com>
2016-11-22 10:07:32 +0100

    Cleanup

--- Database/TeleoptiCCC7/Releases/00000769.sql
+++ Database/TeleoptiCCC7/Releases/00000769.sql
@@ -1,16 +1,7 @@
-begin
-	declare @minutes int
-	select @minutes =value from 	dbo.RequestStrategySettings
-	where setting = 'AbsenceFarFutureTime'
-	if @minutes = 60
-		update dbo.RequestStrategySettings
-		set value = 15
-		where Setting = 'AbsenceFarFutureTime' 
+update dbo.RequestStrategySettings
+set value = 15
+where Setting = 'AbsenceFarFutureTime' and value = 60
 
-	select @minutes =value from 	dbo.RequestStrategySettings
-	where setting = 'AbsenceNearFutureTime'
-	if @minutes = 20
-		update dbo.RequestStrategySettings
-		set value = 5
-		where Setting = 'AbsenceNearFutureTime' 
-end;
+update dbo.RequestStrategySettings
+set value = 5
+where Setting = 'AbsenceNearFutureTime' and value = 20
