45987315ca4be746e337f2d2c49b40ec67ca620a
David Jonsson <davidj@teleopti.com>
2016-11-23 09:28:02 -0700

    #41795: remove temp  constraints added by Service Desk

--- Database/TeleoptiCCC7/Releases/00000770.sql
+++ Database/TeleoptiCCC7/Releases/00000770.sql
@@ -1,2 +1,6 @@
+--#remove the temp fix added for some customers
+IF OBJECT_ID('mart.[UC_dim_date_date_date]', 'UQ') IS NOT NULL 
+    ALTER TABLE [mart].[dim_date] DROP CONSTRAINT [UC_dim_date_date_date]
+
 ALTER TABLE [mart].[dim_date] ADD CONSTRAINT UK_dim_date_date_date UNIQUE (date_date)
 GO
