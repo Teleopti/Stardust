76ee2d8e77e602a35cb3f0f31179b9bedb70a6b1
Justin! <justin@teleopti.com>
2016-11-27 19:16:35 +0100

    Backed out: #41383 #42006 Fix it for real, real?

--- Domain/ApplicationLayer/Rta/Service/StateCodeAdder.cs
+++ Domain/ApplicationLayer/Rta/Service/StateCodeAdder.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.Repositories;
-using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 {
@@ -14,28 +13,23 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 	public class EventualStateCodeAdder : IStateCodeAdder
 	{
 		private readonly IRtaStateGroupRepository _stateGroupRepository;
-		private readonly object defaultStateGroupLock = new object();
 
 		public EventualStateCodeAdder(IRtaStateGroupRepository stateGroupRepository)
 		{
 			_stateGroupRepository = stateGroupRepository;
 		}
-		
+
 		public MappedState AddUnknownStateCode(IEnumerable<Mapping> mappings, Guid businessUnitId, Guid platformTypeId, string stateCode, string stateDescription)
 		{
-			var stateGroups = _stateGroupRepository.LoadAllCompleteGraph();
+			var stateGroups = _stateGroupRepository.LoadAllExclusive();
 
-			IRtaStateGroup existingStateGroup;
-			lock (defaultStateGroupLock)
-			{
-				existingStateGroup = (
-					from g in stateGroups
-					from s in g.StateCollection
-					where g.BusinessUnit.Id.Value == businessUnitId
-						  && s.StateCode == stateCode
-					select g
-					).SingleOrDefault();
-			}
+			var existingStateGroup = (
+				from g in stateGroups
+				from s in g.StateCollection
+				where g.BusinessUnit.Id.Value == businessUnitId
+					  && s.StateCode == stateCode
+				select g
+				).SingleOrDefault();
 
 			if (existingStateGroup != null)
 				return new MappedState
@@ -53,8 +47,13 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service
 
 			if (defaultStateGroup != null)
 			{
-				lock (defaultStateGroupLock)
+
+				// locking here only for domain testing
+				// because threads in domain share the same instance of the aggregate
+				// and we dont want locks in the aggregate
+				lock (defaultStateGroup)
 					defaultStateGroup.AddState(stateDescription ?? stateCode, stateCode, platformTypeId);
+
 				return new MappedState
 				{
 					StateGroupId = defaultStateGroup.Id.Value,
