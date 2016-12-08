fb5c50ce562ab41aada900f9ec0bf3f10471661e
Roger Kratz <roger.kratz@teleopti.com>
2016-12-08 16:05:50 +0100

    #42049 Refactor - a lot faster reducing islands avoiding proxies

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/IslandController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/IslandController.cs
@@ -1,22 +1,26 @@
 ﻿using System.Web.Http;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.Islands.ClientModel;
+using Teleopti.Ccc.Domain.Repositories;
 
 namespace Teleopti.Ccc.Web.Areas.ResourcePlanner
 {
 	public class IslandController : ApiController
 	{
 		private readonly IslandModelFactory _islandModelFactory;
+		private readonly ISkillRepository _skillRepository;
 
-		public IslandController(IslandModelFactory islandModelFactory)
+		public IslandController(IslandModelFactory islandModelFactory, ISkillRepository skillRepository)
 		{
 			_islandModelFactory = islandModelFactory;
+			_skillRepository = skillRepository;
 		}
 
 		[UnitOfWork]
 		[HttpGet, Route("api/ResourcePlanner/Islands")]
 		public virtual IHttpActionResult Islands()
 		{
+			_skillRepository.LoadAllSkills();//perf - avoid billions of proxy calls on skills
 			return Json(_islandModelFactory.Create());
 		}
 	}
