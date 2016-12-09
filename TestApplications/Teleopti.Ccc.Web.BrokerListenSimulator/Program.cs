e4dbd38b249ca6510c4e2a425229389d3675d321
Justin! <justin@teleopti.com>
2016-12-09 13:53:04 +0100

    #41900 New broker listen simulator

---
+++ TestApplications/Teleopti.Ccc.Web.BrokerListenSimulator/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Config;
+using Teleopti.Ccc.Domain.Helper;
+using Teleopti.Ccc.Domain.MessageBroker.Client;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.BrokerListenSimulator
+{
+
+	public class SimulationData
+	{
+		public string DataSource { get; set; }
+		public Guid BusinessUnit { get; set; }
+		public Guid Scenario { get; set; }
+
+		public string Url { get; set; }
+
+		public DateTime SchedulingScreenStartDate { get; set; }
+		public DateTime SchedulingScreenEndDate { get; set; }
+	}
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var data = new SimulationData
+			{
+				DataSource = "Teleopti WFM",
+				BusinessUnit = Guid.Parse("C087E9B6-5F52-4969-968A-A24200ACE742"),
+				Scenario = Guid.Parse("3348D359-D15F-4069-9277-A24200ACEB3D"),
+				Url = "http://wfmrc2/TeleoptiWFM/Web",
+				SchedulingScreenStartDate = "2016-01-01 00:00".Utc(),
+				SchedulingScreenEndDate = "2016-12-31 00:00".Utc()
+			};
+			Simulate(data, 200, 2);
+		}
+		
+		private static void Simulate(SimulationData data, int clients, int screens)
+		{
+			var currentDatasource = new FakeCurrentDatasource(new DataSourceState());
+			currentDatasource.FakeName(data.DataSource);
+
+			var businessUnit = new BusinessUnit("..");
+			businessUnit.SetId(data.BusinessUnit);
+			var currentBusinessUnit = new FakeCurrentBusinessUnit();
+			currentBusinessUnit.FakeBusinessUnit(businessUnit);
+
+			var scenario = new Scenario("..");
+			scenario.SetId(data.Scenario);
+			var currentScenario = new FakeCurrentScenario();
+			currentScenario.FakeScenario(scenario);
+
+			Console.WriteLine("starting {0} clients with {1} screens each", clients, screens);
+
+			var s = new Stopwatch();
+			s.Start();
+			Enumerable.Range(1, clients).ForEach(client =>
+			{
+				var builder = new ContainerBuilder();
+				var iocArgs = new IocArgs(new ConfigReader())
+				{
+					MessageBrokerListeningEnabled = true,
+					ImplementationTypeForCurrentUnitOfWork = typeof(FromFactory),
+				};
+				var configuration = new IocConfiguration(iocArgs, CommonModule.ToggleManagerForIoc(iocArgs));
+				builder.RegisterModule(new CommonModule(configuration));
+				builder.RegisterInstance(currentScenario).As<ICurrentScenario>();
+				builder.RegisterInstance(currentDatasource).As<ICurrentDataSource>();
+				builder.RegisterInstance(currentBusinessUnit).As<ICurrentBusinessUnit>();
+				builder.RegisterType<SimulateSchedulingScreen>().InstancePerDependency();
+				var container = builder.Build();
+
+				container.Resolve<IMessageBrokerUrl>()
+					.Configure(data.Url);
+
+				var messageBroker = container.Resolve<IMessageBrokerComposite>();
+				messageBroker.StartBrokerService(true);
+				while (!messageBroker.IsAlive)
+					Task.Delay(500);
+
+				var subscriptions = Enumerable.Range(1, screens).Select(screen => {
+					var schedulingScreen = container.Resolve<SimulateSchedulingScreen>();
+					return Task.Factory.StartNew(() =>
+					{
+						schedulingScreen.Simulate(data, screen, client);
+					}, TaskCreationOptions.LongRunning);
+				});
+
+				Task.WaitAll(subscriptions.ToArray());
+			});
+
+			Console.WriteLine("Started listening, took {0}", s.Elapsed);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ TestApplications/Teleopti.Ccc.Web.BrokerListenSimulator/SimulateSchedulingScreen.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.MessageBroker;
+using Teleopti.Ccc.Domain.MessageBroker.Client;
+using Teleopti.Ccc.Domain.MessageBroker.Legacy;
+using Teleopti.Interfaces;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.BrokerListenSimulator
+{
+
+	public class SimulateSchedulingScreen 
+    {
+	    private readonly IMessageBrokerUrl _url;
+	    private readonly ICurrentDataSource _dataSource;
+	    private readonly ICurrentBusinessUnit _businessUnit;
+	    private readonly ICurrentScenario _scenario;
+	    private readonly IJsonSerializer _serializer;
+	    private readonly IJsonDeserializer _deserializer;
+	    private readonly IMessageBrokerComposite _messageBroker;
+	    private readonly HttpClient _httpClient;
+	    private string _number;
+
+	    public SimulateSchedulingScreen(
+			IMessageBrokerUrl url, 
+			ICurrentDataSource dataSource, 
+			ICurrentBusinessUnit businessUnit, 
+			ICurrentScenario scenario, 
+			IJsonSerializer serializer, 
+			IJsonDeserializer deserializer,
+			IMessageBrokerComposite messageBroker)
+	    {
+		    _url = url;
+		    _dataSource = dataSource;
+		    _businessUnit = businessUnit;
+		    _scenario = scenario;
+		    _serializer = serializer;
+		    _deserializer = deserializer;
+		    _messageBroker = messageBroker;
+		    _httpClient = new HttpClient();
+		}
+
+		public void Simulate(SimulationData data, int screen, int client)
+		{
+			_number = $"#{screen}/{client}";
+
+			AddSubscription(new Subscription
+            {
+				MailboxId = Guid.NewGuid().ToString(),
+				DomainType = typeof(IScheduleChangedEvent).Name,
+                DomainReferenceId = Subscription.IdToString(_scenario.Current().Id.Value),
+                DomainReferenceType = typeof(Scenario).AssemblyQualifiedName,
+                LowerBoundary = Subscription.DateToString(data.SchedulingScreenStartDate),
+                UpperBoundary = Subscription.DateToString(data.SchedulingScreenEndDate),
+                DataSource = _dataSource.CurrentName(),
+                BusinessUnitId = Subscription.IdToString(_scenario.Current().Id.Value),
+            }, callback);
+
+            AddSubscription(new Subscription
+            {
+				MailboxId = Guid.NewGuid().ToString(),
+				DomainType = typeof(IPersistableScheduleData).Name,
+                DomainReferenceId = null,
+                DomainReferenceType = null,
+                LowerBoundary = Subscription.DateToString(data.SchedulingScreenStartDate),
+                UpperBoundary = Subscription.DateToString(data.SchedulingScreenEndDate),
+                DataSource = _dataSource.CurrentName(),
+                BusinessUnitId = Subscription.IdToString(_businessUnit.Current().Id.Value),
+			}, callback);
+
+			AddSubscription(new Subscription
+            {
+				MailboxId = Guid.NewGuid().ToString(),
+				DomainType = typeof(IMeeting).Name,
+                DomainReferenceId = null,
+                DomainReferenceType = null,
+                LowerBoundary = Subscription.DateToString(Consts.MinDate),
+                UpperBoundary = Subscription.DateToString(Consts.MaxDate),
+                DataSource = _dataSource.CurrentName(),
+                BusinessUnitId = Subscription.IdToString(_businessUnit.Current().Id.Value),
+			}, callback);
+
+			AddSubscription(new Subscription
+            {
+				MailboxId = Guid.NewGuid().ToString(),
+                DomainType = typeof(IPersonRequest).Name,
+                DomainReferenceId = null,
+                DomainReferenceType = null,
+                LowerBoundary = Subscription.DateToString(Consts.MinDate),
+                UpperBoundary = Subscription.DateToString(Consts.MaxDate),
+                DataSource = _dataSource.CurrentName(),
+                BusinessUnitId = Subscription.IdToString(_businessUnit.Current().Id.Value),
+			}, callback);
+		}
+
+		private void AddSubscription(Subscription subscription, EventHandler<EventMessageArgs> callback)
+		{
+			//_messageBroker.RegisterSubscription(subscription, callback);
+			startPoll(subscription);
+		}
+
+		private string url(string call)
+		{
+			if (_url == null)
+				return null;
+			if (string.IsNullOrEmpty(_url.Url))
+				return null;
+			return $"{_url.Url.TrimEnd('/')}/{call}";
+		}
+
+		private async void startPoll(Subscription subscription)
+		{
+			while (true)
+			{
+				await Task.Delay(TimeSpan.FromSeconds(120)); // default at this time
+				var result = await get(url($"MessageBroker/PopMessages?route={subscription.Route()}&id={subscription.MailboxId}"));
+				var messages = _deserializer.DeserializeObject<Message[]>(result);
+				messages.ForEach(m =>
+				{
+					callback(this, null);
+				});
+			}
+		}
+
+		private async void post(string url, string content)
+		{
+			var result = await _httpClient.PostAsync(url, new StringContent(content, Encoding.UTF8, "application/json"));
+			if (result.StatusCode != HttpStatusCode.OK)
+				throw new Exception($"POST failed! {result.StatusCode}");
+		}
+
+		private async Task<string> get(string url)
+		{
+			var result = await _httpClient.GetAsync(url);
+			if (result.StatusCode != HttpStatusCode.OK)
+				throw new Exception($"GET failed! {result.StatusCode}");
+			return await result.Content.ReadAsStringAsync();
+		}
+
+		private void callback(object sender, EventMessageArgs e)
+		{
+			Console.WriteLine($"{_number} callbacked");
+		}
+	}
+
+}
