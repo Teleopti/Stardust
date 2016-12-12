98665e0d8e841f6cde3b44d1f4cfc5ecb2c81d79
Justin! <justin@teleopti.com>
2016-12-12 11:36:24 +0100

    Attempt to fix performance tests - optimizing database prepare

--- Teleopti.Ccc.Rta.PerformanceTest/Code/DataCreator.cs
+++ Teleopti.Ccc.Rta.PerformanceTest/Code/DataCreator.cs
@@ -1,16 +1,24 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using Teleopti.Ccc.Domain.AgentInfo;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.Common.TimeLogger;
 using Teleopti.Ccc.Domain.Helper;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.UnitOfWork;
 using Teleopti.Ccc.Infrastructure.MultiTenancy.Server.NHibernate;
-using Teleopti.Ccc.TestCommon.TestData.Analytics;
+using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.TestData.Core;
 using Teleopti.Ccc.TestCommon.TestData.Setups.Configurable;
 using Teleopti.Ccc.TestCommon.TestData.Setups.Default;
+using Teleopti.Interfaces.Domain;
 using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Rta.PerformanceTest.Code
@@ -23,6 +31,17 @@ namespace Teleopti.Ccc.Rta.PerformanceTest.Code
 		private readonly IEventPublisher _eventPublisher;
 		private readonly ITenantUnitOfWork _tenantUnitOfWork;
 		private readonly ICurrentTenantSession _currentTenantSession;
+		private readonly AnalyticsDatabase _analytics;
+		private readonly Database _database;
+		private readonly WithUnitOfWork _withUnitOfWork;
+		private readonly ITeamRepository _teams;
+		private readonly IPersonRepository _persons;
+		private readonly IContractRepository _contracts;
+		private readonly IPartTimePercentageRepository _partTimePercentages;
+		private readonly IContractScheduleRepository _contractSchedules;
+		private readonly ICurrentScenario _scenario;
+		private readonly IPersonAssignmentRepository _assignments;
+		private readonly IActivityRepository _activities;
 
 		public DataCreator(
 			MutableNow now,
@@ -30,7 +49,18 @@ namespace Teleopti.Ccc.Rta.PerformanceTest.Code
 			ICurrentUnitOfWork unitOfWork,
 			IEventPublisher eventPublisher,
 			ITenantUnitOfWork tenantUnitOfWork,
-			ICurrentTenantSession currentTenantSession
+			ICurrentTenantSession currentTenantSession,
+			AnalyticsDatabase analytics,
+			Database database,
+			WithUnitOfWork withUnitOfWork,
+			ITeamRepository teams,
+			IPersonRepository persons,
+			IContractRepository contracts,
+			IPartTimePercentageRepository partTimePercentages,
+			IContractScheduleRepository contractSchedules,
+			ICurrentScenario scenario,
+			IPersonAssignmentRepository assignments,
+			IActivityRepository activities
 			)
 		{
 			_now = now;
@@ -39,52 +69,140 @@ namespace Teleopti.Ccc.Rta.PerformanceTest.Code
 			_eventPublisher = eventPublisher;
 			_tenantUnitOfWork = tenantUnitOfWork;
 			_currentTenantSession = currentTenantSession;
+			_analytics = analytics;
+			_database = database;
+			_withUnitOfWork = withUnitOfWork;
+			_teams = teams;
+			_persons = persons;
+			_contracts = contracts;
+			_partTimePercentages = partTimePercentages;
+			_contractSchedules = contractSchedules;
+			_scenario = scenario;
+			_assignments = assignments;
+			_activities = activities;
 		}
 
 		[TestLogTime]
-		[UnitOfWork]
 		public virtual void Create()
 		{
 			_now.Is("2016-02-25 08:00".Utc());
 
-			var data = new TestDataFactory(_unitOfWork, _currentTenantSession, _tenantUnitOfWork);
+			_analytics
+				.WithDataSource(_testConfiguration.DataSourceId, _testConfiguration.SourceId)
+				;
 
-			var datasource = new Datasources(_testConfiguration.DataSourceId, " ", -1, " ", -1, " ", " ", 1, false, _testConfiguration.SourceId, false);
-			new AnalyticsDataFactory().Apply(datasource);
+			createCommonData();
+			createPersonsAndSchedules();
+
+			// to create/update any data that is periodically kept up to date
+			// like the rule mappings
+			_eventPublisher.Publish(new TenantMinuteTickEvent());
+			_eventPublisher.Publish(new TenantHourTickEvent());
+
+		}
+
+		private void createPersonsAndSchedules()
+		{
+			IList<IPerson> rogers = new List<IPerson>();
+
+			_withUnitOfWork.Do(() =>
+			{
+				var contract = _contracts.LoadAll().Single(x => x.Description.Name == "contract");
+				var partTimePercentage = _partTimePercentages.LoadAll().Single(x => x.Description.Name == "partTimePercentage");
+				var contractSchedule = _contractSchedules.LoadAll().Single(x => x.Description.Name == "contractSchedule");
+				var teams = _teams.LoadAll();
+
+				Enumerable.Range(0, _testConfiguration.NumberOfAgents)
+					.ForEach(roger =>
+					{
+						var name = "roger" + roger;
+						var team = teams.Single(x => x.Description.Name == "team" + (roger/10));
+
+						var person = new Person {Name = new Name(name, name)};
+						person.PermissionInformation.SetDefaultTimeZone(TimeZoneInfo.Utc);
+						_persons.Add(person);
+
+						var personContract = new PersonContract(contract, partTimePercentage, contractSchedule);
+						var personPeriod = new PersonPeriod("2001-01-01".Date(), personContract, team);
+						person.AddPersonPeriod(personPeriod);
+
+						rogers.Add(person);
+					});
+
+			});
+
+			var phone = _withUnitOfWork.Get(() => _activities.LoadAll().Single(x => x.Name == "Phone"));
+			var brejk = _withUnitOfWork.Get(() => _activities.LoadAll().Single(x => x.Name == "Break"));
+			var lunch = _withUnitOfWork.Get(() => _activities.LoadAll().Single(x => x.Name == "Lunch"));
+
+			rogers.ForEach(roger =>
+			{
+				_withUnitOfWork.Do(() =>
+				{
+					var assignment1 = new PersonAssignment(roger, _scenario.Current(), "2016-02-25".Date());
+					assignment1.AddActivity(phone, "2016-02-25 08:00".Utc(), "2016-02-25 17:00".Utc());
+					assignment1.AddActivity(brejk, "2016-02-25 10:00".Utc(), "2016-02-25 10:15".Utc());
+					assignment1.AddActivity(lunch, "2016-02-25 11:30".Utc(), "2016-02-25 12:00".Utc());
+					assignment1.AddActivity(brejk, "2016-02-25 15:00".Utc(), "2016-02-25 15:15".Utc());
+					_assignments.Add(assignment1);
+
+					var assignment2 = new PersonAssignment(roger, _scenario.Current(), "2016-02-26".Date());
+					assignment2.AddActivity(phone, "2016-02-26 08:00".Utc(), "2016-02-26 17:00".Utc());
+					assignment2.AddActivity(brejk, "2016-02-26 10:00".Utc(), "2016-02-26 10:15".Utc());
+					assignment2.AddActivity(lunch, "2016-02-26 11:30".Utc(), "2016-02-26 12:00".Utc());
+					assignment2.AddActivity(brejk, "2016-02-26 15:00".Utc(), "2016-02-26 15:15".Utc());
+					_assignments.Add(assignment2);
+
+					var assignment3 = new PersonAssignment(roger, _scenario.Current(), "2016-02-27".Date());
+					assignment3.AddActivity(phone, "2016-02-27 08:00".Utc(), "2016-02-27 17:00".Utc());
+					assignment3.AddActivity(brejk, "2016-02-27 10:00".Utc(), "2016-02-27 10:15".Utc());
+					assignment3.AddActivity(lunch, "2016-02-27 11:30".Utc(), "2016-02-27 12:00".Utc());
+					assignment3.AddActivity(brejk, "2016-02-27 15:00".Utc(), "2016-02-27 15:15".Utc());
+					_assignments.Add(assignment3);
+
+				});
+			});
+
+		}
+
+		[UnitOfWork]
+		protected virtual void createCommonData()
+		{
+			var data = new TestDataFactory(_unitOfWork, _currentTenantSession, _tenantUnitOfWork);
 
 			var businessUnit = DefaultBusinessUnit.BusinessUnit;
 
-			data.Apply(new ScenarioConfigurable { Name = "Default", BusinessUnit = businessUnit.Name });
-			data.Apply(new ActivityConfigurable { Name = "Phone" });
-			data.Apply(new ActivityConfigurable { Name = "Lunch" });
-			data.Apply(new ActivityConfigurable { Name = "Break" });
+			data.Apply(new ScenarioConfigurable {Name = "Default", BusinessUnit = businessUnit.Name});
+			data.Apply(new ActivityConfigurable {Name = "Phone"});
+			data.Apply(new ActivityConfigurable {Name = "Lunch"});
+			data.Apply(new ActivityConfigurable {Name = "Break"});
 
-			data.Apply(new ContractConfigurable { Name = "contract" });
-			data.Apply(new PartTimePercentageConfigurable { Name = "partTimePercentage" });
-			data.Apply(new ContractScheduleConfigurable { Name = "contractSchedule" });
+			data.Apply(new ContractConfigurable {Name = "contract"});
+			data.Apply(new PartTimePercentageConfigurable {Name = "partTimePercentage"});
+			data.Apply(new ContractScheduleConfigurable {Name = "contractSchedule"});
 
-			data.Apply(new RtaMapConfigurable { Activity = "Phone", PhoneState = "Ready", Adherence = "In", Name = "InAdherence" });
-			data.Apply(new RtaMapConfigurable { Activity = "Phone", PhoneState = "LoggedOff", Adherence = "Out", Name = "OutOfAdherence" });
+			data.Apply(new RtaMapConfigurable {Activity = "Phone", PhoneState = "Ready", Adherence = "In", Name = "InAdherence"});
+			data.Apply(new RtaMapConfigurable {Activity = "Phone", PhoneState = "LoggedOff", Adherence = "Out", Name = "OutOfAdherence"});
 
-			data.Apply(new RtaMapConfigurable { Activity = "Break", PhoneState = "LoggedOff", Adherence = "In", Name = "InAdherence" });
-			data.Apply(new RtaMapConfigurable { Activity = "Break", PhoneState = "Ready", Adherence = "Out", Name = "OutOfAdherence" });
+			data.Apply(new RtaMapConfigurable {Activity = "Break", PhoneState = "LoggedOff", Adherence = "In", Name = "InAdherence"});
+			data.Apply(new RtaMapConfigurable {Activity = "Break", PhoneState = "Ready", Adherence = "Out", Name = "OutOfAdherence"});
 
-			data.Apply(new RtaMapConfigurable { Activity = "Lunch", PhoneState = "LoggedOff", Adherence = "In", Name = "InAdherence" });
-			data.Apply(new RtaMapConfigurable { Activity = "Lunch", PhoneState = "Ready", Adherence = "Out", Name = "OutOfAdherence" });
+			data.Apply(new RtaMapConfigurable {Activity = "Lunch", PhoneState = "LoggedOff", Adherence = "In", Name = "InAdherence"});
+			data.Apply(new RtaMapConfigurable {Activity = "Lunch", PhoneState = "Ready", Adherence = "Out", Name = "OutOfAdherence"});
 
-			data.Apply(new RtaMapConfigurable { Activity = null, PhoneState = "LoggedOff", Adherence = "In", Name = "InAdherence" });
+			data.Apply(new RtaMapConfigurable {Activity = null, PhoneState = "LoggedOff", Adherence = "In", Name = "InAdherence"});
 
-			Enumerable.Range(0, (_testConfiguration.NumberOfMappings / 4))
+			Enumerable.Range(0, (_testConfiguration.NumberOfMappings/4))
 				.ForEach(code =>
 				{
 					var phoneState = $"Misc{code}";
-					data.Apply(new RtaMapConfigurable { Activity = null, PhoneState = phoneState, Adherence = "Neutral", Name = "NeutralAdherence" });
-					data.Apply(new RtaMapConfigurable { Activity = "Phone", PhoneState = phoneState, Adherence = "In", Name = "InAdherence" });
-					data.Apply(new RtaMapConfigurable { Activity = "Break", PhoneState = phoneState, Adherence = "Out", Name = "OutOfAdherence" });
-					data.Apply(new RtaMapConfigurable { Activity = "Lunch", PhoneState = phoneState, Adherence = "Out", Name = "OutOfAdherence" });
+					data.Apply(new RtaMapConfigurable {Activity = null, PhoneState = phoneState, Adherence = "Neutral", Name = "NeutralAdherence"});
+					data.Apply(new RtaMapConfigurable {Activity = "Phone", PhoneState = phoneState, Adherence = "In", Name = "InAdherence"});
+					data.Apply(new RtaMapConfigurable {Activity = "Break", PhoneState = phoneState, Adherence = "Out", Name = "OutOfAdherence"});
+					data.Apply(new RtaMapConfigurable {Activity = "Lunch", PhoneState = phoneState, Adherence = "Out", Name = "OutOfAdherence"});
 				});
 
-			Enumerable.Range(0, (_testConfiguration.NumberOfAgents / 100) + 1)
+			Enumerable.Range(0, (_testConfiguration.NumberOfAgents/100) + 1)
 				.ForEach(site =>
 				{
 					data.Apply(new SiteConfigurable
@@ -94,66 +212,15 @@ namespace Teleopti.Ccc.Rta.PerformanceTest.Code
 					});
 				});
 
-			Enumerable.Range(0, (_testConfiguration.NumberOfAgents / 10) + 1)
+			Enumerable.Range(0, (_testConfiguration.NumberOfAgents/10) + 1)
 				.ForEach(team =>
 				{
 					data.Apply(new TeamConfigurable
 					{
 						Name = "team" + team,
-						Site = "site" + (team / 10)
+						Site = "site" + (team/10)
 					});
 				});
-
-			Enumerable.Range(0, _testConfiguration.NumberOfAgents)
-				.ForEach(roger =>
-				{
-					var name = "roger" + roger;
-					data.Person(name).Apply(new PersonPeriodConfigurable
-					{
-						StartDate = "2000-01-01 00:00".Utc(),
-						ExternalLogon = name,
-						ExternalLogonDataSourceId = 9,
-						Contract = "contract",
-						PartTimePercentage = "partTimePercentage",
-						ContractSchedule = "contractSchedule",
-						Team = "team" + (roger / 10)
-					});
-
-					data.Person(name).Apply(new ShiftConfigurable
-					{
-						Scenario = "Default"
-					}
-						.AddActivity("Phone", "2016-02-25 08:00".Utc(), "2016-02-25 17:00".Utc())
-						.AddActivity("Break", "2016-02-25 10:00".Utc(), "2016-02-25 10:15".Utc())
-						.AddActivity("Lunch", "2016-02-25 11:30".Utc(), "2016-02-25 12:00".Utc())
-						.AddActivity("Break", "2016-02-25 15:00".Utc(), "2016-02-25 15:15".Utc())
-						);
-
-					data.Person(name).Apply(new ShiftConfigurable
-					{
-						Scenario = "Default"
-					}
-						.AddActivity("Phone", "2016-02-26 08:00".Utc(), "2016-02-26 17:00".Utc())
-						.AddActivity("Break", "2016-02-26 10:00".Utc(), "2016-02-26 10:15".Utc())
-						.AddActivity("Lunch", "2016-02-26 11:30".Utc(), "2016-02-26 12:00".Utc())
-						.AddActivity("Break", "2016-02-26 15:00".Utc(), "2016-02-26 15:15".Utc())
-						);
-
-					data.Person(name).Apply(new ShiftConfigurable
-					{
-						Scenario = "Default"
-					}
-						.AddActivity("Phone", "2016-02-27 08:00".Utc(), "2016-02-27 17:00".Utc())
-						.AddActivity("Break", "2016-02-27 10:00".Utc(), "2016-02-27 10:15".Utc())
-						.AddActivity("Lunch", "2016-02-27 11:30".Utc(), "2016-02-27 12:00".Utc())
-						.AddActivity("Break", "2016-02-27 15:00".Utc(), "2016-02-27 15:15".Utc())
-						);
-				});
-
-			// to create/update any data that is periodically kept up to date
-			// like the rule mappings
-			_eventPublisher.Publish(new TenantMinuteTickEvent());
-			_eventPublisher.Publish(new TenantHourTickEvent());
 
 		}
 	}
--- Teleopti.Ccc.Rta.PerformanceTest/Code/TestConfiguration.cs
+++ Teleopti.Ccc.Rta.PerformanceTest/Code/TestConfiguration.cs
@@ -2,18 +2,27 @@ namespace Teleopti.Ccc.Rta.PerformanceTest.Code
 {
 	public class TestConfiguration
 	{
-		// change this whenever anything created by DataCreator changes
-		public static int HashValue = 2349875;
+		public static int HashValue =>
+			sourceId.GetHashCode() ^
+			dataSourceId ^
+			numberOfAgents ^
+			numberOfMappings ^ 
+			585182; // change this whenever anything created by DataCreator changes
 
-		public int NumberOfAgents = 2000;
-		public int NumberOfMappings= 10000;
+		private const int numberOfAgents = 2000;
+		private const int numberOfMappings = 10000;
+		private const string sourceId = "8";
+		private const int dataSourceId = 9;
+
+		public int NumberOfAgents => numberOfAgents;
+		public int NumberOfMappings => numberOfMappings;
 
 		// this is the switch id
 		// and is sent with the state
-		public string SourceId = "8";
+		public string SourceId => sourceId;
 
 		// this is the analytics data source id
 		// and is saved with the external logon in wfm
-		public int DataSourceId = 9;
+		public int DataSourceId => dataSourceId;
 	}
 }
