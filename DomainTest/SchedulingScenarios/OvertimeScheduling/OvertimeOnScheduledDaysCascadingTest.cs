c7606c5ebe7f38593d5413fb437231da3d527fa8
Claes Stenman <claes.stenman@teleopti.com>
2016-12-13 14:56:56 +0100

    #41318 ShouldNotPlaceOverTimeShiftDueToNoUnderstaffingOnPrimarySkill (scheduled days)

--- DomainTest/SchedulingScenarios/OvertimeScheduling/OvertimeOnScheduledDaysCascadingTest.cs
+++ DomainTest/SchedulingScenarios/OvertimeScheduling/OvertimeOnScheduledDaysCascadingTest.cs
@@ -3,14 +3,18 @@ using System.Drawing;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.Forecasting;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.Overtime;
 using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
-using Teleopti.Ccc.Domain.Scheduling.TimeLayer; 
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
+using Teleopti.Ccc.IocCommon.Toggle;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.IoC;
@@ -23,6 +27,7 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.OvertimeScheduling
 	{
 		public ScheduleOvertime Target;
 		public Func<ISchedulerStateHolder> SchedulerStateHolderFrom;
+		public FakeToggleManager ToggleManager;
 
 		[Test]
 		public void ShouldNotPlaceOvertimeShiftDueToNoUnderstaffingAfterShoveling()
@@ -78,6 +83,51 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.OvertimeScheduling
 
 			stateHolder.Schedules[agentThatShouldNotGetOverTime].ScheduledDay(dateOnly).PersonAssignment(true).OvertimeActivities()
 				.Should().Be.Empty();
+		}
+
+		[Ignore("41318")]
+		[TestCase(true, ExpectedResult = true)]
+		[TestCase(false, ExpectedResult = false)]
+		public bool ShouldNotPlaceOverTimeShiftDueToNoUnderstaffingOnPrimarySkill(bool resourcePlannerCascadingScheduleOvertimeOnPrimary41318)
+		{
+			if (resourcePlannerCascadingScheduleOvertimeOnPrimary41318)
+				ToggleManager.Enable(Toggles.ResourcePlanner_CascadingScheduleOvertimeOnPrimary_41318);
+			else
+				ToggleManager.Disable(Toggles.ResourcePlanner_CascadingScheduleOvertimeOnPrimary_41318);
+
+			var scenario = new Scenario("_");
+			var activity = new Activity("_").WithId();
+			var dateOnly = DateOnly.Today;
+			var definitionSet = new MultiplicatorDefinitionSet("overtime", MultiplicatorType.Overtime);
+			var contract = new Contract("_") { WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(10), TimeSpan.FromHours(83), TimeSpan.FromHours(1), TimeSpan.FromHours(16)) };
+			contract.AddMultiplicatorDefinitionSetCollection(definitionSet);
+			var skillA = new Skill("A", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			var skillB = new Skill("B", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			skillA.SetCascadingIndex(1);
+			skillB.SetCascadingIndex(2);
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillA, new TimePeriod(8, 0, 16, 0));
+			WorkloadFactory.CreateWorkloadWithOpenHours(skillB, new TimePeriod(8, 0, 16, 0));
+			var skillDayA = skillA.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var skillDayB = skillB.CreateSkillDayWithDemand(scenario, dateOnly, 10);
+			var agentAandB = new Person().WithId().InTimeZone(TimeZoneInfo.Utc);
+			agentAandB.AddPeriodWithSkills(new PersonPeriod(dateOnly, new PersonContract(contract, new PartTimePercentage("_"), new ContractSchedule("_")), new Team { Site = new Site("_") }), new[] { skillA, skillB });
+			agentAandB.AddSchedulePeriod(new SchedulePeriod(dateOnly, SchedulePeriodType.Day, 1));
+			agentAandB.AddSchedulePeriod(new SchedulePeriod(dateOnly, SchedulePeriodType.Day, 1));
+			var assAandB = new PersonAssignment(agentAandB, scenario, dateOnly);
+			assAandB.AddActivity(activity, new TimePeriod(8, 0, 15, 0));
+			var stateHolder = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(dateOnly, dateOnly), new[] { agentAandB }, new[] { assAandB }, new[] { skillDayA, skillDayB });
+			var overtimePreference = new OvertimePreferences
+			{
+				OvertimeType = definitionSet,
+				ScheduleTag = new ScheduleTag(),
+				SelectedSpecificTimePeriod = new TimePeriod(15, 0, 16, 0),
+				SelectedTimePeriod = new TimePeriod(1, 0, 1, 0),
+				SkillActivity = activity
+			};
+
+			Target.Execute(overtimePreference, new NoSchedulingProgress(), new[] { stateHolder.Schedules[agentAandB].ScheduledDay(dateOnly) });
+
+			return stateHolder.Schedules[agentAandB].ScheduledDay(dateOnly).PersonAssignment(true).OvertimeActivities().IsEmpty();
 		}
 	}
 }
