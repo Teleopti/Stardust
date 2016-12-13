8cd630951efd1d24c512edfb94bbb8f210222293
Justin! <justin@teleopti.com>
2016-12-13 17:34:19 +0100

    Backed out changeset: TEMP

--- Infrastructure/ApplicationLayer/HangfireEventProcessor.cs
+++ Infrastructure/ApplicationLayer/HangfireEventProcessor.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
-using Teleopti.Ccc.Domain.Config;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
@@ -14,18 +11,15 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 		private readonly IJsonEventDeserializer _deserializer;
 		private readonly ResolveEventHandlers _resolver;
 		private readonly CommonEventProcessor _processor;
-		private readonly IConfigReader _config;
 
 		public HangfireEventProcessor(
 			IJsonEventDeserializer deserializer,
 			ResolveEventHandlers resolver,
-			CommonEventProcessor processor,
-			IConfigReader config)
+			CommonEventProcessor processor)
 		{
 			_deserializer = deserializer;
 			_resolver = resolver;
 			_processor = processor;
-			_config = config;
 		}
 
 		public void Process(string displayName, string tenant, string eventType, string serializedEvent, string handlerType)
@@ -42,24 +36,7 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 
 			var publishTo = handlers.Single(o => o == handlerT);
 
-			runFor(tenant, @event, publishTo, TimeSpan.FromSeconds(_config.ReadValue("HangfireJobTimeoutSeconds", 15 * 60)));
-		}
-
-		private void runFor(string tenant, IEvent @event, Type publishTo, TimeSpan timeout)
-		{
-			var task = Task.Run(() =>
-			{
-				_processor.Process(tenant, @event, publishTo);
-			});
-
-			var delay = Task.Delay(timeout);
-			var timeoutTask = Task.WhenAny(task, delay);
-			if (delay == timeoutTask.GetAwaiter().GetResult())
-				throw new TimeoutException();
-			timeoutTask.Result.GetAwaiter().GetResult();
-
-			//if (!task.Wait(timeout))
-			//	throw new TimeoutException();
+			_processor.Process(tenant, @event, publishTo);
 		}
 
 		public void Process(string tenant, IEvent @event, Type handlerType)
