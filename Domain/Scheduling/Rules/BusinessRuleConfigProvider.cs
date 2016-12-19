99abb9841ad57eec3247320cedc8c3d5a9f2e0d2
junwen.luo <none@none>
2016-12-19 10:27:21 +0800

    PBI #40770 use inheritance to reuse the method GetDefaultConfigForShiftTradeRequest

--- Domain/Scheduling/Rules/BusinessRuleConfigProvider.cs
+++ Domain/Scheduling/Rules/BusinessRuleConfigProvider.cs
@@ -40,37 +40,12 @@ namespace Teleopti.Ccc.Domain.Scheduling.Rules
 		}
 	}
 
-	public class BusinessRuleConfigProvider25635ToggleOff : IBusinessRuleConfigProvider
+	public class BusinessRuleConfigProvider25635ToggleOff : BusinessRuleConfigProvider
 	{
-		private readonly IBusinessRuleProvider _businessRuleProvider;
-		private readonly ISchedulingResultStateHolder _schedulingResultStateHolder;
-
-		public BusinessRuleConfigProvider25635ToggleOff(IBusinessRuleProvider businessRuleProvider, ISchedulingResultStateHolder schedulingResultStateHolder)
+		public BusinessRuleConfigProvider25635ToggleOff(IBusinessRuleProvider businessRuleProvider,
+			ISchedulingResultStateHolder schedulingResultStateHolder) : base(businessRuleProvider, schedulingResultStateHolder)
 		{
-			_businessRuleProvider = businessRuleProvider;
-			_schedulingResultStateHolder = schedulingResultStateHolder;
-			_schedulingResultStateHolder.UseMinWeekWorkTime = false;
-		}
-
-		public IEnumerable<IShiftTradeBusinessRuleConfig> GetDefaultConfigForShiftTradeRequest()
-		{
-			var result = new List<IShiftTradeBusinessRuleConfig>();
-
-			var allRules = _businessRuleProvider.GetBusinessRulesForShiftTradeRequest(_schedulingResultStateHolder, true);
-			if (!allRules.Any()) return result;
-
-			// Rules "removed" from NewBusinessRuleCollection will not be actual removed,
-			// Only the "HaltModify" flag will be set to false if not IsMandatory
-			var configurableRules = allRules.Where(x => x.Configurable && (x.IsMandatory || x.HaltModify));
-			result.AddRange(configurableRules.Select(x => new ShiftTradeBusinessRuleConfig
-			{
-				BusinessRuleType = x.GetType().FullName,
-				FriendlyName = x.Description,
-				Enabled = true,
-				HandleOptionOnFailed = RequestHandleOption.Pending
-			}));
-
-			return result;
+			schedulingResultStateHolder.UseMinWeekWorkTime = false;
 		}
 	}
 }
