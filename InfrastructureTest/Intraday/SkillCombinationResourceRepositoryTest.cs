dc581df5e2884bb3eceb4c37c7c535cdde72b542
Ola <none@none>
2016-12-20 15:19:12 +0100

    fix build

--- InfrastructureTest/Intraday/SkillCombinationResourceRepositoryTest.cs
+++ InfrastructureTest/Intraday/SkillCombinationResourceRepositoryTest.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.InfrastructureTest.Intraday
 {
 	[TestFixture]
 	[UnitOfWorkTest]
+    [AllTogglesOn]
 	public class SkillCombinationResourceRepositoryTest
 	{
 		public ISkillCombinationResourceRepository Target;
