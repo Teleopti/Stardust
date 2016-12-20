f722e2bb9a7fe66b9ad2245af29825ed7fee5f8e
Claes Stenman <claes.stenman@teleopti.com>
2016-12-20 08:46:32 +0100

    #41857 ShouldCalculateResourceBasedOnPrimarSkill ignore

--- Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/JobSchedule/PBI41857.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/JobSchedule/PBI41857.cs
@@ -28,6 +28,60 @@ namespace Teleopti.Analytics.Etl.CommonTest.JobSchedule
 	public class PBI41857
 	{
 		[Test, Ignore("PBI41857")]
+		public void ShouldCalculateResourceBasedOnPrimarSkill()
+		{
+			var date = new DateOnly(2016, 12, 20);
+			var dateAsUtc = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, DateTimeKind.Utc);
+			var dateList = new JobMultipleDate(TimeZoneInfo.FindSystemTimeZoneById("UTC"));
+			dateList.Add(date.Date, date.Date, JobCategoryType.Schedule);
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var primarySkill = new Skill("primary", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			primarySkill.SetCascadingIndex(1);
+			var secondarySkill = new Skill("secondary", "_", Color.Empty, 15, new SkillTypePhone(new Description(), ForecastSource.InboundTelephony)) { Activity = activity, TimeZone = TimeZoneInfo.Utc }.WithId();
+			secondarySkill.SetCascadingIndex(2);
+			WorkloadFactory.CreateWorkloadWithFullOpenHours(primarySkill);
+			WorkloadFactory.CreateWorkloadWithFullOpenHours(secondarySkill);
+			var skillDays = new Dictionary<ISkill, IEnumerable<ISkillDay>>
+			{
+				[primarySkill] = new[] { primarySkill.CreateSkillDayWithDemand(scenario, date, 1) },
+				[secondarySkill] = new[] { secondarySkill.CreateSkillDayWithDemand(scenario, date, 1) }
+			};
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc).KnowsSkill(primarySkill, secondarySkill).WithId();
+			var ass = new PersonAssignment(agent, scenario, date);
+			ass.AddActivity(activity, new TimePeriod(0, 24));
+			var scheduleDictionary = ScheduleDictionaryForTest.WithScheduleDataForManyPeople(scenario, new DateTimePeriod(dateAsUtc, dateAsUtc.AddDays(1)), ass);
+			var raptorRep = new RaptorRepositoryForTest();
+			raptorRep.SetLoadSkillWithSkillDays(new[] { primarySkill, secondarySkill });
+			raptorRep.SetLoadScenario(scenario);
+			raptorRep.SetLoadSkillDays(skillDays);
+			raptorRep.SetLoadSchedule(scheduleDictionary);
+			var jobHelper = new JobHelperForTest(raptorRep, null);
+			var containerHolder = new JobParametersFactory.FakeContainerHolder();
+			var jobParameters = new JobParameters(dateList, 0, "UTC", 15, "", "", null, containerHolder, false) { Helper = jobHelper };
+			var target = new StageScheduleForecastSkillJobStep(jobParameters) { ClearDataTablesAfterRun = false };
+
+			target.Run(new List<IJobStep>(), null, null, false);
+
+			var result = target.BulkInsertDataTable1;
+			const double expectedScheduledOnPrimary = 1;
+			const double expectedScheduledOnSecondary = 0;
+			var rowsOnDate = result.Rows.Cast<DataRow>().Where(x => (DateTime)x["date"] == dateAsUtc);
+			var primaryRows = rowsOnDate.Where(x => (Guid)x["skill_code"] == primarySkill.Id.Value);
+			var secondaryRows = rowsOnDate.Where(x => (Guid)x["skill_code"] == secondarySkill.Id.Value);
+
+			foreach (var primaryRow in primaryRows)
+			{
+				primaryRow["scheduled_resources"].Should().Be.EqualTo(expectedScheduledOnPrimary);
+			}
+
+			foreach (var secondaryRow in secondaryRows)
+			{
+				secondaryRow["scheduled_resources"].Should().Be.EqualTo(expectedScheduledOnSecondary);
+			}
+		}
+
+		[Test, Ignore("PBI41857")]
 		public void ShouldShovel()
 		{
 			var date = new DateOnly(2016, 12, 20);
