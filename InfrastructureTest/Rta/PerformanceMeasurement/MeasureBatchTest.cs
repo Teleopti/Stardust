446ae4d1dd79c38770f25c2ca68b40efb153bceb
amandaj <none@none>
2016-12-29 13:44:59 +0100

    ops, forgot to remove this one

--- InfrastructureTest/Rta/PerformanceMeasurement/MeasureBatchTest.cs
+++ InfrastructureTest/Rta/PerformanceMeasurement/MeasureBatchTest.cs
@@ -8,43 +8,13 @@ using Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.FeatureFlags;
-using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeRepositories.Rta;
 using Teleopti.Ccc.TestCommon.IoC;
-using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.InfrastructureTest.Rta.PerformanceMeasurement
 {
-	[TestFixture]
-	[InfrastructureTest]
-	[Explicit]
-	[Category("LongRunning")]
-	[Toggle(Toggles.AbsenceRequests_Intraday_UseCascading_41969)]
-	public class Measure : PerformanceMeasurementTestBase, ISetup
-	{
-		public UpdateStaffingLevelReadModel UpdateStaffingLevel;
-		public MutableNow Now;
-
-		public void Setup(ISystem system, IIocConfiguration configuration)
-		{
-		}
-
-		public override void OneTimeSetUp()
-		{
-			Now.Is("2016-12-28");
-			var now = Now.UtcDateTime().Date;
-			var period = new DateTimePeriod(now.AddDays(-1), now.AddDays(1));
-			UpdateStaffingLevel.Update(period);
-		}
-
-		[Test]
-		public void DoTheThing()
-		{
-		}
-	}
-
 	[TestFixture]
 	[InfrastructureTest]
 	[Toggle(Toggles.RTA_FasterUpdateOfScheduleChanges_40536)]
