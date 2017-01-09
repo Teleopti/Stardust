811ce15bd0b927d906b53aef2cae51a850cb99ea
Alina Morariu <none@none>
2017-01-09 17:19:40 +0100

    #42334 a little more refact for controller builder

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta.controller.fortest.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta.controller.fortest.js
@@ -7,28 +7,33 @@
 	ControllerBuilder.$inject = ['$controller', '$interval', '$httpBackend', '$rootScope'];
 
 	function ControllerBuilder($controller, $interval, $httpBackend, $rootScope) {
-
-		var service = this;
-
 		var controllerName = "hejsan";
 		var scope;
 
-		this.setup = function(name) {
+		var service = {
+			setup: setup,
+			createController: createController
+		};
+
+		return service;
+		/////////////////////
+
+		function setup(name) {
 			controllerName = name;
 			scope = $rootScope.$new();
 			return scope;
 		};
 
-		this.createController = function() {
+		function createController() {
 
 			var vm = $controller(controllerName, {
 				$scope: scope
 			});
+
 			scope.$digest();
 			safeBackendFlush();
 
 			var callbacks = {
-
 				apply: function(apply) {
 					if (typeof apply === "function") {
 						apply();
@@ -49,19 +54,17 @@
 			};
 
 			return callbacks;
+
+			function safeBackendFlush() {
+				try { // the internal mock will throw if no requests were made
+					$httpBackend.flush();
+				} catch (e) {
+					if (e.message.includes("No pending request to flush !"))
+						return;
+					console.error(e.message)
+				}
+			};
 		}
-
-		var safeBackendFlush = function() {
-			try { // the internal mock will throw if no requests were made
-				$httpBackend.flush();
-			} catch (e) {
-				if (e.message.includes("No pending request to flush !"))
-					return;
-				console.error(e.message)
-			}
-		};
-
-		return this;
 
 	};
 })();
