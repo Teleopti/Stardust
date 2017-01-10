753d699d8785ca891b2a5fe6aa19caf4ffa1bd8a
Alina Morariu <none@none>
2017-01-10 11:24:53 +0100

    #42334 refact agents filter

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/refact/agentsrefact/rta.agents.filters.refact.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/refact/agentsrefact/rta.agents.filters.refact.js
@@ -1,36 +1,39 @@
 ﻿
 (function() {
 	'use strict';
-	var rtaAgentsFilters = angular.module('wfm.rta');
+	angular
+		.module('wfm.rta')
+		.filter('agentFilter', agentFilter);
 
-	rtaAgentsFilters.filter('agentFilter', [
-		function() {
-			function escapeRegExp(str) {
-				return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-			}
-			return function(data, input, includes) {
-				var matchedItems = data.slice();
-				var keywords = input.split(' ');
-				keywords.forEach(function(keyword) {
-					for (var i = 0; i < data.length; i++) {
-						var item = data[i];
-						var matched = false;
-						for (var property in item) {
-							if (includes !== undefined && includes.indexOf(property) === -1)
-								continue;
-							if (item[property] !== null && item[property] !== undefined)
-								matched = matched || (item[property].toString().search(new RegExp(escapeRegExp(keyword), "i")) !== -1 ? true : false);
-						}
-						if (matched === false) {
-							var index = matchedItems.indexOf(item);
-							if (index > -1) {
-								matchedItems.splice(index, 1);
-							}
+	function agentFilter() {
+		var filter = this;
+
+		function escapeRegExp(str) {
+			return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+		}
+		return function(data, input, includes) {
+			var matchedItems = data.slice();
+			var keywords = input.split(' ');
+			keywords.forEach(function(keyword) {
+				for (var i = 0; i < data.length; i++) {
+					var item = data[i];
+					var matched = false;
+					for (var property in item) {
+						if (includes !== undefined && includes.indexOf(property) === -1)
+							continue;
+						if (item[property] !== null && item[property] !== undefined)
+							matched = matched || (item[property].toString().search(new RegExp(escapeRegExp(keyword), "i")) !== -1 ? true : false);
+					}
+					if (matched === false) {
+						var index = matchedItems.indexOf(item);
+						if (index > -1) {
+							matchedItems.splice(index, 1);
 						}
 					}
-				});
-				return matchedItems;
-			};
-		}
-	]);
+				}
+			});
+			return matchedItems;
+		};
+		return filter;
+	};
 })();
