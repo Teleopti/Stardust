5b2c92c780e2972bd58aac28d9e90a767ca05959
Alina Morariu <none@none>
2017-01-10 13:00:58 +0100

    #42334 fix other issues with ui-grid

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/refact/agentsrefact/rta.agents.grid.service.refact.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/refact/agentsrefact/rta.agents.grid.service.refact.js
@@ -2,11 +2,11 @@
 	'use strict';
 	angular
 		.module('wfm.rta')
-		.factory('RtaGridServiceRefact', RtaGridServiceRefact);
+		.factory('RtaGridServiceRefact', RtaGridService);
 
-	RtaGridServiceRefact.$inject = ['Toggle', 'uiGridConstants', 'RtaLocaleLanguageSortingService'];
+	RtaGridService.$inject = ['Toggle', 'uiGridConstants', 'RtaLocaleLanguageSortingService'];
 
-	function RtaGridServiceRefact(toggleService, uiGridConstants, RtaLocaleLanguageSortingService) {
+	function RtaGridService(toggleService, uiGridConstants, RtaLocaleLanguageSortingService) {
 
 		var service = {
 			makeAllGrid: makeAllGrid,
@@ -25,32 +25,11 @@
 		};
 
 		function makeGridOptions(alarmOnly) {
-
-			var service = {
-				rowTemplate: rowTemplate,
-				columnDefs: columnDefs,
-				enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
-				enableVerticalScrollbar: uiGridConstants.scrollbars.NEVER,
-				enableGridMenu: true,
-				enableColumnMenus: true,
-				enableColumnResizing: true,
-				enableSorting: !alarmOnly
-			};
-
-			return service;
-			//////////////////////////////////
-
-
-			//if (toggleService.RTA_AdherenceDetails_34267)
-			//	rowTemplate = 'app/rta/agents/rta-agents-rowtemplate-AdherenceDetails_34267.html';
-			var rowTemplate = 'app/rta/refact/agentsrefact/rta-agents-rowtemplate.refact.html';
-			if (toggleService.RTA_SeeAllOutOfAdherencesToday_39146)
-				rowTemplate = 'app/rta/refact/agentsrefact/rta-agents-rowtemplate-AllOutOfAdherences_39146.refact.html';
 			var coloredCellTemplate = '<div class="ui-grid-cell-contents">{{COL_FIELD}}</div>';
+
 			var alarmCellTemplate = '<div class="ui-grid-cell-contents"><div class="label rta-label" ng-attr-style="font-size: 14px; color: white; background-color: {{grid.appScope.vm.hexToRgb(row.entity.Color)}}">{{COL_FIELD}}</div></div>';
 			var headerCellTemplate = 'app/rta/agents/rta-agents-headercelltemplate.html';
 
-			var columnDefs = [];
 			var name = {
 				displayName: 'Name',
 				field: 'Name',
@@ -62,7 +41,6 @@
 				},
 				sortingAlgorithm: RtaLocaleLanguageSortingService.sort
 			};
-
 			var siteAndTeam = {
 				displayName: 'Site/Team',
 				field: 'SiteAndTeamName',
@@ -108,7 +86,7 @@
 				displayName: 'Shift',
 				field: 'Shift',
 				enableColumnMenu: false,
-				headerCellTemplate: 'app/rta/agents/rta-agents-headershiftcelltemplate.html',
+				headerCellTemplate: 'app/rta/refact/agentsrefact/rta-agents-headershiftcelltemplate.refact.html',
 				cellClass: 'shift-class',
 				cellTemplate: 'app/rta/agents/rta-agents-shiftcelltemplate.html',
 				headerCellFilter: 'translate',
@@ -117,6 +95,15 @@
 				enableHiding: false
 			};
 
+			var columnDefs = [];
+
+			//if (toggleService.RTA_AdherenceDetails_34267)
+			//	rowTemplate = 'app/rta/agents/rta-agents-rowtemplate-AdherenceDetails_34267.html';
+
+			var rowTemplate = 'app/rta/refact/agentsrefact/rta-agents-rowtemplate.refact.html';
+			if (toggleService.RTA_SeeAllOutOfAdherencesToday_39146)
+				rowTemplate = 'app/rta/refact/agentsrefact/rta-agents-rowtemplate-AllOutOfAdherences_39146.refact.html'
+
 			columnDefs.push(name);
 			columnDefs.push(siteAndTeam);
 			columnDefs.push(shift);
@@ -124,6 +111,19 @@
 			columnDefs.push(timeOutOfAdherence);
 			columnDefs.push(timeInAlarm);
 			columnDefs.push(state);
+
+
+			return {
+				rowTemplate: rowTemplate,
+				columnDefs: columnDefs,
+				enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
+				enableVerticalScrollbar: uiGridConstants.scrollbars.NEVER,
+				enableGridMenu: true,
+				enableColumnMenus: true,
+				enableColumnResizing: true,
+				enableSorting: !alarmOnly
+			};
 		};
+
 	};
 })();
