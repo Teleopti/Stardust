c9e0d475c188669560be20e46aa1164688a70dc6
johan Damm <none@none>
2017-01-13 09:09:51 +0100

    #42532 WIP

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/staffing/staffing.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/staffing/staffing.controller.js
@@ -9,17 +9,38 @@
     function staffingController(staffingService) {
         var vm = this;
         vm.staffingArea = 0;
-        vm.staffing = monitorskillstaffing.get();
-        vm.getStaffingForSkillArea = getStaffingForSkillArea(areaId);
+        var getSkills = getSkillStaffing;
+        var getSkillsForArea = getSkillAreaStaffing
 
-
+        var query = getSkills("F08D75B3-FDB4-484A-AE4C-9F0800E2F753");
+        query.$promise.then(function(result){
+            console.log(result)
+        })
         ////////////////
 
-        getSkillAreaStaffing = function (areaId) {
+        function getSkillAreaStaffing(areaId) {
             return staffingService.getSkillAreaStaffing.get(areaId);
         }
-        getSkillStaffing = function (skillId) {
+        function getSkillStaffing(skillId) {
             return staffingService.getSkillAreaStaffing.get(skillId)
         }
+
+        var generateChart = function () {
+            c3.generate({
+                bindto: '#staffingChart',
+                data: {
+                    columns: [
+                        ['data1', ],
+                    ],
+                    selection: {
+                        enabled: true,
+                    },
+                },
+                zoom: {
+                    enabled: true,
+                },
+            });
+
+        }();
     }
 })();
