2e705caada5c13b2ee20a882e6216e044c8ed954
Erik Sundberg <erik.sundberg@teleopti.com>
2017-01-13 12:48:37 +0100

    Skip localhost restriction on RTA tool, superuser seems enough

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaToolController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaToolController.cs
@@ -5,7 +5,6 @@ using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
-using Teleopti.Ccc.Web.Areas.PerformanceTool.Controllers;
 using Teleopti.Ccc.Web.Filters;
 using Teleopti.Interfaces.Domain;
 
@@ -31,7 +30,7 @@ namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 			_now = now;
 		}
 
-		[LocalHostAccess, AnalyticsUnitOfWork, UnitOfWork, HttpGet, Route("RtaTool/Agents/For")]
+		[AnalyticsUnitOfWork, UnitOfWork, HttpGet, Route("RtaTool/Agents/For")]
 		public virtual IHttpActionResult GetAgents()
 		{
 			var today = new DateOnly(_now.UtcDateTime());
@@ -42,20 +41,20 @@ namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 				let period = p.Period(today)
 				let logon = period.ExternalLogOnCollection.OrderBy(x => x.DataSourceId).FirstOrDefault()
 				let external = logon?.AcdLogOnOriginalId
-				let dataSource = logon?.DataSourceId
-				let dataScoure2 = dataSources.FirstOrDefault(x => x.Value == dataSource).Key
+				let d = logon?.DataSourceId
+				let datasource = dataSources.FirstOrDefault(x => x.Value == d).Key
 				where logon != null
 				select new
 				{
 					Name = p.Name.FirstName + " " + p.Name.LastName,
 					UserCode = external,
-					DataSource = dataScoure2
+					DataSource = datasource
 				})
 				.ToArray();
 			return Ok(result);
 		}
 
-		[LocalHostAccess, UnitOfWork, HttpGet, Route("RtaTool/PhoneStates/For")]
+		[UnitOfWork, HttpGet, Route("RtaTool/PhoneStates/For")]
 		public virtual IHttpActionResult GetPhoneStates()
 		{
 			return Ok(_stateGroups.LoadAllCompleteGraph()
