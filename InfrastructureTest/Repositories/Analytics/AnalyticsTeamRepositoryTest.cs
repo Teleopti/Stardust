92f01d2f84fdc205e61ab804caffd12bbda346ae
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-01-18 15:59:03 +0100

    #42656 Changed a flaky test to be less error prone

--- InfrastructureTest/Repositories/Analytics/AnalyticsTeamRepositoryTest.cs
+++ InfrastructureTest/Repositories/Analytics/AnalyticsTeamRepositoryTest.cs
@@ -7,18 +7,18 @@ using Teleopti.Ccc.TestCommon.TestData.Analytics;
 using Teleopti.Ccc.TestCommon.TestData.Core;
 using BusinessUnit = Teleopti.Ccc.TestCommon.TestData.Analytics.BusinessUnit;
 using Teleopti.Ccc.Domain.Repositories;
-using Teleopti.Interfaces.Infrastructure;
+using Teleopti.Ccc.Domain.UnitOfWork;
 
 namespace Teleopti.Ccc.InfrastructureTest.Repositories.Analytics
 {
 	[TestFixture]
 	[Category("BucketB")]
-	[AnalyticsUnitOfWorkTest]
+	[AnalyticsDatabaseTest]
 	public class AnalyticsTeamRepositoryTest
 	{
-		public ICurrentAnalyticsUnitOfWork UnitOfWork;
 		public IAnalyticsPersonPeriodRepository AnalyticsPersonPeriodRepository;
 		public IAnalyticsTeamRepository Target;
+		public WithAnalyticsUnitOfWork WithAnalyticsUnitOfWork;
 
 		private int siteId;
 		BusinessUnit businessUnit;
@@ -39,32 +39,34 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories.Analytics
 		[Test]
 		public void GetOrCreate_ShouldReturnTeamId()
 		{
-			siteId = AnalyticsPersonPeriodRepository.GetOrCreateSite(Guid.NewGuid(), "Site name 1", businessUnit.BusinessUnitId);
+			siteId = WithAnalyticsUnitOfWork.Get(() => AnalyticsPersonPeriodRepository.GetOrCreateSite(Guid.NewGuid(), "Site name 1", businessUnit.BusinessUnitId));
 
-			var teamId = Target.GetOrCreate(Guid.NewGuid(), siteId, "Team Name", businessUnit.BusinessUnitId);
+			var teamId = WithAnalyticsUnitOfWork.Get(() => Target.GetOrCreate(Guid.NewGuid(), siteId, "Team Name", businessUnit.BusinessUnitId));
 			teamId.Should().Be.GreaterThan(0);
 		}
 
 		[Test]
 		public void GetAll_ShouldReturnAllTeams()
 		{
-			siteId = AnalyticsPersonPeriodRepository.GetOrCreateSite(Guid.NewGuid(), "Site name 1", businessUnit.BusinessUnitId);
-
 			var teamGuid1 = Guid.NewGuid();
 			var teamGuid2 = Guid.NewGuid();
-			var teamName1 = "Team1";
-			var teamName2 = "Team2";
-			var teamId1 = Target.GetOrCreate(teamGuid1, siteId, teamName1, businessUnit.BusinessUnitId);
-			var teamId2 = Target.GetOrCreate(teamGuid2, siteId, teamName2, businessUnit.BusinessUnitId);
-			var teams = Target.GetTeams();
+			var teamId1 = 0;
+			var teamId2 = 0;
+			WithAnalyticsUnitOfWork.Do(() =>
+			{
+				siteId = AnalyticsPersonPeriodRepository.GetOrCreateSite(Guid.NewGuid(), "Site name 1",
+					businessUnit.BusinessUnitId);
+				teamId1 = Target.GetOrCreate(teamGuid1, siteId, "Team1", businessUnit.BusinessUnitId);
+				teamId2 = Target.GetOrCreate(teamGuid2, siteId, "Team2", businessUnit.BusinessUnitId);
+			});
+
+
+			var teams = WithAnalyticsUnitOfWork.Get(() => Target.GetTeams());
 
 			teams.Should().Not.Be.Empty();
-			var team1 = teams.First();
-			team1.TeamCode.Should().Be.EqualTo(teamGuid1);
-			team1.TeamId.Should().Be.EqualTo(teamId1);
-			var team2 = teams.Last();
-			team2.TeamCode.Should().Be.EqualTo(teamGuid2);
-			team2.TeamId.Should().Be.EqualTo(teamId2);
+			teams.Count.Should().Be.EqualTo(2);
+			teams.FirstOrDefault(x => x.TeamCode == teamGuid1 && x.TeamId == teamId1).Should().Not.Be.Null();
+			teams.FirstOrDefault(x => x.TeamCode == teamGuid2 && x.TeamId == teamId2).Should().Not.Be.Null();
 		}
 	}
 }
