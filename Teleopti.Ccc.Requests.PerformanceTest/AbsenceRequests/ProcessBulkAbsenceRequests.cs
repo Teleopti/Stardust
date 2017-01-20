e29abb3aed8f9730bb58de0aba0afab45037bf85
junwen.luo <none@none>
2017-01-20 15:38:37 +0800

    PBI #39868 recover test case ShouldProcessMultipleAbsenceRequestsWithIntradayValidator but use other people id

--- Teleopti.Ccc.Requests.PerformanceTest/AbsenceRequests/ProcessBulkAbsenceRequests.cs
+++ Teleopti.Ccc.Requests.PerformanceTest/AbsenceRequests/ProcessBulkAbsenceRequests.cs
@@ -52,31 +52,72 @@ namespace Teleopti.Ccc.Requests.PerformanceTest.AbsenceRequests
 		[Test]
 		public void ShouldProcessMultipleAbsenceRequests()
 		{
-			var personReqs = processMultipleAbsenceRequests(false);
+			var expectedStatuses = new Dictionary<Guid, int>
+			{
+				// people from team 'FL_Sup_Sun7_71631' and 'FL_Sup_Sun6_00333'
+				//DO NOT CHANGE THE ORDER OF THE GUIDS!
+				[new Guid("55E3A133-6305-4C9A-8AEA-A1410113C47D")] = 2,
+				[new Guid("87E6CEF0-388A-4365-94FA-A1410113C47D")] = 4,
+				[new Guid("391DB822-3936-4C4D-9634-A1410113C47D")] = 4,
+				[new Guid("47721DE4-A0CB-45EE-A123-A1410113C47D")] = 4,
+				[new Guid("DCF2EA04-3031-4436-A229-A1410113C47D")] = 4,
+				[new Guid("4886AEDD-E30F-416C-B5E8-A1410113C47D")] = 4,
+				[new Guid("C922055A-B4D0-4C06-B9AD-A1410113C47D")] = 4,
+				[new Guid("CADD42C6-5419-48DD-8514-A25B009AD59D")] = 4,
+				[new Guid("A35F2179-9A4B-40C9-BF7B-A27400997C79")] = 4,
+				[new Guid("42394840-F905-4B22-915F-A332008A5288")] = 4,
+				[new Guid("F25262A1-E3C3-4A54-B202-A33200B2E94F")] = 4,
+				[new Guid("AE476FA3-7A6C-4948-89C2-A3BF00D0577C")] = 2,
+				[new Guid("1EBFEE75-CE35-40FB-975E-A3BF00D0577C")] = 4,
+				[new Guid("E4C7A7A7-8D2C-4591-ACA4-A53D00F82C88")] = 2,
+				[new Guid("12728761-0ED6-422B-B2B5-A5E001051F1E")] = 2,
+				[new Guid("6069902E-5760-4DF4-B733-A5E00105B099")] = 2,
+				[new Guid("BF50C741-A780-4930-A64B-A5E00105F325")] = 2
+			};
 
-			var expectedStatuses = new Dictionary<Guid, int>();
+			var personReqs = processMultipleAbsenceRequests(expectedStatuses.Keys, false);
+
+			requestStatusesAssert(personReqs, expectedStatuses);
+		}
+
+		[Test]
+		public void ShouldProcessMultipleAbsenceRequestsWithIntradayValidator()
+		{
+			var expectedStatuses = new Dictionary<Guid, int>
+			{
+				// people from team 'FL_SaveDesk_Far4_00282' and 'OM_D_SME_MBS_45428'
+				[new Guid("889F7A33-BE61-4FA7-BFAD-A14100FFA30F")] = 2,
+				[new Guid("7843C186-3DB9-4D91-9F34-A14100FFA313")] = 4,
+				[new Guid("51552353-F770-4340-88FA-A1CE00B56199")] = 2,
+				[new Guid("C6232E9E-9234-4983-8055-A1AF0090A8D7")] = 4,
+				[new Guid("DD0FE7BF-B56A-4309-BED2-A51A010E01BB")] = 4,
+				[new Guid("189295E6-6AFF-41C5-87CF-A49C01026BD0")] = 2,
+				[new Guid("1CFD93FB-7ABF-4839-BBFC-A4C3009216A2")] = 4,
+				[new Guid("C89644EF-371F-45BB-9F01-A47500C13EB4")] = 2,
+				[new Guid("B21D5EA1-7CD9-4237-B451-A65900EB4857")] = 2,
+				[new Guid("3606EB98-53E3-4323-BC47-A3A1009DE023")] = 2,
+				[new Guid("35E1A58D-97CB-4117-B9C3-A141010AA74C")] = 2,
+				[new Guid("B94F331B-56C3-4667-85BB-A14100FFA31D")] = 2,
+				[new Guid("144F887C-5AFF-4CC1-93F4-A65900F1C833")] = 2,
+				[new Guid("4AA6D8E5-B679-4F60-98F8-A65900F41B8B")] = 2,
+				[new Guid("1B53C821-2837-4893-8ACC-A65900F41C93")] = 2,
+				[new Guid("4FD5816A-CB41-418B-837E-A65900F0F1FA")] = 2,
+				[new Guid("32E2CCEF-7D00-45DC-8E0B-A65900F2EC3F")] = 2
+			};
+
+			var personReqs = processMultipleAbsenceRequests(expectedStatuses.Keys, true, RequestValidatorsFlag.IntradayValidator);
+
+			requestStatusesAssert(personReqs, expectedStatuses);
+		}
+
+		private void requestStatusesAssert(List<IPersonRequest> personRequests, Dictionary<Guid, int> expectedStatuses)
+		{
 			var resultStatuses = new Dictionary<Guid, int>();
-
-			expectedStatuses.Add(new Guid("BF50C741-A780-4930-A64B-A5E00105F325"), 2);
-			expectedStatuses.Add(new Guid("6069902E-5760-4DF4-B733-A5E00105B099"), 2);
-			expectedStatuses.Add(new Guid("12728761-0ED6-422B-B2B5-A5E001051F1E"), 2);
-			expectedStatuses.Add(new Guid("E4C7A7A7-8D2C-4591-ACA4-A53D00F82C88"), 2);
-			expectedStatuses.Add(new Guid("1EBFEE75-CE35-40FB-975E-A3BF00D0577C"), 4);
-			expectedStatuses.Add(new Guid("AE476FA3-7A6C-4948-89C2-A3BF00D0577C"), 2);
-			expectedStatuses.Add(new Guid("F25262A1-E3C3-4A54-B202-A33200B2E94F"), 4); //
-			expectedStatuses.Add(new Guid("42394840-F905-4B22-915F-A332008A5288"), 4); //
-			expectedStatuses.Add(new Guid("A35F2179-9A4B-40C9-BF7B-A27400997C79"), 4); //
-			expectedStatuses.Add(new Guid("CADD42C6-5419-48DD-8514-A25B009AD59D"), 4);
-			expectedStatuses.Add(new Guid("C922055A-B4D0-4C06-B9AD-A1410113C47D"), 4); //
-			expectedStatuses.Add(new Guid("4886AEDD-E30F-416C-B5E8-A1410113C47D"), 4);
-			expectedStatuses.Add(new Guid("DCF2EA04-3031-4436-A229-A1410113C47D"), 4);
-			expectedStatuses.Add(new Guid("47721DE4-A0CB-45EE-A123-A1410113C47D"), 4); //
-			expectedStatuses.Add(new Guid("391DB822-3936-4C4D-9634-A1410113C47D"), 4); //
-			expectedStatuses.Add(new Guid("87E6CEF0-388A-4365-94FA-A1410113C47D"), 4);
-			expectedStatuses.Add(new Guid("55E3A133-6305-4C9A-8AEA-A1410113C47D"), 2);
-
-			fillResultStatuses(personReqs, resultStatuses);
-
+			fillResultStatuses(personRequests, resultStatuses);
+			foreach (var resultStatuse in resultStatuses)
+			{
+				Console.WriteLine($"key:{resultStatuse.Key},value:{resultStatuse.Value}");
+			}
 			CollectionAssert.AreEquivalent(expectedStatuses, resultStatuses);
 		}
 
@@ -93,31 +134,8 @@ namespace Teleopti.Ccc.Requests.PerformanceTest.AbsenceRequests
 			});
 		}
 
-		private List<IPersonRequest> processMultipleAbsenceRequests(bool queueRequest, RequestValidatorsFlag requestValidatorsFlag = RequestValidatorsFlag.None)
+		private List<IPersonRequest> processMultipleAbsenceRequests(IEnumerable<Guid> personIds, bool queueRequest, RequestValidatorsFlag requestValidatorsFlag = RequestValidatorsFlag.None)
 		{
-			IEnumerable<Guid> personIds = new List<Guid>
-			{
-				// people from team 'FL_Sup_Sun7_71631' and 'FL_Sup_Sun6_00333'
-				//DO NOT CHANGE THE ORDER OF THE GUIDS!
-				new Guid("55E3A133-6305-4C9A-8AEA-A1410113C47D"),
-				new Guid("87E6CEF0-388A-4365-94FA-A1410113C47D"),
-				new Guid("391DB822-3936-4C4D-9634-A1410113C47D"),
-				new Guid("47721DE4-A0CB-45EE-A123-A1410113C47D"),
-				new Guid("DCF2EA04-3031-4436-A229-A1410113C47D"),
-				new Guid("4886AEDD-E30F-416C-B5E8-A1410113C47D"),
-				new Guid("C922055A-B4D0-4C06-B9AD-A1410113C47D"),
-				new Guid("CADD42C6-5419-48DD-8514-A25B009AD59D"),
-				new Guid("A35F2179-9A4B-40C9-BF7B-A27400997C79"),
-				new Guid("42394840-F905-4B22-915F-A332008A5288"),
-				new Guid("F25262A1-E3C3-4A54-B202-A33200B2E94F"),
-				new Guid("AE476FA3-7A6C-4948-89C2-A3BF00D0577C"),
-				new Guid("1EBFEE75-CE35-40FB-975E-A3BF00D0577C"),
-				new Guid("E4C7A7A7-8D2C-4591-ACA4-A53D00F82C88"),
-				new Guid("12728761-0ED6-422B-B2B5-A5E001051F1E"),
-				new Guid("6069902E-5760-4DF4-B733-A5E00105B099"),
-				new Guid("BF50C741-A780-4930-A64B-A5E00105F325")
-			};
-
 			using (DataSource.OnThisThreadUse("Teleopti WFM"))
 				AsSystem.Logon("Teleopti WFM", new Guid("1fa1f97c-ebff-4379-b5f9-a11c00f0f02b"));
 
