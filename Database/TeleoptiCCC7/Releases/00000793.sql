017a4737dfce1c97a531c7f2ec13721c19319555
Erik Sundberg <erik.sundberg@teleopti.com>
2017-01-24 09:23:35 +0100

    #42039 fix rebase

--- Database/TeleoptiCCC7/Releases/00000793.sql
+++ Database/TeleoptiCCC7/Releases/00000793.sql
@@ -1,5 +1,6 @@
-ALTER TABLE [ReadModel].[AgentState] ADD FirstName [nvarchar](25) null;
-ALTER TABLE [ReadModel].[AgentState] ADD LastName [nvarchar](25) null;
-ALTER TABLE [ReadModel].[AgentState] ADD EmploymentNumber [nvarchar](25) null;
-ALTER TABLE [ReadModel].[AgentState] ADD SiteName [nvarchar](50) null;
-ALTER TABLE [ReadModel].[AgentState] ADD TeamName [nvarchar](50) null;
+CREATE NONCLUSTERED INDEX IX_ShiftLayer_AUD_Parent_REVTYPE
+ON [Auditing].[ShiftLayer_AUD] ([Parent],[REVTYPE])
+
+
+CREATE NONCLUSTERED INDEX IX_PersonAssignment_AUD_Person_Date
+ON [Auditing].[PersonAssignment_AUD] ([Person],[Date])
---
+++ Database/TeleoptiCCC7/Releases/00000794.sql
@@ -0,0 +1,5 @@
+ALTER TABLE [ReadModel].[AgentState] ADD FirstName [nvarchar](25) null;
+ALTER TABLE [ReadModel].[AgentState] ADD LastName [nvarchar](25) null;
+ALTER TABLE [ReadModel].[AgentState] ADD EmploymentNumber [nvarchar](25) null;
+ALTER TABLE [ReadModel].[AgentState] ADD SiteName [nvarchar](50) null;
+ALTER TABLE [ReadModel].[AgentState] ADD TeamName [nvarchar](50) null;
