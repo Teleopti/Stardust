72fc50034e093a5cf90a227a58e62bba30759478
Claes Stenman <claes.stenman@teleopti.com>
2017-01-24 08:47:59 +0100

    #42680 continue with first test

--- DomainTest/SchedulingScenarios/Scheduling/ShiftCategoryLimitationTest.cs
+++ DomainTest/SchedulingScenarios/Scheduling/ShiftCategoryLimitationTest.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using SharpTestsEx;
 using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.Optimization;
 using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
@@ -21,27 +26,42 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Scheduling
 		[Test, Ignore("Starting with some experimention for 42680")]
 		public void fOo()
 		{
-			var date = new DateOnly(2017, 1, 22);
-			var shiftCategory = new ShiftCategory("_");
+			var firstDate = new DateOnly(2017, 1, 22);
+			var secondDate = new DateOnly(2017, 1, 23);
+			var shiftCategoryA = new ShiftCategory("_").WithId();
+			var shiftCategoryB = new ShiftCategory("_").WithId();
 			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var contract = new Contract("_") { WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(10), TimeSpan.FromHours(83), TimeSpan.FromHours(1), TimeSpan.FromHours(16)) };
+			var skill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().IsOpen();
+			var skillDayA = skill.CreateSkillDayWithDemand(scenario, firstDate, 10);
+			var skillDayB = skill.CreateSkillDayWithDemand(scenario, secondDate, 1);
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(14, 0, 14, 0, 15), new TimePeriodWithSegment(22, 0, 22, 0, 15), shiftCategoryB));
 			var optimizerOriginalPreferences = new OptimizerOriginalPreferences
 			{
 				SchedulingOptions =
 				{
 					GroupOnGroupPageForTeamBlockPer = new GroupPageLight("not interesting", GroupPageType.SingleAgent),
-					UseTeam = true
+					UseTeam = true,
+					UseShiftCategoryLimitations = true
 				}
 			};
-			var agent = new Person().WithSchedulePeriodOneWeek(date).WithPersonPeriod();
-			agent.SchedulePeriod(date).AddShiftCategoryLimitation(new ShiftCategoryLimitation(shiftCategory)
+			var agent = new Person().WithSchedulePeriodOneWeek(firstDate).WithPersonPeriod(ruleSet, contract, skill);
+			agent.SchedulePeriod(firstDate).AddShiftCategoryLimitation(new ShiftCategoryLimitation(shiftCategoryA)
 			{
-				MaxNumberOf = 3
+				MaxNumberOf = 1
 			});
-			var ass = new PersonAssignment(agent, scenario, date).ShiftCategory(shiftCategory);
-			var stateholder = SchedulerStateHolderFrom.Fill(scenario, date.ToDateOnlyPeriod(), new [] {agent}, new [] {ass}, Enumerable.Empty<ISkillDay>());
-			
-			Target.Execute(optimizerOriginalPreferences, new NoSchedulingProgress(), new[] { stateholder.Schedules[agent].ScheduledDay(date) }, null, null);
 
+			var period = new DateOnlyPeriod(firstDate, secondDate);
+			var assA = new PersonAssignment(agent, scenario, firstDate).ShiftCategory(shiftCategoryA).WithLayer(activity, new TimePeriod(6, 14));
+			var assB = new PersonAssignment(agent, scenario, secondDate).ShiftCategory(shiftCategoryA).WithLayer(activity, new TimePeriod(6, 14));
+			var stateholder = SchedulerStateHolderFrom.Fill(scenario, period, new[] { agent }, new[] { assA, assB }, new[] { skillDayA, skillDayB });
+			var scheduleDays = stateholder.Schedules[agent].ScheduledDayCollection(period).ToList();
+
+			Target.Execute(optimizerOriginalPreferences, new NoSchedulingProgress(), scheduleDays, new OptimizationPreferences(), null);
+
+			stateholder.Schedules[agent].ScheduledDay(firstDate).PersonAssignment().ShiftCategory.Should().Be.EqualTo(shiftCategoryA);
+			stateholder.Schedules[agent].ScheduledDay(secondDate).PersonAssignment().ShiftCategory.Should().Be.EqualTo(shiftCategoryB);
 		}
 	}
 }
