fa74e7405eb2ac2b3e603252a281f2ed294aeed3
Anders Forsberg <anders.forsberg@teleopti.com>
2017-01-25 09:53:20 +0100

    #42758

---
+++ Database/TeleoptiCCC7/Releases/00000796.sql
@@ -0,0 +1,11 @@
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[Auditing].[PersonAssignment_AUD]') AND name = N'IX_PersonAssignment_AUD_REV')
+	CREATE NONCLUSTERED INDEX IX_PersonAssignment_AUD_REV
+	ON [Auditing].[PersonAssignment_AUD] ([REV])
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[Auditing].[PersonAbsence_AUD]') AND name = N'IX_PersonAbsence_AUD_REV')
+	CREATE NONCLUSTERED INDEX IX_PersonAbsence_AUD_REV
+	ON [Auditing].[PersonAbsence_AUD] ([REV])
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[Auditing].[PersonDayOff_AUD]') AND name = N'IX_PersonDayOff_AUD_REV')
+	CREATE NONCLUSTERED INDEX IX_PersonDayOff_AUD_REV
+	ON [Auditing].[PersonDayOff_AUD] ([REV])
