19ee18f0fe702ccce27be04c64c95a8d75882a8b
Jonas Nordh <jonas.nordh@teleopti.com>
2017-01-25 14:11:12 +0100

    fix release script to support sql 2008

--- Database/TeleoptiCCC7/Releases/00000797.sql
+++ Database/TeleoptiCCC7/Releases/00000797.sql
@@ -4,14 +4,12 @@
 --Desc: Cleaning data and adding unique constraint for SkillDataPeriod to avoid duplicate records.
 ---------------- 
 
-delete SkillDataPeriod
-from SkillDataPeriod sdp1
-inner join	(select Parent, StartDateTime, max(Id) IdMax, count(*) c 
-			from SkillDataPeriod
-			group by Parent, StartDateTime
-			having count(*) > 1
-			) as sdp2
-			on sdp1.Id = sdp2.IdMax
+WITH cte as(
+  SELECT ROW_NUMBER() OVER (PARTITION BY Parent, StartDateTime
+                            ORDER BY Id) RN
+  FROM   dbo.SkillDataPeriod
+  )
+DELETE FROM cte WHERE RN>1
 
 ALTER TABLE dbo.SkillDataPeriod ADD CONSTRAINT UQ_SkillDataPeriod_Parent_StartDateTime UNIQUE NONCLUSTERED 
 (
