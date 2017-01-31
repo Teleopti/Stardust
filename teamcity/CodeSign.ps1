71196b1d11bb13bddc5bf51d64614befdb29ee15
Kunning Mao <none@none>
2017-01-31 15:02:46 +0100

    #40972 correct the parameters again

--- teamcity/CodeSign.ps1
+++ teamcity/CodeSign.ps1
@@ -1,4 +1,4 @@
 ﻿$SignTool = "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool"
-$Target = "$env:WorkingDirectory\Teleopti.Ccc.SmartClientPortal\Teleopti.Ccc.SmartClientPortal.Shell\bin\$system:Configuration\Teleopti.Ccc.SmartClientPortal.Shell.exe"
+$Target = "$env:WorkingDirectory\Teleopti.Ccc.SmartClientPortal\Teleopti.Ccc.SmartClientPortal.Shell\bin\$env:Configuration\Teleopti.Ccc.SmartClientPortal.Shell.exe"
 &$SignTool sign /debug /f "$env:WorkingDirectory\teamcity\Azure\TeleoptiCCC\bin\teleopti.pfx" /p T3l30pt1 $Target
 &$SignTool timestamp /t "http://timestamp.verisign.com/scripts/timstamp.dll" $Target
