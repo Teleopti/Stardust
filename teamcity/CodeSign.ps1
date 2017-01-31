8cedd5595c01d9617b62c973ceffa0c8154992c8
Kunning Mao <none@none>
2017-01-31 14:12:59 +0100

    #40972 added CodeSign powershell

---
+++ teamcity/CodeSign.ps1
@@ -0,0 +1,4 @@
+﻿$SignTool = "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool"
+$Target = "$WorkingDirectory\Teleopti.Ccc.SmartClientPortal\Teleopti.Ccc.SmartClientPortal.Shell\bin\$Configuration\Teleopti.Ccc.SmartClientPortal.Shell.exe"
+&$SignTool sign /debug /f "$WorkingDirectory\teamcity\Azure\TeleoptiCCC\bin\teleopti.pfx" /p T3l30pt1 $Target
+&$SignTool timestamp /t "http://timestamp.verisign.com/scripts/timstamp.dll" $Target
