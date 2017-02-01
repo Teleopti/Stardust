bd678f47d694094ac9fc20223bcfdbd137bec45b
Robin Karlsson <robin.karlsson@teleopti.com>
2017-02-01 13:13:01 +0100

    Resolving an issue with failing script (#42835)

--- Database/TeleoptiCCC7/Releases/00000787.sql
+++ Database/TeleoptiCCC7/Releases/00000787.sql
@@ -3,6 +3,14 @@
 --Date: 2017-01-11
 --Desc: Adding unique constraint for PersonSkill to avoid duplicate entries
 ---------------- 
+WITH cte as(
+  SELECT ROW_NUMBER() OVER (PARTITION BY Parent, Skill
+                            ORDER BY Id) RN
+  FROM   dbo.PersonSkill
+  )
+DELETE FROM cte WHERE RN>1
+GO
+
 ALTER TABLE [dbo].[PersonSkill] ADD CONSTRAINT UQ_PersonSkill_Parent_Skill UNIQUE NONCLUSTERED
 (
 	[Parent] ASC,
