f761f725873ff77029d240eabb4fde3a9d99794d
Roger Kratz <roger.kratz@teleopti.com>
2017-02-03 14:26:34 +0100

    #42594 Minor refactoring

--- Domain/Scheduling/TeamBlock/WorkShiftCalculation/IsAnySkillOpen.cs
+++ Domain/Scheduling/TeamBlock/WorkShiftCalculation/IsAnySkillOpen.cs
@@ -10,14 +10,15 @@ namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock.WorkShiftCalculation
 	{
 		public bool Check(IEnumerable<ISkillDay> skillDays, IVisualLayer layer, TimeZoneInfo agentTimeZoneInfo)
 		{
-			if (!((IActivity)layer.Payload).RequiresSkill)
+			var layerActivity = (IActivity) layer.Payload;
+			if (!layerActivity.RequiresSkill)
 				return true;
 			var layerStartTime = layer.Period.ToDateOnlyPeriod(agentTimeZoneInfo).StartDate;
 			var layerTimePeriod = layer.Period.TimePeriod(agentTimeZoneInfo);
 			foreach (var skillDay in skillDays)
 			{
 				if (!(skillDay.Skill is MaxSeatSkill) &&
-					skillDay.Skill.Activity.Equals((IActivity)layer.Payload) &&
+					skillDay.Skill.Activity.Equals(layerActivity) &&
 					skillDay.CurrentDate.Equals(layerStartTime))
 				{
 					if (skillDay.OpenHours().Any(timePeriod => timePeriod.Contains(layerTimePeriod)))
