9c0b99ba45be7634efc0101a34bcb59f7d3f8275
Roger Kratz <roger.kratz@teleopti.com>
2017-02-03 14:24:49 +0100

    #42594 Avoid converting layer timeperiod to agent time zone lots of time

--- Domain/Scheduling/TeamBlock/WorkShiftCalculation/IsAnySkillOpen.cs
+++ Domain/Scheduling/TeamBlock/WorkShiftCalculation/IsAnySkillOpen.cs
@@ -13,13 +13,14 @@ namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock.WorkShiftCalculation
 			if (!((IActivity)layer.Payload).RequiresSkill)
 				return true;
 			var layerStartTime = layer.Period.ToDateOnlyPeriod(agentTimeZoneInfo).StartDate;
+			var layerTimePeriod = layer.Period.TimePeriod(agentTimeZoneInfo);
 			foreach (var skillDay in skillDays)
 			{
 				if (!(skillDay.Skill is MaxSeatSkill) &&
 					skillDay.Skill.Activity.Equals((IActivity)layer.Payload) &&
 					skillDay.CurrentDate.Equals(layerStartTime))
 				{
-					if (skillDay.OpenHours().Any(timePeriod => timePeriod.Contains(layer.Period.TimePeriod(agentTimeZoneInfo))))
+					if (skillDay.OpenHours().Any(timePeriod => timePeriod.Contains(layerTimePeriod)))
 					{
 						return true;
 					}
