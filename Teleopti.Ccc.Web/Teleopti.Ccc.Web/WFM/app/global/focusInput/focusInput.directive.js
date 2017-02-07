a74cd85c8f8d3f5cc81b8ce748dd66ab71869aa8
Jianfeng <none@none>
2017-02-07 14:22:21 +0800

    input focus : minor improvement

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/focusInput/focusInput.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/focusInput/focusInput.directive.js
@@ -1,17 +1,15 @@
 (function() {
 	'use strict';
 
-	angular.module('wfm.inputFocus', []);
-	
-	angular.module('wfm.inputFocus').directive('focusInput', function ($timeout, $interval) {
+	angular.module('wfm.inputFocus', []).directive('focusInput', function ($timeout, $interval) {
 		return {
 			link: function(scope, element, attrs) {
 				scope.$watch(attrs.focusInput, function(value) {
 					if (value) {
 						$timeout(function() {
-							var focusSearch = $interval(function() {
+							var focusInputInterval = $interval(function() {
 								angular.element($(element[0]).find('input')).focus();
-								$interval.cancel(focusSearch);
+								$interval.cancel(focusInputInterval);
 							}, 100);
 							scope[attrs.focusInput] = false;
 						},0);
