04b12c427d3654c890476d07cd8b4e896f88c0a5
Roger Kratz <roger.kratz@teleopti.com>
2017-02-07 16:38:36 +0100

    #42593 Failing, ignored test

---
+++ DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationTeamBlockDesktopShiftTabTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.DayOffPlanning;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.Domain.Scheduling.WebLegacy;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
+{
+	[DomainTest]
+	public class IntradayOptimizationTeamBlockDesktopShiftTabTest : ISetup
+	{
+		public IOptimizationCommand Target;
+		public Func<ISchedulerStateHolder> SchedulerStateHolderFrom;
+		public FakeBusinessUnitRepository BusinessUnitRepository;
+		public IResourceCalculation ResourceCalculation;
+
+		[Test]
+		[Ignore("Fix with #42593")]
+		public void ShouldRespectKeepShiftCategories()
+		{
+			BusinessUnitRepository.Has(ServiceLocatorForEntity.CurrentBusinessUnit.Current());
+			var scenario = new Scenario("_");
+			var activity = new Activity {InContractTime = true};
+			var dateOnly = new DateOnly(2010, 1, 1);
+			var shiftCategoryBefore = new ShiftCategory("before").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(9, 15, 9, 15, 15), new TimePeriodWithSegment(17, 15, 17, 15, 15), new ShiftCategory("_").WithId()));
+			var skill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().IsOpen();
+			var skillDay = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly, TimeSpan.FromMinutes(1), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(100)));
+			var skillDaySHOULDNOTBENEEDED = skill.CreateSkillDayWithDemandPerHour(scenario, dateOnly.AddDays(1), TimeSpan.FromMinutes(1), new Tuple<int, TimeSpan>(17, TimeSpan.FromMinutes(100)));
+			var contract = new Contract("_") { WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(1), TimeSpan.FromHours(1), TimeSpan.FromHours(1), TimeSpan.FromHours(16)) };
+			var agent = new Person().WithId().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(ruleSet, contract, skill).WithSchedulePeriodOneDay(dateOnly);
+			var ass = new PersonAssignment(agent, scenario, dateOnly).WithLayer(activity, new TimePeriod(8, 16)).ShiftCategory(shiftCategoryBefore);
+			//SHOULD NOT BE NEEDED
+			var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(dateOnly, dateOnly.AddDays(1)), new[] { agent }, new[] { ass }, new[] { skillDay, skillDaySHOULDNOTBENEEDED});
+			//SHOULD BE ENOUGH
+			// var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, dateOnly, new[] { agent }, new[] { ass }, skillDay);
+
+			var optPreferences = new OptimizationPreferences
+			{
+				General = { ScheduleTag = new ScheduleTag(), OptimizationStepShiftsWithinDay = true },
+				Extra = { UseTeams = true, TeamGroupPage = new GroupPageLight("_", GroupPageType.Hierarchy) },
+				Shifts = {KeepShiftCategories = true}
+			};
+			ResourceCalculation.ResourceCalculate(dateOnly, new ResourceCalculationData(schedulerStateHolderFrom.SchedulingResultState, false, false));
+
+			Target.Execute(null,
+				new NoSchedulingProgress(),
+				schedulerStateHolderFrom,
+				new [] { schedulerStateHolderFrom.Schedules[agent].ScheduledDay(dateOnly) },
+				optPreferences,
+				false,
+				null,
+				null);
+
+			schedulerStateHolderFrom.Schedules[agent].ScheduledDay(dateOnly).PersonAssignment().ShiftCategory
+				.Should().Be.EqualTo(shiftCategoryBefore);
+		}
+
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.UseTestDouble<DesktopOptimizationContext>().For<IFillSchedulerStateHolder, ISynchronizeIntradayOptimizationResult, IOptimizationPreferencesProvider, IPeopleInOrganization>();
+		}
+	}
+}
