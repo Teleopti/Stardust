8a6d3061828fd5fa0a966efc56124cde63a0b3af
Roger Kratz <roger.kratz@teleopti.com>
2017-02-08 15:50:49 +0100

    #42593 Focus on PBI is shift tab for maxseat, so changing test

---
+++ DomainTest/SchedulingScenarios/MaxSeat/MaxSeatOptimizationPreferencesTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.Domain.Scheduling.SeatLimitation;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.DomainTest.SchedulingScenarios.MaxSeat.TestData;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.MaxSeat
+{
+	[DomainTest]
+	[Toggle(Toggles.ResourcePlanner_MaxSeatsNew_40939)]
+	public class MaxSeatOptimizationPreferencesTest
+	{
+		public MaxSeatOptimization Target;
+		public GroupScheduleGroupPageDataProvider GroupScheduleGroupPageDataProvider;
+
+		[Test]
+		[Ignore("Fix with #42593")]
+		public void ShouldRespectKeepShiftCategoriesTeamHierarchyOneAgent()
+		{
+			var site = new Site("_") { MaxSeats = 1 }.WithId();
+			var team = new Team { Site = site }.WithDescription(new Description("_"));
+			GroupScheduleGroupPageDataProvider.SetBusinessUnit_UseFromTestOnly(BusinessUnitFactory.CreateBusinessUnitAndAppend(team));
+			var activity = new Activity("_") { RequiresSeat = true }.WithId();
+			var dateOnly = new DateOnly(2016, 10, 25);
+			var scenario = new Scenario("_");
+			var newShiftCategory = new ShiftCategory("_").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(9, 0, 9, 0, 60), new TimePeriodWithSegment(17, 0, 17, 0, 60), newShiftCategory));
+			var agentScheduledForAnHourData = MaxSeatDataFactory.CreateAgentWithAssignment(dateOnly, new Team { Site = site }, new RuleSetBag(ruleSet), scenario, activity, new TimePeriod(8, 0, 9, 0));
+			var agentData = MaxSeatDataFactory.CreateAgentWithAssignment(dateOnly, team, new RuleSetBag(ruleSet), scenario, activity, new TimePeriod(8, 0, 16, 0));
+			var schedules = ScheduleDictionaryCreator.WithData(scenario, dateOnly.ToDateOnlyPeriod(), new [] { agentData.Assignment, agentScheduledForAnHourData.Assignment });
+
+			var optPreferences = new OptimizationPreferences
+			{
+				Extra = { UseTeams = true, TeamGroupPage = new GroupPageLight("_", GroupPageType.Hierarchy) },
+				Shifts = { KeepShiftCategories = true },
+				Advanced = { UserOptionMaxSeatsFeature = MaxSeatsFeatureOptions.ConsiderMaxSeats }
+			};
+
+			Target.Optimize(new NoSchedulingProgress(), dateOnly.ToDateOnlyPeriod(), new[] { agentData.Agent }, schedules, Enumerable.Empty<ISkillDay>(), optPreferences, null);
+
+			schedules[agentData.Agent].ScheduledDay(dateOnly).PersonAssignment().ShiftCategory
+				.Should().Not.Be.EqualTo(newShiftCategory);
+		}
+	}
+}
