ec2d0b88c01fd076306bde49ab69ae9ef076b94b
micke <micke@teleopti.com>
2017-02-13 11:57:45 +0100

    Clean up

--- DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationTeamBlockAsClassicTest.cs
+++ DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationTeamBlockAsClassicTest.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.Common;
@@ -36,11 +34,12 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
 			var shiftCategory = new ShiftCategory("DY").WithId();
 			var shiftCategoryAm = new ShiftCategory("AM").WithId();
 			var activity = new Activity("Phone");
-			var skill = new Skill().WithId().For(activity).InTimeZone(TimeZoneInfo.Utc);
+			var skill = new Skill().WithId().For(activity).InTimeZone(TimeZoneInfo.Utc).IsOpen();
 			var workShiftRuleSet =new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(7, 0, 7, 0, 15), new TimePeriodWithSegment(15, 0, 15, 0, 15), shiftCategoryAm));
 			var agent = new Person().WithPersonPeriod(workShiftRuleSet, skill).WithId().InTimeZone(TimeZoneInfo.Utc).WithSchedulePeriodOneDay(date);
 			var ass = new PersonAssignment(agent, scenario, date).WithLayer(activity, new TimePeriod(8, 16)).ShiftCategory(shiftCategory);
-			var skillDay = createSkillDay(skill, date, scenario);
+			var skillDay = skill.CreateSkillDayWithDemandOnInterval(scenario, date, 0,
+				new Tuple<TimePeriod, double>(new TimePeriod(15, 16), 1));
 			var stateHolder = SchedulerStateHolderOrg.Fill(scenario, date, new [] {agent}, new[] {ass}, skillDay);
 			var optimizationPreferences = new OptimizationPreferences
 			{
@@ -52,28 +51,6 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
 
 			stateHolder.Schedules[agent].ScheduledDay(date).PersonAssignment().ShiftCategory
 				.Should().Be.EqualTo(shiftCategoryAm);
-		}
-
-		private static SkillDay createSkillDay(ISkill skill, DateOnly date, IScenario scenario)
-		{
-			var dateAsUtc = DateTime.SpecifyKind(date.Date, DateTimeKind.Utc);
-			var period = new DateTimePeriod(dateAsUtc, dateAsUtc.AddHours(0.25));
-
-			var skillDay = new SkillDay(date, skill, scenario, new List<IWorkloadDay>(), new List<ISkillDataPeriod>());
-			skillDay.SetupSkillDay();
-			var newSkillStaffPeriodValues = new NewSkillStaffPeriodValues(Enumerable.Range(0, 96).Select(i =>
-			{
-				var skillStaffPeriod = new SkillStaffPeriod(period.MovePeriod(TimeSpan.FromMinutes(i*15)),
-					new Task(2, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(2)),
-					ServiceAgreement.DefaultValues(), skill.SkillType.StaffingCalculatorService);
-				skillStaffPeriod.Payload.ManualAgents = 0;
-				return skillStaffPeriod;
-			}).ToArray());
-			skillDay.SetCalculatedStaffCollection(newSkillStaffPeriodValues);
-			skillDay.SkillDayCalculator = new SkillDayCalculator(skill, new[] {skillDay},
-				new DateOnlyPeriod(2014, 3, 22, 2014, 4, 7));
-			newSkillStaffPeriodValues.BatchCompleted();
-			return skillDay;
 		}
 
 		public void Setup(ISystem system, IIocConfiguration configuration)
