97a417e2be76f47251b05be8b1debe61ac46a227
Justin! <justin@teleopti.com>
2017-02-14 18:43:29 +0100

    #42963 Sure why not

---
+++ Database/TeleoptiCCC7/Releases/00000816.sql
@@ -0,0 +1,9 @@
+
+CREATE INDEX IX_PersonId ON ReadModel.ExternalLogon (PersonId);
+GO
+
+CREATE INDEX IX_Added ON ReadModel.ExternalLogon (Added);
+GO
+
+CREATE INDEX IX_Deleted ON ReadModel.ExternalLogon (Deleted);
+GO
