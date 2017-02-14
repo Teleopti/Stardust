f7a3a37b21c262f200a60ac4a3305535d21f6d92
Justin! <justin@teleopti.com>
2017-02-14 14:14:21 +0100

    #42963 Just to be sure

--- Database/TeleoptiCCC7/Releases/00000815.sql
+++ Database/TeleoptiCCC7/Releases/00000815.sql
@@ -1,2 +1,12 @@
+WITH cte AS (
+  SELECT PersonId, row_number() OVER(PARTITION BY PersonId ORDER BY PersonId) AS [rn]
+  FROM ReadModel.CurrentSchedule
+)
+DELETE cte WHERE [rn] > 1
+GO
+
+UPDATE ReadModel.CurrentSchedule SET Valid = 0
+GO
+
 ALTER TABLE ReadModel.CurrentSchedule ADD CONSTRAINT PK_PersonId PRIMARY KEY CLUSTERED (PersonId)
 GO
