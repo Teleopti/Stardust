2d89367e2659b0ae94992c3843b70793a298bc8d
Justin! <justin@teleopti.com>
2017-02-14 10:29:43 +0100

    #42978 Removing dist lock from analytics repository, adding 42 extra days, optimistically fail with retry on PK violation, as agreed.

--- Infrastructure/Repositories/Analytics/AnalyticsDateRepositoryWithCreation.cs
+++ Infrastructure/Repositories/Analytics/AnalyticsDateRepositoryWithCreation.cs
@@ -2,7 +2,6 @@ using System;
 using Teleopti.Ccc.Domain.Analytics;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
-using Teleopti.Ccc.Domain.DistributedLock;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Infrastructure;
 using Teleopti.Interfaces.Infrastructure.Analytics;
@@ -11,14 +10,12 @@ namespace Teleopti.Ccc.Infrastructure.Repositories.Analytics
 {
 	public class AnalyticsDateRepositoryWithCreation : AnalyticsDateRepositoryBase, IAnalyticsDateRepository
 	{
-		private readonly IDistributedLockAcquirer _distributedLockAcquirer;
 		private readonly IEventPublisher _eventPublisher;
 		private readonly IAnalyticsConfigurationRepository _analyticsConfigurationRepository;
 		private static bool shouldPublish;
 
-		public AnalyticsDateRepositoryWithCreation(ICurrentAnalyticsUnitOfWork analyticsUnitOfWork, IDistributedLockAcquirer distributedLockAcquirer, IEventPublisher eventPublisher, IAnalyticsConfigurationRepository analyticsConfigurationRepository) : base(analyticsUnitOfWork)
+		public AnalyticsDateRepositoryWithCreation(ICurrentAnalyticsUnitOfWork analyticsUnitOfWork, IEventPublisher eventPublisher, IAnalyticsConfigurationRepository analyticsConfigurationRepository) : base(analyticsUnitOfWork)
 		{
-			_distributedLockAcquirer = distributedLockAcquirer;
 			_eventPublisher = eventPublisher;
 			_analyticsConfigurationRepository = analyticsConfigurationRepository;
 		}
@@ -30,28 +27,21 @@ namespace Teleopti.Ccc.Infrastructure.Repositories.Analytics
 
 		private IAnalyticsDate createDatesTo(DateTime dateDate)
 		{
-			using (_distributedLockAcquirer.LockForTypeOf(this))
+			var toDate = dateDate.AddDays(42);
+			var currentDay = base.MaxDate().DateDate;
+			var culture = _analyticsConfigurationRepository.GetCulture();
+			while ((currentDay += TimeSpan.FromDays(1)) <= toDate)
 			{
-				// Check again to see that we still don't have the date after aquiring the lock
-				var date = base.Date(dateDate);
-				if (date != null)
-					return date;
-
-				var currentDay = base.MaxDate().DateDate;
-				var culture = _analyticsConfigurationRepository.GetCulture();
-				while ((currentDay += TimeSpan.FromDays(1)) <= dateDate)
-				{
-					AnalyticsUnitOfWork.Current().Session().Save(new AnalyticsDate(currentDay, culture));
-					shouldPublish = true;
-				}
-				AnalyticsUnitOfWork.Current().AfterSuccessfulTx(() =>
-				{
-					if (!shouldPublish) return;
-					_eventPublisher.Publish(new AnalyticsDatesChangedEvent());
-					shouldPublish = false;
-				});
-				return base.Date(dateDate);
+				AnalyticsUnitOfWork.Current().Session().Save(new AnalyticsDate(currentDay, culture));
+				shouldPublish = true;
 			}
+			AnalyticsUnitOfWork.Current().AfterSuccessfulTx(() =>
+			{
+				if (!shouldPublish) return;
+				_eventPublisher.Publish(new AnalyticsDatesChangedEvent());
+				shouldPublish = false;
+			});
+			return base.Date(dateDate);
 		}
 	}
 }
