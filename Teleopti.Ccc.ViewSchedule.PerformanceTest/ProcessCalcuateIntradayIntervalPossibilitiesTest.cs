c6c9ca3820239c360ae37ea7e0aa68c7a60036cc
junwen.luo <none@none>
2017-02-15 16:51:51 +0800

    PBI #41608 implement test using more than 200 personIds

--- Teleopti.Ccc.ViewSchedule.PerformanceTest/ProcessCalcuateIntradayIntervalPossibilitiesTest.cs
+++ Teleopti.Ccc.ViewSchedule.PerformanceTest/ProcessCalcuateIntradayIntervalPossibilitiesTest.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
 using Teleopti.Ccc.Domain.AgentInfo;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Time;
-using Teleopti.Ccc.Domain.Helper;
 using Teleopti.Ccc.Domain.Intraday;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.Repositories;
@@ -24,7 +23,6 @@ namespace Teleopti.Ccc.ViewSchedule.PerformanceTest
 	public class ProcessCalcuateIntradayIntervalPossibilities
 	{
 		private const string tenantName = "Teleopti WFM";
-		// BusinesUnit "Telia Sverige"
 		private readonly Guid businessUnitId = new Guid("1FA1F97C-EBFF-4379-B5F9-A11C00F0F02B");
 
 		private ScheduleStaffingPossibilityCalculator _scheduleStaffingPossibilityCalculator;
@@ -54,64 +52,70 @@ namespace Teleopti.Ccc.ViewSchedule.PerformanceTest
 		public IScheduleStorage ScheduleStorage;
 		public ICurrentScenario CurrentScenario;
 
-		private IList<IPersonRequest> requests;
-		public void setup()
+		[Theory]
+		public void ShouldProcessMultipleCalculationAbsencePossibilities1000()
+		{
+			setup();
+			var personIds = loadPersonIds();
+			WithUnitOfWork.Do(() =>
+			{
+				var personCount = personIds.Length;
+				var firstTimeElapsedMilliseconds = calcuatePossibilities(personIds)/personCount;
+				var secondTimeElapsedMilliseconds = calcuatePossibilities(personIds)/personCount;
+
+				Console.WriteLine(
+					$"querying possibilities for {personIds.Length} persons,{Environment.NewLine}" +
+					$"the first time it takes {firstTimeElapsedMilliseconds} milliseconds for one person,{Environment.NewLine}" +
+					$"and the second time it takes {secondTimeElapsedMilliseconds} milliseconds for one person");
+			});
+		}
+
+		private static string[] loadPersonIds()
+		{
+			var personIds = new List<string>();
+			var path = AppDomain.CurrentDomain.BaseDirectory + "/../../" + "PersonIds.txt";
+			var content = File.ReadAllText(path);
+			using (var stringReader = new StringReader(content))
+			{
+				string personId;
+				while ((personId = stringReader.ReadLine()) != null)
+				{
+					personIds.Add(personId);
+				}
+			}
+			return personIds.ToArray();
+		}
+
+		private long calcuatePossibilities(string[] personIds)
+		{
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+			foreach (var personId in personIds)
+			{
+				var person = PersonRepository.Get(new Guid(personId));
+				var currentUser = new FakeLoggedOnUser(person);
+				var cacheableStaffingViewModelCreator = new CacheableStaffingViewModelCreator(StaffingViewModelCreator, currentUser);
+				_scheduleStaffingPossibilityCalculator = new ScheduleStaffingPossibilityCalculator(Now, currentUser,
+					cacheableStaffingViewModelCreator, ScheduleStorage, CurrentScenario);
+				var possibilities = _scheduleStaffingPossibilityCalculator.CalcuateIntradayAbsenceIntervalPossibilities();
+				if (!possibilities.Any())
+				{
+					Console.WriteLine($"{personId} no data");
+				}
+			}
+			stopwatch.Stop();
+			return stopwatch.ElapsedMilliseconds;
+		}
+
+		private void setup()
 		{
 			logonSystem();
 			Now.Is("2016-03-16 07:00");
-
-			using (var connection = new SqlConnection(ConfigReader.ConnectionString("Tenancy")))
-			{
-				connection.Open();
-				var path = AppDomain.CurrentDomain.BaseDirectory + "/../../" + "Prepare200RequestForIntradayTest.sql";
-				var script = File.ReadAllText(path);
-
-				using (var command = new SqlCommand(script, connection))
-				{
-					command.ExecuteNonQuery();
-				}
-				connection.Close();
-			}
-
 			var now = Now.UtcDateTime();
 			var period = new DateTimePeriod(now.AddDays(-1), now.AddDays(1));
-			requests = new List<IPersonRequest>();
 			WithUnitOfWork.Do(() =>
 			{
-				WorkflowControlSetRepository.LoadAll();
-				AbsenceRepository.LoadAll();
-				WorkloadRepository.LoadAll();
-				SkillRepository.LoadAllSkills();
-				ContractRepository.LoadAll();
-				SkillTypeRepository.LoadAll();
-				PartTimePercentageRepository.LoadAll();
-				ContractScheduleRepository.LoadAllAggregate();
-				ActivityRepository.LoadAll();
-				DayOffTemplateRepository.LoadAll();
-
 				UpdateStaffingLevel.Update(period);
-				requests = PersonRequestRepository.FindPersonRequestWithinPeriod(new DateTimePeriod(new DateTime(2016, 03, 16, 7, 0, 0).Utc(), new DateTime(2016, 03, 16, 10, 0, 0).Utc()));
-				PersonRepository.FindPeople(requests.Select(x => x.Person.Id.GetValueOrDefault()));
-			});
-
-		}
-
-		[Test]
-		public void ShouldProcessMultipleCalculationAbsencePossibilities1000()
-		{
-			//setup();
-			logonSystem();
-			var now = new DateTime(2016, 03, 16);
-			WithUnitOfWork.Do(() =>
-			{
-				var person = PersonRepository.Get(new Guid("38E7D01D-7530-4213-8364-A14100F34EA1"));
-				var currentUser = new FakeLoggedOnUser(person);
-				ICacheableStaffingViewModelCreator cacheableStaffingViewModelCreator = new CacheableStaffingViewModelCreator(StaffingViewModelCreator, currentUser);
-
-				_scheduleStaffingPossibilityCalculator = new ScheduleStaffingPossibilityCalculator(new MutableNow(now), currentUser,
-					cacheableStaffingViewModelCreator, ScheduleStorage, CurrentScenario);
-				var ret = _scheduleStaffingPossibilityCalculator.CalcuateIntradayAbsenceIntervalPossibilities();
-					
 			});
 		}
 
