9842058668f71b151e6ace29db2e046f3f99d7e1
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-02-16 11:43:59 +0100

    #42909 Some minor improvements to unlinked performance

--- Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_fact_schedule_update_unlinked_personids.sql
+++ Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_fact_schedule_update_unlinked_personids.sql
@@ -8,80 +8,64 @@ CREATE PROCEDURE [mart].[etl_fact_schedule_update_unlinked_personids]
 AS
 BEGIN
 
-CREATE TABLE #person(person_id int, 
-					valid_from_date smalldatetime, 
-					valid_to_date smalldatetime, 
-					to_be_deleted bit, 
-					valid_from_date_id_local int, 
-					valid_to_date_id_local int)
-
-CREATE TABLE #schedule(shift_startdate_local_id int)
-CREATE TABLE #schedule_day(shift_startdate_local_id int)
-CREATE TABLE #schedule_deviation(shift_startdate_local_id int)						
+DECLARE @person TABLE
+(
+  person_id int primary key, 
+  valid_from_date smalldatetime, 
+  valid_to_date smalldatetime, 
+  to_be_deleted bit, 
+  valid_from_date_id_local int, 
+  valid_to_date_id_local int
+)
 
 -- select persons with the specific person period ids
-INSERT #person(person_id, valid_from_date, valid_to_date, to_be_deleted, valid_from_date_id_local, valid_to_date_id_local)
+INSERT @person(person_id, valid_from_date, valid_to_date, to_be_deleted, valid_from_date_id_local, valid_to_date_id_local)
 SELECT person_id, valid_from_date, valid_to_date, to_be_deleted, valid_from_date_id_local, valid_to_date_id_local
 FROM mart.dim_person WITH (NOLOCK) 
 WHERE person_id IN (SELECT id from mart.SplitStringInt(@person_periodids))
 
 -- update the eternity date id with max date id
-UPDATE #person
+UPDATE @person
 SET valid_to_date_id_local = (SELECT TOP 1 date_id from mart.dim_date with (nolock) ORDER BY date_id DESC) 
 WHERE valid_to_date_id_local = -2
 
-INSERT #schedule(shift_startdate_local_id)
-SELECT DISTINCT shift_startdate_local_id -- 
-FROM mart.fact_schedule f 
-INNER JOIN #person p
-ON p.person_id = f.person_id
-WHERE p.valid_to_date_id_local < f.shift_startdate_local_id OR p.valid_from_date_id_local > f.shift_startdate_local_id OR p.to_be_deleted=1
-
 -- update fact schedule with correct person period id
-UPDATE mart.fact_schedule
-SET [person_id]=p.person_id
-FROM mart.fact_schedule f
-	INNER JOIN (SELECT person_id, valid_from_date_id_local, valid_to_date_id_local FROM #person WHERE to_be_deleted=0)p
-		ON f.shift_startdate_local_id between p.valid_from_date_id_local and p.valid_to_date_id_local
-		WHERE f.person_id in (SELECT person_id FROM #person)
-		AND f.shift_startdate_local_id in (SELECT shift_startdate_local_id FROM #schedule)
-
--- select fact schedule day count which need to be updated
-INSERT #schedule_day(shift_startdate_local_id)
-SELECT DISTINCT shift_startdate_local_id
-FROM mart.fact_schedule_day_count f
-INNER JOIN #person p
-ON p.person_id = f.person_id
-WHERE p.valid_to_date_id_local < f.shift_startdate_local_id or p.valid_from_date_id_local > f.shift_startdate_local_id or p.to_be_deleted=1
+UPDATE fs
+SET fs.[person_id]=newpp.person_id
+FROM mart.fact_schedule fs
+INNER JOIN @person newpp ON 
+	fs.person_id in (select person_id from @person)
+	AND fs.shift_startdate_local_id BETWEEN newpp.valid_from_date_id_local AND newpp.valid_to_date_id_local
+	AND newpp.to_be_deleted=0
+INNER JOIN @person oldpp ON oldpp.person_id = fs.person_id
+WHERE oldpp.valid_to_date_id_local < fs.shift_startdate_local_id 
+	OR oldpp.valid_from_date_id_local > fs.shift_startdate_local_id 
+	OR oldpp.to_be_deleted=1
 
 -- update fact schedule day count with correct person period id
-UPDATE mart.fact_schedule_day_count
-SET [person_id]=p.person_id
-FROM mart.fact_schedule_day_count f
-INNER JOIN (SELECT person_id, valid_from_date_id_local, valid_to_date_id_local FROM #person WHERE to_be_deleted=0)p
-ON f.shift_startdate_local_id between p.valid_from_date_id_local and p.valid_to_date_id_local
-WHERE f.person_id in (SELECT person_id FROM #person)
-and f.shift_startdate_local_id in (SELECT shift_startdate_local_id FROM #schedule_day)
-
--- select fact schedule deviation which need to be updated
-INSERT #schedule_deviation(shift_startdate_local_id)
-SELECT DISTINCT shift_startdate_local_id 
-FROM mart.fact_schedule_deviation f
-INNER JOIN #person p
-ON p.person_id = f.person_id
-WHERE p.valid_to_date_id_local < f.shift_startdate_local_id OR p.valid_from_date_id_local > f.shift_startdate_local_id OR p.to_be_deleted=1
-
+UPDATE fsdc
+SET fsdc.[person_id]=newpp.person_id
+FROM mart.fact_schedule_day_count fsdc
+INNER JOIN @person newpp ON 
+	fsdc.person_id in (select person_id from @person)
+	AND fsdc.shift_startdate_local_id BETWEEN newpp.valid_from_date_id_local AND newpp.valid_to_date_id_local 
+	AND newpp.to_be_deleted=0
+INNER JOIN @person oldpp ON oldpp.person_id = fsdc.person_id
+WHERE oldpp.valid_to_date_id_local < fsdc.shift_startdate_local_id 
+	OR oldpp.valid_from_date_id_local > fsdc.shift_startdate_local_id 
+	OR oldpp.to_be_deleted=1
 
 -- update fact schedule deviation with correct person period id
-UPDATE mart.fact_schedule_deviation
-SET [person_id]=p.person_id
-FROM mart.fact_schedule_deviation f
-INNER JOIN (SELECT person_id, valid_from_date, valid_to_date, valid_from_date_id_local, valid_to_date_id_local FROM #person WHERE to_be_deleted=0)p
-ON f.shift_startdate_local_id BETWEEN p.valid_from_date_id_local AND p.valid_to_date_id_local
-WHERE f.person_id IN (SELECT person_id FROM #person)
-AND f.shift_startdate_local_id IN (SELECT shift_startdate_local_id FROM #schedule_deviation)
-
-
-drop table #person, #schedule, #schedule_day, #schedule_deviation
+UPDATE fsd
+SET fsd.[person_id]=newpp.person_id
+FROM mart.fact_schedule_deviation fsd
+INNER JOIN @person newpp ON 
+	fsd.person_id in (select person_id from @person)
+	AND fsd.shift_startdate_local_id BETWEEN newpp.valid_from_date_id_local AND newpp.valid_to_date_id_local 
+	AND newpp.to_be_deleted=0
+INNER JOIN @person oldpp ON oldpp.person_id = fsd.person_id
+WHERE oldpp.valid_to_date_id_local < fsd.shift_startdate_local_id 
+	OR oldpp.valid_from_date_id_local > fsd.shift_startdate_local_id 
+	OR oldpp.to_be_deleted=1
 
 END
