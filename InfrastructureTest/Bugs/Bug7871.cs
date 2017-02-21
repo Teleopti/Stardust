7bab3921e879990efc5dde7c1a47437c0a435ebe
Roger Kratz <roger.kratz@teleopti.com>
2017-02-21 14:06:02 +0100

    #43105 Fixed, now no errors locally either

--- InfrastructureTest/Bugs/Bug7871.cs
+++ InfrastructureTest/Bugs/Bug7871.cs
@@ -1,40 +1,50 @@
 ﻿using NUnit.Framework;
 using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Security.AuthorizationEntities;
-using Teleopti.Ccc.Infrastructure.Repositories;
-using Teleopti.Ccc.InfrastructureTest.Helper;
+using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.InfrastructureTest.Bugs
 {
-    [TestFixture]
-    public class Bug7871 : DatabaseTest
-    {
-        [Test]
-        public void CanDeleteAndAddTheSameTeamToAvailableData()
-        {
-            //setup
-            ISite site = new Site("Site");
-            PersistAndRemoveFromUnitOfWork(site);
-            ITeam team1 = TeamFactory.CreateSimpleTeam("Team1");
-            site.AddTeam(team1);
-            PersistAndRemoveFromUnitOfWork(team1);
-            IApplicationRole applicationRole = ApplicationRoleFactory.CreateRole("Role", "Role");
-            PersistAndRemoveFromUnitOfWork(applicationRole);
-            IAvailableData availableData = new AvailableData();
-            availableData.ApplicationRole = applicationRole;
-            availableData.AvailableDataRange = AvailableDataRangeOption.MySite;
-            PersistAndRemoveFromUnitOfWork(availableData);
+	[DatabaseTest]
+	public class Bug7871
+	{
+		public ISiteRepository SiteRepository;
+		public ITeamRepository TeamRepository;
+		public IApplicationRoleRepository ApplicationRoleRepository;
+		public IAvailableDataRepository AvailableDataRepository;
 
-            //do it
-            AvailableDataRepository rep = new AvailableDataRepository(UnitOfWork);
-            IAvailableData a = rep.Get(availableData.Id.Value);
-            a.AddAvailableTeam(team1);
-            a.DeleteAvailableTeam(team1);
-            a.AddAvailableTeam(team1);
-            Session.Flush();
-        }
+		[Test]
+		public void CanDeleteAndAddTheSameTeamToAvailableData()
+		{
+			var team1 = TeamFactory.CreateSimpleTeam("Team1");
+			var applicationRole = ApplicationRoleFactory.CreateRole("Role", "Role");
+			var availableData = new AvailableData
+			{
+				ApplicationRole = applicationRole,
+				AvailableDataRange = AvailableDataRangeOption.MySite
+			};
+			using (var setupUow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			{
+				var site = new Site("Site");
+				SiteRepository.Add(site);
+				site.AddTeam(team1);
+				TeamRepository.Add(team1);
+				ApplicationRoleRepository.Add(applicationRole);
+				AvailableDataRepository.Add(availableData);
+				setupUow.PersistAll();
+			}
 
-    }
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			{
+				var a = AvailableDataRepository.Get(availableData.Id.Value);
+				a.AddAvailableTeam(team1);
+				a.DeleteAvailableTeam(team1);
+				a.AddAvailableTeam(team1);
+				uow.PersistAll();
+			}
+		}
+	}
 }
