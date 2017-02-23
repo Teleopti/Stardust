6054cc2f045d563cb9ce6d299bddd7cbf1d7aa78
Jianfeng <none@none>
2017-02-23 12:58:51 +0800

    #43178: make date range picker consistent

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/reports/leaderboard.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/reports/leaderboard.controller.js
@@ -1,119 +1,64 @@
-﻿(function () {
+﻿(function() {
 	'use strict';
 
-	angular.module('wfm.reports').controller('LeaderBoardController', LeaderBoardCtrl);
+	angular.module('wfm.reports')
+		.controller('LeaderBoardController', ['$scope', 'LeaderBoardService', 'Toggle', 'LeaderBoardViewModelFactory', LeaderBoardCtrl]);
 
-	LeaderBoardCtrl.$inject = ['LeaderBoardService', 'Toggle', 'LeaderBoardViewModelFactory'];
-
-	function LeaderBoardCtrl(LeaderBoardSvc, ToggleSvc, VMFactory) {
+	function LeaderBoardCtrl($scope, LeaderBoardSvc, ToggleSvc, LeaderBoardViewModelFactory) {
 		var vm = this;
 		vm.isLoading = true;
 		vm.selectedDate = new Date();
-		vm.dateRangePickerTemplateType = "inline";
-		vm.toggleDateRangePicker = false;
 		vm.showInputTimeErrorMessage = false;
 		vm.isLeaderBoardEnabled = ToggleSvc.WfmReportPortal_LeaderBoard_39440;
 
 		vm.searchOptions = {
 			keyword: '',
 			searchKeywordChanged: false,
-			searchFields : [
+			focusingSearch: false,
+			searchFields: [
 				'FirstName', 'LastName', 'EmploymentNumber', 'Organization', 'Role', 'Contract', 'ContractSchedule', 'ShiftBags',
 				'PartTimePercentage', 'Skill', 'BudgetGroup', 'Note'
 			]
 		};
 
 		vm.selectedPeriod = {
-			startDate: new Date(moment(vm.selectedDate).subtract(30,'days')),
+			startDate: new Date(moment(vm.selectedDate).subtract(30, 'days')),
 			endDate: new Date(moment(vm.selectedDate).subtract(1, 'days'))
 		};
-
-		vm.shortDateFormatStart = moment(vm.selectedPeriod.startDate).format('YYYY-MM-DD');
-		vm.shortDateFormatEnd = moment(vm.selectedPeriod.endDate).format('YYYY-MM-DD');
-
-		vm.toggleDateRangePickerFn = function($event, status){
-			$event.stopPropagation();
-			vm.toggleDateRangePicker = !vm.toggleDateRangePicker;		
-			if(vm.toggleDateRangePicker && !vm.selectedPeriod){
-				vm.selectedPeriod = {
-					startDate: new Date(vm.shortDateFormatStart), 
-					endDate: new Date(vm.shortDateFormatEnd) 
-				};
-			}
-
-			checkInputTimeErrorMessageStatus();
-		};
-
-		vm.toggleOffDateRangePickerFn = function(){
-			vm.toggleDateRangePicker = false;
-			checkInputTimeErrorMessageStatus();
-		};
-
-		vm.afterShortDateStringChange = function() {
-			var start = new Date(vm.shortDateFormatStart);
-			var end = new Date(vm.shortDateFormatEnd);
-
-			if((!isNaN(start.getTime()) && start.getTime() > 0) && (!isNaN(end.getTime()) && end.getTime() > 0)){
-				vm.selectedPeriod = {
-					startDate: start,
-					endDate: end
-				};
-				vm.afterSelectedDateChange();
-			}
-
-			checkInputTimeErrorMessageStatus();
-		};
-
-		function checkInputTimeErrorMessageStatus(){
-			var start = new Date(vm.shortDateFormatStart);
-			var end = new Date(vm.shortDateFormatEnd);
-
-			if(vm.toggleDateRangePicker){
-				vm.showInputTimeInvalidErrorMessage = false;
-				vm.showInputTimeFormatErrorMessage = false;
-			}else{
-				vm.showInputTimeInvalidErrorMessage = start > end
-				vm.showInputTimeFormatErrorMessage = (isNaN(start.getTime()) || start.getTime() <= 0) || (isNaN(end.getTime()) || end.getTime() <= 0);
-			}
-		};
+		vm.dateRangePickerTemplateType = 'popup';
 
 		vm.afterSelectedDateChange = function() {
-			if (vm.selectedPeriod && vm.selectedPeriod.startDate <= vm.selectedPeriod.endDate) {
-				vm.isLoading = true;
-				LeaderBoardSvc.getLeaderBoardDataByPeriod(vm.searchOptions.keyword, vm.selectedPeriod).then(function(data) {
-					vm.leaderBoardTableList = VMFactory.Create(data.AgentBadges);
-					vm.isLoading = false;
-				});
-				vm.shortDateFormatStart = moment(vm.selectedPeriod.startDate).format('YYYY-MM-DD');
-				vm.shortDateFormatEnd = moment(vm.selectedPeriod.endDate).format('YYYY-MM-DD');
-			}
+			vm.loadLeaderBoardData();
 		};
 
-		vm.onKeyWordInSearchInputChanged = function () {
+		vm.onKeyWordInSearchInputChanged = function() {
+			vm.loadLeaderBoardData();
+		};
+
+		vm.loadLeaderBoardData = function() {
 			if (vm.selectedPeriod && vm.selectedPeriod.startDate <= vm.selectedPeriod.endDate) {
-				vm.isLoading = true;
-				LeaderBoardSvc.getLeaderBoardDataByPeriod(vm.searchOptions.keyword, vm.selectedPeriod).then(function(data) {
-					vm.searchOptions.keyword = data.Keyword;
-					vm.leaderBoardTableList = VMFactory.Create(data.AgentBadges);
-					vm.isLoading = false;
-				});
-			}else{
-				vm.selectedPeriod = {
-					startDate: new Date(vm.shortDateFormatStart),
-					endDate: new Date(vm.shortDateFormatEnd)
+				var period = {
+					startDate: moment(vm.selectedPeriod.startDate).format('YYYY-MM-DD'),
+					endDate: moment(vm.selectedPeriod.endDate).format('YYYY-MM-DD')
 				};
-				LeaderBoardSvc.getLeaderBoardDataByPeriod(vm.searchOptions.keyword, vm.selectedPeriod).then(function(data) {
-					vm.leaderBoardTableList = VMFactory.Create(data.AgentBadges);
+
+				vm.isLoading = true;
+				vm.searchOptions.focusingSearch = false;
+				LeaderBoardSvc.getLeaderBoardDataByPeriod(vm.searchOptions.keyword, period).then(function(data) {
+					vm.searchOptions.keyword = data.Keyword;
+					vm.leaderBoardTableList = LeaderBoardViewModelFactory.Create(data.AgentBadges);
 					vm.isLoading = false;
 				});
 			}
+
 		};
 
-		vm.init = function () {
-			LeaderBoardSvc.getLeaderBoardDataByPeriod(vm.searchOptions.keyword, vm.selectedPeriod).then(function (data) {
-				vm.searchOptions.keyword = data.Keyword;
-				vm.leaderBoardTableList = VMFactory.Create(data.AgentBadges);
-				vm.isLoading = false;
+		vm.init = function() {
+			$scope.$watch(function() {
+				return vm.selectedPeriod;
+			}, function(newValue, oldValue) {
+				if (newValue)
+					vm.afterSelectedDateChange();
 			});
 		};
 
