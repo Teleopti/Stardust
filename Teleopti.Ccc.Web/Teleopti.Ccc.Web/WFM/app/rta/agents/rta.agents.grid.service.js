ad3a375923a2cf573ac011f31a53da8b9ee567e9
Sabbir <none@none>
2017-02-27 16:50:57 +0100

    #43167 Fix

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/agents/rta.agents.grid.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/agents/rta.agents.grid.service.js
@@ -31,11 +31,11 @@
 				rowTemplate = 'app/rta/agents/rta-agents-rowtemplate-AllOutOfAdherences_39146.html';
 			var coloredCellTemplate = '<div class="ui-grid-cell-contents">{{COL_FIELD}}</div>';
 			var alarmCellTemplate = '<div class="ui-grid-cell-contents"><div class="label rta-label" ng-attr-style="font-size: 14px; color: white; background-color: {{grid.appScope.vm.hexToRgb(row.entity.Color)}}">{{COL_FIELD}}</div></div>';
-			
+
 			var shiftHeaderTemplate_htmlTemplatesHaveTimingIssues = '<div class="shift-cell-header rta-default-cursor"><div col-index="renderIndex" title="TOOLTIP">'
 				+ '<label ng-repeat="row in grid.appScope.vm.timeline" class="label label-info"'
 				+ 'ng-attr-style="position: absolute; left: {{row.Offset}}">{{row.Time}}</label></div></div>';
-				
+
 			var cellHeaderTemplate_htmlTemplatesHaveTimingIssues = '<div role="columnheader" ng-class="{ \'sortable\': sortable }" ui-grid-one-bind-aria-labelledby-grid="col.uid + \'-header-text \' + col.uid + \'-sortdir-text\'"'
 				+ 'aria-sort="{{col.sort.direction == asc ? \'ascending\' : ( col.sort.direction == desc ? \'descending\' : (!col.sort.direction ? \'none\' : \'other\'))}}">'
 				+ '<div role="button" tabindex="0" class="ui-grid-cell-contents ui-grid-header-cell-primary-focus" col-index="renderIndex" title="TOOLTIP" style="width: 84%">'
@@ -47,7 +47,7 @@
 				+ '<div role="button" tabindex="0" ui-grid-one-bind-id-grid="col.uid + \'-menu-button\'" class="ui-grid-column-menu-button" ng-if="grid.options.enableColumnMenus && !col.isRowHeader  && col.colDef.enableColumnMenu !== false"'
 				+ 'ng-click="toggleMenu($event)" ng-class=" {\'ui-grid-column-menu-button-last-col\': isLastCol}" ui-grid-one-bind-aria-label="i18n.headerCell.aria.columnMenuButtonLabel" aria-haspopup="true">'
 				+ '<i class="ui-grid-icon-angle-down" aria-hidden="true">&nbsp;</i></div><div ui-grid-filter></div></div></div>';
-				
+
 			var columnDefs = [];
 
 			var name = {
@@ -130,8 +130,45 @@
 				enableGridMenu: true,
 				enableColumnMenus: true,
 				enableColumnResizing: true,
-				enableSorting: !alarmOnly
+				enableSorting: !alarmOnly,
+				onRegisterApi: function (gridApi) {
+					if (!alarmOnly)
+						gridApi.core.on.sortChanged(null, function (grid, sortColumns) {
+							var sortColumnsFilteredByPriority = sortColumns.filter(function (col) { return angular.isDefined(col.sort) && angular.isDefined(col.sort.priority); })
+							updateSortPriority(sortColumnsFilteredByPriority, false);
+							var priority = findMinPriorityForRereprioritize(sortColumnsFilteredByPriority);
+							if( priority > 0)
+								updateSortPriority(sortColumnsFilteredByPriority, true, priority);					
+						});
+				}
 			};
 		};
+		
+		function updateSortPriority(columns, setIfExceedsMinPriority, minPriority) {
+			var priorityArray = columns.map(function (col) { return col.sort.priority; });
+			minPriority = minPriority || Math.min.apply(Math, priorityArray);
+
+			columns.forEach(function (col) {
+				if(setIfExceedsMinPriority){
+					if(col.sort.priority > minPriority)
+						col.sort.priority = col.sort.priority - 1;
+				}
+				else
+					col.sort.priority = col.sort.priority - minPriority;
+			});
+		}
+
+		function findMinPriorityForRereprioritize(columns) {
+			if (columns.length < 2)
+				return 0;
+			var i = 0;
+			var priorityArray = columns.map(function (col) { return col.sort.priority; }).sort();
+			while (i <= priorityArray.length - 0){
+				if (priorityArray[i + 1] - priorityArray[i] > 1)
+					return i + 1;
+				i++;
+			}
+			return 0;
+		}
 	};
 })();
