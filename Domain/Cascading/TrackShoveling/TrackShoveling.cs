07a3e852ad9d15c73f2cb3c13876a51a3d74edc7
Roger Kratz <roger.kratz@teleopti.com>
2017-02-27 09:50:59 +0100

    #43186 Now supports tracking multiple skills

---
+++ Domain/Cascading/TrackShoveling/TrackShoveling.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Cascading.TrackShoveling
+{
+	public class TrackShoveling : IShovelingCallback
+	{
+		private readonly IDictionary<ISkill, TrackShovelingOneSkill> _trackShovelingOneSkills;
+
+		public TrackShoveling(IEnumerable<ISkill> skills, DateTimePeriod interval)
+		{
+			_trackShovelingOneSkills = new Dictionary<ISkill, TrackShovelingOneSkill>();
+			foreach (var skill in skills)
+			{
+				_trackShovelingOneSkills[skill] = new TrackShovelingOneSkill(skill, interval);
+			}
+		}
+
+		public TrackShovelingOneSkill For(ISkill skill)
+		{
+			return _trackShovelingOneSkills[skill];
+		}
+
+		void IShovelingCallback.ResourcesWasMovedTo(ISkill skillToMoveTo, DateTimePeriod interval, IEnumerable<CascadingSkillGroup> skillGroups, CascadingSkillGroup fromSkillGroup, double resources)
+		{
+			foreach (IShovelingCallback trackShovelingOneSkill in _trackShovelingOneSkills.Values)
+			{
+				trackShovelingOneSkill.ResourcesWasMovedTo(skillToMoveTo, interval, skillGroups, fromSkillGroup, resources);
+			}
+		}
+
+		void IShovelingCallback.ResourcesWasRemovedFrom(ISkill primarySkill, DateTimePeriod interval, IEnumerable<CascadingSkillGroup> skillGroups, double resources)
+		{
+			foreach (IShovelingCallback trackShovelingOneSkill in _trackShovelingOneSkills.Values)
+			{
+				trackShovelingOneSkill.ResourcesWasRemovedFrom(primarySkill, interval, skillGroups, resources);
+			}
+		}
+
+		void IShovelingCallback.BeforeShoveling(IShovelResourceData shovelResourceData)
+		{
+			foreach (IShovelingCallback trackShovelingOneSkill in _trackShovelingOneSkills.Values)
+			{
+				trackShovelingOneSkill.BeforeShoveling(shovelResourceData);
+			}
+		}
+	}
+}
--- DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationTrackShovelingTest.cs
+++ DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationTrackShovelingTest.cs
@@ -108,5 +108,53 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
 			trackShoveling.ResourcesBeforeShoveling
 				.Should().Be.EqualTo(2);
 		}
+
+		[Test]
+		public void ShouldTrackMultipleSkills()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = new DateOnly(2000, 1, 1);
+			var primarySkill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(1).IsOpenBetween(8, 9);
+			var primarySkillDay = primarySkill.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var subskill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 9);
+			var subSkillDay = subskill.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var agent1 = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(primarySkill, subskill);
+			var ass1 = new PersonAssignment(agent1, scenario, dateOnly).WithLayer(activity, new TimePeriod(5, 10));
+			var agent2 = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(primarySkill, subskill);
+			var ass2 = new PersonAssignment(agent2, scenario, dateOnly).WithLayer(activity, new TimePeriod(5, 10));
+			var trackShoveling = new TrackShoveling(new[] {primarySkill, subskill}, new DateTimePeriod(new DateTime(2000, 1, 1, 8, 0, 0, DateTimeKind.Utc), new DateTime(2000, 1, 1, 8, 15, 0, 0, DateTimeKind.Utc)));
+
+			Target.ResourceCalculate(dateOnly, ResourceCalculationDataCreator.WithData(trackShoveling, scenario, dateOnly, new[] { ass1, ass2 }, new[] { primarySkillDay, subSkillDay }, false, false));
+
+			trackShoveling.For(primarySkill).RemovedResources.Single()
+				.ResourcesMoved.Should().Be.EqualTo(1);
+			trackShoveling.For(subskill).AddedResources.Single()
+				.ResourcesMoved.Should().Be.EqualTo(1);
+		}
+
+		[Test]
+		public void ShouldTrackResourcesBeforeShovelingMultipleSkills()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = new DateOnly(2000, 1, 1);
+			var primarySkill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(1).IsOpenBetween(7, 8);
+			var primarySkillDay = primarySkill.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var subskill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(7, 8);
+			var subSkillDay = subskill.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var agent1 = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(primarySkill, subskill);
+			var ass1 = new PersonAssignment(agent1, scenario, dateOnly).WithLayer(activity, new TimePeriod(5, 10));
+			var agent2 = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(primarySkill, subskill);
+			var ass2 = new PersonAssignment(agent2, scenario, dateOnly).WithLayer(activity, new TimePeriod(5, 10));
+			var trackShoveling = new TrackShoveling(new[] { primarySkill, subskill }, new DateTimePeriod(new DateTime(2000, 1, 1, 7, 30, 0, DateTimeKind.Utc), new DateTime(2000, 1, 1, 7, 45, 0, 0, DateTimeKind.Utc)));
+
+			Target.ResourceCalculate(dateOnly, ResourceCalculationDataCreator.WithData(trackShoveling, scenario, dateOnly, new[] { ass1, ass2 }, new[] { primarySkillDay, subSkillDay }, false, false));
+
+			trackShoveling.For(primarySkill).ResourcesBeforeShoveling
+				.Should().Be.EqualTo(2);
+			trackShoveling.For(subskill).ResourcesBeforeShoveling
+				.Should().Be.EqualTo(0);
+		}
 	}
 }
