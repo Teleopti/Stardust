c7b9007dc286d95c7eadae33ba03a5982651878d
Roger Kratz <roger.kratz@teleopti.com>
2017-02-28 14:57:23 +0100

    refactor: one file per type

--- Teleopti.Messaging/Teleopti.Interfaces/Domain/TimeHelper.cs
+++ Teleopti.Messaging/Teleopti.Interfaces/Domain/TimeHelper.cs
@@ -6,72 +6,6 @@ using System.Text.RegularExpressions;
 namespace Teleopti.Interfaces.Domain
 {
 	/// <summary>
-	/// Time formatter
-	/// </summary>
-	public interface ITimeFormatter
-	{
-		
-		/// <summary>
-		/// TimeSpan to xx:yy format, xx hours, yy minutes
-		/// </summary>
-		/// <param name="timeSpan">TimeSpan want to format</param>
-		/// <returns></returns>
-		string GetLongHourMinuteTimeString(TimeSpan timeSpan);
-	}
-
-	/// <summary>
-	/// Time formatter
-	/// </summary>
-	public class TimeFormatter : ITimeFormatter
-	{
-		private readonly IUserCulture _culture;
-
-		/// <summary>
-		/// constructor
-		/// </summary>
-		/// <param name="culture">time culture</param>
-		public TimeFormatter(IUserCulture culture) {
-			_culture = culture;
-		}
-
-		/// <summary>
-		/// TimeSpan to xx:yy format, xx hours, yy minutes
-		/// </summary>
-		/// <param name="timeSpan">TimeSpan want to format</param>
-		/// <returns></returns>
-		public string GetLongHourMinuteTimeString(TimeSpan timeSpan)
-		{
-			string separator = _culture.GetCulture().DateTimeFormat.TimeSeparator;
-			
-            string signChar = string.Empty;
-            if (timeSpan < TimeSpan.Zero)
-            {
-                signChar = "-";
-                timeSpan = timeSpan.Negate();
-            }
-
-            int hour = (int)timeSpan.TotalHours;
-            int minutes = timeSpan.Minutes;
-
-            if (timeSpan.Seconds >= 30)
-				minutes += 1;
-			if (minutes == 60)
-			{
-				hour++;
-				minutes = 0;
-			}
-
-			string min = Convert.ToString(minutes, CultureInfo.CurrentCulture);
-			string hours = Convert.ToString(hour, CultureInfo.CurrentCulture);
-
-			if (min.Length == 1)
-				min = "0" + min;
-
-			return string.Concat(signChar, hours, separator, min);
-		}
-
-	}
-    /// <summary>
     /// Time helper class with utilities for time handling
     /// </summary>
     /// <remarks>
@@ -624,21 +558,4 @@ namespace Teleopti.Interfaces.Domain
             return CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern.Contains("H");
         }
     }
-
-
-	/// <summary>
-	/// Return value of ParseTimeOfDayFromTimeSpan method
-	/// </summary>
-	public class ParsedTimeOfDay
-	{
-		/// <summary>
-		/// Days that the time of day timespan included. 1 = Next day
-		/// </summary>
-		public int Days { get; set; }
-		/// <summary>
-		/// The time of day the timespan included
-		/// </summary>
-		public TimeSpan TimeOfDay { get; set; }
-	}
-
 }
