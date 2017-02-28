213722b5095c74e1718cf44da1a111d1275b4cc4
Roger Kratz <roger.kratz@teleopti.com>
2017-02-28 12:38:21 +0100

    Fixing build

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Common/DataTransferObject/Commands/EmployPersonCommandDto.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Common/DataTransferObject/Commands/EmployPersonCommandDto.cs
@@ -1,28 +1,27 @@
 ﻿using System.Runtime.Serialization;
 using Teleopti.Ccc.Sdk.Common.Contracts;
-using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Sdk.Common.DataTransferObject.Commands
 {
     /// <summary>
-	/// This command adds a new person period to an existing <see cref="IPerson"/> and updates the <see cref="IPerson"/> with new details or creates a new <see cref="IPerson"/> if the Id of <see cref="PersonDto"/> is set to null. The person period is created according to the specified <see cref="Period"/>, <see cref="PersonContract"/> and <see cref="Team"/>. 
+	/// This command adds a new person period to an existing person and updates the person with new details or creates a new person if the Id of <see cref="PersonDto"/> is set to null. The person period is created according to the specified <see cref="Period"/>, <see cref="PersonContract"/> and <see cref="Team"/>. 
     /// </summary>
 	/// <remarks>This command requires the user to have permissions to open the People module.</remarks>
 	[DataContract(Namespace = "http://schemas.ccc.teleopti.com/sdk/2011/07/")]
     public class EmployPersonCommandDto : CommandDto
-    {  
-        /// <summary>
-		/// Gets or sets the mandatory person. The entire <see cref="PersonDto"/> is required as the details are used to update or create a <see cref="IPerson"/>.
-        /// </summary>
-        /// <value>The PersonDto.</value>
-        [DataMember(IsRequired = true)]
+    {
+		/// <summary>
+		/// Gets or sets the mandatory person. The entire <see cref="PersonDto"/> is required as the details are used to update or create a person.
+		/// </summary>
+		/// <value>The PersonDto.</value>
+		[DataMember(IsRequired = true)]
         public PersonDto Person { get; set; }
 
         /// <summary>
         /// Gets or sets the mandatory period.
         /// </summary>
         /// <value>The period.</value>
-        /// <remarks>Currently only the start date is used, as it will be the start date of the <see cref="IPersonPeriod"/>.</remarks>
+        /// <remarks>Currently only the start date is used, as it will be the start date of the personperiod.</remarks>
         [DataMember(IsRequired = true)]
         public DateOnlyPeriodDto Period { get; set; }
 
