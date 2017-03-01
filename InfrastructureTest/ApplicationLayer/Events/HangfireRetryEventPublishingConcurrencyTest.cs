d68c86cffda0ed5d9cd6c019ad56a14eab550bd7
Justin! <justin@teleopti.com>
2017-03-01 20:40:06 +0100

    #43194 Failing test

---
+++ InfrastructureTest/ApplicationLayer/Events/HangfireRetryEventPublishingConcurrencyTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Infrastructure.Hangfire;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.IoC;
+
+namespace Teleopti.Ccc.InfrastructureTest.ApplicationLayer.Events
+{
+	[TestFixture]
+	[HangfireTest]
+	public class HangfireRetryEventPublishingConcurrencyTest : ISetup
+	{
+		public HangfireUtilities Hangfire;
+		public IEventPublisher Publisher;
+		public FailingHandlerImpl FailingHandler;
+		public ConcurrencyRunner Run;
+
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.AddService<FailingHandlerImpl>();
+		}
+
+		[Test]
+		public void ShouldRetryDifferentAmountOfTimesConcurrently()
+		{
+			10.Times(() =>
+			{
+				Publisher.Publish(new ThreeRetryEvent());
+				Publisher.Publish(new TenRetryEvent());
+			});
+
+			while (Hangfire.NumberOfEnqueuedJobs() > 0)
+			{
+				var workers = (int)Math.Min(Hangfire.NumberOfEnqueuedJobs(), 8);
+				workers.Times(() =>
+				{
+					Run.InParallel(() =>
+					{
+						Hangfire.WorkerIteration();
+					});
+				});
+				Run.Wait();
+
+				Hangfire.RequeueScheduledJobs();
+			}
+
+			FailingHandler.Attempts.Should().Be(130);
+		}
+
+		public class ThreeRetryEvent : IEvent
+		{
+		}
+
+		public class TenRetryEvent : IEvent
+		{
+		}
+
+		public class FailingHandlerImpl :
+			IHandleEvent<ThreeRetryEvent>,
+			IHandleEvent<TenRetryEvent>,
+			IRunOnHangfire
+		{
+
+			public int Attempts = 0;
+
+			[Attempts(3)]
+			public void Handle(ThreeRetryEvent @event)
+			{
+				Interlocked.Increment(ref Attempts);
+				throw new Exception("fail!");
+			}
+
+			[Attempts(10)]
+			public void Handle(TenRetryEvent @event)
+			{
+				Interlocked.Increment(ref Attempts);
+				throw new Exception("fail!");
+			}
+		}
+
+	}
+}
