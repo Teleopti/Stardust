5657806439d953071b072848644086e4dab7140d
Claes Stenman <claes.stenman@teleopti.com>
2017-03-06 13:11:06 +0100

    #43299 Ignored tests

--- DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationTest.cs
+++ DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationTest.cs
@@ -340,6 +340,60 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
 
 		[Test]
 		[Ignore("#43299")]
+		public void ShouldShovelResourcesFromClosedPrimarySkillToSubskillWithDemandBeforeSubskillWithNoDemand()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var primarySkill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(1).IsOpenBetween(8, 9);
+			var subskill1 = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 10);
+			var subskill2 = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 10);
+			var primarySkillDay = primarySkill.CreateSkillDayWithDemand(scenario, dateOnly, 100);
+			var subskill1Day = subskill1.CreateSkillDayWithDemand(scenario, dateOnly, 0);
+			var subskill2Day = subskill2.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(primarySkill, subskill1, subskill2);
+			var ass = new PersonAssignment(agent, scenario, dateOnly).WithLayer(activity, new TimePeriod(9, 10));
+			var agentOnSubskill = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(subskill1);
+			var assOnSubskill = new PersonAssignment(agentOnSubskill, scenario, dateOnly).WithLayer(activity, new TimePeriod(9, 10));
+
+			Target.ResourceCalculate(dateOnly, ResourceCalculationDataCreator.WithData(scenario, dateOnly, new[] { ass, assOnSubskill }, new[] { primarySkillDay, subskill1Day, subskill2Day }, false, false));
+
+			var skillDay1Resources = subskill1Day.SkillStaffPeriodCollection.Last().CalculatedResource;
+			var skillDay2Resources = subskill2Day.SkillStaffPeriodCollection.Last().CalculatedResource;
+			(skillDay1Resources + skillDay2Resources).Should().Be.EqualTo(2);
+			skillDay1Resources.Should().Be.EqualTo(1);
+			skillDay2Resources.Should().Be.EqualTo(1);
+		}
+
+		[Test]
+		[Ignore("#43299")]
+		public void ShouldShovelResourcesFromClosedPrimarySkill_ResultingInAllSubSkillsOverStaffed()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var primarySkill = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(1).IsOpenBetween(8, 9);
+			var subskill1 = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 10);
+			var subskill2 = new Skill("_").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 10);
+			var primarySkillDay = primarySkill.CreateSkillDayWithDemand(scenario, dateOnly, 100);
+			var subskill1Day = subskill1.CreateSkillDayWithDemand(scenario, dateOnly, 0.9); //0.1 overstaffed
+			var subskill2Day = subskill2.CreateSkillDayWithDemand(scenario, dateOnly, 0.1);	//0.1 understaffed
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(primarySkill, subskill1, subskill2);
+			var ass = new PersonAssignment(agent, scenario, dateOnly).WithLayer(activity, new TimePeriod(9, 10));
+			var agentOnSubskill = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(subskill1);
+			var assOnSubskill = new PersonAssignment(agentOnSubskill, scenario, dateOnly).WithLayer(activity, new TimePeriod(9, 10));
+
+			Target.ResourceCalculate(dateOnly, ResourceCalculationDataCreator.WithData(scenario, dateOnly, new[] { ass, assOnSubskill }, new[] { primarySkillDay, subskill1Day, subskill2Day }, false, false));
+
+			var skillDay1Resources = subskill1Day.SkillStaffPeriodCollection.Last().CalculatedResource;
+			var skillDay2Resources = subskill2Day.SkillStaffPeriodCollection.Last().CalculatedResource;
+			(skillDay1Resources + skillDay2Resources).Should().Be.EqualTo(2);
+			skillDay1Resources.Should().Be.GreaterThan(1.4);
+			skillDay2Resources.Should().Be.GreaterThan(0.5);
+		}
+
+		[Test]
+		[Ignore("#43299")]
 		public void ShouldShovelAllResourcesFromClosedPrimaryToSubSkillEvenIfNoneIsUnderstaffed()
 		{
 			var scenario = new Scenario("_");
