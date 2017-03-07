2ae243c95eaf76b80c4ff7053dc08527a8ca47cd
amandaj <none@none>
2017-03-07 10:58:27 +0100

    perf test for staffingviewmodelcreator

--- Teleopti.Ccc.Staffing.PerformanceTest/ResourceCalculate3DaysTest.cs
+++ Teleopti.Ccc.Staffing.PerformanceTest/ResourceCalculate3DaysTest.cs
@@ -8,6 +8,8 @@ using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.Config;
 using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Intraday;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.ResourceCalculation;
@@ -30,8 +32,10 @@ namespace Teleopti.Ccc.Staffing.PerformanceTest
 		public IConfigReader ConfigReader;
 		public ISkillStaffingIntervalProvider SkillStaffingIntervalProvider;
 		public ISkillCombinationResourceRepository SkillCombinationResourceRepository;
+		public IStaffingViewModelCreator StaffingViewModelCreator;
+		public ISkillRepository SkillRepository;
 
-		private IEnumerable<SkillCombinationResource> skillCombinationResources;
+		private IEnumerable<ISkill> skills;
 
 		public override void OneTimeSetUp()
 		{
@@ -42,35 +46,36 @@ namespace Teleopti.Ccc.Staffing.PerformanceTest
 			var now = Now.UtcDateTime();
 			var period = new DateTimePeriod(now.AddHours(-1), now.AddDays(3));
 			WithUnitOfWork.Do(() =>
-			{
+							  {
 
-				using (var connection = new SqlConnection(ConfigReader.ConnectionString("Tenancy")))
-				{
-					connection.Open();
+								  using (var connection = new SqlConnection(ConfigReader.ConnectionString("Tenancy")))
+								  {
+									  connection.Open();
 
-					using (var command = new SqlCommand(@"truncate table readmodel.SkillCombinationResource", connection))
-					{
-						command.ExecuteNonQuery();
-					}
-					using (var command = new SqlCommand(@"truncate table readmodel.SkillCombinationResourceDelta", connection))
-					{
-						command.ExecuteNonQuery();
-					}
-					using (var command = new SqlCommand(@"truncate table readmodel.SkillCombinationResource", connection))
-					{
-						command.ExecuteNonQuery();
-					}
-					using (var command = new SqlCommand(@"truncate table readmodel.ScheduleForecastSkill", connection))
-					{
-						command.ExecuteNonQuery();
-					}
-					using (var command = new SqlCommand(@"truncate table readmodel.ScheduleForecastSkillChange", connection))
-					{
-						command.ExecuteNonQuery();
-					}
-				}
-				UpdateStaffingLevel.Update(period);
-			});
+									  using (var command = new SqlCommand(@"truncate table readmodel.SkillCombinationResource", connection))
+									  {
+										  command.ExecuteNonQuery();
+									  }
+									  using (var command = new SqlCommand(@"truncate table readmodel.SkillCombinationResourceDelta", connection))
+									  {
+										  command.ExecuteNonQuery();
+									  }
+									  using (var command = new SqlCommand(@"truncate table readmodel.SkillCombinationResource", connection))
+									  {
+										  command.ExecuteNonQuery();
+									  }
+									  using (var command = new SqlCommand(@"truncate table readmodel.ScheduleForecastSkill", connection))
+									  {
+										  command.ExecuteNonQuery();
+									  }
+									  using (var command = new SqlCommand(@"truncate table readmodel.ScheduleForecastSkillChange", connection))
+									  {
+										  command.ExecuteNonQuery();
+									  }
+								  }
+								  skills = SkillRepository.LoadAllSkills();
+								  UpdateStaffingLevel.Update(period);
+							  });
 		}
 
 		[Test]
@@ -83,10 +88,62 @@ namespace Teleopti.Ccc.Staffing.PerformanceTest
 
 			var period = new DateTimePeriod(now, now.AddDays(3));
 			WithUnitOfWork.Do(() =>
-			{
-				skillCombinationResources = SkillCombinationResourceRepository.LoadSkillCombinationResources(period);
-				SkillStaffingIntervalProvider.GetSkillStaffIntervalsAllSkills(period, skillCombinationResources.ToList(), true); //with shrinkage
-			}); 
+							  {
+								  var skillCombinationResources = SkillCombinationResourceRepository.LoadSkillCombinationResources(period);
+								  var result = SkillStaffingIntervalProvider.GetSkillStaffIntervalsAllSkills(period, skillCombinationResources.ToList(), true); //with shrinkage
+								  Assert.Greater(result.Count, 0);
+							  });
+		}
+
+		[Test]
+		public void ResourceCalculate3DaysWithoutShrinkage()
+		{
+			Now.Is("2016-03-07 07:00");
+			var now = Now.UtcDateTime();
+			using (DataSource.OnThisThreadUse("Teleopti WFM"))
+				AsSystem.Logon("Teleopti WFM", new Guid("1fa1f97c-ebff-4379-b5f9-a11c00f0f02b"));
+
+			var period = new DateTimePeriod(now, now.AddDays(3));
+			WithUnitOfWork.Do(() =>
+							  {
+								  var skillCombinationResources = SkillCombinationResourceRepository.LoadSkillCombinationResources(period);
+								  var result = SkillStaffingIntervalProvider.GetSkillStaffIntervalsAllSkills(period, skillCombinationResources.ToList(), false);
+								  Assert.Greater(result.Count, 0);
+							  });
+		}
+
+
+
+		[Test]
+		public void GetForecastAndStaffingInfoForTodayWithoutShrinkage()
+		{
+			Now.Is("2016-03-07 07:00");
+			using (DataSource.OnThisThreadUse("Teleopti WFM"))
+				AsSystem.Logon("Teleopti WFM", new Guid("1fa1f97c-ebff-4379-b5f9-a11c00f0f02b"));
+			WithUnitOfWork.Do(() =>
+							  {
+								  var result = StaffingViewModelCreator.Load(skills.Select(x => x.Id.GetValueOrDefault()).ToArray());
+								  Assert.Greater(result.DataSeries.Time.Length, 0);
+								  Assert.Greater(result.DataSeries.ForecastedStaffing.Length, 0);
+								  Assert.Greater(result.DataSeries.ScheduledStaffing.Length, 0);
+								  Assert.Greater(result.DataSeries.ActualStaffing.Length, 0);
+							  });
+		}
+
+		[Test]
+		public void GetForecastAndStaffingInfoForTodayWithShrinkage()
+		{
+			Now.Is("2016-03-07 07:00");
+			using (DataSource.OnThisThreadUse("Teleopti WFM"))
+				AsSystem.Logon("Teleopti WFM", new Guid("1fa1f97c-ebff-4379-b5f9-a11c00f0f02b"));
+			WithUnitOfWork.Do(() =>
+							  {
+								  var result = StaffingViewModelCreator.Load(skills.Select(x => x.Id.GetValueOrDefault()).ToArray(), true);
+								  Assert.Greater(result.DataSeries.Time.Length, 0);
+								  Assert.Greater(result.DataSeries.ForecastedStaffing.Length, 0);
+								  Assert.Greater(result.DataSeries.ScheduledStaffing.Length, 0);
+								  Assert.Greater(result.DataSeries.ActualStaffing.Length, 0);
+							  });
 		}
 
 	}
