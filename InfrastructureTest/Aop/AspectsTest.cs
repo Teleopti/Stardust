98c29097009eb80315a2b23886e826d00e750808
Justin! <justin@teleopti.com>
2017-03-08 14:31:35 +0100

    #43296 Aspect test refact

--- InfrastructureTest/Aop/AspectsTest.cs
+++ InfrastructureTest/Aop/AspectsTest.cs
@@ -5,241 +5,182 @@ using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.Aop.Core;
 using Teleopti.Ccc.Infrastructure.Aop;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon.IoC;
 
 namespace Teleopti.Ccc.InfrastructureTest.Aop
 {
 	[TestFixture]
-	public class AspectsTest
+	[InfrastructureTest]
+	public class AspectsTest : ISetup
 	{
-		private static IContainer setupContainer()
+		public void Setup(ISystem system, IIocConfiguration configuration)
 		{
-			var builder = new ContainerBuilder();
-			builder.RegisterType<AspectInterceptor>();
-			builder.RegisterType<AspectedClass>().ApplyAspects();
-			builder.RegisterType<DoubleAspectedClass>().ApplyAspects();
-			builder.RegisterType<AResolvedAspect.TheResolvedAspect>();
-			builder.RegisterType<ThrowingOnBeforeAspect.TheResolvedAspect>();
-			builder.RegisterType<ThrowingOnAfterAspect.TheResolvedAspect>();
-			return builder.Build();
+			system.AddService<AspectedService>();
+			system.AddService<Aspect1Attribute.Aspect1>();
+			system.AddService<Aspect2Attribute.Aspect2>();
+			system.AddService<Aspect3Attribute.Aspect3>();
 		}
 
-		[SetUp]
-		public void Setup()
-		{
-			AResolvedAspect.BeforeCallback = null;
-			AResolvedAspect.AfterCallback = null;
-		}
+		public AspectedService Target;
+		public Aspect1Attribute.Aspect1 Aspect1;
+		public Aspect2Attribute.Aspect2 Aspect2;
+		public Aspect3Attribute.Aspect3 Aspect3;
 
 		[Test]
 		public void ShouldInvokeAspectBeforeMethod()
 		{
-			var beforeInvoked = false;
-			var container = setupContainer();
-			AResolvedAspect.BeforeCallback = () => beforeInvoked = true;
+			Target.AspectedMethod();
 
-			container.Resolve<AspectedClass>().ResolvedAspectMethod();
-
-			beforeInvoked.Should().Be.True();
+			Aspect1.BeforeInvoked.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldInvokeAspectAfterMethod()
 		{
-			var afterInvokted = false;
-			var container = setupContainer();
-			AResolvedAspect.AfterCallback = () => afterInvokted = true;
+			Target.AspectedMethod();
 
-			container.Resolve<AspectedClass>().ResolvedAspectMethod();
-
-			afterInvokted.Should().Be.True();
+			Aspect1.AfterInvoked.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldInvokeOriginalMethod()
 		{
-			var methodInvoked = false;
-			var container = setupContainer();
+			Target.AspectedMethod();
 
-			var target = container.Resolve<AspectedClass>();
-			target.AspectedMethodCallback = () => methodInvoked = true;
-			target.ResolvedAspectMethod();
-			
-			methodInvoked.Should().Be.True();
+			Target.Invoked.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldIgnoreOtherAttributes()
 		{
-			var container = setupContainer();
-			container.Resolve<AspectedClass>().AttributedMethod();
+			Target.AttributedMethod();
 		}
 
 		[Test]
 		public void ShouldResolveAspectFromAttribute()
 		{
-			var beforeInvoked = false;
-			var afterInvoked = false;
-			var container = setupContainer();
-			AResolvedAspect.BeforeCallback = () => beforeInvoked = true;
-			AResolvedAspect.AfterCallback = () => afterInvoked = true;
+			Target.AspectedMethod();
 
-			container.Resolve<AspectedClass>().ResolvedAspectMethod();
-
-			beforeInvoked.Should().Be.True();
-			afterInvoked.Should().Be.True();
+			Aspect1.BeforeInvoked.Should().Be.True();
+			Aspect1.AfterInvoked.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldThrowOnAspectException()
 		{
-			var container = setupContainer();
-			AResolvedAspect.BeforeCallback = () => { throw new FileNotFoundException(); };
+			Aspect1.BeforeFailsWith = new FileNotFoundException();
 
-			var target = container.Resolve<AspectedClass>();
-
-			Assert.Throws<AspectInvocationException>(target.ResolvedAspectMethod);
+			Assert.Throws<AspectInvocationException>(Target.AspectedMethod);
 		}
 
 		[Test]
 		public void ShouldInvokeAspectAfterMethodEvenThoughInvokationThrows()
 		{
-			var afterInvoked = false;
-			var container = setupContainer();
-			AResolvedAspect.AfterCallback = () => afterInvoked = true;
+			Target.FailsWith = new FileNotFoundException();
 
-			var target = container.Resolve<AspectedClass>();
-			target.AspectedMethodCallback = () => { throw new FileNotFoundException(); };
-
-			Assert.Throws<FileNotFoundException>(target.ResolvedAspectMethod);
-			afterInvoked.Should().Be.True();
+			Assert.Throws<FileNotFoundException>(Target.AspectedMethod);
+			Aspect1.AfterInvoked.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldInvokeAspectAfterMethodWithExceptionFromInvokation()
 		{
 			Exception expected = new FileNotFoundException();
-			Exception actual = null;
-			var container = setupContainer();
-			AResolvedAspect.AfterCallbackWithException = e => actual = e;
+			Target.FailsWith  = expected;
 
-			var target = container.Resolve<AspectedClass>();
-			target.AspectedMethodCallback = () => { throw expected; };
+			Assert.Throws<FileNotFoundException>(Target.AspectedMethod);
 
-			Assert.Throws<FileNotFoundException>(target.ResolvedAspectMethod);
-			actual.Should().Be.SameInstanceAs(expected);
+			Aspect1.AfterInvokedWith.Should().Be.SameInstanceAs(expected);
 		}
 
 		[Test]
 		public void ShouldInvokeAllAfterInvocationMethods()
 		{
-			var afterInvoke = false;
-			var container = setupContainer();
-			AResolvedAspect.AfterCallback = () => afterInvoke = true;
+			Aspect1.AfterFailsWith = new FileNotFoundException();
 
-			var target = container.Resolve<DoubleAspectedClass>();
-
-			Assert.Throws<AspectInvocationException>(target.ThrowsAfterAspectedMethod);
-			afterInvoke.Should().Be.True();
+			Assert.Throws<AspectInvocationException>(Target.AspectedMethod);
+			Aspect2.AfterInvoked.Should().Be.True();
+			Aspect3.AfterInvoked.Should().Be.True();
 		}
 
 		[Test]
 		public void ShouldInvokeAfterInvocationForCompletedBeforeInvocationMethods()
 		{
-			var afterInvoke = false;
-			var container = setupContainer();
-			AResolvedAspect.AfterCallback = () => afterInvoke = true;
+			Aspect3.BeforeFailsWith = new FileNotFoundException();
 
-			var target = container.Resolve<DoubleAspectedClass>();
-
-			Assert.Throws<AspectInvocationException>(target.ThrowsBeforeAspectedMethod);
-			afterInvoke.Should().Be.True();
+			Assert.Throws<AspectInvocationException>(Target.AspectedMethod);
+			Aspect1.AfterInvoked.Should().Be.True();
+			Aspect2.AfterInvoked.Should().Be.True();
 		}
-
-		public class DoubleAspectedClass
+		
+		public class AspectedService
 		{
-			[AResolvedAspect]
-			[ThrowingOnBeforeAspect]
-			public virtual void ThrowsBeforeAspectedMethod() { }
-
-			[AResolvedAspect]
-			[ThrowingOnAfterAspect]
-			public virtual void ThrowsAfterAspectedMethod() { }
-		}
-
-		public class AspectedClass
-		{
-			public Action AspectedMethodCallback;
+			public bool Invoked;
+			public Exception FailsWith;
 
 			[An]
 			public virtual void AttributedMethod() { }
 
-			[AResolvedAspect]
-			public virtual void ResolvedAspectMethod()
+			[Aspect1]
+			[Aspect2]
+			[Aspect3]
+			public virtual void AspectedMethod()
 			{
-				AspectedMethodCallback?.Invoke();
+				Invoked = true;
+				if (FailsWith != null)
+					throw FailsWith;
 			}
 
 		}
 
-		private class AnAttribute : Attribute
+		public class AnAttribute : Attribute
 		{
 		}
 
-		private class AResolvedAspect : AspectAttribute
+		public class Aspect1Attribute : AspectAttribute
 		{
-			public static Action BeforeCallback;
-			public static Action AfterCallback;
-			public static Action<Exception> AfterCallbackWithException;
+			public Aspect1Attribute() : base(typeof(Aspect1)) { }
+			public class Aspect1 : FakeAspect { }
+		}
 
-			public AResolvedAspect() : base(typeof(TheResolvedAspect)) { }
+		public class Aspect2Attribute : AspectAttribute
+		{
+			public Aspect2Attribute() : base(typeof(Aspect2)) { }
+			public class Aspect2 : FakeAspect { }
+		}
 
-			public class TheResolvedAspect : IAspect
+
+		public class Aspect3Attribute : AspectAttribute
+		{
+			public Aspect3Attribute() : base(typeof(Aspect3)) { }
+			public class Aspect3 : FakeAspect { }
+		}
+
+		public class FakeAspect : IAspect
+		{
+			public bool BeforeInvoked;
+			public bool AfterInvoked;
+			public Exception BeforeFailsWith;
+			public Exception AfterFailsWith;
+			public Exception AfterInvokedWith;
+
+			public void OnBeforeInvocation(IInvocationInfo invocation)
 			{
-				public void OnBeforeInvocation(IInvocationInfo invocation)
-				{
-					BeforeCallback?.Invoke();
-				}
+				BeforeInvoked = true;
+				if (BeforeFailsWith != null)
+					throw BeforeFailsWith;
+			}
 
-				public void OnAfterInvocation(Exception exception, IInvocationInfo invocation)
-				{
-					AfterCallback?.Invoke();
-					AfterCallbackWithException?.Invoke(exception);
-				}
+			public void OnAfterInvocation(Exception exception, IInvocationInfo invocation)
+			{
+				AfterInvokedWith = exception;
+				AfterInvoked = true;
+				if (AfterFailsWith != null)
+					throw AfterFailsWith;
 			}
 		}
-
-		private class ThrowingOnBeforeAspect : AspectAttribute
-		{
-			public ThrowingOnBeforeAspect() : base(typeof(TheResolvedAspect)) { }
-
-			public class TheResolvedAspect : IAspect
-			{
-				public void OnBeforeInvocation(IInvocationInfo invocation)
-				{
-					throw new FileNotFoundException();
-				}
-
-				public void OnAfterInvocation(Exception exception, IInvocationInfo invocation)
-				{
-				}
-			}
-		}
-
-		private class ThrowingOnAfterAspect : AspectAttribute
-		{
-			public ThrowingOnAfterAspect() : base(typeof(TheResolvedAspect)) { }
-
-			public class TheResolvedAspect : IAspect
-			{
-				public void OnBeforeInvocation(IInvocationInfo invocation)
-				{
-				}
-
-				public void OnAfterInvocation(Exception exception, IInvocationInfo invocation)
-				{
-					throw new FileNotFoundException();
-				}
-			}
-		}
+		
 	}
+
 }
