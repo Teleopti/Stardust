6c91c9b78165dd80bb5e1f8751164f625af64268
Roger Kratz <roger.kratz@teleopti.com>
2017-03-09 14:10:27 +0100

    Backed out changeset: 9ecbc6c7a333 Need to investigate locally

--- Domain/Optimization/TeamBlock/TeamBlockDayOffOptimizerService.cs
+++ Domain/Optimization/TeamBlock/TeamBlockDayOffOptimizerService.cs
@@ -404,22 +404,12 @@ namespace Teleopti.Ccc.Domain.Optimization.TeamBlock
 			                                         movedDaysOff.RemovedDaysOff,
 			                                         rollbackService, resourceCalculateDelayer,
 			                                         schedulingResultStateHolder);
-			if (success)
-			{
-				if (optimizationPreferences.Extra.IsClassic())
-				{
-					//TODO - investigate... 
-					// * run even if not success?
-					// * run also when normal team/block?
-					// * lock also added DO days?
-					lockDaysInMatrixes(movedDaysOff.RemovedDaysOff, teamInfo);
-				}
-			}
-			else
+			if (!success)
 			{
 				checkPeriodValue = true;
 				return false;
 			}
+
 
 			if (!optimizationPreferences.General.OptimizationStepDaysOff && optimizationPreferences.General.OptimizationStepDaysOffForFlexibleWorkTime)
 			{
