58d7df2d6cca4a13890923262d802a42614d7e33
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-03-15 17:03:59 +0100

    #43343 Fixed release script

--- Database/TeleoptiCCC7/Releases/00000830.sql
+++ Database/TeleoptiCCC7/Releases/00000830.sql
@@ -15,6 +15,8 @@ CREATE UNIQUE NONCLUSTERED INDEX [UQ_DayOffRules_DefaultSettings] ON [dbo].[DayO
 )
 WHERE ([DefaultSettings]=(1))
 
+GO
+
 INSERT INTO dbo.DayOffRules (Id, UpdatedBy, UpdatedOn, BusinessUnit, MinDayOffsPerWeek, MaxDayOffsPerWeek, MinConsecutiveWorkDays, MaxConsecutiveWorkDays, MinConsecutiveDayOffs, MaxConsecutiveDayOffs, DefaultSettings, Name, AgentGroup)
        SELECT newid(), UpdatedBy, UpdatedOn, BusinessUnit, 1, 3, 2, 6, 1, 3, 1, 'Default', Id
        FROM dbo.AgentGroup;
