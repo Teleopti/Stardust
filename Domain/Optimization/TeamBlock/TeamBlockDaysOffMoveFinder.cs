33c0e858f54e82b48e2bfcd3f1531d4edcac31e8
Roger Kratz <roger.kratz@teleopti.com>
2017-03-20 09:48:40 +0100

    #40294 Backing out "max iteration" perf fix, because it didn't take screw.

--- Domain/Optimization/TeamBlock/TeamBlockDaysOffMoveFinder.cs
+++ Domain/Optimization/TeamBlock/TeamBlockDaysOffMoveFinder.cs
@@ -31,8 +31,6 @@ namespace Teleopti.Ccc.Domain.Optimization.TeamBlock
 											  IOptimizationPreferences optimizationPreferences,
 											  IDaysOffPreferences daysOffPreferences, ISchedulingResultStateHolder schedulingResultStateHolder)
 		{
-			var maxDoBackToLegalStateIterations = optimizationPreferences.Extra.IsClassic() ? 25 : 100;
-
 			//should use agggregated skills
 			var scheduleResultDataExtractorValues =
 				_scheduleResultDataExtractorProvider.CreatePersonalSkillDataExtractor(matrix, optimizationPreferences.Advanced, schedulingResultStateHolder).Values();
@@ -46,7 +44,7 @@ namespace Teleopti.Ccc.Domain.Optimization.TeamBlock
 				var workingBitArray = (ILockableBitArray)originalArray.Clone();
 				if (!dayOffDecisionMaker.Execute(workingBitArray, scheduleResultDataExtractorValues))
 				{
-					if (!_daysOffBackToLegal.Execute(_daysOffBackToLegal.BuildSolverList(workingBitArray, daysOffPreferences, maxDoBackToLegalStateIterations), 100, new List<string>()))
+					if (!_daysOffBackToLegal.Execute(_daysOffBackToLegal.BuildSolverList(workingBitArray, daysOffPreferences, 100), 100, new List<string>()))
 						continue;
 
 					if (!dayOffDecisionMaker.Execute(workingBitArray, scheduleResultDataExtractorValues))
@@ -54,7 +52,7 @@ namespace Teleopti.Ccc.Domain.Optimization.TeamBlock
 				}
 
 
-				if(!_daysOffBackToLegal.Execute(_daysOffBackToLegal.BuildSolverList(workingBitArray, daysOffPreferences, maxDoBackToLegalStateIterations), 100, new List<string>()))
+				if(!_daysOffBackToLegal.Execute(_daysOffBackToLegal.BuildSolverList(workingBitArray, daysOffPreferences, 100), 100, new List<string>()))
 					continue;
 				
 				return workingBitArray;
