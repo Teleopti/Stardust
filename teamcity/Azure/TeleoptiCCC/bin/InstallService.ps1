ddd099f70a2d72232447ad415d9fde9d3f37f627
Jimmy Broman <none@none>
2017-03-22 14:55:03 +0100

    Removed unused lines

--- teamcity/Azure/TeleoptiCCC/bin/InstallService.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/InstallService.ps1
@@ -81,13 +81,13 @@ Try
 Catch [Exception]
 {
     $ErrorMessage = $_.Exception.Message
-    Write-EventLog -LogName Application -Source $JOB -EventID 1 -EntryType Error -Message "$ErrorMessage"
+    #Write-EventLog -LogName Application -Source $JOB -EventID 1 -EntryType Error -Message "$ErrorMessage"
 	log-error "Script failed, Check Windows event log for details!"
 	Throw "Script failed, Check Windows event log for details"
 }
 Finally
 {
 	log-info "Done."
-	Write-EventLog -LogName Application -Source $JOB -EventID 0 -EntryType Information -Message "$newFiles files synced from: $BlobSource to: $FILEWATCH"
+	#Write-EventLog -LogName Application -Source $JOB -EventID 0 -EntryType Information -Message "$newFiles files synced from: $BlobSource to: $FILEWATCH"
 
 }
