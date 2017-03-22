fb230c0a7ace71259631cde0c9c66cf6d51601a3
Zulong Chen <none@none>
2017-03-22 10:41:51 +0800

    #43419 Create toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -145,6 +145,7 @@
 		Wfm_People_PrepareForRelease_39040,
 		Wfm_People_ImportAndCreateAgentFromFile_42528,
 		Wfm_People_SetFallbackValuesForImporting_43289,
+		Wfm_People_MoveImportJobToBackgroundService_43419,
 
 		Report_UseOpenXmlFormat_35797,
 		Scheduler_IntradayOptimization_36617,
