a3c9d5d876d2fe94479335132dfe73e3e8eb5d9b
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-03-27 14:05:00 +0200

    #43340 Changes to updating frame ancestors to support azure

--- Teleopti.Support.Tool/Tool/FrameAncestorsUpdator.cs
+++ Teleopti.Support.Tool/Tool/FrameAncestorsUpdator.cs
@@ -9,14 +9,18 @@ namespace Teleopti.Support.Tool.Tool
 		{
 			using (var serverManager = new ServerManager())
 			{
-				var webConfiguration = serverManager.GetWebConfiguration("Default Web Site/TeleoptiWFM/Web");
-				var authenticationBridgeConfiguration = serverManager.GetWebConfiguration("Default Web Site/TeleoptiWFM/AuthenticationBridge");
-				var windowsIdentityProviderConfiguration = serverManager.GetWebConfiguration("Default Web Site/TeleoptiWFM/WindowsIdentityProvider");
-
-				updateFrameAncestors(webConfiguration, url);
-				updateFrameAncestors(authenticationBridgeConfiguration, url);
-				updateFrameAncestors(windowsIdentityProviderConfiguration, url);
-
+				foreach (var site in serverManager.Sites)
+				{
+					foreach (var application in site.Applications)
+					{
+						if (application.Path.EndsWith("/Web") || 
+							application.Path.EndsWith("/AuthenticationBridge") ||
+							application.Path.EndsWith("/WindowsIdentityProvider"))
+						{
+							updateFrameAncestors(application.GetWebConfiguration(), url);
+						}
+					}
+				}
 				serverManager.CommitChanges();
 			}
 		}
@@ -25,8 +29,20 @@ namespace Teleopti.Support.Tool.Tool
 		{
 			var httpProtocolSection = configuration.GetSection("system.webServer/httpProtocol");
 			var customHeadersCollection = httpProtocolSection.GetCollection("customHeaders");
-			var contentSecurityPolicy = customHeadersCollection.Single(x => (string)x["name"] == @"Content-Security-Policy");
-			contentSecurityPolicy["value"] = "script-src 'self' 'unsafe-inline' 'unsafe-eval'; frame-ancestors 'self' " + url + ";";
+			var value = $"script-src \'self\' \'unsafe-inline\' \'unsafe-eval\'; frame-ancestors \'self\' {url};";
+			createOrUpdate(customHeadersCollection, "Content-Security-Policy", value);
+		}
+
+		private void createOrUpdate(ConfigurationElementCollection customHeadersCollection, string name, string value)
+		{
+			var contentSecurityPolicy = customHeadersCollection.SingleOrDefault(x => (string)x["name"] == name);
+			if (contentSecurityPolicy == null)
+			{
+				contentSecurityPolicy = customHeadersCollection.CreateElement("add");
+				contentSecurityPolicy["name"] = name;
+				customHeadersCollection.Add(contentSecurityPolicy);
+			}
+			contentSecurityPolicy["value"] = value;
 		}
 	}
 }
