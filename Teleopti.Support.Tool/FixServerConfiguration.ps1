313c5fdd2fd14bf063b1ddc8393396a4c15472c8
Kunning Mao <none@none>
2017-03-28 08:52:28 +0200

    #43340 get correct path in administration mode

--- Teleopti.Support.Tool/FixServerConfiguration.ps1
+++ Teleopti.Support.Tool/FixServerConfiguration.ps1
@@ -1,4 +1,23 @@
-﻿$webConfig = '..\TeleoptiCCC\Administration\Web.config';
+﻿function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value;
+    if($Invocation.PSScriptRoot)
+    {
+        $Invocation.PSScriptRoot;
+    }
+    Elseif($Invocation.MyCommand.Path)
+    {
+        Split-Path $Invocation.MyCommand.Path
+    }
+    else
+    {
+        $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+    }
+}
+
+$directory = Get-ScriptDirectory
+
+$webConfig = $directory + '\..\TeleoptiCCC\Administration\Web.config';
 $doc = (Get-Content $webConfig) -as [Xml];
 $root = $doc.get_DocumentElement();
 $connectionStr = ($root.connectionStrings.add|where {$_.name -eq "Tenancy"}).connectionString;
