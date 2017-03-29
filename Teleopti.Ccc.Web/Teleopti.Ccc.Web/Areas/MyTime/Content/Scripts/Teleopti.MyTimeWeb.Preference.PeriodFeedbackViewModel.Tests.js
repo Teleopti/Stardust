10de6fe23170ff65778cea1147a33a24bc9fe1d1
Xinfeng Li <xinfeng.li@teleopti.com>
2017-03-29 16:23:24 +0800

    PBI #43322: Try fix broken js tests.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.PeriodFeedbackViewModel.Tests.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Preference.PeriodFeedbackViewModel.Tests.js
@@ -121,17 +121,17 @@
 
 		equal(viewModel.PossibleNightRestViolations()()[0].firstDay, Teleopti.MyTimeWeb.Common.FormatDate(currentDay1));// 2016-02-04
 		equal(viewModel.PossibleNightRestViolations()()[0].sencondDay, Teleopti.MyTimeWeb.Common.FormatDate(plusOneDay1));// 2016-02-03
-		equal(viewModel.PossibleNightRestViolations()()[0].hoursBetweenTwoDays, '10:00');
-		equal(viewModel.PossibleNightRestViolations()()[0].nightRestTimes, '11:00');
+		equal(viewModel.PossibleNightRestViolations()()[0].hoursBetweenTwoDays, '10:00:00');
+		equal(viewModel.PossibleNightRestViolations()()[0].nightRestTimes, '11:00:00');
 
 		equal(viewModel.PossibleNightRestViolations()()[1].firstDay, Teleopti.MyTimeWeb.Common.FormatDate(minusOneDay2));//2016-01-25
 		equal(viewModel.PossibleNightRestViolations()()[1].sencondDay, Teleopti.MyTimeWeb.Common.FormatDate(currentDay2));//2016-01-26
-		equal(viewModel.PossibleNightRestViolations()()[1].hoursBetweenTwoDays, '10:00');
-		equal(viewModel.PossibleNightRestViolations()()[1].nightRestTimes, '11:00');
+		equal(viewModel.PossibleNightRestViolations()()[1].hoursBetweenTwoDays, '10:00:00');
+		equal(viewModel.PossibleNightRestViolations()()[1].nightRestTimes, '11:00:00');
 
 		equal(viewModel.PossibleNightRestViolations()()[2].firstDay, Teleopti.MyTimeWeb.Common.FormatDate(currentDay2));//2016-01-26
 		equal(viewModel.PossibleNightRestViolations()()[2].sencondDay, Teleopti.MyTimeWeb.Common.FormatDate(plusOneDay2));//2016-01-27
-		equal(viewModel.PossibleNightRestViolations()()[2].hoursBetweenTwoDays, '10:00');
-		equal(viewModel.PossibleNightRestViolations()()[2].nightRestTimes, '11:00');
+		equal(viewModel.PossibleNightRestViolations()()[2].hoursBetweenTwoDays, '10:00:00');
+		equal(viewModel.PossibleNightRestViolations()()[2].nightRestTimes, '11:00:00');
 	});
 });
