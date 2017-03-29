81377c933720eb3c47893a46dc7502d45a508323
amandaj <none@none>
2017-03-29 15:34:31 +0200

    PBI #43636 Bulk - Multi skilled agent with high and low priority skill DONE

---
+++ Teleopti.Wfm.Test/MultiSkilledGoldSilverBronzeAgentBulkTest.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo.Requests;
+using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Common.Time;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.Helper;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Logon;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.UnitOfWork;
+using Teleopti.Ccc.InfrastructureTest;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.UserTexts;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Wfm.Test
+{
+	[DatabaseTest]
+	[Toggle(Toggles.Staffing_ReadModel_UseSkillCombination_xx)]
+	public class MultiSkilledGoldSilverBronzeAgentBulkTest : SetUpCascadingShifts
+	{
+		public ICurrentUnitOfWork CurrentUnitOfWork;
+		public IBusinessUnitRepository BusinessUnitRepository;
+		public IDataSourceScope DataSourceScope;
+		public ImpersonateSystem ImpersonateSystem;
+		public WithUnitOfWork WithUnitOfWork;
+
+
+		public IAbsenceRepository AbsenceRepository;
+		public IPersonRepository PersonRepository;
+		public IHandleEvent<NewMultiAbsenceRequestsCreatedEvent> UpdateRequestHandler;
+		public IPersonRequestRepository PersonRequestRepository;
+		public MutableNow Now;
+
+
+		[SetUp]
+		public void Setup()
+		{
+			SetupFixtureForAssembly.BeginTest();
+		}
+
+
+		[Test]
+		public void ShouldBeApprovedIfOverstaffedSingleInterval()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpLowDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkills1");
+
+				var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddMinutes(30).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+			
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> { personRequest.Id.GetValueOrDefault()},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.True();
+			}
+		}
+
+		[Test]
+		public void ShouldBeApprovedIfOverstaffedMultipleIntervals()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpLowDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkills1");
+
+				var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(3).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				PersonRequestRepository.Add(personRequest);
+				personRequest.Pending();
+				uow.PersistAll();
+			}
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> { personRequest.Id.GetValueOrDefault() },
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.True();
+			}
+		}
+
+		[Test]
+		public void ShouldBeDeniedIfUnderstaffedSingleInterval()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			IPerson person;
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpHighDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkills1");
+
+				var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddMinutes(30).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> { personRequest.Id.GetValueOrDefault() },
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.False();
+				req.DenyReason.Should().StartWith(Resources.ResourceManager.GetString("InsufficientStaffingHours", person.PermissionInformation.Culture()).Substring(0, 10));
+			}
+		}
+
+		[Test]
+		public void ShouldBeDeniedIfUnderstaffedMultipleIntervals()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			IPerson person;
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpHighDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkills1");
+				var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(3).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> {personRequest.Id.GetValueOrDefault()},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.False();
+				req.DenyReason.Should().StartWith(Resources.ResourceManager.GetString("InsufficientStaffingHours", person.PermissionInformation.Culture()).Substring(0, 10));
+			}
+		}
+
+		[Test]
+		public void ShouldBeDeniedIfUnderstaffedOnFirstHourAndOverstaffedOnSecond()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			IPerson person;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpMixedSkillDays(1, new Tuple<int, double>(requestStart.Hour, 10));
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkills1");
+
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(2).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> {personRequest.Id.GetValueOrDefault()},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.False();
+				req.DenyReason.Should().Be.EqualTo(CreateDenyMessage30Min(requestStart.Hour, person.PermissionInformation.Culture(), person.PermissionInformation.Culture(), TimeZoneInfo.Utc, requestStart.Date));
+			}
+		}
+
+		[Test]
+		public void ShouldBeApprovedIfNoShiftInRequestPeriodAndOverStaffed()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpLowDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkillsNoShift");
+
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(2).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> {personRequest.Id.GetValueOrDefault()},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.True();
+			}
+		}
+
+		[Test]
+		public void ShouldBeApprovedIfNoShiftInRequestPeriodAndUnderstaffed()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+			SetUpHighDemandSkillDays();
+
+			var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+			var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkillsNoShift");
+
+			var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(2).Utc()));
+			 personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+			PersonRequestRepository.Add(personRequest);
+			uow.PersistAll();
+			}
+
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> { personRequest.Id.GetValueOrDefault() },
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.True();
+			}
+		}
+
+		[Test]
+		public void ShouldBeApprovedWhenActivityIsNotConnectedToPersonSkillAndOverStaffed()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+
+				SetUpLowDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkillsWrongAct");
+
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(2).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> {personRequest.Id.GetValueOrDefault()},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.True();
+			}
+		}
+
+		[Test]
+		public void ShouldBeApprovedWhenActivityIsNotConnectedToPersonSkillAndUnderStaffed()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			IPerson person;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpHighDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				 person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkillsWrongAct");
+
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(2).Utc()));
+				 personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid> {personRequest.Id.GetValueOrDefault()},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.False();
+				req.DenyReason.Should().StartWith(Resources.ResourceManager.GetString("InsufficientStaffingHours", person.PermissionInformation.Culture()).Substring(0, 10));
+			}
+		}
+
+		[Test]
+		public void ShouldBeApprovedIfOverstaffedAndActivityIsOvertime()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpLowDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkillsOvertime");
+
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(3).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid>
+				{
+					personRequest.Id.GetValueOrDefault()
+				},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.True();
+			}
+		}
+
+		[Test]
+		public void ShouldBeDeniedIfUnderstaffedAndActivityIsOvertime()
+		{
+			Now.Is(DateTime.UtcNow);
+			IPersonRequest personRequest;
+			var requestStart = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour + 2, 0, 0);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SetUpRelevantStuffWithCascading();
+				SetUpHighDemandSkillDays();
+
+				var absence = AbsenceRepository.LoadRequestableAbsence().Single(x => x.Name == "Holiday");
+				var person = PersonRepository.LoadAll().Single(x => x.Name.FirstName == "PersonAllSkillsOvertime");
+
+				var absenceRequest = new AbsenceRequest(absence, new DateTimePeriod(requestStart.Utc(), requestStart.AddHours(3).Utc()));
+				personRequest = new PersonRequest(person, absenceRequest);
+				personRequest.Pending();
+				PersonRequestRepository.Add(personRequest);
+				uow.PersistAll();
+			}
+
+			UpdateRequestHandler.Handle(new NewMultiAbsenceRequestsCreatedEvent
+			{
+				PersonRequestIds = new List<Guid>
+				{
+					personRequest.Id.GetValueOrDefault()
+				},
+				InitiatorId = Guid.Empty,
+				JobName = "JobName",
+				LogOnBusinessUnitId = TestState.BusinessUnit.Id.GetValueOrDefault(),
+				LogOnDatasource = "TestData",
+				Sent = DateTime.UtcNow
+			});
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var req = PersonRequestRepository.Load(personRequest.Id.GetValueOrDefault());
+				req.IsApproved.Should().Be.False();
+			}
+		}
+	}
+}
