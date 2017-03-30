b7aeae4ff7e235ffef9f845706551b0e971009b9
amandaj <none@none>
2017-03-30 16:12:25 +0200

    bug #43563 Let node lock the job while working

--- Domain/Staffing/UpdateStaffingLevelReadModelHandler.cs
+++ Domain/Staffing/UpdateStaffingLevelReadModelHandler.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Intraday;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Interfaces.Domain;
@@ -15,27 +18,47 @@ namespace Teleopti.Ccc.Domain.Staffing
 		private readonly IUpdateStaffingLevelReadModel _updateStaffingLevelReadModel;
 		private readonly ICurrentUnitOfWorkFactory _currentFactory;
 		private readonly INow _now;
+		private readonly IJobStartTimeRepository _jobStartTimeRepository;
+		private readonly ICurrentBusinessUnit _currentBusinessUnit;
 
-		public UpdateStaffingLevelReadModelHandler(IUpdateStaffingLevelReadModel updateStaffingLevelReadModel, ICurrentUnitOfWorkFactory current, INow now)
+		public UpdateStaffingLevelReadModelHandler(IUpdateStaffingLevelReadModel updateStaffingLevelReadModel, ICurrentUnitOfWorkFactory current, INow now, IJobStartTimeRepository jobStartTimeRepository, ICurrentBusinessUnit currentBusinessUnit)
 		{
 			_updateStaffingLevelReadModel = updateStaffingLevelReadModel;
 			_currentFactory = current;
 			_now = now;
+			_jobStartTimeRepository = jobStartTimeRepository;
+			_currentBusinessUnit = currentBusinessUnit;
 		}
 
 		[AsSystem]
 		[UnitOfWork]
 		public virtual void Handle(UpdateStaffingLevelReadModelEvent @event)
 		{
-			//start thread with time and set time to now + 1 min (or 2?)
-			var period = new DateTimePeriod(_now.UtcDateTime().AddDays(-1).AddHours(-1), _now.UtcDateTime().AddDays(@event.Days).AddHours(1));
-
-			_updateStaffingLevelReadModel.Update(period);
 			
-			var current = _currentFactory.Current().CurrentUnitOfWork();
-			//an ugly solution for bug 39594
-			if (current.IsDirty())
-				current.Clear();
+			var jobLockTimer = new Timer(30000) {AutoReset = true};
+			jobLockTimer.Elapsed += updateJobLock;
+
+			try
+			{
+				_jobStartTimeRepository.UpdateLockTimestamp(_currentBusinessUnit.Current().Id.GetValueOrDefault());
+				var period = new DateTimePeriod(_now.UtcDateTime().AddDays(-1).AddHours(-1), _now.UtcDateTime().AddDays(@event.Days).AddHours(1));
+
+				_updateStaffingLevelReadModel.Update(period);
+
+				var current = _currentFactory.Current().CurrentUnitOfWork();
+				//an ugly solution for bug 39594
+				if (current.IsDirty())
+					current.Clear();
+			}
+			finally
+			{
+				jobLockTimer.Dispose();
+			}
+		}
+
+		private void updateJobLock(object source, ElapsedEventArgs e)
+		{
+			_jobStartTimeRepository.UpdateLockTimestamp(_currentBusinessUnit.Current().Id.GetValueOrDefault());
 		}
 	}
 }
