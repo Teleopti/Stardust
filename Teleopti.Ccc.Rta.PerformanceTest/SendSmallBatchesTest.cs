5a451e3050746f6a07d4f4b1dbc92df6a52c9a8c
Justin! <justin@teleopti.com>
2017-04-05 22:20:01 +0200

    Backing in - New perf test measurement stuff moved to test common

--- Teleopti.Ccc.Rta.PerformanceTest/SendSmallBatchesTest.cs
+++ Teleopti.Ccc.Rta.PerformanceTest/SendSmallBatchesTest.cs
@@ -9,7 +9,7 @@ namespace Teleopti.Ccc.Rta.PerformanceTest
 	public class SendSmallBatchesTest
 	{
 		public StatesSender States;
-		public PerformanceTest PerformanceTest;
+		public TestCommon.PerformanceTest.PerformanceTest PerformanceTest;
 
 		[Test]
 		public void MeasurePerformance()
