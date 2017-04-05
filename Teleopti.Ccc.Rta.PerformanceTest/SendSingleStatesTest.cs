b2e8ddef5ea8e34573ef1ae157ffb1b1b13f5630
Justin! <justin@teleopti.com>
2017-04-05 22:04:11 +0200

    Backing out again

--- Teleopti.Ccc.Rta.PerformanceTest/SendSingleStatesTest.cs
+++ Teleopti.Ccc.Rta.PerformanceTest/SendSingleStatesTest.cs
@@ -9,7 +9,7 @@ namespace Teleopti.Ccc.Rta.PerformanceTest
 	public class SendSingleStatesTest
 	{
 		public StatesSender States;
-		public TestCommon.PerformanceTest.PerformanceTest PerformanceTest;
+		public PerformanceTest PerformanceTest;
 
 		[Test]
 		public void MeasurePerformance()
