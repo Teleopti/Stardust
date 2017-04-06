b9d51e987928d0d2558de30ce6e1c5904c78bcfe
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-04-06 10:25:03 +0200

    #43807 Added resource type to pre-validation messages in web scheduling

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/Validation/SchedulingValidationError.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/Validation/SchedulingValidationError.cs
@@ -8,5 +8,12 @@ namespace Teleopti.Ccc.Web.Areas.ResourcePlanner.Validation
 		public Guid ResourceId { get; set; }
 		public string ResourceName { get; set; }
 		public ICollection<string> ValidationErrors { get; set; }
+		public ValidationResourceType ResourceType { get; set; }
+	}
+
+	public enum ValidationResourceType
+	{
+		Agent,
+		Skill
 	}
 }
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/Validation/ValidationResult.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/ResourcePlanner/Validation/ValidationResult.cs
@@ -19,15 +19,29 @@ namespace Teleopti.Ccc.Web.Areas.ResourcePlanner.Validation
 			if (InvalidResources.Any(x => x.ResourceId == error.PersonId))
 				InvalidResources.First(x => x.ResourceId == error.PersonId).ValidationErrors.Add(error.ValidationError);
 			else
-				InvalidResources.Add(new SchedulingValidationError { ResourceId = error.PersonId, ResourceName = error.PersonName, ValidationErrors = new List<string> { error.ValidationError } });
+				InvalidResources.Add(new SchedulingValidationError
+				{
+					ResourceId = error.PersonId,
+					ResourceName = error.PersonName,
+					ValidationErrors = new List<string> {error.ValidationError},
+					ResourceType = ValidationResourceType.Agent
+				});
 		}
 
 		public void Add(MissingForecastModel error)
 		{
 			if (InvalidResources.Any(x => x.ResourceName == error.SkillName))
-				InvalidResources.First(x => x.ResourceName == error.SkillName).ValidationErrors.AddRange(error.MissingRanges.Select(x => $"{Resources.MissingForecastFrom} {x.StartDate:d} {Resources.ToText} {x.EndDate:d}"));
+				InvalidResources.First(x => x.ResourceName == error.SkillName)
+					.ValidationErrors.AddRange(
+						error.MissingRanges.Select(
+							x => $"{Resources.MissingForecastFrom} {x.StartDate:d} {Resources.ToText} {x.EndDate:d}"));
 			else
-				InvalidResources.Add(new SchedulingValidationError { ResourceName = error.SkillName, ValidationErrors = error.MissingRanges.Select(x => $"{Resources.MissingForecastFrom} {x.StartDate:d} {Resources.ToText} {x.EndDate:d}").ToList()});
+				InvalidResources.Add(new SchedulingValidationError
+				{
+					ResourceName = error.SkillName,
+					ValidationErrors = error.MissingRanges.Select(x => $"{Resources.MissingForecastFrom} {x.StartDate:d} {Resources.ToText} {x.EndDate:d}").ToList(),
+					ResourceType = ValidationResourceType.Skill
+				});
 		}
 	}
 }
