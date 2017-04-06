8f2d51e5e4bb83faf27d0255bf71a6000abec12a
Herman <none@none>
2017-04-06 13:55:52 +0200

    New permission scenarios

---
+++ Teleopti.Ccc.TestCommon/TestData/Setups/Configurable/ApplicationFunctionConfigurable.cs
@@ -0,0 +1,26 @@
+﻿using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Security.AuthorizationEntities;
+using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.TestCommon.TestData.Core;
+
+namespace Teleopti.Ccc.TestCommon.TestData.Setups.Configurable
+{
+	public class ApplicationFunctionConfigurable : IDataSetup
+	{
+
+		public string Name { get; set; }
+		public string LocalizedFunctionDescription { get; set; }
+		public IApplicationFunction Function;
+		
+		public void Apply(ICurrentUnitOfWork currentUnitOfWork)
+		{
+			Function = new ApplicationFunction(Name);
+			var applicationFunctionRepository = new ApplicationFunctionRepository(currentUnitOfWork);
+			applicationFunctionRepository.Add(Function);
+		}
+	}
+}
+		}
+	}
+}
