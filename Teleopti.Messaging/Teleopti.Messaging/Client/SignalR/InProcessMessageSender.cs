db61e43c8a78fa697a40ffb36290bba7c1d69f28
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-04-10 18:53:38 +0200

    #43552 Probably easier way to process in process

--- Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/InProcessMessageSender.cs
+++ Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/InProcessMessageSender.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 using Teleopti.Ccc.Domain.MessageBroker;
@@ -13,64 +14,47 @@ namespace Teleopti.Messaging.Client.SignalR
 	{
 		private readonly IMessageBrokerServer _messageBrokerServer;
 		private readonly ConcurrentQueue<Action> _actions;
-		private bool isProcessingQueue;
-		private readonly object processingLock = new object();
 		private readonly ILog logger = LogManager.GetLogger(typeof(InProcessMessageSender));
 
 		public InProcessMessageSender(IMessageBrokerServer messageBrokerServer)
 		{
 			_messageBrokerServer = messageBrokerServer;
 			_actions = new ConcurrentQueue<Action>();
+			Task.Run(() => processQueue());
 		}
 
 		public void Send(Message message)
 		{
 			_actions.Enqueue(() => _messageBrokerServer.NotifyClients(message));
-			startProcessing();
 		}
 
 		public void SendMultiple(IEnumerable<Message> messages)
 		{
 			_actions.Enqueue(() => _messageBrokerServer.NotifyClientsMultiple(messages));
-			startProcessing();
-		}
-
-		private void startProcessing()
-		{
-			lock (processingLock)
-			{
-				if (!isProcessingQueue)
-				{
-					isProcessingQueue = true;
-					Task.Run(() => processQueue());
-				}
-			}
 		}
 
 		private void processQueue()
 		{
-			try
+			while (true)
 			{
-				while (!_actions.IsEmpty)
+				if (_actions.IsEmpty)
 				{
-					Action action;
-					if (_actions.TryDequeue(out action))
-					{
-						try
-						{
-							action();
-						}
-						catch (Exception e)
-						{
-							logger.Error("An error occured while sending messages in process.", e);
-						}
-					}
-						
+					Thread.Sleep(TimeSpan.FromSeconds(1));
+					continue;
 				}
-			}
-			finally
-			{
-				isProcessingQueue = false;
+				Action action;
+				if (_actions.TryDequeue(out action))
+				{
+					try
+					{
+						action();
+					}
+					catch (Exception e)
+					{
+						logger.Error("An error occured while sending messages in process.", e);
+					}
+				}
+
 			}
 		}
 	}
