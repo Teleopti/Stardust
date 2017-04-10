9d7d9d2788b50e438153b048e552be4f6f941317
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-04-10 17:51:15 +0200

    #43552 Made inprocess message sender not overload sql connection pool in some extreme cases

--- Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/InProcessMessageSender.cs
+++ Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/InProcessMessageSender.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using log4net;
 using Teleopti.Ccc.Domain.MessageBroker;
 using Teleopti.Ccc.Domain.MessageBroker.Client;
 using Teleopti.Ccc.Domain.MessageBroker.Server;
@@ -9,20 +12,66 @@ namespace Teleopti.Messaging.Client.SignalR
 	public class InProcessMessageSender : IMessageSender
 	{
 		private readonly IMessageBrokerServer _messageBrokerServer;
+		private readonly ConcurrentQueue<Action> _actions;
+		private bool isProcessingQueue;
+		private readonly object processingLock = new object();
+		private readonly ILog logger = LogManager.GetLogger(typeof(InProcessMessageSender));
 
 		public InProcessMessageSender(IMessageBrokerServer messageBrokerServer)
 		{
 			_messageBrokerServer = messageBrokerServer;
+			_actions = new ConcurrentQueue<Action>();
 		}
 
 		public void Send(Message message)
 		{
-			Task.Run(() => _messageBrokerServer.NotifyClients(message));
+			_actions.Enqueue(() => _messageBrokerServer.NotifyClients(message));
+			startProcessing();
 		}
 
 		public void SendMultiple(IEnumerable<Message> messages)
 		{
-			Task.Run(() => _messageBrokerServer.NotifyClientsMultiple(messages));
+			_actions.Enqueue(() => _messageBrokerServer.NotifyClientsMultiple(messages));
+			startProcessing();
+		}
+
+		private void startProcessing()
+		{
+			lock (processingLock)
+			{
+				if (!isProcessingQueue)
+				{
+					isProcessingQueue = true;
+					Task.Run(() => processQueue());
+				}
+			}
+		}
+
+		private void processQueue()
+		{
+			try
+			{
+				while (!_actions.IsEmpty)
+				{
+					Action action;
+					if (_actions.TryDequeue(out action))
+					{
+						try
+						{
+							action();
+						}
+						catch (Exception e)
+						{
+							logger.Error("An error occured while sending messages in process.", e);
+						}
+					}
+						
+				}
+			}
+			finally
+			{
+				isProcessingQueue = false;
+			}
 		}
 	}
 }
