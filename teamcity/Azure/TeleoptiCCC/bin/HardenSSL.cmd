4c5af9d0cbb7f8a03c214129a67ab23ada8856aa
Jimmy Broman <none@none>
2017-04-12 13:59:41 +0200

    HardenSSL is pointing to wrong script file..

--- teamcity/Azure/TeleoptiCCC/bin/HardenSSL.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/HardenSSL.cmd
@@ -1,2 +1,2 @@
-PowerShell -ExecutionPolicy Unrestricted .\DisableSslV3.ps1 >> log-DisableSslV3.txt 2>&1
+PowerShell -ExecutionPolicy Unrestricted .\HardenSSL.ps1 >> log-DisableSslV3.txt 2>&1
 EXIT /B 0
