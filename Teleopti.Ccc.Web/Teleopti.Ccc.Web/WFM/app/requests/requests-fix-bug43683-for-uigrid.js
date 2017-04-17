9eac71d47d37962b88670b92295e3ce6dd2126e9
Xinfeng Li <xinfeng.li@teleopti.com>
2017-04-17 15:05:34 +0800

    Fix bug #43683: Column picker in web Requests is adding duplicate items in the list

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/requests/requests-fix-bug43683-for-uigrid.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+	// Bug #43683 seems caused by design problm of ui-grid.
+	// DOM updating of grid menu items was block by something in IE.
+	// I did not find root cause of this problem, but this modification works :-p
+	// -- by xinfli
+	angular.module("ui.grid").decorator("uiGridGridMenuService", ["$delegate", "$timeout", "uiGridConstants",
+		function ($delegate, $timeout, uiGridConstants) {
+			$delegate.toggleColumnVisibility = function(gridCol) {
+				gridCol.colDef.visible = !(gridCol.colDef.visible === true || gridCol.colDef.visible === undefined);
+
+				$timeout(function() {
+					gridCol.grid.refresh();
+					gridCol.grid.api.core.notifyDataChange(uiGridConstants.dataChange.COLUMN);
+					gridCol.grid.api.core.raise.columnVisibilityChanged(gridCol);
+				}, 200);
+			};
+			return $delegate;
+		}
+	]);
+})();
