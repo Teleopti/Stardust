6b23157f93850a655433dfffae2bb90dd497a6b6
Asad Mirza <asad.mirza@teleopti.com>
2017-04-21 09:42:55 +0200

    Cleanup

--- Domain/Staffing/UpdateStaffingLevelReadModelHandler.cs
+++ Domain/Staffing/UpdateStaffingLevelReadModelHandler.cs
@@ -3,7 +3,6 @@ using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.Intraday;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.ResourceCalculation;
@@ -14,15 +13,13 @@ namespace Teleopti.Ccc.Domain.Staffing
 	public class UpdateStaffingLevelReadModelHandler : IHandleEvent<UpdateStaffingLevelReadModelEvent>, IRunOnStardust
 	{
 		private readonly IUpdateStaffingLevelReadModel _updateStaffingLevelReadModel;
-		private readonly ICurrentUnitOfWorkFactory _currentFactory;
 		private readonly INow _now;
 		private readonly IJobStartTimeRepository _jobStartTimeRepository;
 		private readonly ICurrentBusinessUnit _currentBusinessUnit;
 
-		public UpdateStaffingLevelReadModelHandler(IUpdateStaffingLevelReadModel updateStaffingLevelReadModel, ICurrentUnitOfWorkFactory current, INow now, IJobStartTimeRepository jobStartTimeRepository, ICurrentBusinessUnit currentBusinessUnit)
+		public UpdateStaffingLevelReadModelHandler(IUpdateStaffingLevelReadModel updateStaffingLevelReadModel, INow now, IJobStartTimeRepository jobStartTimeRepository, ICurrentBusinessUnit currentBusinessUnit)
 		{
 			_updateStaffingLevelReadModel = updateStaffingLevelReadModel;
-			_currentFactory = current;
 			_now = now;
 			_jobStartTimeRepository = jobStartTimeRepository;
 			_currentBusinessUnit = currentBusinessUnit;
