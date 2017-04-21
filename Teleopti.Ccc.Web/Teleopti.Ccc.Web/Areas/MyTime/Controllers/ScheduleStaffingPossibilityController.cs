4e28935949b71a03a53b56ad70fc8f94ffbd2edb
junwen.luo <none@none>
2017-04-21 15:03:19 +0800

    PBI #43880 separate getting probabilities to another controller

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/ScheduleStaffingPossibilityController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.Common.Time;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Security.AuthorizationData;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.WeekSchedule.ViewModelFactory;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.ScheduleStaffingPossibility;
+using Teleopti.Ccc.Web.Core;
+using Teleopti.Ccc.Web.Filters;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
+{
+	[ApplicationFunctionApi(DefinedRaptorApplicationFunctionPaths.MyTimeWeb)]
+	public class ScheduleStaffingPossibilityController : ApiController
+	{
+		private readonly IStaffingPossibilityViewModelFactory _staffingPossibilityViewModelFactory;
+		private readonly INow _now;
+
+		public ScheduleStaffingPossibilityController(IStaffingPossibilityViewModelFactory staffingPossibilityViewModelFactory, INow now)
+		{
+			_staffingPossibilityViewModelFactory = staffingPossibilityViewModelFactory;
+			_now = now;
+		}
+
+		[UnitOfWork, Route("api/ScheduleStaffingPossibility"), HttpGet]
+		public virtual IEnumerable<PeriodStaffingPossibilityViewModel> GetIntradayAbsencePossibility(
+			[ModelBinder(typeof(DateOnlyModelBinder))] DateOnly? date,
+			StaffingPossiblityType staffingPossiblityType = StaffingPossiblityType.None)
+		{
+			var showForDate = date ?? _now.LocalDateOnly();
+			return
+				_staffingPossibilityViewModelFactory.CreatePeriodStaffingPossibilityViewModels(
+					date.GetValueOrDefault(showForDate), staffingPossiblityType);
+		}
+	}
+}
