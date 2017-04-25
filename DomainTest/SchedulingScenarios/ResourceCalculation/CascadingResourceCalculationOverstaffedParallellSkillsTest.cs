e118a20606082d2868fc9b4069d982112ce2c765
Roger Kratz <roger.kratz@teleopti.com>
2017-04-25 16:56:16 +0200

    #44091 Failing test

--- DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationOverstaffedParallellSkillsTest.cs
+++ DomainTest/SchedulingScenarios/ResourceCalculation/CascadingResourceCalculationOverstaffedParallellSkillsTest.cs
@@ -503,5 +503,29 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
 			skillDayB2.SkillStaffPeriodCollection.First().AbsoluteDifference
 				.Should().Be.EqualTo(0);
 		}
+
+		[Test]
+		[Ignore("44091 - making this green will most probably make other tests red. Investigate/talk with Anders...")]
+		public void ShouldTryToMakeSubSkillHaveSameRelativeDifferenceAfterShoveling()
+		{
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var dateOnly = DateOnly.Today;
+			var skillA = new Skill("A").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(1).IsOpenBetween(8, 9);
+			var skillDayA = skillA.CreateSkillDayWithDemand(scenario, dateOnly, 0);
+			var skillB1 = new Skill("B1").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 9);
+			var skillDayB1 = skillB1.CreateSkillDayWithDemand(scenario, dateOnly, 1);
+			var skillB2 = new Skill("B2").For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().CascadingIndex(2).IsOpenBetween(8, 9);
+			var skillDayB2 = skillB2.CreateSkillDayWithDemand(scenario, dateOnly, 2);
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(skillA, skillB1, skillB2);
+			var ass = new PersonAssignment(agent, scenario, dateOnly).WithLayer(activity, new TimePeriod(5, 10));
+
+			Target.ResourceCalculate(dateOnly, ResourceCalculationDataCreator.WithData(scenario, dateOnly, new[] { ass }, new[] { skillDayA, skillDayB1, skillDayB2 }, false, false));
+
+			var b1RelativeDiff = skillDayB1.SkillStaffPeriodCollection.First().RelativeDifference;
+			var b2RelativeDiff = skillDayB2.SkillStaffPeriodCollection.First().RelativeDifference;
+			var diffBetweenB1AndB2 = Math.Abs(b1RelativeDiff - b2RelativeDiff);
+			diffBetweenB1AndB2.Should().Be.LessThan(0.1);
+		}
 	}
 }
