210ed7c9160bbf8fa91b8fa6064d219d1dc23bf9
johan Damm <none@none>
2017-04-26 11:53:02 +0200

    #44075 changed FAB from focus to clickevent to enable ios devices

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.js
@@ -13,91 +13,102 @@ if (typeof (Teleopti) === 'undefined') {
 }
 
 Teleopti.MyTimeWeb.Request = (function ($) {
-    var readyForInteraction = function () { };
-    var completelyLoaded = function () { };
+	var readyForInteraction = function () { };
+	var completelyLoaded = function () { };
 
-    var requestNavigationViewModel = null;
-    
-    function RequestNavigationViewModel() {
-        var self = this;
+	var requestNavigationViewModel = null;
+
+	function RequestNavigationViewModel() {
+		var self = this;
 
 		self.toggle = Teleopti.MyTimeWeb.Common.IsToggleEnabled('MyTimeWeb_MobileResponsive_43826')
 		self.hideFab = ko.observable(false);
 
-        self.addTextRequestActive = ko.observable(false);
-        self.addAbsenceRequestActive = ko.observable(false);
-        self.addShiftTradeRequestActive = ko.observable(false);
-        self.addShiftTradeBulletinBoardActive = ko.observable(false);
-        self.addPostShiftForTradeActive = ko.observable(false);
+		self.addTextRequestActive = ko.observable(false);
+		self.addAbsenceRequestActive = ko.observable(false);
+		self.addShiftTradeRequestActive = ko.observable(false);
+		self.addShiftTradeBulletinBoardActive = ko.observable(false);
+		self.addPostShiftForTradeActive = ko.observable(false);
+		self.menuIsVisible = ko.observable(false);
 
-        self.addTextRequest = function () {
+		self.enableMenu = function () {
+			self.menuIsVisible(true);
+		};
+
+		self.disableMenu = function () {
+			self.menuIsVisible(false);
+		};
+
+		self.addTextRequest = function () {
 			self.hideFab(true);
-            self.resetToolbarActiveButtons();
-            self.addTextRequestActive(true);
-            Teleopti.MyTimeWeb.Request.RequestDetail.AddTextRequestClick();
-            Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
-        };
+			self.resetToolbarActiveButtons();
+			self.addTextRequestActive(true);
+			Teleopti.MyTimeWeb.Request.RequestDetail.AddTextRequestClick();
+			Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
+		};
 
-        self.addAbsenceRequest = function () {
+		self.addAbsenceRequest = function () {
 			self.hideFab(true);
-            self.resetToolbarActiveButtons();
-            self.addAbsenceRequestActive(true);
-            Teleopti.MyTimeWeb.Request.RequestDetail.AddAbsenceRequestClick();
-            Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
-        };
+			self.resetToolbarActiveButtons();
+			self.addAbsenceRequestActive(true);
+			Teleopti.MyTimeWeb.Request.RequestDetail.AddAbsenceRequestClick();
+			Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
+		};
 
-        self.addShiftTradeRequest = function (date) {
-	        self.resetToolbarActiveButtons();
+		self.addShiftTradeRequest = function (date) {
+			// self.disableMenu()
+			self.resetToolbarActiveButtons();
 			self.addShiftTradeRequestActive(true);
 			Teleopti.MyTimeWeb.Request.AddShiftTradeRequest.OpenAddShiftTradeWindow(date);
 			Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
-        };
+		};
 
-        self.addShiftTradeBulletinBoardRequest = function (date) {
-	        self.resetToolbarActiveButtons();
-	        self.addShiftTradeBulletinBoardActive(true);
-	        Teleopti.MyTimeWeb.Request.AddShiftTradeRequest.OpenAddShiftTradeBulletinWindow(date);
+		self.addShiftTradeBulletinBoardRequest = function (date) {
+			// self.disableMenu()
+			self.resetToolbarActiveButtons();
+			self.addShiftTradeBulletinBoardActive(true);
+			Teleopti.MyTimeWeb.Request.AddShiftTradeRequest.OpenAddShiftTradeBulletinWindow(date);
 			Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
-        };
+		};
 
-        self.addPostShiftForTradeRequest = function (date) {
+		self.addPostShiftForTradeRequest = function (date) {
 			self.hideFab(true);
-	        self.resetToolbarActiveButtons();
-	        self.addPostShiftForTradeActive(true);
-	        Teleopti.MyTimeWeb.Request.RequestDetail.AddPostShiftForTradeClick(date);
+			self.resetToolbarActiveButtons();
+			self.addPostShiftForTradeActive(true);
+			Teleopti.MyTimeWeb.Request.RequestDetail.AddPostShiftForTradeClick(date);
 			Teleopti.MyTimeWeb.Common.Layout.ActivatePlaceHolder();
-        };
+		};
 
-        self.resetToolbarActiveButtons = function() {
-            self.addTextRequestActive(false);
-            self.addAbsenceRequestActive(false);
-            self.addShiftTradeRequestActive(false);
-            self.addShiftTradeBulletinBoardActive(false);
-            self.addPostShiftForTradeActive(false);
-        };
-    }
+		self.resetToolbarActiveButtons = function () {
+			self.addTextRequestActive(false);
+			self.addAbsenceRequestActive(false);
+			self.addShiftTradeRequestActive(false);
+			self.addShiftTradeBulletinBoardActive(false);
+			self.addPostShiftForTradeActive(false);
+		};
+	}
 
-    function _initNavigationViewModel() {
-    	requestNavigationViewModel = new RequestNavigationViewModel();
+	function _initNavigationViewModel() {
+		requestNavigationViewModel = new RequestNavigationViewModel();
 		ko.applyBindings(requestNavigationViewModel, $('div.navbar-container')[0]);
 	}
 
 	return {
 		Init: function () {
-		    Teleopti.MyTimeWeb.Portal.RegisterPartialCallBack('Requests/Index',
-		        Teleopti.MyTimeWeb.Request.RequestPartialInit,
-		        Teleopti.MyTimeWeb.Request.RequestPartialDispose);
+			Teleopti.MyTimeWeb.Portal.RegisterPartialCallBack('Requests/Index',
+				Teleopti.MyTimeWeb.Request.RequestPartialInit,
+				Teleopti.MyTimeWeb.Request.RequestPartialDispose);
 		},
 		RequestPartialInit: function (readyForInteractionCallback, completelyLoadedCallback) {
-		    readyForInteraction = readyForInteractionCallback;
-		    completelyLoaded = completelyLoadedCallback;
-		    
-		    if (!$('#Requests-body-inner').length) {
-		        readyForInteraction();
-		        readyForInteraction();
-		        completelyLoaded();
-		        return;
-		    }
+			readyForInteraction = readyForInteractionCallback;
+			completelyLoaded = completelyLoadedCallback;
+
+			if (!$('#Requests-body-inner').length) {
+				readyForInteraction();
+				readyForInteraction();
+				completelyLoaded();
+				return;
+			}
 
 			Teleopti.MyTimeWeb.Request.AddShiftTradeRequest.Init();
 			Teleopti.MyTimeWeb.Request.List.Init(readyForInteraction, completelyLoaded);
@@ -105,25 +116,26 @@ Teleopti.MyTimeWeb.Request = (function ($) {
 			_initNavigationViewModel();
 			Teleopti.MyTimeWeb.Request.RequestDetail.Init();
 		},
-	    RequestPartialDispose: function() {
-	    	Teleopti.MyTimeWeb.Request.List.Dispose();
-		    Teleopti.MyTimeWeb.Request.AddShiftTradeRequest.Dispose();
-	    },
-	    ResetToolbarActiveButtons: function () {
-	        requestNavigationViewModel.resetToolbarActiveButtons();
-	    },
-	    ShiftTradeRequest: function (date) {
-	    	requestNavigationViewModel.addShiftTradeRequest(date);
-	    },
-		ShiftTradeBulletinBoardRequest: function(date) {
+		RequestPartialDispose: function () {
+			Teleopti.MyTimeWeb.Request.List.Dispose();
+			Teleopti.MyTimeWeb.Request.AddShiftTradeRequest.Dispose();
+		},
+		ResetToolbarActiveButtons: function () {
+			requestNavigationViewModel.resetToolbarActiveButtons();
+		},
+		ShiftTradeRequest: function (date) {
+			requestNavigationViewModel.addShiftTradeRequest(date);
+		},
+		ShiftTradeBulletinBoardRequest: function (date) {
 			requestNavigationViewModel.addShiftTradeBulletinBoardRequest(date);
 		},
 		PostShiftForTradeRequest: function (date) {
 			requestNavigationViewModel.addPostShiftForTradeRequest(date);
 		},
-		HideFab: function(show){
+		HideFab: function (show) {
 			if (requestNavigationViewModel != null)
-			requestNavigationViewModel.hideFab(show);
+				requestNavigationViewModel.hideFab(show);
+			requestNavigationViewModel.menuIsVisible(show);
 		}
 	};
 
