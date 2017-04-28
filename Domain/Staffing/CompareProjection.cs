2fd520ab243fc96b96b856910188ba4ed646f57c
amandaj <none@none>
2017-04-28 14:17:17 +0200

    PBI #43447 Adding cases to CompareProjection

--- Domain/Staffing/CompareProjection.cs
+++ Domain/Staffing/CompareProjection.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Staffing
@@ -10,24 +12,85 @@ namespace Teleopti.Ccc.Domain.Staffing
 	public class CompareProjection
 	{
 		private readonly IIntervalLengthFetcher _intervalLengthFetcher;
+		private readonly IActivityRepository _activityRepository;
+		private readonly IDisableDeletedFilter _disableDeletedFilter;
 
-		public CompareProjection(IIntervalLengthFetcher intervalLengthFetcher)
+		public CompareProjection(IIntervalLengthFetcher intervalLengthFetcher, IActivityRepository activityRepository, IDisableDeletedFilter disableDeletedFilter)
 		{
 			_intervalLengthFetcher = intervalLengthFetcher;
+			_activityRepository = activityRepository;
+			_disableDeletedFilter = disableDeletedFilter;
 		}
 
-		public IEnumerable<ActivityResourceInterval> Compare(IScheduleDay before, IScheduleDay after)
+		public IEnumerable<ActivityResourceInterval> Compare(IScheduleDay scheduleDayBefore, IScheduleDay scheduleDayAfter)
 		{
-			var resourceChanges = new List<ActivityResourceInterval>();
+			IEnumerable<IActivity> allActivities;
+			using (_disableDeletedFilter.Disable())
+			{
+				allActivities = _activityRepository.LoadAll();
+			}
+
 			var resolution = _intervalLengthFetcher.IntervalLength;
-			resourceChanges.AddRange(remove(before, resolution));
-			return resourceChanges;
+			var beforeIntervals = getActivityIntervals(scheduleDayBefore, resolution);
+			var afterIntervals = getActivityIntervals(scheduleDayAfter, resolution);
+
+			var allIntervals = new List<ActivityResourceInterval>();
+			allIntervals.AddRange(beforeIntervals);
+			allIntervals.AddRange(afterIntervals);
+
+			var totalStart = allIntervals.Min(x => x.Interval.StartDateTime);
+			var totaltEnd = allIntervals.Max(x => x.Interval.EndDateTime);
+
+			var output = new List<ActivityResourceInterval>();
+			var intervalStart = totalStart;
+			while (intervalStart < totaltEnd)
+			{
+				var before = beforeIntervals.FirstOrDefault(x => x.Interval.StartDateTime == intervalStart);
+				var after = afterIntervals.FirstOrDefault(x => x.Interval.StartDateTime == intervalStart);
+				intervalStart = intervalStart.AddMinutes(resolution);
+
+				if (before != null && after != null)
+				{
+					if (before.Activity == after.Activity && Math.Abs(before.Resource - after.Resource) <= 0.01)
+						continue;
+
+
+					if (before.Activity == after.Activity && allActivities.First(x => x.Id == after.Activity).RequiresSkill)
+					{
+						output.Add(new ActivityResourceInterval
+						{
+							Interval = before.Interval,
+							Activity = before.Activity,
+							Resource = after.Resource - before.Resource
+						});
+						continue;
+					}
+
+					//activity is different
+					if (allActivities.First(x => x.Id == before.Activity).RequiresSkill)
+						output.Add(new ActivityResourceInterval
+						{
+							Interval = before.Interval,
+							Activity = before.Activity,
+							Resource = -before.Resource
+						});
+					if (allActivities.First(x => x.Id == after.Activity).RequiresSkill)
+						output.Add(new ActivityResourceInterval
+						{
+							Interval = after.Interval,
+							Activity = after.Activity,
+							Resource = after.Resource
+						});
+
+				}
+			}
+			return output;
 		}
 
-		private IEnumerable<ActivityResourceInterval> remove(IScheduleDay before, int resolution)
+		private IEnumerable<ActivityResourceInterval> getActivityIntervals(IScheduleDay scheduleDay, int resolution)
 		{
 			var activityResouceIntervals = new List<ActivityResourceInterval>();
-			var projection = before.ProjectionService().CreateProjection();
+			var projection = scheduleDay.ProjectionService().CreateProjection();
 
 			var layers = projection.ToResourceLayers(resolution).ToList();
 			foreach (var layer in layers)
@@ -36,11 +99,13 @@ namespace Teleopti.Ccc.Domain.Staffing
 				{
 					Activity = layer.PayloadId,
 					Interval = layer.Period,
-					Resource = -layer.Resource
+					Resource = layer.Resource
 				});
 			}
 			return activityResouceIntervals;
 		}
+
+		
 	}
 
 	public class ActivityResourceInterval
--- DomainTest/Staffing/CompareProjectionTest.cs
+++ DomainTest/Staffing/CompareProjectionTest.cs
@@ -2,10 +2,12 @@
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
+using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Staffing;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
@@ -29,7 +31,7 @@ namespace Teleopti.Ccc.DomainTest.Staffing
 		public IScheduleStorage ScheduleStorage;
 
 		[Test]
-		public void ShouldRemoveBeforeResourcesOnInterval()
+		public void ShouldNotDoAnythingIfSame()
 		{
 			IntervalLengthFetcher.Has(15);
 			var now = new DateTime(2017, 5, 1, 0, 0, 0);
@@ -44,13 +46,12 @@ namespace Teleopti.Ccc.DomainTest.Staffing
 			PersonAssignmentRepository.Has(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, assignmentPeriod, new ShiftCategory("category")));
 			
 			var scheduleRange = ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod, scenario)[person];
-			var activityResourceIntervals = Target.Compare(scheduleRange.ScheduledDay(new DateOnly(now)), null);
-			activityResourceIntervals.Count().Should().Be.EqualTo(4);
-			activityResourceIntervals.First().Resource.Should().Be.EqualTo(-1);
+			var activityResourceIntervals = Target.Compare(scheduleRange.ScheduledDay(new DateOnly(now)), scheduleRange.ScheduledDay(new DateOnly(now)));
+			activityResourceIntervals.Count().Should().Be.EqualTo(0);
 		}
 
 		[Test]
-		public void ShouldRemoveBeforeResourcesOnIntervalAndConsiderFractions()
+		public void ShouldAddResourceWhenSameActivityButMoreResourcesAfter()
 		{
 			IntervalLengthFetcher.Has(120);
 			var now = new DateTime(2017, 5, 1, 0, 0, 0);
@@ -59,15 +60,102 @@ namespace Teleopti.Ccc.DomainTest.Staffing
 			var scenario = ScenarioRepository.Has("scenario");
 			var activity = ActivityRepository.Has("activity");
 			var skill = SkillRepository.Has("skill", activity).WithId();
+			skill.DefaultResolution = 60;
 			var person = PersonRepository.Has(skill);
 
-			var assignmentPeriod = new DateTimePeriod(2017, 5, 1, 8, 2017, 5, 1, 9);
-			PersonAssignmentRepository.Has(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, assignmentPeriod, new ShiftCategory("category")));
-
-			var scheduleRange = ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod, scenario)[person];
-			var activityResourceIntervals = Target.Compare(scheduleRange.ScheduledDay(new DateOnly(now)), null);
+			var assignmentPeriod1 = new DateTimePeriod(2017, 5, 1, 8, 2017, 5, 1, 9);
+			var assignmentPeriod2 = new DateTimePeriod(2017, 5, 1, 9, 2017, 5, 1, 10);
+			var assignment = PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, assignmentPeriod1, new ShiftCategory("category"));
+			PersonAssignmentRepository.Has(assignment);
+			var beforeCopy = (IScheduleDay)ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod1, scenario)[person].ScheduledDay(new DateOnly(now)).Clone();
+			assignment.AddActivity(activity, assignmentPeriod2);
+			var after = ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod1, scenario)[person].ScheduledDay(new DateOnly(now));
+			var activityResourceIntervals = Target.Compare(beforeCopy, after);
 			activityResourceIntervals.Count().Should().Be.EqualTo(1);
-			activityResourceIntervals.First().Resource.Should().Be.EqualTo(-0.5);
+			activityResourceIntervals.First(x => x.Interval.StartDateTime == assignmentPeriod1.StartDateTime).Resource.Should().Be.EqualTo(0.5);
 		}
+
+		[Test]
+		public void ShouldRemoveResourceWhenSameActivityButLessResourcesAfter()
+		{
+			IntervalLengthFetcher.Has(120);
+			var now = new DateTime(2017, 5, 1, 0, 0, 0);
+			Now.Is(now);
+
+			var scenario = ScenarioRepository.Has("scenario");
+			var activity = ActivityRepository.Has("activity");
+			var skill = SkillRepository.Has("skill", activity).WithId();
+			skill.DefaultResolution = 60;
+			var person = PersonRepository.Has(skill);
+
+			var assignmentPeriod1 = new DateTimePeriod(2017, 5, 1, 8, 2017, 5, 1, 9);
+			var assignmentPeriod2 = new DateTimePeriod(2017, 5, 1, 9, 2017, 5, 1, 10);
+			var assignment = PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, assignmentPeriod1, new ShiftCategory("category"));
+			PersonAssignmentRepository.Has(assignment);
+			var afterCopy = (IScheduleDay)ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod1, scenario)[person].ScheduledDay(new DateOnly(now)).Clone();
+			assignment.AddActivity(activity, assignmentPeriod2);
+			var before = ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod1, scenario)[person].ScheduledDay(new DateOnly(now));
+			var activityResourceIntervals = Target.Compare(before, afterCopy);
+			activityResourceIntervals.Count().Should().Be.EqualTo(1);
+			activityResourceIntervals.First(x => x.Interval.StartDateTime == assignmentPeriod1.StartDateTime).Resource.Should().Be.EqualTo(-0.5);
+		}
+
+		[Test]
+		public void ShouldRemoveResourceWhenAddingNoSkillActivity()
+		{
+			IntervalLengthFetcher.Has(60);
+			var now = new DateTime(2017, 5, 1, 0, 0, 0);
+			Now.Is(now);
+
+			var scenario = ScenarioRepository.Has("scenario");
+			var activity = ActivityRepository.Has("activity");
+			var activityNoSKill = ActivityRepository.Has("activityNoSkill");
+			activityNoSKill.RequiresSkill = false;
+			var skill = SkillRepository.Has("skill", activity).WithId();
+			skill.DefaultResolution = 60;
+			var person = PersonRepository.Has(skill);
+
+			var assignmentPeriod = new DateTimePeriod(2017, 5, 1, 8, 2017, 5, 1, 11);
+			var noSkillPeriod = new DateTimePeriod(2017, 5, 1, 9, 2017, 5, 1, 10);
+			var assignment = PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, assignmentPeriod, new ShiftCategory("category"));
+			PersonAssignmentRepository.Has(assignment);
+			var beforeCopy = (IScheduleDay)ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod, scenario)[person].ScheduledDay(new DateOnly(now)).Clone();
+			assignment.AddActivity(activityNoSKill, noSkillPeriod);
+			var after = ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod, scenario)[person].ScheduledDay(new DateOnly(now));
+
+			var activityResourceIntervals = Target.Compare(beforeCopy, after);
+			activityResourceIntervals.Count().Should().Be.EqualTo(1);
+			activityResourceIntervals.First(x => x.Interval.StartDateTime == noSkillPeriod.StartDateTime).Resource.Should().Be.EqualTo(-1);
+		}
+
+		[Test]
+		public void ShouldAddResourceWhenRemovingNoSkillActivity()
+		{
+			IntervalLengthFetcher.Has(60);
+			var now = new DateTime(2017, 5, 1, 0, 0, 0);
+			Now.Is(now);
+
+			var scenario = ScenarioRepository.Has("scenario");
+			var activity = ActivityRepository.Has("activity");
+			var activityNoSKill = ActivityRepository.Has("activityNoSkill");
+			activityNoSKill.RequiresSkill = false;
+			var skill = SkillRepository.Has("skill", activity).WithId();
+			skill.DefaultResolution = 60;
+			var person = PersonRepository.Has(skill);
+
+			var assignmentPeriod = new DateTimePeriod(2017, 5, 1, 8, 2017, 5, 1, 11);
+			var noSkillPeriod = new DateTimePeriod(2017, 5, 1, 9, 2017, 5, 1, 10);
+			var assignment = PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, assignmentPeriod, new ShiftCategory("category"));
+			PersonAssignmentRepository.Has(assignment);
+			var afterCopy = (IScheduleDay)ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod, scenario)[person].ScheduledDay(new DateOnly(now)).Clone();
+			assignment.AddActivity(activityNoSKill, noSkillPeriod);
+			var before = ScheduleStorage.FindSchedulesForPersonOnlyInGivenPeriod(person, new ScheduleDictionaryLoadOptions(false, false), assignmentPeriod, scenario)[person].ScheduledDay(new DateOnly(now));
+
+			var activityResourceIntervals = Target.Compare(before, afterCopy);
+			activityResourceIntervals.Count().Should().Be.EqualTo(1);
+			activityResourceIntervals.First(x => x.Interval.StartDateTime == noSkillPeriod.StartDateTime).Resource.Should().Be.EqualTo(1);
+		}
+
+		//activity change in the middle of interval
 	}
 }
