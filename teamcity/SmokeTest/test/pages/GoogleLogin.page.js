70b2ae9df84e2b5bc58dfcf45aaef91178722105
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-05-03 17:50:46 +0200

    Still doesn't work, more logging

--- teamcity/SmokeTest/test/pages/GoogleLogin.page.js
+++ teamcity/SmokeTest/test/pages/GoogleLogin.page.js
@@ -3,24 +3,21 @@ var Page = require('./Page.page')
 var GoogleLoginPage = Object.create(Page, {
 
     username: { get: function () { return browser.element('input[type=email]'); } },
-    next: { get: function () { return browser.element('#identifierNext > content'); } },
+    next: { get: function () { return browser.element('#identifierNext'); } },
     password: { get: function () { return browser.element('input[type=password]'); } },
     signinButton: { get: function () { return browser.element('#passwordNext'); } },
 	
-    clickSignin: { value: function() {
-        this.signinButton.click();
-    } },
-	
 	signin: { value: function() {
 		console.log('Signing in using Google');
-		if (!browser.isExisting('input[type=password]')) {
-			this.username.waitForVisible(60 * 1000);
-			console.log('Username input exists, entering username');
-			this.username.setValue('demo.teleopti@gmail.com');
-			console.log('Clicking next button');
-			this.next.click();
-		}		
-		
+		console.log('Waiting for username input');
+		this.username.waitForVisible(60 * 1000);
+		console.log('Username input exists, entering username');
+		this.username.setValue('demo.teleopti@gmail.com');
+		console.log('Clicking next button');
+		this.next.click();
+
+		browser.pause(1000);
+		console.log('Waiting for password input');
 		this.password.waitForVisible(60 * 1000);
 		console.log('Password input exists, entering password');
 		this.password.setValue('m8kemew0rk');
