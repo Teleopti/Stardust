bec0ea8d63d253abf7409d69958f662a1db2dd76
Jianfeng <none@none>
2017-05-05 10:13:41 +0800

    using tab indent for Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel.Tests.js

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel.Tests.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel.Tests.js
@@ -3,158 +3,158 @@
 /// <reference path="~/Content/Scripts/qunit.js" />
 
 $(document).ready(function() {
-    module("Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel");
+	module("Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel");
 
-    test("should set current date", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+	test("should set current date", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
 
-        viewModel.setCurrentDate(moment().format('YYYY-MM-DD'));
+		viewModel.setCurrentDate(moment().format('YYYY-MM-DD'));
 
-        equal(viewModel.selectedDate(), moment().format('YYYY-MM-DD'));
-    });
+		equal(viewModel.selectedDate(), moment().format('YYYY-MM-DD'));
+	});
 
-    test("should set display date", function () {
-         Teleopti.MyTimeWeb.Common.SetupCalendar({
-            DateFormat: "DD/MM/YYYY",
-            UseJalaaliCalendar: false
-        });
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+	test("should set display date", function () {
+		 Teleopti.MyTimeWeb.Common.SetupCalendar({
+			DateFormat: "DD/MM/YYYY",
+			UseJalaaliCalendar: false
+		});
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
 
-        var rawData = {
-            Date: moment().format('YYYY-MM-DD'),
-            Schedule: {
+		var rawData = {
+			Date: moment().format('YYYY-MM-DD'),
+			Schedule: {
 				FixedDate: null,
-                Summary: {
-                    Color: null,
-                    Title: null,
+				Summary: {
+					Color: null,
+					Title: null,
 					TimeSpan: null
-                },
+				},
 				Header:{Title: null}
-            }
-        };
-        viewModel.readData(rawData);
+			}
+		};
+		viewModel.readData(rawData);
 
-        equal(viewModel.displayDate(), moment().format('DD/MM/YYYY'));
-    });
+		equal(viewModel.displayDate(), moment().format('DD/MM/YYYY'));
+	});
 
-    test("should display date correctly when in Persian date format", function () {
-        Teleopti.MyTimeWeb.Common.SetupCalendar({
-            DateFormat: "DD/MM/YYYY",
-            UseJalaaliCalendar: true
-        });
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+	test("should display date correctly when in Persian date format", function () {
+		Teleopti.MyTimeWeb.Common.SetupCalendar({
+			DateFormat: "DD/MM/YYYY",
+			UseJalaaliCalendar: true
+		});
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
 
-        var rawData = {
-            Date: moment().format('YYYY-MM-DD'),
-            Schedule: {
-                FixedDate: null,
-                Summary: {
-                    Color: null,
-                    Title: null,
-                    TimeSpan: null
-                },
-                Header:{Title: null}
-            }
-        };
-        viewModel.readData(rawData);
-
-        equal(viewModel.displayDate(), moment(rawData.Date).format(Teleopti.MyTimeWeb.Common.DateFormat));
-    });
-
-    test("should set summary color", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
-
-        var rawData = {
-            Schedule: {
+		var rawData = {
+			Date: moment().format('YYYY-MM-DD'),
+			Schedule: {
 				FixedDate: null,
-                Summary: {
-                    Color: "0, 255, 0",
-                    Title: null,
+				Summary: {
+					Color: null,
+					Title: null,
 					TimeSpan: null
-                },
+				},
 				Header:{Title: null}
-            }
-        };
-        viewModel.readData(rawData);
+			}
+		};
+		viewModel.readData(rawData);
 
-        equal(viewModel.summaryColor(), rawData.Schedule.Summary.Color);
-    });
+		equal(viewModel.displayDate(), moment(rawData.Date).format(Teleopti.MyTimeWeb.Common.DateFormat));
+	});
 
-    test("should set summary name", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+	test("should set summary color", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
 
-        var rawData = {
-            Schedule: {
+		var rawData = {
+			Schedule: {
 				FixedDate: null,
-                Summary: {
-                    Color: null,
-                    Title: "Early",
+				Summary: {
+					Color: "0, 255, 0",
+					Title: null,
 					TimeSpan: null
-                },
+				},
 				Header:{Title: null}
-            }
-        };
-        viewModel.readData(rawData);
+			}
+		};
+		viewModel.readData(rawData);
 
-        equal(viewModel.summaryName(), rawData.Schedule.Summary.Title);
-    });
+		equal(viewModel.summaryColor(), rawData.Schedule.Summary.Color);
+	});
 
-    test("should set summary time", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+	test("should set summary name", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
 
-        var rawData = {
-            Schedule: {
+		var rawData = {
+			Schedule: {
 				FixedDate: null,
-                Summary: {
-                    Color: null,
-                    Title: null,
+				Summary: {
+					Color: null,
+					Title: "Early",
+					TimeSpan: null
+				},
+				Header:{Title: null}
+			}
+		};
+		viewModel.readData(rawData);
+
+		equal(viewModel.summaryName(), rawData.Schedule.Summary.Title);
+	});
+
+	test("should set summary time", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+
+		var rawData = {
+			Schedule: {
+				FixedDate: null,
+				Summary: {
+					Color: null,
+					Title: null,
 					TimeSpan: "9:00-18:00"
-                },
+				},
 				Header:{Title: null}
-            }
-        };
-        viewModel.readData(rawData);
+			}
+		};
+		viewModel.readData(rawData);
 
-        equal(viewModel.summaryTime(), rawData.Schedule.Summary.TimeSpan);
-    });
+		equal(viewModel.summaryTime(), rawData.Schedule.Summary.TimeSpan);
+	});
 
-    test("should set whether current day is day off", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+	test("should set whether current day is day off", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
 
-        var rawData = {
-            Schedule: {
+		var rawData = {
+			Schedule: {
 				FixedDate: null,
-                Summary: {
-                    Color: null,
-                    Title: null,
+				Summary: {
+					Color: null,
+					Title: null,
 					TimeSpan: null
-                },
-                Header: { Title: null },
+				},
+				Header: { Title: null },
 				IsDayOff: true
-            }
-        };
-        viewModel.readData(rawData);
+			}
+		};
+		viewModel.readData(rawData);
 
-        equal(viewModel.isDayOff(), rawData.Schedule.IsDayOff);
-    }); 
+		equal(viewModel.isDayOff(), rawData.Schedule.IsDayOff);
+	}); 
 
-    test("should set to next date", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
-        var today = moment().format('YYYY-MM-DD');
-        viewModel.selectedDate(today);
+	test("should set to next date", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+		var today = moment().format('YYYY-MM-DD');
+		viewModel.selectedDate(today);
 
-        viewModel.nextDay();
+		viewModel.nextDay();
 
-        equal(viewModel.selectedDate().format("YYYY-MM-DD"), moment(today).add(1, 'days').format("YYYY-MM-DD"));
-    });
+		equal(viewModel.selectedDate().format("YYYY-MM-DD"), moment(today).add(1, 'days').format("YYYY-MM-DD"));
+	});
 
-    test("should set to previous date", function () {
-        var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
-        var today = moment().format('YYYY-MM-DD');
-        viewModel.selectedDate(today);
+	test("should set to previous date", function () {
+		var viewModel = new Teleopti.MyTimeWeb.Schedule.MobileStartDayViewModel();
+		var today = moment().format('YYYY-MM-DD');
+		viewModel.selectedDate(today);
 
-        viewModel.previousDay();
+		viewModel.previousDay();
 
-        equal(viewModel.selectedDate().format("YYYY-MM-DD"), moment(today).add(-1, 'days').format("YYYY-MM-DD"));
-    });
+		equal(viewModel.selectedDate().format("YYYY-MM-DD"), moment(today).add(-1, 'days').format("YYYY-MM-DD"));
+	});
 });
