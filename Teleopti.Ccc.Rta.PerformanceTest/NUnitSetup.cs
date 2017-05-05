acb585d6ae46ef141d21582fa7ba2884ab75c422
Justin! <justin@teleopti.com>
2017-05-05 14:42:47 +0200

    #43924 Minor cleanup

--- Teleopti.Ccc.Rta.PerformanceTest/NUnitSetup.cs
+++ Teleopti.Ccc.Rta.PerformanceTest/NUnitSetup.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using Autofac;
 using log4net.Config;
 using NUnit.Framework;
-using Teleopti.Ccc.Domain.Common;
-using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.MessageBroker.Client;
-using Teleopti.Ccc.Domain.Repositories;
-using Teleopti.Ccc.Domain.UnitOfWork;
 using Teleopti.Ccc.Infrastructure.Aop;
-using Teleopti.Ccc.Infrastructure.Hangfire;
-using Teleopti.Ccc.Infrastructure.UnitOfWork;
 using Teleopti.Ccc.Rta.PerformanceTest.Code;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.IoC;
-using Teleopti.Ccc.TestCommon.TestData.Setups.Default;
 using Teleopti.Ccc.TestCommon.Web.WebInteractions;
 using Teleopti.Messaging.Client;
 
