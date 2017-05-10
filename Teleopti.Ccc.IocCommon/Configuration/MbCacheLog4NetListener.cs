c51d1474e1d45ae51c1d31413c72f341c6ee183b
Roger Kratz <roger.kratz@teleopti.com>
2017-05-10 10:46:24 +0200

    Upgrading mbcache to 2.11

--- Teleopti.Ccc.IocCommon/Configuration/MbCacheLog4NetListener.cs
+++ Teleopti.Ccc.IocCommon/Configuration/MbCacheLog4NetListener.cs
@@ -9,6 +9,7 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 		private const string putMessage = "Adding cache value {0} for {1} (key: {2})";
 		private const string deleteMessage = "Removing cache entries for {0} (key starts with: {1})";
 		private const string cacheHitLogMessage = "Cache hit for {0} (key: {1})";
+		private const string nullReplacer = "[all methods]";
 
 		private static readonly ILog log = LogManager.GetLogger("Teleopti.MbCache");
 
@@ -16,7 +17,7 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 		{
 			if (log.IsDebugEnabled)
 			{
-				log.DebugFormat(cacheHitLogMessage, cachedItem.EventInformation.MethodName(), cachedItem.EventInformation.CacheKey);
+				log.DebugFormat(cacheHitLogMessage, cachedItem.EventInformation.Method?.ToString() ?? nullReplacer, cachedItem.EventInformation.CacheKey);
 			}
 		}
 
@@ -24,7 +25,7 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 		{
 			if (log.IsDebugEnabled)
 			{
-				log.DebugFormat(deleteMessage, cachedItem.EventInformation.MethodName(), cachedItem.EventInformation.CacheKey);
+				log.DebugFormat(deleteMessage, cachedItem.EventInformation.Method?.ToString() ?? nullReplacer, cachedItem.EventInformation.CacheKey);
 			}
 		}
 
@@ -32,7 +33,7 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 		{
 			if (log.IsDebugEnabled)
 			{
-				log.DebugFormat(putMessage, cachedItem.CachedValue, cachedItem.EventInformation.MethodName(), cachedItem.EventInformation.CacheKey);
+				log.DebugFormat(putMessage, cachedItem.CachedValue, cachedItem.EventInformation.Method?.ToString() ?? nullReplacer, cachedItem.EventInformation.CacheKey);
 			}
 		}
 
