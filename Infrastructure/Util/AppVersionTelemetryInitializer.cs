60767c00a3b69d2528531ca89b74f9b9e65bd120
Robin Karlsson <robin.karlsson@teleopti.com>
2017-05-10 09:18:07 +0200

    #41102 Added ApplicationInsights log4net appender for service bus host

---
+++ Infrastructure/Util/AppVersionTelemetryInitializer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Teleopti.Ccc.Infrastructure.Util
+{
+	public class AppVersionTelemetryInitializer : ITelemetryInitializer
+	{
+		private static readonly string applicationVersion = getApplicationVersion();
+
+		private static string getApplicationVersion()
+		{
+			var attr2 = Attribute
+					.GetCustomAttribute(
+						typeof(AppVersionTelemetryInitializer).Assembly,
+						typeof(AssemblyInformationalVersionAttribute))
+				as AssemblyInformationalVersionAttribute;
+			return attr2 == null ? "1.0.0.0" : attr2.InformationalVersion;
+		}
+
+		public void Initialize(ITelemetry telemetry)
+		{
+			telemetry.Context.Properties["TeleoptiVersion"] = applicationVersion;
+		}
+	}
+}
---
+++ Infrastructure/Util/DeployedHostTelemetryInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Infrastructure.Util
+{
+	public class DeployedHostTelemetryInitializer : ITelemetryInitializer
+	{
+		private static readonly Lazy<string> deployedHost = new Lazy<string>(getHost);
+
+		private static string getHost()
+		{
+			return ConfigurationManager.AppSettings["MessageBroker"] ?? (StateHolderReader.IsInitialized ? StateHolderReader.Instance.StateReader.ApplicationScopeData.AppSettings["MessageBroker"] : "");
+		}
+
+		public void Initialize(ITelemetry telemetry)
+		{
+			telemetry.Context.Properties["DeployedHost"] = deployedHost.Value;
+		}
+	}
+}
