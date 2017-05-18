6d46d8ab39ba922200ea0fb6c6c25ce700fe0c57
johan Damm <none@none>
2017-05-18 15:34:37 +0200

    #44194 alina thinks this might be related to the browser timeout, lets find out :)

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.js
@@ -1,84 +1,85 @@
 // Karma configuration
 // Generated on Wed Feb 04 2015 10:25:13 GMT+0100 (W. Europe Standard Time)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+	config.set({
 
-	// base path that will be used to resolve all patterns (eg. files, exclude)
-	basePath: '',
-
-
-	// frameworks to use
-	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-	frameworks: ['jasmine'],
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
 
-	// list of files / patterns to load in the browser
-	files: [
-		'dist/modules.*',
-		'node_modules/angular-mocks/angular-mocks.js',
-		'dist/templates.*',
-		'app/**/*.js',
-
-		//served seat image file at browser because addSeat function need to create seat object from image in seatManagement test.
-		{ pattern: 'app/seatManagement/images/*.svg', watched: false, included: false, served: true }
-	],
-
-	proxies: {
-		'/app/seatManagement/images/': '/base/app/seatManagement/images/'
-	},
-
-	// list of files to exclude
-	exclude: [
-		'**/Gruntfile.js'
-	],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['jasmine'],
 
 
-	// preprocess matching files before serving them to the browser
-	// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-	preprocessors: {
-	},
+		// list of files / patterns to load in the browser
+		files: [
+			'dist/modules.*',
+			'node_modules/angular-mocks/angular-mocks.js',
+			'dist/templates.*',
+			'app/**/*.js',
+
+			//served seat image file at browser because addSeat function need to create seat object from image in seatManagement test.
+			{ pattern: 'app/seatManagement/images/*.svg', watched: false, included: false, served: true }
+		],
+
+		proxies: {
+			'/app/seatManagement/images/': '/base/app/seatManagement/images/'
+		},
+
+		// list of files to exclude
+		exclude: [
+			'**/Gruntfile.js'
+		],
+		browserNoActivityTimeout: 100000,
+
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+		},
 
 
-	// test results reporter to use
-	// possible values: 'dots', 'progress'
-	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-	reporters: ['progress'],
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress'],
 
 
-	// web server port
-	port: 9876,
+
+		// web server port
+		port: 9876,
 
 
-	// enable / disable colors in the output (reporters and logs)
-	colors: true,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
 
-	// level of logging
-	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-	logLevel: config.LOG_INFO,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-	// enable / disable watching file and executing tests whenever any file changes
-	autoWatch: true,
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
 
-	// start these browsers
-	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-	browsers: ['Chrome'],
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['Chrome'],
 
-   customLaunchers: {
-     Chrome_small: {
-       base: 'Chrome',
-       flags: [
-           '--window-size=400,400',
-           '--window-position=-400,0'
-       ]
-     }
-   },
+		customLaunchers: {
+			Chrome_small: {
+				base: 'Chrome',
+				flags: [
+					'--window-size=400,400',
+					'--window-position=-400,0'
+				]
+			}
+		},
 
-	// Continuous Integration mode
-	// if true, Karma captures browsers, runs the tests and exits
-	singleRun: true
-  });
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true
+	});
 };
