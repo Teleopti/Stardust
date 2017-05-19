b86e05f8fcc36695c1b47bca9a700b65bc95310b
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-05-19 10:54:53 +0200

    #44404 Added a delay to message sending to try to prevent signalr from consuming too many connections

--- Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/InProcessMessageSender.cs
+++ Teleopti.Messaging/Teleopti.Messaging/Client/SignalR/InProcessMessageSender.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using log4net;
+using Teleopti.Ccc.Domain.Config;
 using Teleopti.Ccc.Domain.MessageBroker;
 using Teleopti.Ccc.Domain.MessageBroker.Client;
 using Teleopti.Ccc.Domain.MessageBroker.Server;
@@ -15,46 +16,58 @@ namespace Teleopti.Messaging.Client.SignalR
 		private readonly IMessageBrokerServer _messageBrokerServer;
 		private readonly ConcurrentQueue<Action> _actions;
 		private readonly ILog logger = LogManager.GetLogger(typeof(InProcessMessageSender));
+		private readonly int _messagesPerSecond;
+		private bool isRunning;
+		private readonly object lockObject = new object();
 
-		public InProcessMessageSender(IMessageBrokerServer messageBrokerServer)
+		public InProcessMessageSender(IMessageBrokerServer messageBrokerServer, IConfigReader config)
 		{
 			_messageBrokerServer = messageBrokerServer;
 			_actions = new ConcurrentQueue<Action>();
-			Task.Run(() => processQueue());
+			_messagesPerSecond = config.ReadValue("MessagesPerSecond", 80);
 		}
 
 		public void Send(Message message)
 		{
 			_actions.Enqueue(() => _messageBrokerServer.NotifyClients(message));
+			ensureProcessing();
 		}
 
 		public void SendMultiple(IEnumerable<Message> messages)
 		{
 			_actions.Enqueue(() => _messageBrokerServer.NotifyClientsMultiple(messages));
+			ensureProcessing();
+		}
+
+		private void ensureProcessing()
+		{
+			lock (lockObject)
+			{
+				if (isRunning) return;
+				isRunning = true;
+				Task.Run(() => processQueue());
+			}
 		}
 
 		private void processQueue()
 		{
-			while (true)
+			while (!_actions.IsEmpty)
 			{
-				if (_actions.IsEmpty)
-				{
-					Thread.Sleep(TimeSpan.FromSeconds(1));
-					continue;
-				}
 				Action action;
-				if (_actions.TryDequeue(out action))
+				if (!_actions.TryDequeue(out action)) continue;
+				try
 				{
-					try
-					{
-						action();
-					}
-					catch (Exception e)
-					{
-						logger.Error("An error occured while sending messages in process.", e);
-					}
+					action();
 				}
-
+				catch (Exception e)
+				{
+					logger.Error("An error occurred while sending messages in process.", e);
+				}
+				Thread.Sleep(1000 / _messagesPerSecond);
+			}
+			lock (lockObject)
+			{
+				isRunning = false;
 			}
 		}
 	}
