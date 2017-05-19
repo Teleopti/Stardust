0068ba8ab0a8efc47902953d0e4624907165f142
Erik Sundberg <erik.sundberg@teleopti.com>
2017-05-19 11:04:03 +0200

    #43924 Remove single state perf test and add a new queue test

---
+++ Teleopti.Ccc.Rta.PerformanceTest/QueueWorkSmallBatchesTest.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using Teleopti.Ccc.Infrastructure.Hangfire;
+using Teleopti.Ccc.Infrastructure.Rta;
+using Teleopti.Ccc.Rta.PerformanceTest.Code;
+
+namespace Teleopti.Ccc.Rta.PerformanceTest
+{
+	[TestFixture]
+	[RtaPerformanceTest]
+	public class QueueWorkSmallBatchesTest
+	{
+		public StatesSender States;
+		public HangfireUtilities Hangfire;
+		public StateQueueUtilities StateQueue;
+		public TestCommon.PerformanceTest.PerformanceTest PerformanceTest;
+
+		[Test]
+		public void MeasurePerformance()
+		{
+			PerformanceTest.Measure("1mKUHvBlk5wIk0LDZESO2prWvRuimhpjiWaSvoKk2gsE", "QueueWorkSmallBatchesTest", () =>
+			{
+				States.SendAllAsSmallBatches();
+				StateQueue.WaitForQueue();
+				Hangfire.WaitForQueue();
+			});
+		}
+	}
+}
