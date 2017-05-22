ec1586d7d6f53e5076110ae50c45f60414395068
Robin Karlsson <robin.karlsson@teleopti.com>
2017-05-22 10:59:38 +0200

    Cleanup

--- Domain/MessageBroker/Client/MessageFilterManager.cs
+++ Domain/MessageBroker/Client/MessageFilterManager.cs
@@ -9,7 +9,6 @@ using Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service;
 using Teleopti.Ccc.Domain.ApplicationLayer.AbsenceRequests;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Messaging;
-using Teleopti.Ccc.Domain.Forecasting;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.Kpi;
@@ -25,7 +24,7 @@ namespace Teleopti.Ccc.Domain.MessageBroker.Client
 {
 	public class MessageFilterManager : IMessageFilterManager
 	{
-		private static readonly Lazy<MessageFilterManager> _messageFilterManager = new Lazy<MessageFilterManager>(() => new MessageFilterManager(Initialise()));
+		private static readonly Lazy<MessageFilterManager> _messageFilterManager = new Lazy<MessageFilterManager>(() => new MessageFilterManager(initialise()));
 		private readonly IDictionary<Type, IList<Type>> _aggregateRoots;
 		private readonly ReaderWriterLock _readerWriterLock = new ReaderWriterLock();
 		private int _timeOut = 20;
@@ -35,15 +34,9 @@ namespace Teleopti.Ccc.Domain.MessageBroker.Client
 			_aggregateRoots = aggregateRoots;
 		}
 
-		public static MessageFilterManager Instance
-		{
-			get
-			{
-				return _messageFilterManager.Value;
-			}
-		}
+		public static MessageFilterManager Instance => _messageFilterManager.Value;
 
-	    public bool HasType(Type type)
+		public bool HasType(Type type)
 	    {
 	        return _aggregateRoots.ContainsKey(type);
 	    }
@@ -57,13 +50,7 @@ namespace Teleopti.Ccc.Domain.MessageBroker.Client
         {
             return lookupType(domainObjectType, list => list[0]);
         }
-
-	    /// <summary>
-	    /// Looks up type.
-	    /// </summary>
-	    /// <param name="domainObjectType">Type of the domain object.</param>
-	    /// <param name="typeFinder"></param>
-	    /// <returns></returns>
+		
 	    private Type lookupType(Type domainObjectType, Func<IList<Type>, Type> typeFinder)
 		{
 			try
@@ -92,58 +79,69 @@ namespace Teleopti.Ccc.Domain.MessageBroker.Client
 			throw new DomainObjectNotInFilterException("Cannot find type " + domainObjectType.AssemblyQualifiedName);
 		}
 
-		private static IDictionary<Type, IList<Type>> Initialise()
+		private static IDictionary<Type, IList<Type>> initialise()
 		{
-			IDictionary<Type, IList<Type>> aggregateRoots = new Dictionary<Type, IList<Type>>();
-			AddTypeFilterExceptions(aggregateRoots);
+			var aggregateRoots = new Dictionary<Type, IList<Type>>
+			{
+				{ typeof(IStatisticTask), new List<Type> { typeof(IStatisticTask) } },
+				{ typeof(AgentStateReadModel), new List<Type> { typeof(AgentStateReadModel) } },
+				{ typeof(IJobResultProgress), new List<Type> { typeof(IJobResultProgress) } },
+				{ typeof(IMeetingChangedEntity), new List<Type> { typeof(IMeetingChangedEntity) } },
+				{ typeof(MeetingChangedEntity), new List<Type> { typeof(IMeetingChangedEntity) } },
+				{
+					typeof(IScheduleChangedInDefaultScenario),
+					new List<Type> { typeof(IScheduleChangedInDefaultScenario) }
+				},
+				{
+					typeof(IShiftTradeScheduleChangedInDefaultScenario),
+					new List<Type> { typeof(IShiftTradeScheduleChangedInDefaultScenario) }
+				},
+				{
+					typeof(IRunRequestWaitlistEventMessage),
+					new List<Type> { typeof(IRunRequestWaitlistEventMessage) }
+				},
+				{
+					typeof(IApproveRequestsWithValidatorsEventMessage),
+					new List<Type> { typeof(IApproveRequestsWithValidatorsEventMessage) }
+				},
+				{
+					typeof(ITeleoptiDiagnosticsInformation),
+					new List<Type> { typeof(ITeleoptiDiagnosticsInformation) }
+				},
+				{
+					typeof(IPersonScheduleDayReadModel),
+					new List<Type> { typeof(IPersonScheduleDayReadModel) }
+				},
+				{ typeof(IScheduleChangedEvent), new List<Type> { typeof(IScheduleChangedEvent) } },
+
+				{ typeof(Scenario), new List<Type> { typeof(IScenario) } },
+
+				{ typeof(Note), new List<Type> { typeof(IPersistableScheduleData), typeof(INote) } },
+				{ typeof(AgentDayScheduleTag), new List<Type> { typeof(IPersistableScheduleData), typeof(IAgentDayScheduleTag) } },
+
+				{ typeof(PublicNote), new List<Type> { typeof(IPersistableScheduleData), typeof(IPublicNote) } },
+				{ typeof(PreferenceDay), new List<Type> { typeof(IPersistableScheduleData), typeof(IPreferenceDay) } },
+				{ typeof(StudentAvailabilityDay), new List<Type> { typeof(IPersistableScheduleData), typeof(IStudentAvailabilityDay) } },
+
+				{ typeof(Multiplicator), new List<Type> { typeof(IMultiplicator) } },
+				{
+					typeof(MultiplicatorDefinitionSet),
+					new List<Type> { typeof(IMultiplicatorDefinitionSet) }
+				},
+				{ typeof(DayOffTemplate), new List<Type> { typeof(IDayOffTemplate) } },
+				{ typeof(ShiftCategory), new List<Type> { typeof(IShiftCategory) } },
+				{ typeof(Meeting), new List<Type> { typeof(IMeeting) } },
+				{ typeof(PersonRequest), new List<Type> { typeof(IPersonRequest) } },
+				{ typeof(RtaStateGroup), new List<Type> { typeof(IRtaStateGroup) } },
+				{ typeof(Activity), new List<Type> { typeof(IActivity) } },
+				{ typeof(RtaRule), new List<Type> { typeof(IRtaRule) } },
+				{ typeof(Team), new List<Type> { typeof(ITeam) } },
+				{ typeof(Site), new List<Type> { typeof(ISite) } },
+				{ typeof(Scorecard), new List<Type> { typeof(IScorecard) } },
+				{ typeof(PayrollResult), new List<Type> { typeof(IPayrollResult) } },
+				{ typeof(PushMessageDialogue), new List<Type> { typeof(IPushMessageDialogue) } }
+			};
 			return aggregateRoots;
-		}
-
-		private static void AddTypeFilterExceptions(IDictionary<Type, IList<Type>> aggregateRoots)
-		{
-			aggregateRoots.Add(typeof (IStatisticTask), new List<Type> {typeof (IStatisticTask)});
-			aggregateRoots.Add(typeof (AgentStateReadModel), new List<Type> {typeof (AgentStateReadModel)});
-			aggregateRoots.Add(typeof (IJobResultProgress), new List<Type> {typeof (IJobResultProgress)});
-			aggregateRoots.Add(typeof (IMeetingChangedEntity), new List<Type> {typeof (IMeetingChangedEntity)});
-			aggregateRoots.Add(typeof (MeetingChangedEntity), new List<Type> {typeof (IMeetingChangedEntity)});
-			aggregateRoots.Add(typeof (IScheduleChangedInDefaultScenario),
-							   new List<Type> {typeof (IScheduleChangedInDefaultScenario)});
-			aggregateRoots.Add(typeof(IShiftTradeScheduleChangedInDefaultScenario),
-								new List<Type> { typeof(IShiftTradeScheduleChangedInDefaultScenario) });
-			aggregateRoots.Add(typeof(IRunRequestWaitlistEventMessage),
-								new List<Type> { typeof(IRunRequestWaitlistEventMessage) });
-			aggregateRoots.Add(typeof(IApproveRequestsWithValidatorsEventMessage),
-								new List<Type> { typeof(IApproveRequestsWithValidatorsEventMessage) });
-			aggregateRoots.Add(typeof(ITeleoptiDiagnosticsInformation),
-							   new List<Type> { typeof(ITeleoptiDiagnosticsInformation) });
-			aggregateRoots.Add(typeof (IPersonScheduleDayReadModel),
-							   new List<Type> {typeof (IPersonScheduleDayReadModel)});
-			aggregateRoots.Add(typeof (IScheduleChangedEvent), new List<Type> {typeof (IScheduleChangedEvent)});
-
-			aggregateRoots.Add(typeof (Scenario), new List<Type> {typeof (IScenario)});
-
-			aggregateRoots.Add(typeof(Note), new List<Type> { typeof(IPersistableScheduleData), typeof(INote)});
-			aggregateRoots.Add(typeof(AgentDayScheduleTag), new List<Type> { typeof(IPersistableScheduleData), typeof(IAgentDayScheduleTag) });
-
-			aggregateRoots.Add(typeof(PublicNote), new List<Type> { typeof(IPersistableScheduleData), typeof(IPublicNote) });
-			aggregateRoots.Add(typeof(PreferenceDay), new List<Type> { typeof(IPersistableScheduleData), typeof(IPreferenceDay) });
-			aggregateRoots.Add(typeof(StudentAvailabilityDay), new List<Type> { typeof(IPersistableScheduleData), typeof(IStudentAvailabilityDay) });
-
-			aggregateRoots.Add(typeof (Multiplicator), new List<Type> {typeof (IMultiplicator)});
-			aggregateRoots.Add(typeof (MultiplicatorDefinitionSet),
-							   new List<Type> {typeof (IMultiplicatorDefinitionSet)});
-			aggregateRoots.Add(typeof (DayOffTemplate), new List<Type> {typeof (IDayOffTemplate)});
-			aggregateRoots.Add(typeof (ShiftCategory), new List<Type> {typeof (IShiftCategory)});
-			aggregateRoots.Add(typeof (Meeting), new List<Type> {typeof (IMeeting)});
-			aggregateRoots.Add(typeof (PersonRequest), new List<Type> {typeof (IPersonRequest)});
-			aggregateRoots.Add(typeof (RtaStateGroup), new List<Type> {typeof (IRtaStateGroup)});
-			aggregateRoots.Add(typeof (Activity), new List<Type> {typeof (IActivity)});
-			aggregateRoots.Add(typeof (RtaRule), new List<Type> {typeof (IRtaRule)});
-			aggregateRoots.Add(typeof (Team), new List<Type> {typeof (ITeam)});
-			aggregateRoots.Add(typeof (Site), new List<Type> {typeof (ISite)});
-			aggregateRoots.Add(typeof (Scorecard), new List<Type> {typeof (IScorecard)});
-			aggregateRoots.Add(typeof (PayrollResult), new List<Type> {typeof (IPayrollResult)});
-			aggregateRoots.Add(typeof (PushMessageDialogue), new List<Type> {typeof (IPushMessageDialogue)});
 		}
 	}
 }
