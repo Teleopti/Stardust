788f6979c3c1b016600a108e9548c07f22dce2b8
Jianfeng <none@none>
2017-05-25 14:12:37 +0800

    minor

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Content/moment-datepicker/moment-datepicker-ext.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Content/moment-datepicker/moment-datepicker-ext.js
@@ -33,7 +33,7 @@ datepicker.place = function() {
             left: offset.left
         });
     }
-}
+};
 
 datepicker.show = function (e) {
     if (this.isInput && this.element.is(':disabled')) { return; }
@@ -43,10 +43,11 @@ datepicker.show = function (e) {
     if(this.picker.isOpened){
         this.picker.hide();
         return;
-    }else{
-        this.picker.show();
-        this.picker.isOpened = true;
     }
+
+    this.picker.show();
+    this.picker.isOpened = true;
+
     this.height = (this.component && this.component.outerHeight()) || this.element.outerHeight();
     this.place();
     $(window).on('resize', $.proxy(this.place, this));
