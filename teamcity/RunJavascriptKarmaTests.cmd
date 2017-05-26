871244d7b5fff685e3fa9d10039b2e0669c99e05
johan Damm <none@none>
2017-05-26 10:49:59 +0200

    lets toggle our js test from grunt directly to npm-scripts

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -1,7 +1,7 @@
 @ECHO off
 SET /A ERRORLEV=0
 :: Runs from [repo]\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM
-call ..\.node\npm run-script continuous
+call ..\.node\npm test
 IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=1 & GOTO :error
 :: Clear all dev dependencies and only install what we need for production
 call ..\.node\npm install rimraf
