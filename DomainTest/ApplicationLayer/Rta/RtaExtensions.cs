ea126cf1dc5e107307785b3fd4548b35dfdc4145
Justin! <justin@teleopti.com>
2017-05-26 23:45:33 +0200

    #43884 Build trigger

--- DomainTest/ApplicationLayer/Rta/RtaExtensions.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using Teleopti.Ccc.Domain.ApplicationLayer.Rta;
-using Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service;
-
-namespace Teleopti.Ccc.DomainTest.ApplicationLayer.Rta
-{
-
-}
