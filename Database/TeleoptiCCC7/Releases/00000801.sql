e6312e702f73ea8be237992de9f116acbfde07aa
Erik Sundberg <erik.sundberg@teleopti.com>
2017-05-30 11:19:23 +0200

    #44208 Change release script for customers upgrading from old version

--- Database/TeleoptiCCC7/Releases/00000801.sql
+++ Database/TeleoptiCCC7/Releases/00000801.sql
@@ -1,10 +1,15 @@
-alter table Readmodel.AgentState alter column EmploymentNumber nvarchar(50)
+ALTER TABLE Readmodel.AgentState ALTER COLUMN EmploymentNumber NVARCHAR(50)
 
-UPDATE Readmodel.AgentState
-SET 
-	FirstName = p.FirstName,
-	LastName = p.LastName,
-	EmploymentNumber = p.EmploymentNumber
-FROM Readmodel.AgentState a
-INNER JOIN dbo.Person p
-	ON a.PersonId = p.Id
+IF (SELECT COUNT(1) FROM ReadModel.AgentState) = 0
+	INSERT INTO 
+	ReadModel.AgentState (PersonId, FirstName, LastName, EmploymentNumber, IsDeleted) 
+	SELECT Id, FirstName, LastName, EmploymentNumber, 1 FROM dbo.Person
+ELSE
+	UPDATE Readmodel.AgentState
+	SET 
+		FirstName = p.FirstName,
+		LastName = p.LastName,
+		EmploymentNumber = p.EmploymentNumber
+	FROM Readmodel.AgentState a
+	INNER JOIN dbo.Person p
+		ON a.PersonId = p.Id
