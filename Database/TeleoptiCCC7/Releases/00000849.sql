7430afe0659787eb2b1cd80ae06d259a980e43d5
Erik Nordenhök <erik.nordenhok@teleopti.com>
2017-06-01 16:03:25 +0200

    #44332 to please roger/dj

--- Database/TeleoptiCCC7/Releases/00000849.sql
+++ Database/TeleoptiCCC7/Releases/00000849.sql
@@ -1,8 +1,3 @@
-IF NOT EXISTS(SELECT *
-          FROM   INFORMATION_SCHEMA.COLUMNS
-          WHERE  TABLE_NAME = 'PersonAssignment'
-                 AND COLUMN_NAME = 'Source') 
-BEGIN
-	ALTER TABLE [dbo].[PersonAssignment] 
-	ADD [Source] nvarchar(50) NULL
-END
+ALTER TABLE [dbo].[PersonAssignment] 
+ADD [Source] nvarchar(50) NULL
+
--- Database/TeleoptiCCC7/Releases/00000850.sql
+++ Database/TeleoptiCCC7/Releases/00000850.sql
@@ -1,8 +1,2 @@
-IF NOT EXISTS(SELECT *
-          FROM   INFORMATION_SCHEMA.COLUMNS
-          WHERE  TABLE_NAME = 'PersonAssignment_AUD'
-                 AND COLUMN_NAME = 'Source') 
-BEGIN
-	ALTER TABLE [Auditing].[PersonAssignment_AUD] 
-	ADD [Source] nvarchar(50) NULL
-END
+ALTER TABLE [Auditing].[PersonAssignment_AUD] 
+ADD [Source] nvarchar(50) NULL
