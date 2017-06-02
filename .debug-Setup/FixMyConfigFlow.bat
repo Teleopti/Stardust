f032016150ad89fa40736b06d731b57b9b641580
Xinfeng Li <xinfeng.li@teleopti.com>
2017-06-02 15:43:59 +0800

    Make batch files works with customized baseline name

--- .debug-Setup/FixMyConfigFlow.bat
+++ .debug-Setup/FixMyConfigFlow.bat
@@ -4,9 +4,12 @@ SETLOCAL
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
 
+SET BASELINE_NAME=%1
+IF "%BASELINE_NAME%"=="" SET BASELINE_NAME=DemoSales
+
 CALL :Branch %~dp0..
-SET CCC7DB=%Branch%_DemoSales_TeleoptiCCC7
-SET AnalyticsDB=%Branch%_DemoSales_TeleoptiAnalytics
+SET CCC7DB=%Branch%_%BASELINE_NAME%_TeleoptiCCC7
+SET AnalyticsDB=%Branch%_%BASELINE_NAME%_TeleoptiAnalytics
 
 CALL %ROOTDIR%\FixMyConfig.bat %CCC7DB% %AnalyticsDB%
 
--- .debug-Setup/PatchDatabase-Internal-flow.bat
+++ .debug-Setup/PatchDatabase-Internal-flow.bat
@@ -3,12 +3,17 @@ SETLOCAL
 
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
+
+SET BASELINE_NAME=%1
+IF "%BASELINE_NAME%"=="" SET BASELINE_NAME=DemoSales
+
 SET CD=%ROOTDIR%\..
 CALL :Branch "%CD%"
+
 SET SqlInstance=.
-SET CCC7DB=%Branch%_DemoSales_TeleoptiCCC7
-SET AnalyticsDB=%Branch%_DemoSales_TeleoptiAnalytics
-SET AggDB=%Branch%_DemoSales_TeleoptiCCCAgg
+SET CCC7DB=%Branch%_%BASELINE_NAME%_TeleoptiCCC7
+SET AnalyticsDB=%Branch%_%BASELINE_NAME%_TeleoptiAnalytics
+SET AggDB=%Branch%_%BASELINE_NAME%_TeleoptiCCCAgg
 
 CALL "%~dp0PatchDatabase-Internal.bat" %SqlInstance% %CCC7DB% %AnalyticsDB% %AggDB%
 
