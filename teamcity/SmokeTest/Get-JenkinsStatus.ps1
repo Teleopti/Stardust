b08858e5046b63ce86084ce3a22dac6c737e6955
Jimmy Broman <none@none>
2017-06-02 13:15:21 +0200

    Fixing jenkins url

--- teamcity/SmokeTest/Get-JenkinsStatus.ps1
+++ teamcity/SmokeTest/Get-JenkinsStatus.ps1
@@ -5,15 +5,16 @@
   $ServerName
  )
 
-$ServerURL = "$ServerName" + ':8080'
+#$ServerURL = "$ServerName" + ':8080'
+$JenkinsURL = "http://" + $ServerName + ":8080/job/$ENV:JOB_NAME/lastBuild/api/json"
 
-$Check_webstatus = invoke-webrequest "http://$ServerURL/job/$ServerName-AutoDeploy/lastBuild/api/json"
+$Check_webstatus = invoke-webrequest $JenkinsURL
 $Job_status = ConvertFrom-Json $Check_webstatus
 
 # Check if deploy is still running
 do {($job_status.building)  
 
-$Check_webstatus = invoke-webrequest "http://$ServerURL/job/$ServerName-AutoDeploy/lastBuild/api/json"
+$Check_webstatus = invoke-webrequest $JenkinsURL
 $Job_status = ConvertFrom-Json $Check_webstatus
 
 "Waiting for Jenkins deploy to complete..."
@@ -22,13 +23,14 @@ Start-Sleep -Seconds 15
 } while ($Job_status.building -eq "True")
 
 #Check if deploy was successful
-$Check_webstatus = invoke-webrequest "http://$ServerURL/job/$ServerName-AutoDeploy/lastBuild/api/json"
+$Check_webstatus = invoke-webrequest $JenkinsURL
 $Job_status = ConvertFrom-Json $Check_webstatus
 
 if ($Job_status.result -eq "SUCCESS") { Write-Host "Jenkins deploy successfully completed!" }
 
 else { 
-Write-Host "Jenkins deploy failed! Check http://$ServerURL/job/$ServerName-AutoDeploy for more information!"
+$linkurl = "http://" + $ServerName + ":8080/job/$ENV:JOB_NAME"
+Write-Host "Jenkins deploy failed! Check $linkurl for more information!"
 $ErrorMessage = $Error[0].Exception.Message
 Write-Host $( '##teamcity[message text=''{0}'']' -f $ErrorMessage ) 
 exit 1
