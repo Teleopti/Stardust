f4efd00399db7dab7632bf06f4e36080ef6b93d6
Zulong Chen <none@none>
2017-06-02 16:29:57 +0800

    #44578 fixed to show names with certain characters

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/organizationPicker/organizationPicker.filter.searchFilter.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/organizationPicker/organizationPicker.filter.searchFilter.js
@@ -2,12 +2,37 @@
 	'use strict';
 
 	angular.module('wfm.organizationPicker')
-		.filter('searchFilter', searchFilter);
+		.filter('searchFilter', ['$sce', searchFilter]);
 
-	function searchFilter() {
+	var entityMap = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#39;',
+		'/': '&#x2F;',
+		'`': '&#x60;',
+		'=': '&#x3D;'
+	};
+
+	function escape(text) {
+		if (!angular.isString(text))
+			return ''
+		return text.replace(/[&<>"'`=\/]/g, function (s) { return entityMap[s] })
+	}
+
+	function searchFilter($sce) {
 		return function (input, query) {
-			var r = new RegExp(query, 'i')
-			return input.replace(r, '<span class="match">$&</span>')
+			if (query === '')
+				return escape(input)
+
+			var r = new RegExp('(' + query + ')', 'ig')
+
+			if (input.search(r) !== -1) {
+				return $sce.trustAsHtml(escape(input.replace(r, '{{||{{$&}}||}}')).replace(/({{\|\|{{)/g, '<span class="match">').replace(/(}}\|\|}})/g, '</span>'))
+			}
+
+			return escape(input)
 		}
 	}
 })(angular);
