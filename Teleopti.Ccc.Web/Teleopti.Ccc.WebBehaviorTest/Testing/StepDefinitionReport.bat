9f7b4c9b3144f993fd795f16100f83d15bb18947
Justin! <justin@teleopti.com>
2017-06-08 16:45:17 +0200

    #40660 Minor updates to specflow reports. Doesnt work because of https://github.com/techtalk/SpecFlow/issues/710 currently.

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Testing/StepDefinitionReport.bat
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Testing/StepDefinitionReport.bat
@@ -1,6 +1,6 @@
-@echo off
+::@echo off
 
-set specflowFolder=..\..\..\packages\SpecFlow.1.9.0\tools\
+set specflowFolder=..\..\..\packages\SpecFlow.2.1.0\tools\
 set specflow=%specflowFolder%specflow.exe
 set project=..\Teleopti.Ccc.WebBehaviorTest.csproj
 set action=stepdefinitionreport
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Testing/TestExecutionReport.bat
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Testing/TestExecutionReport.bat
@@ -10,7 +10,7 @@ exit
 
 :: set nunit=..\..\..\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe
 set nunit="C:\Program Files (x86)\NUnit 2.6\bin\nunit-console.exe"
-set specflow=..\..\..\packages\SpecFlow.1.9.0\tools\specflow.exe
+set specflow=..\..\..\packages\SpecFlow.2.1.0\tools\specflow.exe
 set assembly=..\bin\Debug\Teleopti.Ccc.WebBehaviorTest.dll
 set project=..\Teleopti.Ccc.WebBehaviorTest.csproj
 :: set tests=/run=Teleopti.Ccc.WebBehaviorTest.PreferencesFeature.AddStandardPreference
