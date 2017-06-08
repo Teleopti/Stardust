e7c1ba10d13d7526b581ccc0ce87117539fcd592
Justin! <justin@teleopti.com>
2017-06-08 11:56:59 +0200

    #40660 Clean up ununed code

--- Teleopti.Ccc.IocCommon/Configuration/RtaModule.cs
+++ Teleopti.Ccc.IocCommon/Configuration/RtaModule.cs
@@ -93,7 +93,6 @@ namespace Teleopti.Ccc.IocCommon.Configuration
 			builder.RegisterType<ProperAlarm>().SingleInstance();
 
 			builder.RegisterType<OrganizationViewModelBuilder>().SingleInstance();
-			builder.RegisterType<TeamViewModelBuilder>().SingleInstance();
 			builder.RegisterType<NumberOfAgentsInSiteReader>().As<INumberOfAgentsInSiteReader>().SingleInstance();
 			builder.RegisterType<NumberOfAgentsInTeamReader>().As<INumberOfAgentsInTeamReader>().SingleInstance();
 			builder.RegisterType<HardcodedSkillGroupingPageId>().SingleInstance();
