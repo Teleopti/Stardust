b71fbf0f26634f6b1da20b149e645732f7c4eb62
Kunning Mao <none@none>
2017-06-12 17:38:29 +0200

    Backed out changeset: 2aed8b041ba6

--- teamcity/SmokeTest/test/AzureAdWsFedTest.js
+++ teamcity/SmokeTest/test/AzureAdWsFedTest.js
@@ -5,8 +5,8 @@ var MicrosoftLoginPage = require('./pages/MicrosoftLogin.page');
 
 describe('Azure AD using WsFed', function() {
 	this.timeout(360 * 1000); // Set global timeout for this test to 6 minutes
-	
-    it('should be able to sign in with AD user using WsFed (metadata)', function () {
+	// ignore this one for now, need to change APP ID URI in Teleopti Azure AD first, and the claim input in claim policy need to change to name instead of nameidentifier
+    xit('should be able to sign in with AD user using WsFed (metadata)', function () {
 		browser.deleteCookie();
 		// Given that we are showing the IdentityProviders selection page
 		MytimePage.open();
