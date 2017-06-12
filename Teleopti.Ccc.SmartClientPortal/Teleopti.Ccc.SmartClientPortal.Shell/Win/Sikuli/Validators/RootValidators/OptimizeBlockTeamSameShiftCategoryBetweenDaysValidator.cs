28a653b17566e7ef499a55d4105e0de4f584181c
Behrooz Aghakhanian <antipattern>
2017-06-12 09:40:31 +0200

    Increase Sikulischeduler Stdev validators threshold

--- Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Win/Sikuli/Validators/RootValidators/OptimizeBlockTeamSameShiftCategoryBetweenDaysValidator.cs
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Win/Sikuli/Validators/RootValidators/OptimizeBlockTeamSameShiftCategoryBetweenDaysValidator.cs
@@ -13,7 +13,7 @@ namespace Teleopti.Ccc.SmartClientPortal.Shell.Win.Sikuli.Validators.RootValidat
 
 		protected override SikuliValidationResult Validate(SchedulerTestData data)
 		{
-			const double periodStandardDeviationLimit = 0.08d;
+			const double periodStandardDeviationLimit = 0.09d;
 			AtomicValidators.Add(new PeriodStandardDeviationValidator(data.SchedulerState, data.TotalSkill, periodStandardDeviationLimit));
 			return ValidateAtomicValidators(AtomicValidators);
 		}
