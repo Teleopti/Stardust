0f1a35d4b52b24e3c7ab4e15239969e362f71ec7
AndersF <none@none>
2017-06-14 10:08:39 +0200

    #44726

--- Database/TeleoptiCCC7/Releases/00000848.sql
+++ Database/TeleoptiCCC7/Releases/00000848.sql
@@ -1,7 +1,22 @@
+CREATE TABLE #pp (Id uniqueidentifier)
+GO
+
 WITH cte as(
-	SELECT Parent, StartDate, ROW_NUMBER() OVER (PARTITION BY Parent, StartDate ORDER BY Parent) RN
+	SELECT Id, Parent, StartDate, ROW_NUMBER() OVER (PARTITION BY Parent, StartDate ORDER BY Parent) RN
 	FROM   PersonPeriod)
-DELETE FROM cte WHERE RN>1
+INSERT INTO #pp SELECT Id FROM cte WHERE RN>1
+
+delete ExternalLogOnCollection
+from ExternalLogOnCollection elc
+inner join #pp pp on elc.personperiod = pp.Id 
+
+delete PersonSkill
+from PersonSkill ps
+inner join #pp pp on ps.parent = pp.Id 
+
+delete PersonPeriod
+from PersonPeriod
+inner join #pp pp on PersonPeriod.Id = pp.Id
 GO
 	
 ALTER TABLE [dbo].[PersonPeriod] ADD CONSTRAINT UQ_PersonPeriod UNIQUE
