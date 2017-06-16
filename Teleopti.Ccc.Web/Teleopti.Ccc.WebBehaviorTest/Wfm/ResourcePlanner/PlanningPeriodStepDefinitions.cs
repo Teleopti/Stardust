8c34039c0243615c48238dbb8e9685ee15e3a710
junwen.luo <none@none>
2017-06-16 10:06:40 +0800

    convert to date string by current culture

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/ResourcePlanner/PlanningPeriodStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/ResourcePlanner/PlanningPeriodStepDefinitions.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 using TechTalk.SpecFlow;
 using Teleopti.Ccc.WebBehaviorTest.Core;
 
@@ -29,6 +31,10 @@ namespace Teleopti.Ccc.WebBehaviorTest.Wfm.ResourcePlanner
 		[Then(@"I should see a planning period between '(.*)' and '(.*)'" ), SetCulture("sv-SE")]
 		public void ThenIShouldSeeAPlanningPeriodBetweenAnd(string from, string to)
 		{
+			var format = "d MMMM yyyy";
+			var culture = CultureInfo.CurrentUICulture;
+			from = DateTime.Parse(from).ToString(format, culture);
+			to = DateTime.Parse(to).ToString(format, culture);
 			Browser.Interactions.AssertAnyContains(".plan-group-pp > div.list-header h1", $"{from} - {to}");
 		}
 		
