c792e99f56cbfc9fd2e590e29f7e45b2a0458e7f
Alina Morariu <none@none>
2017-06-19 17:27:45 +0200

    #44772 fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/_modules.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/_modules.js
@@ -17,19 +17,19 @@
 		])
 		.run(runRtaModule);
 
-	runRtaModule.$inject = ['$rootScope', '$state', '$location', 'Toggle'];
+	runRtaModule.$inject = ['$rootScope', '$state', '$location'];
 
-	function runRtaModule($rootScope, $state, $location, Toggle) {
-		var toggles = {};
+	function runRtaModule($rootScope, $state, $location) {
+		// var toggles = {};
 
-		Toggle.togglesLoaded.then(function () {
-			toggles = Toggle
-		});
+		// Toggle.togglesLoaded.then(function () {
+		// 	toggles = Toggle
+		// });
 
 		var result = $rootScope.$on('$stateChangeSuccess',
 			function (event, toState) {
 				if ($location.url() == $state.current.url && toState.name == 'rta') $state.go('rta.sites');
-				else if (toggles.RTA_FrontEndRefactor_44772 && $location.url() == $state.current.url && toState.name == 'rta')	$state.go('refact-rta');
+				//else if (toggles.RTA_FrontEndRefactor_44772 && $location.url() == $state.current.url && toState.name == 'rta')	$state.go('refact-rta');
 			});
 		return result;
 	}
