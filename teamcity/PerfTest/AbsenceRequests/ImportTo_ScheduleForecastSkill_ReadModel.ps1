337d4c9ec04f8a3108add12ec87b76ba4cd6bed7
amandaj <none@none>
2017-06-19 09:06:43 +0200

    remove

--- teamcity/PerfTest/AbsenceRequests/ImportTo_ScheduleForecastSkill_ReadModel.ps1
+++
@@ -1,3 +0,0 @@
-﻿param( [Parameter(Mandatory=$true)][string]$server)
-
-invoke-sqlcmd -inputfile "$PSScriptRoot/Import_ScheduleForecastSkill_ReadModel.sql" -serverinstance "$server" -database "PerfA" 
