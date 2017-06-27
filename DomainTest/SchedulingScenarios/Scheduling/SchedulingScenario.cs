00a06d56effa6f9b16fd1d6830697f2a50d9e151
Roger Kratz <roger.kratz@teleopti.com>
2017-06-27 13:00:20 +0200

    #44757 Now all scheduling tests are turned on when toggle is true

--- DomainTest/SchedulingScenarios/Scheduling/SchedulingScenario.cs
+++ DomainTest/SchedulingScenarios/Scheduling/SchedulingScenario.cs
@@ -28,8 +28,7 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Scheduling
 			if(ResourcePlannerMergeTeamblockClassicScheduling44289)
 				toggleManager.Enable(Toggles.ResourcePlanner_MergeTeamblockClassicScheduling_44289);
 			if (_resourcePlannerSchedulingIslands44757)
-				Assert.Ignore("Need to fix a lot of tests first");
-				//toggleManager.Enable(Toggles.ResourcePlanner_SchedulingIslands_44757);
+				toggleManager.Enable(Toggles.ResourcePlanner_SchedulingIslands_44757);
 		}
 	}
 }
