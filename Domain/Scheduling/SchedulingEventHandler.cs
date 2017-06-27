0711c8cf8b9b68a4bddff3dc90358b5be904b4a6
Roger Kratz <roger.kratz@teleopti.com>
2017-06-27 11:07:48 +0200

    #44757 Using ISynchronizeSchedulesAfterIsland (only works for web ATM)

--- Domain/Scheduling/SchedulingEventHandler.cs
+++ Domain/Scheduling/SchedulingEventHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.ApplicationLayer.ResourcePlanner;
+using Teleopti.Ccc.Domain.Optimization;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.WebLegacy;
 using Teleopti.Interfaces.Domain;
@@ -16,18 +17,21 @@ namespace Teleopti.Ccc.Domain.Scheduling
 		private readonly IScheduleExecutor _scheduleExecutor;
 		private readonly ISchedulingOptionsProvider _schedulingOptionsProvider;
 		private readonly ICurrentSchedulingCallback _currentSchedulingCallback;
+		private readonly ISynchronizeSchedulesAfterIsland _synchronizeSchedulesAfterIsland;
 
 		public SchedulingEventHandler(Func<ISchedulerStateHolder> schedulerStateHolder,
 						IFillSchedulerStateHolder fillSchedulerStateHolder,
 						IScheduleExecutor scheduleExecutor, 
 						ISchedulingOptionsProvider schedulingOptionsProvider,
-						ICurrentSchedulingCallback currentSchedulingCallback)
+						ICurrentSchedulingCallback currentSchedulingCallback,
+						ISynchronizeSchedulesAfterIsland synchronizeSchedulesAfterIsland)
 		{
 			_schedulerStateHolder = schedulerStateHolder;
 			_fillSchedulerStateHolder = fillSchedulerStateHolder;
 			_scheduleExecutor = scheduleExecutor;
 			_schedulingOptionsProvider = schedulingOptionsProvider;
 			_currentSchedulingCallback = currentSchedulingCallback;
+			_synchronizeSchedulesAfterIsland = synchronizeSchedulesAfterIsland;
 		}
 
 		public void Handle(SchedulingWasOrdered @event)
@@ -41,6 +45,7 @@ namespace Teleopti.Ccc.Domain.Scheduling
 				var fixThis = new NoSchedulingProgress();
 				_scheduleExecutor.Execute(_currentSchedulingCallback.Current(), _schedulingOptionsProvider.Fetch(), fixThis, schedulerStateHolder.AllPermittedPersons,
 					selectedPeriod, @event.RunWeeklyRestSolver);
+				_synchronizeSchedulesAfterIsland.Synchronize(schedulerStateHolder.Schedules, selectedPeriod);
 			}
 		}
 	}
