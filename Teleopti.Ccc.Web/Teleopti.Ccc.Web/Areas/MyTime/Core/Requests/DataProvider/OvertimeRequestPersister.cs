9ba0137c67541e48863b46bad5063835a11414bb
junwen.luo <none@none>
2017-07-04 15:33:10 +0800

    PBI #44558 add overtime request to queue and set it as pending

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/DataProvider/OvertimeRequestPersister.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Requests/DataProvider/OvertimeRequestPersister.cs
@@ -1,4 +1,5 @@
 using System;
+using Teleopti.Ccc.Domain.AgentInfo.Requests;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.Mapping;
@@ -10,13 +11,15 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.DataProvider {
 		private readonly IPersonRequestRepository _personRequestRepository;
 		private readonly OvertimeRequestFormMapper _mapper;
 		private readonly RequestsViewModelMapper _requestsMapper;
+		private readonly IQueuedOvertimeRequestRepository _queuedOvertimeRequestRepository;
 
 		public OvertimeRequestPersister(IPersonRequestRepository personRequestRepository, OvertimeRequestFormMapper mapper,
-			RequestsViewModelMapper requestsMapper)
+			RequestsViewModelMapper requestsMapper, IQueuedOvertimeRequestRepository queuedOvertimeRequestRepository)
 		{
 			_personRequestRepository = personRequestRepository;
 			_mapper = mapper;
 			_requestsMapper = requestsMapper;
+			_queuedOvertimeRequestRepository = queuedOvertimeRequestRepository;
 		}
 
 		public RequestViewModel Persist(OvertimeRequestForm form)
@@ -37,9 +40,23 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.DataProvider {
 			if (isCreatingNew)
 			{
 				_personRequestRepository.Add(personRequest);
+				queueOvertimeRequest(personRequest);
 			}
 
 			return _requestsMapper.Map(personRequest);
+		}
+
+		private void queueOvertimeRequest(IPersonRequest personRequest)
+		{
+			personRequest.Pending();
+			var queuedOvertimeRequest = new QueuedOvertimeRequest()
+			{
+				PersonRequest = personRequest.Id.GetValueOrDefault(),
+				Created = personRequest.CreatedOn.GetValueOrDefault(),
+				StartDateTime = personRequest.Request.Period.StartDateTime,
+				EndDateTime = personRequest.Request.Period.EndDateTime
+			};
+			_queuedOvertimeRequestRepository.Add(queuedOvertimeRequest);
 		}
 	}
 }
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Core/Requests/DataProvider/OvertimeRequestPersisterTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Common.DataProvider;
+using Teleopti.Ccc.Web.Areas.MyTime.Core.Requests.DataProvider;
+using Teleopti.Ccc.Web.Areas.MyTime.Models.Requests;
+using Teleopti.Ccc.WebTest.Areas.Requests.Core.IOC;
+
+namespace Teleopti.Ccc.WebTest.Core.Requests.DataProvider
+{
+	[TestFixture, RequestsTest]
+	public class OvertimeRequestPersisterTest : ISetup
+	{
+		public IOvertimeRequestPersister Target;
+		public IMultiplicatorDefinitionSetRepository MultiplicatorDefinitionSetRepository;
+		private IPerson _person;
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			_person = PersonFactory.CreatePerson();
+
+			system.UseTestDouble(new FakeLoggedOnUser(_person)).For<ILoggedOnUser>();
+			system.UseTestDouble<FakeMultiplicatorDefinitionSetRepository>().For<IMultiplicatorDefinitionSetRepository>();
+			system.UseTestDouble<FakeQueuedOvertimeRequestRepository>().For<IQueuedOvertimeRequestRepository>();
+			system.UseTestDouble(new FakeLinkProvider()).For<ILinkProvider>();
+		}
+
+		[Test]
+		public void ShouldChangeStatusToPendingWhenPersistOvertimeRequest()
+		{
+			var multiplicatorDefinitionSet = new MultiplicatorDefinitionSet("test", MultiplicatorType.Overtime).WithId();
+			MultiplicatorDefinitionSetRepository.Add(multiplicatorDefinitionSet);
+
+			var overtimeRequestForm = new OvertimeRequestForm {MultiplicatorDefinitionSet = multiplicatorDefinitionSet.Id.Value};
+
+			var result = Target.Persist(overtimeRequestForm);
+			Assert.AreEqual(result.IsPending, true);
+		}
+	}
+}
