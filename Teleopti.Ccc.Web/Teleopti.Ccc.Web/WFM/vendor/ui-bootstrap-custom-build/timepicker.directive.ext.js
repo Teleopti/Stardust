9b8ef59167558dbd409115031bb82c0309e7bce0
Zulong Chen <none@none>
2017-07-05 09:27:33 +0800

    #45018 Disable autocomplete to avoid dropdown in Edge * Same issue as #38197. Change dropped when upgrading this custom build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/vendor/ui-bootstrap-custom-build/timepicker.directive.ext.js
@@ -9,11 +9,11 @@
 					'  <tbody>' +
 					'    <tr>' +
 					'      <td class="form-group uib-time hours" ng-class="{\'has-error\': invalidHours}">' +
-					'        <input type="text" placeholder="HH" ng-model="hours" ng-change="updateHours()" class="text-center" ng-readonly="::readonlyInput" maxlength="2" tabindex="{{::tabindex}}" ng-disabled="noIncrementHours()" ng-blur="blur()">' +
+					'        <input type="text" autocomplete="off" placeholder="HH" ng-model="hours" ng-change="updateHours()" class="text-center" ng-readonly="::readonlyInput" maxlength="2" tabindex="{{::tabindex}}" ng-disabled="noIncrementHours()" ng-blur="blur()">' +
 					'      </td>' +
 					'      <td class="uib-separator">:</td>' +
 					'      <td class="form-group uib-time minutes" ng-class="{\'has-error\': invalidMinutes}">' +
-					'        <input type="text" placeholder="MM" ng-model="minutes" ng-change="updateMinutes()" class="text-center" ng-readonly="::readonlyInput" maxlength="2" tabindex="{{::tabindex}}" ng-disabled="noIncrementMinutes()" ng-blur="blur()">' +
+					'        <input type="text" autocomplete="off" placeholder="MM" ng-model="minutes" ng-change="updateMinutes()" class="text-center" ng-readonly="::readonlyInput" maxlength="2" tabindex="{{::tabindex}}" ng-disabled="noIncrementMinutes()" ng-blur="blur()">' +
 					'      </td>' +
 					'      <td ng-show="showMeridian" class="uib-time am-pm"><md-button type="button" ng-class="{disabled: noToggleMeridian()}" ng-click="toggleMeridian()" ng-disabled="noToggleMeridian()" tabindex="{{::tabindex}}">{{meridian}}</md-button></td>' +
 					'    </tr>' +
