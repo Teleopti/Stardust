2616600cf74c2a674a7077591ba2f33a31e9d74d
Claes Stenman <claes.stenman@teleopti.com>
2017-07-20 10:53:42 +0200

    #45214 removed unused...

--- DomainTest/SchedulingScenarios/TimeBetweenDaysOptimization/TimeBetweenDaysOptimizationTeamBlockDesktopTest.cs
+++ DomainTest/SchedulingScenarios/TimeBetweenDaysOptimization/TimeBetweenDaysOptimizationTeamBlockDesktopTest.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 using Teleopti.Ccc.Domain.AgentInfo;
 using Teleopti.Ccc.Domain.Common;
-using Teleopti.Ccc.Domain.Forecasting;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Optimization;
-using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling;
-using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
