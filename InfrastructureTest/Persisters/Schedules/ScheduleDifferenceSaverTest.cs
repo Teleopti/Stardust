d012d172f5d41b25a74ef7eb279c0ee579a7960d
Chundan Xu <chundan.xu@teleopti.com>
2017-07-21 16:56:42 +0800

    bug #44948 now can reproduce it in test.

---
+++ InfrastructureTest/Persisters/Schedules/ScheduleDifferenceSaverTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.Persisters.Schedules
+{
+	[InfrastructureTest, Ignore("for robin")]
+	public class ScheduleDifferenceSaverTest
+	{
+		public IScheduleDifferenceSaver Target;
+		public IPersonAssignmentRepository PersonAssignmentRepository;
+		public IScenarioRepository ScenarioRepository;
+		public IActivityRepository ActivityRepository;
+		public IPersonRepository PersonRepository;
+		public ICurrentUnitOfWorkFactory CurrentUnitOfWorkFactory;
+		public IScheduleStorage ScheduleStorage;
+		public FakeTransactionHook TransactionHook;
+		public IMultiplicatorDefinitionSetRepository DefinitionSetRepository;
+
+		[Test]
+		public virtual void ShouldKeepTheEventInModifiedAssignmentWhenAddingAnOvertime()
+		{
+			var date = new DateOnly(2000, 1, 1);
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc);
+			var scenario = new Scenario("_");
+			var activity = new Activity("_");
+			var multiplicatorDefinitionSet = new MultiplicatorDefinitionSet("_", MultiplicatorType.Overtime);
+			using (var setup = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				ActivityRepository.Add(activity);
+				PersonRepository.Add(agent);
+				ScenarioRepository.Add(scenario);
+				var orgAss = new PersonAssignment(agent, scenario, date);
+				orgAss.AddActivity(activity, new TimePeriod(7, 10));
+				PersonAssignmentRepository.Add(orgAss);
+				DefinitionSetRepository.Add(multiplicatorDefinitionSet);
+				setup.PersistAll();
+			}
+
+			IScheduleDictionary scheduleDictionary;
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				scheduleDictionary = ScheduleStorage.FindSchedulesForPersons(new ScheduleDateTimePeriod(new DateTimePeriod(1900, 1, 1, 2100, 1, 1)), scenario, new PersonProvider(new[] { agent }), new ScheduleDictionaryLoadOptions(false, false), new[] { agent });
+				//make sure we clone the assignment
+				var scheduleRange = scheduleDictionary[agent];
+				var scheduleDay = scheduleDictionary[agent].ScheduledDay(date);
+				scheduleDay.CreateAndAddOvertime(activity, new DateTimePeriod(2000, 1, 1, 10, 2000, 1, 1, 11), multiplicatorDefinitionSet);
+				scheduleDictionary.Modify(scheduleDay, new DoNothingScheduleDayChangeCallBack());
+				Target.SaveChanges(
+					scheduleRange.DifferenceSinceSnapshot(new DifferenceEntityCollectionService<IPersistableScheduleData>()),
+					(ScheduleRange)scheduleRange);
+			}
+			
+			var events = (TransactionHook.ModifiedRoots.Single(r => r is IPersonAssignment) as IAggregateRootWithEvents).PopAllEvents();
+			events.Single().Should().Be.OfType<ActivityAddedEvent>();
+		}
+	}
+}
