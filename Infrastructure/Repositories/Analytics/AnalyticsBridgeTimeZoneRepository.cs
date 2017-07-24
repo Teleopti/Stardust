0ae7741089903801e4709d9dac7a7d9bebd4015e
Robin Karlsson <robin.karlsson@teleopti.com>
2017-07-24 13:27:28 +0200

    Using bulk insert for bridge timezone for better performance

--- Infrastructure/Repositories/Analytics/AnalyticsBridgeTimeZoneRepository.cs
+++ Infrastructure/Repositories/Analytics/AnalyticsBridgeTimeZoneRepository.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using NHibernate.Transform;
 using Teleopti.Ccc.Domain.Analytics;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.Repositories;
-using Teleopti.Interfaces.Infrastructure;
 
 namespace Teleopti.Ccc.Infrastructure.Repositories.Analytics
 {
@@ -36,12 +38,38 @@ namespace Teleopti.Ccc.Infrastructure.Repositories.Analytics
 		{
 			if (!toBeAdded.Any()) return;
 			using (var session = _analyticsUnitOfWork.Current().Session().SessionFactory.OpenStatelessSession())
-			using (var transaction = session.BeginTransaction())
+			using (var transaction = session.Connection.BeginTransaction())
 			{
-				foreach (var bridgeTimeZone in toBeAdded)
+				var dt = new DataTable();
+				dt.Columns.Add("date_id", typeof(int));
+				dt.Columns.Add("interval_id", typeof(short));
+				dt.Columns.Add("time_zone_id", typeof(short));
+				dt.Columns.Add("local_date_id", typeof(int));
+				dt.Columns.Add("local_interval_id", typeof(short));
+				dt.Columns.Add("datasource_id", typeof(int));
+				dt.Columns.Add("insert_date", typeof(DateTime));
+				dt.Columns.Add("update_date", typeof(DateTime));
+				
+				foreach (var skill in toBeAdded)
 				{
-					session.Insert(bridgeTimeZone);
+					var row = dt.NewRow();
+					row["date_id"] = skill.DateId;
+					row["interval_id"] = skill.IntervalId;
+					row["time_zone_id"] = skill.TimeZoneId;
+					row["local_date_id"] = skill.LocalDateId;
+					row["local_interval_id"] = skill.LocalIntervalId;
+					row["datasource_id"] = 1;
+					row["insert_date"] = DateTime.UtcNow;
+					row["update_date"] = DateTime.UtcNow;
+					dt.Rows.Add(row);
 				}
+				
+				using (var sqlBulkCopy = new SqlBulkCopy((SqlConnection) session.Connection, SqlBulkCopyOptions.Default, (SqlTransaction) transaction))
+				{
+					sqlBulkCopy.DestinationTableName = "[mart].[bridge_time_zone]";
+					sqlBulkCopy.WriteToServer(dt);
+				}
+				
 				transaction.Commit();
 			}
 		}
