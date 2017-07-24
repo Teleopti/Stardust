34632ff194ce46a5c36971fc1232b7604df936b9
Herman <none@none>
2017-07-24 13:44:39 +0200

    #44772 refact

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/refact/main.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/refact/main.controller.js
@@ -20,31 +20,26 @@
 		vm.organizationSelection = false;
 		vm.skillSelected = vm.skillIds.length;
 		vm.goToAgentsView = function () { rtaRouteService.goToSelectSkill(); };
-
 		vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: undefined };
 
-		(function OverviewComponentHandler() {
-			var teamPolling;
-			var sitePolling;
-			var sitePollingWithSkills;
-			var pollingIntervals = [];
+		var teamPolling;
+		var sitePolling;
+		var sitePollingWithSkills;
+		var pollingIntervals = [];
 
-			function initialSetUp () {
-				if (angular.isDefined(vm.urlParams.skillAreaId)) {
-					vm.selectedItems.skillAreaId = vm.urlParams.skillAreaId;
-					vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillAreaId: "' + vm.urlParams.skillAreaId + '"})';
-				}
-				else if (angular.isDefined(vm.urlParams.skillIds)) {
-					vm.selectedItems.skillIds = vm.urlParams.skillIds;
-					vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillIds: ["' + vm.urlParams.skillIds[0] + '"]})';
-				}
-				else {
-					vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: undefined };
-					vm.agentsState = 'rta.agents({siteIds: card.site.Id})';
-				}
+		(function () {
+			if (angular.isDefined(vm.urlParams.skillAreaId)) {
+				vm.selectedItems.skillAreaId = vm.urlParams.skillAreaId;
+				vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillAreaId: "' + vm.urlParams.skillAreaId + '"})';
 			}
-
-			initialSetUp();
+			else if (angular.isDefined(vm.urlParams.skillIds)) {
+				vm.selectedItems.skillIds = vm.urlParams.skillIds;
+				vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillIds: ["' + vm.urlParams.skillIds[0] + '"]})';
+			}
+			else {
+				vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: undefined };
+				vm.agentsState = 'rta.agents({siteIds: card.site.Id})';
+			}
 
 			if (angular.isDefined(vm.skillAreaId)) {
 				vm.skillIds = getSkillIdsFromSkillAreaId(vm.skillAreaId);
@@ -54,280 +49,268 @@
 			} else {
 				getSiteCards();
 			}
+		})();
 
-			function getSiteCards(ids) {
-				if (angular.isDefined(ids)) {
-					rtaService.getSiteCardsFor(ids).then(function (result) {
-						vm.siteCards = buildSiteCards(result);
-						vm.noSiteCards = !vm.siteCards.length;
-					});
-				} else {
-					rtaService.getSiteCardsFor().then(function (result) {
-						vm.siteCards = buildSiteCards(result);
-						vm.noSiteCards = !vm.siteCards.length;
-					});
-				}
+		function getSiteCards(ids) {
+			if (angular.isDefined(ids)) {
+				rtaService.getSiteCardsFor(ids).then(function (result) {
+					vm.siteCards = buildSiteCards(result);
+					vm.noSiteCards = !vm.siteCards.length;
+				});
+			} else {
+				rtaService.getSiteCardsFor().then(function (result) {
+					vm.siteCards = buildSiteCards(result);
+					vm.noSiteCards = !vm.siteCards.length;
+				});
 			}
+		}
 
-			function buildSiteCards(sites) {
-				return sites.map(function (site) {
-					site.Color = translateSiteColors(site);
-					return {
-						site: site,
-						isOpen: $stateParams.open != "false",
-						fetchTeamData: fetchTeamData
+		function buildSiteCards(sites) {
+			return sites.map(function (site) {
+				site.Color = translateSiteColors(site);
+				return {
+					site: site,
+					isOpen: $stateParams.open != "false",
+					fetchTeamData: fetchTeamData
+				}
+			});
+		}
+
+		function fetchTeamData(card) {
+			if (!card.isOpen) {
+				var match = pollingIntervals.find(function (interval) {
+					return interval.siteId === card.site.Id;
+				});
+				var index = pollingIntervals.indexOf(match);
+				pollingIntervals.splice(index, 1);
+				$interval.cancel(match.interval);
+			} else {
+				fetchTeams(card);
+				teamPolling = $interval(function () {
+					if (vm.skillIds.length) {
+						rtaService.getTeamCardsFor({ siteIds: card.site.Id, skillIds: vm.skillIds }).then(function (teams) {
+							updateTeamCards(card, teams);
+						})
 					}
-				});
-			}
-
-			function fetchTeamData(card) {
-				if (!card.isOpen) {
-					var match = pollingIntervals.find(function (interval) {
-						return interval.siteId === card.site.Id;
-					});
-					var index = pollingIntervals.indexOf(match);
-					pollingIntervals.splice(index, 1);
-					$interval.cancel(match.interval);
-				} else {
-					fetchTeams(card);
-					teamPolling = $interval(function () {
-						if (vm.skillIds.length) {
-							rtaService.getTeamCardsFor({ siteIds: card.site.Id, skillIds: vm.skillIds }).then(function (teams) {
-								updateTeamCards(card, teams);
-							})
-						}
-						else {
-							rtaService.getTeamCardsFor({ siteIds: card.site.Id }).then(function (teams) {
-								updateTeamCards(card, teams);
-							});
-						}
-					}, 5000);
-
-					pollingIntervals.push({
-						siteId: card.site.Id,
-						interval: teamPolling
-					});
-
-				}
-			}
-
-			function fetchTeams(card) {
-				if (vm.skillIds.length) {
-					rtaService.getTeamCardsFor({ siteIds: card.site.Id, skillIds: vm.skillIds }).then(function (teams) {
-						card.teams = teams;
-						setTeamToSelected(card);
-					});
-				} else {
-					rtaService.getTeamCardsFor({ siteIds: card.site.Id }).then(function (teams) {
-						card.teams = teams;
-						setTeamToSelected(card);
-					});
-
-				}
-			}
-
-			function updateTeamCards(card, teams) {
-				card.teams.forEach(function (team) {
-					var match = teams.find(function (t) {
-						return t.Id === team.Id;
-					});
-					team.Color = match.Color;
-					team.InAlarmCount = match.InAlarmCount;
-				});
-			}
-
-			function setTeamToSelected(card) {
-				var indexOfSite = vm.selectedItems.siteIds.indexOf(card.site.Id);
-				if (indexOfSite > -1) {
-					card.teams.forEach(function (team) {
-						team.isSelected = true;
-					});
-				}
-				else {
-					card.teams.forEach(function (team) {
-						var indexOfTeam = vm.selectedItems.teamIds.indexOf(team.Id);
-						if (indexOfTeam > -1) team.isSelected = true;
-					});
-				}
-			}
-
-			function getSkillIdsFromSkillAreaId(id) {
-				var temp = [];
-				var match = vm.skillAreas.find(function (sa) {
-					return sa.Id === id;
-				});
-
-				match.Skills.forEach(function (skill) {
-					temp.push(skill.Id);
-				});
-
-				return temp;
-			}
-
-			function translateSiteColors(site) {
-				if (site.Color === 'good') {
-					return '#C2E085';
-				} else if (site.Color === 'warning') {
-					return '#FFC285';
-				} else if (site.Color === 'danger') {
-					return '#EE8F7D';
-				} else {
-					return '#fff';
-				}
-			}
-
-			$scope.$watch(function () { return vm.skillIds; },
-				function (newValue, oldValue) {
-					if (angular.isDefined(sitePollingWithSkills)) {
-						$interval.cancel(sitePollingWithSkills);
-					}
-					if (pollingIntervals.length) {
-						pollingIntervals.forEach(function (i) {
-							$interval.cancel(i.interval);
+					else {
+						rtaService.getTeamCardsFor({ siteIds: card.site.Id }).then(function (teams) {
+							updateTeamCards(card, teams);
 						});
-						pollingIntervals = [];
 					}
-					if (newValue.length) {
-						vm.skillSelected = true;
-						sitePollingWithSkills = $interval(function () {
-							rtaService.getSiteCardsFor(vm.skillIds).then(function (result) {
-								result.forEach(function (r) {
-									updateSiteCard(r);
-								})
-							});
-						}, 5000);
-						$interval.cancel(sitePolling);
-					} else {
-						vm.skillSelected = false;
-						sitePolling = $interval(function () {
-							rtaService.getSiteCardsFor().then(function (result) {
-								result.forEach(function (r) {
-									updateSiteCard(r);
-								})
-							});
-						}, 5000);
-						$interval.cancel(sitePollingWithSkills);
-					}
+				}, 5000);
+
+				pollingIntervals.push({
+					siteId: card.site.Id,
+					interval: teamPolling
+				});
+			}
+		}
+
+		function fetchTeams(card) {
+			if (vm.skillIds.length) {
+				rtaService.getTeamCardsFor({ siteIds: card.site.Id, skillIds: vm.skillIds }).then(function (teams) {
+					card.teams = teams;
+					setTeamToSelected(card);
+				});
+			} else {
+				rtaService.getTeamCardsFor({ siteIds: card.site.Id }).then(function (teams) {
+					card.teams = teams;
+					setTeamToSelected(card);
 				});
 
-			function updateSiteCard(site) {
+			}
+		}
+
+		function setTeamToSelected(card) {
+			var indexOfSite = vm.selectedItems.siteIds.indexOf(card.site.Id);
+			if (indexOfSite > -1) {
+				card.teams.forEach(function (team) {
+					team.isSelected = true;
+				});
+			}
+			else {
+				card.teams.forEach(function (team) {
+					var indexOfTeam = vm.selectedItems.teamIds.indexOf(team.Id);
+					if (indexOfTeam > -1) team.isSelected = true;
+				});
+			}
+		}
+
+		function updateTeamCards(card, teams) {
+			card.teams.forEach(function (team) {
+				var match = teams.find(function (t) {
+					return t.Id === team.Id;
+				});
+				team.Color = match.Color;
+				team.InAlarmCount = match.InAlarmCount;
+			});
+		}
+
+		function getSkillIdsFromSkillAreaId(id) {
+			return vm.skillAreas.find(function (sa) { return sa.Id === id; })
+				.Skills.map(function (skill) { return skill.Id; });
+		}
+
+		function translateSiteColors(site) {
+			if (site.Color === 'good') {
+				return '#C2E085';
+			} else if (site.Color === 'warning') {
+				return '#FFC285';
+			} else if (site.Color === 'danger') {
+				return '#EE8F7D';
+			} else {
+				return '#fff';
+			}
+		}
+
+		$scope.$watch(function () { return vm.skillIds; },
+			function (newValue) {
+				clearSitePolls();
+
+				if (newValue.length) {
+					vm.skillSelected = true;
+					sitePollingWithSkills = $interval(function () {
+						rtaService
+							.getSiteCardsFor(vm.skillIds)
+							.then(updateSiteCards);
+					}, 5000);
+				} else {
+					vm.skillSelected = false;
+					sitePolling = $interval(function () {
+						rtaService
+							.getSiteCardsFor()
+							.then(updateSiteCards)
+					}, 5000);
+				}
+			});
+
+		function clearSitePolls() {
+			if (angular.isDefined(sitePollingWithSkills)) {
+				$interval.cancel(sitePollingWithSkills);
+			}
+			if (angular.isDefined(sitePolling)) {
+				$interval.cancel(sitePolling);
+			}
+			if (pollingIntervals.length) {
+				pollingIntervals.forEach(function (i) {
+					$interval.cancel(i.interval);
+				});
+				pollingIntervals = [];
+			}
+		}
+
+		function updateSiteCards(sites) {
+			sites.forEach(function (site) {
 				var match = vm.siteCards.find(function (card) {
 					return card.site.Id === site.Id;
 				});
 				match.site.Color = translateSiteColors(site);
 				match.site.InAlarmCount = site.InAlarmCount;
-			}
-
-			$scope.$on('$destroy', function () {
-				if (pollingIntervals.length) {
-					pollingIntervals.forEach(function (i) {
-						$interval.cancel(i.interval);
-					});
-					pollingIntervals = [];
-				}
-
-				$interval.cancel(sitePolling);
-				$interval.cancel(sitePollingWithSkills);
 			});
+		}
 
-			vm.filterOutput = function (selectedItem) {
-				if (!angular.isDefined(selectedItem)) {
-					resetOnNoSkills();
-					getSiteCards();
-				} else if (selectedItem.hasOwnProperty('Skills')) {
-					setUpForSkillArea(selectedItem);
-					getSiteCards(vm.skillIds);
-				} else {
-					setUpForSkill(selectedItem);
-					getSiteCards(vm.skillIds);
-				}
+		$scope.$on('$destroy', function () {
+			clearSitePolls();
+		});
+
+		vm.filterOutput = function (selectedItem) {
+			if (!angular.isDefined(selectedItem)) {
+				resetOnNoSkills();
+				getSiteCards();
+			} else if (selectedItem.hasOwnProperty('Skills')) {
+				setUpForSkillArea(selectedItem);
+				getSiteCards(vm.skillIds);
+			} else {
+				setUpForSkill(selectedItem);
+				getSiteCards(vm.skillIds);
 			}
+		}
 
-			function resetOnNoSkills() {
-				vm.skillIds = [];
-				vm.urlParams.skillIds = undefined;
-				vm.urlParams.skillAreaId = undefined;
-				vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: undefined };
-				vm.agentsState = 'rta.agents({siteIds: card.site.Id})';
-				$state.go($state.current.name, { skillAreaId: undefined, skillIds: undefined }, { notify: false });
-			}
+		function resetOnNoSkills() {
+			vm.skillIds = [];
+			vm.urlParams.skillIds = undefined;
+			vm.urlParams.skillAreaId = undefined;
+			vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: undefined };
+			vm.agentsState = 'rta.agents({siteIds: card.site.Id})';
+			$state.go($state.current.name, { skillAreaId: undefined, skillIds: undefined }, { notify: false });
+		}
 
-			function setUpForSkillArea(selectedItem) {
-				vm.skillIds = getSkillIdsFromSkillAreaId(selectedItem.Id);
-				vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: selectedItem.Id };
-				vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillAreaId: "' + selectedItem.Id + '"})';
-				$state.go($state.current.name, { skillAreaId: selectedItem.Id, skillIds: undefined }, { notify: false });
-			}
+		function setUpForSkillArea(selectedItem) {
+			vm.skillIds = getSkillIdsFromSkillAreaId(selectedItem.Id);
+			vm.selectedItems = { siteIds: [], teamIds: [], skillIds: [], skillAreaId: selectedItem.Id };
+			vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillAreaId: "' + selectedItem.Id + '"})';
+			$state.go($state.current.name, { skillAreaId: selectedItem.Id, skillIds: undefined }, { notify: false });
+		}
 
-			function setUpForSkill(selectedItem) {
-				vm.skillIds = [selectedItem.Id];
-				vm.selectedItems = { siteIds: [], teamIds: [], skillIds: vm.skillIds, skillAreaId: undefined };
-				vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillIds: ["' + selectedItem.Id + '"]})';
-				$state.go($state.current.name, { skillAreaId: undefined, skillIds: vm.skillIds }, { notify: false });
-			}
+		function setUpForSkill(selectedItem) {
+			vm.skillIds = [selectedItem.Id];
+			vm.selectedItems = { siteIds: [], teamIds: [], skillIds: vm.skillIds, skillAreaId: undefined };
+			vm.agentsState = 'rta.agents({siteIds: card.site.Id, skillIds: ["' + selectedItem.Id + '"]})';
+			$state.go($state.current.name, { skillAreaId: undefined, skillIds: vm.skillIds }, { notify: false });
+		}
 
-			vm.getSelectedItems = function (item) {
-				var itemIsSite = angular.isDefined(item.site);
-				if (itemIsSite) {
-					var indexOfSite = vm.selectedItems.siteIds.indexOf(item.site.Id);
-					var siteAlreadySelected = indexOfSite > -1;
+		vm.getSelectedItems = function (item) {
+			var itemIsSite = angular.isDefined(item.site);
+			if (itemIsSite) {
+				var indexOfSite = vm.selectedItems.siteIds.indexOf(item.site.Id);
+				var siteAlreadySelected = indexOfSite > -1;
 
-					if (!siteAlreadySelected) {
-						vm.selectedItems.siteIds.push(item.site.Id);
-						if (angular.isDefined(item.teams)) {
-							item.teams.forEach(function (team) {
-								var indexOfTeam = vm.selectedItems.teamIds.indexOf(team.Id);
-								if (indexOfTeam > -1) vm.selectedItems.teamIds.splice(indexOfTeam, 1);
-							});
-						}
-					}
-					else {
-						vm.selectedItems.siteIds.splice(indexOfSite, 1);
+				if (!siteAlreadySelected) {
+					vm.selectedItems.siteIds.push(item.site.Id);
+					if (angular.isDefined(item.teams)) {
+						item.teams.forEach(function (team) {
+							var indexOfTeam = vm.selectedItems.teamIds.indexOf(team.Id);
+							if (indexOfTeam > -1) vm.selectedItems.teamIds.splice(indexOfTeam, 1);
+						});
 					}
 				}
 				else {
-					var indexOfTeam = vm.selectedItems.teamIds.indexOf(item.Id);
-					var teamAlreadySelected = indexOfTeam > -1;
-					var match = vm.siteCards.find(function (card) {
-						return card.site.Id === item.SiteId;
-					});
-					var siteIndex = vm.selectedItems.siteIds.indexOf(match.site.Id);
+					vm.selectedItems.siteIds.splice(indexOfSite, 1);
+				}
+			}
+			else {
+				var indexOfTeam = vm.selectedItems.teamIds.indexOf(item.Id);
+				var teamAlreadySelected = indexOfTeam > -1;
+				var match = vm.siteCards.find(function (card) {
+					return card.site.Id === item.SiteId;
+				});
+				var siteIndex = vm.selectedItems.siteIds.indexOf(match.site.Id);
 
-					if (match.isSelected) {
-						match.teams.forEach(function (team) {
-							var index = vm.selectedItems.teamIds.indexOf(team.Id);
-							vm.selectedItems.teamIds.splice(index, 1);
-						});
-						vm.selectedItems.siteIds.push(match.site.Id);
-					}
-					else if (!match.isSelected && siteIndex > -1) {
-						vm.selectedItems.siteIds.splice(siteIndex, 1);
-						match.teams.forEach(function (team) {
-							if (team.Id !== item.Id) {
-								vm.selectedItems.teamIds.push(team.Id);
-							}
-						});
+				if (match.isSelected) {
+					match.teams.forEach(function (team) {
+						var index = vm.selectedItems.teamIds.indexOf(team.Id);
+						vm.selectedItems.teamIds.splice(index, 1);
+					});
+					vm.selectedItems.siteIds.push(match.site.Id);
+				}
+				else if (!match.isSelected && siteIndex > -1) {
+					vm.selectedItems.siteIds.splice(siteIndex, 1);
+					match.teams.forEach(function (team) {
+						if (team.Id !== item.Id) {
+							vm.selectedItems.teamIds.push(team.Id);
+						}
+					});
+				}
+				else {
+					if (!teamAlreadySelected) {
+						vm.selectedItems.teamIds.push(item.Id);
 					}
 					else {
-						if (!teamAlreadySelected) {
-							vm.selectedItems.teamIds.push(item.Id);
-						}
-						else {
-							vm.selectedItems.teamIds.splice(indexOfTeam, 1);
-						}
+						vm.selectedItems.teamIds.splice(indexOfTeam, 1);
 					}
 				}
-				vm.organizationSelection = vm.selectedItems.siteIds.length || vm.selectedItems.teamIds.length;
 			}
+			vm.organizationSelection = vm.selectedItems.siteIds.length || vm.selectedItems.teamIds.length;
+		}
 
-			vm.goToAgents = function () {
-				$state.go('rta.agents', vm.selectedItems);
-			}
+		vm.goToAgents = function () {
+			$state.go('rta.agents', vm.selectedItems);
+		}
 
-			vm.openTeam = function (team) {
-				vm.selectedItems.teamIds = [team.Id];
-				$state.go('rta.agents', vm.selectedItems);
-			}
-		})();
+		vm.openTeam = function (team) {
+			vm.selectedItems.teamIds = [team.Id];
+			$state.go('rta.agents', vm.selectedItems);
+		}
+
 	}
 })();
