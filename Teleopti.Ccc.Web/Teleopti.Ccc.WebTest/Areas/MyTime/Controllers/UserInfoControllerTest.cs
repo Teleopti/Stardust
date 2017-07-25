39b0bce1bf29caa9c78f66dd1ccd5b73d9064aeb
junwen.luo <none@none>
2017-07-25 14:33:36 +0800

    PBI #44558: fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/UserInfoControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/UserInfoControllerTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using NUnit.Framework;
 using Rhino.Mocks;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Web.Areas.MyTime.Controllers;
 using Teleopti.Interfaces.Domain;
 
@@ -14,7 +16,7 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Controllers
 		{
 			var cultureStub = MockRepository.GenerateStub<IUserCulture>();
 			cultureStub.Expect(c => c.GetCulture()).Return(CultureInfo.GetCultureInfo("sv-SE"));
-			var target = new UserInfoController(cultureStub);
+			var target = new UserInfoController(cultureStub, getUserTimeZoneStub());
 			
 			var result = target.Culture();
 			dynamic content = result.Data;
@@ -28,13 +30,34 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Controllers
 		{
 			var cultureStub = MockRepository.GenerateStub<IUserCulture>();
 			cultureStub.Expect(c => c.GetCulture()).Return(CultureInfo.GetCultureInfo("sv-SE"));
-			var target = new UserInfoController(cultureStub);
+			var target = new UserInfoController(cultureStub, getUserTimeZoneStub());
 
 			var result = target.Culture();
 			dynamic content = result.Data;
 
 			Assert.That((object)content.DateFormatForMoment, Is.EqualTo("YYYY-MM-DD"));
 			Assert.That(result, Is.Not.Null);
+		}
+
+		[Test]
+		public void Culture_BaseUtcOffsetInMinutesIsCorrectForSwidishCulture()
+		{
+			var cultureStub = MockRepository.GenerateStub<IUserCulture>();
+			cultureStub.Expect(c => c.GetCulture()).Return(CultureInfo.GetCultureInfo("sv-SE"));
+			var target = new UserInfoController(cultureStub, getUserTimeZoneStub());
+
+			var result = target.Culture();
+			dynamic content = result.Data;
+
+			Assert.That((object)content.BaseUtcOffsetInMinutes, Is.EqualTo(60));
+			Assert.That(result, Is.Not.Null);
+		}
+
+		private IUserTimeZone getUserTimeZoneStub()
+		{
+			var timezoneStub = MockRepository.GenerateStub<IUserTimeZone>();
+			timezoneStub.Expect(c => c.TimeZone()).Return(TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+			return timezoneStub;
 		}
 	}
 }
