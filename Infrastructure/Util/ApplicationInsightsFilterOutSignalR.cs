57cc06328a953996f4d4427f7151b3f70e891979
Robin Karlsson <robin.karlsson@teleopti.com>
2017-07-26 09:59:35 +0200

    Filtering out signalr from application insights #45133 & #44416

---
+++ Infrastructure/Util/ApplicationInsightsFilterOutSignalR.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Teleopti.Ccc.Infrastructure.Util
+{
+	public class ApplicationInsightsFilterOutSignalR : ITelemetryProcessor
+	{
+		private const string signalr = "/signalr/";
+		private ITelemetryProcessor Next { get; }
+
+		public ApplicationInsightsFilterOutSignalR(ITelemetryProcessor next)
+		{
+			Next = next;
+		}
+
+		public void Process(ITelemetry item)
+		{
+			var request = item as RequestTelemetry;
+			if (request?.Url != null &&
+				request.Url.AbsolutePath.IndexOf(signalr, StringComparison.InvariantCultureIgnoreCase) > -1)
+			{
+				return;
+			}
+
+			Next.Process(item);
+		}
+	}
+}
