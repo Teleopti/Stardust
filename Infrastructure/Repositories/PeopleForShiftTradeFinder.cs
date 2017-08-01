4251ebe6986e5bd5d8502469580b3b701ca8d622
Xinfeng Li <xinfeng.li@teleopti.com>
2017-08-01 09:31:57 +0800

    Backed out changeset: c0997f755a23

--- Infrastructure/Repositories/PeopleForShiftTradeFinder.cs
+++ Infrastructure/Repositories/PeopleForShiftTradeFinder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Security.Claims;
-using NHibernate;
 using NHibernate.Impl;
 using NHibernate.Transform;
 using Teleopti.Ccc.Domain.Collection;
@@ -25,9 +24,12 @@ namespace Teleopti.Ccc.Infrastructure.Repositories
 			_unitOfWork = unitOfWork;
 		}
 
-		public IList<IPersonAuthorization> GetPeople(IPerson personFrom, DateOnly shiftTradeDate, IList<Guid> groupIdList,
+		public IList<IPersonAuthorization> GetPeople (IPerson personFrom, DateOnly shiftTradeDate, IList<Guid> groupIdList,
 			string name, NameFormatSetting nameFormat = NameFormatSetting.FirstNameThenLastName)
 		{
+			var useChineseNameFormat = name != null && StringHelper.StringContainsChinese(name);
+			var firstNameFirst = nameFormat == NameFormatSetting.FirstNameThenLastName;
+
 			const string sql = "exec ReadModel.FindPeopleForShiftTrade @scheduleDate=:scheduleDate, "
 							   + "@groupIdList=:groupIdList, @businessUnitId=:businessUnitId, @name=:name, "
 							   + "@noSpaceInName = :noSpaceInName, "
@@ -36,36 +38,26 @@ namespace Teleopti.Ccc.Infrastructure.Repositories
 							   + "@fromPersonId = :fromPersonId, "
 							   + "@shiftStartUTC = :shiftStartUTC, "
 							   + "@shiftEndUTC = :shiftEndUTC";
-
-			var useChineseNameFormat = name != null && StringHelper.StringContainsChinese(name);
-			var firstNameFirst = nameFormat == NameFormatSetting.FirstNameThenLastName;
-			var personFromId = personFrom.Id.GetValueOrDefault();
-			var workflowControlSetId = personFrom.WorkflowControlSet.Id.GetValueOrDefault();
-
 			var result = new List<IPersonAuthorization>();
-
 			//ROBTODO: Temporary - Person From Shift Period is currently not being passed to Get People, this is currently not being used by the query,
 			//	but is a requested parameter to add for further optimisation
 			var dummyDateTimePeriod = new DateTimePeriod(DateTime.Today.ToUniversalTime(), DateTime.Now.ToUniversalTime());
-			var statelessSession = ((NHibernateUnitOfWork)_unitOfWork.Current()).Session.SessionFactory.OpenStatelessSession();
-			groupIdList.Batch(100).ForEach(groupIdBatch =>
+			groupIdList.Batch(100).ForEach(l =>
 			{
-				var groupIdString = string.Join(",", groupIdBatch);
-				var batchResult = statelessSession.CreateSQLQuery(sql)
-					.SetDateOnly("scheduleDate", shiftTradeDate)
-					.SetParameter("groupIdList", groupIdString, NHibernateUtil.StringClob)
-					.SetString("name", name)
-					.SetBoolean("noSpaceInName", useChineseNameFormat)
-					.SetBoolean("firstNameFirst", firstNameFirst)
-					.SetGuid("businessUnitId", getBusinessUnitId())
-					.SetGuid("workflowControlSetId", workflowControlSetId)
-					.SetGuid("fromPersonId", personFromId)
-					.SetDateTime("shiftStartUTC", dummyDateTimePeriod.StartDateTime)
-					.SetDateTime("shiftEndUTC", dummyDateTimePeriod.EndDateTime)
-					.SetResultTransformer(Transformers.AliasToBean(typeof(PersonSelectorShiftTrade)))
-					.SetReadOnly(true)
-					.List<IPersonAuthorization>();
-
+				var batchResult = ((NHibernateUnitOfWork)_unitOfWork.Current()).Session.CreateSQLQuery(sql)
+				.SetDateOnly("scheduleDate", shiftTradeDate)
+				.SetString("groupIdList", string.Join(",", l))
+				.SetString("name", name)
+				.SetBoolean("noSpaceInName", useChineseNameFormat)
+				.SetBoolean("firstNameFirst", firstNameFirst)
+				.SetGuid("businessUnitId", getBusinessUnitId())
+				.SetGuid("workflowControlSetId", personFrom.WorkflowControlSet.Id.GetValueOrDefault())
+				.SetGuid("fromPersonId", personFrom.Id.GetValueOrDefault())
+				.SetDateTime("shiftStartUTC", dummyDateTimePeriod.StartDateTime)
+				.SetDateTime("shiftEndUTC", dummyDateTimePeriod.EndDateTime)
+				.SetResultTransformer(Transformers.AliasToBean(typeof(PersonSelectorShiftTrade)))
+				.SetReadOnly(true)
+				.List<IPersonAuthorization>();
 				result.AddRange(batchResult);
 			});
 			return result;
@@ -74,9 +66,12 @@ namespace Teleopti.Ccc.Infrastructure.Repositories
 		private Guid getBusinessUnitId()
 		{
 			var filter = (FilterImpl)_unitOfWork.Session().GetEnabledFilter("businessUnitFilter");
-			return !filter.Parameters.TryGetValue("businessUnitParameter", out object businessUnitId)
-				? ((ITeleoptiIdentity) ClaimsPrincipal.Current.Identity).BusinessUnit.Id.GetValueOrDefault()
-				: Guid.Parse(businessUnitId.ToString());
+			object businessUnitId;
+			if (!filter.Parameters.TryGetValue("businessUnitParameter", out businessUnitId))
+			{
+				return ((ITeleoptiIdentity)ClaimsPrincipal.Current.Identity).BusinessUnit.Id.GetValueOrDefault();
+			}
+			return Guid.Parse(businessUnitId.ToString());
 		}
 	}
 }
