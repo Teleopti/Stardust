0755804be5fe3564651e2178658bf1fd9eb6261a
Jianfeng <none@none>
2017-08-08 13:05:04 +0800

    fix #45307: disable time picker editing, restore request page, restore cursor position

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/knockout.bindings.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/knockout.bindings.js
@@ -186,7 +186,6 @@ ko.bindingHandlers.scrollIntoViewWhenClick = {
 			var ele = this;
 			setTimeout(function(){
 				$(valueAccessor()).scrollTop($(ele).offset().top);
-				$(ele).val($(ele).val());
 			}, 500);
 		});
 	}
