199a5500f4baa8befbe0cc3253b6fd264af9e435
Asad Mirza <asad.mirza@teleopti.com>
2017-08-23 13:55:07 +0200

    PBI: #45411 hide permission node if no license found

--- Infrastructure/Toggle/ApplicationFunctionsToggleFilter.cs
+++ Infrastructure/Toggle/ApplicationFunctionsToggleFilter.cs
@@ -1,4 +1,5 @@
-﻿using Teleopti.Ccc.Domain.FeatureFlags;
+﻿using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
 using Teleopti.Ccc.Domain.Security.AuthorizationEntities;
 
@@ -8,11 +9,13 @@ namespace Teleopti.Ccc.Infrastructure.Toggle
 	{
 		private readonly IApplicationFunctionsProvider _applicationFunctionsProvider;
 		private readonly IToggleManager _toggleManager;
+		private ICurrentDataSource _currentDataSource;
 
-		public ApplicationFunctionsToggleFilter(IApplicationFunctionsProvider applicationFunctionsProvider, IToggleManager toggleManager)
+		public ApplicationFunctionsToggleFilter(IApplicationFunctionsProvider applicationFunctionsProvider, IToggleManager toggleManager, ICurrentDataSource currentDataSource)
 		{
 			_applicationFunctionsProvider = applicationFunctionsProvider;
 			_toggleManager = toggleManager;
+			_currentDataSource = currentDataSource;
 		}
 
 		public AllFunctions FilteredFunctions()
@@ -21,7 +24,25 @@ namespace Teleopti.Ccc.Infrastructure.Toggle
 
 			if (!_toggleManager.IsEnabled(Toggles.MyTimeWeb_OvertimeRequest_44558)) hideOvertimeRequest(functions);
 
+			hideBpoExchangeIfNotLicensed(functions);
 			return functions;
+		}
+
+		private void hideBpoExchangeIfNotLicensed(AllFunctions functions)
+		{
+			var currentName = _currentDataSource.CurrentName();
+			var isLicenseAvailible = DefinedLicenseDataFactory.HasLicense(currentName) &&
+									 DefinedLicenseDataFactory.GetLicenseActivator(currentName).EnabledLicenseOptionPaths.Contains(
+										 DefinedLicenseOptionPaths.TeleoptiCccPilotCustomersBpoExchange);
+			if (!isLicenseAvailible)
+			{
+				var foundFunction = functions.FindByForeignId(DefinedRaptorApplicationFunctionForeignIds.BpoExchange);
+				if (foundFunction != null)
+				{
+					foundFunction.SetHidden();
+				}
+			}
+
 		}
 
 		private static void hideOvertimeRequest(AllFunctions functions)
