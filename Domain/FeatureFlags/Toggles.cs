2b44dcf485d6f50b0f9e481bd50db11b3d6d4406
Ola <none@none>
2017-08-23 10:28:04 +0200

    PBI #43389 Add toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -174,6 +174,7 @@
 		Staffing_ReadModel_BetterAccuracy_43447,
 		Staffing_ReadModel_BetterAccuracy_Step2_44271,
 		Staffing_ReadModel_BetterAccuracy_Step3_44331,
+		Staffing_ReadModel_BetterAccuracy_Step4_43389,
 		Wfm_Intraday_SupportSkillTypeEmail_44002,
 		WFM_Intraday_Show_For_Other_Days_43504,
 		WFM_Intraday_SupportMultisiteSkill_43874,
