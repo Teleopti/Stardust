c87f166196145122ac5479c43bf9ddf488354f72
Jonas Nordh <jonas.nordh@teleopti.com>
2017-08-24 10:23:18 +0200

    Bug #45579 - Handle UTF-8 file with BOM in Forecast Workload import

--- Domain/Forecasting/Import/ForecastsFileContentProvider.cs
+++ Domain/Forecasting/Import/ForecastsFileContentProvider.cs
@@ -9,68 +9,70 @@ using Teleopti.Ccc.Domain.InterfaceLegacy.Messages.General;
 
 namespace Teleopti.Ccc.Domain.Forecasting.Import
 {
-    public interface IForecastsFileContentProvider
-    {
-        ICollection<IForecastsRow> LoadContent(byte[] fileContent, TimeZoneInfo timeZone);
-    }
+	public interface IForecastsFileContentProvider
+	{
+		ICollection<IForecastsRow> LoadContent(byte[] fileContent, TimeZoneInfo timeZone);
+	}
 
-    public class ForecastsFileContentProvider : IForecastsFileContentProvider
-    {
-        private readonly IForecastsRowExtractor _rowExtractor;
+	public class ForecastsFileContentProvider : IForecastsFileContentProvider
+	{
+		private readonly IForecastsRowExtractor _rowExtractor;
 
-        public ForecastsFileContentProvider(IForecastsRowExtractor rowExtractor)
-        {
-            _rowExtractor = rowExtractor;
-        }
+		public ForecastsFileContentProvider(IForecastsRowExtractor rowExtractor)
+		{
+			_rowExtractor = rowExtractor;
+		}
 
-        public ICollection<IForecastsRow> LoadContent(byte[] fileContent, TimeZoneInfo timeZone)
-        {
-            var rowNumber = 1;
-            var result = new List<IForecastsRow>();
-            try
-            {
-                foreach (var line in Encoding.UTF8.GetString(fileContent).Split(new[] { Environment.NewLine },
-                                                                                StringSplitOptions.RemoveEmptyEntries))
-                {
-                    var forecastsRow = _rowExtractor.Extract(line, timeZone);
-                    if (timeZone.IsAmbiguousTime(forecastsRow.LocalDateTimeFrom))
-                    {
-                        var missingRow = new ForecastsRow
-                                             {
-                                                 AfterTaskTime = forecastsRow.AfterTaskTime,
-                                                 Agents = forecastsRow.Agents,
-                                                 LocalDateTimeFrom = forecastsRow.LocalDateTimeFrom,
-                                                 LocalDateTimeTo = forecastsRow.LocalDateTimeTo,
-                                                 SkillName = forecastsRow.SkillName,
-                                                 Tasks = forecastsRow.Tasks,
-                                                 TaskTime = forecastsRow.TaskTime,
-                                                 UtcDateTimeFrom = adjustMissingUtcTime(forecastsRow.UtcDateTimeFrom, timeZone),
-                                                 UtcDateTimeTo = adjustMissingUtcTime(forecastsRow.UtcDateTimeTo, timeZone)
-                                             };
-                        result.Add(missingRow);
-                    }
-                    else
-                    {
-                        if (timeZone.IsAmbiguousTime(forecastsRow.LocalDateTimeTo))
-                            forecastsRow.UtcDateTimeTo = adjustMissingUtcTime(forecastsRow.UtcDateTimeTo,
-                                timeZone);
-                    }
-                    result.Add(forecastsRow);
-                    rowNumber++;
-                }
-            }
-            catch (ValidationException exception)
-            {
-                throw new ValidationException(string.Format(CultureInfo.InvariantCulture,"Line {0}, Error:{1}", rowNumber, exception.Message));
-            }
-            return result;
-        }
-        
-        private static DateTime adjustMissingUtcTime(DateTime utcTime, TimeZoneInfo timeZone)
-        {
-            var rules = timeZone.GetAdjustmentRules();
-            var rul = rules.FirstOrDefault(r => r.DateStart < new DateTime(utcTime.Year, 1, 1) && r.DateEnd > new DateTime(utcTime.Year, 1, 1));
-            return rul == null ? utcTime : utcTime.Add(-rul.DaylightDelta);
-        }
-    }
+		public ICollection<IForecastsRow> LoadContent(byte[] fileContent, TimeZoneInfo timeZone)
+		{
+			var rowNumber = 1;
+			var result = new List<IForecastsRow>();
+			try
+			{
+				var fileContentString = Encoding.UTF8.GetString(fileContent).TrimStart('\uFEFF');
+
+				foreach (var line in fileContentString.Split(new[] { Environment.NewLine },
+																				StringSplitOptions.RemoveEmptyEntries))
+				{
+					var forecastsRow = _rowExtractor.Extract(line, timeZone);
+					if (timeZone.IsAmbiguousTime(forecastsRow.LocalDateTimeFrom))
+					{
+						var missingRow = new ForecastsRow
+						{
+							AfterTaskTime = forecastsRow.AfterTaskTime,
+							Agents = forecastsRow.Agents,
+							LocalDateTimeFrom = forecastsRow.LocalDateTimeFrom,
+							LocalDateTimeTo = forecastsRow.LocalDateTimeTo,
+							SkillName = forecastsRow.SkillName,
+							Tasks = forecastsRow.Tasks,
+							TaskTime = forecastsRow.TaskTime,
+							UtcDateTimeFrom = adjustMissingUtcTime(forecastsRow.UtcDateTimeFrom, timeZone),
+							UtcDateTimeTo = adjustMissingUtcTime(forecastsRow.UtcDateTimeTo, timeZone)
+						};
+						result.Add(missingRow);
+					}
+					else
+					{
+						if (timeZone.IsAmbiguousTime(forecastsRow.LocalDateTimeTo))
+							forecastsRow.UtcDateTimeTo = adjustMissingUtcTime(forecastsRow.UtcDateTimeTo,
+								timeZone);
+					}
+					result.Add(forecastsRow);
+					rowNumber++;
+				}
+			}
+			catch (ValidationException exception)
+			{
+				throw new ValidationException(string.Format(CultureInfo.InvariantCulture, "Line {0}, Error:{1}", rowNumber, exception.Message));
+			}
+			return result;
+		}
+
+		private static DateTime adjustMissingUtcTime(DateTime utcTime, TimeZoneInfo timeZone)
+		{
+			var rules = timeZone.GetAdjustmentRules();
+			var rul = rules.FirstOrDefault(r => r.DateStart < new DateTime(utcTime.Year, 1, 1) && r.DateEnd > new DateTime(utcTime.Year, 1, 1));
+			return rul == null ? utcTime : utcTime.Add(-rul.DaylightDelta);
+		}
+	}
 }
--- DomainTest/Forecasting/Import/ForecastsFileContentProviderTest.cs
+++ DomainTest/Forecasting/Import/ForecastsFileContentProviderTest.cs
@@ -1,89 +1,105 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using SharpTestsEx;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Forecasting.Import;
 
 namespace Teleopti.Ccc.DomainTest.Forecasting.Import
 {
-    [TestFixture]
-    public class ForecastsFileContentProviderTest
-    {
-        private IForecastsFileContentProvider _target;
-        private byte[] _fileContent;
-        private TimeZoneInfo _timeZone;
+	[TestFixture]
+	public class ForecastsFileContentProviderTest
+	{
+		private IForecastsFileContentProvider _target;
+		private byte[] _fileContent;
+		private TimeZoneInfo _timeZone;
 
-        [SetUp]
-        public void Setup()
-        {
-            _timeZone = (TimeZoneInfo.Utc);
-            _target = new ForecastsFileContentProvider(new ForecastsRowExtractor());
-        }
+		[SetUp]
+		public void Setup()
+		{
+			_timeZone = (TimeZoneInfo.Utc);
+			_target = new ForecastsFileContentProvider(new ForecastsRowExtractor());
+		}
 
-        [Test]
-        public void ShouldHandleWrongFileStream()
-        {
-            _fileContent = Encoding.UTF8.GetBytes("Insurance,20120301 12:45,20120301 13:00,17,179,0,");
+		[Test]
+		public void ShouldHandleWrongFileStream()
+		{
+			_fileContent = Encoding.UTF8.GetBytes("Insurance,20120301 12:45,20120301 13:00,17,179,0,");
 
 			Assert.Throws<ValidationException>(() => _target.LoadContent(_fileContent, _timeZone));
-        }
+		}
 
-        [Test]
-        public void ShouldLoadContentFromFileStream()
-        {
-            _fileContent = Encoding.UTF8.GetBytes("Insurance,20120301 12:45,20120301 13:00,17,179,0,4.05");
-            var forecastRow = _target.LoadContent(_fileContent, _timeZone).First();
+		[Test]
+		public void ShouldLoadContentFromFileStream()
+		{
+			_fileContent = Encoding.UTF8.GetBytes("Insurance,20120301 12:45,20120301 13:00,17,179,0,4.05");
+			var forecastRow = _target.LoadContent(_fileContent, _timeZone).First();
 
-            Assert.That(forecastRow.TaskTime, Is.EqualTo(179));
-            Assert.That(forecastRow.AfterTaskTime, Is.EqualTo(0));
-            Assert.That(forecastRow.Agents, Is.EqualTo(4.05));
-            Assert.That(forecastRow.LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 3, 1, 12, 45, 0)));
-            Assert.That(forecastRow.LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 3, 1, 13, 0, 0)));
-            Assert.That(forecastRow.SkillName, Is.EqualTo("Insurance"));
-            Assert.That(forecastRow.Tasks, Is.EqualTo(17));
-            Assert.That(forecastRow.UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 3, 1, 12, 45, 0, DateTimeKind.Utc)));
-            Assert.That(forecastRow.UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 3, 1, 13, 0, 0, DateTimeKind.Utc)));
-        }
+			Assert.That(forecastRow.TaskTime, Is.EqualTo(179));
+			Assert.That(forecastRow.AfterTaskTime, Is.EqualTo(0));
+			Assert.That(forecastRow.Agents, Is.EqualTo(4.05));
+			Assert.That(forecastRow.LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 3, 1, 12, 45, 0)));
+			Assert.That(forecastRow.LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 3, 1, 13, 0, 0)));
+			Assert.That(forecastRow.SkillName, Is.EqualTo("Insurance"));
+			Assert.That(forecastRow.Tasks, Is.EqualTo(17));
+			Assert.That(forecastRow.UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 3, 1, 12, 45, 0, DateTimeKind.Utc)));
+			Assert.That(forecastRow.UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 3, 1, 13, 0, 0, DateTimeKind.Utc)));
+		}
 
-        [Test]
-        public void ShouldImportWinterTime()
-        {
-            _fileContent = Encoding.UTF8.GetBytes("Insurance,20121028 02:00,20121028 02:15,17,179,0,4.05\r\nInsurance,20121028 02:45,20121028 03:00,17,179,0,4.05");
-            var timeZone = (TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
-            var forecastRows = _target.LoadContent(_fileContent, timeZone).ToArray();
+		[Test]
+		public void ShouldImportWinterTime()
+		{
+			_fileContent = Encoding.UTF8.GetBytes("Insurance,20121028 02:00,20121028 02:15,17,179,0,4.05\r\nInsurance,20121028 02:45,20121028 03:00,17,179,0,4.05");
+			var timeZone = (TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+			var forecastRows = _target.LoadContent(_fileContent, timeZone).ToArray();
 
-            Assert.That(forecastRows.Length, Is.EqualTo(4));
-            Assert.That(forecastRows[0].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
-            Assert.That(forecastRows[0].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 15, 0)));
-            Assert.That(forecastRows[0].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 0, 0, 0)));
-            Assert.That(forecastRows[0].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 0, 15, 0)));
-            Assert.That(forecastRows[1].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
-            Assert.That(forecastRows[1].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 15, 0)));
-            Assert.That(forecastRows[1].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 1, 0, 0)));
-            Assert.That(forecastRows[1].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 1, 15, 0)));
-            Assert.That(forecastRows[2].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 45, 0)));
-            Assert.That(forecastRows[2].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 3, 0, 0)));
-            Assert.That(forecastRows[2].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 0, 45, 0)));
-            Assert.That(forecastRows[2].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 1, 0, 0)));
-            Assert.That(forecastRows[3].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 45, 0)));
-            Assert.That(forecastRows[3].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 3, 0, 0)));
-            Assert.That(forecastRows[3].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 1, 45, 0)));
-            Assert.That(forecastRows[3].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
-        }
+			Assert.That(forecastRows.Length, Is.EqualTo(4));
+			Assert.That(forecastRows[0].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
+			Assert.That(forecastRows[0].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 15, 0)));
+			Assert.That(forecastRows[0].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 0, 0, 0)));
+			Assert.That(forecastRows[0].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 0, 15, 0)));
+			Assert.That(forecastRows[1].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
+			Assert.That(forecastRows[1].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 15, 0)));
+			Assert.That(forecastRows[1].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 1, 0, 0)));
+			Assert.That(forecastRows[1].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 1, 15, 0)));
+			Assert.That(forecastRows[2].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 45, 0)));
+			Assert.That(forecastRows[2].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 3, 0, 0)));
+			Assert.That(forecastRows[2].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 0, 45, 0)));
+			Assert.That(forecastRows[2].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 1, 0, 0)));
+			Assert.That(forecastRows[3].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 2, 45, 0)));
+			Assert.That(forecastRows[3].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 3, 0, 0)));
+			Assert.That(forecastRows[3].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 1, 45, 0)));
+			Assert.That(forecastRows[3].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
+		}
 
-        [Test]
-        public void ShouldImportWinterTimeForAmbiguousTime()
-        {
-            _fileContent = Encoding.UTF8.GetBytes("Insurance,20121028 01:45,20121028 02:00,17,179,0,4.05");
-            var timeZone = (TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
-            var forecastRows = _target.LoadContent(_fileContent, timeZone).ToArray();
+		[Test]
+		public void ShouldImportWinterTimeForAmbiguousTime()
+		{
+			_fileContent = Encoding.UTF8.GetBytes("Insurance,20121028 01:45,20121028 02:00,17,179,0,4.05");
+			var timeZone = (TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+			var forecastRows = _target.LoadContent(_fileContent, timeZone).ToArray();
 
-            Assert.That(forecastRows.Length, Is.EqualTo(1));
-            Assert.That(forecastRows[0].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 1, 45, 0)));
-            Assert.That(forecastRows[0].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
-            Assert.That(forecastRows[0].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 27, 23, 45, 0)));
-            Assert.That(forecastRows[0].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 0, 0, 0)));
-        }
-    }
+			Assert.That(forecastRows.Length, Is.EqualTo(1));
+			Assert.That(forecastRows[0].LocalDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 28, 1, 45, 0)));
+			Assert.That(forecastRows[0].LocalDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 2, 0, 0)));
+			Assert.That(forecastRows[0].UtcDateTimeFrom, Is.EqualTo(new DateTime(2012, 10, 27, 23, 45, 0)));
+			Assert.That(forecastRows[0].UtcDateTimeTo, Is.EqualTo(new DateTime(2012, 10, 28, 0, 0, 0)));
+		}
+
+		[Test]
+		public void ShouldHandleFileWithEncodingUtfWithByteOrderMark()
+		{
+			using (var ms = new MemoryStream())
+			using (var sw = new StreamWriter(ms, new UTF8Encoding(true)))
+			{
+				sw.Write("Insurance,20121028 01:45,20121028 02:00,17,179,0,4.05");
+				sw.Flush();
+				_fileContent = ms.ToArray();
+			}
+			var forecastRows = _target.LoadContent(_fileContent, _timeZone).ToArray();
+			forecastRows[0].SkillName.Should().Be.EqualTo("Insurance");
+		}
+	}
 }
