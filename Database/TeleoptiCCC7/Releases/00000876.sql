abedc69db3b69f66e96d22f21867c3c7e0c76067
Bharath Muchi <none@none>
2017-09-05 13:42:07 +0200

    Bug #45685 fixing release script 876

--- Database/TeleoptiCCC7/Releases/00000876.sql
+++ Database/TeleoptiCCC7/Releases/00000876.sql
@@ -1,1 +1,9 @@
-DELETE FROM ChildSkill WHERE ParentSkill IS NULL
+Delete  TemplateMultisitePeriodDistribution FROM TemplateMultisitePeriodDistribution tm Inner join ChildSkill ck
+on ck.Skill = tm.ChildSkill
+WHERE ck.ParentSkill IS NULL
+
+Delete  MultisitePeriodDistribution FROM MultisitePeriodDistribution mp Inner join ChildSkill ck
+on ck.Skill = mp.ChildSkill
+WHERE ck.ParentSkill IS NULL
+
+Delete FROM ChildSkill WHERE ParentSkill IS NULL
