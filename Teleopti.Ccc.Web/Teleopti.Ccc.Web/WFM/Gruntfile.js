f26586cc88bdac6f97d8c89658dedd2057d0294e
LinM <none@none>
2017-09-07 13:03:11 +0200

    remove hashcode in filename / disable cacheBust task from grunt

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/Gruntfile.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/Gruntfile.js
@@ -449,13 +449,13 @@ module.exports = function (grunt) {
     grunt.registerTask('nova', ['devDist', 'iisexpress:web', 'watch:dev']); // this task run the main task and then watch for file changes
     grunt.registerTask('build', ['msbuild:build']); // build the solution
     grunt.registerTask('rebuild', ['msbuild:rebuild']); // rebuild the solution
-    grunt.registerTask('generateIndex', ['processhtml:dist', 'cacheBust:dist']);
-    grunt.registerTask('generateIndexDev', ['processhtml:dev', 'cacheBust:dist']);
+    grunt.registerTask('generateIndex', ['processhtml:dist']);
+    grunt.registerTask('generateIndexDev', ['processhtml:dev']);
     grunt.registerTask('eslint-beta', ['eslint']);
     grunt.registerTask('devDistWatch', ['devDist', 'watch:dev']);
-    grunt.registerTask('dist', ['ngtemplates', 'sass', 'imageEmbed', 'concat:distModules', 'concat:distCss', 'concat:distDarkCss', 'cssmin', 'uglify:dist', 'copy:extras','copy:bootstrap', 'generateIndex', 'clean:dist', 'cacheBust:dist']); // this task should only be used by the build. It's kind of packaging for production.
+    grunt.registerTask('dist', ['ngtemplates', 'sass', 'imageEmbed', 'concat:distModules', 'concat:distCss', 'concat:distDarkCss', 'cssmin', 'uglify:dist', 'copy:extras','copy:bootstrap', 'generateIndex', 'clean:dist']); // this task should only be used by the build. It's kind of packaging for production.
 
     // for desktop client
-    grunt.registerTask('buildForDesktop', ['copy:sourceMaps', 'processhtml:distForDesktop', 'cacheBust:dist']);
+    grunt.registerTask('buildForDesktop', ['copy:sourceMaps', 'processhtml:distForDesktop']);
 
 };
