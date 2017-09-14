363fe15cb636abf1c3c396a173c57d6ab1788520
anderss <none@none>
2017-09-14 12:17:28 +0200

    Refactoring - intraday config - 1

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/intraday/intraday.config.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/intraday/intraday.config.controller.js
@@ -1,69 +1,72 @@
-(function() {
-    'use strict';
-    angular.module('wfm.intraday').controller('IntradayConfigController', [
-        '$scope',
-        '$state',
-        'intradayService',
-        '$filter',
-        'NoticeService',
-        '$translate',
+(function () {
+	'use strict';
+	angular.module('wfm.intraday').controller('IntradayConfigController', [
+		'$scope',
+		'$state',
+		'intradayService',
+		'$filter',
+		'NoticeService',
+		'$translate',
 		'Toggle',
-        'skillIconService',
-        function($scope, $state, intradayService, $filter, NoticeService, $translate, toggleSvc, skillIconService) {
-            $scope.skills = [];
-            $scope.skillAreaName = '';
-            $scope.getSkillIcon = skillIconService.get;
-			$scope.toggles = {
-				unifiedSkillGroupManagement: []
-			};
-			toggleSvc.togglesLoaded.then(function () {
-				$scope.toggles.unifiedSkillGroupManagement = toggleSvc.WFM_Unified_Skill_Group_Management_45417;
+		'skillIconService',
+		IntradayConfigController]);
+
+	function IntradayConfigController($scope, $state, intradayService, $filter, NoticeService, $translate, toggleSvc, skillIconService) {
+		$scope.skills = [];
+		$scope.skillAreaName = '';
+		$scope.getSkillIcon = skillIconService.get;
+		$scope.toggles = {
+			unifiedSkillGroupManagement: []
+		};
+
+		$scope.exitConfigMode = function () {
+			$state.go('intraday', {isNewSkillArea: false});
+		};
+
+		$scope.skillSelected = function () {
+			var selectedSkills = $filter('filter')($scope.skills, {isSelected: true});
+			var selectedSkillIds = selectedSkills.map(function (skill) {
+				return skill.Id;
 			});
-            $scope.exitConfigMode = function() {
-                $state.go('intraday', {isNewSkillArea: false});
-            };
+			return selectedSkillIds.length > 0;
+		};
 
-            intradayService.getSkills.query().$promise.then(function(skills) {
-                $scope.skills = skills;
-            });
+		$scope.saveSkillArea = function (form) {
+			if (form.$invalid) {
+				return;
+			}
+			var selectedSkills = $filter('filter')($scope.skills, {isSelected: true});
 
-            $scope.skillSelected = function() {
-                var selectedSkills = $filter('filter')($scope.skills, {isSelected: true});
-                var selectedSkillIds = selectedSkills.map(function(skill) {
-                    return skill.Id;
-                });
-                return selectedSkillIds.length > 0;
-            };
+			var selectedSkillIds = selectedSkills.map(function (skill) {
+				return skill.Id;
+			});
 
-            $scope.saveSkillArea = function(form) {
-                if (form.$invalid) {
-                    return;
-                }
-                var selectedSkills = $filter('filter')($scope.skills, {isSelected: true});
+			if (selectedSkillIds.length <= 0) {
+				NoticeService.error($translate.instant('SkillAreaNoSkillSelected'), 5000, false);
+				return;
+			}
 
-                var selectedSkillIds = selectedSkills.map(function(skill) {
-                    return skill.Id;
-                });
+			intradayService.createSkillArea
+				.query({
+					Name: $scope.skillAreaName,
+					Skills: selectedSkillIds
+				})
+				.$promise.then(function (result) {
+				notifySkillAreaCreation();
+				$state.go('intraday', {isNewSkillArea: true});
+			});
+		};
 
-                if (selectedSkillIds.length <= 0) {
-                    NoticeService.error($translate.instant('SkillAreaNoSkillSelected'), 5000, false);
-                    return;
-                }
+		toggleSvc.togglesLoaded.then(function () {
+			$scope.toggles.unifiedSkillGroupManagement = toggleSvc.WFM_Unified_Skill_Group_Management_45417;
+		});
 
-                intradayService.createSkillArea
-                    .query({
-                        Name: $scope.skillAreaName,
-                        Skills: selectedSkillIds
-                    })
-                    .$promise.then(function(result) {
-                        notifySkillAreaCreation();
-                        $state.go('intraday', {isNewSkillArea: true});
-                    });
-            };
+		var notifySkillAreaCreation = function () {
+			NoticeService.success($translate.instant('Created') + ' ' + $scope.skillAreaName, 5000, false);
+		};
 
-            var notifySkillAreaCreation = function() {
-                NoticeService.success($translate.instant('Created') + ' ' + $scope.skillAreaName, 5000, false);
-            };
-        }
-    ]);
+		intradayService.getSkills.query().$promise.then(function (skills) {
+			$scope.skills = skills;
+		});
+	}
 })();
