af14d51df98f2399d16040bffbac92ac1f60c998
johan Damm <none@none>
2017-09-25 14:50:15 +0200

    fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'dist/modules.*',
+			'dist/resources/modules.*',
 			'node_modules/angular-material/angular-material-mocks.js',
 			'node_modules/angular-mocks/angular-mocks.js',
 			'dist/templates.*',
