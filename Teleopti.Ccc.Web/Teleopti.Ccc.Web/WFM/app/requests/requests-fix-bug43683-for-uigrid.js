6063238ef03ef5ec658d58f3b9b03675035c7a8d
Jianfeng <none@none>
2017-09-26 18:40:32 +0800

    fix #45655: ui-grid popup menu positioning issue.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/requests/requests-fix-bug43683-for-uigrid.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/requests/requests-fix-bug43683-for-uigrid.js
@@ -1,12 +1,12 @@
-﻿(function () {
-    "use strict";
+﻿(function() {
+	"use strict";
 
 	// Bug #43683 seems caused by design problm of ui-grid.
 	// DOM updating of grid menu items was block by something in IE.
 	// I did not find root cause of this problem, but this modification works :-p
 	// -- by xinfli
 	angular.module("ui.grid").decorator("uiGridGridMenuService", ["$delegate", "$timeout", "uiGridConstants",
-		function ($delegate, $timeout, uiGridConstants) {
+		function($delegate, $timeout, uiGridConstants) {
 			$delegate.toggleColumnVisibility = function(gridCol) {
 				gridCol.colDef.visible = !(gridCol.colDef.visible === true || angular.isUndefined(gridCol.colDef.visible));
 
@@ -19,4 +19,37 @@
 			return $delegate;
 		}
 	]);
+})();
+
+
+
+(function() {
+	'use strict';
+	angular.module('ui.grid').decorator('uiGridColumnMenuService', ['$delegate', 'i18nService', 'uiGridConstants', 'gridUtil', function($delegate, i18nService, uiGridConstants, gridUtil) {
+		console.log($delegate, '$delegate');
+		$delegate.getColumnElementPosition = function($scope, column, $columnElement) {
+			var positionData = {};
+			positionData.left = $columnElement[0].offsetLeft;
+			positionData.top = $columnElement[0].offsetTop;
+			positionData.parentLeft = $columnElement[0].offsetParent.offsetLeft;
+
+			// Get the grid scrollLeft
+			positionData.offset = 0;
+			if (column.grid.options.offsetLeft) {
+				positionData.offset = column.grid.options.offsetLeft;
+			}
+
+			//positionData.height = gridUtil.elementHeight($columnElement, true);
+			//positionData.width = gridUtil.elementWidth($columnElement, true);
+			positionData.width = $columnElement[0].clientWidth;
+			positionData.height = $columnElement[0].clientHeight;
+			if (/MSIE/.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+				positionData.width = $columnElement[0].clientWidth - 20;
+			}
+
+			return positionData;
+		};
+
+		return $delegate;
+  }]);
 })();
