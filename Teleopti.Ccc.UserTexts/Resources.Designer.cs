a65f1e192422cf0f2e6270017638584fc2ea1cf7
junwen.luo <none@none>
2017-09-29 15:04:19 +0800

    #46125 add error message resource missing overtime type

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -14458,7 +14458,7 @@ namespace Teleopti.Ccc.UserTexts {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Individual Flexible.
+        ///   Looks up a localized string similar to Flexible.
         /// </summary>
         public static string IndividualFlexible {
             get {
@@ -17596,6 +17596,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string MissingForecastFrom {
             get {
                 return ResourceManager.GetString("MissingForecastFrom", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Missing Overtime Type.
+        /// </summary>
+        public static string MissingOvertimeType {
+            get {
+                return ResourceManager.GetString("MissingOvertimeType", resourceCulture);
             }
         }
         
