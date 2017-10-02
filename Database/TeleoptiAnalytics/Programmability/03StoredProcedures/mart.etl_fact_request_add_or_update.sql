9202f3abc9865dcec1367178593677347b679c9a
Jonas Nordh <jonas.nordh@teleopti.com>
2017-10-02 12:41:52 +0200

    Bug #46081: Trying to fix Violation of PK constraint 'PK_fact_request'

--- Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_fact_request_add_or_update.sql
+++ Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_fact_request_add_or_update.sql
@@ -22,54 +22,97 @@ CREATE PROCEDURE [mart].[etl_fact_request_add_or_update]
       ,@request_endtime smalldatetime
       ,@requested_time_m int
 AS
-declare @rows int
 
-INSERT INTO mart.fact_request
-SELECT 
-	@request_code
-	,@person_id
-	,@request_start_date_id
-	,@application_datetime
-	,@request_startdate
-	,@request_enddate
-	,@request_type_id
-	,@request_status_id
-	,@request_day_count
-	,@request_start_date_count
-	,@business_unit_id
+MERGE mart.fact_request AS target  
+    USING (SELECT 
+				@request_code
+				,@person_id
+				,@request_start_date_id
+				,@application_datetime
+				,@request_startdate
+				,@request_enddate
+				,@request_type_id
+				,@request_status_id
+				,@request_day_count
+				,@request_start_date_count
+				,@business_unit_id
+				,@datasource_update_date
+				,@absence_id
+				,@request_starttime
+				,@request_endtime
+				,@requested_time_m)	AS src (request_code,
+											person_id,
+											request_start_date_id,
+											application_datetime,
+											request_startdate,
+											request_enddate,
+											request_type_id,
+											request_status_id,
+											request_day_count,
+											request_start_date_count,
+											business_unit_id,
+											datasource_update_date,
+											absence_id,
+											request_starttime,
+											request_endtime,
+											requested_time_m)
+		ON (target.request_code = src.request_code)  
+    WHEN MATCHED THEN   
+        UPDATE set target.person_id =src.person_id
+		,request_start_date_id = src.request_start_date_id
+		,application_datetime = src.application_datetime
+		,request_startdate = src.request_startdate
+		,request_enddate = src.request_enddate
+		,request_type_id = src.request_type_id
+		,request_status_id = src.request_status_id
+		,request_day_count = src.request_day_count
+		,request_start_date_count = src.request_start_date_count
+		,business_unit_id = src.business_unit_id
+		,datasource_id = 1
+		,update_date = GETUTCDATE()
+		,datasource_update_date = src.datasource_update_date
+		,absence_id = src.absence_id
+		,request_starttime = src.request_starttime
+		,request_endtime = src.request_endtime
+		,requested_time_m = src.requested_time_m
+WHEN NOT MATCHED THEN  
+    INSERT (request_code ,
+	person_id,
+	request_start_date_id,
+	application_datetime,
+	request_startdate,
+	request_enddate,
+	request_type_id,
+	request_status_id,
+	request_day_count,
+	request_start_date_count,
+	business_unit_id,
+	datasource_id,
+	insert_date,
+	update_date,
+	datasource_update_date,
+	absence_id,
+	request_starttime,
+	request_endtime,
+	requested_time_m)
+    VALUES (src.request_code
+	,src.person_id
+	,src.request_start_date_id
+	,src.application_datetime
+	,src.request_startdate
+	,src.request_enddate
+	,src.request_type_id
+	,src.request_status_id
+	,src.request_day_count
+	,src.request_start_date_count
+	,src.business_unit_id
 	,1
 	,GETUTCDATE()
 	,GETUTCDATE()
-	,@datasource_update_date
-	,@absence_id
-	,@request_starttime
-	,@request_endtime
-	,@requested_time_m
-WHERE NOT EXISTS(SELECT 1 FROM mart.fact_request WHERE request_code=@request_code)
+	,src.datasource_update_date
+	,src.absence_id
+	,src.request_starttime
+	,src.request_endtime
+	,src.requested_time_m);
 
-SET @rows = (SELECT @@ROWCOUNT)
-IF @rows = 0
-BEGIN
-	UPDATE mart.fact_request
-	SET
-		 person_id = @person_id
-		,request_start_date_id = @request_start_date_id
-		,application_datetime = @application_datetime
-		,request_startdate = @request_startdate
-		,request_enddate = @request_enddate
-		,request_type_id = @request_type_id
-		,request_status_id = @request_status_id
-		,request_day_count = @request_day_count
-		,request_start_date_count = @request_start_date_count
-		,business_unit_id = @business_unit_id
-		,datasource_id = 1
-		,update_date = GETUTCDATE()
-		,datasource_update_date = @datasource_update_date
-		,absence_id = @absence_id
-		,request_starttime = @request_starttime
-		,request_endtime = @request_endtime
-		,requested_time_m = @requested_time_m
-	WHERE
-	request_code = @request_code
-END
 GO
