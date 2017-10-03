34369814b3905b33723d2375ea283428aa54192b
anderss <none@none>
2017-10-03 17:43:31 +0200

    Fix JS build error

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/rta.state.provider.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/rta.state.provider.js
@@ -21,16 +21,16 @@ angular.module('wfm.rta').provider('RtaState', function() {
             })
             .state('rta', {
                 url: '/rta/?siteIds&teamIds&skillIds&skillAreaId&open',
-                params: {siteIds: {array: true}, teamIds: {array: true}, skillIds: {array: true}, es: {array: true}},
-						siteIds: {array: true},
-						teamIds: {array: true},
-						skillIds: {array: true},
-						es: {array: true}
-					},
+                params: {
+                    siteIds: {array: true},
+                    teamIds: {array: true},
+                    skillIds: {array: true},
+                    es: {array: true}
+                },
                 templateUrl: function() {
                     if (toggles.RTA_RememberMyPartOfTheBusiness_39082)
-                        return 'app/rta/overview/view.RTA_RememberMyPartOfTheBusiness_39082.html';
-                    else return 'app/rta/overview/view.html';
+                        return 'app/rta/rta/overview/view.RTA_RememberMyPartOfTheBusiness_39082.html';
+                    else return 'app/rta/rta/overview/view.html';
                 },
                 controllerProvider: function() {
                     if (toggles.RTA_RememberMyPartOfTheBusiness_39082) return 'RtaOverviewController39082 as vm';
@@ -49,22 +49,22 @@ angular.module('wfm.rta').provider('RtaState', function() {
             })
             .state('rta-agents', {
                 url: '/rta/agents/?siteIds&teamIds&skillIds&skillAreaId&es&showAllAgents',
-                params: {siteIds: {array: true}, teamIds: {array: true}, skillIds: {array: true}, es: {array: true}},
-						siteIds: {array: true},
-						teamIds: {array: true},
-						skillIds: {array: true},
-						es: {array: true}
-					},
+                params: {
+                    siteIds: {array: true},
+                    teamIds: {array: true},
+                    skillIds: {array: true},
+                    es: {array: true}
+                },
                 templateUrl: function() {
                     if (toggles.RTA_RememberMyPartOfTheBusiness_39082)
-                        return 'app/rta/agents/rta-agents.RTA_RememberMyPartOfTheBusiness_39082.html';
-                    else return 'app/rta/agents/rta-agents.html';
+                        return 'app/rta/rta/agents/rta-agents.RTA_RememberMyPartOfTheBusiness_39082.html';
+                    else return 'app/rta/rta/agents/rta-agents.html';
                 },
                 controller: 'RtaAgentsController as vm'
             })
             .state('rta-historical', {
                 url: '/rta/agent-historical/:personId?open',
-                templateUrl: 'app/rta/historical/rta-historical.html',
+                templateUrl: 'app/rta/rta/historical/rta-historical.html',
                 controller: 'RtaHistoricalController as vm'
             })
             .state('rta-teams-legacy', {
@@ -72,6 +72,18 @@ angular.module('wfm.rta').provider('RtaState', function() {
                 controller: function($state, $stateParams) {
                     $state.go('rta', $stateParams);
                 }
+            })
+            .state('rta-skill-group-config', {
+                url: '/rta/skill-group-config',
+                templateUrl: 'app/global/skill-group/skillgroup.html',
+                params: {returnState: 'rta'},
+                controller: 'SkillGroupController as vm'
+            })
+            .state('rta-filter-skill-group-config', {
+                url: '/rta/agents/skill-group-config',
+                templateUrl: 'app/global/skill-group/skillgroup.html',
+                params: {returnState: 'rta-agents'},
+                controller: 'SkillGroupController as vm'
             });
     };
 });
