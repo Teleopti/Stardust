0950887b351aa1f731ade79c4267a9e0c0ec6d06
Justin! <justin@teleopti.com>
2017-10-03 15:11:22 +0200

    #45965 Bound view

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/tracer/rtaTracer.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/tracer/rtaTracer.controller.js
@@ -50,15 +50,6 @@
 		}, 1000).start();
 		$scope.$on('$destroy', poller.destroy);
 
-		vm.process1 = "box1:487";
-		vm.process2 = "box1:1476";
-		vm.process3 = "box2:1254";
-		vm.process4 = "box2:157";
-
-		vm.usercode1 = "usercode34";
-		vm.name1 = "Ashley Andeen";
-		vm.usercode2 = "usercode2";
-
 	}
 
 })();
