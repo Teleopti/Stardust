701ae493dbef771f96ffeb4a7d9d810ff9d0245d
Justin! <justin@teleopti.com>
2017-10-03 13:35:54 +0200

    #45965 Dummy backend controller

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaTracerController.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using System.Web.Http;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.Security.AuthorizationData;
+using Teleopti.Ccc.Web.Filters;
+
+namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
+{
+	[ApplicationFunctionApi(DefinedRaptorApplicationFunctionPaths.All)]
+	public class RtaTracerController : ApiController
+	{
+		[UnitOfWork, HttpGet, Route("RtaTracer/Tracer/Traces")]
+		public virtual IHttpActionResult Traces()
+		{
+			return Ok(new RtaTracerViewModel
+			{
+				Tracers = new[]
+				{
+					new RtaTracerTracerViewModel
+					{
+						Process = "box1:487",
+						DataReceivedAt = "",
+						ActivityCheckAt = "10:05:01",
+						Tracing = "usercode34, Ashley Andeen"
+					},
+					new RtaTracerTracerViewModel
+					{
+						Process = "box1:1476",
+						DataReceivedAt = "10:05:56",
+						ActivityCheckAt = "10:06:01",
+						Tracing = "usercode34, Ashley Andeen"
+					}
+				},
+				UserCodeTraces = new[]
+				{
+					new RtaTracerUserCodeTracesViewModel
+					{
+						Header = "usercode34, Ashley Andeen",
+						Traces = new[]
+						{
+							new Trace
+							{
+								StateCode = "AUX34",
+								Lines = new[]
+								{
+									"Received",
+									"Processing",
+									"Processed",
+									"PersonStateChangeEvent"
+								}
+							},
+							new Trace
+							{
+								StateCode = "AUX34",
+								Lines = new[]
+								{
+									"Received",
+									"Processing",
+									"No change"
+								}
+							},
+							new Trace
+							{
+								StateCode = "AUX50",
+								Lines = new[]
+								{
+									"Received",
+									"Processing",
+									"Processed",
+									"PersonStateChangeEvent"
+								}
+							}
+						}
+					}
+				}
+			});
+		}
+
+		[UnitOfWork, HttpGet, Route("RtaTracer/Tracer/Trace")]
+		public virtual IHttpActionResult Trace()
+		{
+			return Ok();
+		}
+
+		[UnitOfWork, HttpGet, Route("RtaTracer/Tracer/Stop")]
+		public virtual IHttpActionResult Stop()
+		{
+			return Ok();
+		}
+
+		[UnitOfWork, HttpGet, Route("RtaTracer/Tracer/Clear")]
+		public virtual IHttpActionResult Clear()
+		{
+			return Ok();
+		}
+	}
+
+	public class RtaTracerViewModel
+	{
+		public IEnumerable<RtaTracerTracerViewModel> Tracers;
+		public IEnumerable<RtaTracerUserCodeTracesViewModel> UserCodeTraces;
+	}
+
+	public class RtaTracerUserCodeTracesViewModel
+	{
+		public string Header;
+		public IEnumerable<Trace> Traces;
+	}
+
+	public class RtaTracerTracerViewModel
+	{
+		public string Process;
+		public string DataReceivedAt;
+		public string ActivityCheckAt;
+		public string Tracing;
+	}
+
+	public class Trace
+	{
+		public string StateCode;
+		public IEnumerable<string> Lines;
+	}
+}
