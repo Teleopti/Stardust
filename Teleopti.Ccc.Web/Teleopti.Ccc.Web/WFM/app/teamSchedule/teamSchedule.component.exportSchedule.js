f8c8725824f5cb0266c3bc104ce38b1cb2a73268
honglih <none@none>
2017-10-12 14:38:20 +0800

    #45795 create export schedule component

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.component.exportSchedule.js
@@ -0,0 +1,31 @@
+﻿(function (angular) {
+	'use strict';
+	angular.module('wfm.teamSchedule').component('teamsExportSchedule', {
+		controller: TeamsExportScheduleCtrl,
+		templateUrl: 'app/teamSchedule/html/exportSchedule.html',
+		controllerAs: 'vm'
+	});
+
+	TeamsExportScheduleCtrl.$inject = ['$q', 'groupPageService'];
+	function TeamsExportScheduleCtrl($q, groupPageService) {
+		var vm = this;
+
+		vm.availableGroups = {BusinessHierarchy:[],GroupPages:[]};
+		vm.selectedGroups = {
+			mode: 'BusinessHierarchy',
+			groupIds: [],
+			groupPageId: ''
+		};
+
+		vm.getGroupPagesAsync = function (date) {
+			var dateStr = moment(date).format('YYYY-MM-DD');
+			groupPageService.fetchAvailableGroupPages(dateStr, dateStr).then(function (data) {
+				vm.availableGroups = data;
+			});
+		};
+		
+		vm.$onInit = function () {
+			vm.getGroupPagesAsync(new Date());
+		}
+	}
+})(angular);
