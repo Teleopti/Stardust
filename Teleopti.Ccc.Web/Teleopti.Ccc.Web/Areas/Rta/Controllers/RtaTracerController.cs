789cca04980b64ee860640cdc9454845c66da19b
Justin! <justin@teleopti.com>
2017-10-12 13:55:32 +0200

    #45597 Api controller

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaTracerController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/RtaTracerController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Web.Http;
 using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.ApplicationLayer.Rta.Tracer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta.ViewModels;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
 using Teleopti.Ccc.Web.Filters;
@@ -10,86 +11,36 @@ namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 	[ApplicationFunctionApi(DefinedRaptorApplicationFunctionPaths.All)]
 	public class RtaTracerController : ApiController
 	{
-		[UnitOfWork, HttpGet, Route("api/RtaTracer/Traces")]
+		private readonly RtaTracerViewModelBuilder _viewModel;
+		private readonly IRtaTracer _tracer;
+
+		public RtaTracerController(RtaTracerViewModelBuilder viewModel, IRtaTracer tracer)
+		{
+			_viewModel = viewModel;
+			_tracer = tracer;
+		}
+
+		[AnalyticsUnitOfWork, HttpGet, Route("api/RtaTracer/Traces")]
 		public virtual IHttpActionResult Traces()
 		{
-			return Ok(new RtaTracerViewModel
-			{
-				Tracers = new[]
-				{
-					new Tracer
-					{
-						Process = "box1:487",
-						DataReceivedAt = "",
-						ActivityCheckAt = "10:05:01",
-						Tracing = "usercode34, Ashley Andeen"
-					},
-					new Tracer
-					{
-						Process = "box1:1476",
-						DataReceivedAt = "10:05:56",
-						ActivityCheckAt = "10:06:01",
-						Tracing = "usercode34, Ashley Andeen"
-					}
-				},
-				TracedUsers = new[]
-				{
-					new TracedUser
-					{
-						User = "usercode34, Ashley Andeen",
-						States = new[]
-						{
-							new TracedState
-							{
-								StateCode = "AUX34",
-								Traces = new[]
-								{
-									"Received",
-									"Processing",
-									"Processed",
-									"PersonStateChangeEvent"
-								}
-							},
-							new TracedState
-							{
-								StateCode = "AUX34",
-								Traces = new[]
-								{
-									"Received",
-									"Processing",
-									"No change"
-								}
-							},
-							new TracedState
-							{
-								StateCode = "AUX50",
-								Traces = new[]
-								{
-									"Received",
-									"Processing",
-									"Processed",
-									"PersonStateChangeEvent"
-								}
-							}
-						}
-					}
-				}
-			});
+			return Ok(_viewModel.Build());
 		}
 
-		[UnitOfWork, HttpGet, Route("api/RtaTracer/Trace")]
-		public virtual IHttpActionResult Trace()
+		[HttpGet, Route("api/RtaTracer/Trace")]
+		public virtual IHttpActionResult Trace(string userCode)
 		{
+			_tracer.Trace(userCode);
 			return Ok();
 		}
 
-		[UnitOfWork, HttpGet, Route("api/RtaTracer/Stop")]
+		[HttpGet, Route("api/RtaTracer/Stop")]
 		public virtual IHttpActionResult Stop()
 		{
+			_tracer.Stop();
 			return Ok();
 		}
 
-		[UnitOfWork, HttpGet, Route("api/RtaTracer/Clear")]
+		[HttpGet, Route("api/RtaTracer/Clear")]
 		public virtual IHttpActionResult Clear()
 		{
 			return Ok();
