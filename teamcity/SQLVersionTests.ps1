a2ec65b0f14ab6f65dc1fbcd37d32470113f01f7
jimmyb <none@none>
2017-10-26 14:55:04 +0200

    Using retry policy instead

--- teamcity/SQLVersionTests.ps1
+++ teamcity/SQLVersionTests.ps1
@@ -119,7 +119,7 @@ function global:CheckEditionAndVersion ()
 
     Write-Output "$(Get-Date -f $timeStampFormat) - Checking SQL Version & Edition on [$global:SQLServerInstance]"		
 	
-    $CheckVersion = ExecuteSQLQuery $ConnectionString $query
+    $CheckVersion = RunAndRetryNonQuery $ConnectionString $query
 
     $global:SQLVersion = $CheckVersion.Column1
 
