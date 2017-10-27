a5bfd877d96a76bc1102dea884619b038ed219ed
Xinfeng Li <xinfeng.li@teleopti.com>
2017-10-27 09:39:01 +0800

    Remove unused namespace reference to fix warning "Type parameter 'T' hides class 'T'"

--- Domain/ApplicationLayer/Rta/Tracer/RtaTracer.cs
+++ Domain/ApplicationLayer/Rta/Tracer/RtaTracer.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using NPOI.SS.Formula.Functions;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta.Service;
 using Teleopti.Ccc.Domain.Collection;
