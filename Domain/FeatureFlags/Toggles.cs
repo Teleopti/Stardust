73161ade40384038b22de3645643343386d6d372
Xinfeng Li <xinfeng.li@teleopti.com>
2017-10-30 09:13:41 +0800

    PBI #45003: Add toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -193,7 +193,8 @@
 		WFM_Unified_Skill_Group_Management_45417,
 		WFM_Modify_Skill_Groups_45727,
 		WFM_AuditTrail_44006,
-		Report_Show_Utc_In_Report_Selection_When_In_Use_45079
+		Report_Show_Utc_In_Report_Selection_When_In_Use_45079,
+		WFM_Gamification_Setting_With_External_Quality_Values_45003
 		// ReSharper restore InconsistentNaming
 	}
 }
