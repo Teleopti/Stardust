7e7d2257de228207fdd79832d885114161d07427
Robin Karlsson <robin.karlsson@teleopti.com>
2017-10-31 16:33:42 +0100

    No need to convert to iso string date, when using datetime directly

--- Database/TeleoptiCCC7/Programmability/03StoredProcedures/dbo.PersonInTeams.sql
+++ Database/TeleoptiCCC7/Programmability/03StoredProcedures/dbo.PersonInTeams.sql
@@ -12,24 +12,17 @@ BEGIN
 
 SET NOCOUNT ON;
 
-
 CREATE TABLE #TeamIds (tId uniqueidentifier NOT NULL)
 
-DECLARE @start_date_ISO nvarchar(10)
-DECLARE @end_date_ISO nvarchar(10)
-
-SELECT @start_date_ISO = CONVERT(NVARCHAR(10), @start_date,120)
-SELECT @end_date_ISO = CONVERT(NVARCHAR(10), @end_date,120)
-    
 INSERT INTO #TeamIds SELECT * FROM dbo.SplitStringString(@team_ids)
 
 SELECT DISTINCT p.Id FROM #TeamIds t 
 INNER JOIN dbo.PersonPeriod pp with (nolock) ON t.tId = pp.Team 
 INNER JOIN dbo.Person p with (nolock) ON pp.Parent = p.Id 
-WHERE ISNULL(p.TerminalDate, '2100-01-01') >=  @start_date_ISO  
+WHERE ISNULL(p.TerminalDate, '2100-01-01') >= @start_date 
 AND p.IsDeleted = 0
-AND (pp.StartDate IS NULL OR pp.StartDate <=  @start_date_ISO  ) 
-AND pp.StartDate <= @end_date_ISO AND (pp.EndDate IS NULL OR EndDate >= @start_date_ISO)
+AND (pp.StartDate IS NULL OR pp.StartDate <= @start_date) 
+AND pp.StartDate <= @end_date AND (pp.EndDate IS NULL OR EndDate >= @start_date)
 
 END
 GO
