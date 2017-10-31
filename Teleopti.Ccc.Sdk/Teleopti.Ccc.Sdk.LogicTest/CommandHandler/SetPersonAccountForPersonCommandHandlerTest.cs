a86ca2d9d80a413a5660ebaf190646a8ace80eab
Robin Karlsson <robin.karlsson@teleopti.com>
2017-10-31 14:03:51 +0100

    One more fake usage removed

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/CommandHandler/SetPersonAccountForPersonCommandHandlerTest.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/CommandHandler/SetPersonAccountForPersonCommandHandlerTest.cs
@@ -2,75 +2,62 @@
 using System.ServiceModel;
 using NUnit.Framework;
 using SharpTestsEx;
-using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.PersonalAccount;
 using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.Domain.Tracking;
+using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.Sdk.Common.DataTransferObject;
 using Teleopti.Ccc.Sdk.Common.DataTransferObject.Commands;
 using Teleopti.Ccc.Sdk.Logic.CommandHandler;
+using Teleopti.Ccc.Sdk.WcfHost.Ioc;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 {
     [TestFixture]
-    public class SetPersonAccountForPersonCommandHandlerTest
+	[DomainTest]
+    public class SetPersonAccountForPersonCommandHandlerTest : ISetup
     {
-        private FakePersonRepository _personRepository;
-        private FakePersonAbsenceAccountRepository _personAbsenceAccountRepository;
-        private FakeAbsenceRepository _absenceRepository;
-        private SetPersonAccountForPersonCommandHandler _target;
-        private IPerson _person;
-        private IAbsence _absence;
+        public FakePersonRepository PersonRepository;
+        public FakePersonAbsenceAccountRepository PersonAbsenceAccountRepository;
+        public FakeAbsenceRepository AbsenceRepository;
+        public SetPersonAccountForPersonCommandHandler Target;
+		
         private static readonly DateTime _startDate = new DateTime(2012, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         private readonly DateOnly _dateOnly = new DateOnly(_startDate);
 		private readonly DateOnlyDto _dateOnlydto = new DateOnlyDto { DateTime = _startDate.Date };
-        private SetPersonAccountForPersonCommandDto _setPersonAccountForPersonCommandDto;
-        private ICurrentUnitOfWorkFactory _currentUnitOfWorkFactory;
-        private ITraceableRefreshService _tracesableRefreshService;
-
-        [SetUp]
-        public void Setup()
-        {
-            _personRepository = new FakePersonRepository(new FakeStorage());
-            _personAbsenceAccountRepository = new FakePersonAbsenceAccountRepository();
-            _absenceRepository = new FakeAbsenceRepository();
-            _currentUnitOfWorkFactory = new FakeCurrentUnitOfWorkFactory(new FakeStorage());
-            _tracesableRefreshService = new TraceableRefreshService(new FakeCurrentScenario_DoNotUse(), new FakeScheduleStorage_DoNotUse());
-            _target = new SetPersonAccountForPersonCommandHandler(_personRepository,_personAbsenceAccountRepository,_absenceRepository,_currentUnitOfWorkFactory,_tracesableRefreshService);
-
-            _person = PersonFactory.CreatePerson("test").WithId();
-            _personRepository.Add(_person);
-
-            _absence = AbsenceFactory.CreateAbsence("test absence").WithId();
-            _absenceRepository.Add(_absence);
-
-            _setPersonAccountForPersonCommandDto = new SetPersonAccountForPersonCommandDto
-                                                       {
-                                                           PersonId = _person.Id.GetValueOrDefault(),
-                                                           AbsenceId = _absence.Id.GetValueOrDefault(),
-                                                           DateFrom = _dateOnlydto,
-                                                           Accrued = TimeSpan.FromMinutes(10).Ticks,
-                                                           BalanceIn = TimeSpan.FromMinutes(11).Ticks,
-                                                           Extra = TimeSpan.FromMinutes(12).Ticks
-                                                       };
-        }
-
+        
 	    [Test]
 	    public void ShouldSetPersonAccountForPerson()
 	    {
-		    var personAbsenceAccount = new PersonAbsenceAccount(_person, _absence);
+			var person = PersonFactory.CreatePerson("test").WithId();
+			PersonRepository.Add(person);
+
+			var absence = AbsenceFactory.CreateAbsence("test absence").WithId();
+			AbsenceRepository.Add(absence);
+
+			var setPersonAccountForPersonCommandDto = new SetPersonAccountForPersonCommandDto
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				AbsenceId = absence.Id.GetValueOrDefault(),
+				DateFrom = _dateOnlydto,
+				Accrued = TimeSpan.FromMinutes(10).Ticks,
+				BalanceIn = TimeSpan.FromMinutes(11).Ticks,
+				Extra = TimeSpan.FromMinutes(12).Ticks
+			};
+
+			var personAbsenceAccount = new PersonAbsenceAccount(person, absence);
 		    var accountDay = new AccountDay(_dateOnly);
 		    personAbsenceAccount.Add(accountDay);
-		    _personAbsenceAccountRepository.Add(personAbsenceAccount);
+		    PersonAbsenceAccountRepository.Add(personAbsenceAccount);
 
-		    _target.Handle(_setPersonAccountForPersonCommandDto);
+		    Target.Handle(setPersonAccountForPersonCommandDto);
 
 		    accountDay.Accrued.Should().Be.EqualTo(TimeSpan.FromMinutes(10));
 		    accountDay.BalanceIn.Should().Be.EqualTo(TimeSpan.FromMinutes(11));
@@ -79,30 +66,85 @@ namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 
 	    [Test]
 	    public void ShouldThrowExceptionIfPersonDoesNotFound()
-	    {
-		    _personRepository.Remove(_person);
-		    Assert.Throws<FaultException>(() => _target.Handle(_setPersonAccountForPersonCommandDto));
+		{
+			var absence = AbsenceFactory.CreateAbsence("test absence").WithId();
+			AbsenceRepository.Add(absence);
+
+			var setPersonAccountForPersonCommandDto = new SetPersonAccountForPersonCommandDto
+			{
+				PersonId = Guid.NewGuid(),
+				AbsenceId = absence.Id.GetValueOrDefault(),
+				DateFrom = _dateOnlydto,
+				Accrued = TimeSpan.FromMinutes(10).Ticks,
+				BalanceIn = TimeSpan.FromMinutes(11).Ticks,
+				Extra = TimeSpan.FromMinutes(12).Ticks
+			};
+			
+		    Assert.Throws<FaultException>(() => Target.Handle(setPersonAccountForPersonCommandDto));
 	    }
 
 	    [Test]
 	    public void ShouldThrowExceptionIfAbsenceDoesNotFound()
-	    {
-		    _absenceRepository.Remove(_absence);
-		    Assert.Throws<FaultException>(() => _target.Handle(_setPersonAccountForPersonCommandDto));
+		{
+			var person = PersonFactory.CreatePerson("test").WithId();
+			PersonRepository.Add(person);
+			
+			var setPersonAccountForPersonCommandDto = new SetPersonAccountForPersonCommandDto
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				AbsenceId = Guid.NewGuid(),
+				DateFrom = _dateOnlydto,
+				Accrued = TimeSpan.FromMinutes(10).Ticks,
+				BalanceIn = TimeSpan.FromMinutes(11).Ticks,
+				Extra = TimeSpan.FromMinutes(12).Ticks
+			};
+			
+		    Assert.Throws<FaultException>(() => Target.Handle(setPersonAccountForPersonCommandDto));
 	    }
 
 	    [Test]
 	    public void ShouldThrowExceptionIfNotPermitted()
-	    {
-		    using (CurrentAuthorization.ThreadlyUse(new NoPermission()))
+		{
+			var person = PersonFactory.CreatePerson("test").WithId();
+			PersonRepository.Add(person);
+
+			var absence = AbsenceFactory.CreateAbsence("test absence").WithId();
+			AbsenceRepository.Add(absence);
+
+			var setPersonAccountForPersonCommandDto = new SetPersonAccountForPersonCommandDto
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				AbsenceId = absence.Id.GetValueOrDefault(),
+				DateFrom = _dateOnlydto,
+				Accrued = TimeSpan.FromMinutes(10).Ticks,
+				BalanceIn = TimeSpan.FromMinutes(11).Ticks,
+				Extra = TimeSpan.FromMinutes(12).Ticks
+			};
+
+			using (CurrentAuthorization.ThreadlyUse(new NoPermission()))
 		    {
-			    Assert.Throws<FaultException>(() => _target.Handle(_setPersonAccountForPersonCommandDto));
+			    Assert.Throws<FaultException>(() => Target.Handle(setPersonAccountForPersonCommandDto));
 		    }
 	    }
-    }
 
-	public class Bug44502Test
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.AddService<SetPersonAccountForPersonCommandHandler>();
+			system.AddModule(new AssemblerModule());
+		}
+	}
+
+	[DomainTest]
+	public class Bug44502Test : ISetup
 	{
+		public FakePersonRepository PersonRepository;
+		public FakePersonAbsenceAccountRepository PersonAbsenceAccountRepository;
+		public FakeAbsenceRepository AbsenceRepository;
+		public FakePersonAbsenceRepository PersonAbsenceRepository;
+		public FakePersonAssignmentRepository PersonAssignmentRepository;
+		public FakeScenarioRepository ScenarioRepository;
+		public SetPersonAccountForPersonCommandHandler Target;
+	
 		[Test]
 		public void ShouldNotHappen()
 		{
@@ -112,16 +154,9 @@ namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 			DateOnlyDto dateOnlydto2 = new DateOnlyDto { DateTime = startDate.Date };
 			DateOnlyDto dateOnlydto3 = new DateOnlyDto { DateTime = startDate.Date.AddDays(10) };
 
-			var personRepository = new FakePersonRepository(new FakeStorage());
-			var personAbsenceAccountRepository = new FakePersonAbsenceAccountRepository();
-			var absenceRepository = new FakeAbsenceRepository();
-			var currentUnitOfWorkFactory = new FakeCurrentUnitOfWorkFactory(new FakeStorage());
-			var currentScenario = new FakeCurrentScenario_DoNotUse();
-			var scheduleStorage = new FakeScheduleStorage_DoNotUse();
-			var tracesableRefreshService = new TraceableRefreshService(currentScenario, scheduleStorage);
-			
+			var scenario = ScenarioRepository.Has("Default");
 			var person = PersonFactory.CreatePerson("test").WithId();
-			personRepository.Add(person);
+			PersonRepository.Add(person);
 
 			var absence = AbsenceFactory.CreateAbsence("Holiday").WithId();
 			absence.Priority = 100;
@@ -131,32 +166,31 @@ namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 			absenceSecond.Priority = 100;
 			absenceSecond.InContractTime = true;
 
-			absenceRepository.Add(absence);
-			absenceRepository.Add(absenceSecond);
+			AbsenceRepository.Add(absence);
+			AbsenceRepository.Add(absenceSecond);
 
-			var assignment = new PersonAssignment(person, currentScenario.Current(), dateOnly);
+			var assignment = new PersonAssignment(person, scenario, dateOnly);
 			var activity = ActivityFactory.CreateActivity("Phone");
 			assignment.AddActivity(activity,new DateTimePeriod(startDate.AddHours(8),startDate.AddHours(17)));
-			scheduleStorage.Add(assignment);
+			PersonAssignmentRepository.Add(assignment);
 
-			var assignment2 = new PersonAssignment(person, currentScenario.Current(), dateOnly.AddDays(1));
+			var assignment2 = new PersonAssignment(person, scenario, dateOnly.AddDays(1));
 			assignment2.AddActivity(activity, new DateTimePeriod(startDate.AddDays(1).AddHours(8), startDate.AddDays(1).AddHours(17)));
-			scheduleStorage.Add(assignment2);
+			PersonAssignmentRepository.Add(assignment2);
 
-			var firstAbsence = new PersonAbsence(person, currentScenario.Current(),
+			var firstAbsence = new PersonAbsence(person, scenario,
 				new AbsenceLayer(absence, new DateTimePeriod(startDate.AddHours(8), startDate.AddHours(17))));
-			scheduleStorage.Add(firstAbsence);
+			PersonAbsenceRepository.Add(firstAbsence);
 
-			var firstAbsence2 = new PersonAbsence(person, currentScenario.Current(),
+			var firstAbsence2 = new PersonAbsence(person, scenario,
 				new AbsenceLayer(absence, new DateTimePeriod(startDate.AddDays(1).AddHours(8), startDate.AddDays(1).AddHours(17))));
-			scheduleStorage.Add(firstAbsence2);
+			PersonAbsenceRepository.Add(firstAbsence2);
 
-			var secondAbsence = new PersonAbsence(person, currentScenario.Current(),
+			var secondAbsence = new PersonAbsence(person, scenario,
 				new AbsenceLayer(absenceSecond, new DateTimePeriod(startDate.AddHours(8), startDate.AddHours(17))));
-			scheduleStorage.Add(secondAbsence);
+			PersonAbsenceRepository.Add(secondAbsence);
 
-			var target = new SetPersonAccountForPersonCommandHandler(personRepository, personAbsenceAccountRepository, absenceRepository, currentUnitOfWorkFactory, tracesableRefreshService);
-			target.Handle(new SetPersonAccountForPersonCommandDto
+			Target.Handle(new SetPersonAccountForPersonCommandDto
 			{
 				PersonId = person.Id.GetValueOrDefault(),
 				AbsenceId = absence.Id.GetValueOrDefault(),
@@ -165,7 +199,7 @@ namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 				BalanceIn = 0L,
 				Extra = 0L
 			});
-			target.Handle(new SetPersonAccountForPersonCommandDto
+			Target.Handle(new SetPersonAccountForPersonCommandDto
 			{
 				PersonId = person.Id.GetValueOrDefault(),
 				AbsenceId = absence.Id.GetValueOrDefault(),
@@ -174,7 +208,7 @@ namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 				BalanceIn = 0L,
 				Extra = 0L
 			});
-			target.Handle(new SetPersonAccountForPersonCommandDto
+			Target.Handle(new SetPersonAccountForPersonCommandDto
 			{
 				PersonId = person.Id.GetValueOrDefault(),
 				AbsenceId = absence.Id.GetValueOrDefault(),
@@ -184,8 +218,14 @@ namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 				Extra = 0L
 			});
 
-			personAbsenceAccountRepository.Find(person).Find(absence, dateOnly).LatestCalculatedBalance.Should().Be
+			PersonAbsenceAccountRepository.Find(person).Find(absence, dateOnly).LatestCalculatedBalance.Should().Be
 				.EqualTo(TimeSpan.FromHours(9));
+		}
+
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.AddService<SetPersonAccountForPersonCommandHandler>();
+			system.AddModule(new AssemblerModule());
 		}
 	}
 }
