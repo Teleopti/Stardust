3013d8d4b1cfe78f7a927873cefa429b37637ccf
MingdiWu <mingdi.wu@teleopti.com>
2017-11-06 10:08:49 +0800

    minor clean

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Gamification/Models/GamificationSettingViewModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Gamification/Models/GamificationSettingViewModel.cs
@@ -1,12 +1,9 @@
 using System;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-using Teleopti.Ccc.Domain.Security.AuthorizationData;
-using Teleopti.Ccc.Web.Filters;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.Gamification.Models
 {
-	[ApplicationFunctionApi(DefinedRaptorApplicationFunctionPaths.OpenOptionsPage)]
 	public class GamificationSettingViewModel
 	{
 		public Guid? Id { get; set; }
