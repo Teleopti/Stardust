d7bd0b4b31f409052e0dc5f552b4cb948ce3fbda
Rockman <none@none>
2017-11-09 10:28:32 +0100

    #46648 inline js removed

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/index_desktop_client_mods.js
@@ -0,0 +1,35 @@
+function iAmCalledFromFatClient() {
+	removeElements();
+	expandMaterialContainer();
+	changeToClassicTheme();
+};
+
+var removeElements = function() {
+	var elementsToRemove = [
+		document.getElementById('nav'),
+		document.getElementById('leftsidenav'),
+		document.getElementById('notice'),
+		document.getElementById('lefttoogle')
+	];
+
+	elementsToRemove.forEach(function(elmt) {
+		if (elmt) elmt.style.display = 'none';
+	});
+};
+
+var expandMaterialContainer = function() {
+	var materialcontainer = document.getElementById('materialcontainer');
+	if (materialcontainer) {
+		materialcontainer.style["margin-top"] = '0';
+		materialcontainer.style.height = '100vh';
+	}
+};
+
+var changeToClassicTheme = function() {
+	if (angular.element(document.getElementById('darkTheme')).scope() == null || angular.element(document.getElementById('overlay')).scope() == null) {
+		return;
+	} else {
+		angular.element(document.getElementById('darkTheme')).scope().toggleTheme('classic');
+		angular.element(document.getElementById('overlay')).scope().toggleOverlay(false);
+	}
+};
