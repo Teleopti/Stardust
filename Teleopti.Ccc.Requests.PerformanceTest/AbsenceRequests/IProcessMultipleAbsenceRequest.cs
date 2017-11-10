2fdfeb7c8995c23f4aa07135299dd896a7e7e018
Mamanda <none@none>
2017-11-10 14:57:47 -0700

    Fixing red reqeuests performance test: Deleting legacy tests that is already covered in the tuning test project

--- Teleopti.Ccc.Requests.PerformanceTest/AbsenceRequests/IProcessMultipleAbsenceRequest.cs
+++
@@ -1,9 +0,0 @@
-using Teleopti.Ccc.Domain.ApplicationLayer.Events;
-
-namespace Teleopti.Ccc.Requests.PerformanceTest.AbsenceRequests
-{
-	public interface IProcessMultipleAbsenceRequest
-	{
-		void Process(NewMultiAbsenceRequestsCreatedEvent absenceRequests);
-	}
-}
