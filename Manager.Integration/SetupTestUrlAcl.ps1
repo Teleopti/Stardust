f27ce5a7f9c9c732868cb153de66b2d4861a22ba
mwedmark <magnus@wedmark.se>
2017-11-10 14:51:33 +0100

    Added script to open urlacl ports in tests

---
+++ Manager.Integration/SetupTestUrlAcl.ps1
@@ -0,0 +1,47 @@
+﻿$port = 9000
+for($i=0
+     $i -le 6
+     $i++){
+		$global:value = (netsh http show urlacl url=http://+:$port/)
+		if(!($global:value  -like '*http://+:' + $port + '/*'))
+		{
+			$global:value = (netsh http add urlacl url=http://+:$port/ user=Everyone listen=yes)
+			if($global:value  -like '*Error: 5*')
+			{
+				Write-host 'You must run this as administrator to add permissions to listen on ports'
+				start-sleep -seconds 5
+				Exit   
+			}			
+		}
+		$port++
+     }
+
+$port = 9050
+for($i=0
+     $i -le 5
+     $i++){
+		$global:value = (netsh http show urlacl url=http://+:$port/)
+		if(!($global:value  -like '*http://+:' + $port + '/*'))
+		{
+			$global:value = (netsh http add urlacl url=http://+:$port/ user=Everyone listen=yes)
+			if($global:value  -like '*Error: 5*')
+			{
+				Write-host 'You must run this as administrator to add permissions to listen on ports'
+				start-sleep -seconds 5
+				Exit   
+			}			
+		}
+		$port++
+     }
+	 
+$port = 9100
+if(!($global:value  -like '*http://+:' + $port + '/*'))
+{
+	$global:value = (netsh http add urlacl url=http://+:$port/ user=Everyone listen=yes)
+	if($global:value  -like '*Error: 5*')
+	{
+		Write-host 'You must run this as administrator to add permissions to listen on ports'
+		start-sleep -seconds 5
+		Exit   
+	}			
+}
