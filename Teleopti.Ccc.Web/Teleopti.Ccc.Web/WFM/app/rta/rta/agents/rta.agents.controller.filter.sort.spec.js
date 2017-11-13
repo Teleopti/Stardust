886b77813b8c1f1f6f0c481d2ece483e355a3218
LevelUp! <levelup@teleopti.com>
2017-11-13 09:45:53 +0100

    #46475 fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/agents/rta.agents.controller.filter.sort.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/agents/rta.agents.controller.filter.sort.spec.js
@@ -1,5 +1,5 @@
 'use strict';
-describe('RtaAgentsController', function() {
+describe('RtaAgentsController', function () {
 
 	var $interval,
 		$httpBackend,
@@ -15,16 +15,16 @@ describe('RtaAgentsController', function() {
 	beforeEach(module('wfm.rta'));
 	beforeEach(module('wfm.rtaTestShared'));
 
-	beforeEach(function() {
-		module(function($provide) {
-			$provide.factory('$stateParams', function() {
+	beforeEach(function () {
+		module(function ($provide) {
+			$provide.factory('$stateParams', function () {
 				stateParams = {};
 				return stateParams;
 			});
 		});
 	});
 
-	beforeEach(inject(function(_$httpBackend_, _$interval_, _$state_, _$sessionStorage_, _FakeRtaBackend_, _ControllerBuilder_) {
+	beforeEach(inject(function (_$httpBackend_, _$interval_, _$state_, _$sessionStorage_, _FakeRtaBackend_, _ControllerBuilder_) {
 		$interval = _$interval_;
 		$state = _$state_;
 		$sessionStorage = _$sessionStorage_;
@@ -35,61 +35,73 @@ describe('RtaAgentsController', function() {
 		spyOn($state, 'go');
 	}));
 
-	afterEach(function() {
+	afterEach(function () {
 		$fakeBackend.clear();
 		$sessionStorage.$reset();
 	});
 
-	it('should filter text', function() {
+	it('should filter text', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
 		c.apply(function () {
 			vm.filterText = 'Charley';
-		});
-		
+		}).wait(5000);
+
 		expect($fakeBackend.lastAgentStatesRequestParams.textFilter).toBe('Charley');
 	});
 
-	it('should not add textFilter to params when no filter', function() {
-		var c = $controllerBuilder.createController();
-		var vm = c.vm;
-
-		expect($fakeBackend.lastAgentStatesRequestParams.textFilter).toBeUndefined();
-	});
-	
-	it('should not add textFilter to params when removing text filter', function() {
+	it('should not filter text immediately', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
 		c.apply(function () {
 			vm.filterText = 'Charley';
 		});
-		c.apply(function () {
-			vm.filterText = '';
-		});
 
 		expect($fakeBackend.lastAgentStatesRequestParams.textFilter).toBeUndefined();
 	});
 
-	it('should default sort by name ascending', function() {
+	it('should not add textFilter to params when no filter', function () {
+		$controllerBuilder.createController();
+
+		expect($fakeBackend.lastAgentStatesRequestParams.textFilter).toBeUndefined();
+	});
+
+	it('should not add textFilter to params when removing text filter', function () {
+		var c = $controllerBuilder.createController();
+		var vm = c.vm;
+		c
+			.apply(function () {
+				vm.filterText = 'Charley';
+			})
+			.wait(5000)
+			.apply(function () {
+				vm.filterText = '';
+			})
+			.wait(5000);
+
+		expect($fakeBackend.lastAgentStatesRequestParams.textFilter).toBeUndefined();
+	});
+
+	it('should default sort by name ascending', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
 
-		c.apply(function() {
+		c.apply(function () {
 			vm.showInAlarm = false;
 		});
-		
+
 		expect($fakeBackend.lastAgentStatesRequestParams.orderBy).toBe('Name');
 		expect($fakeBackend.lastAgentStatesRequestParams.direction).toBe('asc');
 	});
-	
-	it('should sort by name descending first time', function() {
+
+	it('should sort by name descending first time', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
-		
-		c.apply(function() {
+
+		c.apply(function () {
 			vm.showInAlarm = false;
 		});
-		c.apply(function() {
+		c.apply(function () {
 			vm.sort("Name");
 		});
 
@@ -97,31 +109,31 @@ describe('RtaAgentsController', function() {
 		expect($fakeBackend.lastAgentStatesRequestParams.direction).toBe('desc');
 	});
 
-	it('should sort by name', function() {
+	it('should sort by name', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
 
-		c.apply(function() {
+		c.apply(function () {
 			vm.showInAlarm = false;
 		});
-		c.apply(function() {
+		c.apply(function () {
 			vm.sort("SiteAndTeamName");
 		});
-		c.apply(function() {
+		c.apply(function () {
 			vm.sort("Name");
 		});
 
 		expect($fakeBackend.lastAgentStatesRequestParams.orderBy).toBe('Name');
 		expect($fakeBackend.lastAgentStatesRequestParams.direction).toBe('asc');
 	});
-	
-	it('should sort by site name', function() {
+
+	it('should sort by site name', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
-		c.apply(function() {
+		c.apply(function () {
 			vm.showInAlarm = false;
 		});
-		c.apply(function() {
+		c.apply(function () {
 			vm.sort("SiteAndTeamName");
 		});
 
@@ -129,7 +141,7 @@ describe('RtaAgentsController', function() {
 		expect($fakeBackend.lastAgentStatesRequestParams.direction).toBe('asc');
 	});
 
-	it('should not sort when viewing agents in alarm', function() {
+	it('should not sort when viewing agents in alarm', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
 
@@ -137,7 +149,7 @@ describe('RtaAgentsController', function() {
 		expect($fakeBackend.lastAgentStatesRequestParams.direction).toBeUndefined();
 	});
 
-	it('should not sort when viewing agents in alarm after switching', function() {
+	it('should not sort when viewing agents in alarm after switching', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
 		c.apply(function () {
@@ -146,21 +158,21 @@ describe('RtaAgentsController', function() {
 		c.apply(function () {
 			vm.showInAlarm = true;
 		});
-		
+
 		expect($fakeBackend.lastAgentStatesRequestParams.orderBy).toBeUndefined();
 		expect($fakeBackend.lastAgentStatesRequestParams.direction).toBeUndefined();
 	});
-	
-	it('should switch direction', function() {
+
+	it('should switch direction', function () {
 		var c = $controllerBuilder.createController();
 		var vm = c.vm;
-		c.apply(function() {
+		c.apply(function () {
 			vm.showInAlarm = false;
 		});
-		c.apply(function() {
+		c.apply(function () {
 			vm.sort("State");
 		});
-		c.apply(function() {
+		c.apply(function () {
 			vm.sort("State");
 		});
 
