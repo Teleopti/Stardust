0bc6e2f2e29b3d6b53c6ed85d95f760350db9d94
Jonas Nordh <jonas.nordh@teleopti.com>
2017-11-13 14:29:04 +0100

    Bug #46548 - Fail fast for BudgetDay duplicates, part 3

--- Database/TeleoptiCCC7/Releases/00000905.sql
+++ Database/TeleoptiCCC7/Releases/00000905.sql
@@ -3,11 +3,25 @@ IF OBJECT_ID('dbo.UQ_BudgetDay', 'UQ') IS NULL
 BEGIN
 	-- Delete potential duplicates for budget days
 	WITH cte AS (
-	  SELECT UpdatedOn, [Day], Scenario, BudgetGroup, row_number() OVER(PARTITION BY [Day], Scenario, BudgetGroup 
+	  SELECT Id, UpdatedOn, [Day], Scenario, BudgetGroup, row_number() OVER(PARTITION BY [Day], Scenario, BudgetGroup 
 		ORDER BY UpdatedOn DESC) AS [rn]
 	  FROM BudgetDay
 	)
-	DELETE cte WHERE [rn] > 1
+	SELECT * INTO #duplicates FROM cte WHERE [rn] > 1
+
+	DELETE cesb
+	FROM CustomEfficiencyShrinkageBudget cesb
+	INNER JOIN #duplicates d ON d.Id = cesb.Parent
+	
+	DELETE csb
+	FROM CustomShrinkageBudget csb
+	INNER JOIN #duplicates d ON d.Id = csb.Parent
+
+	DELETE bd
+	FROM BudgetDay bd
+	INNER JOIN #duplicates d ON bd.Id = d.Id
+	
+	DROP TABLE #duplicates
 
 	ALTER TABLE dbo.BudgetDay ADD CONSTRAINT UQ_BudgetDay UNIQUE
 		(
