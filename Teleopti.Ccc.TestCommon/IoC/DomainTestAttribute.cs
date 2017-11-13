81079d3ad8c257bf2108cc1045c274c6352ff8de
Roger Kratz <roger.kratz@teleopti.com>
2017-11-13 16:46:47 +0100

    #46680 Tests not being specific about toggle, now runs with this toggle on.

--- Teleopti.Ccc.TestCommon/IoC/DomainTestAttribute.cs
+++ Teleopti.Ccc.TestCommon/IoC/DomainTestAttribute.cs
@@ -45,8 +45,7 @@ using Teleopti.Ccc.TestCommon.Services;
 namespace Teleopti.Ccc.TestCommon.IoC
 {
 	[Toggle(Domain.FeatureFlags.Toggles.RTA_SpreadTransactionLocksStrategy_41823)]
-	//enable this when feature is done!
-	//[Toggle(Domain.FeatureFlags.Toggles.ResourcePlanner_RemoveImplicitResCalcContext_46680)]
+	[Toggle(Domain.FeatureFlags.Toggles.ResourcePlanner_RemoveImplicitResCalcContext_46680)]
 	public class DomainTestAttribute : IoCTestAttribute
 	{
 		public static string DefaultTenantName = "default";
