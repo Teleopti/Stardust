d6c2494a0d1750ae6c0bfdf21d44d3aa822531a0
Alina Morariu <none@none>
2017-11-16 14:39:48 +0100

    semi select tree 3

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/resourceplanner/tree_data/main.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/resourceplanner/tree_data/main.controller.js
@@ -23,6 +23,14 @@
             RootSelectUnique: true
         }
 
+        vm.optionThree = {
+            NodeDisplayName: "label",
+            NodeChildrenName: "nodes",
+            NodeSelectedMark: "selected",
+            nodeSemiSelected: "semiSelected"
+        }
+
+
         var data = {
             nodes: [
                 {
@@ -171,8 +179,179 @@
             ]
         };
 
+        var dataForSemi = {
+            nodes: [
+                {
+                    label: 'parent1',
+                    id: '1',
+                    selected: false,
+                    semiSelected: false,
+                    nodes: [
+                        {
+                            label: 'child1',
+                            id: '2',
+                            selected: false,
+                            semiSelected: false,
+                            nodes: [
+                                {
+                                    label: 'grandchild1',
+                                    id: '3',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: [
+                                        {
+                                            label: 'child1',
+                                            id: '12',
+                                            selected: false,
+                                            semiSelected: false,
+                                            nodes: [
+                                                {
+                                                    label: 'grandchild1',
+                                                    id: '13',
+                                                    selected: false,
+                                                    semiSelected: false,
+                                                    nodes: []
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            label: 'child1',
+                                            id: '22',
+                                            selected: false,
+                                            semiSelected: false,
+                                            nodes: [
+                                                {
+                                                    label: 'grandchild2',
+                                                    id: '23',
+                                                    selected: false,
+                                                    semiSelected: false,
+                                                    nodes: []
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: 'parent2',
+                    id: '4',
+                    selected: false,
+                    semiSelected: false,
+                    nodes: [
+                        {
+                            label: 'child1',
+                            id: '5',
+                            selected: false,
+                            semiSelected: false,
+                            nodes: [
+                                {
+                                    label: 'grandchild1',
+                                    id: '6',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                }
+                            ]
+                        },
+                        {
+                            label: 'child2',
+                            id: '7',
+                            selected: false,
+                            semiSelected: false,
+                            nodes: [
+                                {
+                                    label: 'grandchild2',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                }
+                            ]
+                        },
+                        {
+                            label: 'child3',
+                            id: '7',
+                            selected: false,
+                            semiSelected: false,
+                            nodes: [
+                                {
+                                    label: 'grandchild1',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                },
+                                {
+                                    label: 'grandchild2',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                },
+                                {
+                                    label: 'grandchild3',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                },
+                                {
+                                    label: 'grandchild4',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                },
+                                {
+                                    label: 'grandchild5',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                }
+                            ]
+                        },
+                        {
+                            label: 'child4',
+                            id: '7',
+                            selected: false,
+                            semiSelected: false,
+                            nodes: [
+                                {
+                                    label: 'grandchild2',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                }
+                            ]
+                        },
+                        {
+                            label: 'child5',
+                            id: '7',
+                            selected: false,
+                            semiSelected: false,
+                            nodes: [
+                                {
+                                    label: 'grandchild2',
+                                    id: '8',
+                                    selected: false,
+                                    semiSelected: false,
+                                    nodes: []
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+
         vm.dataOne = angular.copy(data);
         vm.dataTwo = angular.copy(data);
         vm.dataThree = angular.copy(data);
+        vm.dataFour = angular.copy(dataForSemi);
     }
 })();
