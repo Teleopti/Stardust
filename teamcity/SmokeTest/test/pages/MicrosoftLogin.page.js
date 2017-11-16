0c59b7c8ba58c53582db9880768c21958b1303f6
Kunning Mao <none@none>
2017-11-16 14:29:08 +0100

    make login tests work with new microsoft login page

--- teamcity/SmokeTest/test/pages/MicrosoftLogin.page.js
+++ teamcity/SmokeTest/test/pages/MicrosoftLogin.page.js
@@ -2,7 +2,8 @@ var Page = require('./Page.page')
 
 var MicrosoftLoginPage = Object.create(Page, {
 
-    username: { get: function () { return browser.element('#cred_userid_inputtext'); } },
+    username: { get: function () { return browser.element("input[type='email']"); } },
+    nextbutton: { get: function () { return browser.element("input[type='submit']"); } },
     password: { get: function () { return browser.element('#cred_password_inputtext'); } },
 	password2: { get: function () { return browser.element('#passwordInput'); } },
     signinButton:     { get: function () { return browser.element('#submitButton'); } },
@@ -15,10 +16,11 @@ var MicrosoftLoginPage = Object.create(Page, {
 		console.log('Signing in using Teleopti Azure AD');
 		this.username.waitForVisible(60 * 1000);
 		this.username.setValue('demo@teleopti.com');
-		this.password.setValue('teleoptidemo');
+        this.nextbutton.click();
 		this.password2.waitForVisible(60 * 1000);
 		this.password2.setValue('teleoptidemo');
         this.signinButton.click();
+        this.nextbutton.click();
     } }
 });
 
