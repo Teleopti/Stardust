0416355d9c41552e0369c959589c2224cd0ccb07
Jonas Nordh <jonas.nordh@teleopti.com>
2017-11-22 16:06:32 +0100

    PBI #46880 - check web based etl toggle

---
+++ Teleopti.Wfm.Administration/Controllers/EtlController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Infrastructure.Toggle;
+using Teleopti.Wfm.Administration.Core;
+
+namespace Teleopti.Wfm.Administration.Controllers
+{
+	[TenantTokenAuthentication]
+	public class EtlController : ApiController
+	{
+		private readonly IToggleManager _toggleManager;
+
+		public EtlController(IToggleManager toggleManager)
+		{
+			_toggleManager = toggleManager;
+		}
+		[HttpGet, Route("Etl/ShouldEtlToolBeVisible")]
+		public IHttpActionResult ShouldEtlToolBeVisible()
+		{
+			return Json(_toggleManager.IsEnabled(Toggles.ETL_Show_Web_Based_ETL_Tool_46880));
+		}
+	}
+}
