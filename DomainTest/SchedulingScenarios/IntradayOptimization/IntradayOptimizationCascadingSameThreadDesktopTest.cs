4f5b089fae17d9de64bc0c1700ba16334172d5ce
Roger Kratz <roger.kratz@teleopti.com>
2017-11-23 17:36:22 +0100

    removing dead code in test

--- DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationCascadingSameThreadDesktopTest.cs
+++ DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationCascadingSameThreadDesktopTest.cs
@@ -6,7 +6,6 @@ using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Forecasting;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Optimization;
-using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
@@ -28,9 +27,6 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
 	{
 		public OptimizationDesktopExecuter Target;
 		public Func<ISchedulerStateHolder> SchedulerStateHolderFrom;
-		public CascadingResourceCalculationContextFactory ResourceCalculationContextFactory;
-		public IResourceCalculation ResourceCalculation;
-		public ResourceCalculateWithNewContext ResourceCalculateWithNewContext;
 
 		[Test]
 		public void ShouldNotOverwriteLocalResourceCalculationContextInIsland()
