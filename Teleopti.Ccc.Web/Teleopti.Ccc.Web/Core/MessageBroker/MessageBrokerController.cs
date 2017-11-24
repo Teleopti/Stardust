bdf27ce1b513dba3d0e9d23ad673f46cdbcaeafe
LevelUp! <levelup@teleopti.com>
2017-11-24 13:46:48 +0100

    #46922 Lets not hide all exceptions

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/MessageBroker/MessageBrokerController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Core/MessageBroker/MessageBrokerController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Web.Http;
+using System.Web.Mvc;
 using Teleopti.Ccc.Domain.MessageBroker;
 using Teleopti.Ccc.Domain.MessageBroker.Server;
 
@@ -14,71 +15,22 @@ namespace Teleopti.Ccc.Web.Broker
 			_server = server;
 		}
 
-		[HttpPost, Route("MessageBroker/NotifyClients")]
-		public void NotifyClients([FromBody]Message message)
+		[System.Web.Http.HttpPost, System.Web.Http.Route("MessageBroker/NotifyClients")]
+		public void NotifyClients([FromBody] Message message)
 		{
 			_server.NotifyClients(message);
 		}
 
-		[HttpPost, Route("MessageBroker/NotifyClientsMultiple")]
-		public void NotifyClientsMultiple([FromBody]Message[] notifications)
+		[System.Web.Http.HttpPost, System.Web.Http.Route("MessageBroker/NotifyClientsMultiple")]
+		public void NotifyClientsMultiple([FromBody] Message[] notifications)
 		{
 			_server.NotifyClientsMultiple(notifications);
 		}
 
-		[HttpGet, Route("MessageBroker/PopMessages")]
+		[System.Web.Http.HttpGet, System.Web.Http.Route("MessageBroker/PopMessages")]
 		public IHttpActionResult PopMessages(string route, string id)
 		{
-			try
-			{
-				var messages = _server.PopMessages(route, id);
-				return Ok(messages);
-			}
-			catch (Exception)
-			{
-				return InternalServerError();
-			}
+			return Ok(_server.PopMessages(route, id));
 		}
 	}
-	/*
-	public class MessageBinder : IModelBinder
-	{
-		public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
-		{
-			if (bindingContext.ModelType != typeof(Message))
-			{
-				return false;
-			}
-
-			string DataSource { get; set; }
-		string BusinessUnitId { get; set; }
-
-		string DomainType { get; set; }
-		string DomainQualifiedType { get; set; }
-		string DomainId { get; set; }
-		string ModuleId { get; set; }
-		string DomainReferenceId { get; set; }
-		string EndDate { get; set; }
-		string StartDate { get; set; }
-		int DomainUpdateType { get; set; }
-		string BinaryData { get; set; }
-
-			ValueProviderResult username = bindingContext.ValueProvider.GetValue("username");
-			ValueProviderResult password = bindingContext.ValueProvider.GetValue("password");
-			if (username == null || password == null)
-			{
-				return false;
-			}
-
-			if (username.RawValue == null || password.RawValue == null)
-			{
-				bindingContext.ModelState.AddModelError(
-					bindingContext.ModelName, "Cannot convert value to application authentication");
-				return false;
-			}
-
-			bindingContext.Model = new ApplicationAuthenticationModel { UserName = username.RawValue.ToString(), Password = password.RawValue.ToString() };
-			return true;
-		}
-	}*/
 }
