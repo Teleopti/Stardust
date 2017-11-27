d263203c0b5296b923a95dab647a49436d43547f
honglih <none@none>
2017-11-27 15:45:02 +0800

    PBI #46595, merge ASMScheduleChangeTime sql

--- Database/TeleoptiAnalytics/Releases/00000597.sql
+++ Database/TeleoptiAnalytics/Releases/00000597.sql
@@ -1,9 +1,14 @@
-CREATE TABLE [msg].[ASMScheduleChangeTime](
-	[PersonId] [uniqueidentifier] NOT NULL,
-	[TimeStamp] [datetime] NOT NULL,
-	 CONSTRAINT [PK_ASMScheduleChangeTime] PRIMARY KEY CLUSTERED 
+-- For PBI #46841
+CREATE TABLE [dbo].[ExternalPerformanceData](
+	[ExternalPerformanceId] [int] NOT NULL,
+	[DateFrom] [smalldatetime] NOT NULL,
+	[PersonId] [int] NOT NULL,
+	[OriginalPersonId] [nvarchar](100) NOT NULL,
+	[Score] [real] NULL
+ CONSTRAINT [PK_ExternalPerformanceData] PRIMARY KEY CLUSTERED 
 (
+	[ExternalPerformanceid] ASC,
+	[DateFrom] ASC,
 	[PersonId] ASC
-)
-) ON [MSG]
+)) ON [PRIMARY]
 GO
---
+++ Database/TeleoptiAnalytics/Releases/00000598.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [msg].[ASMScheduleChangeTime](
+	[PersonId] [uniqueidentifier] NOT NULL,
+	[TimeStamp] [datetime] NOT NULL,
+	 CONSTRAINT [PK_ASMScheduleChangeTime] PRIMARY KEY CLUSTERED 
+(
+	[PersonId] ASC
+)
+) ON [MSG]
+GO
