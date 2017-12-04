8aa382df9e99575d6e608a0a2fb95779edf932f4
Robin Karlsson <robin.karlsson@teleopti.com>
2017-12-04 15:12:51 +0100

    Avoid locking tables on budget things

--- Database/TeleoptiCCC7/Programmability/03StoredProcedures/ReadModel.LoadBudgetAllowanceReadModel.sql
+++ Database/TeleoptiCCC7/Programmability/03StoredProcedures/ReadModel.LoadBudgetAllowanceReadModel.sql
@@ -11,6 +11,7 @@ GO
 --				2013-05-24	DeeFlex & ErikS	Improved last change
 --				2014-10-06	Ola				Removed temptable to reduce writes, reads and cpu usage (bug #30425 )
 --				2015-07-21  RobW			Removed slow join to PersonPeriodWithEndDate, instead use PersonPeriod table now that it has enddate
+--				2017-12-04	RobinK			Changed to nolock for joined tables
 -- =============================================
 
 CREATE PROCEDURE [ReadModel].[LoadBudgetAllowanceReadModel]
@@ -29,18 +30,18 @@ BEGIN
 		SUM(sp.ContractTime) as TotalContractTime,
 		Count(*) as HeadCounts
 	FROM ReadModel.ScheduleProjectionReadOnly sp
-	INNER JOIN  dbo.PersonPeriod t	
+	INNER JOIN  dbo.PersonPeriod t with (nolock) 
 		ON t.parent = sp.PersonId
 		AND t.budgetgroup=@BudgetGroupId
 		AND sp.BelongsToDate BETWEEN t.startdate AND t.enddate
 			AND EXISTS (
 			SELECT DISTINCT Absence
-			FROM dbo.budgetabsencecollection bac
-			INNER JOIN dbo.customshrinkage cs
+			FROM dbo.budgetabsencecollection bac with (nolock)
+			INNER JOIN dbo.customshrinkage cs with (nolock)
 				ON cs.Id = bac.CustomShrinkage AND cs.Parent=@BudgetGroupId
 				AND bac.Absence=sp.PayloadId
 				)
-	INNER JOIN Person p ON p.Id = t.Parent AND p.IsDeleted = 0
+	INNER JOIN Person p with (nolock) ON p.Id = t.Parent AND p.IsDeleted = 0
 	WHERE sp.ScenarioId=@ScenarioId
 	AND sp.BelongsToDate BETWEEN @DateFrom AND @DateTo
 
