1c462a53ff1cc394dee1b7dc19b075d7ead48c5d
Robin Karlsson <robin.karlsson@teleopti.com>
2017-12-05 13:55:39 +0100

    Resolving issue #46981

--- Domain/GroupPageCreator/OptionalColumnGroupPage.cs
+++ Domain/GroupPageCreator/OptionalColumnGroupPage.cs
@@ -22,13 +22,15 @@ namespace Teleopti.Ccc.Domain.GroupPageCreator
 			foreach (var person in groupPageOptions.Persons)
 			{
 				var columnValue = person.OptionalColumnValueCollection
-					.FirstOrDefault(y => y.Parent.Id.Value == optionalColumn.Id.Value && y.Description != string.Empty);
+					.FirstOrDefault(y => y.Parent.Id.Value == optionalColumn.Id.Value && !string.IsNullOrEmpty(y.Description.Trim()));
 				if(columnValue == null)
 					continue;
-				if (groups.ContainsKey(columnValue.Description))
-					groups[columnValue.Description].Add(person);
+
+				var key = columnValue.Description.Trim();
+				if (groups.TryGetValue(key,out var list))
+					list.Add(person);
 				else
-					groups.Add(columnValue.Description, new List<IPerson> {person});
+					groups.Add(key, new List<IPerson> {person});
 			}
 
 			foreach (var group in groups)
--- DomainTest/GroupPageCreator/GroupingsCreatorTest.cs
+++ DomainTest/GroupPageCreator/GroupingsCreatorTest.cs
@@ -79,5 +79,38 @@ namespace Teleopti.Ccc.DomainTest.GroupPageCreator
 				Assert.AreEqual(groupPages[7].Description.Name, optionalColumn.Name);
 			}
 		}
+
+		[Test]
+		public void ShouldHandleOptionalColumnValueWithSpace()
+		{
+			var mocks = new MockRepository();
+			var groupingsDataProvider = mocks.StrictMock<IGroupPageDataProvider>();
+			var optionalColumn = new OptionalColumn("MyOptionalColumnAsGroupPage").WithId();
+			var person = new Person().WithId();
+			person.AddOptionalColumnValue(new OptionalColumnValue(" "), optionalColumn);
+
+			using (mocks.Record())
+			{
+				Expect.Call(groupingsDataProvider.BusinessUnit).Return(BusinessUnitFactory.BusinessUnitUsedInTest);
+				Expect.Call(groupingsDataProvider.ContractCollection).Return(new List<IContract>());
+				Expect.Call(groupingsDataProvider.ContractScheduleCollection).Return(new List<IContractSchedule>());
+				Expect.Call(groupingsDataProvider.PartTimePercentageCollection).Return(new List<IPartTimePercentage>());
+				Expect.Call(groupingsDataProvider.PersonCollection)
+					.Return(new List<IPerson> { person }).Repeat.AtLeastOnce();
+				Expect.Call(groupingsDataProvider.RuleSetBagCollection).Return(new List<IRuleSetBag>());
+				Expect.Call(groupingsDataProvider.SelectedPeriod).Return(new DateOnlyPeriod());
+				Expect.Call(groupingsDataProvider.SkillCollection).Return(new List<ISkill>());
+				Expect.Call(groupingsDataProvider.OptionalColumnCollectionAvailableAsGroupPage)
+					.Return(new List<IOptionalColumn> { optionalColumn });
+			}
+			using (mocks.Playback())
+			{
+				var target = new GroupingsCreatorOptionalColumn(groupingsDataProvider);
+
+				IList<IGroupPage> groupPages = target.CreateBuiltInGroupPages(true);
+
+				Assert.AreEqual(7, groupPages.Count);
+			}
+		}
 	}
 }
