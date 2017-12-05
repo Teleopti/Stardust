43f1ccf47ecbf328e70298dbf565c5d11d9427f7
SquidSquad <bharath.muchi@teleopti.com>
2017-12-05 14:27:04 +0100

    PBI #46936 - start of unit test for adminstration project

---
+++ Teleopti.Wfm.AdministrationTest/AdministrationTestAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon.FakeRepositories.Tenant;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Wfm.Administration.Core.Modules;
+
+namespace Teleopti.Wfm.AdministrationTest
+{
+	public class AdministrationTestAttribute : DomainTestAttribute
+	{
+		protected override void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			base.Setup(system, configuration);
+			system.AddModule(new EtlToolModule());
+		}
+	}
+}
---
+++ Teleopti.Wfm.AdministrationTest/Jobs/EtlToolJobCollectionModelProviderTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Wfm.Administration.Core.EtlTool;
+
+namespace Teleopti.Wfm.AdministrationTest.Jobs
+{
+	[AdministrationTest]
+	public class EtlToolJobCollectionModelProviderTest
+	{
+		public EtlToolJobCollectionModelProvider Target;
+
+		[Test, Ignore("under construction")]
+		public void ShouldReturnJobCollection()
+		{
+			var result = Target.Create();
+			result.Count.Should().Be.GreaterThanOrEqualTo(13);
+		}
+
+
+	}
+}
