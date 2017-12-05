b83dde7f4b6a3643539148670264c8f2b0d74e07
claess <claes.stenman@teleopti.com>
2017-12-05 13:07:29 +0100

    #45818 new (temp)classes to evaluate...

---
+++ Domain/Scheduling/TeamBlock/MedianCalculatorForDaysNew.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.Scheduling.TeamBlock.SkillInterval;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock
+{
+	public class MedianCalculatorForDaysNew : IMedianCalculatorForDays
+	{
+		private readonly IMedianCalculatorForSkillInterval _medianCalculatorForSkillInterval;
+
+		public MedianCalculatorForDaysNew(IMedianCalculatorForSkillInterval medianCalculatorForSkillInterval)
+		{
+			_medianCalculatorForSkillInterval = medianCalculatorForSkillInterval;
+		}
+
+		public Dictionary<DateTime, ISkillIntervalData> CalculateMedian(Dictionary<DateOnly, Dictionary<DateTime, ISkillIntervalData>> days, double resolution, DateOnly returnListDateOnly)
+		{
+			var result = new Dictionary<DateTime, ISkillIntervalData>();
+			if (!days.Any())
+				return result;
+
+			var temp = new Dictionary<TimeSpan, IList<ISkillIntervalData>>();
+			foreach (var dateOnlyList in days)
+			{
+				var baseDate = dateOnlyList.Key;
+				foreach (var interval in dateOnlyList.Value)
+				{
+					var timeSpanKey = interval.Key.TimeOfDay;
+
+					if (new DateOnly(interval.Key).Equals(returnListDateOnly.AddDays(-1)))
+						timeSpanKey = timeSpanKey.Add(TimeSpan.FromDays(-1));
+
+					if (new DateOnly(interval.Key).Equals(baseDate.AddDays(-1)))
+						timeSpanKey = timeSpanKey.Add(TimeSpan.FromDays(-1));
+
+					if (new DateOnly(interval.Key).Equals(baseDate.AddDays(1)))
+						timeSpanKey = timeSpanKey.Add(TimeSpan.FromDays(1));
+
+					IList<ISkillIntervalData> value;
+					if (!temp.TryGetValue(timeSpanKey, out value))
+					{
+						value = new List<ISkillIntervalData>();
+						temp.Add(timeSpanKey, value);
+					}
+
+					value.Add(interval.Value);
+				}
+			}
+
+			foreach (var interval in temp)
+			{
+				ISkillIntervalData skillIntervalData = _medianCalculatorForSkillInterval.CalculateMedian(interval.Key,
+					interval.Value,
+					resolution,
+					returnListDateOnly);
+				if (skillIntervalData != null)
+					result.Add(skillIntervalData.Period.StartDateTime, skillIntervalData);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Domain/Scheduling/TeamBlock/SkillInterval/CreateSkillIntervalDataPerDateAndActivityNew.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling.TeamBlock.SkillInterval
+{
+	public class CreateSkillIntervalDataPerDateAndActivityNew : ICreateSkillIntervalDataPerDateAndActivity
+	{
+		private readonly ICreateSkillIntervalDatasPerActivtyForDate _createSkillIntervalDatasPerActivtyForDate;
+
+		public CreateSkillIntervalDataPerDateAndActivityNew(ICreateSkillIntervalDatasPerActivtyForDate createSkillIntervalDatasPerActivtyForDate)
+		{
+			_createSkillIntervalDatasPerActivtyForDate = createSkillIntervalDatasPerActivtyForDate;
+		}
+
+		public Dictionary<DateOnly, IDictionary<IActivity, IList<ISkillIntervalData>>> CreateFor(ITeamBlockInfo teamBlockInfo, IEnumerable<ISkillDay> allSkillDays, IGroupPersonSkillAggregator groupPersonSkillAggregator)
+		{
+			var dayIntervalDataPerDateAndActivity = new Dictionary<DateOnly, IDictionary<IActivity, IList<ISkillIntervalData>>>();
+			var groupMembers = teamBlockInfo.TeamInfo.GroupMembers.ToList();
+			var blockPeriod = teamBlockInfo.BlockInfo.BlockPeriod;
+			var skills = groupPersonSkillAggregator.AggregatedSkills(groupMembers, blockPeriod).ToList();
+
+			var extraDateBefore = blockPeriod.StartDate.AddDays(-1);
+			var extraDayBeforeIntervalDataPerActivity = _createSkillIntervalDatasPerActivtyForDate.CreateFor(blockPeriod.StartDate.AddDays(-1), skills, allSkillDays);
+			dayIntervalDataPerDateAndActivity.Add(extraDateBefore, extraDayBeforeIntervalDataPerActivity);
+
+			foreach (var dateOnly in blockPeriod.DayCollection())
+			{
+				var dayIntervalDataPerActivity = _createSkillIntervalDatasPerActivtyForDate.CreateFor(dateOnly, skills, allSkillDays);
+				dayIntervalDataPerDateAndActivity.Add(dateOnly, dayIntervalDataPerActivity);
+			}
+
+			var extraDate = blockPeriod.EndDate.AddDays(1);
+			var extraDayIntervalDataPerActivity = _createSkillIntervalDatasPerActivtyForDate.CreateFor(blockPeriod.EndDate.AddDays(1), skills, allSkillDays);
+
+			dayIntervalDataPerDateAndActivity.Add(extraDate, extraDayIntervalDataPerActivity);
+			return dayIntervalDataPerDateAndActivity;
+		}
+	}
+}
