7f2e98acfedd38dd39b5351f468fded635fbae16
LevelUp! <levelup@teleopti.com>
2017-12-07 10:44:29 +0100

    bat works with new nunit episode 10

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Testing/TestRun.Console.bat
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Testing/TestRun.Console.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set nunit=..\..\..\packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe
+set nunit=..\..\..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe
 set assembly=..\bin\Release\Teleopti.Ccc.WebBehaviorTest.dll
 set output=/out:TestResult.txt
 set result=/result:TestResult.xml
-set tests=--test=Teleopti.Ccc.WebBehaviorTest.Wfm.ResourcePlanner.PlanningPeriodFeature.TheFirstPlanningPeriodSuggestionShouldBeTheNextUpcomingSchedulePeriod
+set tests=--test=Teleopti.Ccc.WebBehaviorTest.Wfm.RealTimeAdherence
 
 set command=%nunit% %assembly% %tests% %output% %result%
 
