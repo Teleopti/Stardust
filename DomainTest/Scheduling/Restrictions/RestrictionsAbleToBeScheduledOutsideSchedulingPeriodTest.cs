082b313dd8b0ff25619109e4fe4290d128acb6e9
micke <micke@teleopti.com>
2017-12-07 15:38:47 +0100

    #47013 Test to ensure it handles first week of the month, green at last

--- DomainTest/Scheduling/Restrictions/RestrictionsAbleToBeScheduledOutsideSchedulingPeriodTest.cs
+++ DomainTest/Scheduling/Restrictions/RestrictionsAbleToBeScheduledOutsideSchedulingPeriodTest.cs
@@ -93,11 +93,43 @@ namespace Teleopti.Ccc.DomainTest.Scheduling.Restrictions
 			stateHolder.Schedules[agent].CalculatedContractTimeHolderOnPeriod(period).TotalHours.Should().Be.LessThan(176);
 		}
 
-		//[Test]
-		//public void ShouldCheckOnWeekMaxTimeAndIncludeFullWeekBefore()
-		//{
+		//ShouldSkipFirstWeekWhenThisTheFirstPersonPeriod
 
-		//}
+		[Test]
+		public void ShouldCheckOnWeekMaxTimeAndIncludeFullWeekBefore()
+		{
+			var period = createStandardSetup(out var scenario, out var agent, out var skillDays);
+			var extendedPeriod = new DateOnlyPeriod(period.StartDate, period.EndDate.AddDays(6));
+			var preferenceDaysOrAss = new List<IPersistableScheduleData>();
+			var shiftCategory = new ShiftCategory("_");
+			var activity = new Activity().WithId();
+			var firstWeekStartDate = new DateOnly(2017, 10, 30);
+			for (int i = 0; i < 2; i++)
+			{
+				var ass = new PersonAssignment(agent, scenario, firstWeekStartDate.AddDays(i));
+				ass.AddActivity(activity, new TimePeriod(8, 18));
+				ass.SetShiftCategory(shiftCategory);
+				preferenceDaysOrAss.Add(ass);
+			}
+
+			preferenceDaysOrAss.Add(new PreferenceDay(agent, new DateOnly(2017, 11, 1),
+				new PreferenceRestriction { WorkTimeLimitation = new WorkTimeLimitation(TimeSpan.FromHours(10), TimeSpan.FromHours(10)) }));
+			preferenceDaysOrAss.Add(new PreferenceDay(agent, new DateOnly(2017, 11, 2),
+				new PreferenceRestriction { WorkTimeLimitation = new WorkTimeLimitation(TimeSpan.FromHours(10), TimeSpan.FromHours(10)) }));
+			preferenceDaysOrAss.Add(new PreferenceDay(agent, new DateOnly(2017, 11, 3),
+				new PreferenceRestriction { WorkTimeLimitation = new WorkTimeLimitation(TimeSpan.FromHours(10), TimeSpan.FromHours(10)) }));
+
+			var stateHolder = SchedulerStateHolderFrom.Fill(scenario, period, new[] { agent }, preferenceDaysOrAss, skillDays);
+
+			var result = Target.Execute(agent.VirtualSchedulePeriod(period.StartDate));
+			result.Should().Be.False();
+
+			Target2.Execute(new NoSchedulingCallback(), new SchedulingOptions(), new NoSchedulingProgress(), new[] { agent }, extendedPeriod);
+			stateHolder.Schedules[agent].CalculatedContractTimeHolderOnPeriod(period).TotalHours.Should().Be.LessThan(176);
+		}
+
+		//night rest
+		//no shift found
 
 		[Test]
 		public void ShouldCheckOnWeekMaxTimeAndIncludeFullWeekAfter()
@@ -141,10 +173,9 @@ namespace Teleopti.Ccc.DomainTest.Scheduling.Restrictions
 				new ShiftCategory("_").WithId()));
 			agent = new Person().WithId()
 				.WithPersonPeriod(new RuleSetBag(ruleSet), skill)
-				.WithSchedulePeriodOneMonth(period.StartDate);
+				.WithSchedulePeriodOneMonth(new DateOnly(2017, 10, 1));
 			agent.Period(period.StartDate).PersonContract = new PersonContract(new Contract("_"), new PartTimePercentage("_"),
 				new ContractScheduleWorkingMondayToFriday());
-			//period = period.Extend(6); //making this behave correctly, makes the test go red. it goes green if remark it though
 			skillDays = skill.CreateSkillDaysWithDemandOnConsecutiveDays(scenario, period, 1);
 			return period;
 		}
