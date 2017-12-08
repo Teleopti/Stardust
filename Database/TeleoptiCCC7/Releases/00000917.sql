3f04be08a558eb8d3de2183e867b90897a684a41
Xinfeng Li <xinfeng.li@teleopti.com>
2017-12-08 10:19:33 +0800

    PBI #46841: Fix failed test case by add clustered index for new table.

--- Database/TeleoptiCCC7/Releases/00000917.sql
+++ Database/TeleoptiCCC7/Releases/00000917.sql
@@ -8,11 +8,29 @@
 DROP TABLE [Dbo].[ExternalPerformanceData]
 
 CREATE TABLE [ReadModel].[ExternalPerformanceData](
-	[ExternalPerformance] [uniqueidentifier] NULL,
-	[DateFrom] [smalldatetime] NULL,
-	[Person] [uniqueidentifier] NULL,
-	[OriginalPersonId] [nvarchar](130) NULL,
-	[Score] [int] NULL
-) ON [PRIMARY]
+    [ExternalPerformance] [uniqueidentifier] NOT NULL,
+    [DateFrom] [smalldatetime] NOT NULL,
+    [Person] [uniqueidentifier] NOT NULL,
+    [OriginalPersonId] [nvarchar](130) NOT NULL,
+    [Score] [real] NOT NULL
+ CONSTRAINT [PK_ReadModel_ExternalPerformanceData] PRIMARY KEY CLUSTERED
+(
+    [ExternalPerformance] ASC,
+    [DateFrom] ASC,
+    [Person] ASC
+)) ON [PRIMARY]
+GO
 
+ALTER TABLE [ReadModel].[ExternalPerformanceData] WITH CHECK ADD CONSTRAINT [FK_ReadModel_ExternalPerformanceData_ExternalPerformance] FOREIGN KEY([ExternalPerformance])
+REFERENCES [dbo].[ExternalPerformance] ([Id])
+GO
+
+ALTER TABLE [ReadModel].[ExternalPerformanceData] CHECK CONSTRAINT [FK_ReadModel_ExternalPerformanceData_ExternalPerformance]
+GO
+
+ALTER TABLE [ReadModel].[ExternalPerformanceData] WITH CHECK ADD CONSTRAINT [FK_ReadModel_ExternalPerformanceData_Person] FOREIGN KEY([Person])
+REFERENCES [dbo].[Person] ([Id])
+GO
+
+ALTER TABLE [ReadModel].[ExternalPerformanceData] CHECK CONSTRAINT [FK_ReadModel_ExternalPerformanceData_Person]
 GO
