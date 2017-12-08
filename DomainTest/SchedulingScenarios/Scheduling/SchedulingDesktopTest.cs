f48e1cbdec96c0a5189f6da1a3c120f0364183de
claess <claes.stenman@teleopti.com>
2017-12-08 13:25:19 +0100

    #47185 ShouldValidateSolvedWeeklyRest ignored

--- DomainTest/SchedulingScenarios/Scheduling/SchedulingDesktopTest.cs
+++ DomainTest/SchedulingScenarios/Scheduling/SchedulingDesktopTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
@@ -11,6 +12,7 @@ using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+using Teleopti.Ccc.Domain.Scheduling.Rules;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
@@ -207,6 +209,37 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Scheduling
 					schedulerStateHolder.Schedules[agent].ScheduledDay(firstDay.AddDays(i)).IsScheduled().Should().Be.True();
 				}
 			}
+		}
+
+		[Test]
+		[Ignore("#47185 to be fixed")]
+		public void ShouldValidateSolvedWeeklyRest()
+		{
+			var date = new DateOnly(2017, 5, 15);
+			var schedulePeriod = new DateOnlyPeriod(date, date.AddDays(6));
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var activity = new Activity().WithId();
+			activity.InWorkTime = true;
+			var skill = new Skill().For(activity).InTimeZone(TimeZoneInfo.Utc).WithId().IsOpen();
+			var scenario = new Scenario();
+			var weeklyRest = TimeSpan.FromHours(20);
+			var workTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(0), TimeSpan.FromHours(90), TimeSpan.FromHours(8), weeklyRest);
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 16, 0, 15), new ShiftCategory("_").WithId()));
+			var contract = new Contract("_") { WorkTimeDirective = workTimeDirective, EmploymentType = EmploymentType.FixedStaffNormalWorkTime };
+			var agent = new Person().WithId().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(ruleSet, contract, skill).WithSchedulePeriodOneWeek(date);
+			var skillDays = skill.CreateSkillDaysWithDemandOnConsecutiveDays(scenario, date, 10, 10, 10, 10, 10, 10, 10);
+			var asses = new List<IPersonAssignment>();
+			for (var i = -1; i < 8; i++)
+			{
+				var ass = new PersonAssignment(agent, scenario, date.AddDays(i)).ShiftCategory(shiftCategory).WithLayer(activity, new TimePeriod(8, 16));
+				asses.Add(ass);
+			}
+			var schedulerStateHolder = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(date.AddDays(-1), date.AddDays(7)), new[] { agent }, asses, skillDays);
+			schedulerStateHolder.Schedules.ValidateBusinessRulesOnPersons(new []{agent}, CultureInfo.InvariantCulture, NewBusinessRuleCollection.All(schedulerStateHolder.SchedulingResultState));
+
+			Target.Execute(new NoSchedulingCallback(), new SchedulingOptions(), new NoSchedulingProgress(), new[] { agent }, schedulePeriod);
+
+			schedulerStateHolder.Schedules[agent].BusinessRuleResponseInternalCollection.Should().Be.Empty();
 		}
 
 		public SchedulingDesktopTest(SeperateWebRequest seperateWebRequest, RemoveClassicShiftCategory resourcePlannerRemoveClassicShiftCat46582, RemoveImplicitResCalcContext removeImplicitResCalcContext46680, bool resourcePlannerTimeZoneIssues45818) : base(seperateWebRequest, resourcePlannerRemoveClassicShiftCat46582, removeImplicitResCalcContext46680, resourcePlannerTimeZoneIssues45818)
