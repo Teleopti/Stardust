e29017f5416d088b7d3e0be19ddf63dfa1381367
LevelUp! <levelup@teleopti.com>
2017-12-13 15:53:30 +0100

    #46786 Accepting date in the api controller

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/HistoricalAdherenceController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/HistoricalAdherenceController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.Web.Http;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Rta.ViewModels;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.Security.AuthorizationData;
 using Teleopti.Ccc.Web.Filters;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 {
@@ -31,8 +34,13 @@ namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
 
 		[ReadModelUnitOfWork, UnitOfWork]
 		[HttpGet, Route("api/HistoricalAdherence/ForPerson")]
-		public virtual IHttpActionResult ForPerson(Guid personId) =>
-			Ok(_historicalAdherenceViewModelBuilder.Build(personId));
+		public virtual IHttpActionResult ForPerson(Guid personId, string date = null)
+		{
+			if (date == null)
+				return Ok(_historicalAdherenceViewModelBuilder.Build(personId));
+			var dateTime = DateTime.ParseExact(date, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None);
+			return Ok(_historicalAdherenceViewModelBuilder.Build(personId, new DateOnly(dateTime)));
+		}
 
 		[UnitOfWork]
 		[HttpGet, Route("api/HistoricalAdherence/MostRecentShiftDate")]
