51db30eb7cc3a643a1e910e2f3033c84c5854395
jimmyb <none@none>
2017-12-18 13:12:03 +0100

    #47217 fixing small issue

--- teamcity/Azure/TeleoptiCCC/bin/InitializeCopyPayroll.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/InitializeCopyPayroll.ps1
@@ -4,7 +4,7 @@ $scriptLog = "$path\$scriptName.log"
 
 $admin = 'ScheduleTaskAdmin'
 $taskName = 'CopyPayrollDll'
-$taskName2 = 'ClickOnceSign'
+#$taskName2 = 'ClickOnceSign'
 
 function New-RandomPassword() {
     [CmdletBinding()]
@@ -23,19 +23,20 @@ Try
     
     $userExits = Get-localuser -Name $admin -ErrorAction SilentlyContinue
 
-        if ($userExits) {
+    if ($userExits) {
 
-            Remove-LocalUser -Name $admin | Out-null
+        Remove-LocalUser -Name $admin | Out-null
+    }
 
-            $pwd = New-RandomPassword
-            $SecurePassword = ConvertTo-SecureString "$pwd" –asplaintext –force 
+    $pwd = New-RandomPassword
+    $SecurePassword = ConvertTo-SecureString "$pwd" –asplaintext –force 
 
-            Write-Output "Creating temporary user '$admin' with password $pwd" | out-file $scriptLog -Append
-            New-LocalUser -Name $admin -Password $SecurePassword -FullName "Payroll Automation" -Description "Payroll"
+    Write-Output "Creating temporary user '$admin' with password $pwd" | out-file $scriptLog -Append
+    New-LocalUser -Name $admin -Password $SecurePassword -FullName "Payroll Automation" -Description "Payroll" | out-null
 
-            Write-Output "Adding user '$admin' to 'Administrators' group..." | out-file $scriptLog -Append
-            Add-LocalGroupMember -Group "Administrators" -Member "$admin"
-        }
+    Write-Output "Adding user '$admin' to 'Administrators' group..." | out-file $scriptLog -Append
+    Add-LocalGroupMember -Group "Administrators" -Member "$admin"
+    
 
     Start-Service "task scheduler" | out-file $scriptLog -Append
 
@@ -47,17 +48,26 @@ Try
             Unregister-ScheduledTask -TaskName $taskName -Confirm:$false | out-file $scriptLog -Append
         }
 
-    $taskExists = Get-ScheduledTask -TaskName $taskName2 -ErrorAction SilentlyContinue
+ <#   $taskExists = Get-ScheduledTask -TaskName $taskName2 -ErrorAction SilentlyContinue
 
         if ($taskExists) {
         
             Write-Host "Removing Scheduled task '$taskName2'..."
             Unregister-ScheduledTask -TaskName $taskName2 -Confirm:$false | out-file $scriptLog -Append
         }
-
+ #>
+   
     #Schedule task every 20 minutes
-    schtasks /create /sc minute /mo 20 /tn "$taskName" /f /ru "$admin" /rp "$pwd" /tr "$path\CopyPayrollDll.cmd" | Out-File $scriptLog -Append
-
+    $setupTask = schtasks /create /sc minute /mo 20 /tn "$taskName" /f /ru "$admin" /rp "$pwd" /tr "$path\CopyPayrollDll.cmd" 2>&1 #| Out-File $scriptLog -Append
+    $setupTask | out-file $scriptLog -Append
+    
+    if ($lastexitcode -ne 0) {
+        
+        New-EventLog -LogName Application -Source "InitilizeCopyPayroll" -ErrorAction SilentlyContinue
+        Write-EventLog -LogName Application -Source "InitilizeCopyPayroll" -EntryType Warning -EventId 666 -Message "$setupTask"
+        Write-Output $setupTask   
+    }
+    
     #Run task for the first time
     schtasks /RUN /TN "$taskName" | Out-File $scriptLog
 }
