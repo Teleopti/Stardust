5208ab5a2752b70b730e10424515a5302159e854
Roger Kratz <roger.kratz@teleopti.com>
2017-12-20 16:54:38 +0100

    Removing dead GroupingsCreator

--- DomainTest/GroupPageCreator/GroupingsCreatorTest.cs
+++ DomainTest/GroupPageCreator/GroupingsCreatorTest.cs
@@ -14,39 +14,6 @@ namespace Teleopti.Ccc.DomainTest.GroupPageCreator
 	public class GroupingsCreatorTest
 	{
 		[Test]
-		public void VerifyCreationOfBuiltInGroupings()
-		{
-			var mocks = new MockRepository();
-			var groupingsDataProvider = mocks.StrictMock<IGroupPageDataProvider>();
-			using (mocks.Record())
-			{
-				Expect.Call(groupingsDataProvider.BusinessUnit).Return(BusinessUnitFactory.BusinessUnitUsedInTest);
-				Expect.Call(groupingsDataProvider.ContractCollection).Return(new List<IContract>());
-				Expect.Call(groupingsDataProvider.ContractScheduleCollection).Return(new List<IContractSchedule>());
-				Expect.Call(groupingsDataProvider.PartTimePercentageCollection).Return(new List<IPartTimePercentage>());
-				Expect.Call(groupingsDataProvider.PersonCollection).Return(new List<IPerson>()).Repeat.AtLeastOnce();
-				Expect.Call(groupingsDataProvider.RuleSetBagCollection).Return(new List<IRuleSetBag>());
-				Expect.Call(groupingsDataProvider.SelectedPeriod).Return(new DateOnlyPeriod());
-				Expect.Call(groupingsDataProvider.SkillCollection).Return(new List<ISkill>());
-			}
-			using (mocks.Playback())
-			{
-				var target = new GroupingsCreator(groupingsDataProvider);
-
-				IList<IGroupPage> groupPages = target.CreateBuiltInGroupPages(true);
-
-				Assert.AreEqual(7, groupPages.Count);
-				Assert.AreEqual(groupPages[0].DescriptionKey, "Main");
-				Assert.AreEqual(groupPages[1].DescriptionKey, "Contracts");
-				Assert.AreEqual(groupPages[2].DescriptionKey, "ContractSchedule");
-				Assert.AreEqual(groupPages[3].DescriptionKey, "PartTimepercentages");
-				Assert.AreEqual(groupPages[4].DescriptionKey, "Note");
-				Assert.AreEqual(groupPages[5].DescriptionKey, "RuleSetBag");
-				Assert.AreEqual(groupPages[6].DescriptionKey, "Skill");
-			}
-		}
-
-		[Test]
 		public void VerifyCreationOfBuiltInGroupingsIncludingOptionalColumns()
 		{
 			var mocks = new MockRepository();
