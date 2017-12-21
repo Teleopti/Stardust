906c41f8eebf7b521908091a506bd4edcea6adf2
Squid Squad <jonas.nordh@teleopti.com>
2017-12-21 17:32:41 +0100

    Bug #47317 - Release script to get correct person_id for fact_tables

---
+++ Database/TeleoptiAnalytics/Releases/00000600.sql
@@ -0,0 +1,29 @@
+/* Correct data in some fact tables where person_period is incorrect due to bug #47317 */
+
+SET NOCOUNT ON
+
+DECLARE @current_person_code uniqueidentifier, @person_ids nvarchar(max)
+
+DECLARE AgentCursor CURSOR FOR
+SELECT person_code FROM mart.dim_person 
+WHERE person_id <>-1
+GROUP BY person_code
+OPEN AgentCursor
+
+FETCH NEXT FROM AgentCursor INTO @current_person_code
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	SET @person_ids = (Select CONVERT(nvarchar(8), person_id) + ',' AS [text()]
+	From mart.dim_person
+	WHERE person_code = @current_person_code
+	For XML PATH (''))
+
+	EXEC mart.etl_fact_schedule_update_unlinked_personids @person_ids
+	EXEC mart.etl_fact_schedule_preference_update_unlinked_personids @person_ids
+	EXEC mart.etl_fact_request_update_unlinked_personids @person_ids
+	EXEC mart.etl_fact_hourly_availability_update_unlinked_personids @person_ids
+
+	FETCH NEXT FROM AgentCursor INTO @current_person_code
+END
+CLOSE AgentCursor
+DEALLOCATE AgentCursor
