9a116530ddd625a47a48c851c9ec22f0a33d5eba
Ola <none@none>
2018-01-02 12:43:33 +0100

    fixing permission for BPO Exchange

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Portal/DataProvider/AreaWithPermissionPathProvider.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Core/Portal/DataProvider/AreaWithPermissionPathProvider.cs
@@ -36,7 +36,8 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Core.Portal.DataProvider
 			new AreaWithPermissionPath(DefinedRaptorApplicationFunctionPaths.AccessToReports, () => Resources.Reports, "reports"),
 			new AreaWithPermissionPath(DefinedRaptorApplicationFunctionPaths.WebStaffing, () => Resources.WebStaffing, "staffing"),
 			new AreaWithPermissionPath(DefinedRaptorApplicationFunctionPaths.OpenOptionsPage, () => Resources.Gamification, "gamification"),
-			new AreaWithPermissionPath(DefinedRaptorApplicationFunctionPaths.MyTimeWeb, () => Resources.MyTimeWeb, "myTime")
+			new AreaWithPermissionPath(DefinedRaptorApplicationFunctionPaths.MyTimeWeb, () => Resources.MyTimeWeb, "myTime"),
+			new AreaWithPermissionPath(DefinedRaptorApplicationFunctionPaths.BpoExchange, () => Resources.MyTimeWeb, "bpo-gatekeeper")
 		};
 
 		public AreaWithPermissionPathProvider(IPermissionProvider permissionProvider, IToggleManager toggleManager, ILicenseActivatorProvider licenseActivatorProvider, IApplicationFunctionsToggleFilter applicationFunctionsToggleFilter)
