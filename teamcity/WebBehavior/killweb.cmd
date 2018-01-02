a418471e55cbd561d4cd09647cddded43f718e4c
Kunning Mao <none@none>
2018-01-02 14:33:16 +0100

    #47256 kill service bus console host as well in perf test

--- teamcity/WebBehavior/killweb.cmd
+++ teamcity/WebBehavior/killweb.cmd
@@ -3,4 +3,5 @@
 taskkill.exe /IM chromedriver.exe /F /FI "memusage gt 2" /t
 taskkill.exe /IM iisexpress.exe /F /FI "memusage gt 2" /t
 taskkill.exe /IM chrome.exe /F /FI "memusage gt 2" /t
+taskkill.exe /IM Teleopti.Ccc.Sdk.ServiceBus.ConsoleHost.exe /F /FI "memusage gt 2" /t
 taskkill.exe /IM iisexpresstray.exe /F /FI "memusage gt 2" /T 2>&1 | exit /B 0
