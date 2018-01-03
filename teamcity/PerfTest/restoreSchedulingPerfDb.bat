518abec0a50c89b999edb3639844075c9b08c24c
Kunning Mao <none@none>
2018-01-03 11:31:23 +0100

    #47256 added bat for restore both app and analytics for scheduling perf tests

---
+++ teamcity/PerfTest/restoreSchedulingPerfDb.bat
@@ -0,0 +1,74 @@
+@ECHO off
+
+::Get path to this batchfile
+SET THIS=%~dp0
+Set RepoRoot=%THIS:~0,-19%
+set destinationBakFile=%repoRoot%\ccc7.bak
+set destinationAnalyticsBakFile=%repoRoot%\analytics.bak
+set appDb=PerfApp
+set analDb=PerfAnal
+set aggDb=PerfAgg
+set dbServer=%1
+set sourceFolder=%~2
+set securityExe=%RepoRoot%\Teleopti.Support.Security\bin\release\Teleopti.Support.Security.exe
+set dbmanagerExe=%RepoRoot%\Teleopti.Ccc.DBManager\Teleopti.Ccc.DBManager\bin\release\DBManager.exe
+IF [%1]==[] goto wrongInput
+if [%2]==[] goto wrongInput
+if not exist "%securityExe%" goto missingAssemblies
+if not exist "%dbmanagerExe%" goto missingAssemblies
+
+::copy bak file
+COPY "%sourceFolder%\ccc7.bak" "%destinationBakFile%" /Y
+COPY "%sourceFolder%\analytics.bak" "%destinationAnalyticsBakFile%" /Y
+
+::restore app db
+SQLCMD -S%dbServer% -E -dmaster -i"%RepoRoot%\.debug-Setup\database\tsql\DemoDatabase\RestoreDatabase.sql" -v BAKFILE="%destinationBakFile%" DATAFOLDER="%RepoRoot%" -v DATABASENAME="%appDb%"
+IF %ERRORLEVEL% NEQ 0 GOTO :restoreError
+
+::restore analytics db
+SQLCMD -S%dbServer% -E -dmaster -i"%RepoRoot%\.debug-Setup\database\tsql\DemoDatabase\RestoreAnalytics.sql" -v BAKFILE="%destinationAnalyticsBakFile%" DATAFOLDER="%RepoRoot%" -v DATABASENAME="%analDb%"
+IF %ERRORLEVEL% NEQ 0 GOTO :restoreAnalyticsDbError
+
+
+::Skapa agg
+SQLCMD -S%dbServer% -E -Q "alter database [%aggDb%] set single_user with rollback immediate"
+SQLCMD -S%dbServer% -E -Q "if exists(select 1 from sys.databases where name=""%aggDb%"") drop database [%aggDb%]"
+SQLCMD -S%dbServer% -E -d"%appDb%" -i"%RepoRoot%\.debug-Setup\database\tsql\DemoDatabase\SetTenantActive.sql"
+%dbmanagerExe% -S%dbServer% -D%aggDb% -E -OTeleoptiCCCAgg -F"%RepoRoot%\Database" -C
+
+
+::upgrade appdb
+%dbmanagerExe% -S%dbServer% -D"%appDb%" -E -OTeleoptiCCC7 -F"%RepoRoot%\Database" -T
+%dbmanagerExe% -S%dbServer% -D"%analDb%" -E -OTeleoptiAnalytics -F"%RepoRoot%\Database" -T
+%securityExe% -DS%dbServer% -AP"%appDb%" -AN"%analDb%" -CD"%aggDb%" -EE 
+
+
+::copy app.config
+COPY "%sourceFolder%\app.config" "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\app.config" /Y
+COPY "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\app.config" "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\bin\debug\Teleopti.Ccc.Scheduling.PerformanceTest.dll.config" /Y
+COPY "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\app.config" "%RepoRoot%\Teleopti.Ccc.Scheduling.PerformanceTest\bin\release\Teleopti.Ccc.Scheduling.PerformanceTest.dll.config" /Y
+
+exit
+
+:wrongInput
+echo Two arguments are needed. Pass in server name and a path to a folder containing "ccc7.bak", "analytics.bak" and "app.config". Eg,
+echo restoreAppDb.bat . \\buildsrv01\perftests\demosales
+echo.
+pause
+exit -1
+
+:restoreError
+echo Something went wrong with restore database
+echo.
+exit -1
+
+:restoreAnalyticsDbError
+echo Something went wrong with restore analytics database
+echo.
+exit -1
+
+:missingAssemblies
+echo To run this script, please first compile DBManager and Security in release mode!
+echo.
+pause
+exit -1
