e43c8b4e74a6d2e0f6ff3b78581c124e879d76cf
LevelUp! <levelup@teleopti.com>
2018-01-05 11:36:23 +0100

    #46826 fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/agents/rta.agents.controller.excludeStates.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/agents/rta.agents.controller.excludeStates.spec.js
@@ -1,5 +1,5 @@
 'use strict';
-rtaTester.fdescribe('RtaAgentsController', function (it, fit, xit, _,
+rtaTester.describe('RtaAgentsController', function (it, fit, xit, _,
 													 $state,
 													 $fakeBackend,
 													 $controllerBuilder,
