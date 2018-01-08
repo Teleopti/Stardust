01e0252b34aa8316644c628832713cea4cbad5e4
jimmyb <none@none>
2018-01-08 14:31:06 +0100

    #46238 Provided credentials when authenticate to Jenkins

--- teamcity/SmokeTest/Msi/TriggerJenkins.ps1
+++ teamcity/SmokeTest/Msi/TriggerJenkins.ps1
@@ -1,4 +1,12 @@
-$result = Invoke-WebRequest $Env:JenkinsTriggerUrl -UseBasicParsing
+#Defining install credentials
+$username = "tfsintegration"
+$domain = "toptinet"
+$password = "m8kemew0rk"
+$secstr = New-Object -TypeName System.Security.SecureString
+$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
+$AdminCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $domain\$username, $secstr
+
+$result = Invoke-WebRequest $Env:JenkinsTriggerUrl -Credential $AdminCredentials
 if($result.StatusCode -ne 201) {
     Write-Error "fail to trigger jenkins auto deploy"
 }
