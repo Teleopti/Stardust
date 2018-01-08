3c0d8d6353c43df7da7c29eb45de9827dd68a431
Aidin Dadashzadeh <aidin.dadashzadeh@gmail.com>
2018-01-08 09:45:23 +0100

    Bug #47046: Added behaviour tests for staffing page

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Staffing/Staffing.feature
@@ -0,0 +1,73 @@
+﻿@WFM
+Feature: Staffing
+	In order to manage staffing
+	As an admin
+	I want to see staffing for skills
+
+Background:
+Given I have a role with
+| Field              | Value |
+| Access to Staffing | True  |
+And There is a skill to monitor called 'Skill A' with queue id '1' and queue name 'queue1' and activity 'Phone'
+And There is a skill to monitor called 'Skill B' with queue id '2' and queue name 'queue2' and activity 'Chat'
+
+
+Scenario: View staffing for one skill on selected date
+	Given the time is '2017-09-25 08:00'
+	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
+	And there is staffing data for skills 'Skill A' for date '2017-09-25'
+	When I am viewing staffing page
+	And I change staffing date to '2017-09-25'
+	Then I should see staffing data in the graph
+
+
+Scenario: View staffing for one skillgroup
+	Given the time is '2017-09-25 08:00'
+	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
+	And there is staffing data for skills 'Skill A' for date '2017-09-25'
+	And there is queue statistics for skill 'Skill B' until '2017-09-27 17:00'
+	And there is staffing data for skills 'Skill B' for date '2017-09-25'
+	And there is a skill group with name 'Group A' with skills 'Skill A, Skill B'
+	When I am viewing staffing page
+	And I select skill group 'Group A'
+	And I change staffing date to '2017-09-25'
+	Then I should see the selected skill group 'Group A' 
+	And I should see staffing data in the graph
+
+
+Scenario: Show overtime settings
+	Given the time is '2017-09-25 08:00'
+	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
+	And there is staffing data for skills 'Skill A' for date '2017-09-25'
+	When I am viewing staffing page
+	And I change staffing date to '2017-09-25'
+	And I can not see overtime settings
+	And I press the get suggestions for overtime button
+	Then I should see overtime settings
+
+
+Scenario: Use shrinkage when toggling shrinkage to on
+	Given the time is '2017-09-25 08:00'
+	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
+	And there is staffing data for skills 'Skill A' for date '2017-09-25'
+	When I am viewing staffing page
+	And I change staffing date to '2017-09-25'
+	And I see staffing data in the graph
+	And Using shrinkage is off
+	And I turn using shrinkage to on
+	Then Using shrinkage should be used
+	#Then I should see the staffing chart update // How?
+	
+
+Scenario: Show bpo exchange page
+	Given I am viewing staffing page
+	When I press the BPO exchange button 
+	Then I should see the bpo exchange page
+
+
+#Scenario: Show manage skill group page
+#	Given I am viewing staffing page
+#	When I press the create skill button 
+#	Then I should see the manage skill group page
+
+#Shrinkage 
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Staffing/StaffingStepDefintions.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Teleopti.Ccc.TestCommon.TestData.Analytics;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Configurable;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Default;
+using Teleopti.Ccc.TestCommon.Web.WebInteractions.BrowserDriver;
+using Teleopti.Ccc.WebBehaviorTest.Core;
+using Teleopti.Ccc.WebBehaviorTest.Data;
+
+namespace Teleopti.Ccc.WebBehaviorTest.Wfm.Staffing
+{
+	[Binding]
+	public class StaffingStepDefintions
+	{
+		[Given(@"there is a skill to monitor called '(.*)' with activity '(.*')")]
+		public void GivenThereIsASkillToMonitorCalled(string skillName, string activity)
+		{
+			var datasourceData = DefaultAnalyticsDataCreator.GetDataSources();
+
+			DataMaker.Data().Apply(new ActivityConfigurable
+			{
+				Name = activity
+			});
+
+			DataMaker.Data().Apply(new SkillConfigurable
+			{
+				Name = skillName,
+				Activity = activity
+			});
+		}
+
+		[Given(@"there is a skill with name '(.*)', queue id '([^']*)', queue name '([^']*)' and activity '(.*')")]
+		public void GivenThereIsASkillToMonitorWithName(string skillName, int queueId, string queueName, string activity)
+		{
+			var datasourceData = DefaultAnalyticsDataCreator.GetDataSources();
+
+			DataMaker.Data().Apply(new ActivityConfigurable
+			{
+				Name = activity
+			});
+
+			DataMaker.Data().Apply(new SkillConfigurable
+			{
+				Name = skillName,
+				Activity = activity
+			});
+
+			DataMaker.Data().Analytics().Apply(new AQueue(datasourceData) { QueueId = queueId });
+
+			DataMaker.Data().Apply(new QueueSourceConfigurable
+			{
+				Name = queueName,
+				QueueId = queueId
+			});
+
+			DataMaker.Data().Apply(new WorkloadConfigurable
+			{
+				WorkloadName = skillName,
+				SkillName = skillName,
+				QueueSourceName = queueName,
+				Open24Hours = true
+			});
+		}
+
+		[Given(@"there is a skill group with name '(.*)' with skills '(.*)'")]
+		public void GivenThereIsASkillGroupWithName(string name, string skills)
+		{
+			DataMaker.Data().Apply(new SkillGroupConfigurable
+			{
+				Name = name,
+				Skills = skills
+			});
+		}
+
+		[Given(@"there is a skill with name '(.*)' with activity '(.*)'")]
+		public void GivenThereIsASkillWithName(string skillName, string activity)
+		{
+			var datasourceData = DefaultAnalyticsDataCreator.GetDataSources();
+
+			DataMaker.Data().Apply(new ActivityConfigurable
+			{
+				Name = activity
+			});
+
+			DataMaker.Data().Apply(new SkillConfigurable
+			{
+				Name = skillName,
+				Activity = activity
+			});
+		}
+
+		[Given(@"there is queue statistics for skill '(.*)' until '(.*)'")]
+		public void GivenThereIsQueueStatisticsForSkillUntill(string skillName, string time)
+		{
+			var latestStatisticsTime = DateTime.Parse(time);
+
+			DataMaker.Data().Analytics().Apply(new QueueStatisticsForSkill(skillName, latestStatisticsTime));
+		}
+
+		[Given(@"there is staffing data for skills '(.*)' for date '(.*)'")]
+		public void GivenThereIsForecastDataForSkillForDate(string skillName, string date)
+		{
+			var theDate = date == "today" ? DateTime.Now.Date : DateTime.Parse(date);
+
+			DataMaker.Data().Apply(new ForecastConfigurable(skillName, theDate));
+		}
+
+		[When(@"I select skill group '(.*)'")]
+		public void WhenISelectSkillGroup(string group)
+		{
+			/*
+			Browser.Interactions.AssertExists("md-autocomplete");
+			var javascript = "var scope = angular.element(document.querySelector('md-autocomplete')).scope();" +
+							 "var sg = scope.vm.skillAreas.find(function(e){{return e.Name === '" + group + "'}});" +
+							 "scope.vm.selectedArea = sg;" +
+							 "scope.vm.selectedAreaChange(sg);";
+			Browser.Interactions.TryUntil(
+				() => { Browser.Interactions.Javascript(javascript); },
+				() => Browser.Interactions.IsVisible("md-autocomplete")
+				, TimeSpan.FromSeconds(1)
+			);*/
+
+			
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('#area-id')).scope();" +
+				"var skillGroup = scope.vm.querySearchAreas()[0].Name;" +
+				$"return (skillGroup === '{group}')", "True");
+
+
+			Browser.Interactions.Javascript(
+				"var scope = angular.element(document.querySelector('#area-id')).scope();" +
+				"var sg = scope.vm.allSkillAreas.find(function(e){{return e.Name === '" + group + "'}});" +
+				$"scope.vm.selectedSkillArea = sg;" +
+				$"scope.vm.selectedAreaChange(sg);"
+			);
+
+			Browser.Interactions.PressEnter("#area-id");
+			
+		}
+
+		[When(@"I change staffing date to '(.*)'")]
+		public void WhenIChangeStaffingDateTo(string date)
+		{
+			Browser.Interactions.FillWith(
+				"#teamschedule-datepicker-input",
+				date
+			);
+			Browser.Interactions.PressEnter("#teamschedule-datepicker-input");
+		}
+
+		[When(@"I press the create skill button")]
+		public void WhenIPressCreateSkillButton()
+		{
+			Browser.Interactions.Click("#manage_skill_group_button");
+		}
+
+		[When(@"I press the BPO exchange button")]
+		public void WhenIPressBpoExchangeButton()
+		{
+			Browser.Interactions.Click("#bpoExchangeBtn");
+		}
+
+		[When(@"I press the get suggestions for overtime button")]
+		public void WhenIPressOvertimeSuggestionButton()
+		{
+			Browser.Interactions.Click("#overstaffBtn.wfm-btn-invis-primary");
+		}
+
+
+		[When(@"Using shrinkage is on")]
+		public void WhenUsingShringageIsOn()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('md-checkbox')).scope();" +
+				"var isUsingShrinkage = scope.vm.useShrinkage;" +
+				"return isUsingShrinkage", "True"
+			);
+		}
+
+		[When(@"Using shrinkage is off")]
+		public void WhenUsingShringageIsOff()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('md-checkbox')).scope();" +
+				"var isUsingShrinkage = scope.vm.useShrinkage;" +
+				"return isUsingShrinkage", "False"
+			);
+		}
+
+
+		[When(@"I turn using shrinkage to on")]
+		public void WhenTogglingUsingShrinkageToOn()
+		{
+			Browser.Interactions.Click("md-checkbox");
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('md-checkbox')).scope();" +
+				"var isUsingShrinkage = scope.vm.useShrinkage;" +
+				"return isUsingShrinkage", "True"
+			);
+		}
+
+
+		[When(@"I turn using shrinkage to off")]
+		public void WhenTogglingUsingShrinkageToOff()
+		{
+			Browser.Interactions.Click("md-checkbox");
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('md-checkbox')).scope();" +
+				"var isUsingShrinkage = scope.vm.useShrinkage;" +
+				"return isUsingShrinkage", "False"
+			);
+		}
+
+
+		[Then(@"I should see the selected skill group '(.*)'")]
+		public void ThenIShouldSeeTheSelectedSkillGroup(string group)
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('#area-id')).scope();" +
+				"var skillGroup = scope.vm.selectedArea;" +
+				$"return (skillGroup.Name === '{group}')", "True");
+		}
+
+
+		[When(@"I see staffing data in the graph")]
+		[Then(@"I should see staffing data in the graph")]
+		public void ThenIShouldSeeStaffingDataInTheGraph()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('#staffingChart')).scope();" +
+				"var chartHasData = scope.vm.staffingDataAvailable;" +
+				"return chartHasData", "True"
+			);
+		}
+
+
+		[Then(@"Using shrinkage should be used")]
+		public void ThenUsingShrinkageShouldBeUsed()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('#staffingChart')).scope();" +
+				"var isUsingShrinkage = scope.vm.useShrinkage;" +
+				"return isUsingShrinkage", "True"
+			);
+		}
+
+
+		[Then(@"I should see the staffing chart update")]
+		public void ThenIShouldSeeStaffingChartUpdate()
+		{
+			// TODO: how to check for this?
+		}
+
+
+		[Then(@"I should see the manage skill group page")]
+		public void ThenIShouldSeeManageSkillGroupPage()
+		{
+			var urlString = "skill-area-config";
+			Browser.Interactions.AssertUrlContains(urlString);
+		}
+
+
+		[Then(@"I should see the bpo exchange page")]
+		public void ThenIShouldSeeBpoExchangePage()
+		{
+			var urlString = "bpo";
+			Browser.Interactions.AssertUrlContains(urlString);
+		}
+
+
+		[When(@"I can see overtime settings")]
+		[Then(@"I should see overtime settings")]
+		public void ThenIShouldSeeOvertimeSettings()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('#staffingChart')).scope();" +
+				"var showOvertime = scope.vm.showOverstaffSettings;" +
+				"return showOvertime", "True"
+			);
+		}
+
+		[When(@"I can not see overtime settings")]
+		[Then(@"I should not see overtime settings")]
+		public void ThenIShouldNotSeeOvertimeSettings()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(
+				"var scope = angular.element(document.querySelector('#staffingChart')).scope();" +
+				"var showOvertime = scope.vm.showOverstaffSettings;" +
+				"return showOvertime", "False"
+			);
+		}
+	}
+}
