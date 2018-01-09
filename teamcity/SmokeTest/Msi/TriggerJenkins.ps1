8737bfaa79916bddfeb09f5425636bf177daa6a8
jimmyb <none@none>
2018-01-09 14:36:46 +0100

    #46238 Fixing Crumb issue when trigger build with Jenkins CSRF enabled

--- teamcity/SmokeTest/Msi/TriggerJenkins.ps1
+++ teamcity/SmokeTest/Msi/TriggerJenkins.ps1
@@ -1,12 +1,24 @@
-#Defining install credentials
-$username = "tfsintegration"
-$domain = "toptinet"
-$password = "m8kemew0rk"
-$secstr = New-Object -TypeName System.Security.SecureString
-$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
-$AdminCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $domain\$username, $secstr
+$JenkinsUrl = New-Object -TypeName 'System.Uri' -ArgumentList $Env:JenkinsTriggerUrl
 
-$result = Invoke-WebRequest $Env:JenkinsTriggerUrl -Credential $AdminCredentials
+$username = "tfsintegration"
+$password = "m8kemew0rk"
+$URL = $JenkinsUrl.Host
+$Port = $JenkinsUrl.Port
+
+$h = @{}
+$h.Add('Authorization', 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$(${username}):$(${password})")))
+
+$Params = @{uri = "http://${URL}:${Port}/crumbIssuer/api/json";
+            Method = 'Get';
+            Headers = $h;}
+$API_Crumb = Invoke-RestMethod @Params
+
+$h.Add('Jenkins-Crumb', $API_Crumb.crumb)
+$Params['uri'] = "$Env:JenkinsTriggerUrl"
+$Params['Method'] = 'Post'
+$Params['Headers'] = $h
+
+$result = Invoke-WebRequest @Params
 if($result.StatusCode -ne 201) {
     Write-Error "fail to trigger jenkins auto deploy"
 }
