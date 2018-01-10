e69ece75f57404a82a259e112049a7ceedbef29f
Dan Brinkheden <none@none>
2018-01-10 10:43:22 +0100

    Fixed focus

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/skill-group/skill-group-manager.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/skill-group/skill-group-manager.controller.js
@@ -8,19 +8,20 @@
 		return {
 			//scope: true,   // optionally create a child scope
 			link: function(scope, element, attrs) {
+				//console.log(scope, element, attrs);
 				var model = $parse(attrs.focusMe);
 				scope.$watch(model, function(value) {
 					if (value === true) {
 						$timeout(function() {
 							element[0].focus();
-						}, 100);
+						});
 					}
 				});
 				// to address @blesh's comment, set attribute value to 'false'
 				// on blur event:
-				element.bind('blur', function() {
+				/*element.bind('blur', function() {
 					scope.$apply(model.assign(scope, false));
-				});
+				});*/
 			}
 		};
 	}
@@ -172,8 +173,6 @@
 		};
 
 		vm.saveNameEdit = function(ev) {
-			console.log('isNew', isNew);
-
 			if (isNew) {
 				if (vm.newGroupName && vm.newGroupName.length > 0) {
 					vm.newGroup.Name = vm.newGroupName;
