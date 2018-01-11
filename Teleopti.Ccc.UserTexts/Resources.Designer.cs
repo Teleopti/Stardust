ef895f46215b92750c33fd2c45e9fa2cc7176d37
honglih <none@none>
2018-01-11 16:39:25 +0800

    #40555 add resources

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -24254,6 +24254,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string PickASmallerDatePeriodDot {
             get {
                 return ResourceManager.GetString("PickASmallerDatePeriodDot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Pick a template.
+        /// </summary>
+        public static string PickATemplate {
+            get {
+                return ResourceManager.GetString("PickATemplate", resourceCulture);
             }
         }
         
