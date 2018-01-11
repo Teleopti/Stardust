a36eb55b7a4d26d7e91cd3f39f5c05517cb14f8d
Kunning Mao <none@none>
2018-01-11 10:07:09 +0100

    #47562 make skillprio an exception for permission check

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/app.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/app.js
@@ -167,7 +167,9 @@ var wfm = angular.module('wfm', [
 									hasModulePermission = true;
 								} else if (url && (area.InternalName.indexOf(url) > -1 || url.indexOf(area.InternalName) > -1)) {
 									hasModulePermission = true;
-								} else if ('resourceplanner.importschedule' === next.name || 'resourceplanner.archiveschedule' === next.name) {
+								} else if ('resourceplanner.importschedule' === next.name.toLowerCase() || 'resourceplanner.archiveschedule' === next.name.toLowerCase()) {
+									hasModulePermission = true;
+								} else if ('skillprio' === next.name.toLowerCase()) {
 									hasModulePermission = true;
 								}
 							});
