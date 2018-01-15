f8cb1b9ee7db593e2403f947a7da61de172ae608
honglih <none@none>
2018-01-15 10:47:35 +0800

    #40555 add resources for remove day off

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -25953,6 +25953,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string RemoveActivity {
             get {
                 return ResourceManager.GetString("RemoveActivity", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Remove Day Off.
+        /// </summary>
+        public static string RemoveDayOff {
+            get {
+                return ResourceManager.GetString("RemoveDayOff", resourceCulture);
             }
         }
         
