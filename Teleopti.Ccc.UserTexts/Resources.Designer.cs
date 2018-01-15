019c5bef5a327fdf634e0c04c10d4653c374a40f
Ola <none@none>
2018-01-15 16:33:58 +0100

    STI #47684

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -34391,6 +34391,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string TheGroupHasToBeEmptyBeforeDeleted {
             get {
                 return ResourceManager.GetString("TheGroupHasToBeEmptyBeforeDeleted", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The import failed!.
+        /// </summary>
+        public static string TheImportFailed {
+            get {
+                return ResourceManager.GetString("TheImportFailed", resourceCulture);
             }
         }
         
