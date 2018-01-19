8e54530501da9af9c5a33af5a06a6e6334e68da9
Rockman <none@none>
2018-01-19 09:52:22 +0100

    #47591 #47588 #47647 flaky staffing tests

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/staffing/staffing.controller.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/staffing/staffing.controller.spec.js
@@ -1,63 +1,152 @@
-﻿'use strict';
+'use strict';
 describe('StaffingController', function () {
-    var $httpBackend,
-        $controller,
-        scope,
-        fakeBackend,
-        vm;
+  var $httpBackend,
+  $controller,
+  scope,
+  UtilService,
+  fakeBackend,
+  vm;
 
-    beforeEach(function () {
-        module('wfm.staffing');
+  beforeEach(function () {
+    module('wfm.staffing');
+  });
+
+  beforeEach(inject(function (_$rootScope_, _$httpBackend_, _$controller_, _UtilService_ ) {
+    $httpBackend = _$httpBackend_;
+    $controller = _$controller_;
+    UtilService = _UtilService_;
+    scope = _$rootScope_.$new();
+
+    // $httpBackend.whenGET('../api/').respond(function (method, url, data, headers) {
+    //   return [200, fakeChanges]
+    // });
+
+  }));
+
+  it('should return mdi-alert if do display data is false', function () {
+    var skill = { DoDisplayData: false, SkillType: 'SkillTypeChat' }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
     });
+    var a = vm.dynamicIcon(skill)
 
-    beforeEach(inject(function (_$rootScope_, _$httpBackend_, _$controller_ ) {
-        $httpBackend = _$httpBackend_;
-        $controller = _$controller_;
-        scope = _$rootScope_.$new();
-    }));
+    expect(a).toBe('mdi mdi-alert');
+  });
 
-    it('should return mdi-alert if do display data is false', function () {
-        var skill = { DoDisplayData: false, SkillType: 'SkillTypeChat' }
-
-        var vm = $controller('StaffingController', {
-			$scope: scope
-		});
-        var a = vm.dynamicIcon(skill)
-
-        expect(a).toBe('mdi mdi-alert');
+  it('should toggle overtime settings', function () {
+    var vm = $controller('StaffingController', {
+      $scope: scope
     });
+    vm.toggleOverstaffSettings()
 
-    it('should toggle overtime settings', function () {
-        var vm = $controller('StaffingController', {
-			$scope: scope
-		});
-        vm.toggleOverstaffSettings()
+    expect(vm.showOverstaffSettings).toBe(true);
+    vm.toggleOverstaffSettings()
+    expect(vm.showOverstaffSettings).toBe(false);
+  });
 
-        expect(vm.showOverstaffSettings).toBe(true);
-        vm.toggleOverstaffSettings()
-        expect(vm.showOverstaffSettings).toBe(false);
+  it('should return multisite icon if multisite', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: true }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
     });
+    var a = vm.dynamicIcon(skill)
 
-    it('should return multisite icon if multisite', function () {
-        var skill = { DoDisplayData: true, IsMultisiteSkill: true }
+    expect(a).toBe('mdi mdi-hexagon-multiple');
+  });
 
-        var vm = $controller('StaffingController', {
-			$scope: scope
-		});
-        var a = vm.dynamicIcon(skill)
+  it('should return text icon if chat', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat' }
 
-        expect(a).toBe('mdi mdi-hexagon-multiple');
+    var vm = $controller('StaffingController', {
+      $scope: scope
     });
+    var a = vm.dynamicIcon(skill)
 
-    it('should return text icon if chat', function () {
-        var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat' }
+    expect(a).toBe('mdi mdi-message-text-outline');
+  });
 
-        var vm = $controller('StaffingController', {
-			$scope: scope
-		});
-        var a = vm.dynamicIcon(skill)
 
-        expect(a).toBe('mdi mdi-message-text-outline');
+  it('should be able to use shrinkage', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat', Id:'123'  }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
     });
+    var a = vm.dynamicIcon(skill)
+
+    vm.selectedSkill = skill;
+    vm.useShrinkageForStaffing();
+
+    expect(vm.useShrinkage).toBe(true);
+    expect(vm.hasSuggestionData).toBe(false);
+  });
+
+  it('should be able to view staffing for selected date ', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat', Id:'123'  }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
+    });
+    var a = vm.dynamicIcon(skill)
+
+    spyOn(vm, 'generateChart').and.callThrough();
+    vm.selectedSkill = skill;
+    vm.navigateToNewDay();
+
+    expect(vm.showOverstaffSettings).toBe(false);
+    expect(vm.generateChart).toHaveBeenCalled();
+  });
+
+  it('should be able to use use overstaff settings', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat', Id:'123'  }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
+    });
+    var a = vm.dynamicIcon(skill)
+    vm.showOverstaffSettings = false;
+
+    vm.selectedSkill = skill;
+    vm.toggleOverstaffSettings()
+
+    expect(vm.showOverstaffSettings).toBe(true);
+  });
+
+  it('should be able to use see overstaff settings', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat', Id:'123'  }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
+    });
+    var a = vm.dynamicIcon(skill)
+    vm.showOverstaffSettings = false;
+
+    vm.selectedSkill = skill;
+    vm.toggleOverstaffSettings()
+
+    expect(vm.showOverstaffSettings).toBe(true);
+  });
+
+  it('should be able to use use overstaff settings', function () {
+    var skill = { DoDisplayData: true, IsMultisiteSkill: false, SkillType: 'SkillTypeChat', Id:'123'  }
+
+    var vm = $controller('StaffingController', {
+      $scope: scope
+    });
+    var a = vm.dynamicIcon(skill)
+
+    vm.selectedSkill = skill;
+    vm.selectedSkillChange(skill);
+    vm.overtimeForm = {
+      Compensation:"9019d62f-0086-44b1-a977-9bb900b8c361",
+      MinMinutesToAdd: new Date(),
+      MaxMinutesToAdd: new Date()
+    }
+
+    vm.suggestOvertime(vm.overtimeForm);
+    // expect query to have been called without error
+  });
 
 });
--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Staffing/Staffing.feature
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Staffing/Staffing.feature
@@ -9,18 +9,10 @@ Background:
 Given I have a role with
 | Field              | Value |
 | Access to Staffing | True  |
+And I have a role with full access
 And There is a skill to monitor called 'Skill A' with queue id '1' and queue name 'queue1' and activity 'Phone'
 And There is a skill to monitor called 'Skill B' with queue id '2' and queue name 'queue2' and activity 'Chat'
 And There is a compensation with name 'Comparera'
-
-
-Scenario: View staffing for one skill on selected date
-	Given the time is '2017-09-25 08:00'
-	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
-	And there is staffing data for skills 'Skill A' for date '2017-09-25'
-	When I am viewing staffing page
-	And I change staffing date to '2017-09-25'
-	Then I should see staffing data in the graph
 
 Scenario: View staffing for one skillgroup
 	Given the time is '2017-09-25 08:00'
@@ -34,30 +26,6 @@ Scenario: View staffing for one skillgroup
 	And I change staffing date to '2017-09-25'
 	Then I should see the selected skill group 'Group A' 
 	And I should see staffing data in the graph
-
-@OnlyRunIfEnabled('WfmStaffing_AddOvertime_42524')
-Scenario: Show overtime settings
-	Given the time is '2017-09-25 08:00'
-	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
-	And there is staffing data for skills 'Skill A' for date '2017-09-25'
-	When I am viewing staffing page
-	And I change staffing date to '2017-09-25'
-	And I can not see overtime settings
-	And I press the get suggestions for overtime button
-	Then I should see overtime settings
-
-Scenario: Use shrinkage when toggling shrinkage to on
-	Given the time is '2017-09-25 08:00'
-	And there is queue statistics for skill 'Skill A' until '2017-09-27 17:00'
-	And there is staffing data for skills 'Skill A' for date '2017-09-25'
-	When I am viewing staffing page
-	And I change staffing date to '2017-09-25'
-	And I see staffing data in the graph
-	And Using shrinkage is off
-	And I turn using shrinkage to on
-	Then Using shrinkage should be used
-	#Then I should see the staffing chart update // How?
-	
 	
 @OnlyRunIfEnabled('Staffing_BPOExchangeImport_45202')
 Scenario: Show bpo exchange page
@@ -65,10 +33,10 @@ Scenario: Show bpo exchange page
 	When I press the BPO exchange button 
 	Then I should see the bpo exchange page
 
-
-#Scenario: Show manage skill group page
-#	Given I am viewing staffing page
-#	When I press the create skill button 
-#	Then I should see the manage skill group page
+@OnlyRunIfEnabled('WFM_Unified_Skill_Group_Management_45417')
+Scenario: Show manage skill group page
+	Given I am viewing staffing page
+	When I press the create skill button 
+	Then I should see the manage skill group page
 
 #Shrinkage 
