2a3d7de4c7099e81d9ab4bdd76b192acfa64652c
oskare <none@none>
2018-01-19 10:06:29 +0100

    #47678 Moving some Erlang tests from secrets to domain.

--- DomainTest/Forecasting/StaffingCalculatorServiceFacadeTest.cs
+++ DomainTest/Forecasting/StaffingCalculatorServiceFacadeTest.cs
@@ -75,5 +75,54 @@ namespace Teleopti.Ccc.DomainTest.Forecasting
 			var esl = _target.LinearEsl(0, 0, .8);
 			esl.Should().Be.EqualTo(0);
 		}
+
+		[Test]
+		public void TestMultipleCombinationsForCallsAndAht([Values(0.1,1,10,100,1000,10000)] double tasks, [Values(0.1,1,10,100,1000,10000)] double aht)
+
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, tasks, aht, TimeSpan.FromMinutes(15), 0, 1, 1);
+			agents.Should().Not.Be.EqualTo(double.NaN);
+		}
+
+		[Test]
+		public void ErlangCErlangAComparison()
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, 37, 320, TimeSpan.FromMinutes(15), 0.3, 0.9, 1);
+			Math.Round(agents,2).Should().Be.EqualTo(16.87);
+		}
+		[Test]
+		public void ErlangCErlangAComparison1()
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, 38, 320, TimeSpan.FromMinutes(15), 0.3, 0.9, 1);
+			Math.Round(agents, 1).Should().Be.EqualTo(17.3);
+		}
+
+		[Test]
+		public void ErlangCErlangAComparison2()
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, 40, 3.75, TimeSpan.FromMinutes(15), 0.3, 0.9, 1);
+			Math.Round(agents, 2).Should().Be.EqualTo(0.56);
+		}
+
+		[Test]
+		public void ErlangCErlangAComparison3()
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, 17, 320, TimeSpan.FromMinutes(30), 0.3, 0.9, 1);
+			Math.Round(agents, 1).Should().Be.EqualTo(5.1);
+		}
+
+		[Test]
+		public void ErlangCErlangAComparison4()
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, 8, 320, TimeSpan.FromMinutes(30), 0.3, 0.9, 1);
+			Math.Round(agents, 2).Should().Be.EqualTo(2.97);
+		}
+
+		[Test]
+		public void ErlangCErlangAComparison5()
+		{
+			var agents = _target.AgentsUseOccupancy(.8, 20, 32, 10, TimeSpan.FromMinutes(30), 0.3, 0.9, 1);
+			Math.Round(agents, 2).Should().Be.EqualTo(0.59);
+		}
 	}
 }
