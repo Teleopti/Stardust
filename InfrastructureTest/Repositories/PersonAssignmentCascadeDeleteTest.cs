0d0fa6ab7a24656f02ddf913f60499b9ff7c8270
Roger Kratz <roger.kratz@teleopti.com>
2018-01-24 14:33:09 +0100

    #47649 Fixing (some) more red tests. Now like real client -> uses transaction when persist

--- InfrastructureTest/Repositories/PersonAssignmentCascadeDeleteTest.cs
+++ InfrastructureTest/Repositories/PersonAssignmentCascadeDeleteTest.cs
@@ -2,105 +2,146 @@ using System.Drawing;
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.TimeLayer;
-using Teleopti.Ccc.InfrastructureTest.Helper;
+using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.InfrastructureTest.Repositories
 {
-	[TestFixture]
-	public class PersonAssignmentCascadeDeleteTest : DatabaseTest
+	[InfrastructureTest]
+	public class PersonAssignmentCascadeDeleteTest
 	{
 		/// PersonAssignment
 		///   0: PersonalLayer
 		///   1: MainLayer
 		///   2: OvertimeLayer
 
-		private IPersonAssignment target;
+		public ICurrentUnitOfWorkFactory CurrentUnitOfWorkFactory;
 
 		[Test]
 		public void DeleteOvertimeShouldGenerateCorrectNumberOfStatements()
 		{
-			target.RemoveActivity(target.OvertimeActivities().First());
-			PersistAndRemoveFromUnitOfWork(target);
-			Session.SessionFactory.Statistics.PrepareStatementCount
-				.Should().Be.EqualTo(2); //delete overtime layer + update personassignment  (no index changes)
-			Session.SessionFactory.Statistics.EntityDeleteCount
-				.Should().Be.EqualTo(1); //delete overtimeshift layer
+			var target = createAssigment();
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				initAssAndClearStatistics(uow, target);
+				target.RemoveActivity(target.OvertimeActivities().First());
+				uow.Merge(target);
+				uow.PersistAll();
+				uow.FetchSession().SessionFactory.Statistics.PrepareStatementCount
+					.Should().Be.EqualTo(2); //delete overtime layer + update personassignment  (no index changes)
+				uow.FetchSession().SessionFactory.Statistics.EntityDeleteCount
+					.Should().Be.EqualTo(1); //delete overtimeshift layer
+			}
 		}
 
 		[Test]
 		public void DeleteMainShouldGenerateCorrectNumberOfStatements()
 		{
-			target.ClearMainActivities();
-			PersistAndRemoveFromUnitOfWork(target);
-			Session.SessionFactory.Statistics.PrepareStatementCount
-				.Should().Be.EqualTo(3); //delete mainshift layer + update personassignment + update overtime index
-			Session.SessionFactory.Statistics.EntityDeleteCount
-				.Should().Be.EqualTo(1); //delete mainshiftlayer
+			var target = createAssigment();
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				initAssAndClearStatistics(uow, target);
+				target.ClearMainActivities();
+				uow.Merge(target);
+				uow.PersistAll();
+				uow.FetchSession().SessionFactory.Statistics.PrepareStatementCount
+					.Should().Be.EqualTo(3); //delete mainshift layer + update personassignment + update overtime index
+				uow.FetchSession().SessionFactory.Statistics.EntityDeleteCount
+					.Should().Be.EqualTo(1); //delete mainshiftlayer
+			}
 		}
 
 		[Test]
 		public void DeletePersonalShouldGenerateCorrectNumberOfStatements()
 		{
-			target.RemoveActivity(target.PersonalActivities().First());
-			PersistAndRemoveFromUnitOfWork(target);
-			Session.SessionFactory.Statistics.PrepareStatementCount
-				.Should().Be.EqualTo(4); //delete overtime layer + update personassignment + update main index + update overtime index
-			Session.SessionFactory.Statistics.EntityDeleteCount
-				.Should().Be.EqualTo(1); //delete personalshiftlayer
+			var target = createAssigment();
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				initAssAndClearStatistics(uow, target);
+				target.RemoveActivity(target.PersonalActivities().First());
+				uow.Merge(target);
+				uow.PersistAll();
+				uow.FetchSession().SessionFactory.Statistics.PrepareStatementCount
+					.Should().Be
+					.EqualTo(4); //delete overtime layer + update personassignment + update main index + update overtime index
+				uow.FetchSession().SessionFactory.Statistics.EntityDeleteCount
+					.Should().Be.EqualTo(1); //delete personalshiftlayer	
+			}
 		}
 
 		[Test]
 		public void DeleteAggregateShouldGenerateOneStatementForEachNonCascadeLeaf()
 		{
-			Session.Delete(target);
-			Session.Flush();
-			Session.SessionFactory.Statistics.PrepareStatementCount
-				.Should().Be.EqualTo(1); //delete pers assignment, (no layers - cascading)
-			Session.SessionFactory.Statistics.EntityDeleteCount
-				.Should().Be.EqualTo(4); //delete pers assignment, otlayer, mslayer, pslayer
+			var target = createAssigment();
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				initAssAndClearStatistics(uow, target);
+				PersonAssignmentRepository.Remove(target);
+				uow.PersistAll();
+				uow.FetchSession().SessionFactory.Statistics.PrepareStatementCount
+					.Should().Be.EqualTo(1); //delete pers assignment, (no layers - cascading)
+				uow.FetchSession().SessionFactory.Statistics.EntityDeleteCount
+					.Should().Be.EqualTo(4); //delete pers assignment, otlayer, mslayer, pslayer
+			}
 		}
 
-
-
-		//puhh - vi har såinih***ete med beroenden...
-		protected override void SetupForRepositoryTest()
+		private static void initAssAndClearStatistics(IUnitOfWork uow, IPersonAssignment personAssignment)
 		{
-			var dummyCat = ShiftCategoryFactory.CreateShiftCategory("dummyCat");
-			var dummyActivity = new Activity("dummy") { DisplayColor = Color.DodgerBlue };
-			PersistAndRemoveFromUnitOfWork(dummyActivity);
-			var dummyAgent = PersonFactory.CreatePerson("m");
-			var dummyScenario = ScenarioFactory.CreateScenarioAggregate("Default", false);
-			var dummyCategory = ShiftCategoryFactory.CreateShiftCategory("Morning");
-			var definitionSet = new MultiplicatorDefinitionSet("sdf", MultiplicatorType.Overtime);
-
-			PersistAndRemoveFromUnitOfWork(dummyCategory);
-			PersistAndRemoveFromUnitOfWork(dummyScenario);
-			PersistAndRemoveFromUnitOfWork(dummyCat);
-			PersistAndRemoveFromUnitOfWork(definitionSet);
-
-			PersonFactory.AddDefinitionSetToPerson(dummyAgent, definitionSet);
-			var per = dummyAgent.Period(new DateOnly(2000, 1, 1));
-			var site = SiteFactory.CreateSimpleSite("df");
-			PersistAndRemoveFromUnitOfWork(site);
-			per.Team.Site = site;
-			PersistAndRemoveFromUnitOfWork(per.PersonContract.PartTimePercentage);
-			PersistAndRemoveFromUnitOfWork(per.PersonContract.ContractSchedule);
-			PersistAndRemoveFromUnitOfWork(per.PersonContract.Contract);
-			PersistAndRemoveFromUnitOfWork(per.Team);
-			PersistAndRemoveFromUnitOfWork(dummyAgent);
-
-			target = PersonAssignmentFactory.CreateAssignmentWithMainShiftAndPersonalShift(dummyAgent,
-										dummyScenario, dummyActivity, new DateTimePeriod(2000, 1, 1, 2000, 1, 2), dummyCat);
-			target.AddOvertimeActivity(dummyActivity, new DateTimePeriod(2000, 1, 1, 2000, 1, 2), definitionSet);
-			Session.Save(target);
-			Session.Flush();
-			Session.SessionFactory.Statistics.Clear();
+			uow.Reassociate(personAssignment);
+			personAssignment.ShiftLayers.ToArray();
+			uow.FetchSession().SessionFactory.Statistics.Clear();
 		}
 
+		public IActivityRepository ActivityRepository;
+		public IShiftCategoryRepository ShiftCategoryRepository;
+		public IScenarioRepository ScenarioRepository;
+		public IMultiplicatorDefinitionSetRepository MultiplicatorDefinitionSetRepository;
+		public ISiteRepository SiteRepository;
+		public IPersonAssignmentRepository PersonAssignmentRepository;
+		public IPersonRepository PersonRepository;
+		public ITeamRepository TeamRepository;
+		public IPartTimePercentageRepository PartTimePercentageRepository;
+		public IContractRepository ContractRepository;
+		public IContractScheduleRepository ContractScheduleRepository;
+		
+		private IPersonAssignment createAssigment()
+		{
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				var dummyCat = ShiftCategoryFactory.CreateShiftCategory("dummyCat");
+				var dummyActivity = new Activity("dummy") {DisplayColor = Color.DodgerBlue};
+				ActivityRepository.Add(dummyActivity);
+				var dummyAgent = PersonFactory.CreatePerson("m");
+				var dummyScenario = ScenarioFactory.CreateScenarioAggregate("Default", false);
+				var definitionSet = new MultiplicatorDefinitionSet("sdf", MultiplicatorType.Overtime);
+				ScenarioRepository.Add(dummyScenario);
+				ShiftCategoryRepository.Add(dummyCat);
+				MultiplicatorDefinitionSetRepository.Add(definitionSet);
+				PersonFactory.AddDefinitionSetToPerson(dummyAgent, definitionSet);
+				var per = dummyAgent.Period(new DateOnly(2000, 1, 1));
+				var site =new Site("_");
+				per.Team.Site = site;
+				SiteRepository.Add(site);
+				TeamRepository.Add(per.Team);
+				PartTimePercentageRepository.Add(per.PersonContract.PartTimePercentage);
+				ContractRepository.Add(per.PersonContract.Contract);
+				ContractScheduleRepository.Add(per.PersonContract.ContractSchedule);
+				PersonRepository.Add(dummyAgent);
+				var ass = PersonAssignmentFactory.CreateAssignmentWithMainShiftAndPersonalShift(dummyAgent,
+					dummyScenario, dummyActivity, new DateTimePeriod(2000, 1, 1, 2000, 1, 2), dummyCat);
+				ass.AddOvertimeActivity(dummyActivity, new DateTimePeriod(2000, 1, 1, 2000, 1, 2), definitionSet);
+				PersonAssignmentRepository.Add(ass);
+				uow.PersistAll();
+				return ass;
+			}
+		}
 	}
 }
