bb20b89085a03d81ecf381f813421f83e7309e32
LevelUp! <levelup@teleopti.com>
2018-01-24 16:12:17 +0100

    #47230 Using backtick

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical/_rta.card.panel.component.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical/_rta.card.panel.component.js
@@ -11,10 +11,14 @@
 				'header': 'rtaCardPanelHeader',
 				'content': 'rtaCardPanelContent'
 			},
-			template: 
-				'<div class="panel material-depth-1">' +
-				'<div class="card-panel-header-wrapper pointer" ng-transclude="header" ng-click="$ctrl.open = !$ctrl.open" ng-style="{\'border-left\': \'10px solid \' + $ctrl.leftBorderColor}"></div>' +
-				'<div class="card-panel-content-wrapper" ng-transclude="content" ng-if="$ctrl.open"></div>' +
-				'</div>'
+			template: `
+				<div class="panel material-depth-1">
+					<div class="card-panel-header-wrapper pointer"
+						ng-transclude="header" 
+						ng-click="$ctrl.open = !$ctrl.open" 
+						ng-style="{'border-left': '10px solid ' + $ctrl.leftBorderColor}"></div>
+					<div class="card-panel-content-wrapper" ng-transclude="content" ng-if="$ctrl.open"></div>
+				</div>
+				`
 		});
 })();
