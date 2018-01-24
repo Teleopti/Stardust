972d905dd37339c2d7e93ae3c06154812dc5c117
Roger Kratz <roger.kratz@teleopti.com>
2018-01-24 10:39:00 +0100

    #47645 Skillday "number" in test now is consistent.

--- DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationIslandDesktopTest.cs
+++ DomainTest/SchedulingScenarios/IntradayOptimization/IntradayOptimizationIslandDesktopTest.cs
@@ -683,14 +683,14 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.IntradayOptimization
 			}
 			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(phone, new TimePeriodWithSegment(21, 15, 21, 15, 15), new TimePeriodWithSegment(30, 15, 30, 15, 15), new ShiftCategory("_").WithId()));
 			ruleSet.AddExtender(new ActivityAbsoluteStartExtender(other, new TimePeriodWithSegment(0, 15, 0, 15, 15), new TimePeriodWithSegment(30, 0, 30, 0, 15)));
-			var skillDayPhone1 = phoneSkill.CreateSkillDayWithDemand(scenario, dateOnly, 10);
-			var skillDayPhone2 = phoneSkill.CreateSkillDayWithDemand(scenario, dateOnly.AddDays(1), 100);
+			var skillDayPhone2 = phoneSkill.CreateSkillDayWithDemand(scenario, dateOnly, 10);
+			var skillDayPhone3 = phoneSkill.CreateSkillDayWithDemand(scenario, dateOnly.AddDays(1), 100);
 			var skillDayOther1 = otherActivity.CreateSkillDayWithDemand(scenario, dateOnly.AddDays(-1), 0.1);
 			var skillDayOther2 = otherActivity.CreateSkillDayWithDemand(scenario, dateOnly, 0.1);
 			var skillDayOther3 = otherActivity.CreateSkillDayWithDemand(scenario, dateOnly.AddDays(1), 0.1);
 			var agent = new Person().WithId().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(ruleSet, new ContractWithMaximumTolerance(), phoneSkill, otherActivity).WithSchedulePeriodOneWeek(dateOnly);
 			var ass = new PersonAssignment(agent, scenario, dateOnly).ShiftCategory(new ShiftCategory("_").WithId()).WithLayer(phone, new TimePeriod(13, 22));
-			var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(dateOnly.AddDays(-1), dateOnly.AddDays(1)), agent, ass, new[] { skillDayPhone1, skillDayPhone2, skillDayOther1, skillDayOther2, skillDayOther3 });
+			var schedulerStateHolderFrom = SchedulerStateHolderFrom.Fill(scenario, new DateOnlyPeriod(dateOnly.AddDays(-1), dateOnly.AddDays(1)), agent, ass, new[] { skillDayPhone2, skillDayPhone3, skillDayOther1, skillDayOther2, skillDayOther3 });
 			var optimizationPreferences = new OptimizationPreferences { General = { ScheduleTag = new ScheduleTag(), OptimizationStepShiftsWithinDay = true } };
 
 			Target.Execute(new NoSchedulingProgress(), schedulerStateHolderFrom, new[] { agent }, new DateOnlyPeriod(dateOnly, dateOnly), optimizationPreferences, null);
