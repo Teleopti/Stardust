15d231c3ed195f9c6c86412566ae2419144666b2
MagnusW <none@none>
2018-01-25 13:24:06 +0100

    Changed text for ImportBpo when skill id missing

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -14430,7 +14430,7 @@ namespace Teleopti.Ccc.UserTexts {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Skill with skill id:{0} not found..
+        ///   Looks up a localized string similar to Could not match imported skill to existing skill..
         /// </summary>
         public static string ImportBpoSkillNotFound {
             get {
