827d973b93d74d7d9996673bda1b2ea0f5f8b2a1
Roger Kratz <roger.kratz@teleopti.com>
2018-01-25 14:23:36 +0100

    #47654 Adding an already green test to make sure it'll still work after fix

---
+++ InfrastructureTest/Persisters/Schedules/DeleteOneOfManyLayersTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.Persisters.Schedules
+{
+	public class DeleteOneOfManyLayersTest: ScheduleRangeConflictTest
+	{
+		private readonly DateOnly date = new DateOnly(2000, 1, 1);
+
+		protected override IEnumerable<IPersistableScheduleData> Given()
+		{
+			return new[]
+			{
+				new PersonAssignment(Person, Scenario, date)
+					.WithLayer(Activity, new TimePeriod(1, 10))
+					.WithLayer(Activity, new TimePeriod(2, 10))
+					.WithLayer(Activity, new TimePeriod(3, 10))
+			};
+		}
+
+		protected override void WhenImChanging(IScheduleRange myScheduleRange)
+		{
+			var day = myScheduleRange.ScheduledDay(new DateOnly(2000, 1, 1));
+			var layer2remove = day.PersonAssignment().ShiftLayers.First();
+			day.PersonAssignment().RemoveActivity(layer2remove);
+			DoModify(day);
+		}
+
+		protected override void Then(IScheduleRange myScheduleRange)
+		{
+			myScheduleRange.ScheduledDay(date).PersonAssignment().ShiftLayers
+				.Select(x => x.Period.StartDateTime.Hour)
+				.Should().Have.SameSequenceAs(2, 3);
+		}
+	}
+}
