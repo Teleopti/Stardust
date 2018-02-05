d1b001c2e9cec52ecd8854536c6fce9767123aca
codemonkeys <junwen.luo@teleopti.com>
2018-02-05 16:06:26 +0800

    PBI #47964: Add toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -142,6 +142,7 @@
 		OvertimeRequestPeriodSkillTypeSetting_47290,
 		OvertimeRequestCheckCalendarMonthMaximumOvertime_47024,
 		OvertimeRequestUseMostUnderStaffedSkill_47853,
+		OvertimeRequestMaxContinuousWorkTime_47964,
 		ETL_SpeedUpNightlyDayOff_38213,
 		MyTimeWeb_MobileResponsive_43826,
 		Wfm_Intraday_SupportSkillTypeEmail_44002,
