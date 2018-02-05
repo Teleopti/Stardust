3becedfc97afabb0f38bbbae42d0582581ab7fcb
LevelUp! <levelup@teleopti.com>
2018-02-05 10:03:39 +0100

    #47230 Minor refacts

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical/rta.timeline.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical/rta.timeline.service.js
@@ -1,27 +1,29 @@
 ﻿(function () {
 	'use strict';
-	
+
 	angular.module('wfm.rta')
 		.service('RtaTimeline', function () {
 
-			function positionForTime(offsetTime, time, totalSeconds) {
+			function positionForTime(offsetTime, time, timelineTotalSeconds) {
 				var diff = moment(time).diff(moment(offsetTime), 'seconds');
-				return (diff / totalSeconds) * 100 + '%';
+				return (diff / timelineTotalSeconds) * 100 + '%';
 			}
 
-			function makeOffsetCalculator(offsetTime, totalSeconds) {
+			function makeOffsetCalculator(timelineStart, timelineEnd) {
 				return function (time) {
-					if (moment(time).isBefore(offsetTime))
+					if (moment(time).isBefore(timelineStart))
 						return '0%';
-					return positionForTime(offsetTime, time, totalSeconds);
+					var timelineTotalSeconds = timelineEnd.diff(timelineStart, 'seconds');
+					return positionForTime(timelineStart, time, timelineTotalSeconds);
 				}
 			}
 
-			function makeWidthCalculator(offsetTime, totalSeconds) {
+			function makeWidthCalculator(timelineStart, timelineEnd) {
 				return function (start, end) {
-					if (moment(start).isBefore(offsetTime))
-						start = offsetTime;
-					return positionForTime(start, end, totalSeconds);
+					if (moment(start).isBefore(timelineStart))
+						start = timelineStart;
+					var timelineTotalSeconds = timelineEnd.diff(timelineStart, 'seconds');
+					return positionForTime(start, end, timelineTotalSeconds);
 				}
 			}
 
@@ -29,10 +31,9 @@
 				makeCalculator: function (timelineStart, timelineEnd) {
 					timelineStart = moment(timelineStart);
 					timelineEnd = moment(timelineEnd);
-					var totalSeconds = timelineEnd.diff(timelineStart, 'seconds');
 					return {
-						Offset: makeOffsetCalculator(timelineStart, totalSeconds),
-						Width: makeWidthCalculator(timelineStart, totalSeconds),
+						Offset: makeOffsetCalculator(timelineStart, timelineEnd),
+						Width: makeWidthCalculator(timelineStart, timelineEnd),
 					}
 				}
 			}
