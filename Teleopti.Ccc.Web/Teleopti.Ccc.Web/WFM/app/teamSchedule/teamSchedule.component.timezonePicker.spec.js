1806296c7816c935de49948ce9094eb5203bf5c2
XiaojuD <none@none>
2018-02-05 15:30:38 +0800

    #47993, show timezone list order by name.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.component.timezonePicker.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.component.timezonePicker.spec.js
@@ -1,9 +1,10 @@
 ﻿describe('timezonePicker component tests', function() {
 
-	var $componentController;
+	var $componentController,
+		$rootScope,
+		$compile;
 
 
-	beforeEach(module('wfm.teamSchedule'));
 	var mockCurrentUserInfo = {
 		CurrentUserInfo: function () {
 			return { DefaultTimeZone: "Asia/Hong_Kong" };
@@ -11,6 +12,7 @@
 	};
 
 	beforeEach(function () {
+		module('wfm.templates');
 		module("wfm.teamSchedule");
 		module(function ($provide) {
 			$provide.service('CurrentUserInfo', function () {
@@ -19,8 +21,10 @@
 		});
 	});
 
-	beforeEach(inject(function (_$componentController_) {
+	beforeEach(inject(function (_$componentController_, _$rootScope_, _$compile_) {
 		$componentController = _$componentController_;
+		$rootScope = _$rootScope_;
+		$compile = _$compile_;
 	}));
 
 	it('should expose default timezone to be currentUserInfo timezone', function() {
@@ -93,5 +97,38 @@
 		ctrl.onSelectionChanged();
 		expect(triggeredTimezone).toEqual("Asia/Shanghai");
 	});
+
+	it("should list timezone order by displayName", function () {
+		var panel = setUp();
+		var timezoneSelect = panel[0].querySelectorAll("md-select");
+		var timezoneOptions = panel[0].querySelectorAll("md-option");
+
+		expect(timezoneSelect.length).toEqual(1);
+		expect(timezoneOptions.length).toEqual(3);
+		expect(timezoneOptions[0].innerText.trim()).toEqual("(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna");
+		expect(timezoneOptions[1].innerText.trim()).toEqual("(UTC+08:00) Beijing, Chongqing, Hong Kong, Urumqi");
+		expect(timezoneOptions[2].innerText.trim()).toEqual("");
+		
+	});
+
+	function setUp() {
+		var scope = $rootScope.$new();
+		var html = '<timezone-picker class="timezone-picker" on-pick="onPick" available-timezones="availableTimezones"></timezone-picker> ';
+		scope.availableTimezones = [
+			{
+				IanaId: "Asia/Shanghai",
+				DisplayName: "(UTC+08:00) Beijing, Chongqing, Hong Kong, Urumqi"
+			}, {
+				IanaId: "Europe/Berlin",
+				DisplayName: "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna"
+			}];
+		scope.onPick = {};
+
+		var element = $compile(html)(scope);
+		scope.$apply();
+
+		return element;
+
+	}
 
 });
