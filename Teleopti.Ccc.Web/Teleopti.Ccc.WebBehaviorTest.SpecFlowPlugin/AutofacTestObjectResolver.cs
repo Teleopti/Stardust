4c79c9e35b9efe5f0a6eae13e8592cf6af73866c
LevelUp! <levelup@teleopti.com>
2018-02-06 10:28:00 +0100

    Backin: Dependency injection for step definitions - Another guess attempt

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest.SpecFlowPlugin/AutofacTestObjectResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+using BoDi;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace Teleopti.Ccc.WebBehaviorTest.SpecFlowPlugin
+{
+	public class AutofacTestObjectResolver : ITestObjectResolver
+	{
+		public object ResolveBindingInstance(Type bindingType, IObjectContainer scenarioContainer)
+		{
+			return scenarioContainer
+				.Resolve<IComponentContext>()
+				.Resolve(bindingType);
+		}
+	}
+}
