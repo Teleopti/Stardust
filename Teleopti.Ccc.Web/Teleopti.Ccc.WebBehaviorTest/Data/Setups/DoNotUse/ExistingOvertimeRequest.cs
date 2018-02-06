6fe49453d5e80a0e1c64069d7f001792edf332bf
codemonkeys <junwen.luo@teleopti.com>
2018-02-06 10:47:04 +0800

    PBI #47964: fix scenario

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/DoNotUse/ExistingOvertimeRequest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Data/Setups/DoNotUse/ExistingOvertimeRequest.cs
@@ -18,13 +18,13 @@ namespace Teleopti.Ccc.WebBehaviorTest.Data.Setups.DoNotUse
 
 		public void Apply(ICurrentUnitOfWork unitOfWork, IPerson person, CultureInfo cultureInfo)
 		{
-			var today = DateTime.UtcNow.Date;
+			var today = DateTime.UtcNow;
 
 			var multiplicatorDefinitionSetRepository = new MultiplicatorDefinitionSetRepository(unitOfWork);
 			var multiplicatorDefinitionSet = new MultiplicatorDefinitionSet("test", MultiplicatorType.Overtime);
 			multiplicatorDefinitionSetRepository.Add(multiplicatorDefinitionSet);
 
-			var overtimeRequest = new OvertimeRequest(multiplicatorDefinitionSet, new DateTimePeriod(today, today.AddHours(5)));
+			var overtimeRequest = new OvertimeRequest(multiplicatorDefinitionSet, new DateTimePeriod(today.AddHours(10), today.AddHours(11)));
 			PersonRequest = new PersonRequest(person, overtimeRequest) {Subject = Subject };
 			PersonRequest.TrySetMessage(
 				"This is just a short text that doesn't say anything, except explaining that it doesn't say anything");
