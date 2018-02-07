7e7094fdc2f5bf884211d440b6244d823fae47a8
codemonkeys <junwen.luo@teleopti.com>
2018-02-07 15:07:18 +0800

    PBI #47964: refactor and add more tests

--- Domain/ApplicationLayer/OvertimeRequests/OvertimeRequestMaximumContinuousWorkTimeValidator.cs
+++ Domain/ApplicationLayer/OvertimeRequests/OvertimeRequestMaximumContinuousWorkTimeValidator.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.UserTexts;
 using Teleopti.Interfaces.Domain;
 
@@ -28,79 +30,29 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.OvertimeRequests
 				return validResult();
 			}
 
-			var maximumContinuousWorkTime = person.WorkflowControlSet.OvertimeRequestMaximumContinuousWorkTime;
+			var maximumContinuousWorkTime =
+				person.WorkflowControlSet.OvertimeRequestMaximumContinuousWorkTime.GetValueOrDefault(TimeSpan.Zero);
 			var timezone = person.PermissionInformation.DefaultTimeZone();
 			var dic = getScheduleDictionary(context.PersonRequest.Request.Period.ToDateOnlyPeriod(timezone), person);
-
+			var scheduleRange = dic[person];
 			var requestPeriod = context.PersonRequest.Request.Period;
-			var personAssignment = dic[person].ScheduledDay(new DateOnly(requestPeriod.StartDateTimeLocal(timezone))).PersonAssignment();
 
-			if (personAssignment == null)
+			var shiftLayers = loadShiftLayers(scheduleRange, requestPeriod, timezone);
+
+			var continuousWorkTimeInfo = getContinuousWorkTime(shiftLayers, requestPeriod, timezone);
+			if (!isSatisfiedMaximumContinuousWorkTime(continuousWorkTimeInfo, maximumContinuousWorkTime))
 			{
-				personAssignment = dic[person].ScheduledDay(new DateOnly(requestPeriod.StartDateTimeLocal(timezone)).AddDays(-1))
-					.PersonAssignment();
+				return invalidResult(continuousWorkTimeInfo.ContinuousWorkTimePeriod,
+					continuousWorkTimeInfo.ContinuousWorkTime,
+					maximumContinuousWorkTime);
 			}
 
-			var continuousWorkTimePeriod = string.Empty;
-			var continuousWorkTime = TimeSpan.Zero;
+			return validResult();
+		}
 
-			if (personAssignment != null)
-			{
-				var previousShiftLayer = personAssignment.ShiftLayers.FirstOrDefault(shift =>
-					shift.Period.EndDateTime.CompareTo(requestPeriod.StartDateTime) <= 0);
-				var nextShiftLayer = personAssignment.ShiftLayers.FirstOrDefault(shift =>
-					shift.Period.StartDateTime.CompareTo(requestPeriod.EndDateTime) >= 0);
-
-				if (previousShiftLayer != null)
-				{
-					continuousWorkTime = previousShiftLayer.Period.ElapsedTime() + requestPeriod.ElapsedTime();
-					continuousWorkTimePeriod =
-						$"{previousShiftLayer.Period.StartDateTimeLocal(timezone)} - {requestPeriod.EndDateTimeLocal(timezone)}";
-				}
-
-				if (nextShiftLayer != null)
-				{
-					continuousWorkTime = nextShiftLayer.Period.ElapsedTime() + requestPeriod.ElapsedTime();
-					continuousWorkTimePeriod =
-						$"{requestPeriod.StartDateTimeLocal(timezone)} - {nextShiftLayer.Period.EndDateTimeLocal(timezone)}";
-				}
-
-				if (previousShiftLayer == null && nextShiftLayer == null)
-				{
-					continuousWorkTime = requestPeriod.ElapsedTime();
-					continuousWorkTimePeriod =
-						$"{requestPeriod.StartDateTimeLocal(timezone)} - {requestPeriod.EndDateTimeLocal(timezone)}";
-				}
-			}
-			else
-			{
-				continuousWorkTime = requestPeriod.ElapsedTime();
-				continuousWorkTimePeriod =
-					$"{requestPeriod.StartDateTimeLocal(timezone)} - {requestPeriod.EndDateTimeLocal(timezone)}";
-
-				if (requestPeriod.EndDateTimeLocal(timezone).Date.CompareTo(requestPeriod.StartDateTimeLocal(timezone).Date) > 0)
-				{
-					personAssignment = dic[person].ScheduledDay(new DateOnly(requestPeriod.StartDateTimeLocal(timezone)).AddDays(1))
-						.PersonAssignment();
-
-					var nextShiftLayer = personAssignment.ShiftLayers.FirstOrDefault(shift =>
-						shift.Period.StartDateTime.CompareTo(requestPeriod.EndDateTime) >= 0);
-
-					if (nextShiftLayer != null)
-					{
-						continuousWorkTime = nextShiftLayer.Period.ElapsedTime() + requestPeriod.ElapsedTime();
-						continuousWorkTimePeriod =
-							$"{requestPeriod.StartDateTimeLocal(timezone)} - {nextShiftLayer.Period.EndDateTimeLocal(timezone)}";
-					}
-				}
-			}
-
-
-			if (continuousWorkTime.CompareTo(maximumContinuousWorkTime) <= 0)
-			{
-				return validResult();
-			}
-
+		private static OvertimeRequestValidationResult invalidResult(string continuousWorkTimePeriod,
+			TimeSpan continuousWorkTime, TimeSpan maximumContinuousWorkTime)
+		{
 			return new OvertimeRequestValidationResult
 			{
 				InvalidReasons = new[]
@@ -108,9 +60,90 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.OvertimeRequests
 					string.Format(Resources.OvertimeRequestContinuousWorkTimeDenyReason,
 						continuousWorkTimePeriod,
 						DateHelper.HourMinutesString(continuousWorkTime.TotalMinutes),
-						DateHelper.HourMinutesString(maximumContinuousWorkTime.Value.TotalMinutes))
+						DateHelper.HourMinutesString(maximumContinuousWorkTime.TotalMinutes))
 				}
 			};
+		}
+
+		private List<ShiftLayer> loadShiftLayers(IScheduleRange scheduleRange, DateTimePeriod requestPeriod,
+			TimeZoneInfo timezone)
+		{
+			var shiftLayers = new List<ShiftLayer>();
+			var today = new DateOnly(requestPeriod.StartDateTimeLocal(timezone));
+
+			var yesterDayPersonAssignment = scheduleRange.ScheduledDay(today.AddDays(-1)).PersonAssignment();
+			if (yesterDayPersonAssignment != null)
+				shiftLayers.AddRange(yesterDayPersonAssignment.ShiftLayers);
+
+			var todayDayPersonAssignment = scheduleRange.ScheduledDay(today).PersonAssignment();
+			if (todayDayPersonAssignment != null)
+				shiftLayers.AddRange(todayDayPersonAssignment.ShiftLayers);
+
+			var tomorrowPersonAssignment = scheduleRange.ScheduledDay(today.AddDays(1)).PersonAssignment();
+			if (tomorrowPersonAssignment != null)
+				shiftLayers.AddRange(tomorrowPersonAssignment.ShiftLayers);
+
+			return shiftLayers.OrderBy(shift => shift.Period.StartDateTime).ToList();
+		}
+
+		private ContinuousWorkTimeInfo getContinuousWorkTime(List<ShiftLayer> shiftLayers,
+			DateTimePeriod requestPeriod,
+			TimeZoneInfo timezone)
+		{
+			if (!shiftLayers.Any())
+			{
+				return new ContinuousWorkTimeInfo
+				{
+					ContinuousWorkTime = requestPeriod.ElapsedTime(),
+					ContinuousWorkTimePeriod =
+						$"{requestPeriod.StartDateTimeLocal(timezone)} - {requestPeriod.EndDateTimeLocal(timezone)}"
+				};
+			}
+
+			string continuousWorkTimePeriod;
+			TimeSpan continuousWorkTime;
+			var previousShiftLayer = shiftLayers.FirstOrDefault(shift =>
+				shift.Period.EndDateTime.CompareTo(requestPeriod.StartDateTime) <= 0);
+			var nextShiftLayer = shiftLayers.FirstOrDefault(shift =>
+				shift.Period.StartDateTime.CompareTo(requestPeriod.EndDateTime) >= 0);
+
+			if (previousShiftLayer != null && nextShiftLayer != null)
+			{
+				continuousWorkTime = previousShiftLayer.Period.ElapsedTime() + requestPeriod.ElapsedTime() +
+									 nextShiftLayer.Period.ElapsedTime();
+				continuousWorkTimePeriod =
+					$"{previousShiftLayer.Period.StartDateTimeLocal(timezone)} - {nextShiftLayer.Period.EndDateTimeLocal(timezone)}";
+			}
+			else if (previousShiftLayer != null)
+			{
+				continuousWorkTime = previousShiftLayer.Period.ElapsedTime() + requestPeriod.ElapsedTime();
+				continuousWorkTimePeriod =
+					$"{previousShiftLayer.Period.StartDateTimeLocal(timezone)} - {requestPeriod.EndDateTimeLocal(timezone)}";
+			}
+			else if (nextShiftLayer != null)
+			{
+				continuousWorkTime = nextShiftLayer.Period.ElapsedTime() + requestPeriod.ElapsedTime();
+				continuousWorkTimePeriod =
+					$"{requestPeriod.StartDateTimeLocal(timezone)} - {nextShiftLayer.Period.EndDateTimeLocal(timezone)}";
+			}
+			else
+			{
+				continuousWorkTime = requestPeriod.ElapsedTime();
+				continuousWorkTimePeriod =
+					$"{requestPeriod.StartDateTimeLocal(timezone)} - {requestPeriod.EndDateTimeLocal(timezone)}";
+			}
+
+			return new ContinuousWorkTimeInfo
+			{
+				ContinuousWorkTime = continuousWorkTime,
+				ContinuousWorkTimePeriod = continuousWorkTimePeriod
+			};
+		}
+
+		private bool isSatisfiedMaximumContinuousWorkTime(ContinuousWorkTimeInfo continuousWorkTimeInfo,
+			TimeSpan maximumContinuousWorkTime)
+		{
+			return continuousWorkTimeInfo.ContinuousWorkTime.CompareTo(maximumContinuousWorkTime) <= 0;
 		}
 
 		private IScheduleDictionary getScheduleDictionary(DateOnlyPeriod period, IPerson person)
@@ -130,6 +163,13 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.OvertimeRequests
 			{
 				IsValid = true
 			};
+		}
+
+		private class ContinuousWorkTimeInfo
+		{
+			public TimeSpan ContinuousWorkTime { get; set; }
+
+			public string ContinuousWorkTimePeriod { get; set; }
 		}
 	}
 }
--- DomainTest/ApplicationLayer/OvertimeRequests/OvertimeRequestProcessorMaximumContinuousWorkTimeTest.cs
+++ DomainTest/ApplicationLayer/OvertimeRequests/OvertimeRequestProcessorMaximumContinuousWorkTimeTest.cs
@@ -245,6 +245,75 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.OvertimeRequests
 			personRequest.DenyReason.Should().Be.EqualTo(expectedDenyReason);
 		}
 
+		[Test]
+		[Toggle(Domain.FeatureFlags.Toggles.OvertimeRequestMaxContinuousWorkTime_47964)]
+		public void ShouldDenyWhenContinuousWorkTimeExceedsMaximumContinuousWorkTimeWithShiftBeforeAndAfter()
+		{
+			setupPerson(0, 24);
+			setupIntradayStaffingForSkill(setupPersonSkill(new TimePeriod(TimeSpan.Zero, TimeSpan.FromDays(1))), 10d, 8d);
+
+			var person = LoggedOnUser.CurrentUser();
+			var period1 = new DateTimePeriod(2017, 7, 17, 1, 2017, 7, 17, 2);
+			var pa1 = createMainPersonAssignment(person, period1);
+
+			var activity = ActivityFactory.CreateActivity("another activity");
+			var period2 = new DateTimePeriod(2017, 7, 17, 3, 2017, 7, 17, 10);
+			pa1.AddActivity(activity, period2);
+			ScheduleStorage.Add(pa1);
+
+			var workflowControlSet =
+				new WorkflowControlSet
+				{
+					OvertimeRequestMaximumContinuousWorkTimeEnabled = true,
+					OvertimeRequestMaximumContinuousWorkTime = TimeSpan.FromHours(8),
+					OvertimeRequestMaximumContinuousWorkTimeHandleType = OvertimeValidationHandleType.Deny
+				};
+			person.WorkflowControlSet = workflowControlSet;
+
+			var personRequest = createOvertimeRequest(2, 1);
+			getTarget().Process(personRequest, true);
+
+			var expectedDenyReason = buildDenyReason("7/17/2017 1:00:00 AM - 7/17/2017 10:00:00 AM", TimeSpan.FromHours(9),
+				TimeSpan.FromHours(8));
+
+			personRequest.IsDenied.Should().Be.True();
+			personRequest.DenyReason.Should().Be.EqualTo(expectedDenyReason);
+		}
+
+		[Test]
+		[Toggle(Domain.FeatureFlags.Toggles.OvertimeRequestMaxContinuousWorkTime_47964)]
+		public void ShouldDenyWhenContinuousWorkTimeExceedsMaximumContinuousWorkTimeWithOvernightShiftBeforeAndAfter()
+		{
+			setupPerson(0, 24);
+			setupIntradayStaffingForSkill(setupPersonSkill(new TimePeriod(TimeSpan.Zero, TimeSpan.FromDays(1))), 10d, 8d);
+
+			var person = LoggedOnUser.CurrentUser();
+			var period1 = new DateTimePeriod(2017, 7, 16, 23, 2017, 7, 17, 0);
+			var pa1 = createMainPersonAssignment(person, period1);
+
+			var period2 = new DateTimePeriod(2017, 7, 17, 10, 2017, 7, 18, 0);
+			var pa2 = createMainPersonAssignment(person, period2);
+			ScheduleStorage.Add(pa1);
+			ScheduleStorage.Add(pa2);
+
+			var workflowControlSet =
+				new WorkflowControlSet
+				{
+					OvertimeRequestMaximumContinuousWorkTimeEnabled = true,
+					OvertimeRequestMaximumContinuousWorkTime = TimeSpan.FromHours(24),
+					OvertimeRequestMaximumContinuousWorkTimeHandleType = OvertimeValidationHandleType.Deny
+				};
+			person.WorkflowControlSet = workflowControlSet;
+
+			var personRequest = createOvertimeRequest(0, 10);
+			getTarget().Process(personRequest, true);
+
+			var expectedDenyReason = buildDenyReason("7/16/2017 11:00:00 PM - 7/18/2017 12:00:00 AM", TimeSpan.FromHours(25),
+				TimeSpan.FromHours(24));
+
+			personRequest.IsDenied.Should().Be.True();
+			personRequest.DenyReason.Should().Be.EqualTo(expectedDenyReason);
+		}
 
 		private string buildDenyReason(string continuousWorkPeriod, TimeSpan continuousWorkTime, TimeSpan maximumContinuousWorkTime)
 		{
