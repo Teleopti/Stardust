9ace89a0ef54609d247aaa8f62df10bcbec96994
jimmyb <none@none>
2018-02-09 10:45:48 +0100

    Testing reorder startup task and flagging if a startup task has already been run

--- teamcity/Azure/TeleoptiCCC/bin/PatchDatabases.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/PatchDatabases.cmd
@@ -1,3 +1,11 @@
+REM   If Task1_Success.txt exists, then Application 1 is already installed.
+IF EXIST "%RoleRoot%\Task1_Success.txt" (
+  ECHO Application 1 is already installed. Exiting. >> "%TEMP%\StartupLog.txt" 2>&1
+  GOTO Finish
+)
+
+REM   Run your real exe task
+ECHO Running PatchDatabase.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
 ::current dir
 SET DIRECTORY=%~dp0
 ::remove trailer slash
@@ -5,6 +13,25 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 ::execute
-powershell . .\PatchDatabases.ps1  >nul
-exit /b 0
-::exit %ERRORLEVEL%
+powershell . .\PatchDatabases.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+  REM   The application installed without error. Create a file to indicate that the task
+  REM   does not need to be run again.
+
+  ECHO This line will create a file to indicate that Application 1 installed correctly. > "%RoleRoot%\PatchDatabases_Success.txt"
+
+) ELSE (
+  REM   An error occurred. Log the error and exit with the error code.
+
+  DATE /T >> "%TEMP%\StartupLog.txt" 2>&1
+  TIME /T >> "%TEMP%\StartupLog.txt" 2>&1
+  ECHO  An error occurred running task 1. Errorlevel = %ERRORLEVEL%. >> "%TEMP%\StartupLog.txt" 2>&1
+
+  EXIT %ERRORLEVEL%
+)
+
+:Finish
+
+REM   Exit normally.
+EXIT /B 0
--- teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/PrepareConfigAndSignClickOnce.cmd
@@ -1,3 +1,11 @@
+REM   If Task1_Success.txt exists, then Application 1 is already installed.
+IF EXIST "%RoleRoot%\Task1_Success.txt" (
+  ECHO Application 1 is already installed. Exiting. >> "%TEMP%\StartupLog.txt" 2>&1
+  GOTO Finish
+)
+
+REM   Run your real exe task
+ECHO Running PrepareCOnfigAndSignClickOnce >> "%TEMP%\StartupLog.txt" 2>&1
 ::current dir
 SET DIRECTORY=%~dp0
 ::remove trailer slash
@@ -5,10 +13,30 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 ::execute
-powershell . .\PrepareConfigAndSignClickOnce.ps1 >nul
+powershell . .\PrepareConfigAndSignClickOnce.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
 set /A customError=%ERRORLEVEL%
 ::execute custom powershell script
 if exist "%DIRECTORY%\CustomStartup.ps1" (
 powershell -file "%DIRECTORY%\CustomStartup.ps1" >"%DIRECTORY%\CustomStartup.log"
 )
-exit /b 0
+
+IF %ERRORLEVEL% EQU 0 (
+  REM   The application installed without error. Create a file to indicate that the task
+  REM   does not need to be run again.
+
+  ECHO This line will create a file to indicate that Application 1 installed correctly. > "%RoleRoot%\PrepareConfigAndSignClickOnce_Success.txt"
+
+) ELSE (
+  REM   An error occurred. Log the error and exit with the error code.
+
+  DATE /T >> "%TEMP%\StartupLog.txt" 2>&1
+  TIME /T >> "%TEMP%\StartupLog.txt" 2>&1
+  ECHO  An error occurred running task 1. Errorlevel = %ERRORLEVEL%. >> "%TEMP%\StartupLog.txt" 2>&1
+
+  EXIT %ERRORLEVEL%
+)
+
+:Finish
+
+REM   Exit normally.
+EXIT /B 0
