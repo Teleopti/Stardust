6a9b980c99cd1331688e2d8467c56152cd5628b0
David Jonsson <none@none>
2018-02-11 13:41:53 -0700

    bug #48215 - always specify schema name when adding a database object

--- Database/TeleoptiCCC7/Releases/00000869.sql
+++ Database/TeleoptiCCC7/Releases/00000869.sql
@@ -1,7 +1,7 @@
 DROP TABLE [ReadModel].[SourceBpo]
 GO
 
-CREATE TABLE [BusinessProcessOutsourcer](
+CREATE TABLE dbo.[BusinessProcessOutsourcer]( --add dbo
 	[Id] [uniqueidentifier] NOT NULL,
 	[Source] varchar(100) NOT NULL
  CONSTRAINT [PK_BusinessProcessOutsourcer] PRIMARY KEY CLUSTERED 
@@ -14,5 +14,5 @@ GO
 
 ALTER TABLE [ReadModel].[SkillCombinationResourceBpo]
 ADD CONSTRAINT FK_SkillCombinationResourceBpo_BusinessProcessOutsourcer_Id
-foreign key (SourceId) references [BusinessProcessOutsourcer] ([Id])
+foreign key (SourceId) references dbo.[BusinessProcessOutsourcer] ([Id]) --add dbo
 GO
--- Database/TeleoptiCCC7/Releases/00000929.sql
+++ Database/TeleoptiCCC7/Releases/00000929.sql
@@ -1,2 +1,22 @@
-ALTER TABLE BusinessProcessOutsourcer
+--#48215
+--get potential wrong schema
+declare @table varchar(1000) = 'BusinessProcessOutsourcer'
+declare @newschema varchar(1000) = 'dbo'
+declare @sql varchar(8000)
+declare @oldschema varchar(1000)
+select  @oldschema = s.name
+from sys.schemas s
+inner join sys.objects so
+	on so.schema_id = s.schema_id
+where so.[name] = @table
+
+--transfer to 'dbo' if it ended up on the wrong schema
+if (@newschema <> @oldschema)
+begin
+	set @sql = 'alter schema ' + @newschema + ' transfer ' + @oldschema + '.' + @table
+	exec(@sql)
+end
+
+
+ALTER TABLE dbo.BusinessProcessOutsourcer --add dbo
 	ALTER COLUMN [Source] nvarchar(100) not null
