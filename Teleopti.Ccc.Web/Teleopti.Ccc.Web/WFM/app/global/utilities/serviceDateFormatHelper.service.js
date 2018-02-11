99e037fef09c3a02d07b41ba0de652cd2bbe7b4e
honglih <none@none>
2018-02-11 13:31:40 +0800

    service date format helper

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/utilities/serviceDateFormatHelper.service.js
@@ -0,0 +1,30 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('wfm.utilities')
+		.service('serviceDateFormatHelper', serviceDateFormatHelper);
+
+	function serviceDateFormatHelper() {
+		var serviceDateFormat = {
+			dateTime: "YYYY-MM-DD HH:mm",
+			dateOnly: "YYYY-MM-DD",
+			timeOnly: "HH:mm"
+		};
+
+		this.getDateOnly = function (date) {
+			var localeSafeMoment = moment(date).locale('en');
+			return localeSafeMoment.format(serviceDateFormat.dateOnly);
+		}
+
+		this.getTimeOnly = function (date) {
+			var localeSafeMoment = moment(date).locale('en');
+			return localeSafeMoment.format(serviceDateFormat.timeOnly);
+		}
+
+		this.getDateTime = function (date) {
+			var localeSafeMoment = moment(date).locale('en');
+			return localeSafeMoment.format(serviceDateFormat.dateTime);
+		}
+	}
+})();
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/global/utilities/serviceDateFormatHelper.service.spec.js
@@ -0,0 +1,85 @@
+﻿describe('<serviceDateFormatHelper>', function () {
+	var serviceDateFormatHelper;
+	beforeEach(module('wfm.utilities'));
+	beforeEach(inject(function ($injector) {
+		serviceDateFormatHelper = $injector.get('serviceDateFormatHelper');
+	}));
+
+
+	it('should get correct service date only', function () {
+		var dateString = serviceDateFormatHelper.getDateOnly(new Date(2018, 1, 11));
+		expect(dateString).toEqual('2018-02-11');
+	});
+
+	it('should get correct service time only', function () {
+		var timeString = serviceDateFormatHelper.getTimeOnly(new Date(2018, 1, 11, 10, 30));
+		expect(timeString).toEqual('10:30');
+	});
+
+	it('should get correct service date time', function () {
+		var dateTimeString = serviceDateFormatHelper.getDateTime(new Date(2018, 1, 11, 10, 30));
+		expect(dateTimeString).toEqual('2018-02-11 10:30');
+	});
+});
+
+describe('#serviceDateFormatHelper for ar-AE#', function () {
+	var serviceDateFormatHelper;
+	beforeEach(module('wfm.utilities'));
+	beforeEach(inject(function ($injector) {
+		serviceDateFormatHelper = $injector.get('serviceDateFormatHelper');
+	}));
+
+	beforeEach(function () {
+		moment.locale('ar-AE');
+	});
+
+	afterEach(function () {
+		moment.locale('en');
+	});
+
+	it('should get correct service date string', function () {
+		var dateString = serviceDateFormatHelper.getDateOnly(new Date(2018, 1, 11));
+		expect(dateString).toEqual('2018-02-11');
+	});
+
+	it('should get correct service time only', function () {
+		var timeString = serviceDateFormatHelper.getTimeOnly(new Date(2018, 1, 11, 10, 30));
+		expect(timeString).toEqual('10:30');
+	});
+
+	it('should get correct service date time', function () {
+		var dateTimeString = serviceDateFormatHelper.getDateTime(new Date(2018, 1, 11, 10, 30));
+		expect(dateTimeString).toEqual('2018-02-11 10:30');
+	});
+});
+
+describe('#serviceDateFormatHelper for Persian#', function () {
+	var serviceDateFormatHelper;
+	beforeEach(module('wfm.utilities'));
+	beforeEach(inject(function ($injector) {
+		serviceDateFormatHelper = $injector.get('serviceDateFormatHelper');
+	}));
+
+	beforeEach(function () {
+		moment.locale('fa-IR');
+	});
+
+	afterEach(function () {
+		moment.locale('en');
+	});
+
+	it('should get correct service date string', function () {
+		var dateString = serviceDateFormatHelper.getDateOnly(new Date(2018, 1, 11));
+		expect(dateString).toEqual('2018-02-11');
+	});
+
+	it('should get correct service time only', function () {
+		var timeString = serviceDateFormatHelper.getTimeOnly(new Date(2018, 1, 11, 10, 30));
+		expect(timeString).toEqual('10:30');
+	});
+
+	it('should get correct service date time', function () {
+		var dateTimeString = serviceDateFormatHelper.getDateTime(new Date(2018, 1, 11, 10, 30));
+		expect(dateTimeString).toEqual('2018-02-11 10:30');
+	});
+});
