51391fa88ee120e28a155b7b0884479aaed9fddd
jimmyb <none@none>
2018-02-12 09:23:49 +0100

    Fix restartservice logic for cloud

--- teamcity/Azure/TeleoptiCCC/bin/RestartService.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/RestartService.cmd
@@ -1,3 +1,4 @@
+SET PROCESSNAME=RestartService
 ::current dir
 SET DIRECTORY=%~dp0
 ::remove trailer slash
@@ -6,11 +7,12 @@ SET DIRECTORY=%DIRECTORY:~0,-1%
 IF EXIST "%DIRECTORY%\PendingReboot.txt" (
   del %DIRECTORY%\PendingReboot.txt /f
   Shutdown /r /t 0
+  exit /b 0
 )
 
 ::allow un-signed
 powershell set-executionpolicy unrestricted
 ::execute
-powershell . .\RestartService.ps1  >nul
+powershell . %DIRECTORY%\%PROCESSNAME%.ps1 >> "%DIRECTORY%\StartupLog.txt" 2>&1
 exit /b 0
 
