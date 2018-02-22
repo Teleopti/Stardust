48329373b235383773ff59696b09c0ce17762969
Roger Kratz <roger.kratz@teleopti.com>
2018-02-22 17:12:57 +0100

    Removing dead ISwapAndModifyService.Swap

--- Domain/ResourceCalculation/ISwapAndModifyService.cs
+++ Domain/ResourceCalculation/ISwapAndModifyService.cs
@@ -8,8 +8,5 @@ namespace Teleopti.Ccc.Domain.ResourceCalculation
     public interface ISwapAndModifyService
     {
         IEnumerable<IBusinessRuleResponse> SwapShiftTradeSwapDetails(ReadOnlyCollection<IShiftTradeSwapDetail> shiftTradeSwapDetails, IScheduleDictionary scheduleDictionary, INewBusinessRuleCollection newBusinessRuleCollection, IScheduleTagSetter scheduleTagSetter);
-
-        IEnumerable<IBusinessRuleResponse> Swap(IPerson person1, IPerson person2, IList<DateOnly> dates,
-                                                                IScheduleDictionary scheduleDictionary, INewBusinessRuleCollection newBusinessRuleCollection, IScheduleTagSetter scheduleTagSetter);
     }
 }
--- Domain/ResourceCalculation/SwapAndModifyService.cs
+++ Domain/ResourceCalculation/SwapAndModifyService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-using Teleopti.Ccc.Domain.Security;
-using Teleopti.Ccc.Domain.Security.AuthorizationData;
-using Teleopti.Ccc.Domain.Security.Principal;
-using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.ResourceCalculation
 {
@@ -47,48 +42,6 @@ namespace Teleopti.Ccc.Domain.ResourceCalculation
 			ruleRepsonses = scheduleDictionary.Modify(ScheduleModifier.Scheduler, modifiedParts, newBusinessRuleCollection, _scheduleDayChangeCallback, scheduleTagSetter);
 			
 			return ruleRepsonses.Where(r => !r.Overridden).ToList();
-        }
-
-        public IEnumerable<IBusinessRuleResponse> Swap(IPerson person1, IPerson person2, IList<DateOnly> dates,
-            IScheduleDictionary scheduleDictionary, INewBusinessRuleCollection newBusinessRuleCollection, IScheduleTagSetter scheduleTagSetter)
-        {
-            InParameter.NotNull(nameof(person1), person1);
-            InParameter.NotNull(nameof(person2), person2);
-            InParameter.NotNull(nameof(dates), dates);
-            InParameter.NotNull(nameof(scheduleDictionary), scheduleDictionary);
-            InParameter.ListCannotBeEmpty(nameof(dates), dates);
-            if (person1 == person2)
-                throw new ArgumentException("The Persons must be different");
-
-            var authorizationService = PrincipalAuthorization.Current();
-            var applicationFunction = DefinedRaptorApplicationFunctionPaths.ModifyPersonAssignment;
-
-            // a list to hold all modified parts
-            List<IScheduleDay> modifiedParts = new List<IScheduleDay>();
-            // for every day, take each persons schedule and send to swapservice and swap
-            //IScheduleDictionary scheduleDictionary = schedulingResultStateHolder.Schedules;
-            foreach (DateOnly dateOnly in dates)
-            {
-                if (!authorizationService.IsPermitted(applicationFunction, dateOnly, person1)
-                    || !authorizationService.IsPermitted(applicationFunction, dateOnly, person2))
-                    throw new PermissionException("No permission to change the person assignment");
-
-                IScheduleDay part1 = scheduleDictionary[person1].ScheduledDay(dateOnly);
-                IScheduleDay part2 = scheduleDictionary[person2].ScheduledDay(dateOnly);
-                IList<IScheduleDay> selectedSchedules = new List<IScheduleDay> {part1, part2};
-                modifiedParts.AddRange(swapParts(scheduleDictionary, selectedSchedules, false));
-
-	            var ass1 = part1.PersonAssignment();
-	            ass1?.CheckRestrictions();
-
-	            var ass2 = part1.PersonAssignment();
-	            ass2?.CheckRestrictions();
-            }
-
-            IEnumerable<IBusinessRuleResponse> responses =
-                scheduleDictionary.Modify(ScheduleModifier.Scheduler, modifiedParts, newBusinessRuleCollection, _scheduleDayChangeCallback, scheduleTagSetter);
-
-            return responses.Where(r => !r.Overridden).ToList();
         }
 
         private IEnumerable<IScheduleDay> swapParts(IScheduleDictionary scheduleDictionary, IList<IScheduleDay> selectedSchedules, bool ignoreAssignmentPermission)
