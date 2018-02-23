4f187c3e4fa5ed8be0102fcdfe200e6b4fe7b7aa
Zulong Chen <none@none>
2018-02-23 14:13:32 +0800

    formatting

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.directive.commandMenu.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.directive.commandMenu.js
@@ -1,5 +1,5 @@
-﻿"use strict";
-(function () {
+﻿(function(angular) {
+	'use strict';
 	angular.module('wfm.teamSchedule').directive('teamscheduleCommandMenu', teamscheduleCommandMenuDirective);
 
 	function teamscheduleCommandMenuDirective() {
@@ -8,162 +8,238 @@
 			scope: {
 				validateWarningEnabled: '=?',
 				triggerCommand: '&?',
-				selectedDate:'<'
+				selectedDate: '<'
 			},
-			controller: ['$scope', 'PersonSelection', 'ValidateRulesService','ShortCuts', 'keyCodes', 'teamsPermissions', 'teamsToggles',  teamscheduleCommandMenuCtrl],
+			controller: [
+				'$scope',
+				'PersonSelection',
+				'ValidateRulesService',
+				'ShortCuts',
+				'keyCodes',
+				'teamsPermissions',
+				'teamsToggles',
+				teamscheduleCommandMenuCtrl
+			],
 			controllerAs: 'vm',
 			bindToController: true,
 			templateUrl: 'app/teamSchedule/html/commandMenu.tpl.html'
 		};
 	}
 
-	function teamscheduleCommandMenuCtrl($scope, personSelectionSvc, validateRulesService, shortCuts, keyCodes, teamsPermissions, teamsToggles) {
+	function teamscheduleCommandMenuCtrl(
+		$scope,
+		personSelectionSvc,
+		validateRulesService,
+		shortCuts,
+		keyCodes,
+		teamsPermissions,
+		teamsToggles
+	) {
 		var vm = this;
 
 		function buildAction(label, openSidePanel) {
-			return function () {
+			return function() {
 				if (vm.triggerCommand) {
 					vm.triggerCommand({
 						label: label,
 						needToOpenSidePanel: openSidePanel
 					});
 				}
-			}
+			};
 		}
 
 		vm.commands = [
 			{
-				label: "AddAbsence",
-				shortcut: "Alt+A",
+				label: 'AddAbsence',
+				shortcut: 'Alt+A',
 				keys: [[keyCodes.A], [keyCodes.ALT]],
-				action: buildAction("AddAbsence", true),
-				clickable: function () { return personSelectionSvc.anyAgentChecked(); },
-				visible: function () { return vm.canActiveAddAbsence(); }
+				action: buildAction('AddAbsence', true),
+				clickable: function() {
+					return personSelectionSvc.anyAgentChecked();
+				},
+				visible: function() {
+					return vm.canActiveAddAbsence();
+				}
 			},
 			{
-				label: "AddActivity",
-				shortcut: "Alt+T",
+				label: 'AddActivity',
+				shortcut: 'Alt+T',
 				keys: [[keyCodes.T], [keyCodes.ALT]],
-				action: buildAction("AddActivity", true),
-				clickable: function () { return personSelectionSvc.anyAgentChecked(); },
-				visible: function () { return vm.canActiveAddActivity(); }
+				action: buildAction('AddActivity', true),
+				clickable: function() {
+					return personSelectionSvc.anyAgentChecked();
+				},
+				visible: function() {
+					return vm.canActiveAddActivity();
+				}
 			},
 			{
-				label: "AddPersonalActivity",
-				shortcut: "Alt+P",
+				label: 'AddPersonalActivity',
+				shortcut: 'Alt+P',
 				keys: [[keyCodes.P], [keyCodes.ALT]],
-				action: buildAction("AddPersonalActivity", true),
-				clickable: function () { return personSelectionSvc.anyAgentChecked(); },
-				visible: function () { return vm.canActiveAddPersonalActivity(); }
+				action: buildAction('AddPersonalActivity', true),
+				clickable: function() {
+					return personSelectionSvc.anyAgentChecked();
+				},
+				visible: function() {
+					return vm.canActiveAddPersonalActivity();
+				}
 			},
 			{
-				label: "AddOvertimeActivity",
-				shortcut: "Alt+O",
+				label: 'AddOvertimeActivity',
+				shortcut: 'Alt+O',
 				keys: [[keyCodes.O], [keyCodes.ALT]],
-				action: buildAction("AddOvertimeActivity", true),
-				clickable: function () { return vm.canAddOvertime(); },
-				visible: function () { return vm.canActiveAddOvertime(); }
+				action: buildAction('AddOvertimeActivity', true),
+				clickable: function() {
+					return vm.canAddOvertime();
+				},
+				visible: function() {
+					return vm.canActiveAddOvertime();
+				}
 			},
 			{
-				label: "AddDayOff",
-				shortcut: "Alt+F",
+				label: 'AddDayOff',
+				shortcut: 'Alt+F',
 				keys: [[keyCodes.F], [keyCodes.ALT]],
-				action: buildAction("AddDayOff", true),
-				clickable: function () { return personSelectionSvc.anyAgentChecked(); },
-				visible: function () { return vm.canActiveDayOff(); }
+				action: buildAction('AddDayOff', true),
+				clickable: function() {
+					return personSelectionSvc.anyAgentChecked();
+				},
+				visible: function() {
+					return vm.canActiveDayOff();
+				}
 			},
 			{
-				label: "MoveActivity",
-				shortcut: "Alt+M",
+				label: 'MoveActivity',
+				shortcut: 'Alt+M',
 				keys: [[keyCodes.M], [keyCodes.ALT]],
-				action: buildAction("MoveActivity", true),
-				clickable: function () { return vm.canMoveActivity(); },
-				visible: function () { return vm.canActiveMoveActivity(); }
+				action: buildAction('MoveActivity', true),
+				clickable: function() {
+					return vm.canMoveActivity();
+				},
+				visible: function() {
+					return vm.canActiveMoveActivity();
+				}
 			},
 			{
-				label: "MoveInvalidOverlappedActivity",
-				shortcut: "Alt+I",
+				label: 'MoveInvalidOverlappedActivity',
+				shortcut: 'Alt+I',
 				keys: [[keyCodes.I], [keyCodes.ALT]],
-				action: buildAction("MoveInvalidOverlappedActivity", false),
-				clickable: function () { return vm.canMoveInvalidOverlappedActivity(); },
-				visible: function () { return vm.canActiveMoveInvalidOverlappedActivity(); }
+				action: buildAction('MoveInvalidOverlappedActivity', false),
+				clickable: function() {
+					return vm.canMoveInvalidOverlappedActivity();
+				},
+				visible: function() {
+					return vm.canActiveMoveInvalidOverlappedActivity();
+				}
 			},
 			{
-				label: "MoveShift",
-				shortcut: "Alt+E",
+				label: 'MoveShift',
+				shortcut: 'Alt+E',
 				keys: [[keyCodes.E], [keyCodes.ALT]],
-				action: buildAction("MoveShift", true),
-				clickable: function () { return vm.canMoveShift(); },
-				visible: function () { return vm.canActiveMoveShift(); }
+				action: buildAction('MoveShift', true),
+				clickable: function() {
+					return vm.canMoveShift();
+				},
+				visible: function() {
+					return vm.canActiveMoveShift();
+				}
 			},
 			{
-				label: "SwapShifts",
-				shortcut: "Alt+S",
+				label: 'SwapShifts',
+				shortcut: 'Alt+S',
 				keys: [[keyCodes.S], [keyCodes.ALT]],
-				action: buildAction("SwapShifts", false),
-				clickable: function () { return personSelectionSvc.canSwapShifts(); },
-				visible: function () { return vm.canActiveSwapShifts(); }
+				action: buildAction('SwapShifts', false),
+				clickable: function() {
+					return personSelectionSvc.canSwapShifts();
+				},
+				visible: function() {
+					return vm.canActiveSwapShifts();
+				}
 			},
 			{
-				label: "RemoveAbsence",
-				shortcut: "Alt+R",
+				label: 'RemoveAbsence',
+				shortcut: 'Alt+R',
 				keys: [[keyCodes.R], [keyCodes.ALT]],
-				action: buildAction("RemoveAbsence", false),
-				clickable: function () { return vm.canRemoveAbsence(); },
-				visible: function () { return vm.canActiveRemoveAbsence(); }
+				action: buildAction('RemoveAbsence', false),
+				clickable: function() {
+					return vm.canRemoveAbsence();
+				},
+				visible: function() {
+					return vm.canActiveRemoveAbsence();
+				}
 			},
 			{
-				label: "RemoveActivity",
-				shortcut: "Alt+X",
+				label: 'RemoveActivity',
+				shortcut: 'Alt+X',
 				keys: [[keyCodes.X], [keyCodes.ALT]],
-				action: buildAction("RemoveActivity", false),
-				clickable: function () { return vm.canRemoveActivity(); },
-				visible: function () { return vm.canActiveRemoveActivity(); }
+				action: buildAction('RemoveActivity', false),
+				clickable: function() {
+					return vm.canRemoveActivity();
+				},
+				visible: function() {
+					return vm.canActiveRemoveActivity();
+				}
 			},
 			{
-				label: "RemoveDayOff",
-				shortcut: "Alt+B",
+				label: 'RemoveDayOff',
+				shortcut: 'Alt+B',
 				keys: [[keyCodes.B], [keyCodes.ALT]],
-				action: buildAction("RemoveDayOff", false),
-				clickable: function () { return vm.canRemoveDayOff(); },
-				visible: function () { return vm.canActiveDayOff(); }
+				action: buildAction('RemoveDayOff', false),
+				clickable: function() {
+					return vm.canRemoveDayOff();
+				},
+				visible: function() {
+					return vm.canActiveDayOff();
+				}
 			},
 			{
-				label: "RemoveShift",
-				shortcut: "Alt+K",
+				label: 'RemoveShift',
+				shortcut: 'Alt+K',
 				keys: [[keyCodes.K], [keyCodes.ALT]],
-				action: buildAction("RemoveShift", true),
-				clickable: function () { return vm.canRemoveShift(); },
-				visible: function () { return vm.canActiveRemoveShift(); }
+				action: buildAction('RemoveShift', true),
+				clickable: function() {
+					return vm.canRemoveShift();
+				},
+				visible: function() {
+					return vm.canActiveRemoveShift();
+				}
 			},
 			{
-				label: "EditShiftCategory",
-				shortcut: "Alt+C",
+				label: 'EditShiftCategory',
+				shortcut: 'Alt+C',
 				keys: [[keyCodes.C], [keyCodes.ALT]],
-				action: buildAction("EditShiftCategory", true),
-				clickable: function () { return vm.canModifyShiftCategory(); },
-				visible: function () { return vm.canActiveModifyShiftCategory(); }
+				action: buildAction('EditShiftCategory', true),
+				clickable: function() {
+					return vm.canModifyShiftCategory();
+				},
+				visible: function() {
+					return vm.canActiveModifyShiftCategory();
+				}
 			},
 			{
-				label: "Undo",
-				shortcut: "Alt+U",
+				label: 'Undo',
+				shortcut: 'Alt+U',
 				keys: [[keyCodes.U], [keyCodes.ALT]],
-				action: buildAction("Undo", false),
-				clickable: function () { return vm.canUndoSchedule(); },
-				visible: function () { return true; }
+				action: buildAction('Undo', false),
+				clickable: function() {
+					return vm.canUndoSchedule();
+				},
+				visible: function() {
+					return true;
+				}
 			}
 		];
 
-
-		vm.canActiveAddActivity = function () {
+		vm.canActiveAddActivity = function() {
 			return vm.permissions.HasAddingActivityPermission;
 		};
 
-		vm.canActiveAddPersonalActivity = function () {
+		vm.canActiveAddPersonalActivity = function() {
 			return vm.permissions.HasAddingPersonalActivityPermission;
 		};
 
-		vm.canActiveAddAbsence = function () {
+		vm.canActiveAddAbsence = function() {
 			return vm.permissions.IsAddFullDayAbsenceAvailable || vm.permissions.IsAddIntradayAbsenceAvailable;
 		};
 
@@ -171,20 +247,19 @@
 			return vm.permissions.HasAddingOvertimeActivityPermission;
 		};
 
-		vm.canActiveDayOff = function () {
+		vm.canActiveDayOff = function() {
 			return vm.toggles.WfmTeamSchedule_AddNDeleteDayOff_40555;
 		};
 
-		vm.canActiveRemoveShift = function () {
+		vm.canActiveRemoveShift = function() {
 			return vm.toggles.WfmTeamSchedule_RemoveShift_46322 && vm.permissions.HasRemoveShiftPermission;
 		};
 
-		vm.canActiveMoveActivity = function () {
-			return vm.permissions.HasMoveActivityPermission
-				|| (vm.permissions.HasMoveOvertimePermission);
+		vm.canActiveMoveActivity = function() {
+			return vm.permissions.HasMoveActivityPermission || vm.permissions.HasMoveOvertimePermission;
 		};
 
-		vm.canActiveMoveInvalidOverlappedActivity = function () {
+		vm.canActiveMoveInvalidOverlappedActivity = function() {
 			return vm.permissions.HasMoveInvalidOverlappedActivityPermission;
 		};
 
@@ -192,56 +267,62 @@
 			return vm.permissions.HasMoveActivityPermission;
 		};
 
-		vm.canActiveRemoveAbsence = function () {
+		vm.canActiveRemoveAbsence = function() {
 			return vm.permissions.IsRemoveAbsenceAvailable;
 		};
 
-		vm.canActiveRemoveActivity = function () {
+		vm.canActiveRemoveActivity = function() {
 			return vm.permissions.HasRemoveActivityPermission || vm.permissions.HasRemoveOvertimePermission;
 		};
 
-		vm.canActiveSwapShifts = function () {
+		vm.canActiveSwapShifts = function() {
 			return vm.permissions.IsSwapShiftsAvailable;
 		};
 
-		vm.canActiveModifyShiftCategory = function(){
+		vm.canActiveModifyShiftCategory = function() {
 			return vm.permissions.HasEditShiftCategoryPermission;
 		};
 
-		vm.canMoveActivity = function () {
-			return personSelectionSvc.isAnyAgentSelected() &&
+		vm.canMoveActivity = function() {
+			return (
+				personSelectionSvc.isAnyAgentSelected() &&
 				personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedAbsenceInfo.AbsenceCount === 0 &&
-				personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0;
+				personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0
+			);
 		};
 
-		vm.canMoveInvalidOverlappedActivity = function () {			
+		vm.canMoveInvalidOverlappedActivity = function() {
 			return personSelectionSvc.anyAgentChecked() && vm.validateWarningEnabled;
 		};
 
 		vm.canMoveShift = function() {
-			return personSelectionSvc.anyAgentChecked() &&
-				personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0;
+			return (
+				personSelectionSvc.anyAgentChecked() &&
+				personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0
+			);
 		};
 
 		vm.canAddOvertime = function() {
 			return personSelectionSvc.anyAgentChecked();
 		};
 
-		vm.canRemoveAbsence = function () {
+		vm.canRemoveAbsence = function() {
 			return personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedAbsenceInfo.AbsenceCount > 0;
 		};
 
-		vm.canRemoveActivity = function () {
+		vm.canRemoveActivity = function() {
 			return personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0;
 		};
 
 		vm.canModifyShiftCategory = function() {
-			return vm.permissions.HasEditShiftCategoryPermission &&
-					personSelectionSvc.anyAgentChecked() &&
-					personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0;
+			return (
+				vm.permissions.HasEditShiftCategoryPermission &&
+				personSelectionSvc.anyAgentChecked() &&
+				personSelectionSvc.getTotalSelectedPersonAndProjectionCount().SelectedActivityInfo.ActivityCount > 0
+			);
 		};
 
-		vm.canUndoSchedule = function () {
+		vm.canUndoSchedule = function() {
 			return personSelectionSvc.anyAgentChecked();
 		};
 
@@ -255,22 +336,31 @@
 				return dayOffsOnCurrentDay.length > 0;
 			});
 			return selectedDayOffs.length > 0;
-		}
+		};
 
-		vm.canRemoveShift = function () {
+		vm.canRemoveShift = function() {
 			var selectedPersonInfoList = personSelectionSvc.getCheckedPersonInfoList();
-		
-			return !!selectedPersonInfoList.length && !!selectedPersonInfoList.filter(function (p) {
-				return p.Checked && p.SelectedActivities && p.SelectedActivities.filter(function (a) { return !a.isOvertime;}).length > 0;
-			}).length;
-		}
 
-		vm.activateCommandMenu = function(){
+			return (
+				!!selectedPersonInfoList.length &&
+				!!selectedPersonInfoList.filter(function(p) {
+					return (
+						p.Checked &&
+						p.SelectedActivities &&
+						p.SelectedActivities.filter(function(a) {
+							return !a.isOvertime;
+						}).length > 0
+					);
+				}).length
+			);
+		};
+
+		vm.activateCommandMenu = function() {
 			return vm.canRemoveActivity() || vm.canRemoveAbsence() || personSelectionSvc.anyAgentChecked();
 		};
 
 		function registerShortCuts() {
-			vm.commands.forEach(function (cmd) {
+			vm.commands.forEach(function(cmd) {
 				function wrappedAction() {
 					if (cmd.clickable() && cmd.visible()) {
 						cmd.action();
@@ -282,11 +372,11 @@
 			});
 		}
 
-		vm.init = function () {
+		vm.init = function() {
 			vm.toggles = teamsToggles.all();
 			vm.permissions = teamsPermissions.all();
-		
-			vm.isMenuVisible = function () {
+
+			vm.isMenuVisible = function() {
 				return true;
 			};
 			registerShortCuts();
@@ -294,4 +384,4 @@
 
 		vm.init();
 	}
-})();
+})(angular);
