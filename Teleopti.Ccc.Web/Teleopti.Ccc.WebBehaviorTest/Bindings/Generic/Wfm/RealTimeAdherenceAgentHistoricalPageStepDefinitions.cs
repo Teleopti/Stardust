89e823d38757f7b12f50584dbf4195d63907eb3e
LevelUp! <levelup@teleopti.com>
2018-02-27 14:31:56 +0100

    #47721 implement scenario step

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Wfm/RealTimeAdherenceAgentHistoricalPageStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/Wfm/RealTimeAdherenceAgentHistoricalPageStepDefinitions.cs
@@ -38,11 +38,10 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic.Wfm
 		}
 
 		[When(@"I remove approved period between '(.*)' and '(.*)'")]
-		public void WhenIRemoveApprovedPeriodBetweenAnd(string start, string end)
+		public void WhenIRemoveApprovedPeriodBetweenAnd(string from, string to)
 		{
-			ScenarioContext.Current.Pending();
+			Browser.Interactions.Click($".remove-approved-period[data-starttime='{from}'][data-endtime='{to}']");
 		}
-
 
 		[Then(@"I should see out of adherences")]
 		public void ThenIShouldSeeOutOfAdherences(Table table)
