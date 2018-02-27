5edaf1da3805b13a2494391820ceaec2ae06e4be
claess <claes.stenman@teleopti.com>
2018-02-27 10:47:32 +0100

    #48370 Refactor tests

--- Teleopti.Ccc.WinCodeTest/Scheduler/OptimizationBug37126Test.cs
+++ Teleopti.Ccc.WinCodeTest/Scheduler/OptimizationBug37126Test.cs
@@ -12,11 +12,10 @@ using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
 using Teleopti.Ccc.Domain.Scheduling.Restriction;
 using Teleopti.Ccc.Domain.Scheduling.ScheduleTagging;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
-using Teleopti.Ccc.Domain.UndoRedo;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
-using Teleopti.Ccc.TestCommon.FakeRepositories;
 using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.TestCommon.Scheduling;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WinCodeTest.Scheduler
@@ -25,79 +24,44 @@ namespace Teleopti.Ccc.WinCodeTest.Scheduler
 	public class OptimizationBug37126Test
 	{
 		public OptimizationDesktopExecuter Target;
-		public ISchedulerStateHolder StateHolder;
 		public IOptimizationPreferences OptimizationPreferences;
-		public FakeBusinessUnitRepository BusinessUnitRepository;
+		public Func<ISchedulerStateHolder> StateHolder;
 
 		[Test]
 		public void ShouldNotCrashWhenDoingRollback()
 		{
-			BusinessUnitRepository.Has(ServiceLocatorForEntity.CurrentBusinessUnit.Current());
 			var date = new DateOnly(2014, 3, 31);
-			var scenario = new Scenario("Default").WithId();
-			var shiftCategory = new ShiftCategory("DY").WithId();
-			var shiftCategoryAM = new ShiftCategory("AM").WithId();
-			var activity = new Activity("Phone")
-			{
-				InContractTime = true,
-				InWorkTime = true,
-				InPaidTime = true,
-				RequiresSkill = true
-			};
-			var skill = SkillFactory.CreateSkill("Direct Sales").WithId();
-			skill.Activity = activity;
-
-			var workShiftRuleSet =
-				new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(7, 0, 9, 0, 15),
-					new TimePeriodWithSegment(15, 0, 17, 0, 15), shiftCategoryAM));
-			workShiftRuleSet.AddExtender(new ActivityRelativeStartExtender(ActivityFactory.CreateActivity("Lunch"),
-				new TimePeriodWithSegment(1, 0, 1, 0, 15), new TimePeriodWithSegment(4, 0, 5, 0, 15)));
+			var scenario = new Scenario().WithId();
+			var shiftCategory = new ShiftCategory().WithId();
+			var shiftCategoryAm = new ShiftCategory().WithId();
+			var activity = new Activity {InContractTime = true, InWorkTime = true, InPaidTime = true, RequiresSkill = true};
+			var skill = new Skill().For(activity).WithId().IsOpen();
+			var workShiftRuleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(7, 0, 9, 0, 15),new TimePeriodWithSegment(15, 0, 17, 0, 15), shiftCategoryAm));
+			workShiftRuleSet.AddExtender(new ActivityRelativeStartExtender(new Activity(),new TimePeriodWithSegment(1, 0, 1, 0, 15), new TimePeriodWithSegment(4, 0, 5, 0, 15)));
 			var bag = new RuleSetBag(workShiftRuleSet);
-			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson().WithId(),
-				new DateOnly(2014, 3, 1));
-			person.AddSkill(skill, new DateOnly(2014, 3, 1));
-			var personPeriod = person.Period(new DateOnly(2014, 3, 1));
-			personPeriod.RuleSetBag = bag;
-			personPeriod.Team.Site = SiteFactory.CreateSimpleSite();
-			personPeriod.PersonContract.Contract.NegativePeriodWorkTimeTolerance = TimeSpan.FromHours(24);
-			personPeriod.PersonContract.Contract.PositivePeriodWorkTimeTolerance = TimeSpan.FromHours(24);
-
-			var dictionary = new ScheduleDictionaryForTest(scenario, new DateTimePeriod(2014, 3, 22, 2014, 4, 4));
-			dictionary.UsePermissions(false);
-			dictionary.SetUndoRedoContainer(new UndoRedoContainer());
-			dictionary.AddPersonAbsence(PersonAbsenceFactory.CreatePersonAbsence(person, scenario,
-				new DateTimePeriod(2014, 3, 24, 0, 2014, 3, 25, 0)));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 25, 8, 2014, 3, 25, 22), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 26, 8, 2014, 3, 26, 22), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 27, 8, 2014, 3, 27, 22), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 28, 12, 2014, 3, 28, 20), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithDayOff(person,
-				scenario, new DateOnly(2014, 3, 29), DayOffFactory.CreateDayOff()));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithDayOff(person,
-				scenario, new DateOnly(2014, 3, 30), DayOffFactory.CreateDayOff()));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 31, 8, 2014, 3, 31, 22), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithDayOff(person,
-				scenario, new DateOnly(2014, 4, 1), DayOffFactory.CreateDayOff()));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 2, 8, 2014, 4, 2, 22), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 3, 12, 2014, 4, 3, 21), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 4, 8, 2014, 4, 4, 22), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 5, 8, 2014, 4, 5, 18), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 6, 11, 2014, 4, 6, 21), shiftCategory));
-			dictionary.AddPersonAssignment(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 7, 8, 2014, 4, 7, 22), shiftCategory));
-			dictionary.AddScheduleDataManyPeople(new PreferenceDay(person, new DateOnly(2014, 3, 31),
-				new PreferenceRestriction {ShiftCategory = shiftCategory}));
-
-			StateHolder.SetRequestedScenario(scenario);
-			StateHolder.SchedulingResultState.Schedules = dictionary;
-			StateHolder.ChoosenAgents.Add(person);
-			StateHolder.SchedulingResultState.LoadedAgents = StateHolder.ChoosenAgents;
-			StateHolder.RequestedPeriod = new DateOnlyPeriodAsDateTimePeriod(new DateOnlyPeriod(2014, 3, 22, 2014, 4, 4),
-				TimeZoneInfo.Utc);
-			StateHolder.SchedulingResultState.AddSkills(skill);
+			var contract = new Contract("_") { WorkTimeDirective = new WorkTimeDirective(TimeSpan.FromHours(1), TimeSpan.FromHours(80), TimeSpan.FromHours(11), TimeSpan.FromHours(36)) };
+			var person = new Person().WithId().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(bag, contract, skill).WithSchedulePeriodOneMonth(new DateOnly(2014,3,24));
+			var data = new List<IPersistableScheduleData>
+			{
+				PersonAbsenceFactory.CreatePersonAbsence(person, scenario,new DateTimePeriod(2014, 3, 24, 0, 2014, 3, 25, 0)),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 25, 8, 2014, 3, 25, 22), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 26, 8, 2014, 3, 26, 22), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 27, 8, 2014, 3, 27, 22), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 28, 12, 2014, 3, 28, 20), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithDayOff(person, scenario, new DateOnly(2014, 3, 29), DayOffFactory.CreateDayOff()),
+				PersonAssignmentFactory.CreateAssignmentWithDayOff(person, scenario, new DateOnly(2014, 3, 30), DayOffFactory.CreateDayOff()),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 3, 31, 8, 2014, 3, 31, 22), shiftCategory),
+				new PreferenceDay(person, new DateOnly(2014, 3, 31), new PreferenceRestriction {ShiftCategory = shiftCategory}),
+				PersonAssignmentFactory.CreateAssignmentWithDayOff(person, scenario, new DateOnly(2014, 4, 1), DayOffFactory.CreateDayOff()),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 2, 8, 2014, 4, 2, 22), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 3, 12, 2014, 4, 3, 21), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 4, 8, 2014, 4, 4, 22), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 5, 8, 2014, 4, 5, 18), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 6, 11, 2014, 4, 6, 21), shiftCategory),
+				PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, activity, new DateTimePeriod(2014, 4, 7, 8, 2014, 4, 7, 22), shiftCategory)
+			};
 
 			var skillDays = Enumerable.Range(-7, 10).Select(i => createSkillDay(skill, date.AddDays(i), scenario)).ToArray();
-			StateHolder.SchedulingResultState.SkillDays = new Dictionary<ISkill, IEnumerable<ISkillDay>> {{skill, skillDays}};
-
 			OptimizationPreferences.General = new GeneralPreferences
 			{
 				OptimizationStepShiftsWithinDay = true,
@@ -115,13 +79,11 @@ namespace Teleopti.Ccc.WinCodeTest.Scheduler
 				UseAverageShiftLengths = true,
 				UserOptionMaxSeatsFeature = MaxSeatsFeatureOptions.ConsiderMaxSeats
 			};
-			Target.Execute(
-				new NoSchedulingProgress(), StateHolder,
-				new[] { person }, date.ToDateOnlyPeriod(),
-					OptimizationPreferences, 
-					new FixedDayOffOptimizationPreferenceProvider(new DaysOffPreferences()));
+			var stateHolder = StateHolder.Fill(scenario, new DateOnlyPeriod(new DateOnly(2014, 3, 22), new DateOnly(2014, 4, 8)), new[] { person }, data, skillDays);
 
-			dictionary[person].ScheduledDay(date).PersonAssignment().ShiftLayers.Should().Be.Empty();
+			Target.Execute(new NoSchedulingProgress(), stateHolder,new[] { person }, date.ToDateOnlyPeriod(), OptimizationPreferences, new FixedDayOffOptimizationPreferenceProvider(new DaysOffPreferences()));
+
+			stateHolder.Schedules[person].ScheduledDay(date).PersonAssignment().ShiftLayers.Should().Be.Empty();
 		}
 
 		private static SkillDay createSkillDay(ISkill skill, DateOnly date, Scenario scenario)
