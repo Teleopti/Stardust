be4a14bcd90da29abd77bc85b91d909ec22257af
emils <none@none>
2018-02-28 09:33:20 +0100

    #47795: Activate new Angular People route

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/people/people.routes.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/people/people.routes.js
@@ -1,28 +1,11 @@
 ﻿(function () {
-	'use strict';
+  'use strict';
+  angular.module ('wfm.people').config (stateConfig);
 
-	angular
-		.module('wfm.people')
-		.config(stateConfig);
-
-	function stateConfig($stateProvider) {
-		$stateProvider.state('people', {
-				url: '/people',
-				params: {
-					selectedPeopleIds: [],
-					currentKeyword: '',
-					paginationOptions: {}
-				},
-				templateUrl: 'app/people/html/people.start.html',
-				controller: 'PeopleDefault'
-		})
-		.state('people.mock', {
-			templateUrl: 'app/people/html/people.html',
-			controller: 'PeopleStart'
-		})
-		.state('people.new', {
-			url: '/new',
-			template: '<ng2-people></ng2-people>'
-		})
-	}
-})();
+  function stateConfig ($stateProvider) {
+    $stateProvider.state ('people', {
+      url: '/people',
+      template: '<ng2-people></ng2-people>',
+    });
+  }
+}) ();
