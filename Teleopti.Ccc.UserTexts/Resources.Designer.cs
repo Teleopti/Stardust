0747261c3a4934d939f2ac3ab6fef6a5b0459a72
Zulong Chen <none@none>
2018-03-01 10:36:49 +0800

    #48403 UI - date range picking

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -24632,6 +24632,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string PerWeek {
             get {
                 return ResourceManager.GetString("PerWeek", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Pick a date range to start badge calculation.
+        /// </summary>
+        public static string PickADateRangeToStartBadgeCalculation {
+            get {
+                return ResourceManager.GetString("PickADateRangeToStartBadgeCalculation", resourceCulture);
             }
         }
         
