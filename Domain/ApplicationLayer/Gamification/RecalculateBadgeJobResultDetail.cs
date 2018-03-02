d6508bd6f887e52f6e271f075d34160c173a892b
Qing Luo <none@none>
2018-03-02 14:19:40 +0800

    #48403 Intergrate with backend service.

--- Domain/ApplicationLayer/Gamification/RecalculateBadgeJobResultDetail.cs
+++ Domain/ApplicationLayer/Gamification/RecalculateBadgeJobResultDetail.cs
@@ -9,7 +9,10 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer.Gamification
 
 		public string Owner { get; set; }
 
-		public DateOnlyPeriod Period { get; set; }
+		public DateTime StartDate { get; set; }
+
+		public DateTime EndDate { get; set; }	
+
 		public DateTime CreateDateTime { get; set; }
 
 		public string Status { get; set; }
