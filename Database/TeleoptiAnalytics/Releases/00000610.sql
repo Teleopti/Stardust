422ac06515b876526d50e9b41e35d94efa3773d1
Xinfeng Li <xinfeng.li@teleopti.com>
2018-03-02 16:15:35 +0100

    Fix bug #48496: ETL step stg_overtime fail when we run schedule job.

--- Database/TeleoptiAnalytics/Releases/00000610.sql
+++ Database/TeleoptiAnalytics/Releases/00000610.sql
@@ -1,3 +1,4 @@
+-- Bug #48351
 ALTER TABLE [stage].[stg_person]
    ALTER COLUMN [windows_domain] [nvarchar](100)
 GO
@@ -12,4 +13,13 @@ GO
 
 ALTER TABLE [mart].[dim_person]
    ALTER COLUMN [windows_username] [nvarchar](100)
+GO
+
+-- Bug #48496
+ALTER TABLE [stage].[stg_overtime]
+   ALTER COLUMN [overtime_name] nvarchar(250)
+GO
+
+ALTER TABLE [mart].[dim_overtime]
+   ALTER COLUMN [overtime_name] nvarchar(250)
 GO
