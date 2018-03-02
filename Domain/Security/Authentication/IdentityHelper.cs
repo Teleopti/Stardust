706d836c9959f8fe747c9a2c2bfd173dc0e89da3
Xinfeng Li <xinfeng.li@teleopti.com>
2018-03-02 11:40:27 +0100

    Minor change for IdentityHelper

--- Domain/Security/Authentication/IdentityHelper.cs
+++ Domain/Security/Authentication/IdentityHelper.cs
@@ -4,30 +4,41 @@ namespace Teleopti.Ccc.Domain.Security.Authentication
 {
 	public static class IdentityHelper
 	{
+		private const char identitySplitter = '\\';
+
+		// This function is used only once in test, should it moved to test project?
 		public static string Merge(string domainName, string userName)
 		{
-			if (string.IsNullOrEmpty(domainName)) return userName;
-			return domainName + "\\" + userName;
+			var trimmedDomainName = domainName?.Trim() ?? string.Empty;
+			var trimmedUserName = userName?.Trim() ?? string.Empty;
+			return !string.IsNullOrEmpty(trimmedDomainName)
+				? string.Format("{0}" + identitySplitter + "{1}", trimmedDomainName, trimmedUserName)
+				: trimmedUserName;
 		}
 
 		public static Tuple<string, string> Split(string identity)
 		{
-			var logOn = string.Empty;
-			var domain = string.Empty;
-			if (!string.IsNullOrEmpty(identity))
+			if (string.IsNullOrEmpty(identity))
 			{
-				var parts = identity.Split('\\');
-				if (parts.Length > 1)
-				{
-					domain = parts[0];
-					logOn = parts[1];
-				}
-				else
-				{
-					logOn = parts[0];
-				}
+				return new Tuple<string, string>(string.Empty, string.Empty);
 			}
-			return new Tuple<string, string>(domain,logOn);
+
+			string logOn;
+			string domain;
+
+			var parts = identity.Split(identitySplitter);
+			if (parts.Length > 1)
+			{
+				domain = parts[0];
+				logOn = parts[1];
+			}
+			else
+			{
+				domain = string.Empty;
+				logOn = parts[0];
+			}
+
+			return new Tuple<string, string>(domain.Trim(), logOn.Trim());
 		}
 	}
 }
---
+++ DomainTest/Security/Authentication/IdentityHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Security.Authentication;
+
+namespace Teleopti.Ccc.DomainTest.Security.Authentication
+{
+	[TestFixture]
+	public class IdentityHelperTest
+	{
+		private const string toyStory = "ToyStory";
+		private const string buzzLightyear = "BuzzLightyear";
+
+		[TestCase(toyStory, buzzLightyear, toyStory + "\\" + buzzLightyear)]
+		[TestCase(" " + toyStory + " ", " " + buzzLightyear + " ", toyStory + "\\" + buzzLightyear)]
+		[TestCase("", buzzLightyear, buzzLightyear)]
+		[TestCase("   ", buzzLightyear, buzzLightyear)]
+		[TestCase(null, buzzLightyear, buzzLightyear)]
+		[TestCase(null, null, "")]
+		public void ShouldMergeIdentity(string domainName, string userName, string identity)
+		{
+			var userIdentity = IdentityHelper.Merge(domainName, userName);
+			userIdentity.Should().Be.EqualTo(identity);
+		}
+
+		[TestCase(toyStory + "\\" + buzzLightyear, toyStory, buzzLightyear)]
+		[TestCase(toyStory + "\\" + buzzLightyear + "\\AndWoody", toyStory, buzzLightyear)]
+		[TestCase(toyStory + "\\", toyStory, "")]
+		[TestCase(" " + toyStory + " \\ " + buzzLightyear + " ", toyStory, buzzLightyear)]
+		[TestCase(buzzLightyear, "", buzzLightyear)]
+		public void ShouldSplitIdentity(string identity, string domainName, string userName)
+		{
+			var winLogon = IdentityHelper.Split(identity);
+			winLogon.Item1.Should().Be.EqualTo(domainName);
+			winLogon.Item2.Should().Be.EqualTo(userName);
+		}
+	}
+}
