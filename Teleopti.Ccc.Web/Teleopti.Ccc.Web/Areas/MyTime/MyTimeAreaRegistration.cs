116891239eceb23e8ce892178115c817429e1b1e
Zulong Chen <none@none>
2018-03-05 11:34:17 +0800

    #48483 serve the static appdownload file through web.config

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/MyTimeAreaRegistration.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/MyTimeAreaRegistration.cs
@@ -12,9 +12,6 @@ namespace Teleopti.Ccc.Web.Areas.MyTime
 
 		public override void RegisterArea(AreaRegistrationContext context)
 		{
-			context.Routes.Add(new StaticHtmlRoutesHandler());
-			
-
 			var mapRoute = context.MapRoute(
 				"MyTime-authentication",
 				"MyTime/Authentication/{action}",
@@ -61,32 +58,6 @@ namespace Teleopti.Ccc.Web.Areas.MyTime
 				new { controller = "Portal", action = "Index", id = UrlParameter.Optional }
 				);
 
-		}
-	}
-
-	public class StaticHtmlRoutesHandler : RouteBase
-	{
-		public override RouteData GetRouteData(HttpContextBase httpContext)
-		{
-			if (httpContext.Request.Url == null)
-			{
-				return null;
-			}
-			var url = httpContext.Request.Url.ToString();
-
-			if (Regex.IsMatch(url.ToLower(), @"/mytime/static/.*\.(html|htm)"))
-			{
-				httpContext.Response.ContentType = "text/html";
-				httpContext.Response.TransmitFile($"~/Areas/MyTime/Views/Static/{httpContext.Request.Url.Segments.Last()}");
-				httpContext.Response.End();
-			}
-			return null;
-		}
-
-		public override VirtualPathData GetVirtualPath(RequestContext requestContext,
-			RouteValueDictionary values)
-		{
-			return null;
 		}
 	}
 }
