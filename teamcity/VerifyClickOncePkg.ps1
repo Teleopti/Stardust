5aa10e43f3dd9d6d82b53029830cb3782171322c
jimmyb <none@none>
2018-03-05 18:53:10 +0100

    Backed out changeset: 89afac4175bd

--- teamcity/VerifyClickOncePkg.ps1
+++ teamcity/VerifyClickOncePkg.ps1
@@ -17,7 +17,6 @@ $output =  "$PSScriptroot\Teleopti.Ccc.SmartClientPortal.Shell.application"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
-
 #Download Client
 $p = Start-Process "$PSScriptroot\Teleopti.Ccc.SmartClientPortal.Shell.application" -PassThru
 $Title = "Application Install - Security Warning"
