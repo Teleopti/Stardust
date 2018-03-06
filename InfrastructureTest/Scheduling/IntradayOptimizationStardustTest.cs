decec9f4a54e0e3c961ef8a467b863ee8682d381
Roger Kratz <roger.kratz@teleopti.com>
2018-03-06 11:10:00 +0100

    #48512 Starting with test. Need to fake away stardust...

---
+++ InfrastructureTest/Scheduling/IntradayOptimizationStardustTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using Teleopti.Ccc.Domain.ApplicationLayer.ResourcePlanner;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.InfrastructureTest.Scheduling
+{
+	[DatabaseTest]
+	[Ignore("#48512")]
+	public class IntradayOptimizationStardustTest
+	{
+		public IntradayOptimizationFromWeb Target;
+
+		public IActivityRepository ActivityRepository;
+		public IPersonRepository PersonRepository;
+		public ISkillRepository SkillRepository;
+		public IWorkloadRepository WorkloadRepository;
+		public ISkillTypeRepository SkillTypeRepository;
+		public IPlanningPeriodRepository PlanningPeriodRepository;
+		public ITeamRepository TeamRepository;
+		public ISiteRepository SiteRepository;
+		public IPartTimePercentageRepository PartTimePercentageRepository;
+		public IContractRepository ContractRepository;
+		public IContractScheduleRepository ContractScheduleRepository;
+		public ICurrentUnitOfWorkFactory CurrentUnitOfWorkFactory;
+		
+		
+		[Test]
+		public void ShouldNotCrashReadingBlockPreference()
+		{
+			var date = new DateOnly(2000, 1, 1);
+			var planningPeriod = new PlanningPeriod(date.ToDateOnlyPeriod(), SchedulePeriodType.Day, 1);
+			var activity = new Activity("_");
+			var skill = new Skill().IsOpen().InTimeZone(TimeZoneInfo.Utc);
+			skill.Activity = activity;
+			skill.SkillType.Description = new Description("_");
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(skill);
+			var personPeriod = agent.Period(date);
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				SiteRepository.Add(personPeriod.Team.Site);
+				TeamRepository.Add(personPeriod.Team);
+				PartTimePercentageRepository.Add(personPeriod.PersonContract.PartTimePercentage);
+				ContractRepository.Add(personPeriod.PersonContract.Contract);
+				ContractScheduleRepository.Add(personPeriod.PersonContract.ContractSchedule);
+				ActivityRepository.Add(activity);
+				SkillTypeRepository.Add(skill.SkillType);
+				SkillRepository.Add(skill);
+				PersonRepository.Add(agent);
+				PlanningPeriodRepository.Add(planningPeriod);
+				
+				WorkloadRepository.AddRange(skill.WorkloadCollection);
+				uow.PersistAll();
+			}
+			
+			Target.Execute(planningPeriod.Id.Value, true);
+		}
+	}
+}
