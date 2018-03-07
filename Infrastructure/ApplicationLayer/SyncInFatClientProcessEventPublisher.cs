1e78e877f99830bbded9780d15a88f0294aaec82
Roger Kratz <roger.kratz@teleopti.com>
2018-03-07 10:30:15 +0100

    #48512 Refactor - removing base class

--- Infrastructure/ApplicationLayer/SyncInFatClientProcessEventPublisher.cs
+++ Infrastructure/ApplicationLayer/SyncInFatClientProcessEventPublisher.cs
@@ -1,12 +1,33 @@
-﻿using Teleopti.Ccc.Domain.ApplicationLayer;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 {
-	public class SyncInFatClientProcessEventPublisher : InProcessEventPublisher<IRunInSyncInFatClientProcess>
+	public class SyncInFatClientProcessEventPublisher : IEventPublisher
 	{
-		public SyncInFatClientProcessEventPublisher(ResolveEventHandlers resolver, CommonEventProcessor processor) : base(resolver, processor)
+		private readonly ResolveEventHandlers _resolver;
+		private readonly CommonEventProcessor _processor;
+
+		protected SyncInFatClientProcessEventPublisher(ResolveEventHandlers resolver, CommonEventProcessor processor)
 		{
+			_resolver = resolver;
+			_processor = processor;
+		}
+
+		public void Publish(params IEvent[] events)
+		{
+			Task.WaitAll((
+					from @event in events
+#pragma warning disable 618
+					from handlerType in _resolver.HandlerTypesFor<IRunInSyncInFatClientProcess>(@event)
+					select Task.Run(() =>
+							_processor.Process(@event, handlerType)
+#pragma warning restore 618
+					))
+				.ToArray());
 		}
 	}
 }
