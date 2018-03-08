1ff5e8e5ef2f38e14a23c8fc7f02b0020ca90cdc
claess <claes.stenman@teleopti.com>
2018-03-08 13:08:43 +0100

    #48512 ShouldNotCrashReadingPlanningGroupSettingFilters ignored

--- InfrastructureTest/Scheduling/IntradayOptimizationStardustTest.cs
+++ InfrastructureTest/Scheduling/IntradayOptimizationStardustTest.cs
@@ -35,6 +35,8 @@ namespace Teleopti.Ccc.InfrastructureTest.Scheduling
 		public IContractScheduleRepository ContractScheduleRepository;
 		public ICurrentUnitOfWorkFactory CurrentUnitOfWorkFactory;
 		public IJobResultRepository JobResultRepository;
+		public IPlanningGroupSettingsRepository PlanningGroupSettingsRepository;
+		public IPlanningGroupRepository PlanningGroupRepository;
 		
 		
 		[Test]
@@ -67,6 +69,53 @@ namespace Teleopti.Ccc.InfrastructureTest.Scheduling
 				uow.PersistAll();
 			}
 			
+			Target.Execute(planningPeriod.Id.Value, true);
+
+			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				JobResultRepository
+					.LoadAllWithNoLock()
+					.Single()
+					.RethrowIfException();
+			}
+		}
+
+		[Test]
+		[Ignore("48512 - to be fixed")]
+		public void ShouldNotCrashReadingPlanningGroupSettingFilters()
+		{
+			var date = new DateOnly(2000, 1, 1);
+			var scenario = new Scenario("_") { DefaultScenario = true };
+			var activity = new Activity("_");
+			var skill = new Skill().IsOpen().InTimeZone(TimeZoneInfo.Utc);
+			skill.Activity = activity;
+			skill.SkillType.Description = new Description("_");
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc).WithPersonPeriod(skill);
+			var personPeriod = agent.Period(date);
+			var planningGroup = new PlanningGroup("_");
+			var planningGroupSetting = new PlanningGroupSettings(planningGroup);
+			var planningPeriod = new PlanningPeriod(date.ToDateOnlyPeriod(), SchedulePeriodType.Day, 1, planningGroup);
+
+			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				ScenarioRepository.Add(scenario);
+				SiteRepository.Add(personPeriod.Team.Site);
+				TeamRepository.Add(personPeriod.Team);
+				PartTimePercentageRepository.Add(personPeriod.PersonContract.PartTimePercentage);
+				ContractRepository.Add(personPeriod.PersonContract.Contract);
+				ContractScheduleRepository.Add(personPeriod.PersonContract.ContractSchedule);
+				ActivityRepository.Add(activity);
+				SkillTypeRepository.Add(skill.SkillType);
+				SkillRepository.Add(skill);
+				PersonRepository.Add(agent);
+				PlanningGroupRepository.Add(planningGroup);
+				PlanningGroupSettingsRepository.Add(planningGroupSetting);
+				PlanningPeriodRepository.Add(planningPeriod);
+
+				WorkloadRepository.AddRange(skill.WorkloadCollection);
+				uow.PersistAll();
+			}
+
 			Target.Execute(planningPeriod.Id.Value, true);
 
 			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
