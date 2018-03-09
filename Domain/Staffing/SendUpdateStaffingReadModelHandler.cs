d5c60c5740d89af5b56059efa8b0fd9e310c754d
Ola <none@none>
2018-03-09 08:42:20 +0100

    bug #48552

--- Domain/Staffing/SendUpdateStaffingReadModelHandler.cs
+++ Domain/Staffing/SendUpdateStaffingReadModelHandler.cs
@@ -1,9 +1,11 @@
-﻿using Teleopti.Ccc.Domain.AgentInfo.Requests;
-using Teleopti.Ccc.Domain.Aop;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.AgentInfo.Requests;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
-using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.Intraday;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Security.Principal;
@@ -11,63 +13,76 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Staffing
 {
-    public class SendUpdateStaffingReadModelHandler : IHandleEvent<TenantMinuteTickEvent>, IRunOnHangfire
-    {
-	    private readonly IJobStartTimeRepository _jobStartTimeRepository;
-        private readonly IBusinessUnitRepository _businessUnitRepository;
-	    private readonly IRequestStrategySettingsReader _requestStrategySettingsReader;
-	    private readonly IUpdatedBySystemUser _updatedBySystemUser;
-	    private readonly IEventPublisher _publisher;
+	public class SendUpdateStaffingReadModelHandler : IHandleEvent<TenantMinuteTickEvent>, IRunOnHangfire
+	{
+		private readonly IJobStartTimeRepository _jobStartTimeRepository;
+		private readonly IBusinessUnitRepository _businessUnitRepository;
+		private readonly IRequestStrategySettingsReader _requestStrategySettingsReader;
+		private readonly IUpdatedBySystemUser _updatedBySystemUser;
+		private readonly IEventPublisher _publisher;
 		private readonly IStaffingSettingsReader _staffingSettingsReader;
 		private readonly ISkillDayRepository _skillDayRepository;
-	    private readonly IScenarioRepository _scenarioRepository;
+		private readonly IScenarioRepository _scenarioRepository;
+		private readonly IBusinessUnitScope _businessUnitScope;
+		private readonly ICurrentUnitOfWorkFactory _currentUnitOfWorkFactory;
 
-		public SendUpdateStaffingReadModelHandler(IBusinessUnitRepository businessUnitRepository, 
-			IRequestStrategySettingsReader requestStrategySettingsReader, IJobStartTimeRepository jobStartTimeRepository, 
+		public SendUpdateStaffingReadModelHandler(IBusinessUnitRepository businessUnitRepository,
+			IRequestStrategySettingsReader requestStrategySettingsReader, IJobStartTimeRepository jobStartTimeRepository,
 			IUpdatedBySystemUser updatedBySystemUser, IEventPublisher publisher, IStaffingSettingsReader staffingSettingsReader,
-			ISkillDayRepository skillDayRepository, IScenarioRepository scenarioRepository)
-        {
-            _businessUnitRepository = businessUnitRepository;
-	        _requestStrategySettingsReader = requestStrategySettingsReader;
-	        _jobStartTimeRepository = jobStartTimeRepository;
-	        _updatedBySystemUser = updatedBySystemUser;
-	        _publisher = publisher;
+			ISkillDayRepository skillDayRepository, IScenarioRepository scenarioRepository, IBusinessUnitScope businessUnitScope,
+			ICurrentUnitOfWorkFactory currentUnitOfWorkFactory)
+		{
+			_businessUnitRepository = businessUnitRepository;
+			_requestStrategySettingsReader = requestStrategySettingsReader;
+			_jobStartTimeRepository = jobStartTimeRepository;
+			_updatedBySystemUser = updatedBySystemUser;
+			_publisher = publisher;
 			_staffingSettingsReader = staffingSettingsReader;
 			_skillDayRepository = skillDayRepository;
-	        _scenarioRepository = scenarioRepository;
+			_scenarioRepository = scenarioRepository;
+			_businessUnitScope = businessUnitScope;
+			_currentUnitOfWorkFactory = currentUnitOfWorkFactory;
 		}
 
-		[UnitOfWork]
 		public virtual void Handle(TenantMinuteTickEvent @event)
-        {
-		    var updateResourceReadModelIntervalMinutes =
-			    _requestStrategySettingsReader.GetIntSetting("UpdateResourceReadModelIntervalMinutes", 60);
+		{
+			var updateResourceReadModelIntervalMinutes =
+				_requestStrategySettingsReader.GetIntSetting("UpdateResourceReadModelIntervalMinutes", 60);
 			var numberOfDays = _staffingSettingsReader.GetIntSetting("StaffingReadModelNumberOfDays", 14);
-			var businessUnits = _businessUnitRepository.LoadAll();
-	        using (_updatedBySystemUser.Context())
-	        {
-				businessUnits.ForEach(businessUnit =>
-				{	
-					var businessUnitId = businessUnit.Id.GetValueOrDefault();
+			List<IBusinessUnit> businessUnits;
+			using (_currentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				businessUnits = _businessUnitRepository.LoadAll().ToList();
+			}
 
-					var scenario = _scenarioRepository.LoadDefaultScenario(businessUnit);
-					if (scenario != null)
+			businessUnits.ForEach(businessUnit =>
+			{
+				var businessUnitId = businessUnit.Id.GetValueOrDefault();
+				using (_businessUnitScope.OnThisThreadUse(businessUnit))
+				{
+					using (_currentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
 					{
-						if (_skillDayRepository.HasSkillDaysWithinPeriod(DateOnly.Today, DateOnly.Today.AddDays(numberOfDays),
-							businessUnit, scenario))
+						using (_updatedBySystemUser.Context())
 						{
-							if (!_jobStartTimeRepository.CheckAndUpdate(updateResourceReadModelIntervalMinutes, businessUnitId)) return;
-
-							_publisher.Publish(new UpdateStaffingLevelReadModelEvent
+							var scenario = _scenarioRepository.LoadDefaultScenario();
+							if (scenario != null)
 							{
-								Days = numberOfDays,
-								LogOnBusinessUnitId = businessUnitId
-							});
+								if (_skillDayRepository.HasSkillDaysWithinPeriod(DateOnly.Today, DateOnly.Today.AddDays(numberOfDays),
+									businessUnit, scenario))
+								{
+									if (!_jobStartTimeRepository.CheckAndUpdate(updateResourceReadModelIntervalMinutes, businessUnitId)) return;
+
+									_publisher.Publish(new UpdateStaffingLevelReadModelEvent
+									{
+										Days = numberOfDays,
+										LogOnBusinessUnitId = businessUnitId
+									});
+								}
+							}
 						}
 					}
-						
-				});
-			}
+				}
+			});
 		}
-    }
+	}
 }
