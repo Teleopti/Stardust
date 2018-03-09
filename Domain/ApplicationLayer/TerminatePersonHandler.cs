5bf70f8499c51df61a03ee8a47cf9122ad48243f
SquidSquad <bharath.muchi@teleopti.com>
2018-03-09 07:41:16 +0100

    PBI #47768 publish schedulechangeevent on deleting schedules

--- Domain/ApplicationLayer/TerminatePersonHandler.cs
+++ Domain/ApplicationLayer/TerminatePersonHandler.cs
@@ -1,28 +1,38 @@
-﻿using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.ApplicationLayer
 {
+	[EnabledBy(Toggles.WFM_Clear_Data_After_Leaving_Date_47768)]
 	public class TerminatePersonHandler:IHandleEvent<PersonTerminalDateChangedEvent>, IRunOnHangfire
-
 	{
 		private readonly IPersonRepository _personRepository;
 		private readonly IScenarioRepository _scenarioRepository;
 		private readonly IPersonAssignmentRepository _personAssignmentRepository;
 		private readonly IPersonAbsenceRepository _personAbsenceRepository;
+		private readonly IEventPopulatingPublisher _eventPublisher;
 
 		public TerminatePersonHandler(IPersonRepository personRepository, IScenarioRepository scenarioRepository,
-			IPersonAssignmentRepository personAssignmentRepository, IPersonAbsenceRepository personAbsenceRepository)
+			IPersonAssignmentRepository personAssignmentRepository, IPersonAbsenceRepository personAbsenceRepository,
+			IEventPopulatingPublisher eventPublisher)
 		{
 			_personRepository = personRepository;
 			_scenarioRepository = scenarioRepository;
 			_personAssignmentRepository = personAssignmentRepository;
 			_personAbsenceRepository = personAbsenceRepository;
+			_eventPublisher = eventPublisher;
 		}
 
-		public void Handle(PersonTerminalDateChangedEvent @event)
+		[ImpersonateSystem]
+		[UnitOfWork]
+		public virtual void Handle(PersonTerminalDateChangedEvent @event)
 		{
 			if(!@event.TerminationDate.HasValue)
 				return;
@@ -30,17 +40,20 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer
 			var person = _personRepository.Get(@event.PersonId);
 			var period = new DateOnlyPeriod(leavingDate.AddDays(1), DateOnly.MaxValue);
 			var dateTimePeriod = period.ToDateTimePeriod(person.PermissionInformation.DefaultTimeZone());
+			var events = new List<IEvent>();
 
-			foreach (var scenario in _scenarioRepository.LoadAll())
+			var scenarios = _scenarioRepository.LoadAll();
+			foreach (var scenario in scenarios)
 			{
 				var assignmentsToRemove = _personAssignmentRepository.Find(new[] { person }, period, scenario);
+				var allPeriods = new List<DateTimePeriod>();
 				foreach (var personAssignment in assignmentsToRemove)
 				{
 					_personAssignmentRepository.Remove(personAssignment);
+					allPeriods.Add(personAssignment.Period);
 				}
 
-				var absencesToRemove = _personAbsenceRepository.Find(new[] { person },
-					dateTimePeriod, scenario);
+				var absencesToRemove = _personAbsenceRepository.Find(new[] { person }, dateTimePeriod, scenario);
 				foreach (var personAbsence in absencesToRemove)
 				{
 					if (personAbsence.Period.StartDateTime < dateTimePeriod.StartDateTime)
@@ -48,10 +61,31 @@ namespace Teleopti.Ccc.Domain.ApplicationLayer
 						continue;
 					}
 					((IRepository<IPersonAbsence>)_personAbsenceRepository).Remove(personAbsence);
+					allPeriods.Add(personAbsence.Period);
 				}
+
+				if (!allPeriods.Any())
+				{
+					continue;
+				}
+
+				var periodStart = allPeriods.Select(x => x.StartDateTime).Min();
+				var periodEnd = allPeriods.Select(x => x.EndDateTime).Max();
+				
+				events.Add(new ScheduleChangedEvent
+				{
+					LogOnBusinessUnitId = scenario.BusinessUnit.Id.GetValueOrDefault(),
+					ScenarioId = scenario.Id.Value,
+					PersonId = @event.PersonId,
+					StartDateTime = periodStart,
+					EndDateTime = periodEnd
+				});
 			}
-			
-			person.RemoveAllPeriodsAfter(leavingDate);
+
+			//Ask anders
+			//person.RemoveAllPeriodsAfter(leavingDate);
+
+			_eventPublisher.Publish(events.ToArray());
 		}
 	}
 }
--- DomainTest/ApplicationLayer/TerminatePersonHandlerTest.cs
+++ DomainTest/ApplicationLayer/TerminatePersonHandlerTest.cs
@@ -7,6 +7,7 @@ using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
@@ -17,7 +18,7 @@ using Teleopti.Interfaces.Domain;
 namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 {
 	[DomainTest]
-	public class TerminatePersonHandlerTest
+	public class TerminatePersonHandlerTest : ISetup
 	{
 		public MutableNow Now;
 		public IPersonAssignmentRepository PersonAssignmentRepository;
@@ -25,21 +26,36 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		public IScenarioRepository ScenarioRepository;
 		public IPersonRepository PersonRepository;
 		public TerminatePersonHandler Target;
+		public FakeEventPublisher Publisher;
+		public IBusinessUnitRepository BusinessUnitRepository;
+
+		private IBusinessUnit businessUnit;
+
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.AddService<TerminatePersonHandler>();
+			businessUnit = BusinessUnitFactory.CreateWithId("something");
+		}
 
 		[Test]
 		public void ShouldRemovePersonAssignmentAfterLeavingDate()
 		{
+			BusinessUnitRepository.Add(businessUnit);
 			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
+			ScenarioRepository.Add(scenario);
+
 			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
 				new DateOnly(2001, 1, 1));
-
-			ScenarioRepository.Add(scenario);
 			PersonRepository.Add(person);
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 3, 8, 2018, 1, 3, 17)).WithId());
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 4, 8, 2018, 1, 4, 17)).WithId());
-			
-			Target.Handle(new PersonTerminalDateChangedEvent()
+
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 3, 8, 2018, 1, 3, 17)).WithId());
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 4, 8, 2018, 1, 4, 17)).WithId());
+
+			Target.Handle(new PersonTerminalDateChangedEvent
 			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
 				PersonId = person.Id.Value,
 				TerminationDate = new DateTime(2018, 1, 3)
 			});
@@ -52,19 +68,24 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		[Test]
 		public void ShouldNotRemoveScheduleWhenClearingLeavingDate()
 		{
+			BusinessUnitRepository.Add(businessUnit);
 			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
+			ScenarioRepository.Add(scenario);
+
 			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
 				new DateOnly(2001, 1, 1));
-
-			ScenarioRepository.Add(scenario);
 			PersonRepository.Add(person);
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 1, 8, 2018, 1, 1, 17)).WithId());
-			
-			Target.Handle(new PersonTerminalDateChangedEvent()
+
+			var personAssignment = PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 1, 8, 2018, 1, 1, 17)).WithId();
+			PersonAssignmentRepository.Add(personAssignment);
+
+			Target.Handle(new PersonTerminalDateChangedEvent
 			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
 				PersonId = person.Id.Value
 			});
-			
+
 			PersonAssignmentRepository.Find(Enumerable.Repeat(person, 1), new DateOnlyPeriod(2018, 1, 1, 2018, 1, 2), scenario)
 				.Count
 				.Should().Be.EqualTo(1);
@@ -73,30 +94,40 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		[Test]
 		public void ShouldRemoveScheduleAfterLeavingDateInAllScenarios()
 		{
+			BusinessUnitRepository.Add(businessUnit);
 			var defaultScenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
 			var scenario = ScenarioFactory.CreateScenario("High", false, false).WithId();
-			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
-				new DateOnly(2001, 1, 1));
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, defaultScenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
-			PersonAssignmentRepository.Add(PersonAssignmentFactory.CreateAssignmentWithMainShift(person, defaultScenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
-
-			PersonRepository.Add(person);
 			ScenarioRepository.Add(defaultScenario);
 			ScenarioRepository.Add(scenario);
-			
-			Target.Handle(new PersonTerminalDateChangedEvent()
+
+			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
+				new DateOnly(2001, 1, 1));
+			PersonRepository.Add(person);
+
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, defaultScenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17))
+				.WithId());
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, defaultScenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17))
+				.WithId());
+
+			Target.Handle(new PersonTerminalDateChangedEvent
 			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
 				PersonId = person.Id.Value,
 				TerminationDate = new DateTime(2001, 1, 31)
 			});
-			
+
 			PersonAssignmentRepository.Find(Enumerable.Repeat(person, 1), new DateOnlyPeriod(2001, 1, 1, 2001, 2, 28), scenario)
 				.Single().Period
 				.Should().Be(new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17));
 
-			PersonAssignmentRepository.Find(Enumerable.Repeat(person, 1), new DateOnlyPeriod(2001, 1, 1, 2001, 2, 28), defaultScenario)
+			PersonAssignmentRepository.Find(Enumerable.Repeat(person, 1), new DateOnlyPeriod(2001, 1, 1, 2001, 2, 28),
+					defaultScenario)
 				.Single().Period
 				.Should().Be(new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17));
 		}
@@ -105,16 +136,22 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		[Test]
 		public void ShouldRemoveAbsenceAfterLeavingDate()
 		{
+			BusinessUnitRepository.Add(businessUnit);
 			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
 			ScenarioRepository.Add(scenario);
+
 			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
 				new DateOnly(2001, 1, 1));
 			PersonRepository.Add(person);
-			PersonAbsenceRepository.Add(PersonAbsenceFactory.CreatePersonAbsence(person, scenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
-			PersonAbsenceRepository.Add(PersonAbsenceFactory.CreatePersonAbsence(person, scenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
+
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, scenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, scenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
 
 			Target.Handle(new PersonTerminalDateChangedEvent
 			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
 				PersonId = person.Id.Value,
 				TerminationDate = new DateTime(2001, 1, 31)
 			});
@@ -127,6 +164,7 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		[Test]
 		public void ShouldNotRemoveAbsenceSpanningOverLeavingDate()
 		{
+			BusinessUnitRepository.Add(businessUnit);
 			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
 			scenario.SetBusinessUnit(BusinessUnitFactory.BusinessUnitUsedInTest);
 			ScenarioRepository.Add(scenario);
@@ -135,10 +173,12 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 				new DateOnly(2001, 1, 1));
 			PersonRepository.Add(person);
 
-			((IRepository<IPersonAbsence>)PersonAbsenceRepository).Add(PersonAbsenceFactory.CreatePersonAbsence(person, scenario, new DateTimePeriod(2001, 1, 31, 8, 2001, 2, 1, 17)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, scenario, new DateTimePeriod(2001, 1, 31, 8, 2001, 2, 1, 17)).WithId());
 
 			Target.Handle(new PersonTerminalDateChangedEvent
 			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
 				PersonId = person.Id.Value,
 				TerminationDate = new DateTime(2001, 1, 31)
 			});
@@ -151,31 +191,160 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer
 		[Test]
 		public void ShouldRemoveAbsenceAfterLeavingDateInAllScenarios()
 		{
+			BusinessUnitRepository.Add(businessUnit);
 			var defaultScenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
 			var otherScenario = ScenarioFactory.CreateScenario("High", false, false).WithId();
-			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
-				new DateOnly(2001, 1, 1));
-			PersonAbsenceRepository.Add(PersonAbsenceFactory.CreatePersonAbsence(person, defaultScenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
-			PersonAbsenceRepository.Add(PersonAbsenceFactory.CreatePersonAbsence(person, defaultScenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
-			PersonAbsenceRepository.Add(PersonAbsenceFactory.CreatePersonAbsence(person, otherScenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
-			PersonAbsenceRepository.Add(PersonAbsenceFactory.CreatePersonAbsence(person, otherScenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
-
-			PersonRepository.Add(person);
 			ScenarioRepository.Add(defaultScenario);
 			ScenarioRepository.Add(otherScenario);
 
+			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
+				new DateOnly(2001, 1, 1));
+			PersonRepository.Add(person);
+
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, defaultScenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, defaultScenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, otherScenario, new DateTimePeriod(2001, 1, 1, 8, 2001, 1, 1, 17)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, otherScenario, new DateTimePeriod(2001, 2, 1, 8, 2001, 2, 1, 17)).WithId());
+
 			Target.Handle(new PersonTerminalDateChangedEvent
 			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
 				PersonId = person.Id.Value,
 				TerminationDate = new DateTime(2001, 1, 31)
 			});
 
-			PersonAbsenceRepository.Find(Enumerable.Repeat(person, 1), new DateTimePeriod(2001, 1, 1, 2001, 2, 28), defaultScenario)
+			PersonAbsenceRepository
+				.Find(Enumerable.Repeat(person, 1), new DateTimePeriod(2001, 1, 1, 2001, 2, 28), defaultScenario)
 				.Count
 				.Should().Be.EqualTo(1);
-			PersonAbsenceRepository.Find(Enumerable.Repeat(person, 1), new DateTimePeriod(2001, 1, 1, 2001, 2, 28), otherScenario)
+			PersonAbsenceRepository
+				.Find(Enumerable.Repeat(person, 1), new DateTimePeriod(2001, 1, 1, 2001, 2, 28), otherScenario)
 				.Count
 				.Should().Be.EqualTo(1);
 		}
+
+		[Test]
+		public void ShouldPublishScheduleChangeEventOnEveryScenario()
+		{
+			BusinessUnitRepository.Add(businessUnit);
+			var defaultScenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
+			var otherScenario = ScenarioFactory.CreateScenario("High", false, false).WithId();
+			ScenarioRepository.Add(defaultScenario);
+			ScenarioRepository.Add(otherScenario);
+
+			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
+				new DateOnly(2001, 1, 1));
+			PersonRepository.Add(person);
+
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, defaultScenario, new DateTimePeriod(2018, 1, 2, 7, 2018, 1, 2, 16))
+				.WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, otherScenario, new DateTimePeriod(2018, 1, 3, 8, 2018, 1, 3, 17)).WithId());
+
+			var @event = new PersonTerminalDateChangedEvent
+			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
+				PersonId = person.Id.Value,
+				TerminationDate = new DateTime(2018, 01, 01)
+			};
+			Target.Handle(@event);
+
+			var published = Publisher.PublishedEvents.OfType<ScheduleChangedEvent>().ToList();
+			published.Count.Should().Be(2);
+			published.Any(x => x.ScenarioId == defaultScenario.Id.Value).Should().Be.True();
+			published.Any(x => x.ScenarioId == otherScenario.Id.Value).Should().Be.True();
+			published.All(x => x.PersonId == @event.PersonId).Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldPublishScheduleChangeEventWithValidPeriod()
+		{
+			BusinessUnitRepository.Add(businessUnit);
+			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
+			ScenarioRepository.Add(scenario);
+
+			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
+				new DateOnly(2001, 1, 1));
+			PersonRepository.Add(person);
+
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 2, 7, 2018, 1, 2, 16)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, scenario, new DateTimePeriod(2018, 1, 3, 8, 2018, 1, 3, 17)).WithId());
+			PersonAbsenceRepository.Add(PersonAbsenceFactory
+				.CreatePersonAbsence(person, scenario, new DateTimePeriod(2018, 1, 01, 8, 2018, 1, 02, 17)).WithId());
+
+			var @event = new PersonTerminalDateChangedEvent
+			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
+				PersonId = person.Id.Value,
+				TerminationDate = new DateTime(2018, 01, 01),
+			};
+			Target.Handle(@event);
+
+			var published = Publisher.PublishedEvents.OfType<ScheduleChangedEvent>().Single();
+			published.StartDateTime.Should().Be(new DateTime(2018, 1, 2, 7, 0, 0));
+			published.EndDateTime.Should().Be(new DateTime(2018, 1, 3, 17, 0, 0));
+		}
+
+		[Test]
+		public void ShouldNotPublishScheduleChangeEventWhenNoSchedulesDeleted()
+		{
+			BusinessUnitRepository.Add(businessUnit);
+			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
+			ScenarioRepository.Add(scenario);
+
+			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
+				new DateOnly(2001, 1, 1));
+			PersonRepository.Add(person);
+
+			var @event = new PersonTerminalDateChangedEvent
+			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
+				PersonId = person.Id.Value,
+				TerminationDate = new DateTime(2018, 01, 01)
+			};
+			Target.Handle(@event);
+
+			Publisher.PublishedEvents.OfType<ScheduleChangedEvent>().Count().Should().Be(0);
+		}
+
+		[Test]
+		public void ShouldOnlyPublishScheduleChangeEventForScenarioWithSchedulesDeleted()
+		{
+			BusinessUnitRepository.Add(businessUnit);
+			var scenario = ScenarioFactory.CreateScenario("Default", true, false).WithId();
+			ScenarioRepository.Add(scenario);
+
+			var anotherScenario = ScenarioFactory.CreateScenario("High", true, false).WithId();
+			ScenarioRepository.Add(anotherScenario);
+
+			var person = PersonFactory.CreatePersonWithValidVirtualSchedulePeriod(PersonFactory.CreatePerson("A", "B").WithId(),
+				new DateOnly(2001, 1, 1));
+			PersonRepository.Add(person);
+
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, scenario, new DateTimePeriod(2018, 1, 2, 7, 2018, 1, 2, 16)).WithId());
+			PersonAssignmentRepository.Add(PersonAssignmentFactory
+				.CreateAssignmentWithMainShift(person, anotherScenario, new DateTimePeriod(2018, 1, 1, 7, 2018, 1, 1, 16))
+				.WithId());
+
+			var @event = new PersonTerminalDateChangedEvent
+			{
+				LogOnBusinessUnitId = businessUnit.Id.Value,
+				PersonId = person.Id.Value,
+				TerminationDate = new DateTime(2018, 01, 01)
+			};
+			Target.Handle(@event);
+
+			var published = Publisher.PublishedEvents.OfType<ScheduleChangedEvent>().Single();
+			published.ScenarioId.Should().Be(scenario.Id.Value);
+		}
+
 	}
 }
