9c768a037a523824205363e35b8cc5e9c5a4c3f9
Kunning Mao <kunning.mao@teleopti.com>
2018-03-14 15:32:01 +0100

    #47654 Failing test. Should pick relativedifference from possible more than one skillday.

---
+++ DomainTest/SchedulingScenarios/DayOffOptimization/SchedulingAndDayOffOptimizationStardustResultTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.ApplicationLayer.ResourcePlanner;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Optimization;
+using Teleopti.Ccc.Domain.Optimization.Filters;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Security;
+using Teleopti.Ccc.Domain.Security.Authentication;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.DayOffOptimization
+{
+	[DomainTest]
+	public class SchedulingAndDayOffOptimizationStardustResultTest : DayOffOptimizationScenario
+	{
+		public FakePersonAssignmentRepository PersonAssignmentRepository;
+		public FakeSkillDayRepository SkillDayRepository;
+		public FakeSkillRepository SkillRepository;
+		public FakePersonRepository PersonRepository;
+		public FakeScenarioRepository ScenarioRepository;
+		public FakeActivityRepository ActivityRepository;
+		public FakePlanningPeriodRepository PlanningPeriodRepository;
+		public FakePlanningGroupRepository PlanningGroupRepository;
+		public FakeBusinessUnitRepository BusinessUnitRepository;
+		public ICurrentBusinessUnit CurrentBusinessUnit;
+		public FakeJobResultRepository JobResultRepository;
+		public FakeEventPublisher FakeEventPublisher;
+
+		public SchedulePlanningPeriodCommandHandler Target;
+
+		public SchedulingAndDayOffOptimizationStardustResultTest(SeperateWebRequest seperateWebRequest, bool resourcePlannerDayOffOptimizationIslands47208) : base(seperateWebRequest, resourcePlannerDayOffOptimizationIslands47208)
+		{
+		}
+
+		//remove somehow?
+		public override void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.UseTestDouble<UserTimeZone>().For<IUserTimeZone>();
+			system.UseTestDouble<TimeZoneGuard>().For<ITimeZoneGuard>();
+		}
+
+		[TestCase("Mountain Standard Time", "Mountain Standard Time")]
+		[TestCase("Mountain Standard Time", "UTC")]
+		[Ignore("47654")]
+		public void ShouldHandleTimeZoneIssues(string skillTimeZoneStr, string enduserTimeZoneStr)
+		{
+			//TODO
+			FakeEventPublisher.AddHandler<WebDayoffOptimizationStardustHandler>();
+			FakeEventPublisher.AddHandler<WebScheduleStardustHandler>();
+			//
+
+			//fix
+			var systemUser = new Person().WithName(new Name("system","system")).WithId(SystemUser.Id)
+				.InTimeZone(TimeZoneInfo.Utc);
+			TimeZoneGuard.Instance.TimeZone = systemUser.PermissionInformation.DefaultTimeZone();
+			PersonRepository.Has(systemUser);
+			BusinessUnitRepository.Has(CurrentBusinessUnit.Current());
+			//
+			var date = new DateOnly(2015, 10, 12); //mon
+			var activity = ActivityRepository.Has("_");
+			var skillTimeZone = TimeZoneInfo.FindSystemTimeZoneById(skillTimeZoneStr);
+			var skill = SkillRepository.Has("relevant skill", activity).InTimeZone(skillTimeZone);
+			var scenario = ScenarioRepository.Has("some name");
+			var schedulePeriod = new SchedulePeriod(date, SchedulePeriodType.Day, 1);
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var filterContract = new ContractWithMaximumTolerance().WithId();
+			var agent = PersonRepository.Has(filterContract, new ContractSchedule("_"), new PartTimePercentage("_"), new Team { Site = new Site("site") }, schedulePeriod, skill)
+				.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById(enduserTimeZoneStr)); //to "match" 0-24 in end user's timezone
+			var planningGroup = new PlanningGroup("_").AddFilter(new ContractFilter(filterContract));
+			var planningPeriod = PlanningPeriodRepository.Has(date, 1, SchedulePeriodType.Day, planningGroup);
+			PlanningGroupRepository.Has(planningGroup);
+			SkillDayRepository.Has(skill.CreateSkillDaysWithDemandOnConsecutiveDays(scenario, date, 1, 1, 1));
+			PersonAssignmentRepository.Has(agent, scenario, activity, shiftCategory, date.ToDateOnlyPeriod(), new TimePeriod(0, 24));
+
+			Target.Execute(planningPeriod.Id.Value, true);
+
+			var result = JsonConvert.DeserializeObject<OptimizationResultModel>(
+					JobResultRepository.LoadAllWithNoLock().Single().Details.Last().Message); 
+			var skillResult = result.SkillResultList.ToList();
+			var endUserDate = skillResult.First().SkillDetails.First();
+			endUserDate.RelativeDifference.Should().Be(0);
+		}
+	}
+}
