442558dda90c4b64ff55fb8ea95caaaada9467c5
Robin Karlsson <robin.karlsson@teleopti.com>
2018-03-20 16:30:29 +0100

    Query listing test. (https://teleopti.visualstudio.com/TeleoptiWFM/_workitems/edit/147)

---
+++ Teleopti.Wfm.Api.Test/QueryListingTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Teleopti.Wfm.Api.Test
+{
+	public class QueryListingTest : ApiTest
+	{
+		[Test]
+		public async Task ShouldListQueries()
+		{
+			Authorize();
+
+			var result = await Client.GetAsync("/api/wfm/query");
+			result.EnsureSuccessStatusCode();
+
+			var content = await result.Content.ReadAsStringAsync();
+			var obj = JToken.Parse(content);
+			obj["Result"].Children().Should().Be.Empty();
+		}
+	}
+}
