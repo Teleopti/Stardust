4aa832f58f3f56dce40cfc4a4dc18aecd8bcc578
Rockman <none@none>
2018-03-22 15:13:18 +0100

    #46942

--- Teleopti.Wfm.Administration/Etl/etlConfigController.js
+++ Teleopti.Wfm.Administration/Etl/etlConfigController.js
@@ -18,8 +18,15 @@
       $http.get("./Etl/GetTenants", tokenHeaderService.getHeaders())
       .success(function (data) {
         vm.tenants  = data;
+        addUrl();
       });
     }
 
+    function addUrl() {
+      for (var i = 0; i < vm.tenants.length; i++) {
+        vm.tenants[i].Url = encodeURIComponent(vm.tenants[i].TenantName)
+      }
+    }
+    
   }
 })();
