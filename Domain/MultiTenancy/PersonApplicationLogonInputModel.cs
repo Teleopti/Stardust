f5f1b69271003a1d4c40466f560b85c4cd1345c4
Dan Brinkheden <none@none>
2018-03-22 09:58:15 +0100

    #48142 Added route for changing the logonname

---
+++ Domain/MultiTenancy/PersonApplicationLogonInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.MultiTenancy
+{
+	public class PersonApplicationLogonInputModel
+	{
+		public List<PersonApplicationLogonModel> People;
+	}
+}
---
+++ Domain/MultiTenancy/PersonApplicationLogonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.MultiTenancy
+{
+	public class PersonApplicationLogonModel
+	{
+		public string ApplicationLogonName { get; set; }
+		public Guid
+			PersonId { get; set; }
+	}
+}
