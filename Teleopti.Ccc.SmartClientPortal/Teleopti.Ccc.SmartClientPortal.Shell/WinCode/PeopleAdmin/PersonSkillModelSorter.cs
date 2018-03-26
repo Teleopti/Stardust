032efc27a8b045f891f32dcf4502240856e7d98f
claess <claes.stenman@teleopti.com>
2018-03-26 11:47:34 +0200

    #48794 Sort skills people module

---
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/WinCode/PeopleAdmin/PersonSkillModelSorter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin.Models;
+namespace Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin
+{
+	public class PersonSkillModelSorter
+	{
+		public IEnumerable<PersonSkillModel> Sort(IEnumerable<PersonSkillModel> personSkillModels)
+		{
+			var sorted = new List<PersonSkillModel>();
+			var someHave = new List<PersonSkillModel>();
+			var noneHave = new List<PersonSkillModel>();
+
+			foreach (var personSkillModel in personSkillModels.OrderBy(x => x.DescriptionText))
+			{
+				switch (personSkillModel.TriState)
+				{
+					case 1:
+						sorted.Add(personSkillModel);
+						break;
+					case 2:
+						someHave.Add(personSkillModel);
+						break;
+					default:
+						noneHave.Add(personSkillModel);
+						break;
+				}
+			}
+
+			sorted.AddRange(someHave);
+			sorted.AddRange(noneHave);
+
+			return sorted;
+		}
+	}
+}
---
+++ Teleopti.Ccc.WinCodeTest/PeopleAdmin/PersonSkillModelSorterTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin;
+using Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin.Models;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.WinCodeTest.PeopleAdmin
+{
+	[TestFixture]
+	public class PersonSkillModelSorterTest
+	{
+
+		[Test]
+		public void ShouldSort()
+		{
+			var personSkillModelSorter = new PersonSkillModelSorter();
+			var personSkillModelAllHave1 = new PersonSkillModel {TriState = 1, ContainedEntity = new PersonSkill(new Skill("A_AllHave"), new Percent()).WithId()};
+			var personSkillModelAllHave2 = new PersonSkillModel {TriState = 1, ContainedEntity = new PersonSkill(new Skill("B_AllHave"), new Percent()).WithId()};
+			var personSkillModelSomeHave1 = new PersonSkillModel {TriState = 2, ContainedEntity = new PersonSkill(new Skill("A_SomeHave"), new Percent()).WithId()};
+			var personSkillModelSomeHave2 = new PersonSkillModel {TriState = 2, ContainedEntity = new PersonSkill(new Skill("B_SomeHave"), new Percent()).WithId()};
+			var personSkillModelNoneHave1 = new PersonSkillModel {TriState = 0, ContainedEntity = new PersonSkill(new Skill("A_NoneHave"), new Percent()).WithId()};
+			var personSkillModelNoneHave2 = new PersonSkillModel {TriState = 0, ContainedEntity = new PersonSkill(new Skill("B_NoneHave"), new Percent()).WithId()};
+			var personSkillModels = new List<PersonSkillModel>
+			{
+				personSkillModelNoneHave2,
+				personSkillModelAllHave2,
+				personSkillModelSomeHave1,
+				personSkillModelNoneHave1,
+				personSkillModelAllHave1,
+				personSkillModelSomeHave2
+			};
+
+			var result = personSkillModelSorter.Sort(personSkillModels).ToList();
+
+			result[0].Should().Be.EqualTo(personSkillModelAllHave1);
+			result[1].Should().Be.EqualTo(personSkillModelAllHave2);
+			result[2].Should().Be.EqualTo(personSkillModelSomeHave1);
+			result[3].Should().Be.EqualTo(personSkillModelSomeHave2);
+			result[4].Should().Be.EqualTo(personSkillModelNoneHave1);
+			result[5].Should().Be.EqualTo(personSkillModelNoneHave2);
+		}
+	}
+}
