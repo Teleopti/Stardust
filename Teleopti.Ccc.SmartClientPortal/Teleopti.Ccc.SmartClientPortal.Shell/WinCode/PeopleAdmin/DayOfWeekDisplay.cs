8c30743137fd14b282c2765c92022ba9850c7eb2
Robin Karlsson <robin.karlsson@teleopti.com>
2018-03-26 15:52:50 +0200

    Cleaning up

--- Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/WinCode/PeopleAdmin/DayOfWeekDisplay.cs
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/WinCode/PeopleAdmin/DayOfWeekDisplay.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Interfaces.Domain;
 
@@ -8,8 +9,8 @@ namespace Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin
 {
 	public class DayOfWeekDisplay : IComparable
 	{
-		public DayOfWeek DayOfWeek { get; private set; }
-		public string DisplayName { get; private set; }
+		public DayOfWeek DayOfWeek { get; }
+		public string DisplayName { get; }
 
 		public DayOfWeekDisplay(DayOfWeek dayOfWeek, string displayName)
 		{
@@ -17,28 +18,13 @@ namespace Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin
 			DisplayName = displayName;
 		}
 
-		public static IList<DayOfWeekDisplay> ListOfDayOfWeek
-		{
-		get
-		{
-			var ret = new List<DayOfWeekDisplay>();
-			IList<DayOfWeek> dayOfWeekCollection = DateHelper.GetDaysOfWeek(CultureInfo.CurrentUICulture);
-
-			foreach (var dayOfWeek in dayOfWeekCollection)
-			{
-				ret.Add(new DayOfWeekDisplay(dayOfWeek, CultureInfo.CurrentUICulture.DateTimeFormat.GetDayName(dayOfWeek)));
-			}
-
-			return ret;
-		}
-			
-		}
+		public static IList<DayOfWeekDisplay> ListOfDayOfWeek => DateHelper.GetDaysOfWeek(CultureInfo.CurrentUICulture).Select(dayOfWeek =>
+			new DayOfWeekDisplay(dayOfWeek, CultureInfo.CurrentUICulture.DateTimeFormat.GetDayName(dayOfWeek))).ToList();
 
 		public int CompareTo(object other)
 		{
 			var o = other as DayOfWeekDisplay;
 			return o != null ? string.Compare(DisplayName, o.DisplayName, true, TeleoptiPrincipal.CurrentPrincipal.Regional.Culture) : -1;
 		}
-		
 	}
 }
--- Teleopti.Ccc.WinCodeTest/PeopleAdmin/DayOfWeekDisplayTest.cs
+++ Teleopti.Ccc.WinCodeTest/PeopleAdmin/DayOfWeekDisplayTest.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.SmartClientPortal.Shell.WinCode.PeopleAdmin;
@@ -9,62 +8,61 @@ namespace Teleopti.Ccc.WinCodeTest.PeopleAdmin
     [TestFixture]
     public class DayOfWeekDisplayTest
     {
-		IList<DayOfWeekDisplay> _days;
-
-		[SetUp]
-		public void Setup()
-		{
-			_days = DayOfWeekDisplay.ListOfDayOfWeek;
-		}
-
 		[Test, SetCulture("en-US"), SetUICulture("sv-SE")]
         public void ShouldReturnListWithSevenDays()
         {
-        	Assert.That(_days.Count, Is.EqualTo(7));
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			Assert.That(days.Count, Is.EqualTo(7));
         }
 
 		[Test, SetUICulture("sv-SE")]
 		public void ShouldDisplayWeekdayNamesInSwedish()
 		{
-			Assert.That(_days[0].DisplayName, Is.EqualTo("måndag"));
-			Assert.That(_days[6].DisplayName, Is.EqualTo("söndag"));
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			Assert.That(days[0].DisplayName, Is.EqualTo("måndag"));
+			Assert.That(days[6].DisplayName, Is.EqualTo("söndag"));
 		}
 
 		[Test, SetUICulture("en-US")]
 		public void ShouldDisplayWeekdayNamesInEnglish()
 		{
-			Assert.That(_days[0].DisplayName, Is.EqualTo("Sunday"));
-			Assert.That(_days[6].DisplayName, Is.EqualTo("Saturday"));
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			Assert.That(days[0].DisplayName, Is.EqualTo("Sunday"));
+			Assert.That(days[6].DisplayName, Is.EqualTo("Saturday"));
 		}
 
 		[Test, SetCulture("sv-SE"), SetUICulture("en-US")]
         public void ShouldHaveEnglishNamesOnEnglishCulture()
         {
-            Assert.That(_days.Count, Is.EqualTo(7));
-            Assert.That((int)_days[0].DayOfWeek, Is.EqualTo(0));
-            Assert.That((int)_days[1].DayOfWeek, Is.EqualTo(1));
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			Assert.That(days.Count, Is.EqualTo(7));
+            Assert.That((int)days[0].DayOfWeek, Is.EqualTo(0));
+            Assert.That((int)days[1].DayOfWeek, Is.EqualTo(1));
         }
 
 		[Test, SetUICulture("en-GB")]
 		public void ShouldReturnMondayAsFirstDayOfWeek()
 		{
-			_days[0].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Monday);
-			_days[6].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Sunday);
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			days[0].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Monday);
+			days[6].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Sunday);
 		}
 
 		[Test, SetUICulture("en-US")]
 		public void ShouldReturnSundayAsFirstDayOfWeek()
 		{
-			_days[0].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Sunday);
-			_days[6].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Saturday);
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			days[0].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Sunday);
+			days[6].DayOfWeek.Should().Be.EqualTo(DayOfWeek.Saturday);
 		}
 
 		[Test, SetUICulture("sv-SE")]
 	    public void ShouldCompare()
 		{
-			var monday = _days[0];
-			var tuesday = _days[1];
-			var anotherTuesday = _days[1];
+			var days = DayOfWeekDisplay.ListOfDayOfWeek;
+			var monday = days[0];
+			var tuesday = days[1];
+			var anotherTuesday = days[1];
 
 			var result = monday.CompareTo(tuesday);
 			Assert.AreEqual(-1, result);
