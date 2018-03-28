f4768413eb3ba3c27f4bc210fcbc7a13db604ba5
LevelUp! <levelup@teleopti.com>
2018-03-28 17:24:44 +0200

    #48724 View

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -30644,6 +30644,15 @@ namespace Teleopti.Ccc.UserTexts {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Select sites / teams.
+        /// </summary>
+        public static string SelectSitesTeams {
+            get {
+                return ResourceManager.GetString("SelectSitesTeams", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Select a skill.
         /// </summary>
         public static string SelectSkill {
@@ -30685,6 +30694,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string SelectStartDateAndEndDate {
             get {
                 return ResourceManager.GetString("SelectStartDateAndEndDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Select states.
+        /// </summary>
+        public static string SelectStates {
+            get {
+                return ResourceManager.GetString("SelectStates", resourceCulture);
             }
         }
         
