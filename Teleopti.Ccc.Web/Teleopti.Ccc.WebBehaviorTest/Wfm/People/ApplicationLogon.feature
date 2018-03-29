c54ea60f10ad2a4a72c1dc6782858b86c3b8a3b8
emils <none@none>
2018-03-29 09:39:17 +0200

    #74921: Behaviour tests (ignored initially)

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/People/ApplicationLogon.feature
@@ -0,0 +1,46 @@
+﻿@People
+@OnlyRunIfEnabled('Wfm_PeopleWeb_PrepareForRelease_47766')
+Feature: AppLogon
+	In order to change application logon for a person
+	As a teamleader
+	I want to be able to see current application logon and easily change it for multiple people
+
+Background:
+	Given I have a role with full access
+	And WA People exists
+	| Name		|
+	| Ashley	|
+	| Anthony	|
+	| Abraham	|
+	
+	And Person 'Ashley' has app logon name ''
+	And Person 'Anthony' has app logon name 'anthony@dish.com'
+	And Person 'Abraham' has app logon name 'abraham'
+
+	And I view people
+	And I searched for 'a'
+	And I have selected person 'Ashley'
+	And I have selected person 'I'
+	And I have selected person 'Abraham'
+
+@ignore
+Scenario: Change application logon for people
+	When I navigate to application logon page
+	Then The application logon page is shown
+	And I can see current application logons
+	| Name		| AppLogon			|
+	| Ashley	| 					|
+	| Anthony	| anthony@dish.com	|
+	| Abraham	| abraham			|
+	When I change Application logon for 'Ashley' into 'ashley'
+	And I press the save button
+	Then I should not the see the application logon page
+
+@ignore
+Scenario: Feedback from server when entering already taken logon
+	When I navigate to application logon page
+	Then The application logon page is shown
+	When I change Application logon for 'Ashley' into 'abraham'
+	Then I should see a validation error on 'Ashley'
+	And The save button should be disabled
+
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/People/ApplicationLogonStepDefinition.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Configurable;
+using Teleopti.Ccc.WebBehaviorTest.Core;
+using Teleopti.Ccc.WebBehaviorTest.Data;
+using Teleopti.Ccc.WebBehaviorTest.Data.Setups.Configurable;
+
+namespace Teleopti.Ccc.WebBehaviorTest.Wfm.People
+{
+	[Binding]
+	public sealed class ApplicationLogonStepDefinition
+	{
+		[Given("Person '(.*)' has app logon name '(.*)'")]
+		public void PersonXHasAppLogonNameY(string name, string logonName)
+		{
+			DataMaker.Person(name).Set(logonName);
+		}
+
+
+		[When("I navigate to application logon page")]
+		public void INavigateToApplicationLogon()
+		{
+			Browser.Interactions.AssertExists("[data-test-applicationlogon-button]");
+			Browser.Interactions.Javascript("document.querySelector('[data-test-applicationlogon-button]').click()");
+		}
+
+
+		[When("I change Application logon for '(.*)' into '(.*)'")]
+		public void IChangeApplicationLogonFor(string name, string appLogon)
+		{
+			Browser.Interactions.Javascript($@"
+				Array.from(document.querySelectorAll('[data-test-person]'))
+				.find(r => r.textContent.includes('{name}'))
+				.value = '{appLogon}'	
+			");
+		}
+
+
+		[Then("The application logon page is shown")]
+		public void ApplicationLogonPageIsShown()
+		{
+			Browser.Interactions.AssertExists("[data-test-application-logon]");
+		}
+
+
+		[Then("I can see current application logons")]
+		public void ICanSeeCurrentApplicationLogons(Table table)
+		{
+			foreach (var row in table.Rows)
+			{
+				row.TryGetValue("Name", out string name);
+				row.TryGetValue("AppLogon", out string logonName);
+
+				Browser.Interactions.AssertJavascriptResultContains($@"
+					return Array.from(document.querySelectorAll('[data-test-person]'))
+					.find(r => r.textContent.includes('{name}'))
+					.querySelector('[data-test-person-logon] input').value.includes('{logonName}')
+				", "True");
+			}
+		}
+
+
+		[Then("I should not the see the application logon page")]
+		public void ApplicationLogonPageIsNotShown()
+		{
+			Browser.Interactions.AssertNotExists("", "[data-test-application-logon]");
+		}
+
+	}
+}
