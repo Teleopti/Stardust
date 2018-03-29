4174b8085e18fb123656a78a100757a5044d83f1
MingdiWu <mingdi.wu@teleopti.com>
2018-03-29 15:03:55 +0800

    PBI #74866 add toggle

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -176,6 +176,7 @@
 		WFM_Gamification_Setting_With_External_Quality_Values_45003,
 		WFM_Gamification_Calculate_Badges_47250,
 		WFM_Gamification_Recalculate_Badges_Within_Period_48403,
+		WFM_Gamification_Create_Rolling_Periods_74866,
 		Report_Remove_Realtime_AuditTrail_44006,
 		ETL_Show_Web_Based_ETL_Tool_46880,
 		WFM_Remember_My_Selection_In_Intraday_47254,
