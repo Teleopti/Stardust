540f27853a42b5dfff5261eb82b15acfa8ded862
Squid Squad <jonas.nordh@teleopti.com>
2018-04-06 09:55:26 +0200

    Fix failing infra tests

--- Database/TeleoptiAnalytics/Releases/00000614.sql
+++ Database/TeleoptiAnalytics/Releases/00000614.sql
@@ -1,1 +1,3 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Mart].[etl_job_disable_schedule]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [Mart].[etl_job_disable_schedule]
+GO
