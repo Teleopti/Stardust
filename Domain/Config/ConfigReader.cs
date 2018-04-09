60060710a803aa0098132352d0b8911f43efbc60
MagnusW <none@none>
2018-04-09 09:58:10 +0200

    Story #75149: Added a test framework for integration test for stardust

--- Domain/Config/ConfigReader.cs
+++ Domain/Config/ConfigReader.cs
@@ -4,12 +4,12 @@ namespace Teleopti.Ccc.Domain.Config
 {
 	public class ConfigReader : IConfigReader
 	{
-		public string AppConfig(string name)
+		public virtual string AppConfig(string name)
 		{
 			return ConfigurationManager.AppSettings[name];
 		}
 
-		public string ConnectionString(string name)
+		public virtual string ConnectionString(string name)
 		{
 			var connectionStringSetting = ConfigurationManager.ConnectionStrings[name];
 			return connectionStringSetting?.ConnectionString;
