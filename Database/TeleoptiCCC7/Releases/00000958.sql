b1b2a7583edca3e0fcbc7ad625687489ba4e7e5d
henry.greijer@teleopti.com <henry.greijer@teleopti.com>
2018-04-10 06:56:08 +0200

    #75391 Starting with dbo.ExternalLogOnCollection

--- Database/TeleoptiCCC7/Releases/00000958.sql
+++ Database/TeleoptiCCC7/Releases/00000958.sql
@@ -21,3 +21,46 @@ GO
 ALTER TABLE  [dbo].[OvertimeRequestOpenPeriodSkillType]  WITH CHECK ADD  CONSTRAINT [FK_OvertimeRequestOpenPeriodSkillType_SkillType] FOREIGN KEY([SkillType])
 REFERENCES [dbo].[SkillType] ([Id])
 GO 
+
+
+-----------------------------------------------------------  
+-- Name: DevOops
+-- Date: 2018-04-09
+-- Desc: Fixing PK's with auto generated names. VSTS #75391 
+-----------------------------------------------------------
+
+ALTER TABLE  [dbo].[OvertimeRequestOpenPeriodSkillType]  WITH CHECK ADD  CONSTRAINT [FK_OvertimeRequestOpenPeriodSkillType_OvertimeRequestOpenPeriod] FOREIGN KEY([Parent])
+REFERENCES [dbo].[OvertimeRequestOpenPeriod] ([Id])
+GO
+
+ALTER TABLE  [dbo].[OvertimeRequestOpenPeriodSkillType]  WITH CHECK ADD  CONSTRAINT [FK_OvertimeRequestOpenPeriodSkillType_SkillType] FOREIGN KEY([SkillType])
+REFERENCES [dbo].[SkillType] ([Id])
+GO
+
+DECLARE @DROPINDEXQUERY nvarchar(MAX)
+DECLARE @INDEXNAME nvarchar(MAX)
+
+set @INDEXNAME = (
+	SELECT name FROM sys.objects 
+	WHERE parent_object_id = OBJECT_ID(N'[ExternalLogOnCollection]') 
+	AND type in (N'UQ')
+	and name like 'UQ%[_][_]%%' -- Look for UQ__ExternalLogOnCol__XXXXXXX
+	)
+
+select @INDEXNAME
+
+if (@INDEXNAME <> '')
+begin
+	SET @DROPINDEXQUERY = N'alter table dbo.ExternalLogOnCollection drop constraint [' + @INDEXNAME + ']'
+	print 'Dropping index with auto-generated name: ' + @INDEXNAME
+	EXEC sp_executeSQL @DROPINDEXQUERY;
+end
+
+ALTER TABLE dbo.ExternalLogOnCollection ADD CONSTRAINT
+	PK_ExternalLogOnCollection PRIMARY KEY CLUSTERED 
+	(
+		PersonPeriod,
+		ExternalLogOn
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
