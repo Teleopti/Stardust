e9d8cbe131f878338ea2ad3beb166b7b3bc3a8c7
Asad Mirza <asad.mirza@teleopti.com>
2018-04-10 09:07:36 +0200

    Story #75149 Added integration test framework for end to end

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web.IntegrationTest/Areas/Stardust/StardustTestAttribute.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IO;
+using System.Linq;
+using Autofac;
+using NUnit.Framework.Interfaces;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Logon;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.UnitOfWork;
+using Teleopti.Ccc.Infrastructure.Hangfire;
+using Teleopti.Ccc.Infrastructure.RealTimeAdherence.Domain.Service;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Default;
+using Teleopti.Ccc.TestCommon.Web.WebInteractions;
+
+namespace Teleopti.Ccc.Web.IntegrationTest.Areas.Stardust
+{
+	public class StardustTestAttribute : IntegrationIoCTestAttribute
+	{
+		public IComponentContext Container;
+		public DefaultDataCreator DefaultDataCreator;
+		public DataCreator DataCreator;
+		public DefaultAnalyticsDataCreator DefaultAnalyticsDataCreator;
+		public ImpersonateSystem Impersonate;
+		public IDataSourceScope DataSource;
+		public WithUnitOfWork WithUnitOfWork;
+		public IBusinessUnitRepository BusinessUnits;
+		public HangfireClientStarter HangfireClientStarter;
+		public HangfireUtilities Hangfire;
+		public StateQueueUtilities StateQueue;
+
+		public override void BeforeTest(ITest testDetails)
+		{
+			base.BeforeTest(testDetails);
+
+			var dataHash = DefaultDataCreator.HashValue;// ^ TestConfiguration.HashValue;
+			var path = Path.Combine(InfraTestConfigReader.DatabaseBackupLocation, "Stardust");
+
+			var haveDatabases =
+				DataSourceHelper.TryRestoreApplicationDatabaseBySql(path, dataHash) &&
+				DataSourceHelper.TryRestoreAnalyticsDatabaseBySql(path, dataHash);
+			if (!haveDatabases)
+				DataSourceHelper.CreateDatabases();
+
+			if (!haveDatabases)
+			{
+				StateHolderProxyHelper.SetupFakeState(
+					DataSourceHelper.CreateDataSource(Container),
+					DefaultPersonThatCreatesData.PersonThatCreatesDbData,
+					DefaultBusinessUnit.BusinessUnit
+				);
+
+				DefaultDataCreator.Create();
+				//DefaultAnalyticsDataCreator.OneTimeSetup();
+				//DataSourceHelper.ClearAnalyticsData();
+				//DefaultAnalyticsDataCreator.Create();
+				DataCreator.Create();
+
+				//DataSourceHelper.BackupApplicationDatabaseBySql(path, dataHash);
+				//DataSourceHelper.BackupAnalyticsDatabaseBySql(path, dataHash);
+
+				StateHolderProxyHelper.Logout();
+			}
+
+			HangfireClientStarter.Start();
+
+			Guid businessUnitId;
+			using (DataSource.OnThisThreadUse(DataSourceHelper.TestTenantName))
+				businessUnitId = WithUnitOfWork.Get(() => BusinessUnits.LoadAll().First()).Id.Value;
+			Impersonate.Impersonate(DataSourceHelper.TestTenantName, businessUnitId);
+
+			TestSiteConfigurationSetup.Setup();
+		}
+
+		public override void AfterTest(ITest testDetails)
+		{
+			base.AfterTest(testDetails);
+
+			Hangfire.WaitForQueue();
+			//StateQueue.WaitForQueue();
+
+			TestSiteConfigurationSetup.TearDown();
+
+			Impersonate?.EndImpersonation();
+
+		}
+	}
+}
