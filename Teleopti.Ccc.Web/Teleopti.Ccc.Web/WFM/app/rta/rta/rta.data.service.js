f2f2c0ae3b2c5f9b3d15051bc1ecaa4bfad50045
LevelUp! <levelup@teleopti.com>
2018-04-10 15:45:13 +0200

    #48724 Data service loads on first demand

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/rta.data.service.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/rta.data.service.js
@@ -12,38 +12,44 @@
 		var skillAreas = [];
 		var states = [];
 
-		var loaded = $q.all([
-			rtaService.getOrganization()
-				.then(function (data) {
-					organization = data;
-					organization.forEach(function (site) {
-						site.Teams = site.Teams || [];
-					});
-				}),
-			rtaService.getSkills()
-				.then(function (data) {
-					skills = data;
-				}),
-			rtaService.getSkillAreas()
-				.then(function (data) {
-					skillAreas = data;
-				}),
-			$http.get('../api/PhoneStates')
-				.then(function (response) {
-					states = response.data;
-					states.push({Id: null, Name: $translate.instant('NoState')});
-				})
-		]).then(function () {
-			return {
-				organization: organization,
-				skills: skills,
-				skillAreas: skillAreas,
-				states: states
-			}
-		});
+		var loaded = null;
+
+		function load() {
+			return $q.all([
+				rtaService.getOrganization()
+					.then(function (data) {
+						organization = data;
+						organization.forEach(function (site) {
+							site.Teams = site.Teams || [];
+						});
+					}),
+				rtaService.getSkills()
+					.then(function (data) {
+						skills = data;
+					}),
+				rtaService.getSkillAreas()
+					.then(function (data) {
+						skillAreas = data;
+					}),
+				$http.get('../api/PhoneStates')
+					.then(function (response) {
+						states = response.data;
+						states.push({Id: null, Name: $translate.instant('NoState')});
+					})
+			]).then(function () {
+				return {
+					organization: organization,
+					skills: skills,
+					skillAreas: skillAreas,
+					states: states
+				}
+			})
+		}
 
 		return {
 			load: function () {
+				if (!loaded)
+					loaded = load();
 				return loaded;
 			}
 		};
