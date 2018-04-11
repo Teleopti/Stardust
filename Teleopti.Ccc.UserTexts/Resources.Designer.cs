0d3fe949b3a28882b6e613549cd398658b05f0df
Qing Luo <none@none>
2018-04-11 14:05:42 +0800

    #74866 Add Text for Gaming period

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -13571,6 +13571,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string GamificationViewImportFileFormat {
             get {
                 return ResourceManager.GetString("GamificationViewImportFileFormat", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gaming Period.
+        /// </summary>
+        public static string GamingPeriod {
+            get {
+                return ResourceManager.GetString("GamingPeriod", resourceCulture);
             }
         }
         
