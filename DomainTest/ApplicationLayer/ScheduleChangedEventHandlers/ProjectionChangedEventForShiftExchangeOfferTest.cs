0cb2df00ac1b4535b6a67576ae001997da1d7744
Roger Kratz <roger.kratz@teleopti.com>
2018-04-13 10:19:17 +0200

    #75415 Fixing test now when event has logon info

--- DomainTest/ApplicationLayer/ScheduleChangedEventHandlers/ProjectionChangedEventForShiftExchangeOfferTest.cs
+++ DomainTest/ApplicationLayer/ScheduleChangedEventHandlers/ProjectionChangedEventForShiftExchangeOfferTest.cs
@@ -6,10 +6,11 @@ using Teleopti.Ccc.Domain.ApplicationLayer.Events;
 using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.Helper;
-using Teleopti.Ccc.IocCommon.Toggle;
+using Teleopti.Ccc.Domain.Infrastructure;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
 using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.TestCommon.TestData;
 
 namespace Teleopti.Ccc.DomainTest.ApplicationLayer.ScheduleChangedEventHandlers
 {
@@ -56,7 +57,9 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.ScheduleChangedEventHandlers
 				StartDateTime = "2016-06-30 08:00".Utc(),
 				EndDateTime = "2016-06-30 17:00".Utc(),
 				PersonId = person,
-				ScenarioId = scenario
+				ScenarioId = scenario,
+				LogOnBusinessUnitId = Database.CurrentBusinessUnitId(),
+				LogOnDatasource = Database.TenantName()
 			};
 			
 			Target.Handle(@event);
@@ -66,7 +69,8 @@ namespace Teleopti.Ccc.DomainTest.ApplicationLayer.ScheduleChangedEventHandlers
 			result.PersonId.Should().Be.EqualTo(person);
 			result.Days.Last().Checksum.Should().Not.Be.EqualTo(0);
 			result.Days.Last().Date.Should().Be.EqualTo("2016-06-30".Utc());
-			
+			result.LogOnBusinessUnitId.Should().Be.EqualTo(@event.LogOnBusinessUnitId);
+			result.LogOnDatasource.Should().Be.EqualTo(@event.LogOnDatasource);
 		}
 	}
 }
