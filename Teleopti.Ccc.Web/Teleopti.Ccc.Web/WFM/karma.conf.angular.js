f3889727ec4c8d0cdba5d0181f3d83dd9cbcb739
emils <none@none>
2018-04-14 02:43:34 +0200

    Provide a config for running only Angular tests

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/karma.conf.angular.js
@@ -0,0 +1,46 @@
+module.exports = function(config) {
+	config.set({
+		basePath: '',
+		files: [],
+		exclude: ['app/**/*'],
+		frameworks: ['jasmine', '@angular/cli'],
+		plugins: [
+			require('@angular/cli/plugins/karma'),
+			require('karma-jasmine'),
+			require('karma-mocha-reporter'),
+			require('karma-chrome-launcher')
+		],
+
+		preprocessors: {
+			'./src/test.ts': ['@angular/cli']
+		},
+		captureTimeout: 210000,
+		browserDisconnectTolerance: 3,
+		browserDisconnectTimeout: 210000,
+		browserNoActivityTimeout: 210000,
+		angularCli: {
+			environment: 'dev'
+		},
+		mochaReporter: {
+			symbols: {
+				success: '✔'
+			}
+		},
+		reporters: ['mocha'],
+		port: 9876,
+		colors: true,
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['Chrome'],
+		customLaunchers: {
+			Chrome_small: {
+				base: 'Chrome',
+				flags: ['--window-size=400,400', '--window-position=-400,0']
+			}
+		}
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		// singleRun: true
+	});
+};
