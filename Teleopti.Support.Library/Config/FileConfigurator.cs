9ca5c3cb19f12d4824350381a85291e859fd197d
LevelUp! <levelup@teleopti.com>
2018-04-17 12:00:01 +0200

    If configuration doesnt change, keep existing file. This may prevent some Resharper/Rider reindexing.

--- Teleopti.Support.Library/Config/FileConfigurator.cs
+++ Teleopti.Support.Library/Config/FileConfigurator.cs
@@ -8,11 +8,17 @@ namespace Teleopti.Support.Library.Config
 		public void Configure(string sourceFile, string targetFile, SearchReplaceCollection searchReplaceCollection)
 		{
 			var contents = File.ReadAllText(sourceFile);
+
 			searchReplaceCollection.ForReplacing()
-				.ForEach(sr =>
-				{
-					contents = contents.Replace(sr.SearchFor, sr.ReplaceWith);
-				});
+				.ForEach(sr => { contents = contents.Replace(sr.SearchFor, sr.ReplaceWith); });
+
+			if (File.Exists(targetFile))
+			{
+				var existingContents = File.ReadAllText(targetFile);
+				if (contents == existingContents)
+					return;
+			}
+
 			File.WriteAllText(targetFile, contents);
 		}
 	}
