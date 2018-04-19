4becd3b88d66f697cce5d74774f7b14f5fd3ca81
henry.greijer@teleopti.com <henry.greijer@teleopti.com>
2018-04-19 10:17:06 +0200

    #75391 PK-mania

---
+++ Database/TeleoptiCCC7/Releases/00000969.sql
@@ -0,0 +1,81 @@
+
+-----------------------------------------------------------  
+-- Name: DevOops
+-- Date: 2018-04-19
+-- Desc: Fixing PK's. VSTS #75391 
+-----------------------------------------------------------
+
+ALTER TABLE dbo.PlanningPeriodJobResult
+	DROP CONSTRAINT UQ_PlanningPeriodJobResult
+
+ALTER TABLE dbo.PlanningPeriodJobResult ADD CONSTRAINT
+	PK_PlanningPeriodJobResult PRIMARY KEY CLUSTERED 
+	(
+		PlanningPeriod,
+		JobResult
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.PlanningPeriodJobResult SET (LOCK_ESCALATION = TABLE)
+GO
+-----------------------------------------------------------
+
+ALTER TABLE dbo.QueueSourceCollection
+	DROP CONSTRAINT UQ_QueueSourceCollection
+GO
+ALTER TABLE dbo.QueueSourceCollection ADD CONSTRAINT
+	PK_QueueSourceCollection PRIMARY KEY CLUSTERED 
+	(
+		Workload,
+		QueueSource
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.QueueSourceCollection SET (LOCK_ESCALATION = TABLE)
+GO
+-----------------------------------------------------------
+
+ALTER TABLE dbo.RecurrentWeeklyMeetingWeekDays
+	DROP CONSTRAINT FK_RecurrentWeeklyMeetingWeekDays_RecurrentWeeklyMeeting
+GO
+ALTER TABLE dbo.RecurrentWeeklyMeeting SET (LOCK_ESCALATION = TABLE)
+GO
+
+CREATE TABLE dbo.Tmp_RecurrentWeeklyMeetingWeekDays
+	(
+		RecurrentWeeklyMeeting uniqueidentifier NOT NULL,
+		DayOfWeek int NOT NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.Tmp_RecurrentWeeklyMeetingWeekDays SET (LOCK_ESCALATION = TABLE)
+GO
+
+IF EXISTS(SELECT * FROM dbo.RecurrentWeeklyMeetingWeekDays)
+	 EXEC('INSERT INTO dbo.Tmp_RecurrentWeeklyMeetingWeekDays (RecurrentWeeklyMeeting, DayOfWeek)
+		SELECT RecurrentWeeklyMeeting, DayOfWeek FROM dbo.RecurrentWeeklyMeetingWeekDays WITH (HOLDLOCK TABLOCKX)')
+GO
+DROP TABLE dbo.RecurrentWeeklyMeetingWeekDays
+GO
+
+EXECUTE sp_rename N'dbo.Tmp_RecurrentWeeklyMeetingWeekDays', N'RecurrentWeeklyMeetingWeekDays', 'OBJECT' 
+GO
+
+ALTER TABLE dbo.RecurrentWeeklyMeetingWeekDays ADD CONSTRAINT
+	PK_RecurrentWeeklyMeetingWeekDays PRIMARY KEY CLUSTERED 
+	(
+		RecurrentWeeklyMeeting,
+		DayOfWeek
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.RecurrentWeeklyMeetingWeekDays ADD CONSTRAINT
+	FK_RecurrentWeeklyMeetingWeekDays_RecurrentWeeklyMeeting FOREIGN KEY
+	(
+		RecurrentWeeklyMeeting
+	) REFERENCES dbo.RecurrentWeeklyMeeting
+	(
+		RecurrentMeetingOption
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
