540add7131dd4be20149fbdd4551e2fd80f8da22
magnuse <none@none>
2018-04-19 08:59:51 +0200

    Integration testfix

--- Teleopti.Ccc.TestCommon/IoC/IntegrationIoCTest.cs
+++ Teleopti.Ccc.TestCommon/IoC/IntegrationIoCTest.cs
@@ -19,6 +19,8 @@ namespace Teleopti.Ccc.TestCommon.IoC
 
 			builder.RegisterModule(new CommonModule(configuration));
 			builder.RegisterType<TenantAuthenticationAlwaysAuthenticated>().As<ITenantAuthentication>().SingleInstance();
+			builder.RegisterType<CurrentTenantUserFake>().As<ICurrentTenantUser>().SingleInstance();
+
 			builder.RegisterModule(new TestModule());
 
 			registrations?.Invoke(builder);
