afe717edac80e0ef80575c93d245a7bfb7cceb7d
codemonkeys <junwen.luo@teleopti.com>
2018-04-19 10:55:45 +0800

    remove mock tests

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/Preference/Mapping/PreferenceDayFeedbackViewModelMapperTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Core/Preference/Mapping/PreferenceDayFeedbackViewModelMapperTest.cs
@@ -1,174 +1,144 @@
 using System;
-using System.Collections.Generic;
+using System.Globalization;
 using NUnit.Framework;
-using Rhino.Mocks;
 using SharpTestsEx;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
 using Teleopti.Ccc.UserTexts;
 using Teleopti.Ccc.Web.Areas.MyTime.Core;
-using Teleopti.Ccc.Web.Areas.MyTime.Core.Preference.DataProvider;
 using Teleopti.Ccc.Web.Areas.MyTime.Core.Preference.Mapping;
+using Teleopti.Ccc.WebTest.Core.IoC;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.WebTest.Areas.MyTime.Core.Preference.Mapping
 {
 	[TestFixture]
+	[MyTimeWebTest]
 	[SetCulture("sv-SE")]
 	public class PreferenceDayFeedbackViewModelMapperTest
 	{
-		[SetUp]
-		public void Setup()
-		{
-			var today = DateOnly.Today;
-			preferenceFeedbackProvider = MockRepository.GenerateMock<IPreferenceFeedbackProvider>();
-			preferenceFeedbackProvider.Stub(x => x.CheckNightRestViolation(today))
-				.Return(new PreferenceNightRestCheckResult());
-
-			var perriodNightRestCheckResult =
-				new Dictionary<DateOnly, PreferenceNightRestCheckResult>
-				{
-					{today, new PreferenceNightRestCheckResult()}
-				};
-			preferenceFeedbackProvider.Stub(x => x.CheckNightRestViolation(new DateOnlyPeriod(today, today)))
-				.Return(perriodNightRestCheckResult);
-
-			target = new PreferenceDayFeedbackViewModelMapper(preferenceFeedbackProvider);
-		}
-
-		private IPreferenceFeedbackProvider preferenceFeedbackProvider;
-		private PreferenceDayFeedbackViewModelMapper target;
+		public PreferenceDayFeedbackViewModelMapper Target;
+		public FakeRuleSetBagRepository RuleSetBagRepository;
+		public FakeLoggedOnUser LoggedOnUser;
 
 		[Test]
 		public void ShouldMapDate()
 		{
-			var today = DateOnly.Today;
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForPeriod(new DateOnlyPeriod(today, today)))
-				.Return(new Dictionary<DateOnly, WorkTimeMinMaxCalculationResult>
-				{
-					{today, new WorkTimeMinMaxCalculationResult()}
-				});
-
-			var result = target.Map(DateOnly.Today);
+			var result = Target.Map(DateOnly.Today);
 			result.Date.Should().Be.EqualTo(DateOnly.Today.ToFixedClientDateOnlyFormat());
 		}
 
 		[Test]
 		public void ShouldMapPossibleContractTimeMinutesLower()
 		{
-			var today = DateOnly.Today;
-			var workTimeMinMax = new WorkTimeMinMax
-			{
-				WorkTimeLimitation = new WorkTimeLimitation(TimeSpan.FromHours(6), TimeSpan.FromHours(10))
-			};
-
-			var workTimeMinMaxCalculationResult = new WorkTimeMinMaxCalculationResult {WorkTimeMinMax = workTimeMinMax};
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForDate(today))
-				.Return(workTimeMinMaxCalculationResult);
-
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForPeriod(new DateOnlyPeriod(today, today)))
-				.Return(new Dictionary<DateOnly, WorkTimeMinMaxCalculationResult>
+			var shiftCategory = new ShiftCategory("test").WithId();
+			var activity = new Activity("test");
+			var ruleSetBag = RuleSetBagRepository.Has(
+				new RuleSetBag(new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity,
+					new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 16, 0, 15), shiftCategory)))
 				{
-					{today, workTimeMinMaxCalculationResult}
-				});
+					Description = new Description("_")
+				}.WithId());
 
-			var result = target.Map(DateOnly.Today);
+			var team = TeamFactory.CreateTeamWithId("test");
+			var personPeriod = new PersonPeriod(DateOnly.Today.AddDays(-1)
+				, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), team)
+			{
+				RuleSetBag = ruleSetBag
+			};
+			LoggedOnUser.CurrentUser().AddPersonPeriod(personPeriod);
+
+			var result = Target.Map(DateOnly.Today);
 
 			result.PossibleContractTimeMinutesLower.Should()
-				.Be(workTimeMinMax.WorkTimeLimitation.StartTime.Value.TotalMinutes.ToString());
+				.Be(TimeSpan.FromHours(8).TotalMinutes.ToString(CultureInfo.InvariantCulture));
 		}
 
 		[Test]
 		public void ShouldMapPossibleContractTimeMinutesUpper()
 		{
-			var today = DateOnly.Today;
-			var workTimeMinMax = new WorkTimeMinMax
-			{
-				WorkTimeLimitation = new WorkTimeLimitation(TimeSpan.FromHours(6), TimeSpan.FromHours(10))
-			};
-
-			var workTimeMinMaxCalculationResult = new WorkTimeMinMaxCalculationResult
-			{
-				WorkTimeMinMax = workTimeMinMax
-			};
-
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForDate(DateOnly.Today))
-				.Return(workTimeMinMaxCalculationResult);
-
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForPeriod(new DateOnlyPeriod(today, today)))
-				.Return(new Dictionary<DateOnly, WorkTimeMinMaxCalculationResult>
+			var shiftCategory = new ShiftCategory("test").WithId();
+			var activity = new Activity("test");
+			var ruleSetBag = RuleSetBagRepository.Has(
+				new RuleSetBag(new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity,
+					new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 20, 0, 15), shiftCategory)))
 				{
-					{today, workTimeMinMaxCalculationResult}
-				});
+					Description = new Description("_")
+				}.WithId());
 
-			var result = target.Map(DateOnly.Today);
+			var team = TeamFactory.CreateTeamWithId("test");
+			var personPeriod = new PersonPeriod(DateOnly.Today.AddDays(-1)
+				, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), team)
+			{
+				RuleSetBag = ruleSetBag
+			};
+			LoggedOnUser.CurrentUser().AddPersonPeriod(personPeriod);
+
+			var result = Target.Map(DateOnly.Today);
 
 			result.PossibleContractTimeMinutesUpper.Should()
-				.Be(workTimeMinMax.WorkTimeLimitation.EndTime.Value.TotalMinutes.ToString());
+				.Be(TimeSpan.FromHours(12).TotalMinutes.ToString(CultureInfo.InvariantCulture));
 		}
 
 		[Test]
 		public void ShouldMapPossibleEndTimes()
 		{
-			var today = DateOnly.Today;
-			var workTimeMinMax = new WorkTimeMinMax
-			{
-				EndTimeLimitation = new EndTimeLimitation(TimeSpan.FromHours(15), TimeSpan.FromHours(19))
-			};
-
-			var workTimeMinMaxCalculationResult = new WorkTimeMinMaxCalculationResult
-			{
-				WorkTimeMinMax = workTimeMinMax
-			};
-
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForDate(DateOnly.Today))
-				.Return(workTimeMinMaxCalculationResult);
-
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForPeriod(new DateOnlyPeriod(today, today)))
-				.Return(new Dictionary<DateOnly, WorkTimeMinMaxCalculationResult>
+			var shiftCategory = new ShiftCategory("test").WithId();
+			var activity = new Activity("test");
+			var ruleSetBag = RuleSetBagRepository.Has(
+				new RuleSetBag(new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity,
+					new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 20, 0, 15), shiftCategory)))
 				{
-					{today, workTimeMinMaxCalculationResult}
-				});
+					Description = new Description("_")
+				}.WithId());
 
-			var result = target.Map(DateOnly.Today);
+			var team = TeamFactory.CreateTeamWithId("test");
+			var personPeriod = new PersonPeriod(DateOnly.Today.AddDays(-1)
+				, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), team)
+			{
+				RuleSetBag = ruleSetBag
+			};
+			LoggedOnUser.CurrentUser().AddPersonPeriod(personPeriod);
+
+			var result = Target.Map(DateOnly.Today);
 			result.PossibleEndTimes.Should()
-				.Be(workTimeMinMax.EndTimeLimitation.StartTimeString + "-" + workTimeMinMax.EndTimeLimitation.EndTimeString);
+				.Be("16:00-20:00");
 		}
 
 		[Test]
 		public void ShouldMapPossibleStartTimes()
 		{
-			var today = DateOnly.Today;
-			var workTimeMinMax = new WorkTimeMinMax
-			{
-				StartTimeLimitation = new StartTimeLimitation(TimeSpan.FromHours(6), TimeSpan.FromHours(10))
-			};
-
-			var workTimeMinMaxCalculationResult  = new WorkTimeMinMaxCalculationResult
-			{
-				WorkTimeMinMax = workTimeMinMax
-			};
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForDate(DateOnly.Today))
-				.Return(workTimeMinMaxCalculationResult);
-
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForPeriod(new DateOnlyPeriod(today, today)))
-				.Return(new Dictionary<DateOnly, WorkTimeMinMaxCalculationResult>
+			var shiftCategory = new ShiftCategory("test").WithId();
+			var activity = new Activity("test");
+			var ruleSetBag = RuleSetBagRepository.Has(
+				new RuleSetBag(new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity,
+					new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 20, 0, 15), shiftCategory)))
 				{
-					{today, workTimeMinMaxCalculationResult }
-				});
+					Description = new Description("_")
+				}.WithId());
 
-			var result = target.Map(DateOnly.Today);
-			result.PossibleStartTimes.Should().Be(workTimeMinMax.StartTimeLimitation.ToString());
+			var team = TeamFactory.CreateTeamWithId("test");
+			var personPeriod = new PersonPeriod(DateOnly.Today.AddDays(-1)
+				, new PersonContract(new Contract("_"), new PartTimePercentage("_"), new ContractSchedule("_")), team)
+			{
+				RuleSetBag = ruleSetBag
+			};
+			LoggedOnUser.CurrentUser().AddPersonPeriod(personPeriod);
+
+			var result = Target.Map(DateOnly.Today);
+			result.PossibleStartTimes.Should().Be("08:00-08:00");
 		}
 
 		[Test]
 		public void ShouldMapValidationErrors()
 		{
-			var today = DateOnly.Today;
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForDate(today)).Return(null);
-			preferenceFeedbackProvider.Stub(x => x.WorkTimeMinMaxForPeriod(new DateOnlyPeriod(today,today))).Return(null);
-
-			var result = target.Map(DateOnly.Today);
+			var result = Target.Map(DateOnly.Today);
 			result.FeedbackError.Should().Be(Resources.NoAvailableShifts);
 		}
 	}
