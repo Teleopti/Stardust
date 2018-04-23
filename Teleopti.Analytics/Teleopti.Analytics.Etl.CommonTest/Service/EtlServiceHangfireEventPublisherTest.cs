00773de1e60a584df0b193a90f3dd21da68cda6f
LevelUp! <levelup@teleopti.com>
2018-04-23 14:06:32 +0200

    Remove inheritance of DomainTestAttribute: EtlTestAttribute

--- Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/Service/EtlServiceHangfireEventPublisherTest.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/Service/EtlServiceHangfireEventPublisherTest.cs
@@ -1,6 +1,8 @@
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
+using Teleopti.Analytics.Etl.Common;
+using Teleopti.Analytics.Etl.Common.Infrastructure;
 using Teleopti.Analytics.Etl.Common.Service;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
@@ -8,15 +10,17 @@ using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.Helper;
 using Teleopti.Ccc.Domain.Infrastructure.Events;
+using Teleopti.Ccc.Domain.MultiTenancy;
 using Teleopti.Ccc.Infrastructure.MultiTenancy.Server;
+using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeRepositories.Tenant;
 using Teleopti.Ccc.TestCommon.IoC;
 
 namespace Teleopti.Analytics.Etl.CommonTest.Service
 {
-	[EtlTest]
-	public class EtlServiceHangfireEventPublisherTest
+	[DomainTest]
+	public class EtlServiceHangfireEventPublisherTest : ISetup
 	{
 		public FakeRecurringEventPublisher Publisher;
 		public FakeAllTenantEtlSettings FakeAllTenantEtlSettings;
@@ -27,6 +31,18 @@ namespace Teleopti.Analytics.Etl.CommonTest.Service
 		public TenantTickEventPublisher TenantTickEventPublisher;
 		public IRecurringEventPublisher RecurringEventPublisher;
 
+
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.AddModule(new EtlModule(configuration));
+			system.UseTestDouble<FakeBaseConfigurationRepository>().For<IBaseConfigurationRepository>();
+			
+			// same as domain test attribute does, sweet!
+			var tenants = new FakeAllTenantEtlSettings();
+			tenants.Has(DomainTestAttribute.DefaultTenantName);
+			system.UseTestDouble(tenants).For<IAllTenantEtlSettings>();
+		}
+		
 		private void hasTenant(string tenantName)
 		{
 			FakeAllTenantEtlSettings.Has(new Tenant(tenantName));
--- Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/Service/ResolveTest.cs
+++ Teleopti.Analytics/Teleopti.Analytics.Etl.CommonTest/Service/ResolveTest.cs
@@ -1,12 +1,29 @@
 ﻿using NUnit.Framework;
 using SharpTestsEx;
+using Teleopti.Analytics.Etl.Common;
+using Teleopti.Analytics.Etl.Common.Infrastructure;
 using Teleopti.Analytics.Etl.Common.Service;
+using Teleopti.Ccc.Domain.MultiTenancy;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon.FakeRepositories.Tenant;
+using Teleopti.Ccc.TestCommon.IoC;
 
 namespace Teleopti.Analytics.Etl.CommonTest.Service
 {
-	[EtlTest]
-	public class ResolveTest
+	[DomainTest]
+	public class ResolveTest : ISetup
 	{
+		public void Setup(ISystem system, IIocConfiguration configuration)
+		{
+			system.AddModule(new EtlModule(configuration));
+			system.UseTestDouble<FakeBaseConfigurationRepository>().For<IBaseConfigurationRepository>();
+			
+			// same as domain test attribute does, sweet!
+			var tenants = new FakeAllTenantEtlSettings();
+			tenants.Has(DomainTestAttribute.DefaultTenantName);
+			system.UseTestDouble(tenants).For<IAllTenantEtlSettings>();
+		}
+		
 		public EtlService Service;
 
 		[Test]
