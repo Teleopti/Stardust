7b9c1397faf05c9c3a6881bf12deb0e15f914f4a
Kunning Mao <kunning.mao@teleopti.com>
2018-04-25 11:41:32 +0200

    #75339 Red test ShouldConsiderMinimumStaffingWhenMovingDayOffFrom

--- DomainTest/SchedulingScenarios/DayOffOptimization/DayOffOptimizationMinimumStaffingTest.cs
+++ DomainTest/SchedulingScenarios/DayOffOptimization/DayOffOptimizationMinimumStaffingTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.AgentInfo;
@@ -32,7 +33,7 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.DayOffOptimization
 		public FakePlanningPeriodRepository PlanningPeriodRepository;
 
 		[Test]
-		public void ShouldConsiderMinimumStaffing()
+		public void ShouldConsiderMinimumStaffingWhenMovingDayOffTo()
 		{
 			var firstDay = new DateOnly(2015, 10, 12); //mon
 			var activity = ActivityRepository.Has("_");
@@ -63,6 +64,38 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.DayOffOptimization
 				.Should().Be.EqualTo(skillDays[1].CurrentDate); //only DO is on tuesday
 		}
 
+		[Test] 
+		[Ignore("#75339")]
+		public void ShouldConsiderMinimumStaffingWhenMovingDayOffFrom()
+		{
+			var firstDay = new DateOnly(2015, 10, 12); //mon
+			var activity = ActivityRepository.Has("_");
+			var skill = SkillRepository.Has("skill", activity);
+			var planningPeriod = PlanningPeriodRepository.Has(firstDay, 1);
+			var scenario = ScenarioRepository.Has("some name");
+			var schedulePeriod = new SchedulePeriod(firstDay, SchedulePeriodType.Week, 1);
+			schedulePeriod.SetDaysOff(1);
+			var shiftCategory = new ShiftCategory("_").WithId();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 16, 0, 15), shiftCategory));
+			var agent = PersonRepository.Has(new Team {Site = new Site("site")}, schedulePeriod, ruleSet, skill);
+			var skillDays = SkillDayRepository.Has(skill.CreateSkillDaysWithDemandOnConsecutiveDays(scenario, firstDay,
+				1,
+				1,
+				1,
+				1,
+				1,
+				1,
+				0.1) //DO from beginning, minimum staffing 1 so we should move DO from this spot
+			);
+			skillDays.Last().SetMinimumAgents(1);
+			PersonAssignmentRepository.Has(agent, scenario, activity, shiftCategory, new DateOnlyPeriod(firstDay, firstDay.AddDays(7)), new TimePeriod(8, 0, 16, 0));
+			PersonAssignmentRepository.GetSingle(skillDays[6].CurrentDate).SetDayOff(new DayOffTemplate());
+
+			Target.Execute(planningPeriod.Id.Value);
+			
+			PersonAssignmentRepository.LoadAll().Single(x => x.DayOff() != null).Date
+				.Should().Not.Be.EqualTo(skillDays[6].CurrentDate); //should have been moved
+		}
 		
 		public DayOffOptimizationMinimumStaffingTest(SeperateWebRequest seperateWebRequest, bool resourcePlannerDayOffOptimizationIslands47208, bool resourcePlannerDayOffUsePredictorEverywhere75667, bool resourcePlannerMinimumStaffing75339) : base(seperateWebRequest, resourcePlannerDayOffOptimizationIslands47208, resourcePlannerDayOffUsePredictorEverywhere75667, resourcePlannerMinimumStaffing75339)
 		{
