de98a8657fa01d8958703e5c09577599eb7b9385
Robin Karlsson <robin.karlsson@teleopti.com>
2018-04-27 08:39:50 +0200

    Using a wrapping Teleopti configuration repository for authbridge #223

---
+++ Teleopti.Ccc.Web.AuthenticationBridge/Configuration/TeleoptiConfigurationRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AuthBridge.Configuration;
+using AuthBridge.Model;
+
+namespace Teleopti.Ccc.Web.AuthenticationBridge.Configuration
+{
+	public class TeleoptiConfigurationRepository : IConfigurationRepository
+	{
+		private readonly IConfigurationRepository inner = new DefaultConfigurationRepository();
+
+		public ClaimProvider RetrieveIssuer(Uri host, Uri identifier)
+		{
+			return inner.RetrieveIssuer(host, identifier);
+		}
+
+		public ClaimProvider[] RetrieveIssuers(Uri host)
+		{
+			return inner.RetrieveIssuers(host);
+		}
+
+		public Scope RetrieveScope(Uri host, Uri identifier)
+		{
+			return inner.RetrieveScope(host, identifier);
+		}
+
+		public ScopeElement RetrieveDefaultScope(Uri host)
+		{
+			return inner.RetrieveDefaultScope(host);
+		}
+
+		public MultiProtocolIssuer MultiProtocolIssuer => inner.MultiProtocolIssuer;
+	}
+}
