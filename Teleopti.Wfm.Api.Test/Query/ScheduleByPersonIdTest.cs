aa9724d159ddcea770e30acbe78985bde82e4f75
Robin Karlsson <robin.karlsson@teleopti.com>
2018-05-03 20:57:45 +0200

    More on trying to get it to work #223

--- Teleopti.Wfm.Api.Test/Query/ScheduleByPersonIdTest.cs
+++ Teleopti.Wfm.Api.Test/Query/ScheduleByPersonIdTest.cs
@@ -1,16 +1,13 @@
-﻿using System.Net.Http;
-using Autofac;
+﻿using System;
+using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
-using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
-using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Wfm.Api.Test.Query
@@ -21,9 +18,10 @@ namespace Teleopti.Wfm.Api.Test.Query
 		public IApiHttpClient Client;
 		public FakePersonRepository PersonRepository;
 		public FakeScenarioRepository ScenarioRepository;
+		public FakeActivityRepository ActivityRepository;
 		public FakePersonAssignmentRepository PersonAssignmentRepository;
 
-		[Test]
+		[Test, Ignore("Work in progress")]
 		public void ShouldGetBasicScheduleInformation()
 		{
 			Client.Authorize();
@@ -31,15 +29,26 @@ namespace Teleopti.Wfm.Api.Test.Query
 			var person = PersonFactory.CreatePerson().WithId();
 			PersonRepository.Add(person);
 			var scenario = ScenarioRepository.Has("Default");
-			PersonAssignmentRepository.Add(new PersonAssignment(person,scenario,new DateOnly(2001,1,1)));
+			var activity = ActivityRepository.Has("Phone");
+			var personAssignment = new PersonAssignment(person,scenario,new DateOnly(2001,1,1)).WithId();
+			personAssignment.AddActivity(activity,new TimePeriod(8,17));
+			PersonAssignmentRepository.Add(personAssignment);
 
-			var result = Client.PostAsync("/query/User/UserByEmail", new StringContent(JsonConvert.SerializeObject(new { Email = "a@b.c" })));
+			var result = Client.PostAsync("/query/Schedule/ScheduleByPersonId",
+				new StringContent(JsonConvert.SerializeObject(new
+				{
+					PersonId = person.Id.GetValueOrDefault(),
+					StartDate = new DateTime(2001, 1, 1),
+					EndDate = new DateTime(2001, 1, 1)
+				})));
 			var obj = JObject.Parse(result.Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result)["Result"][0];
 
-			obj["Id"].Value<string>().Should().Be.EqualTo(person.Id.Value.ToString());
-			obj["FirstName"].Value<string>().Should().Be.EqualTo(person.Name.FirstName);
-			obj["LastName"].Value<string>().Should().Be.EqualTo(person.Name.LastName);
-			obj["Email"].Value<string>().Should().Be.EqualTo(person.Email);
+			obj["PersonId"].Value<string>().Should().Be.EqualTo(person.Id.Value.ToString());
+			obj["Date"].Value<DateTime>().Should().Be.EqualTo(new DateTime(2001,1,1));
+			obj["Shift"][0]["PayloadId"].Value<string>().Should().Be.EqualTo(activity.Id.ToString());
+			obj["Shift"][0]["Name"].Value<string>().Should().Be.EqualTo("Phone");
+			obj["Shift"][0]["StartTime"].Value<DateTime>().Should().Be.EqualTo(new DateTime(2001,1,1,8,0,0));
+			obj["Shift"][0]["EndTime"].Value<DateTime>().Should().Be.EqualTo(new DateTime(2001,1,1,17,0,0));
 		}
 	}
 }
