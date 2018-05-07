33b8c956501a48caeccae9d7f764b51c46f23796
Amanda <amanda.jonasson@teleopti.com>
2018-05-07 16:44:42 -0600

    #75728 adding some skeleton, including Azure AD logon

--- StaffHubPoC/StaffHubPoC/Startup.cs
+++ StaffHubPoC/StaffHubPoC/Startup.cs
@@ -1,34 +1,85 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Notifications;
+using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+[assembly: OwinStartup(typeof(StaffHubPoC.Startup))]
 
 namespace StaffHubPoC
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		// The Client ID (a.k.a. Application ID) is used by the application to uniquely identify itself to Azure AD
+		string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
 
-        public IConfiguration Configuration { get; }
+		// RedirectUri is the URL where the user will be redirected to after they sign in
+		string redirectUrl = System.Configuration.ConfigurationManager.AppSettings["redirectUrl"];
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// Tenant is the tenant ID (e.g. contoso.onmicrosoft.com, or 'common' for multi-tenant)
+		static string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenant"];
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// Authority is the URL for authority, composed by Azure Active Directory endpoint and the tenant name (e.g. https://login.microsoftonline.com/contoso.onmicrosoft.com)
+		string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, System.Configuration.ConfigurationManager.AppSettings["Authority"], tenant);
 
-            app.UseMvc();
-        }
-    }
+		/// <summary>
+		/// Configure OWIN to use OpenIdConnect 
+		/// </summary>
+		/// <param name="app"></param>
+		public void Configuration(IAppBuilder app)
+		{
+			app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+			app.UseCookieAuthentication(new CookieAuthenticationOptions());
+			app.UseOpenIdConnectAuthentication(
+				new OpenIdConnectAuthenticationOptions
+				{
+				// Sets the ClientId, authority, RedirectUri as obtained from web.config
+				ClientId = clientId,
+					Authority = authority,
+					RedirectUri = redirectUrl,
+
+				// PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
+				PostLogoutRedirectUri = redirectUrl,
+
+				//Scope is the requested scope: OpenIdConnectScopes.OpenIdProfileis equivalent to the string 'openid profile': in the consent screen, this will result in 'Sign you in and read your profile'
+				Scope = OpenIdConnectScope.OpenIdProfile,
+
+				// ResponseType is set to request the id_token - which contains basic information about the signed-in user
+				ResponseType = OpenIdConnectResponseType.IdToken,
+
+				// ValidateIssuer set to false to allow work accounts from any organization to sign in to your application
+				// To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name or Id (example: contoso.onmicrosoft.com)
+				// To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter
+				TokenValidationParameters = new TokenValidationParameters()
+					{
+						ValidateIssuer = true
+					},
+
+				// OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to OnAuthenticationFailed method
+				Notifications = new OpenIdConnectAuthenticationNotifications
+					{
+						AuthenticationFailed = OnAuthenticationFailed
+					}
+				}
+			);
+		}
+
+		/// <summary>
+		/// Handle failed authentication requests by redirecting the user to the home page with an error in the query string
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> context)
+		{
+			context.HandleResponse();
+			context.Response.Redirect("/?errormessage=" + context.Exception.Message);
+			return Task.FromResult(0);
+		}
+	}
 }
