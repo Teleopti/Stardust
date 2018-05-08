44288019f71d4d057194953d21fd70cb9ac018b3
Emil Sundin <Emil.Sundin@teleopti.com>
2018-05-08 06:25:54 +0000

    Merged PR 76: Implemented 401 exception handler in Angular
    Related work items: #75830

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/core/interceptors/authenticated.interceptor.ts
@@ -0,0 +1,27 @@
+import { DOCUMENT } from '@angular/common';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class AuthenticatedInterceptor implements HttpInterceptor {
+	constructor(@Inject(DOCUMENT) private document: Document) {}
+
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		return next.handle(req).pipe(
+			tap(
+				event => {},
+				error => {
+					if (error instanceof HttpErrorResponse && error.status == 401) {
+						this.handleUnauthorized();
+					}
+				}
+			)
+		);
+	}
+
+	handleUnauthorized() {
+		this.document.location.href = 'Authentication';
+	}
+}
