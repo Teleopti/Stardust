d46d523c8af5e09141d497dea25adbe340c1d6cb
Qing Luo <Qing.Luo@teleopti.com>
2018-05-09 06:11:41 +0000

    Merged PR 103: Add max consecutive working days to workflowcontrolset
    Add max consecutive working days to workflowcontrolset
    
    Related work items: #74889

--- Domain/WorkflowControl/WorkflowControlSet.cs
+++ Domain/WorkflowControl/WorkflowControlSet.cs
@@ -35,6 +35,7 @@ namespace Teleopti.Ccc.Domain.WorkflowControl
 		private bool _lockTrading;
 		private bool _absenceRequestWaitlistEnabled;
 		private bool _absenceProbabilityEnabled;
+		private int _maxConsecutiveWorkingDays;
 		private WaitlistProcessOrder _absenceRequestWaitlistProcessOrder;
 		private int? _absenceRequestCancellationThreshold;
 		private int? _absenceRequestExpiredThreshold;
@@ -464,6 +465,11 @@ namespace Teleopti.Ccc.Domain.WorkflowControl
 		public virtual OvertimeValidationHandleType? OvertimeRequestMaximumContinuousWorkTimeHandleType { get => _overtimeRequestMaximumContinuousWorkTimeHandleType; set => _overtimeRequestMaximumContinuousWorkTimeHandleType = value; }
 		public virtual bool OvertimeRequestMaximumContinuousWorkTimeEnabled { get => _overtimeRequestMaximumContinuousWorkTimeEnabled; set => _overtimeRequestMaximumContinuousWorkTimeEnabled = value; }
 		public virtual OvertimeRequestStaffingCheckMethod OvertimeRequestStaffingCheckMethod { get => _overtimeRequestStaffingCheckMethod; set => _overtimeRequestStaffingCheckMethod = value; }
+		public virtual int MaximumConsecutiveWorkingDays
+		{
+			get => _maxConsecutiveWorkingDays;
+			set => _maxConsecutiveWorkingDays = value;
+		}
 
 		public virtual void AddAllowedAbsenceForReport(IAbsence absence)
 		{
