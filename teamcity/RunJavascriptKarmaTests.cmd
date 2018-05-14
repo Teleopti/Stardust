f8a0d538006b144a3f8c8195b033f8bb3003ab1e
Emil Sundin <Emil.Sundin@teleopti.com>
2018-05-14 16:51:05 +0000

    Merged PR 121: Update build script RunJavaScriptKarmaTests

--- teamcity/RunJavascriptKarmaTests.cmd
+++ teamcity/RunJavascriptKarmaTests.cmd
@@ -1,14 +1,19 @@
 @ECHO off
+
+:: Set nodevars so node/npm/npx is available via path
+call ..\..\..\packages\NodeEnv.1.0.7\nodevars.bat
+
+
 SET /A ERRORLEV=0
 :: Runs from [repo]\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM
-call ..\.node\npm test
+npm test
 IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=1 & GOTO :error
 :: Clear all dev dependencies and only install what we need for production
-call ..\.node\npm install rimraf
+call npm install rimraf
 IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=2 & GOTO :error
-call ..\.node\node node_modules\rimraf\bin.js node_modules
+npx rimraf node_modules
 IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=3 & GOTO :error
-call ..\.node\npm install --production
+npm install --production
 IF %ERRORLEVEL% NEQ 0 SET /A ERRORLEV=4 & GOTO :error
 echo %errorlevel%
 
