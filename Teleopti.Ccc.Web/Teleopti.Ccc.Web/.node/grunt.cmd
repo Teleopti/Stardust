dd53cb31e997bc942f4754bb0e954d2818d8572b
Emil Sundin <emil.sundin@teleopti.com>
2018-05-16 09:53:35 +0200

    Wrap grunt command with npx

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/grunt.cmd
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/grunt.cmd
@@ -1,3 +1,3 @@
 @echo off
-@SET PATH=%PATH%;%~dp0
-node "%~dp0..\..\..\packages\nodeenv.1.0.6\node_modules\grunt-cli\bin\grunt" %*
+call %~dp0..\..\..\packages\NodeEnv.1.0.7\nodevars.bat
+npx grunt %*
