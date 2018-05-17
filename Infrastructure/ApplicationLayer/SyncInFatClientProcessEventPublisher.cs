77da565d66630fdc09d1e70e03e9426a4a68761f
Claes Stenman <claes.stenman@teleopti.com>
2018-05-17 10:49:41 +0200

    74915 prepare to throttle number of tasks using Parallell.ForEach

--- Infrastructure/ApplicationLayer/SyncInFatClientProcessEventPublisher.cs
+++ Infrastructure/ApplicationLayer/SyncInFatClientProcessEventPublisher.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.FeatureFlags;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 
 namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
@@ -10,24 +12,66 @@ namespace Teleopti.Ccc.Infrastructure.ApplicationLayer
 	{
 		private readonly ResolveEventHandlers _resolver;
 		private readonly CommonEventProcessor _processor;
+		private readonly IEventRunner _eventRunner;
 
-		public SyncInFatClientProcessEventPublisher(ResolveEventHandlers resolver, CommonEventProcessor processor)
+		public SyncInFatClientProcessEventPublisher(ResolveEventHandlers resolver, CommonEventProcessor processor, IEventRunner eventRunner)
 		{
 			_resolver = resolver;
 			_processor = processor;
+			_eventRunner = eventRunner;
 		}
 
 		public void Publish(params IEvent[] events)
 		{
+			_eventRunner.RunEvents(_resolver, _processor, events);
+		}
+	}
+
+	[RemoveMeWithToggle(Toggles.ResourcePlanner_LessResourcesXXL_74915)]
+	public interface IEventRunner
+	{
+		void RunEvents(ResolveEventHandlers resolver, CommonEventProcessor processor, params IEvent[] events);
+	}
+
+	[RemoveMeWithToggle(Toggles.ResourcePlanner_LessResourcesXXL_74915)]
+	public class EventRunner : IEventRunner
+	{
+		public void RunEvents(ResolveEventHandlers resolver, CommonEventProcessor processor, params IEvent[] events)
+		{
 			Task.WaitAll((
 					from @event in events
 #pragma warning disable 618
-					from handlerType in _resolver.HandlerTypesFor<IRunInSyncInFatClientProcess>(@event)
+					from handlerType in resolver.HandlerTypesFor<IRunInSyncInFatClientProcess>(@event)
 					select Task.Run(() =>
-							_processor.Process(@event, handlerType)
+							processor.Process(@event, handlerType)
 #pragma warning restore 618
 					))
 				.ToArray());
 		}
 	}
+
+	[RemoveMeWithToggle("temp, move back into SyncInFatClientProcessEventPublisher", Toggles.ResourcePlanner_LessResourcesXXL_74915)]
+	public class EventRunnerThrottled : IEventRunner
+	{
+		public void RunEvents(ResolveEventHandlers resolver, CommonEventProcessor processor, params IEvent[] events)
+		{
+#pragma warning disable 618
+			var eventInfos = from @event in events
+				from handlerType in resolver.HandlerTypesFor<IRunInSyncInFatClientProcess>(@event)
+				select new eventAndHandlerType { Event = @event, HandlerType = handlerType };
+
+			Parallel.ForEach(eventInfos, 
+			eventInfo =>
+			{
+				processor.Process(eventInfo.Event, eventInfo.HandlerType);
+			});
+#pragma warning restore 618
+		}
+
+		private class eventAndHandlerType
+		{
+			public IEvent @Event { get; set; }
+			public Type HandlerType { get; set; }
+		}
+	}	
 }
