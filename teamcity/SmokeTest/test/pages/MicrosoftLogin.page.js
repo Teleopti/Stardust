5ae1360c0a11199629f05342999be86e2df69e8c
Kunning <kunning.mao@teleopti.com>
2018-05-17 08:50:53 +0200

    try to fix azure installation, seems microsoft login page has changed

--- teamcity/SmokeTest/test/pages/MicrosoftLogin.page.js
+++ teamcity/SmokeTest/test/pages/MicrosoftLogin.page.js
@@ -4,8 +4,7 @@ var MicrosoftLoginPage = Object.create(Page, {
 
     username: { get: function () { return browser.element("input[type='email']"); } },
     nextbutton: { get: function () { return browser.element("input[type='submit']"); } },
-    password: { get: function () { return browser.element('#cred_password_inputtext'); } },
-	password2: { get: function () { return browser.element('#passwordInput'); } },
+    password: { get: function () { return browser.element("input[type='password']"); } },
     signinButton:     { get: function () { return browser.element('#submitButton'); } },
     keepSignedInButton:     { get: function () { return browser.element('#idSIButton9'); } },
 	
@@ -18,8 +17,8 @@ var MicrosoftLoginPage = Object.create(Page, {
 		this.username.waitForVisible(60 * 1000);
 		this.username.setValue('demo@teleopti.com');
         this.nextbutton.click();
-		this.password2.waitForVisible(60 * 1000);
-		this.password2.setValue('teleoptidemo');
+		this.password.waitForVisible(60 * 1000);
+		this.password.setValue('teleoptidemo');
         this.signinButton.click();
         this.keepSignedInButton.waitForVisible(60 * 1000);
         this.keepSignedInButton.click();
