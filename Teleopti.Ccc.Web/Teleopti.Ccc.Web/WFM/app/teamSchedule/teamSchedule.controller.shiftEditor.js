eda96dd2d29a78f14317c005f27233a6388e1b65
Xiaoju Dai <Xiaoju.Dai@teleopti.com>
2018-05-22 07:16:27 +0000

    Story75978 show overtime and scroll to schedule start when init page

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.controller.shiftEditor.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/teamSchedule/teamSchedule.controller.shiftEditor.js
@@ -1,4 +1,5 @@
-﻿(function () {
+﻿
+(function () {
 	'use strict';
 
 	angular.module("wfm.teamSchedule").controller("ShiftEditorViewController", ['$stateParams', 'serviceDateFormatHelper', function ($stateParams, serviceDateFormatHelper) {
@@ -18,26 +19,51 @@
 		}
 	});
 
-	ShiftEditorController.$inject = ['serviceDateFormatHelper'];
+	ShiftEditorController.$inject = ['$element', '$timeout', '$window', 'serviceDateFormatHelper'];
 
-	function ShiftEditorController(serviceDateFormatHelper) {
+	function ShiftEditorController($element, $timeout, $window, serviceDateFormatHelper) {
 		var vm = this;
 		vm.intervals = [];
+		vm.projections = [];
 
 		vm.$onInit = function () {
+			if (!hasShift()) return;
 			vm.intervals = getIntervals();
+			vm.projections = getProjections();
+			scrollToScheduleStart();
+			angular.element($window).bind('resize', scrollToScheduleStart);
 		}
 
-		vm.getProjections = function () {
-			var projections = !!vm.personSchedule && !!vm.personSchedule.Shifts ? vm.personSchedule.Shifts[0].Projections : [];
+		function getProjections() {
+			var projections = vm.personSchedule.Shifts[0].Projections || [];
 			projections.forEach(function (projection) {
-				projection.Left = getDiffInHoursFromCurrentDate(projection.Start) * 60 + moment(projection.Start).minutes();
-				projection.Width = projection.Minutes;
+				projection.Left = getTotalMinutes(projection.Start);
+				projection.Width = getTotalMinutes(projection.End) - getTotalMinutes(projection.Start);
 			});
+
 			return projections;
 		}
 
-		function getDiffInHoursFromCurrentDate(dateTime) {
+
+		function scrollToScheduleStart() {
+			var shiftContainerEl = $element[0].querySelector('.shift-container');
+			$timeout(function () {
+				var shiftProjectionTimeRange = vm.personSchedule.Shifts[0].ProjectionTimeRange;
+				var shiftStart = getTotalMinutes(shiftProjectionTimeRange.Start);
+				var shiftLength = getTotalMinutes(shiftProjectionTimeRange.End) - shiftStart;
+				var scrollTo = shiftContainerEl.clientWidth <= shiftLength ?
+					(shiftStart - 120) : (shiftStart - ((shiftContainerEl.clientWidth - shiftLength) / 2));
+				if (scrollTo <= 0) return;
+				shiftContainerEl.scrollLeft = scrollTo;
+			});
+		}
+
+		function hasShift() {
+			return !!vm.personSchedule && !!vm.personSchedule.Shifts;
+		}
+
+
+		function getTotalMinutes(dateTime) {
 			var hours = 0;
 			var currentDate = moment.tz(vm.date, vm.timezone);
 			var dateTimeMoment = moment.tz(dateTime, vm.timezone);
@@ -45,15 +71,20 @@
 				hours += 1;
 				currentDate = currentDate.add('hours', 1);
 			}
-			return hours;
+			return hours * 60 + moment(dateTime).minutes();
 		}
+
 
 		function getIntervals() {
 			var intervals = [];
 			var startTime = moment.tz(vm.date, vm.timezone);
 			var endTime = moment.tz(vm.date, vm.timezone).add(1, 'days').hours(12);
 			while (startTime <= endTime) {
-				intervals.push({ label: startTime.format('LT'), time: startTime, ticks: getTicks(startTime, startTime.isSame(endTime)) });
+				intervals.push({
+					label: startTime.format('LT'),
+					time: startTime.clone(),
+					ticks: getTicks(startTime, startTime.isSame(endTime))
+				});
 				startTime = startTime.add(1, 'hours');
 			}
 			return intervals;
@@ -72,7 +103,7 @@
 				var minutes = startTime.minutes();
 				var isHalfHour = minutes !== 0 && minutes % 30 === 0;
 				var isHour = minutes === 0;
-				ticks.push({ time: startTime, isHalfHour: isHalfHour, isHour: isHour });
+				ticks.push({ time: startTime.clone(), isHalfHour: isHalfHour, isHour: isHour });
 				startTime.add(5, 'minutes');
 			}
 			return ticks;
