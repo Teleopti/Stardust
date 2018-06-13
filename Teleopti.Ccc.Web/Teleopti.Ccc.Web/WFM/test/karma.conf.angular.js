6e8a325354dc0e977c01f58225ed8da1ed111805
Emil Sundin <emil.sundin@teleopti.com>
2018-06-13 10:55:11 +0200

    Change karma reporter from dots to mocha

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/test/karma.conf.angular.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/test/karma.conf.angular.js
@@ -2,13 +2,4 @@ const { applyBaseConfig } = require('./karma.shared');
 
 module.exports = function(config) {
 	applyBaseConfig(config);
-	config.set({
-		reporters: ['mocha'],
-		mochaReporter: {
-			symbols: {
-				success: '✔'
-			}
-		},
-		colors: true
-	});
 };
