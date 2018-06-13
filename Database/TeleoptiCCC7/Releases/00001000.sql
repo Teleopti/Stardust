5017481d383591793fb800541928b158bee8e850
LevelUp! <LevelUp@teleopti.com>
2018-06-13 16:46:04 +0200

    #75656 whoops

--- Database/TeleoptiCCC7/Releases/00001000.sql
+++ Database/TeleoptiCCC7/Releases/00001000.sql
@@ -1,19 +1,2 @@
-
-DROP INDEX IDX_RuleMappings ON ReadModel.RuleMappings
-GO
-
-ALTER TABLE ReadModel.RuleMappings ALTER COLUMN BusinessUnitId [uniqueidentifier] NOT NULL
-GO
-ALTER TABLE ReadModel.RuleMappings ALTER COLUMN StateCode [nvarchar](300) NOT NULL
-GO
-ALTER TABLE ReadModel.RuleMappings ALTER COLUMN ActivityId [uniqueidentifier] NOT NULL
-GO
-
-ALTER TABLE ReadModel.RuleMappings 
-ADD CONSTRAINT [PK_RuleMappings] PRIMARY KEY CLUSTERED 
-(
-	[BusinessUnitId],
-	[StateCode],
-	[ActivityId]
-)
+EXEC sp_rename N'[ReadModel].[CurrentSchedule].[PK_PersonId]', N'PK_CurrentSchedule', N'INDEX'
 GO
