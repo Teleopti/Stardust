f16f5efbfa6dd3c8c1a347f476383adb85526068
Roger Kratz <roger.kratz@gmail.com>
2018-06-15 16:35:07 +0200

    Fixing ctor in test

--- DomainTest/SchedulingScenarios/DayOffOptimization/DayOffOptimizationMinimumAgentsDesktopTest.cs
+++ DomainTest/SchedulingScenarios/DayOffOptimization/DayOffOptimizationMinimumAgentsDesktopTest.cs
@@ -66,7 +66,7 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.DayOffOptimization
 				.Should().Be.EqualTo(skillDays[expectedDayWithDO].CurrentDate); 
 		}
 
-		public DayOffOptimizationMinimumAgentsDesktopTest(SeperateWebRequest seperateWebRequest, bool resourcePlannerDayOffOptimizationIslands47208, bool resourcePlannerMinimumAgents75339, bool resourcePlannerLessResourcesXXL74915) : base(seperateWebRequest, resourcePlannerDayOffOptimizationIslands47208, resourcePlannerLessResourcesXXL74915)
+		public DayOffOptimizationMinimumAgentsDesktopTest(SeperateWebRequest seperateWebRequest, bool resourcePlannerDayOffOptimizationIslands47208, bool resourcePlannerLessResourcesXXL74915) : base(seperateWebRequest, resourcePlannerDayOffOptimizationIslands47208, resourcePlannerLessResourcesXXL74915)
 		{
 		}
 	}
