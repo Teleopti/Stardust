6d0e526dea45880848f1b75e4c986815faca6954
Mathias Stenbom <mathias.stenbom@teleopti.com>
2018-06-26 11:06:00 +0200

    #76139 Fix build

--- Teleopti.Wfm.Administration.IntegrationTest/SetUpFixtureForAssembly.cs
+++ Teleopti.Wfm.Administration.IntegrationTest/SetUpFixtureForAssembly.cs
@@ -8,17 +8,10 @@ namespace Teleopti.Wfm.Administration.IntegrationTest
 	[SetUpFixture]
 	public class SetupFixtureForAssembly
 	{
-		internal static IPerson loggedOnPerson;
-		internal static IApplicationData ApplicationData;
-		internal static IDataSource DataSource;
-		private static int dataHash = 0;
-
 		[OneTimeSetUp]
 		public void OneTimeSetUp()
 		{
 			Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
 		}
-
 	}
-
 }
