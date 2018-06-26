ffb1e52cc4ae2ab11cb7292d6b47808eb107e207
Daniel Sund <Daniel.Sund@teleopti.com>
2018-06-26 14:16:37 +0000

    Merged PR 351: #76352 Intraday sum of individual skills incoming traffic does not equal skill grouping total
    Changed sp to only include interval with forecast for each skill when supplying list of skills
    
    Related work items: #76352

--- Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.web_intraday.sql
+++ Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.web_intraday.sql
@@ -5,6 +5,7 @@ GO
 -- =============================================
 -- Author:		Jonas & Maria S
 -- Create date: 2016-02-24
+-- Changed by: Daniel S : 2018-06-26
 -- Description:	Load queue statistics and forecast data both by interval and by day. Used by web intraday.
 -- =============================================
 -- EXEC [mart].[web_intraday] 'W. Europe Standard Time', '2017-01-13', 'F08D75B3-FDB4-484A-AE4C-9F0800E2F753'
@@ -18,33 +19,30 @@ CREATE PROCEDURE [mart].[web_intraday]
 AS
 BEGIN
 	SET NOCOUNT ON;
-            
 	DECLARE @time_zone_id as int
 	DECLARE @default_scenario_id int
 	DECLARE @bu_id int
 	
 	CREATE TABLE #skills(id uniqueidentifier)
 	
-	CREATE TABLE #queues(
-		queue_id int,
-		percentage_offered float,
-		percentage_overflow_in float,
-		percentage_overflow_out float,
-		percentage_abandoned float,
-		percentage_abandoned_short float,
-		percentage_abandoned_within_service_level float,
-		percentage_abandoned_after_service_level float
-	)
-	
 	CREATE TABLE #queue_stats(
 		date_id int,
 		interval_id smallint,
+		skill_id int,
 		calculated_calls decimal(19,0), 
 		answered_calls decimal(19,0), 
 		answered_calls_within_SL decimal(19,0), 
 		handle_time_s decimal(19,0),
 		abandoned_calls decimal(19,0),
 		speed_of_answer decimal (19,0)
+	)
+
+	CREATE TABLE #forecast(
+		[date] smalldatetime,
+		interval_id smallint,
+		skill_id int,
+		forecasted_calls decimal(28,4), 
+		forecasted_handle_time_s decimal(28,4), 
 	)
 
 	CREATE TABLE #result(
@@ -76,48 +74,65 @@ BEGIN
 	WHERE business_unit_id = @bu_id
 		AND default_scenario = 1
                          
-	INSERT INTO #queues
+	-- Prepare Queue stats
+	DECLARE @current_date_id int
+	SELECT @current_date_id = date_id FROM mart.dim_date WHERE date_date = @today
+
+	-- Queue incoming statistics
+	INSERT INTO #queue_stats
 	SELECT DISTINCT 
-		qw.queue_id,
-		w.percentage_offered,
-		percentage_overflow_in,
-		percentage_overflow_out,
-		percentage_abandoned,
-		percentage_abandoned_short,
-		percentage_abandoned_within_service_level,
-		percentage_abandoned_after_service_level
+		fq.date_id,
+		fq.interval_id,
+		ds.skill_id,
+		SUM(ISNULL(mart.CalculateQueueStatistics(
+			w.percentage_offered,
+			w.percentage_overflow_in,
+			w.percentage_overflow_out,
+			w.percentage_abandoned,
+			w.percentage_abandoned_short,
+			w.percentage_abandoned_within_service_level,
+			w.percentage_abandoned_after_service_level,
+			ISNULL(fq.offered_calls, 0),
+			ISNULL(fq.abandoned_calls, 0),
+			ISNULL(fq.abandoned_calls_within_SL, 0),
+			ISNULL(fq.abandoned_short_calls, 0),
+			ISNULL(fq.overflow_out_calls, 0),
+			ISNULL(fq.overflow_in_calls, 0)), 0)) as calculated_calls,
+		SUM(ISNULL(answered_calls, 0)) as answered_calls,
+		SUM(ISNULL(answered_calls_within_SL, 0)) as answered_calls_within_SL,
+		SUM(ISNULL(handle_time_s, 0)) as handle_time_s,
+		SUM(ISNULL(abandoned_calls, 0)) as abandoned_calls,
+		SUM(ISNULL(speed_of_answer_s, 0)) as speed_of_answer
 	FROM 
 		mart.bridge_queue_workload qw WITH (NOLOCK)
-		INNER JOIN mart.dim_workload w WITH (NOLOCK) 
-			ON qw.workload_id = w.workload_id
-		INNER JOIN mart.dim_skill ds WITH (NOLOCK) 
-			ON qw.skill_id = ds.skill_id
-		INNER JOIN #skills s 
-			ON ds.skill_code = s.id
+		INNER JOIN mart.fact_queue fq ON qw.queue_id = fq.queue_id
+		INNER JOIN mart.dim_workload w WITH (NOLOCK) ON qw.workload_id = w.workload_id
+		INNER JOIN mart.dim_skill ds WITH (NOLOCK) ON qw.skill_id = ds.skill_id
+		INNER JOIN #skills s ON ds.skill_code = s.id
 	WHERE 
+		fq.date_id between @current_date_id - 1 and @current_date_id + 1 AND
 		w.is_deleted = 0
+	GROUP BY
+ 		date_id,
+		interval_id,
+		ds.skill_id
 
 	-- Forecast
-	INSERT INTO #result([date], interval_id, forecasted_calls, forecasted_handle_time_s)
+	INSERT INTO #forecast([date], interval_id, skill_id, forecasted_calls, forecasted_handle_time_s)
 	SELECT 
 		d.date_date,
 		i.interval_id,
+		ds.skill_id,
 		SUM(ISNULL(fw.forecasted_calls, 0)),
 		SUM(ISNULL(fw.forecasted_handling_time_s, 0))
 	FROM
 		mart.fact_forecast_workload fw WITH (NOLOCK) 
-		INNER JOIN mart.dim_skill ds WITH (NOLOCK) 
-			ON fw.skill_id = ds.skill_id
-		INNER JOIN #skills s 
-			ON ds.skill_code = s.id
-		INNER JOIN mart.bridge_time_zone bz WITH (NOLOCK) 
-			ON fw.date_id = bz.date_id AND fw.interval_id = bz.interval_id
-		INNER JOIN mart.dim_date d WITH (NOLOCK) 
-			ON bz.local_date_id = d.date_id
-		INNER JOIN mart.dim_interval i WITH (NOLOCK) 
-			ON bz.local_interval_id = i.interval_id
-		INNER JOIN mart.dim_workload wl WITH (NOLOCK)
-			ON wl.workload_id = fw.workload_id
+		INNER JOIN mart.dim_skill ds WITH (NOLOCK) ON fw.skill_id = ds.skill_id
+		INNER JOIN #skills s ON ds.skill_code = s.id
+		INNER JOIN mart.bridge_time_zone bz WITH (NOLOCK) ON fw.date_id = bz.date_id AND fw.interval_id = bz.interval_id
+		INNER JOIN mart.dim_date d WITH (NOLOCK) ON bz.local_date_id = d.date_id
+		INNER JOIN mart.dim_interval i WITH (NOLOCK) ON bz.local_interval_id = i.interval_id
+		INNER JOIN mart.dim_workload wl WITH (NOLOCK) ON wl.workload_id = fw.workload_id
 	WHERE
 		fw.scenario_id = @default_scenario_id
 		AND bz.time_zone_id = @time_zone_id
@@ -125,84 +140,56 @@ BEGIN
 		AND wl.is_deleted = 0
 	GROUP BY
 		d.date_date,
-		i.interval_id
+		i.interval_id,
+		ds.skill_id
 
-	-- Prepare Queue stats
-	DECLARE @current_date_id int
-	SELECT @current_date_id = date_id FROM mart.dim_date WHERE date_date = @today
-
-	INSERT INTO #queue_stats
-	SELECT
-		date_id,
-		interval_id,
-		mart.CalculateQueueStatistics(
-			q.percentage_offered,
-			q.percentage_overflow_in,
-			q.percentage_overflow_out,
-			q.percentage_abandoned,
-			q.percentage_abandoned_short,
-			q.percentage_abandoned_within_service_level,
-			q.percentage_abandoned_after_service_level,
-			ISNULL(fq.offered_calls, 0),
-			ISNULL(fq.abandoned_calls, 0),
-			ISNULL(fq.abandoned_calls_within_SL, 0),
-			ISNULL(fq.abandoned_short_calls, 0),
-			ISNULL(fq.overflow_out_calls, 0),
-			ISNULL(fq.overflow_in_calls, 0)) AS calculated_calls,
-		answered_calls,
-		answered_calls_within_SL,
-		handle_time_s,
-		abandoned_calls,
-		speed_of_answer_s
-	FROM 
-		#queues q
-		INNER JOIN mart.fact_queue fq 
-			ON q.queue_id = fq.queue_id
-	WHERE
-		date_id between @current_date_id - 1 and @current_date_id + 1
-	ORDER BY date_id,
-		interval_id,
-		fq.queue_id
-
-		
-	-- Queue stats - update result
-	UPDATE r
-	SET 
-		calculated_calls = fq.calculated_calls,
-		answered_calls = fq.answered_calls,
-		answered_calls_within_SL = fq.answered_calls_within_SL,
-		handle_time_s = fq.handle_time_s,
-		abandoned_calls = fq.abandoned_calls,
-		speed_of_answer = fq.speed_of_answer
-	FROM 
-			(SELECT 
-				date_id,
-				interval_id,
-				SUM(ISNULL(calculated_calls, 0)) as calculated_calls,
-				SUM(ISNULL(answered_calls, 0)) as answered_calls,
-				SUM(ISNULL(answered_calls_within_SL, 0)) as answered_calls_within_SL,
-				SUM(ISNULL(handle_time_s, 0)) as handle_time_s,
-				SUM(ISNULL(abandoned_calls, 0)) as abandoned_calls,
-				SUM(ISNULL(speed_of_answer, 0)) as speed_of_answer
-			FROM 
-				#queue_stats
-			GROUP BY
-				date_id,
-				interval_id
-		) AS fq
-		INNER JOIN mart.bridge_time_zone bz WITH (NOLOCK) 
-			ON fq.date_id = bz.date_id AND fq.interval_id = bz.interval_id
-		INNER JOIN mart.dim_date d WITH (NOLOCK) 
-			ON bz.local_date_id = d.date_id
-		INNER JOIN mart.dim_interval i WITH (NOLOCK) 
-			ON bz.local_interval_id = i.interval_id
-		INNER JOIN #result r 
-			ON i.interval_id = r.interval_id
-	WHERE
-		bz.time_zone_id = @time_zone_id 
-		AND d.date_date = @today
-
-
+	-- Merge forecast and incoming
+	INSERT INTO #result
+	SELECT forecast.[date],
+		forecast.interval_id,
+		forecast.forecasted_calls, 
+		forecast.forecasted_handle_time_s, 
+		incoming.calculated_calls, 
+		incoming.answered_calls,
+		incoming.answered_calls_within_SL,
+		incoming.handle_time_s,
+		incoming.abandoned_calls,
+		incoming.speed_of_answer 
+	FROM
+		(SELECT 
+			f.[date],
+			f.interval_id,
+			SUM(ISNULL(f.forecasted_calls, 0)) AS forecasted_calls,
+			SUM(ISNULL(f.forecasted_handle_time_s, 0)) AS forecasted_handle_time_s
+		FROM
+			#forecast f
+		GROUP BY 
+			[date],
+			interval_id) AS forecast
+	LEFT JOIN 	
+		(SELECT 
+			f.[date],
+			f.interval_id,
+			SUM(ISNULL(fq.calculated_calls, 0)) as calculated_calls,
+			SUM(ISNULL(answered_calls, 0)) as answered_calls,
+			SUM(ISNULL(answered_calls_within_SL, 0)) as answered_calls_within_SL,
+			SUM(ISNULL(handle_time_s, 0)) as handle_time_s,
+			SUM(ISNULL(abandoned_calls, 0)) as abandoned_calls,
+			SUM(ISNULL(speed_of_answer, 0)) as speed_of_answer
+		FROM 
+			#queue_stats AS fq
+			INNER JOIN mart.bridge_time_zone bz WITH (NOLOCK) ON fq.date_id = bz.date_id AND fq.interval_id = bz.interval_id
+			INNER JOIN mart.dim_date d WITH (NOLOCK) ON bz.local_date_id = d.date_id
+			INNER JOIN mart.dim_interval i WITH (NOLOCK) ON bz.local_interval_id = i.interval_id
+			INNER JOIN #forecast f ON i.interval_id = f.interval_id AND f.skill_id = fq.skill_id
+		WHERE
+			bz.time_zone_id = @time_zone_id 
+			AND d.date_date = @today
+		GROUP BY 
+			f.[date],
+			f.interval_id
+		) AS incoming ON incoming.[date] = forecast.[date] AND incoming.[interval_id] = forecast.[interval_id]
+	
 	-- Return data by interval
 	SELECT
 		[date] AS IntervalDate,
@@ -244,8 +231,5 @@ BEGIN
 	FROM #result 
 	ORDER BY interval_id
 END
-
-
-
 GO
 
