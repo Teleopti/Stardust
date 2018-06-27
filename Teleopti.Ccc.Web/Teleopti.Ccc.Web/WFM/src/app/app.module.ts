5ed2bef4b557bbdab91923f921112f9bf5365fa6
Aidin Dadashzadeh <Aidin.Dadashzadeh@teleopti.com>
2018-06-27 12:10:13 +0000

    Merged PR 365: Remove unnecessary junk
    Remove unnecessary junk

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/app.module.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 
-import { AppComponent } from './app.component';
 import { PeopleModule } from './people/people.module';
 import { ApiAccessModule } from './api-access/api-access.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [],
 	imports: [CoreModule, BrowserModule, UpgradeModule, PeopleModule, ApiAccessModule, HttpClientModule],
-	entryComponents: [AppComponent]
+	entryComponents: []
 })
 export class AppModule {
 	constructor(private upgrade: UpgradeModule) {}
