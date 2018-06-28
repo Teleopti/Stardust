b32a7320ffa2c8d08467527adc707d4b62daefaa
Roger Kratz <roger.kratz@gmail.com>
2018-06-28 13:29:57 +0200

    #74915 Preparing to get rid of Person ref on projection Step 2: Fixed it in sorting in scheduler

--- Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/WinCode/Scheduling/ScheduleSortingCommands/SortByEndDescendingCommand.cs
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/WinCode/Scheduling/ScheduleSortingCommands/SortByEndDescendingCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
@@ -21,10 +22,10 @@ namespace Teleopti.Ccc.SmartClientPortal.Shell.WinCode.Scheduling.ScheduleSortin
             MergeLists();
         }
 
-        private static List<IVisualLayerCollection> sort(IEnumerable<IVisualLayerCollection> projections)
+        private static List<Tuple<IVisualLayerCollection, IPerson>> sort(IEnumerable<Tuple<IVisualLayerCollection, IPerson>> projections)
         {
             var sorted = from p in projections
-                         orderby p.Period().Value.EndDateTime descending
+                         orderby p.Item1.Period().Value.EndDateTime descending
                          select p;
             return sorted.ToList();
         }
