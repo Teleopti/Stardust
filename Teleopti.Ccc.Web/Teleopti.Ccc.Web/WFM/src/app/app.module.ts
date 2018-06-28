79434829d6dd55806c65435cd71a74e3495cbd92
Emil Sundin <emil.sundin@teleopti.com>
2018-06-28 13:34:02 +0200

    Prefer 'en-GB' instead of 'en'

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/app.module.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeModule } from '@angular/upgrade/static';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { CoreModule } from './core/core.module';
-import { PeopleModule } from './people/people.module';
 import { ApiAccessModule } from './api-access/api-access.module';
+import { CoreModule } from './core/core.module';
+import { UserPreferences, UserService } from './core/services';
 import { LanguageLoaderFactory } from './core/translation';
-import { UserService, UserPreferences } from './core/services';
+import { PeopleModule } from './people/people.module';
 
 @NgModule({
 	declarations: [],
@@ -31,7 +30,7 @@ import { UserService, UserPreferences } from './core/services';
 })
 export class AppModule {
 	constructor(private upgrade: UpgradeModule, private userService: UserService, private translate: TranslateService) {
-		translate.setDefaultLang('en');
+		translate.setDefaultLang('en-GB');
 		userService.getPreferences().subscribe({
 			next: (preferences: UserPreferences) => {
 				translate.use(preferences.Language);
