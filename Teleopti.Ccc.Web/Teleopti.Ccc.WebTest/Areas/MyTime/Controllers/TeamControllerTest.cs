88f9258d9767b99422c3b1431099f1d6c2eba73e
Robin Karlsson <robin.karlsson@teleopti.com>
2018-06-29 16:36:19 +0200

    Removing test cases for removed unused code

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/TeamControllerTest.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebTest/Areas/MyTime/Controllers/TeamControllerTest.cs
@@ -26,32 +26,6 @@ namespace Teleopti.Ccc.WebTest.Areas.MyTime.Controllers
 	[TestFixture]
 	public class TeamControllerTest
 	{
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope"), Test]
-		public void ShouldReturnTeamsAndOrGroupingssAsJson()
-		{
-			var teams = new[] { new SelectGroup() };
-			var viewModelFactory = MockRepository.GenerateMock<ITeamViewModelFactory>();
-			viewModelFactory.Stub(x => x.CreateTeamOrGroupOptionsViewModel(DateOnly.Today)).Return(teams);
-
-			var target = new TeamController(viewModelFactory, new Now(), null);
-
-			var result = target.TeamsAndOrGroupings(DateOnly.Today);
-
-			var data = result.Data as IEnumerable<SelectGroup>;
-			data.Should().Have.SameValuesAs(teams);
-		}
-
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope"), Test]
-		public void ShouldUseTodayWhenDateNotSpecifiedForTeamsAndOrGroupings()
-		{
-			var viewModelFactory = MockRepository.GenerateMock<ITeamViewModelFactory>();
-			var target = new TeamController(viewModelFactory, new Now(), null);
-
-			target.TeamsAndOrGroupings(null);
-
-			viewModelFactory.AssertWasCalled(x => x.CreateTeamOrGroupOptionsViewModel(DateOnly.Today));
-		}
-
 		[Test]
 		public void ShouldUseTodayWhenDateNotSpecifiedForTeams()
 		{
