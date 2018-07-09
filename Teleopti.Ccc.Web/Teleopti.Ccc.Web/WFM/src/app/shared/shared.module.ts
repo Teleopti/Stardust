44d53422ac3210dbe7cd9dbd7494fac2b992b97c
Emil Sundin <emil.sundin@teleopti.com>
2018-07-09 15:47:16 +0200

    Fix Ant import

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/shared/shared.module.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/shared/shared.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
-import { NzAlertModule } from 'ng-zorro-antd';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { en_US, NgZorroAntdModule, NZ_I18N } from 'ng-zorro-antd';
 
 @NgModule({
-	imports: [CommonModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, HttpClientModule, NzAlertModule],
-	exports: [CommonModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, HttpClientModule, NzAlertModule],
-	providers: [],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		NgZorroAntdModule
+	],
+	exports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		NgZorroAntdModule
+	],
+	providers: [{ provide: NZ_I18N, useValue: en_US }],
 	entryComponents: []
 })
 export class SharedModule {}
