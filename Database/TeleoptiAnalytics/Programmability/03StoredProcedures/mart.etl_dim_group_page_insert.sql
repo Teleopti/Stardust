da480463dfa23e5b76792c73c6e882e2f8fcd122
Xinfeng Li <xinfeng.li@teleopti.com>
2018-07-11 10:19:09 +0200

    Fix bug #76690: Hangfire job : AnalyticsOptionalColumnGroupPageHandler got OptionalColumnValueChangedEvent on Teleopti WFM fails

--- Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_dim_group_page_insert.sql
+++ Database/TeleoptiAnalytics/Programmability/03StoredProcedures/mart.etl_dim_group_page_insert.sql
@@ -16,37 +16,67 @@ CREATE PROCEDURE [mart].[etl_dim_group_page_insert]
 
 AS
 BEGIN 
-  INSERT INTO [mart].[dim_group_page] (
-			[group_page_code], 
-			[group_page_name], 
-			[group_page_name_resource_key], 
-			[group_code], 
-			[group_name], 
-			[group_is_custom], 
-			[business_unit_id], 
-			[business_unit_code], 
-			[business_unit_name], 
-			[datasource_id], 
-			[insert_date], 
-			[datasource_update_date])
-  SELECT 
-            @group_page_code
-            ,@group_page_name
-            ,@group_page_name_resource_key
-            ,@group_code
-            ,@group_name
-            ,@group_is_custom
-            ,bu.business_unit_id
-            ,@business_unit_code
-            ,bu.business_unit_name
-            ,bu.datasource_id
-            ,GETUTCDATE()
-            ,GETUTCDATE()
-  FROM [mart].[dim_business_unit] bu
-  WHERE business_unit_code = @business_unit_code
-  AND NOT EXISTS (SELECT 1 
-                     FROM [mart].[dim_group_page] WITH (NOLOCK)
-                    WHERE group_code = @group_code and business_unit_id = bu.business_unit_id)
+	DECLARE @business_unit_id int, @business_unit_name nvarchar(100)
+
+	SELECT 
+		@business_unit_id = business_unit_id,
+		@business_unit_name = business_unit_name
+	FROM mart.dim_business_unit WITH (NOLOCK)
+	WHERE business_unit_code = @business_unit_code
+
+	MERGE mart.dim_group_page AS target  
+    USING (SELECT 
+				@group_page_code,
+				@group_page_name,
+				@group_page_name_resource_key,
+				@group_code,
+				@group_name,
+				@group_is_custom,
+				@business_unit_code
+				) AS src 
+					(
+						group_page_code,
+						group_page_name,
+						group_page_name_resource_key,
+						group_code,
+						group_name,
+						group_is_custom,
+						business_unit_code
+					)
+		ON (
+			target.group_page_code = src.group_page_code
+			and target.group_name COLLATE DATABASE_DEFAULT = src.group_name COLLATE DATABASE_DEFAULT
+			and target.business_unit_code = src.business_unit_code
+			)
+	WHEN NOT MATCHED THEN  
+		INSERT (
+					[group_page_code], 
+					[group_page_name], 
+					[group_page_name_resource_key], 
+					[group_code], 
+					[group_name], 
+					[group_is_custom], 
+					[business_unit_id], 
+					[business_unit_code], 
+					[business_unit_name], 
+					[datasource_id], 
+					[insert_date], 
+					[datasource_update_date]
+				)
+		VALUES (
+					@group_page_code,
+					@group_page_name,
+					@group_page_name_resource_key,
+					@group_code,
+					@group_name,
+					@group_is_custom,
+					@business_unit_id,
+					@business_unit_code,
+					@business_unit_name,
+					1,
+					GETUTCDATE(),
+					GETUTCDATE()
+				);
 
 END
 
