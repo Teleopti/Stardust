8391fc372fb17292ec24a29effabba6dbad43656
Claes Stenman <claes.stenman@teleopti.com>
2018-07-11 09:07:29 +0200

    PBI75509 refactor test

--- DomainTest/SchedulingScenarios/ResourceCalculation/ResourceCalculationResultTest.cs
+++ DomainTest/SchedulingScenarios/ResourceCalculation/ResourceCalculationResultTest.cs
@@ -200,15 +200,17 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.ResourceCalculation
 		}
 
 		[Ignore("PBI75509 to be fixed")]
-		[TestCase("Arabian Standard Time")] //+04:00
-		[TestCase("Iran Standard Time")]	//+03:30
-		public void ShouldHandleSkillInHalfHourTimeZone(string timeZoneId)
+		[TestCase("Arabian Standard Time", 15)] //+04:00
+		[TestCase("Iran Standard Time", 15)]    //+03:30
+		[TestCase("Arabian Standard Time", 60)] //+04:00
+		[TestCase("Iran Standard Time", 60)]    //+03:30
+		public void ShouldHandleSkillInHalfHourTimeZoneAndOneHourResolution(string timeZoneId, int defaultResolution)
 		{
 			var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
 			var scenario = new Scenario();
 			var activity = new Activity();
 			var dateOnly = DateOnly.Today;
-			var skill = new Skill("_", "_", Color.Empty, 60, new SkillTypeEmail(new Description(), ForecastSource.InboundTelephony)).For(activity).InTimeZone(timeZone).WithId().IsOpenBetween(8, 9);
+			var skill = new Skill().For(activity).InTimeZone(timeZone).DefaultResolution(defaultResolution).WithId().IsOpenBetween(8, 9);
 			var skillDay = skill.CreateSkillDayWithDemand(scenario, dateOnly, 2);
 			var agent = new Person().InTimeZone(timeZone).WithPersonPeriod(skill);
 			var assignment = new PersonAssignment(agent, scenario, dateOnly).WithLayer(activity, new TimePeriod(5, 10));
