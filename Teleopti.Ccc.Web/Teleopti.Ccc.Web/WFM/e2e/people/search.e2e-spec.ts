bdd259c5968ad757595f251a152e26e5d9559fed
Emil Sundin <emil.sundin@teleopti.com>
2018-07-20 14:13:47 +0200

    Add real e2e test for people

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/e2e/people/search.e2e-spec.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/e2e/people/search.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, Key } from 'protractor';
 
 describe('People Search', () => {
 	let page: Page;
@@ -11,6 +11,12 @@ describe('People Search', () => {
 		page.navigateTo();
 		expect(page.title).toContain('People');
 	});
+
+	it('should be searchable', async () => {
+		await page.navigateTo();
+		await page.search('a');
+		expect(page.searchResult.count()).toBeGreaterThan(0);
+	});
 });
 
 class Page {
@@ -18,7 +24,24 @@ class Page {
 		return browser.get('/TeleoptiWFM/Web/WFM/#/people/search');
 	}
 
+	async search(keyword: string) {
+		await this.searchInput.sendKeys(keyword);
+		await this.searchInput.sendKeys(Key.ENTER);
+		await browser.wait(async () => {
+			const resultCount = await this.searchResult.count();
+			return resultCount > 0;
+		}, 4 * 1000);
+	}
+
 	get title() {
 		return element(by.css('.view-title h1')).getText();
+	}
+
+	get searchInput() {
+		return element(by.css('[data-test-searchinput]'));
+	}
+
+	get searchResult() {
+		return element.all(by.css('[data-test-person]'));
 	}
 }
