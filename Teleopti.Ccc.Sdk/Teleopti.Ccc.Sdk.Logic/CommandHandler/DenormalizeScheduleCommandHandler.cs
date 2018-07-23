b93343ed8a3d02daa1aea25a24483042e6e0f1cf
Ola Håkansson <lars.ola.hakansson@gmail.com>
2018-07-23 14:23:12 +0200

    Bug #76976 adding validation of the command so the person for example exists

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/DenormalizeScheduleCommandHandler.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/CommandHandler/DenormalizeScheduleCommandHandler.cs
@@ -1,32 +1,70 @@
 ﻿using System;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Sdk.Common.DataTransferObject.Commands;
 
 namespace Teleopti.Ccc.Sdk.Logic.CommandHandler
 {
-    public class DenormalizeScheduleCommandHandler : IHandleCommand<DenormalizeScheduleCommandDto>
-    {
+	public class DenormalizeScheduleCommandHandler : IHandleCommand<DenormalizeScheduleCommandDto>, IInitiatorIdentifier
+	{
 		private readonly IEventPopulatingPublisher _eventPopulatingPublisher;
+		private readonly IPersonRepository _personRepository;
+		private readonly IScenarioRepository _scenarioRepository;
+		private readonly IInitiatorIdentifierScope _initiatorIdentifierScope;
+		private readonly ICurrentUnitOfWorkFactory _unitOfWorkFactory;
 
-		public DenormalizeScheduleCommandHandler(IEventPopulatingPublisher eventPopulatingPublisher)
+
+		public DenormalizeScheduleCommandHandler(IEventPopulatingPublisher eventPopulatingPublisher,
+			IPersonRepository personRepository, IScenarioRepository scenarioRepository,
+			IInitiatorIdentifierScope initiatorIdentifierScope, ICurrentUnitOfWorkFactory unitOfWorkFactory)
 		{
 			_eventPopulatingPublisher = eventPopulatingPublisher;
+			_personRepository = personRepository;
+			_scenarioRepository = scenarioRepository;
+			_initiatorIdentifierScope = initiatorIdentifierScope;
+			_unitOfWorkFactory = unitOfWorkFactory;
 		}
 
 		public void Handle(DenormalizeScheduleCommandDto command)
 		{
-			var message = new ScheduleChangedEvent
-			              	{
-			              		StartDateTime = DateTime.SpecifyKind(command.StartDateTime, DateTimeKind.Utc),
-			              		EndDateTime = DateTime.SpecifyKind(command.EndDateTime, DateTimeKind.Utc),
-			              		ScenarioId = command.ScenarioId,
-			              		PersonId = command.PersonId
-			              	};
+			var personExists = false;
+			var scenarioExists = false;
 
-			_eventPopulatingPublisher.Publish(message);
+			using (_unitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			{
+				personExists = _personRepository.Get(command.PersonId) != null;
+				scenarioExists = _scenarioRepository.Get(command.ScenarioId) != null;
+			}
 
-			command.Result = new CommandResultDto { AffectedId = Guid.Empty, AffectedItems = 1 };
+			if (!personExists || !scenarioExists ||
+				command.StartDateTime == DateTime.MinValue ||
+				command.EndDateTime == DateTime.MinValue ||
+				command.StartDateTime > command.EndDateTime
+			)
+			{
+				command.Result = new CommandResultDto { AffectedId = Guid.Empty, AffectedItems = 0 };
+			}
+			else
+			{
+				var message = new ScheduleChangedEvent
+				{
+					StartDateTime = DateTime.SpecifyKind(command.StartDateTime, DateTimeKind.Utc),
+					EndDateTime = DateTime.SpecifyKind(command.EndDateTime, DateTimeKind.Utc),
+					ScenarioId = command.ScenarioId,
+					PersonId = command.PersonId
+				};
+				using (_initiatorIdentifierScope.OnThisThreadUse(this))
+				{
+					_eventPopulatingPublisher.Publish(message);
+				}
+
+				command.Result = new CommandResultDto { AffectedId = Guid.Empty, AffectedItems = 1 };
+			}
+
 		}
-    }
+
+		public Guid InitiatorId => new Guid("2406c0c3-123b-4a00-902f-ceac53cb835f");
+	}
 }
--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/CommandHandler/DenormalizeScheduleCommandHandlerTest.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.LogicTest/CommandHandler/DenormalizeScheduleCommandHandlerTest.cs
@@ -1,53 +1,167 @@
-﻿using NUnit.Framework;
-using Rhino.Mocks;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
 using Teleopti.Ccc.Domain.ApplicationLayer;
-using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.Sdk.Common.DataTransferObject.Commands;
 using Teleopti.Ccc.Sdk.Logic.CommandHandler;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
 
 namespace Teleopti.Ccc.Sdk.LogicTest.CommandHandler
 {
-    [TestFixture]
-    public class DenormalizeScheduleCommandHandlerTest
-    {
-		private IEventPopulatingPublisher _busSender;
-        private MockRepository _mock;
-        private DenormalizeScheduleCommandHandler _target;
-        private DenormalizeScheduleCommandDto _denormalizeScheduleCommandDto;
+	[TestFixture]
+	[DomainTest]
+	public class DenormalizeScheduleCommandHandlerTest : IExtendSystem, IIsolateSystem
+	{
+		public DenormalizeScheduleCommandHandler Target;
+		public FakeEventPopulatingPublisherForTest EventPopulatingPublisher;
+		public FakePersonRepository PersonRepository;
+		public FakeScenarioRepository ScenarioRepository;
 
-        [SetUp]
-        public void Setup()
-        {
-            _mock = new MockRepository();
-			_busSender = _mock.StrictMock<IEventPopulatingPublisher>();
-            _target = new DenormalizeScheduleCommandHandler(_busSender);
-            _denormalizeScheduleCommandDto = new DenormalizeScheduleCommandDto();
-        }
+		public void Extend(IExtend extend, IIocConfiguration configuration)
+		{
+			extend.AddService<DenormalizeScheduleCommandHandler>();
+		}
 
-        [Test]
-        public void ShouldProcessDenormalizeScheduleCommand()
-        {
-            using (_mock.Record())
-            {
-                Expect.Call(() => _busSender.Publish(new ScheduleChangedEvent())).IgnoreArguments();
-            }
-            using (_mock.Playback())
-            {
-                _target.Handle(_denormalizeScheduleCommandDto);
-            }
-        }
+		public void Isolate(IIsolate isolate)
+		{
+			isolate.UseTestDouble<FakeEventPopulatingPublisherForTest>().For<IEventPopulatingPublisher>();
+		}
 
-        [Test]
-        public void ShouldThrowExceptionIfServiceBusNotEnabled()
-        {
-            using (_mock.Record())
-            {
-                Expect.Call(() => _busSender.Publish(new ScheduleChangedEvent())).IgnoreArguments();
-            }
-            using (_mock.Playback())
-            {
-                _target.Handle(_denormalizeScheduleCommandDto);
-            }
-        }
-    }
+		[Test]
+		public void ShouldPublishEvent()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var scenario = ScenarioFactory.CreateScenarioWithId("d", true);
+			ScenarioRepository.Add(scenario);
+			var startDate = new DateTime(2018, 07, 24, 10, 0, 0);
+			var endDate = new DateTime(2018, 07, 25, 10, 0, 0);
+			var command = new DenormalizeScheduleCommandDto()
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				ScenarioId = scenario.Id.GetValueOrDefault(),
+				StartDateTime = startDate,
+				EndDateTime = endDate
+			};
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(1);
+			EventPopulatingPublisher.PublishedEvent.Count.Should().Be(1);
+		}
+
+		[Test]
+		public void ShouldNotPublishIfPersonNotFound()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			var command = new DenormalizeScheduleCommandDto() { PersonId = person.Id.GetValueOrDefault() };
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(0);
+		}
+
+		[Test]
+		public void ShouldPublishEventIfPersonFound()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var scenario = ScenarioFactory.CreateScenarioWithId("d", true);
+			ScenarioRepository.Add(scenario);
+			var startDate = new DateTime(2018, 07, 24, 10, 0, 0);
+			var endDate = new DateTime(2018, 07, 25, 10, 0, 0);
+			var command = new DenormalizeScheduleCommandDto()
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				ScenarioId = scenario.Id.GetValueOrDefault(),
+				StartDateTime = startDate,
+				EndDateTime = endDate
+			};
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(1);
+		}
+
+
+		[Test]
+		public void ShouldNotPublishEventIfScenarioIsInvalid()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var command = new DenormalizeScheduleCommandDto() { PersonId = person.Id.GetValueOrDefault(), ScenarioId = Guid.NewGuid() };
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(0);
+		}
+
+		[Test]
+		public void ShouldPublishEventIfScenarioIsValid()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var scenario = ScenarioFactory.CreateScenarioWithId("d", true);
+			ScenarioRepository.Add(scenario);
+			var startDate = new DateTime(2018, 07, 24, 10, 0, 0);
+			var endDate = new DateTime(2018, 07, 25, 10, 0, 0);
+			var command = new DenormalizeScheduleCommandDto()
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				ScenarioId = scenario.Id.GetValueOrDefault(),
+				StartDateTime = startDate,
+				EndDateTime = endDate
+			};
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(1);
+		}
+
+		[Test]
+		public void ShouldNotPublishIfDateIsEmpty()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var scenario = ScenarioFactory.CreateScenarioWithId("d", true);
+			ScenarioRepository.Add(scenario);
+			var command = new DenormalizeScheduleCommandDto() { PersonId = person.Id.GetValueOrDefault(), ScenarioId = scenario.Id.GetValueOrDefault() };
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(0);
+		}
+
+		[Test]
+		public void ShouldNotPublishIfDatePeriodIsInvalid()
+		{
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var scenario = ScenarioFactory.CreateScenarioWithId("d", true);
+			ScenarioRepository.Add(scenario);
+			var startDate = new DateTime(2018, 07, 25, 10, 0, 0);
+			var endDate = new DateTime(2018, 07, 24, 10, 0, 0);
+			var command = new DenormalizeScheduleCommandDto()
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				ScenarioId = scenario.Id.GetValueOrDefault(),
+				StartDateTime = startDate,
+				EndDateTime = endDate
+			};
+			Target.Handle(command);
+			command.Result.AffectedId.Should().Be.EqualTo(Guid.Empty);
+			command.Result.AffectedItems.Should().Be(0);
+		}
+	}
+
+
+	public class FakeEventPopulatingPublisherForTest : IEventPopulatingPublisher
+	{
+		public List<IEvent> PublishedEvent = new List<IEvent>();
+
+		public void Publish(params IEvent[] events)
+		{
+			PublishedEvent.AddRange(events);
+		}
+	}
 }
