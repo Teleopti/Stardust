039d516be24e1bc8d5cadd50157947e52f65b1bf
Mathias, Alina, Sabbir <Mathias_Alina_Sabbir_LevelUp@teleopti.com>
2018-08-01 10:31:06 +0200

    #74770 trying out stuff- organization picker with Angular (mocked data for now)

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical-overview/rta.historical.overview.module.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical-overview/rta.historical.overview.module.ts
@@ -1,88 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RtaHistoricalOverviewComponent} from "./rta.historical.overview.component";
 import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule,
-	MatBadgeModule,
-	MatBottomSheetModule,
-	MatButtonModule,
-	MatButtonToggleModule,
-	MatCardModule,
-	MatCheckboxModule,
-	MatChipsModule,
-	MatDatepickerModule,
-	MatDialogModule,
-	MatDividerModule,
-	MatExpansionModule,
-	MatGridListModule,
-	MatIconModule,
-	MatInputModule,
-	MatListModule,
-	MatMenuModule,
-	MatNativeDateModule,
-	MatPaginatorModule,
-	MatProgressBarModule,
-	MatProgressSpinnerModule,
-	MatRadioModule,
-	MatRippleModule,
-	MatSelectModule,
-	MatSidenavModule,
-	MatSliderModule,
-	MatSlideToggleModule,
-	MatSnackBarModule,
-	MatSortModule,
-	MatStepperModule,
-	MatTableModule,
-	MatTabsModule,
-	MatToolbarModule,
-	MatTooltipModule,
-	MatTreeModule} from '@angular/material'
+import {FormsModule} from "@angular/forms";
+import {SearchPipe} from "./search.pipe";
 
 @NgModule({
-	declarations: [
-		RtaHistoricalOverviewComponent
-	],
-	imports: [BrowserModule,
-		FormsModule,
-		ReactiveFormsModule,
-		MatAutocompleteModule,
-		MatBadgeModule,
-		MatBottomSheetModule,
-		MatButtonModule,
-		MatButtonToggleModule,
-		MatCardModule,
-		MatCheckboxModule,
-		MatChipsModule,
-		MatDatepickerModule,
-		MatDialogModule,
-		MatDividerModule,
-		MatExpansionModule,
-		MatGridListModule,
-		MatIconModule,
-		MatInputModule,
-		MatListModule,
-		MatMenuModule,
-		MatNativeDateModule,
-		MatPaginatorModule,
-		MatProgressBarModule,
-		MatProgressSpinnerModule,
-		MatRadioModule,
-		MatRippleModule,
-		MatSelectModule,
-		MatSidenavModule,
-		MatSliderModule,
-		MatSlideToggleModule,
-		MatSnackBarModule,
-		MatSortModule,
-		MatStepperModule,
-		MatTableModule,
-		MatTabsModule,
-		MatToolbarModule,
-		MatTooltipModule,
-		MatTreeModule],
-	entryComponents: [
-		RtaHistoricalOverviewComponent
-	]
+	declarations: [RtaHistoricalOverviewComponent,SearchPipe],
+	imports: [BrowserModule, FormsModule],
+	exports:[SearchPipe],
+	entryComponents: [RtaHistoricalOverviewComponent]
 })
 
 export class RtaHistoricalOverviewModule {
