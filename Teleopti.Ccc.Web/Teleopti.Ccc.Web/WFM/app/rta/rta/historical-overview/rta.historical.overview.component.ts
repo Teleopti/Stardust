b21086de4bca57a07a291c7038dadf9fba0a3462
Per, Alina, Sabbir <Per_Alina_Sabbir_LevelUp@teleopti.com>
2018-08-03 11:35:04 +0200

    #74770 basic usage of angularjs service in angular component

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical-overview/rta.historical.overview.component.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/historical-overview/rta.historical.overview.component.ts
@@ -1,16 +1,31 @@
-import {Component} from '@angular/core'
+import {Component, Inject, OnInit} from '@angular/core'
+import { RTA_SERVICE } from "./rtaService.provider";
 
 @Component({
 	selector: 'rta-historical-overview',
 	templateUrl: './rta.historical.overview.html'
 })
 
-export class RtaHistoricalOverviewComponent {
+export class RtaHistoricalOverviewComponent implements OnInit{
 	organizationPickerOpen: boolean;
 	organizationPickerSelectionText: string;
 	organizationPickerSearchTerm: string;
 	organizationPickerClearEnabled: string;
 	openCard: boolean;
+	sites: any [];
+	
+	constructor(
+		@Inject(RTA_SERVICE) private rtaService: any) {
+		console.log('in constructor');
+	}
+
+	ngOnInit(): void {
+	  this.rtaService.getOrganization().then(
+	  	(s)=> {
+			this.sites = s;
+		})
+	  //console.log(foo);
+	}	
 
 	clearOrganizationSelection() {
 		console.log('hej')
@@ -20,37 +35,37 @@ export class RtaHistoricalOverviewComponent {
 		this.organizationPickerOpen = false;
 	}
 
-	sites: any [] = [{
-		"Teams": [{
-			"Id": "94230b19-c23e-46c2-a500-a69600b7c715",
-			"Name": "Team Linda"
-		}, {"Id": "8aabe24f-cb06-40cf-9acc-a69600b70691", "Name": "Team Robert"}],
-		"Id": "d341c1ba-7079-46bb-a47c-a69600b5b585",
-		"Name": "Denver"
-	}, {
-		"Teams": [{"Id": "34590a63-6331-4921-bc9f-9b5e015ab495", "Name": "Team Preferences"}],
-		"Id": "d970a45a-90ff-4111-bfe1-9b5e015ab45c",
-		"Name": "London"
-	}, {
-		"Teams": [{
-			"Id": "fa169ee6-2d71-4d6c-bb6d-a69600b5c0bd",
-			"Name": "Team Angelica"
-		}, {"Id": "0f6d7644-1134-4561-97ca-a69600b6021d", "Name": "Team Joshua"}],
-		"Id": "6c48e01f-87a8-47c6-b27f-a69600b5982f",
-		"Name": "Manila"
-	}, {
-		"Teams": [{"Id": "0a1cdb27-bc01-4bb9-b0b3-9b5e015ab495", "Name": "Team 1"}],
-		"Id": "6a21c802-7a34-4917-8dfd-9b5e015ab461",
-		"Name": "Paris"
-	}, {
-		"Teams": [{
-			"Id": "04d552aa-b39e-4afc-81c2-a69600f2a59e",
-			"Name": "Team Gunnar"
-		}, {"Id": "589b1752-d391-476f-87f6-a69600f2ad11", "Name": "Team Therese"}],
-		"Id": "4400c969-a6f7-44f2-8200-a69600ec3310",
-		"Name": "Stockholm"
-	}];
-
+	// sites: any [] = [{
+	// 	"Teams": [{
+	// 		"Id": "94230b19-c23e-46c2-a500-a69600b7c715",
+	// 		"Name": "Team Linda"
+	// 	}, {"Id": "8aabe24f-cb06-40cf-9acc-a69600b70691", "Name": "Team Robert"}],
+	// 	"Id": "d341c1ba-7079-46bb-a47c-a69600b5b585",
+	// 	"Name": "Denver"
+	// }, {
+	// 	"Teams": [{"Id": "34590a63-6331-4921-bc9f-9b5e015ab495", "Name": "Team Preferences"}],
+	// 	"Id": "d970a45a-90ff-4111-bfe1-9b5e015ab45c",
+	// 	"Name": "London"
+	// }, {
+	// 	"Teams": [{
+	// 		"Id": "fa169ee6-2d71-4d6c-bb6d-a69600b5c0bd",
+	// 		"Name": "Team Angelica"
+	// 	}, {"Id": "0f6d7644-1134-4561-97ca-a69600b6021d", "Name": "Team Joshua"}],
+	// 	"Id": "6c48e01f-87a8-47c6-b27f-a69600b5982f",
+	// 	"Name": "Manila"
+	// }, {
+	// 	"Teams": [{"Id": "0a1cdb27-bc01-4bb9-b0b3-9b5e015ab495", "Name": "Team 1"}],
+	// 	"Id": "6a21c802-7a34-4917-8dfd-9b5e015ab461",
+	// 	"Name": "Paris"
+	// }, {
+	// 	"Teams": [{
+	// 		"Id": "04d552aa-b39e-4afc-81c2-a69600f2a59e",
+	// 		"Name": "Team Gunnar"
+	// 	}, {"Id": "589b1752-d391-476f-87f6-a69600f2ad11", "Name": "Team Therese"}],
+	// 	"Id": "4400c969-a6f7-44f2-8200-a69600ec3310",
+	// 	"Name": "Stockholm"
+	// }];
+    //
 
 
 	
@@ -426,6 +441,5 @@ export class RtaHistoricalOverviewComponent {
 			]
 		}
 	];
-
 }
 
