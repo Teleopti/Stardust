6da4de0ace5d2ae340f96e881eec08e3cc1a8ddb
Amanda Jonasson <amanda.jonasson@teleopti.com>
2018-08-14 20:38:31 +0000

    Merged PR 494: #76514 GetAllActivites
    #76514 GetAllActivites
    
    Related work items: #76514

---
+++ Teleopti.Wfm.Api/Query/ActivityDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Teleopti.Wfm.Api.Query
+{
+	public class ActivityDto
+	{
+		public string Name { get; set; }
+		public bool InReadyTime { get; set; }
+		public bool RequiresSkill { get; set; }
+		public bool InWorkTime { get; set; }
+		public bool InPaidTime { get; set; }
+		public string ReportLevelDetail { get; set; }
+		public bool RequiresSeat { get; set; }
+		public string PayrollCode { get; set; }
+		public bool AllowOverwrite { get; set; }
+		public bool IsOutboundActivity { get; set; }
+	}
+}
