f33d7cf4b1c85495bc71fa9fcfaeea03b3d9aa39
Kunning <kunning.mao@teleopti.com>
2018-08-14 11:25:32 +0200

    #76496 new scheduling and DO command handler handles day off optimization

--- Domain/Scheduling/SchedulingAndDayOffOptimization.cs
+++ Domain/Scheduling/SchedulingAndDayOffOptimization.cs
@@ -1,19 +1,23 @@
 ﻿using System;
+using Teleopti.Ccc.Domain.Optimization;
 
 namespace Teleopti.Ccc.Domain.Scheduling
 {
 	public class SchedulingAndDayOffOptimization
 	{
 		private readonly FullScheduling _fullScheduling;
+		private readonly DayOffOptimizationWeb _dayOffOptimizationWeb;
 
-		public SchedulingAndDayOffOptimization(FullScheduling fullScheduling)
+		public SchedulingAndDayOffOptimization(FullScheduling fullScheduling, DayOffOptimizationWeb dayOffOptimizationWeb)
 		{
 			_fullScheduling = fullScheduling;
+			_dayOffOptimizationWeb = dayOffOptimizationWeb;
 		}
 
 		public void Execute(Guid planningPeriodId)
 		{
 			_fullScheduling.DoScheduling(planningPeriodId);
+			_dayOffOptimizationWeb.Execute(planningPeriodId);
 		}
 	}
 }
