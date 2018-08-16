4f2642fca12028e21a433554df9a50368fd448a6
Emil Sundin <emil.sundin@teleopti.com>
2018-08-16 21:10:51 +0200

    Upgrade to Node 10.8 and npm 6.2.0
    npm 6.2.0 provides support for the "npm ci" command

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/NpmInstallWFM-Dev.ps1
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/NpmInstallWFM-Dev.ps1
@@ -9,7 +9,7 @@ Invoke-Expression .\NpmChecks.ps1
 if($depsChanged.Equals("ALL") -or $depsChanged.Equals("WFM")) {
     Write-Host "Installing npm deps"
     Set-Location $PSScriptRoot\..\WFM
-    Invoke-Expression "npm install"
+    Invoke-Expression "npm ci"
     Set-Location $PSScriptRoot
 } else {
     Write-Host "No need to install npm deps"
