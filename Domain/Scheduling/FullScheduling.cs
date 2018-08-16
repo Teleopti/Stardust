aa00b6e7d2ee15b07d0fe1c7ff1594dbb998f3b9
Roger Kratz <roger.kratz@gmail.com>
2018-08-16 10:27:26 +0200

    #76496 Don't load all schedules at use case start (no duplicates)

--- Domain/Scheduling/FullScheduling.cs
+++ Domain/Scheduling/FullScheduling.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.ResourcePlanner;
-using Teleopti.Ccc.Domain.InterfaceLegacy;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Optimization;
-using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
-using Teleopti.Ccc.Domain.Scheduling.WebLegacy;
+using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Scheduling
@@ -14,22 +13,19 @@ namespace Teleopti.Ccc.Domain.Scheduling
 	public class FullScheduling
 	{
 		private readonly SchedulingCommandHandler _schedulingCommandHandler;
-		private readonly FillSchedulerStateHolder _fillSchedulerStateHolder;
-		private readonly Func<ISchedulerStateHolder> _schedulerStateHolder;
+		private readonly IPersonRepository _personRepository;
 		private readonly SchedulingInformationProvider _schedulingInformationProvider;
 		private readonly ISchedulingOptionsProvider _schedulingOptionsProvider;
 		private readonly FullSchedulingResult _fullSchedulingResult;
 
 		public FullScheduling(SchedulingCommandHandler schedulingCommandHandler, 
-			FillSchedulerStateHolder fillSchedulerStateHolder,
-			Func<ISchedulerStateHolder> schedulerStateHolder,
+			IPersonRepository personRepository,
 			SchedulingInformationProvider schedulingInformationProvider,
 			ISchedulingOptionsProvider schedulingOptionsProvider, 
 			FullSchedulingResult fullSchedulingResult)
 		{
 			_schedulingCommandHandler = schedulingCommandHandler;
-			_fillSchedulerStateHolder = fillSchedulerStateHolder;
-			_schedulerStateHolder = schedulerStateHolder;
+			_personRepository = personRepository;
 			_schedulingInformationProvider = schedulingInformationProvider;
 			_schedulingOptionsProvider = schedulingOptionsProvider;
 			_fullSchedulingResult = fullSchedulingResult;
@@ -39,27 +35,26 @@ namespace Teleopti.Ccc.Domain.Scheduling
 		public FullSchedulingResultModel DoSchedulingAndDO(Guid planningPeriodId, bool runDayOffOptimization = true)
 		{			
 			var schedulingInformation = _schedulingInformationProvider.GetInfoFromPlanningPeriod(planningPeriodId);
-			var stateHolder = _schedulerStateHolder();
 			var schedulingOptions = _schedulingOptionsProvider.Fetch(null);
-			Setup(schedulingInformation.Period, schedulingInformation.PersonIds);
+			var agents = LoadAgents(schedulingInformation.Period, schedulingInformation.PersonIds).ToArray();
 			_schedulingCommandHandler.Execute(new SchedulingCommand
 			{
 				Period = schedulingInformation.Period,
 				RunWeeklyRestSolver = false,
 				FromWeb = true,
-				AgentsToSchedule = stateHolder.SchedulingResultState.LoadedAgents,
+				AgentsToSchedule = agents,
 				PlanningPeriodId = planningPeriodId,
 				RunDayOffOptimization = runDayOffOptimization
 			});
-			return _fullSchedulingResult.Create(schedulingInformation.Period, stateHolder.SchedulingResultState.LoadedAgents, schedulingInformation.PlanningGroup, schedulingOptions.UsePreferences);
-			
+			return _fullSchedulingResult.Create(schedulingInformation.Period, agents, schedulingInformation.PlanningGroup, schedulingOptions.UsePreferences);
 		}
 
 		[TestLog]
 		[UnitOfWork]
-		protected virtual void Setup(DateOnlyPeriod period, IEnumerable<Guid> people)
+		protected virtual IEnumerable<IPerson> LoadAgents(DateOnlyPeriod period, IEnumerable<Guid> people)
 		{
-			_fillSchedulerStateHolder.Fill(_schedulerStateHolder(), people, null, period);
+			var allPeople = _personRepository.FindAllAgentsLight(period);
+			return allPeople.Where(x => people.Contains(x.Id.Value));			
 		}
 	}
 }
