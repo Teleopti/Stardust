3fdfc9a7bc9ebbc022a7d602bafeadde1fa84180
Kunning <kunning.mao@teleopti.com>
2018-08-20 09:44:39 +0200

    #76496 separate scheduling and intradayOpti behavior tests

--- Teleopti.Ccc.TestCommon/TestData/Setups/Configurable/PlanningPeriodConfigurable.cs
+++ Teleopti.Ccc.TestCommon/TestData/Setups/Configurable/PlanningPeriodConfigurable.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.Domain.InterfaceLegacy;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Infrastructure.Repositories;
+using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.TestData.Core;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Default;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.TestCommon.TestData.Setups.Configurable
 {
@@ -17,6 +21,7 @@ namespace Teleopti.Ccc.TestCommon.TestData.Setups.Configurable
 		public DateTime Date { get; set; }
 		public string PlanningGroupName { get; set; }
 		public SchedulePeriodType? Type { get; set; }
+		public bool HasScheduled { get; set; }
 		
 		public void Apply (ICurrentUnitOfWork currentUnitOfWork)
 		{
@@ -36,6 +41,34 @@ namespace Teleopti.Ccc.TestCommon.TestData.Setups.Configurable
 			var planningPeriodSuggestions = new PlanningPeriodSuggestions (new MutableNow (Date),new[] {period});
 			
 			Period = new PlanningPeriod (planningPeriodSuggestions, planningGroup);
+
+			if (HasScheduled)
+			{
+				var personRepository = new PersonRepository(currentUnitOfWork);
+				var jobResult = new JobResult(JobCategory.WebSchedule, Period.Range, personRepository.LoadAll().FirstOrDefault(), DateTime.UtcNow)
+				{
+					FinishedOk = true
+				};
+				jobResult.AddDetail(new JobResultDetail(DetailLevel.Info, 
+					"{\"ScheduledAgentsCount\":1,\"BusinessRulesValidationResults\":[],\"SkillResultList\":[{\"SkillName\":\"Skill 1\",\"SkillDetails\":[{\"Date\":{\"Date\":\"2016-06-08T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2},{\"Date\":{\"Date\":\"2016-06-09T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2},{\"Date\":{\"Date\":\"2016-06-10T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2},{\"Date\":{\"Date\":\"2016-06-11T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2},{\"Date\":{\"Date\":\"2016-06-12T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2},{\"Date\":{\"Date\":\"2016-06-13T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2},{\"Date\":{\"Date\":\"2016-06-14T00:00:00\"},\"RelativeDifference\":-0.82222222222222219,\"ColorId\":2}]}]}",
+					DateTime.UtcNow, null));
+				new JobResultRepository(currentUnitOfWork).Add(jobResult);
+				Period.JobResults.Add(jobResult);
+				var scenario = DefaultScenario.Scenario;
+				var activity = new ActivityRepository(currentUnitOfWork).LoadAll().FirstOrDefault();
+				var perons = personRepository.FindPeopleInPlanningGroup(planningGroup, Period.Range);
+				var personAssignmentRepository = new PersonAssignmentRepository(currentUnitOfWork);
+				foreach (var person in perons)
+				{
+					foreach (var date in Period.Range.DayCollection())
+					{
+						var startTimeUtc = person.PermissionInformation.DefaultTimeZone().SafeConvertTimeToUtc(date.Date.AddHours(8));
+						var endTimeUtc = person.PermissionInformation.DefaultTimeZone().SafeConvertTimeToUtc(date.Date.AddHours(16));
+						var assignment = PersonAssignmentFactory.CreateAssignmentWithPersonalShift(person, scenario, activity, new DateTimePeriod(startTimeUtc, endTimeUtc));
+						personAssignmentRepository.Add(assignment);
+					}
+				}
+			}
 
 			new PlanningPeriodRepository(currentUnitOfWork).Add(Period);
 		}
