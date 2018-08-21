2d55e5b924faf9d4542ee61e1838d0fa266b463a
Aidin <aidin.dadashzadeh@teleopti.com>
2018-08-21 14:38:20 +0200

    Making behaviour test

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Authentication/Authentication.feature
@@ -0,0 +1,21 @@
+﻿@WFM
+Feature: Authentication
+	As an agent
+	I want to be able to change my password
+
+Background: 
+	Given I am an agent with password 'tdemo'
+	And I have a role with full access
+	And I view people
+
+
+@OnlyRunIfEnabled('Wfm_Authentication_ChangePasswordMenu_76666')
+Scenario: Password changes when changing password
+	Given I navigate to change your password
+	And I enter 'tdemo' in 'data-test-input-current-password'
+	And I enter 'newPassword' in 'data-test-input-new-password'
+	And I enter 'newPassword' in 'data-test-input-confirm-password'
+	When I click Ok
+	Then The password modal should close
+	
+
---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Wfm/Authentication/AuthenticationStepDefinition.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Teleopti.Ccc.Domain.AgentInfo;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Configurable;
+using Teleopti.Ccc.TestCommon.TestData.Setups.Default;
+using Teleopti.Ccc.Web.Areas.MultiTenancy.Core;
+using Teleopti.Ccc.WebBehaviorTest.Bindings.DoNotUse;
+using Teleopti.Ccc.WebBehaviorTest.Core;
+using Teleopti.Ccc.WebBehaviorTest.Data;
+using Teleopti.Ccc.WebBehaviorTest.Data.Setups.Configurable;
+
+namespace Teleopti.Ccc.WebBehaviorTest.Wfm.Authentication
+{
+	[Binding]
+	public sealed class AuthenticationStepDefinition
+	{
+
+		[Given("I am an agent with password '(.*)'")]
+		public void IAmAnAgentWithPassword(string password)
+		{
+			DataMaker.Data().RemoveLastPerson();
+			DataMaker.Data().Person("agent")
+				.Apply(new PersonUserConfigurable
+				{
+					UserName = "temp",
+					Password = password
+				});
+		}
+
+		[Given("I navigate to change your password")]
+		public void INavigateToChangeYourPassword()
+		{
+			Browser.Interactions.AssertExists("[data-test-nav-item-settings]");
+			Browser.Interactions.PressEnter("[data-test-nav-item-settings]");
+			Browser.Interactions.AssertExists("[data-test-change-password-button]");
+			Browser.Interactions.PressEnter("[data-test-change-password-button]");
+		}
+
+		[Given("I enter '(.*)' in '(.*)'")]
+		public void IEnterXInInputY(string password, string selector)
+		{
+			Browser.Interactions.AssertExists("[" +  selector + "]");
+			Browser.Interactions.FillWith("[" +  selector + "]", password);
+		}
+
+		[When("I click Ok")]
+		public void IClickOk()
+		{
+			Browser.Interactions.AssertExists("[data-test-change-password-modal] .ant-btn-primary");
+			Browser.Interactions.PressEnter("[data-test-change-password-modal] .ant-btn-primary");
+		}
+
+		[Then("The password modal should close")]
+		public void ThePasswordModalShouldClose()
+		{
+			Browser.Interactions.AssertJavascriptResultContains(@"
+				return Array.from(
+					document.querySelector('[data-test-change-password-modal]')
+					.getAttribute('ng-reflect-nz-visible')
+				", "false");
+		}
+		
+	}
+}
