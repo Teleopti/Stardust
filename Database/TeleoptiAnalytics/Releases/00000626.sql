91bae314f1cf5f6d6068c6c733ac9d3a634aba91
Xinfeng Li <xinfeng.li@teleopti.com>
2018-08-23 16:08:35 +0200

    PBI #75767: Fill tenant name column on update

--- Database/TeleoptiAnalytics/Releases/00000626.sql
+++ Database/TeleoptiAnalytics/Releases/00000626.sql
@@ -1,2 +1,13 @@
 ALTER TABLE [mart].[etl_job_execution]
 ADD [tenant_name] nvarchar(255) null
+
+GO
+
+UPDATE history
+   SET history.tenant_name = schedule.tenant_name
+  FROM [mart].[etl_job_execution] history
+  JOIN [mart].[etl_job_schedule] schedule
+    ON history.schedule_id = schedule.schedule_id
+ WHERE history.schedule_id > 0
+
+GO
