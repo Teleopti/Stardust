f1784b8223b0dbb2018de79a5d27b3010dabc3aa
Roger Kratz <roger.kratz@gmail.com>
2018-08-27 16:35:02 +0200

    #76496 Adding retry logic around storing hangfire events (for BackgroundJobClientException)

---
+++ InfrastructureTest/Hangfire/HangfireRetryEnqueuingTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.States;
+using log4net;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.ApplicationLayer.Events;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Infrastructure.ApplicationLayer;
+using Teleopti.Ccc.Infrastructure.Hangfire;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
+
+namespace Teleopti.Ccc.InfrastructureTest.Hangfire
+{
+	[DomainTest]
+	[Toggle(Toggles.ResourcePlanner_XXL_76496)]
+	public class HangfireRetryEnqueuingTest : IIsolateSystem
+	{
+		public HangfireEventPublisher Target;
+		public backgroundJobClientThatThrows BackgroundJobClient;
+
+		[TestCase(HangfireEventClientWithRetry.NumberOfRetries, ExpectedResult = true)]
+		[TestCase(HangfireEventClientWithRetry.NumberOfRetries + 1, ExpectedResult = false)]
+		[TestCase(HangfireEventClientWithRetry.NumberOfRetries -1, ExpectedResult = true)]
+		public bool ShouldRetryWhenBackgroundJobClientExceptionTest(int numberOfTimesHangfireThrows)
+		{
+			BackgroundJobClient.NumberOfTimesToThrow = numberOfTimesHangfireThrows;
+
+			try
+			{
+				Target.Publish(new ScheduleChangedEvent());			
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		[Test]
+		public void NonBackgroundJobClientExceptionShouldBubbleUp()
+		{
+			BackgroundJobClient.NumberOfTimesToThrow = 1;
+			BackgroundJobClient.ExceptionToThrow = new Exception();
+
+			try
+			{
+				Target.Publish(new ScheduleChangedEvent());
+			}
+			catch (Exception e)
+			{
+				e.Should().Be.SameInstanceAs(BackgroundJobClient.ExceptionToThrow);
+				return;
+			}
+			Assert.Fail("No exception was thrown");
+		}
+
+		public void Isolate(IIsolate isolate)
+		{
+			isolate.UseTestDouble<backgroundJobClientThatThrows>().For<IBackgroundJobClient>();
+		}
+
+		public class backgroundJobClientThatThrows : IBackgroundJobClient
+		{
+			public int NumberOfTimesToThrow { get; set; }
+			public Exception ExceptionToThrow { get; set; } = new BackgroundJobClientException("_", null); 
+
+			private int _numberOfTimesIHaveThrown = 0;
+			
+			public string Create(Job job, IState state)
+			{
+				if (NumberOfTimesToThrow > _numberOfTimesIHaveThrown++)
+					throw ExceptionToThrow;
+
+				return string.Empty;
+			}
+
+			public bool ChangeState(string jobId, IState state, string expectedState)
+			{
+				return false;
+			}
+		}
+	}
+}
