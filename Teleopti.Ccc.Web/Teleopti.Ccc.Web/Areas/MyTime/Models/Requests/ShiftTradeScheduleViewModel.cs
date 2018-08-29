ad3fcd249b10436abf2509d5f8c6d6752036d6fb
Mingdi Wu <Mingdi.Wu@teleopti.com>
2018-08-29 04:19:25 +0000

    Merged PR 731: define interface
    define interface

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/Requests/ShiftTradeScheduleViewModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Models/Requests/ShiftTradeScheduleViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Web.Areas.MyTime.Models.Requests
 {
@@ -19,9 +21,20 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Models.Requests
 		public IEnumerable<ShiftTradeMultiScheduleViewModel> MultiSchedulesForShiftTrade { get; set; }
 	}
 
+	public class ContractTimeInfoViewModel
+	{
+		public DateOnly PeriodStart { get; set; }
+		public DateOnly PeriodEnd { get; set; }
+		public int ContractTimeMinutes { get; set; }
+		public int ToleranceLackMinutes { get; set; }
+		public int ToleranceOverMinutes { get; set; }
+	}
+
 	public class ShiftTradeToleranceInfoViewModel
 	{
 		public bool IsNeedToCheck { get; set; }
+		private IList<ContractTimeInfoViewModel> MyInfos { get; set; }
+		private IList<ContractTimeInfoViewModel> PersonToInfos { get; set; }
 		public int MyToleranceMinutes { get; set; }
 		public int PersonToToleranceMInutes { get; set; }
 	}
