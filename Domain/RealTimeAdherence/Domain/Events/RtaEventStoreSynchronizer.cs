4cd109f15ac0891006017287b8f23b0a4447f22e
Alina_Per_Sabbir_Mathias <Alina_Per_Sabbir_Mathias_LevelUp@teleopti.com>
2018-09-03 16:52:32 +0200

    #74770 wip syncronizer should not use loader?

--- Domain/RealTimeAdherence/Domain/Events/RtaEventStoreSynchronizer.cs
+++ Domain/RealTimeAdherence/Domain/Events/RtaEventStoreSynchronizer.cs
@@ -1,8 +1,15 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Dynamic;
+using System.Linq;
 using System.Text.RegularExpressions;
+using Teleopti.Ccc.Domain.Cascading;
 using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.RealTimeAdherence.ApplicationLayer.ReadModels;
 using Teleopti.Ccc.Domain.RealTimeAdherence.Domain.AgentAdherenceDay;
+using Teleopti.Ccc.Domain.RealTimeAdherence.Domain.Service;
 using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.RealTimeAdherence.Domain.Events
@@ -34,6 +41,51 @@ namespace Teleopti.Ccc.Domain.RealTimeAdherence.Domain.Events
 			_readModels = readModels;
 			_adherenceDayLoader = adherenceDayLoader;
 		}
+
+
+		public void syncfoo()
+		{
+			//truncate all
+			
+			var events = _events.LoadAll();
+			var bigRM = new List<HistoricalOverviewReadModel>();
+
+			var eventsForPerson = from e in events
+				group e by (e as IRtaStoredEvent).QueryData().PersonId
+				into eventsGroupedOnPerson
+				select eventsGroupedOnPerson;
+			
+			eventsForPerson.ForEach(x => bigRM.AddRange(AddAllDaysForPerson(x)));
+
+			//BatchInsert(rm);
+
+
+		}
+
+		private IEnumerable<HistoricalOverviewReadModel> AddAllDaysForPerson(IGrouping<Guid?, IEvent> grouping)
+		{
+			var rmPerPerson = new List<HistoricalOverviewReadModel>();
+
+			
+			grouping.ForEach(g =>
+			{
+				var storedEvent = g as IRtaStoredEvent;
+				var typeOfEvent = g?.GetType().FullName;
+				var lateForWork = typeOfEvent == typeof(PersonArrivedLateForWorkEvent).Assembly.GetName().Name;
+//				var lateForWorkText = lateForWork ? string.Format(UserTexts.Resources.LateXMinutes, Math.Round(new DateTimePeriod(storedEvent.ShiftStart, storedEvent.Timestamp).ElapsedTime().TotalMinutes)) : "0";
+				
+				rmPerPerson.Add(new HistoricalOverviewReadModel()
+				{
+					PersonId = storedEvent.QueryData().PersonId.Value,
+					Date = storedEvent.QueryData().StartTime.Value.ToDateOnly(),
+					WasLateForWork = lateForWork
+				});
+				
+			});
+
+			return rmPerPerson;
+		}
+
 
 		public void Synchronize()
 		{
