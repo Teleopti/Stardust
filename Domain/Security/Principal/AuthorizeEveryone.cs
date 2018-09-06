83d533998d2aa8883cd2471a79992459adfc4cb2
Feng Gao <Feng.Gao@teleopti.com>
2018-09-06 05:16:49 +0000

    Merged PR 778: formatting to trigger build
    formatting to trigger build

--- Domain/Security/Principal/AuthorizeEveryone.cs
+++ Domain/Security/Principal/AuthorizeEveryone.cs
@@ -3,40 +3,40 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Security.Principal
 {
-    public class AuthorizeEveryone : IAuthorizeAvailableData
-    {
-        public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, IPerson person)
-        {
-            return true;
-        }
+	public class AuthorizeEveryone : IAuthorizeAvailableData
+	{
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, IPerson person)
+		{
+			return true;
+		}
 
-        public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ITeam team)
-        {
-            return true;
-        }
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ITeam team)
+		{
+			return true;
+		}
 
-        public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ISite site)
-        {
-            return true;
-        }
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ISite site)
+		{
+			return true;
+		}
 
-        public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, IBusinessUnit businessUnit)
-        {
-            return true;
-        }
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, IBusinessUnit businessUnit)
+		{
+			return true;
+		}
 
-    	public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, IPersonAuthorization authorization)
-    	{
-    		return true;
-    	}
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, IPersonAuthorization authorization)
+		{
+			return true;
+		}
 
-	    public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ITeamAuthorization authorization)
-	    {
-		    return true;
-	    }
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ITeamAuthorization authorization)
+		{
+			return true;
+		}
 
-	    public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ISiteAuthorization authorization)
-	    {
+		public bool Check(IOrganisationMembership queryingPerson, DateOnly dateOnly, ISiteAuthorization authorization)
+		{
 			return true;
 		}
 	}
