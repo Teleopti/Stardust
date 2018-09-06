015c6737db725edfb0bcfbd91eb9bac6f22d58f7
Alina_Per_Sabbir_Mathias <Alina_Per_Sabbir_Mathias_LevelUp@teleopti.com>
2018-09-06 13:48:21 +0200

    #74770 synchronizer infra working

---
+++ Domain/RealTimeAdherence/Domain/AgentAdherenceDay/IScheduleLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.RealTimeAdherence.Domain.AgentAdherenceDay
+{
+	public interface IScheduleLoader
+	{
+		IEnumerable<IVisualLayer> Load(Guid personId, DateOnly date);
+	}
+}
