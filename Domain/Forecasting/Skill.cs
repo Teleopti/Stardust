a5d733179cc2eebb3d5e214632dd0e39934f0acd
Roger Kratz <roger.kratz@gmail.com>
2018-09-10 13:56:51 +0200

    Revert "Moving skillextensions to a seperate file"
    This reverts commit 0b6c626

--- Domain/Forecasting/Skill.cs
+++ Domain/Forecasting/Skill.cs
@@ -14,6 +14,38 @@ using Teleopti.Interfaces.Domain;
 
 namespace Teleopti.Ccc.Domain.Forecasting
 {
+	public static class SkillExtensions
+	{
+		public static bool HasSameOpenHours(this IEnumerable<ISkill> skills)
+		{
+			//now returns true only if openhourlist is exactly the same. Could be "optimized" and look at _actual_ open hours intead...
+			
+			IEnumerable<TimePeriod> openHourList=null;
+			foreach (var skill in skills)
+			{
+				foreach (var workload in skill.WorkloadCollection)
+				{
+					foreach (var dayTemplate in workload.TemplateWeekCollection.Values)
+					{
+						if (openHourList == null)
+						{
+							openHourList = dayTemplate.OpenHourList;
+						}
+						else
+						{
+							if (!openHourList.SequenceEqual(dayTemplate.OpenHourList))
+							{
+								return false;
+							}
+						}
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+	
 	[RemoveMeWithToggle(Toggles.ResourcePlanner_HideSkillPrioSliders_41312)]
 	public interface ISkillPriority
 	{
