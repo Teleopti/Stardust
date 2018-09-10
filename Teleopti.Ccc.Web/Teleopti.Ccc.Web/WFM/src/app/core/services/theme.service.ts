313fe480228d94500a095acbb6eb5c72cb3292b6
Emil Sundin <emil.sundin@teleopti.com>
2018-09-10 17:26:16 +0200

    Save theme pref locally

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/core/services/theme.service.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/core/services/theme.service.ts
@@ -7,16 +7,22 @@ export interface Theme {
 	readonly Overlay: boolean;
 }
 
-type ThemeType = 'classic' | 'dark' | '';
+type ThemeType = 'classic' | 'dark';
 
 @Injectable()
 export class ThemeService {
 	private theme$ = new ReplaySubject<Theme>(1);
 
 	constructor(private http: HttpClient) {
+		const themeFromBrowser = (localStorage.getItem('theme') || 'classic') as ThemeType;
+		this.theme$.next({
+			Name: themeFromBrowser,
+			Overlay: false
+		});
 		this.http.get('../api/Theme').subscribe({
 			next: (theme: Theme) => {
 				this.theme$.next(theme);
+				this.saveLocally(theme);
 				this.applyTheme(theme.Name);
 			}
 		});
@@ -26,25 +32,25 @@ export class ThemeService {
 		return this.theme$;
 	}
 
+	saveLocally(theme: Theme) {
+		localStorage.setItem('theme', theme.Name);
+	}
+
 	saveThemePreference(theme: Theme): void {
+		this.saveLocally(theme);
 		this.theme$.next(theme);
 		this.http.post('../api/Theme/Change', theme).subscribe();
 	}
 
 	// This can be made platform agnostic
 	applyTheme(themeToApply: ThemeType) {
-		// this.theme$.subscribe({
-		// next: theme => {
-		// const themeStyle = theme.Name;
-		// if (themeStyle != themeToApply) {
-		const waitForThemes = Promise.all([
-			this.applyAngularMatrialTheme(themeToApply),
-			this.applyStyleguideTheme(themeToApply),
-			this.applyAntTheme(themeToApply)
-		]);
-		// }
-		// }
-		// });
+		if (this.getCurrentTheme() !== themeToApply) {
+			const waitForThemes = Promise.all([
+				this.applyAngularMatrialTheme(themeToApply),
+				this.applyStyleguideTheme(themeToApply),
+				this.applyAntTheme(themeToApply)
+			]);
+		}
 	}
 
 	async applyAngularMatrialTheme(theme: ThemeType) {
@@ -92,7 +98,7 @@ export class ThemeService {
 		} else if (classList.contains('angular-theme-classic')) {
 			return 'classic';
 		} else {
-			return '';
+			return null;
 		}
 	}
 }
