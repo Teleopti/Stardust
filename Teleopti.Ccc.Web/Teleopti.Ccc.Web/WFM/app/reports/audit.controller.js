e3baaadf10c5dcca1c480faca50840b1d21462cf
Magnus Ekström <magnus.ekstrom@teleopti.com>
2018-09-10 16:27:01 +0200

    Bug #76634: Scroll Bar doesn't work in Schedule Audit Trail

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/reports/audit.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/reports/audit.controller.js
@@ -1,13 +1,31 @@
-(function () {
+(function() {
 	'use strict';
 
-	angular
-	.module('wfm.reports')
-	.controller('AuditTrailController', AuditTrailController);
+	angular.module('wfm.reports').controller('AuditTrailController', AuditTrailController);
 
-	AuditTrailController.$inject = ['$filter', 'Toggle', 'uiGridConstants', 'ReportsService', 'NoticeService', '$translate', 'localeLanguageSortingService', 'CurrentUserInfo', 'i18nService'];
+	AuditTrailController.$inject = [
+		'$filter',
+		'Toggle',
+		'uiGridConstants',
+		'ReportsService',
+		'NoticeService',
+		'$translate',
+		'localeLanguageSortingService',
+		'CurrentUserInfo',
+		'i18nService'
+	];
 
-	function AuditTrailController($filter, ToggleSvc, uiGridConstants, ReportsService, NoticeService, $translate, localeLanguageSortingService, CurrentUserInfo, i18nService) {
+	function AuditTrailController(
+		$filter,
+		ToggleSvc,
+		uiGridConstants,
+		ReportsService,
+		NoticeService,
+		$translate,
+		localeLanguageSortingService,
+		CurrentUserInfo,
+		i18nService
+	) {
 		var vm = this;
 
 		var local = CurrentUserInfo.CurrentUserInfo().DateFormatLocale;
@@ -19,11 +37,15 @@
 		vm.gridOptions = {};
 		vm.dateRangeCustomValidators = [];
 		vm.dateChangeRange = {
-			startDate: moment().locale(JSON.stringify(local)).subtract(1, 'days'),
+			startDate: moment()
+				.locale(JSON.stringify(local))
+				.subtract(1, 'days'),
 			endDate: moment().locale(JSON.stringify(local))
 		};
 		vm.dateModifyRange = {
-			startDate: moment().locale(JSON.stringify(local)).subtract(1, 'days'),
+			startDate: moment()
+				.locale(JSON.stringify(local))
+				.subtract(1, 'days'),
 			endDate: moment().locale(JSON.stringify(local))
 		};
 		vm.changesData = [];
@@ -31,11 +53,11 @@
 		vm.filteredOrgData = [];
 		vm.orgData = {};
 		vm.option = {
-			NodeDisplayName: "Name",
-			NodeChildrenName: "Children",
-			NodeSelectedMark: "selected",
+			NodeDisplayName: 'Name',
+			NodeChildrenName: 'Children',
+			NodeSelectedMark: 'selected',
 			DisplayTreeFilter: true
-		}
+		};
 
 		vm.sendForm = sendForm;
 		vm.refreshData = refreshData;
@@ -51,25 +73,30 @@
 		}
 
 		function getChangedBy() {
-			ReportsService.getAuditTrailChangedByPerson.query().$promise.then(function (result) {
+			ReportsService.getAuditTrailChangedByPerson.query().$promise.then(function(result) {
 				result.push({
-					Id:'',
+					Id: '',
 					Name: $translate.instant('Everyone'),
 					Default: true
-				})
+				});
 				vm.changedBy = result;
 			});
 		}
 
 		function getOrgData() {
-			if (angular.isUndefined(vm.dateModifyRange) || !vm.dateModifyRange.startDate || !vm.dateModifyRange.endDate || vm.dateModifyRange.startDate > vm.dateModifyRange.endDate)  {
+			if (
+				angular.isUndefined(vm.dateModifyRange) ||
+				!vm.dateModifyRange.startDate ||
+				!vm.dateModifyRange.endDate ||
+				vm.dateModifyRange.startDate > vm.dateModifyRange.endDate
+			) {
 				return;
 			}
 			var postObj = {
-				startDate: moment(vm.dateModifyRange.startDate).format("YYYY-MM-DD"),
-				endDate: moment(vm.dateModifyRange.endDate).format("YYYY-MM-DD"),
-			}
-			ReportsService.getOrganization.org(postObj).$promise.then(function (response) {
+				startDate: moment(vm.dateModifyRange.startDate).format('YYYY-MM-DD'),
+				endDate: moment(vm.dateModifyRange.endDate).format('YYYY-MM-DD')
+			};
+			ReportsService.getOrganization.org(postObj).$promise.then(function(response) {
 				vm.filteredOrgData = [];
 				updateLabel();
 				vm.orgData = {
@@ -85,7 +112,7 @@
 		}
 
 		function updateLabel() {
-			vm.label = $translate.instant('SeveralTeamsSelected').replace('{0}', vm.filteredOrgData.length );
+			vm.label = $translate.instant('SeveralTeamsSelected').replace('{0}', vm.filteredOrgData.length);
 		}
 
 		function sendForm(form) {
@@ -97,95 +124,114 @@
 				angular.isUndefined(vm.dateModifyRange) ||
 				!vm.dateModifyRange.startDate ||
 				!vm.dateModifyRange.endDate ||
-				vm.dateModifyRange.startDate > vm.dateModifyRange.endDate)  {
-					NoticeService.warning($translate.instant('InvalidEndDate').replace('{0}', vm.maxResults), 5000, true);
-					return;
-				}
-
-				vm.postError = false;
-
-				var postObj =	{
-					ChangedByPersonId: form.drop.Id,
-					ChangesOccurredStartDate: moment(vm.dateChangeRange.startDate).format("YYYY-MM-DD"),
-					ChangesOccurredEndDate: moment(vm.dateChangeRange.endDate).format("YYYY-MM-DD"),
-					AffectedPeriodStartDate: moment(vm.dateModifyRange.startDate).format("YYYY-MM-DD"),
-					AffectedPeriodEndDate: moment(vm.dateModifyRange.endDate).format("YYYY-MM-DD"),
-					MaximumResults: vm.maxResults,
-					TeamIds: vm.filteredOrgData
-				};
-				vm.loading = true;
-				ReportsService.getAuditTrailResult.searching(postObj).$promise.then(function (response) {
-					vm.loading = false;
-					if (response.length < 1) {
-						NoticeService.warning($translate.instant('noSearchResults'), 5000, true);
-						return;
-					}
-					else {
-						vm.changesData = response;
-						generateTable();
-
-						if (response.length >= vm.maxResults) {
-							NoticeService.info($translate.instant('MaxResultsReached').replace('{0}', vm.maxResults), 5000, true);
-						}
-					}
-				});
+				vm.dateModifyRange.startDate > vm.dateModifyRange.endDate
+			) {
+				NoticeService.warning($translate.instant('InvalidEndDate').replace('{0}', vm.maxResults), 5000, true);
+				return;
 			}
 
-			function generateTable() {
-				vm.gridOptions = {
-					exporterCsvFilename: 'audit-trail.csv',
-					exporterOlderExcelCompatibility: true,
-					exporterMenuPdf: false,
-					enableSelectAll: false,
-					enableFullRowSelection: false,
-					enableRowHeaderSelection: false,
-					enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
-					selectionRowHeaderWidth: 35,
-					data: vm.changesData,
-					enableGridMenu: true,
-					columnDefs: [
-						{field: 'ModifiedAt', displayName: $translate.instant('ModifiedAt'), type: 'date', cellFilter: 'date:"dd-MM-yyyy HH:mm"', sort: { direction: 'desc', priority: 0 } },
-						{field: 'ModifiedBy', displayName: $translate.instant('ModifiedBy')},
-						{field: 'ScheduledAgent', displayName: $translate.instant('ScheduledAgent')},
-						{field: 'ShiftType', displayName: $translate.instant('Type')},
-						{field: 'AuditType', displayName: $translate.instant('Action')},
-						{field: 'Detail', displayName: $translate.instant('Details')},
-						{field: 'ScheduleStart', displayName: $translate.instant('StartTime'), type: 'date', cellFilter: 'date:"dd-MM-yyyy HH:mm"' },
-						{field: 'ScheduleEnd', displayName: $translate.instant('FullDayAbsenceReqEndTime'), type: 'date', cellFilter: 'date:"dd-MM-yyyy HH:mm"' }
-					]
-				};
-				var defaultGridMenu = i18nService.get('en');
-				defaultGridMenu.gridMenu.exporterAllAsCsv = $translate.instant('UIGridExportAllCSV');
-				defaultGridMenu.gridMenu.columns = $translate.instant('UIGridColumnsColon');
-				defaultGridMenu.gridMenu.exporterVisibleAsCsv = $translate.instant('UIGridExportVisibleCSV');
-				defaultGridMenu.gridMenu.exporterSelectedAsCsv = $translate.instant('UIGridExportSelectedCSV');
-				defaultGridMenu.sort.ascending = $translate.instant('UIGridSortAscending');
-				defaultGridMenu.sort.descending = $translate.instant('UIGridSortDescending');
-				defaultGridMenu.sort.remove = $translate.instant('UIGridNoSort');
-				defaultGridMenu.column.hide = $translate.instant('UIGridHideCol');
+			vm.postError = false;
 
-				vm.chartLoaded = true;
-			}
-
-			function calculateOrgSelection(nodes) {
-				for (var i = 0; i < nodes.length; i++) {
-					if (angular.isUndefined(nodes[i].Children)) {
-						if (vm.filteredOrgData.indexOf(nodes[i].Id) == -1 && nodes[i].selected === true) {
-							vm.filteredOrgData.push(nodes[i].Id)
-						}
-						else if(vm.filteredOrgData.indexOf(nodes[i].Id) !== -1 && nodes[i].selected === false){
-							vm.filteredOrgData.splice(vm.filteredOrgData.indexOf(nodes[i].Id), 1)
-						}
-					}
-					else if (nodes[i].Children){
-						calculateOrgSelection(nodes[i].Children);
-					}
-				}
-				updateLabel();
-			}
-
-			function refreshData(keyword) {
-				vm.gridOptions.data = $filter('filter')(vm.changesData, keyword);
+			var postObj = {
+				ChangedByPersonId: form.drop.Id,
+				ChangesOccurredStartDate: moment(vm.dateChangeRange.startDate).format('YYYY-MM-DD'),
+				ChangesOccurredEndDate: moment(vm.dateChangeRange.endDate).format('YYYY-MM-DD'),
+				AffectedPeriodStartDate: moment(vm.dateModifyRange.startDate).format('YYYY-MM-DD'),
+				AffectedPeriodEndDate: moment(vm.dateModifyRange.endDate).format('YYYY-MM-DD'),
+				MaximumResults: vm.maxResults,
+				TeamIds: vm.filteredOrgData
 			};
+			vm.loading = true;
+			ReportsService.getAuditTrailResult.searching(postObj).$promise.then(function(response) {
+				vm.loading = false;
+				if (response.length < 1) {
+					NoticeService.warning($translate.instant('noSearchResults'), 5000, true);
+					return;
+				} else {
+					vm.changesData = response;
+					generateTable();
+
+					if (response.length >= vm.maxResults) {
+						NoticeService.info(
+							$translate.instant('MaxResultsReached').replace('{0}', vm.maxResults),
+							5000,
+							true
+						);
+					}
+				}
+			});
 		}
-	})();
+
+		function generateTable() {
+			vm.gridOptions = {
+				exporterCsvFilename: 'audit-trail.csv',
+				exporterOlderExcelCompatibility: true,
+				exporterMenuPdf: false,
+				enableSelectAll: false,
+				enableFullRowSelection: false,
+				enableRowHeaderSelection: false,
+				enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
+				selectionRowHeaderWidth: 35,
+				data: vm.changesData,
+				enableGridMenu: true,
+				columnDefs: [
+					{
+						field: 'ModifiedAt',
+						displayName: $translate.instant('ModifiedAt'),
+						type: 'date',
+						cellFilter: 'date:"dd-MM-yyyy HH:mm"',
+						sort: { direction: 'desc', priority: 0 }
+					},
+					{ field: 'ModifiedBy', displayName: $translate.instant('ModifiedBy') },
+					{ field: 'ScheduledAgent', displayName: $translate.instant('ScheduledAgent') },
+					{ field: 'ShiftType', displayName: $translate.instant('Type') },
+					{ field: 'AuditType', displayName: $translate.instant('Action') },
+					{ field: 'Detail', displayName: $translate.instant('Details') },
+					{
+						field: 'ScheduleStart',
+						displayName: $translate.instant('StartTime'),
+						type: 'date',
+						cellFilter: 'date:"dd-MM-yyyy HH:mm"'
+					},
+					{
+						field: 'ScheduleEnd',
+						displayName: $translate.instant('FullDayAbsenceReqEndTime'),
+						type: 'date',
+						cellFilter: 'date:"dd-MM-yyyy HH:mm"'
+					}
+				]
+			};
+			var defaultGridMenu = i18nService.get('en');
+			defaultGridMenu.gridMenu.exporterAllAsCsv = $translate.instant('UIGridExportAllCSV');
+			defaultGridMenu.gridMenu.columns = $translate.instant('UIGridColumnsColon');
+			defaultGridMenu.gridMenu.exporterVisibleAsCsv = $translate.instant('UIGridExportVisibleCSV');
+			defaultGridMenu.gridMenu.exporterSelectedAsCsv = $translate.instant('UIGridExportSelectedCSV');
+			defaultGridMenu.sort.ascending = $translate.instant('UIGridSortAscending');
+			defaultGridMenu.sort.descending = $translate.instant('UIGridSortDescending');
+			defaultGridMenu.sort.remove = $translate.instant('UIGridNoSort');
+			defaultGridMenu.column.hide = $translate.instant('UIGridHideCol');
+
+			vm.chartLoaded = true;
+		}
+
+		function calculateOrgSelection(nodes) {
+			for (var i = 0; i < nodes.length; i++) {
+				if (angular.isUndefined(nodes[i].Children)) {
+					if (vm.filteredOrgData.indexOf(nodes[i].Id) == -1 && nodes[i].selected === true) {
+						vm.filteredOrgData.push(nodes[i].Id);
+					} else if (vm.filteredOrgData.indexOf(nodes[i].Id) !== -1 && nodes[i].selected === false) {
+						vm.filteredOrgData.splice(vm.filteredOrgData.indexOf(nodes[i].Id), 1);
+					}
+				} else if (nodes[i].Children) {
+					calculateOrgSelection(nodes[i].Children);
+				}
+			}
+			updateLabel();
+			console.log(vm.filteredOrgData);
+		}
+
+		function refreshData(keyword) {
+			vm.gridOptions.data = $filter('filter')(vm.changesData, keyword);
+		}
+	}
+})();
