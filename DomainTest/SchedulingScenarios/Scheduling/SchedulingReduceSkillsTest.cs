884e4e19d34e55f163efae88ff0ceb154ef59fa3
Kunning <kunning.mao@teleopti.com>
2018-09-12 11:15:47 +0200

    #76176 Failing test for web/stardust

---
+++ DomainTest/SchedulingScenarios/Scheduling/SchedulingReduceSkillsTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Forecasting;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Islands;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Scheduling.Assignment;
+using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.Scheduling
+{
+	[DomainTest]
+	public class SchedulingReduceSkillsTest : SchedulingScenario
+	{
+		public FullScheduling Target;
+		public FakePersonRepository PersonRepository;
+		public FakeActivityRepository ActivityRepository;
+		public FakeSkillRepository SkillRepository;
+		public FakeScenarioRepository ScenarioRepository;
+		public FakePersonAssignmentRepository PersonAssignmentRepository;
+		public FakeSkillDayRepository SkillDayRepository;
+		public FakeDayOffTemplateRepository DayOffTemplateRepository;
+		public FakePlanningPeriodRepository PlanningPeriodRepository;
+		public ReduceIslandsLimits ReduceIslandsLimits;
+		public MergeIslandsSizeLimit MergeIslandsSizeLimit;
+
+		[TestCase(0, 12, 12, 24, true)]
+		[TestCase(8, 16, 8, 16, true)]
+		[TestCase(0, 12, 13, 24, false)]
+		[TestCase(0, 12, 0, 12, false)]
+		[Ignore("To be fixed #76176")]
+		public void ShouldHandleDifferentOpenHoursWhenReducingSkills(int skill1Start, int skill1End, int skill2Start, int skill2End, bool canBeScheduled)
+		{
+			MergeIslandsSizeLimit.TurnOff_UseOnlyFromTest();
+			ReduceIslandsLimits.SetValues_UseOnlyFromTest(0, 1);
+			DayOffTemplateRepository.Has(DayOffFactory.CreateDayOff());
+			var date = new DateOnly(2015, 10, 12);
+			var activity = ActivityRepository.Has();
+			var skill1 = new Skill().For(activity).IsOpenBetween(skill1Start, skill1End).WithId();
+			var skill2 = new Skill().For(activity).IsOpenBetween(skill2Start, skill2End).WithId();
+			SkillRepository.Has(skill1, skill2); 
+			var scenario = ScenarioRepository.Has();
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 16, 0, 15), new ShiftCategory("_").WithId()));
+			var agentToSchedule = PersonRepository.Has(new SchedulePeriod(date, SchedulePeriodType.Day, 1), ruleSet, skill1, skill2);
+			PersonRepository.Has(skill1);
+			SkillDayRepository.Has(skill1.CreateSkillDayWithDemand(scenario, date, 100), skill2.CreateSkillDayWithDemand(scenario, date, 100));
+			var planningPeriod = PlanningPeriodRepository.Has(date, 1, SchedulePeriodType.Day, null);
+			
+			Target.DoSchedulingAndDO(planningPeriod.Id.Value);
+			
+			PersonAssignmentRepository.LoadAll().Single(x => x.Person.Equals(agentToSchedule) && x.Date == date).ShiftLayers.Any()
+				.Should().Be.EqualTo(canBeScheduled);
+		}
+
+		
+		
+		public SchedulingReduceSkillsTest(SeperateWebRequest seperateWebRequest, bool resourcePlannerXXL76496, bool resourcePlannerHalfHourSkillTimeZon75509, bool resourcePlannerReducingSkillsDifferentOpeningHours76176) : base(seperateWebRequest, resourcePlannerXXL76496, resourcePlannerHalfHourSkillTimeZon75509, resourcePlannerReducingSkillsDifferentOpeningHours76176)
+		{
+		}
+	}
+}
