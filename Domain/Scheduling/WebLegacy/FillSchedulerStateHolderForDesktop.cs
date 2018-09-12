ae9351f52d667fe5cd819734c1e9390b34f3d77e
Kunning <kunning.mao@teleopti.com>
2018-09-12 13:24:22 +0200

    minor

--- Domain/Scheduling/WebLegacy/FillSchedulerStateHolderForDesktop.cs
+++ Domain/Scheduling/WebLegacy/FillSchedulerStateHolderForDesktop.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.FeatureFlags;
-using Teleopti.Ccc.Domain.Forecasting;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.ResourceCalculation;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
