eec4acc12dacace051d022916c33f2219c6a98ff
Emil Sundin <emil.sundin@teleopti.com>
2018-09-13 15:14:03 +0200

    Speed up fcheck and match more greedy

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/EnsureAllTestsIsRun.ps1
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/EnsureAllTestsIsRun.ps1
@@ -2,23 +2,27 @@
 #To be sure that all tests is beeing run and no one accidentally commited it
 
 $WFMRepoWebPath = (Resolve-Path "$PSScriptRoot\..\WFM").Path
-function fcheck 
-{
+function fcheck {
+	$textSearch = "fdescribe\s*?\(|fit\s*?\("
 
-$MatchesJs = Get-ChildItem -Recurse -Filter *.spec.js $WFMRepoWebPath |
-Select-String -Pattern "fdescribe\(|fit\(" -List
-$MatchesTs = Get-ChildItem -Recurse -Filter *.spec.ts $WFMRepoWebPath |
-Select-String -Pattern "fdescribe\(|fit\(" -List
-$Paths = @($MatchesJs.Path) + @($MatchesTs.Path)
-$Count = $MatchesJs.Matches.Count + $MatchesTs.Matches.Count
-		
+	$MatchesJs = Get-ChildItem -Exclude vendor,node_modules -Path $WFMRepoWebPath |
+	Get-ChildItem -Recurse -Filter *.spec.js |
+	Select-String -Pattern $textSearch -List
+
+	$MatchesTs = Get-ChildItem -Exclude vendor,node_modules -Path $WFMRepoWebPath |
+	Get-ChildItem -Recurse -Filter *.spec.ts |
+	Select-String -Pattern $textSearch -List
+
+	$Paths = @($MatchesJs.Path) + @($MatchesTs.Path)
+	$Count = $MatchesJs.Matches.Count + $MatchesTs.Matches.Count
+			
 	if($Count -gt 0){
-	Write-Host "Found $Count matches for fdescribe/fit in the following files:" -ForegroundColor Red
-	foreach($path in $Paths) {
-		Write-Host $path
-	}
-	exit 1
+		Write-Host "Found $Count matches for fdescribe/fit in the following files:" -ForegroundColor Red
+		foreach($path in $Paths) {
+			Write-Host $path
+		}
+		exit 1
 	}
 }
-	
+
 fcheck
