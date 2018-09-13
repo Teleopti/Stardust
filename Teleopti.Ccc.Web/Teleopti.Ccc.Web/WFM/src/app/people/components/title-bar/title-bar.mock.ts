6a055feaa23d48ab724d4d032552a777d5ffbb51
Emil Sundin <emil.sundin@teleopti.com>
2018-09-13 13:45:43 +0200

    Fix build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/people/components/title-bar/title-bar.mock.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/people/components/title-bar/title-bar.mock.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
 	selector: 'people-title-bar',
 	template: '<div>MockTitleBarComponent</div>'
 })
 export class MockTitleBarComponent {}
+
+@NgModule({
+	declarations: [MockTitleBarComponent],
+	exports: [MockTitleBarComponent]
+})
+export class MockTitleBarModule {}
