1455d7d76f3cf109b98b93a4cd450d6e3476f837
Alina_Per_Sabbir_Mathias <Alina_Per_Sabbir_Mathias_LevelUp@teleopti.com>
2018-09-13 11:17:19 +0200

    #74770 syncronizer refacts

--- Teleopti.Wfm.Adherence/Domain/Events/RtaEventStoreSynchronizer.cs
+++ Teleopti.Wfm.Adherence/Domain/Events/RtaEventStoreSynchronizer.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text.RegularExpressions;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.Collection;
-using Teleopti.Ccc.Domain.Logon;
-using Teleopti.Ccc.Domain.Logon.Aspects;
 using Teleopti.Ccc.Domain.RealTimeAdherence.ApplicationLayer.ReadModels;
 using Teleopti.Ccc.Domain.RealTimeAdherence.Domain.AgentAdherenceDay;
 using Teleopti.Interfaces.Domain;
@@ -48,13 +45,13 @@ namespace Teleopti.Ccc.Domain.RealTimeAdherence.Domain.Events
 			var events = _events.LoadFrom(_keyValueStore.Get("LatestSynchronizedRTAEvent", 0));
 
 			_keyValueStore.Update("LatestSynchronizedRTAEvent", events.MaxId.ToString());
-
+			
 			events.Events
 				.Cast<IRtaStoredEvent>()
-				.Select(x => x.QueryData())
-				.ForEach(q =>
+				.GroupBy(e => new { e.QueryData().PersonId, Day = e.QueryData().StartTime.Value.ToDateOnly() })
+				.ForEach(group =>
 				{
-					var adherenceDay = _adherenceDayLoader.Load(q.PersonId.Value, q.StartTime.Value.ToDateOnly());
+					var adherenceDay = _adherenceDayLoader.Load(group.Key.PersonId.Value, group.Key.Day);
 					var lateForWork = adherenceDay.Changes().FirstOrDefault(c => c.LateForWork != null);
 					var lateForWorkText = lateForWork != null ? lateForWork.LateForWork : "0";
 					var minutesLateForWork = int.Parse(Regex.Replace(lateForWorkText, "[^0-9.]", ""));
@@ -63,8 +60,8 @@ namespace Teleopti.Ccc.Domain.RealTimeAdherence.Domain.Events
 
 					_readModels.Upsert(new HistoricalOverviewReadModel
 					{
-						PersonId = q.PersonId.Value,
-						Date = q.StartTime.Value.ToDateOnly(),
+						PersonId = group.Key.PersonId.Value,
+						Date = group.Key.Day,
 						Adherence = adherenceDay.Percentage(),
 						WasLateForWork = lateForWork != null,
 						MinutesLateForWork = minutesLateForWork,
