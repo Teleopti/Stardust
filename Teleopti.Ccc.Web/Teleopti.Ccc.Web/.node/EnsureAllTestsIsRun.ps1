c3abb06d833d0ddd34181d1c90f6ec1069c4ab72
Emil Sundin <emil.sundin@teleopti.com>
2018-09-13 14:21:03 +0200

    Fix test check

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/EnsureAllTestsIsRun.ps1
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/.node/EnsureAllTestsIsRun.ps1
@@ -1,19 +1,22 @@
 #Searching for the words fit & fdescribe in all spec.js files under web project. 
 #To be sure that all tests is beeing run and no one accidentally commited it
 
-$WFMRepoWebPath = "$env:WorkingDirectory\Teleopti.Ccc.Web\Teleopti.Ccc.Web\WFM"
-
+$WFMRepoWebPath = (Resolve-Path "$PSScriptRoot\..\WFM").Path
 function fcheck 
 {
 
-$Matches = Get-ChildItem -Recurse -Filter *.spec.ts $WFMRepoWebPath |
+$MatchesJs = Get-ChildItem -Recurse -Filter *.spec.js $WFMRepoWebPath |
 Select-String -Pattern "fdescribe\(|fit\(" -List
-$Paths = $Matches.Path
-$Count = $Matches.Matches.Count
+$MatchesTs = Get-ChildItem -Recurse -Filter *.spec.ts $WFMRepoWebPath |
+Select-String -Pattern "fdescribe\(|fit\(" -List
+$Paths = @($MatchesJs.Path) + @($MatchesTs.Path)
+$Count = $MatchesJs.Matches.Count + $MatchesTs.Matches.Count
 		
 	if($Count -gt 0){
 	Write-Host "Found $Count matches for fdescribe/fit in the following files:" -ForegroundColor Red
-	Write-Host $Paths
+	foreach($path in $Paths) {
+		Write-Host $path
+	}
 	exit 1
 	}
 }
