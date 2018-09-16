5ce15c221c568d2ebbbc89d629e2eb80ffab5745
Mikael Deigård <micke@teleopti.com>
2018-09-16 18:07:41 +0200

    #76348 adding file as well

---
+++ Domain/Optimization/MatrixLockers/MatrixClosedDayLocker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Teleopti.Ccc.Domain.FeatureFlags;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Scheduling.Legacy.Commands;
+
+namespace Teleopti.Ccc.Domain.Optimization.MatrixLockers
+{
+	[RemoveMeWithToggle(Toggles.ResourcePlanner_RespectClosedDaysWhenDoingDOBackToLegal_76348)]
+	public interface IMatrixClosedDayLocker
+	{
+		void Execute(IEnumerable<IScheduleMatrixPro> matrixList);
+	}
+
+	[RemoveMeWithToggle(Toggles.ResourcePlanner_RespectClosedDaysWhenDoingDOBackToLegal_76348)]
+	public class MatrixClosedDaysLockerDoNothing : IMatrixClosedDayLocker
+	{
+		public void Execute(IEnumerable<IScheduleMatrixPro> matrixList)
+		{}
+	}
+
+	public class MatrixClosedDayLocker : IMatrixClosedDayLocker
+	{
+		private readonly Func<ISchedulerStateHolder> _schedulerStateHolder;
+
+		public MatrixClosedDayLocker(Func<ISchedulerStateHolder> schedulerStateHolder)
+		{
+			_schedulerStateHolder = schedulerStateHolder;
+		}
+
+		public void Execute(IEnumerable<IScheduleMatrixPro> matrixList)
+		{
+			var stateHolder = _schedulerStateHolder();
+			foreach (var matrix in matrixList)
+			{
+				var personPeriod = matrix.Person.Period(matrix.SchedulePeriod.DateOnlyPeriod.StartDate);
+				var personSkills = personPeriod.PersonSkillCollection.Where(ps => ps.Active);
+				var skills = personSkills.Select(personSkill => personSkill.Skill).ToList();
+
+				foreach (var dateOnly in matrix.SchedulePeriod.DateOnlyPeriod.DayCollection())
+				{
+					var skillDays = stateHolder.SchedulingResultState.SkillDaysOnDateOnly(new[] { dateOnly });
+					var isOpen = skillDays.Any(skillDay => skillDay.OpenForWork.IsOpen && skills.Contains(skillDay.Skill));
+
+					if (!isOpen)
+					{
+						matrix.LockDay(dateOnly);
+					}
+				}
+
+			}
+		}
+	}
+}
