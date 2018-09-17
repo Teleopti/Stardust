c8f3f0cf0f7e58a32aeec4cb496fe6c8ea9be46d
Amanda Jonasson <amanda.jonasson@teleopti.com>
2018-09-17 09:45:45 -0600

    #76512 AddAbsence endpoint to API

---
+++ Teleopti.Wfm.Api.Test/Command/AddAbsenceTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.FakeRepositories;
+using Teleopti.Wfm.Api.Command;
+
+namespace Teleopti.Wfm.Api.Test.Command
+{
+	[ApiTest]
+	public class AddAbsenceTest
+	{
+		public IApiHttpClient Client;
+		public FakePersonRepository PersonRepository;
+		public FakeActivityRepository ActivityRepository;
+		public FakeScenarioRepository ScenarioRepository;
+		public FakePersonAbsenceRepository PersonAbsenceRepository;
+		public FakeAbsenceRepository AbsenceRepository;
+
+		[Test]
+		public void ShouldAddAbsenceLayer()
+		{
+			Client.Authorize();
+			var scenario = ScenarioFactory.CreateScenario("TestScenario", true, false).WithId();
+			ScenarioRepository.Has(scenario);
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var absence = AbsenceFactory.CreateAbsence("absence").WithId();
+			AbsenceRepository.Has(absence);
+			var addAbsenceDto = new AddAbsenceDto
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				AbsenceId = absence.Id.GetValueOrDefault(),
+				UtcStartTime = new DateTime(2018, 1, 1),
+				UtcEndTime = new DateTime(2018, 1, 2)
+			};
+
+			var result = Client.PostAsync("/command/AddAbsence",
+				new StringContent(JsonConvert.SerializeObject(addAbsenceDto)));
+			var resultDto = JObject.Parse(result.Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result)
+				.ToObject<ResultDto>();
+			resultDto.Successful.Should().Be.EqualTo(true);
+			PersonAbsenceRepository.LoadAll().Count().Should().Be.EqualTo(1);
+		}
+
+		[Test]
+		public void ShouldValidateStartAndEndDateTime()
+		{
+			Client.Authorize();
+			var scenario = ScenarioFactory.CreateScenario("TestScenario", true, false).WithId();
+			ScenarioRepository.Has(scenario);
+			var person = PersonFactory.CreatePerson().WithId();
+			PersonRepository.Add(person);
+			var absence = AbsenceFactory.CreateAbsence("absence").WithId();
+			AbsenceRepository.Has(absence);
+			var addAbsenceDto = new AddAbsenceDto
+			{
+				PersonId = person.Id.GetValueOrDefault(),
+				AbsenceId = absence.Id.GetValueOrDefault(),
+				UtcStartTime = new DateTime(2018, 1, 1),
+				UtcEndTime = new DateTime(2018, 1, 1)
+			};
+
+			var result = Client.PostAsync("/command/AddAbsence",
+				new StringContent(JsonConvert.SerializeObject(addAbsenceDto)));
+			var resultDto = JObject.Parse(result.Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result)
+				.ToObject<ResultDto>();
+			resultDto.Successful.Should().Be.EqualTo(false);
+			PersonAbsenceRepository.LoadAll().Count().Should().Be.EqualTo(0);
+		}
+	}
+}
---
+++ Teleopti.Wfm.Api/Command/AddAbsenceHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using log4net;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.Helper;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Wfm.Api.Command
+{
+	public class AddAbsenceHandler : ICommandHandler<AddAbsenceDto>
+	{
+		private readonly IPersonRepository _personRepository;
+		private readonly IScenarioRepository _scenarioRepository;
+		private readonly IAbsenceRepository _absenceRepository;
+		private readonly ILog _logger = LogManager.GetLogger(typeof(AddAbsenceHandler));
+		private readonly IPersonAbsenceRepository _personAbsenceRepository;
+
+		public AddAbsenceHandler(IPersonRepository personRepository,
+			IScenarioRepository scenarioRepository, IAbsenceRepository absenceRepository, IPersonAbsenceRepository personAbsenceRepository)
+		{
+			_personRepository = personRepository;
+			_scenarioRepository = scenarioRepository;
+			_absenceRepository = absenceRepository;
+			_personAbsenceRepository = personAbsenceRepository;
+		}
+
+		[UnitOfWork]
+		public virtual ResultDto Handle(AddAbsenceDto command)
+		{
+			try
+			{
+				if (command.UtcStartTime >= command.UtcEndTime)
+					return new ResultDto
+					{
+						Successful = false,
+						Message = "UtcEndTime must be greater than UtcStartTime"
+					};
+
+				var scenario = command.ScenarioId == null
+					? _scenarioRepository.LoadDefaultScenario()
+					: _scenarioRepository.Load(command.ScenarioId.GetValueOrDefault());
+
+				var person = _personRepository.Load(command.PersonId);
+				var absence = _absenceRepository.Load(command.AbsenceId);
+				var absenceLayer = new AbsenceLayer(absence, new DateTimePeriod(command.UtcStartTime.Utc(), command.UtcEndTime.Utc()));
+				var personAbsence = new PersonAbsence(person, scenario, absenceLayer);
+				((IRepository<IPersonAbsence>)_personAbsenceRepository).Add(personAbsence);
+				return new ResultDto
+				{
+					Id = personAbsence.Id,
+					Successful = true
+				};
+			}
+			catch (Exception e)
+			{
+				_logger.Error(e.Message + e.StackTrace);
+				return new ResultDto
+				{
+					Successful = false
+				};
+			}
+
+
+		}
+	}
+}
