0049614bc16354742ec8487effc14ad9d0c69bf5
Mathias Stenbom <mathias@stenbom.com>
2018-09-22 14:10:34 +0200

    #74770 Shorter transactions in the synchonizer. May fix performance test. Plus some small refacts.

--- Teleopti.Wfm.Adherence/Domain/Events/RtaEventStoreSynchronizer.cs
+++ Teleopti.Wfm.Adherence/Domain/Events/RtaEventStoreSynchronizer.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Interfaces.Domain;
 using Teleopti.Wfm.Adherence.ApplicationLayer.ReadModels;
@@ -32,7 +34,7 @@ namespace Teleopti.Wfm.Adherence.Domain.Events
 		private readonly IKeyValueStorePersister _keyValueStore;
 
 		public const string SynchronizedEventKey = "HistoricalOverviewReadModelSynchronizedEvent";
-		
+
 		public RtaEventStoreSynchronizer(
 			IRtaEventStoreReader events,
 			IHistoricalOverviewReadModelPersister readModels,
@@ -45,28 +47,50 @@ namespace Teleopti.Wfm.Adherence.Domain.Events
 			_keyValueStore = keyValueStore;
 		}
 
-		[FullPermissions]
-		[AllBusinessUnitsUnitOfWork]
-		[ReadModelUnitOfWork]
-		public virtual void Synchronize()
+		public void Synchronize()
 		{
-			var events = _events.LoadFrom(_keyValueStore.Get(SynchronizedEventKey, 0));
-
-			_keyValueStore.Update(SynchronizedEventKey, events.MaxId.ToString());
-
-			events.Events
-				.Cast<IRtaStoredEvent>()
-				.GroupBy(e => new {e.QueryData().PersonId, Day = e.QueryData().StartTime.Value.ToDateOnly()})
-				.ForEach(personAndDay => { synchronize(personAndDay.Key.PersonId.Value, personAndDay.Key.Day); });
+			var fromEventId = SynchronizedEventId();
+			var events = LoadEvents(fromEventId);
+			Synchronize(events.Events);
+			UpdateSynchronizedEventId(events.MaxId);
 		}
 
-		private void synchronize(Guid personId, DateOnly day)
+		[UnitOfWork]
+		protected virtual LoadedEvents LoadEvents(int fromEventId) =>
+			_events.LoadFrom(fromEventId);
+
+		[ReadModelUnitOfWork]
+		protected virtual void UpdateSynchronizedEventId(int toEventId) =>
+			_keyValueStore.Update(SynchronizedEventKey, toEventId.ToString());
+
+		[ReadModelUnitOfWork]
+		protected virtual int SynchronizedEventId() =>
+			_keyValueStore.Get(SynchronizedEventKey, 0);
+
+		[AllBusinessUnitsUnitOfWork]
+		[FullPermissions]
+		protected virtual void Synchronize(IEnumerable<IEvent> events) =>
+			events
+				.Cast<IRtaStoredEvent>()
+				.Select(e =>
+				{
+					var data = e.QueryData();
+					return new
+					{
+						PersonId = data.PersonId.Value,
+						Day = data.StartTime.Value.ToDateOnly()
+					};
+				})
+				.Distinct()
+				.ForEach(key => { synchronizeAdherenceDay(key.PersonId, key.Day); });
+
+		private void synchronizeAdherenceDay(Guid personId, DateOnly day)
 		{
 			var adherenceDay = _adherenceDayLoader.Load(personId, day);
 			var lateForWork = adherenceDay.Changes().FirstOrDefault(c => c.LateForWork != null);
 			var lateForWorkText = lateForWork != null ? lateForWork.LateForWork : "0";
 			var minutesLateForWork = int.Parse(Regex.Replace(lateForWorkText, "[^0-9.]", ""));
-			
+
 			_readModels.Upsert(new HistoricalOverviewReadModel
 			{
 				PersonId = personId,
