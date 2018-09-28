8d6f4d61b2f0c8ffcc49688ccd4619141069963f
Magnus Wedmark <magnus.wedmark@teleopti.com>
2018-09-28 16:28:22 +0200

    Added using statement used when not in Debug

--- Teleopti.Wfm.Stardust.IntegrationTest/Stardust/StardustTestAttribute.cs
+++ Teleopti.Wfm.Stardust.IntegrationTest/Stardust/StardustTestAttribute.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using Autofac;
 using NUnit.Framework.Interfaces;
@@ -38,7 +39,6 @@ namespace Teleopti.Wfm.Stardust.IntegrationTest.Stardust
 			TestSiteConfigurationSetup.Setup();
 
 			var dataHash = DefaultDataCreator.HashValue;
-			
 			var path = "";
 #if DEBUG
 			path = "./";
