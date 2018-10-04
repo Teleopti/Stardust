aecc41c5c6f780803e9995fa8d8aa42f44f03180
Amanda Jonasson <amanda.jonasson@teleopti.com>
2018-10-04 13:37:24 -0600

    #78082 try to collect all signalR_topic dependencies in one collection

--- Infrastructure/Util/ApplicationInsightsFilterOutSignalR.cs
+++ Infrastructure/Util/ApplicationInsightsFilterOutSignalR.cs
@@ -17,6 +17,11 @@ namespace Teleopti.Ccc.Infrastructure.Util
 
 		public void Process(ITelemetry item)
 		{
+			if (item is DependencyTelemetry dependency && dependency.Name.StartsWith("PUT /SIGNALR_TOPIC"))
+			{
+				dependency.Name = "PUT /SIGNALR_TOPIC_collection";
+			}
+
 			var request = item as RequestTelemetry;
 			if (request?.Url != null &&
 				request.Url.AbsolutePath.IndexOf(signalr, StringComparison.InvariantCultureIgnoreCase) > -1)
