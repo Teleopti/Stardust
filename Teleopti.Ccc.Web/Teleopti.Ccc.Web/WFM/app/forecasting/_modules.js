7f55761f783d96468e67d576c0c303091381fc9d
Dan <dan.brinkheden@teleopti.com>
2018-10-11 11:20:08 +0200

    fixed styleguide issue

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/forecasting/_modules.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/forecasting/_modules.js
@@ -1,28 +1,25 @@
-﻿(function () {
-	"use strict";
+﻿(function() {
+	'use strict';
 	angular
-		.module("wfm.forecasting", [
-			"gridshore.c3js.chart",
-			"ngResource",
-			"toggleService",
-			"ui.router",
-			"wfm.daterangepicker",
-			"wfm.workinghourspicker",
-			"pascalprecht.translate",
-			"wfm.modal",
-			"wfm.autofocus",
-			"wfm.notice",
-			"wfm.utilities"
+		.module('wfm.forecasting', [
+			'gridshore.c3js.chart',
+			'ngResource',
+			'toggleService',
+			'ui.router',
+			'wfm.daterangepicker',
+			'wfm.workingHoursPicker',
+			'pascalprecht.translate',
+			'wfm.modal',
+			'wfm.autofocus',
+			'wfm.notice',
+			'wfm.utilities'
 		])
-		.run(["$rootScope", "$state", "$location", onStateChangeSuccess]);
+		.run(['$rootScope', '$state', '$location', onStateChangeSuccess]);
 
 	function onStateChangeSuccess($rootScope, $state, $location) {
-		$rootScope.$on("$stateChangeSuccess", function (_event, toState) {
-			if (
-				$location.url() === $state.current.url &&
-					toState.name === "forecasting"
-			)
-				$state.go("forecasting.start");
+		$rootScope.$on('$stateChangeSuccess', function(_event, toState) {
+			if ($location.url() === $state.current.url && toState.name === 'forecasting')
+				$state.go('forecasting.start');
 		});
 	}
 })();
