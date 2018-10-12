bc3f534fa694c76e6197dbfb953a319252c362b2
Emil Sundin <emil.sundin@teleopti.com>
2018-10-12 19:39:09 +0200

    Adjust paths

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/assets/appInsights.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/assets/appInsights.js
@@ -1,55 +1,62 @@
-﻿
-var xmlhttp = new XMLHttpRequest();
-var url = "../Start/Config/SharedSettings";
+﻿var xmlhttp = new XMLHttpRequest();
+var url = '../Start/Config/SharedSettings';
 
-xmlhttp.onreadystatechange = function () {
+xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var instrumentationKey = JSON.parse(this.responseText).InstrumentationKey;
-		if (instrumentationKey === "00000000-0000-0000-0000-000000000000") return;
-		var iKey =
-			{
-				instrumentationKey: instrumentationKey
-			};
-		var appInsights = window.appInsights ||
-			function (a) {
+		if (instrumentationKey === '00000000-0000-0000-0000-000000000000') return;
+		var iKey = {
+			instrumentationKey: instrumentationKey
+		};
+		var appInsights =
+			window.appInsights ||
+			(function(a) {
 				function b(a) {
-					c[a] = function () {
+					c[a] = function() {
 						var b = arguments;
-						c.queue.push(function () { c[a].apply(c, b) })
-					}
+						c.queue.push(function() {
+							c[a].apply(c, b);
+						});
+					};
 				}
 
-				var c = { config: a }, d = document, e = window;
-				setTimeout(
-					function () {
-						var b = d.createElement("script");
-						b.src = a.url || "ai.js";
-						d.getElementsByTagName("script")[0].parentNode.appendChild(b);
-					});
+				var c = { config: a },
+					d = document,
+					e = window;
+				setTimeout(function() {
+					var b = d.createElement('script');
+					b.src = a.url || 'dist/ng2/assets/ai.js';
+					d.getElementsByTagName('script')[0].parentNode.appendChild(b);
+				});
 				try {
-					c.cookie = d.cookie
-				} catch (a) {
-				}
+					c.cookie = d.cookie;
+				} catch (a) {}
 				c.queue = [];
-				for (var f = ["Event", "Exception", "Metric", "PageView", "Trace", "Dependency"];
-					f.length;
-				) b("track" + f.pop());
-				if (b("setAuthenticatedUserContext"), b("clearAuthenticatedUserContext"), b("startTrackEvent"),
-					b("stopTrackEvent"),
-					b("startTrackPage"), b("stopTrackPage"), b("flush"), !a.disableExceptionTracking) {
-					f = "onerror", b("_" + f);
+				for (var f = ['Event', 'Exception', 'Metric', 'PageView', 'Trace', 'Dependency']; f.length; )
+					b('track' + f.pop());
+				if (
+					(b('setAuthenticatedUserContext'),
+					b('clearAuthenticatedUserContext'),
+					b('startTrackEvent'),
+					b('stopTrackEvent'),
+					b('startTrackPage'),
+					b('stopTrackPage'),
+					b('flush'),
+					!a.disableExceptionTracking)
+				) {
+					(f = 'onerror'), b('_' + f);
 					var g = e[f];
-					e[f] = function (a, b, d, e, h) {
+					e[f] = function(a, b, d, e, h) {
 						var i = g && g(a, b, d, e, h);
-						return !0 !== i && c["_" + f](a, b, d, e, h), i
-					}
+						return !0 !== i && c['_' + f](a, b, d, e, h), i;
+					};
 				}
 
-				return c
-			}(iKey);
-		window.appInsights = appInsights //, appInsights && appInsights.queue && 0 === appInsights.queue.length && appInsights.trackPageView();
+				return c;
+			})(iKey);
+		window.appInsights = appInsights; //, appInsights && appInsights.queue && 0 === appInsights.queue.length && appInsights.trackPageView();
 	}
 };
 
-xmlhttp.open("GET", url, true);
+xmlhttp.open('GET', url, true);
 xmlhttp.send();
