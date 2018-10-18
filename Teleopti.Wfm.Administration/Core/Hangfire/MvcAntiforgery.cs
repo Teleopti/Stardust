5e0f877724bd7b68008bd1e73d8878621b055f58
Robin Karlsson <robin.karlsson@teleopti.com>
2018-10-18 10:53:57 +0200

    Updating to latest Hangfire to solve issue with deadlocks
    Related Work Items: #78294

---
+++ Teleopti.Wfm.Administration/Core/Hangfire/MvcAntiforgery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Hangfire.Dashboard.Owin;
+
+namespace Teleopti.Wfm.Administration.Core.Hangfire
+{
+	public class MvcAntiforgery : IOwinDashboardAntiforgery
+	{
+		public MvcAntiforgery()
+		{
+			AntiForgeryConfig.UniqueClaimTypeIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
+		}
+
+		public string HeaderName => "X-CSRF-Token";
+
+		public string GetToken(IDictionary<string, object> environment)
+		{
+			AntiForgery.GetTokens(GetCookieToken(), out _, out var formToken);
+
+			return formToken;
+		}
+
+		public bool ValidateRequest(IDictionary<string, object> environment)
+		{
+			try
+			{
+				if (HttpContext.Current.Request.HttpMethod.Equals("POST", StringComparison.OrdinalIgnoreCase))
+				{
+					AntiForgery.Validate(GetCookieToken(), GetHeaderToken());
+				}
+
+				return true;
+			}
+			catch (HttpAntiForgeryException)
+			{
+				return false;
+			}
+		}
+
+		private string GetHeaderToken()
+		{
+			return HttpContext.Current.Request.Headers[HeaderName];
+		}
+
+		private string GetCookieToken()
+		{
+			var cookie = HttpContext.Current.Request.Cookies[AntiForgeryConfig.CookieName];
+			return cookie != null && !String.IsNullOrEmpty(cookie.Value) ? cookie.Value : null;
+		}
+	}
+}
