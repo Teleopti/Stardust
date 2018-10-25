572790fe22b07fb6015bbc4563fa0169c6de3bc9
Hongli Hou <hongli.hou@teleopti.com>
2018-10-25 07:54:56 +0000

    Merged PR 1060: bug77977 fixed build and reviewed the shift detail code of requests module
    bug77977 fixed build and reviewed the shift detail code of requests module

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/requests/requests.directive.shiftDetail.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/requests/requests.directive.shiftDetail.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-	var requestsShiftDetailDirective = function (teamScheduleSvc, groupScheduleFactory, currentUserInfo) {
+	var requestsShiftDetailDirective = function (teamScheduleSvc, groupScheduleFactory, currentUserInfo, serviceDateFormatHelper) {
 		return {
 			restrict: "A",
 			scope: {
@@ -16,13 +16,13 @@
 
 		function postlink(scope, elem, attrs, ctrls) {
 			var personIds = scope.personIds;
-			var scheduleDate = scope.date;
+			var scheduleDate = serviceDateFormatHelper.getDateOnly(scope.date);
 			var targetTimezone = scope.targetTimezone;
 			var showShiftDetail = scope.showShiftDetail;
 
 			elem.bind("click", function (oEvent) {
 				var position = getShiftDetailDisplayPosition(oEvent);
-				updateShiftStatusForSelectedPerson(personIds, moment(scheduleDate), targetTimezone, position, showShiftDetail);
+				updateShiftStatusForSelectedPerson(personIds, scheduleDate, targetTimezone, position, showShiftDetail);
 				oEvent.stopPropagation();
 			});
 		}
@@ -46,43 +46,19 @@
 				return;
 			}
 
-			var currentDate = scheduleDate.format("YYYY-MM-DD");
 			var currentUserTimezone = currentUserInfo.CurrentUserInfo().DefaultTimeZone;
 
-			teamScheduleSvc.getSchedules(currentDate, personIds)
+			teamScheduleSvc.getSchedules(scheduleDate, personIds)
 				.then(function (result) {
 					var schedulesToDisplay = result.Schedules.filter(function (schedule) {
-						return schedule.Date === currentDate;
+						return schedule.Date === scheduleDate;
 					});
-
-					var scheduleStartDate = scheduleDate;
-					if (schedulesToDisplay.length > 0 && targetTimezone != null && targetTimezone !== currentUserTimezone) {
-						var currentUserUtcOffset = moment().tz(currentUserTimezone).utcOffset();
-						var targetUserUtcOffset = moment().tz(targetTimezone).utcOffset();
-						var timeDifferent = targetUserUtcOffset - currentUserUtcOffset;
-
-						scheduleStartDate = moment(new Date(8640000000000000)); // Maximum date in js
-						schedulesToDisplay.forEach(function (schedule) {
-							// Time returned from server side is in current user's timezone;
-							// Need convert into target timezone
-							schedule.Projection.forEach(function (projection) {
-								var startMoment = moment(projection.Start).add(timeDifferent, "minutes");
-								projection.Start = startMoment.format("YYYY-MM-DD HH:mm");
-								if (startMoment.isBefore(scheduleStartDate, "day")) {
-									scheduleStartDate = startMoment;
-								}
-
-								var endMoment = moment(projection.End).add(timeDifferent, "minutes");
-								projection.End = endMoment.format("YYYY-MM-DD HH:mm");
-							});
-						});
-					}
-					var schedules = groupScheduleFactory.Create(schedulesToDisplay, scheduleStartDate.format('YYYY-MM-DD'), targetTimezone, 48);
+					var schedules = groupScheduleFactory.Create(schedulesToDisplay, scheduleDate, targetTimezone, 48);
 					showShiftDetail && showShiftDetail({ params: { left: position.left, top: position.top, schedules: schedules } });
 				});
 		}
 	};
 
 	angular.module("wfm.requests").directive("requestsShiftDetail",
-		["TeamSchedule", "GroupScheduleFactory", "CurrentUserInfo", requestsShiftDetailDirective]);
+		["TeamSchedule", "GroupScheduleFactory", "CurrentUserInfo", 'serviceDateFormatHelper', requestsShiftDetailDirective]);
 })();
