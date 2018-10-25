bacdf34ded4b09fc0fc7a767e0628a8b71781213
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2018-10-25 16:35:44 +0200

    #78485 Trigger person association to get timezone populated

--- Database/TeleoptiCCC7/Releases/00001023.sql
+++ Database/TeleoptiCCC7/Releases/00001023.sql
@@ -2,3 +2,6 @@
 ALTER TABLE ReadModel.ExternalLogon 
 ADD [TimeZone] nvarchar(50) NULL 
 GO
+
+UPDATE ReadModel.KeyValueStore SET [Value] = 'True' WHERE [Key] = 'PersonAssociationChangedPublishTrigger'
+GO
