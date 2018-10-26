6af56d26a92ea2bed6caba09e4692797347de066
Emil Sundin <emil.sundin@teleopti.com>
2018-10-26 21:03:02 +0200

    Fix Angular interceptor thingy

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/core/services/version.service.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/core/services/version.service.ts
@@ -1,23 +1,16 @@
-import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
 
 type Version = string;
 
 @Injectable()
 export class VersionService {
-	private version$ = new ReplaySubject<Version>(1);
-	private http: HttpClient;
+	private version = '';
 
-	constructor(handler: HttpBackend) {
-		this.http = new HttpClient(handler);
-		this.version$.next(''); // Default value
-		this.http.get('../api/Global/Version').subscribe((version: Version) => {
-			this.version$.next(version);
-		});
+	getVersion(): Version {
+		return this.version;
 	}
 
-	getVersion(): Observable<Version> {
-		return this.version$;
+	setVersion(version: Version) {
+		this.version = version;
 	}
 }
