8dccbd56edbc248d7651e0dce870c673be6407ac
Roger Kratz <roger.kratz@gmail.com>
2018-10-26 16:17:59 +0200

    Removing not used IncomingTask.GetEstimatedOutgoingBacklogOnDate

--- DomainTest/Backlog/IncomingTaskTest.cs
+++ DomainTest/Backlog/IncomingTaskTest.cs
@@ -21,20 +21,6 @@ namespace Teleopti.Ccc.DomainTest.Backlog
 		}
 
 		[Test]
-		public void ShouldCalculateOutgoingBacklogForDate()
-		{
-			setupOpenDays();
-
-			Assert.AreEqual(TimeSpan.FromHours(80), _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 1)));
-			Assert.AreEqual(TimeSpan.FromHours(60), _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 2)));
-			Assert.AreEqual(TimeSpan.FromHours(40), _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 3)));
-			Assert.AreEqual(TimeSpan.FromHours(20), _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 4)));
-			Assert.AreEqual(TimeSpan.Zero, _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 5)));
-			Assert.AreEqual(TimeSpan.Zero, _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 6)));
-			Assert.AreEqual(TimeSpan.Zero, _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 6, 7)));
-		}
-
-		[Test]
 		public void GetOverstaffTimeOnDateShouldWorkForScheduledTime()
 		{
 			setupOpenDays();
@@ -65,19 +51,6 @@ namespace Teleopti.Ccc.DomainTest.Backlog
 		}
 
 		[Test]
-		public void EstimatedOutgoingBacklogCanNeverBeNegative()
-		{
-			setupOpenDays();
-
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 1), TimeSpan.FromHours(30), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 2), TimeSpan.FromHours(30), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 3), TimeSpan.FromHours(30), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 4), TimeSpan.FromHours(30), PlannedTimeTypeEnum.Scheduled);
-
-			Assert.AreEqual(TimeSpan.Zero, _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 06, 4)));
-		}
-
-		[Test]
 		public void GetOverstaffTimeOnDateShouldWorkForManualTime()
 		{
 			setupOpenDays();
@@ -90,20 +63,6 @@ namespace Teleopti.Ccc.DomainTest.Backlog
 			_target.SetTimeOnDate(new DateOnly(2015, 6, 4), TimeSpan.FromHours(30), PlannedTimeTypeEnum.Manual);
 
 			Assert.AreEqual(TimeSpan.FromHours(20), _target.GetOverstaffTimeOnDate(new DateOnly(2015, 06, 4)));
-		}
-
-		[Test]
-		public void EstimatedOutgoingBacklogOnLastDayShouldBeZeroBecauseIfExistsItWillBeReportedInBacklogOutsideSLA()
-		{
-			setupOpenDays();
-
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 1), TimeSpan.FromHours(10), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 2), TimeSpan.FromHours(10), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 3), TimeSpan.FromHours(10), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 4), TimeSpan.FromHours(10), PlannedTimeTypeEnum.Scheduled);
-			_target.SetTimeOnDate(new DateOnly(2015, 6, 5), TimeSpan.FromHours(10), PlannedTimeTypeEnum.Scheduled);
-
-			Assert.AreEqual(TimeSpan.Zero, _target.GetEstimatedOutgoingBacklogOnDate(new DateOnly(2015, 06, 7)));
 		}
 
 		[Test]
