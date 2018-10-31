8db5f3db1405e575aaacb60990782929754c1a10
Emil Sundin <emil.sundin@teleopti.com>
2018-10-31 14:33:05 +0100

    Same config for teamcity. Should fix buildpipe test.

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/test/karma.conf.teamcity.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/test/karma.conf.teamcity.js
@@ -13,6 +13,11 @@ module.exports = function(config) {
 			{ pattern: 'app/**/!(*.spec|app_desktop_client).js' },
 			{ pattern: 'app/**/*.spec.js' },
 
+			{
+				pattern: 'node_modules/angular-i18n/angular-locale_*.js',
+				watched: false
+			},
+
 			//served seat image file at browser because addSeat function need to create seat object from image in seatManagement test.
 			{ pattern: 'app/seatManagement/images/*.svg', watched: false, included: false }
 		],
