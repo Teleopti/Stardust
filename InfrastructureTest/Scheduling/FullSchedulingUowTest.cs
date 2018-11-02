979613b5eec0b0c376040d192355a99a4410f8f1
Robin Karlsson <robin.karlsson@teleopti.com>
2018-11-02 07:11:23 +0100

    Resolving slow test case

--- InfrastructureTest/Scheduling/FullSchedulingUowTest.cs
+++ InfrastructureTest/Scheduling/FullSchedulingUowTest.cs
@@ -13,9 +13,11 @@ using Teleopti.Ccc.Domain.Repositories;
 using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
 using Teleopti.Ccc.Domain.Scheduling.ShiftCreator;
+using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.Domain.Staffing;
 using Teleopti.Ccc.IocCommon;
 using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.IoC;
 using Teleopti.Ccc.TestCommon.TestData;
 using Teleopti.Interfaces.Domain;
@@ -207,6 +209,7 @@ namespace Teleopti.Ccc.InfrastructureTest.Scheduling
 		public void Isolate(IIsolate isolate)
 		{
 			isolate.UseTestDouble<ScheduleDayDifferenceSaver>().For<IScheduleDayDifferenceSaver>();
+			isolate.UseTestDouble<FullPermission>().For<IAuthorization>();
 		}
 	}
 }
