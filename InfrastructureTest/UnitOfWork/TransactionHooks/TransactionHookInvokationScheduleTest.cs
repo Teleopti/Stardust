1176b54a744860d517b64fe8b466691cfcc88b21
Robin Karlsson <robin.karlsson@teleopti.com>
2018-11-02 08:27:04 +0100

    Hopefully the last test case resolved for now

--- InfrastructureTest/UnitOfWork/TransactionHooks/TransactionHookInvokationScheduleTest.cs
+++ InfrastructureTest/UnitOfWork/TransactionHooks/TransactionHookInvokationScheduleTest.cs
@@ -3,14 +3,17 @@ using NUnit.Framework;
 using SharpTestsEx;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Scheduling;
+using Teleopti.Ccc.Domain.Security.Principal;
 using Teleopti.Ccc.InfrastructureTest.Persisters.Schedules;
 using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
 
 namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork.TransactionHooks
 {
 	[TestFixture]
 	[ScheduleDictionaryPersistTest]
-	public class TransactionHookInvokationScheduleTest
+	public class TransactionHookInvokationScheduleTest : IIsolateSystem
 	{
 		public FakeTransactionHook TransactionHook;
 		public IScheduleDictionaryPersister Target;
@@ -33,6 +36,9 @@ namespace Teleopti.Ccc.InfrastructureTest.UnitOfWork.TransactionHooks
 			TransactionHook.ModifiedRoots.OfType<IPersonAssignment>().Should().Not.Be.Empty();
 		}
 
-
+		public void Isolate(IIsolate isolate)
+		{
+			isolate.UseTestDouble<FullPermission>().For<IAuthorization>();
+		}
 	}
 }
