9b7ede31770a950e6cfba71f5ceb08b9559bd1fb
Xinfeng Li <xinfli@gmail.com>
2018-11-02 10:19:06 +0100

    Implement embed report with PowerBI client directly instead 3rd party package to get more control of embedded report

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/pm/components/workspace/workspace.component.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/pm/components/workspace/workspace.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PowerBIService } from '../../core/powerbi.service';
+import * as pbi from 'powerbi-client';
 
+import { PowerBIService } from '../../core/powerbi.service';
 import { ReportConfig } from '../../models/ReportConfig.model';
 
 @Component({
@@ -9,16 +10,53 @@ import { ReportConfig } from '../../models/ReportConfig.model';
 	styleUrls: ['./workspace.component.scss']
 })
 export class WorkspaceComponent implements OnInit {
-	@Input() initialized: boolean;
-	@Input() reportConfig: ReportConfig;
+	public initialized: boolean;
+	public reportConfig: ReportConfig;
+
+	private pbiCoreService: pbi.service.Service;
 
 	constructor(private pbiService: PowerBIService) {
 		this.initialized = false;
+		this.pbiCoreService = new pbi.service.Service(
+			pbi.factories.hpmFactory,
+			pbi.factories.wpmpFactory,
+			pbi.factories.routerFactory
+		);
 	}
 
 	ngOnInit() {
 		this.pbiService.getReportConfig().then((config) => {
-			this.reportConfig = config;
+			// Refer to https://github.com/Microsoft/PowerBI-JavaScript/wiki/Embed-Configuration-Details for more details
+			const embedConfig = {
+				type: 'report',
+				tokenType: pbi.models.TokenType.Embed,
+				accessToken: config.AccessToken,
+				embedUrl: config.ReportUrl,
+				id: config.ReportId,
+				permissions: pbi.models.Permissions.All,
+				viewMode: pbi.models.ViewMode.Edit,
+				settings: {
+					filterPaneEnabled: true,
+					navContentPaneEnabled: true,
+					localeSettings: {
+						language: 'en',
+						formatLocale: 'en'
+					}
+				}
+			};
+
+			// Embed the report and display it within the div container.
+			const reportContainer = <HTMLElement>document.getElementById('reportContainer');
+			const report = this.pbiCoreService.embed(reportContainer, embedConfig);
+
+			// Report.off removes a given event handler if it exists.
+			report.off('loaded');
+
+			// Report.on will add an event handler which prints to Log window.
+			report.on('loaded', function() {
+				console.log('Report loaded :-)');
+			});
+
 			this.initialized = true;
 		});
 	}
