e1163fe5c1019b30fc9be0f05e565f0965f29d7a
Mathias Stenbom <mathias@stenbom.com>
2018-11-05 11:56:14 +0100

    #78669 Simple first test

---
+++ InfrastructureTest/ApplicationLayer/PersonLoadAllWithAssociationTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+
+namespace Teleopti.Ccc.InfrastructureTest.ApplicationLayer
+{
+	[DatabaseTest]
+	public class PersonLoadAllWithAssociationTest
+	{
+		public Database Database;
+		public IPersonLoadAllWithAssociation Target;
+		public WithUnitOfWork UnitOfWork;
+
+		[Test]
+		public void ShouldLoad()
+		{
+			Database.WithAgent();
+
+			UnitOfWork.Get(() => Target.LoadAll())
+				.Should().Not.Be.Empty();
+		}
+	}
+}
