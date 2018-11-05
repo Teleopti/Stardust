Teleopti/Stardust

commit 0dc9791615d853c97eb17e71858b6728c376c776

story #77447 and #78640: refactor current week view header icon styles

Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Css/WeekView.css
@@ -93,13 +93,38 @@
 	);
 }
 
-.weekview-day-show-request {
-	cursor: pointer;
+.weekview-day-symbol {
+	height: 25px;
+	border-bottom: 1px solid #dfdfdf;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	line-height: 1;
 }
 
-.weekview-day-symbol {
+.weekview-day-symbol > div {
+	margin: 2px;
+	line-height: 1;
+}
+.weekview-day-symbol > div > * {
+	display: inline-block;
+}
+
+.weekview-day-symbol i {
+	font-size: 16px;
+}
+
+.weekview-day-symbol .seatbooking-symbol {
+	width: 18px;
 	height: 18px;
-	border-bottom: 1px solid #dfdfdf;
+	background-repeat: no-repeat;
+	background-size: cover;
+	margin-top: 0;
+}
+
+.holiday-agents .small-circle {
+	width: 16px;
+	height: 16px;
 }
 
 .weekview-day-symbol div[class='tooltip-inner'] {
@@ -167,7 +192,7 @@
 
 .weekview-timeline {
 	width: 31px;
-	height: 796px;
+	height: 804px;
 	background: transparent;
 	border-right: 1px solid #dfdfdf;
 	position: relative;

Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Views/Schedule/WeekPartial.cshtml
@@ -170,27 +170,26 @@ Layout = null;
 					<!-- /ko -->
 				</div>
 				<div class="weekview-day-symbol" data-bind="click: $parent.showAddRequestForm, css: { 'weekview-day-show-request': requestPermission }">
-					<div class="weekview-day-symbol-request pull-right weekview-day-icon" data-bind="visible: hasRequests, tooltip: { title: requestsText, html: true }, click: navigateToRequests">
-						<i class="glyphicon glyphicon-comment"></i>
-					</div>
-					<div class="holiday-agents weekview-day-show-request pull-right weekview-day-icon" data-bind="visible: absenceRequestPermission, tooltip: { title: holidayChanceText, html: true }">
-						<div class="small-circle" data-bind="style: {background: holidayChanceColor()}">
-						</div>
-					</div>
-					<div class="overtime-availability-symbol floatright weekview-day-icon" data-bind="visible: overtimeAvailability().HasOvertimeAvailability, tooltip: { title: textOvertimeAvailabilityText, html: true }">
-						<i class="glyphicon glyphicon-time floatright" />
-					</div>
-
-					<div class="seatbooking-symbol pull-right weekview-day-icon " data-bind="visible: seatBookingIconVisible(), tooltip: { title: seatBookingMessage, html: true }">
-						<i class="mdi mdi-chair-school" />
-					</div>
-
 					<!-- ko if: hasNote -->
-					<div class="pull-right weekview-day-icon" data-bind="tooltip: { title: noteMessage, html: true }">
+					<div class="weekview-day-icon" data-bind="tooltip: { title: noteMessage, html: true }">
 						<i class="glyphicon glyphicon-exclamation-sign"></i>
 					</div>
 					<!-- /ko -->
-					<div id="add-request-cell" data-bind="css:{'add-request-space show-request': requestPermission}">
+
+					<div class="seatbooking-symbol weekview-day-icon " data-bind="visible: seatBookingIconVisible(), tooltip: { title: seatBookingMessage, html: true }">
+						<i class="mdi mdi-chair-school"></i>
+					</div>
+
+					<div class="overtime-availability-symbol floatright weekview-day-icon" data-bind="visible: overtimeAvailability().HasOvertimeAvailability, tooltip: { title: textOvertimeAvailabilityText, html: true }">
+						<i class="glyphicon glyphicon-time floatright"></i>
+					</div>
+
+					<div class="weekview-day-symbol-request weekview-day-icon" data-bind="visible: hasRequests, tooltip: { title: requestsText, html: true }, click: navigateToRequests">
+						<i class="glyphicon glyphicon-comment"></i>
+					</div>
+
+					<div class="holiday-agents weekview-day-icon cursorpointer" data-bind="visible: absenceRequestPermission, tooltip: { title: holidayChanceText, html: true }">
+						<div class="small-circle" data-bind="style: {background: holidayChanceColor()}"></div>
 					</div>
 				</div>
 				<div>

Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Css/ScheduleStyles.css
@@ -10,9 +10,7 @@
 .small-circle {
 	width: 10px;
 	height: 10px;
-	-webkit-border-radius: 6px;
-	-moz-border-radius: 6px;
-	border-radius: 8px;
+	border-radius: 50%;
 	margin-top: 2px;
 }
 
@@ -37,8 +35,4 @@
 	width: 14px;
 	height: 14px;
 	background: url(../Images/seat.svg);
-}
-
-.weekview-day-symbol .weekview-day-icon {
-	margin-right: 5px;
 }

commit f12d0b6c6e73b2ff1cc4fa2648830f6ed00f1d71

fix bug #78691: caused by our refactoring in 0dc9791615d853c97eb17e71858b6728c376c776

Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Css/WeekView.css
@@ -15,6 +15,7 @@
 	position: relative;
 	width: 955px;
 	margin: 0 auto;
+	padding-bottom: 20px;
 }
 
 .weekview-day {
@@ -27,8 +28,11 @@
 	width: 129px;
 	border-left: 1px solid #f9f9f9;
 	border-right: 1px solid #dfdfdf;
-	border-top: 1px solid #f9f9f9;
 	border-bottom: 1px solid #dfdfdf;
+}
+
+.weekview-timeline  + .weekview-day{
+	border-left: 1px solid #dfdfdf;
 }
 
 .weekview-day-header {
@@ -192,19 +196,17 @@
 
 .weekview-timeline {
 	width: 31px;
-	height: 804px;
 	background: transparent;
-	border-right: 1px solid #dfdfdf;
 	position: relative;
-	margin-bottom: 20px;
+	margin-top: 129px;
 }
 
 .weekview-timeline-label {
 	font-size: 7pt;
 	width: 30px;
-	height: 14px;
 	text-align: center;
 	white-space: pre-wrap;
+	transform: translateY(calc(-7pt / 2));
 }
 
 .weekview-day-time-indicator {

Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Schedule.js
@@ -9,7 +9,6 @@
 Teleopti.MyTimeWeb.Schedule = (function($) {
 	var vm,
 		timeIndicatorDateTime,
-		timeLineOffset = 119,
 		ajax = new Teleopti.MyTimeWeb.Ajax(),
 		completelyLoaded,
 		daylightSavingAdjustment,
@@ -648,7 +647,7 @@ Teleopti.MyTimeWeb.Schedule = (function($) {
 			return new Teleopti.MyTimeWeb.Schedule.TimelineViewModel(
 				item,
 				constants.scheduleHeight,
-				timeLineOffset,
+				0,
 				true
 			);
 		});
@@ -693,7 +692,6 @@ Teleopti.MyTimeWeb.Schedule = (function($) {
 		}
 
 		var timelineHeight = Teleopti.MyTimeWeb.Common.Constants.scheduleHeight;
-		var offset = 123;
 		var timeindicatorHeight = 2;
 
 		var hours = theDate.hours();
@@ -714,7 +712,7 @@ Teleopti.MyTimeWeb.Schedule = (function($) {
 
 		if (timelineStartMinutes <= clientNowMinutes && clientNowMinutes <= timelineEndMinutes) {
 			timeIndicator.css('top', position).show();
-			timeIndicatorTimeLine.css('top', position + offset).show();
+			timeIndicatorTimeLine.css('top', position).show();
 		} else {
 			timeIndicator.hide();
 			timeIndicatorTimeLine.hide();

Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Views/Schedule/WeekPartial.cshtml
@@ -139,7 +139,7 @@ Layout = null;
 	<div class="schedule-table-container">
 		<div class="week-schedule-ASM-permission-granted" data-bind="if : asmEnabled">yes</div>
 		<div class="week-schedule-current-week" data-bind="if : isCurrentWeek">yes</div>
-		<div class="body-weekview-inner clearfix" style="padding: 0">
+		<div class="body-weekview-inner clearfix">
 			<div class="weekview-timeline floatleft">
 				<div class="weekview-day-time-indicator-small absolute">
 				</div>