fe17b51b3ca9bd91b65191c85b80255a883afa2e
Xinfeng Li <xinfli@gmail.com>
2018-11-06 15:14:34 +0100

    Fix test case for PMNextGen workspace component

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/pm/components/workspace/workspace.component.spec.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/pm/components/workspace/workspace.component.spec.ts
@@ -1,59 +1,58 @@
-// import { HttpClient, HttpClientModule } from '@angular/common/http';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { configureTestSuite, PageObject } from '@wfm/test';
-// import { NzButtonModule, NzTableModule, NzToolTipModule } from 'ng-zorro-antd';
-// import { of } from 'rxjs';
-// import { MockTranslationModule } from '../../../../mocks/translation';
-// import { WorkspaceComponent } from './workspace.component';
-// import { PowerBIService } from '../../core/powerbi.service';
-// // import { adina, eva, fakeBackendProvider, myles, SearchService, WorkspaceService } from '../../services';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { configureTestSuite, PageObject } from '@wfm/test';
+import { NzButtonModule, NzTableModule, NzToolTipModule } from 'ng-zorro-antd';
+import { of } from 'rxjs';
+import { MockTranslationModule } from '../../../../mocks/translation';
+import { WorkspaceComponent } from './workspace.component';
+import { PowerBIService } from '../../core/powerbi.service';
 
-// describe('WorkspaceComponent', () => {
-// 	let component: WorkspaceComponent;
-// 	let fixture: ComponentFixture<WorkspaceComponent>;
+describe('WorkspaceComponent', () => {
+	let component: WorkspaceComponent;
+	let fixture: ComponentFixture<WorkspaceComponent>;
 
-// 	// let page: Page;
-// 	let powerBIService: PowerBIService;
+	// let page: Page;
+	let powerBIService: PowerBIService;
 
-// 	configureTestSuite();
+	configureTestSuite();
 
-// 	beforeEach(async(() => {
-// 		TestBed.configureTestingModule({
-// 			declarations: [WorkspaceComponent],
-// 			imports: [MockTranslationModule, HttpClientModule, NzTableModule, NzButtonModule, NzToolTipModule],
-// 			providers: [
-// 				PowerBIService,
-// 				{
-// 					provide: HttpClient,
-// 					useValue: {}
-// 				}
-// 			]
-// 		}).compileComponents();
-// 		powerBIService = TestBed.get(powerBIService);
-// 	}));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [WorkspaceComponent],
+			imports: [MockTranslationModule, HttpClientModule, NzTableModule, NzButtonModule, NzToolTipModule],
+			providers: [
+				PowerBIService,
+				{
+					provide: HttpClient,
+					useValue: {}
+				}
+			]
+		}).compileComponents();
+		powerBIService = TestBed.get(PowerBIService);
+	}));
 
-// 	beforeEach(() => {
-// 		fixture = TestBed.createComponent(WorkspaceComponent);
-// 		component = fixture.componentInstance;
-// 		// page = new Page(fixture);
-// 		fixture.detectChanges();
-// 	});
+	beforeEach(() => {
+		fixture = TestBed.createComponent(WorkspaceComponent);
+		component = fixture.componentInstance;
+		// page = new Page(fixture);
+		fixture.detectChanges();
+	});
 
-// 	it('should create PMNextGen WorkspaceComponent', () => {
-// 		const reportConfigResponse = {
-// 			TokenType: 'Embed',
-// 			AccessToken: 'DummyAccessToken',
-// 			ReportUrl: 'https://app.powerbi.com/reportEmbed?reportId=TestReportId&groupId=TestGroupId',
-// 			ReportId: 'TestReportId'
-// 		};
-// 		spyOn(powerBIService, 'getReportConfig').and.returnValue(of(reportConfigResponse));
+	it('should create PMNextGen WorkspaceComponent', () => {
+		const reportConfigResponse = {
+			TokenType: 'Embed',
+			AccessToken: 'DummyAccessToken',
+			ReportUrl: 'https://app.powerbi.com/reportEmbed?reportId=TestReportId&groupId=TestGroupId',
+			ReportId: 'TestReportId'
+		};
+		spyOn(powerBIService, 'getReportConfig').and.returnValue(of(reportConfigResponse));
 
-// 		expect(component).toBeTruthy();
-// 	});
-// });
+		expect(component).toBeTruthy();
+	});
+});
 
-// // class Page extends PageObject {
-// // 	// get getPeople() {
-// // 	//     return this.queryAll('[data-test-workspace] [data-test-person]');
-// // 	// }
-// // }
+// class Page extends PageObject {
+// 	// get getPeople() {
+// 	//     return this.queryAll('[data-test-workspace] [data-test-person]');
+// 	// }
+// }
