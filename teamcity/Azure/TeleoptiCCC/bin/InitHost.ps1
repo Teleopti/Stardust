96cb7094a330246740d8977f71bdde6d883e1282
David Jonsson <david.jonsson@teleopti.com>
2018-11-08 08:47:19 -0700

    Clean up

--- teamcity/Azure/TeleoptiCCC/bin/InitHost.ps1
+++ teamcity/Azure/TeleoptiCCC/bin/InitHost.ps1
@@ -6,24 +6,13 @@ function TeleoptiDriveMapProperty-get {
       [string]$name
       )
     $computer = gc env:computername
-    ## Local debug values
-    if ($computer.ToUpper().StartsWith("T16284")) {
-		switch ($name){
-		BlobPath		{$TeleoptiDriveMapProperty="http://teleopticcc7.blob.core.windows.net/"; break}
-		ContainerName	{$TeleoptiDriveMapProperty="teleopticcc/Settings"; break}
-		AccountKey		{$TeleoptiDriveMapProperty="IqugZC5poDWLu9wwWocT42TAy5pael77JtbcZtnPcm37QRThCkdrnzOh3HEu8rDD1S8E6dU5D0aqS4sJA1BTxQ=="; break}
-		DataSourceName	{$TeleoptiDriveMapProperty="teleopticcc-dev"; break}
-		default			{$TeleoptiDriveMapProperty="Unknown Value"; break}
-        }
-     }
-    else {
-		switch ($name){
-		BlobPath		{$TeleoptiDriveMapProperty = [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::GetConfigurationSettingValue("TeleoptiDriveMap.BlobPath"); break}
-        ContainerName	{$TeleoptiDriveMapProperty="teleopticcc/Settings"; break}        
-		AccountKey		{$TeleoptiDriveMapProperty = [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::GetConfigurationSettingValue("TeleoptiDriveMap.AccountKey"); break}
-		DataSourceName	{$TeleoptiDriveMapProperty = [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::GetConfigurationSettingValue("TeleoptiDriveMap.DataSourceName"); break}
-		default			{$TeleoptiDriveMapProperty="Unknown Value"; break}
-        }
+
+	switch ($name){
+	BlobPath		{$TeleoptiDriveMapProperty = [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::GetConfigurationSettingValue("TeleoptiDriveMap.BlobPath"); break}
+    ContainerName	{$TeleoptiDriveMapProperty="teleopticcc/Settings"; break}        
+	AccountKey		{$TeleoptiDriveMapProperty = [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::GetConfigurationSettingValue("TeleoptiDriveMap.AccountKey"); break}
+	DataSourceName	{$TeleoptiDriveMapProperty = [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::GetConfigurationSettingValue("TeleoptiDriveMap.DataSourceName"); break}
+	default			{$TeleoptiDriveMapProperty="Unknown Value"; break}
            
     }
 	return $TeleoptiDriveMapProperty
@@ -114,11 +103,11 @@ Try
 	}
 
 	#Setting $Env:TeleoptiIsAzure = $true
-	#[Environment]::SetEnvironmentVariable("TeleoptiIsAzure", "true", "Machine")
+	[Environment]::SetEnvironmentVariable("TeleoptiIsAzure", "true", "Machine")
 
 	#Set environment variables for RoleInstanceID & Rolename
-	#[Environment]::SetEnvironmentVariable("RoleName", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Role.Name, "Machine") 
-	#[Environment]::SetEnvironmentVariable("RoleInstanceID", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Id, "Machine")
+	[Environment]::SetEnvironmentVariable("RoleName", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Role.Name, "Machine") 
+	[Environment]::SetEnvironmentVariable("RoleInstanceID", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Id, "Machine")
 
     #74478, #76734, #78787
     $DataSourceName = TeleoptiDriveMapProperty-get -name "DataSourceName"
