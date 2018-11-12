7ef2a76a6d6b0a1a48c9006ec51f59494f5ff7de
Mikael Deigård <micke@teleopti.com>
2018-11-12 11:16:23 +0100

    Preparing more refact in scheduling screen

---
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Win/Scheduling/LockMenuBuilders/LockRestrictionsMenuBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Teleopti.Ccc.SmartClientPortal.Shell.Win.Scheduling.LockMenuBuilders
+{
+	public class LockRestrictionsMenuBuilder
+	{
+		public void Build(ToolStripMenuItem toolStripMenuItemLockRestrictions, UserLockHelper userLockHelper)
+		{
+
+		}
+
+		//private void buildLockAllRestrictions(ToolStripMenuItem toolStripMenuItemLockRestrictions)
+		//{
+		//	//toolStripMenuItemLockAllRestrictions.MouseUp += toolStripMenuItemLockAllRestrictionsMouseUp;
+		//}
+	}
+}
