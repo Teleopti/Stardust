9a7bf289575bdb723a5b5ee7fba39733147cedbe
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2018-11-14 09:20:46 +0100

    #77750 #74770 first infra test for synchronizer

---
+++ Teleopti.Wfm.Adherence.Test/Historical/Infrastructure/HistoricalOverviewViewModel/AdherenceTest.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Domain.Common.Time;
+using Teleopti.Ccc.Domain.Helper;
+using Teleopti.Ccc.Domain.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Wfm.Adherence.Domain.Events;
+using Teleopti.Wfm.Adherence.Test.InfrastructureTesting;
+
+namespace Teleopti.Wfm.Adherence.Test.Historical.Infrastructure.HistoricalOverviewViewModel
+{
+	[DatabaseTest]
+	public class AdherenceTest
+	{
+		public ApplicationLayer.ViewModels.HistoricalOverviewViewModelBuilder Target;
+		public Database Database;
+		public MutableNow Now;
+		public IEventPublisher Publisher;
+		public IRtaEventStoreSynchronizer Synchronizer;
+		public WithUnitOfWork UnitOfWork;
+		public IRtaEventStoreSynchronizerWaiter Waiter;
+
+		[Test]
+		public void ShouldWorkAfterSynchronize()
+		{
+			Now.Is("2018-11-13 08:00");
+			Database
+				.WithActivity("Phone")
+				.WithTeam()
+				.WithAgent()
+				.WithAssignment("2018-11-13")
+				.WithAssignedActivity("Phone", "2018-11-13 08:00", "2018-11-13 17:00");
+			var person = Database.CurrentPersonId();
+			var team = Database.CurrentTeamId();
+			Publisher.Publish(new PersonStateChangedEvent
+			{
+				PersonId = person,
+				BelongsToDate = "2018-11-13".Date(),
+				Timestamp = "2018-11-13 08:00".Utc(),
+				Adherence = EventAdherence.In
+			});
+			Now.Is("2018-11-14 08:00");
+			Synchronizer.Synchronize();
+
+			var data = UnitOfWork.Get(() => Target.Build(null, new[] {team}).First());
+
+			data.Agents.Single().Days.Last().Adherence.Should().Be("100");
+		}		
+	}
+}
