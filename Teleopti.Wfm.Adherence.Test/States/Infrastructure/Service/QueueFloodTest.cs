e7be8caeca3642d375f00e975583420f9139b859
Mathias Stenbom <mathias@stenbom.com>
2018-11-14 10:40:06 +0100

    #77710 Infra coverage

---
+++ Teleopti.Wfm.Adherence.Test/States/Infrastructure/Service/QueueFloodTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeRepositories.Rta;
+using Teleopti.Wfm.Adherence.Domain.Service;
+using Teleopti.Wfm.Adherence.Test.InfrastructureTesting;
+using Teleopti.Wfm.Adherence.Test.States.Unit.Service;
+
+namespace Teleopti.Wfm.Adherence.Test.States.Infrastructure.Service
+{
+	[TestFixture]
+	[DatabaseTest]
+	public class QueueFloodTest
+	{
+		public Rta Target;
+		public IStateQueueHealthChecker Checker;
+
+		[Test]
+		public void ShouldStopEnqueueAfter100Batches()
+		{
+			100.Times(() =>
+			{
+				Checker.Reset();
+				Target.Enqueue(new BatchForTest());
+			});
+			
+			Assert.Throws<StateQueueHealthException>(() =>
+			{
+				Checker.Reset();
+				Target.Enqueue(new BatchForTest());
+			});
+		}
+
+	}
+}
