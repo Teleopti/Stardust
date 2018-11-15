5d5dd0eae6e550616820eb1ac14483308f34cff9
Mathias Stenbom <mathias@stenbom.com>
2018-11-15 14:17:41 +0100

    #77750 Using fake time in state queue worker again, so it stops when time moves forward in behavior tests. Some are red if not. Dont know why.

--- Teleopti.Wfm.Adherence/Domain/Service/StateQueueTenants.cs
+++ Teleopti.Wfm.Adherence/Domain/Service/StateQueueTenants.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 
 namespace Teleopti.Wfm.Adherence.Domain.Service
 {
 	public class StateQueueTenants
 	{
 		private readonly ICurrentDataSource _dataSource;
+		private readonly INow _now;
 		private readonly object _lock = new object();
 		private readonly IList<TenantInfo> _tenants = new List<TenantInfo>();
 
-		public StateQueueTenants(ICurrentDataSource dataSource)
+		public StateQueueTenants(ICurrentDataSource dataSource, INow now)
 		{
 			_dataSource = dataSource;
+			_now = now;
 		}
 
 		internal class TenantInfo
@@ -34,7 +37,7 @@ namespace Teleopti.Wfm.Adherence.Domain.Service
 					_tenants.Add(info);
 				}
 
-				info.ProcessQueueUntil = DateTime.UtcNow.AddMinutes(1);
+				info.ProcessQueueUntil = _now.UtcDateTime().AddMinutes(1);
 			}
 		}
 
@@ -42,7 +45,7 @@ namespace Teleopti.Wfm.Adherence.Domain.Service
 		{
 			lock (_lock)
 			{
-				var toRemove = _tenants.Where(x => x.ProcessQueueUntil < DateTime.UtcNow).ToList();
+				var toRemove = _tenants.Where(x => x.ProcessQueueUntil < _now.UtcDateTime()).ToList();
 				toRemove.ForEach(x => _tenants.Remove(x));
 				return _tenants.Select(x => x.Name).ToArray();
 			}
