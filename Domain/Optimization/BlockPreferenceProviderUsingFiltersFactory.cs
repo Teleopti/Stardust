7899e6ff21ab28fc6ca7ab9e6a652890e35e7a38
Roger Kratz <roger.kratz@gmail.com>
2018-11-15 11:44:37 +0100

    #76289 Removing no longer needed dependency

--- Domain/Optimization/BlockPreferenceProviderUsingFiltersFactory.cs
+++ Domain/Optimization/BlockPreferenceProviderUsingFiltersFactory.cs
@@ -1,17 +1,14 @@
 using Teleopti.Ccc.Domain.InterfaceLegacy;
-using Teleopti.Ccc.Domain.Scheduling;
 
 namespace Teleopti.Ccc.Domain.Optimization
 {
 	public class BlockPreferenceProviderUsingFiltersFactory
 	{
 		private readonly IPlanningGroupSettingsRepository _planningGroupSettingsRepository;
-		private readonly ISchedulingOptionsProvider _schedulingOptionsProvider;
 
-		public BlockPreferenceProviderUsingFiltersFactory(IPlanningGroupSettingsRepository planningGroupSettingsRepository, ISchedulingOptionsProvider schedulingOptionsProvider)
+		public BlockPreferenceProviderUsingFiltersFactory(IPlanningGroupSettingsRepository planningGroupSettingsRepository)
 		{
 			_planningGroupSettingsRepository = planningGroupSettingsRepository;
-			_schedulingOptionsProvider = schedulingOptionsProvider;
 		}
 
 		public IBlockPreferenceProvider Create(IPlanningGroup planningGroup)
