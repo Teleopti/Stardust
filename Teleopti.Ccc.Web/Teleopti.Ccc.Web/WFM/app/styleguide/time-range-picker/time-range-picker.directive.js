63adee83e44a82a145f9b65642cd1ca8770028fd
Jianfeng Zhu <jianfeng.zhu@teleopti.com>
2018-11-15 07:26:56 +0000

    Merged PR 1183: fix bug #78963 and refactor
    fix bug #78963 and refactor
    
    Related work items: #78963

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/styleguide/time-range-picker/time-range-picker.directive.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/styleguide/time-range-picker/time-range-picker.directive.js
@@ -1,216 +1,102 @@
-(function () {
+(function() {
+	'use strict';
 
-    'use strict';
+	angular
+		.module('wfm.timerangepicker', ['pascalprecht.translate'])
+		.controller('timeRangePickerController', ['$scope', '$translate', '$locale', timeRangePickerController])
+		.directive('timeRangePicker', timeRangePickerDirective);
 
-    angular.module('wfm.timerangepicker', ['pascalprecht.translate'])
-        .directive('timepickerWrap', ['$locale', timepickerWrap])
-        .directive('timeRangePicker', ['$filter', timeRangePicker]);
+	function timeRangePickerDirective() {
+		return {
+			templateUrl: 'app/styleguide/time-range-picker/time-range-picker.tpl.html',
+			scope: {
+				disableNextDay: '=?',
+				maxHoursRange: '=?',
+				ngModel: '=ngModel'
+			},
+			controller: 'timeRangePickerController',
+			controllerAs: 'vm',
+			bindToController: true
+		};
+	}
 
-    var defaultTemplate = 'app/styleguide/time-range-picker/time-range-picker.tpl.html';
+	function timeRangePickerController($scope, $translate, $locale) {
+		var vm = this;
 
-    function timeRangePicker($filter) {
-        return {
-            templateUrl: function (element, attrs) {
-                return attrs.templateUrl || defaultTemplate;
-            },
-            scope: {
-                disableNextDay: '=?',
-                maxHoursRange: '=?'
-            },
-            controller: ['$scope', '$element', '$translate', timeRangePickerCtrl],
-            require: ['ngModel', 'timeRangePicker'],
-            transclude: true,
-            link: postlink
-        };
+		var meridianInfo = getMeridiemInfoByLocale($locale);
 
-        function timeRangePickerCtrl($scope, $element, $translate) {
+		vm.showMeridian = meridianInfo.showMeridian;
+		vm.nextDay = !isSameDate(vm.ngModel.startTime, vm.ngModel.endTime);
+		vm.meridians = [meridianInfo.am, meridianInfo.pm];
+		vm.minuteStep = 5;
+		vm.invalidTimeRangeMessage = $translate.instant('InvalidHoursRange').replace('{0}', vm.maxHoursRange);
 
-            /* jshint validthis: true */
+		vm.showInvalidError = false;
+		vm.showOrderError = false;
+		vm.showEmptyError = false;
 
-            var vm = this;
-            $element.addClass('wfm-time-range-picker-wrap');
+		vm.toggleNextDay = function() {
+			if (!vm.disableNextDay) {
+				vm.nextDay = !vm.nextDay;
 
-            function format(target, arg) {
-                return target.replace('{0}', arg);
-            }
+				if (vm.nextDay) {
+					vm.ngModel.endTime = moment(vm.ngModel.startTime)
+						.startOf('day')
+						.add(1, 'days')
+						.add(vm.ngModel.endTime.getHours(), 'hours')
+						.add(vm.ngModel.endTime.getMinutes(), 'minutes')
+						.toDate();
+				} else {
+					vm.ngModel.endTime = moment(vm.ngModel.startTime)
+						.startOf('day')
 
-            $scope.toggleNextDay = toggleNextDay;
-            //$scope.invalidTimeRange = 'The period cannot exceed 24 hours.'; //$translate('InvalidHoursRange');
-            $scope.invalidTimeRange = format($translate.instant('InvalidHoursRange'), $scope.maxHoursRange);
+						.add(vm.ngModel.endTime.getHours(), 'hours')
+						.add(vm.ngModel.endTime.getMinutes(), 'minutes')
+						.toDate();
+				}
+			}
+		};
 
-            vm.mutateMoment = mutateMoment;
-            vm.sameDate = sameDate;
+		$scope.$watch(
+			function() {
+				return angular.toJson({
+					startTime: moment(vm.ngModel.startTime),
+					endTime: moment(vm.ngModel.endTime),
+					nextDay: vm.nextDay
+				});
+			},
+			function(newVal, oldVal) {
+				if (!vm.ngModel) {
+					vm.showInvalidError = true;
+					return;
+				}
+				if (vm.ngModel.startTime > vm.ngModel.endTime) {
+					vm.showOrderError = true;
+					return;
+				}
+				if (!vm.ngModel.startTime || !vm.ngModel.endTime) {
+					vm.showEmptyError = true;
+				}
+			}
+		);
 
-            function toggleNextDay() {
-                if (!$scope.disableNextDay) {
-                    $scope.nextDay = !$scope.nextDay;
-                }
-            }
+		function isSameDate(date1, date2) {
+			return date1.toLocaleDateString() === date2.toLocaleDateString();
+		}
 
-            function mutateMoment(mDate, date) {
-                var hour = date.getHours(),
-                    minute = date.getMinutes();
+		function getMeridiemInfoByLocale($locale) {
+			var timeFormat = $locale.DATETIME_FORMATS.shortTime;
+			var info = {};
 
-                mDate.set('hour', hour).set('minute', minute);
-            }
+			if (/h:/.test(timeFormat)) {
+				info.showMeridian = true;
+				info.am = $locale.DATETIME_FORMATS.AMPMS[0];
+				info.pm = $locale.DATETIME_FORMATS.AMPMS[1];
+			} else {
+				info.showMeridian = false;
+			}
 
-            function sameDate(date1, date2) {
-                return date1.toLocaleDateString() === date2.toLocaleDateString();
-            }
-        }
-
-        function postlink(scope, elem, attrs, ctrls) {
-            var ngModel = ctrls[0],
-                timeRangeCtrl = ctrls[1];
-
-            scope.$watch(watchUIChange, respondToUIChange, true);
-
-            ngModel.$parsers.push(parseView);
-            ngModel.$formatters.push(formatModel);
-            ngModel.$render = render;
-            ngModel.$validators.order = validateCorrectOrder;
-            ngModel.$validators.range = validateRange;
-
-            function formatModel(modelValue) {
-                if (!modelValue) {
-                    return undefined;
-                }
-
-                var nextDay =
-                    !timeRangeCtrl.sameDate(modelValue.startTime, modelValue.endTime);
-
-                var viewModel = makeViewValue(
-                    modelValue.startTime, modelValue.endTime, nextDay);
-
-                return viewModel;
-            }
-
-            function parseView(viewValue) {
-                if (!viewValue) {
-                    return undefined;
-                }
-
-                return {
-                    startTime: viewValue.startTime.toDate(),
-                    endTime: viewValue.endTime.toDate()
-                };
-            }
-
-            function render() {
-                if (!ngModel.$viewValue) {
-                    return;
-                }
-
-                var mStartTime = ngModel.$viewValue.startTime,
-                    mEndTime = ngModel.$viewValue.endTime;
-
-                scope.startTime = mStartTime.toDate();
-                scope.endTime = mEndTime.toDate();
-                scope.nextDay = !mStartTime.isSame(mEndTime, 'day');
-            }
-
-            function validateCorrectOrder(modelValue, viewValue) {
-                if (modelValue === undefined) {
-                    return true;
-                }
-                return modelValue.startTime <= modelValue.endTime;
-            }
-
-            function validateRange(modelValue, viewValue) {
-                if (modelValue === undefined || scope.maxHoursRange === undefined || scope.maxHoursRange === '') {
-                    return true;
-                }
-
-                return modelValue.endTime - modelValue.startTime < parseInt(scope.maxHoursRange) * 1000 * 60 * 60;
-            }
-
-            function makeViewValue(startTime, endTime, nextDay) {
-                var viewValue = {
-                    startTime: moment(),
-                    endTime: moment()
-                };
-
-                timeRangeCtrl.mutateMoment(viewValue.startTime, startTime);
-                timeRangeCtrl.mutateMoment(viewValue.endTime, endTime);
-
-                if (nextDay) {
-                    viewValue.endTime.add(1, 'day');
-                }
-
-                return viewValue;
-            }
-
-            function respondToUIChange(change, old) {
-                if (!scope.startTime || !scope.endTime) {
-                    ngModel.$setViewValue(null);
-                    return;
-                }
-
-                if (scope.disableNextDay) {
-                    scope.nextDay = change.strEndTime === '00:00';
-                }
-
-                ngModel.$setViewValue(
-                    makeViewValue(scope.startTime, scope.endTime, scope.nextDay));
-            }
-
-            function watchUIChange() {
-                return {
-                    strStartTime: scope.startTime ? $filter('date')(scope.startTime, 'HH:mm') : '',
-                    strEndTime: scope.endTime ? $filter('date')(scope.endTime, 'HH:mm') : '',
-                    boolNextDay: scope.nextDay
-                };
-            }
-        }
-    }
-
-    function timepickerWrap($locale) {
-        var meridianInfo = getMeridiemInfoByLocale($locale);
-
-        return {
-            template: '<div uib-timepicker></div>',
-            controller: ['$scope', timepickerWrapCtrl],
-            compile: compileFn
-        };
-
-        function compileFn(tElement, tAttributes) {
-            var binding = tAttributes.ngModel;
-            tElement.addClass('wfm-timepicker-wrap');
-
-            var cellElement = tElement.find('timepicker');
-            cellElement.attr('ng-model', binding);
-            cellElement.attr('show-meridian', 'showMeridian');
-            cellElement.attr('minute-step', 'minuteStep');
-
-            if (meridianInfo.showMeridian) {
-                cellElement.attr('meridians', 'meridians');
-            }
-        }
-
-        function timepickerWrapCtrl($scope) {
-            $scope.showMeridian = meridianInfo.showMeridian;
-            $scope.minuteStep = 5;
-
-            if (meridianInfo.showMeridian) {
-                $scope.meridians = [meridianInfo.am, meridianInfo.pm];
-            }
-        }
-
-    }
-
-    function getMeridiemInfoByLocale($locale) {
-        var timeFormat = $locale.DATETIME_FORMATS.shortTime;
-        var info = {};
-
-        if (/h:/.test(timeFormat)) {
-            info.showMeridian = true;
-            info.am = $locale.DATETIME_FORMATS.AMPMS[0];
-            info.pm = $locale.DATETIME_FORMATS.AMPMS[1];
-        } else {
-            info.showMeridian = false;
-        }
-
-        return info;
-    }
-
+			return info;
+		}
+	}
 })();
