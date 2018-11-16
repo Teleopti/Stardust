3a416dd5705942ee8060847bd445ef9728d7544d
Magnus Wedmark <magnus.wedmark@teleopti.com>
2018-11-16 09:51:25 +0100

    Cleaning and dummy trigger build

--- Domain/Auditing/PurgeAuditRunner.cs
+++ Domain/Auditing/PurgeAuditRunner.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Teleopti.Ccc.Domain.ApplicationLayer;
+﻿using Teleopti.Ccc.Domain.ApplicationLayer;
 using Teleopti.Ccc.Domain.ApplicationLayer.Audit;
 
 namespace Teleopti.Ccc.Domain.Auditing
