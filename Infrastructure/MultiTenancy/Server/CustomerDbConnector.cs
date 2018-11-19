cc331e777bd92b16cd8df0206b193608e8ddcf63
Magnus Ekström <magnus.ekstrom@teleopti.com>
2018-11-19 08:02:26 +0100

    Some refactorization and added test.

---
+++ Infrastructure/MultiTenancy/Server/CustomerDbConnector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Teleopti.Ccc.Infrastructure.MultiTenancy.Server
+{
+	public interface ICustomerDbConnector
+	{
+		bool TryGetEmailAddress(PersonInfo pi, out string emailAddress);
+	}
+
+	public class CustomerDbConnector : ICustomerDbConnector
+	{
+		public bool TryGetEmailAddress(PersonInfo pi, out string emailAddress)
+		{
+			using (var con = new SqlConnection(pi.Tenant.DataSourceConfiguration.ApplicationConnectionString))
+			using (var cmd = new SqlCommand("SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; SELECT Email FROM Person WHERE Id = @userId", con)) // set transaction isolation level read uncommitted;
+			{
+				cmd.CommandType = CommandType.Text;
+				cmd.Parameters.Add("@userId", SqlDbType.UniqueIdentifier);
+				cmd.Parameters["@userId"].Value = pi.Id;
+				try
+				{
+					con.Open();
+					emailAddress = cmd.ExecuteScalar().ToString();
+					return true;
+				}
+				catch (Exception ex)
+				{
+					var asd = ex.Message;
+					emailAddress = null;
+					return false;
+				}
+			}
+		}
+	}
+}
