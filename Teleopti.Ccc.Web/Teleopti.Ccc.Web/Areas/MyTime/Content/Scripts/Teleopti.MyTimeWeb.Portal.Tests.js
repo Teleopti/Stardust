f32cdcbd4fc69fb368761b17ea5396fa18114d79
Jianfeng Zhu <jianfeng.zhu@teleopti.com>
2018-11-19 16:53:30 +0800

    story #78569: remove old abandoned unused mobile week view in MyTime

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Portal.Tests.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Portal.Tests.js
@@ -4,25 +4,11 @@
 		teardown: function() {}
 	});
 
-	test('should navigate to binded action view after clicking menu item', function() {
-		var html =
-			'<div id="innerNavBar"><a id="test-menu" href="#Schedule/MobileWeek" data-mytime-action ="#Schedule/MobileWeekAction"></a></div>';
-		$('body').append(html);
-
-		var fakeWindow = getFakeWindow();
-		setup(fakeWindow);
-		init(fakeWindow);
-
-		$('#test-menu').click();
-
-		equal('#Schedule/MobileWeekAction', fakeWindow.location.url);
-	});
-
 	test('should navigate to defaultNavigation', function() {
 		setup();
 		var fakeWindow = getFakeWindow();
 		init(fakeWindow);
-		equal('#Schedule/MobileDay', fakeWindow.location.url);
+		equal('', fakeWindow.location.url);
 	});
 
 	test('should navigate to mobile day page', function() {
@@ -30,23 +16,6 @@
 		var fakeWindow = getFakeWindow();
 		init(fakeWindow);
 		equal('#Schedule/MobileDay', fakeWindow.location.url);
-	});
-
-	test('should navigate to mobile week page when access from ipad', function() {
-		setup();
-		var fakeWindow = getFakeWindow();
-		fakeWindow.navigator.userAgent = 'iPad';
-		init(fakeWindow);
-		equal('#Schedule/MobileWeek', fakeWindow.location.url);
-	});
-
-	test('should navigate to mobile week page when access from tablet', function() {
-		setup();
-		var fakeWindow = getFakeWindow();
-		fakeWindow.navigator.userAgent =
-			'Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13';
-		init(fakeWindow);
-		equal('#Schedule/MobileWeek', fakeWindow.location.url);
 	});
 
 	function setup(fakeWindow) {
@@ -94,7 +63,7 @@
 
 	function getDefaultSetting() {
 		return {
-			defaultNavigation: 'Schedule/MobileWeek',
+			defaultNavigation: 'Schedule',
 			baseUrl: '/',
 			startBaseUrl: '/'
 		};
