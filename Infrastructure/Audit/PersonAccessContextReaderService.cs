832a67e35580dfbef43b78752f68f7e6223cb964
TOPTINET\EmilW <emil.widell@teleopti.com>
2018-11-20 08:58:25 +0100

    Story #74938 The person access audit does not use foreign keys in db anymore. Changed how they are saved in db.

--- Infrastructure/Audit/PersonAccessContextReaderService.cs
+++ Infrastructure/Audit/PersonAccessContextReaderService.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using Teleopti.Ccc.Domain.Aop;
 using Teleopti.Ccc.Domain.ApplicationLayer.Audit;
 using Teleopti.Ccc.Domain.Auditing;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.Logon;
 using Teleopti.Ccc.Domain.Repositories;
@@ -18,32 +19,43 @@ namespace Teleopti.Ccc.Infrastructure.Audit
 		private readonly IApplicationRoleRepository _applicationRoleRepository;
 		private readonly IPurgeSettingRepository _purgeSettingRepository;
 		private readonly INow _now;
+		private readonly ICommonAgentNameProvider _commonAgentNameProvider;
 
-		public PersonAccessContextReaderService(IPersonAccessAuditRepository personAccessAuditRepository, IApplicationRoleRepository applicationRoleRepository, IPurgeSettingRepository purgeSettingRepository, INow now)
+		public PersonAccessContextReaderService(IPersonAccessAuditRepository personAccessAuditRepository, IApplicationRoleRepository applicationRoleRepository, IPurgeSettingRepository purgeSettingRepository, INow now, ICommonAgentNameProvider commonAgentNameProvider)
 		{
 			_personAccessAuditRepository = personAccessAuditRepository;
 			_applicationRoleRepository = applicationRoleRepository;
 			_purgeSettingRepository = purgeSettingRepository;
 			_now = now;
+			_commonAgentNameProvider = commonAgentNameProvider;
 		}
 
 		private IEnumerable<AuditServiceModel> getAuditServiceModel(IEnumerable<IPersonAccess> personAccessAudit)
 		{
 			var auditServiceModelList = new List<AuditServiceModel>();
+			var commonAgentNameSetting = _commonAgentNameProvider.CommonAgentNameSettings;
 			foreach (var audit in personAccessAudit)
 			{
 				var auditServiceModel = new AuditServiceModel
 				{
 					TimeStamp = audit.TimeStamp, Context = "PersonAccess", Action = audit.Action,
-					ActionPerformedBy = audit.ActionPerformedBy.Name.ToString(NameOrderOption.FirstNameLastName)
+					ActionPerformedBy = extractPersonAuditInfo(audit.ActionPerformedBy, commonAgentNameSetting)
+						//audit.ActionPerformedBy.Name.ToString(NameOrderOption.FirstNameLastName)
 				};
 				var deserializedRole = JsonConvert.DeserializeObject<PersonAccessModel>(audit.Data);
 				var appRole = _applicationRoleRepository.Load(deserializedRole.RoleId);
-				auditServiceModel.Data = $"Person: {audit.ActionPerformedOn.Name} Role: {appRole.Name} Action: {audit.Action}";
+				var actionPerformedOn = extractPersonAuditInfo(audit.ActionPerformedOn, commonAgentNameSetting);
+				auditServiceModel.Data = $"Person: {actionPerformedOn} Role: {appRole.Name} Action: {audit.Action}";
 				auditServiceModelList.Add(auditServiceModel);
 			}
 
 			return auditServiceModelList;
+		}
+
+		private string extractPersonAuditInfo(string person, ICommonNameDescriptionSetting commonAgentNameSetting)
+		{
+			var personInfo = JsonConvert.DeserializeObject<PersonAuditInfo>(person);
+			return commonAgentNameSetting.BuildFor(personInfo);
 		}
 
 		public class PersonAccessModel
