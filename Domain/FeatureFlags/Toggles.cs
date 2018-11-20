1fce8e818d6587254e2dfca825a70afb25dfa430
Zhenwei He <Zhenwei.He@teleopti.com>
2018-11-20 09:56:50 +0800

    remove toggle MyTimeWeb_PreferenceForJalaliCalendar_42965

--- Domain/FeatureFlags/Toggles.cs
+++ Domain/FeatureFlags/Toggles.cs
@@ -25,7 +25,6 @@
 		RTA_StateQueueFloodPrevention_77710,
 		RTA_ReviewHistoricalAdherence_74770,
 
-		MyTimeWeb_PreferenceForJalaliCalendar_42965,
 		MyTimeWeb_DayScheduleForStartPage_Command_44209,
 		MyTimeWeb_WaitListPositionEnhancement_46301,
 		MyTimeWeb_NewTeamScheduleView_75989,
