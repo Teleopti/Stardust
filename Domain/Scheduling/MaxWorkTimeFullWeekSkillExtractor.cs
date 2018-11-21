b41d7f75ca4a46d7751c3fb25259f29388a400a1
Claes Stenman <claes.stenman@teleopti.com>
2018-11-21 07:31:10 +0100

    76118 added toggle class

---
+++ Domain/Scheduling/MaxWorkTimeFullWeekSkillExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.Scheduling.TeamBlock;
+using Teleopti.Interfaces.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling
+{
+	public interface IMaxWorkTimeFullWeekSkillExtractor
+	{
+		Dictionary<DateOnly, TimeSpan> Extract(IScheduleMatrixPro scheduleMatrixPro, ITeamBlockInfo teamBlockInfo, IEnumerable<ISkillDay> skillDays, IGroupPersonSkillAggregator groupPersonSkillAggregator);
+	}
+
+	public class MaxWorkTimeFullWeekSkillExtractor : IMaxWorkTimeFullWeekSkillExtractor
+	{
+		public Dictionary<DateOnly, TimeSpan> Extract(IScheduleMatrixPro scheduleMatrixPro, ITeamBlockInfo teamBlockInfo, IEnumerable<ISkillDay> skillDays, IGroupPersonSkillAggregator groupPersonSkillAggregator)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class MaxWorkTimeFullWeekSkillExtractorDoNothing : IMaxWorkTimeFullWeekSkillExtractor
+	{
+		public Dictionary<DateOnly, TimeSpan> Extract(IScheduleMatrixPro scheduleMatrixPro, ITeamBlockInfo teamBlockInfo, IEnumerable<ISkillDay> skillDays, IGroupPersonSkillAggregator groupPersonSkillAggregator)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
