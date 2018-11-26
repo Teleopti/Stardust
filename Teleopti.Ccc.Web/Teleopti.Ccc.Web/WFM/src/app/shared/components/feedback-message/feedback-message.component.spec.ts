d59addb43e51838ad1f99a94a680c00b88147342
Johan Öbrink <johan.erik.obrink@gmail.com>
2018-11-26 15:00:40 +0100

    improve karma test

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/shared/components/feedback-message/feedback-message.component.spec.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/shared/components/feedback-message/feedback-message.component.spec.ts
@@ -2,13 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MockTranslationModule } from '@wfm/mocks/translation';
-import { configureTestSuite } from '@wfm/test';
+import { configureTestSuite, PageObject } from '@wfm/test';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { FeedbackMessageComponent } from './feedback-message.component';
 
 describe('FeedbackMessageComponent', () => {
 	let component: FeedbackMessageComponent;
 	let fixture: ComponentFixture<FeedbackMessageComponent>;
+	let page: Page;
 
 	configureTestSuite();
 
@@ -22,10 +23,23 @@ describe('FeedbackMessageComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(FeedbackMessageComponent);
 		component = fixture.componentInstance;
-		fixture.detectChanges();
+		page = new Page(fixture);
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+	it('should show feedback message', () => {
+		component.feedbackType = 'newRelease';
+		fixture.detectChanges();
+		fixture.whenStable().then(() => {
+			expect(page.feedbackMessage.nativeElement.innerText.length).toBeGreaterThan(0);
+		});
+	});
 });
+
+class Page extends PageObject {
+	get feedbackMessage() {
+		return this.queryAll('[data-test-feedbackmessage]')[0];
+	}
+}
