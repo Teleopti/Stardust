e53016313d0a45d9dc910d0b29499ca71e9cfc2f
Daniel Sund <Daniel.Sund@teleopti.com>
2018-12-10 09:55:31 +0000

    Merged PR 1391: Added comments to files containing the concerned methods
    added comments to files containing the concerned methods
    
    Related work items: #79221

--- Teleopti.Wfm.Administration/Controllers/DatabaseController.cs
+++ Teleopti.Wfm.Administration/Controllers/DatabaseController.cs
@@ -18,6 +18,10 @@ using Teleopti.Wfm.Administration.Core;
 
 namespace Teleopti.Wfm.Administration.Controllers
 {
+	/// <summary>
+	/// Changes to this file may affect third party connections, i.e. Twillio, TalkDesk etc.
+	/// Please contact CloudOps when changes are required and made. 
+	/// </summary>
 	[TenantTokenAuthentication]
 	public class DatabaseController : ApiController
 	{
