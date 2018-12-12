f27a9e480280e96fc3f35619d0cd010ff7b4b853
Ola Håkansson <lars.ola.hakansson@gmail.com>
2018-12-12 15:14:46 +0100

    bug #79664

--- Teleopti.Wfm.Administration/Controllers/UpgradeDatabasesController.cs
+++ Teleopti.Wfm.Administration/Controllers/UpgradeDatabasesController.cs
@@ -9,6 +9,7 @@ using Teleopti.Wfm.Administration.Models;
 
 namespace Teleopti.Wfm.Administration.Controllers
 {
+	[TenantTokenAuthentication]
 	public class UpgradeDatabasesController : ApiController
 	{
 		private readonly ILoadAllTenants _loadAllTenants;
