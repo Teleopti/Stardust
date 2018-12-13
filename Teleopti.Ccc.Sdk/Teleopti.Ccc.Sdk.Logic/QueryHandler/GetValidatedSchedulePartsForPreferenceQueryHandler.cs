5826e11476e18973be09e938ea081a6315ddacf7
Roger Kratz <roger.kratz@gmail.com>
2018-12-13 09:58:44 +0100

    removing no longer needed stateholder in GetValidatedSchedulePartsForPreferenceQueryHandler

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/QueryHandler/GetValidatedSchedulePartsForPreferenceQueryHandler.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/QueryHandler/GetValidatedSchedulePartsForPreferenceQueryHandler.cs
@@ -109,11 +109,6 @@ namespace Teleopti.Ccc.Sdk.Logic.QueryHandler
 
 				IScheduleDictionary scheduleDictionary = _scheduleStorage.FindSchedulesForPersonsOnlyInGivenPeriod(personList, new ScheduleDictionaryLoadOptions(true, false), period.ToDateOnlyPeriod(timeZoneInfo), _scenarioRepository.Current());
 
-
-				var stateHolder = new SchedulingResultStateHolder();
-					stateHolder.Schedules = scheduleDictionary;
-					stateHolder.LoadedAgents = personList;
-
 					var fullWeekOuterWeekPeriodCreator = new FullWeekOuterWeekPeriodCreator(schedulePeriod.DateOnlyPeriod, person);
 					var scheduleMatrix = new ScheduleMatrixPro(scheduleDictionary, fullWeekOuterWeekPeriodCreator, schedulePeriod);
 
