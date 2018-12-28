87a6e0fd63147d0cd709b116dc4fd1db283ef4d4
Robin Karlsson <robin.karlsson@teleopti.com>
2018-12-28 12:37:23 +0100

    Removing unused code

--- Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/PasswordPolicyStepDefinitions.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.WebBehaviorTest/Bindings/Generic/PasswordPolicyStepDefinitions.cs
@@ -1,9 +1,6 @@
 using System;
-using System.IO;
 using log4net;
 using TechTalk.SpecFlow;
-using TechTalk.SpecFlow.Assist;
-using Teleopti.Ccc.TestCommon.Web.WebInteractions;
 using Teleopti.Ccc.WebBehaviorTest.Core;
 using Table = TechTalk.SpecFlow.Table;
 
@@ -12,59 +9,12 @@ namespace Teleopti.Ccc.WebBehaviorTest.Bindings.Generic
 	[Binding]
 	public class PasswordPolicyStepDefinitions
 	{
-		private IDisposable _timeoutScope;
 		private static readonly ILog Logger = LogManager.GetLogger(typeof(PasswordPolicyStepDefinitions));
-
-		[BeforeScenario("PasswordPolicy")]
-		public void BeforePasswordPolicyScenario()
-		{
-			_timeoutScope = Browser.TimeoutScope(TimeSpan.FromSeconds(20));
-		}
-
-		[AfterScenario("PasswordPolicy")]
-		public void AfterPasswordPolicyScenario()
-		{
-			var targetTestPasswordPolicyFile = Path.Combine(Paths.WebPath(),  "PasswordPolicy.xml");
-			if (File.Exists(targetTestPasswordPolicyFile))
-				File.Move(targetTestPasswordPolicyFile,Path.GetRandomFileName());
-			_timeoutScope.Dispose();
-			_timeoutScope = null;
-		}
-
+		
 		[Given(@"There is a password policy with")]
 		public void GivenThereIsAPasswordPolicyWith(Table table)
 		{
-			var targetTestPasswordPolicyFile = Path.Combine(Paths.WebPath(), "PasswordPolicy.xml");
-			Logger.ErrorFormat("Using password policy: {0}", targetTestPasswordPolicyFile);
-			if (File.Exists(targetTestPasswordPolicyFile))
-			{
-				Logger.Error("File already exists!");
-				return;
-			}
-			var contents = File.ReadAllText("Data\\PasswordPolicy.xml");
-			var passwordPolicy = table.CreateInstance<PasswordPolicyInfo>();
-
-			contents = contents.Replace("_MaxNumberOfAttempts_", passwordPolicy.MaxNumberOfAttempts.ToString());
-			contents = contents.Replace("_InvalidAttemptWindow_", passwordPolicy.InvalidAttemptWindow.ToString());
-			contents = contents.Replace("_PasswordValidForDayCount_", passwordPolicy.PasswordValidForDayCount.ToString());
-			contents = contents.Replace("_PasswordExpireWarningDayCount_", passwordPolicy.PasswordExpireWarningDayCount.ToString());
-
-			if (passwordPolicy.Rule1.Equals("PasswordLengthMin8"))
-			{
-			}
-
-			File.WriteAllText(targetTestPasswordPolicyFile, contents);
+			// Handled within the test controller when a policy is enabled through UsePasswordPolicy argument.
 		}
-
-		public class PasswordPolicyInfo
-		{
-			public int MaxNumberOfAttempts { get; set; }
-			public int InvalidAttemptWindow { get; set; }
-			public int PasswordValidForDayCount { get; set; }
-			public int PasswordExpireWarningDayCount { get; set; }
-			public string Rule1 { get; set; }
-		}
-
-
 	}
 }
