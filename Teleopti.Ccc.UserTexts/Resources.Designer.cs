bb47b46fd31006d8abd8ee26880ab06add3421e4
Oskar Ekelius <oskar.ekelius@teleopti.com>
2019-01-03 10:12:47 +0100

    #79207 Translations

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -21766,6 +21766,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string NoAgentsWithinPlanningGroup {
             get {
                 return ResourceManager.GetString("NoAgentsWithinPlanningGroup", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to There are no agents in this planning group. Please review the filters. .
+        /// </summary>
+        public static string NoAgentsWithinPlanningGroupReviewFilters {
+            get {
+                return ResourceManager.GetString("NoAgentsWithinPlanningGroupReviewFilters", resourceCulture);
             }
         }
         
