e9cea85c2bba67d61644919571f176d42ba9a553
Magnus Wedmark <magnus.wedmark@teleopti.com>
2019-01-08 13:26:20 +0100

    Removed unused variables

--- Teleopti.Ccc.DBManager/Teleopti.Ccc.DBManager.Library/ServerVersionHelper.cs
+++ Teleopti.Ccc.DBManager/Teleopti.Ccc.DBManager.Library/ServerVersionHelper.cs
@@ -2,8 +2,6 @@ namespace Teleopti.Ccc.DBManager.Library
 {
 	public class ServerVersionHelper
 	{
-		private const string AzureEdition = "SQL Azure";
-		
 		private readonly ExecuteSql _masterExecuteSql;
 
 		public ServerVersionHelper(ExecuteSql masterExecuteSql)
@@ -13,7 +11,6 @@ namespace Teleopti.Ccc.DBManager.Library
 
 		public SqlVersion Version()
 		{
-			const string determineAzureSql = "IF (SELECT CONVERT(NVARCHAR(200), SERVERPROPERTY('edition'))) = @azure_edition SELECT 1 ELSE SELECT 0";
 			const string majorProductVersionSql = "select CAST(LEFT(CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max)),CHARINDEX('.',CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max))) - 1) AS int)";
 
 			return
