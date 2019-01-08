cd4d615c74337816d924742947ce294512154d32
Johan Öbrink <johan.erik.obrink@gmail.com>
2019-01-08 10:16:53 +0100

    WIP - frontend

---
+++ Teleopti.Wfm.Administration/Scripts/App/NotificationSettings/EmailSettingsComponent.js
@@ -0,0 +1,17 @@
+﻿(function () {
+	'use strict';
+	function EmailSettingsController($scope) {
+		var vm = this;
+		vm.Host = 'Arne';
+
+		// This would be loaded by $http etc.
+		console.log('batman nananannanananan');
+	}
+
+	EmailSettingsController.$inject = ['$scope'];
+
+	angular.module('emailSettingsModule').component('emailSettings', {
+		templateUrl: '/Scripts/App/NotificationSettings/emailSettings.template.html',
+		controller: EmailSettingsController
+	});
+})();
---
+++ Teleopti.Wfm.Administration/Scripts/App/NotificationSettings/SmsSettingsComponent.js
@@ -0,0 +1,16 @@
+﻿(function () {
+	'use strict';
+	function SmsSettingsController($scope) {
+		var vm = this;
+
+		// This would be loaded by $http etc.
+		console.log('batman sms nananannanananan');
+	}
+
+	SmsSettingsController.$inject = ['$scope'];
+
+	angular.module('smsSettingsModule').component('smsSettings', {
+		templateUrl: '/Scripts/App/NotificationSettings/smsSettings.template.html',
+		controller: SmsSettingsController
+	});
+})();
