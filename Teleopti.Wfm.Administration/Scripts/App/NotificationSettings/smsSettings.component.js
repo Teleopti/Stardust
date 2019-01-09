e00b6028f8a315f42943e66680d1f3461b519fa1
Johan Öbrink <johan.erik.obrink@gmail.com>
2019-01-09 10:14:47 +0100

    wip postdata sms

--- Teleopti.Wfm.Administration/Scripts/App/NotificationSettings/smsSettings.component.js
+++ Teleopti.Wfm.Administration/Scripts/App/NotificationSettings/smsSettings.component.js
@@ -2,18 +2,34 @@
 	'use strict';
 	function SmsSettingsController($scope, smsSettingsService) {
 		var vm = this;
+		vm.error = false;
 
 		vm.LoadSmsSettings = function () {
 			console.log('sms', smsSettingsService);
 			return smsSettingsService.get().then(function (data) {
-				console.log(data);
-				vm.smsSettings = data;
+				if (data.error) {
+					vm.error = true;
+					vm.message = data.message;
+					//Error when fetching settings, should it be the same error as when the form fails?
+				} else {
+					vm.smsSettings = data;
+				}
+				
 				return vm.smsSettings;
 			});
 		}
 
 		vm.PostSmsSettings = function(data) {
-			console.log(data);
+			return smsSettingsService.post(vm.smsSettings).then(function (data) {
+				if (data.error) {
+					vm.error = true;
+					vm.message = data.message;
+				} else {
+					vm.success = true;
+					vm.error = false;
+					vm.message = data.message;
+				}
+			});
 		}
 
 		vm.LoadSmsSettings();
--- Teleopti.Wfm.Administration/Scripts/App/NotificationSettings/smsSettings.service.js
+++ Teleopti.Wfm.Administration/Scripts/App/NotificationSettings/smsSettings.service.js
@@ -9,7 +9,8 @@
         var vm = this;
 
         var smsSettings = {
-            get: get
+			get: get,
+			post: post
         };
 
         return smsSettings;
@@ -22,8 +23,36 @@
             }
 
             function getSmsSettingsFailed(error) {
-                console.log('failed' + error.data);
+				return {
+					error: true,
+					message: "Something went wrong in loading data"
+				};
             }
-        }
+		}
+
+		function post(data) {
+			return $http(
+				{
+					url: '/smsnotificationSettings.json',
+					method: 'POST',
+					data: data
+				}
+			).then(postSmsSettings).catch(postSmsSettingsFailed);
+
+			function postSmsSettings(response) {
+				return {
+					error: false,
+					message: "Settings saved",
+					data: response.data
+				};
+			}
+
+			function postSmsSettingsFailed(error) {
+				return {
+					error: true,
+					message: "Something went wrong in saving data"
+				};
+			}
+		}
     }
 })();
