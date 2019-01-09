6a4d0252ca28469f5c4b92bcd105d0a3c899b345
Jianfeng Zhu <jianfeng.zhu@teleopti.com>
2019-01-09 16:16:19 +0800

    story #79899: front end - assign calendar to site

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/system-settings/components/bank-holiday-calendar-assign-to-sites/bank-holiday-calendar-assign-to-sites.component.spec.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/system-settings/components/bank-holiday-calendar-assign-to-sites/bank-holiday-calendar-assign-to-sites.component.spec.ts
@@ -12,7 +12,7 @@ import { BankHolidayCalendarAssignToSitesComponent } from './bank-holiday-calend
 
 import { GroupPageService } from 'src/app/shared/services/group-page-service';
 
-describe('BankHolidayCalendarAssignToSitesComponent', () => {
+fdescribe('BankHolidayCalendarAssignToSitesComponent', () => {
 	let fixture: ComponentFixture<BankHolidayCalendarAssignToSitesComponent>;
 	let document: Document;
 	let component: BankHolidayCalendarAssignToSitesComponent;
@@ -46,31 +46,92 @@ describe('BankHolidayCalendarAssignToSitesComponent', () => {
 	});
 
 	it('should render sites list', () => {
-		let groupPagesReq = httpTestingController.match(
-			'../api/GroupPage/AvailableGroupPages?date=' + moment().format('YYYY-MM-DD')
-		);
-		groupPagesReq[0].flush({
-			BusinessHierarchy: [
-				{
-					Children: [{ Name: 'BTS', Id: '9d013613-7c79-4621-b166-a39a00b9d634' }],
-					Id: '7a6c0754-4de8-48fb-8aee-a39a00b9d1c3',
-					Name: 'BTS'
-				},
-				{
-					Children: [{ Name: 'Dubai 1', Id: '3f6236f8-dfb0-4b44-93ed-a7ba00f816bf' }],
-					Id: '6bf99381-0110-4866-a3f6-a7ba00f7fdd4',
-					Name: 'Dubai'
-				}
-			],
-			GroupPages: []
-		});
+		httpTestingController
+			.match('../api/GroupPage/AvailableGroupPages?date=' + moment().format('YYYY-MM-DD'))[0]
+			.flush({
+				BusinessHierarchy: [
+					{
+						Children: [{ Name: 'BTS', Id: '9d013613-7c79-4621-b166-a39a00b9d634' }],
+						Id: '7a6c0754-4de8-48fb-8aee-a39a00b9d1c3',
+						Name: 'BTS'
+					},
+					{
+						Children: [{ Name: 'Dubai 1', Id: '3f6236f8-dfb0-4b44-93ed-a7ba00f816bf' }],
+						Id: '6bf99381-0110-4866-a3f6-a7ba00f7fdd4',
+						Name: 'Dubai'
+					}
+				],
+				GroupPages: []
+			});
 
 		fixture.detectChanges();
 
 		let container = document.getElementsByClassName('bank-holiday-calendar-assign-to-sites')[0];
-		let listItems = container.getElementsByTagName('nz-list-item');
+		let list = container.getElementsByClassName('bank-holiday-calendar-sites-list')[0];
+		let listItems = list.getElementsByTagName('nz-list-item');
 
 		expect(listItems[0].innerHTML.indexOf('BTS') > -1).toBeTruthy();
 		expect(listItems[1].innerHTML.indexOf('Dubai') > -1).toBeTruthy();
+	});
+
+	it('should be able to select a calendar for a site', () => {
+		component.bankHolidayCalendarsList = [
+			{
+				Id: 'e0e97b97-1f4c-4834-9cc1-a9c3003b10df',
+				Name: 'Bank holiday calendar',
+				CurrentYearIndex: 0,
+				Years: [
+					{
+						Year: '2013',
+						Dates: [
+							{
+								Id: '1a9e52aa-ca90-42a0-aa6d-a9c3003b10df',
+								Date: '2013-01-09',
+								Description: 'BankHoliday1',
+								IsDeleted: false
+							},
+							{
+								Id: '876b72ef-4238-423a-a05b-a9c3003b10df',
+								Date: '2013-01-10',
+								Description: 'BankHoliday2',
+								IsDeleted: false
+							}
+						]
+					}
+				]
+			}
+		];
+		httpTestingController
+			.match('../api/GroupPage/AvailableGroupPages?date=' + moment().format('YYYY-MM-DD'))[0]
+			.flush({
+				BusinessHierarchy: [
+					{
+						Children: [{ Name: 'BTS', Id: '9d013613-7c79-4621-b166-a39a00b9d634' }],
+						Id: '7a6c0754-4de8-48fb-8aee-a39a00b9d1c3',
+						Name: 'BTS'
+					},
+					{
+						Children: [{ Name: 'Dubai 1', Id: '3f6236f8-dfb0-4b44-93ed-a7ba00f816bf' }],
+						Id: '6bf99381-0110-4866-a3f6-a7ba00f7fdd4',
+						Name: 'Dubai'
+					}
+				],
+				GroupPages: []
+			});
+
+		fixture.detectChanges();
+
+		let container = document.getElementsByClassName('bank-holiday-calendar-assign-to-sites')[0];
+		let list = container.getElementsByClassName('bank-holiday-calendar-sites-list')[0];
+		let listItems = list.getElementsByTagName('nz-list-item');
+
+		listItems[0].getElementsByTagName('input')[0].dispatchEvent(new Event('click'));
+
+		document.getElementsByClassName('ant-select-dropdown-menu-item')[0].dispatchEvent(new Event('click'));
+
+		httpTestingController.match('../api/SiteBankHolidayCalendars/Update')[0].flush('true');
+		fixture.detectChanges();
+
+		expect(listItems[0].innerHTML.indexOf('BTS') > -1).toBeTruthy();
 	});
 });
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/system-settings/components/bank-holiday-calendar-assign-to-sites/bank-holiday-calendar-assign-to-sites.component.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/system-settings/components/bank-holiday-calendar-assign-to-sites/bank-holiday-calendar-assign-to-sites.component.ts
@@ -1,20 +1,63 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit, Input } from '@angular/core';
+import { NzNotificationService } from 'ng-zorro-antd';
+
 import { GroupPageService } from 'src/app/shared/services/group-page-service';
-import { GroupPageSite } from 'src/app/shared/interface';
+import { GroupPageSiteItem } from 'src/app/shared/interface';
+import { BankHolidayCalendarItem, UpdateSiteBankHolidayCalendarsFormData } from '../../interface';
+import { BankCalendarDataService } from '../../shared';
 
 @Component({
 	selector: 'bank-holiday-calendar-assign-to-sites',
 	templateUrl: './bank-holiday-calendar-assign-to-sites.component.html',
 	styleUrls: ['./bank-holiday-calendar-assign-to-sites.component.scss'],
-	providers: [GroupPageService]
+	providers: [GroupPageService, BankCalendarDataService]
 })
 export class BankHolidayCalendarAssignToSitesComponent implements OnInit {
-	sitesList: GroupPageSite[];
-	constructor(private groupPageService: GroupPageService) {}
+	@Input() bankHolidayCalendarsList: BankHolidayCalendarItem[];
+
+	sitesList: GroupPageSiteItem[];
+	selectedBankHolidayCalendarId: string;
+
+	constructor(
+		private translate: TranslateService,
+		private noticeService: NzNotificationService,
+		private groupPageService: GroupPageService,
+		private bankCalendarDataService: BankCalendarDataService
+	) {}
 
 	ngOnInit(): void {
 		this.groupPageService.getAvailableGroupPagesForDate(moment().format('YYYY-MM-DD')).subscribe(result => {
-			this.sitesList = result.BusinessHierarchy;
+			this.sitesList = result.BusinessHierarchy as GroupPageSiteItem[];
+
+			this.bankCalendarDataService.getSiteBankHolidayCalendars().subscribe(siteCalendars => {
+				console.log(siteCalendars);
+			});
 		});
+	}
+
+	updateCalendarForSite(calId: string, site: GroupPageSiteItem) {
+		let data: UpdateSiteBankHolidayCalendarsFormData = {
+			Settings: [{ Site: site.Id, Calendars: [calId] }]
+		};
+		this.bankCalendarDataService.updateCalendarForSite(data).subscribe(
+			result => {
+				if (result) {
+					console.log('saved');
+				} else {
+					this.updateError();
+				}
+			},
+			error => {
+				this.updateError();
+			}
+		);
+	}
+
+	updateError() {
+		this.noticeService.error(
+			this.translate.instant('Error'),
+			this.translate.instant('AnErrorOccurredPleaseCheckTheNetworkConnectionAndTryAgain')
+		);
 	}
 }
