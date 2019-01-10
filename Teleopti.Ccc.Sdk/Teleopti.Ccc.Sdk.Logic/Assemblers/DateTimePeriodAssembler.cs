bcce1e68471bf38bb054379d5d689bf7973c6e4c
Mathias Stenbom <mathias@stenbom.com>
2019-01-10 13:42:28 +0100

    Remove IPerson dependency from ITeleoptiPrincipal. Step 1. Attempt 2. Fix 1.

--- Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/Assemblers/DateTimePeriodAssembler.cs
+++ Teleopti.Ccc.Sdk/Teleopti.Ccc.Sdk.Logic/Assemblers/DateTimePeriodAssembler.cs
@@ -8,21 +8,21 @@ namespace Teleopti.Ccc.Sdk.Logic.Assemblers
 {
 	public class DateTimePeriodAssembler : Assembler<DateTimePeriod,DateTimePeriodDto>, IDateTimePeriodAssembler
 	{
-        public DateTimePeriodAssembler()
-        {
-            TimeZone = TeleoptiPrincipal.CurrentPrincipal.Regional.TimeZone;
-        }
-
         public TimeZoneInfo TimeZone { get; set; }
 
+		private TimeZoneInfo timeZone()
+		{
+			return TimeZone ?? TeleoptiPrincipal.CurrentPrincipal.Regional.TimeZone;
+		}
+		
         public override DateTimePeriodDto DomainEntityToDto(DateTimePeriod entity)
         {
             return new DateTimePeriodDto
                        {
                            UtcStartTime = entity.StartDateTime,
                            UtcEndTime = entity.EndDateTime,
-                           LocalStartDateTime = entity.StartDateTimeLocal(TimeZone),
-                           LocalEndDateTime = entity.EndDateTimeLocal(TimeZone)
+                           LocalStartDateTime = entity.StartDateTimeLocal(timeZone()),
+                           LocalEndDateTime = entity.EndDateTimeLocal(timeZone())
                        };
         }
 
@@ -31,7 +31,7 @@ namespace Teleopti.Ccc.Sdk.Logic.Assemblers
             if (dto.UtcStartTime == DateTime.MinValue)
             {
                 return TimeZoneHelper.NewUtcDateTimePeriodFromLocalDateTime(dto.LocalStartDateTime,
-                                                                              dto.LocalEndDateTime, TimeZone);
+                                                                              dto.LocalEndDateTime, timeZone());
             }
             if (dto.UtcStartTime != DateTime.MinValue)
             {
