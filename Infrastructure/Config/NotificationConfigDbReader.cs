9c3a3f6eca4c0968fa14612ae12bc3965cb44a1f
Robin Karlsson <robin.karlsson@teleopti.com>
2019-01-11 20:28:56 +0100

    Trying to resolve red build, should probably be another test case for this one

--- Infrastructure/Config/NotificationConfigDbReader.cs
+++ Infrastructure/Config/NotificationConfigDbReader.cs
@@ -9,93 +9,110 @@ namespace Teleopti.Ccc.Infrastructure.Config
 	public class NotificationConfigDbReader : INotificationConfigReader
 	{
 		private readonly IServerConfigurationRepository _serverRepo;
-		private readonly Dictionary<ServerConfigurationKey, string> _config;
+		private readonly Dictionary<ServerConfigurationKey, string> _config = new Dictionary<ServerConfigurationKey, string>();
+
+		private static readonly object LockObj = new object();
 
 		public NotificationConfigDbReader(IServerConfigurationRepository serverRepo)
 		{
 			_serverRepo = serverRepo;
-			_config = new Dictionary<ServerConfigurationKey, string>();
-			loadConfig();
-			HasLoadedConfig = true;
 		}
 
 		private void loadConfig()
 		{
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderUrl);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderUser);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderPassword);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderFrom);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderClass);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderAssembly);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderApiId);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderData);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderFindSuccessOnError);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderErrorCode);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderSuccessCode);
-			AddConfigEntry(ServerConfigurationKey.NotificationExtProviderSkipSearch);
-			AddConfigEntry(ServerConfigurationKey.NotificationSmtpHost);
-			AddConfigEntry(ServerConfigurationKey.NotificationSmtpPort);
-			AddConfigEntry(ServerConfigurationKey.NotificationSmtpUseSsl);
-			AddConfigEntry(ServerConfigurationKey.NotificationSmtpUser);
-			AddConfigEntry(ServerConfigurationKey.NotificationSmtpPassword);
-			AddConfigEntry(ServerConfigurationKey.NotificationSmtpUseRelay);
-			AddConfigEntry(ServerConfigurationKey.NotificationContentType);
-			AddConfigEntry(ServerConfigurationKey.NotificationEncodingName);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderUrl);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderUser);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderPassword);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderFrom);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderClass);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderAssembly);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderApiId);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderData);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderFindSuccessOnError);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderErrorCode);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderSuccessCode);
+			addConfigEntry(ServerConfigurationKey.NotificationExtProviderSkipSearch);
+			addConfigEntry(ServerConfigurationKey.NotificationSmtpHost);
+			addConfigEntry(ServerConfigurationKey.NotificationSmtpPort);
+			addConfigEntry(ServerConfigurationKey.NotificationSmtpUseSsl);
+			addConfigEntry(ServerConfigurationKey.NotificationSmtpUser);
+			addConfigEntry(ServerConfigurationKey.NotificationSmtpPassword);
+			addConfigEntry(ServerConfigurationKey.NotificationSmtpUseRelay);
+			addConfigEntry(ServerConfigurationKey.NotificationContentType);
+			addConfigEntry(ServerConfigurationKey.NotificationEncodingName);
+			HasLoadedConfig = true;
 		}
 
-		private void AddConfigEntry(ServerConfigurationKey key)
+		private void addConfigEntry(ServerConfigurationKey key)
 		{
 			var dbValue = _serverRepo.Get(key.ToString()) ?? string.Empty;
 			_config.Add(key, dbValue);
 		}
 
-		public bool HasLoadedConfig { get; }
+		public bool HasLoadedConfig { get; private set; }
+
+		private string readConfig(ServerConfigurationKey key)
+		{
+			string val;
+			if (_config.TryGetValue(key, out val)) return val;
+
+			lock (LockObj)
+			{
+				if (!HasLoadedConfig)
+				{
+					loadConfig();
+				}
+			}
+
+			_config.TryGetValue(key, out val);
+			return val;
+		}
 
 		public Uri Url
 		{
 			get
 			{
-				var dbValue = _config[ServerConfigurationKey.NotificationExtProviderUrl];
+				var dbValue = readConfig(ServerConfigurationKey.NotificationExtProviderUrl);
 				return string.IsNullOrEmpty(dbValue) ? null : new Uri(dbValue);
 			}
 		}
 
-		public string User => _config[ServerConfigurationKey.NotificationExtProviderUser];
+		public string User => readConfig(ServerConfigurationKey.NotificationExtProviderUser);
 
-		public string Password => _config[ServerConfigurationKey.NotificationExtProviderPassword];
+		public string Password => readConfig(ServerConfigurationKey.NotificationExtProviderPassword);
 
-		public string From => _config[ServerConfigurationKey.NotificationExtProviderFrom];
+		public string From => readConfig(ServerConfigurationKey.NotificationExtProviderFrom);
 
-		public string ClassName => _config[ServerConfigurationKey.NotificationExtProviderClass];
+		public string ClassName => readConfig(ServerConfigurationKey.NotificationExtProviderClass);
 
-		public string Assembly => _config[ServerConfigurationKey.NotificationExtProviderAssembly];
+		public string Assembly => readConfig(ServerConfigurationKey.NotificationExtProviderAssembly);
 
-		public string Api => _config[ServerConfigurationKey.NotificationExtProviderApiId];
+		public string Api => readConfig(ServerConfigurationKey.NotificationExtProviderApiId);
 
-		public string Data => _config[ServerConfigurationKey.NotificationExtProviderData];
+		public string Data => readConfig(ServerConfigurationKey.NotificationExtProviderData);
 
-		public string FindSuccessOrError => _config[ServerConfigurationKey.NotificationExtProviderFindSuccessOnError];
+		public string FindSuccessOrError => readConfig(ServerConfigurationKey.NotificationExtProviderFindSuccessOnError);
 
-		public string ErrorCode => _config[ServerConfigurationKey.NotificationExtProviderErrorCode];
+		public string ErrorCode => readConfig(ServerConfigurationKey.NotificationExtProviderErrorCode);
 
-		public string SuccessCode => _config[ServerConfigurationKey.NotificationExtProviderSuccessCode];
+		public string SuccessCode => readConfig(ServerConfigurationKey.NotificationExtProviderSuccessCode);
 
-		public bool SkipSearch => bool.TryParse(_config[ServerConfigurationKey.NotificationExtProviderSkipSearch], out var value) ? value : false;
+		public bool SkipSearch => bool.TryParse(readConfig(ServerConfigurationKey.NotificationExtProviderSkipSearch), out var value) && value;
 
-		public string SmtpHost => _config[ServerConfigurationKey.NotificationSmtpHost];
+		public string SmtpHost => readConfig(ServerConfigurationKey.NotificationSmtpHost);
 
-		public int SmtpPort => int.TryParse(_config[ServerConfigurationKey.NotificationSmtpPort], out var value) ? value : -1;
+		public int SmtpPort => int.TryParse(readConfig(ServerConfigurationKey.NotificationSmtpPort), out var value) ? value : -1;
 
-		public bool SmtpUseSsl => bool.TryParse(_config[ServerConfigurationKey.NotificationSmtpUseSsl], out var value) ? value : false;
+		public bool SmtpUseSsl => bool.TryParse(readConfig(ServerConfigurationKey.NotificationSmtpUseSsl), out var value) && value;
 
-		public string SmtpUser => _config[ServerConfigurationKey.NotificationSmtpUser];
+		public string SmtpUser => readConfig(ServerConfigurationKey.NotificationSmtpUser);
 
-		public string SmtpPassword => _config[ServerConfigurationKey.NotificationSmtpPassword];
+		public string SmtpPassword => readConfig(ServerConfigurationKey.NotificationSmtpPassword);
 
-		public bool SmtpUseRelay => bool.TryParse(_config[ServerConfigurationKey.NotificationSmtpUseRelay], out var value) ? value : false;
+		public bool SmtpUseRelay => bool.TryParse(readConfig(ServerConfigurationKey.NotificationSmtpUseRelay), out var value) && value;
 
-		public string ContentType => _config[ServerConfigurationKey.NotificationContentType];
+		public string ContentType => readConfig(ServerConfigurationKey.NotificationContentType);
 
-		public string EncodingName => _config[ServerConfigurationKey.NotificationEncodingName];
+		public string EncodingName => readConfig(ServerConfigurationKey.NotificationEncodingName);
 	}
 }
