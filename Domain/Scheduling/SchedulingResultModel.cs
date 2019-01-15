e5ef96d2d55838a768cedb76d537d38b4236bbf3
Roger Kratz <roger.kratz@gmail.com>
2019-01-15 13:20:25 +0100

    removing not used BusinessRulesValidationResult

--- Domain/Scheduling/SchedulingResultModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Teleopti.Ccc.Domain.Scheduling
-{
-	public struct BusinessRulesValidationResult
-	{
-		public string Name { get; set; }
-		public string Message { get; set; }
-		public string BusinessRuleCategoryText { get; set; }
-	}
-}
