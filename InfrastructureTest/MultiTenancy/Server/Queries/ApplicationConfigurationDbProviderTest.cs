80f73ce4b7feaa60295869ea02d23a0a215dd8fe
Magnus Ekström <magnus.ekstrom@teleopti.com>
2019-01-16 20:08:29 +0100

    Build fix?

--- InfrastructureTest/MultiTenancy/Server/Queries/ApplicationConfigurationDbProviderTest.cs
+++ InfrastructureTest/MultiTenancy/Server/Queries/ApplicationConfigurationDbProviderTest.cs
@@ -18,19 +18,20 @@ namespace Teleopti.Ccc.InfrastructureTest.MultiTenancy.Server
 		private TenantUnitOfWorkManager tenantUnitOfWorkManager;
 		private PersonInfo personInfo;
 		private ApplicationConfigurationDbProvider Target;
+		private FakeHttpContext fakeContext;
 		public CurrentHttpContext HttpContext;
 		public PersistTenant Persist;
 		
+
 		protected override void SetupForRepositoryTest()
 		{
 			base.SetupForRepositoryTest();
 			tenantUnitOfWorkManager = TenantUnitOfWorkManager.Create(InfraTestConfigReader.ConnectionString);
 			tenantUnitOfWorkManager.EnsureUnitOfWorkIsStarted();
 			personInfo = new PersonInfo();
-			var fakeContext = new FakeHttpContext();
+			fakeContext = new FakeHttpContext();
 			fakeContext.Items.Add(WebTenantAuthenticationConfiguration.PersonInfoKey, personInfo);
 			HttpContext = new CurrentHttpContext();
-			HttpContext.GloballyUse(fakeContext);
 			Persist = new PersistTenant(tenantUnitOfWorkManager);
 
 			Target = new ApplicationConfigurationDbProvider(new ServerConfigurationRepository(tenantUnitOfWorkManager),
@@ -46,51 +47,60 @@ namespace Teleopti.Ccc.InfrastructureTest.MultiTenancy.Server
 		[Test]
 		public void ShouldGetAllConfigurationData()
 		{
-			var appConfig = Target.GetConfiguration();
+			using (HttpContext.OnThisThreadUse(fakeContext))
+			{
+				var appConfig = Target.GetConfiguration();
 
-			var serverKey = "ServerKey";
-			var tenantKey = "TenantKey";
-			var serverRepo = new ServerConfigurationRepository(tenantUnitOfWorkManager);
-			serverRepo.Update(serverKey, "ServerValue");
-			personInfo.Tenant.SetApplicationConfig(tenantKey, "TenantValue");
-			Persist.Persist(personInfo.Tenant);
-			tenantUnitOfWorkManager.CurrentSession().Flush();
+				var serverKey = "ServerKey";
+				var tenantKey = "TenantKey";
+				var serverRepo = new ServerConfigurationRepository(tenantUnitOfWorkManager);
+				serverRepo.Update(serverKey, "ServerValue");
+				personInfo.Tenant.SetApplicationConfig(tenantKey, "TenantValue");
+				Persist.Persist(personInfo.Tenant);
+				tenantUnitOfWorkManager.CurrentSession().Flush();
 
-			var acAfterUpdate = Target.GetConfiguration();
+				var acAfterUpdate = Target.GetConfiguration();
 
-			Assert.AreEqual(acAfterUpdate.Server.Count, appConfig.Server.Count + 1);
-			Assert.AreEqual(acAfterUpdate.Tenant.Count, appConfig.Tenant.Count + 1);
-			acAfterUpdate.Server.ContainsKey(serverKey).Should().Be.True();
-			acAfterUpdate.Tenant.ContainsKey(tenantKey).Should().Be.True();
+				Assert.AreEqual(acAfterUpdate.Server.Count, appConfig.Server.Count + 1);
+				Assert.AreEqual(acAfterUpdate.Tenant.Count, appConfig.Tenant.Count + 1);
+				acAfterUpdate.Server.ContainsKey(serverKey).Should().Be.True();
+				acAfterUpdate.Tenant.ContainsKey(tenantKey).Should().Be.True();
+			}
 		}
 
 		[Test]
 		public void ShouldGetCorrectConfigurationData()
 		{
-			var server = new { Key = "ServerKey", Value = "ServerValue"};
-			var tenant = new { Key = "TenantKey", Value = "TenantValue"};
-			var serverRepo = new ServerConfigurationRepository(tenantUnitOfWorkManager);
-			serverRepo.Update(server.Key, server.Value);
-			personInfo.Tenant.SetApplicationConfig(tenant.Key, tenant.Value);
-			Persist.Persist(personInfo.Tenant);
-			tenantUnitOfWorkManager.CurrentSession().Flush();
+			using (HttpContext.OnThisThreadUse(fakeContext))
+			{
+				var server = new {Key = "ServerKey", Value = "ServerValue"};
+				var tenant = new {Key = "TenantKey", Value = "TenantValue"};
+				var serverRepo = new ServerConfigurationRepository(tenantUnitOfWorkManager);
+				serverRepo.Update(server.Key, server.Value);
+				personInfo.Tenant.SetApplicationConfig(tenant.Key, tenant.Value);
+				Persist.Persist(personInfo.Tenant);
+				tenantUnitOfWorkManager.CurrentSession().Flush();
 
-			var serverRead = Target.TryGetServerValue(server.Key);
-			var tenantRead = Target.TryGetTenantValue(tenant.Key);
+				var serverRead = Target.TryGetServerValue(server.Key);
+				var tenantRead = Target.TryGetTenantValue(tenant.Key);
 
-			Assert.AreEqual(serverRead, server.Value);
-			Assert.AreEqual(tenantRead, tenant.Value);
+				Assert.AreEqual(serverRead, server.Value);
+				Assert.AreEqual(tenantRead, tenant.Value);
+			}
 		}
 
 		[Test]
 		public void ShouldGetDefaultValuesOnMissingConfigurationKey()
 		{
-			var defaultValue = "DEFAULT";
-			var serverRead = Target.TryGetServerValue("InvalidKey", defaultValue);
-			var tenantRead = Target.TryGetTenantValue("InvalidKey", defaultValue);
+			using (HttpContext.OnThisThreadUse(fakeContext))
+			{
+				var defaultValue = "DEFAULT";
+				var serverRead = Target.TryGetServerValue("InvalidKey", defaultValue);
+				var tenantRead = Target.TryGetTenantValue("InvalidKey", defaultValue);
 
-			Assert.AreEqual(serverRead, defaultValue);
-			Assert.AreEqual(tenantRead, defaultValue);
+				Assert.AreEqual(serverRead, defaultValue);
+				Assert.AreEqual(tenantRead, defaultValue);
+			}
 		}
 	}
 }
