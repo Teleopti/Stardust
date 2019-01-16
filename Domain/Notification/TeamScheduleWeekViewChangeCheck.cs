db885b0447f651ffa6ed73fcbe03ccfd113ca849
XiaojuDai <30306712+daixiaoju@users.noreply.github.com>
2019-01-16 16:43:10 +0800

    story80242 send message when have relevant changes;

---
+++ Domain/Notification/TeamScheduleWeekViewChangeCheck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Teleopti.Ccc.Domain.ApplicationLayer;
+using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers;
+using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.ScheduleDayReadModel;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.MessageBroker.Client;
+
+namespace Teleopti.Ccc.Domain.Notification
+{
+	public interface ITeamScheduleWeekViewChangeCheck
+	{
+		bool IsRelevantChange(DateOnly date, IPerson person, ScheduleDayReadModel newReadModel);
+		void InitiateNotify(TeamScheduleWeekViewChangeCheckModel model);
+	}
+
+	public interface ITeamScheduleWeekViewChange { }
+
+	public class TeamScheduleWeekViewChangeCheck : ITeamScheduleWeekViewChangeCheck
+	{
+		private readonly IScheduleDayReadModelRepository _scheduleDayReadModelRepository;
+		private readonly IMessageBrokerComposite _messageBroker;
+
+		public TeamScheduleWeekViewChangeCheck(
+			IScheduleDayReadModelRepository scheduleDayReadModelRepository,
+			ICurrentDataSource currentDataSource,
+			IMessageBrokerComposite messageBroker)
+		{
+			_scheduleDayReadModelRepository = scheduleDayReadModelRepository;
+			_messageBroker = messageBroker;
+		}
+
+		public void InitiateNotify(TeamScheduleWeekViewChangeCheckModel model)
+		{
+			if (IsRelevantChange(model.Date, model.Person, model.NewReadModel))
+			{
+				_messageBroker.Send(
+					model.LogOnDatasource,
+					model.LogOnBusinessUnitId,
+					model.Date.Date,
+					model.Date.Date,
+					Guid.Empty,
+					model.Person.Id.GetValueOrDefault(),
+					typeof(Person),
+					Guid.Empty,
+					typeof(ITeamScheduleWeekViewChange),
+					DomainUpdateType.NotApplicable,
+					null);
+			}
+		}
+
+		public bool IsRelevantChange(DateOnly date, IPerson person, ScheduleDayReadModel newReadModel)
+		{
+			var existingReadModel = _scheduleDayReadModelRepository.ForPerson(date, person.Id.GetValueOrDefault());
+
+			if (existingReadModel?.Workday != newReadModel?.Workday
+				|| existingReadModel?.StartDateTime != newReadModel?.StartDateTime
+				|| existingReadModel?.EndDateTime != newReadModel?.EndDateTime
+				|| existingReadModel?.Label != newReadModel?.Label)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	public class TeamScheduleWeekViewChangeCheckModel : EventWithLogOnContext
+	{
+		public DateOnly Date { get; set; }
+		public IPerson Person { get; set; }
+		public ScheduleDayReadModel NewReadModel { get; set; }
+	}
+}
--- DomainTest/Notification/TeamScheduleWeekViewChangeCheckTest.cs
+++ DomainTest/Notification/TeamScheduleWeekViewChangeCheckTest.cs
@@ -1,25 +1,34 @@
 ﻿using NUnit.Framework;
 using SharpTestsEx;
 using System;
+using System.Linq;
 using Teleopti.Ccc.Domain.ApplicationLayer.ScheduleChangedEventHandlers.ScheduleDayReadModel;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.MessageBroker.Client;
 using Teleopti.Ccc.Domain.Notification;
 using Teleopti.Ccc.TestCommon.FakeData;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
 using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.TestCommon.Messaging;
 
 namespace Teleopti.Ccc.DomainTest.Notification
 {
 	[TestFixture]
 	[DomainTest]
-	public class TeamScheduleWeekViewChangeCheckerTest : IIsolateSystem
+	public class TeamScheduleWeekViewChangeCheckTest : IIsolateSystem
 	{
-		public ITeamScheduleWeekViewChangeChecker Target;
+		public ITeamScheduleWeekViewChangeCheck Target;
 		public FakeScheduleDayReadModelRepository FakeScheduleDayReadModelRepository;
+		public FakeScenarioRepository ScenarioRepository;
+		public FakePersonRepository PersonRepository;
+		public FakeMessageBrokerComposite MessageBrokerComposite;
+		public ICurrentDataSource DataSource;
 
 		public void Isolate(IIsolate isolate)
 		{
-			isolate.UseTestDouble<TeamScheduleWeekViewChangeChecker>().For<ITeamScheduleWeekViewChangeChecker>();
+			isolate.UseTestDouble<TeamScheduleWeekViewChangeCheck>().For<ITeamScheduleWeekViewChangeCheck>();
+			isolate.UseTestDouble<FakeMessageBrokerComposite>().For<IMessageBrokerComposite>();
 		}
 
 		[Test]
@@ -79,7 +88,8 @@ namespace Teleopti.Ccc.DomainTest.Notification
 		}
 
 		[Test]
-		public void ShouldBeRelevantChangeIfTheScheduleStartTimeIsChanged() {
+		public void ShouldBeRelevantChangeIfTheScheduleStartTimeIsChanged()
+		{
 			var date = new DateOnly(2019, 01, 16);
 			var person = PersonFactory.CreatePerson("person", "1");
 			var newReadModel = new ScheduleDayReadModel
@@ -164,7 +174,8 @@ namespace Teleopti.Ccc.DomainTest.Notification
 		}
 
 		[Test]
-		public void ShouldNotBeRelevantChange() {
+		public void ShouldNotBeRelevantChange()
+		{
 			var date = new DateOnly(2019, 01, 16);
 			var person = PersonFactory.CreatePerson("person", "1");
 			var newReadModel = new ScheduleDayReadModel
@@ -190,6 +201,48 @@ namespace Teleopti.Ccc.DomainTest.Notification
 			result.Should().Be.False();
 		}
 
+		[Test]
+		public void ShouldSendCorrectMessage()
+		{
+			var date = new DateOnly(2019, 01, 16);
+			var person = PersonFactory.CreatePerson("person", "1");
+			var newReadModel = new ScheduleDayReadModel
+			{
+				StartDateTime = new DateTime(2019, 01, 16, 8, 0, 0),
+				EndDateTime = new DateTime(2019, 01, 16, 17, 0, 0),
+				PersonId = person.Id.GetValueOrDefault(),
+				Workday = true,
+				Date = date.Date
+			};
 
+			FakeScheduleDayReadModelRepository.SaveReadModel(new ScheduleDayReadModel
+			{
+				StartDateTime = new DateTime(2019, 01, 16, 8, 0, 0),
+				EndDateTime = new DateTime(2019, 01, 16, 18, 0, 0),
+				PersonId = person.Id.GetValueOrDefault(),
+				Workday = true,
+				Date = date.Date
+			});
+
+			var buId = Guid.NewGuid();
+
+			var model = new TeamScheduleWeekViewChangeCheckModel
+			{
+				Date = date,
+				Person = person,
+				NewReadModel = newReadModel,
+				LogOnDatasource = "Teleopti",
+				LogOnBusinessUnitId = buId
+			};
+			Target.InitiateNotify(model);
+
+			var message = MessageBrokerComposite.GetMessages().Single();
+			message.DataSource.Should().Be.EqualTo("Teleopti");
+			message.BusinessUnitId.Should().Be.EqualTo(buId.ToString());
+			message.StartDate.Should().Be.EqualTo("D2019-01-16T00:00:00");
+			message.EndDate.Should().Be.EqualTo("D2019-01-16T00:00:00");
+			message.DomainType.Should().Be.EqualTo(nameof(ITeamScheduleWeekViewChange));
+		}
 	}
+	
 }
