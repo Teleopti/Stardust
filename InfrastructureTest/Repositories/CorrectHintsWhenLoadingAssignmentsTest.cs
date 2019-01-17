d6f3d7949e3d79bf1f7a64a03318e3379ffbfc10
Roger Kratz <roger.kratz@gmail.com>
2019-01-17 16:49:24 +0100

    #79780 Simplify test by using [UnitOfWorkTest]

--- InfrastructureTest/Repositories/CorrectHintsWhenLoadingAssignmentsTest.cs
+++ InfrastructureTest/Repositories/CorrectHintsWhenLoadingAssignmentsTest.cs
@@ -16,12 +16,12 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories
 {
 	[TestFixture(true)]
 	[TestFixture(false)]
-	[InfrastructureTest]
+	[UnitOfWorkTest]
 	public class CorrectHintsWhenLoadingAssignmentsTest : IConfigureToggleManager
 	{
 		private readonly bool _toggle;
 		public IPersonAssignmentRepository PersonAssignmentRepository;
-		public ICurrentUnitOfWorkFactory CurrentUnitOfWorkFactory;
+		public ICurrentUnitOfWork CurrentUnitOfWork;
 
 		public CorrectHintsWhenLoadingAssignmentsTest(bool toggle)
 		{
@@ -32,35 +32,29 @@ namespace Teleopti.Ccc.InfrastructureTest.Repositories
 		public void ShouldForceSeekHintOnLayersWhenLoadingAssignments()
 		{
 			const string pattern = @"left outer join dbo.ShiftLayer\s+(\S+)\s+WITH \(FORCESEEK\)";
-			using (CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			using (var spy = new SqlSpy())
 			{
-				using (var spy = new SqlSpy())
-				{
-					PersonAssignmentRepository.Find(new[]{new Person().WithId(), },
-						DateOnly.Today.ToDateOnlyPeriod(), new Scenario().WithId());
+				PersonAssignmentRepository.Find(new[]{new Person().WithId(), },
+					DateOnly.Today.ToDateOnlyPeriod(), new Scenario().WithId());
 
-					var log = spy.WholeLog();
-					log.Should().Not.Be.Empty();
-					var queryContainsHint = new Regex(pattern).Match(log).Success;
-					queryContainsHint.Should().Be.EqualTo(_toggle);
-				}				
-			}
+				var log = spy.WholeLog();
+				log.Should().Not.Be.Empty();
+				var queryContainsHint = new Regex(pattern).Match(log).Success;
+				queryContainsHint.Should().Be.EqualTo(_toggle);
+			}				
 		}
 		
 		[Test]
 		public void ShouldNotCreateHintByDefault()
 		{
-			using (var uow = CurrentUnitOfWorkFactory.Current().CreateAndOpenUnitOfWork())
+			using (var spy = new SqlSpy())
 			{
-				using (var spy = new SqlSpy())
-				{
-					uow.FetchSession().CreateCriteria<PersonAssignment>()
-						.Fetch("ShiftLayers")
-						.List();
-					var log = spy.WholeLog();
-					log.Should().Not.Be.Empty();
-					log.Should().Not.Contain("WITH (");
-				}				
+				CurrentUnitOfWork.Current().FetchSession().CreateCriteria<PersonAssignment>()
+					.Fetch("ShiftLayers")
+					.List();
+				var log = spy.WholeLog();
+				log.Should().Not.Be.Empty();
+				log.Should().Not.Contain("WITH (");
 			}
 		}
 		
