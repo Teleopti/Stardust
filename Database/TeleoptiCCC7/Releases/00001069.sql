ba307ef213d4f80d2a019c1d3564763f679cece7
Kunning <kunning.mao@teleopti.com>
2019-01-24 11:00:51 +0100

    #79283 default to 0

--- Database/TeleoptiCCC7/Releases/00001069.sql
+++ Database/TeleoptiCCC7/Releases/00001069.sql
@@ -1,2 +1,2 @@
-alter table planninggroup add GroupPageType int not null
-alter table planninggroup add TeamSameType int not null
+alter table planninggroup add GroupPageType int not null default 0
+alter table planninggroup add TeamSameType int not null default 0
