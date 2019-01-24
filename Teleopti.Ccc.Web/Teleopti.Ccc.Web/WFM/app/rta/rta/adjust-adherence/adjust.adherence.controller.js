07a226cdedb85510cc1ca0a955dcaf3c690e9959
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2019-01-24 16:15:40 +0100

    #80594 viewcontroller part 3

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/adjust-adherence/adjust.adherence.controller.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/adjust-adherence/adjust.adherence.controller.js
@@ -5,23 +5,59 @@
         .module('wfm.rta')
         .controller('AdjustAdherenceController', AdjustAdherenceController);
 
-    AdjustAdherenceController.$inject = ['CurrentUserInfo'];
+    AdjustAdherenceController.$inject = ['CurrentUserInfo', '$scope'];
 
-    function AdjustAdherenceController(currentUserInfo) {
+    function AdjustAdherenceController(currentUserInfo, $scope) {
         var vm = this;
+        var startDate, startTime, endDate, endTime;
         vm.showAdjustToNeutralForm = false;
-        
-        vm.startDate = moment(new Date()).add(-1, 'days');
-        vm.endDate = moment(new Date()).add(-1, 'days');
-        
-        vm.startTime = moment(new Date()).set({h: 8, m: 0});
-        vm.endTime = moment(new Date()).set({h: 18, m: 0});
-        
-        currentUserInfo.Load().then(function(data){
+
+        currentUserInfo.Load().then(function (data) {
+            vm.startDate = moment(new Date()).add(-1, 'days');
+            vm.endDate = moment(new Date()).add(-1, 'days');
+
+            vm.startTime = moment(new Date()).set({h: 8, m: 0});
+            vm.endTime = moment(new Date()).set({h: 18, m: 0});
+
             vm.showMeridian = data.DateTimeFormat.ShowMeridian;
+
+            buildSelectedPeriod();
         });
-        
-        vm.toggleAdjustToNeutralForm = function() {
+
+        $scope.$watch(function () {
+            return vm.startDate;
+        }, function (newValue, oldValue) {
+            startDate = moment(newValue);
+            buildSelectedPeriod();
+        });
+
+        $scope.$watch(function () {
+            return vm.startTime;
+        }, function (newValue, oldValue) {
+            startTime = moment(newValue);
+            buildSelectedPeriod();
+        });
+
+        $scope.$watch(function () {
+            return vm.endDate;
+        }, function (newValue, oldValue) {
+            endDate = moment(newValue);
+            buildSelectedPeriod();
+        });
+
+        $scope.$watch(function () {
+            return vm.endTime;
+        }, function (newValue, oldValue) {
+            endTime = moment(newValue);
+            buildSelectedPeriod();
+        });
+
+        function buildSelectedPeriod() {
+            if (startDate && startTime && endDate && endTime)
+                vm.selectedPeriod = startDate.format('L') + ' ' + startTime.format('LT') + ' - ' + endDate.format('L') + ' ' + endTime.format('LT');
+        }
+
+        vm.toggleAdjustToNeutralForm = function () {
             vm.showAdjustToNeutralForm = !vm.showAdjustToNeutralForm;
         }
     }
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/adjust-adherence/adjust.adherence.controller.spec.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/app/rta/rta/adjust-adherence/adjust.adherence.controller.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-rtaTester.describe('AdjustAdherenceController', function (it, fit, xit) {
+rtaTester.fdescribe('AdjustAdherenceController', function (it, fit, xit) {
     it('should not show adjust section on load', function (t) {
         var vm = t.createController();
 
@@ -55,5 +55,41 @@ rtaTester.describe('AdjustAdherenceController', function (it, fit, xit) {
         var vm = t.createController();
 
         expect(vm.showMeridian).toBe(true);
-    })
+    });
+
+    it('should have preselected period', function (t) {
+        var now = new Date('2019-01-22T08:00:00');
+        jasmine.clock().mockDate(now);
+        t.backend.with.currentUserInfo({
+            DateFormatLocale: "sv-SE"
+        });
+        
+        var vm = t.createController();
+        
+        expect(vm.selectedPeriod).toEqual("2019-01-21 08:00 - 2019-01-21 18:00");
+    });
+
+    it('should update preselected period', function (t) {
+        var now = new Date('2019-01-22T08:00:00');
+        jasmine.clock().mockDate(now);
+        t.backend.with.currentUserInfo({
+            DateFormatLocale: "sv-SE"
+        });
+        var vm = t.createController();
+        
+        t.apply(function () {
+            vm.startDate = new Date(2019, 0, 20);
+        });
+        t.apply(function () {
+            vm.startTime = new Date('2019-01-22T09:00:00');
+        });
+        t.apply(function () {
+            vm.endDate = new Date(2019, 0, 20);
+        });
+        t.apply(function () {
+            vm.endTime = new Date('2019-01-22T20:00:00');
+        });
+
+        expect(vm.selectedPeriod).toEqual("2019-01-20 09:00 - 2019-01-20 20:00");
+    });
 });
