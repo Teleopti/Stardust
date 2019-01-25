4f9352702e7ff40e7bb26237e780c1c360c3bc40
Roger Kratz <roger.kratz@gmail.com>
2019-01-25 13:12:49 +0100

    #80988 Adding Teleopti.Wfm.SchedulingTest assembly

---
+++ Teleopti.Wfm.SchdulingTest/ToBeRemoved.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Teleopti.Wfm.SchdulingTest
+{
+	public class ToBeRemoved
+	{
+		[Test]
+		public void TheTest()
+		{
+			1.Should().Be.EqualTo(1);
+		}
+	}
+}
