0a5070df072b41edb6d0b9f5d8c48b1156a66480
Kunning <kunning.mao@teleopti.com>
2019-01-25 11:25:04 +0100

    #79283 moved one test

--- DomainTest/SchedulingScenarios/DayOffOptimization/DayOffOptimizationTeamBlockTest.cs
+++ DomainTest/SchedulingScenarios/DayOffOptimization/DayOffOptimizationTeamBlockTest.cs
@@ -148,5 +148,56 @@ namespace Teleopti.Ccc.DomainTest.SchedulingScenarios.DayOffOptimization
 			shiftsOnSaturday.First().ShiftCategory.Should().Be
 				.EqualTo(shiftsOnSaturday.Last().ShiftCategory);
         }
+		
+		[Test]
+		public void ShouldNotMoveDOsForOneAgentOnlyButChangeAfterEachPeriod([Values(true, false)] bool useTeams)
+		{
+			const int numberOfAttempts = 20;
+			var firstDay = new DateOnly(2015, 10, 12);
+			var activity = ActivityRepository.Has("_");
+			var skill = SkillRepository.Has("skill", activity);
+			var planningPeriod = PlanningPeriodRepository.Has(firstDay, 2);
+			var scenario = ScenarioRepository.Has("_");
+			var team = new Team {Site = new Site("site")};
+			var ruleSet = new WorkShiftRuleSet(new WorkShiftTemplateGenerator(activity, new TimePeriodWithSegment(8, 0, 8, 0, 15), new TimePeriodWithSegment(16, 0, 16, 0, 15), new ShiftCategory("_").WithId()));
+			var agent1 = PersonRepository.Has(new Contract("_"), new ContractSchedule("_"), new PartTimePercentage("_"), team, new SchedulePeriod(firstDay, SchedulePeriodType.Week, 1).NumberOfDaysOff(1), ruleSet, skill);
+			var agent2 = PersonRepository.Has(new Contract("_"), new ContractSchedule("_"), new PartTimePercentage("_"), team, new SchedulePeriod(firstDay, SchedulePeriodType.Week, 1).NumberOfDaysOff(1), ruleSet, skill);
+			BusinessUnitRepository.Has(BusinessUnitFactory.CreateBusinessUnitAndAppend(team).WithId(ServiceLocatorForEntity.CurrentBusinessUnit.Current().Id.Value));
+			SkillDayRepository.Has(skill.CreateSkillDaysWithDemandOnConsecutiveDays(scenario, firstDay,
+				1, 2, 2, 2, 2, 2, 2,
+				1, 2, 2, 2, 2, 2, 2));
+			planningPeriod.PlanningGroup.ModifyDefault(x =>
+			{
+				x.ConsecutiveWorkdays = new MinMax<int>(1, 20); //just to make sure anything goes
+			}); 
+			planningPeriod.PlanningGroup.SetTeamSettings(new TeamSettings()
+			{
+				GroupPageType = GroupPageType.Hierarchy,
+				TeamSameType = TeamSameType.ShiftCategory
+			});
+
+			for (var i = 0; i < numberOfAttempts; i++)
+			{
+				PersonAssignmentRepository.Clear();
+				PersonAssignmentRepository.Has(agent1, scenario, activity, new ShiftCategory("_").WithId(), DateOnlyPeriod.CreateWithNumberOfWeeks(firstDay, 2), new TimePeriod(8, 0, 16, 0));
+				var dayOffTemplate = new DayOffTemplate();
+				PersonAssignmentRepository.GetSingle(firstDay.AddWeeks(0).AddDays(6), agent1).SetDayOff(dayOffTemplate);
+				PersonAssignmentRepository.GetSingle(firstDay.AddWeeks(1).AddDays(6), agent1).SetDayOff(dayOffTemplate);
+				PersonAssignmentRepository.Has(agent2, scenario, activity, new ShiftCategory("_").WithId(), DateOnlyPeriod.CreateWithNumberOfWeeks(firstDay, 2), new TimePeriod(8, 0, 16, 0));
+				PersonAssignmentRepository.GetSingle(firstDay.AddWeeks(0).AddDays(6), agent2).SetDayOff(dayOffTemplate);
+				PersonAssignmentRepository.GetSingle(firstDay.AddWeeks(1).AddDays(6), agent2).SetDayOff(dayOffTemplate);
+
+				Target.DoSchedulingAndDO(planningPeriod.Id.Value);
+
+				var allDOs = PersonAssignmentRepository.LoadAll().Where(x => x.DayOff() != null);
+				var movedD01 = allDOs.Single(x => x.Date == firstDay);
+				var movedD02 = allDOs.Single(x => x.Date == firstDay.AddWeeks(1));
+				if (!movedD01.Person.Equals(movedD02.Person))
+					return;
+			}
+
+			Assert.Fail(
+				$"Tried optimize {numberOfAttempts} number of times but always moving DOs from same agent. Giving up...");
+		}
 	}
 }
