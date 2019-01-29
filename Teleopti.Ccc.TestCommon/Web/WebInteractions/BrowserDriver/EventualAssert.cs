3a2ee93279b155a7dbd59eb25fe09139478a8b0f
Robin Karlsson <robin.karlsson@teleopti.com>
2019-01-29 21:04:41 +0100

    Might solve red build

--- Teleopti.Ccc.TestCommon/Web/WebInteractions/BrowserDriver/EventualAssert.cs
+++ Teleopti.Ccc.TestCommon/Web/WebInteractions/BrowserDriver/EventualAssert.cs
@@ -11,27 +11,19 @@ namespace Teleopti.Ccc.TestCommon.Web.WebInteractions.BrowserDriver
 			ReusableConstraint reusableConstraint = constraint;
 			Exception exception = null;
 			Func<bool> longPollTimeSafeAssert = () =>
+			{
+				Func<Exception, T> failingValue = e =>
 				{
-
-					try
-					{
-						Func<Exception, T> failingValue = e =>
-							{
-								exception = e;
-								return default(T);
-							};
-
-						Func<T> robustValue = () => exceptionCatcher.Action(value.Invoke, failingValue);
-
-						Assert.That(() => robustValue.Invoke(), reusableConstraint);
-						return true;
-					}
-					catch (AssertionException ex)
-					{
-						exception = ex;
-						return false;
-					}
+					exception = e;
+					return default(T);
 				};
+
+				Func<T> robustValue = () => exceptionCatcher.Action(value.Invoke, failingValue);
+
+				var result = robustValue.Invoke();
+				var constraintResult = reusableConstraint.Resolve().ApplyTo(result);
+				return constraintResult.IsSuccess;
+			};
 
 			if (longPollTimeSafeAssert.WaitUntil(poll, timeout)) return;
 
