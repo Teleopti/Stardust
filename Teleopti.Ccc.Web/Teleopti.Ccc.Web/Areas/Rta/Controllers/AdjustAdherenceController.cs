3ee96fb7f8fab4c514653d1a10321579bdcdd2fc
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2019-01-29 12:56:35 +0100

    #80594 controller dummy

---
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/Rta/Controllers/AdjustAdherenceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using Teleopti.Ccc.Domain.Aop;
+using Teleopti.Ccc.Domain.Security.AuthorizationData;
+using Teleopti.Ccc.Web.Filters;
+
+namespace Teleopti.Ccc.Web.Areas.Rta.Controllers
+{
+	[ApplicationFunctionApi(DefinedRaptorApplicationFunctionPaths.AdjustAdherence)]
+	public class AdjustAdherenceController : ApiController
+	{
+		[UnitOfWork,HttpPost, Route("api/Adherence/AdjustPeriod")]
+		public virtual IHttpActionResult AdjustPeriod([FromBody] AdjustAdherenceToNeutralCommand command)
+		{
+			//call command handler
+			return Ok();
+		}	
+		
+		[UnitOfWork, HttpGet, Route("api/Adherence/AdjustedPeriods")]
+		public virtual IHttpActionResult Load()
+		{
+			return Ok(new[] {new AdjustedPeriod() {StartTime = "2019-01-28 12:00", EndTime = "2019-01-28 14:00"}});
+		}					
+	}
+
+	public class AdjustAdherenceToNeutralCommand
+	{
+		public string StartTime;
+		public string EndTime;		
+	}
+
+	public class AdjustedPeriod
+	{
+		public string StartTime;
+		public string EndTime;
+	}
+}
