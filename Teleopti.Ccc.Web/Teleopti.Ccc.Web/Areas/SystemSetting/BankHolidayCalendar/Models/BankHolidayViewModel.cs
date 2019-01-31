2b48b4c5416e3c3c1c6067016b38e05934d2f38b
Mingdi <mingdi.wu@teleopti.com>
2019-01-31 10:25:31 +0800

    trigger build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/SystemSetting/BankHolidayCalendar/Models/BankHolidayViewModel.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/SystemSetting/BankHolidayCalendar/Models/BankHolidayViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-using Teleopti.Ccc.Domain.SystemSetting;
 
 namespace Teleopti.Ccc.Web.Areas.SystemSetting.BankHolidayCalendar.Models
 {
