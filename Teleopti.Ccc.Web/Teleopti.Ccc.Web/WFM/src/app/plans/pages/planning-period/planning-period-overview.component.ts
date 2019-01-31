2c9947e71356dc5a32fa736e60c16e60c2255dda
Kunning <kunning.mao@teleopti.com>
2019-01-31 14:02:04 +0100

    #79112 be able to get scheduling status

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/plans/pages/planning-period/planning-period-overview.component.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/plans/pages/planning-period/planning-period-overview.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject} from '@angular/core';
 import {PlanningPeriodService} from "../../shared";
 import {IStateService} from "angular-ui-router";
+import {TranslateService} from "@ngx-translate/core";
+
 @Component({
 	selector: 'plans-period-overview',
 	templateUrl: './planning-period-overview.component.html',
@@ -10,6 +12,12 @@ import {IStateService} from "angular-ui-router";
 export class PlanningPeriodOverviewComponent {
 
 	ppId: string;
+	schedulingPerformed: boolean = false;
+	status: string='';
+	isScheduled: boolean = false;
+	scheduledAgents: number = 0;
+	timer: any;
+	
 	valLoading: boolean = true;
 
 	dayNodes;
@@ -22,17 +30,22 @@ export class PlanningPeriodOverviewComponent {
 		preValidation: []
 	};
 
-	constructor(private planningPeriodService: PlanningPeriodService, @Inject('$state') private $state: IStateService ) {
+	constructor(private planningPeriodService: PlanningPeriodService, @Inject('$state') private $state: IStateService, private translate: TranslateService) {
 		this.ppId = $state.params.ppId.trim();
 	}
 
 	ngOnInit(){
-		this.planningPeriodService.lastJobResult(this.ppId).subscribe(data=>{
-			let fullSchedulingResult = data.FullSchedulingResult;
+		this.loadLastResult();
+		this.checkState();
+	}
 
-			if (!fullSchedulingResult) return;
-			this.dayNodes = fullSchedulingResult.SkillResultList ? fullSchedulingResult.SkillResultList : undefined;
-		});
+	ngOnDestroy(): void {
+		clearInterval(this.timer);
+	}
+
+	private checkState() {
+		this.checkProgress();
+		this.timer = setInterval(this.checkProgress, 10000);
 	}
 	
 	public launchSchedule(){
@@ -41,7 +54,60 @@ export class PlanningPeriodOverviewComponent {
 		});
 	}
 	
-	private checkProgress(){
-		
+	private checkProgress = ()=>{
+		this.planningPeriodService.lastJobStatus(this.ppId).subscribe((data)=>{
+			let schedulingStatus = data.SchedulingStatus;
+			if (!schedulingStatus || !schedulingStatus.HasJob) {
+				this.schedulingPerformed = false;
+			} else {
+				if (!schedulingStatus.Successful && !schedulingStatus.Failed) {
+					this.schedulingPerformed = true;
+					this.status = this.translate.instant('PresentTenseSchedule');
+					return;
+				}
+				if (schedulingStatus.Failed) {
+					this.schedulingPerformed = false;
+				}
+				if (schedulingStatus.Successful && this.schedulingPerformed) {
+					this.schedulingPerformed = false;
+					this.loadLastResult();
+				}
+			}
+		});
+	};
+	
+	private updateValidationErrorsNumber(){
+		this.valData.totalValNum = 0;
+		this.valData.totalPreValNum = 0;
+		this.valData.totalLastValNum = 0;
+		let pre = this.valData.preValidation;
+		let after = this.valData.scheduleIssues;
+		if (pre.length > 0) {
+			pre.forEach(item =>{
+				if (item.ValidationErrors !== null) this.valData.totalPreValNum += item.ValidationErrors.length;
+			});
+		}
+		if (after.length > 0) {
+			after.forEach(item=>{
+				if (item.ValidationErrors !== null) this.valData.totalLastValNum += item.ValidationErrors.length;
+			});
+		}
+		this.valData.totalValNum = this.valData.totalPreValNum + this.valData.totalLastValNum;
+	}
+	
+	private loadLastResult(){
+		this.planningPeriodService.lastJobResult(this.ppId).subscribe(data=>{
+			let fullSchedulingResult = data.FullSchedulingResult;
+			if (fullSchedulingResult) {
+				this.isScheduled = true;
+				this.scheduledAgents = data.FullSchedulingResult.ScheduledAgentsCount;
+				this.valData.scheduleIssues = data.FullSchedulingResult.BusinessRulesValidationResults;
+				this.updateValidationErrorsNumber();
+				if (!fullSchedulingResult) return;
+				this.dayNodes = fullSchedulingResult.SkillResultList ? fullSchedulingResult.SkillResultList : undefined;
+			} else {
+				this.isScheduled = false;
+			}
+		});
 	}
 }
--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/plans/shared/planningperiod.service.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/plans/shared/planningperiod.service.ts
@@ -7,10 +7,14 @@ export class PlanningPeriodService {
 	constructor(private httpClient: HttpClient) {}
 
 	public lastJobResult(planningPeriodId: string): Observable<any> {
-		return this.httpClient.get('../api/resourceplanner/planningperiod/'+planningPeriodId+'/result') as Observable<any>; 
+		return this.httpClient.get('../api/resourceplanner/planningperiod/'+planningPeriodId+'/result') as Observable<any>;
 	}
 	
 	public launchScheduling(planningPeriodId: string) {
 		return this.httpClient.post('../api/resourceplanner/planningperiod/'+planningPeriodId+'/schedule', {}) as Observable<string>;
+	}
+	
+	public lastJobStatus(planningPeriodId: string){
+		return this.httpClient.get('../api/resourceplanner/planningperiod/'+planningPeriodId+'/status') as Observable<any>;
 	}
 }
