8c46a6bf074a54dbd6ad5e960f16ffd5eb083f51
Roger Kratz <roger.kratz@gmail.com>
2019-02-04 15:05:53 +0100

    remove empty file

--- Teleopti.Wfm.Shared/UnitOfWork/IAggregateRoot_Events.cs
+++
@@ -1,8 +0,0 @@
-//using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
-//
-//namespace Teleopti.Ccc.Domain.Common.EntityBaseTypes
-//{
-//	public interface IAggregateRoot_Events : IAggregateRoot, IPublishEvents
-//	{
-//	}
-//}
