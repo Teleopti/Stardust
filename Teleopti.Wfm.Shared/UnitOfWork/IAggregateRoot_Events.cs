c084b4e382d5c5a4d8ae3f531ba73c6d7b2f9eb5
Mathias Stenbom <mathias@stenbom.com>
2019-02-04 12:13:47 +0100

    Preparatory refact: Cleaning up and clarifying aggregate root inheritance heirarchy step 2/?.

--- Teleopti.Wfm.Shared/UnitOfWork/IAggregateRoot_Events.cs
+++ Teleopti.Wfm.Shared/UnitOfWork/IAggregateRoot_Events.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 
 namespace Teleopti.Ccc.Domain.Common.EntityBaseTypes
 {
-	public interface IAggregateRoot_Events : IAggregateRoot
+	public interface IAggregateRoot_Events : IAggregateRoot, IPublishEvents
 	{
-		void NotifyCommandId(Guid commandId);
-		void NotifyTransactionComplete(DomainUpdateType operation);
-		void NotifyDelete();
-		IEnumerable<IEvent> PopAllEvents(IPopEventsContext context);
-		bool HasEvents();
 	}
 }
