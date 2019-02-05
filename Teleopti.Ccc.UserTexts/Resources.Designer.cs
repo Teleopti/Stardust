1655b0e5b6c5ec57dc862b558cce8630fef2f383
Kunning <kunning.mao@teleopti.com>
2019-02-05 11:40:48 +0100

    #81198 fix test issue: correct the text message

--- Teleopti.Ccc.UserTexts/Resources.Designer.cs
+++ Teleopti.Ccc.UserTexts/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Teleopti.Ccc.UserTexts {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -22486,6 +22486,15 @@ namespace Teleopti.Ccc.UserTexts {
         public static string NoPersonPeriodIsDefinedForTheDate {
             get {
                 return ResourceManager.GetString("NoPersonPeriodIsDefinedForTheDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No planning group found..
+        /// </summary>
+        public static string NoPlanningGroupFound {
+            get {
+                return ResourceManager.GetString("NoPlanningGroupFound", resourceCulture);
             }
         }
         
