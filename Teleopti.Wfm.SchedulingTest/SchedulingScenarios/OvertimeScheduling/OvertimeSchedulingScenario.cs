b24457b9561aa6c74f8f8287929c0d3cc3e12198
Roger Kratz <roger.kratz@gmail.com>
2019-02-06 13:53:03 +0100

    starting to use [DefaultData] in OvertimeScheduling

--- Teleopti.Wfm.SchedulingTest/SchedulingScenarios/OvertimeScheduling/OvertimeSchedulingScenario.cs
+++ Teleopti.Wfm.SchedulingTest/SchedulingScenarios/OvertimeScheduling/OvertimeSchedulingScenario.cs
@@ -3,6 +3,7 @@
 namespace Teleopti.Wfm.SchedulingTest.SchedulingScenarios.OvertimeScheduling
 {
 	[DontSendEventsAtPersist]
+	[DefaultData]
 	public abstract class OvertimeSchedulingScenario 
 	{
 	}
