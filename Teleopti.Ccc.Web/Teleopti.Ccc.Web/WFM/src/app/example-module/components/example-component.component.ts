c97c9e2847e15c9f53dc831a6a339be32886eca7
E.Sundin <Emil.sundin@teleopti.com>
2019-02-07 12:40:55 +0100

    #81182 Fix template

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/example-module/components/example-component.component.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/example-module/components/example-component.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'example-component-selector'
+	selector: 'example-component-selector',
+	template: `
+		Example component with inline template
+	`
 })
 export class ExampleComponent {}
