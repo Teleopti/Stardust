8b4c0f6a0b4a78e8df993cca90b3255b35b0d037
Xinfeng Li <xinfeng.li@teleopti.com>
2019-02-12 14:49:42 +0100

    Story #81536: Improved security with tamper proof userid for the conversation

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/GrantBotApiController.cs
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Controllers/GrantBotApiController.cs
@@ -3,8 +3,10 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Newtonsoft.Json;
 using Teleopti.Ccc.Domain.Config;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
 using Teleopti.Ccc.Domain.MultiTenancy;
+using Teleopti.Ccc.Domain.Security;
 using Teleopti.Ccc.Infrastructure.MultiTenancy.Admin;
 using Teleopti.Ccc.Infrastructure.Web;
 using Teleopti.Ccc.Web.Areas.MyTime.Models.Portal;
@@ -18,14 +20,21 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 		private readonly IHttpServer _httpRequestHandler;
 		private readonly ICurrentHttpContext _currentHttpContext;
 		private readonly IServerConfigurationRepository _serverConfigurationRepository;
+		private readonly SignatureCreator _signatureCreator;
+		private readonly INow _now;
+		private readonly ILoggedOnUser _loggedOnUser;
 
-		public GrantBotApiController(IConfigReader configReader,
-			IHttpServer httpRequestHandler, ICurrentHttpContext currentHttpContext, IServerConfigurationRepository serverConfigurationRepository)
+		public GrantBotApiController(IConfigReader configReader, IHttpServer httpRequestHandler,
+			ICurrentHttpContext currentHttpContext, IServerConfigurationRepository serverConfigurationRepository,
+			SignatureCreator signatureCreator, INow now, ILoggedOnUser loggedOnUser)
 		{
 			_configReader = configReader;
 			_httpRequestHandler = httpRequestHandler;
 			_currentHttpContext = currentHttpContext;
 			_serverConfigurationRepository = serverConfigurationRepository;
+			_signatureCreator = signatureCreator;
+			_now = now;
+			_loggedOnUser = loggedOnUser;
 		}
 
 		[HttpGet, Route("api/GrantBot/Config"), TenantUnitOfWork]
@@ -57,8 +66,11 @@ namespace Teleopti.Ccc.Web.Areas.MyTime.Controllers
 				token = JsonConvert.DeserializeObject<DirectLineToken>(body).token;
 			}
 
+			var timestamp = _now.UtcDateTime().Ticks;
 			var config = new GrantBotConfig
 			{
+				Timestamp = timestamp,
+				Signature = _signatureCreator.Create($"{_loggedOnUser.CurrentUser().Id.ToString().ToLowerInvariant()}{timestamp}"),
 				Token = token,
 			};
 			return config;
