c8bfb249da0ffe81d4bae8604d4e606f3ffd6a5a
TOPTINET\EmilW <emil.widell@teleopti.com>
2019-02-15 15:02:10 +0100

    Story #78305 Moved tests to new project

---
+++ Teleopti.Wfm.OvertimeRequest.PerformanceTest/OvertimeRequestPerformanceTest.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using NUnit.Framework;
+using SharpTestsEx;
+using Teleopti.Ccc.Domain.Common;
+using Teleopti.Ccc.Domain.Common.Time;
+using Teleopti.Ccc.Domain.Helper;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Logon;
+using Teleopti.Ccc.Domain.Repositories;
+using Teleopti.Ccc.Domain.ResourceCalculation;
+using Teleopti.Ccc.Domain.Security.AuthorizationEntities;
+using Teleopti.Ccc.Domain.Staffing;
+using Teleopti.Ccc.Domain.UnitOfWork;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+
+namespace Teleopti.Wfm.OvertimeRequest.PerformanceTest
+{
+	[RequestPerformanceTuningTest]
+	public class OvertimeRequestPerformanceTest : PerformanceTestWithOneTimeSetup
+	{
+		public IUpdateStaffingLevelReadModel UpdateStaffingLevel;
+		public MutableNow Now;
+		public WithUnitOfWork WithUnitOfWork;
+		public IDataSourceScope DataSource;
+		public AsSystem AsSystem;
+		public FakeConfigReader ConfigReader;
+		public IPersonRequestRepository PersonRequestRepository;
+		public IStardustJobFeedback StardustJobFeedback;
+		public IOvertimeRequestProcessor OvertimeRequestProcessor;
+		public IMultiplicatorDefinitionSetRepository MultiplicatorDefinitionSetRepository;
+		public UpdateStaffingLevelReadModelStartDate UpdateStaffingLevelReadModelStartDate;
+
+		public ISiteRepository SiteRepository;
+
+		private IList<IPersonRequest> requests;
+		private DateTime _nowDateTime;
+
+		private const string tenantName = "Teleopti WFM";
+		private readonly Guid businessUnitId = new Guid("1fa1f97c-ebff-4379-b5f9-a11c00f0f02b");
+
+		public override void OneTimeSetUp()
+		{
+			var schema = LicenseDataFactory.CreateDefaultActiveLicenseSchemaForTest();
+			LicenseSchema.SetActiveLicenseSchema(tenantName, schema);
+
+			_nowDateTime = new DateTime(2016, 01, 16, 7, 0, 0).Utc();
+			Now.Is(_nowDateTime);
+			using (DataSource.OnThisThreadUse(tenantName))
+				AsSystem.Logon(tenantName, businessUnitId);
+
+			using (var connection = new SqlConnection(ConfigReader.ConnectionString("Tenancy")))
+			{
+				connection.Open();
+				var path = AppDomain.CurrentDomain.BaseDirectory + "/../../" + "Prepare100OvertimeRequests.sql";
+				var script = File.ReadAllText(path);
+
+				using (var command = new SqlCommand(script, connection))
+				{
+					command.ExecuteNonQuery();
+				}
+				connection.Close();
+			}
+
+			var now = Now.UtcDateTime();
+			var period = new DateTimePeriod(now.AddDays(-1), now.AddDays(1));
+			requests = new List<IPersonRequest>();
+			UpdateStaffingLevelReadModelStartDate.RememberStartDateTime(Now.UtcDateTime().AddDays(-1).AddHours(-1));
+			WithUnitOfWork.Do(() =>
+			{
+				MultiplicatorDefinitionSetRepository.LoadAll();
+				SiteRepository.LoadAllWithFetchingOpenHours();
+				UpdateStaffingLevel.Update(period);
+				requests = PersonRequestRepository.FindPersonRequestWithinPeriod(
+					new DateTimePeriod(new DateTime(2016, 01, 16, 16, 0, 0).Utc(), new DateTime(2016, 01, 16, 20, 0, 0).Utc()));
+			});
+		}
+
+		[Test, Ignore("Waiting for a fast lane Build")]
+		public void Run100Requests()
+		{
+			Now.Is("2016-01-16 07:01");
+
+			using (DataSource.OnThisThreadUse(tenantName))
+				AsSystem.Logon(tenantName, businessUnitId);
+			Console.WriteLine($"Will process {requests.Count} requests");
+
+			foreach (var request in requests)
+			{
+				WithUnitOfWork.Do(() =>
+				{
+					OvertimeRequestProcessor.Process(request);
+				});
+			}
+			WithUnitOfWork.Do(() =>
+			{
+				var reqs = PersonRequestRepository.FindPersonRequestWithinPeriod(
+					new DateTimePeriod(new DateTime(2016, 01, 16, 16, 0, 0).Utc(), new DateTime(2016, 01, 16, 20, 0, 0).Utc()));
+				reqs.Count.Should().Be(100);
+			});
+		}
+
+		[Test, Ignore("Waiting for a fast lane Build")]
+		public void Run100RequestsWithRequestPeriodSetting()
+		{
+			Now.Is("2016-01-16 07:01");
+
+			using (DataSource.OnThisThreadUse(tenantName))
+				AsSystem.Logon(tenantName, businessUnitId);
+			Console.WriteLine($"Will process {requests.Count} requests");
+
+			foreach (var request in requests)
+			{
+				WithUnitOfWork.Do(() =>
+				{
+					OvertimeRequestProcessor.Process(request);
+				});
+			}
+			WithUnitOfWork.Do(() =>
+			{
+				var reqs = PersonRequestRepository.FindPersonRequestWithinPeriod(
+					new DateTimePeriod(new DateTime(2016, 01, 16, 16, 0, 0).Utc(), new DateTime(2016, 01, 16, 20, 0, 0).Utc()));
+				reqs.Count.Should().Be(100);
+			});
+		}
+	}
+}
---
+++ Teleopti.Wfm.OvertimeRequest.PerformanceTest/RequestPerformanceTestAttribute.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using Teleopti.Ccc.Domain.Common.Time;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.MessageBroker.Client;
+using Teleopti.Ccc.Infrastructure.Foundation;
+using Teleopti.Ccc.Infrastructure.Hangfire;
+using Teleopti.Ccc.IocCommon;
+using Teleopti.Ccc.TestCommon;
+using Teleopti.Ccc.TestCommon.FakeData;
+using Teleopti.Ccc.TestCommon.IoC;
+using Teleopti.Ccc.Web.Areas.Requests.Core.IOC;
+using Teleopti.Ccc.Web.Areas.TeamSchedule.IoC;
+using Teleopti.Ccc.Web.Core.IoC;
+using Teleopti.Messaging.Client;
+
+namespace Teleopti.Wfm.OvertimeRequest.PerformanceTest
+{
+	public class RequestPerformanceTuningTestAttribute : IoCTestAttribute
+	{
+		protected override FakeConfigReader Config()
+		{
+			var config = base.Config();
+			config.FakeConnectionString("Tenancy", InfraTestConfigReader.ConnectionString);
+			config.FakeConnectionString("Hangfire", InfraTestConfigReader.AnalyticsConnectionString);
+			return config;
+		}
+
+		protected override void Extend(IExtend extend, IocConfiguration configuration)
+		{
+			base.Extend(extend, configuration);
+			extend.AddService<Database>();
+			extend.AddModule(new LegacyRegistrationsFromAnywhere(configuration));
+			extend.AddModule(new RequestsAreaModule());
+			extend.AddModule(new TeamScheduleAreaModule());
+		}
+
+		protected override void Isolate(IIsolate isolate)
+		{
+			base.Isolate(isolate);
+
+			var intervalFetcher = new FakeIntervalLengthFetcher();
+			intervalFetcher.Has(15);
+			isolate.UseTestDouble(intervalFetcher).For<IIntervalLengthFetcher>();
+
+			isolate.UseTestDouble<FakeStardustJobFeedback>().For<IStardustJobFeedback>();
+			isolate.UseTestDouble<NoMessageSender>().For<IMessageSender>();
+			isolate.UseTestDouble<MutableNow>().For<INow>();
+		}
+
+		protected override void Startup(IComponentContext container)
+		{
+			base.Startup(container);
+			container.Resolve<IHangfireClientStarter>().Start();
+		}
+	}
+}
