355f65d0bbad08d5dbfb1ac307469706ccabcfb5
Roger Kratz <roger.kratz@gmail.com>
2019-02-19 14:09:27 +0100

    #81639 No ECMA6 code. Also reformatting the code.

--- Teleopti.Wfm.Administration/Scripts/App/monitor.js
+++ Teleopti.Wfm.Administration/Scripts/App/monitor.js
@@ -9,31 +9,31 @@
 		mounted() {
 			var vm = this;
 			axios.get('/monitor/list')
-				.then(monitorSteps => {
-				monitorSteps.data.forEach(function (step) {
-					vm.steps.push({
-						stepName: step,
-						stepSuccess : false,
-						output: 'Loading...'
+				.then(function (monitorSteps) {
+					monitorSteps.data.forEach(function (step) {
+						vm.steps.push({
+							stepName: step,
+							stepSuccess: false,
+							output: 'Loading...'
+						});
+						axios.get('/monitor/check/' + step)
+							.then(function (result) {
+								var stepToUse = vm.findStep(step);
+								stepToUse.output = result.data;
+								stepToUse.stepSuccess = true;
+							})
+							.catch(function (error) {
+								var stepToUse = vm.findStep(step);
+								stepToUse.output = error.response.data;
+							})
 					});
-					axios.get('/monitor/check/' + step)
-						.then(result => {
-						var stepToUse = vm.findStep(step);
-					stepToUse.output = result.data;
-					stepToUse.stepSuccess = true;
-				})
-				.catch(error => {
-						var stepToUse = vm.findStep(step);
-					stepToUse.output = error.response.data;
-				})
 				});
-		});
 		},
-		methods : {
-			outputColor : function(value){
+		methods: {
+			outputColor: function (value) {
 				return value ? 'stepSuccess' : 'stepFailure';
 			},
-			findStep : function(stepName){
+			findStep: function (stepName) {
 				return this.steps.find(function (stepToFind) {
 					return stepToFind.stepName === stepName;
 				});
