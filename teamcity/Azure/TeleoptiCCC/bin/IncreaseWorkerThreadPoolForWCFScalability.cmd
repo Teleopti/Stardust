9b313bfcbe215495fbb8cd2e691c675263ea8cb4
Robin Karlsson <robin.karlsson@teleopti.com>
2019-02-22 09:45:45 +0100

    #81813, better default values

--- teamcity/Azure/TeleoptiCCC/bin/IncreaseWorkerThreadPoolForWCFScalability.cmd
+++ teamcity/Azure/TeleoptiCCC/bin/IncreaseWorkerThreadPoolForWCFScalability.cmd
@@ -3,13 +3,13 @@ REM Uses the recommended values from http://msdn.microsoft.com/en-us/library/ms9
 REM Values may be subject to change depending on your needs
 REM Assumes you're running on two cores (medium instance on Azure)
  
-%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxWorkerThreads:600 /clr:4 >> log.txt 2>> err.txt
-%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minWorkerThreads:100 /clr:4 >> log.txt 2>> err.txt
-%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minIoThreads:100 /clr:4 >> log.txt 2>> err.txt
-%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxIoThreads:600 /clr:4 >> log.txt 2>> err.txt
+%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxWorkerThreads:100 /clr:4 >> log.txt 2>> err.txt
+%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minWorkerThreads:50 /clr:4 >> log.txt 2>> err.txt
+%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minIoThreads:50 /clr:4 >> log.txt 2>> err.txt
+%windir%\system32\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxIoThreads:100 /clr:4 >> log.txt 2>> err.txt
 
 
-%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxWorkerThreads:600 /clr:4 >> log.txt 2>> err.txt
-%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minWorkerThreads:100 /clr:4 >> log.txt 2>> err.txt
-%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minIoThreads:100 /clr:4 >> log.txt 2>> err.txt
-%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxIoThreads:600 /clr:4 >> log.txt 2>> err.txt
+%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxWorkerThreads:100 /clr:4 >> log.txt 2>> err.txt
+%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minWorkerThreads:50 /clr:4 >> log.txt 2>> err.txt
+%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -minIoThreads:50 /clr:4 >> log.txt 2>> err.txt
+%windir%\SysWOW64\inetsrv\appcmd set config /commit:MACHINE -section:processModel -maxIoThreads:100 /clr:4 >> log.txt 2>> err.txt
