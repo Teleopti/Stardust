15820726bb4621639f9f5ccb71de04c21ccd84be
Karin Jeppsson <karin.jeppsson@teleopti.com>
2019-02-25 16:23:24 +0100

    Revert "#BUG 81507 PM reports are slow"
    This reverts commit 4cabf18e61d9101f5a8f88b7593fbb8ca1881832.
    
    Revert "#BUG 81507 PM reports are slow"

--- Cube/build/1-BuildXMLA.bat
+++ Cube/build/1-BuildXMLA.bat
@@ -8,7 +8,8 @@ SET ROOTDIR=%ROOTDIR:~0,-1%
 SET msbuildproj=%ROOTDIR%\BuildXMLA.msbuild
 SET CCNetWorkingDirectory=%ROOTDIR%\..\..
 
-"%ROOTDIR%\..\..\.nuget\NuGet.exe" install packages.config -o ..\..\packages -source "http://hestia/nuget";"https://nuget.org/api/v2
+CD "%ROOTDIR%\..\..\.nuget"
+NuGet.exe install packages.config -o ..\packages -source "http://hestia/nuget";"https://nuget.org/api/v2
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 %msbuild% "%msbuildproj%" /t:BuildXMLA
 
