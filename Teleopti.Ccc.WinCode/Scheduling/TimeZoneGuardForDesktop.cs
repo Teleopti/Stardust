066cd620bd57dec3a635154f1885fdb30e088568
Roger Kratz <roger.kratz@gmail.com>
2019-02-26 16:02:39 +0100

    #81299 No need to keep pragma disables

--- Teleopti.Ccc.WinCode/Scheduling/TimeZoneGuardForDesktop.cs
+++ Teleopti.Ccc.WinCode/Scheduling/TimeZoneGuardForDesktop.cs
@@ -4,10 +4,8 @@ namespace Teleopti.Ccc.WinCode.Scheduling
 {
 	public static class TimeZoneGuardForDesktop
 	{
-#pragma warning disable 618
 		//Use ITimeZoneGuard or field in schedulingscreen instead
 		public static ITimeZoneGuard Instance_DONTUSE { get; private set; }
-#pragma warning restore 618
 
 		public static void Set(ITimeZoneGuard timeZoneGuard)
 		{
