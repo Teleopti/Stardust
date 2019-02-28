42b72db1bad30e47d8f5d6cb6dc4201847ff5b9a
Asad Mirza <asad.mirza@live.com>
2019-02-28 14:57:55 +0100

    minor refactoring

--- Node/NodeTest/NodeControllerTests.cs
+++ Node/NodeTest/NodeControllerTests.cs
@@ -437,39 +437,4 @@ namespace NodeTest
 		}
 
 	}
-
-	public class MutableNow : INow, IMutateNow
-	{
-		private DateTime? _mutatedUtc;
-
-		public MutableNow()
-		{
-		}
-
-		public MutableNow(DateTime utc)
-		{
-			Is(utc);
-		}
-
-		public DateTime UtcDateTime()
-		{
-			return _mutatedUtc ?? DateTime.UtcNow;
-		}
-
-		public void Reset()
-		{
-			_mutatedUtc = null;
-		}
-
-		public virtual void Is(DateTime? utc)
-		{
-			_mutatedUtc = utc;
-		}
-
-		public bool IsMutated()
-		{
-			return _mutatedUtc.HasValue;
-		}
-
-	}
 }
