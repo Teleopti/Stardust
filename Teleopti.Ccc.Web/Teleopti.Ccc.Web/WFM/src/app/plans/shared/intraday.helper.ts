eaca3384288ce6ebb1216c1a5322ea49ebccbd7e
Oskar Ekelius <oskar.ekelius@teleopti.com>
2019-03-04 10:31:20 +0100

    #79113 Adjusted how we identify a critical interval

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/plans/shared/intraday.helper.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/plans/shared/intraday.helper.ts
@@ -5,6 +5,8 @@ export class IntradayHelper {
 	constructor() {
 	}
 	public static isCritical(interval, dayAverage: number, dayRelativeDifference: number) {
-		return (interval.f - interval.s > 0.4 * dayAverage) && (interval.f > 0.5) && (dayRelativeDifference - (interval.s - interval.f)/interval.f > 0.7);
+		
+		return ((interval.f - interval.s)/dayAverage > 0.5 ) && (interval.f > 0.5) && (dayRelativeDifference>0?0:dayRelativeDifference - (interval.s - interval.f)/interval.f > 0.4);
+
 	}
 }
