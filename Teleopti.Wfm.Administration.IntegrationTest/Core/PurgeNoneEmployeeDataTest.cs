09d8f1e5a8c86beb61667b20920e0fa0ea999007
Magnus Wedmark <magnus.wedmark@teleopti.com>
2019-03-04 11:17:24 +0100

    Revert "Story #77460 Changed from using MonthsToKeepPersonalData setting from DB to using static value of 7 days in code"
    This reverts commit 1d7afa7be7997b8335a37f382dc3437be1f2ccf7.

--- Teleopti.Wfm.Administration.IntegrationTest/Core/PurgeNoneEmployeeDataTest.cs
+++ Teleopti.Wfm.Administration.IntegrationTest/Core/PurgeNoneEmployeeDataTest.cs
@@ -85,6 +85,9 @@ namespace Teleopti.Wfm.Administration.IntegrationTest.Core
 			{
 				var allTenants = LoadAllTenants.Tenants();
 				allTenants.ForEach(createPersons);
+				
+				updateSetting(allTenants.First().DataSourceConfiguration.ApplicationConnectionString, 1);
+				updateSetting(allTenants.Last().DataSourceConfiguration.ApplicationConnectionString, 2);
 			}
 
 			Target.Purge();
@@ -92,7 +95,7 @@ namespace Teleopti.Wfm.Administration.IntegrationTest.Core
 			using (uow.EnsureUnitOfWorkIsStarted())
 			{
 				var personInfos = loadPersonFromTenant(uow.CurrentSession());
-				personInfos.Count.Should().Be.EqualTo(20);
+				personInfos.Count.Should().Be.EqualTo(9);
 			}
 		}
 
@@ -203,17 +206,14 @@ namespace Teleopti.Wfm.Administration.IntegrationTest.Core
 				personId = Guid.NewGuid();
 				createPersonInTenant(connection, personId, Now.UtcDateTime().AddMonths(1));
 				persistPersonInfo(tenant, personId, 85);
-				
-				personId = Guid.NewGuid();
-				createPersonInTenant(connection, personId, Now.UtcDateTime().AddMonths(-3));
-				persistPersonInfo(tenant, personId, 86);
 
-				foreach (var i in Enumerable.Range(0,10))
+				foreach (var i in Enumerable.Range(0,5))
 				{
 					personId = Guid.NewGuid();
 
-					createPersonInTenant(connection, personId, Now.UtcDateTime().AddDays(-i));
+					createPersonInTenant(connection, personId, Now.UtcDateTime().AddMonths(-i));
 					persistPersonInfo(tenant,personId,i);
+
 				}
 			}
 		}
