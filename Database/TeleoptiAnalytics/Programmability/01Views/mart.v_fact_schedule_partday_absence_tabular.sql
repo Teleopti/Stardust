e53d0356ae4dce78cfbfee3b87b8ba8e83708a1b
Karin Jeppsson <karin.jeppsson@teleopti.com>
2019-03-04 12:16:31 +0100

    #81494 Insights view for part day absence

---
+++ Database/TeleoptiAnalytics/Programmability/01Views/mart.v_fact_schedule_partday_absence_tabular.sql
@@ -0,0 +1,19 @@
+DROP VIEW [mart].[v_fact_schedule_partday_absence_tabular]
+GO
+
+CREATE VIEW [mart].[v_fact_schedule_partday_absence_tabular]
+AS
+SELECT f.shift_startdate_local_id,f.person_id,f.scenario_id,f.absence_id,f.business_unit_id,sum([scheduled_contract_time_absence_m]) as 'scheduled_contract_time_absence_m',1 as 'part_day_absence_count'
+FROM mart.fact_schedule f
+LEFT JOIN  mart.fact_schedule_day_count dc on 
+		f.shift_startdate_local_id=dc.shift_startdate_local_id 
+		AND f.person_id=dc.person_id 
+		AND f.scenario_id=dc.scenario_id
+		AND f.absence_id=dc.absence_id
+WHERE f.absence_id<>-1 
+AND dc.shift_startdate_local_id IS NULL
+GROUP BY f.shift_startdate_local_id,f.person_id,f.scenario_id,f.absence_id ,f.business_unit_id
+
+GO
+
+
