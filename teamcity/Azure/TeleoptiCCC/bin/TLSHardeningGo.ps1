a77404ff66ecde849a37c97ae18f597e660e9073
Jimmy Broman <jimmy.broman@teleopti.com>
2019-03-05 15:21:30 +0100

    Testing to breakout Azure startup task to blob instead of having in repo. Do not release this version

--- teamcity/Azure/TeleoptiCCC/bin/TLSHardeningGo.ps1
+++
@@ -1,5 +0,0 @@
-﻿.\TLSHardeningScript.ps1 -Proto TLS10 -Target Client -Action Disable -Verbose
-.\TLSHardeningScript.ps1 -Proto TLS10 -Target Server -Action Disable -Verbose
-
-.\TLSHardeningScript.ps1 -Proto TLS11 -Target Client -Action Disable -Verbose
-.\TLSHardeningScript.ps1 -Proto TLS11 -Target Server -Action Disable -Verbose
