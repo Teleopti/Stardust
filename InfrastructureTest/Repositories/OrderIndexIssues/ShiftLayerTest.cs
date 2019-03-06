dcb627c8bde25b90071fa82dca28dadf52ceaff6
Roger Kratz <roger.kratz@gmail.com>
2019-03-06 11:38:34 +0100

    #81337 Better test

--- InfrastructureTest/Repositories/OrderIndexIssues/ShiftLayerTest.cs
+++ InfrastructureTest/Repositories/OrderIndexIssues/ShiftLayerTest.cs
@@ -4,36 +4,54 @@ using NUnit.Framework;
 using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Infrastructure;
+using Teleopti.Ccc.Domain.Scheduling;
 using Teleopti.Ccc.Domain.Scheduling.Assignment;
-using Teleopti.Ccc.Domain.Security.Principal;
-using Teleopti.Ccc.Infrastructure.Repositories;
 using Teleopti.Ccc.Infrastructure.UnitOfWork;
-using Teleopti.Ccc.InfrastructureTest.Repositories.Audit;
 using Teleopti.Ccc.TestCommon;
 
 namespace Teleopti.Ccc.InfrastructureTest.Repositories.OrderIndexIssues
 {
 	[Ignore("Same strack trace as the old order index bug - related to #81337")]
-	public class ShiftLayerTest : AuditTest //audittest is not needed but easier setup that way
+	[DatabaseTest]
+	public class ShiftLayerTest
 	{
+		public IPersonAssignmentRepository PersonAssignmentRepository;
+		public ICurrentUnitOfWorkFactory CurrentUnitOfWorkFactory;
+		
 		[Test]
 		public void MakesNormalSchedulesGoBanana()
 		{
+			var agent = new Person().InTimeZone(TimeZoneInfo.Utc);
+			var scenario = new Scenario();
+			var activity = new Activity("_");
+			var personAssignment = new PersonAssignment(agent, scenario, new DateOnly(2000, 1, 1));
+			
+			using (var uow = UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
+			{
+				var session = uow.FetchSession();
+				session.Save(agent);
+				session.Save(scenario);
+				session.Save(activity);
+				session.Save(personAssignment);
+				
+				uow.PersistAll();
+			}
+			
 			using (var uow =UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
 			{
-				PersonAssignment.AddActivity(PersonAssignment.ShiftLayers.First().Payload, new TimePeriod(10, 12));
+				personAssignment.AddActivity(activity, new TimePeriod(10, 12));
 				//uown känner inte till assignment, bara lager. När händer detta?
-				PersonAssignment.ShiftLayers.ForEach(x => uow.FetchSession().Merge<ShiftLayer>(x));
+				personAssignment.ShiftLayers.ForEach(x => uow.FetchSession().Merge(x));
 				///////////////////////////////////////////
 				uow.PersistAll();
 			}
 			
 			using (UnitOfWorkFactory.Current.CreateAndOpenUnitOfWork())
 			{
-				var repo = new PersonAssignmentRepository(CurrentUnitOfWork.Make(), CurrentBusinessUnit.Make(), new Lazy<IUpdatedBy>(UpdatedBy.Make));
 				Assert.DoesNotThrow(() =>
 				{
-					repo.Get(PersonAssignment.Id.Value).ShiftLayers.Count();
+					PersonAssignmentRepository.Get(personAssignment.Id.Value).ShiftLayers.Count();
 				});
 			}
 		}
