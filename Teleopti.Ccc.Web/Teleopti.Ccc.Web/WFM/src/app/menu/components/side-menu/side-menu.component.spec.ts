da86dca92c1c4bb1ee2c58fad86afb8f45b63fd0
Qiong Liang <Qiong.Liang@teleopti.com>
2019-03-08 01:08:15 +0000

    Merged PR 1973: bug #81705: change wfmAreasWithPermissions to be observable
    bug #81705: change wfmAreasWithPermissions to be observable
    
    Related work items: #81705

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/menu/components/side-menu/side-menu.component.spec.ts
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/WFM/src/app/menu/components/side-menu/side-menu.component.spec.ts
@@ -1,20 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { configureTestSuite, PageObject } from '@wfm/test';
 import { IStateService } from 'angular-ui-router';
-import { Observable, of, ReplaySubject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 import { MediaQueryService } from 'src/app/browser/services/media-query.service';
 import { Area, AreaService } from '../../shared/area.service';
 import { ToggleMenuService } from '../../shared/toggle-menu.service';
 import { SideMenuComponent } from './side-menu.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 const areaOne: Area = { InternalName: 'people' };
 const areaTwo: Area = { InternalName: 'permissions' };
-
-class MockAreaService implements Partial<AreaService> {
-	getAreas(): Observable<Area[]> {
-		return of([areaOne, areaTwo]);
-	}
-}
 
 class MockToggleMenuService implements Partial<ToggleMenuService> {
 	public get showMenu$() {
@@ -34,6 +29,12 @@ class MockMediaQueryService implements Partial<MediaQueryService> {
 const mockStateService: Partial<IStateService> = {
 	current: {
 		name: areaOne.InternalName
+	},
+	href() {
+		return '';
+	},
+	get() {
+		return this.current.name;
 	}
 };
 
@@ -41,21 +42,22 @@ describe('SideMenuComponent', () => {
 	let component: SideMenuComponent;
 	let fixture: ComponentFixture<SideMenuComponent>;
 	let page: Page;
+	let httpMock: HttpTestingController;
 
 	configureTestSuite();
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [SideMenuComponent],
-			imports: [],
+			imports: [HttpClientTestingModule],
 			providers: [
 				{
 					provide: '$state',
 					useValue: mockStateService
 				},
-				{ provide: AreaService, useClass: MockAreaService },
 				{ provide: ToggleMenuService, useClass: MockToggleMenuService },
-				{ provide: MediaQueryService, useClass: MockMediaQueryService }
+				{ provide: MediaQueryService, useClass: MockMediaQueryService },
+				AreaService
 			]
 		}).compileComponents();
 	}));
@@ -64,6 +66,7 @@ describe('SideMenuComponent', () => {
 		fixture = TestBed.createComponent(SideMenuComponent);
 		component = fixture.componentInstance;
 		page = new Page(fixture);
+		httpMock = TestBed.get(HttpTestingController);
 	});
 
 	it('should create', () => {
@@ -71,11 +74,17 @@ describe('SideMenuComponent', () => {
 	});
 
 	it('should display areas', () => {
+		const areas = httpMock.match('../api/Global/Application/WfmAreasWithPermission')[0];
+		areas.flush([areaOne, areaTwo]);
+
 		fixture.detectChanges();
 		expect(page.menuItems.length).toEqual(2);
 	});
 
 	it('should hightlight active area', () => {
+		const areas = httpMock.match('../api/Global/Application/WfmAreasWithPermission')[0];
+		areas.flush([areaOne, areaTwo]);
+
 		fixture.detectChanges();
 		expect(page.menuItems[0].classes['main-menu-link-active']).toBeTruthy();
 	});
