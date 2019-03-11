33acb8508deb009b1129b361aee9ed4fff70014f
Chundan Xu <Chundan.Xu@teleopti.com>
2019-03-11 02:40:03 +0000

    Merged PR 1993: bug #81825: AlwaysOnTopASM doesn't work with HTTPS
    bug #81825: AlwaysOnTopASM dosn't work with HTTPS
    
    Related work items: #81825

--- Teleopti.Runtime.Environment/Teleopti.Runtime.Environment/Program.cs
+++ Teleopti.Runtime.Environment/Teleopti.Runtime.Environment/Program.cs
@@ -20,6 +20,16 @@ namespace Teleopti.Runtime.Environment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+			try
+			{
+				ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072;
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message, @"Error");
+			}
+			
+
 			var client = new WebClient();
 	        client.Credentials = CredentialCache.DefaultNetworkCredentials;
             var starturl = ConfigurationManager.AppSettings["url"];
