678a6e534c26e64e731fe4965520207262af153e
Magnus Ekström <magnus.ekstrom@teleopti.com>
2019-03-12 15:03:49 +0100

    #80915 - New repo to get schedule changes from scheduleaudittral.

---
+++ Domain/Scheduling/PersonDayAbsenceProjectionChanged.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
+
+namespace Teleopti.Ccc.Domain.Scheduling
+{
+	public class PersonDayAbsenceProjectionChanged
+	{
+		public PersonDayAbsenceProjectionChanged() { }
+
+		public Guid PersonId { get; set; }
+		public DateTimePeriod Period { get; set; }
+
+		public PersonDayProjectionChanged Convert(TimeZoneInfo timeZone)
+		{
+			var timeZonePeriod = Period.ToDateOnlyPeriod(timeZone);
+			return new PersonDayProjectionChanged(PersonId, timeZonePeriod.StartDate, timeZonePeriod.EndDate);
+		}
+	}
+}
