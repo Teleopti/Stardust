91da58b36acb5c39d9ba922d36e44f4efe894cc0
Roger Kratz <roger.kratz@gmail.com>
2019-03-13 17:20:24 +0100

    #82154 Do not care about coverage now

--- Teleopti.Wfm.Administration/Tests/runTests.bat
+++ Teleopti.Wfm.Administration/Tests/runTests.bat
@@ -7,4 +7,3 @@ Set repoRoot=%THIS:~0,-35%
 %repoRoot%\.nuget\nuget.exe install chutzpah -Version 4.4.4 -o %repoRoot%\packages
 
 %repoRoot%\packages\Chutzpah.4.4.4\tools\chutzpah.console.exe /path %THIS%
-::%repoRoot%\packages\
