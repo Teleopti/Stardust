dfc1741cc101937bece12fbc52bca33b2936206b
Roger Kratz <roger.kratz@gmail.com>
2019-03-13 13:09:02 +0100

    #82154 Adding runTests.bat to run tests locally and on TC

---
+++ Teleopti.Wfm.Administration/Tests/runTests.bat
@@ -0,0 +1,10 @@
+@ECHO off
+
+SET THIS=%~dp0
+Set repoRoot=%THIS:~0,-35%
+
+
+%repoRoot%\.nuget\nuget.exe install chutzpah -Version 4.4.4 -o %repoRoot%\packages
+
+%repoRoot%\packages\Chutzpah.4.4.4\tools\chutzpah.console.exe /path %THIS%
+::%repoRoot%\packages\
