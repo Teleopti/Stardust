5c45cf59852ef0405bbca24656557e441cd40363
Roger Kratz <roger.kratz@gmail.com>
2019-03-13 10:06:45 +0100

    #82154 Adding jasmine test html page + first dummy tests

---
+++ Teleopti.Wfm.Administration/Status/statusController.spec.js
@@ -0,0 +1,9 @@
+describe("statusController", function() {
+	it("should succeed", function() {
+		expect(true).toBe(true);
+	});
+	it("should fail", function() {
+		expect(false).toBe(true);
+	});
+
+});
