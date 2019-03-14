34bb3ccecc37c28e1c92623ae11482888c1f3801
Jianfeng Zhu <jianfeng.zhu@teleopti.com>
2019-03-14 10:59:11 +0800

    formatting - to trigger build

--- Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.ChooseHistoryViewModel.Tests.js
+++ Teleopti.Ccc.Web/Teleopti.Ccc.Web/Areas/MyTime/Content/Scripts/Teleopti.MyTimeWeb.Request.ChooseHistoryViewModel.Tests.js
@@ -1,24 +1,32 @@
 ﻿$(document).ready(function() {
+	module('Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel');
 
-	module("Teleopti.MyTimeWeb.Request.ShiftTradeRequestDetailViewModel");
-
-	test("Should use meridian correctly when calculating timeline", function() {
-
+	test('Should use meridian correctly when calculating timeline', function() {
 		var mySchedule = {
 				isDayOff: false,
-				scheduleStartTime: function() { return moment('2015-01-01 21:00:00') },
-				scheduleEndTime: function() { return moment('2015-01-01 23:00:00') }
+				scheduleStartTime: function() {
+					return moment('2015-01-01 21:00:00');
+				},
+				scheduleEndTime: function() {
+					return moment('2015-01-01 23:00:00');
+				}
 			},
 			tradedSchedule = {
 				isDayOff: false,
-				scheduleStartTime: function() { return moment('2015-01-01 7:00:00') },
-				scheduleEndTime: function() { return moment('2015-01-01 19:00:00') }
-			};		
+				scheduleStartTime: function() {
+					return moment('2015-01-01 7:00:00');
+				},
+				scheduleEndTime: function() {
+					return moment('2015-01-01 19:00:00');
+				}
+			};
 
-		var result = Teleopti.MyTimeWeb.Request.ChooseHistoryViewModelUtil.findScheduleTimeRange(mySchedule, tradedSchedule);
+		var result = Teleopti.MyTimeWeb.Request.ChooseHistoryViewModelUtil.findScheduleTimeRange(
+			mySchedule,
+			tradedSchedule
+		);
 
-		equal(result.scheduleStartTime.format('HH:mm'), "07:00");
-		equal(result.scheduleEndTime.format('HH:mm'), "23:00");
+		equal(result.scheduleStartTime.format('HH:mm'), '07:00');
+		equal(result.scheduleEndTime.format('HH:mm'), '23:00');
 	});
-
 });
