c7310d52eee22cafc50cbcf85d9675136c493f0d
Magnus Wedmark <magnus.wedmark@teleopti.com>
2019-03-15 15:58:45 +0100

    Added missing Fake

---
+++ Node/NodeTest/Fakes/WorkerWrapperServiceFake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Stardust.Node;
+using Stardust.Node.Interfaces;
+using Stardust.Node.Workers;
+
+namespace NodeTest.Fakes
+{
+    public class WorkerWrapperServiceFake : WorkerWrapperService
+    {
+
+        public IWorkerWrapper WorkerWrapper;
+
+        public WorkerWrapperServiceFake(ILifetimeScope componentContext = null,
+            NodeConfigurationService nodeConfigurationService = null) : base(null,null )
+        {
+
+        }
+
+
+        public override IWorkerWrapper GetWorkerWrapperByPort(int port)
+        {
+            return WorkerWrapper;
+        }
+
+
+
+    }
+}
