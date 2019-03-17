1e5e5102aae33f112c5c1fefaa9961b98126cd27
Mikael Deigård <micke@teleopti.com>
2019-03-17 18:12:12 +0100

    And the ... file

---
+++ Teleopti.Ccc.SmartClientPortal/Teleopti.Ccc.SmartClientPortal.Shell/Win/Sikuli/SikuliCreateInstanceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teleopti.Ccc.SmartClientPortal.Shell.Win.Sikuli
+{
+	public class SikuliCreateInstanceException : Exception
+	{
+		public SikuliCreateInstanceException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+	}
+}
