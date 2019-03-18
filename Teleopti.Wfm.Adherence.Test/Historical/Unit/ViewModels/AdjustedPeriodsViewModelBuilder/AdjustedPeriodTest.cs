b0532f961c634b395debd6713fe9736516873c3f
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2019-03-18 10:38:34 +0100

    ##82147 Removing adjustments from adjust view when cancelled

--- Teleopti.Wfm.Adherence.Test/Historical/Unit/ViewModels/AdjustedPeriodsViewModelBuilder/AdjustedPeriodTest.cs
+++ Teleopti.Wfm.Adherence.Test/Historical/Unit/ViewModels/AdjustedPeriodsViewModelBuilder/AdjustedPeriodTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using NUnit.Framework;
 using SharpTestsEx;
+using Teleopti.Ccc.Domain.Collection;
 using Teleopti.Ccc.Domain.Common.Time;
 using Teleopti.Ccc.TestCommon;
 using Teleopti.Ccc.TestCommon.FakeRepositories;
@@ -20,34 +21,49 @@ namespace Teleopti.Wfm.Adherence.Test.Historical.Unit.ViewModels.AdjustedPeriods
 
 		[Test]
 		public void ShouldGetEmptyModel()
-		{			
+		{
 			Target.Build()
-				.Should().Have.SameValuesAs(Enumerable.Empty<AdjustedPeriodViewModel>());		
-		}		
-				
+				.Should().Have.SameValuesAs(Enumerable.Empty<AdjustedPeriodViewModel>());
+		}
+
 		[Test]
 		public void ShouldBuild()
 		{
 			History.AdjustedAdherenceToNeutral("2019-03-05 12:00", "2019-03-05 14:00");
-			
+
 			var data = Target.Build();
 
 			data.Single().StartTime.Should().Be("2019-03-05 12:00");
 			data.Single().EndTime.Should().Be("2019-03-05 14:00");
 		}
-		
+
+		[Test]
+		public void ShouldBuildTwo()
+		{
+			History
+				.AdjustedAdherenceToNeutral("2019-03-05 12:00", "2019-03-05 14:00")
+				.AdjustedAdherenceToNeutral("2019-03-05 15:00", "2019-03-05 16:00");
+
+			var data = Target.Build();
+
+			data.First().StartTime.Should().Be("2019-03-05 12:00");
+			data.First().EndTime.Should().Be("2019-03-05 14:00");
+			data.Second().StartTime.Should().Be("2019-03-05 15:00");
+			data.Second().EndTime.Should().Be("2019-03-05 16:00");
+		}
+
 		[Test]
 		public void ShouldBuildOnDifferentDay()
 		{
 			History
 				.AdjustedAdherenceToNeutral("2019-03-04 12:00", "2019-03-04 14:00");
-			
+
 			var data = Target.Build();
 
 			data.Single().StartTime.Should().Be("2019-03-04 12:00");
 			data.Single().EndTime.Should().Be("2019-03-04 14:00");
 		}
-		
+
 		[Test]
 		public void ShouldBuildWithSwedenTimeZone()
 		{
@@ -56,11 +72,68 @@ namespace Teleopti.Wfm.Adherence.Test.Historical.Unit.ViewModels.AdjustedPeriods
 			History.AdjustedAdherenceToNeutral("2019-03-05 12:00", "2019-03-05 14:00");
 
 			var data = Target.Build();
-			
+
 			data.Single().StartTime.Should().Be("2019-03-05 13:00");
 			data.Single().EndTime.Should().Be("2019-03-05 15:00");
+		}
+
+		[Test]
+		public void ShouldRemoveAdjustment()
+		{
+			History
+				.AdjustedAdherenceToNeutral("2019-03-18 10:00", "2019-03-18 11:00")
+				.AdjustedAdherenceToNeutral("2019-03-18 12:00", "2019-03-18 14:00")
+				.CanceledAdjustment("2019-03-18 12:00", "2019-03-18 14:00");
+
+			var data = Target.Build();
+
+			data.Single().StartTime.Should().Be("2019-03-18 10:00");
+			data.Single().EndTime.Should().Be("2019-03-18 11:00");
+		}
+
+		[Test]
+		public void ShouldRemoveTwoAdjustments()
+		{
+			History
+				.AdjustedAdherenceToNeutral("2019-03-18 10:00", "2019-03-18 11:00")
+				.AdjustedAdherenceToNeutral("2019-03-18 12:00", "2019-03-18 14:00")
+				.AdjustedAdherenceToNeutral("2019-03-18 15:00", "2019-03-18 16:00")
+				.CanceledAdjustment("2019-03-18 12:00", "2019-03-18 14:00")
+				.CanceledAdjustment("2019-03-18 15:00", "2019-03-18 16:00");
+
+			var data = Target.Build();
+
+			data.Single().StartTime.Should().Be("2019-03-18 10:00");
+			data.Single().EndTime.Should().Be("2019-03-18 11:00");
+		}
+		
+		
+		[Test]
+		public void ShouldRemoveFirstAdjustment()
+		{
+			History
+				.AdjustedAdherenceToNeutral("2019-03-18 10:00", "2019-03-18 11:00")
+				.AdjustedAdherenceToNeutral("2019-03-18 12:00", "2019-03-18 14:00")
+				.CanceledAdjustment("2019-03-18 10:00", "2019-03-18 11:00");
+
+			var data = Target.Build();
+
+			data.Single().StartTime.Should().Be("2019-03-18 12:00");
+			data.Single().EndTime.Should().Be("2019-03-18 14:00");
 		}		
 		
-		
+		[Test]
+		public void ShouldRemoveOnlyOneAdjustment()
+		{
+			History
+				.AdjustedAdherenceToNeutral("2019-03-18 10:00", "2019-03-18 11:00")
+				.AdjustedAdherenceToNeutral("2019-03-18 10:00", "2019-03-18 11:00")
+				.CanceledAdjustment("2019-03-18 10:00", "2019-03-18 11:00");
+
+			var data = Target.Build();
+
+			data.Single().StartTime.Should().Be("2019-03-18 10:00");
+			data.Single().EndTime.Should().Be("2019-03-18 11:00");
+		}
 	}
 }
--- Teleopti.Wfm.Adherence/Historical/AdjustedPeriodsViewModelBuilder.cs
+++ Teleopti.Wfm.Adherence/Historical/AdjustedPeriodsViewModelBuilder.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using NHibernate.Util;
+using Teleopti.Ccc.Domain.Collection;
+using Teleopti.Ccc.Domain.Common;
 using Teleopti.Ccc.Domain.InterfaceLegacy.Domain;
 using Teleopti.Wfm.Adherence.Historical.Events;
 using Teleopti.Wfm.Adherence.Historical.Infrastructure;
@@ -20,22 +23,34 @@ namespace Teleopti.Wfm.Adherence.Historical
 
 		public IEnumerable<AdjustedPeriodViewModel> Build()
 		{
-			var events = _eventStore.LoadOfTypeForPeriod<PeriodAdjustedToNeutralEvent>
+			var adjustments = _eventStore.LoadOfTypeForPeriod<PeriodAdjustedToNeutralEvent>
+				(
+					new DateTimePeriod(DateTime.MinValue, DateTime.MaxValue)
+				)
+				.Cast<IRtaStoredEvent>()
+				.Select(x => new OpenPeriod(x.QueryData().StartTime, x.QueryData().EndTime))
+				.ToList();
+			
+			var cancellations = _eventStore.LoadOfTypeForPeriod<PeriodAdjustmentToNeutralCanceledEvent>
 			(
 				new DateTimePeriod(DateTime.MinValue, DateTime.MaxValue)
-			);
-
-			return events
+			)
 				.Cast<IRtaStoredEvent>()
+				.Select(x => new OpenPeriod(x.QueryData().StartTime, x.QueryData().EndTime))
+				.ToArray();
+
+			cancellations.ForEach(x => adjustments.Remove(x));
+			
+			return adjustments
 				.Select(e => new AdjustedPeriodViewModel
 					{
-						StartTime = formatForUser(e.QueryData().StartTime),
-						EndTime = formatForUser(e.QueryData().EndTime)
+						StartTime = formatForUser(e.StartTime),
+						EndTime = formatForUser(e.EndTime)
 					}
 				).ToArray();
 		}
 
-		private string formatForUser(DateTime? time) => 
+		private string formatForUser(DateTime? time) =>
 			TimeZoneInfo.ConvertTimeFromUtc(time.Value, _timezone.TimeZone()).ToString("yyyy-MM-dd HH\\:mm");
 	}
 }
